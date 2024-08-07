LibreOffice/core

commit 3aca57fb9c7c979d971cae6bb3ad73c6dc412685

TOX: Extract generation of tab stop attributes to its own class.
This allows us to pass through less parameters which are only used for
this functionality.

Conflicts:
	sw/inc/ToxTextGenerator.hxx
	sw/source/core/tox/ToxTextGenerator.cxx

Change-Id: I5a03cf8ec6d86d05f2a7f7771668585669c1e595
Reviewed-on: https://gerrit.libreoffice.org/9613
Reviewed-by: Caolán McNamara <caolanm@redhat.com>
Tested-by: Caolán McNamara <caolanm@redhat.com>

sw/inc/ToxTabStopTokenHandler.hxx
@@ -1,0 +1,114 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+
+#ifndef TOXTABSTOPTOKENHANDLER_HXX_
+#define TOXTABSTOPTOKENHANDLER_HXX_
+
+#include "swdllapi.h"
+#include "rtl/ustring.hxx"
+
+#include "editeng/tstpitem.hxx"
+
+class SwFormToken;
+class SwPageDesc;
+class SwRootFrm;
+class SwTxtNode;
+
+namespace sw {
+
+/** This class handles tab stop tokens in the pattern for tox entries.
+ *
+ * @internal
+ * This is an interface class. It allows to mock the class in unit tests
+ */
+class SW_DLLPUBLIC ToxTabStopTokenHandler
+{
+public:
+    virtual ~ToxTabStopTokenHandler() {}
+
+    /** Return value of HandleTabStopToken */
+    struct HandledTabStopToken {
+        OUString text;
+        SvxTabStop tabStop;
+    };
+
+    /** Handle a tab stop token.
+     *
+     * @returns A HandledTabStopToken. Make sure to append the text field to the text of the target node
+     *          and to provide the returned SvxTabStop to the attributes of the node.
+     */
+    virtual HandledTabStopToken
+    HandleTabStopToken(const SwFormToken& aToken, const SwTxtNode& targetNode,
+            const SwRootFrm *currentLayout) const = 0;
+};
+
+/** The default implementation of ToxTabStopTokenHandler */
+class SW_DLLPUBLIC DefaultToxTabStopTokenHandler : public ToxTabStopTokenHandler
+{
+public:
+
+    enum TabStopReferencePolicy {TABSTOPS_RELATIVE_TO_INDENT, TABSTOPS_RELATIVE_TO_PAGE};
+
+   /**
+    * @param indexOfSectionNode
+    * The index of the section node. It is needed to determine whether a page description was given by a node
+    * before the tox section.
+    *
+    * @param defaultPageDescription
+    * Which page description shall be used if we do not find one or the found page description was provided by
+    * a node before the tox section
+    *
+    * @param tabPositionIsRelativeToParagraphIndent
+    * Whether the tab position is relative to the paragraph indent. (toxForm.IsRelTabPos() is true or false.)
+    *
+    * @param tabstopReferencePolicy
+    * How tab stops are positioned. (#i21237) The default behavior is to place tab stops relative to the page.
+    */
+    DefaultToxTabStopTokenHandler(sal_uInt32 indexOfSectionNode, const SwPageDesc& defaultPageDescription,
+            bool tabPositionIsRelativeToParagraphIndent,
+            TabStopReferencePolicy referencePolicy = TABSTOPS_RELATIVE_TO_PAGE);
+
+    /** Handle a tab stop token.
+     *
+     * If the token contains tab alignment information, that is used to create the SvxTabStop.
+     * Else, the information about the tab stop is taken from a page description.
+     * Depending on the TabStopReferencePolicy provided in the constructor, the
+     * method behaves differently when deriving the tab stop position.
+     */
+    ToxTabStopTokenHandler::HandledTabStopToken
+    HandleTabStopToken(const SwFormToken& aToken, const SwTxtNode& targetNode,
+            const SwRootFrm *currentLayout) const SAL_OVERRIDE;
+
+private:
+    /** Test whether the page layout can be obtained by a layout rectangle.
+     *
+     * Is used to determine how to find tab stop position.
+     */
+    static bool
+    CanUseLayoutRectangle(const SwTxtNode& targetNode, const SwRootFrm *currentLayout);
+
+    /** Calculate the page margin from the page description.
+     *
+     * This is the fallback method to determine the position of a tab stop.
+     */
+    long
+    CalculatePageMarginFromPageDescription(const SwTxtNode& targetNode) const;
+
+    sal_uInt32 mIndexOfSectionNode;
+    const SwPageDesc& mDefaultPageDescription;
+    bool mTabPositionIsRelativeToParagraphIndent;
+    TabStopReferencePolicy mTabStopReferencePolicy;
+};
+
+}
+
+#endif /* TOXTABSTOPTOKENHANDLER_HXX_ */
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sw/inc/ToxTextGenerator.hxx
@@ -38,19 +38,26 @@ class SwDoc;
 class SwForm;
 struct SwFormToken;
 class SwPageDesc;
+class SwRootFrm;
 class SwTxtAttr;
 class SwTxtNode;
 struct SwTOXSortTabBase;
+class SvxTabStop;
 class ToxTextGeneratorTest;
 
 namespace sw {
 
 class ToxLinkProcessor;
+class ToxTabStopTokenHandler;
 
+/** This class generates text for the entries of a table of x.
+ *
+ * You can control its behavior by calling @link SetTabstopPolicy() and specifying the desired behavior.
+ */
 class SW_DLLPUBLIC ToxTextGenerator
 {
 public:
-    ToxTextGenerator(const SwForm& toxForm);
+    ToxTextGenerator(const SwForm& toxForm, boost::shared_ptr<ToxTabStopTokenHandler> tabStopHandler);
 
     virtual ~ToxTextGenerator();
 
@@ -61,12 +68,12 @@ public:
      */
     void
     GenerateText(SwDoc *doc, const std::vector<SwTOXSortTabBase*>& entries,
-                      sal_uInt16 indexOfEntryToProcess, sal_uInt16 numberOfEntriesToProcess,
-                      sal_uInt32 _nTOXSectNdIdx, const SwPageDesc* _pDefaultPageDesc);
+                      sal_uInt16 indexOfEntryToProcess, sal_uInt16 numberOfEntriesToProcess);
 
 private:
     const SwForm& mToxForm;
     boost::shared_ptr<ToxLinkProcessor> mLinkProcessor;
+    boost::shared_ptr<ToxTabStopTokenHandler> mTabStopTokenHandler;
 
     /** A handled text token.
      * It contains the information which should be added to the target text node.

sw/source/core/doc/doctxm.cxx
@@ -65,6 +65,9 @@
 #include <scriptinfo.hxx>
 #include <switerator.hxx>
 #include <ToxTextGenerator.hxx>
+#include <ToxTabStopTokenHandler.hxx>
+
+#include <boost/make_shared.hpp>
 
 using namespace ::com::sun::star;
 
@@ -975,8 +978,15 @@ void SwTOXBaseSection::Update(const SfxItemSet* pAttr,
         // pass node index of table-of-content section and default page description
         // to method <GenerateText(..)>.
         ::SetProgressState( 0, pDoc->GetDocShell() );
-        sw::ToxTextGenerator ttgn(GetTOXForm());
-        ttgn.GenerateText((SwDoc*) GetFmt()->GetDoc(), aSortArr, nCnt, nRange, pSectNd->GetIndex(), pDefaultPageDesc);
+
+        boost::shared_ptr<sw::ToxTabStopTokenHandler> tabStopTokenHandler =
+                boost::make_shared<sw::DefaultToxTabStopTokenHandler>(
+                        pSectNd->GetIndex(), *pDefaultPageDesc, GetTOXForm().IsRelTabPos(),
+                        pDoc->GetDocumentSettingManager().get(IDocumentSettingAccess::TABS_RELATIVE_TO_INDENT) ?
+                                sw::DefaultToxTabStopTokenHandler::TABSTOPS_RELATIVE_TO_INDENT :
+                                sw::DefaultToxTabStopTokenHandler::TABSTOPS_RELATIVE_TO_PAGE);
+        sw::ToxTextGenerator ttgn(GetTOXForm(), tabStopTokenHandler);
+        ttgn.GenerateText((SwDoc*) GetFmt()->GetDoc(), aSortArr, nCnt, nRange);
         nCnt += nRange - 1;
     }
 

sw/source/core/tox/ToxTabStopTokenHandler.cxx
@@ -1,0 +1,126 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "ToxTabStopTokenHandler.hxx"
+
+#include "editeng/tstpitem.hxx"
+#include "editeng/lrspitem.hxx"
+
+#include "cntfrm.hxx"
+#include "fmtfsize.hxx"
+#include "fmtpdsc.hxx"
+#include "frmfmt.hxx"
+#include "ndtxt.hxx"
+#include "pagedesc.hxx"
+#include "pagefrm.hxx"
+#include "swrect.hxx"
+#include "tox.hxx"
+
+namespace sw {
+
+DefaultToxTabStopTokenHandler::DefaultToxTabStopTokenHandler(sal_uInt32 indexOfSectionNode,
+        const SwPageDesc& defaultPageDescription,
+        bool tabPositionIsRelativeToParagraphIndent,
+        TabStopReferencePolicy referencePolicy)
+: mIndexOfSectionNode(indexOfSectionNode),
+  mDefaultPageDescription(defaultPageDescription),
+  mTabPositionIsRelativeToParagraphIndent(tabPositionIsRelativeToParagraphIndent),
+  mTabStopReferencePolicy(referencePolicy)
+{
+}
+
+
+ToxTabStopTokenHandler::HandledTabStopToken
+DefaultToxTabStopTokenHandler::HandleTabStopToken(
+        const SwFormToken& aToken, const SwTxtNode& targetNode, const SwRootFrm *currentLayout) const
+{
+    HandledTabStopToken result;
+
+    if (aToken.bWithTab) { // #i21237#
+        result.text = "\t";
+    }
+
+    // check whether a tab adjustment has been specified.
+    if (SVX_TAB_ADJUST_END > aToken.eTabAlign) {
+        const SvxLRSpaceItem& rLR = (SvxLRSpaceItem&) targetNode.SwCntntNode::GetAttr(RES_LR_SPACE, true);
+
+        long nTabPosition = aToken.nTabStopPosition;
+        if (!mTabPositionIsRelativeToParagraphIndent && rLR.GetTxtLeft()) {
+            nTabPosition -= rLR.GetTxtLeft();
+        }
+        result.tabStop = SvxTabStop(nTabPosition, aToken.eTabAlign, cDfltDecimalChar, aToken.cTabFillChar);
+        return result;
+    }
+
+    SwRect aNdRect;
+    if (CanUseLayoutRectangle(targetNode, currentLayout)) {
+        aNdRect = targetNode.FindLayoutRect(true);
+    }
+    long nRightMargin;
+    if (aNdRect.IsEmpty()) {
+        nRightMargin = CalculatePageMarginFromPageDescription(targetNode);
+    } else {
+        nRightMargin = aNdRect.Width();
+    }
+    //#i24363# tab stops relative to indent
+    if (mTabStopReferencePolicy == TABSTOPS_RELATIVE_TO_INDENT) {
+        // left margin of paragraph style
+        const SvxLRSpaceItem& rLRSpace = targetNode.GetTxtColl()->GetLRSpace();
+        nRightMargin -= rLRSpace.GetLeft();
+        nRightMargin -= rLRSpace.GetTxtFirstLineOfst();
+    }
+
+    result.tabStop = SvxTabStop(nRightMargin, SVX_TAB_ADJUST_RIGHT, cDfltDecimalChar, aToken.cTabFillChar);
+    return result;
+}
+
+long
+DefaultToxTabStopTokenHandler::CalculatePageMarginFromPageDescription(const SwTxtNode& targetNode) const
+{
+    sal_uInt32 nPgDescNdIdx = targetNode.GetIndex() + 1;
+    const SwPageDesc *pPageDesc = targetNode.FindPageDesc(false, &nPgDescNdIdx);
+    if (!pPageDesc || nPgDescNdIdx < mIndexOfSectionNode) {
+        // Use default page description, if none is found or the found one is given by a Node before the
+        // table-of-content section.
+        pPageDesc = &mDefaultPageDescription;
+    }
+    const SwFrmFmt& rPgDscFmt = pPageDesc->GetMaster();
+    long result = rPgDscFmt.GetFrmSize().GetWidth() - rPgDscFmt.GetLRSpace().GetLeft()
+            - rPgDscFmt.GetLRSpace().GetRight();
+    return result;
+}
+
+
+/*static*/ bool
+DefaultToxTabStopTokenHandler::CanUseLayoutRectangle(const SwTxtNode& targetNode, const SwRootFrm *currentLayout)
+{
+    const SwPageDesc* pageDescription =
+            ((SwFmtPageDesc&) targetNode.SwCntntNode::GetAttr(RES_PAGEDESC)).GetPageDesc();
+
+    if (!pageDescription) {
+        return false;
+    }
+    const SwFrm* pFrm = targetNode.getLayoutFrm(currentLayout, 0, 0, true);
+    if (!pFrm) {
+        return false;
+    }
+    pFrm = pFrm->FindPageFrm();
+    if (!pFrm) {
+        return false;
+    }
+    const SwPageFrm* pageFrm = static_cast<const SwPageFrm*>(pFrm);
+    if (pageDescription != pageFrm->GetPageDesc()) {
+        return false;
+    }
+    return true;
+}
+
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 0633189fabe85f73062ff2ce67b5f40af7d3f504

tdf#162121 sw: fix tab stop position in columned ToX
There is a SvxTabAdjust::End but this only exists in the definition of
index entry templates, it is not possible to set this on a tab stop on a
text node.

So the ToxTabStopTokenHandler converts this into a SvxTabAdjust::Right
tab stop with a fixed position.

DefaultToxTabStopTokenHandler::CanUseLayoutRectangle() has inverted
condition but that doesn't matter because there are no layout frames at
that point.
(regression from commit 3aca57fb9c7c979d971cae6bb3ad73c6dc412685)

The main problem is that getting the with from the layout rectangle
never works because there's no layout frame, and the alternative only
takes into account the page dimensions and not any margins of the
section, columns, or gap between columns, not to mention that the ToX
could also be in a table etc.

Refactor this so that the tab stops are set only after all the layout
frames are created.

An unfixable problem remains in case the ToX has columns of different
width.

The wrong tab positions are problematic if TabOverMargin is enabled
since commit 10d753b8aadb50ec4309551b97d4cf2163ea3e3d

Change-Id: Ia712c9bf42b2518e396f1b9e7efd65869ebc5ab4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171585
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/inc/ToxTabStopTokenHandler.hxx
@@ -45,8 +45,11 @@ public:
      *          and to provide the returned SvxTabStop to the attributes of the node.
      */
     virtual HandledTabStopToken
-    HandleTabStopToken(const SwFormToken& aToken, const SwTextNode& targetNode,
-            const SwRootFrame *currentLayout) const = 0;
+    HandleTabStopToken(const SwFormToken& aToken, const SwTextNode& targetNode)
+        const = 0;
+
+    virtual auto CalcEndStop(SwTextNode const& rNode,
+            SwRootFrame const* pLayout) const -> tools::Long = 0;
 };
 
 /** The default implementation of ToxTabStopTokenHandler */
@@ -83,8 +86,11 @@ public:
      * method behaves differently when deriving the tab stop position.
      */
     ToxTabStopTokenHandler::HandledTabStopToken
-    HandleTabStopToken(const SwFormToken& aToken, const SwTextNode& targetNode,
-            const SwRootFrame *currentLayout) const override;
+    HandleTabStopToken(const SwFormToken& aToken, const SwTextNode& targetNode)
+        const override;
+
+    auto CalcEndStop(SwTextNode const& rNode,
+            SwRootFrame const* pLayout) const -> tools::Long override;
 
 private:
     /** Test whether the page layout can be obtained by a layout rectangle.

sw/inc/ToxTextGenerator.hxx
@@ -26,6 +26,7 @@
 #include "fmtautofmt.hxx"
 
 #include <memory>
+#include <optional>
 #include <vector>
 #include <unordered_map>
 
@@ -67,7 +68,7 @@ public:
      * This method will process the entries in @p entries, starting at @p indexOfEntryToProcess and
      * process @p numberOfEntriesToProcess entries.
      */
-    void
+    std::optional<std::pair<SwTextNode *, SvxTabStopItem>>
     GenerateText(SwDoc *doc,
         std::unordered_map<OUString, int> & rMarkURLs,
         const std::vector<std::unique_ptr<SwTOXSortTabBase>>& entries,

sw/source/core/doc/doctxm.cxx
@@ -1067,6 +1067,13 @@ void SwTOXBaseSection::Update(const SfxItemSet* pAttr,
     // Sort the List of all TOC Marks and TOC Sections
     std::vector<SwTextFormatColl*> aCollArr( GetTOXForm().GetFormMax(), nullptr );
     std::unordered_map<OUString, int> markURLs;
+    std::vector<std::pair<SwTextNode *, SvxTabStopItem>> tabStops;
+    std::shared_ptr<sw::ToxTabStopTokenHandler> const pTabStopTokenHandler =
+        std::make_shared<sw::DefaultToxTabStopTokenHandler>(
+            pSectNd->GetIndex(), *pDefaultPageDesc, GetTOXForm().IsRelTabPos(),
+            rDoc.GetDocumentSettingManager().get(DocumentSettingId::TABS_RELATIVE_TO_INDENT)
+                ? sw::DefaultToxTabStopTokenHandler::TABSTOPS_RELATIVE_TO_INDENT
+                : sw::DefaultToxTabStopTokenHandler::TABSTOPS_RELATIVE_TO_PAGE);
     SwNodeIndex aInsPos( *pFirstEmptyNd, 1 );
     for( size_t nCnt = 0; nCnt < m_aSortArr.size(); ++nCnt )
     {
@@ -1081,7 +1088,6 @@ void SwTOXBaseSection::Update(const SfxItemSet* pAttr,
             aCollArr[ nLvl ] = pColl;
         }
 
-        // Generate: Set dynamic TabStops
         SwTextNode* pTOXNd = rDoc.GetNodes().MakeTextNode( aInsPos.GetNode() , pColl );
         m_aSortArr[ nCnt ]->pTOXNd = pTOXNd;
 
@@ -1112,14 +1118,13 @@ void SwTOXBaseSection::Update(const SfxItemSet* pAttr,
         // to method <GenerateText(..)>.
         ::SetProgressState( 0, rDoc.GetDocShell() );
 
-        std::shared_ptr<sw::ToxTabStopTokenHandler> tabStopTokenHandler =
-                std::make_shared<sw::DefaultToxTabStopTokenHandler>(
-                        pSectNd->GetIndex(), *pDefaultPageDesc, GetTOXForm().IsRelTabPos(),
-                        rDoc.GetDocumentSettingManager().get(DocumentSettingId::TABS_RELATIVE_TO_INDENT) ?
-                                sw::DefaultToxTabStopTokenHandler::TABSTOPS_RELATIVE_TO_INDENT :
-                                sw::DefaultToxTabStopTokenHandler::TABSTOPS_RELATIVE_TO_PAGE);
-        sw::ToxTextGenerator ttgn(GetTOXForm(), std::move(tabStopTokenHandler));
-        ttgn.GenerateText(GetFormat()->GetDoc(), markURLs, m_aSortArr, nCnt, nRange, pLayout);
+        sw::ToxTextGenerator ttgn(GetTOXForm(), pTabStopTokenHandler);
+        std::optional<std::pair<SwTextNode *, SvxTabStopItem>> const oTabStops =
+            ttgn.GenerateText(GetFormat()->GetDoc(), markURLs, m_aSortArr, nCnt, nRange, pLayout);
+        if (oTabStops)
+        {
+            tabStops.emplace_back(*oTabStops);
+        }
         nCnt += nRange - 1;
     }
 
@@ -1156,6 +1161,28 @@ void SwTOXBaseSection::Update(const SfxItemSet* pAttr,
     for ( const auto& rpLayout : aAllLayouts )
     {
         SwFrame::CheckPageDescs( static_cast<SwPageFrame*>(rpLayout->Lower()) );
+    }
+    // delay setting tab stops until the layout frames exist, in case the ToX
+    // is in columns or other non-body environment; best way is to check uppers
+    // (what if columns have different widths? no idea what to do about that...)
+    for (auto & it : tabStops)
+    {
+        std::vector<SvxTabStop> tabs;
+        for (size_t i = 0; i < it.second.Count(); ++i)
+        {
+            tabs.emplace_back(it.second.At(i));
+        }
+        it.second.Remove(0, it.second.Count());
+        for (SvxTabStop & rTab : tabs)
+        {
+            if (rTab.GetAdjustment() == SvxTabAdjust::Right)
+            {
+                assert(rTab.GetTabPos() == 0);
+                rTab.GetTabPos() = pTabStopTokenHandler->CalcEndStop(*it.first, pLayout);
+            }
+            it.second.Insert(rTab);
+        }
+        it.first->SetAttr(it.second);
     }
 
     SetProtect( SwTOXBase::IsProtected() );

sw/source/core/tox/ToxTabStopTokenHandler.cxx
@@ -18,6 +18,7 @@
 #include <fmtpdsc.hxx>
 #include <frmfmt.hxx>
 #include <frmatr.hxx>
+#include <frmtool.hxx>
 #include <ndtxt.hxx>
 #include <pagedesc.hxx>
 #include <pagefrm.hxx>
@@ -40,7 +41,7 @@ DefaultToxTabStopTokenHandler::DefaultToxTabStopTokenHandler(SwNodeOffset indexO
 
 ToxTabStopTokenHandler::HandledTabStopToken
 DefaultToxTabStopTokenHandler::HandleTabStopToken(
-        const SwFormToken& aToken, const SwTextNode& targetNode, const SwRootFrame *currentLayout) const
+        const SwFormToken& aToken, const SwTextNode& targetNode) const
 {
     HandledTabStopToken result;
 
@@ -61,29 +62,62 @@ DefaultToxTabStopTokenHandler::HandleTabStopToken(
         return result;
     }
 
-    SwRect aNdRect;
-    if (CanUseLayoutRectangle(targetNode, currentLayout)) {
-        aNdRect = targetNode.FindLayoutRect(true);
-    }
+    // note: this will be filled later by CalcEndStop()
+    result.tabStop = SvxTabStop(0, SvxTabAdjust::Right, cDfltDecimalChar, aToken.cTabFillChar);
+    return result;
+}
+
+auto DefaultToxTabStopTokenHandler::CalcEndStop(SwTextNode const& rNode,
+        SwRootFrame const*const pLayout) const -> tools::Long
+{
     tools::Long nRightMargin;
-    if (aNdRect.IsEmpty()) {
-        nRightMargin = CalculatePageMarginFromPageDescription(targetNode);
-    } else {
-        nRightMargin = aNdRect.Width();
+    if (CanUseLayoutRectangle(rNode, pLayout))
+    {
+        // in case it's in a header, any frame should do
+        SwContentFrame const*const pFrame(rNode.getLayoutFrame(pLayout));
+        assert(pFrame); // created in SwTOXBaseSection::Update()
+        SwRectFnSet const fnRect(pFrame->GetUpper());
+        SwRect rect = pFrame->getFramePrintArea();
+        if (fnRect.GetWidth(rect) == 0) // typically it's newly created
+        {
+            if (pFrame->GetUpper()->IsSctFrame())
+            {   // this is set in SwSectionFrame::Init()
+                rect = pFrame->GetUpper()->GetUpper()->getFramePrintArea();
+                assert(fnRect.GetWidth(rect) != 0);
+            }
+            else if (pFrame->GetUpper()->IsColBodyFrame())
+            {
+                SwFrame const*const pColFrame(pFrame->GetUpper()->GetUpper());
+                assert(pColFrame->IsColumnFrame());
+                rect = pColFrame->getFrameArea();
+                // getFramePrintArea() is not valid yet, manually subtract...
+                // (it can have a border too!)
+                SwBorderAttrAccess access(SwFrame::GetCache(), pColFrame);
+                SwBorderAttrs const& rAttrs(*access.Get());
+                auto const nLeft(rAttrs.CalcLeft(pColFrame));
+                auto const nRight(rAttrs.CalcRight(pColFrame));
+                fnRect.SetWidth(rect, fnRect.GetWidth(rect) - nLeft - nRight);
+                assert(fnRect.GetWidth(rect) != 0);
+            }
+            else assert(false);
+        }
+        nRightMargin = fnRect.GetWidth(rect);
+    }
+    else
+    {
+        nRightMargin = CalculatePageMarginFromPageDescription(rNode);
     }
     //#i24363# tab stops relative to indent
     if (mTabStopReferencePolicy == TABSTOPS_RELATIVE_TO_INDENT) {
         // left margin of paragraph style
         SvxFirstLineIndentItem const& rFirstLine(
-            targetNode.GetTextColl()->GetFirstLineIndent());
+            rNode.GetTextColl()->GetFirstLineIndent());
         SvxTextLeftMarginItem const& rTextLeftMargin(
-            targetNode.GetTextColl()->GetTextLeftMargin());
+            rNode.GetTextColl()->GetTextLeftMargin());
         nRightMargin -= rTextLeftMargin.GetLeft(rFirstLine);
         nRightMargin -= rFirstLine.GetTextFirstLineOffset();
     }
-
-    result.tabStop = SvxTabStop(nRightMargin, SvxTabAdjust::Right, cDfltDecimalChar, aToken.cTabFillChar);
-    return result;
+    return nRightMargin - 1; // subtract 1 twip to avoid equal for TabOverMargin
 }
 
 tools::Long
@@ -113,7 +147,7 @@ DefaultToxTabStopTokenHandler::CanUseLayoutRectangle(const SwTextNode& targetNod
             targetNode.SwContentNode::GetAttr(RES_PAGEDESC).GetPageDesc();
 
     if (!pageDescription) {
-        return false;
+        return true;
     }
     const SwFrame* pFrame = targetNode.getLayoutFrame(currentLayout);
     if (!pFrame) {

sw/source/core/tox/ToxTextGenerator.cxx
@@ -165,15 +165,14 @@ ToxTextGenerator::GenerateTextForChapterToken(const SwFormToken& chapterToken, c
     return retval;
 }
 
-// Add parameter <_TOXSectNdIdx> and <_pDefaultPageDesc> in order to control,
-// which page description is used, no appropriate one is found.
-void
+std::optional<std::pair<SwTextNode *, SvxTabStopItem>>
 ToxTextGenerator::GenerateText(SwDoc* pDoc,
         std::unordered_map<OUString, int> & rMarkURLs,
         const std::vector<std::unique_ptr<SwTOXSortTabBase>> &entries,
         sal_uInt16 indexOfEntryToProcess, sal_uInt16 numberOfEntriesToProcess,
         SwRootFrame const*const pLayout)
 {
+    std::optional<std::pair<SwTextNode *, SvxTabStopItem>> oRet;
     // pTOXNd is only set at the first mark
     SwTextNode* pTOXNd = const_cast<SwTextNode*>(entries.at(indexOfEntryToProcess)->pTOXNd);
     // FIXME this operates directly on the node text
@@ -188,7 +187,7 @@ ToxTextGenerator::GenerateText(SwDoc* pDoc,
         sal_uInt16 nLvl = rBase.GetLevel();
         OSL_ENSURE( nLvl < mToxForm.GetFormMax(), "invalid FORM_LEVEL");
 
-        SvxTabStopItem aTStops( 0, 0, SvxTabAdjust::Default, RES_PARATR_TABSTOP );
+        oRet.emplace(pTOXNd, SvxTabStopItem(0, 0, SvxTabAdjust::Default, RES_PARATR_TABSTOP));
         // create an enumerator
         // #i21237#
         SwFormTokens aPattern = mToxForm.GetPattern(nLvl);
@@ -228,9 +227,9 @@ ToxTextGenerator::GenerateText(SwDoc* pDoc,
 
             case TOKEN_TAB_STOP: {
                 ToxTabStopTokenHandler::HandledTabStopToken htst =
-                        mTabStopTokenHandler->HandleTabStopToken(aToken, *pTOXNd, pDoc->getIDocumentLayoutAccess().GetCurrentLayout());
+                    mTabStopTokenHandler->HandleTabStopToken(aToken, *pTOXNd);
                 rText += htst.text;
-                aTStops.Insert(htst.tabStop);
+                oRet->second.Insert(htst.tabStop);
                 break;
             }
 
@@ -304,10 +303,9 @@ ToxTextGenerator::GenerateText(SwDoc* pDoc,
                 }
             }
         }
-
-        pTOXNd->SetAttr( aTStops );
     }
     mLinkProcessor->InsertLinkAttributes(*pTOXNd);
+    return oRet;
 }
 
 /*static*/ std::shared_ptr<SfxItemSet>

sw/qa/core/test_ToxTextGenerator.cxx
@@ -113,9 +113,14 @@ namespace {
 class MockedToxTabStopTokenHandler : public ToxTabStopTokenHandler {
 public:
     virtual HandledTabStopToken
-    HandleTabStopToken(const SwFormToken&, const SwTextNode&,
-            const SwRootFrame *) const override {
+    HandleTabStopToken(const SwFormToken&, const SwTextNode&) const override
+    {
         return HandledTabStopToken();
+    }
+
+    auto CalcEndStop(SwTextNode const&, SwRootFrame const*) const -> tools::Long override
+    {
+        return 0;
     }
 };
 

sw/qa/extras/uiwriter/uiwriter6.cxx
@@ -2201,7 +2201,7 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf116403)
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1), aTabs.getLength());
     // This was still 17000, refreshing ToX didn't take borders spacings and widths into account
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Page borders must be considered for right-aligned tabstop",
-                                 static_cast<sal_Int32>(17000 - 2 * 500 - 2 * 1),
+                                 static_cast<sal_Int32>(17000 - 2 * 500 - 2 * 1 - 1),
                                  aTabs[0].Position);
 }
 