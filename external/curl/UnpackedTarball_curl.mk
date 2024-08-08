7a82664542401519caa3ca2fdc988fb0c9171a66
Michael Stahl <michael.stahl@allotropia.de>
2024-08-08 11:02:47 +0200

    curl: add patch for CVE-2024-7264
    Change-Id: I0e7e3533f8216f701e1439c2438e3c339c0003df
    (cherry picked from commit 7bc74494f7ff6b5e43abb9d1cba44effe784e799)

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -24,6 +24,8 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/zlib.patch.0 \
 	external/curl/CVE-2023-38545_7.87.0.patch \
 	external/curl/2b0994c29a721c91c57.patch \
+	external/curl/0001-x509asn1-clean-up-GTime2str.patch \
+	external/curl/0001-x509asn1-unittests-and-fixes-for-gtime2str.patch \
 ))
 
 ifeq ($(SYSTEM_NSS),)
