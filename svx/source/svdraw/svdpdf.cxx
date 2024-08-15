5e9b02bbac8cf476d419cd363a91b96464a87813
Miklos Vajna <vmiklos@collabora.com>
2024-08-15 10:09:52 +0200

    Update pdfium to 6656
    Drop external/pdfium/annot.patch.1, it has been upstreamed as
    <https://pdfium-review.googlesource.com/c/pdfium/+/120750>.
    
    Update PDFiumPageObjectImpl::getFontName() to match
    <https://pdfium-review.googlesource.com/c/pdfium/+/121911> "Rename
    FPDFFont_GetFontName() to FPDFFont_GetFamilyName()".
    
    Extend external/pdfium/build.patch.1 to work around:
    
    > C:/cygwin/home/tdf/jenkins/workspace/gerrit_windows/workdir/UnpackedTarball/pdfium/fpdfsdk/fpdf_annot.cpp(1083): fatal error C1001: Internal compiler error.
    
    Change-Id: I94ed21265a79d484759f240f3baeb51c92365c78
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171884
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 0cde8913d8cb183224d03d5cbb37072e63e745d1)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193764
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit 70cbe79e3343285812c809736641ce4625a53e82)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193881
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: allotropia jenkins <jenkins@allotropia.de>

--- svx/source/svdraw/svdpdf.cxx
+++ svx/source/svdraw/svdpdf.cxx
@@ -827,7 +827,7 @@ void ImpSdrPdfImport::ImportText(FPDF_PAGEOBJECT pPageObject, FPDF_TEXTPAGE pTex
     const int nFontName = 80 + 1;
     std::unique_ptr<char[]> pFontName(new char[nFontName]); // + terminating null
     FPDF_FONT pFontObject = FPDFTextObj_GetFont(pPageObject);
-    int nFontNameChars = FPDFFont_GetFontName(pFontObject, pFontName.get(), nFontName);
+    int nFontNameChars = FPDFFont_GetFamilyName(pFontObject, pFontName.get(), nFontName);
     if (nFontName >= nFontNameChars)
     {
         OUString sFontName = OUString::createFromAscii(pFontName.get());
