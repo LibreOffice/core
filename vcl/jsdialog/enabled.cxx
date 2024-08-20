9672841f6d4afb11debe268ee45a686643c54a4e
Rashesh <rashesh.padia@collabora.com>
2024-08-20 13:10:15 +0530

    lokit: calc: send named ranges as well on calcfunctionlist callback
    Change-Id: Ib8082d81da0eec54ba201b5958b86770a508ec91
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172099
    Reviewed-by: Gökay ŞATIR <gokaysatir@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit d805785b2cf7c570fc0bb480100bc50d8fbc5077)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172745
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- vcl/jsdialog/enabled.cxx
+++ vcl/jsdialog/enabled.cxx
@@ -153,6 +153,7 @@ bool isBuilderEnabled(std::u16string_view rUIFile, bool bMobile)
         || rUIFile == u"modules/scalc/ui/validationhelptabpage.ui"
         || rUIFile == u"modules/scalc/ui/warnautocorrect.ui"
         || rUIFile == u"modules/scalc/ui/ztestdialog.ui"
+        || rUIFile == u"modules/scalc/ui/definedatabaserangedialog.ui"
         // schart
         || rUIFile == u"modules/schart/ui/attributedialog.ui"
         || rUIFile == u"modules/schart/ui/charttypedialog.ui"
