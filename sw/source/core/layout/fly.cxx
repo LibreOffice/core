a2cc13d4cd7b627f7694929beb15f19bacebdf9c
Mike Kaganski <mike.kaganski@collabora.com>
2024-08-23 23:30:26 +0500

    Flatten SwFlyFrame::Grow_
    Change-Id: I6fe29fef686e00dce30a024063f684f1eeaf3f64
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172338
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- sw/source/core/layout/fly.cxx
+++ sw/source/core/layout/fly.cxx
@@ -1960,82 +1960,80 @@ void SwFlyFrame::UpdateUnfloatButton(SwWrtShell* pWrtSh, bool bShow) const
 
 SwTwips SwFlyFrame::Grow_( SwTwips nDist, bool bTst )
 {
+    if (!Lower() || IsColLocked() || HasFixSize())
+        return 0;
+
     SwRectFnSet aRectFnSet(this);
-    if ( Lower() && !IsColLocked() && !HasFixSize() )
-    {
-        SwTwips nSize = aRectFnSet.GetHeight(getFrameArea());
-        if( nSize > 0 && nDist > ( LONG_MAX - nSize ) )
-            nDist = LONG_MAX - nSize;
+    SwTwips nSize = aRectFnSet.GetHeight(getFrameArea());
+    if( nSize > 0 && nDist > ( LONG_MAX - nSize ) )
+        nDist = LONG_MAX - nSize;
 
-        if ( nDist <= 0 )
-            return 0;
+    if ( nDist <= 0 )
+        return 0;
 
-        if ( Lower()->IsColumnFrame() )
-        {   // If it's a Column Frame, the Format takes control of the
-            // resizing (due to the adjustment).
-            if ( !bTst )
-            {
-                // #i28701# - unlock position of Writer fly frame
-                UnlockPosition();
-                InvalidatePos_();
-                InvalidateSize();
-            }
-            return 0;
-        }
-
+    if ( Lower()->IsColumnFrame() )
+    {   // If it's a Column Frame, the Format takes control of the
+        // resizing (due to the adjustment).
         if ( !bTst )
         {
-            const SwRect aOld( GetObjRectWithSpaces() );
-            InvalidateSize_();
-            const bool bOldLock = m_bLocked;
-            Unlock();
-            if ( IsFlyFreeFrame() )
-            {
-                // #i37068# - no format of position here
-                // and prevent move in method <CheckClip(..)>.
-                // This is needed to prevent layout loop caused by nested
-                // Writer fly frames - inner Writer fly frames format its
-                // anchor, which grows/shrinks the outer Writer fly frame.
-                // Note: position will be invalidated below.
-                setFrameAreaPositionValid(true);
-
-                // #i55416#
-                // Suppress format of width for autowidth frame, because the
-                // format of the width would call <SwTextFrame::CalcFitToContent()>
-                // for the lower frame, which initiated this grow.
-                const bool bOldFormatHeightOnly = m_bFormatHeightOnly;
-                const SwFormatFrameSize& rFrameSz = GetFormat()->GetFrameSize();
-                if ( rFrameSz.GetWidthSizeType() != SwFrameSize::Fixed )
-                {
-                    m_bFormatHeightOnly = true;
-                }
-                SwViewShell* pSh = getRootFrame()->GetCurrShell();
-                if (pSh)
-                {
-                    static_cast<SwFlyFreeFrame*>(this)->SetNoMoveOnCheckClip( true );
-                    static_cast<SwFlyFreeFrame*>(this)->SwFlyFreeFrame::MakeAll(pSh->GetOut());
-                    static_cast<SwFlyFreeFrame*>(this)->SetNoMoveOnCheckClip( false );
-                }
-                // #i55416#
-                if ( rFrameSz.GetWidthSizeType() != SwFrameSize::Fixed )
-                {
-                    m_bFormatHeightOnly = bOldFormatHeightOnly;
-                }
-            }
-            else
-                MakeAll(getRootFrame()->GetCurrShell()->GetOut());
-            InvalidateSize_();
-            InvalidatePos();
-            if ( bOldLock )
-                Lock();
-            const SwRect aNew( GetObjRectWithSpaces() );
-            if ( aOld != aNew )
-                ::Notify( this, FindPageFrame(), aOld );
-            return aRectFnSet.GetHeight(aNew)-aRectFnSet.GetHeight(aOld);
+            // #i28701# - unlock position of Writer fly frame
+            UnlockPosition();
+            InvalidatePos_();
+            InvalidateSize();
         }
-        return nDist;
+        return 0;
     }
-    return 0;
+
+    if (bTst)
+        return nDist;
+
+    const SwRect aOld( GetObjRectWithSpaces() );
+    InvalidateSize_();
+    const bool bOldLock = m_bLocked;
+    Unlock();
+    if ( IsFlyFreeFrame() )
+    {
+        // #i37068# - no format of position here
+        // and prevent move in method <CheckClip(..)>.
+        // This is needed to prevent layout loop caused by nested
+        // Writer fly frames - inner Writer fly frames format its
+        // anchor, which grows/shrinks the outer Writer fly frame.
+        // Note: position will be invalidated below.
+        setFrameAreaPositionValid(true);
+
+        // #i55416#
+        // Suppress format of width for autowidth frame, because the
+        // format of the width would call <SwTextFrame::CalcFitToContent()>
+        // for the lower frame, which initiated this grow.
+        const bool bOldFormatHeightOnly = m_bFormatHeightOnly;
+        const SwFormatFrameSize& rFrameSz = GetFormat()->GetFrameSize();
+        if ( rFrameSz.GetWidthSizeType() != SwFrameSize::Fixed )
+        {
+            m_bFormatHeightOnly = true;
+        }
+        SwViewShell* pSh = getRootFrame()->GetCurrShell();
+        if (pSh)
+        {
+            static_cast<SwFlyFreeFrame*>(this)->SetNoMoveOnCheckClip( true );
+            static_cast<SwFlyFreeFrame*>(this)->SwFlyFreeFrame::MakeAll(pSh->GetOut());
+            static_cast<SwFlyFreeFrame*>(this)->SetNoMoveOnCheckClip( false );
+        }
+        // #i55416#
+        if ( rFrameSz.GetWidthSizeType() != SwFrameSize::Fixed )
+        {
+            m_bFormatHeightOnly = bOldFormatHeightOnly;
+        }
+    }
+    else
+        MakeAll(getRootFrame()->GetCurrShell()->GetOut());
+    InvalidateSize_();
+    InvalidatePos();
+    if ( bOldLock )
+        Lock();
+    const SwRect aNew( GetObjRectWithSpaces() );
+    if ( aOld != aNew )
+        ::Notify( this, FindPageFrame(), aOld );
+    return aRectFnSet.GetHeight(aNew)-aRectFnSet.GetHeight(aOld);
 }
 
 SwTwips SwFlyFrame::Shrink_( SwTwips nDist, bool bTst )
