39de116b733685881f0defe9cc10070c826ad9c2
Stephan Bergmann <stephan.bergmann@allotropia.de>
2024-08-26 09:04:04 +0200

    +WARN is the default with an unset SAL_LOG env var, anyway
    Change-Id: Ia8bbc1bdc09705f1ecab783d0d73a417f0ef84e5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172378
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- static/emscripten/environment.js
+++ static/emscripten/environment.js
@@ -1,6 +1,5 @@
 if (!('preRun' in Module)) Module['preRun'] = [];
 Module.preRun.push(function() {
     ENV.MAX_CONCURRENCY = '4';
-    ENV.SAL_LOG = "+WARN"
 });
 Module.ignoreApplicationExit = true;
