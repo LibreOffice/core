LibreOffice/core

commit bc70f674c2487ad826f8fe04e626de1829f5b40d

Use getXWeak in configmgr
Change-Id: Ia7d9475cecd2208ccf672f2b35c0c841fed7f4dd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/150840
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

configmgr/source/childaccess.cxx
@@ -141,7 +141,7 @@ css::uno::Reference< css::uno::XInterface > ChildAccess::getParent()
     assert(thisIs(IS_ANY));
     osl::MutexGuard g(*lock_);
     checkLocalizedPropertyAccess();
-    return static_cast< cppu::OWeakObject * >(parent_.get());
+    return cppu::getXWeak(parent_.get());
 }
 
 void ChildAccess::setParent(css::uno::Reference< css::uno::XInterface > const &)
@@ -150,7 +150,7 @@ void ChildAccess::setParent(css::uno::Reference< css::uno::XInterface > const &)
     osl::MutexGuard g(*lock_);
     checkLocalizedPropertyAccess();
     throw css::lang::NoSupportException(
-        "setParent", static_cast< cppu::OWeakObject * >(this));
+        "setParent", getXWeak());
 }
 
 void ChildAccess::bind(
@@ -245,8 +245,7 @@ css::uno::Any ChildAccess::asValue()
                 return child.is() ? child->asValue() : css::uno::Any();
             }
         }
-        value <<= css::uno::Reference< css::uno::XInterface >(
-                            static_cast< cppu::OWeakObject * >(this));
+        value <<= css::uno::Reference(getXWeak());
     }
     return value;
 }

configmgr/source/configurationprovider.cxx
@@ -248,7 +248,7 @@ Service::createInstanceWithArguments(
         throw css::uno::Exception(
             ("com.sun.star.configuration.ConfigurationProvider does not support"
              " service " + ServiceSpecifier),
-            static_cast< cppu::OWeakObject * >(this));
+            getXWeak());
     }
     osl::MutexGuard guard(*lock_);
     Components & components = Components::getSingleton(context_);
@@ -258,10 +258,10 @@ Service::createInstanceWithArguments(
         throw css::uno::Exception(
             ("com.sun.star.configuration.ConfigurationProvider: there is a leaf"
              " value at nodepath " + nodepath),
-            static_cast< cppu::OWeakObject * >(this));
+            getXWeak());
     }
     components.addRootAccess(root);
-    return static_cast< cppu::OWeakObject * >(root.get());
+    return root->getXWeak();
 }
 
 css::uno::Sequence< OUString > Service::getAvailableServiceNames()
@@ -273,7 +273,7 @@ void Service::refresh() {
     //TODO
     std::unique_lock g(m_aMutex);
     if (maRefreshListeners.getLength(g)) {
-        css::lang::EventObject ev(static_cast< cppu::OWeakObject * >(this));
+        css::lang::EventObject ev(getXWeak());
         maRefreshListeners.notifyEach(g, &css::util::XRefreshListener::refreshed, ev);
     }
 }
@@ -296,7 +296,7 @@ void Service::flush() {
     flushModifications();
     std::unique_lock g(m_aMutex);
     if (maFlushListeners.getLength(g)) {
-        css::lang::EventObject ev(static_cast< cppu::OWeakObject * >(this));
+        css::lang::EventObject ev(getXWeak());
         maFlushListeners.notifyEach(g, &css::util::XFlushListener::flushed, ev);
     }
 }
@@ -400,7 +400,7 @@ com_sun_star_comp_configuration_ConfigurationProvider_get_implementation(
 css::uno::Reference< css::uno::XInterface > createDefault(
     css::uno::Reference< css::uno::XComponentContext > const & context)
 {
-    return static_cast< cppu::OWeakObject * >(new Service(context));
+    return getXWeak(new Service(context));
 }
 
 }

configmgr/source/configurationregistry.cxx
@@ -263,7 +263,7 @@ void Service::open(OUString const & rURL, sal_Bool bReadOnly, sal_Bool)
         throw css::lang::WrappedTargetRuntimeException(
             "com.sun.star.configuration.ConfigurationRegistry: open failed: " +
             e.Message,
-            static_cast< cppu::OWeakObject * >(this), anyEx );
+            getXWeak(), anyEx );
     }
     url_ = rURL;
     readOnly_ = bReadOnly;
@@ -285,7 +285,7 @@ void Service::destroy()
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Reference< css::registry::XRegistryKey > Service::getRootKey()
@@ -305,14 +305,14 @@ void Service::mergeKey(OUString const &, OUString const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void Service::flush()
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void Service::addFlushListener(
@@ -320,7 +320,7 @@ void Service::addFlushListener(
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void Service::removeFlushListener(
@@ -328,14 +328,14 @@ void Service::removeFlushListener(
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void Service::checkValid() {
     if (!access_.is()) {
         throw css::registry::InvalidRegistryException(
             "com.sun.star.configuration.ConfigurationRegistry: not valid",
-            static_cast< cppu::OWeakObject * >(this));
+            getXWeak());
     }
 }
 
@@ -343,7 +343,7 @@ void Service::checkValid_RuntimeException() {
     if (!access_.is()) {
         throw css::uno::RuntimeException(
             "com.sun.star.configuration.ConfigurationRegistry: not valid",
-            static_cast< cppu::OWeakObject * >(this));
+            getXWeak());
     }
 }
 
@@ -360,7 +360,7 @@ OUString RegistryKey::getKeyName() {
     }
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 sal_Bool RegistryKey::isReadOnly()
@@ -418,14 +418,14 @@ sal_Int32 RegistryKey::getLongValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setLongValue(sal_Int32)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Sequence< sal_Int32 > RegistryKey::getLongListValue()
@@ -438,14 +438,14 @@ css::uno::Sequence< sal_Int32 > RegistryKey::getLongListValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setLongListValue(css::uno::Sequence< sal_Int32 > const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 OUString RegistryKey::getAsciiValue()
@@ -458,14 +458,14 @@ OUString RegistryKey::getAsciiValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setAsciiValue(OUString const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Sequence< OUString > RegistryKey::getAsciiListValue()
@@ -478,14 +478,14 @@ css::uno::Sequence< OUString > RegistryKey::getAsciiListValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setAsciiListValue(css::uno::Sequence< OUString > const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 OUString RegistryKey::getStringValue()
@@ -498,14 +498,14 @@ OUString RegistryKey::getStringValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setStringValue(OUString const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Sequence< OUString > RegistryKey::getStringListValue()
@@ -518,7 +518,7 @@ css::uno::Sequence< OUString > RegistryKey::getStringListValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setStringListValue(
@@ -526,7 +526,7 @@ void RegistryKey::setStringListValue(
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Sequence< sal_Int8 > RegistryKey::getBinaryValue()
@@ -539,14 +539,14 @@ css::uno::Sequence< sal_Int8 > RegistryKey::getBinaryValue()
     }
     throw css::registry::InvalidValueException(
         "com.sun.star.configuration.ConfigurationRegistry",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::setBinaryValue(css::uno::Sequence< sal_Int8 > const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Reference< css::registry::XRegistryKey > RegistryKey::openKey(
@@ -569,7 +569,7 @@ css::uno::Reference< css::registry::XRegistryKey > RegistryKey::createKey(
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 void RegistryKey::closeKey()
@@ -582,7 +582,7 @@ void RegistryKey::deleteKey(OUString const &)
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Sequence< css::uno::Reference< css::registry::XRegistryKey > >
@@ -590,14 +590,14 @@ RegistryKey::openKeys()
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 css::uno::Sequence< OUString > RegistryKey::getKeyNames()
 {
     throw css::uno::RuntimeException(
         "com.sun.star.configuration.ConfigurationRegistry: not implemented",
-        static_cast< cppu::OWeakObject * >(this));
+        getXWeak());
 }
 
 sal_Bool RegistryKey::createLink(OUString const &, OUString const &)

configmgr/source/readonlyaccess.cxx
@@ -86,12 +86,12 @@ void Service::initialize(css::uno::Sequence< css::uno::Any > const & aArguments)
     if (aArguments.getLength() != 1 || !(aArguments[0] >>= locale)) {
         throw css::lang::IllegalArgumentException(
             "not exactly one string argument",
-            static_cast< cppu::OWeakObject * >(this), -1);
+            getXWeak(), -1);
     }
     std::unique_lock g1(mutex_);
     if (root_.is()) {
         throw css::uno::RuntimeException(
-            "already initialized", static_cast< cppu::OWeakObject * >(this));
+            "already initialized", getXWeak());
     }
     osl::MutexGuard g2(*lock());
     Components & components = Components::getSingleton(context_);
@@ -103,7 +103,7 @@ rtl::Reference< RootAccess > Service::getRoot() {
     std::unique_lock g(mutex_);
     if (!root_.is()) {
         throw css::lang::NotInitializedException(
-            "not initialized", static_cast< cppu::OWeakObject * >(this));
+            "not initialized", getXWeak());
     }
     return root_;
 }

configmgr/source/readwriteaccess.cxx
@@ -108,12 +108,12 @@ void Service::initialize(css::uno::Sequence< css::uno::Any > const & aArguments)
     if (aArguments.getLength() != 1 || !(aArguments[0] >>= locale)) {
         throw css::lang::IllegalArgumentException(
             "not exactly one string argument",
-            static_cast< cppu::OWeakObject * >(this), -1);
+            getXWeak(), -1);
     }
     std::unique_lock g1(mutex_);
     if (root_.is()) {
         throw css::uno::RuntimeException(
-            "already initialized", static_cast< cppu::OWeakObject * >(this));
+            "already initialized", getXWeak());
     }
     osl::MutexGuard g2(*lock());
     Components & components = Components::getSingleton(context_);
@@ -125,7 +125,7 @@ rtl::Reference< RootAccess > Service::getRoot() {
     std::unique_lock g(mutex_);
     if (!root_.is()) {
         throw css::lang::NotInitializedException(
-            "not initialized", static_cast< cppu::OWeakObject * >(this));
+            "not initialized", getXWeak());
     }
     return root_;
 }

configmgr/source/rootaccess.cxx
@@ -118,7 +118,7 @@ void RootAccess::addChangesListener(
         checkLocalizedPropertyAccess();
         if (!aListener.is()) {
             throw css::uno::RuntimeException(
-                "null listener", static_cast< cppu::OWeakObject * >(this));
+                "null listener", getXWeak());
         }
         if (!isDisposed()) {
             changesListeners_.insert(aListener);
@@ -127,7 +127,7 @@ void RootAccess::addChangesListener(
     }
     try {
         aListener->disposing(
-            css::lang::EventObject(static_cast< cppu::OWeakObject * >(this)));
+            css::lang::EventObject(getXWeak()));
     } catch (css::lang::DisposedException &) {}
 }
 
@@ -269,7 +269,7 @@ void RootAccess::initDisposeBroadcaster(Broadcaster * broadcaster) {
     {
         broadcaster->addDisposeNotification(
             changesListener,
-            css::lang::EventObject(static_cast< cppu::OWeakObject * >(this)));
+            css::lang::EventObject(getXWeak()));
     }
     Access::initDisposeBroadcaster(broadcaster);
 }

commit d65ccee2917baadbc1ebf85e61107cfdfcc159cf

Related: tdf#161599 ChildAccess::asValue() must return Reference<XInterface>
... otherwise the unotools/source/config/configitem.cxx
ConfigItem::impl_packLocalizedProperties() checking for
(getValueTypeName() == "com.sun.star.uno.XInterface") would not
hit and thus not pack the properties, returning just the useless
XWeak pointer as property.

This in ScUnoAddInCollection::ReadConfiguration() lead to English
names not being extracted from configured Add-Ins' DisplayName or
CompatibilityName, nor any CompatibilityName added to the list.

Fallout from

    commit bc70f674c2487ad826f8fe04e626de1829f5b40d
    CommitDate: Sun Jun 18 08:41:27 2023 +0200

        Use getXWeak in configmgr

Change-Id: Ibaab8c08a06113b27ceffe1c45ef548c1d6fa225
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172476
Reviewed-by: Eike Rathke <erack@redhat.com>
Tested-by: Jenkins

configmgr/source/childaccess.cxx
@@ -245,7 +245,7 @@ css::uno::Any ChildAccess::asValue()
                 return child.is() ? child->asValue() : css::uno::Any();
             }
         }
-        value <<= css::uno::Reference(getXWeak());
+        value <<= css::uno::Reference< css::uno::XInterface >(getXWeak());
     }
     return value;
 }