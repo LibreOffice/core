LibreOffice/core

commit f5f6db55a5938f37a1c136be904ad7f10a3438ef

tdf#159483 sc HTML paste: handle data-sheets-value here, too
HTML import into Calc could already create text cells, but HTML paste
with the same content remained auto-converted to numbers
unconditionally.

Turns out HTML paste goes via ScHTMLLayoutParser instead of the HTML
import's ScHTMLQueryParser, so the data-sheets-value was ignored for
paste.

Fix the problem by extracting the old data-sheets-value handler from
ScHTMLQueryParser to a separate ParseDataSheetsValue(), and use it also
in ScHTMLLayoutParser.

For the actual handling, still only text is handled, no other formats
yet.

Change-Id: I0b2bf4665af331d07624ed42e30a24e31bfca331
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163068
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins
(cherry picked from commit 543e52481e764b8e0eea6cf0123a77cf492bdf8e)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163130
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sc/source/filter/html/htmlpars.cxx
@@ -72,6 +72,31 @@
 using ::editeng::SvxBorderLine;
 using namespace ::com::sun::star;
 
+namespace
+{
+/// data-sheets-value from google sheets, value is a JSON.
+void ParseDataSheetsValue(const OUString& rDataSheetsValue, sal_uInt32& rNumberFormat)
+{
+    // data-sheets-value from google sheets, value is a JSON.
+    OString aEncodedOption = rDataSheetsValue.toUtf8();
+    const char* pEncodedOption = aEncodedOption.getStr();
+    std::stringstream aStream(pEncodedOption);
+    boost::property_tree::ptree aTree;
+    boost::property_tree::read_json(aStream, aTree);
+    // The "1" key describes the original data type.
+    auto it = aTree.find("1");
+    if (it != aTree.not_found())
+    {
+        int nValueType = std::stoi(it->second.get_value<std::string>());
+        // 2 is text.
+        if (nValueType == 2)
+        {
+            rNumberFormat = NF_STANDARD_FORMAT_TEXT;
+        }
+    }
+}
+}
+
 ScHTMLStyles::ScHTMLStyles() : maEmpty() {}
 
 void ScHTMLStyles::add(const char* pElemName, size_t nElemName, const char* pClassName, size_t nClassName,
@@ -911,6 +936,7 @@ void ScHTMLLayoutParser::TableDataOn( HtmlImportInfo* pInfo )
     bInCell = true;
     bool bHorJustifyCenterTH = (pInfo->nToken == HtmlTokenId::TABLEHEADER_ON);
     const HTMLOptions& rOptions = static_cast<HTMLParser*>(pInfo->pParser)->GetOptions();
+    sal_uInt32 nNumberFormat = NUMBERFORMAT_ENTRY_NOT_FOUND;
     for (const auto & rOption : rOptions)
     {
         switch( rOption.GetToken() )
@@ -979,9 +1005,17 @@ void ScHTMLLayoutParser::TableDataOn( HtmlImportInfo* pInfo )
                 mxActEntry->pNumStr = rOption.GetString();
             }
             break;
+            case HtmlOptionId::DSVAL:
+            {
+                ParseDataSheetsValue(rOption.GetString(), nNumberFormat);
+            }
+            break;
             default: break;
         }
     }
+
+    if (nNumberFormat != NUMBERFORMAT_ENTRY_NOT_FOUND)
+        mxActEntry->aItemSet.Put(SfxUInt32Item(ATTR_VALUE_FORMAT, nNumberFormat));
 
     mxActEntry->nCol = nColCnt;
     mxActEntry->nRow = nRowCnt;
@@ -2119,23 +2153,7 @@ void ScHTMLTable::DataOn( const HtmlImportInfo& rInfo )
                 break;
                 case HtmlOptionId::DSVAL:
                 {
-                    // data-sheets-value from google sheets, value is a JSON.
-                    OString aEncodedOption = rOption.GetString().toUtf8();
-                    const char* pEncodedOption = aEncodedOption.getStr();
-                    std::stringstream aStream(pEncodedOption);
-                    boost::property_tree::ptree aTree;
-                    boost::property_tree::read_json(aStream, aTree);
-                    // The "1" key describes the original data type.
-                    auto it = aTree.find("1");
-                    if (it != aTree.not_found())
-                    {
-                        int nValueType = std::stoi(it->second.get_value<std::string>());
-                        // 2 is text.
-                        if (nValueType == 2)
-                        {
-                            nNumberFormat = NF_STANDARD_FORMAT_TEXT;
-                        }
-                    }
+                    ParseDataSheetsValue(rOption.GetString(), nNumberFormat);
                 }
                 break;
                 default: break;

sc/qa/filter/html/html.cxx
@@ -16,16 +16,19 @@
 
 #include <comphelper/propertyvalue.hxx>
 
+#include <helper/qahelper.hxx>
+#include <impex.hxx>
+
 using namespace com::sun::star;
 
 namespace
 {
 /// Covers sc/source/filter/html/ fixes.
-class Test : public UnoApiXmlTest, public HtmlTestTools
+class Test : public ScModelTestBase, public HtmlTestTools
 {
 public:
     Test()
-        : UnoApiXmlTest("/sc/qa/filter/html/data/")
+        : ScModelTestBase("/sc/qa/filter/html/data/")
     {
     }
 };
@@ -54,6 +57,31 @@ CPPUNIT_TEST_FIXTURE(Test, testTdAsText)
     // - Actual  : 1 (VALUE)
     // i.e. data-sheets-value was ignored on import.
     CPPUNIT_ASSERT_EQUAL(table::CellContentType_TEXT, eType);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testPasteTdAsText)
+{
+    // Given an empty document:
+    createScDoc();
+
+    // When pasting HTML with an A2 cell that contains "01" as text:
+    ScDocument* pDoc = getScDoc();
+    ScAddress aCellPos(/*nColP=*/0, /*nRowP=*/0, /*nTabP=*/0);
+    ScImportExport aImporter(*pDoc, aCellPos);
+    SvFileStream aFile(createFileURL(u"text.html"), StreamMode::READ);
+    SvMemoryStream aMemory;
+    aMemory.WriteStream(aFile);
+    aMemory.Seek(0);
+    CPPUNIT_ASSERT(aImporter.ImportStream(aMemory, OUString(), SotClipboardFormatId::HTML));
+
+    // Then make sure "01" is not auto-converted to 1, as a number:
+    aCellPos = ScAddress(/*nColP=*/0, /*nRowP=*/1, /*nTabP=*/0);
+    CellType eCellType = pDoc->GetCellType(aCellPos);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 2 (CELLTYPE_STRING)
+    // - Actual  : 1 (CELLTYPE_VALUE)
+    // i.e. data-sheets-value was ignored on paste.
+    CPPUNIT_ASSERT_EQUAL(CELLTYPE_STRING, eCellType);
 }
 }
 

sc/CppunitTest_sc_filter_html.mk
@@ -58,6 +58,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sc_filter_html, \
 $(eval $(call gb_CppunitTest_set_include,sc_filter_html,\
     -I$(SRCDIR)/sc/source/ui/inc \
     -I$(SRCDIR)/sc/inc \
+    -I$(SRCDIR)/sc/qa/unit \
     $$(INCLUDE) \
 ))
 

sc/source/filter/inc/htmlpars.hxx
@@ -149,6 +149,7 @@ class HTMLOption;
 typedef ::std::map<SCROW, SCROW> InnerMap;
 typedef ::std::map<sal_uInt16, InnerMap*> OuterMap;
 
+/// HTML parser used during paste into Calc.
 class ScHTMLLayoutParser : public ScHTMLParser
 {
 private:
@@ -575,6 +576,8 @@ public:
 
     Builds the table structure correctly, ignores extended formatting like
     pictures or column widths.
+
+    Used during file load / import into Calc.
  */
 class ScHTMLQueryParser : public ScHTMLParser
 {

commit 45c7bb44b4d76a387dbb0dc6b10e353440cb3923

tdf#162574 sc HTML paste: handle not well-formed json for td data-sheets-value
Regression from commit f5f6db55a5938f37a1c136be904ad7f10a3438ef
(tdf#159483 sc HTML paste: handle data-sheets-value here, too,
2024-02-08), in case the HTML markup is like:

<td data-sheets-value="... garbage ...">

then just ignore it, as if the attribute would be missing. We're only
interested in the case where it contains well-formed JSON.

Change-Id: Id625205e56ae2bfe05a26296a1a0f3abf6d5a4f2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172452
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sc/source/filter/html/htmlpars.cxx
@@ -84,7 +84,15 @@ void ParseDataSheetsValue(const OUString& rDataSheetsValue, std::optional<OUStri
     const char* pEncodedOption = aEncodedOption.getStr();
     std::stringstream aStream(pEncodedOption);
     boost::property_tree::ptree aTree;
-    boost::property_tree::read_json(aStream, aTree);
+    try
+    {
+        boost::property_tree::read_json(aStream, aTree);
+    }
+    catch (const std::exception&)
+    {
+        SAL_WARN("sc", "ParseDataSheetsValue: not well-formed json");
+        return;
+    }
     // The "1" key describes the original data type.
     auto it = aTree.find("1");
     if (it != aTree.not_found())

sc/qa/filter/html/data/bad-json.html
@@ -1,0 +1,4 @@
+<meta http-equiv="content-type" content="text/html; charset=utf-8"><div><table>
+<tbody>
+<tr><td data-sheets-value="{"></td></tr>
+</tbody></table></div>

sc/qa/filter/html/html.cxx
@@ -85,6 +85,21 @@ CPPUNIT_TEST_FIXTURE(Test, testPasteTdAsText)
     CPPUNIT_ASSERT_EQUAL(CELLTYPE_STRING, eCellType);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testPasteBadJson)
+{
+    createScDoc();
+
+    // Just care we don't crash on not-well-formed JSON:
+    ScDocument* pDoc = getScDoc();
+    ScAddress aCellPos(/*nColP=*/0, /*nRowP=*/0, /*nTabP=*/0);
+    ScImportExport aImporter(*pDoc, aCellPos);
+    SvFileStream aFile(createFileURL(u"bad-json.html"), StreamMode::READ);
+    SvMemoryStream aMemory;
+    aMemory.WriteStream(aFile);
+    aMemory.Seek(0);
+    CPPUNIT_ASSERT(aImporter.ImportStream(aMemory, OUString(), SotClipboardFormatId::HTML));
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testPasteTdAsBools)
 {
     // Given an empty document: