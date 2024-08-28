LibreOffice/core

commit 2a9b53bb9f85c2869c779dde36a2d77353fef14b

tdf#160345 Slow rendering of filled polygon
Partial fix of this problem.
The tiled image is a single pixel big, so we can do a solid color fill.
Ideally vcl needs to pass the operation down to skia/cairo and let those
layers do the tiling.

Change-Id: Ifcb1d1a71f0b23b772bbacf1b1e2b15ea315f578
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169619
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -701,8 +701,31 @@ bool VclProcessor2D::RenderFillGraphicPrimitive2DImpl(
 
     // check if offset is used
     const sal_Int32 nOffsetX(basegfx::fround(rFillGraphicAttribute.getOffsetX() * nBWidth));
+    const sal_Int32 nOffsetY(basegfx::fround(rFillGraphicAttribute.getOffsetY() * nBHeight));
 
-    if (nOffsetX)
+    if (nOffsetX == 0 && nOffsetY == 0)
+    {
+        if (!bPreScaled)
+            aBitmapEx.Scale(aNeededBitmapSizePixel);
+
+        // if the tile is a single pixel big, just flood fill with that pixel color
+        if (aNeededBitmapSizePixel.getWidth() == 1 && aNeededBitmapSizePixel.getHeight() == 1)
+        {
+            Color col = aBitmapEx.GetPixelColor(0, 0);
+            mpOutputDevice->SetLineColor(col);
+            mpOutputDevice->SetFillColor(col);
+            mpOutputDevice->DrawRect(aVisiblePixel);
+        }
+        else
+        {
+            // TODO vcl does not have an optimised path here, it should be passing some kind of fill/tile
+            // operation down to the cairo/skia layers
+            Wallpaper aWallpaper(aBitmapEx);
+            aWallpaper.SetColor(COL_TRANSPARENT);
+            mpOutputDevice->DrawWallpaper(aVisiblePixel, aWallpaper);
+        }
+    }
+    else if (nOffsetX)
     {
         // offset in X, so iterate over Y first and draw lines
         for (sal_Int32 nYPos(nBTop); nYPos < nOTop + nOHeight; nYPos += nBHeight, nPosY++)
@@ -727,11 +750,8 @@ bool VclProcessor2D::RenderFillGraphicPrimitive2DImpl(
             }
         }
     }
-    else
+    else // nOffsetY is used
     {
-        // check if offset is used
-        const sal_Int32 nOffsetY(basegfx::fround(rFillGraphicAttribute.getOffsetY() * nBHeight));
-
         // possible offset in Y, so iterate over X first and draw columns
         for (sal_Int32 nXPos(nBLeft); nXPos < nOLeft + nOWidth; nXPos += nBWidth, nPosX++)
         {

commit df6293ad0634a29ff4d0196d1f9230062ccc95ca

tdf#161983 Page background image follows scrolled view
regression from
    commit 2a9b53bb9f85c2869c779dde36a2d77353fef14b
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jun 27 10:18:15 2024 +0200
    tdf#160345 Slow rendering of filled polygon

Change-Id: I38abd82d33cc25a56a26d8ccac26fb5f90e9c0fb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172526
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -722,27 +722,14 @@ bool VclProcessor2D::RenderFillGraphicPrimitive2DImpl(
     const sal_Int32 nOffsetX(basegfx::fround(rFillGraphicAttribute.getOffsetX() * nBWidth));
     const sal_Int32 nOffsetY(basegfx::fround(rFillGraphicAttribute.getOffsetY() * nBHeight));
 
-    if (nOffsetX == 0 && nOffsetY == 0)
+    // if the tile is a single pixel big, just flood fill with that pixel color
+    if (nOffsetX == 0 && nOffsetY == 0 && aNeededBitmapSizePixel.getWidth() == 1
+        && aNeededBitmapSizePixel.getHeight() == 1)
     {
-        if (!bPreScaled)
-            aBitmapEx.Scale(aNeededBitmapSizePixel);
-
-        // if the tile is a single pixel big, just flood fill with that pixel color
-        if (aNeededBitmapSizePixel.getWidth() == 1 && aNeededBitmapSizePixel.getHeight() == 1)
-        {
-            Color col = aBitmapEx.GetPixelColor(0, 0);
-            mpOutputDevice->SetLineColor(col);
-            mpOutputDevice->SetFillColor(col);
-            mpOutputDevice->DrawRect(aVisiblePixel);
-        }
-        else
-        {
-            // TODO vcl does not have an optimised path here, it should be passing some kind of fill/tile
-            // operation down to the cairo/skia layers
-            Wallpaper aWallpaper(aBitmapEx);
-            aWallpaper.SetColor(COL_TRANSPARENT);
-            mpOutputDevice->DrawWallpaper(aVisiblePixel, aWallpaper);
-        }
+        Color col = aBitmapEx.GetPixelColor(0, 0);
+        mpOutputDevice->SetLineColor(col);
+        mpOutputDevice->SetFillColor(col);
+        mpOutputDevice->DrawRect(aVisiblePixel);
     }
     else if (nOffsetX)
     {