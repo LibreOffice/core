4de9d58daefef6da0b64c3db0637f9f9d314dd09
Jonathan Clark <jonathan@libreoffice.org>
2024-08-28 05:02:49 -0600

    tdf#152142 sw: fix RTL as-char anchored textbox positioning
    Fixes an issue causing incorrect textbox positions when the containing
    shape is anchored 'as character' inside RTL text.
    
    Change-Id: I4a68348b778adbdb893a62a29a1fb31ffbcf55e8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172529
    Tested-by: Jenkins
    Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>

--- sw/qa/extras/layout/layout3.cxx
+++ sw/qa/extras/layout/layout3.cxx
@@ -3273,6 +3273,36 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf162614)
     // clang-format on
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf152142)
+{
+    // Regression test for textbox positioning when anchored as-char in RTL context.
+    createSwDoc("tdf152142.fodt");
+    auto pXmlDoc = parseLayoutDump();
+
+    assertXPath(pXmlDoc, "//page"_ostr, 1);
+
+    SwTwips nTextBoxBegin
+        = getXPath(pXmlDoc, "/root/page/body/txt[2]/anchored/fly/txt/infos/bounds"_ostr,
+                   "left"_ostr)
+              .toInt32();
+    SwTwips nTextBoxEnd
+        = getXPath(pXmlDoc, "/root/page/body/txt[2]/anchored/fly/txt/infos/bounds"_ostr,
+                   "right"_ostr)
+              .toInt32();
+
+    SwTwips nShapeBegin
+        = getXPath(pXmlDoc, "/root/page/body/txt[2]/anchored/SwAnchoredDrawObject/bounds"_ostr,
+                   "left"_ostr)
+              .toInt32();
+    SwTwips nShapeEnd
+        = getXPath(pXmlDoc, "/root/page/body/txt[2]/anchored/SwAnchoredDrawObject/bounds"_ostr,
+                   "right"_ostr)
+              .toInt32();
+
+    CPPUNIT_ASSERT_GREATER(nShapeBegin, nTextBoxBegin);
+    CPPUNIT_ASSERT_LESS(nShapeEnd, nTextBoxEnd);
+}
+
 } // end of anonymous namespace
 
 CPPUNIT_PLUGIN_IMPLEMENT();
