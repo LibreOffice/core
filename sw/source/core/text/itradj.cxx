LibreOffice/core

commit 17eaebee279772b6062ae3448012133897fc71bb

tdf#119908 sw smart justify: fix justification by shrinking
Shrink the exceeding lines by shrinking the spaces
between the words.

Because the negative spacing values are used
for the extra space between the characters in the main
data structure of the justified line layout, and not for
the space between the words, like the positive values,
the negative space values for shrinking are stored over
LONG_MAX/2 as absolute values to avoid of bigger changes
of the data structure before designing a better
justification algorithm, where it's possible to mix
different methods for the more visible text layout.

Note: the text cursor doesn't follow the new word
positions yet.

Follow-up to commit 7d08767b890e723cd502b1c61d250924f695eb98
"tdf#130088 tdf#119908 smart justify: fix DOCX line count + compat opt."

Change-Id: I9a63b5a93d6bce230e963ebc88ea2d0f9aa8fffb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159511
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/itradj.cxx
@@ -290,6 +290,8 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
     bool bDoNotJustifyTab = false;
 
     SwLinePortion *pPos = pCurrent->GetNextPortion();
+    // calculate real text width for shrinking
+    tools::Long nBreakWidth = 0;
 
     while( pPos )
     {
@@ -367,6 +369,11 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
                 if( nGluePortion )
                 {
                     tools::Long nSpaceAdd = nGluePortionWidth / sal_Int32(nGluePortion);
+                    // shrink, if portions exceed the line width
+                    tools::Long nSpaceSub = ( nBreakWidth > pCurrent->Width() )
+                        ? (nBreakWidth - pCurrent->Width()) * SPACING_PRECISION_FACTOR /
+                                sal_Int32(nGluePortion) + LONG_MAX/2
+                        : 0;
 
                     // i60594
                     if( rSI.CountKashida() && !bSkipKashida )
@@ -382,7 +389,7 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
                         }
                     }
 
-                    pCurrent->SetLLSpaceAdd( nSpaceAdd , nSpaceIdx );
+                    pCurrent->SetLLSpaceAdd( nSpaceSub ? nSpaceSub : nSpaceAdd, nSpaceIdx );
                     pPos->Width( static_cast<SwGluePortion*>(pPos)->GetFixWidth() );
                 }
                 else if (IsOneBlock() && nCharCnt > TextFrameIndex(1))
@@ -398,6 +405,10 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
             }
             else
                 ++nGluePortion;
+        }
+        else
+        {
+            nBreakWidth += pPos->Width();
         }
         GetInfo().SetIdx( GetInfo().GetIdx() + pPos->GetLen() );
         if ( pPos == pStopAt )

sw/source/core/text/portxt.cxx
@@ -394,12 +394,7 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                           ! rInf.GetLast()->IsErgoSumPortion() &&
                           lcl_HasContent(*static_cast<SwFieldPortion*>(rInf.GetLast()),rInf ) ) ) ) )
         {
-            // GetLineWidth() takes care of DocumentSettingId::TAB_OVER_MARGIN.
-            if (aGuess.BreakWidth() <= rInf.GetLineWidth())
-                Width( aGuess.BreakWidth() );
-            else
-                // this actually should not happen
-                Width( sal_uInt16(rInf.Width() - rInf.X()) );
+            Width( aGuess.BreakWidth() );
 
             SetLen( aGuess.BreakPos() - rInf.GetIdx() );
 

sw/qa/core/text/frmform.cxx
@@ -63,9 +63,7 @@ CPPUNIT_TEST_FIXTURE(Test, testFloattableNegativeVertOffset)
     CPPUNIT_ASSERT_LESS(pPara2->getFrameArea().Top(), rFlyRect.Bottom());
 }
 
-// FIXME: because breaking the lines at the right place, this test
-// became obsolete: proposed fix is to modify compatibilityMode to "14"
-// in the DOCX test file to use the old justification algorithm
+// FIXME: because breaking the lines at the right place, test document became obsolete
 #if 0
 CPPUNIT_TEST_FIXTURE(Test, testFloattableAvoidManipOfst)
 {

sw/source/core/inc/drawfont.hxx
@@ -544,7 +544,11 @@ public:
         }
         else
         {
-            m_nSpace = nNew;
+            // negative space (shrinking) stored over LONG_MAX/2
+            if ( nNew < LONG_MAX/2 )
+                m_nSpace = nNew;
+            else
+                m_nSpace = LONG_MAX/2 - nNew;
             m_nCharacterSpacing = 0;
         }
 #ifdef DBG_UTIL

sw/source/core/text/inftxt.cxx
@@ -634,7 +634,7 @@ void SwTextPaintInfo::DrawText_( const OUString &rText, const SwLinePortion &rPo
     aDrawInf.SetUnderFnt( m_pUnderFnt );
 
     const tools::Long nSpaceAdd = ( rPor.IsBlankPortion() || rPor.IsDropPortion() ||
-                             rPor.InNumberGrp() ) ? 0 : GetSpaceAdd();
+                             rPor.InNumberGrp() ) ? 0 : GetSpaceAdd(/*bShrink=*/true);
     if ( nSpaceAdd )
     {
         TextFrameIndex nCharCnt(0);

sw/source/core/text/inftxt.hxx
@@ -443,8 +443,10 @@ public:
     void SetSpaceIdx( sal_uInt16 nNew ) { m_nSpaceIdx = nNew; }
     void IncSpaceIdx() { ++m_nSpaceIdx; }
     void RemoveFirstSpaceAdd() { m_pSpaceAdd->erase( m_pSpaceAdd->begin() ); }
-    tools::Long GetSpaceAdd() const
-        { return ( m_pSpaceAdd && m_nSpaceIdx < m_pSpaceAdd->size() )
+    tools::Long GetSpaceAdd( bool bShrink = false ) const
+        { return ( m_pSpaceAdd && m_nSpaceIdx < m_pSpaceAdd->size() &&
+                   // get shrink data only if asked explicitly, otherwise zero it
+                   ( bShrink || (*m_pSpaceAdd)[m_nSpaceIdx] < LONG_MAX/2 ) )
                    ? (*m_pSpaceAdd)[m_nSpaceIdx] : 0; }
 
     void SetpSpaceAdd( std::vector<tools::Long>* pNew ){ m_pSpaceAdd = pNew; }

sw/source/core/text/porlay.hxx
@@ -192,7 +192,12 @@ public:
         else
             (*m_pLLSpaceAdd)[ nIdx ] = nNew;
     }
-    tools::Long GetLLSpaceAdd( sal_uInt16 nIdx ) { return (*m_pLLSpaceAdd)[ nIdx ]; }
+    tools::Long GetLLSpaceAdd( sal_uInt16 nIdx, bool bShrink = false )
+    {
+        // get shrink data only if asked explicitly, otherwise zero it
+        return ( bShrink || (*m_pLLSpaceAdd)[ nIdx ] < LONG_MAX/2 )
+            ? (*m_pLLSpaceAdd)[ nIdx ] : 0;
+    }
     void RemoveFirstLLSpaceAdd() { m_pLLSpaceAdd->erase( m_pLLSpaceAdd->begin() ); }
     std::vector<tools::Long>* GetpLLSpaceAdd() const { return m_pLLSpaceAdd.get(); }
 

commit 22eac3145ca62d15b47d95f4df60ce38d4f5aa46

tdf#162220 sw smart justify: fix shrinking for single portion lines
Follow-up to commit 6b857398a59d16308d6185d01e003e401439f060
"tdf#162109 sw smart justify: fix overhanging last line".

Regression from commit 17eaebee279772b6062ae3448012133897fc71bb
"tdf#119908 sw smart justify: fix justification by shrinking".

Change-Id: I6e2dbdbf3628bdae765c235d5f29c1aa0b8987ca
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172599
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

sw/source/core/text/itradj.cxx
@@ -45,24 +45,9 @@ void SwTextAdjuster::FormatBlock( )
     const SwLinePortion *pFly = nullptr;
 
     bool bSkip = !IsLastBlock() &&
+        // don't skip, if the last paragraph line needs space shrinking
+        m_pCurr->ExtraShrunkWidth() <= m_pCurr->Width() &&
         m_nStart + m_pCurr->GetLen() >= TextFrameIndex(GetInfo().GetText().getLength());
-
-    // tdf#162109 if the last line is longer, than the paragraph width,
-    // it contains shrinking spaces: don't skip block format here
-    if( bSkip )
-    {
-        // sum width of the text portions to calculate the line width without shrinking
-        tools::Long nBreakWidth = 0;
-        const SwLinePortion *pPos = m_pCurr->GetNextPortion();
-        while( pPos && bSkip )
-        {
-            if( !pPos->InGlueGrp() )
-                nBreakWidth += pPos->Width();
-            if( nBreakWidth > m_pCurr->Width() )
-                bSkip = false;
-            pPos = pPos->GetNextPortion();
-        }
-    }
 
     // Multi-line fields are tricky, because we need to check whether there are
     // any other text portions in the paragraph.
@@ -408,6 +393,9 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
                 if( nGluePortion )
                 {
                     tools::Long nSpaceAdd = nGluePortionWidth / sal_Int32(nGluePortion);
+                    // shrink, if not shrunk line width exceed the set line width
+                    if ( pCurrent->ExtraShrunkWidth() > 0 )
+                        nBreakWidth = pCurrent->ExtraShrunkWidth();
                     // shrink, if portions exceed the line width
                     tools::Long nSpaceSub = ( nBreakWidth > pCurrent->Width() )
                         ? (nBreakWidth - pCurrent->Width()) * SPACING_PRECISION_FACTOR /

sw/source/core/text/itrform2.cxx
@@ -2135,6 +2135,7 @@ TextFrameIndex SwTextFormatter::FormatLine(TextFrameIndex const nStartPos)
 
                 m_pCurr->SetLen(TextFrameIndex(0));
                 m_pCurr->Width(0);
+                m_pCurr->ExtraShrunkWidth(0);
                 m_pCurr->Truncate();
             }
         }

sw/source/core/text/porlin.hxx
@@ -49,6 +49,8 @@ private:
     bool m_bJoinBorderWithPrev;
     bool m_bJoinBorderWithNext;
     SwTwips m_nExtraBlankWidth = 0;    // width of spaces after the break
+    SwTwips m_nExtraShrunkWidth = 0;   // width of not shrunk line
+
     std::optional<SwLinePortionLayoutContext> m_nLayoutContext;
 
     void Truncate_();
@@ -72,6 +74,8 @@ public:
     void SubPrtWidth( const SwTwips nNew ) { Width( Width() - nNew ); }
     SwTwips ExtraBlankWidth() const { return m_nExtraBlankWidth; }
     void ExtraBlankWidth(const SwTwips nNew) { m_nExtraBlankWidth = nNew; }
+    SwTwips ExtraShrunkWidth() const { return m_nExtraShrunkWidth; }
+    void ExtraShrunkWidth(const SwTwips nNew) { m_nExtraShrunkWidth = nNew; }
     SwTwips GetHangingBaseline() const { return mnHangingBaseline; }
     void SetHangingBaseline( const SwTwips nNewBaseline ) { mnHangingBaseline = nNewBaseline; }
     std::optional<SwLinePortionLayoutContext> GetLayoutContext() const { return m_nLayoutContext; }
@@ -185,6 +189,7 @@ inline SwLinePortion &SwLinePortion::operator=(const SwLinePortion &rPortion)
     m_bJoinBorderWithPrev = rPortion.m_bJoinBorderWithPrev;
     m_bJoinBorderWithNext = rPortion.m_bJoinBorderWithNext;
     m_nExtraBlankWidth = rPortion.m_nExtraBlankWidth;
+    m_nExtraShrunkWidth = rPortion.m_nExtraShrunkWidth;
     m_nLayoutContext = rPortion.m_nLayoutContext;
     return *this;
 }
@@ -199,6 +204,7 @@ inline SwLinePortion::SwLinePortion(const SwLinePortion &rPortion) :
     m_bJoinBorderWithPrev( rPortion.m_bJoinBorderWithPrev ),
     m_bJoinBorderWithNext( rPortion.m_bJoinBorderWithNext ),
     m_nExtraBlankWidth(rPortion.m_nExtraBlankWidth),
+    m_nExtraShrunkWidth(rPortion.m_nExtraShrunkWidth),
     m_nLayoutContext(rPortion.m_nLayoutContext)
 {
 }

sw/source/core/text/portxt.cxx
@@ -258,6 +258,7 @@ void SwTextPortion::BreakCut( SwTextFormatInfo &rInf, const SwTextGuess &rGuess 
     {
         SetLen( TextFrameIndex(0) );
         Width( 0 );
+        ExtraShrunkWidth( 0 );
     }
 }
 
@@ -266,6 +267,7 @@ void SwTextPortion::BreakUnderflow( SwTextFormatInfo &rInf )
     Truncate();
     Height( 0 );
     Width( 0 );
+    ExtraShrunkWidth( 0 );
     SetLen( TextFrameIndex(0) );
     SetAscent( 0 );
     rInf.SetUnderflow( this );
@@ -301,6 +303,7 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
         return bFull;
     }
 
+    ExtraShrunkWidth( 0 );
     std::unique_ptr<SwTextGuess> pGuess(new SwTextGuess());
     bool bFull = !pGuess->Guess( *this, rInf, Height() );
 
@@ -339,8 +342,12 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
 
         if ( nSpacesInLine > 0 )
         {
+            SwTwips nOldWidth = pGuess->BreakWidth();
             pGuess.reset(new SwTextGuess());
             bFull = !pGuess->Guess( *this, rInf, Height(), nSpacesInLine );
+
+            if ( pGuess->BreakWidth() > nOldWidth )
+                ExtraShrunkWidth( pGuess->BreakWidth() );
         }
     }
 
@@ -492,6 +499,7 @@ bool SwTextPortion::Format( SwTextFormatInfo &rInf )
     {
         Height( 0 );
         Width( 0 );
+        ExtraShrunkWidth( 0 );
         SetLen( TextFrameIndex(0) );
         SetAscent( 0 );
         SetNextPortion( nullptr );  // ????