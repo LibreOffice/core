LibreOffice/core

commit f9c8d97d82a85b897520a2fe897352ee5ad879d9

tdf#95213 DOCX import: don't reuse list label styles
We used to optimize the creation of these styles, so in case two list
label had the same character properties, then we could avoid creating
two styles for those.

This isn't correct though: it means if the style is changed later by the
user, then unexpected other places in the document will change as well.
Do what the binary DOC filter does: create one character style for each
level of a numbering separately.

Change-Id: I967b30fc078b1be30f7ef81b2706df2962fc3fb0
Reviewed-on: https://gerrit.libreoffice.org/21191
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -501,7 +501,7 @@ DECLARE_OOXMLEXPORT_TEST(testfdo76934, "fdo76934.docx")
         return;
 
     // Ensure that after fix LO is preserving AutoSpacing property in styles.xml
-    assertXPath ( pXmlDoc, "/w:styles[1]/w:style[36]/w:pPr[1]/w:spacing[1]", "beforeAutospacing", "1" );
+    assertXPath ( pXmlDoc, "/w:styles[1]/w:style[@w:styleId='Title']/w:pPr[1]/w:spacing[1]", "beforeAutospacing", "1" );
 }
 
 DECLARE_OOXMLEXPORT_TEST(testfdo79540, "fdo79540.docx")

sw/qa/extras/ooxmlimport/ooxmlimport.cxx
@@ -3033,6 +3033,20 @@ DECLARE_OOXMLIMPORT_TEST(testTdf92045, "tdf92045.docx")
     CPPUNIT_ASSERT_EQUAL(false, getProperty<bool>(getRun(getParagraph(1), 1), "CharFlash"));
 }
 
+DECLARE_OOXMLIMPORT_TEST(testTdf95213, "tdf95213.docx")
+{
+    // Get the second paragraph's numbering style's 2nd level's character style name.
+    uno::Reference<text::XTextRange> xParagraph = getParagraph(2);
+    auto xLevels = getProperty< uno::Reference<container::XIndexAccess> >(xParagraph, "NumberingRules");
+    uno::Sequence<beans::PropertyValue> aLevel;
+    xLevels->getByIndex(1) >>= aLevel; // 2nd level
+    OUString aName = std::find_if(aLevel.begin(), aLevel.end(), [](const beans::PropertyValue& rValue) { return rValue.Name == "CharStyleName"; })->Value.get<OUString>();
+
+    uno::Reference<beans::XPropertySet> xStyle(getStyles("CharacterStyles")->getByName(aName), uno::UNO_QUERY);
+    // This was awt::FontWeight::BOLD.
+    CPPUNIT_ASSERT_EQUAL(awt::FontWeight::NORMAL, getProperty<float>(xStyle, "CharWeight"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

writerfilter/source/dmapper/DomainMapper.cxx
@@ -3081,7 +3081,7 @@ void DomainMapper::lcl_utext(const sal_uInt8 * data_, size_t len)
     {
         PropertyMapPtr pContext = m_pImpl->GetTopContext();
         PropertyValueVector_t aProps = comphelper::sequenceToContainer< PropertyValueVector_t >(pContext->GetPropertyValues());
-        OUString sStyle = getOrCreateCharStyle(aProps);
+        OUString sStyle = getOrCreateCharStyle(aProps, /*bAlwaysCreate=*/false);
         m_pImpl->SetRubyText(sText,sStyle);
         return;
     }
@@ -3542,10 +3542,10 @@ uno::Reference< text::XTextRange > DomainMapper::GetCurrentTextRange()
     return m_pImpl->GetTopTextAppend()->getEnd();
 }
 
-OUString DomainMapper::getOrCreateCharStyle( PropertyValueVector_t& rCharProperties )
+OUString DomainMapper::getOrCreateCharStyle( PropertyValueVector_t& rCharProperties, bool bAlwaysCreate )
 {
     StyleSheetTablePtr pStyleSheets = m_pImpl->GetStyleSheetTable();
-    return pStyleSheets->getOrCreateCharStyle( rCharProperties );
+    return pStyleSheets->getOrCreateCharStyle( rCharProperties, bAlwaysCreate );
 }
 
 StyleSheetTablePtr DomainMapper::GetStyleSheetTable( )

writerfilter/source/dmapper/DomainMapper.hxx
@@ -104,7 +104,7 @@ public:
     css::uno::Reference<css::lang::XMultiServiceFactory> GetTextFactory() const;
     css::uno::Reference<css::text::XTextRange> GetCurrentTextRange();
 
-    OUString getOrCreateCharStyle( PropertyValueVector_t& rCharProperties );
+    OUString getOrCreateCharStyle( PropertyValueVector_t& rCharProperties, bool bAlwaysCreate );
     std::shared_ptr< StyleSheetTable > GetStyleSheetTable( );
     GraphicZOrderHelper* graphicZOrderHelper();
 

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2994,7 +2994,7 @@ void  DomainMapper_Impl::handleRubyEQField( FieldContextPtr pContext)
             pRubyContext->Insert(PROP_CHAR_HEIGHT_ASIAN, aVal);
         }
         PropertyValueVector_t aProps = comphelper::sequenceToContainer< PropertyValueVector_t >(pRubyContext->GetPropertyValues());
-        aInfo.sRubyStyle = m_rDMapper.getOrCreateCharStyle(aProps);
+        aInfo.sRubyStyle = m_rDMapper.getOrCreateCharStyle(aProps, /*bAlwaysCreate=*/false);
         PropertyMapPtr pCharContext(new PropertyMap());
         if (m_pLastCharacterContext.get())
             pCharContext->InsertProps(m_pLastCharacterContext);

writerfilter/source/dmapper/NumberingManager.cxx
@@ -567,7 +567,7 @@ void ListDef::CreateNumberingRules( DomainMapper& rDMapper,
 
                     //create (or find) a character style containing the character
                     // attributes of the symbol and apply it to the numbering level
-                    OUString sStyle = rDMapper.getOrCreateCharStyle( aStyleProps );
+                    OUString sStyle = rDMapper.getOrCreateCharStyle( aStyleProps, /*bAlwaysCreate=*/true );
                     aLvlProps.push_back(comphelper::makePropertyValue(getPropertyName(PROP_CHAR_STYLE_NAME), sStyle));
                 }
 

writerfilter/source/dmapper/StyleSheetTable.cxx
@@ -1519,11 +1519,12 @@ void StyleSheetTable::applyDefaults(bool bParaProperties)
 }
 
 
-OUString StyleSheetTable::getOrCreateCharStyle( PropertyValueVector_t& rCharProperties )
+OUString StyleSheetTable::getOrCreateCharStyle( PropertyValueVector_t& rCharProperties, bool bAlwaysCreate )
 {
     //find out if any of the styles already has the required properties then return its name
     OUString sListLabel = m_pImpl->HasListCharStyle(rCharProperties);
-    if( !sListLabel.isEmpty() )
+    // Don't try to reuse an existing character style if requested.
+    if( !sListLabel.isEmpty() && !bAlwaysCreate)
         return sListLabel;
     const char cListLabel[] = "ListLabel ";
     uno::Reference< style::XStyleFamiliesSupplier > xStylesSupplier( m_pImpl->m_xTextDocument, uno::UNO_QUERY_THROW );

commit 32b667acd665ce9e66ec6db952e1effb7b4c1031

tdf#162707 DOCX export: fix lost paragraph styles with many numberings
Open the bugdoc, style of the only para of the only footer is Footer,
save as DOCX, reopen, style changes from Footer to Default.

What seems to happen is that the document has too many numberings, and
each numbering has 9 levels, so if we have a char style for each level,
that easily runs into the MSWORD_MAX_STYLES_LIMIT limit, after which we
silently start dropping styles during export.

Fix the problem by tweaking the fix done in commit
7376a47680b65cbdfd747a736f288e06f51f7f2d (tdf#92335 DOCX: fix
multiplying of "ListLabel" styles, 2019-05-28): if we don't just avoid
writing those ListLabel styles (where the real info is written as a
non-style in numbering.xml, so no formatting is list) but we filter them
out earlier, then MSWordStyles::BuildStylesTable() will count styles
correctly and we'll still have space for the lost Footer style.

Regression from commit f9c8d97d82a85b897520a2fe897352ee5ad879d9
(tdf#95213 DOCX import: don't reuse list label styles, 2016-01-07).

Change-Id: I7a6b0b7b5a7b3c813a844c5fd185cf2f419e1a5c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172647
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/filter/ww8/wrtw8sty.cxx
@@ -320,7 +320,22 @@ void MSWordStyles::BuildStylesTable()
     const SwCharFormats& rArr = *m_rExport.m_rDoc.GetCharFormats();       // first CharFormat
     // the default character style ( 0 ) will not be outputted !
     for (size_t n = 1; n < rArr.size() && m_aStyles.size() < MSWORD_MAX_STYLES_LIMIT; ++n)
+    {
+        if (m_rExport.GetExportFormat() == MSWordExportBase::DOCX
+            && rArr[n]->GetName().startsWith("ListLabel"))
+        {
+            // tdf#92335 don't export redundant DOCX import style "ListLabel"
+            continue;
+        }
+
         m_aStyles.emplace_back(rArr[n]);
+    }
+
+    if (m_aStyles.size() == MSWORD_MAX_STYLES_LIMIT)
+    {
+        SAL_WARN("sw.ww8", "MSWordStyles::BuildStylesTable: too many styles, have "
+                               << rArr.size() << " char styles");
+    }
 
     const SwTextFormatColls& rArr2 = *m_rExport.m_rDoc.GetTextFormatColls();   // then TextFormatColls
     // the default paragraph style ( 0 ) will not be outputted !
@@ -699,13 +714,6 @@ void MSWordStyles::OutputStyle(sal_uInt16 nSlot)
         sal_uInt16 nWwLink = 0x0FFF;
 
         GetStyleData(entry.format, bFormatColl, nBase, nWwNext, nWwLink);
-
-        if (!bFormatColl && m_rExport.GetExportFormat() == MSWordExportBase::DOCX &&
-                        entry.style_id.startsWith("ListLabel"))
-        {
-            // tdf#92335 don't export redundant DOCX import style "ListLabel"
-            return;
-        }
 
         m_rExport.AttrOutput().StartStyle(entry.ww_name, (bFormatColl ? STYLE_TYPE_PARA : STYLE_TYPE_CHAR),
                 nBase, nWwNext, nWwLink, m_aStyles[nSlot].ww_id, nSlot,

sw/qa/extras/ooxmlexport/ooxmlexport5.cxx
@@ -700,6 +700,21 @@ CPPUNIT_TEST_FIXTURE(Test, testfdo79968_sldx)
         u"PowerPoint.Slide.12"_ustr);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testLostFooterStyle)
+{
+    // Given a document with many numberings and a Footer para style:
+    loadAndSave("lost-footer-style.docx");
+
+    // When saving that to DOCX and checking the Footer style:
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/styles.xml"_ustr);
+    // Without the accompanying fix in place, this test would have failed, the Footer style was
+    // lost on export due to the many ListLabel char styles.
+    OUString aType = getXPath(pXmlDoc, "/w:styles/w:style[@w:styleId='Footer']"_ostr, "type"_ostr);
+
+    // Then make sure we have a Footer para style:
+    CPPUNIT_ASSERT_EQUAL(u"paragraph"_ustr, aType);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */