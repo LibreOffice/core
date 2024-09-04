6246e86f563a17444e0748492c617272578782a4
Miklos Vajna <vmiklos@collabora.com>
2024-09-04 11:05:56 +0200

    tdf#162782 SVG export: fix handling of semi-transparent text inside a list
    Open the bugdoc, try to export as SVG, results in an assertion failure
    in debug builds, the produced XML would not be well-formed.
    
    Commit 666f252457bdb4371d15380a0289e107b2dfbe84 (SVG export: fix lost
    semi-transparent text on shapes, 2020-07-17) added support for text
    opacity on shapes, but this assumes that the entire shape has the same
    opacity, while this shape has 3 paragraphs and only the middle one has
    an opacity set, at a text span level. Additionally, it's a bullet, so
    the text (for the bullet, has no transparency set) starts before the
    transparency metafile action would start. This means that the existing
    logic won't realize that opacity should be exported using the
    fill-opacity attribute instead of a <g> element.
    
    Fix the problem by checking for the isTextShapeStarted() case in
    SVGActionWriter::ImplWriteMask(): if we're already inside text, then we
    always want to map a transparency mask to the fill-opacity attribute
    instead of a <g> element.
    
    Leave the shape-level code at SVGTextWriter::setTextPosition()
    unchanged, that continues to deal with per-shape text opacity.
    
    Change-Id: I8cb0ca2e839fba911a75e1925cf79145f69af151
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172856
    Tested-by: Jenkins
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 0a89d65e6bb7be293c1a7b4615a08292701694dc)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172823
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit dde9a8b6c6841e89bec958dcfa1bb36d8728598f)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172824

--- filter/source/svg/svgwriter.cxx
+++ filter/source/svg/svgwriter.cxx
@@ -1390,6 +1390,8 @@ void SVGTextWriter::endTextPosition()
 
 bool SVGTextWriter::hasTextOpacity() const { return !maTextOpacity.isEmpty(); }
 
+OUString& SVGTextWriter::getTextOpacity() { return maTextOpacity; }
+
 void SVGTextWriter::implExportHyperlinkIds()
 {
     if( !msHyperlinkIdList.isEmpty() )
@@ -2544,6 +2546,15 @@ void SVGActionWriter::ImplWriteMask(GDIMetaFile& rMtf, const Point& rDestPt, con
     if (nMoveX || nMoveY)
         rMtf.Move(nMoveX, nMoveY);
 
+    std::optional<OUString> oTextOpacity;
+    if (maTextWriter.isTextShapeStarted())
+    {
+        // We're inside <text>, then try to use the fill-opacity attribute instead of a <g> element
+        // to express transparency to ensure well-formed output.
+        oTextOpacity = maTextWriter.getTextOpacity();
+        StartMask(rDestPt, rDestSize, rGradient, nWriteFlags, pColorStops, &maTextWriter.getTextOpacity());
+    }
+
     {
         std::unique_ptr<SvXMLElementExport> pElemG;
         if (!maTextWriter.hasTextOpacity())
@@ -2556,6 +2567,11 @@ void SVGActionWriter::ImplWriteMask(GDIMetaFile& rMtf, const Point& rDestPt, con
         mpVDev->Push();
         ImplWriteActions( rMtf, nWriteFlags, "" );
         mpVDev->Pop();
+    }
+
+    if (oTextOpacity)
+    {
+        maTextWriter.getTextOpacity() = *oTextOpacity;
     }
 }
 
