LibreOffice/core

commit ece0729e18b4b306933439d1a329df192fc504d3

use less exception throwing for flow control
Change-Id: Ib376a1bcb75e72d6e5754818a353b0d9c7dbd26d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171851
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sfx2/source/doc/guisaveas.cxx
@@ -225,20 +225,20 @@ public:
     {
         try
         {
-            uno::Reference< lang::XMultiServiceFactory > xDocSettingsSupplier( xModel, uno::UNO_QUERY_THROW );
-            m_xDocumentSettings.set(
-                xDocSettingsSupplier->createInstance( u"com.sun.star.document.Settings"_ustr ),
-                uno::UNO_QUERY_THROW );
-
-            try
+            uno::Reference< lang::XMultiServiceFactory > xDocSettingsSupplier( xModel, uno::UNO_QUERY );
+            if (xModel)
             {
-                OUString aLoadReadonlyString( u"LoadReadonly"_ustr );
-                m_xDocumentSettings->getPropertyValue( aLoadReadonlyString ) >>= m_bPreserveReadOnly;
-                m_xDocumentSettings->setPropertyValue( aLoadReadonlyString, uno::Any( bReadOnly ) );
-                m_bReadOnlySupported = true;
+                m_xDocumentSettings.set(
+                    xDocSettingsSupplier->createInstance( u"com.sun.star.document.Settings"_ustr ),
+                    uno::UNO_QUERY );
+                if (m_xDocumentSettings)
+                {
+                    OUString aLoadReadonlyString( u"LoadReadonly"_ustr );
+                    m_xDocumentSettings->getPropertyValue( aLoadReadonlyString ) >>= m_bPreserveReadOnly;
+                    m_xDocumentSettings->setPropertyValue( aLoadReadonlyString, uno::Any( bReadOnly ) );
+                    m_bReadOnlySupported = true;
+                }
             }
-            catch( const uno::Exception& )
-            {}
         }
         catch( const uno::Exception& )
         {}

sfx2/source/dialog/dinfdlg.cxx
@@ -1294,10 +1294,13 @@ void SfxDocumentPage::Reset( const SfxItemSet* rSet )
     {
         try
         {
-            uno::Reference< lang::XMultiServiceFactory > xFac( pDocSh->GetModel(), uno::UNO_QUERY_THROW );
-            uno::Reference< beans::XPropertySet > xProps( xFac->createInstance(u"com.sun.star.document.Settings"_ustr), uno::UNO_QUERY_THROW );
-
-            xProps->getPropertyValue(u"ImagePreferredDPI"_ustr) >>= nImagePreferredDPI;
+            uno::Reference< lang::XMultiServiceFactory > xFac( pDocSh->GetModel(), uno::UNO_QUERY );
+            if (xFac)
+            {
+                uno::Reference< beans::XPropertySet > xProps( xFac->createInstance(u"com.sun.star.document.Settings"_ustr), uno::UNO_QUERY );
+                if (xProps)
+                    xProps->getPropertyValue(u"ImagePreferredDPI"_ustr) >>= nImagePreferredDPI;
+            }
         }
         catch( uno::Exception& )
         {

sfx2/source/dialog/documentfontsdialog.cxx
@@ -60,14 +60,19 @@ void SfxDocumentFontsPage::Reset( const SfxItemSet* )
     {
         try
         {
-            uno::Reference< lang::XMultiServiceFactory > xFac( pDocSh->GetModel(), uno::UNO_QUERY_THROW );
-            uno::Reference< beans::XPropertySet > xProps( xFac->createInstance(u"com.sun.star.document.Settings"_ustr), uno::UNO_QUERY_THROW );
-
-            xProps->getPropertyValue(u"EmbedFonts"_ustr) >>= bEmbedFonts;
-            xProps->getPropertyValue(u"EmbedOnlyUsedFonts"_ustr) >>= bEmbedUsedFonts;
-            xProps->getPropertyValue(u"EmbedLatinScriptFonts"_ustr) >>= bEmbedLatinScriptFonts;
-            xProps->getPropertyValue(u"EmbedAsianScriptFonts"_ustr) >>= bEmbedAsianScriptFonts;
-            xProps->getPropertyValue(u"EmbedComplexScriptFonts"_ustr) >>= bEmbedComplexScriptFonts;
+            uno::Reference< lang::XMultiServiceFactory > xFac( pDocSh->GetModel(), uno::UNO_QUERY );
+            if (xFac)
+            {
+                uno::Reference< beans::XPropertySet > xProps( xFac->createInstance(u"com.sun.star.document.Settings"_ustr), uno::UNO_QUERY);
+                if (xProps)
+                {
+                    xProps->getPropertyValue(u"EmbedFonts"_ustr) >>= bEmbedFonts;
+                    xProps->getPropertyValue(u"EmbedOnlyUsedFonts"_ustr) >>= bEmbedUsedFonts;
+                    xProps->getPropertyValue(u"EmbedLatinScriptFonts"_ustr) >>= bEmbedLatinScriptFonts;
+                    xProps->getPropertyValue(u"EmbedAsianScriptFonts"_ustr) >>= bEmbedAsianScriptFonts;
+                    xProps->getPropertyValue(u"EmbedComplexScriptFonts"_ustr) >>= bEmbedComplexScriptFonts;
+                }
+            }
         }
         catch( uno::Exception& )
         {
@@ -95,13 +100,19 @@ bool SfxDocumentFontsPage::FillItemSet( SfxItemSet* )
     {
         try
         {
-            uno::Reference< lang::XMultiServiceFactory > xFac( pDocSh->GetModel(), uno::UNO_QUERY_THROW );
-            uno::Reference< beans::XPropertySet > xProps( xFac->createInstance(u"com.sun.star.document.Settings"_ustr), uno::UNO_QUERY_THROW );
-            xProps->setPropertyValue(u"EmbedFonts"_ustr, uno::Any(bEmbedFonts));
-            xProps->setPropertyValue(u"EmbedOnlyUsedFonts"_ustr, uno::Any(bEmbedUsedFonts));
-            xProps->setPropertyValue(u"EmbedLatinScriptFonts"_ustr, uno::Any(bEmbedLatinScriptFonts));
-            xProps->setPropertyValue(u"EmbedAsianScriptFonts"_ustr, uno::Any(bEmbedAsianScriptFonts));
-            xProps->setPropertyValue(u"EmbedComplexScriptFonts"_ustr, uno::Any(bEmbedComplexScriptFonts));
+            uno::Reference< lang::XMultiServiceFactory > xFac( pDocSh->GetModel(), uno::UNO_QUERY );
+            if (xFac)
+            {
+                uno::Reference< beans::XPropertySet > xProps( xFac->createInstance(u"com.sun.star.document.Settings"_ustr), uno::UNO_QUERY );
+                if (xProps)
+                {
+                    xProps->setPropertyValue(u"EmbedFonts"_ustr, uno::Any(bEmbedFonts));
+                    xProps->setPropertyValue(u"EmbedOnlyUsedFonts"_ustr, uno::Any(bEmbedUsedFonts));
+                    xProps->setPropertyValue(u"EmbedLatinScriptFonts"_ustr, uno::Any(bEmbedLatinScriptFonts));
+                    xProps->setPropertyValue(u"EmbedAsianScriptFonts"_ustr, uno::Any(bEmbedAsianScriptFonts));
+                    xProps->setPropertyValue(u"EmbedComplexScriptFonts"_ustr, uno::Any(bEmbedComplexScriptFonts));
+                }
+            }
         }
         catch( uno::Exception& )
         {

sfx2/source/doc/DocumentSigner.cxx
@@ -98,9 +98,12 @@ bool DocumentSigner::signDocument(uno::Reference<security::XCertificate> const& 
             if (bSuccess)
             {
                 uno::Reference<embed::XTransactedObject> xTransact(xWriteableZipStore,
-                                                                   uno::UNO_QUERY_THROW);
-                xTransact->commit();
-                bResult = true;
+                                                                   uno::UNO_QUERY);
+                if (xTransact)
+                {
+                    xTransact->commit();
+                    bResult = true;
+                }
             }
         }
         else

sfx2/source/doc/docfile.cxx
@@ -4676,12 +4676,15 @@ bool SfxMedium::SwitchDocumentToFile( const OUString& aURL )
         {
             try
             {
-                uno::Reference< io::XTruncate > xTruncate( pImpl->xStream, uno::UNO_QUERY_THROW );
-                xTruncate->truncate();
-                if ( xOptStorage.is() )
-                    xOptStorage->writeAndAttachToStream( pImpl->xStream );
-                pImpl->xStorage = xStorage;
-                bResult = true;
+                uno::Reference< io::XTruncate > xTruncate( pImpl->xStream, uno::UNO_QUERY );
+                if (xTruncate)
+                {
+                    xTruncate->truncate();
+                    if ( xOptStorage.is() )
+                        xOptStorage->writeAndAttachToStream( pImpl->xStream );
+                    pImpl->xStorage = xStorage;
+                    bResult = true;
+                }
             }
             catch( const uno::Exception& )
             {}

sfx2/source/doc/doctemplates.cxx
@@ -674,11 +674,14 @@ void SfxDocTplService::getTitleFromURL( const OUString& rURL, OUString& aTitle, 
         if ( !aDocType.isEmpty() )
             try
             {
-                uno::Reference< container::XNameAccess > xTypeDetection( mxType, uno::UNO_QUERY_THROW );
-                SequenceAsHashMap aTypeProps( xTypeDetection->getByName( aDocType ) );
-                aType = aTypeProps.getUnpackedValueOrDefault(
-                            u"MediaType"_ustr,
-                            OUString() );
+                uno::Reference< container::XNameAccess > xTypeDetection( mxType, uno::UNO_QUERY );
+                if (xTypeDetection)
+                {
+                    SequenceAsHashMap aTypeProps( xTypeDetection->getByName( aDocType ) );
+                    aType = aTypeProps.getUnpackedValueOrDefault(
+                                u"MediaType"_ustr,
+                                OUString() );
+                }
             }
             catch( uno::Exception& )
             {}

sfx2/source/doc/objserv.cxx
@@ -1839,8 +1839,9 @@ uno::Sequence< security::DocumentSignatureInformation > SfxObjectShell::GetDocum
                 OUString aVersion;
                 try
                 {
-                    uno::Reference < beans::XPropertySet > xPropSet( GetStorage(), uno::UNO_QUERY_THROW );
-                    xPropSet->getPropertyValue(u"Version"_ustr) >>= aVersion;
+                    uno::Reference < beans::XPropertySet > xPropSet( GetStorage(), uno::UNO_QUERY );
+                    if (xPropSet)
+                        xPropSet->getPropertyValue(u"Version"_ustr) >>= aVersion;
                 }
                 catch( uno::Exception& )
                 {

sfx2/source/doc/objstor.cxx
@@ -1532,8 +1532,9 @@ bool SfxObjectShell::SaveTo_Impl
             OUString aODFVersion;
             try
             {
-                uno::Reference < beans::XPropertySet > xPropSet( GetStorage(), uno::UNO_QUERY_THROW );
-                xPropSet->getPropertyValue(u"Version"_ustr) >>= aODFVersion;
+                uno::Reference < beans::XPropertySet > xPropSet( GetStorage(), uno::UNO_QUERY );
+                if (xPropSet)
+                    xPropSet->getPropertyValue(u"Version"_ustr) >>= aODFVersion;
             }
             catch( uno::Exception& )
             {}
@@ -1769,9 +1770,10 @@ bool SfxObjectShell::SaveTo_Impl
 
                 try
                 {
-                    uno::Reference< beans::XPropertySet > xProps( rMedium.GetStorage(), uno::UNO_QUERY_THROW );
-                    xProps->setPropertyValue(u"MediaType"_ustr,
-                                            uno::Any( aDataFlavor.MimeType ) );
+                    uno::Reference< beans::XPropertySet > xProps( rMedium.GetStorage(), uno::UNO_QUERY );
+                    if (xProps)
+                        xProps->setPropertyValue(u"MediaType"_ustr,
+                                                uno::Any( aDataFlavor.MimeType ) );
                 }
                 catch( uno::Exception& )
                 {
@@ -1975,8 +1977,9 @@ bool SfxObjectShell::SaveTo_Impl
                 OUString aVersion;
                 try
                 {
-                    uno::Reference < beans::XPropertySet > xPropSet( rMedium.GetStorage(), uno::UNO_QUERY_THROW );
-                    xPropSet->getPropertyValue(u"Version"_ustr) >>= aVersion;
+                    uno::Reference < beans::XPropertySet > xPropSet( rMedium.GetStorage(), uno::UNO_QUERY );
+                    if (xPropSet)
+                        xPropSet->getPropertyValue(u"Version"_ustr) >>= aVersion;
                 }
                 catch( uno::Exception& )
                 {
@@ -2289,15 +2292,17 @@ bool SfxObjectShell::ConnectTmpStorage_Impl(
                 // Get rid of this workaround after issue i113914 is fixed
                 try
                 {
-                    uno::Reference< script::XStorageBasedLibraryContainer > xBasicLibraries( pImpl->xBasicLibraries, uno::UNO_QUERY_THROW );
-                    xBasicLibraries->setRootStorage( xTmpStorage );
+                    uno::Reference< script::XStorageBasedLibraryContainer > xBasicLibraries( pImpl->xBasicLibraries, uno::UNO_QUERY );
+                    if (xBasicLibraries)
+                        xBasicLibraries->setRootStorage( xTmpStorage );
                 }
                 catch( uno::Exception& )
                 {}
                 try
                 {
-                    uno::Reference< script::XStorageBasedLibraryContainer > xDialogLibraries( pImpl->xDialogLibraries, uno::UNO_QUERY_THROW );
-                    xDialogLibraries->setRootStorage( xTmpStorage );
+                    uno::Reference< script::XStorageBasedLibraryContainer > xDialogLibraries( pImpl->xDialogLibraries, uno::UNO_QUERY );
+                    if (xDialogLibraries)
+                        xDialogLibraries->setRootStorage( xTmpStorage );
                 }
                 catch( uno::Exception& )
                 {}
@@ -2453,15 +2458,17 @@ bool SfxObjectShell::DoSaveCompleted( SfxMedium* pNewMed, bool bRegisterRecent )
         // Get rid of this workaround after issue i113914 is fixed
         try
         {
-            uno::Reference< script::XStorageBasedLibraryContainer > xBasicLibraries( pImpl->xBasicLibraries, uno::UNO_QUERY_THROW );
-            xBasicLibraries->setRootStorage( xStorage );
+            uno::Reference< script::XStorageBasedLibraryContainer > xBasicLibraries( pImpl->xBasicLibraries, uno::UNO_QUERY );
+            if (xBasicLibraries)
+                xBasicLibraries->setRootStorage( xStorage );
         }
         catch( uno::Exception& )
         {}
         try
         {
-            uno::Reference< script::XStorageBasedLibraryContainer > xDialogLibraries( pImpl->xDialogLibraries, uno::UNO_QUERY_THROW );
-            xDialogLibraries->setRootStorage( xStorage );
+            uno::Reference< script::XStorageBasedLibraryContainer > xDialogLibraries( pImpl->xDialogLibraries, uno::UNO_QUERY );
+            if (xDialogLibraries)
+                xDialogLibraries->setRootStorage( xStorage );
         }
         catch( uno::Exception& )
         {}
@@ -4137,9 +4144,12 @@ bool SfxObjectShell::GenerateAndStoreThumbnail(bool bEncrypted, const uno::Refer
 
             if (xStream.is() && WriteThumbnail(bEncrypted, xStream))
             {
-                uno::Reference<embed::XTransactedObject> xTransactedObject(xThumbnailStorage, uno::UNO_QUERY_THROW);
-                xTransactedObject->commit();
-                bResult = true;
+                uno::Reference<embed::XTransactedObject> xTransactedObject(xThumbnailStorage, uno::UNO_QUERY);
+                if (xTransactedObject)
+                {
+                    xTransactedObject->commit();
+                    bResult = true;
+                }
             }
         }
     }

commit 70b6d04570b22e766d1f04921cf05f9f9cdd3108

tdf#162728 Crash on saving in Math Formula editor
regression from
    commit ece0729e18b4b306933439d1a329df192fc504d3
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Tue Aug 13 13:43:25 2024 +0200
    use less exception throwing for flow control

Change-Id: I52dcb717e309c70d10a0609b04b2c9c659c423cf
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172908
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sfx2/source/doc/guisaveas.cxx
@@ -226,7 +226,7 @@ public:
         try
         {
             uno::Reference< lang::XMultiServiceFactory > xDocSettingsSupplier( xModel, uno::UNO_QUERY );
-            if (xModel)
+            if (xDocSettingsSupplier)
             {
                 m_xDocumentSettings.set(
                     xDocSettingsSupplier->createInstance( u"com.sun.star.document.Settings"_ustr ),