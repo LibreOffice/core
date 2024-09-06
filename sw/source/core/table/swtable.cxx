LibreOffice/core

commit ae7807c889c19145f89cec40afac82eee191837c

ITEM: No longer register Items at Pool
The issue is that the flag RegisteredAtPool
at the SfxPoolItem is Pool-dependent: It marks that
the Item is registeres at *one* Pool/Model. This
makes it Pool-dependent. Due to this there is no way
to share Items that need to be registered globally/
in multiple Pools/Models what is one of the goals
for optimal sharing.

We can also not live without having access to all
Items associated with the Pool, due to mechanisms
in place like the Surrogate stuff.

This again is used for two purposes:

(1) Access all Items associated with one Pool/Model,
at least that is the assumption. This is not valid
since it gets corrupted with a single ItemSet/Holder
used that does not host model data, e.g. an open
Dialog or the Sidebar (or...). But works in
principle.

(2) Access extra-Items that are held nowhere and
are created using DirectPutItemInPool, e.g. infos
for a Dialog. These would need a instance/place to
host them, the Pool is (ab)used for that.

Both are 'compromizes' (to not use a more bad word)
and should not exist. (1) should iterate over the
Model and do actions. There are even places that
use (1) to *change* Items, by casting them to
non-const, even RefCounted ones, so having no control
over what all might be changed doing so. Since we
talk about ca. 100+ places there is no way to get
away from this - I can imagine how this happened:
The 'poolable' attr traditionally needed for the old
binary format was one day 'used' to not need to
iterate over the Model, an API was added to access
and this usage was copied. Sigh..

It is even used when ODF is loaded: E.g. the
FillStyle is imported from XML, interpreted, and
put into an ItemSet. Then it gets set at the
XShape using UNO API and a *name* -> that name and
the Surrogate mechanism is used to find and set the
FillStyle at the Model Object. The FillStyle could
probably just be set using UNO API and the data
directly.

The association between Model/Pool and Item is

sw/source/core/table/swtable.cxx
@@ -1630,7 +1630,9 @@ bool SwTable::IsDeleted() const
 
 void SwTable::GatherFormulas(std::vector<SwTableBoxFormula*>& rvFormulas)
 {
-    for(const SfxPoolItem* pItem: GetFrameFormat()->GetDoc()->GetAttrPool().GetItemSurrogates(RES_BOXATR_FORMULA))
+    ItemSurrogates aSurrogates;
+    GetFrameFormat()->GetDoc()->GetAttrPool().GetItemSurrogates(aSurrogates, RES_BOXATR_FORMULA);
+    for(const SfxPoolItem* pItem: aSurrogates)
     {
         auto pBoxFormula = dynamic_cast<const SwTableBoxFormula*>(pItem);
         assert(pBoxFormula); // use StaticWhichCast instead?
@@ -1720,7 +1722,9 @@ void SwTable::UpdateFields(TableFormulaUpdateFlags eFlags)
         }
     }
     // process all table box formulas
-    for(const SfxPoolItem* pItem : pDoc->GetAttrPool().GetItemSurrogates(RES_BOXATR_FORMULA))
+    ItemSurrogates aSurrogates;
+    pDoc->GetAttrPool().GetItemSurrogates(aSurrogates, RES_BOXATR_FORMULA);
+    for(const SfxPoolItem* pItem : aSurrogates)
     {
         auto pBoxFormula = const_cast<SwTableBoxFormula*>(pItem->DynamicWhichCast(RES_BOXATR_FORMULA));
         if(pBoxFormula && pBoxFormula->GetDefinedIn())

compilerplugins/clang/store/staticvar.cxx
@@ -53,7 +53,6 @@ public:
             || fn == SRCDIR "/sal/qa/rtl/digest/rtl_digest.cxx"
             || fn == SRCDIR "/sal/qa/rtl/strings/test_oustring_endswith.cxx"
             || fn == SRCDIR "/sal/qa/rtl/strings/test_oustring_convert.cxx"
-            || fn == SRCDIR "/svl/qa/unit/items/test_itempool.cxx"
             // contains mutable state
             || fn == SRCDIR "/vcl/unx/generic/dtrans/X11_selection.cxx"
             || fn == SRCDIR "/sax/qa/cppunit/xmlimport.cxx"

cui/source/tabpages/tabarea.cxx
@@ -83,7 +83,7 @@ void SvxAreaTabDialog::SavePalettes()
         if ( pShell )
             pShell->PutItem( aColorListItem );
         else
-            mpDrawModel->GetItemPool().DirectPutItemInPool(aColorListItem,SID_COLOR_TABLE);
+            mpDrawModel->GetItemPool().DirectPutItemInPool(aColorListItem);
         mpColorList = mpDrawModel->GetColorList();
     }
     if( mpNewGradientList != mpDrawModel->GetGradientList() )
@@ -93,7 +93,7 @@ void SvxAreaTabDialog::SavePalettes()
         if ( pShell )
             pShell->PutItem( aItem );
         else
-            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem,SID_GRADIENT_LIST);
+            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem);
         mpGradientList = mpDrawModel->GetGradientList();
     }
     if( mpNewHatchingList != mpDrawModel->GetHatchList() )
@@ -103,7 +103,7 @@ void SvxAreaTabDialog::SavePalettes()
         if ( pShell )
             pShell->PutItem( aItem );
         else
-            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem,SID_HATCH_LIST);
+            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem);
         mpHatchingList = mpDrawModel->GetHatchList();
     }
     if( mpNewBitmapList != mpDrawModel->GetBitmapList() )
@@ -113,7 +113,7 @@ void SvxAreaTabDialog::SavePalettes()
         if ( pShell )
             pShell->PutItem( aItem );
         else
-            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem,SID_BITMAP_LIST);
+            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem);
         mpBitmapList = mpDrawModel->GetBitmapList();
     }
     if( mpNewPatternList != mpDrawModel->GetPatternList() )
@@ -123,7 +123,7 @@ void SvxAreaTabDialog::SavePalettes()
         if( pShell )
             pShell->PutItem( aItem );
         else
-            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem,SID_PATTERN_LIST);
+            mpDrawModel->GetItemPool().DirectPutItemInPool(aItem);
         mpPatternList = mpDrawModel->GetPatternList();
     }
 

editeng/source/editeng/impedit4.cxx
@@ -306,7 +306,9 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel )
         else if ( nScriptType == 2 )
             nWhich = EE_CHAR_FONTINFO_CTL;
 
-        for (const SfxPoolItem* pItem : maEditDoc.GetItemPool().GetItemSurrogates(nWhich))
+        ItemSurrogates aSurrogates;
+        maEditDoc.GetItemPool().GetItemSurrogates(aSurrogates, nWhich);
+        for (const SfxPoolItem* pItem : aSurrogates)
         {
             SvxFontItem const*const pFontItem = static_cast<const SvxFontItem*>(pItem);
             bool bAlreadyExist = false;
@@ -390,7 +392,9 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel )
     {
         aColorList.push_back(rDefault.GetValue());
     }
-    for (const SfxPoolItem* pItem : maEditDoc.GetItemPool().GetItemSurrogates(EE_CHAR_COLOR))
+    ItemSurrogates aSurrogates;
+    maEditDoc.GetItemPool().GetItemSurrogates(aSurrogates, EE_CHAR_COLOR);
+    for (const SfxPoolItem* pItem : aSurrogates)
     {
         auto pColorItem(dynamic_cast<SvxColorItem const*>(pItem));
         if (pColorItem && pColorItem->GetValue() != COL_AUTO) // may be null!

include/svl/itempool.hxx
@@ -29,9 +29,7 @@
 #include <unordered_set>
 #include <o3tl/sorted_vector.hxx>
 #include <salhelper/simplereferenceobject.hxx>
-
-class SfxBroadcaster;
-struct SfxItemPool_Impl;
+#include <svl/SfxBroadcaster.hxx>
 
 struct SfxItemInfo
 {
@@ -52,13 +50,16 @@ struct SfxItemInfo
     bool             _bShareable : 1;
 };
 
-class SfxItemPool;
-typedef std::unordered_set<const SfxPoolItem*> registeredSfxPoolItems;
-
 #ifdef DBG_UTIL
 SVL_DLLPUBLIC size_t getAllDirectlyPooledSfxPoolItemCount();
 SVL_DLLPUBLIC size_t getRemainingDirectlyPooledSfxPoolItemCount();
 #endif
+
+typedef std::unordered_set<const SfxItemSet*> registeredSfxItemSets;
+class SfxPoolItemHolder;
+typedef std::unordered_set<const SfxPoolItemHolder*> registeredSfxPoolItemHolders;
+typedef std::unordered_set<SfxPoolItemHolder*> directPutSfxPoolItemHolders;
+typedef std::vector<const SfxPoolItem*> ItemSurrogates;
 
 /** Base class for providers of defaults of SfxPoolItems.
  *
@@ -69,21 +70,33 @@ SVL_DLLPUBLIC size_t getRemainingDirectlyPooledSfxPoolItemCount();
  */
 class SVL_DLLPUBLIC SfxItemPool : public salhelper::SimpleReferenceObject
 {
-    friend struct SfxItemPool_Impl;
     friend class SfxItemSet;
+    friend class SfxPoolItemHolder;
     friend class SfxAllItemSet;
 
     // allow ItemSetTooling to access
     friend SfxPoolItem const* implCreateItemEntry(SfxItemPool&, SfxPoolItem const*, sal_uInt16, bool);
-    friend void implCleanupItemEntry(SfxItemPool&, SfxPoolItem const*);
+    friend void implCleanupItemEntry(SfxPoolItem const*);
 
     // unit testing
     friend class PoolItemTest;
 
     const SfxItemInfo*              pItemInfos;
-    std::unique_ptr<SfxItemPool_Impl>               pImpl;
+    SfxBroadcaster                  aBC;
+    OUString                        aName;
+    std::vector<SfxPoolItem*>       maPoolDefaults;
+    std::vector<SfxPoolItem*>*      mpStaticDefaults;
+    SfxItemPool*                    mpMaster;
+    rtl::Reference<SfxItemPool>     mpSecondary;
+    WhichRangesContainer            mpPoolRanges;
+    sal_uInt16                      mnStart;
+    sal_uInt16                      mnEnd;
+    MapUnit                         eDefMetric;
 
-    registeredSfxPoolItems** ppRegisteredSfxPoolItems;
+    registeredSfxItemSets maRegisteredSfxItemSets;
+    registeredSfxPoolItemHolders maRegisteredSfxPoolItemHolders;
+    directPutSfxPoolItemHolders maDirectPutItems;
+    bool mbPreDeleteDone;
 
 private:
     sal_uInt16                      GetIndex_Impl(sal_uInt16 nWhich) const;
@@ -93,6 +106,14 @@ private:
     { return pItemInfos[nPos]._bNeedsPoolRegistration; }
     SVL_DLLPRIVATE bool             Shareable_Impl(sal_uInt16 nPos) const
     { return pItemInfos[nPos]._bShareable; }
+
+    void registerItemSet(SfxItemSet& rSet);
+    void unregisterItemSet(SfxItemSet& rSet);
+
+    void registerPoolItemHolder(SfxPoolItemHolder& rHolder);
+    void unregisterPoolItemHolder(SfxPoolItemHolder& rHolder);
+
+    void CollectSurrogates(std::unordered_set<const SfxPoolItem*>& rTarget, sal_uInt16 nWhich) const;
 
 public:
     // for default SfxItemSet::CTOR, set default WhichRanges
@@ -112,7 +133,6 @@ public:
                                                  const SfxItemInfo *pItemInfos,
                                                  std::vector<SfxPoolItem*> *pDefaults = nullptr );
 
-public:
     virtual                         ~SfxItemPool();
 
     SfxBroadcaster&                 BC();
@@ -167,34 +187,24 @@ public:
     virtual rtl::Reference<SfxItemPool> Clone() const;
     const OUString&                 GetName() const;
 
-    template<class T> const T&      DirectPutItemInPool( std::unique_ptr<T> xItem, sal_uInt16 nWhich = 0 )
-    { return static_cast<const T&>(DirectPutItemInPoolImpl( *xItem.release(), nWhich, /*bPassingOwnership*/true)); }
-    template<class T> const T&      DirectPutItemInPool( const T& rItem, sal_uInt16 nWhich = 0 )
-    { return static_cast<const T&>(DirectPutItemInPoolImpl( rItem, nWhich, /*bPassingOwnership*/false)); }
-    void                            DirectRemoveItemFromPool( const SfxPoolItem& );
+    const SfxPoolItem& DirectPutItemInPool(const SfxPoolItem&);
+    void DirectRemoveItemFromPool(const SfxPoolItem&);
 
     const SfxPoolItem&              GetDefaultItem( sal_uInt16 nWhich ) const;
     template<class T> const T&      GetDefaultItem( TypedWhichId<T> nWhich ) const
     { return static_cast<const T&>(GetDefaultItem(sal_uInt16(nWhich))); }
-
-    struct Item2Range
-    {
-        o3tl::sorted_vector<SfxPoolItem*>::const_iterator m_begin;
-        o3tl::sorted_vector<SfxPoolItem*>::const_iterator m_end;
-        o3tl::sorted_vector<SfxPoolItem*>::const_iterator const & begin() const { return m_begin; }
-        o3tl::sorted_vector<SfxPoolItem*>::const_iterator const & end() const { return m_end; }
-    };
     const SfxPoolItem *             GetItem2Default(sal_uInt16 nWhich) const;
     template<class T> const T*      GetItem2Default( TypedWhichId<T> nWhich ) const
     { return static_cast<const T*>(GetItem2Default(sal_uInt16(nWhich))); }
 
-    const registeredSfxPoolItems&   GetItemSurrogates(sal_uInt16 nWhich) const;
+public:
+    void GetItemSurrogates(ItemSurrogates& rTarget, sal_uInt16 nWhich) const;
     /*
         This is only valid for SfxPoolItem that override IsSortable and operator<.
         Returns a range of items defined by using operator<.
         @param rNeedle must be the same type or a supertype of the pool items for nWhich.
     */
-    std::vector<const SfxPoolItem*> FindItemSurrogate(sal_uInt16 nWhich, SfxPoolItem const & rNeedle) const;
+    void FindItemSurrogate(ItemSurrogates& rTarget, sal_uInt16 nWhich, SfxPoolItem const & rNeedle) const;
 
     sal_uInt16                      GetFirstWhich() const;
     sal_uInt16                      GetLastWhich() const;
@@ -229,24 +239,6 @@ public:
                                         return nId && nId <= SFX_WHICH_MAX; }
     static bool                     IsSlot(sal_uInt16 nId) {
                                         return nId && nId > SFX_WHICH_MAX; }
-
-    // This method will try to register the Item at this Pool.
-    void registerSfxPoolItem(const SfxPoolItem& rItem);
-
-    // this method will unregister an Item from this Pool
-    void unregisterSfxPoolItem(const SfxPoolItem& rItem);
-
-    // check if this Item is registered at this Pool, needed to detect
-    // if an Item is to be set at another Pool and needs to be cloned
-    bool isSfxPoolItemRegisteredAtThisPool(const SfxPoolItem& rItem) const;
-
-    // try to find an equal existing Item to given one in pool
-    const SfxPoolItem* tryToGetEqualItem(const SfxPoolItem& rItem, sal_uInt16 nWhich) const;
-
-    void                            dumpAsXml(xmlTextWriterPtr pWriter) const;
-
-protected:
-    const SfxPoolItem&      DirectPutItemInPoolImpl( const SfxPoolItem&, sal_uInt16 nWhich = 0, bool bPassingOwnership = false );
 
 private:
     const SfxItemPool&              operator=(const SfxItemPool &) = delete;

include/svl/itemset.hxx
@@ -40,7 +40,7 @@ SVL_DLLPUBLIC size_t getUsedSfxPoolItemHolderCount();
 
 // ItemSet/ItemPool helpers
 SfxPoolItem const* implCreateItemEntry(SfxItemPool& rPool, SfxPoolItem const* pSource, sal_uInt16 nWhich, bool bPassingOwnership);
-void implCleanupItemEntry(SfxItemPool& rPool, SfxPoolItem const* pSource);
+void implCleanupItemEntry(SfxPoolItem const* pSource);
 
 class SAL_WARN_UNUSED SVL_DLLPUBLIC SfxPoolItemHolder
 {
@@ -79,11 +79,12 @@ class SAL_WARN_UNUSED SVL_DLLPUBLIC SfxItemSet
 
     // allow ItemSetTooling to access
     friend SfxPoolItem const* implCreateItemEntry(SfxItemPool&, SfxPoolItem const*, sal_uInt16, bool);
-    friend void implCleanupItemEntry(SfxItemPool&, SfxPoolItem const*);
+    friend void implCleanupItemEntry(SfxPoolItem const*);
 
     SfxItemPool*      m_pPool;         ///< pool that stores the items
     const SfxItemSet* m_pParent;       ///< derivation
     sal_uInt16        m_nCount;        ///< number of items
+    sal_uInt16        m_nRegister;     ///< number of items with NeedsPoolRegistration
     sal_uInt16        m_nTotalCount;   ///< number of WhichIDs, also size of m_ppItems array
 
     // bitfield (better packaging if a bool needs to be added)
@@ -94,6 +95,10 @@ class SAL_WARN_UNUSED SVL_DLLPUBLIC SfxItemSet
 
     // Notification-Callback mechanism for SwAttrSet in SW, functionPtr for callback
     std::function<void(const SfxPoolItem*, const SfxPoolItem*)> m_aCallback;
+
+    // helpers to keep m_nRegister up-to-date
+    void checkRemovePoolRegistration(const SfxPoolItem* pItem);
+    void checkAddPoolRegistration(const SfxPoolItem* pItem);
 
 protected:
     // Notification-Callback mechanism for SwAttrSet in SW

include/svl/poolitem.hxx
@@ -115,7 +115,7 @@ class SVL_DLLPUBLIC SfxPoolItem
 
     // allow ItemSetTooling to access
     friend SfxPoolItem const* implCreateItemEntry(SfxItemPool&, SfxPoolItem const*, sal_uInt16, bool);
-    friend void implCleanupItemEntry(SfxItemPool&, SfxPoolItem const*);
+    friend void implCleanupItemEntry(SfxPoolItem const*);
 
     mutable sal_uInt32 m_nRefCount;
     sal_uInt16  m_nWhich;
@@ -133,13 +133,12 @@ class SVL_DLLPUBLIC SfxPoolItem
     bool        m_bIsVoidItem : 1;          // bit 0
     bool        m_bStaticDefault : 1;       // bit 1
     bool        m_bPoolDefault : 1;         // bit 2
-    bool        m_bRegisteredAtPool : 1;    // bit 3
-    bool        m_bIsSetItem : 1;           // bit 5
+    bool        m_bIsSetItem : 1;           // bit 3
 
 protected:
 #ifdef DBG_UTIL
     // this flag will make debugging item stuff much simpler
-    bool        m_bDeleted : 1;             // bit 6
+    bool        m_bDeleted : 1;             // bit 4
 #endif
 
 private:
@@ -153,7 +152,6 @@ protected:
     void setIsVoidItem() { m_bIsVoidItem = true; }
     void setStaticDefault() { m_bStaticDefault = true; }
     void setPoolDefault() { m_bPoolDefault = true; }
-    void setRegisteredAtPool(bool bNew) { m_bRegisteredAtPool = bNew; }
     void setIsSetItem() { m_bIsSetItem = true; }
 
 public:
@@ -171,7 +169,6 @@ public:
     bool isVoidItem() const { return m_bIsVoidItem; }
     bool isStaticDefault() const { return m_bStaticDefault; }
     bool isPoolDefault() const { return m_bPoolDefault; }
-    bool isRegisteredAtPool() const { return m_bRegisteredAtPool; }
     bool isSetItem() const { return m_bIsSetItem; }
 
     // version that allows nullptrs

sc/inc/fillinfo.hxx
@@ -119,7 +119,7 @@ struct ScCellInfo
         , pConditionSet(nullptr)
         , pDataBar(nullptr)
         , pIconSet(nullptr)
-        , pBackground(nullptr)   // TODO: omit?
+        , maBackground()
         , pLinesAttr(nullptr)
         , mpTLBRLine(nullptr)
         , mpBLTRLine(nullptr)
@@ -156,7 +156,7 @@ struct ScCellInfo
     const ScDataBarInfo*        pDataBar;
     const ScIconSetInfo*        pIconSet;
 
-    const SvxBrushItem*         pBackground;
+    SfxPoolItemHolder           maBackground;
 
     const SvxBoxItem*           pLinesAttr;         /// original item from document.
     const SvxLineItem*          mpTLBRLine;         /// original item from document.

commit 7b16c7db04affde593bfaf75965c4271e4c3ea58

use less GetItemSurrogates in SwTable::UpdateFields
which speeds up loading a large document with lots
of nested tables from several minutes to under 15s

Regression from
    commit ae7807c889c19145f89cec40afac82eee191837c
    Author: Armin Le Grand (allotropia) <armin.le.grand.extern@allotropia.de>
    Date:   Wed Jan 10 20:07:47 2024 +0100
    ITEM: No longer register Items at Pool

Change-Id: If44bbc50074fc324e9458a4ac498f402500981e4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172937
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/table/swtable.cxx
@@ -1713,21 +1713,26 @@ void SwTable::UpdateFields(TableFormulaUpdateFlags eFlags)
                 break;
         }
     }
+
     // process all table box formulas
-    ItemSurrogates aSurrogates;
-    pDoc->GetAttrPool().GetItemSurrogates(aSurrogates, RES_BOXATR_FORMULA);
-    for(const SfxPoolItem* pItem : aSurrogates)
+    SwTableLines& rTableLines = GetTabLines();
+    for (SwTableLine* pTableLine : rTableLines)
     {
-        // SwTableBoxFormula is non-shareable, so const_cast is somewhat OK
-        auto & rBoxFormula = const_cast<SwTableBoxFormula&>(static_cast<const SwTableBoxFormula&>(*pItem));
-        if(rBoxFormula.GetDefinedIn())
+        SwTableBoxes& rTableBoxes = pTableLine->GetTabBoxes();
+        for (SwTableBox* pTableBox : rTableBoxes)
         {
-            if(eFlags == TBL_BOXPTR)
-                rBoxFormula.TryBoxNmToPtr();
-            else if(eFlags == TBL_RELBOXNAME)
-                rBoxFormula.TryRelBoxNm();
-            else
-                rBoxFormula.ChangeState();
+            SwTableBoxFormat* pTableBoxFormat = static_cast<SwTableBoxFormat*>(pTableBox->GetFrameFormat());
+            if (const SwTableBoxFormula* pItem = pTableBoxFormat->GetItemIfSet( RES_BOXATR_FORMULA, false ))
+            {
+                // SwTableBoxFormula is non-shareable, so const_cast is somewhat OK
+                auto & rBoxFormula = const_cast<SwTableBoxFormula&>(*pItem);
+                if(eFlags == TBL_BOXPTR)
+                    rBoxFormula.TryBoxNmToPtr();
+                else if(eFlags == TBL_RELBOXNAME)
+                    rBoxFormula.TryRelBoxNm();
+                else
+                    rBoxFormula.ChangeState();
+            }
         }
     }
 }