LibreOffice/core

commit e2f3ba12c238c895dfd61ad09fcd76789ebbfd53

sw: fix hiding of flys in hidden sections, part 2
Didn't notice that this depends on a global variable "bObjsDirect" so it
only works for some documents; move the HideAndShowObjects() call to the
end so it works in all cases.

(follow-up to commit 8a13277f797c6e2f1b0d9060ad6e5e4d72eb76d0)

Change-Id: I26a7cecdb660179f3ac1c3ea1e5d19067806b741
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172915
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/source/core/layout/frmtool.cxx
@@ -1587,6 +1587,8 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
     //the SwActualSection class has a member, which points to an upper(section).
     //When the "inner" section finishes, the upper will used instead.
 
+    std::vector<SwSectionFrame *> newHiddenSections;
+
     // Do not consider the end node. The caller (Section/MakeFrames()) has to
     // ensure that the end of this range is positioned before EndIndex!
     for ( ; nEndIndex == SwNodeOffset(0) || nIndex < nEndIndex; ++nIndex)
@@ -1908,12 +1910,8 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
             pActualSection.reset(pActualSection->GetUpper());
             pLay = pLay->FindSctFrame();
             if (pLay->IsHiddenNow())
-            {   // flys were created visible
-                for (SwFlowFrame * pSect = SwFlowFrame::CastFlowFrame(pLay);
-                        pSect; pSect = pSect->GetPrecede())
-                {   // flys were created visible
-                    pSect->GetFrame().HideAndShowObjects();
-                }
+            {
+                newHiddenSections.push_back(static_cast<SwSectionFrame*>(pLay));
             }
             if ( pActualSection )
             {
@@ -1948,11 +1946,7 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                     }
                     else if (pOuterSectionFrame->IsHiddenNow())
                     {
-                        for (SwFlowFrame * pSect = SwFlowFrame::CastFlowFrame(pOuterSectionFrame);
-                                pSect; pSect = pSect->GetPrecede())
-                        {   // flys were created visible
-                            pSect->GetFrame().HideAndShowObjects();
-                        }
+                        newHiddenSections.push_back(pOuterSectionFrame);
                     }
                 }
                 else
@@ -2023,6 +2017,15 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
         if ( !isFlyCreationSuppressed )
             AppendAllObjs( pTable, pLayout );
         bObjsDirect = true;
+    }
+
+    // do it after AppendAllObjs()
+    for (SwSectionFrame * pNew : newHiddenSections)
+    {
+        for (SwFlowFrame * pSect = pNew; pSect; pSect = pSect->GetPrecede())
+        {   // flys were created visible; section may be paginated so iterate
+            pSect->GetFrame().HideAndShowObjects();
+        }
     }
 
     if( pPageMaker )

commit 0c6ec63c81676ed675931d9aa8a78d91ed0d9b6e

sw: fix hiding of flys in hidden sections, part 3
Sigh... the new section frame can be deleted! Missed that...

(regression from commit e2f3ba12c238c895dfd61ad09fcd76789ebbfd53)

Change-Id: I48f2b09ececf8fb8469c53a5ec0a3c75be5f7306
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172966
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/frmtool.cxx
@@ -1909,10 +1909,6 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
             //section again.
             pActualSection.reset(pActualSection->GetUpper());
             pLay = pLay->FindSctFrame();
-            if (pLay->IsHiddenNow())
-            {
-                newHiddenSections.push_back(static_cast<SwSectionFrame*>(pLay));
-            }
             if ( pActualSection )
             {
                 //Could be, that the last SectionFrame remains empty.
@@ -1927,6 +1923,10 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
                 }
                 else
                 {
+                    if (pLay->IsHiddenNow())
+                    {
+                        newHiddenSections.push_back(static_cast<SwSectionFrame*>(pLay));
+                    }
                     pPrv = pLay;
                     pLay = pLay->GetUpper();
                 }
@@ -1970,6 +1970,10 @@ void InsertCnt_( SwLayoutFrame *pLay, SwDoc *pDoc,
             }
             else
             {
+                if (pLay->IsHiddenNow())
+                {
+                    newHiddenSections.push_back(static_cast<SwSectionFrame*>(pLay));
+                }
                 //Nothing more with sections, it goes on right behind
                 //the SectionFrame.
                 pPrv = pLay;