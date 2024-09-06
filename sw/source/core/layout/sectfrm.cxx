LibreOffice/core

commit ff7f1b59e22092d8548459e75fe912db852f056f

tdf#159565 prerequisite: make hidden sections have zero-height frames
As mentioned in commit bb733957dd39e6f0b9d80bb59eb0177188794797 (tdf#114973
sw: enable SelectAll with hidden para at start/end, 2023-01-27), the hidden
sections didn't have frames. That prevented correct handling of the case
when such a frame was in the beginning of the document.

This change re-implements the hidden section to use 0-height frames, like
hidden paragraphs, as a pre-requisite for a follow-up change.

Some layout breakages noticed while working on this are unit-tested now.

This change needed to handle the case when the first section is hidden, and
then goes a page break with page style. In this case, the page style must
apply to the very first page of the document. Implementing this now, when
the frame that defines the page style is not the first in the document, I
accidentally fixed also the previously broken case when the first paragraph
was hidden. Now the page style defined in the second paragraph's page break
will apply correctly.

This change makes hidden sections break outer section's frames. This means
that when text borders are shown, there will be an artifact in the place of
the hidden sections (a horizontal line breaking outer frame). I suppose
it's not a problem, actually helping to see the layout better, so in line
with the "show text borders" helper functionality. If this proves to be
problematic, this can be handled specially in a follow-up.

Change-Id: I14ebf0559b463186aba28902cd10c5cc978ba456
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163151
Tested-by: Mike Kaganski <mike.kaganski@collabora.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/163477
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/sectfrm.cxx
@@ -189,6 +189,13 @@ SwSectionFrame::~SwSectionFrame()
 {
 }
 
+//virtual
+bool SwSectionFrame::IsHiddenNow() const
+{
+    const auto* pSection = GetSection();
+    return !pSection || pSection->CalcHiddenFlag();
+}
+
 void SwSectionFrame::DelEmpty( bool bRemove )
 {
     if( IsColLocked() )
@@ -1371,6 +1378,20 @@ void SwSectionFrame::Format( vcl::RenderContext* pRenderContext, const SwBorderA
 
     SwRectFnSet aRectFnSet(this);
 
+    if (GetSection()->CalcHiddenFlag())
+    {
+        {
+            SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*this);
+            aRectFnSet.SetHeight(aFrm, 0);
+        }
+        {
+            SwFrameAreaDefinition::FramePrintAreaWriteAccess aPrt(*this);
+            aRectFnSet.SetHeight(aPrt, 0);
+        }
+        setFrameAreaSizeValid(true);
+        setFramePrintAreaValid(true);
+    }
+
     if ( !isFramePrintAreaValid() )
     {
         PROTOCOL( this, PROT::PrintArea, DbgAction::NONE, nullptr )
@@ -2182,6 +2203,11 @@ bool SwSectionFrame::Growable() const
 
 SwTwips SwSectionFrame::Grow_( SwTwips nDist, bool bTst )
 {
+    if (GetSection()->CalcHiddenFlag())
+    {
+        return 0;
+    }
+
     if ( !IsColLocked() && !HasFixSize() )
     {
         SwRectFnSet aRectFnSet(this);
@@ -2645,6 +2671,17 @@ void SwSectionFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
         if(&rMod != GetDep())
             return;
         SwSectionFrame::MoveContentAndDelete(this, pHint->IsSaveContent());
+    }
+    else if (rHint.GetId() == SfxHintId::SwSectionHidden)
+    {
+        InvalidateAll();
+        InvalidateObjs(false);
+
+        for (SwFrame* pLowerFrame = Lower(); pLowerFrame; pLowerFrame = pLowerFrame->GetNext())
+        {
+            pLowerFrame->InvalidateAll();
+            pLowerFrame->InvalidateObjs(false);
+        }
     }
     else
         SwFrame::SwClientNotify(rMod, rHint);

sw/inc/crsrsh.hxx
@@ -267,7 +267,7 @@ private:
     SAL_DLLPRIVATE bool LRMargin( bool, bool bAPI = false );
     SAL_DLLPRIVATE bool IsAtLRMargin( bool, bool bAPI = false ) const;
 
-    SAL_DLLPRIVATE bool isInHiddenTextFrame(SwShellCursor* pShellCursor);
+    SAL_DLLPRIVATE bool isInHiddenFrame(SwShellCursor* pShellCursor);
 
     SAL_DLLPRIVATE bool GoStartWordImpl();
     SAL_DLLPRIVATE bool GoEndWordImpl();

sw/inc/node.hxx
@@ -590,7 +590,7 @@ public:
     const SwSection& GetSection() const { return *m_pSection; }
           SwSection& GetSection()       { return *m_pSection; }
 
-    SwFrame *MakeFrame( SwFrame* );
+    SwFrame* MakeFrame(SwFrame* pSib, bool bHidden);
 
     /** Creates the frms for the SectionNode (i.e. the SectionFrames).
        On default the frames are created until the end of the range.

sw/qa/extras/layout/data/largeTopMarginAndHiddenFirstSection.fodt
@@ -1,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:styles>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text">
+   <style:paragraph-properties fo:margin-top="1in" fo:margin-bottom="0" style:contextual-spacing="false" fo:line-height="12pt"/>
+  </style:style>
+ </office:styles>
+ <office:body>
+  <office:text>
+   <text:section text:name="Hidden" text:display="none"/>
+   <text:section text:name="Shown"/>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/data/pageBreakInHiddenSection.fodt
@@ -1,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:master-page-name="Landscape">
+   <style:paragraph-properties style:page-number="1"/>
+  </style:style>
+  <style:style style:name="P2" style:family="paragraph" style:master-page-name="Landscape">
+   <style:paragraph-properties style:page-number="1"/>
+   <style:text-properties text:display="none"/>
+  </style:style>
+ </office:automatic-styles>
+ <office:body>
+  <office:text>
+   <text:p>First line</text:p>
+   <text:section text:name="Hidden" text:display="none">
+    <text:p text:style-name="P1"/>
+   </text:section>
+   <text:section text:name="Shown">
+    <text:p>Before break (still first page)</text:p>
+   </text:section>
+   <text:section text:name="HiddenText">
+    <text:p text:style-name="P2"/>
+   </text:section>
+   <text:p>After break</text:p>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p/>
+   <text:p>Should be together with next</text:p>
+   <text:section text:name="Hidden2" text:display="none">
+    <text:p text:style-name="P2"/>
+   </text:section>
+   <text:p>Should be together with previous</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout3.cxx
@@ -94,15 +94,25 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf119875)
 {
     createSwDoc("tdf119875.odt");
     xmlDocUniquePtr pXmlDoc = parseLayoutDump();
-    sal_Int32 nFirstTop
-        = getXPath(pXmlDoc, "/root/page[2]/body/section[1]/infos/bounds"_ostr, "top"_ostr)
-              .toInt32();
-    sal_Int32 nSecondTop
-        = getXPath(pXmlDoc, "/root/page[2]/body/section[2]/infos/bounds"_ostr, "top"_ostr)
-              .toInt32();
-    // The first section had the same top value as the second one, so they
-    // overlapped.
-    CPPUNIT_ASSERT_LESS(nSecondTop, nFirstTop);
+
+    assertXPath(pXmlDoc, "//page[2]/body/section[1]"_ostr, "formatName"_ostr, u"S10"_ustr);
+    assertXPath(pXmlDoc, "//page[2]/body/section[2]"_ostr, "formatName"_ostr, u"S11"_ustr);
+    assertXPath(pXmlDoc, "//page[2]/body/section[3]"_ostr, "formatName"_ostr, u"S13"_ustr);
+    assertXPath(pXmlDoc, "//page[2]/body/section[4]"_ostr, "formatName"_ostr, u"S14"_ustr);
+    // Sections "S10" and "S13" are hidden -> their frames are zero-height
+    assertXPath(pXmlDoc, "//page[2]/body/section[1]/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
+    assertXPath(pXmlDoc, "//page[2]/body/section[3]/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
+
+    OUString S10Top = getXPath(pXmlDoc, "//page[2]/body/section[1]/infos/bounds"_ostr, "top"_ostr);
+    OUString S11Top = getXPath(pXmlDoc, "//page[2]/body/section[2]/infos/bounds"_ostr, "top"_ostr);
+    OUString S13Top = getXPath(pXmlDoc, "//page[2]/body/section[3]/infos/bounds"_ostr, "top"_ostr);
+    OUString S14Top = getXPath(pXmlDoc, "//page[2]/body/section[4]/infos/bounds"_ostr, "top"_ostr);
+
+    CPPUNIT_ASSERT_EQUAL(S10Top, S11Top);
+    CPPUNIT_ASSERT_EQUAL(S13Top, S14Top);
+
+    // Section "S11" had the same top value as section "S14", so they overlapped.
+    CPPUNIT_ASSERT_LESS(S14Top.toInt32(), S11Top.toInt32());
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf137523)
@@ -2301,6 +2311,46 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf159259)
 
     CPPUNIT_ASSERT_EQUAL(paraRight, flyRight); // The fly is right-aligned
     CPPUNIT_ASSERT_EQUAL(paraHeight, flyHeight);
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testLargeTopParaMarginAfterHiddenSection)
+{
+    // Given a large top margin in Standard paragraph style, and the first section hidden
+    createSwDoc("largeTopMarginAndHiddenFirstSection.fodt");
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    // Make sure there is only one page and two sections, first hidden (zero-height)
+    assertXPath(pXmlDoc, "//page"_ostr, 1);
+    assertXPath(pXmlDoc, "//page/body/section"_ostr, 2);
+    assertXPath(pXmlDoc, "//page/body/section[1]/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
+    // Check that the top margin (1 in = 1440 twip) is added to line height (12 pt = 240 twip)
+    assertXPath(pXmlDoc, "//page/body/section[2]/infos/bounds"_ostr, "height"_ostr, u"1680"_ustr);
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testPageBreakInHiddenSection)
+{
+    // Given a paragraph with page-break-before with page style and page number
+    createSwDoc("pageBreakInHiddenSection.fodt");
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//page"_ostr, 4);
+    assertXPath(pXmlDoc, "//section"_ostr, 4);
+    assertXPath(pXmlDoc, "//page[1]/body/txt"_ostr, 1);
+    // The page break inside the hidden section is ignored (otherwise, there would be one section
+    // on the first page)
+    assertXPath(pXmlDoc, "//page[1]/body/section"_ostr, 2);
+    // The first section is hidden
+    assertXPath(pXmlDoc, "//page[1]/body/section[1]/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
+
+    // Page 2 is empty even page (generated by the next page's section with page-break-before)
+    assertXPath(pXmlDoc, "//page[2]/body"_ostr, 0);
+
+    // The section on page 3 is not hidden, only text in it is, therefore its page break works
+    assertXPath(pXmlDoc, "//page[3]/body/section"_ostr, 1);
+    assertXPath(pXmlDoc, "//page[3]/body/section/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
+
+    // The section on page 4 is hidden, thus page break in it is ignored (no further pages, where
+    // the section would be moved to otherwise)
+    assertXPath(pXmlDoc, "//page[4]/body/section"_ostr, 1);
+    assertXPath(pXmlDoc, "//page[4]/body/section/infos/bounds"_ostr, "height"_ostr, u"0"_ustr);
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/qa/extras/uiwriter/data/hiddenSectionsAroundPageBreak.fodt
@@ -1,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:master-page-name="Landscape">
+   <style:paragraph-properties style:page-number="auto" fo:break-before="page"/>
+  </style:style>
+ </office:automatic-styles>
+ <office:body>
+  <office:text>
+   <text:section text:name="Section 1" text:display="none"/>
+   <text:section text:name="Section 2">
+    <text:p text:style-name="P1">A paragraph with a page-break-before</text:p>
+   </text:section>
+   <text:section text:name="Section 3" text:display="none"/>
+   <text:section text:name="Section 4">
+    <text:p>Lorem</text:p>
+   </text:section>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -122,6 +122,22 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf135083)
     CPPUNIT_ASSERT(!getProperty<OUString>(xLastPara, u"ListId"_ustr).isEmpty());
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testHiddenSectionsAroundPageBreak)
+{
+    createSwDoc("hiddenSectionsAroundPageBreak.fodt");
+
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+
+    auto xModel(mxComponent.queryThrow<frame::XModel>());
+    auto xTextViewCursorSupplier(
+        xModel->getCurrentController().queryThrow<text::XTextViewCursorSupplier>());
+    auto xCursor(xTextViewCursorSupplier->getViewCursor().queryThrow<text::XPageCursor>());
+
+    // Make sure that the page style is set correctly
+    xCursor->jumpToFirstPage();
+    CPPUNIT_ASSERT_EQUAL(u"Landscape"_ustr, getProperty<OUString>(xCursor, "PageStyleName"));
+}
+
 } // end of anonymous namespace
 CPPUNIT_PLUGIN_IMPLEMENT();
 

commit c647937e6066fb911b8ddc5a77b2621d5cb874e4

sw: fix problem with 0-height tables after unhiding section
Mysteriously this works the first time the section is made visible, but
then hide it again and unhide it and a table has height 0 because only
the table itself is invalidated and none of the text frames inside it.

SwTabFrame::MakeAll() does Prepare(PrepareHint::FramePositionChanged) on
all contained text frames, but that may actually do nothing in common
cases.

So just recursively invalidate everything in the section, that should
fix things.

(regression from commit ff7f1b59e22092d8548459e75fe912db852f056f)

Change-Id: I26e72193947685fde52fb8da78e95592a3866c12
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172920
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/source/core/layout/sectfrm.cxx
@@ -2680,6 +2680,24 @@ void SwSectionFrame::CalcEndAtEndFlag()
     }
 }
 
+static void InvalidateFramesInSection(SwFrame * pFrame)
+{
+    while (pFrame)
+    {
+        pFrame->InvalidateAll();
+        pFrame->InvalidateObjs(false);
+        if (pFrame->IsLayoutFrame())
+        {
+            InvalidateFramesInSection(pFrame->GetLower());
+        }
+        else if (pFrame->IsTextFrame())
+        {
+            pFrame->Prepare(PrepareHint::Clear, nullptr, false);
+        }
+        pFrame = pFrame->GetNext();
+    }
+}
+
 void SwSectionFrame::Notify(SfxHint const& rHint)
 {
     SwSectionFormat *const pFormat(GetSection()->GetFormat());
@@ -2742,35 +2760,8 @@ void SwSectionFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
             SwRectFnSet(this).SetHeight(area, HUGE_POSITIVE);
         }
 
-        SwColumnFrame * pColumn{Lower()->IsColumnFrame()
-                ? static_cast<SwColumnFrame*>(Lower()) : nullptr};
-        auto IterateLower = [&pColumn](SwFrame *const pLowerFrame) -> SwFrame*
-        {
-            if (pLowerFrame->GetNext())
-            {
-                return pLowerFrame->GetNext();
-            }
-            if (pColumn)
-            {
-                pColumn = static_cast<SwColumnFrame*>(pColumn->GetNext());
-                if (pColumn)
-                {
-                    return static_cast<SwLayoutFrame*>(pColumn->Lower())->Lower();
-                }
-            }
-            return nullptr;
-        };
-        for (SwFrame* pLowerFrame = pColumn
-                    ? static_cast<SwLayoutFrame*>(pColumn->Lower())->Lower()
-                    : Lower();
-             pLowerFrame;
-             pLowerFrame = IterateLower(pLowerFrame))
-        {
-            pLowerFrame->Prepare(PrepareHint::Clear, nullptr, false);
-            pLowerFrame->InvalidateAll();
-            pLowerFrame->InvalidateObjs(false);
-            pLowerFrame->HideAndShowObjects();
-        }
+        InvalidateFramesInSection(Lower());
+        Lower()->HideAndShowObjects(); // recursive
         // Check if any page-breaks have been unhidden, create the new pages.
         // Call IsHiddenNow() because a parent section could still hide.
         if (!IsFollow() && IsInDocBody() && !IsInTab() && !IsHiddenNow())
@@ -2799,8 +2790,24 @@ void SwSectionFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
                 pFirstOnPage = pFirstOnPage->GetUpper();
             }
             assert(pFirstOnPage->IsContentFrame() || pFirstOnPage->IsTabFrame());
-            pColumn = Lower()->IsColumnFrame()
-                ? static_cast<SwColumnFrame*>(Lower()) : nullptr;
+            SwColumnFrame * pColumn{Lower()->IsColumnFrame()
+                    ? static_cast<SwColumnFrame*>(Lower()) : nullptr};
+            auto IterateLower = [&pColumn](SwFrame *const pLowerFrame) -> SwFrame*
+            {
+                if (pLowerFrame->GetNext())
+                {
+                    return pLowerFrame->GetNext();
+                }
+                if (pColumn)
+                {
+                    pColumn = static_cast<SwColumnFrame*>(pColumn->GetNext());
+                    if (pColumn)
+                    {
+                        return static_cast<SwLayoutFrame*>(pColumn->Lower())->Lower();
+                    }
+                }
+                return nullptr;
+            };
             for (SwFrame* pLowerFrame = pColumn
                         ? static_cast<SwLayoutFrame*>(pColumn->Lower())->Lower()
                         : Lower();