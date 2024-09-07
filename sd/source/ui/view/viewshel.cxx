LibreOffice/core

commit 5e143492320dac8cdf8b2956799ca366f3d8e72c

tdf#33603: fix notespane (side/tool)bar interactions
Introduces OverridingShells that when set, makes the view
act like the MainViewShell.

The main use case is having more then one ViewShell in a
single window, where context (toolbars, sidebars etc.) can
jump in between different ViewShells.

Uses OverridingShells to enable NotesPane to react
ToolBarShells. Accessing the functionality of ToolBarShells
without the previous hacks with slot forwarding.

Change-Id: Icc9721d7f54097025bc9dc7ef7069aed856e6d96
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169658
Tested-by: Jenkins
Reviewed-by: Sarper Akdemir <sarper.akdemir@allotropia.de>

sd/source/ui/view/viewshel.cxx
@@ -17,6 +17,8 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <framework/FrameworkHelper.hxx>
+#include <framework/ViewShellWrapper.hxx>
 #include <memory>
 #include <ViewShell.hxx>
 #include <ViewShellImplementation.hxx>
@@ -58,6 +60,7 @@
 #include <SlideSorterViewShell.hxx>
 #include <ViewShellManager.hxx>
 #include <FormShellManager.hxx>
+#include <EventMultiplexer.hxx>
 #include <svx/extrusionbar.hxx>
 #include <svx/fontworkbar.hxx>
 #include <svx/svdoutl.hxx>
@@ -89,6 +92,12 @@
 #include <strings.hxx>
 #include <sdmod.hxx>
 #include <AccessibleDocumentViewBase.hxx>
+
+#include <com/sun/star/drawing/framework/XControllerManager.hpp>
+#include <com/sun/star/drawing/framework/XConfigurationController.hpp>
+#include <com/sun/star/drawing/framework/XConfiguration.hpp>
+#include <com/sun/star/drawing/framework/XView.hpp>
+#include <com/sun/star/frame/XFrame.hpp>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -403,6 +412,62 @@ void ViewShell::Deactivate(bool bIsMDIActivate)
         mpVerticalRuler->SetActive(false);
 
     SfxShell::Deactivate(bIsMDIActivate);
+}
+
+void ViewShell::BroadcastContextForActivation(const bool bIsActivated)
+{
+    auto getFrameworkResourceIdForShell
+        = [&]() -> uno::Reference<drawing::framework::XResourceId> const
+    {
+        Reference<::css::drawing::framework::XControllerManager> xControllerManager(
+            GetViewShellBase().GetController(), UNO_QUERY);
+        if (!xControllerManager.is())
+            return {};
+
+        Reference<::css::drawing::framework::XConfigurationController> xConfigurationController
+            = xControllerManager->getConfigurationController();
+        if (!xConfigurationController.is())
+            return {};
+
+        Reference<::css::drawing::framework::XConfiguration> xConfiguration
+            = xConfigurationController->getCurrentConfiguration();
+        if (!xConfiguration.is())
+            return {};
+
+        auto aResIdsIndirect
+            = xConfiguration->getResources({}, "", drawing::framework::AnchorBindingMode_INDIRECT);
+
+        for (const uno::Reference<drawing::framework::XResourceId>& rResId : aResIdsIndirect)
+        {
+            auto pFrameworkHelper = framework::FrameworkHelper::Instance(GetViewShellBase());
+
+            uno::Reference<drawing::framework::XView> xView;
+            if (rResId->getResourceURL().match(framework::FrameworkHelper::msViewURLPrefix))
+            {
+                xView.set(xConfigurationController->getResource(rResId), UNO_QUERY);
+
+                if (xView.is())
+                {
+                    auto pViewShellWrapper
+                        = dynamic_cast<framework::ViewShellWrapper*>(xView.get());
+                    if (pViewShellWrapper->GetViewShell().get() == this)
+                    {
+                        return rResId;
+                    }
+                }
+            }
+        }
+        return {};
+    };
+
+    if (bIsActivated)
+    {
+        GetViewShellBase().GetEventMultiplexer()->MultiplexEvent(
+            EventMultiplexerEventId::FocusShifted, nullptr, getFrameworkResourceIdForShell());
+    }
+
+    if (GetDispatcher())
+        SfxShell::BroadcastContextForActivation(bIsActivated);
 }
 
 void ViewShell::Shutdown()

include/sfx2/shell.hxx
@@ -455,7 +455,7 @@ public:
             defined with an earlier call to SetContextName().
             When <FALSE/> then broadcast the 'default' context.
     */
-    void BroadcastContextForActivation (const bool bIsActivated);
+    virtual void BroadcastContextForActivation (const bool bIsActivated);
 
     /** Enabled or disable the context broadcaster.  Returns the old state.
     */

sd/sdi/NotesPanelView.sdi
@@ -499,5 +499,4 @@ include "drtxtob.sdi"
 shell NotesPanelViewShell
 {
     import NotesPanelView;
-    import TextObjectBar;
 }

sd/source/ui/framework/module/ToolBarModule.cxx
@@ -18,10 +18,17 @@
  */
 
 #include "ToolBarModule.hxx"
+#include <ViewShell.hxx>
 #include <ViewShellBase.hxx>
+#include <ViewShellManager.hxx>
 #include <DrawController.hxx>
+#include <EventMultiplexer.hxx>
 #include <comphelper/servicehelper.hxx>
 #include <framework/FrameworkHelper.hxx>
+#include <vcl/EnumContext.hxx>
+
+#include <com/sun/star/frame/XController.hpp>
+#include <comphelper/processfactory.hxx>
 
 using namespace ::com::sun::star;
 using namespace ::com::sun::star::uno;
@@ -76,6 +83,9 @@ ToolBarModule::ToolBarModule (
 
 ToolBarModule::~ToolBarModule()
 {
+    if (mpBase && mbListeningEventMultiplexer)
+        mpBase->GetEventMultiplexer()->RemoveEventListener(
+            LINK(this, ToolBarModule, EventMultiplexerListener));
 }
 
 void ToolBarModule::disposing(std::unique_lock<std::mutex>&)
@@ -92,6 +102,16 @@ void SAL_CALL ToolBarModule::notifyConfigurationChange (
 {
     if (!mxConfigurationController.is())
         return;
+
+    // since EventMultiplexer isn't available when the ToolBarModule is
+    // initialized, subscribing the event listener hacked here.
+    if (!mbListeningEventMultiplexer && mpBase)
+    {
+        mpBase->GetEventMultiplexer()->AddEventListener(
+            LINK(this, ToolBarModule, EventMultiplexerListener));
+        mbListeningEventMultiplexer = true;
+    }
+
 
     sal_Int32 nEventType = 0;
     rEvent.UserData >>= nEventType;
@@ -123,6 +143,34 @@ void SAL_CALL ToolBarModule::notifyConfigurationChange (
     }
 }
 
+void ToolBarModule::HandlePaneViewShellFocused(const css::uno::Reference<css::drawing::framework::XResourceId>& rxResourceId)
+{
+    if(!mpBase)
+        return;
+
+    std::shared_ptr<FrameworkHelper> pFrameworkHelper(FrameworkHelper::Instance(*mpBase));
+    std::shared_ptr<ViewShell> pViewShell
+        = FrameworkHelper::GetViewShell(pFrameworkHelper->GetView(rxResourceId));
+
+    if(mpBase->GetMainViewShell() == pViewShell)
+    {
+        mpBase->GetViewShellManager()->RemoveOverridingMainShell();
+        return;
+    }
+
+    switch(pViewShell->GetShellType())
+    {
+        // shells that override mainviewshell functionality when used in a pane
+        case ViewShell::ST_NOTESPANEL:
+            mpBase->GetViewShellManager()->SetOverridingMainShell(pViewShell);
+            UpdateToolbars(pViewShell.get());
+            break;
+        default:
+            break;
+    }
+    mpToolBarManagerLock.reset();
+}
+
 void ToolBarModule::HandleUpdateStart()
 {
     // Lock the ToolBarManager and tell it to lock the ViewShellManager as
@@ -149,28 +197,45 @@ void ToolBarModule::HandleUpdateEnd()
         std::shared_ptr<ToolBarManager> pToolBarManager (mpBase->GetToolBarManager());
         std::shared_ptr<FrameworkHelper> pFrameworkHelper (
             FrameworkHelper::Instance(*mpBase));
-        ViewShell* pViewShell
-            = pFrameworkHelper->GetViewShell(FrameworkHelper::msCenterPaneURL).get();
-        if (pViewShell != nullptr)
-        {
-            pToolBarManager->MainViewShellChanged(*pViewShell);
-            pToolBarManager->SelectionHasChanged(
-                *pViewShell,
-                *pViewShell->GetView());
-            pToolBarManager->PreUpdate();
-        }
-        else
-        {
-            pToolBarManager->MainViewShellChanged();
-            pToolBarManager->PreUpdate();
-        }
-    }
+        auto pViewShell
+            = pFrameworkHelper->GetViewShell(FrameworkHelper::msCenterPaneURL);
 
+        UpdateToolbars(pViewShell.get());
+    }
     // Releasing the update lock of the ToolBarManager  will let the
     // ToolBarManager with the help of the ViewShellManager take care of
     // updating tool bars and view shell with the minimal amount of
     // shell stack modifications and tool bar updates.
     mpToolBarManagerLock.reset();
+}
+
+void ToolBarModule::UpdateToolbars(ViewShell* pViewShell)
+{
+    // Update the set of visible tool bars and deactivate those that are
+    // no longer visible.  This is done before the old view shell is
+    // destroyed in order to avoid unnecessary updates of those tool
+    // bars.
+    if (!mpBase)
+        return;
+
+    std::shared_ptr<ToolBarManager> pToolBarManager (mpBase->GetToolBarManager());
+
+    if(!pToolBarManager)
+        return;
+
+    if (pViewShell)
+    {
+        pToolBarManager->MainViewShellChanged(*pViewShell);
+        pToolBarManager->SelectionHasChanged(
+            *pViewShell,
+            *pViewShell->GetView());
+        pToolBarManager->PreUpdate();
+    }
+    else
+    {
+        pToolBarManager->MainViewShellChanged();
+        pToolBarManager->PreUpdate();
+    }
 }
 
 void SAL_CALL ToolBarModule::disposing (const lang::EventObject& rEvent)
@@ -181,6 +246,27 @@ void SAL_CALL ToolBarModule::disposing (const lang::EventObject& rEvent)
         // Without the configuration controller this class can do nothing.
         mxConfigurationController = nullptr;
         dispose();
+    }
+}
+
+IMPL_LINK(ToolBarModule, EventMultiplexerListener, sd::tools::EventMultiplexerEvent&, rEvent,
+          void)
+{
+    if (!mpBase)
+        return;
+
+    switch(rEvent.meEventId)
+    {
+        case EventMultiplexerEventId::FocusShifted:
+            {
+                uno::Reference<drawing::framework::XResourceId> xResourceId{ rEvent.mxUserData,
+                                                                             UNO_QUERY };
+                if (xResourceId.is())
+                    HandlePaneViewShellFocused(xResourceId);
+                break;
+            }
+        default:
+            break;
     }
 }
 

sd/source/ui/framework/module/ToolBarModule.hxx
@@ -20,18 +20,32 @@
 #pragma once
 
 #include <ToolBarManager.hxx>
+#include <tools/link.hxx>
 #include <com/sun/star/drawing/framework/XConfigurationChangeListener.hpp>
+#include <com/sun/star/ui/XContextChangeEventListener.hpp>
 #include <comphelper/compbase.hxx>
 #include <o3tl/deleter.hxx>
 #include <rtl/ref.hxx>
 #include <memory>
 
-namespace com::sun::star::drawing::framework { class XConfigurationController; }
-namespace com::sun::star::frame { class XController; }
+namespace com::sun::star::drawing::framework
+{
+class XConfigurationController;
+class XResourceId;
+}
+namespace com::sun::star::frame
+{
+class XController;
+}
 
 namespace sd {
 class DrawController;
 class ViewShellBase;
+}
+
+namespace sd::tools
+{
+class EventMultiplexerEvent;
 }
 
 namespace sd::framework {
@@ -73,9 +87,20 @@ private:
     ViewShellBase* mpBase;
     std::unique_ptr<ToolBarManager::UpdateLock, o3tl::default_delete<ToolBarManager::UpdateLock>> mpToolBarManagerLock;
     bool mbMainViewSwitchUpdatePending;
+    bool mbListeningEventMultiplexer = false;
+
+    /** Update toolbars via ToolbarManager
+
+        @param pViewShell may be nullptr
+    */
+    void UpdateToolbars(ViewShell* pViewShell);
 
     void HandleUpdateStart();
     void HandleUpdateEnd();
+    void HandlePaneViewShellFocused(
+        const css::uno::Reference<css::drawing::framework::XResourceId>& rxResourceId);
+
+    DECL_LINK(EventMultiplexerListener, ::sd::tools::EventMultiplexerEvent&, void);
 };
 
 } // end of namespace sd::framework

sd/source/ui/inc/EventMultiplexer.hxx
@@ -22,6 +22,7 @@
 #include <sal/config.h>
 
 #include <rtl/ref.hxx>
+#include <com/sun/star/uno/XInterface.hpp>
 
 template <typename Arg, typename Ret> class Link;
 
@@ -112,6 +113,10 @@ enum class EventMultiplexerEventId
     /** Edit mode was (or is being) switched to master mode.
     */
     EditModeMaster,
+
+    /** Focus shifted between views.
+     */
+    FocusShifted,
 };
 
 namespace sd::tools
@@ -121,8 +126,10 @@ class EventMultiplexerEvent
 public:
     EventMultiplexerEventId meEventId;
     const void* mpUserData;
+    css::uno::Reference<css::uno::XInterface> mxUserData;
 
-    EventMultiplexerEvent(EventMultiplexerEventId eEventId, const void* pUserData);
+    EventMultiplexerEvent(EventMultiplexerEventId eEventId, const void* pUserData,
+                          const css::uno::Reference<css::uno::XInterface>& xUserData = {});
 };
 
 /** This convenience class makes it easy to listen to various events that
@@ -160,7 +167,8 @@ public:
         @param pUserData
             Some data sent to the listeners along with the event.
     */
-    void MultiplexEvent(EventMultiplexerEventId eEventId, void const* pUserData);
+    void MultiplexEvent(EventMultiplexerEventId eEventId, void const* pUserData,
+                        const css::uno::Reference<css::uno::XInterface>& xUserData = {});
 
 private:
     class Implementation;

sd/source/ui/inc/ToolBarManager.hxx
@@ -178,10 +178,13 @@ public:
             The group is used for the actual tool bars.
         @param nToolBarId
             Id of the tool bar shell.
+        @param bAddBar
+            Add the toolbar itself with the shell. When false only adds the shell.
     */
     void AddToolBarShell (
         ToolBarGroup eGroup,
-        ShellId nToolBarId);
+        ShellId nToolBarId,
+        bool bAddBar = true);
 
     /** Remove the tool bar with the given name from the specified group.
         If the tool bar is not visible then nothing happens.

sd/source/ui/inc/ViewShell.hxx
@@ -522,6 +522,7 @@ protected:
 
     virtual void Activate(bool IsMDIActivate) override;
     virtual void Deactivate(bool IsMDIActivate) override;
+    virtual void BroadcastContextForActivation (const bool bIsActivated) override;
 
     virtual void SetZoomFactor( const Fraction &rZoomX,
                                 const Fraction &rZoomY );

commit ec1a56b2e13f3a70a53adc6308f34ecc950600fa

sd: fix deref before null check
Seen in https://crashreport.libreoffice.org/stats/signature/static%20const%20class%20com::sun::star::uno::Reference%3Ccom::sun::star::drawing::framework::XResourceId%3E%20%60sd::ViewShell::BroadcastContextForActivation'::%602'::%3Clambda_1%3E::operator()()

Regression from
commit 5e143492320dac8cdf8b2956799ca366f3d8e72c
Author: Sarper Akdemir <sarper.akdemir@allotropia.de>
Date:   Fri Jun 7 21:14:44 2024 +0200

    tdf#33603: fix notespane (side/tool)bar interactions

Change-Id: Ic6b821a660d5b5aa444224fcb6b0b4eccba5a85a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172999
Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sd/source/ui/view/viewshel.cxx
@@ -448,11 +448,12 @@ void ViewShell::BroadcastContextForActivation(const bool bIsActivated)
 
                 if (xView.is())
                 {
-                    auto pViewShellWrapper
-                        = dynamic_cast<framework::ViewShellWrapper*>(xView.get());
-                    if (pViewShellWrapper->GetViewShell().get() == this)
+                    if (auto pViewShellWrapper = dynamic_cast<framework::ViewShellWrapper*>(xView.get()))
                     {
-                        return rResId;
+                        if (pViewShellWrapper->GetViewShell().get() == this)
+                        {
+                            return rResId;
+                        }
                     }
                 }
             }