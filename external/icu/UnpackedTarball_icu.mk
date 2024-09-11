5c88efbcfa0310c9b492af2f9144efd76ad5fdf0
Xisco Fauli <xiscofauli@libreoffice.org>
2024-09-11 21:30:04 +0200

    external: remove a couple of gcc9 patches
    Baseline uses GCC 12 now
    
    Change-Id: Id9a0f79aa801aa342fd943cebeb0f528f4964d84
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173235
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

--- external/icu/UnpackedTarball_icu.mk
+++ external/icu/UnpackedTarball_icu.mk
@@ -31,7 +31,6 @@ $(eval $(call gb_UnpackedTarball_add_patches,icu,\
 	external/icu/icu4c-scriptrun.patch.1 \
 	external/icu/icu4c-rtti.patch.1 \
 	external/icu/icu4c-clang-cl.patch.1 \
-	external/icu/gcc9.patch \
 	external/icu/c++20-comparison.patch.1 \
 	external/icu/Wdeprecated-copy-dtor.patch \
 	external/icu/icu4c-windows-cygwin-cross.patch.1 \
--- external/liborcus/UnpackedTarball_liborcus.mk
+++ external/liborcus/UnpackedTarball_liborcus.mk
@@ -17,7 +17,6 @@ $(eval $(call gb_UnpackedTarball_update_autoconf_configs,liborcus))
 
 $(eval $(call gb_UnpackedTarball_add_patches,liborcus,\
 	external/liborcus/rpath.patch.0 \
-	external/liborcus/gcc9.patch.0 \
 	external/liborcus/libtool.patch.0 \
 ))
 
