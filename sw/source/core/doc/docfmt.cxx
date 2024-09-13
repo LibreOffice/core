LibreOffice/core

commit fe444d1f74abe417962be0bcd3340f40f2446b58

fdo#62536: sw: fix AutoCorrect bold/underline on existing AUTOFMT
With the native AUTOFMT in Writer the SETATTR_DONTEXPAND does no longer
work reliably: if there is an existing AUTOFMT at the position then it
will be modified and no new hint with DontExpand will be inserted.

Work around this deficiency by inserting a no-length hint with the
preivous formatting at the end of the range.

(similar fix to the i#75891 problem in SwTextShell::InsertSymbol)

(commit 062eaeffe7cb986255063bb9b0a5f3fb3fc8e34c did not
 introduce the problem but made it far more annoying)

Change-Id: I58ece7f5bd5a786b22a066e5902f1784dafa5dce

sw/source/core/doc/docfmt.cxx
@@ -2475,7 +2475,31 @@ void SwDoc::SetFmtItemByAutoFmt( const SwPaM& rPam, const SfxItemSet& rSet )
         SetRedlineMode_intern( (RedlineMode_t)(eOld | nsRedlineMode_t::REDLINE_IGNORE));
     }
 
+    xub_StrLen const nEnd(rPam.End()->nContent.GetIndex());
+    std::vector<sal_uInt16> whichIds;
+    SfxItemIter iter(rSet);
+    for (SfxPoolItem const* pItem = iter.FirstItem();
+            pItem; pItem = iter.NextItem())
+    {
+        whichIds.push_back(pItem->Which());
+        whichIds.push_back(pItem->Which());
+    }
+    whichIds.push_back(0);
+    SfxItemSet currentSet(GetAttrPool(), &whichIds[0]);
+    pTNd->GetAttr(currentSet, nEnd, nEnd, false, true, false);
+    for (size_t i = 0; whichIds[i]; i += 2)
+    {   // yuk - want to explicitly set the pool defaults too :-/
+        currentSet.Put(currentSet.Get(whichIds[i], true));
+    }
+
     InsertItemSet( rPam, rSet, nsSetAttrMode::SETATTR_DONTEXPAND );
+
+    // fdo#62536: DONTEXPAND does not work when there is already an AUTOFMT
+    // here, so insert the old attributes as an empty hint to stop expand
+    SwPaM endPam(*pTNd, nEnd);
+    endPam.SetMark();
+    InsertItemSet(endPam, currentSet, nsSetAttrMode::SETATTR_DEFAULT);
+
     SetRedlineMode_intern( eOld );
 }
 

editeng/source/misc/svxacorr.cxx
@@ -762,24 +762,27 @@ sal_Bool SvxAutoCorrect::FnChgWeightUnderl( SvxAutoCorrDoc& rDoc, const String& 
 
     if( STRING_NOTFOUND != nFndPos )
     {
-        // Span the Attribute over the area and delete the Character found at
+        // first delete the Character at the end - this allows insertion
+        // of an empty hint in SetAttr which would be removed by Delete
+        // (fdo#62536, AUTOFMT in Writer)
+        rDoc.Delete( nEndPos, nEndPos + 1 );
+        rDoc.Delete( nFndPos, nFndPos + 1 );
+        // Span the Attribute over the area
         // the end.
         if( '*' == cInsChar )           // Bold
         {
             SvxWeightItem aSvxWeightItem( WEIGHT_BOLD, SID_ATTR_CHAR_WEIGHT );
-            rDoc.SetAttr( nFndPos + 1, nEndPos,
+            rDoc.SetAttr( nFndPos, nEndPos - 1,
                             SID_ATTR_CHAR_WEIGHT,
                             aSvxWeightItem);
         }
         else                            // underline
         {
             SvxUnderlineItem aSvxUnderlineItem( UNDERLINE_SINGLE, SID_ATTR_CHAR_UNDERLINE );
-            rDoc.SetAttr( nFndPos + 1, nEndPos,
+            rDoc.SetAttr( nFndPos, nEndPos - 1,
                             SID_ATTR_CHAR_UNDERLINE,
                             aSvxUnderlineItem);
         }
-        rDoc.Delete( nEndPos, nEndPos + 1 );
-        rDoc.Delete( nFndPos, nFndPos + 1 );
     }
 
     return STRING_NOTFOUND != nFndPos;

sw/inc/swtypes.hxx
@@ -188,6 +188,8 @@ typedef sal_uInt16 SetAttrMode;
 namespace nsSetAttrMode
 {
     const SetAttrMode SETATTR_DEFAULT        = 0x0000;  // Default.
+    /// @attention: DONTEXPAND does not work very well for CHARATR
+    ///             because it can expand only the whole AUTOFMT or nothing
     const SetAttrMode SETATTR_DONTEXPAND     = 0x0001;  // Don't expand text attribute any further.
     const SetAttrMode SETATTR_DONTREPLACE    = 0x0002;  // Don't replace another text attribute.
 

commit f6dc87fc730bc48a7c809d2ff460cc36024630b9

tdf#162911 sw: fix crash after autocorrect inserts hyperlinks
The problem is that SwDoc::SetFormatItemByAutoFormat() inserts a static
pool default item of SwFormatINetFormat as SwTextINetFormat into the
document.

SfxPoolItemHolder::SfxPoolItemHolder() does not clone a static pool
default item, hence it will be deleted multiple times.

The code that was added in SwDoc::SetFormatItemByAutoFormat() to fix
tdf#62536 is for the RES_CHRATR_* items, it is not needed for any of the
other items that autocorrect/autoformat may insert (RES_TXTATR_INETFMT
or RES_PARATR_TABSTOP).

(regression from commit fe444d1f74abe417962be0bcd3340f40f2446b58
 and/or commit ca3c6d468f68af1506bf4e56b47655e5d56306a8)

Change-Id: I8b0414995f9fd8f94d1903a8e66f0c25c7038ebd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173267
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/doc/docfmt.cxx
@@ -1884,27 +1884,37 @@ void SwDoc::SetFormatItemByAutoFormat( const SwPaM& rPam, const SfxItemSet& rSet
     SfxItemIter iter(rSet);
     for (SfxPoolItem const* pItem = iter.GetCurItem(); pItem; pItem = iter.NextItem())
     {
-        whichIds.push_back({pItem->Which(), pItem->Which()});
+        if (RES_CHRATR_BEGIN <= pItem->Which() && pItem->Which() < RES_CHRATR_END)
+        {   // tdf#162911 don't add items with static default like INETFMT
+            whichIds.push_back({pItem->Which(), pItem->Which()});
+        }
     }
 
-    // ITEM: Need to sort these due to ItemSet using unordered_set and
-    // WhichRangesContainer expect these sorted ascending
-    std::sort(whichIds.begin(), whichIds.end(), [](WhichPair& a, WhichPair& b) {return a.first < b.first;});
+    ::std::optional<SfxItemSet> oCurrentSet;
+    if (!whichIds.empty())
+    {
+        // ITEM: Need to sort these due to ItemSet using unordered_set and
+        // WhichRangesContainer expect these sorted ascending
+        std::sort(whichIds.begin(), whichIds.end(), [](WhichPair& a, WhichPair& b) {return a.first < b.first;});
 
-    SfxItemSet currentSet(GetAttrPool(), WhichRangesContainer(whichIds.data(), whichIds.size()));
-    pTNd->GetParaAttr(currentSet, nEnd, nEnd);
-    for (const WhichPair& rPair : whichIds)
-    {   // yuk - want to explicitly set the pool defaults too :-/
-        currentSet.Put(currentSet.Get(rPair.first));
+        oCurrentSet.emplace(GetAttrPool(), WhichRangesContainer(whichIds.data(), whichIds.size()));
+        pTNd->GetParaAttr(*oCurrentSet, nEnd, nEnd);
+        for (const WhichPair& rPair : whichIds)
+        {   // yuk - want to explicitly set the pool defaults too :-/
+            oCurrentSet->Put(oCurrentSet->Get(rPair.first));
+        }
     }
 
     getIDocumentContentOperations().InsertItemSet( rPam, rSet, SetAttrMode::DONTEXPAND );
 
-    // fdo#62536: DONTEXPAND does not work when there is already an AUTOFMT
-    // here, so insert the old attributes as an empty hint to stop expand
-    SwPaM endPam(*pTNd, nEnd);
-    endPam.SetMark();
-    getIDocumentContentOperations().InsertItemSet(endPam, currentSet);
+    if (!whichIds.empty())
+    {
+        // fdo#62536: DONTEXPAND does not work when there is already an AUTOFMT
+        // here, so insert the old attributes as an empty hint to stop expand
+        SwPaM endPam(*pTNd, nEnd);
+        endPam.SetMark();
+        getIDocumentContentOperations().InsertItemSet(endPam, *oCurrentSet);
+    }
 
     getIDocumentRedlineAccess().SetRedlineFlags_intern( eOld );
 }