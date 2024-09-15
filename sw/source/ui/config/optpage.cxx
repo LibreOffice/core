LibreOffice/core

commit 745c7bce5fe8b218f7d300667a5409bc7ef58c34

Delete unnecessary writer options from Options dialog
Here https://wiki.documentfoundation.org/Design/Whiteboards/Options/Writer
are unnecessary writer options. These options are set in
Expert Configuration dialog under org.oppenoffice.Office.Writer node.
Some of them have void type therefore they aren't deleted.

Preference name, Property, Location in Options dialog;
org.openoffice.Office.Writer/Layout/Window, HorizontalScroll,
View/View/Horizontal Scrollbar;
org.openoffice.Office.Writer/Layout/Window, VerticalScroll,
View/View/Vertical Scrollbar;
org.openoffice.Office.Writer/Layout/Window, HorizontalRuler,
View/View/Horizontal Ruler;
org.openoffice.Office.Writer/DefaultFont, Document,
Basic Fonts (Western)/Current documentum only;
org.openoffice.Office.Writer/DefaultFontCJK, Document,
Basic Fonts (Asian)/Current documentum only;
org.openoffice.Office.Writer/DefaultFontCTL, Document,
Basic Fonts (CTL)/Current documentum only;

Change-Id: Ia98fa456ec6039bff3ad13ca7668e73320d63920
Reviewed-on: https://gerrit.libreoffice.org/17542
Reviewed-by: Samuel Mehrbrodt <s.mehrbrodt@gmail.com>
Tested-by: Samuel Mehrbrodt <s.mehrbrodt@gmail.com>

sw/source/ui/config/optpage.cxx
@@ -90,10 +90,6 @@ SwContentOptPage::SwContentOptPage( vcl::Window* pParent,
 {
     get (m_pCrossCB, "helplines");
 
-    get (m_pHScrollBox, "hscrollbar");
-    get (m_pVScrollBox, "vscrollbar");
-    get (m_pAnyRulerCB, "ruler");
-    get (m_pHRulerCBox, "hruler");
     get (m_pHMetric, "hrulercombobox");
     get (m_pVRulerCBox, "vruler");
     get (m_pVRulerRightCBox, "vrulerright");
@@ -126,7 +122,6 @@ SwContentOptPage::SwContentOptPage( vcl::Window* pParent,
     if(!aCJKOptions.IsVerticalTextEnabled() )
         m_pVRulerRightCBox->Hide();
     m_pVRulerCBox->SetClickHdl(LINK(this, SwContentOptPage, VertRulerHdl ));
-    m_pAnyRulerCB->SetClickHdl(LINK(this, SwContentOptPage, AnyRulerHdl));
 
     SvxStringArray aMetricArr( SW_RES( STR_ARR_METRIC ) );
     for ( size_t i = 0; i < aMetricArr.Count(); ++i )
@@ -174,10 +169,6 @@ SwContentOptPage::~SwContentOptPage()
 void SwContentOptPage::dispose()
 {
     m_pCrossCB.clear();
-    m_pHScrollBox.clear();
-    m_pVScrollBox.clear();
-    m_pAnyRulerCB.clear();
-    m_pHRulerCBox.clear();
     m_pHMetric.clear();
     m_pVRulerCBox.clear();
     m_pVRulerRightCBox.clear();
@@ -234,10 +225,6 @@ void SwContentOptPage::Reset(const SfxItemSet* rSet)
         m_pFieldNameCB->Check (pElemAttr->bFieldName);
         m_pPostItCB->Check (pElemAttr->bNotes);
         m_pCrossCB->Check (pElemAttr->bCrosshair);
-        m_pHScrollBox->Check (pElemAttr->bHorzScrollbar);
-        m_pVScrollBox->Check (pElemAttr->bVertScrollbar);
-        m_pAnyRulerCB->Check (pElemAttr->bAnyRuler);
-        m_pHRulerCBox->Check (pElemAttr->bHorzRuler);
         m_pVRulerCBox->Check (pElemAttr->bVertRuler);
         m_pVRulerRightCBox->Check (pElemAttr->bVertRulerRight);
         m_pSmoothCBox->Check (pElemAttr->bSmoothScroll);
@@ -246,7 +233,6 @@ void SwContentOptPage::Reset(const SfxItemSet* rSet)
     lcl_SelectMetricLB(m_pMetricLB, SID_ATTR_METRIC, *rSet);
     lcl_SelectMetricLB(m_pHMetric, FN_HSCROLL_METRIC, *rSet);
     lcl_SelectMetricLB(m_pVMetric, FN_VSCROLL_METRIC, *rSet);
-    AnyRulerHdl(m_pAnyRulerCB);
 }
 
 bool SwContentOptPage::FillItemSet(SfxItemSet* rSet)
@@ -263,10 +249,6 @@ bool SwContentOptPage::FillItemSet(SfxItemSet* rSet)
     aElem.bFieldName            = m_pFieldNameCB->IsChecked();
     aElem.bNotes                = m_pPostItCB->IsChecked();
     aElem.bCrosshair            = m_pCrossCB->IsChecked();
-    aElem.bHorzScrollbar        = m_pHScrollBox->IsChecked();
-    aElem.bVertScrollbar        = m_pVScrollBox->IsChecked();
-    aElem.bAnyRuler             = m_pAnyRulerCB->IsChecked();
-    aElem.bHorzRuler            = m_pHRulerCBox->IsChecked();
     aElem.bVertRuler            = m_pVRulerCBox->IsChecked();
     aElem.bVertRulerRight       = m_pVRulerRightCBox->IsChecked();
     aElem.bSmoothScroll         = m_pSmoothCBox->IsChecked();
@@ -307,17 +289,6 @@ bool SwContentOptPage::FillItemSet(SfxItemSet* rSet)
 IMPL_LINK(SwContentOptPage, VertRulerHdl, CheckBox*, pBox)
 {
     m_pVRulerRightCBox->Enable(pBox->IsEnabled() && pBox->IsChecked());
-    return 0;
-}
-
-IMPL_LINK( SwContentOptPage, AnyRulerHdl, CheckBox*, pBox)
-{
-    bool bChecked = pBox->IsChecked();
-    m_pHRulerCBox->Enable(bChecked);
-    m_pHMetric->Enable(bChecked);
-    m_pVRulerCBox->Enable(bChecked);
-    m_pVMetric->Enable(bChecked);
-    VertRulerHdl(m_pVRulerCBox);
     return 0;
 }
 
@@ -612,7 +583,6 @@ SwStdFontTabPage::SwStdFontTabPage( vcl::Window* pParent,
     pIdxBox->SetStyle(pIdxBox->GetStyle() |  WB_SORT);
     get(pIndexHeightLB,"indexheight");
 
-    get(pDocOnlyCB,"doconly");
     get(pStandardPB,"standard");
 
     pStandardPB->SetClickHdl(LINK(this, SwStdFontTabPage, StandardHdl));
@@ -633,8 +603,6 @@ SwStdFontTabPage::SwStdFontTabPage( vcl::Window* pParent,
     pListHeightLB->    SetModifyHdl( aModifyHeightLink );
     pLabelHeightLB->   SetModifyHdl( aModifyHeightLink );
     pIndexHeightLB->   SetModifyHdl( aModifyHeightLink );
-
-    pDocOnlyCB->Check(SW_MOD()->GetModuleConfig()->IsDefaultFontInCurrDocOnly());
 }
 
 SwStdFontTabPage::~SwStdFontTabPage()
@@ -658,7 +626,6 @@ void SwStdFontTabPage::dispose()
     pLabelHeightLB.clear();
     pIdxBox.clear();
     pIndexHeightLB.clear();
-    pDocOnlyCB.clear();
     pStandardPB.clear();
     SfxTabPage::dispose();
 }
@@ -692,9 +659,6 @@ static void lcl_SetColl(SwWrtShell* pWrtShell, sal_uInt16 nType,
 
 bool SwStdFontTabPage::FillItemSet( SfxItemSet* )
 {
-    bool bNotDocOnly = !pDocOnlyCB->IsChecked();
-    SW_MOD()->GetModuleConfig()->SetDefaultFontInCurrDocOnly(!bNotDocOnly);
-
     const OUString sStandard    = pStandardBox->GetText();
     const OUString sTitle       = pTitleBox->GetText();
     const OUString sList        = pListBox->GetText();
@@ -706,39 +670,6 @@ bool SwStdFontTabPage::FillItemSet( SfxItemSet* )
     bool bListHeightChanged = pListHeightLB->IsValueChangedFromSaved() && (!bListHeightDefault || !bSetListHeightDefault );
     bool bLabelHeightChanged = pLabelHeightLB->IsValueChangedFromSaved() && (!bLabelHeightDefault || !bSetLabelHeightDefault );
     bool bIndexHeightChanged = pIndexHeightLB->IsValueChangedFromSaved() && (!bIndexHeightDefault || !bSetIndexHeightDefault );
-    if(bNotDocOnly)
-    {
-        pFontConfig->SetFontStandard(sStandard, nFontGroup);
-        pFontConfig->SetFontOutline(sTitle, nFontGroup);
-        pFontConfig->SetFontList(sList, nFontGroup);
-        pFontConfig->SetFontCaption(sLabel, nFontGroup);
-        pFontConfig->SetFontIndex(sIdx, nFontGroup);
-        if(bStandardHeightChanged)
-        {
-            float fSize = (float)pStandardHeightLB->GetValue() / 10;
-            pFontConfig->SetFontHeight( CalcToUnit( fSize, SFX_MAPUNIT_TWIP ), FONT_STANDARD, nFontGroup );
-        }
-        if(bTitleHeightChanged)
-        {
-            float fSize = (float)pTitleHeightLB->GetValue() / 10;
-            pFontConfig->SetFontHeight( CalcToUnit( fSize, SFX_MAPUNIT_TWIP ), FONT_OUTLINE, nFontGroup );
-        }
-        if(bListHeightChanged)
-        {
-            float fSize = (float)pListHeightLB->GetValue() / 10;
-            pFontConfig->SetFontHeight( CalcToUnit( fSize, SFX_MAPUNIT_TWIP ), FONT_LIST, nFontGroup );
-        }
-        if(bLabelHeightChanged)
-        {
-            float fSize = (float)pLabelHeightLB->GetValue() / 10;
-            pFontConfig->SetFontHeight( CalcToUnit( fSize, SFX_MAPUNIT_TWIP ), FONT_CAPTION, nFontGroup );
-        }
-        if(bIndexHeightChanged)
-        {
-            float fSize = (float)pIndexHeightLB->GetValue() / 10;
-            pFontConfig->SetFontHeight( CalcToUnit( fSize, SFX_MAPUNIT_TWIP ), FONT_INDEX, nFontGroup );
-        }
-    }
     if(pWrtShell)
     {
         pWrtShell->StartAllAction();
@@ -923,8 +854,6 @@ void SwStdFontTabPage::Reset( const SfxItemSet* rSet)
             nLabelHeight = SwStdFontConfig::GetDefaultHeightFor( FONT_CAPTION + nFontGroup * FONT_PER_GROUP, eLanguage);
         if( nIndexHeight <= 0)
             nIndexHeight = SwStdFontConfig::GetDefaultHeightFor( FONT_INDEX + nFontGroup * FONT_PER_GROUP, eLanguage);
-
-       pDocOnlyCB->Enable(false);
     }
     else
     {

sw/source/uibase/config/cfgitems.cxx
@@ -122,10 +122,6 @@ void SwDocDisplayItem::FillViewOptions( SwViewOption& rVOpt) const
 SwElemItem::SwElemItem( sal_uInt16 _nWhich ) :
     SfxPoolItem(_nWhich)
 {
-    bHorzScrollbar =
-    bVertScrollbar =
-    bAnyRuler =
-    bHorzRuler     =
     bVertRuler     =
     bVertRulerRight=
     bCrosshair     =
@@ -146,10 +142,6 @@ SwElemItem::SwElemItem( const SwElemItem& rElemItem ):
 SwElemItem::SwElemItem(const SwViewOption& rVOpt, sal_uInt16 _nWhich) :
             SfxPoolItem( _nWhich )
 {
-    bHorzScrollbar  = rVOpt.IsViewHScrollBar();
-    bVertScrollbar  = rVOpt.IsViewVScrollBar();
-    bAnyRuler       = rVOpt.IsViewAnyRuler();
-    bHorzRuler      = rVOpt.IsViewHRuler(true);
     bVertRuler      = rVOpt.IsViewVRuler(true);
     bVertRulerRight = rVOpt.IsVRulerRight();
     bCrosshair      = rVOpt.IsCrossHair();
@@ -173,11 +165,7 @@ bool SwElemItem::operator==( const SfxPoolItem& rAttr ) const
 
     const SwElemItem& rItem = static_cast<const SwElemItem&>(rAttr);
 
-    return (    bHorzScrollbar  == rItem.bHorzScrollbar &&
-                bVertScrollbar  == rItem.bVertScrollbar &&
-                bAnyRuler       == rItem.bAnyRuler &&
-                bHorzRuler      == rItem.bHorzRuler     &&
-                bVertRuler      == rItem.bVertRuler     &&
+    return (    bVertRuler      == rItem.bVertRuler     &&
                 bVertRulerRight == rItem.bVertRulerRight&&
                 bCrosshair      == rItem.bCrosshair     &&
                 bSmoothScroll   == rItem.bSmoothScroll  &&
@@ -190,10 +178,6 @@ bool SwElemItem::operator==( const SfxPoolItem& rAttr ) const
 
 void  SwElemItem::operator=( const SwElemItem& rElemItem)
 {
-    bHorzScrollbar  = rElemItem.  bHorzScrollbar    ;
-    bVertScrollbar  = rElemItem.  bVertScrollbar    ;
-    bAnyRuler       = rElemItem.  bAnyRuler;
-    bHorzRuler      = rElemItem.  bHorzRuler        ;
     bVertRuler      = rElemItem.  bVertRuler        ;
     bVertRulerRight = rElemItem.  bVertRulerRight   ;
     bCrosshair      = rElemItem.  bCrosshair        ;
@@ -207,10 +191,6 @@ void  SwElemItem::operator=( const SwElemItem& rElemItem)
 
 void SwElemItem::FillViewOptions( SwViewOption& rVOpt) const
 {
-    rVOpt.SetViewHScrollBar(bHorzScrollbar );
-    rVOpt.SetViewVScrollBar(bVertScrollbar );
-    rVOpt.SetViewAnyRuler(bAnyRuler);
-    rVOpt.SetViewHRuler(bHorzRuler    );
     rVOpt.SetViewVRuler(bVertRuler    );
     rVOpt.SetVRulerRight(bVertRulerRight );
     rVOpt.SetCrossHair(bCrosshair     );

sw/source/uibase/inc/cfgitems.hxx
@@ -78,10 +78,6 @@ public:
 class SW_DLLPUBLIC SwElemItem : public SfxPoolItem
 {
     //view
-    bool bHorzScrollbar :1;
-    bool bVertScrollbar :1;
-    bool bAnyRuler : 1;
-    bool bHorzRuler     :1;
     bool bVertRuler     :1;
     bool bVertRulerRight:1;
     bool bSmoothScroll  :1;

sw/source/uibase/inc/optpage.hxx
@@ -43,10 +43,6 @@ class SwContentOptPage : public SfxTabPage
 {
     VclPtr<CheckBox>   m_pCrossCB;
 
-    VclPtr<CheckBox>   m_pHScrollBox;
-    VclPtr<CheckBox>   m_pVScrollBox;
-    VclPtr<CheckBox>   m_pAnyRulerCB;
-    VclPtr<CheckBox>   m_pHRulerCBox;
     VclPtr<ListBox>    m_pHMetric;
     VclPtr<CheckBox>   m_pVRulerCBox;
     VclPtr<CheckBox>   m_pVRulerRightCBox;
@@ -65,7 +61,6 @@ class SwContentOptPage : public SfxTabPage
     VclPtr<ListBox>    m_pMetricLB;
 
     DECL_LINK(VertRulerHdl, CheckBox*);
-    DECL_LINK(AnyRulerHdl, CheckBox*);
 public:
     SwContentOptPage( vcl::Window* pParent, const SfxItemSet& rSet);
     virtual ~SwContentOptPage();
@@ -142,7 +137,6 @@ class SwStdFontTabPage : public SfxTabPage
     VclPtr<FontSizeBox>     pLabelHeightLB;
     VclPtr<ComboBox>        pIdxBox     ;
     VclPtr<FontSizeBox>     pIndexHeightLB;
-    VclPtr<CheckBox>        pDocOnlyCB  ;
     VclPtr<PushButton>      pStandardPB;
 
     OUString sShellStd;

sw/uiconfig/swriter/ui/optfonttabpage.ui
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<!-- Generated with glade 3.18.3 -->
+<!-- Generated with glade 3.16.1 -->
 <interface>
   <requires lib="gtk+" version="3.0"/>
-  <requires lib="LibreOffice" version="1.0"/>
+  <!-- interface-requires LibreOffice 1.0 -->
   <object class="GtkBox" id="OptFontTabPage">
     <property name="visible">True</property>
     <property name="can_focus">False</property>
@@ -42,6 +42,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="top_attach">1</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -52,6 +54,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="top_attach">2</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -62,6 +66,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="top_attach">3</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -72,6 +78,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="top_attach">4</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -82,6 +90,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="top_attach">5</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -95,6 +105,8 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="top_attach">0</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -109,6 +121,8 @@
                   <packing>
                     <property name="left_attach">2</property>
                     <property name="top_attach">0</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -123,6 +137,8 @@
                   <packing>
                     <property name="left_attach">0</property>
                     <property name="top_attach">1</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -137,6 +153,8 @@
                   <packing>
                     <property name="left_attach">0</property>
                     <property name="top_attach">2</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -151,6 +169,8 @@
                   <packing>
                     <property name="left_attach">0</property>
                     <property name="top_attach">3</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -165,6 +185,8 @@
                   <packing>
                     <property name="left_attach">0</property>
                     <property name="top_attach">4</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -179,23 +201,8 @@
                   <packing>
                     <property name="left_attach">0</property>
                     <property name="top_attach">5</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkCheckButton" id="doconly">
-                    <property name="label" translatable="yes">C_urrent document only</property>
-                    <property name="visible">True</property>
-                    <property name="can_focus">True</property>
-                    <property name="receives_default">False</property>
-                    <property name="margin_left">12</property>
-                    <property name="use_underline">True</property>
-                    <property name="xalign">0</property>
-                    <property name="draw_indicator">True</property>
-                  </object>
-                  <packing>
-                    <property name="left_attach">0</property>
-                    <property name="top_attach">6</property>
-                    <property name="width">3</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -214,6 +221,8 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="top_attach">1</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -232,6 +241,8 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="top_attach">2</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -250,6 +261,8 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="top_attach">3</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -268,6 +281,8 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="top_attach">4</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>
@@ -286,6 +301,8 @@
                   <packing>
                     <property name="left_attach">1</property>
                     <property name="top_attach">5</property>
+                    <property name="width">1</property>
+                    <property name="height">1</property>
                   </packing>
                 </child>
                 <child>

sw/uiconfig/swriter/ui/viewoptionspage.ui
@@ -1,6 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.16.1 -->
 <interface>
-  <!-- interface-requires gtk+ 3.0 -->
+  <requires lib="gtk+" version="3.0"/>
   <object class="GtkBox" id="ViewOptionsPage">
     <property name="visible">True</property>
     <property name="can_focus">False</property>
@@ -229,25 +230,8 @@
                         <property name="can_focus">False</property>
                         <property name="row_spacing">6</property>
                         <child>
-                          <object class="GtkCheckButton" id="hscrollbar">
-                            <property name="label" translatable="yes">H_orizontal scrollbar</property>
-                            <property name="visible">True</property>
-                            <property name="can_focus">True</property>
-                            <property name="receives_default">False</property>
-                            <property name="use_underline">True</property>
-                            <property name="xalign">0</property>
-                            <property name="draw_indicator">True</property>
-                          </object>
-                          <packing>
-                            <property name="left_attach">0</property>
-                            <property name="top_attach">0</property>
-                            <property name="width">1</property>
-                            <property name="height">1</property>
-                          </packing>
-                        </child>
-                        <child>
-                          <object class="GtkCheckButton" id="vscrollbar">
-                            <property name="label" translatable="yes">_Vertical scrollbar</property>
+                          <object class="GtkCheckButton" id="smoothscroll">
+                            <property name="label" translatable="yes">S_mooth scroll</property>
                             <property name="visible">True</property>
                             <property name="can_focus">True</property>
                             <property name="receives_default">False</property>
@@ -263,63 +247,10 @@
                           </packing>
                         </child>
                         <child>
-                          <object class="GtkCheckButton" id="ruler">
-                            <property name="label" translatable="yes">R_uler</property>
-                            <property name="visible">True</property>
-                            <property name="can_focus">True</property>
-                            <property name="receives_default">False</property>
-                            <property name="use_underline">True</property>
-                            <property name="xalign">0</property>
-                            <property name="draw_indicator">True</property>
-                          </object>
-                          <packing>
-                            <property name="left_attach">0</property>
-                            <property name="top_attach">2</property>
-                            <property name="width">1</property>
-                            <property name="height">1</property>
-                          </packing>
-                        </child>
-                        <child>
-                          <object class="GtkCheckButton" id="smoothscroll">
-                            <property name="label" translatable="yes">S_mooth scroll</property>
-                            <property name="visible">True</property>
-                            <property name="can_focus">True</property>
-                            <property name="receives_default">False</property>
-                            <property name="use_underline">True</property>
-                            <property name="xalign">0</property>
-                            <property name="draw_indicator">True</property>
-                          </object>
-                          <packing>
-                            <property name="left_attach">0</property>
-                            <property name="top_attach">4</property>
-                            <property name="width">1</property>
-                            <property name="height">1</property>
-                          </packing>
-                        </child>
-                        <child>
                           <object class="GtkGrid" id="grid6">
                             <property name="visible">True</property>
                             <property name="can_focus">False</property>
-                            <property name="margin_left">12</property>
                             <property name="row_spacing">6</property>
-                            <property name="column_spacing">12</property>
-                            <child>
-                              <object class="GtkCheckButton" id="hruler">
-                                <property name="label" translatable="yes">Hori_zontal ruler</property>
-                                <property name="visible">True</property>
-                                <property name="can_focus">True</property>
-                                <property name="receives_default">False</property>
-                                <property name="use_underline">True</property>
-                                <property name="xalign">0</property>
-                                <property name="draw_indicator">True</property>
-                              </object>
-                              <packing>
-                                <property name="left_attach">0</property>
-                                <property name="top_attach">0</property>
-                                <property name="width">1</property>
-                                <property name="height">1</property>
-                              </packing>
-                            </child>
                             <child>
                               <object class="GtkCheckButton" id="vruler">
                                 <property name="label" translatable="yes">Verti_cal ruler</property>
@@ -379,10 +310,26 @@
                                 <property name="height">1</property>
                               </packing>
                             </child>
+                            <child>
+                              <object class="GtkLabel" id="hruler">
+                                <property name="visible">True</property>
+                                <property name="can_focus">False</property>
+                                <property name="label" translatable="yes">Hori_zontal ruler</property>
+                                <property name="use_underline">True</property>
+                                <property name="mnemonic_widget">hrulercombobox</property>
+                                <property name="ellipsize">start</property>
+                              </object>
+                              <packing>
+                                <property name="left_attach">0</property>
+                                <property name="top_attach">0</property>
+                                <property name="width">1</property>
+                                <property name="height">1</property>
+                              </packing>
+                            </child>
                           </object>
                           <packing>
                             <property name="left_attach">0</property>
-                            <property name="top_attach">3</property>
+                            <property name="top_attach">0</property>
                             <property name="width">1</property>
                             <property name="height">1</property>
                           </packing>

commit 60bbbd5593b81d9211375ac3841e2b2e20b6c93d

Writer View options: Right-aligned should be disabled,
if Vertical ruler is unchecked.

Regression since

commit 745c7bce5fe8b218f7d300667a5409bc7ef58c34
Author: Mihály Palenik <palenik.mihaly@gmail.com>
Date:   Thu Aug 6 15:16:41 2015 +0200

Delete unnecessary writer options from Options dialog

Change-Id: I17096e87f83dfd831599d316495c58ac08927895
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173407
Tested-by: Jenkins
Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

sw/source/ui/config/optpage.cxx
@@ -247,7 +247,7 @@ void SwContentOptPage::Reset(const SfxItemSet* rSet)
 
         bReadOnly = officecfg::Office::Writer::Layout::Window::IsVerticalRulerRight::isReadOnly();
         m_xVRulerRightCBox->set_active(pElemAttr->m_bVertRulerRight);
-        m_xVRulerRightCBox->set_sensitive(!bReadOnly);
+        m_xVRulerRightCBox->set_sensitive(pElemAttr->m_bVertRuler && !bReadOnly);
         m_xVRulerRightImg->set_visible(bReadOnly);
 
         bReadOnly = !bWebOptionsPage ? officecfg::Office::Writer::Layout::Window::SmoothScroll::isReadOnly() :