LibreOffice/core

commit 2157a3536f97ff5ae7c82611a801fef7e3708983

sfx2 store: try rename before copying
Rename is cheaper then copying the content over manually, so try that
first.

Change-Id: Ieb1d03e39501c1565dae7e3290e318a09ee18965
Reviewed-on: https://gerrit.libreoffice.org/47612
Tested-by: Jenkins <ci@libreoffice.org>
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>

sfx2/source/doc/docfile.cxx
@@ -1721,29 +1721,36 @@ void SfxMedium::TransactedTransferForFS_Impl( const INetURLObject& aSource,
 
             try
             {
-                if( bOverWrite && ::utl::UCBContentHelper::IsDocument( aDest.GetMainURL( INetURLObject::DecodeMechanism::NONE ) ) )
+                OUString aSourceMainURL = aSource.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                OUString aDestMainURL = aDest.GetMainURL(INetURLObject::DecodeMechanism::NONE);
+                if (comphelper::isFileUrl(aDestMainURL) && osl::File::move(aSourceMainURL, aDestMainURL) == osl::FileBase::E_None)
+                    bResult = true;
+                else
                 {
-                    if( pImpl->m_aBackupURL.isEmpty() )
-                        DoInternalBackup_Impl( aOriginalContent );
-
-                    if( !pImpl->m_aBackupURL.isEmpty() )
+                    if (bOverWrite && ::utl::UCBContentHelper::IsDocument(aDestMainURL))
                     {
-                        Reference< XInputStream > aTempInput = aTempCont.openStream();
-                        bTransactStarted = true;
-                        aOriginalContent.setPropertyValue( "Size", uno::makeAny( (sal_Int64)0 ) );
-                        aOriginalContent.writeStream( aTempInput, bOverWrite );
-                        bResult = true;
+                        if( pImpl->m_aBackupURL.isEmpty() )
+                            DoInternalBackup_Impl( aOriginalContent );
+
+                        if( !pImpl->m_aBackupURL.isEmpty() )
+                        {
+                            Reference< XInputStream > aTempInput = aTempCont.openStream();
+                            bTransactStarted = true;
+                            aOriginalContent.setPropertyValue( "Size", uno::makeAny( (sal_Int64)0 ) );
+                            aOriginalContent.writeStream( aTempInput, bOverWrite );
+                            bResult = true;
+                        }
+                        else
+                        {
+                            pImpl->m_eError = ERRCODE_SFX_CANTCREATEBACKUP;
+                        }
                     }
                     else
                     {
-                        pImpl->m_eError = ERRCODE_SFX_CANTCREATEBACKUP;
+                        Reference< XInputStream > aTempInput = aTempCont.openStream();
+                        aOriginalContent.writeStream( aTempInput, bOverWrite );
+                        bResult = true;
                     }
-                }
-                else
-                {
-                    Reference< XInputStream > aTempInput = aTempCont.openStream();
-                    aOriginalContent.writeStream( aTempInput, bOverWrite );
-                    bResult = true;
                 }
             }
             catch ( const css::ucb::CommandAbortedException& )

commit 63cb8d9ddd6db64e41f66df937f42875637bede5

tdf#161876 sfx2 store: refuse to overwrite read-only files
Create a read-only file, try to save-as the current document and select
the existing read-only file, accept to overwrite it, works fine.

This is a regression from commit
2157a3536f97ff5ae7c82611a801fef7e3708983 (sfx2 store: try rename before
copying, 2018-01-09), previously such read-only targets were rejected.

Fix the problem by explicitly checking if the target is writable: if
not, then avoid the rename optimization, because the fallback path will
reject the request as expected.

Fix this inside an '#ifdef UNX' block, the problem is reportedly not
there on Windows.

Change-Id: Ifd8798e07a1be504c6747514a53cfc4ff2338a0c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173344
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sfx2/source/doc/docfile.cxx
@@ -258,6 +258,11 @@ bool IsFileMovable(const INetURLObject& rURL)
     // Hardlink or symlink: osl::File::move() doesn't play with these nicely.
     if (buf.st_nlink > 1 || S_ISLNK(buf.st_mode))
         return false;
+
+    // Read-only target path: this would be silently replaced.
+    if (access(sPath.toUtf8().getStr(), W_OK) == -1)
+        return false;
+
 #elif defined _WIN32
     if (tools::IsMappedWebDAVPath(rURL.GetMainURL(INetURLObject::DecodeMechanism::NONE)))
         return false;

sfx2/qa/cppunit/test_misc.cxx
@@ -169,6 +169,37 @@ CPPUNIT_TEST_FIXTURE(MiscTest, testHardLinks)
 #endif
 }
 
+CPPUNIT_TEST_FIXTURE(MiscTest, testtestOverwriteReadOnly)
+{
+#ifdef UNX
+    // Given a read-only, already created file:
+    OUString aTargetDir = m_directories.getURLFromWorkdir(u"/CppunitTest/sfx2_misc.test.user/");
+    OUString aURL(aTargetDir + "read-only.odt");
+    osl::File aFile(aURL);
+    aFile.open(osl_File_OpenFlag_Create);
+    aFile.close();
+    osl::File::setAttributes(aURL, osl_File_Attribute_OwnRead);
+    mxComponent = loadFromDesktop(aURL, u"com.sun.star.text.TextDocument"_ustr);
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY);
+    bool bFail = false;
+
+    // When trying to overwrite that:
+    try
+    {
+        xStorable->storeToURL(aURL, { comphelper::makePropertyValue("Overwrite", true) });
+    }
+    catch (const io::IOException&)
+    {
+        bFail = true;
+    }
+
+    // Then make sure we fail:
+    // Without the accompanying fix in place, this test would have failed, the overwrite would
+    // silently ignore the read-only attribute of the file.
+    CPPUNIT_ASSERT(bFail);
+#endif
+}
+
 CPPUNIT_TEST_FIXTURE(MiscTest, testOverwrite)
 {
     // tdf#60237 - try to overwrite an existing file using the different settings of the Overwrite option