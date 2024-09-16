b851cb61d3e4d5018475051f785d7659694a21e8
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-09-16 17:33:44 +0100

    This should iterate over the thesaurus languages, not the spelling ones
    Change-Id: I94410274f7afbc0a6bc33fe43beb9810a905dfe4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173467
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -7971,7 +7971,7 @@ static void preloadData()
 
     // preload all available thesauri
     css::uno::Reference<linguistic2::XThesaurus> xThesaurus(xLngSvcMgr->getThesaurus());
-    css::uno::Reference<linguistic2::XSupportedLocales> xThesLocales(xSpellChecker, css::uno::UNO_QUERY_THROW);
+    css::uno::Reference<linguistic2::XSupportedLocales> xThesLocales(xThesaurus, css::uno::UNO_QUERY_THROW);
     aLocales = xThesLocales->getLocales();
     std::cerr << "Preloading local thesauri: ";
     for (auto &it : std::as_const(aLocales))
