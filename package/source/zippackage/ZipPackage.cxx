5f1995496a305ae08daaf80cb84424944a92dbb3
Michael Stahl <michael.stahl@allotropia.de>
2024-09-16 20:31:06 +0200

    tdf#162866 package: fix loading AutoCorrect file with case-insensitive
    ... duplicates; the directory names of AutoCorrect entries are
    user-editable, so this needs to be supported.
    
    AutoCorrect uses an ODF package because the ODF document loading code
    requires the ODF document to be in an ODF storage with a MediaType
    property.
    
    AutoCorrect writes an empty mimetype file, and if such is present in an
    .odt file that is being loaded, existing checks will detect it as
    corrupted, so we can use this to check that the file is an AutoCorrect
    file and turn off the case-insensitive check.
    
    (regression from commit 4833f131243bdb409ddfaff8b4db87d4ed2af98f)
    
    Change-Id: I43887f7dad0c8cbb465b4c0f1c38bcc3244a7675
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173477
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins
    (cherry picked from commit 9012355a60bd88db582078e38123863a4959b72f)

--- package/source/zippackage/ZipPackage.cxx
+++ package/source/zippackage/ZipPackage.cxx
@@ -806,7 +806,11 @@ void SAL_CALL ZipPackage::initialize( const uno::Sequence< Any >& aArguments )
             {
                 m_pZipFile = std::make_unique<ZipFile>(m_aMutexHolder, m_xContentStream, m_xContext, true,
                     m_bForceRecovery,
-                    m_nFormat == embed::StorageFormats::ZIP ? ZipFile::Checks::Default : ZipFile::Checks::CheckInsensitive);
+                    m_nFormat == embed::StorageFormats::ZIP
+                        ? ZipFile::Checks::Default
+                        : m_nFormat == embed::StorageFormats::OFOPXML
+                            ? ZipFile::Checks::CheckInsensitive
+                            : ZipFile::Checks::TryCheckInsensitive);
                 getZipFileContents();
             }
             catch ( IOException & e )
@@ -1185,7 +1189,11 @@ void ZipPackage::ConnectTo( const uno::Reference< io::XInputStream >& xInStream 
     else
         m_pZipFile = std::make_unique<ZipFile>(m_aMutexHolder, m_xContentStream, m_xContext, false,
             false,
-            m_nFormat == embed::StorageFormats::ZIP ? ZipFile::Checks::Default : ZipFile::Checks::CheckInsensitive);
+            m_nFormat == embed::StorageFormats::ZIP
+                ? ZipFile::Checks::Default
+                : m_nFormat == embed::StorageFormats::OFOPXML
+                    ? ZipFile::Checks::CheckInsensitive
+                    : ZipFile::Checks::TryCheckInsensitive);
 }
 
 namespace
