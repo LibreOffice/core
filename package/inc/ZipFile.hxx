LibreOffice/core

commit 4833f131243bdb409ddfaff8b4db87d4ed2af98f

package: ZipFile: don't accept duplicate entries (case insensitive)
This is required for OOXML, but not for ODF.

Unclear if there are use cases for this with ODF, can add some
conditions if it turns out to be a problem.

Change-Id: I3810da5c2273574135d133b4a9bbad98dc97af44
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170223
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

package/inc/ZipFile.hxx
@@ -30,6 +30,7 @@
 #include "EncryptionData.hxx"
 
 #include <span>
+#include <unordered_set>
 
 class MemoryByteGrabber;
 namespace com::sun::star {
@@ -50,9 +51,12 @@ namespace rtl
 
 class ZipEnumeration;
 
+
 class ZipFile
 {
     rtl::Reference<comphelper::RefCountedMutex> m_aMutexHolder;
+
+    std::unordered_set<OUString> m_EntriesInsensitive;
 
     EntryHash       aEntries;
     ByteGrabber     aGrabber;

package/source/zipapi/ZipFile.cxx
@@ -94,6 +94,7 @@ ZipFile::ZipFile( rtl::Reference<comphelper::RefCountedMutex> aMutexHolder,
     if (bInitialise && readCEN() == -1 )
     {
         aEntries.clear();
+        m_EntriesInsensitive.clear();
         throw ZipException( u"stream data looks to be broken"_ustr );
     }
 }
@@ -118,6 +119,7 @@ ZipFile::ZipFile( rtl::Reference< comphelper::RefCountedMutex > aMutexHolder,
         else if ( readCEN() == -1 )
         {
             aEntries.clear();
+            m_EntriesInsensitive.clear();
             throw ZipException(u"stream data looks to be broken"_ustr );
         }
     }
@@ -1156,15 +1158,19 @@ sal_Int32 ZipFile::readCEN()
                     continue; // This is a directory entry, not a stream - skip it
             }
 
-            if (auto it = aEntries.find(aEntry.sPath); it == aEntries.end())
-            {
-                aEntries[aEntry.sPath] = aEntry;
-            }
-            else
+            if (aEntries.find(aEntry.sPath) != aEntries.end())
             {
                 SAL_INFO("package", "Duplicate CEN entry: \"" << aEntry.sPath << "\"");
                 throw ZipException(u"Duplicate CEN entry"_ustr);
             }
+            // this is required for OOXML, but not for ODF
+            auto const lowerPath(aEntry.sPath.toAsciiLowerCase());
+            if (!m_EntriesInsensitive.insert(lowerPath).second)
+            {
+                SAL_INFO("package", "Duplicate CEN entry (case insensitive): \"" << aEntry.sPath << "\"");
+                throw ZipException(u"Duplicate CEN entry (case insensitive)"_ustr);
+            }
+            aEntries[aEntry.sPath] = aEntry;
         }
 
         if (nCount != nTotal)
@@ -1335,6 +1341,12 @@ void ZipFile::HandlePK34(std::span<const sal_Int8> data, sal_Int64 dataOffset, s
                != aEntries.end())
         return;
 
+    auto const lowerPath(aEntry.sPath.toAsciiLowerCase());
+    if (m_EntriesInsensitive.find(lowerPath) != m_EntriesInsensitive.end())
+    {   // this is required for OOXML, but not for ODF
+        return;
+    }
+    m_EntriesInsensitive.insert(lowerPath);
     aEntries.emplace(aEntry.sPath, aEntry);
 
     // Drop any "directory" entry corresponding to this one's path; since we don't use

commit 9012355a60bd88db582078e38123863a4959b72f

tdf#162866 package: fix loading AutoCorrect file with case-insensitive
... duplicates; the directory names of AutoCorrect entries are
user-editable, so this needs to be supported.

AutoCorrect uses an ODF package because the ODF document loading code
requires the ODF document to be in an ODF storage with a MediaType
property.

AutoCorrect writes an empty mimetype file, and if such is present in an
.odt file that is being loaded, existing checks will detect it as
corrupted, so we can use this to check that the file is an AutoCorrect
file and turn off the case-insensitive check.

(regression from commit 4833f131243bdb409ddfaff8b4db87d4ed2af98f)

Change-Id: I43887f7dad0c8cbb465b4c0f1c38bcc3244a7675
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173477
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

package/inc/ZipFile.hxx
@@ -56,7 +56,7 @@ class ZipEnumeration;
 class ZipFile
 {
 public:
-    enum class Checks { Default, CheckInsensitive };
+    enum class Checks { Default, CheckInsensitive, TryCheckInsensitive };
 
 private:
     rtl::Reference<comphelper::RefCountedMutex> m_aMutexHolder;

package/source/zipapi/ZipFile.cxx
@@ -1500,6 +1500,17 @@ sal_Int32 ZipFile::readCEN()
                 SAL_INFO("package", "Duplicate CEN entry: \"" << aEntry.sPath << "\"");
                 throw ZipException(u"Duplicate CEN entry"_ustr);
             }
+            if (aEntries.empty() && m_Checks == Checks::TryCheckInsensitive)
+            {
+                if (aEntry.sPath == "mimetype" && aEntry.nSize == 0)
+                {   // tdf#162866 AutoCorrect uses ODF package, directories are
+                    m_Checks = Checks::Default; // user-defined => ignore!
+                }
+                else
+                {
+                    m_Checks = Checks::CheckInsensitive;
+                }
+            }
             // this is required for OOXML, but not for ODF
             auto const lowerPath(aEntry.sPath.toAsciiLowerCase());
             if (!m_EntriesInsensitive.insert(lowerPath).second && m_Checks == Checks::CheckInsensitive)

package/source/zippackage/ZipPackage.cxx
@@ -896,7 +896,11 @@ void SAL_CALL ZipPackage::initialize( const uno::Sequence< Any >& aArguments )
     {
         m_pZipFile.emplace(m_aMutexHolder, m_xContentStream, m_xContext, true,
             m_bForceRecovery,
-            m_nFormat == embed::StorageFormats::ZIP ? ZipFile::Checks::Default : ZipFile::Checks::CheckInsensitive);
+            m_nFormat == embed::StorageFormats::ZIP
+                ? ZipFile::Checks::Default
+                : m_nFormat == embed::StorageFormats::OFOPXML
+                    ? ZipFile::Checks::CheckInsensitive
+                    : ZipFile::Checks::TryCheckInsensitive);
         getZipFileContents();
     }
     catch ( IOException & e )
@@ -1270,7 +1274,11 @@ void ZipPackage::ConnectTo( const uno::Reference< io::XInputStream >& xInStream 
     else
         m_pZipFile.emplace(m_aMutexHolder, m_xContentStream, m_xContext, false,
             false,
-            m_nFormat == embed::StorageFormats::ZIP ? ZipFile::Checks::Default : ZipFile::Checks::CheckInsensitive);
+            m_nFormat == embed::StorageFormats::ZIP
+                ? ZipFile::Checks::Default
+                : m_nFormat == embed::StorageFormats::OFOPXML
+                    ? ZipFile::Checks::CheckInsensitive
+                    : ZipFile::Checks::TryCheckInsensitive);
 }
 
 uno::Reference< io::XInputStream > ZipPackage::writeTempFile()