LibreOffice/core

commit 7db1150b2c4a9adb993084d2dceedb450781b1dc

fix macos build with clang 16
Undefined symbols for architecture arm64:
  "o3tl::strong_int<unsigned short, LanguageTypeTag>::strong_int<int>(int, std::__1::enable_if<std::is_integral<int>::value, int>::type)", referenced from:
      sfx2::(anonymous namespace)::SvxInternalLink::Connect(sfx2::SvBaseLink*) in linkmgr2.o
      SfxHTMLParser::GetTableDataOptionsValNum(unsigned int&, o3tl::strong_int<unsigned short, LanguageTypeTag>&, rtl::OUString const&, std::__1::basic_string_view<char16_t, std::__1::char_traits<char16_t>>, SvNumberFormatter&) in sfxhtml.o
      SfxHTMLParser::GetTableDataOptionsValNum(unsigned int&, o3tl::strong_int<unsigned short, LanguageTypeTag>&, rtl::OUString const&, std::__1::basic_string_view<char16_t, std::__1::char_traits<char16_t>>, SvNumberFormatter&) in sfxhtml.o
      SfxHTMLParser::GetTableDataOptionsValNum(unsigned int&, o3tl::strong_int<unsigned short, LanguageTypeTag>&, rtl::OUString const&, std::__1::basic_string_view<char16_t, std::__1::char_traits<char16_t>>, SvNumberFormatter&) in sfxhtml.o
      SfxHTMLParser::GetTableDataOptionsValNum(unsigned int&, o3tl::strong_int<unsigned short, LanguageTypeTag>&, rtl::OUString const&, std::__1::basic_string_view<char16_t, std::__1::char_traits<char16_t>>, SvNumberFormatter&) in sfxhtml.o
ld: symbol(s) not found for architecture arm64
clang++: error: linker command failed with exit code 1 (use -v to see invocation)
make[1]: *** [/Users/noelgrandin/lode/dev/core/sfx2/Library_sfx.mk:20: /Users/noelgrandin/lode/dev/core/instdir/LibreOfficeDev.app/Contents/Frameworks/libsfxlo.dylib] Error 1
make[1]: Target 'build' not remade because of errors.
make: *** [Makefile:296: build] Error 2

Change-Id: Ia8c338a89156e18a1c96630ecad2bccbba7bb745
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173652
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/i18nlangtag/lang.h
@@ -90,10 +90,13 @@ constexpr LanguageType primary(LanguageType lt) { return LanguageType(sal_uInt16
 
 namespace o3tl
 {
+// when compiling LO on macOS, debug builds will display a linking error
+#if !(defined MACOSX && defined __clang__ && __clang_major__ == 16)
     // delete "sal_Int16" constructor via specialization: values > 0x7FFF are
     // actually used, and unfortunately passed around in the API as signed
     // "short", so use this to find all places where casts must be inserted
     template<> template<> constexpr strong_int<unsigned short,LanguageTypeTag>::strong_int(short, std::enable_if<std::is_integral<short>::value, int>::type) = delete;
+#endif
 }
 
 #define LANGUAGE_MASK_PRIMARY 0x03ff

include/o3tl/strong_int.hxx
@@ -84,6 +84,14 @@ template <typename UNDERLYING_TYPE, typename PHANTOM_TYPE>
 struct strong_int
 {
 public:
+// when compiling LO on macOS, debug builds will display a linking error
+#if defined MACOSX && defined __clang__ && __clang_major__ == 16
+    explicit constexpr strong_int(unsigned long long value) : m_value(value) {}
+    explicit constexpr strong_int(unsigned long value) : m_value(value) {}
+    explicit constexpr strong_int(long value) : m_value(value) {}
+    explicit constexpr strong_int(int value) : m_value(value) {}
+    explicit constexpr strong_int(unsigned int value) : m_value(value) {}
+#else
     template<typename T> explicit constexpr strong_int(
         T value,
         typename std::enable_if<std::is_integral<T>::value, int>::type = 0):
@@ -95,6 +103,7 @@ public:
                && "out of range");
 #endif
     }
+#endif
     strong_int() : m_value(0) {}
 
     explicit constexpr operator UNDERLYING_TYPE() const { return m_value; }

commit fea3c9d69cad1a0408883bf5dc44670cc53fcbc2

Confine a hack to known-broken --enable-pch
The hack was introduced in 7db1150b2c4a9adb993084d2dceedb450781b1dc "fix macos
build with clang 16".  (And for our --enable-pch for Clang being known-broken,
also see ffc1ab15ae358315516aab319778a254688afbd3 "Work around some Clang PCH
consteval issue by disabling HAVE_CPP_CONSTEVAL".)

Change-Id: I5dcd7f96ba39bf4d0748940778699cb7d368f449
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173891
Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

config_host/config_options.h.in
@@ -9,6 +9,8 @@
 
 #define ENABLE_MERGELIBS_MORE 0
 
+#define ENABLE_PCH 0
+
 #define ENABLE_RUNTIME_OPTIMIZATIONS 0
 
 // Used to turn off visibility for some classes/symbols when linking with --enable-mergelibs

configure.ac
@@ -6595,6 +6595,9 @@ else
     AC_MSG_ERROR([Unknown value for --enable-pch])
 fi
 AC_SUBST(ENABLE_PCH)
+if test -n "$ENABLE_PCH"; then
+    AC_DEFINE(ENABLE_PCH)
+fi
 # ccache 3.7.1 and older do not properly detect/handle -include .gch in CCACHE_DEPEND mode
 if test -n "$ENABLE_PCH" -a -n "$CCACHE_DEPEND_MODE" -a "$GCC" = "yes" -a "$COM_IS_CLANG" != "TRUE"; then
     AC_PATH_PROG([CCACHE_BIN],[ccache],[not found])

include/i18nlangtag/lang.h
@@ -80,6 +80,7 @@
    http://support.microsoft.com/default.aspx?scid=KB;en-us;q221435
  */
 
+#include <config_options.h>
 #include <sal/types.h>
 #include <o3tl/strong_int.hxx>
 #include <ostream>
@@ -90,8 +91,10 @@ constexpr LanguageType primary(LanguageType lt) { return LanguageType(sal_uInt16
 
 namespace o3tl
 {
-// when compiling LO on macOS, debug builds will display a linking error
-#if !(defined MACOSX && defined __clang__ && __clang_major__ == 16)
+// when compiling LO on macOS, debug builds will display a linking error where, see
+// <https://lists.freedesktop.org/archives/libreoffice/2024-February/091564.html>, "Our Clang
+// --enable-pch setup is known broken":
+#if !(defined MACOSX && defined __clang__ && __clang_major__ == 16 && ENABLE_PCH)
     // delete "sal_Int16" constructor via specialization: values > 0x7FFF are
     // actually used, and unfortunately passed around in the API as signed
     // "short", so use this to find all places where casts must be inserted

include/o3tl/strong_int.hxx
@@ -25,6 +25,7 @@
 #include <cassert>
 #include <type_traits>
 #include <compare>
+#include <config_options.h>
 
 namespace o3tl
 {
@@ -84,8 +85,10 @@ template <typename UNDERLYING_TYPE, typename PHANTOM_TYPE>
 struct strong_int
 {
 public:
-// when compiling LO on macOS, debug builds will display a linking error
-#if defined MACOSX && defined __clang__ && __clang_major__ == 16
+// when compiling LO on macOS, debug builds will display a linking error where, see
+// <https://lists.freedesktop.org/archives/libreoffice/2024-February/091564.html>, "Our Clang
+// --enable-pch setup is known broken":
+#if defined MACOSX && defined __clang__ && __clang_major__ == 16 && ENABLE_PCH
     explicit constexpr strong_int(unsigned long long value) : m_value(value) {}
     explicit constexpr strong_int(unsigned long value) : m_value(value) {}
     explicit constexpr strong_int(long value) : m_value(value) {}