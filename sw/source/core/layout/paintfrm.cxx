LibreOffice/core

commit 13495926390ba54fbcd45ec00fbe613b1f2b734b

sw: do not paint hidden frames
Table frames in hidden sections may still paint their border lines.

Check that the height (or width, if vertical) is not 0 before painting.

(regression from commit ff7f1b59e22092d8548459e75fe912db852f056f)

Change-Id: I6ace6d6608d5e23ff2f3c5a61d37e3f6da958977
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/172250
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/paintfrm.cxx
@@ -3718,6 +3718,11 @@ SwShortCut::SwShortCut( const SwFrame& rFrame, const SwRect& rRect )
 
 void SwLayoutFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const& rRect, PaintFrameMode ePaintFrameMode, SwPrintData const*const) const
 {
+    if (!getFramePrintArea().HasArea())
+    {
+        return; // do not paint hidden frame
+    }
+
     // #i16816# tagged pdf support
     Frame_Info aFrameInfo(*this, false);
     SwTaggedPDFHelper aTaggedPDFHelper( nullptr,
@@ -4668,6 +4673,10 @@ void SwTextFrame::PaintOutlineContentVisibilityButton() const
 
 void SwTabFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const& rRect, PaintFrameMode, SwPrintData const*const) const
 {
+    if (!getFramePrintArea().HasArea())
+    {
+        return; // do not paint hidden frame
+    }
     const SwViewOption* pViewOption = gProp.pSGlobalShell->GetViewOptions();
     if (pViewOption->IsTable())
     {

commit d5fcc8ea25e416a4d2bea5e0f92c0e403997d845

tdf#163032 sw: fix painting of row frame with rowspan > 1 cell
Super non intuitive representation of a rowspan > 1 cell split across
pages is that the follow frame will be in a row that's 0 height, but its
own height extends to the bottom of the following merged rows.

(regression from commit 13495926390ba54fbcd45ec00fbe613b1f2b734b)

Change-Id: I0e32477613ceb5b6dbcc590bf70e0b5fc62a5d0a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174399
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/paintfrm.cxx
@@ -3718,8 +3718,8 @@ SwShortCut::SwShortCut( const SwFrame& rFrame, const SwRect& rRect )
 
 void SwLayoutFrame::PaintSwFrame(vcl::RenderContext& rRenderContext, SwRect const& rRect, PaintFrameMode ePaintFrameMode) const
 {
-    if (!getFramePrintArea().HasArea())
-    {
+    if (!getFramePrintArea().HasArea() && !IsRowFrame())
+    {   // tdf#163032 row frame may contain rowspan>1 cell that must be painted
         return; // do not paint hidden frame
     }
 