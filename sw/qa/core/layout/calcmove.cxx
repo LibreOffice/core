LibreOffice/core

commit 2c23d4ee1e1370b20560e47db7efaeaac1d94b26

tdf#96515 sw Hide Whitespace: avoid creating unneeded page frames
Change-Id: I9b273543ccf2eaa87116c6e1475860e9e872c445

sw/source/core/layout/calcmove.cxx
@@ -1520,7 +1520,34 @@ void SwContentFrame::MakeAll(vcl::RenderContext* /*pRenderContext*/)
         // Bottom(). This might happen with undersized TextFrames on the lower edge of a
         // multi-column section
         const long nPrtBottom = (GetUpper()->*fnRect->fnGetPrtBottom)();
-        const long nBottomDist =  (Frame().*fnRect->fnBottomDist)( nPrtBottom );
+        long nBottomDist = (Frame().*fnRect->fnBottomDist)(nPrtBottom);
+
+        if (getRootFrame()->GetCurrShell()->GetViewOptions()->IsWhitespaceHidden())
+        {
+            // When whitespace is hidden, the page frame has two heights: the
+            // nominal (defined by the frame format), and the actual (which is
+            // at most the nominal height, but can be smaller in case there is
+            // no content for the whole page).
+            // The layout size is the actual one, but we want to move the
+            // content frame to a new page only in case it doesn't fit the
+            // nominal size.
+            if (nBottomDist < 0)
+            {
+                // Content frame doesn't fit the actual size, check if it fits the nominal one.
+                SwPageFrame* pPageFrame = FindPageFrame();
+                const SwFrameFormat* pPageFormat = static_cast<const SwFrameFormat*>(pPageFrame->GetRegisteredIn());
+                const Size& rPageSize = pPageFormat->GetFrameSize().GetSize();
+                long nWhitespace = rPageSize.getHeight() - pPageFrame->Frame().Height();
+                if (nWhitespace > -nBottomDist)
+                {
+                    // It does: don't move it and invalidate our page frame so
+                    // that it gets a larger height.
+                    nBottomDist = 0;
+                    pPageFrame->InvalidateSize();
+                }
+            }
+        }
+
         if( nBottomDist >= 0 )
         {
             if ( bKeep && bMoveable )

sw/qa/extras/uiwriter/uiwriter.cxx
@@ -176,6 +176,7 @@ public:
     void testTdf87922();
     void testTdf77014();
     void testTdf92648();
+    void testTdf96515();
 
     CPPUNIT_TEST_SUITE(SwUiWriterTest);
     CPPUNIT_TEST(testReplaceForward);
@@ -258,6 +259,7 @@ public:
     CPPUNIT_TEST(testTdf87922);
     CPPUNIT_TEST(testTdf77014);
     CPPUNIT_TEST(testTdf92648);
+    CPPUNIT_TEST(testTdf96515);
     CPPUNIT_TEST_SUITE_END();
 
 private:
@@ -2918,6 +2920,25 @@ void SwUiWriterTest::testTdf92648()
         SwFormatFrameSize aSize((*it)->GetFrameSize());
         CPPUNIT_ASSERT(aSize.GetHeight() != 0);
     }
+}
+
+void SwUiWriterTest::testTdf96515()
+{
+    // Enable hide whitespace mode.
+    SwDoc* pDoc = createDoc();
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    SwViewOption aViewOptions(*pWrtShell->GetViewOptions());
+    aViewOptions.SetHideWhitespaceMode(true);
+    pWrtShell->ApplyViewOptions(aViewOptions);
+
+    // Insert a new paragraph at the end of the document.
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<text::XParagraphAppend> xParagraphAppend(xTextDocument->getText(), uno::UNO_QUERY);
+    xParagraphAppend->finishParagraph(uno::Sequence<beans::PropertyValue>());
+    calcLayout();
+
+    // This was 2, a new page was created for the new paragraph.
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SwUiWriterTest);

commit ba7199da2e6ec9d58475373f264326fbeb31f4bd

tdf#156218 sw hide whitespace: fix increased page size on new para at doc end
Create a document of 1 page, add empty paragraphs, so the next empty
paragraph would create a second page. Hide whitespace, then add that
paragraph: no second page is created.

Seems this is a regression from commit
2c23d4ee1e1370b20560e47db7efaeaac1d94b26 (tdf#96515 sw Hide Whitespace:
avoid creating unneeded page frames, 2015-12-16), because previously we
had the opposite problem: creating new page frames all the time, even if
the current page frame could grow.

Fix the problem by:

1) In SwPageFrame::MakeAll(), fix the size of the page frame when
switching to hide whitespace mode, so margins are not part of the
height. At least the last paragraph of the doc is now near the bottom of
the page, so it's more likely to create a new page. This means now we're
back to still tweaking the size of the last page: but we used to
determine the height based on just content and here we just ignore
top/bottom margin.

2) In SwPageFrame::CheckPageHeightValidForHideWhitespace(), handle the
top/bottom margins, otherwise we'll think that the page frame can
still grow, when it should not.

3) Finally in SwPageFrame::MakeAll(), when we determine the height for
non-last pages, the text frame for the new paragraph is already
inserted, so limit the page frame height: if "content height" is more
than "height - whitespace", then that limit should be the height,
otherwise the page frame height will grow forever.

Change-Id: I1125c31ec8b387df41c0881af98469d56ed773db
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174368
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

sw/source/core/layout/calcmove.cxx
@@ -907,11 +907,18 @@ void SwPageFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     setFramePrintAreaValid(true);
                     continue;
                 }
-                else if (pSh && pSh->GetViewOptions()->IsWhitespaceHidden() && pRootFrame->GetLastPage() != this)
+                else if (pSh && pSh->GetViewOptions()->IsWhitespaceHidden())
                 {
                     tools::Long height = 0;
                     SwLayoutFrame *pBody = FindBodyCont();
-                    if ( pBody && pBody->Lower() && pBody->Lower()->IsColumnFrame() )
+                    SwTwips nFullBodyHeight = pAttrs->GetSize().Height() - pAttrs->CalcTop() - pAttrs->CalcBottom();
+                    if (pRootFrame->GetLastPage() == this)
+                    {
+                        // Last page is only reduced by the top/bottom margin, the body frame height
+                        // is not reduced.
+                        height = nFullBodyHeight;
+                    }
+                    else if ( pBody && pBody->Lower() && pBody->Lower()->IsColumnFrame() )
                     {
                         // Columns have a fixed height
                         height = pAttrs->GetSize().Height();
@@ -920,6 +927,12 @@ void SwPageFrame::MakeAll(vcl::RenderContext* pRenderContext)
                     {
                         // No need for borders.
                         height = GetContentHeight(0, 0);
+                        if (height > nFullBodyHeight)
+                        {
+                            // Content height would be larger than the show-whitespace body height,
+                            // limit it.
+                            height = nFullBodyHeight;
+                        }
                     }
 
                     if (height > 0)

sw/source/core/layout/pagechg.cxx
@@ -65,6 +65,7 @@
 #include <sortedobjs.hxx>
 #include <calbck.hxx>
 #include <txtfly.hxx>
+#include <frmatr.hxx>
 
 using namespace ::com::sun::star;
 
@@ -2561,7 +2562,10 @@ bool SwPageFrame::CheckPageHeightValidForHideWhitespace(SwTwips nDiff)
             // Content frame doesn't fit the actual size, check if it fits the nominal one.
             const SwFrameFormat* pPageFormat = static_cast<const SwFrameFormat*>(GetDep());
             const Size& rPageSize = pPageFormat->GetFrameSize().GetSize();
-            tools::Long nWhitespace = rPageSize.getHeight() - getFrameArea().Height();
+            // Count what would be the max size for the body frame, ignoring top/bottom margin.
+            const SvxULSpaceItem& rULSpace = pPageFormat->GetULSpace();
+            SwTwips nNominalHeight = rPageSize.getHeight() - rULSpace.GetUpper() - rULSpace.GetLower();
+            tools::Long nWhitespace = nNominalHeight - getFrameArea().Height();
             if (nWhitespace > -nDiff)
             {
                 // It does: don't move it and invalidate our page frame so

sw/qa/core/layout/calcmove.cxx
@@ -11,6 +11,9 @@
 
 #include <test/xmldocptr.hxx>
 
+#include <docsh.hxx>
+#include <wrtsh.hxx>
+
 namespace
 {
 /// Covers sw/source/core/layout/calcmove.cxx fixes.
@@ -81,6 +84,34 @@ CPPUNIT_TEST_FIXTURE(Test, testIgnoreTopMarginFly)
     // i.e. the top margin was ignored inside shape text for Word compat, while multi-col shape text
     // is a Writer feature.
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(4000), nParaTopMargin);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testHideWhitespaceGrowingLastPage)
+{
+    // Given a document with a full first page, then hiding whitespace:
+    createSwDoc();
+    SwDocShell* pDocShell = getSwDocShell();
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+    while (getPages() == 1)
+    {
+        pWrtShell->SplitNode();
+    }
+    pWrtShell->DelLeft();
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+    SwViewOption aViewOptions(*pWrtShell->GetViewOptions());
+    aViewOptions.SetHideWhitespaceMode(true);
+    pWrtShell->ApplyViewOptions(aViewOptions);
+
+    // When adding a new paragraph at doc end:
+    pWrtShell->SplitNode();
+
+    // Then make sure a new page is created:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 2
+    // - Actual  : 1
+    // i.e. the page was growing instead of creating a new page when it already had a max size.
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
 }
 }
 