570261954089695377cd6910b95fd3b6ee864062
Pranam Lashkari <lpranam@collabora.com>
2024-10-10 07:15:16 +0400

    sc: make sure autofilter color area is caluculated
    problem:
    in autofilter popup entries were made before color area was correctly extended
    
    Change-Id: I38f27666109e99e22e16804c0fdf8df9840864f6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174755
    Reviewed-by: Henry Castro <hcastro@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit 8903ddff872d579b06bd3acc413d3d14f4284228)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174781
    Tested-by: Jenkins
    Reviewed-by: Pranam Lashkari <lpranam@collabora.com>

--- sc/source/core/data/documen3.cxx
+++ sc/source/core/data/documen3.cxx
@@ -1569,6 +1569,7 @@ void ScDocument::GetFilterEntries(
     if (!pDBData)
         return;
 
+    pDBData->ExtendBackColorArea(*this);
     pDBData->ExtendDataArea(*this);
     SCTAB nAreaTab;
     SCCOL nStartCol;
--- sc/source/ui/view/gridwin.cxx
+++ sc/source/ui/view/gridwin.cxx
@@ -1015,7 +1015,6 @@ void ScGridWindow::LaunchAutoFilterMenu(SCCOL nCol, SCROW nRow)
     if (!pDBData)
         return;
 
-    pDBData->ExtendBackColorArea(rDoc);
     pData->mpData = pDBData;
     mpAutoFilterPopup->setExtendedData(std::move(pData));
 
