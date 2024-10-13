cf908e81491b62d6eba7161313a88b9b766ff87d
Thorsten Behrens <thorsten.behrens@allotropia.de>
2024-10-13 14:08:39 +0200

    vcl: add font effects to mtf dump
    Change-Id: Ieb9a5b2e09e5d1d3c7c481b4eeb42e15fa2996d6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174871
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- vcl/source/gdi/mtfxmldump.cxx
+++ vcl/source/gdi/mtfxmldump.cxx
@@ -1333,6 +1333,10 @@ void MetafileXmlDump::writeXml(const GDIMetaFile& rMetaFile, tools::XmlWriter& r
                 rWriter.attribute("orientation", aFont.GetOrientation().get());
                 rWriter.attribute("weight", convertFontWeightToString(aFont.GetWeight()));
                 rWriter.attribute("vertical", aFont.IsVertical() ? "true" : "false");
+                rWriter.attribute("emphasis", aFont.GetEmphasisMark() != FontEmphasisMark::NONE ? "true" : "false");
+                rWriter.attribute("shadow", aFont.IsShadow() ? "true" : "false");
+                rWriter.attribute("wordunderline", aFont.IsWordLineMode() ? "true" : "false");
+                rWriter.attribute("outline", aFont.IsOutline() ? "true" : "false");
 
                 rWriter.endElement();
             }
