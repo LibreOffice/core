LibreOffice/core

commit cccbf8608f3b76651993bc8576c672367c228fa2

feat(json): Allow serializing any
Previously, as far as I can tell, there was no way to send a Size in a
.uno command (as would be needed in, say, '.uno:InsertAVMedia').

We do, however, already have a way to send an `[]any`, and we can use a
similar method to allow sending arbitrary types with `any` rather than
`[]any`.

To use this, the type you really want to send has to be wrapped in any,
so for example...

{
  type: "any",
  value: {
    type: "com.sun.star.awt.Size",
    value: {
      Width: {
        type: "long",
	value: 0,
      },
      Height: {
        type: "long",
	value: 0,
      }
    }
  }
}

Change-Id: Ic33594dee955d475d81733adb21950c0458435e8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/174740
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

comphelper/source/misc/sequenceashashmap.cxx
@@ -356,6 +356,14 @@ std::vector<css::beans::PropertyValue> JsonToPropertyValues(const OString& rJson
                 aValue.Value <<= aSeqByte;
             }
         }
+        else if (rType == "any")
+        {
+            aNodeValue = rPair.second.get_child("value", aNodeNull);
+            if (aNodeValue != aNodeNull && !aNodeValue.empty())
+            {
+                aValue.Value = jsonToUnoAny(aNodeValue);
+            }
+        }
         else if (rType == "[]any")
         {
             aNodeValue = rPair.second.get_child("value", aNodeNull);

commit 94c0664d54d9979dac61cf6e78e0110282f8703e

comphelper: test uno::Any parsing from JSON
Fails with commit cccbf8608f3b76651993bc8576c672367c228fa2 (feat(json):
Allow serializing any, 2024-10-16) reverted.

Change-Id: Ifd59f9921f89cc4f52a21a597f433684c715f186
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175128
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

comphelper/qa/unit/propertyvalue.cxx
@@ -13,6 +13,8 @@
 #include <cppunit/TestFixture.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#include <com/sun/star/awt/Size.hpp>
+
 #include <comphelper/propertyvalue.hxx>
 #include <comphelper/propertysequence.hxx>
 #include <cppu/unotype.hxx>
@@ -29,6 +31,7 @@ class MakePropertyValueTest : public CppUnit::TestFixture
     CPPUNIT_TEST(testRvalue);
     CPPUNIT_TEST(testBitField);
     CPPUNIT_TEST(testJson);
+    CPPUNIT_TEST(testJsonAwtSize);
     CPPUNIT_TEST_SUITE_END();
 
     void testLvalue()
@@ -122,6 +125,44 @@ class MakePropertyValueTest : public CppUnit::TestFixture
         CPPUNIT_ASSERT_EQUAL(OUString("FieldCommand"), aFirstSeq[1].Name);
         CPPUNIT_ASSERT_EQUAL(OUString("ADDIN ZOTERO_ITEM new command 1"),
                              aFirstSeq[1].Value.get<OUString>());
+    }
+
+    void testJsonAwtSize()
+    {
+        // Given a list of beans::PropertyValues in JSON:
+        OString aJson = R"json(
+{
+    "mykey": {
+        "type": "any",
+        "value": {
+            "type": "com.sun.star.awt.Size",
+            "value": {
+                "Width": {
+                    "type": "long",
+                    "value": 42
+                },
+                "Height": {
+                    "type": "long",
+                    "value": 43
+                }
+            }
+        }
+    }
+}
+)json"_ostr;
+
+        // When parsing that:
+        std::vector<beans::PropertyValue> aRet = comphelper::JsonToPropertyValues(aJson);
+
+        // Then make sure we can construct an awt::Size:
+        CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), aRet.size());
+        beans::PropertyValue aFirst = aRet[0];
+        CPPUNIT_ASSERT_EQUAL(OUString("mykey"), aFirst.Name);
+        // Without the accompanying fix in place, this test would have failed with:
+        // - Cannot extract an Any(void) to com.sun.star.awt.Size
+        auto aSize = aFirst.Value.get<awt::Size>();
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(42), aSize.Width);
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(43), aSize.Height);
     }
 };
 