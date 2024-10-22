96a39e8917ff5f6d89450d6666a6a6222c7eb62b
László Németh <nemeth@numbertext.org>
2024-10-22 16:59:24 +0200

    tdf#163575 sw smart justify: fix size resolution for SwBidiPortion
    Negative space sizes (i.e. shrunk lines at image wrapping) stored
    over LONG_MAX/2, and these values had no resolution in SwBidiPortion,
    causing crash/assert in conversion of DOCX document containing e.g.
    Arabic text wrapping around images.
    
    Note: apply the resolution in SwDoubleLinePortion, too.
    
    Regression since commit 1fb6de02709a5f420f21ebd683915da50ce0d198
    "tdf#163149 sw smart justify: fix line shrinking at image wrapping".
    
    Change-Id: I6e45592c4eed247871d35e1f02fd5a038baddf85
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175419
    Reviewed-by: László Németh <nemeth@numbertext.org>
    Tested-by: Jenkins
    (cherry picked from commit 270c96e12c4a14c4f9e130d15310843da3a6af68)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175432
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/qa/extras/odfexport/odfexport2.cxx
+++ sw/qa/extras/odfexport/odfexport2.cxx
@@ -547,6 +547,12 @@ CPPUNIT_TEST_FIXTURE(Test, tdf99631)
     assertXPathContent(pXmlDoc2, "//config:config-item[@config:name='VisibleAreaHeight']"_ostr, u"1355"_ustr);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, tdf163575)
+{
+    // crashes/assert at export time
+    loadAndReload("tdf163575.docx");
+}
+
 CPPUNIT_TEST_FIXTURE(Test, tdf145871)
 {
     loadAndReload("tdf145871.odt");
