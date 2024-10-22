LibreOffice/core

commit c7b7153213619859d5f049b7c60eafa4c81f5225

fastparser in SdXMLLayerContext
Change-Id: I1912d8d5a5f3ea6865d7425be8edb4f24b27e61f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/106229
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

xmloff/source/draw/layerimp.cxx
@@ -55,7 +55,9 @@ class SdXMLLayerContext : public SvXMLImportContext
 public:
     SdXMLLayerContext( SvXMLImport& rImport, const Reference< XFastAttributeList >& xAttrList, const Reference< XNameAccess >& xLayerManager );
 
-    virtual SvXMLImportContextRef CreateChildContext( sal_uInt16 nPrefix, const OUString& rLocalName, const Reference< XAttributeList >& xAttrList ) override;
+    virtual css::uno::Reference< css::xml::sax::XFastContextHandler > SAL_CALL createFastChildContext(
+        sal_Int32 nElement,
+        const css::uno::Reference< css::xml::sax::XFastAttributeList >& AttrList ) override;
     virtual void SAL_CALL endFastElement(sal_Int32 nElement) override;
 
 private:
@@ -94,16 +96,20 @@ SdXMLLayerContext::SdXMLLayerContext( SvXMLImport& rImport, const Reference< XFa
 
 }
 
-SvXMLImportContextRef SdXMLLayerContext::CreateChildContext( sal_uInt16 nPrefix, const OUString& rLocalName, const Reference< XAttributeList >& )
+css::uno::Reference< css::xml::sax::XFastContextHandler > SdXMLLayerContext::createFastChildContext(
+    sal_Int32 nElement,
+    const css::uno::Reference< css::xml::sax::XFastAttributeList >&  )
 {
-    if( (XML_NAMESPACE_SVG == nPrefix) && IsXMLToken(rLocalName, XML_TITLE) )
+    if( nElement == XML_ELEMENT(SVG, XML_TITLE) )
     {
-        return new XMLStringBufferImportContext( GetImport(), nPrefix, rLocalName, sTitleBuffer);
+        return new XMLStringBufferImportContext( GetImport(), sTitleBuffer);
     }
-    else if( (XML_NAMESPACE_SVG == nPrefix) && IsXMLToken(rLocalName, XML_DESC) )
+    else if( nElement == XML_ELEMENT(SVG, XML_DESC) )
     {
-        return new XMLStringBufferImportContext( GetImport(), nPrefix, rLocalName, sDescriptionBuffer);
+        return new XMLStringBufferImportContext( GetImport(), sDescriptionBuffer);
     }
+    else
+        XMLOFF_WARN_UNKNOWN_ELEMENT("xmloff", nElement);
     return nullptr;
 }
 

commit 75d6fd09138283b4af949d8071ecfee5e2545180

tdf#163516 Draw fails to restore Layer data Title and Description
regression from
    commit c7b7153213619859d5f049b7c60eafa4c81f5225
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Fri Nov 20 14:24:51 2020 +0200
    fastparser in SdXMLLayerContext

Change-Id: I0db381fbd383efd23bb254703bea103ca0d270d7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175378
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

xmloff/source/draw/layerimp.cxx
@@ -94,11 +94,11 @@ css::uno::Reference< css::xml::sax::XFastContextHandler > SdXMLLayerContext::cre
     sal_Int32 nElement,
     const css::uno::Reference< css::xml::sax::XFastAttributeList >&  )
 {
-    if( nElement == XML_ELEMENT(SVG, XML_TITLE) )
+    if( nElement == XML_ELEMENT(SVG, XML_TITLE) || nElement == XML_ELEMENT(SVG_COMPAT, XML_TITLE) )
     {
         return new XMLStringBufferImportContext( GetImport(), sTitleBuffer);
     }
-    else if( nElement == XML_ELEMENT(SVG, XML_DESC) )
+    else if( nElement == XML_ELEMENT(SVG, XML_DESC) || nElement == XML_ELEMENT(SVG_COMPAT, XML_DESC) )
     {
         return new XMLStringBufferImportContext( GetImport(), sDescriptionBuffer);
     }