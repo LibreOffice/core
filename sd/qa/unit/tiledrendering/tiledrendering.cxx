3227919ea158378a6a4435410ff6c30b1be6e459
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2024-10-23 11:30:19 +0200

    slideshow: simplify rendering, render text fields each in own layer
    This change significantly simplifies the rendering of layers by
    introducing an analysis pass (when we render the background) that
    creates rendering pass objects, which contain which objects and
    paragraphs need to be rendered in each pass. Previously we tried
    to figure this out durign the rendering, which meant we had to
    track a lot of various states. This all is not longer needed. This
    chaneg also means that the new RenderPassObjectRedirector is fairly
    simple.
    
    In addition this changes the test and implementation to render
    each text field in its own layer as it is expected by the online
    implementation (to enable master pages caching), but it still
    doesn't first render the placeholders for text fields as MasterPage
    layer and the TextField layer as a separate bitmap layer. This
    will be done next.
    
    Change-Id: I5377f4c5e5ebff67da70b248c831837cb9f2e559
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175568
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sd/qa/unit/tiledrendering/tiledrendering.cxx
+++ sd/qa/unit/tiledrendering/tiledrendering.cxx
@@ -3336,6 +3336,7 @@ CPPUNIT_TEST_FIXTURE(SdTiledRenderingTest, testSlideshowLayeredRendering_WithFie
         bool bIsBitmapLayer = false;
         OUString rJsonMsg;
         CPPUNIT_ASSERT(!pXImpressDocument->renderNextSlideLayer(pBuffer.data(), bIsBitmapLayer, rJsonMsg));
+        debugWriteImageToFile(0, pBuffer, nViewWidth, nViewHeight, rJsonMsg.toUtf8().getStr());
     }
 
     {
@@ -3374,6 +3375,30 @@ CPPUNIT_TEST_FIXTURE(SdTiledRenderingTest, testSlideshowLayeredRendering_WithFie
         CPPUNIT_ASSERT_EQUAL(aTransparentColor, aBitmapEx.GetPixelColor(20, 20));
 
         // bottom-left corner
+        CPPUNIT_ASSERT_EQUAL(aTransparentColor, aBitmapEx.GetPixelColor(20, nViewHeight - 20));
+
+        // bottom-right corner
+        CPPUNIT_ASSERT_EQUAL(aTransparentColor, aBitmapEx.GetPixelColor(nViewWidth - 20, nViewHeight - 20));
+
+        // bottom-center
+        CPPUNIT_ASSERT_EQUAL(Color(0x20, 0xaa, 0x00), aBitmapEx.GetPixelColor(nViewWidth / 2, nViewHeight - 20));
+    }
+
+    {
+        std::vector<sal_uInt8> pBuffer(nViewWidth * nViewHeight * 4);
+        bool bIsBitmapLayer = false;
+        OUString rJsonMsg;
+        CPPUNIT_ASSERT(!pXImpressDocument->renderNextSlideLayer(pBuffer.data(), bIsBitmapLayer, rJsonMsg));
+        CPPUNIT_ASSERT(bIsBitmapLayer);
+        // TODO - check JSON content
+        debugWriteImageToFile(3, pBuffer, nViewWidth, nViewHeight, rJsonMsg.toUtf8().getStr());
+
+        BitmapEx aBitmapEx = vcl::bitmap::CreateFromData(pBuffer.data(), nViewWidth, nViewHeight, nViewWidth * 4, /*nBitsPerPixel*/32, true, true);
+
+        // top-left corner
+        CPPUNIT_ASSERT_EQUAL(aTransparentColor, aBitmapEx.GetPixelColor(20, 20));
+
+        // bottom-left corner
         CPPUNIT_ASSERT_EQUAL(Color(0x90, 0x80, 0xff), aBitmapEx.GetPixelColor(20, nViewHeight - 20));
 
         // bottom-right corner
@@ -3387,7 +3412,7 @@ CPPUNIT_TEST_FIXTURE(SdTiledRenderingTest, testSlideshowLayeredRendering_WithFie
         CPPUNIT_ASSERT(!pXImpressDocument->renderNextSlideLayer(pBuffer.data(), bIsBitmapLayer, rJsonMsg));
         CPPUNIT_ASSERT(bIsBitmapLayer);
         // TODO - check JSON content
-        debugWriteImageToFile(3, pBuffer, nViewWidth, nViewHeight, rJsonMsg.toUtf8().getStr());
+        debugWriteImageToFile(4, pBuffer, nViewWidth, nViewHeight, rJsonMsg.toUtf8().getStr());
 
         BitmapEx aBitmapEx = vcl::bitmap::CreateFromData(pBuffer.data(), nViewWidth, nViewHeight, nViewWidth * 4, /*nBitsPerPixel*/32, true, true);
 
