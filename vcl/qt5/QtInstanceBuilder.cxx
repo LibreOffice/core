a0042f104cc90b5fa049020e4622e4019d186eb3
Michael Weghorn <m.weghorn@posteo.de>
2024-10-25 13:43:09 +0200

    tdf#130857 qt weld: Declare support for Math "Alignment" dlg
    Add .ui file of Math's "Format" -> "Alignment" dialog
    to list of files supported by QtInstanceBuilder, so that native
    Qt widgets are used for that dialog now when using the qt5/qt6 VCL
    plugins, unless environment variable SAL_VCL_QT_NO_WELDED_WIDGETS=1
    is set.
    
    Change-Id: Ic0b58b2f77be136eeea9ac8a29501cd30b24dce4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175640
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -42,6 +42,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile)
         u"cui/ui/optnewdictionarydialog.ui"_ustr,
         u"modules/scalc/ui/inputstringdialog.ui"_ustr,
         u"modules/schart/ui/insertaxisdlg.ui"_ustr,
+        u"modules/smath/ui/alignmentdialog.ui"_ustr,
         u"modules/swriter/ui/inforeadonlydialog.ui"_ustr,
         u"modules/swriter/ui/renameobjectdialog.ui"_ustr,
         u"modules/swriter/ui/wordcount.ui"_ustr,
