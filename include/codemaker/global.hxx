7e705f6c80233943603cf0953a0188b0acc75d50
Lorenzo Lovato <lorenzo04lovato@gmail.com>
2024-10-26 11:26:39 +0200

    tdf#143148: #pragma once instead of include guards
    Change-Id: I573a36a488e5a76f5dbe9d629ae0aaf23d3fc711
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175679
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- include/codemaker/global.hxx
+++ include/codemaker/global.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_CODEMAKER_GLOBAL_HXX
-#define INCLUDED_CODEMAKER_GLOBAL_HXX
+#pragma once
 
 #include <utility>
 #include <vector>
@@ -90,7 +89,5 @@ public:
 private:
     OUString message_;
 };
-
-#endif // INCLUDED_CODEMAKER_GLOBAL_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
