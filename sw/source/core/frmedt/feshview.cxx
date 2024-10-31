LibreOffice/core

commit 3e0650fb166cac116a43f6ce5d03bd69830e37b8

tdf#157533 sw: fix cursor position deselecting formula in table
Fix lost cursor by deselecting formula object anchored as
character as single content of table cells. In this case, the
cursor was positioned inside the frame of the formula,
disappeared in a flash.

Fix also bad cursor position by deselecting formula object
anchored as character at beginning of text lines in table
content.

Follow-up to commit 014e5f559a9acf319af24c721dbe6b0bc3bc5882
"tdf#161360 sw: fix cursor position deselecting image in table".

Change-Id: Ia1a96359f473071c4cead580322aa9c1af1d3f24
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168416
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

sw/source/core/frmedt/feshview.cxx
@@ -212,15 +212,29 @@ bool SwFEShell::SelectObj( const Point& rPt, sal_uInt8 nFlag, SdrObject *pObj )
                     if ( SW_LEAVE_FRAME & nFlag )
                     {
                         const SwContact* pContact = GetUserCall(pOldObj);
-                        if ( pContact && pContact->ObjAnchoredAsChar() )
+                        if ( pContact && pContact->ObjAnchoredAsChar() &&
+                                pOldSelFly->GetAnchorFrame() &&
+                                pOldSelFly->GetAnchorFrame()->GetUpper() )
                         {
                             const SwNode * pOldNd = pContact->GetAnchorNode().FindTableNode();
                             // the original image was in a table, but the cursor is not in that
                             if ( pOldNd && pOldNd != GetCursor()->GetPointNode().FindTableNode() )
                             {
+                                const SwRect& rCellFrame =
+                                    pOldSelFly->GetAnchorFrame()->GetUpper()->getFrameArea();
+                                Point aPtCellTopRight( rCellFrame.Pos() );
+                                aPtCellTopRight.setX( aPtCellTopRight.X() + rCellFrame.Width() );
                                 if ( SwWrtShell* pWrtShell = dynamic_cast<SwWrtShell*>(this) )
-                                    // put the text cursor in the same row
-                                    pWrtShell->SelectTableRowCol( aPt );
+                                    // put the text cursor in the same cell
+                                    pWrtShell->SelectTableRowCol( aPtCellTopRight );
+                            }
+                            // same table, but not in the same cell
+                            else if ( pContact->GetAnchorNode().GetTableBox() !=
+                                            GetCursor()->GetPointNode().GetTextNode()->GetTableBox() )
+                            {
+                                aPt.setX( aPt.getX() + 2 + pOldSelFly->getFrameArea().Width() );
+                                // put the text cursor after the object
+                                SetCursor( aPt, true );
                             }
                         }
                     }

sw/qa/extras/uiwriter/uiwriter6.cxx
@@ -41,6 +41,7 @@
 
 #include <com/sun/star/text/XTextTable.hpp>
 #include <com/sun/star/text/XTextViewCursorSupplier.hpp>
+#include <com/sun/star/view/XSelectionSupplier.hpp>
 #include <o3tl/cppunittraitshelper.hxx>
 #include <swdtflvr.hxx>
 #include <comphelper/propertysequence.hxx>
@@ -1743,6 +1744,61 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf161360)
     eType2 = pWrtShell->GetSelectionType();
     // The text cursor is after the floating table
     CPPUNIT_ASSERT_EQUAL(SelectionType::Text, eType2);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf157533)
+{
+    // load a table with objects positioned at beginning of text lines
+    createSwDoc("tdf157533.fodt");
+    SwDoc* pDoc = getSwDoc();
+    CPPUNIT_ASSERT(pDoc);
+    SwWrtShell* pWrtShell = pDoc->GetDocShell()->GetWrtShell();
+    CPPUNIT_ASSERT(pWrtShell);
+    auto pShell = pDoc->GetDocShell()->GetFEShell();
+    CPPUNIT_ASSERT(pShell);
+
+    auto xModel = mxComponent.queryThrow<frame::XModel>();
+    uno::Reference<drawing::XShape> xShape(getShapeByName(u"Objet2"));
+    uno::Reference<view::XSelectionSupplier> xCtrl(xModel->getCurrentController(), uno::UNO_QUERY);
+    xCtrl->select(uno::Any(xShape));
+
+    dispatchCommand(mxComponent, ".uno:Escape", {});
+
+    // Then make sure that the cursor in the table:
+    SelectionType eType2 = pWrtShell->GetSelectionType();
+    // This was false (only SelectionType::Text)
+    bool bCursorInTable = eType2 == (SelectionType::Text | SelectionType::Table);
+    CPPUNIT_ASSERT(bCursorInTable);
+
+    SwTextNode* pTextNode = pWrtShell->GetCursor()->GetPointNode().GetTextNode();
+    // This was false (not in the same paragraph and cell)
+    CPPUNIT_ASSERT(pTextNode->GetText().indexOf("and the second formula") > -1);
+
+    uno::Reference<drawing::XShape> xShape2(getShapeByName(u"Objet11"));
+    xCtrl->select(uno::Any(xShape2));
+
+    dispatchCommand(mxComponent, ".uno:Escape", {});
+
+    SwTextNode* pTextNode2 = pWrtShell->GetCursor()->GetPointNode().GetTextNode();
+    // This was false (lost text cursor inside the frame of the formula)
+    CPPUNIT_ASSERT(pTextNode2->GetTableBox());
+    SwTableNode* pTableNode = pWrtShell->GetCursor()->GetPointNode().FindTableNode();
+    SwTable& rTable = pTableNode->GetTable();
+    // cursor in the same cell
+    bool bSameBox = pTextNode2->GetTableBox() == rTable.GetTableBox("A1");
+    CPPUNIT_ASSERT(bSameBox);
+
+    uno::Reference<drawing::XShape> xShape3(getShapeByName(u"Objet10"));
+    xCtrl->select(uno::Any(xShape3));
+
+    dispatchCommand(mxComponent, ".uno:Escape", {});
+
+    SwTextNode* pTextNode3 = pWrtShell->GetCursor()->GetPointNode().GetTextNode();
+    // This was false (lost text cursor inside the frame of the formula)
+    CPPUNIT_ASSERT(pTextNode3->GetTableBox());
+    // cursor in the same cell
+    bSameBox = pTextNode3->GetTableBox() == rTable.GetTableBox("B1");
+    CPPUNIT_ASSERT(bSameBox);
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf115132)

commit bb4da18c55dff7e6d1146fa70a83c84dea68807f

tdf#163699 sw: fix reported crash using the Navigator
"On a specific file, when double-clicking multiple times on
different images in the navigator panel, LO crashes."
reported by Vladimir Tarakanov with a code pointer.

Regression since 3e0650fb166cac116a43f6ce5d03bd69830e37b8
"tdf#157533 sw: fix cursor position deselecting formula in table".

Change-Id: I550d61d8ee10205ff2357c2511ae0062077d651e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175865
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/frmedt/feshview.cxx
@@ -217,8 +217,9 @@ bool SwFEShell::SelectObj( const Point& rPt, sal_uInt8 nFlag, SdrObject *pObj )
                                 pOldSelFly->GetAnchorFrame()->GetUpper() )
                         {
                             const SwNode * pOldNd = pContact->GetAnchorNode().FindTableNode();
+                            const SwNode * pNewNd = GetCursor()->GetPointNode().FindTableNode();
                             // the original image was in a table, but the cursor is not in that
-                            if ( pOldNd && pOldNd != GetCursor()->GetPointNode().FindTableNode() )
+                            if ( pOldNd && pOldNd != pNewNd )
                             {
                                 const SwRect& rCellFrame =
                                     pOldSelFly->GetAnchorFrame()->GetUpper()->getFrameArea();
@@ -229,8 +230,10 @@ bool SwFEShell::SelectObj( const Point& rPt, sal_uInt8 nFlag, SdrObject *pObj )
                                     pWrtShell->SelectTableRowCol( aPtCellTopRight );
                             }
                             // same table, but not in the same cell
-                            else if ( pContact->GetAnchorNode().GetTableBox() !=
-                                            GetCursor()->GetPointNode().GetTextNode()->GetTableBox() )
+                            else if ( pOldNd && pOldNd == pNewNd &&
+                                    GetCursor()->GetPointNode().GetTextNode() &&
+                                    pContact->GetAnchorNode().GetTableBox() !=
+                                    GetCursor()->GetPointNode().GetTextNode()->GetTableBox() )
                             {
                                 aPt.setX( aPt.getX() + 2 + pOldSelFly->getFrameArea().Width() );
                                 // put the text cursor after the object