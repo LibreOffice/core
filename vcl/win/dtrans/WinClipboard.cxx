LibreOffice/core

commit 2e0664015255ffc0f76a11a9cb254564b34de496

tdf#148647: make sure to update own content on Win clipboard change
Before, only a new call to setContents, or destruction of the data
object referenced from CWinClipboard, would force CWinClipboard to
clean its m_pCurrentClipContent. A data object could be referenced
elsewhere, and so not get destroyed on clipboard change; this kept
the value of m_pCurrentClipContent, and all the following calls to
getContents would still return that stuck own data object.

This change makes CWinClipboard  store new own data object pointer
into temporary m_pNewOwnClipContent.  In onClipboardContentChanged
notification, the value is moved to m_pCurrentOwnClipContent. This
ensures that following onClipboardContentChanged events will clear
m_pCurrentOwnClipContent, preventing stuck clipboard content.

Change-Id: Idd0a64a820d79bede9deceba8d5800b7fc61e66b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170459
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

vcl/win/dtrans/WinClipboard.cxx
@@ -60,7 +60,6 @@ CWinClipboard::CWinClipboard(const uno::Reference<uno::XComponentContext>& rxCon
                              const OUString& aClipboardName)
     : m_xContext(rxContext)
     , m_itsName(aClipboardName)
-    , m_pCurrentClipContent(nullptr)
 {
     // necessary to reassociate from
     // the static callback function
@@ -92,6 +91,12 @@ void CWinClipboard::disposing(std::unique_lock<std::mutex>& mutex)
 
 // XClipboard
 
+CXNotifyingDataObject* CWinClipboard::getOwnClipContent() const
+{
+    assert(!m_pCurrentOwnClipContent || !m_pNewOwnClipContent); // Both can be null, or only one set
+    return m_pCurrentOwnClipContent ? m_pCurrentOwnClipContent : m_pNewOwnClipContent;
+}
+
 // to avoid unnecessary traffic we check first if there is a clipboard
 // content which was set via setContent, in this case we don't need
 // to query the content from the clipboard, create a new wrapper object
@@ -110,12 +115,12 @@ css::uno::Reference<css::datatransfer::XTransferable> CWinClipboard::getContents
         throw lang::DisposedException("object is already disposed",
                                       static_cast<XClipboardEx*>(this));
 
-    assert(!m_pCurrentClipContent || !m_foreignContent); // Both can be null, or only one set
+    assert(!getOwnClipContent() || !m_foreignContent); // Both can be null, or only one set
 
     // use the shortcut or create a transferable from
     // system clipboard
-    if (nullptr != m_pCurrentClipContent)
-        return m_pCurrentClipContent->m_XTransferable;
+    if (auto pOwnClipContent = getOwnClipContent())
+        return pOwnClipContent->m_XTransferable;
 
     // Content cached?
     if (m_foreignContent.is())
@@ -175,18 +180,21 @@ void SAL_CALL CWinClipboard::setContents(
     IDataObjectPtr pIDataObj;
 
     m_foreignContent.clear();
+    m_pCurrentOwnClipContent = nullptr;
 
     if (xTransferable.is())
     {
-        m_pCurrentClipContent = new CXNotifyingDataObject(
+        // Store the new object's pointer to temporary m_pNewOwnClipContent, to be moved to
+        // m_pCurrentOwnClipContent in handleClipboardContentChanged.
+        m_pNewOwnClipContent = new CXNotifyingDataObject(
             CDTransObjFactory::createDataObjFromTransferable(m_xContext, xTransferable),
             xTransferable, xClipboardOwner, this);
 
-        pIDataObj = IDataObjectPtr(m_pCurrentClipContent);
+        pIDataObj = IDataObjectPtr(m_pNewOwnClipContent);
     }
     else
     {
-        m_pCurrentClipContent = nullptr;
+        m_pNewOwnClipContent = nullptr;
     }
 
     m_MtaOleClipboard.setClipboard(pIDataObj.get());
@@ -219,7 +227,7 @@ void SAL_CALL CWinClipboard::flushClipboard()
     // It may be possible to move the request to the clipboard STA thread by saving the
     // DataObject and call OleIsCurrentClipboard before flushing.
 
-    if (nullptr != m_pCurrentClipContent)
+    if (getOwnClipContent())
     {
         aGuard.unlock();
         m_MtaOleClipboard.flushClipboard();
@@ -272,13 +280,16 @@ void SAL_CALL CWinClipboard::removeClipboardListener(
     maClipboardListeners.removeInterface(aGuard, listener);
 }
 
-void CWinClipboard::clearCacheAndAllClipboardListener()
+void CWinClipboard::handleClipboardContentChanged()
 {
     std::unique_lock aGuard(m_aMutex);
     if (m_bDisposed)
         return;
 
     m_foreignContent.clear();
+    // If new own content assignment is pending, do it; otherwise, clear it.
+    // This makes sure that there will be no stuck clipboard content.
+    m_pCurrentOwnClipContent = std::exchange(m_pNewOwnClipContent, nullptr);
 
     if (!maClipboardListeners.getLength(aGuard))
         return;
@@ -351,8 +362,8 @@ void CWinClipboard::onReleaseDataObject(CXNotifyingDataObject* theCaller)
     // because an external source must be the clipboardowner now
     std::unique_lock aGuard(m_aMutex);
 
-    if (m_pCurrentClipContent == theCaller)
-        m_pCurrentClipContent = nullptr;
+    if (getOwnClipContent() == theCaller)
+        m_pCurrentOwnClipContent = m_pNewOwnClipContent = nullptr;
 }
 
 void CWinClipboard::registerClipboardViewer()
@@ -372,7 +383,7 @@ void WINAPI CWinClipboard::onClipboardContentChanged()
     }
 
     if (pWinClipboard)
-        pWinClipboard->clearCacheAndAllClipboardListener();
+        pWinClipboard->handleClipboardContentChanged();
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

vcl/win/dtrans/WinClipboard.hxx
@@ -57,12 +57,15 @@ class CWinClipboard final
     css::uno::Reference<css::uno::XComponentContext> m_xContext;
     const OUString m_itsName;
     CMtaOleClipboard m_MtaOleClipboard;
-    CXNotifyingDataObject* m_pCurrentClipContent;
+    CXNotifyingDataObject* m_pNewOwnClipContent = nullptr; // until onClipboardContentChanged
+    CXNotifyingDataObject* m_pCurrentOwnClipContent = nullptr;
     com::sun::star::uno::Reference<com::sun::star::datatransfer::XTransferable> m_foreignContent;
     comphelper::OInterfaceContainerHelper4<css::datatransfer::clipboard::XClipboardListener>
         maClipboardListeners;
 
-    void clearCacheAndAllClipboardListener();
+    CXNotifyingDataObject* getOwnClipContent() const;
+
+    void handleClipboardContentChanged();
     void onReleaseDataObject(CXNotifyingDataObject* theCaller);
 
     void registerClipboardViewer();

commit 9f53d40fd19b22fe1bdbf64e8ce751cf53f4f517

Related: tdf#163730 Avoid deadlock
Seen locally, with main thread querying clipboard state:
  vclplug_winlo.dll!std::unique_lock<std::mutex>::unique_lock<std::mutex>(std::mutex & _Mtx) Line 145
  vclplug_winlo.dll!CWinClipboard::getContents() Line 109
  vcllo.dll!TransferableDataHelper::CreateFromClipboard(const com::sun::star::uno::Reference<com::sun::star::datatransfer::clipboard::XClipboard> & rClipboard) Line 2162
  vcllo.dll!TransferableDataHelper::CreateFromSystemClipboard(vcl::Window * pWindow) Line 2188
  swlo.dll!SwBaseShell::StateClpbrd(SfxItemSet & rSet) Line 602
  swlo.dll!SfxStubSwBaseShellStateClpbrd(SfxShell * pShell, SfxItemSet & rSet) Line 2220
  sfxlo.dll!SfxDispatcher::FillState_(const SfxSlotServer & rSvr, SfxItemSet & rState, const SfxSlot * pRealSlot) Line 1726
  sfxlo.dll!SfxBindings::Update_Impl(SfxStateCache & rCache) Line 267
  sfxlo.dll!SfxBindings::NextJob_Impl(const Timer * pTimer) Line 1280
  sfxlo.dll!SfxBindings::NextJob(Timer * pTimer) Line 1225
  sfxlo.dll!SfxBindings::LinkStubNextJob(void * instance, Timer * data) Line 1220
  vcllo.dll!Link<Timer *,void>::Call(Timer * data) Line 111
  vcllo.dll!Timer::Invoke() Line 75
  vcllo.dll!Scheduler::CallbackTaskScheduling() Line 509
  vcllo.dll!SalTimer::CallCallback() Line 53
  vclplug_winlo.dll!WinSalTimer::ImplHandleElapsedTimer() Line 169
  vclplug_winlo.dll!ImplSalYield(bool bWait, bool bHandleAllCurrentEvents) Line 525
  vclplug_winlo.dll!WinSalInstance::DoYield(bool bWait, bool bHandleAllCurrentEvents) Line 581
  vcllo.dll!ImplYield(bool i_bWait, bool i_bAllEvents) Line 385
  vcllo.dll!Application::Yield() Line 473
  vcCMtaOleClipboard::runllo.dll!Application::Execute() Line 361
  sofficeapp.dll!desktop::Desktop::Main() Line 1679

CMtaOleClipboard::clipboardChangeNotifier thread holding CWinClipboard's
mutex in handleClipboardContentChanged, and waiting for the destruction
of IDataObject released from m_foreignContent (which was redirected to
CMtaOleClipboard::run thread):

  vclplug_winlo.dll!sal::systools::COMReference<IDataObject>::release(IDataObject * ptr) Line 235
  vclplug_winlo.dll!sal::systools::COMReference<IDataObject>::~COMReference<IDataObject>() Line 163
  vclplug_winlo.dll!CAPNDataObject::~CAPNDataObject() Line 97
  vclplug_winlo.dll!CAPNDataObject::`scalar deleting destructor'(unsigned int)
  vclplug_winlo.dll!CAPNDataObject::Release() Line 137
  vclplug_winlo.dll!sal::systools::COMReference<IDataObject>::release(IDataObject * ptr) Line 235
  vclplug_winlo.dll!sal::systools::COMReference<IDataObject>::~COMReference<IDataObject>() Line 163
  vclplug_winlo.dll!CDOTransferable::~CDOTransferable()
  vclplug_winlo.dll!CDOTransferable::`scalar deleting destructor'(unsigned int)
  cppuhelper3MSC.dll!cppu::OWeakObject::release() Line 230
  vclplug_winlo.dll!cppu::WeakImplHelper<com::sun::star::datatransfer::XTransferable>::release() Line 115
  vclplug_winlo.dll!com::sun::star::uno::Reference<com::sun::star::datatransfer::XTransferable>::clear() Line 234
  vclplug_winlo.dll!CWinClipboard::handleClipboardContentChanged() Line 291
  vclplug_winlo.dll!CWinClipboard::onClipboardContentChanged() Line 385
  vclplug_winlo.dll!CMtaOleClipboard::clipboardChangedNotifierThreadProc(void * pParam) Line 721

and CMtaOleClipboard::run thread waiting for CWinClipboard's mutex
in CWinClipboard::onReleaseDataObject:

  vclplug_winlo.dll!std::_Mutex_base::lock() Line 52
  vclplug_winlo.dll!std::unique_lock<std::mutex>::unique_lock<std::mutex>(std::mutex & _Mtx) Line 145
  vclplug_winlo.dll!CWinClipboard::onReleaseDataObject(CXNotifyingDataObject & theCaller) Line 362
  vclplug_winlo.dll!CXNotifyingDataObject::Release() Line 75
  ole32.dll!CClipDataObject::Release() Line 960
  combase.dll!...
  rpcrt4.dll!Invoke()
  rpcrt4.dll!Ndr64StubWorker(void *,void *,struct _RPC_MESSAGE *,struct _MIDL_SERVER_INFO_ *,long (*const *)(void),struct _MIDL_SYNTAX_INFO *,unsigned long *)
  rpcrt4.dll!NdrStubCall3()
  combase.dll!...
  user32.dll!UserCallWinProcCheckWow(struct _ACTIVATION_CONTEXT *,__int64 (*)(struct tagWND *,unsigned int,unsigned __int64,__int64),struct HWND__ *,enum _WM_VALUE,unsigned __int64,__int64,void *,int)
  user32.dll!DispatchMessageWorker()
  vclplug_winlo.dll!CMtaOleClipboard::run() Line 655
  vclplug_winlo.dll!CMtaOleClipboard::oleThreadProc(void * pParam) Line 673

Caused by changes in commit 2e0664015255ffc0f76a11a9cb254564b34de496

vcl/win/dtrans/WinClipboard.cxx
@@ -284,33 +284,39 @@ void SAL_CALL CWinClipboard::removeClipboardListener(
 
 void CWinClipboard::handleClipboardContentChanged()
 {
-    std::unique_lock aGuard(m_aMutex);
-    if (m_bDisposed)
-        return;
-
-    m_foreignContent.clear();
-    // If new own content assignment is pending, do it; otherwise, clear it.
-    // This makes sure that there will be no stuck clipboard content.
-    m_pCurrentOwnClipContent = std::exchange(m_pNewOwnClipContent, nullptr);
-
-    if (!maClipboardListeners.getLength(aGuard))
-        return;
-
-    try
+    // The object must be destroyed only outside of the mutex lock, because it may call
+    // CWinClipboard::onReleaseDataObject in another thread of this process
+    css::uno::Reference<css::datatransfer::XTransferable> old_foreignContent;
     {
-        uno::Reference<datatransfer::XTransferable> rXTransf(getContents_noLock());
-        datatransfer::clipboard::ClipboardEvent aClipbEvent(static_cast<XClipboard*>(this),
-                                                            rXTransf);
-        maClipboardListeners.notifyEach(
-            aGuard, &datatransfer::clipboard::XClipboardListener::changedContents, aClipbEvent);
-    }
-    catch (const lang::DisposedException&)
-    {
-        OSL_FAIL("Service Manager disposed");
+        std::unique_lock aGuard(m_aMutex);
+        if (m_bDisposed)
+            return;
 
-        aGuard.unlock();
-        // no further clipboard changed notifications
-        unregisterClipboardViewer();
+        old_foreignContent = std::move(m_foreignContent); // clear m_foreignContent
+        assert(!m_foreignContent.is());
+        // If new own content assignment is pending, do it; otherwise, clear it.
+        // This makes sure that there will be no stuck clipboard content.
+        m_pCurrentOwnClipContent = std::exchange(m_pNewOwnClipContent, nullptr);
+
+        if (!maClipboardListeners.getLength(aGuard))
+            return;
+
+        try
+        {
+            uno::Reference<datatransfer::XTransferable> rXTransf(getContents_noLock());
+            datatransfer::clipboard::ClipboardEvent aClipbEvent(static_cast<XClipboard*>(this),
+                                                                rXTransf);
+            maClipboardListeners.notifyEach(
+                aGuard, &datatransfer::clipboard::XClipboardListener::changedContents, aClipbEvent);
+        }
+        catch (const lang::DisposedException&)
+        {
+            OSL_FAIL("Service Manager disposed");
+
+            aGuard.unlock();
+            // no further clipboard changed notifications
+            unregisterClipboardViewer();
+        }
     }
 }
 