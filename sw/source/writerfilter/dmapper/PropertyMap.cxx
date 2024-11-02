b8ea36c08437199ed53a4c88c3fb89ad497a6880
Justin Luth <justin.luth@collabora.com>
2024-11-02 19:22:13 -0400

    tdf#161349 writerfilter: only copy page background vml fill
    This fixes my 24.8 regression.
    
    I had this clause in earlier versions of the patchset,
    but it moved along with most of the other logic
    to the initialization of the default page style.
    
    However, I certainly need to limit copying the properties
    of the default page style to ONLY the fill properties here as well.
    
    Change-Id: I168f13ebeea3720eba53703210314f069fb87ae2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175963
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Jenkins
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/175965
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/writerfilter/dmapper/PropertyMap.cxx
+++ sw/source/writerfilter/dmapper/PropertyMap.cxx
@@ -1211,6 +1211,8 @@ void SectionPropertyMap::HandleMarginsHeaderFooter(DomainMapper_Impl& rDM_Impl)
                     rDM_Impl.GetPageStyles()->getByName(u"Standard"_ustr), uno::UNO_QUERY_THROW);
         for (const beans::Property& rProp : m_aPageStyle->getPropertySetInfo()->getProperties())
         {
+            if (!rProp.Name.startsWith("Fill")) // only copy XATTR_FILL properties
+                continue;
             try
             {
                 const uno::Any aFillValue = xDefaultPageStyle->getPropertyValue(rProp.Name);
