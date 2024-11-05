43a8f573ea596d75739323ed3878c0e9aecca604
Harris Popal <hapopal@csumb.edu>
2024-11-04 21:33:31 -0800

    tdf#143148 use pragma once instead of include guards
    Change-Id: I7b082c51a7092e18bdbf5a1fbab3fa4e5a97a28a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176039
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/inc/ddefld.hxx
+++ sw/inc/ddefld.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SW_INC_DDEFLD_HXX
-#define INCLUDED_SW_INC_DDEFLD_HXX
+#pragma once
 
 #include <sfx2/lnkbase.hxx>
 #include <svl/hint.hxx>
@@ -123,7 +122,5 @@ public:
     virtual OUString GetPar2() const override;
     virtual void SetPar2(const OUString& rStr) override;
 };
-
-#endif // INCLUDED_SW_INC_DDEFLD_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
