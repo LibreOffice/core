LibreOffice/core

commit 7b8cec8322b60673e686660caf875bc71117bbd0

tdf#162268 sw: ApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph
Commit 2b47fae7e3e23ee7c733708500cb0482ad7f8af1 introduced the
compatibility setting ApplyTextAttrToEmptyLineAtEndOfParagraph, but that
was probably a mistake.

What Word is doing there is not applying a text attribute but applying the
formatting of the paragraph marker; add a new compatibility setting
ApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph to do this.

Change SwAttrIter to apply the RES_PARATR_LIST_AUTOFMT formatting when
the position behind the last character is reached, and use it to set the
height of the last line in SwLineLayout::CalcLine() in case it is empty
or contains only spaces/tabs.

Frustratingly this requires another change to fdo74110.docx to get rid
of some odd font that's applied to the paragraph marker.

Also, change SwTextFrame::IsHiddenNow() to take into account paragraph
marker formatting; if all characters are hidden but the paragraph marker
isn't hidden, the paragraph is still displayed in Word.

Change-Id: Icccd3e822ad0301ccbe373b50431c3254f691d6e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173880
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/text/txtfrm.cxx
@@ -25,6 +25,7 @@
 #include <editeng/lspcitem.hxx>
 #include <editeng/lrspitem.hxx>
 #include <editeng/brushitem.hxx>
+#include <editeng/charhiddenitem.hxx>
 #include <editeng/pgrditem.hxx>
 #include <comphelper/configuration.hxx>
 #include <swmodule.hxx>
@@ -1539,6 +1540,22 @@ bool SwTextFrame::IsHiddenNow() const
     {
         bHiddenCharsHidePara = static_cast<SwTextNode const*>(SwFrame::GetDep())->HasHiddenCharAttribute( true );
         bHiddenParaField = static_cast<SwTextNode const*>(SwFrame::GetDep())->IsHiddenByParaField();
+    }
+    if (bHiddenCharsHidePara && GetDoc().getIDocumentSettingAccess().get(
+            DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH))
+    {
+        // apparently in Word it's always the last para marker that determines hidden?
+        // even in case when they are merged by delete redline (it's obvious when they are merged by hidden-attribute
+        SwTextNode const*const pNode{ m_pMergedPara
+            ? m_pMergedPara->pLastNode
+            : static_cast<SwTextNode const*>(SwFrame::GetDep()) };
+        SwFormatAutoFormat const& rListAutoFormat{pNode->GetAttr(RES_PARATR_LIST_AUTOFMT)};
+        std::shared_ptr<SfxItemSet> const pSet{rListAutoFormat.GetStyleHandle()};
+        SvxCharHiddenItem const*const pItem{pSet ? pSet->GetItemIfSet(RES_CHRATR_HIDDEN) : nullptr};
+        if (!pItem || !pItem->GetValue())
+        {
+            bHiddenCharsHidePara = false;
+        }
     }
     const SwViewShell* pVsh = getRootFrame()->GetCurrShell();
 

sw/inc/IDocumentSettingAccess.hxx
@@ -99,6 +99,7 @@ enum class DocumentSettingId
     // tdf#119908 new paragraph justification
     JUSTIFY_LINES_WITH_SHRINKING,
     APPLY_TEXT_ATTR_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH,
+    APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH,
     DO_NOT_MIRROR_RTL_DRAW_OBJS,
     // COMPATIBILITY FLAGS END
     BROWSE_MODE,

sw/qa/extras/layout/data/A020-min.rtf
@@ -1,0 +1,30 @@
+{\rtf1\adeflang1025\ansi\ansicpg1252\uc1\adeff1\deff0\stshfdbch0\stshfloch0\stshfhich0\stshfbi0\deflang3079\deflangfe3079\themelang3079\themelangfe0\themelangcs0
+{\fonttbl{\f0\fbidi \froman\fcharset0\fprq2{\*\panose 02020603050405020304}Times New Roman{\*\falt Times New Roman};}
+{\f1\fbidi \fswiss\fcharset0\fprq2{\*\panose 020b0604020202020204}Arial{\*\falt Arial};}
+}
+{\*\defchp \fs22 }
+{\*\defpap \ql \li0\ri0\sa160\sl259\slmult1\widctlpar\wrapdefault\aspalpha\aspnum\faauto\adjustright\rin0\lin0\itap0 }
+\noqfpromote
+{\stylesheet{\ql \li0\ri0\widctlpar\wrapdefault\faauto\adjustright\rin0\lin0\itap0 \rtlch\fcs1 \af1\afs22\alang1025 
+\ltrch\fcs0 \f1\fs22\lang1031\langfe3079\cgrid\langnp1031\langfenp3079 \snext0 \sqformat \spriority0 Normal;}
+{\*\cs10 \additive \ssemihidden Default Paragraph Font;}
+{\s34\ql \li0\ri0\widctlpar
+\tqc\tx4536\tqr\tx9072\wrapdefault\faauto\adjustright\rin0\lin0\itap0 \rtlch\fcs1 \af1\afs22\alang1025 \ltrch\fcs0 \f1\fs22\lang1031\langfe3079\cgrid\langnp1031\langfenp3079 \sbasedon0 \snext34 \slink35 \sunhideused header;}{\*\cs35 
+\additive \rtlch\fcs1 \af1 \ltrch\fcs0 \f1\lang1031\langfe0\langnp1031\langfenp0 \sbasedon10 \slink34 \slocked Kopfzeile Zchn;}
+}
+
+\paperw11907\paperh16840\margl1418\margr1418\margt851\margb851\gutter0\ltrsect 
+\deftab708\widowctrl\ftnbj\aenddoc\hyphhotz425\trackmoves0\trackformatting1\donotembedsysfont0\relyonvml0\donotembedlingdata1\grfdocevents0\validatexml0\showplaceholdtext0\ignoremixedcontent0\saveinvalidxml0
+\showxmlerrors0\noextrasprl\prcolbl\cvmme\sprsspbf\brkfrm\swpbdr\noxlattoyen\expshrtn\noultrlspc\dntblnsbdb\nospaceforul\lytprtmet\hyphcaps0\formshade\horzdoc\dghspace120\dgvspace120\dghorigin1701\dgvorigin1984\dghshow0\dgvshow3
+\jcompress\viewkind1\viewscale100\pgbrdrhead\pgbrdrfoot\nolnhtadjtbl\fet0{\*\wgrffmtfilter 2450}\ilfomacatclnup0
+
+\ltrpar \sectd \ltrsect\sbknone\linex0\headery709\footery709\colsx709\endnhere\pgbrdropt32\sectdefaultcl\sftnbj {\headerr \ltrpar \pard\plain \ltrpar\s34\ql \li0\ri0\widctlpar
+\tqc\tx4536\tqr\tx9072\wrapdefault\faauto\adjustright\rin0\lin0\itap0\rtlch\fcs1 \af1\afs22\alang1025 \ltrch\fcs0 \f1\fs22\lang1031\langfe3079\cgrid\langnp1031\langfenp3079 {\rtlch\fcs1 \af1\afs16 \ltrch\fcs0 \v\fs16
+\line \line 
+\par \line \line \line \line }{\rtlch\fcs1 \af1 \ltrch\fcs0 
+\par }}
+
+\pard\plain \ltrpar\ql \li0\ri0\widctlpar\wrapdefault\faauto\adjustright\rin0\lin0\itap0 \rtlch\fcs1 \af1\afs22\alang1025 
+\ltrch\fcs0 \f1\fs22\lang1031\langfe3079\cgrid\langnp1031\langfenp3079
+\par
+}

sw/qa/extras/layout/layout2.cxx
@@ -13,8 +13,10 @@
 #include <com/sun/star/text/XTextTable.hpp>
 #include <com/sun/star/linguistic2/XHyphenator.hpp>
 #include <com/sun/star/view/XSelectionSupplier.hpp>
+#include <com/sun/star/view/XViewSettingsSupplier.hpp>
 #include <com/sun/star/linguistic2/XSpellChecker1.hpp>
 
+#include <comphelper/propertysequence.hxx>
 #include <comphelper/scopeguard.hxx>
 #include <comphelper/sequence.hxx>
 #include <unotools/syslocaleoptions.hxx>
@@ -2201,6 +2203,53 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf137819)
     // so the right side of the shape must be greater than the right side of
     // textframe:
     CPPUNIT_ASSERT(sTextRightSidePosition.toInt32() < sShapeRightSidePosition.toInt32());
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testParagraphMarkLineHeight)
+{
+    createSwDoc("A020-min.rtf");
+
+    uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+    uno::Reference<view::XViewSettingsSupplier> xViewSettingsSupplier(
+        xModel->getCurrentController(), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xViewSettings(xViewSettingsSupplier->getViewSettings());
+    uno::Any aOldHidden{ xViewSettings->getPropertyValue(u"ShowHiddenCharacters"_ustr) };
+    uno::Any aOldNon{ xViewSettings->getPropertyValue(u"ShowNonprintingCharacters"_ustr) };
+    comphelper::ScopeGuard g([&] {
+        xViewSettings->setPropertyValue(u"ShowHiddenCharacters"_ustr, aOldHidden);
+        xViewSettings->setPropertyValue(u"ShowNonprintingCharacters"_ustr, aOldNon);
+    });
+
+    xViewSettings->setPropertyValue(u"ShowHiddenCharacters"_ustr, uno::Any(true));
+    xViewSettings->setPropertyValue(u"ShowNonprintingCharacters"_ustr, uno::Any(true));
+
+    {
+        xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+        assertXPath(pXmlDoc, "/root/page/header/txt[1]/SwParaPortion/SwLineLayout[1]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[1]/SwParaPortion/SwLineLayout[2]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[1]/SwParaPortion/SwLineLayout[3]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[2]/SwParaPortion/SwLineLayout[1]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[2]/SwParaPortion/SwLineLayout[2]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[2]/SwParaPortion/SwLineLayout[3]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[2]/SwParaPortion/SwLineLayout[4]"_ostr,
+                    "height"_ostr, "184");
+        assertXPath(pXmlDoc, "/root/page/header/txt[2]/SwParaPortion/SwLineLayout[5]"_ostr,
+                    "height"_ostr, "253");
+    }
+
+    xViewSettings->setPropertyValue(u"ShowNonprintingCharacters"_ustr, uno::Any(false));
+
+    {
+        xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+        assertXPath(pXmlDoc, "/root/page/header/txt[1]/SwParaPortion/SwLineLayout[1]"_ostr,
+                    "height"_ostr, "184"); // FIXME should be 253, but better than 0
+    }
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf122014)

sw/source/core/doc/DocumentSettingManager.cxx
@@ -259,6 +259,8 @@ bool sw::DocumentSettingManager::get(/*[in]*/ DocumentSettingId id) const
         case DocumentSettingId::HYPHENATE_URLS: return mbHyphenateURLs;
         case DocumentSettingId::APPLY_TEXT_ATTR_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH:
             return mbApplyTextAttrToEmptyLineAtEndOfParagraph;
+        case DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH:
+            return mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph;
         case DocumentSettingId::DO_NOT_BREAK_WRAPPED_TABLES:
             return mbDoNotBreakWrappedTables;
         case DocumentSettingId::ALLOW_TEXT_AFTER_FLOATING_TABLE_BREAK:
@@ -462,6 +464,11 @@ void sw::DocumentSettingManager::set(/*[in]*/ DocumentSettingId id, /*[in]*/ boo
         case DocumentSettingId::APPLY_TEXT_ATTR_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH:
             mbApplyTextAttrToEmptyLineAtEndOfParagraph = value;
             break;
+
+        case DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH:
+            mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph = value;
+            break;
+
 
         case DocumentSettingId::DO_NOT_MIRROR_RTL_DRAW_OBJS:
             mbDoNotMirrorRtlDrawObjs = value;
@@ -1142,6 +1149,11 @@ void sw::DocumentSettingManager::dumpAsXml(xmlTextWriterPtr pWriter) const
     (void)xmlTextWriterStartElement(pWriter, BAD_CAST("mbApplyTextAttrToEmptyLineAtEndOfParagraph"));
     (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("value"),
                                 BAD_CAST(OString::boolean(mbApplyTextAttrToEmptyLineAtEndOfParagraph).getStr()));
+    (void)xmlTextWriterEndElement(pWriter);
+
+    (void)xmlTextWriterStartElement(pWriter, BAD_CAST("mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph"));
+    (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("value"),
+                                BAD_CAST(OString::boolean(mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph).getStr()));
     (void)xmlTextWriterEndElement(pWriter);
 
     (void)xmlTextWriterStartElement(pWriter, BAD_CAST("mbDoNotMirrorRtlDrawObjs"));

sw/source/core/inc/DocumentSettingManager.hxx
@@ -178,7 +178,8 @@ class DocumentSettingManager final :
     bool mbDoNotBreakWrappedTables = false;
     bool mbAllowTextAfterFloatingTableBreak = false;
     bool mbJustifyLinesWithShrinking = false;
-    bool mbApplyTextAttrToEmptyLineAtEndOfParagraph = true;
+    bool mbApplyTextAttrToEmptyLineAtEndOfParagraph = false; // this was a mistake
+    bool mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph = false;
     bool mbDoNotMirrorRtlDrawObjs = false;
     // If this is on as_char flys wrapping will be handled the same like in Word
     bool mbNoNumberingShowFollowBy;

sw/source/core/text/atrstck.cxx
@@ -341,15 +341,19 @@ void SwAttrHandler::PushAndChg( const SwTextAttr& rAttr, SwFont& rFnt )
     // they have to be pushed to each stack they belong to
     if ( RES_TXTATR_INETFMT == rAttr.Which() ||
          RES_TXTATR_CHARFMT == rAttr.Which() ||
+         RES_PARATR_LIST_AUTOFMT == rAttr.Which() ||
          RES_TXTATR_AUTOFMT == rAttr.Which() )
     {
-        const SfxItemSet* pSet = CharFormat::GetItemSet( rAttr.GetAttr() );
+        const SfxItemSet* pSet = rAttr.Which() == RES_PARATR_LIST_AUTOFMT
+            ? rAttr.GetAttr().StaticWhichCast(RES_PARATR_LIST_AUTOFMT).GetStyleHandle().get()
+            : CharFormat::GetItemSet( rAttr.GetAttr() );
         if ( !pSet ) return;
 
+        bool const inParent{rAttr.Which() != RES_TXTATR_AUTOFMT && rAttr.Which() != RES_PARATR_LIST_AUTOFMT};
         for ( sal_uInt16 i = RES_CHRATR_BEGIN; i < RES_CHRATR_END; i++)
         {
             const SfxPoolItem* pItem;
-            bool bRet = SfxItemState::SET == pSet->GetItemState( i, rAttr.Which() != RES_TXTATR_AUTOFMT, &pItem );
+            bool bRet = SfxItemState::SET == pSet->GetItemState(i, inParent, &pItem);
 
             if ( bRet )
             {

commit a93ae87551c552b981ac447d27c7ff62379e5d81

tdf#162268 sw: formatting: paragraph style also applies to para marker
... so check it in SwTextFrame::IsHiddenNow().

During RTF import, the \v produces a RTFSprm that may be deleted before
inserting by cloneAndDeduplicate() because the style already has the
same value; this happens in one paragraph in the table in the footer of
the bugdoc and that's why that row is visible.

(regression from commit 7b8cec8322b60673e686660caf875bc71117bbd0)

Change-Id: I064ecdc04c26afab7ef2196ac4da338b9d0bb1f2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176064
Tested-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

sw/source/core/text/txtfrm.cxx
@@ -1578,7 +1578,16 @@ bool SwTextFrame::IsHiddenNow() const
         {
             SwFormatAutoFormat const& rListAutoFormat{pNode->GetAttr(RES_PARATR_LIST_AUTOFMT)};
             std::shared_ptr<SfxItemSet> const pSet{rListAutoFormat.GetStyleHandle()};
-            SvxCharHiddenItem const*const pItem{pSet ? pSet->GetItemIfSet(RES_CHRATR_HIDDEN) : nullptr};
+            SvxCharHiddenItem const* pItem{pSet ? pSet->GetItemIfSet(RES_CHRATR_HIDDEN) : nullptr};
+            if (!pItem)
+            {
+                // don't use node's mpAttrSet, it doesn't apply to para marker
+                SwFormatColl const*const pStyle{pNode->GetFormatColl()};
+                if (pStyle)
+                {
+                    pItem = &pStyle->GetFormatAttr(RES_CHRATR_HIDDEN);
+                }
+            }
             if (!pItem || !pItem->GetValue())
             {
                 bHiddenCharsHidePara = false;