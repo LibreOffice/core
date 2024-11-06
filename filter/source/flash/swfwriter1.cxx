LibreOffice/core

commit d0d46e0f506d7fd3578039ed23c9e4235c29ca8f

INTEGRATION: CWS aw024 (1.15.240); FILE MERGED 2006/09/21 20:38:37 aw 1.15.240.7: RESYNC: (1.19-1.20); FILE MERGED 2006/09/08 18:20:19 aw 1.15.240.6: RESYNC: (1.18-1.19); FILE MERGED 2006/01/26 22:41:46 aw 1.15.240.5: RESYNC: (1.17-1.18); FILE MERGED 2005/11/18 17:59:01 aw 1.15.240.4: RESYNC: (1.16-1.17); FILE MERGED 2005/09/19 16:05:48 aw 1.15.240.3: RESYNC: (1.15-1.16); FILE MERGED 2005/08/08 15:38:01 sj 1.15.240.2: #i48467# some changes within the graphicfilters 2005/05/17 16:02:03 aw 1.15.240.1: #i39529#

filter/source/flash/swfwriter1.cxx
@@ -4,9 +4,9 @@
  *
  *  $RCSfile: swfwriter1.cxx,v $
  *
- *  $Revision: 1.20 $
+ *  $Revision: 1.21 $
  *
- *  last change: $Author: obo $ $Date: 2006-09-17 07:40:51 $
+ *  last change: $Author: ihi $ $Date: 2006-11-14 14:02:17 $
  *
  *  The Contents of this file are made available subject to
  *  the terms of GNU Lesser General Public License Version 2.1.
@@ -633,7 +633,7 @@ void Writer::Impl_writeText( const Point& rPos, const String& rText, const sal_I
         //     for rotatet text
         Rectangle textBounds( 0, 0, static_cast<long>(mnDocWidth*mnDocXScale), static_cast<long>(mnDocHeight*mnDocYScale) );
 
-        Matrix3D m;
+        ::basegfx::B3DHomMatrix m;
 
         double scale = 1.0;
 
@@ -652,9 +652,9 @@ void Writer::Impl_writeText( const Point& rPos, const String& rText, const sal_I
             scale =  (double)n1 / (double)n2;
         }
 
-        m.Rotate( static_cast<double>(nOrientation) * F_PI1800 );
-        m.Translate( double(aPt.X() / scale), double(aPt.Y()) );
-        m.ScaleX( scale );
+        m.rotate( 0.0 , 0.0 , static_cast<double>(nOrientation) * F_PI1800 );
+        m.translate( double(aPt.X() / scale), double(aPt.Y()) , 0.0 );
+        m.scale( scale, 1.0 , 1.0 );
 
         sal_Int16 nHeight = _Int16( map( Size( 0, aFont.GetHeight() ) ).Height() );
 
@@ -888,14 +888,12 @@ sal_uInt16 Writer::defineBitmap( const BitmapEx &bmpSource, sal_Int32 nJPEGQuali
     {
         SvFileStream aDstStm( String( RTL_CONSTASCII_USTRINGPARAM("e:\\test.png") ), STREAM_READ | STREAM_WRITE | STREAM_TRUNC );
         aFilter.ExportGraphic( aGraphic, String(), aDstStm,
-                                    aFilter.GetExportFormatNumberForShortName( OUString( RTL_CONSTASCII_USTRINGPARAM( PNG_SHORTNAME ) ) ),
-                                    false, &aFilterData );
+                                    aFilter.GetExportFormatNumberForShortName( OUString( RTL_CONSTASCII_USTRINGPARAM( PNG_SHORTNAME ) ) ), &aFilterData );
     }
 #endif
 
     if( aFilter.ExportGraphic( aGraphic, String(), aDstStm,
-                                aFilter.GetExportFormatNumberForShortName( OUString( RTL_CONSTASCII_USTRINGPARAM( JPG_SHORTNAME ) ) ),
-                                false, &aFilterData ) == ERRCODE_NONE )
+                                aFilter.GetExportFormatNumberForShortName( OUString( RTL_CONSTASCII_USTRINGPARAM( JPG_SHORTNAME ) ) ), &aFilterData ) == ERRCODE_NONE )
     {
         pJpgData = reinterpret_cast<const sal_uInt8*>(aDstStm.GetData());
         nJpgDataLength = aDstStm.Seek( STREAM_SEEK_TO_END );
@@ -1001,10 +999,10 @@ void Writer::Impl_writeImage( const BitmapEx& rBmpEx, const Point& rPt, const Si
 
             // AS: Since images are being cropped now, no translation is normally necessary.
             //  However, some things like graphical bullet points are still get translated.
-            Matrix3D m;
-            m.Scale(1.0/XScale, 1.0/YScale);
+            ::basegfx::B3DHomMatrix m;
+            m.scale(1.0/XScale, 1.0/YScale, 1.0);
             if (destRect.Left() || destRect.Top())
-                m.Translate(destRect.Left(), destRect.Top());
+                m.translate(destRect.Left(), destRect.Top(), 0.0);
 
             FillStyle aFillStyle( nBitmapId, true, m );
 
@@ -1307,7 +1305,7 @@ bool Writer::Impl_writeFilling( SvtGraphicFill& rFilling )
             // CL->AS: Should we also scale down the quality here depending on image scale?
             sal_uInt16 nBitmapId = defineBitmap( aGraphic.GetBitmapEx(), mnJPEGCompressMode );
 
-            Matrix3D aMatrix;
+            ::basegfx::B3DHomMatrix aMatrix;
 
             SvtGraphicFill::Transform aTransform;
 
@@ -1318,10 +1316,12 @@ bool Writer::Impl_writeFilling( SvtGraphicFill& rFilling )
             {
                 for( b = 0; b < 3; b++ )
                 {
-                    aMatrix[a][b] = aTransform.matrix[a*3+b];
+                    aMatrix.set(a, b, aTransform.matrix[a*3+b]);
                 }
             }
-            aMatrix[2][0] = 0.0; aMatrix[2][1] = 0.0; aMatrix[2][2] = 1.0;
+            aMatrix.set(2, 0, 0.0);
+            aMatrix.set(2, 1, 0.0);
+            aMatrix.set(2, 2, 1.0);
 
             // scale bitmap
             Rectangle originalPixelRect = Rectangle(Point(), aGraphic.GetBitmapEx().GetSizePixel());
@@ -1329,7 +1329,7 @@ bool Writer::Impl_writeFilling( SvtGraphicFill& rFilling )
             double XScale = (double)aNewRect.GetWidth()/aOldRect.GetWidth();
             double YScale = (double)aNewRect.GetHeight()/aOldRect.GetHeight();
 
-             aMatrix.Scale( XScale, YScale );
+             aMatrix.scale( XScale, YScale , 1.0);
 
             FillStyle aFillStyle( nBitmapId, !rFilling.IsTiling(), aMatrix );
 

commit dcbf6dba50ad25d92018f87402ced06cc22fea6f

fix(swfexport): Fix crash on matrix export
Previously, we tried to set row 2 on matrices, which failed on matrices
without multiple rows, leading to a crash.

This was possibly caused by d0d46e0f506d7fd3578039ed23c9e4235c29ca8f,
which migrated to B3DHomMatrix from Matrix3D, however B3DHomMatrix has
the following comment (include/basegfx/matrix/b2dhommatrix.hxx):

// Since this is a graphics matrix, the last row is always 0 0 1, so we don't bother to store it.

It therefore seems likely that we incorrectly migrated this, and didn't
notice for a long while (faulty code is not actually used on all
documents, so there's a good chance you'll miss it)

Co-Authored-By: Caolán McNamara <caolan.mcnamara@collabora.com>
Change-Id: Ib4ba1ab6c9245190051bbf99628b3e9b28b9c34c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176146
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

filter/source/flash/swfwriter1.cxx
@@ -1175,9 +1175,6 @@ bool Writer::Impl_writeFilling(SvtGraphicFill const& rFilling)
                     aMatrix.set(a, b, aTransform.matrix[a * 3 + b]);
                 }
             }
-            aMatrix.set(2, 0, 0.0);
-            aMatrix.set(2, 1, 0.0);
-            aMatrix.set(2, 2, 1.0);
 
             // scale bitmap
             double XScale = aOldRect.GetWidth()