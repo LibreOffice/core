7ede3b80bc9ce7c99637f4d2bbc9662cf1b77b65
Michael Weghorn <m.weghorn@posteo.de>
2024-11-07 22:33:30 +0100

    tdf#130857 qt weld: Support Calc "Insert Cells" dialog
    Declare support for Calc's "Sheet" -> "Insert Cells..."
    dialog.
    
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    Change-Id: I73e514785d6e645f3c3a59a154eeb64e2b308418
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176250
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -52,6 +52,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile)
         u"cui/ui/securityoptionsdialog.ui"_ustr,
         u"cui/ui/tipofthedaydialog.ui"_ustr,
         u"modules/scalc/ui/inputstringdialog.ui"_ustr,
+        u"modules/scalc/ui/insertcells.ui"_ustr,
         u"modules/scalc/ui/selectsource.ui"_ustr,
         u"modules/schart/ui/insertaxisdlg.ui"_ustr,
         u"modules/smath/ui/alignmentdialog.ui"_ustr,
