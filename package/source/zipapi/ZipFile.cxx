70e1c10be8bb32c91235804a0b8d1f7b214bc989
Michael Stahl <michael.stahl@allotropia.de>
2024-11-08 18:08:58 +0100

    tdf#163818 package: fix recovery of zip entry local header with ...
    ... compressed size = 0.
    
    The problem is that vector::data() on a vector of size 0 returns
    nullptr, and osl_readFile into a nullptr buffer returns E_INVAL, which
    causes an exception to be thrown.
    
    Catch the exception, so that there is a chance to read the values from
    the data descriptor instead.
    
    (regression from commit 32cad89592ec04ab552399095c91dd76afb3002c
     and/or commit a6ad198d097fb4a503c8d5831d484ff46721134b)
    
    Change-Id: I9b2d9a930997146faf224d8033955b142fe93f58
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176289
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins
    (cherry picked from commit 80cda6954adc88eac3b99171acafea004976915b)

--- package/source/zipapi/ZipFile.cxx
+++ package/source/zipapi/ZipFile.cxx
@@ -1838,20 +1838,27 @@ bool ZipFile::checkSizeAndCRC( const ZipEntry& aEntry )
 {
     ::osl::MutexGuard aGuard( m_aMutexHolder->GetMutex() );
 
-    sal_Int32 nCRC = 0;
-    sal_Int64 nSize = 0;
-
-    if( aEntry.nMethod == STORED )
-        return ( getCRC( aEntry.nOffset, aEntry.nSize ) == aEntry.nCrc );
-
-    if (aEntry.nCompressedSize < 0)
+    try
     {
-        SAL_WARN("package", "bogus compressed size of: " << aEntry.nCompressedSize);
+        sal_Int32 nCRC = 0;
+        sal_Int64 nSize = 0;
+
+        if( aEntry.nMethod == STORED )
+            return ( getCRC( aEntry.nOffset, aEntry.nSize ) == aEntry.nCrc );
+
+        if (aEntry.nCompressedSize < 0)
+        {
+            SAL_WARN("package", "bogus compressed size of: " << aEntry.nCompressedSize);
+            return false;
+        }
+
+        getSizeAndCRC( aEntry.nOffset, aEntry.nCompressedSize, &nSize, &nCRC );
+        return ( aEntry.nSize == nSize && aEntry.nCrc == nCRC );
+    }
+    catch (uno::Exception const&)
+    {
         return false;
     }
-
-    getSizeAndCRC( aEntry.nOffset, aEntry.nCompressedSize, &nSize, &nCRC );
-    return ( aEntry.nSize == nSize && aEntry.nCrc == nCRC );
 }
 
 sal_Int32 ZipFile::getCRC( sal_Int64 nOffset, sal_Int64 nSize )
