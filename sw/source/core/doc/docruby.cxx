0847c140a16ddc2d39ebfbacd432efb50f25d64c
Caolán McNamara <caolan.mcnamara@collabora.com>
2024-11-14 09:29:10 +0000

    cid#1371295 avoid Missing move assignment operator
    Change-Id: I27cca1b737c441a9f31ed6aa0fc462190ca818da
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176682
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins

--- sw/source/core/doc/docruby.cxx
+++ sw/source/core/doc/docruby.cxx
@@ -221,8 +221,12 @@ void SwDoc::SetRubyList(SwPaM& rPam, const SwRubyList& rList)
 
     // Expand selection to account for insertion
     rPam.Normalize();
-    // coverity[swapped_arguments : FALSE] - this is in the correct order
-    rPam = SwPaM{ *aOrigPam.GetPoint(), *rPam.GetMark() };
+
+    if( !rPam.HasMark() )
+        rPam.SetMark();
+    rPam.Exchange();
+    *rPam.GetMark() = *aOrigPam.GetPoint();
+
     if (*rPam.GetPoint() == *rPam.GetMark())
     {
         rPam.GetPoint()->AdjustContent(-nTotalContentGrowth);
