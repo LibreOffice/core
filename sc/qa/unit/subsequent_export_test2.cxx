LibreOffice/core

commit 2942fdc8dbda375622d0add8c36df2d6679e321a

tdf#144786 Implement XML_hiddenButton functionality
Now hides autofilter button when there's an XML_hiddenButton=true
or a XML_showButton=false attribute

Change-Id: I911ef23fb5e4feff8c7de0ec154bff871a29f2e8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/143300
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

sc/source/filter/excel/excrecds.cxx
@@ -47,6 +47,8 @@
 
 #include <xcl97rec.hxx>
 #include <tabprotection.hxx>
+#include <scitems.hxx>
+#include <attrib.hxx>
 
 using namespace ::oox;
 
@@ -603,11 +605,12 @@ void ExcFilterCondition::SaveText( XclExpStream& rStrm )
     }
 }
 
-XclExpAutofilter::XclExpAutofilter( const XclExpRoot& rRoot, sal_uInt16 nC ) :
+XclExpAutofilter::XclExpAutofilter( const XclExpRoot& rRoot, sal_uInt16 nC, bool bIsEmpty ) :
     XclExpRecord( EXC_ID_AUTOFILTER, 24 ),
     XclExpRoot( rRoot ),
-    meType(FilterCondition),
+    meType(bIsEmpty ? Empty : FilterCondition),
     nCol( nC ),
+    bIsButtonHidden( false ),
     nFlags( 0 ),
     bHasBlankValue( false )
 {
@@ -818,10 +821,13 @@ void XclExpAutofilter::SaveXml( XclExpXmlStream& rStrm )
 
     sax_fastparser::FSHelperPtr& rWorksheet = rStrm.GetCurrentStream();
 
+    std::optional<OString> sHiddenButtonValue;
+    if (bIsButtonHidden)
+        sHiddenButtonValue = "1";
+
     rWorksheet->startElement( XML_filterColumn,
-            XML_colId, OString::number(nCol)
-            // OOXTODO: XML_hiddenButton,   AutoFilter12 fHideArrow?
-            // OOXTODO: XML_showButton
+            XML_colId, OString::number(nCol),
+            XML_hiddenButton, sHiddenButtonValue
     );
 
     switch (meType)
@@ -911,6 +917,8 @@ void XclExpAutofilter::SaveXml( XclExpXmlStream& rStrm )
             rWorksheet->endElement(XML_filters);
         }
         break;
+        // Used for constructing an empty filterColumn element for exporting the XML_hiddenButton attribute
+        case Empty: break;
     }
     rWorksheet->endElement( XML_filterColumn );
 }
@@ -971,6 +979,8 @@ ExcAutoFilterRecs::ExcAutoFilterRecs( const XclExpRoot& rRoot, SCTAB nTab, const
         bool    bContLoop   = true;
         bool        bHasOr      = false;
         SCCOLROW nFirstField = aParam.GetEntry( 0 ).nField;
+        ScDocument& rDoc = rRoot.GetDoc();
+        SCROW nRow = aRange.aStart.Row();
 
         // create AUTOFILTER records for filtered columns
         for( SCSIZE nEntry = 0; !bConflict && bContLoop && (nEntry < aParam.GetEntryCount()); nEntry++ )
@@ -980,7 +990,11 @@ ExcAutoFilterRecs::ExcAutoFilterRecs( const XclExpRoot& rRoot, SCTAB nTab, const
             bContLoop = rEntry.bDoQuery;
             if( bContLoop )
             {
-                XclExpAutofilter* pFilter = GetByCol( static_cast<SCCOL>(rEntry.nField) - aRange.aStart.Col() );
+                SCCOL nCol = static_cast<SCCOL>( rEntry.nField ) - aRange.aStart.Col();
+                auto nFlag = rDoc.GetAttr( nCol, nRow, nTab, ATTR_MERGE_FLAG )->GetValue();
+                bool bIsButtonHidden = !( nFlag & ScMF::Auto );
+                XclExpAutofilter* pFilter = GetByCol( nCol );
+                pFilter->SetButtonHidden( bIsButtonHidden );
 
                 if( nEntry > 0 )
                     bHasOr |= (rEntry.eConnect == SC_OR);
@@ -991,6 +1005,34 @@ ExcAutoFilterRecs::ExcAutoFilterRecs( const XclExpRoot& rRoot, SCTAB nTab, const
                                 (nFirstField != rEntry.nField);
                 if( !bConflict )
                     bConflict = pFilter->AddEntry( rEntry );
+            }
+        }
+
+        sal_uInt16 nColId = 0;
+        for ( auto nCol = aRange.aStart.Col(); nCol <= aRange.aEnd.Col(); nCol++, nColId++ )
+        {
+            auto nFlag = rDoc.GetAttr( nCol, nRow, nTab, ATTR_MERGE_FLAG )->GetValue();
+            bool bIsButtonHidden = !( nFlag & ScMF::Auto );
+            if ( bIsButtonHidden )
+            {
+                // Create filter column with hiddenButton=1 attribute if it doesn't exist
+                XclExpAutofilterRef xFilter;
+                bool bFilterFound = false;
+                for( size_t nPos = 0, nSize = maFilterList.GetSize(); nPos < nSize; ++nPos )
+                {
+                    xFilter = maFilterList.GetRecord( nPos );
+                    if( xFilter->GetCol() == static_cast<sal_uInt16>(nCol) )
+                    {
+                        bFilterFound = true;
+                        break;
+                    }
+                }
+                if ( !bFilterFound )
+                {
+                    xFilter = new XclExpAutofilter( GetRoot(), nColId, /*bIsEmpty*/true );
+                    xFilter->SetButtonHidden( true );
+                    maFilterList.AppendRecord( xFilter );
+                }
             }
         }
 

sc/qa/unit/subsequent_export_test2.cxx
@@ -189,6 +189,7 @@ public:
     void testTdf148820();
     void testEmbeddedTextInDecimal();
     void testTotalsRowFunction();
+    void testAutofilterHiddenButton();
 
     CPPUNIT_TEST_SUITE(ScExportTest2);
 
@@ -315,6 +316,7 @@ public:
     CPPUNIT_TEST(testTdf148820);
     CPPUNIT_TEST(testEmbeddedTextInDecimal);
     CPPUNIT_TEST(testTotalsRowFunction);
+    CPPUNIT_TEST(testAutofilterHiddenButton);
 
     CPPUNIT_TEST_SUITE_END();
 };
@@ -2846,6 +2848,19 @@ void ScExportTest2::testTotalsRowFunction()
                                "totalsRowFunction");
         assertXPath(pDocXml, "/x:table/x:tableColumns/x:tableColumn[6]", "totalsRowFunction",
                     "sum");
+    }
+}
+
+void ScExportTest2::testAutofilterHiddenButton()
+{
+    createScDoc("xlsx/hiddenButton.xlsx");
+    saveAndReload("Calc Office Open XML");
+    xmlDocUniquePtr pDocXml = parseExport("xl/tables/table1.xml");
+    CPPUNIT_ASSERT(pDocXml);
+    for (int i = 1; i <= 5; i++)
+    {
+        auto sPath = "/x:table/x:autoFilter/x:filterColumn[" + std::to_string(i) + "]";
+        assertXPath(pDocXml, sPath.c_str(), "hiddenButton", "1");
     }
 }
 

sc/source/filter/inc/autofilterbuffer.hxx
@@ -197,6 +197,7 @@ public:
     /** Returns converted UNO API filter settings representing all filter
         settings of this column. */
     ApiFilterSettings   finalizeImport();
+    bool                isButtonHidden();
 
 private:
     std::shared_ptr< FilterSettingsBase >

sc/source/filter/inc/excrecds.hxx
@@ -361,6 +361,7 @@ class XclExpAutofilter : public XclExpRecord, protected XclExpRoot
 private:
     enum FilterType
     {
+        Empty,
         FilterCondition,
         MultiValue,
         BlankValue,
@@ -368,6 +369,7 @@ private:
     };
     FilterType              meType;
     sal_uInt16              nCol;
+    bool                    bIsButtonHidden;
     sal_uInt16              nFlags;
     bool                    bHasBlankValue;
     ExcFilterCondition      aCond[ 2 ];
@@ -380,7 +382,7 @@ private:
     virtual void            WriteBody( XclExpStream& rStrm ) override;
 
 public:
-                            XclExpAutofilter( const XclExpRoot& rRoot, sal_uInt16 nC );
+                            XclExpAutofilter( const XclExpRoot& rRoot, sal_uInt16 nC, bool bIsEmpty = false );
 
     sal_uInt16       GetCol() const          { return nCol; }
     bool             HasTop10() const        { return ::get_flag( nFlags, EXC_AFFLAG_TOP10 ); }
@@ -388,6 +390,7 @@ public:
     bool                    HasCondition() const;
     bool                    AddEntry( const ScQueryEntry& rEntry );
     void                    AddMultiValueEntry( const ScQueryEntry& rEntry );
+    void                    SetButtonHidden(bool bValue) { bIsButtonHidden = bValue; }
     void AddColorEntry( const ScQueryEntry& rEntry );
 
     virtual void            SaveXml( XclExpXmlStream& rStrm ) override;

sc/source/filter/oox/autofilterbuffer.cxx
@@ -656,6 +656,11 @@ ApiFilterSettings FilterColumn::finalizeImport()
     return aSettings;
 }
 
+bool FilterColumn::isButtonHidden()
+{
+    return (mbShowButton == false) || (mbHiddenButton == true);
+}
+
 // SortCondition
 
 SortCondition::SortCondition( const WorkbookHelper& rHelper ) :
@@ -743,12 +748,24 @@ void AutoFilter::finalizeImport( const Reference< XDatabaseRange >& rxDatabaseRa
         '(A1 and B1) or (B2 and C1)'. */
     bool bHasOrConnection = false;
 
+    ScDocument& rDoc = getScDocument();
+    SCCOL nCol = maRange.aStart.Col();
+    SCROW nRow = maRange.aStart.Row();
+    SCTAB nTab = maRange.aStart.Tab();
+
     // process all filter column objects, exit when 'or' connection exists
     for( const auto& rxFilterColumn : maFilterColumns )
     {
         // the filter settings object creates a list of filter fields
         ApiFilterSettings aSettings = rxFilterColumn->finalizeImport();
         ApiFilterSettings::FilterFieldVector& rColumnFields = aSettings.maFilterFields;
+
+        if (rxFilterColumn->isButtonHidden())
+        {
+            auto nFlag = rDoc.GetAttr(nCol, nRow, nTab, ATTR_MERGE_FLAG)->GetValue();
+            rDoc.ApplyAttr(nCol, nRow, nTab, ScMergeFlagAttr(nFlag & ~ScMF::Auto));
+        }
+        nCol++;
 
         /*  Check whether mode for regular expressions is compatible with
             the global mode in obNeedsRegExp. If either one is still in
@@ -838,7 +855,6 @@ void AutoFilter::finalizeImport( const Reference< XDatabaseRange >& rxDatabaseRa
         aParam.maKeyState[0].nField += nStartPos;
     }
 
-    ScDocument& rDoc = getScDocument();
     ScDBData* pDBData = rDoc.GetDBAtArea(
         nSheet,
         maRange.aStart.Col(), maRange.aStart.Row(),

commit e43c2c640ec62864f79962cc345d206c2caafffb

Fix the wrong detection of hidden autofilter button
Regression caused by 2942fdc8dbda375622d0add8c36df2d6679e321a

OOXML <filterColumn colId="0">

Autofilter range colID is not absolute id. It starts always with 0 even
if the autofiltered range doesn't start with col 0 in the document.

So we are checking the flag from document (rDoc), not from the autofiltered range. So
we should use absolute column id  to get right flag.

Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
Change-Id: I1f94058caa23686596d57dd6983ddf02cd8f5e71
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176564
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sc/source/filter/excel/excrecds.cxx
@@ -993,10 +993,10 @@ ExcAutoFilterRecs::ExcAutoFilterRecs( const XclExpRoot& rRoot, SCTAB nTab, const
             bContLoop = rEntry.bDoQuery;
             if( bContLoop )
             {
-                SCCOL nCol = static_cast<SCCOL>( rEntry.nField ) - aRange.aStart.Col();
+                SCCOL nCol = static_cast<SCCOL>(rEntry.nField);
+                XclExpAutofilter* pFilter = GetByCol( nCol - aRange.aStart.Col() );
                 auto nFlag = rDoc.GetAttr( nCol, nRow, nTab, ATTR_MERGE_FLAG )->GetValue();
                 bool bIsButtonHidden = !( nFlag & ScMF::Auto );
-                XclExpAutofilter* pFilter = GetByCol( nCol );
                 pFilter->SetButtonHidden( bIsButtonHidden );
 
                 if( nEntry > 0 )

sc/qa/unit/subsequent_export_test2.cxx
@@ -536,6 +536,19 @@ CPPUNIT_TEST_FIXTURE(ScExportTest2, testAutofilterTop10XLSX)
     assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:top10"_ostr, "val"_ostr, "4");
 }
 
+CPPUNIT_TEST_FIXTURE(ScExportTest2, testAutofilterColButton)
+{
+    // Without fix it will fail
+    // - Expression: !xmlGetProp(pXmlNode, BAD_CAST(rAttribute.getStr()))
+    // - In <>, XPath '//x:autoFilter/x:filterColumn' unexpected 'hiddenButton' attribute
+    createScDoc("ods/autofilter-colbutton.ods");
+
+    save("Calc Office Open XML");
+    xmlDocUniquePtr pDoc = parseExport("xl/worksheets/sheet1.xml");
+    CPPUNIT_ASSERT(pDoc);
+    assertXPathNoAttribute(pDoc, "//x:autoFilter/x:filterColumn"_ostr, "hiddenButton"_ostr);
+}
+
 CPPUNIT_TEST_FIXTURE(ScExportTest2, testTdf88657ODS)
 {
     createScDoc("ods/tdf88657.ods");