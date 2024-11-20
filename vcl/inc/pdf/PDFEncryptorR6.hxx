e77d1c2b3f900ba29c1eb1a2547b671628f354d5
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2024-11-20 17:25:26 +0900

    pdf: generate U, UE and O, OE keys for R6 encryption
    Also test the algorithm against the known values from an example,
    to be sure we are calculating the values correctly. For this we
    need a couple of decryption algorithms, but those do mostly just
    the reverse of the encryption.
    
    Change-Id: I5499ed0b57671f44e48fe68961e07cde22be6b39
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176881
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- vcl/inc/pdf/PDFEncryptorR6.hxx
+++ vcl/inc/pdf/PDFEncryptorR6.hxx
@@ -9,16 +9,72 @@
 
 #pragma once
 
+#include <rtl/ustring.hxx>
 #include <string_view>
 #include <vector>
 #include <vcl/dllapi.h>
 
 namespace vcl::pdf
 {
+/** Algorithm 2.B: Computing a hash (revision 6 and later)
+ *
+ * Described in ISO 32000-2:2020(E) - 7.6.4.3.4
+ */
 VCL_DLLPUBLIC std::vector<sal_uInt8>
 computeHashR6(const sal_uInt8* pPassword, size_t nPasswordLength,
               std::vector<sal_uInt8> const& rValidationSalt,
               std::vector<sal_uInt8> const& rUserKey = std::vector<sal_uInt8>());
+
+/** Algorithm 11: Authenticating the user password (Security handlers of revision 6)
+ *
+ * Described in ISO 32000-2:2020(E) - 7.6.4.4.10
+ */
+VCL_DLLPUBLIC bool validateUserPassword(const sal_uInt8* pUserPass, size_t nPasswordLength,
+                                        std::vector<sal_uInt8>& U);
+
+/** Algorithm 12: Authenticating the owner password (Security handlers of revision 6)
+ *
+ * Described in ISO 32000-2:2020(E) - 7.6.4.4.11
+ */
+VCL_DLLPUBLIC bool validateOwnerPassword(const sal_uInt8* pUserPass, size_t nPasswordLength,
+                                         std::vector<sal_uInt8>& U, std::vector<sal_uInt8>& O);
+
+/** Generates the encryption key - random data 32-byte */
+VCL_DLLPUBLIC std::vector<sal_uInt8> generateKey();
+
+/** Algorithm 8: U and UE
+ *
+ * Computing the encryption dictionary’s U (user password) and UE (user encryption) values
+ * (Security handlers of revision 6)
+ *
+ * Described in ISO 32000-2:2020(E) - 7.6.4.4.7
+ */
+VCL_DLLPUBLIC void generateUandUE(const sal_uInt8* pUserPass, size_t nPasswordLength,
+                                  std::vector<sal_uInt8>& rFileEncryptionKey,
+                                  std::vector<sal_uInt8>& U, std::vector<sal_uInt8>& UE);
+
+/** Algorithm 9: O and OE
+ *
+ * Computing the encryption dictionary’s O (owner password) and OE (owner encryption) values
+ * (Security handlers of revision 6)
+ *
+ * Described in ISO 32000-2:2020(E) - 7.6.4.4.8
+ */
+VCL_DLLPUBLIC void generateOandOE(const sal_uInt8* pUserPass, size_t nPasswordLength,
+                                  std::vector<sal_uInt8>& rFileEncryptionKey,
+                                  std::vector<sal_uInt8>& U, std::vector<sal_uInt8>& O,
+                                  std::vector<sal_uInt8>& OE);
+
+/** Algorithm 8 step b) in reverse
+ *
+ * Described in ISO 32000-2:2020(E) - 7.6.4.4.7
+ *
+ * - compute the hash with password and user key salt
+ * - decrypt with hash as key and zero IV
+ */
+VCL_DLLPUBLIC std::vector<sal_uInt8> decryptKey(const sal_uInt8* pUserPass, size_t nPasswordLength,
+                                                std::vector<sal_uInt8>& U,
+                                                std::vector<sal_uInt8>& UE);
 
 } // end vcl::pdf
 
