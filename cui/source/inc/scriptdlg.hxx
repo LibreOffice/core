1fbc7faa91181ae8ac240dfe8db704673c44a14d
Bogdan Buzea <buzea.bogdan@libreoffice.org>
2024-11-21 19:58:17 +0100

    tdf#152299 - Remove unused define(s) from C/C++ files
    Change-Id: Iced153f4258e2f27007cfbb7a63b78db7f6985dc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176962
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Jenkins

--- cui/source/inc/scriptdlg.hxx
+++ cui/source/inc/scriptdlg.hxx
@@ -30,10 +30,6 @@
 
 #include <unordered_map>
 
-#define OBJTYPE_METHOD          2L
-#define OBJTYPE_SCRIPTCONTAINER 3L
-#define OBJTYPE_SFROOT          4L
-
 typedef std::unordered_map < OUString, OUString > Selection_hash;
 
 class SFEntry;
