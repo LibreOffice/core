LibreOffice/core

commit 21734247d58a6e915b058d8fa55ece949d049613

drop internal support for 1-bit images
on modern hardware, these are less efficient than regular alpha
channels.

Also, this greatly simplies the range of stuff that vcl needs to deal
with, which will make the upcoming transparency->alpha patch easier to
deal with.

Enhance vcl::CreateFromData to convert incoming 1-bit data to 8-bit
image.

Change-Id: I35829da750029fe373d0d2911a669d10bab6ad23
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145321
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -9209,9 +9209,12 @@ bool PDFWriterImpl::writeBitmapObject( const BitmapEmit& rObject, bool bMask )
     {
         if( m_aContext.Version < PDFWriter::PDFVersion::PDF_1_4 || ! rObject.m_aBitmap.IsAlpha() )
         {
-            aBitmap = getExportBitmap(rObject.m_aBitmap.GetAlphaMask());
-            aBitmap.Convert( BmpConversion::N1BitThreshold );
-            SAL_WARN_IF(aBitmap.getPixelFormat() != vcl::PixelFormat::N1_BPP, "vcl.pdfwriter", "mask conversion failed" );
+            if( rObject.m_aBitmap.IsAlpha() )
+            {
+                aBitmap = getExportBitmap(rObject.m_aBitmap.GetAlphaMask());
+                aBitmap.Convert( BmpConversion::N1BitThreshold );
+                SAL_WARN_IF(aBitmap.getPixelFormat() != vcl::PixelFormat::N8_BPP, "vcl.pdfwriter", "mask conversion failed" );
+            }
         }
         else if (aBitmap.getPixelFormat() != vcl::PixelFormat::N8_BPP)
         {
@@ -9228,7 +9231,6 @@ bool PDFWriterImpl::writeBitmapObject( const BitmapEmit& rObject, bool bMask )
     auto const ePixelFormat = aBitmap.getPixelFormat();
     switch (ePixelFormat)
     {
-        case vcl::PixelFormat::N1_BPP:
         case vcl::PixelFormat::N8_BPP:
             bTrueColor = false;
             nBitsPerComponent = vcl::pixelFormatBitCount(ePixelFormat);
@@ -9282,7 +9284,7 @@ bool PDFWriterImpl::writeBitmapObject( const BitmapEmit& rObject, bool bMask )
         else if( aBitmap.HasGreyPaletteAny() )
         {
             aLine.append( "/DeviceGray\n" );
-            if (aBitmap.getPixelFormat() == vcl::PixelFormat::N1_BPP)
+            if (aBitmap.getPixelFormat() == vcl::PixelFormat::N8_BPP)
             {
                 // #i47395# 1 bit bitmaps occasionally have an inverted grey palette
                 sal_uInt16 nBlackIndex = pAccess->GetBestPaletteIndex( BitmapColor( COL_BLACK ) );
@@ -9355,21 +9357,8 @@ bool PDFWriterImpl::writeBitmapObject( const BitmapEmit& rObject, bool bMask )
     }
     else
     {
-        if (aBitmap.getPixelFormat() == vcl::PixelFormat::N1_BPP)
-        {
-            aLine.append( "/ImageMask true\n" );
-            sal_Int32 nBlackIndex = pAccess->GetBestPaletteIndex( BitmapColor( COL_BLACK ) );
-            SAL_WARN_IF( nBlackIndex != 0 && nBlackIndex != 1, "vcl.pdfwriter", "wrong black index" );
-            if( nBlackIndex )
-                aLine.append( "/Decode[ 1 0 ]\n" );
-            else
-                aLine.append( "/Decode[ 0 1 ]\n" );
-        }
-        else if (aBitmap.getPixelFormat() == vcl::PixelFormat::N8_BPP)
-        {
-            aLine.append( "/ColorSpace/DeviceGray\n"
-                          "/Decode [ 1 0 ]\n" );
-        }
+        aLine.append( "/ColorSpace/DeviceGray\n"
+                      "/Decode [ 1 0 ]\n" );
     }
 
     if( ! bMask && m_aContext.Version > PDFWriter::PDFVersion::PDF_1_2 && !m_bIsPDF_A1 )
@@ -9614,10 +9603,7 @@ const BitmapEmit& PDFWriterImpl::createBitmapEmit(const BitmapEx& i_rBitmap, con
     BitmapEx aBitmap( i_rBitmap );
     auto ePixelFormat = aBitmap.GetBitmap().getPixelFormat();
     if( m_aContext.ColorMode == PDFWriter::DrawGreyscale )
-    {
-        if (ePixelFormat != vcl::PixelFormat::N1_BPP)
-            aBitmap.Convert(BmpConversion::N8BitGreys);
-    }
+        aBitmap.Convert(BmpConversion::N8BitGreys);
     BitmapID aID;
     aID.m_aPixelSize        = aBitmap.GetSizePixel();
     aID.m_nSize             = vcl::pixelFormatBitCount(ePixelFormat);

avmedia/source/gstreamer/gstframegrabber.cxx
@@ -146,7 +146,7 @@ uno::Reference< graphic::XGraphic > SAL_CALL FrameGrabber::grabFrame( double fMe
         pData = aMapInfo.data;
 
         int nStride = GST_ROUND_UP_4( nWidth * 3 );
-        BitmapEx aBmp = vcl::bitmap::CreateFromData(pData, nWidth, nHeight, nStride, vcl::PixelFormat::N24_BPP);
+        BitmapEx aBmp = vcl::bitmap::CreateFromData(pData, nWidth, nHeight, nStride, /*nBitsPerPixel*/24);
 
         gst_buffer_unmap( pBuf, &aMapInfo );
         xRet = Graphic( aBmp ).GetXGraphic();

cppcanvas/source/mtfrenderer/implrenderer.cxx
@@ -210,7 +210,7 @@ namespace
 
         Bitmap aMask( rBitmap.CreateMask( aWhite ));
         Bitmap aSolid( rBitmap.GetSizePixel(),
-                       vcl::PixelFormat::N1_BPP,
+                       vcl::PixelFormat::N8_BPP,
                        &aBiLevelPalette );
         aSolid.Erase( rMaskColor );
 

desktop/qa/desktop_lib/test_desktop_lib.cxx
@@ -3357,7 +3357,7 @@ void DesktopLOKTest::testRenderSearchResult_WriterNode()
     CPPUNIT_ASSERT_EQUAL(size_t(1440648), nByteSize);
 
     const sal_uInt8* pD = reinterpret_cast<const sal_uInt8*>(pBuffer);
-    BitmapEx aBitmap = vcl::bitmap::CreateFromData(pD, nWidth, nHeight, nWidth * 4, vcl::PixelFormat::N32_BPP, true, true);
+    BitmapEx aBitmap = vcl::bitmap::CreateFromData(pD, nWidth, nHeight, nWidth * 4, /*nBitsPerPixel*/32, true, true);
 
     if (bDumpBitmap)
     {
@@ -3402,7 +3402,7 @@ void DesktopLOKTest::testRenderSearchResult_CommonNode()
     CPPUNIT_ASSERT_EQUAL(size_t(73728), nByteSize);
 
     const sal_uInt8* pD = reinterpret_cast<const sal_uInt8*>(pBuffer);
-    BitmapEx aBitmap = vcl::bitmap::CreateFromData(pD, nWidth, nHeight, nWidth * 4, vcl::PixelFormat::N32_BPP, true, true);
+    BitmapEx aBitmap = vcl::bitmap::CreateFromData(pD, nWidth, nHeight, nWidth * 4, /*nBitsPerPixel*/32, true, true);
 
     if (bDumpBitmap)
     {

drawinglayer/source/tools/wmfemfhelper.cxx
@@ -642,7 +642,7 @@ namespace wmfemfhelper
         };
 
         Bitmap aMask(rBitmap.CreateMask(aWhite));
-        Bitmap aSolid(rBitmap.GetSizePixel(), vcl::PixelFormat::N1_BPP, &aBiLevelPalette);
+        Bitmap aSolid(rBitmap.GetSizePixel(), vcl::PixelFormat::N8_BPP, &aBiLevelPalette);
 
         aSolid.Erase(rMaskColor);
 

emfio/source/reader/wmfreader.cxx
@@ -837,8 +837,8 @@ namespace emfio
                     SAL_WARN("emfio", "\t\t TODO The unsupported Bitmap record. Please fill a bug.");
                     break;
                 }
-                const vcl::PixelFormat ePixelFormat = vcl::bitDepthToPixelFormat( nBitCount );
-                bool bOk = nWidth > 0 && nHeight > 0 && nBytesPerScan > 0 && ePixelFormat != vcl::PixelFormat::INVALID;
+                bool bOk = nWidth > 0 && nHeight > 0 && nBytesPerScan > 0
+                    && (nBitCount == 1 || nBitCount == 8 || nBitCount == 24 || nBitCount == 32);
                 if (bOk)
                 {
                     // must be enough data to fulfil the request
@@ -854,7 +854,7 @@ namespace emfio
                     std::unique_ptr< sal_uInt8[] > pData;
                     pData.reset( new sal_uInt8[ nHeight * nBytesPerScan ] );
                     mpInputStream->ReadBytes( pData.get(), nHeight * nBytesPerScan );
-                    BitmapEx aBitmap = vcl::bitmap::CreateFromData( pData.get(), nWidth, nHeight, nBytesPerScan, ePixelFormat, true );
+                    BitmapEx aBitmap = vcl::bitmap::CreateFromData( pData.get(), nWidth, nHeight, nBytesPerScan, nBitCount, true );
                     if ( nSye && nSxe &&
                          ( nXSrc + nSxe <= nWidth ) &&
                          ( nYSrc + nSye <= nHeight ) )

filter/source/msfilter/msdffimp.cxx
@@ -1412,7 +1412,7 @@ void DffPropertyReader::ApplyFillAttributes( SvStream& rIn, SfxItemSet& rSet, co
                         Bitmap aBmp( aGraf.GetBitmapEx().GetBitmap() );
                         if (aBmp.GetSizePixel().Width() == 8 &&
                             aBmp.GetSizePixel().Height() == 8 &&
-                            aBmp.getPixelFormat() == vcl::PixelFormat::N1_BPP)
+                            aBmp.getPixelFormat() == vcl::PixelFormat::N8_BPP)
                         {
                             Color aCol1( COL_WHITE ), aCol2( COL_WHITE );
 

forms/source/component/imgprod.cxx
@@ -377,7 +377,7 @@ void ImageProducer::ImplUpdateConsumer( const Graphic& rGraphic )
 
     if( !pMskAcc )
     {
-        aMask = Bitmap(aBmp.GetSizePixel(), vcl::PixelFormat::N1_BPP);
+        aMask = Bitmap(aBmp.GetSizePixel(), vcl::PixelFormat::N8_BPP, &Bitmap::GetGreyPalette(256));
         aMask.Erase( COL_BLACK );
         pMskAcc = aMask.AcquireReadAccess();
     }

commit 668dbfd99ada1888d5919cffedcfd7486c101717

tdf#162815 Greyscale images not corrrectly exported to PDF
regression from commit 21734247d58a6e915b058d8fa55ece949d049613
"drop internal support for 1-bit images"

This only works on Linux because the HasGreyPaletteAny() check
is returning false on Linux, because on linux the swap in/out is more aggressive, and after it is has gone through the swap in/out process,
the image palette is no longer one that matches anything that HasGreyPaletteAny() checks for (it contains 256 entries, but only 2 of them are used)

I'm not exactly sure why the greyscale bitmap generation code
is generating bad PDF data, but the RGB code works great, so
lets just use that rather.

Change-Id: Ibb9e837540d5ed567c706e21e7ff93fe92118580
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177329
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/gdi/pdfwriter_impl.cxx
@@ -9738,37 +9738,6 @@ bool PDFWriterImpl::writeBitmapObject( const BitmapEmit& rObject, bool bMask )
         aLine.append( "/ColorSpace" );
         if( bTrueColor )
             aLine.append( "/DeviceRGB\n" );
-        else if( aBitmap.HasGreyPaletteAny() )
-        {
-            aLine.append( "/DeviceGray\n" );
-            if (aBitmap.getPixelFormat() == vcl::PixelFormat::N8_BPP)
-            {
-                // #i47395# 1 bit bitmaps occasionally have an inverted grey palette
-                sal_uInt16 nBlackIndex = pAccess->GetBestPaletteIndex( BitmapColor( COL_BLACK ) );
-                assert( nBlackIndex == 0 || nBlackIndex == 1);
-                sal_uInt16 nWhiteIndex = pAccess->GetBestPaletteIndex( BitmapColor( COL_WHITE ) );
-                if( pAccess->GetPalette()[nBlackIndex] == BitmapColor( COL_BLACK ) &&
-                    pAccess->GetPalette()[nWhiteIndex] == BitmapColor( COL_WHITE ) )
-                {
-                    // It is black and white
-                    if( nBlackIndex == 1 )
-                        aLine.append( "/Decode[1 0]\n" );
-                }
-                else
-                {
-                    // It is two levels of grey
-                    aLine.append( "/Decode[" );
-                    assert( pAccess->GetPalette()[0].GetRed() == pAccess->GetPalette()[0].GetGreen() &&
-                            pAccess->GetPalette()[0].GetRed() == pAccess->GetPalette()[0].GetBlue() &&
-                            pAccess->GetPalette()[1].GetRed() == pAccess->GetPalette()[1].GetGreen() &&
-                            pAccess->GetPalette()[1].GetRed() == pAccess->GetPalette()[1].GetBlue() );
-                    aLine.append( pAccess->GetPalette()[0].GetRed() / 255.0 );
-                    aLine.append( " " );
-                    aLine.append( pAccess->GetPalette()[1].GetRed() / 255.0 );
-                    aLine.append( "]\n" );
-                }
-            }
-        }
         else
         {
             aLine.append( "[ /Indexed/DeviceRGB " );