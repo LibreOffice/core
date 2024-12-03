LibreOffice/core

commit 13ac356a32f279a3fd542d62c12e58abe3d3ad30

tdf#164048 sw a11y: improve error/warning levels with
adding two A11y issue level: Error and warning level, and
categorizing our accesibility issues based on how the
PAC 2024 tools and WCAG behave with the different warnings/errors.

Change-Id: If4e4800497340318bb990326c64de655f24a47e8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177389
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

sw/source/uibase/sidebar/A11yCheckIssuesPanel.cxx
@@ -97,28 +97,9 @@ IMPL_LINK_NOARG(AccessibilityCheckEntry, FixButtonClicked, weld::Button&, void)
         m_pAccessibilityIssue->quickFixIssue();
 }
 
-std::unique_ptr<PanelLayout> A11yCheckIssuesPanel::Create(weld::Widget* pParent,
-                                                          SfxBindings* pBindings)
-{
-    if (pParent == nullptr)
-        throw css::lang::IllegalArgumentException(
-            u"no parent window given to A11yCheckIssuesPanel::Create"_ustr, nullptr, 0);
-    return std::make_unique<A11yCheckIssuesPanel>(pParent, pBindings);
-}
-
-A11yCheckIssuesPanel::A11yCheckIssuesPanel(weld::Widget* pParent, SfxBindings* pBindings)
-    : PanelLayout(pParent, u"A11yCheckIssuesPanel"_ustr,
-                  u"modules/swriter/ui/a11ycheckissuespanel.ui"_ustr)
-    , m_xOptionsButton(m_xBuilder->weld_button(u"bOptions"_ustr))
-    , mxAccessibilityBox(m_xBuilder->weld_box(u"accessibilityCheckBox"_ustr))
-    , mxUpdateBox(m_xBuilder->weld_box(u"updateBox"_ustr))
-    , mxUpdateLinkButton(m_xBuilder->weld_link_button(u"updateLinkButton"_ustr))
-
-    , mpBindings(pBindings)
-    , mpDoc(nullptr)
-    , maA11yCheckController(FN_STAT_ACCESSIBILITY_CHECK, *pBindings, *this)
-    , mnIssueCount(0)
-    , mbAutomaticCheckEnabled(false)
+AccessibilityCheckLevel::AccessibilityCheckLevel(weld::Box* pParent)
+    : m_xBuilder(Application::CreateBuilder(pParent, u"svx/ui/accessibilitychecklevel.ui"_ustr))
+    , m_xContainer(m_xBuilder->weld_box(u"accessibilityCheckLevelBox"_ustr))
 {
     m_xExpanders[0] = m_xBuilder->weld_expander(u"expand_document"_ustr);
     m_xExpanders[1] = m_xBuilder->weld_expander(u"expand_styles"_ustr);
@@ -143,6 +124,84 @@ A11yCheckIssuesPanel::A11yCheckIssuesPanel(weld::Widget* pParent, SfxBindings* p
     m_xBoxes[8] = m_xBuilder->weld_box(u"box_fakes"_ustr);
     m_xBoxes[9] = m_xBuilder->weld_box(u"box_numbering"_ustr);
     m_xBoxes[10] = m_xBuilder->weld_box(u"box_other"_ustr);
+}
+
+void AccessibilityCheckLevel::removeAllEntries()
+{
+    for (auto eGroup : o3tl::enumrange<AccessibilityCheckGroups>())
+    {
+        auto nGroupIndex = size_t(eGroup);
+        for (auto const& xEntry : m_aEntries[nGroupIndex])
+            m_xBoxes[nGroupIndex]->move(xEntry->get_widget(), nullptr);
+    }
+}
+
+void AccessibilityCheckLevel::reset()
+{
+    for (auto& xExpander : m_xExpanders)
+        xExpander.reset();
+
+    for (auto& xBox : m_xBoxes)
+        xBox.reset();
+}
+
+void AccessibilityCheckLevel::addEntryForGroup(
+    AccessibilityCheckGroups eGroup, std::vector<sal_Int32>& rIndices,
+    std::shared_ptr<sfx::AccessibilityIssue> const& pIssue)
+{
+    auto nGroupIndex = size_t(eGroup);
+    // prevent the UI locking up forever, this is effectively an O(n^2) situation, given the way the UI additions work
+    if (m_aEntries[nGroupIndex].size() > 500)
+    {
+        SAL_WARN("sw", "too many a11y issues, not adding to panel");
+        return;
+    }
+    auto xEntry = std::make_unique<AccessibilityCheckEntry>(m_xBoxes[nGroupIndex].get(), pIssue);
+    m_xBoxes[nGroupIndex]->reorder_child(xEntry->get_widget(), rIndices[nGroupIndex]++);
+    m_aEntries[nGroupIndex].push_back(std::move(xEntry));
+}
+
+size_t AccessibilityCheckLevel::getEntrySize(AccessibilityCheckGroups eGroup) const
+{
+    auto nGroupIndex = size_t(eGroup);
+    return m_aEntries[nGroupIndex].size();
+}
+
+void AccessibilityCheckLevel::show(size_t nGroupIndex) { m_xExpanders[nGroupIndex]->show(); }
+
+void AccessibilityCheckLevel::hide(size_t nGroupIndex) { m_xExpanders[nGroupIndex]->hide(); }
+
+std::unique_ptr<PanelLayout> A11yCheckIssuesPanel::Create(weld::Widget* pParent,
+                                                          SfxBindings* pBindings)
+{
+    if (pParent == nullptr)
+        throw css::lang::IllegalArgumentException(
+            u"no parent window given to A11yCheckIssuesPanel::Create"_ustr, nullptr, 0);
+    return std::make_unique<A11yCheckIssuesPanel>(pParent, pBindings);
+}
+
+A11yCheckIssuesPanel::A11yCheckIssuesPanel(weld::Widget* pParent, SfxBindings* pBindings)
+    : PanelLayout(pParent, u"A11yCheckIssuesPanel"_ustr,
+                  u"modules/swriter/ui/a11ycheckissuespanel.ui"_ustr)
+    , m_xOptionsButton(m_xBuilder->weld_button(u"bOptions"_ustr))
+    , mxUpdateBox(m_xBuilder->weld_box(u"updateBox"_ustr))
+    , mxUpdateLinkButton(m_xBuilder->weld_link_button(u"updateLinkButton"_ustr))
+    , m_xListSep(m_xBuilder->weld_widget(u"sep_level"_ustr))
+    , mpBindings(pBindings)
+    , mpDoc(nullptr)
+    , maA11yCheckController(FN_STAT_ACCESSIBILITY_CHECK, *pBindings, *this)
+    , mnIssueCount(0)
+    , mbAutomaticCheckEnabled(false)
+{
+    // errors
+    m_xLevelExpanders[0] = m_xBuilder->weld_expander(u"expand_errors"_ustr);
+    mxAccessibilityBox[0] = m_xBuilder->weld_box(u"accessibilityBoxErr"_ustr);
+    m_aLevelEntries[0] = std::make_unique<AccessibilityCheckLevel>(mxAccessibilityBox[0].get());
+
+    // warnings
+    m_xLevelExpanders[1] = m_xBuilder->weld_expander(u"expand_warnings"_ustr);
+    mxAccessibilityBox[1] = m_xBuilder->weld_box(u"accessibilityBoxWrn"_ustr);
+    m_aLevelEntries[1] = std::make_unique<AccessibilityCheckLevel>(mxAccessibilityBox[1].get());
 
     mxUpdateLinkButton->connect_activate_link(
         LINK(this, A11yCheckIssuesPanel, UpdateLinkButtonClicked));
@@ -171,12 +230,14 @@ A11yCheckIssuesPanel::A11yCheckIssuesPanel(weld::Widget* pParent, SfxBindings* p
     // In desktop don't show the update button and schedule to run the accessibility check async
     if (comphelper::LibreOfficeKit::isActive())
     {
-        mxAccessibilityBox->hide();
+        m_xLevelExpanders[0]->hide();
+        m_xLevelExpanders[1]->hide();
         mxUpdateBox->show();
     }
     else
     {
-        mxAccessibilityBox->show();
+        m_xLevelExpanders[0]->show();
+        m_xLevelExpanders[1]->show();
         mxUpdateBox->hide();
         Application::PostUserEvent(LINK(this, A11yCheckIssuesPanel, PopulateIssuesHdl));
     }
@@ -191,7 +252,8 @@ IMPL_LINK_NOARG(A11yCheckIssuesPanel, OptionsButtonClicked, weld::Button&, void)
 
 IMPL_LINK_NOARG(A11yCheckIssuesPanel, UpdateLinkButtonClicked, weld::LinkButton&, bool)
 {
-    mxAccessibilityBox->show();
+    m_xLevelExpanders[0]->show();
+    m_xLevelExpanders[1]->show();
     mxUpdateBox->hide();
     Application::PostUserEvent(LINK(this, A11yCheckIssuesPanel, PopulateIssuesHdl));
     return true;
@@ -211,44 +273,41 @@ void A11yCheckIssuesPanel::ImplDestroy()
         mpBindings->Invalidate(SID_ACCESSIBILITY_CHECK_ONLINE);
     }
 
-    for (auto& xExpander : m_xExpanders)
-        xExpander.reset();
-
-    for (auto& xBox : m_xBoxes)
-        xBox.reset();
+    for (auto& aLevelEntry : m_aLevelEntries)
+    {
+        aLevelEntry->reset();
+        aLevelEntry.reset();
+    }
 }
 
 A11yCheckIssuesPanel::~A11yCheckIssuesPanel() { suppress_fun_call_w_exception(ImplDestroy()); }
 
 void A11yCheckIssuesPanel::removeAllEntries()
 {
-    for (auto eGroup : o3tl::enumrange<AccessibilityCheckGroups>())
+    for (auto& aLevelEntry : m_aLevelEntries)
     {
-        auto nGroupIndex = size_t(eGroup);
-        for (auto const& xEntry : m_aEntries[nGroupIndex])
-            m_xBoxes[nGroupIndex]->move(xEntry->get_widget(), nullptr);
+        aLevelEntry->removeAllEntries();
     }
 }
 
 void A11yCheckIssuesPanel::addEntryForGroup(AccessibilityCheckGroups eGroup,
-                                            std::vector<sal_Int32>& rIndices,
+                                            std::vector<std::vector<sal_Int32>>& rIndices,
                                             std::shared_ptr<sfx::AccessibilityIssue> const& pIssue)
 {
-    auto nGroupIndex = size_t(eGroup);
     // prevent the UI locking up forever, this is effectively an O(n^2) situation, given the way the UI additions work
-    if (m_aEntries[nGroupIndex].size() > 500)
+    if (m_aLevelEntries[0]->getEntrySize(eGroup) + m_aLevelEntries[1]->getEntrySize(eGroup) > 500)
     {
         SAL_WARN("sw", "too many a11y issues, not adding to panel");
         return;
     }
-    auto xEntry = std::make_unique<AccessibilityCheckEntry>(m_xBoxes[nGroupIndex].get(), pIssue);
-    m_xBoxes[nGroupIndex]->reorder_child(xEntry->get_widget(), rIndices[nGroupIndex]++);
-    m_aEntries[nGroupIndex].push_back(std::move(xEntry));
+
+    size_t nLevel = static_cast<size_t>(pIssue->m_eIssueLvl);
+    m_aLevelEntries[nLevel]->addEntryForGroup(eGroup, rIndices[nLevel], pIssue);
 }
 
 void A11yCheckIssuesPanel::populateIssues()
 {
-    if (!mpDoc || !mxAccessibilityBox->is_visible())
+    if (!mpDoc || !(m_xLevelExpanders[0]->is_visible() || m_xLevelExpanders[1]->is_visible()))
         return;
 
     SfxViewShell* pViewShell = SfxViewShell::Current();
@@ -263,7 +322,7 @@ void A11yCheckIssuesPanel::populateIssues()
 
     removeAllEntries();
 
-    std::vector<sal_Int32> nIndices(11, 0);
+    std::vector<std::vector<sal_Int32>> nIndices(2, std::vector<sal_Int32>(11, 0));
     sal_Int32 nDirectFormats = 0;
 
     for (std::shared_ptr<sfx::AccessibilityIssue> const& pIssue : m_aIssueCollection.getIssues())
@@ -368,22 +427,42 @@ void A11yCheckIssuesPanel::populateIssues()
         }
     }
 
-    // add DirectFormats (if have) as last element to Formatting AccessibilityCheckGroup
+    // add DirectFormats (if have) as last element to Formatting AccessibilityCheckGroup on the Warning level
     if (nDirectFormats > 0)
     {
         size_t nGroupFormatIndex = size_t(AccessibilityCheckGroups::Formatting);
-        nIndices[nGroupFormatIndex]++;
+        // Direct Formats are on the warning level
+        size_t nLevel = static_cast<size_t>(sfx::AccessibilityIssueLevel::WARNLEV);
+        nIndices[nLevel][nGroupFormatIndex]++;
     }
 
-    size_t nGroupIndex = 0;
-    for (sal_Int32 nIndex : nIndices)
+    for (auto eLevel : o3tl::enumrange<sfx::AccessibilityIssueLevel>())
     {
-        if (nIndex > 0)
-            m_xExpanders[nGroupIndex]->show();
+        size_t nGroupIndex = 0;
+        auto nLevelIndex = size_t(eLevel);
+        bool bHaveIssue = false;
+        for (sal_Int32 nIndex : nIndices[nLevelIndex])
+        {
+            if (nIndex > 0)
+            {
+                m_aLevelEntries[nLevelIndex]->show(nGroupIndex);
+                if (!bHaveIssue)
+                    bHaveIssue = true;
+            }
+            else
+                m_aLevelEntries[nLevelIndex]->hide(nGroupIndex);
+            nGroupIndex++;
+        }
+        if (!bHaveIssue)
+            m_xLevelExpanders[nLevelIndex]->hide();
         else
-            m_xExpanders[nGroupIndex]->hide();
-        nGroupIndex++;
+            m_xLevelExpanders[nLevelIndex]->show();
     }
+
+    if (m_xLevelExpanders[0]->is_visible() && m_xLevelExpanders[1]->is_visible())
+        m_xListSep->set_visible(true);
+    else
+        m_xListSep->set_visible(false);
 
     if (pWindow)
         pWindow->SetPointer(PointerStyle::Arrow);

filter/inc/strings.hrc
@@ -76,9 +76,9 @@
 #define STR_WARN_TRANSP_CONVERTED_SHORT         NC_("STR_WARN_TRANSP_CONVERTED_SHORT", "Transparencies removed")
 #define STR_ERR_SIGNATURE_FAILED                NC_("STR_ERR_SIGNATURE_FAILED", "Signature generation failed")
 #define STR_ERR_PDF_EXPORT_ABORTED              NC_("STR_ERR_PDF_EXPORT_ABORTED", "PDF export aborted")
-#define STR_WARN_PDFUA_ISSUES                   NNC_("STR_WARN_PDFUA_ISSUES", "One accessibility issue detected. Do you want to continue?", "%1 accessibility issues detected. Do you want to continue?")
+#define STR_WARN_PDFUA_ISSUES                   NNC_("STR_WARN_PDFUA_ISSUES", "One accessibility error detected. Do you want to continue?", "%1 accessibility errors detected. Do you want to continue?")
 #define STR_PDFUA_IGNORE                        NC_("STR_PDFUA_IGNORE", "Continue")
-#define STR_PDFUA_INVESTIGATE                   NNC_("STR_PDFUA_INVESTIGATE", "Investigate issue", "Investigate issues")
+#define STR_PDFUA_INVESTIGATE                   NNC_("STR_PDFUA_INVESTIGATE", "Investigate error", "Investigate errors")
 
 // Progress bar status indicator when importing or exporting
 #define STR_FILTER_DOC_LOADING                  NC_("STR_FILTER_DOC_LOADING", "Loading: ")

filter/source/pdf/impdialog.cxx
@@ -279,7 +279,8 @@ IMPL_LINK_NOARG(ImpPDFTabDialog, OkHdl, weld::Button&, void)
             const int nIssueCount = std::count_if(aIssues.begin(), aIssues.end(),
                 [](const std::shared_ptr<sfx::AccessibilityIssue >& pIssues)
                 {
-                    return !pIssues->getHidden();
+                    return !pIssues->getHidden() &&
+                        pIssues->m_eIssueLvl == sfx::AccessibilityIssueLevel::ERRORLEV;
                 });
 
             if (nIssueCount)

include/sfx2/AccessibilityIssue.hxx
@@ -54,10 +54,17 @@ enum class AccessibilityIssueID
     FONTWORKS,
 };
 
+enum class AccessibilityIssueLevel : size_t
+{
+    ERRORLEV = 0,
+    WARNLEV = 1,
+    LAST = WARNLEV
+};
+
 class SFX2_DLLPUBLIC AccessibilityIssue
 {
 public:
-    AccessibilityIssue(AccessibilityIssueID eIssueID);
+    AccessibilityIssue(AccessibilityIssueID eIssueID, AccessibilityIssueLevel eIssueLvl);
     AccessibilityIssue(AccessibilityIssue const&) = default;
     virtual ~AccessibilityIssue();
 
@@ -75,6 +82,7 @@ public:
     AccessibilityIssue& operator=(const AccessibilityIssue&) = default;
 
     AccessibilityIssueID m_eIssueID;
+    AccessibilityIssueLevel m_eIssueLvl;
     OUString m_aIssueText;
     bool m_bHidden;
 

sfx2/source/accessibility/AccessibilityIssue.cxx
@@ -12,8 +12,10 @@
 
 namespace sfx
 {
-AccessibilityIssue::AccessibilityIssue(AccessibilityIssueID eIssueID)
+AccessibilityIssue::AccessibilityIssue(AccessibilityIssueID eIssueID,
+                                       AccessibilityIssueLevel eIssueLvl)
     : m_eIssueID(eIssueID)
+    , m_eIssueLvl(eIssueLvl)
     , m_bHidden(false)
     , m_pParent(nullptr)
 {

static/CustomTarget_emscripten_fs_image.mk
@@ -1199,6 +1199,7 @@ gb_emscripten_fs_image_files += \
     $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/absrecbox.ui \
     $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/acceptrejectchangesdialog.ui \
     $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/accessibilitycheckentry.ui \
+    $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/accessibilitychecklevel.ui \
     $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/addconditiondialog.ui \
     $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/adddataitemdialog.ui \
     $(INSTROOT)/$(LIBO_SHARE_FOLDER)/config/soffice.cfg/svx/ui/addinstancedialog.ui \

svx/UIConfig_svx.mk
@@ -13,6 +13,7 @@ $(eval $(call gb_UIConfig_add_uifiles,svx,\
 	svx/uiconfig/ui/absrecbox \
 	svx/uiconfig/ui/acceptrejectchangesdialog \
 	svx/uiconfig/ui/accessibilitycheckentry \
+	svx/uiconfig/ui/accessibilitychecklevel \
 	svx/uiconfig/ui/addconditiondialog \
 	svx/uiconfig/ui/adddataitemdialog \
 	svx/uiconfig/ui/addinstancedialog \

svx/uiconfig/ui/accessibilitychecklevel.ui
@@ -1,0 +1,322 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Generated with glade 3.38.2 -->
+<interface domain="svx">
+  <requires lib="gtk+" version="3.20"/>
+  <object class="GtkBox" id="accessibilityCheckLevelBox">
+    <property name="visible">True</property>
+    <property name="can-focus">False</property>
+    <property name="orientation">vertical</property>
+    <property name="spacing">6</property>
+    <property name="baseline-position">top</property>
+    <child>
+      <object class="GtkExpander" id="expand_document">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_document">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_document_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_document_lb">Document</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">0</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_styles">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_styles">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_styles_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_styles_lb">Styles</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">1</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_linked">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_linked">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="hexpand">True</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_linked_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_linked_lb">Missing linked graphic</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">2</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_no_alt">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_no_alt">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="hexpand">True</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_no_alt_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_no_alt_lb">Missing alternative or description text</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">3</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_table">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_table">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_table_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_table_lb">Table</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">4</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_formatting">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_formatting">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <object class="GtkExpander" id="expand_direct_formatting">
+                <property name="can-focus">True</property>
+                <child>
+                  <object class="GtkBox" id="box_direct_formatting">
+                    <property name="visible">True</property>
+                    <property name="can-focus">False</property>
+                    <property name="margin-start">6</property>
+                    <property name="orientation">vertical</property>
+                    <child>
+                      <placeholder/>
+                    </child>
+                  </object>
+                </child>
+                <child type="label">
+                  <object class="GtkLabel" id="expand_direct_formatting_lb">
+                    <property name="visible">True</property>
+                    <property name="can-focus">False</property>
+                    <property name="label" context="accessibilitychecklevel|expand_direct_formatting_lb">Direct Formatting</property>
+                  </object>
+                </child>
+              </object>
+              <packing>
+                <property name="expand">False</property>
+                <property name="fill">True</property>
+                <property name="position">0</property>
+              </packing>
+            </child>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_formatting_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_formatting_lb">Formatting</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">5</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_hyperlink">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_hyperlink">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_hyperlink_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_hyperlink_lb">Hyperlink</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">7</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_fakes">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_fakes">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_fakes_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_fakes_lb">Simulated captions</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">8</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_numbering">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_numbering">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_numbering_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" translatable="yes" context="accessibilitychecklevel|expand_numbering_lb">Numbering</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">9</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkExpander" id="expand_other">
+        <property name="can-focus">True</property>
+        <child>
+          <object class="GtkBox" id="box_other">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="margin-start">6</property>
+            <property name="orientation">vertical</property>
+            <child>
+              <placeholder/>
+            </child>
+          </object>
+        </child>
+        <child type="label">
+          <object class="GtkLabel" id="expand_other_lb">
+            <property name="visible">True</property>
+            <property name="can-focus">False</property>
+            <property name="label" context="accessibilitychecklevel|expand_other_lb">Other</property>
+          </object>
+        </child>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="fill">True</property>
+        <property name="position">10</property>
+      </packing>
+    </child>
+  </object>
+</interface>

commit 1699d29fca758944a5e477b05e61b5f42cc8907c

tdf#164137 - Fix Accessibility sidebar gives no warnings
We need to check rather the update button is visible instead of the
expanders.

Regression from commit: 13ac356a32f279a3fd542d62c12e58abe3d3ad30
(tdf#164048 sw a11y: improve error/warning levels with...)

Change-Id: I63fb5906ad2f7bb13bc5822c284861a419897c4e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177723
Tested-by: Jenkins
Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

sw/source/uibase/sidebar/A11yCheckIssuesPanel.cxx
@@ -307,7 +307,7 @@ void A11yCheckIssuesPanel::addEntryForGroup(AccessibilityCheckGroups eGroup,
 
 void A11yCheckIssuesPanel::populateIssues()
 {
-    if (!mpDoc || !(m_xLevelExpanders[0]->is_visible() || m_xLevelExpanders[1]->is_visible()))
+    if (!mpDoc || mxUpdateBox->is_visible())
         return;
 
     SfxViewShell* pViewShell = SfxViewShell::Current();