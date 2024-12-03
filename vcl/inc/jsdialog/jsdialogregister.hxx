13af671fda1317175a93fd747e5455ce897ed167
Szymon Kłos <szymon.klos@collabora.com>
2024-12-03 13:25:04 +0100

    jsdialog: share code for widget registry
    - move all helpers for widgets registration into
      separate file
    - share code for Widgets, Menu and Popup register
    
    Change-Id: I3449721d7e436ff4459edb7990c1af860b78054f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177738
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

---
+++ vcl/inc/jsdialog/jsdialogregister.hxx
@@ -0,0 +1,52 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <map>
+#include <rtl/ustring.hxx>
+
+namespace jsdialog
+{
+template <class T> class WidgetRegister
+{
+    // Map to remember the LOKWindowId <-> widget binding.
+    std::map<OUString, T> m_aWidgetMap;
+    std::map<OUString, T>& Map() { return m_aWidgetMap; }
+
+public:
+    void Remember(const OUString& rId, T pWidget);
+    void Forget(const OUString& rId);
+    T Find(const OUString& rId);
+};
+
+template <class T> void WidgetRegister<T>::Remember(const OUString& nWindowId, T pMenu)
+{
+    Map()[nWindowId] = pMenu;
+}
+
+template <class T> void WidgetRegister<T>::Forget(const OUString& nWindowId)
+{
+    auto it = Map().find(nWindowId);
+    if (it != Map().end())
+        Map().erase(it);
+}
+
+template <class T> T WidgetRegister<T>::Find(const OUString& nWindowId)
+{
+    const auto it = Map().find(nWindowId);
+
+    if (it != Map().end())
+        return it->second;
+
+    return nullptr;
+}
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
