fd21dbeeaab7a0ea4fec6594340d45a4799763fe
Mike Kaganski <mike.kaganski@collabora.com>
2024-12-04 14:25:42 +0500

    Disable subpixel AA in GraphicExporter::filter unconditionally
    ... in D2DWriteTextOutRenderer.
    
    Commit 785a56b6be7f3128c2e7a131381e02525a50eb6b (D2DWriteTextOutRenderer:
    use grayscale AA for file output, 2024-11-27) has disabled it only when
    the export settings explicitly specified a concrete AA setting. In case
    when the settings didn't specify explicitly, if AA should be used or not,
    then system settings were used, which in case of D2DWriteTextOutRenderer
    would still enable ClearType (subpixel AA).
    
    This stores additional flag in StyleSettings, similar to what was done
    in commit e6538f5bdd876911ea30f84a6512c03908e620fd (tdf#118966 vcl: add
    a flag to determine if AA of fonts is used from the system, 2018-07-28),
    that tells the renderer to prevent subpixel AA, even if use of AA itself
    is defined by system settings. This flag is currently only considered by
    D2DWriteTextOutRenderer.
    
    Change-Id: Ibd1879d3c222276eee00c37a442881d6d47c831f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177780
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- include/vcl/settings.hxx
+++ include/vcl/settings.hxx
@@ -440,6 +440,11 @@ public:
     void SetUseFontAAFromSystem(bool bUseFontAAFromSystem);
     bool GetUseFontAAFromSystem() const;
 
+    // Using subpixel anti-aliasing is unwanted in some cases, like when saving to file,
+    // even when the use of anti-aliasing itself is defined by system settings.
+    void SetUseSubpixelAA(bool val);
+    bool GetUseSubpixelAA() const;
+
     void                            SetUseFlatBorders( bool bUseFlatBorders );
     bool                            GetUseFlatBorders() const;
 
