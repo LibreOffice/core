LibreOffice/core

commit 65d52af42f329bbe7c5e7d6ac617320f7faa4760

clang-tidy: performance-unnecessary-copy-initialization in svx
Change-Id: I80bdcae1db64f7693600178d86c122c5875369b2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176440
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/sdrhittesthelper.cxx
@@ -136,7 +136,7 @@ bool ViewObjectContactPrimitiveHit(
             // get primitive sequence
             sdr::contact::DisplayInfo aDisplayInfo;
             // have to make a copy of this container here, because it might be changed underneath us
-            const drawinglayer::primitive2d::Primitive2DContainer aSequence(rVOC.getPrimitive2DSequence(aDisplayInfo));
+            const drawinglayer::primitive2d::Primitive2DContainer& aSequence(rVOC.getPrimitive2DSequence(aDisplayInfo));
 
             if(!aSequence.empty())
             {

slideshow/source/engine/smilfunctionparser.cxx
@@ -535,7 +535,7 @@ namespace slideshow::internal
 
             // static parser context, because the actual
             // Spirit parser is also a static object
-            ParserContextSharedPtr pContext = getParserContext();
+            const ParserContextSharedPtr& pContext = getParserContext();
 
             pContext->maShapeBounds = rRelativeShapeBounds;
             pContext->mbParseAnimationFunction = false; // parse with '$' disabled
@@ -578,7 +578,7 @@ namespace slideshow::internal
 
             // static parser context, because the actual
             // Spirit parser is also a static object
-            ParserContextSharedPtr pContext = getParserContext();
+            const ParserContextSharedPtr& pContext = getParserContext();
 
             pContext->maShapeBounds = rRelativeShapeBounds;
             pContext->mbParseAnimationFunction = true; // parse with '$' enabled

svtools/source/misc/embedhlp.cxx
@@ -853,7 +853,7 @@ void EmbeddedObjectRef::SetGraphicToContainer( const Graphic& rGraphic,
     SvMemoryStream aStream;
     aStream.SetVersion( SOFFICE_FILEFORMAT_CURRENT );
 
-    auto pGfxLink = rGraphic.GetSharedGfxLink();
+    const auto& pGfxLink = rGraphic.GetSharedGfxLink();
     if (pGfxLink && pGfxLink->IsNative())
     {
         if (pGfxLink->ExportNative(aStream))

svx/source/core/graphichelper.cxx
@@ -125,7 +125,7 @@ void GraphicHelper::GetPreferredExtension( OUString& rExtension, const Graphic& 
 
 OUString GraphicHelper::GetImageType(const Graphic& rGraphic)
 {
-    auto pGfxLink = rGraphic.GetSharedGfxLink();
+    const auto& pGfxLink = rGraphic.GetSharedGfxLink();
     if (pGfxLink)
     {
         auto iter = constGfxTypeToString.find(pGfxLink->GetType());

svx/source/customshapes/EnhancedCustomShapeFunctionParser.cxx
@@ -1137,7 +1137,7 @@ std::shared_ptr<ExpressionNode> const & FunctionParser::parseFunction( std::u16s
 
     // static parser context, because the actual
     // Spirit parser is also a static object
-    ParserContextSharedPtr pContext = getParserContext();
+    const ParserContextSharedPtr& pContext = getParserContext();
     pContext->mpCustoShape = &rCustoShape;
 
     ExpressionGrammar aExpressionGrammer( pContext );

svx/source/dialog/framelinkarray.cxx
@@ -1277,7 +1277,7 @@ static void HelperClipLine(
 
     if(aResultPP.count() > 0)
     {
-        const basegfx::B2DPolygon aResultP(aResultPP.getB2DPolygon(0));
+        const basegfx::B2DPolygon& aResultP(aResultPP.getB2DPolygon(0));
 
         if(aResultP.count() > 0)
         {

svx/source/dialog/grfflt.cxx
@@ -41,7 +41,7 @@ void SvxGraphicFilter::ExecuteGrfFilterSlot( SfxRequest const & rReq,
                         const GraphicObject& rInputObject,
                         const std::function<void(const Graphic&)>& f)
 {
-    Graphic aInputGraphic = rInputObject.GetGraphic();
+    const Graphic& aInputGraphic = rInputObject.GetGraphic();
 
     if( aInputGraphic.GetType() != GraphicType::Bitmap )
         return;

svx/source/fmcomp/gridcell.cxx
@@ -527,7 +527,7 @@ DbCellControl::DbCellControl( DbGridColumn& _rColumn )
     ,m_pPainter( nullptr )
     ,m_pWindow( nullptr )
 {
-    Reference< XPropertySet > xColModelProps = _rColumn.getModel();
+    const Reference< XPropertySet >& xColModelProps = _rColumn.getModel();
     if ( !xColModelProps.is() )
         return;
 

commit a60617b1f2f1eca31177867e575bed57b1b448a3

tdf#163221 crash in dialog editor
regression from
commit 65d52af42f329bbe7c5e7d6ac617320f7faa4760
clang-tidy: performance-unnecessary-copy-initialization in svx

Change-Id: I48428fe08acdf9590a533c7ea91abe905713b292
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177979
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/sdrhittesthelper.cxx
@@ -136,7 +136,7 @@ bool ViewObjectContactPrimitiveHit(
             // get primitive sequence
             sdr::contact::DisplayInfo aDisplayInfo;
             // have to make a copy of this container here, because it might be changed underneath us
-            const drawinglayer::primitive2d::Primitive2DContainer& aSequence(rVOC.getPrimitive2DSequence(aDisplayInfo));
+            const drawinglayer::primitive2d::Primitive2DContainer aSequence(rVOC.getPrimitive2DSequence(aDisplayInfo));
 
             if(!aSequence.empty())
             {