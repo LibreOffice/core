LibreOffice/core

commit ca435be45f316120b9df6c9d547b781ed975817d

writer: handle styles in multiple languages for online
Use translated name when style is applied and broadcasted.
Avoid using style names created by the first view.

Change-Id: I18abf3388e69f76ec99eda94e0b67782a52ab23f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101831
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

sw/source/core/doc/DocumentStylePoolManager.cxx
@@ -69,6 +69,7 @@
 #include <com/sun/star/text/HoriOrientation.hpp>
 #include <unotools/syslocale.hxx>
 #include <i18nlangtag/languagetag.hxx>
+#include <comphelper/lok.hxx>
 
 using namespace ::editeng;
 using namespace ::com::sun::star;
@@ -583,10 +584,20 @@ SwTextFormatColl* DocumentStylePoolManager::GetTextCollFromPool( sal_uInt16 nId,
 
     SwTextFormatColl* pNewColl;
     sal_uInt16 nOutLvlBits = 0;
+
     for (size_t n = 0, nSize = m_rDoc.GetTextFormatColls()->size(); n < nSize; ++n)
     {
         if( nId == ( pNewColl = (*m_rDoc.GetTextFormatColls())[ n ] )->GetPoolFormatId() )
         {
+            // in online we can have multiple languages, use translated name
+            if (comphelper::LibreOfficeKit::isActive())
+            {
+                OUString aName;
+                SwStyleNameMapper::GetUIName(nId, aName);
+                if (!aName.isEmpty())
+                    pNewColl->SetName(aName);
+            }
+
             return pNewColl;
         }
 

sw/source/core/edit/edfcol.cxx
@@ -100,6 +100,8 @@
 #include <com/sun/star/beans/PropertyAttribute.hpp>
 #include <tools/diagnose_ex.h>
 #include <IDocumentRedlineAccess.hxx>
+#include <SwStyleNameMapper.hxx>
+#include <comphelper/lok.hxx>
 
 #define WATERMARK_NAME "PowerPlusWaterMarkObject"
 #define WATERMARK_AUTO_SIZE sal_uInt32(1)
@@ -2196,6 +2198,17 @@ void SwEditShell::SetTextFormatColl(SwTextFormatColl *pFormat,
     RedlineFlags eRedlMode = GetDoc()->getIDocumentRedlineAccess().GetRedlineFlags(), eOldMode = eRedlMode;
 
     SwRewriter aRewriter;
+
+    // in online we can have multiple languages, use universal name then
+    if (comphelper::LibreOfficeKit::isActive())
+    {
+        OUString aName;
+        sal_uInt16 nId = SwStyleNameMapper::GetPoolIdFromUIName(pLocal->GetName(), SwGetPoolIdFromName::TxtColl);
+        SwStyleNameMapper::FillProgName(nId, aName);
+        if (!aName.isEmpty())
+            pLocal->SetName(aName);
+    }
+
     aRewriter.AddRule(UndoArg1, pLocal->GetName());
 
     GetDoc()->GetIDocumentUndoRedo().StartUndo(SwUndoId::SETFMTCOLL, &aRewriter);

commit a33b6d57d04bc011cd0a8d44023efe1639915b09

fix bug in use of SwStyleNameMapper::GetUIName
introduced in
ca435be45f316120b9df6c9d547b781ed975817d
"writer: handle styles in multiple languages for online"

and sprinkle some [[nodiscard]] around to prevent future such
issues

Change-Id: Ie12a7966a4de93c9ad05fef01fd2ff606f9dde6e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178087
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sw/inc/SwStyleNameMapper.hxx
@@ -87,11 +87,13 @@ class SwStyleNameMapper final
 
 public:
     // This gets the UI Name from the programmatic name
+    SAL_WARN_UNUSED_RESULT
     static const OUString& GetUIName(const OUString& rName, SwGetPoolIdFromName);
     static         void FillUIName(const OUString& rName, OUString& rFillName,
                             SwGetPoolIdFromName);
 
     // Get the programmatic Name from the UI name
+    SAL_WARN_UNUSED_RESULT
     static const OUString& GetProgName(const OUString& rName,
                                        SwGetPoolIdFromName);
     static         void FillProgName(const OUString& rName, OUString& rFillName,
@@ -99,19 +101,23 @@ public:
 
     // This gets the UI Name from the Pool ID
     SW_DLLPUBLIC static void FillUIName(sal_uInt16 nId, OUString& rFillName);
+    SAL_WARN_UNUSED_RESULT
     SW_DLLPUBLIC static const OUString& GetUIName(sal_uInt16 nId,
                                                   const OUString& rName);
 
     // This gets the programmatic Name from the Pool ID
     static         void FillProgName(sal_uInt16 nId, OUString& rFillName);
+    SAL_WARN_UNUSED_RESULT
     SW_DLLPUBLIC static const OUString& GetProgName(sal_uInt16 nId,
                                                     const OUString& rName);
 
     // This gets the PoolId from the UI Name
+    SAL_WARN_UNUSED_RESULT
     SW_DLLPUBLIC static sal_uInt16 GetPoolIdFromUIName(const OUString& rName,
                                                        SwGetPoolIdFromName);
 
     // Get the Pool ID from the programmatic name
+    SAL_WARN_UNUSED_RESULT
     static sal_uInt16 GetPoolIdFromProgName(const OUString& rName,
                                             SwGetPoolIdFromName);
 
@@ -120,8 +126,10 @@ public:
     // RES_POOLCOLL_LABEL_TABLE, RES_POOLCOLL_LABEL_FRAME
     // forth and back.
     // Non-matching names remain unchanged.
+    SAL_WARN_UNUSED_RESULT
     SW_DLLPUBLIC static const OUString & GetSpecialExtraProgName(
                     const OUString& rExtraUIName);
+    SAL_WARN_UNUSED_RESULT
     static const OUString & GetSpecialExtraUIName(const OUString& rExtraProgName);
 
     static const std::vector<OUString>& GetTextUINameArray();

sw/source/core/doc/DocumentStylePoolManager.cxx
@@ -604,8 +604,7 @@ SwTextFormatColl* DocumentStylePoolManager::GetTextCollFromPool( sal_uInt16 nId,
             // in online we can have multiple languages, use translated name
             if (comphelper::LibreOfficeKit::isActive())
             {
-                OUString aName;
-                SwStyleNameMapper::GetUIName(nId, aName);
+                OUString aName = SwStyleNameMapper::GetUIName(nId, OUString());
                 if (!aName.isEmpty())
                     pNewColl->SetFormatName(aName);
             }