44596779cc7704110c9a3340de31727409e5c256
Justin Luth <justin.luth@collabora.com>
2024-12-16 14:02:18 -0500

    tdf#164075 rtfimport: ignore assert developed for DOCX fixing
    I don't exactly remember why I put the assert there,
    but it sounds reasonable to expect that if
    the paragraph defined the numbering
    that it should also have numbering rule content,
    and if it doesn't, then it is worth investigating why.
    
    Investing anything related to RTF is never worthwhile.
    
    Change-Id: I6525d0a40d9535f1888056a610576085941605a3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178607
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Jenkins
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179265
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- writerfilter/source/dmapper/DomainMapper_Impl.cxx
+++ writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -2665,7 +2665,7 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                         return rValue.Name == "NumberingRules";
                     });
 
-                    assert( isNumberingViaRule == (itNumberingRules != aProperties.end()) );
+                    assert(isNumberingViaRule == (itNumberingRules != aProperties.end()) || IsRTFImport());
                     isNumberingViaRule = (itNumberingRules != aProperties.end());
                     if (m_xPreviousParagraph.is() && (isNumberingViaRule || isNumberingViaStyle))
                     {
