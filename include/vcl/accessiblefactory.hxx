486728e582d1932cb0b97a51b8edfacf462ec7f9
Michael Weghorn <m.weghorn@posteo.de>
2024-12-17 12:20:18 +0100

    a11y: Use #pragma once
    Change-Id: I99b22c542c673c5633ebd9fe270e32759eb54457
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178687
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- include/vcl/accessiblefactory.hxx
+++ include/vcl/accessiblefactory.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_VCL_ACCESSIBLEFACTORY_HXX
-#define INCLUDED_VCL_ACCESSIBLEFACTORY_HXX
+#pragma once
 
 #include <vcl/AccessibleBrowseBoxObjType.hxx>
 
@@ -340,7 +339,5 @@ public:
 protected:
     virtual ~AccessibleFactory() override;
 };
-
-#endif // INCLUDED_VCL_ACCESSIBLEFACTORY_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
