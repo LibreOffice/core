LibreOffice/core

commit c8b607b7c0096c58dc5187262bf0133dee728d50

tdf#163883 editeng RTF export: fix broken offsets into the para style table
Regression from commit 70d1bd6ee0eba9d6661cd6280566f77a87f2d068
(tdf#161652 editeng, RTF copy: only write used paragraph styles,
2024-06-20), type a few characters into the title placeholder shape in
Impress, switch to the Outline view, select all, copy, paste into
Writer: the resulting text nodes won't have paragraph styles, just
direct formatting.

Inspecting the produced RTF, the problem is that the styles table
started to omit unused styles, but the style -> offset mapping table
still included them, so the offset didn't match, which results in losing
the style on RTF import.

Fix the problem by building the mapping table later also also ignoring
unused paragraph styles there, to restore consistency between declaring
style entries and referring to them.

Change-Id: I9156a881543cc710eb12990f110ceefc77a1858b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176747
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

editeng/source/editeng/impedit4.cxx
@@ -444,17 +444,6 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel, bool bCl
     // StyleSheets...
     if ( GetStyleSheetPool() )
     {
-        std::shared_ptr<SfxStyleSheetIterator> aSSSIterator = std::make_shared<SfxStyleSheetIterator>(GetStyleSheetPool(),
-                SfxStyleFamily::All);
-        // fill aStyleSheetToIdMap
-        sal_uInt32 nId = 1;
-        for ( SfxStyleSheetBase* pStyle = aSSSIterator->First(); pStyle;
-                                 pStyle = aSSSIterator->Next() )
-        {
-            aStyleSheetToIdMap[pStyle] = nId;
-            nId++;
-        }
-
         // Collect used paragraph styles when copying to the clipboard.
         std::set<SfxStyleSheetBase*> aUsedParagraphStyles;
         if (bClipboard)
@@ -497,6 +486,22 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel, bool bCl
                     }
                 }
             }
+        }
+
+        std::shared_ptr<SfxStyleSheetIterator> aSSSIterator = std::make_shared<SfxStyleSheetIterator>(GetStyleSheetPool(),
+                SfxStyleFamily::All);
+        // fill aStyleSheetToIdMap
+        sal_uInt32 nId = 1;
+        for ( SfxStyleSheetBase* pStyle = aSSSIterator->First(); pStyle;
+                                 pStyle = aSSSIterator->Next() )
+        {
+            if (bClipboard && !aUsedParagraphStyles.contains(pStyle))
+            {
+                // Don't include unused paragraph styles in the clipboard case.
+                continue;
+            }
+            aStyleSheetToIdMap[pStyle] = nId;
+            nId++;
         }
 
         if ( aSSSIterator->Count() )

editeng/qa/editeng/editeng.cxx
@@ -9,8 +9,6 @@
 
 #include <test/bootstrapfixture.hxx>
 
-#include <memory>
-
 #include <editeng/editeng.hxx>
 #include <sfx2/app.hxx>
 #include <svtools/parrtf.hxx>
@@ -54,6 +52,7 @@ public:
     void NextToken(int nToken) override;
 
     int m_nStyles = 0;
+    std::vector<int> m_aStyleValues;
 };
 
 StyleCounter::StyleCounter(SvStream& rStream)
@@ -66,6 +65,7 @@ void StyleCounter::NextToken(int nToken)
     if (nToken == RTF_S)
     {
         ++m_nStyles;
+        m_aStyleValues.push_back(nTokenValue);
     }
 }
 
@@ -93,6 +93,36 @@ CPPUNIT_TEST_FIXTURE(Test, testRTFStyleExport)
     // - Actual  : 1
     // i.e. unreferenced paragraph styles were exported.
     CPPUNIT_ASSERT_EQUAL(0, xReader->m_nStyles);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testRTFStyleExportReferToStyle)
+{
+    // Given a document with one unused and one used style:
+    EditEngine aEditEngine(mpItemPool.get());
+    rtl::Reference<SfxStyleSheetPool> xStyles(new SfxStyleSheetPool(*mpItemPool));
+    xStyles->Make("mystyle", SfxStyleFamily::Para);
+    xStyles->Make("mystyle2", SfxStyleFamily::Para);
+    auto pStyle = static_cast<SfxStyleSheet*>(xStyles->Find("mystyle2", SfxStyleFamily::Para));
+    aEditEngine.SetStyleSheetPool(xStyles.get());
+    OUString aText = u"mytest"_ustr;
+    aEditEngine.SetText(aText);
+    aEditEngine.SetStyleSheet(0, pStyle);
+
+    // When copying a word from that document:
+    uno::Reference<datatransfer::XTransferable> xData
+        = aEditEngine.CreateTransferable(ESelection(0, 0, 0, aText.getLength()));
+
+    // Then make sure the declared and referred style indexes for the used style match:
+    auto pData = dynamic_cast<EditDataObject*>(xData.get());
+    SvMemoryStream& rStream = pData->GetRTFStream();
+    tools::SvRef<StyleCounter> xReader(new StyleCounter(rStream));
+    CPPUNIT_ASSERT(xReader->CallParser() != SvParserState::Error);
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(2), xReader->m_aStyleValues.size());
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1
+    // - Actual  : 2
+    // i.e. \s2 was used to refer to \s1, so the paragraph style was lost.
+    CPPUNIT_ASSERT_EQUAL(xReader->m_aStyleValues[0], xReader->m_aStyleValues[1]);
 }
 }
 

commit 4eac1d004898a2063b21a83a7e5785e22c50ba8a

tdf#164359 editeng RTF export: track unused parent styles recursively
Start edit mode for the shape in the document, double-click on a word
which has a "level 2" bullet associated with it -> crash.

This is a regression from c8b607b7c0096c58dc5187262bf0133dee728d50
(tdf#163883 editeng RTF export: fix broken offsets into the para style
table, 2024-11-19), the direct cause is that ImpEditEngine::WriteRTF()
maps all style pointers to a style ID and once we write the parent
property in the style table, we unconditionally dereference our search
result, because we know all styles should be in that map.

The root of the problem seems to be that once parent styles are marked
as used for the "only write used styles for the clipboard" case, those
parents themselves can have parents, and we didn't mark those as used.
Combined with the unconditional dereference, this leads to a crash. Fix
this by walking up the parent chain till we get no parent.

This assumes that importers create a valid document model, so there
can't be loops while walking the parent chain. If that would be a
problem in practice, we could check for that in the future.

Change-Id: I4b416e78f26bf455040d95ee8ed220cfe870e33a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178677
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

editeng/source/editeng/impedit4.cxx
@@ -443,15 +443,19 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel, bool bCl
 
                 aUsedParagraphStyles.insert(pParaStyle);
 
-                const OUString& rParent = pParaStyle->GetParent();
-                if (!rParent.isEmpty())
+                // Collect parents of the style recursively.
+                OUString aParent = pParaStyle->GetParent();
+                while (!aParent.isEmpty())
                 {
                     auto pParent = static_cast<SfxStyleSheet*>(
-                        GetStyleSheetPool()->Find(rParent, pParaStyle->GetFamily()));
-                    if (pParent)
+                        GetStyleSheetPool()->Find(aParent, pParaStyle->GetFamily()));
+                    if (!pParent)
                     {
-                        aUsedParagraphStyles.insert(pParent);
+                        break;
                     }
+
+                    aUsedParagraphStyles.insert(pParent);
+                    aParent = pParent->GetParent();
                 }
 
                 const OUString& rFollow = pParaStyle->GetFollow();

editeng/qa/editeng/editeng.cxx
@@ -128,6 +128,38 @@ CPPUNIT_TEST_FIXTURE(Test, testRTFStyleExportReferToStyle)
     // i.e. \s2 was used to refer to \s1, so the paragraph style was lost.
     CPPUNIT_ASSERT_EQUAL(xReader->m_aStyleValues[0], xReader->m_aStyleValues[1]);
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testRTFStyleExportParentRecursive)
+{
+    // Given a document with text that has a paragraph style with a parent that itself has a parent:
+    EditEngine aEditEngine(mpItemPool.get());
+    rtl::Reference<SfxStyleSheetPool> xStyles(new SfxStyleSheetPool(*mpItemPool));
+    xStyles->Make("mystyle1", SfxStyleFamily::Para);
+    xStyles->Make("mystyle2", SfxStyleFamily::Para);
+    xStyles->Make("mystyle3", SfxStyleFamily::Para);
+    auto pStyle1 = static_cast<SfxStyleSheet*>(xStyles->Find("mystyle1", SfxStyleFamily::Para));
+    auto pStyle2 = static_cast<SfxStyleSheet*>(xStyles->Find("mystyle2", SfxStyleFamily::Para));
+    pStyle2->SetParent(pStyle1->GetName());
+    auto pStyle3 = static_cast<SfxStyleSheet*>(xStyles->Find("mystyle3", SfxStyleFamily::Para));
+    pStyle3->SetParent(pStyle2->GetName());
+    pStyle3->GetItemSet().SetRanges(svl::Items<WEIGHT_BOLD, EE_CHAR_WEIGHT>);
+    SvxWeightItem aItem(WEIGHT_BOLD, EE_CHAR_WEIGHT);
+    pStyle3->GetItemSet().Put(aItem);
+    aEditEngine.SetStyleSheetPool(xStyles.get());
+    OUString aText = u"mytest"_ustr;
+    aEditEngine.SetText(aText);
+    aEditEngine.SetStyleSheet(0, pStyle3);
+
+    // When copying to the clipboard as RTF:
+    // Without the accompanying fix in place, this test would have crashed here:
+    uno::Reference<datatransfer::XTransferable> xData
+        = aEditEngine.CreateTransferable(ESelection(0, 0, 0, aText.getLength()));
+
+    // Then make sure we produce RTF and not crash:
+    auto pData = dynamic_cast<EditDataObject*>(xData.get());
+    SvMemoryStream& rStream = pData->GetRTFStream();
+    CPPUNIT_ASSERT_GREATER(static_cast<sal_uInt64>(0), rStream.remainingSize());
+}
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();