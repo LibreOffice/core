538cb629d0fc01ca9efb82d9ae86af4f32dc5081
Andrea Gelmini <andrea.gelmini@gelma.net>
2024-12-18 21:55:06 +0100

    Fix typos
    Change-Id: Ifd7be9bdd784f042b27dbdd28d3e4229a41344d1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178747
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
    Tested-by: Jenkins

--- cui/source/options/appearance.cxx
+++ cui/source/options/appearance.cxx
@@ -410,8 +410,8 @@ IMPL_LINK_NOARG(SvxAppearanceTabPage, ColorValueChgHdl, ColorListBox&, void)
     else
         aCurrentEntryColor.nLightColor = m_xColorChangeBtn->GetSelectEntryColor();
 
-    // use nColor for caching the value of color in use. This avoids tidious refactoring which IMO
-    // would use function calls to discriminate between colors. those functions themself call some virtual functions
+    // use nColor for caching the value of color in use. This avoids tedious refactoring which IMO
+    // would use function calls to discriminate between colors. Those functions themself call some virtual functions
     // making the whole thing super slow (comparatively).
     aCurrentEntryColor.nColor = m_xColorChangeBtn->GetSelectEntryColor();
 
