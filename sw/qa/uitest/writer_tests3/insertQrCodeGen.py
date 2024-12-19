36047855334fb7bf988386eb7cdc140a31617c0c
Miklos Vajna <vmiklos@collabora.com>
2024-12-19 15:51:53 +0100

    UITest_writer_tests3: fix indent in insertQrCode
    The indent is 4 spaces, and it hurts readability to violate that, given
    that this is Python, so no curly braces.
    
    Change-Id: I45aa85c23ab3468acbc6a6d7a37f7db19cc94836
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178814
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/qa/uitest/writer_tests3/insertQrCodeGen.py
+++ sw/qa/uitest/writer_tests3/insertQrCodeGen.py
@@ -13,27 +13,27 @@ from uitest.uihelper.common import type_text
 
 class insertQrCode(UITestCase):
 
-   def test_insert_qr_code(self):
-      if os.getenv('ENABLE_ZXING') == 'TRUE':
+    def test_insert_qr_code(self):
+        if os.getenv('ENABLE_ZXING') == 'TRUE':
 
-        with self.ui_test.create_doc_in_start_center("writer") as document:
+            with self.ui_test.create_doc_in_start_center("writer") as document:
 
-            with self.ui_test.execute_dialog_through_command(".uno:InsertQrCode") as xDialog:
+                with self.ui_test.execute_dialog_through_command(".uno:InsertQrCode") as xDialog:
 
-                # Get elements in the Dialog Box
-                xURL = xDialog.getChild("edit_text")
-                xECC_Low = xDialog.getChild("button_low") #How radio button input is written in text.
-                xBorder = xDialog.getChild("edit_margin")
+                    # Get elements in the Dialog Box
+                    xURL = xDialog.getChild("edit_text")
+                    xECC_Low = xDialog.getChild("button_low") #How radio button input is written in text.
+                    xBorder = xDialog.getChild("edit_margin")
 
-                type_text(xURL, "www.libreoffice.org") #set the QR code
-                xECC_Low.executeAction("CLICK", tuple())
-                xBorder.executeAction("UP", tuple())
-                xBorder.executeAction("DOWN", tuple())
+                    type_text(xURL, "www.libreoffice.org") #set the QR code
+                    xECC_Low.executeAction("CLICK", tuple())
+                    xBorder.executeAction("UP", tuple())
+                    xBorder.executeAction("DOWN", tuple())
 
-            # check the QR code in the document
-            element = document.DrawPage[0]
-            self.assertEqual(element.BarCodeProperties.Payload, "www.libreoffice.org")
-            self.assertEqual(element.BarCodeProperties.ErrorCorrection, 1)
-            self.assertEqual(element.BarCodeProperties.Border, 1)
+                # check the QR code in the document
+                element = document.DrawPage[0]
+                self.assertEqual(element.BarCodeProperties.Payload, "www.libreoffice.org")
+                self.assertEqual(element.BarCodeProperties.ErrorCorrection, 1)
+                self.assertEqual(element.BarCodeProperties.Border, 1)
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:
