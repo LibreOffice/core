LibreOffice/core

commit eea523b686b27b4807ab21b62805ce9fbddf74ef

tdf#139809 sc ooxml export autofiltered date columns
Export XML_dateGroupItem, XML_year, XML_month, XML_day,
XML_dateTimeGrouping based on the OOXML standard.

Follow up commit of: d101a6f8579df8241dcd8100908868cd7d7c3f8a
(tdf#116818 sc OOXML: import autofiltered date columns)

Note: DOCX round-trip is not lossless here, because
instead of exporting the original yearly and monthly
dataGroupItem filtering, the export contains daily level
dataGroupItem elements for each day.

Change-Id: I456b2240213839725e2b416622986ee4a9cf60d5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/109699
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sc/source/filter/excel/excrecds.cxx
@@ -24,6 +24,7 @@
 
 #include <svl/zforlist.hxx>
 #include <sal/log.hxx>
+#include <sax/fastattribs.hxx>
 
 #include <string.h>
 
@@ -781,7 +782,7 @@ void XclExpAutofilter::AddMultiValueEntry( const ScQueryEntry& rEntry )
     meType = MultiValue;
     const ScQueryEntry::QueryItemsType& rItems = rEntry.GetQueryItems();
     for (const auto& rItem : rItems)
-        maMultiValues.push_back(rItem.maString.getString());
+        maMultiValues.push_back( std::make_pair(rItem.maString.getString(), rItem.meType == ScQueryEntry::ByDate) );
 }
 
 void XclExpAutofilter::WriteBody( XclExpStream& rStrm )
@@ -839,9 +840,31 @@ void XclExpAutofilter::SaveXml( XclExpXmlStream& rStrm )
             rWorksheet->startElement(XML_filters);
             for (const auto& rMultiValue : maMultiValues)
             {
-                OString aStr = OUStringToOString(rMultiValue, RTL_TEXTENCODING_UTF8);
-                const char* pz = aStr.getStr();
-                rWorksheet->singleElement(XML_filter, XML_val, pz);
+                OString aStr = OUStringToOString(rMultiValue.first, RTL_TEXTENCODING_UTF8);
+                if( !rMultiValue.second )
+                {
+                    const char* pz = aStr.getStr();
+                    rWorksheet->singleElement(XML_filter, XML_val, pz);
+                }
+                else
+                {
+                    sax_fastparser::FastAttributeList* pAttrList = sax_fastparser::FastSerializerHelper::createAttrList();
+                    sal_Int32 aDateGroup[3] = { XML_year, XML_month, XML_day };
+                    sal_Int32 idx = 0;
+                    for (size_t i = 0; idx >= 0 && i < 3; i++)
+                    {
+                        OString kw = aStr.getToken(0, '-', idx);
+                        kw = kw.trim();
+                        if (!kw.isEmpty())
+                        {
+                            pAttrList->add(aDateGroup[i], kw);
+                        }
+                    }
+                    // TODO: date filter can only handle YYYY-MM-DD date formats, so XML_dateTimeGrouping value
+                    // will be "day" as default, until date filter cannot handle HH:MM:SS.
+                    pAttrList->add(XML_dateTimeGrouping, "day");
+                    rWorksheet->singleElement(XML_dateGroupItem, pAttrList);
+                }
             }
             rWorksheet->endElement(XML_filters);
         }

sc/source/filter/inc/excrecds.hxx
@@ -366,7 +366,7 @@ private:
     sal_uInt16              nCol;
     sal_uInt16              nFlags;
     ExcFilterCondition      aCond[ 2 ];
-    std::vector<OUString> maMultiValues;
+    std::vector<std::pair<OUString, bool>> maMultiValues; // first->values, second->bDateFormat
 
     bool                    AddCondition( ScQueryConnect eConn, sal_uInt8 nType,
                                 sal_uInt8 nOp, double fVal, const OUString* pText,

sc/qa/unit/subsequent_export-test.cxx
@@ -211,6 +211,7 @@ public:
     void testTdf95640_ods_to_xlsx();
     void testTdf95640_ods_to_xlsx_with_standard_list();
     void testTdf95640_xlsx_to_xlsx();
+    void testDateAutofilterXLSX();
 
     void testRefStringXLSX();
     void testRefStringConfigXLSX();
@@ -390,6 +391,7 @@ public:
     CPPUNIT_TEST(testTdf95640_ods_to_xlsx);
     CPPUNIT_TEST(testTdf95640_ods_to_xlsx_with_standard_list);
     CPPUNIT_TEST(testTdf95640_xlsx_to_xlsx);
+    CPPUNIT_TEST(testDateAutofilterXLSX);
 
     CPPUNIT_TEST(testRefStringXLSX);
     CPPUNIT_TEST(testRefStringConfigXLSX);
@@ -4442,6 +4444,29 @@ void ScExportTest::testTdf95640_xlsx_to_xlsx()
 
     assertXPath(pDoc, "//x:worksheet/x:autoFilter/x:sortState/x:sortCondition", "customList",
                 "Low,Medium,High");
+}
+
+void ScExportTest::testDateAutofilterXLSX()
+{
+    // XLSX Roundtripping autofilter with date list
+    ScDocShellRef xDocSh = loadDoc(u"dateAutofilter.", FORMAT_XLSX);
+    CPPUNIT_ASSERT(xDocSh.is());
+
+    xmlDocUniquePtr pDoc = XPathHelper::parseExport2(*this, *xDocSh, m_xSFactory, "xl/worksheets/sheet1.xml", FORMAT_XLSX);
+    CPPUNIT_ASSERT(pDoc);
+
+    assertXPath(pDoc, "//x:autoFilter", "ref", "A1:B4");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[1]", "day", "02");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[1]", "month", "03");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[1]", "year", "2017");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[1]", "dateTimeGrouping", "day");
+
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[2]", "day", "01");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[2]", "month", "10");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[2]", "year", "2014");
+    assertXPath(pDoc, "//x:autoFilter/x:filterColumn/x:filters/x:dateGroupItem[2]", "dateTimeGrouping", "day");
+
+    xDocSh->DoClose();
 }
 
 void ScExportTest::testTdf88657ODS()

sc/source/filter/oox/autofilterbuffer.cxx
@@ -237,6 +237,8 @@ void DiscreteFilter::importAttribs( sal_Int32 nElement, const AttributeList& rAt
         case XLS_TOKEN( dateGroupItem ):
         {
             OUString aDateValue;
+            // it is just a fallback, we do not need the XML_day as default value,
+            // because if the dateGroupItem exists also XML_dateTimeGrouping exists!
             sal_uInt16 nToken = rAttribs.getToken(XML_dateTimeGrouping, XML_day);
             if( nToken == XML_year || nToken == XML_month || nToken == XML_day )
             {

sc/source/ui/unoobj/datauno.cxx
@@ -1134,6 +1134,7 @@ void fillQueryParam(
                     aItem.maString = rPool.intern(aStr);
                 }
 
+                // filter all dates starting with the given date filter YYYY or YYYY-MM
                 if( aItem.meType == ScQueryEntry::ByDate && aItem.maString.getLength() < 10 )
                 {
                     ScFilterEntries aFilterEntries;

commit 8b492a1f3057d2b6c3daed7405000d0270ea3dd9

tdf#164417 ooxml export: fix autofilter wrong attribute order
According to ooxml standard, x:filter attributes are always before
x:dateGroupItem.

Regression from: eea523b686b27b4807ab21b62805ce9fbddf74ef

Change-Id: Ibfcbba8ba88162203ab662312347fb49cc836083
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179440
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>
Tested-by: Jenkins

sc/source/filter/excel/excrecds.cxx
@@ -790,8 +790,10 @@ void XclExpAutofilter::AddMultiValueEntry( const ScQueryEntry& rEntry )
     {
         if( rItem.maString.isEmpty() )
             bHasBlankValue = true;
+        else if (rItem.meType == ScQueryEntry::ByDate)
+            maDateValues.push_back(rItem.maString.getString());
         else
-            maMultiValues.push_back(std::make_pair(rItem.maString.getString(), rItem.meType == ScQueryEntry::ByDate));
+            maMultiValues.push_back(rItem.maString.getString());
     }
 }
 
@@ -891,32 +893,31 @@ void XclExpAutofilter::SaveXml( XclExpXmlStream& rStrm )
             else
                 rWorksheet->startElement(XML_filters);
 
+            // CT_Filters
             for (const auto& rMultiValue : maMultiValues)
             {
-                if( !rMultiValue.second )
+                rWorksheet->singleElement(XML_filter, XML_val, rMultiValue);
+            }
+            // CT_DateGroupItems
+            for (const auto& rDateValue : maDateValues)
+            {
+                OString aStr = OUStringToOString(rDateValue, RTL_TEXTENCODING_UTF8);
+                rtl::Reference<sax_fastparser::FastAttributeList> pAttrList = sax_fastparser::FastSerializerHelper::createAttrList();
+                sal_Int32 aDateGroup[3] = { XML_year, XML_month, XML_day };
+                sal_Int32 idx = 0;
+                for (size_t i = 0; idx >= 0 && i < 3; i++)
                 {
-                    rWorksheet->singleElement(XML_filter, XML_val, rMultiValue.first);
-                }
-                else
-                {
-                    OString aStr = OUStringToOString(rMultiValue.first, RTL_TEXTENCODING_UTF8);
-                    rtl::Reference<sax_fastparser::FastAttributeList> pAttrList = sax_fastparser::FastSerializerHelper::createAttrList();
-                    sal_Int32 aDateGroup[3] = { XML_year, XML_month, XML_day };
-                    sal_Int32 idx = 0;
-                    for (size_t i = 0; idx >= 0 && i < 3; i++)
+                    OString kw = aStr.getToken(0, '-', idx);
+                    kw = kw.trim();
+                    if (!kw.isEmpty())
                     {
-                        OString kw = aStr.getToken(0, '-', idx);
-                        kw = kw.trim();
-                        if (!kw.isEmpty())
-                        {
-                            pAttrList->add(aDateGroup[i], kw);
-                        }
+                        pAttrList->add(aDateGroup[i], kw);
                     }
-                    // TODO: date filter can only handle YYYY-MM-DD date formats, so XML_dateTimeGrouping value
-                    // will be "day" as default, until date filter cannot handle HH:MM:SS.
-                    pAttrList->add(XML_dateTimeGrouping, "day");
-                    rWorksheet->singleElement(XML_dateGroupItem, pAttrList);
                 }
+                // TODO: date filter can only handle YYYY-MM-DD date formats, so XML_dateTimeGrouping value
+                // will be "day" as default, until date filter cannot handle HH:MM:SS.
+                pAttrList->add(XML_dateTimeGrouping, "day");
+                rWorksheet->singleElement(XML_dateGroupItem, pAttrList);
             }
             rWorksheet->endElement(XML_filters);
         }

sc/source/filter/inc/excrecds.hxx
@@ -374,7 +374,8 @@ private:
     sal_uInt16              nFlags;
     bool                    bHasBlankValue;
     ExcFilterCondition      aCond[ 2 ];
-    std::vector<std::pair<OUString, bool>> maMultiValues; // first->values, second->bDateFormat
+    std::vector<OUString> maMultiValues; // CT_Filter values
+    std::vector<OUString> maDateValues; // CT_DateGroupItem values
     std::vector<std::pair<::Color, bool>> maColorValues; // first->Color, second->bIsBackgroundColor (vs. TextColor)
 
     bool                    AddCondition( ScQueryConnect eConn, sal_uInt8 nType,

sc/qa/unit/subsequent_export_test4.cxx
@@ -2113,6 +2113,21 @@ CPPUNIT_TEST_FIXTURE(ScExportTest4, testSheetProtections)
     verify();
 }
 
+CPPUNIT_TEST_FIXTURE(ScExportTest4, testTdf164417)
+{
+    createScDoc("xlsx/tdf164417.xlsx");
+
+    save(u"Calc Office Open XML"_ustr);
+
+    xmlDocUniquePtr pSheet1 = parseExport(u"xl/worksheets/sheet1.xml"_ustr);
+    CPPUNIT_ASSERT(pSheet1);
+
+    CPPUNIT_ASSERT_EQUAL(
+        0, getXPathPosition(pSheet1, "//x:autoFilter/x:filterColumn/x:filters", "filter"));
+    CPPUNIT_ASSERT_EQUAL(
+        1, getXPathPosition(pSheet1, "//x:autoFilter/x:filterColumn/x:filters", "dateGroupItem"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */