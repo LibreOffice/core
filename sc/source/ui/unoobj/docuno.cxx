e4a0e9ddd7b95f4bdc833b1e682784600e66f376
Gökay Şatır <gokaysatir@collabora.com>
2025-01-07 11:14:10 +0300

    Online Calc: Update view pos after setting the client zoom.
    Issue: If user zooms in/out while editing is active, the edited area is misplaced and tiles are rendering wrong.
    
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Ib9ec8237da52363bffcf586b85a1bd041785c693
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179857
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Marco Cecchetti <marco.cecchetti@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180513

--- sc/source/ui/unoobj/docuno.cxx
+++ sc/source/ui/unoobj/docuno.cxx
@@ -1082,6 +1082,7 @@ void ScModelObj::setClientZoom(int nTilePixelWidth_, int nTilePixelHeight_, int 
         pHdl->SetRefScale(pViewData->GetZoomX(), pViewData->GetZoomY());
 
     // refresh our view's take on other view's cursors & selections
+    pViewData->GetActiveWin()->UpdateEditViewPos();
     pViewData->GetActiveWin()->updateKitOtherCursors();
     pViewData->GetActiveWin()->updateOtherKitSelections();
 
