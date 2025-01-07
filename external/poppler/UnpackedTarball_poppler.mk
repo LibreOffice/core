be6774fd28da7aab58647c9f0f869c92ce4425ef
Michael Stahl <michael.stahl@allotropia.de>
2025-01-07 18:45:34 +0100

    poppler: upgrade to release 25.01
    Fixes CVE-2024-56378
    
    - remove disable-nss-and-gpgmepp.patch.1 and instead build
      CryptoSignBackend.cc which appears to have working ifdefs now
    - run the update_pch script
    - add clang-std-rotate.patch.1 because jenkins failed
    
    Change-Id: I7fcdd10766287e83dc666041644f129378584d91
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179903
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit e28c5a2c93477d167eb449f08eca220cb3b26b31)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179958
    Tested-by: Michael Stahl <michael.stahl@allotropia.de>
    (cherry picked from commit 3bb689dfbac211696029c767250e841f67711088)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180040
    Tested-by: allotropia jenkins <jenkins@allotropia.de>

--- external/poppler/UnpackedTarball_poppler.mk
+++ external/poppler/UnpackedTarball_poppler.mk
@@ -13,12 +13,13 @@ $(eval $(call gb_UnpackedTarball_set_tarball,poppler,$(POPPLER_TARBALL),,poppler
 
 $(eval $(call gb_UnpackedTarball_add_patches,poppler,\
 	external/poppler/disable-freetype.patch.1 \
-	external/poppler/disable-nss-and-gpgmepp.patch.1 \
 	external/poppler/gcc7-EntityInfo.patch.1 \
 	external/poppler/gcc7-GfxFont.patch.1 \
 	external/poppler/std_charconv.patch.1 \
 	external/poppler/std_string.patch.1 \
 	external/poppler/std_span.patch.1 \
+	external/poppler/std_numbers.patch.1 \
+	external/poppler/clang-std-ranges.patch.1 \
 	external/poppler/poppler-config.patch.1 \
 ))
 
