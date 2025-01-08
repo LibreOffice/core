04585baf973742b0a400b53209391a99ae959d99
Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
2025-01-08 11:51:40 +0100

    wsl-as-helper: fix extracting zxing tarball (dangling symlinks)
    git-bash/msys tar fails when trying to extract the zxing tarball because
    it contains dangling symlinks and MSYS doesn't create symlinks by
    default.
    
    Change-Id: I51f3bb9e2a8cf5395b3fa314f3ac1d10b8873da7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179948
    Tested-by: Jenkins
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- external/zxing/UnpackedTarball_zxing.mk
+++ external/zxing/UnpackedTarball_zxing.mk
@@ -13,6 +13,14 @@ $(eval $(call gb_UnpackedTarball_set_tarball,zxing,$(ZXING_TARBALL)))
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,zxing,1))
 
+ifneq ($(MSYSTEM),)
+# the 2.3.0 tarball contains dangling symlinks (to a submodule component/experimental backend)
+# git-bash/msys tar fails when extracting since MSYS defaults to not create those
+ifeq ($(filter winsymlinks%,$(MSYS)),)
+$(call gb_UnpackedTarball_get_target,zxing): export MSYS:=$(MSYS) winsymlinks
+endif
+endif
+
 $(eval $(call gb_UnpackedTarball_add_patches,zxing, \
 	external/zxing/0001-add-ZXVersion-h.patch \
 ))
