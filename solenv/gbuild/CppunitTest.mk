9e8e01c54826995c92667b52faffe1d8113c7c80
Noel Grandin <noelgrandin@gmail.com>
2025-01-11 00:12:37 +0200

    fix debugging unit tests under macOS 15.2
    as of macOS 15.2, trying to debug a unit test will fail because
    "env" is hardened and we were calling that around our actual
    executable. But we don't need that, since we already pass in the
    required stuff via a command that is inserted before the call to
    lldb.
    
    Change-Id: I4caa91d8ef45326375aa86944b0cee9799acaa59
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180090
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- solenv/gbuild/CppunitTest.mk
+++ solenv/gbuild/CppunitTest.mk
@@ -154,7 +154,9 @@ else
 			PYTHONWARNINGS=default) \
 		LO_RUNNING_UNIT_TEST=1 \
 		$(ICECREAM_RUN) $(gb_CppunitTest_coredumpctl_run) $(gb_CppunitTest_GDBTRACE) $(gb_CppunitTest_VALGRINDTOOL) $(gb_CppunitTest_RR) \
-		$(if $(filter gdb,$(gb_CppunitTest_GDBTRACE)),,env $(gb_CppunitTest_CPPTESTPRECOMMAND)) \
+		$(if $(filter gdb,$(gb_CppunitTest_GDBTRACE)),,\
+			$(if $(filter lldb,$(gb_CppunitTest_GDBTRACE)),,\
+			env $(gb_CppunitTest_CPPTESTPRECOMMAND))) \
 			$(gb_CppunitTest_CPPTESTCOMMAND) \
 		$(call gb_CppunitTest_get_linktarget_target,$*) \
 		$(call gb_CppunitTest__make_args) "-env:CPPUNITTESTTARGET=$@" \
