LibreOffice/core

commit 45c753aff6468b9761e68bc0bf48ab161cd0cba6

svx: pdfium's FPDFImageObj_GetBitmapBgra() is not needed after all
Given that we do check for the different formats just below anyway.

Thanks Ashod Nakashian for pointing this out!

Change-Id: Ie481f6f3fa66ee182668bd6ea7171da2a04cca9d
Reviewed-on: https://gerrit.libreoffice.org/61078
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins

svx/source/svdraw/svdpdf.cxx
@@ -986,7 +986,7 @@ void ImpSdrPdfImport::MapScaling()
 void ImpSdrPdfImport::ImportImage(FPDF_PAGEOBJECT pPageObject, int /*nPageObjectIndex*/)
 {
     std::unique_ptr<std::remove_pointer<FPDF_BITMAP>::type, FPDFBitmapDeleter> bitmap(
-        FPDFImageObj_GetBitmapBgra(pPageObject));
+        FPDFImageObj_GetBitmap(pPageObject));
     if (!bitmap)
     {
         SAL_WARN("sd.filter", "Failed to get IMAGE");

commit d334e7da3013392549694cd7dd3dd326a0387f19

fix import BGRx image from PDF
bug introduced in
    commit 45c753aff6468b9761e68bc0bf48ab161cd0cba6
    Author: Miklos Vajna <vmiklos@collabora.co.uk>
    Date:   Fri Sep 28 12:30:01 2018 +0200
    svx: pdfium's FPDFImageObj_GetBitmapBgra() is not needed after all

Although the confusion was understandable, since the previous commit
which had introduced FPDFImageObj_GetBitmapBgra, which, despite
its name, actually returned RGB* data

Change-Id: I762f1e963d64b4243e21e26cab31c05f4903af49
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180091
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/svdraw/svdpdf.cxx
@@ -879,7 +879,7 @@ void ImpSdrPdfImport::ImportImage(std::unique_ptr<vcl::pdf::PDFiumPageObject> co
             ReadRawDIB(aBitmap, pBuf, ScanlineFormat::N24BitTcBgr, nHeight, nStride);
             break;
         case vcl::pdf::PDFBitmapType::BGRx:
-            ReadRawDIB(aBitmap, pBuf, ScanlineFormat::N32BitTcRgba, nHeight, nStride);
+            ReadRawDIB(aBitmap, pBuf, ScanlineFormat::N32BitTcBgra, nHeight, nStride);
             break;
         case vcl::pdf::PDFBitmapType::BGRA:
             ReadRawDIB(aBitmap, pBuf, ScanlineFormat::N32BitTcBgra, nHeight, nStride);