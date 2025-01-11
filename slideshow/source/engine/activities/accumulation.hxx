b3503b8b33093e4143d0507dc565eb9546d4c14f
sid-sg <sengupta.sid03@gmail.com>
2025-01-12 02:03:04 +0530

    tdf#143148: Replace include guards with #pragma once in slideshow module
    Change-Id: Id48764be9df30bee8d5a803ea78097db9c5c41e8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180129
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Tested-by: Jenkins
    Reviewed-by: Simon Chenery <simon_chenery@yahoo.com>

--- slideshow/source/engine/activities/accumulation.hxx
+++ slideshow/source/engine/activities/accumulation.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SLIDESHOW_SOURCE_ENGINE_ACTIVITIES_ACCUMULATION_HXX
-#define INCLUDED_SLIDESHOW_SOURCE_ENGINE_ACTIVITIES_ACCUMULATION_HXX
+#pragma once
 
 #include <sal/types.h>
 #include <rtl/ustring.hxx>
@@ -78,7 +77,5 @@ namespace slideshow::internal
         }
 
 }
-
-#endif // INCLUDED_SLIDESHOW_SOURCE_ENGINE_ACTIVITIES_ACCUMULATION_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- slideshow/source/engine/activities/activitybase.hxx
+++ slideshow/source/engine/activities/activitybase.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_SLIDESHOW_SOURCE_ENGINE_ACTIVITIES_ACTIVITYBASE_HXX
-#define INCLUDED_SLIDESHOW_SOURCE_ENGINE_ACTIVITIES_ACTIVITYBASE_HXX
+#pragma once
 
 #include <animationactivity.hxx>
 #include "activityparameters.hxx"
@@ -137,7 +136,5 @@ private:
 };
 
 } // namespace presentation::internal
-
-#endif // INCLUDED_SLIDESHOW_SOURCE_ENGINE_ACTIVITIES_ACTIVITYBASE_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
