LibreOffice/core

commit da758c4d15569667cc3d444ba53826b0ee5d2a90

convert RES_OBJECTDYING to SfxHint
and remove SwPtrMsgPoolItem, which is now unused

Change-Id: Ie5bedcd9855256d3ad90189db3df1ae978977e8c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180180
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/docnode/section.cxx
@@ -403,7 +403,14 @@ void SwSection::Notify(SfxHint const& rHint)
         auto rSectionHidden = static_cast<const sw::SectionHidden&>(rHint);
         m_Data.SetHiddenFlag(rSectionHidden.m_isHidden || (m_Data.IsHidden() && m_Data.IsCondHidden()));
         return;
-    } else if (rHint.GetId() != SfxHintId::SwLegacyModify && rHint.GetId() != SfxHintId::SwAttrSetChange)
+    }
+    if (rHint.GetId() == SfxHintId::SwObjectDying)
+    {
+        auto pDyingHint = static_cast<const sw::ObjectDyingHint*>(&rHint);
+        CheckRegistration( *pDyingHint );
+        return;
+    }
+    if (rHint.GetId() != SfxHintId::SwLegacyModify && rHint.GetId() != SfxHintId::SwAttrSetChange)
         return;
 
     bool bUpdateFootnote = false;
@@ -444,10 +451,6 @@ void SwSection::Notify(SfxHint const& rHint)
             {
                 bUpdateFootnote = true;
             }
-            break;
-
-        default:
-            CheckRegistration( pOld );
             break;
         }
     }
@@ -781,6 +784,20 @@ void SwSectionFormat::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
         SwFrameFormat::SwClientNotify(rMod, rHint);
         return;
     }
+    else if (rHint.GetId() == SfxHintId::SwObjectDying)
+    {
+        auto pDyingHint = static_cast<const sw::ObjectDyingHint*>(&rHint);
+        if( !GetDoc()->IsInDtor() &&
+            pDyingHint->m_pDying == GetRegisteredIn() )
+        {
+            // My Parents will be destroyed, so get the Parent's Parent
+            // and update
+            SwFrameFormat::SwClientNotify(rMod, rHint);
+            UpdateParent();
+            return;
+        }
+        SwFrameFormat::SwClientNotify(rMod, rHint);
+    }
     else if (rHint.GetId() != SfxHintId::SwLegacyModify)
         return;
     auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);
@@ -796,19 +813,6 @@ void SwSectionFormat::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
         // Pass through these Messages until the End of the tree!
         GetNotifier().Broadcast(sw::LegacyModifyHint(pOld, pNew));
         return; // That's it!
-
-    case RES_OBJECTDYING:
-        if( !GetDoc()->IsInDtor() && pOld &&
-            static_cast<const SwPtrMsgPoolItem *>(pOld)->pObject == static_cast<void*>(GetRegisteredIn()) )
-        {
-            // My Parents will be destroyed, so get the Parent's Parent
-            // and update
-            SwFrameFormat::SwClientNotify(rMod, rHint);
-            UpdateParent();
-            return;
-        }
-        break;
-
     }
     SwFrameFormat::SwClientNotify(rMod, rHint);
 }

include/svl/hint.hxx
@@ -233,6 +233,7 @@ enum class SfxHintId {
     SwHiddenParaPrint,
     SwFormatChange,
     SwAttrSetChange,
+    SwObjectDying,
 
     ThisIsAnSdrHint,
     ThisIsAnSfxEventHint

include/svl/poolitem.hxx
@@ -486,7 +486,6 @@ enum class SfxItemType : sal_uInt16
     SwPaMItemType,
     SwParaConnectBorderItemType,
     SwPtrItemType,
-    SwPtrMsgPoolItemType,
     SwRegisterItemType,
     SwRotationGrfType,
     SwShadowCursorItemType,

sw/IwyuFilter_sw.yaml
@@ -222,7 +222,6 @@ excludelist:
     - class SwFormatChg
     - class SwFormatMeta
     - class SwMsgPoolItem
-    - class SwPtrMsgPoolItem
     - class SwUpdateAttr
     - class SfxBoolItem
     - class SvxCharSetColorItem

sw/inc/calbck.hxx
@@ -71,6 +71,7 @@ namespace sw
 {
     class ClientIteratorBase;
     class ListenerEntry;
+    class ObjectDyingHint;
     enum class IteratorMode { Exact, UnwrapMulti };
 }
 
@@ -175,7 +176,7 @@ namespace sw
 
         // in case an SwModify object is destroyed that itself is registered in another SwModify,
         // its SwClient objects can decide to get registered to the latter instead by calling this method
-        std::optional<sw::ModifyChangedHint> CheckRegistration( const SfxPoolItem* pOldValue );
+        std::optional<sw::ModifyChangedHint> CheckRegistration( const sw::ObjectDyingHint& rHint );
 
         const T* GetRegisteredIn() const { return m_pRegisteredIn; }
         T* GetRegisteredIn() { return m_pRegisteredIn; }

sw/inc/deletelistener.hxx
@@ -20,10 +20,7 @@ private:
 
     virtual void SwClientNotify(const SwModify&, const SfxHint& rHint) override
     {
-        if (rHint.GetId() != SfxHintId::SwLegacyModify)
-            return;
-        auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);
-        if (pLegacy->GetWhich() == RES_OBJECTDYING)
+        if (rHint.GetId() == SfxHintId::SwObjectDying)
         {
             m_pModify->Remove(*this);
             m_pModify = nullptr;

sw/inc/fmtcol.hxx
@@ -143,14 +143,6 @@ public:
         {
             m_bInSwFntCache = false;
         }
-        else
-        {
-            switch(nWhich)
-            {
-                case RES_OBJECTDYING:
-                    m_bInSwFntCache = false;
-            }
-        }
     };
     virtual void InvalidateInSwFntCache() override
     {

sw/inc/hintids.hxx
@@ -34,7 +34,6 @@ class SwFormatMeta;
 class SwFormatContentControl;
 class SvXMLAttrContainerItem;
 class SwMsgPoolItem;
-class SwPtrMsgPoolItem;
 class SfxBoolItem;
 class SvxColorItem;
 class SvxLeftMarginItem;
@@ -426,10 +425,11 @@ inline constexpr sal_uInt16 RES_FMT_END(167);
 
 // ID's for Messages in the Formats
 inline constexpr sal_uInt16 RES_FORMAT_MSG_BEGIN(RES_FMT_END);
-inline constexpr TypedWhichId<SwPtrMsgPoolItem> RES_OBJECTDYING(RES_FORMAT_MSG_BEGIN); // 167
 inline constexpr sal_uInt16 RES_UPDATEATTR_FMT_CHG(
-    168); // used by SwUpdateAttr just as an ID to communicate what has changed
+    167); // used by SwUpdateAttr just as an ID to communicate what has changed
 inline constexpr sal_uInt16 RES_UPDATEATTR_ATTRSET_CHG(
+    168); // used by SwUpdateAttr just as an ID to communicate what has changed
+inline constexpr sal_uInt16 RES_UPDATEATTR_OBJECTDYING(
     169); // used by SwUpdateAttr just as an ID to communicate what has changed
 inline constexpr TypedWhichId<SwUpdateAttr> RES_UPDATE_ATTR(170);
 // empty

commit f5a23e690d32141544035c0124c22b48bd1e401d

missing return statement in SwSectionFormat::SwClientNotify
regression from
    commit da758c4d15569667cc3d444ba53826b0ee5d2a90
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Sun Jan 5 13:23:59 2025 +0200
    convert RES_OBJECTDYING to SfxHint

Change-Id: I4fdfd2ffe1d395d28b0eadd61613dbee2dc3de51
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180333
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sw/source/core/docnode/section.cxx
@@ -794,9 +794,10 @@ void SwSectionFormat::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
             // and update
             SwFrameFormat::SwClientNotify(rMod, rHint);
             UpdateParent();
-            return;
         }
-        SwFrameFormat::SwClientNotify(rMod, rHint);
+        else
+            SwFrameFormat::SwClientNotify(rMod, rHint);
+        return;
     }
     else if (rHint.GetId() != SfxHintId::SwLegacyModify)
         return;