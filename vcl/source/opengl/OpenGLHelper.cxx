LibreOffice/core

commit 828a0dcdf6fd1600baaf3103583633006a90d9f9

ScanlineDirection is unnecessary
we always use top-down data now.

(which indicates that the Windows OpenGL code has bitrotted a little
 and has probably been broken for some time).

Change-Id: Ic16893d5cf7d2a3f9045b5aa77d4ad2ebc4fe432
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180331
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

vcl/source/opengl/OpenGLHelper.cxx
@@ -561,25 +561,16 @@ BitmapEx OpenGLHelper::ConvertBufferToBitmapEx(const sal_uInt8* const pBuffer, t
         BitmapScopedWriteAccess pAlphaWriteAccess( aAlpha );
 #ifdef _WIN32
         assert(pWriteAccess->GetScanlineFormat() == ScanlineFormat::N24BitTcBgr);
-        assert(pWriteAccess->IsTopDown());
-        assert(pAlphaWriteAccess->IsTopDown());
 #else
         assert(pWriteAccess->GetScanlineFormat() == ScanlineFormat::N24BitTcRgb);
-        assert(!pWriteAccess->IsTopDown());
-        assert(!pAlphaWriteAccess->IsTopDown());
 #endif
         assert(pAlphaWriteAccess->GetScanlineFormat() == ScanlineFormat::N8BitPal);
 
         size_t nCurPos = 0;
         for( tools::Long y = 0; y < nHeight; ++y)
         {
-#ifdef _WIN32
             Scanline pScan = pWriteAccess->GetScanline(y);
             Scanline pAlphaScan = pAlphaWriteAccess->GetScanline(y);
-#else
-            Scanline pScan = pWriteAccess->GetScanline(nHeight-1-y);
-            Scanline pAlphaScan = pAlphaWriteAccess->GetScanline(nHeight-1-y);
-#endif
             for( tools::Long x = 0; x < nWidth; ++x )
             {
                 *pScan++ = pBuffer[nCurPos];

vcl/win/gdi/salbmp.cxx
@@ -251,7 +251,6 @@ std::shared_ptr<Gdiplus::Bitmap> WinSalBitmap::ImplCreateGdiPlusBitmap()
         {
             sal_uInt8* pSrcRGB(pRGB->mpBits);
             const sal_uInt32 nExtraRGB(pRGB->mnScanlineSize - (nW * 3));
-            const bool bTopDown(pRGB->meDirection == ScanlineDirection::TopDown);
             const Gdiplus::Rect aAllRect(0, 0, nW, nH);
             Gdiplus::BitmapData aGdiPlusBitmapData;
             pRetval->LockBits(&aAllRect, Gdiplus::ImageLockModeWrite, PixelFormat24bppRGB, &aGdiPlusBitmapData);
@@ -259,8 +258,7 @@ std::shared_ptr<Gdiplus::Bitmap> WinSalBitmap::ImplCreateGdiPlusBitmap()
             // copy data to Gdiplus::Bitmap; format is BGR here in both cases, so memcpy is possible
             for(sal_uInt32 y(0); y < nH; y++)
             {
-                const sal_uInt32 nYInsert(bTopDown ? y : nH - y - 1);
-                sal_uInt8* targetPixels = static_cast<sal_uInt8*>(aGdiPlusBitmapData.Scan0) + (nYInsert * aGdiPlusBitmapData.Stride);
+                sal_uInt8* targetPixels = static_cast<sal_uInt8*>(aGdiPlusBitmapData.Scan0) + (y * aGdiPlusBitmapData.Stride);
 
                 memcpy(targetPixels, pSrcRGB, nW * 3);
                 pSrcRGB += nW * 3 + nExtraRGB;
@@ -370,7 +368,6 @@ std::shared_ptr<Gdiplus::Bitmap> WinSalBitmap::ImplCreateGdiPlusBitmap(const Win
             sal_uInt8* pSrcA(pA->mpBits);
             const sal_uInt32 nExtraRGB(pRGB->mnScanlineSize - (nW * 3));
             const sal_uInt32 nExtraA(pA->mnScanlineSize - nW);
-            const bool bTopDown(pRGB->meDirection == ScanlineDirection::TopDown);
             const Gdiplus::Rect aAllRect(0, 0, nW, nH);
             Gdiplus::BitmapData aGdiPlusBitmapData;
             pRetval->LockBits(&aAllRect, Gdiplus::ImageLockModeWrite, PixelFormat32bppARGB, &aGdiPlusBitmapData);
@@ -379,8 +376,7 @@ std::shared_ptr<Gdiplus::Bitmap> WinSalBitmap::ImplCreateGdiPlusBitmap(const Win
             // A from alpha, so inner loop is needed (who invented BitmapEx..?)
             for(sal_uInt32 y(0); y < nH; y++)
             {
-                const sal_uInt32 nYInsert(bTopDown ? y : nH - y - 1);
-                sal_uInt8* targetPixels = static_cast<sal_uInt8*>(aGdiPlusBitmapData.Scan0) + (nYInsert * aGdiPlusBitmapData.Stride);
+                sal_uInt8* targetPixels = static_cast<sal_uInt8*>(aGdiPlusBitmapData.Scan0) + (y * aGdiPlusBitmapData.Stride);
 
                 for(sal_uInt32 x(0); x < nW; x++)
                 {

vcl/skia/salbmp.cxx
@@ -312,7 +312,6 @@ BitmapBuffer* SkiaSalBitmap::AcquireBuffer(BitmapAccessMode nMode)
         default:
             abort();
     }
-    buffer->meDirection = ScanlineDirection::TopDown;
     // Refcount all read/write accesses, to catch problems with existing accesses while
     // a bitmap changes, and also to detect when we can free mBuffer if wanted.
     // Write mode implies also reading. It would be probably a good idea to count even
@@ -1147,7 +1146,6 @@ void SkiaSalBitmap::PerformErase()
     if (!ImplFastEraseBitmap(*bitmapBuffer, fastColor))
     {
         FncSetPixel setPixel = BitmapReadAccess::SetPixelFunction(bitmapBuffer->meFormat);
-        assert(bitmapBuffer->meDirection == ScanlineDirection::TopDown);
         // Set first scanline, copy to others.
         Scanline scanline = bitmapBuffer->mpBits;
         for (tools::Long x = 0; x < bitmapBuffer->mnWidth; ++x)

vcl/source/bitmap/salbmp.cxx
@@ -62,19 +62,11 @@ void SalBitmap::updateChecksum() const
                     break;
             }
         }
-        if (pBuf->meDirection == ScanlineDirection::TopDown)
-        {
-            if( pBuf->mnScanlineSize == lineBitsCount / 8 )
-                nCrc = rtl_crc32(nCrc, pBuf->mpBits, pBuf->mnScanlineSize * pBuf->mnHeight);
-            else // Do not include padding with undefined content in the checksum.
-                for( tools::Long y = 0; y < pBuf->mnHeight; ++y )
-                    nCrc = scanlineChecksum(nCrc, pBuf->mpBits + y * pBuf->mnScanlineSize, lineBitsCount, extraBitsMask);
-        }
-        else // Compute checksum in the order of scanlines, to make it consistent between different bitmap implementations.
-        {
-            for( tools::Long y = pBuf->mnHeight - 1; y >= 0; --y )
+        if( pBuf->mnScanlineSize == lineBitsCount / 8 )
+            nCrc = rtl_crc32(nCrc, pBuf->mpBits, pBuf->mnScanlineSize * pBuf->mnHeight);
+        else // Do not include padding with undefined content in the checksum.
+            for( tools::Long y = 0; y < pBuf->mnHeight; ++y )
                 nCrc = scanlineChecksum(nCrc, pBuf->mpBits + y * pBuf->mnScanlineSize, lineBitsCount, extraBitsMask);
-        }
         pThis->ReleaseBuffer(pBuf, BitmapAccessMode::Read);
         pThis->mnChecksum = nCrc;
         pThis->mbChecksumValid = true;

compilerplugins/clang/unusedenumconstants.writeonly.results
@@ -4066,8 +4066,6 @@ include/vcl/salnativewidgets.hxx:198
     enum ControlPart BackgroundDialog
 include/vcl/salnativewidgets.hxx:237
     enum ButtonValue DontKnow
-include/vcl/Scanline.hxx:48
-    enum ScanlineDirection BottomUp
 include/vcl/sysdata.hxx:54
     enum SystemEnvData::Toolkit Qt
 include/vcl/syswin.hxx:50

include/vcl/BitmapBuffer.hxx
@@ -40,7 +40,6 @@ struct VCL_DLLPUBLIC BitmapBuffer
     BitmapPalette   maPalette;
     sal_uInt8*      mpBits;
     ScanlineFormat  meFormat = ScanlineFormat::NONE;
-    ScanlineDirection meDirection = ScanlineDirection::BottomUp;
     ColorMask       maColorMask;
     sal_uInt16      mnBitCount;
 };

include/vcl/BitmapInfoAccess.hxx
@@ -48,15 +48,6 @@ public:
 
     tools::Long Height() const { return mpBuffer ? mpBuffer->mnHeight : 0L; }
 
-    bool IsTopDown() const
-    {
-        assert(mpBuffer && "Access is not valid!");
-
-        return mpBuffer && mpBuffer->meDirection == ScanlineDirection::TopDown;
-    }
-
-    bool IsBottomUp() const { return !IsTopDown(); }
-
     ScanlineFormat GetScanlineFormat() const
     {
         assert(mpBuffer && "Access is not valid!");

include/vcl/BitmapReadAccess.hxx
@@ -49,11 +49,7 @@ public:
         assert(mpBuffer && "Access is not valid!");
         assert(nY < mpBuffer->mnHeight && "y-coordinate out of range!");
 
-        if (mpBuffer->meDirection == ScanlineDirection::TopDown)
-        {
-            return mpBuffer->mpBits + (nY * mpBuffer->mnScanlineSize);
-        }
-        return mpBuffer->mpBits + ((mpBuffer->mnHeight - 1 - nY) * mpBuffer->mnScanlineSize);
+        return mpBuffer->mpBits + (nY * mpBuffer->mnScanlineSize);
     }
 
     BitmapColor GetPixelFromData(const sal_uInt8* pData, tools::Long nX) const

commit a525438eb62f59801899c5ea45e451963b2ec248

fix GDI and Quartz backends
regression from
    commit 828a0dcdf6fd1600baaf3103583633006a90d9f9
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jan 16 08:23:19 2025 +0200
    ScanlineDirection is unnecessary

Change-Id: I5bc9dc8fc1ad74fb75a2656a97f5c994e9be5bc3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180568
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/opengl/OpenGLHelper.cxx
@@ -569,8 +569,13 @@ BitmapEx OpenGLHelper::ConvertBufferToBitmapEx(const sal_uInt8* const pBuffer, t
         size_t nCurPos = 0;
         for( tools::Long y = 0; y < nHeight; ++y)
         {
+#ifdef _WIN32
             Scanline pScan = pWriteAccess->GetScanline(y);
             Scanline pAlphaScan = pAlphaWriteAccess->GetScanline(y);
+#else
+            Scanline pScan = pWriteAccess->GetScanline(nHeight-1-y);
+            Scanline pAlphaScan = pAlphaWriteAccess->GetScanline(nHeight-1-y);
+#endif
             for( tools::Long x = 0; x < nWidth; ++x )
             {
                 *pScan++ = pBuffer[nCurPos];

vcl/win/gdi/gdiimpl.cxx
@@ -538,10 +538,11 @@ void ImplDrawBitmap( HDC hDC, const SalTwoRect& rPosAry, const WinSalBitmap& rSa
                                             WinSalBitmap::ImplGetDIBColorCount( hDrawDIB ) * sizeof( RGBQUAD );
                 const int           nOldStretchMode = SetStretchBltMode( hDC, STRETCH_DELETESCANS );
 
+                int nHeight = -pBI->bmiHeader.biHeight; // height is negative for top-down bitmap
                 StretchDIBits( hDC,
                                static_cast<int>(rPosAry.mnDestX), static_cast<int>(rPosAry.mnDestY),
                                static_cast<int>(rPosAry.mnDestWidth), static_cast<int>(rPosAry.mnDestHeight),
-                               static_cast<int>(rPosAry.mnSrcX), static_cast<int>(pBI->bmiHeader.biHeight - rPosAry.mnSrcHeight - rPosAry.mnSrcY),
+                               static_cast<int>(rPosAry.mnSrcX), static_cast<int>(nHeight - rPosAry.mnSrcHeight - rPosAry.mnSrcY),
                                static_cast<int>(rPosAry.mnSrcWidth), static_cast<int>(rPosAry.mnSrcHeight),
                                pBits, pBI, DIB_RGB_COLORS, nDrawMode );
 

vcl/win/gdi/salbmp.cxx
@@ -517,10 +517,11 @@ bool WinSalBitmap::Create( const SalBitmap& rSSalBmp, SalGraphics* pSGraphics )
 
     if( pBI->bmiHeader.biBitCount == 1 )
     {
-        hNewDDB = CreateBitmap( pBI->bmiHeader.biWidth, pBI->bmiHeader.biHeight, 1, 1, nullptr );
+        int nHeight = -pBI->bmiHeader.biHeight; // height is negative for top-down bitmap
+        hNewDDB = CreateBitmap( pBI->bmiHeader.biWidth, nHeight, 1, 1, nullptr );
 
         if( hNewDDB )
-            SetDIBits( hDC, hNewDDB, 0, pBI->bmiHeader.biHeight, pBits, pBI, DIB_RGB_COLORS );
+            SetDIBits( hDC, hNewDDB, 0, nHeight, pBits, pBI, DIB_RGB_COLORS );
     }
     else
         hNewDDB = CreateDIBitmap( hDC, &pBI->bmiHeader, CBM_INIT, pBits, pBI, DIB_RGB_COLORS );
@@ -687,7 +688,7 @@ HGLOBAL WinSalBitmap::ImplCreateDIB(const Size& rSize, vcl::PixelFormat ePixelFo
 
     pBIH->biSize = sizeof( BITMAPINFOHEADER );
     pBIH->biWidth = rSize.Width();
-    pBIH->biHeight = rSize.Height();
+    pBIH->biHeight = -rSize.Height(); // negative for top-down bitmap
     pBIH->biPlanes = 1;
     pBIH->biBitCount = nBits;
     pBIH->biCompression = BI_RGB;

vcl/win/gdi/salvd.cxx
@@ -33,6 +33,8 @@
 
 HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, tools::Long nDY, sal_uInt16 nBitCount, void **ppData)
 {
+    assert(nDX >= 0);
+    assert(nDY >= 0);
     HBITMAP hBitmap;
 
     if ( nBitCount == 1 )
@@ -52,7 +54,7 @@ HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, to
         BITMAPINFO aBitmapInfo;
         aBitmapInfo.bmiHeader.biSize = sizeof( BITMAPINFOHEADER );
         aBitmapInfo.bmiHeader.biWidth = nDX;
-        aBitmapInfo.bmiHeader.biHeight = nDY;
+        aBitmapInfo.bmiHeader.biHeight = -nDY; // negative for top-down
         aBitmapInfo.bmiHeader.biPlanes = 1;
         aBitmapInfo.bmiHeader.biBitCount = nBitCount;
         aBitmapInfo.bmiHeader.biCompression = BI_RGB;