89904461809e41262233ef9be73aa3c45d14c394
Michael Weghorn <m.weghorn@posteo.de>
2025-01-22 23:23:39 +0100

    tdf#130857 qt weld: Support InputDialog
    Declare support for the InputDialog that can be triggered e.g. like
    this:
    
    1) "Tools" -> "Options" -> "Advanced"
    2) "Parameters"
    3) type anything in the "Java start parameter" edit
    4) confirm using "OK" button
    5) select the param in the list
    6) press the "Edit" button
    
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    Change-Id: I74299b2a920a91508add971e09cc95d4c3c3b3b6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180613
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -90,6 +90,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile)
         u"modules/swriter/ui/splittable.ui"_ustr,
         u"modules/swriter/ui/wordcount.ui"_ustr,
         u"sfx/ui/helpmanual.ui"_ustr,
+        u"sfx/ui/inputdialog.ui"_ustr,
         u"sfx/ui/licensedialog.ui"_ustr,
         u"sfx/ui/password.ui"_ustr,
         u"sfx/ui/querysavedialog.ui"_ustr,
