c9d250ef724a358b9c7ab56d2188cd7e23cd121d
Michael Weghorn <m.weghorn@posteo.de>
2025-01-22 23:28:17 +0100

    tdf#130857 qt weld: Support "Java Start Parameters" dialog
    Declare support for the InputDialog that can be triggered like
    this:
    
    1) "Tools" -> "Options" -> "Advanced"
    2) "Parameters"
    
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    Change-Id: I8dd405bd2957bed8d0c58247285f8df848a89a48
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180614
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -62,6 +62,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile)
     static std::unordered_set<OUString> aSupportedUIFiles = {
         u"cui/ui/aboutdialog.ui"_ustr,
         u"cui/ui/insertrowcolumn.ui"_ustr,
+        u"cui/ui/javastartparametersdialog.ui"_ustr,
         u"cui/ui/namedialog.ui"_ustr,
         u"cui/ui/objecttitledescdialog.ui"_ustr,
         u"cui/ui/optnewdictionarydialog.ui"_ustr,
