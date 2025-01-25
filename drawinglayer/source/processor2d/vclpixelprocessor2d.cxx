LibreOffice/core

commit bc0ab08634f59e1a1814e575fe6ad5e50bf1aee1

Drop F_PI1800/F_PI18000, and unify deg2rad/rad2deg conversions
Change-Id: Ib89b00c3dc8cd440e8a88906eea133becd1cef64
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125509
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -852,8 +852,8 @@ void VclPixelProcessor2D::processFillHatchPrimitive2D(
         const basegfx::B2DVector aDiscreteDistance(
             maCurrentTransformation * basegfx::B2DVector(rFillHatchAttributes.getDistance(), 0.0));
         const sal_uInt32 nDistance(basegfx::fround(aDiscreteDistance.getLength()));
-        const sal_uInt16 nAngle10(
-            static_cast<sal_uInt16>(basegfx::fround(rFillHatchAttributes.getAngle() / F_PI1800)));
+        const sal_uInt32 nAngle10(
+            basegfx::rad2deg<10>(basegfx::fround(rFillHatchAttributes.getAngle())));
         ::Hatch aVCLHatch(eHatchStyle, Color(rFillHatchAttributes.getColor()), nDistance,
                           Degree10(nAngle10));
 
@@ -1216,7 +1216,7 @@ void VclPixelProcessor2D::processFillGradientPrimitive2D(
     Gradient aGradient(eGradientStyle, Color(rFillGradient.getStartColor()),
                        Color(rFillGradient.getEndColor()));
 
-    aGradient.SetAngle(Degree10(static_cast<int>(rFillGradient.getAngle() / F_PI1800)));
+    aGradient.SetAngle(Degree10(static_cast<int>(basegfx::rad2deg<10>(rFillGradient.getAngle()))));
     aGradient.SetBorder(rFillGradient.getBorder() * 100);
     aGradient.SetOfsX(rFillGradient.getOffsetX() * 100.0);
     aGradient.SetOfsY(rFillGradient.getOffsetY() * 100.0);

commit 94f72a396b1e15e710163c37e175552c59048447

tdf#164793: fix misplaced rounding
Regression after commit bc0ab08634f59e1a1814e575fe6ad5e50bf1aee1,
where I confused what neede to be rounded.

Change-Id: Ib150ea1989ed7241747fe4a89bce051478a54bfc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180737
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -797,7 +797,7 @@ void VclPixelProcessor2D::processFillHatchPrimitive2D(
             maCurrentTransformation * basegfx::B2DVector(rFillHatchAttributes.getDistance(), 0.0));
         const sal_uInt32 nDistance(basegfx::fround(aDiscreteDistance.getLength()));
         const sal_uInt32 nAngle10(
-            basegfx::rad2deg<10>(basegfx::fround(rFillHatchAttributes.getAngle())));
+            basegfx::fround(basegfx::rad2deg<10>(rFillHatchAttributes.getAngle())));
         ::Hatch aVCLHatch(eHatchStyle, Color(rFillHatchAttributes.getColor()), nDistance,
                           Degree10(nAngle10));
 