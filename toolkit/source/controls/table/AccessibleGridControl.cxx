33a7e6910f7075c3b24b56feba4e6318caeb4952
Michael Weghorn <m.weghorn@posteo.de>
2025-01-28 09:23:53 +0100

    uno grid a11y: Drop check for a11y context disposed from outside
    AccessibleGridControlAccess owns its
    AccessibleGridControl (`m_xContext` member)
    and is responsible for disposing it.
    
    It does that in AccessibleGridControlAccess::DisposeAccessImpl
    where it also clears `m_xContext` to nullptr.
    
    Therefore, this
    
        // if the context died meanwhile (we're no listener, so it won't tell us explicitly when this happens),
        // then reset and re-create.
    
    check looks very suspicious and should never be reached,
    as anything "from outside", i.e. something not
    owning the AccessibleGridControl should never
    dispose it.
    
    Drop that check.
    
    (In case there actually *is* something disposing the
    accessible context from elsewhere, that should most likely
    be changed in the first place.)
    
    See also
    
        Change-Id: Id16dd7dbf8264d887f52e2fe304b0568079cb924
        Author: Michael Weghorn <m.weghorn@posteo.de>
        Date:   Tue Jan 28 09:17:10 2025 +0100
    
            uno grid a11y: Replace alive check by simple null check
    
            TableControl_Impl owns its accessible (`m_pAccessibleTable`)
            and disposes it in TableControl_Impl::disposeAccessible
            which gets called from TableControl::dispose.
    
            TableControl_Impl::disposeAccessible also sets
            `m_pAccessibleTable` to nullptr.
    
            As `m_pAccessibleTable` should only ever be disposed by
            its owner, I can't think of any (valid) scenario where
            `m_pAccessibleTable` could be non-null but disposed,
            so simply use a null check instead of
            TableControl_Impl::isAccessibleAlive that was checking
            for it being non-null and not diposed.
    
    for a similar commit elsewhere.
    
    Change-Id: Ibe89b39be7540d68144400927bc96c318b2873c1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180831
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- toolkit/source/controls/table/AccessibleGridControl.cxx
+++ toolkit/source/controls/table/AccessibleGridControl.cxx
@@ -255,11 +255,6 @@ css::uno::Reference< css::accessibility::XAccessibleContext > SAL_CALL Accessibl
 {
     SolarMutexGuard g;
 
-    // if the context died meanwhile (we're no listener, so it won't tell us explicitly when this happens),
-    // then reset and re-create.
-    if ( m_xContext.is() && !m_xContext->isAlive() )
-        m_xContext = nullptr;
-
     if (!m_xContext.is() && m_xTable)
         m_xContext = new AccessibleGridControl(m_xParent, this, *m_xTable);
 
