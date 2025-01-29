ff16c4e3f27efc0fc9ed734b19ae778482566cdb
Michael Stahl <michael.stahl@allotropia.de>
2025-01-29 19:54:14 +0100

    tdf#158867 offapi,i18npool,starmath: fix "," in Math again
    This is essentially the same bug that was fixed in commit
    2caa9d7ddcff3f6e380c306b737e9b5e9cdaf4c2 "Resolves: tdf#127873 accept
    ',' comma group separator in number entities again".
    
    The problem is that sw wants to recognize group separators only if there
    are 3 digits following so it really looks like a number (see bug
    tdf#42518), but starmath really wants to recognize either comma or dot
    as a decimal separator and uses the GROUP_SEPARATOR_IN_NUMBER as a hack
    to get this behaviour, so the same parser can work in any locale.
    
    Introduce new constant GROUP_SEPARATOR_IN_NUMBER_3 so that sw and
    starmath can request different parsing.
    
    sc/source/core/tool/compiler.cxx also uses GROUP_SEPARATOR_IN_NUMBER and
    it's unclear to me what is desired there, but there are no known
    regression reports so leave the behaviour there unchanged.
    
    (regression from commit 1bd9a51b826015746069fcc0d02a30a2ddc7e7f5)
    
    Change-Id: Icb7662f26d89677cf84ccad07a2ea4c380587ab4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180920
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -786,7 +786,7 @@ struct Convention_A1 : public ScCompiler::Convention
         static constexpr OUString aAddAllowed(u"?#"_ustr);
         return pCharClass->parseAnyToken( rFormula,
                 nSrcPos, nStartFlags, aAddAllowed,
-                (bGroupSeparator ? nContFlags | KParseTokens::GROUP_SEPARATOR_IN_NUMBER : nContFlags),
+                (bGroupSeparator ? nContFlags | KParseTokens::GROUP_SEPARATOR_IN_NUMBER_3 : nContFlags),
                 aAddAllowed );
     }
 
@@ -1411,7 +1411,7 @@ struct ConventionXL_A1 : public Convention_A1, public ConventionXL
         static constexpr OUString aAddAllowed(u"?!"_ustr);
         return pCharClass->parseAnyToken( rFormula,
                 nSrcPos, nStartFlags, aAddAllowed,
-                (bGroupSeparator ? nContFlags | KParseTokens::GROUP_SEPARATOR_IN_NUMBER : nContFlags),
+                (bGroupSeparator ? nContFlags | KParseTokens::GROUP_SEPARATOR_IN_NUMBER_3 : nContFlags),
                 aAddAllowed );
     }
 
@@ -1762,7 +1762,7 @@ struct ConventionXL_R1C1 : public ScCompiler::Convention, public ConventionXL
 
         return pCharClass->parseAnyToken( rFormula,
                 nSrcPos, nStartFlags, aAddAllowed,
-                (bGroupSeparator ? nContFlags | KParseTokens::GROUP_SEPARATOR_IN_NUMBER : nContFlags),
+                (bGroupSeparator ? nContFlags | KParseTokens::GROUP_SEPARATOR_IN_NUMBER_3 : nContFlags),
                 aAddAllowed );
     }
 
