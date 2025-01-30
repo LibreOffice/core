LibreOffice/core

commit d06de2e049761b7b9e8a95f17557d309812f7acc

Related: tdf#162072 DOCX import: handle font family for characters
Open the bugdoc, the first paragraph is meant to be sans and the second
paragraph is meant to be serif, but both of them are serif.

The bugdoc uses the fonts 'IBM Plex Sans' and 'IBM Plex Serif', which is
not something we bundle, so the font fallback is expected, but the
fallback should have a matching font family (roman vs swiss).

Fix the problem by implementing support for <w:family w:val="..."> in
the DOCX import, which was just ignored previously.

Now the DOCX import result in on par with the ODT import result.

Change-Id: I321b9fc6f63126ca5ad61af57af8a5bc7456d5b4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170772
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -418,6 +418,14 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
             if (m_pImpl->GetTopContext())
             {
                 m_pImpl->GetTopContext()->Insert(PROP_CHAR_FONT_NAME, uno::Any( sStringValue ));
+
+                // Set the matching font family if we have one.
+                FontEntry::Pointer_t pFontEntry = m_pImpl->GetFontTable()->getFontEntryByName(sStringValue);
+                if (pFontEntry)
+                {
+                    m_pImpl->GetTopContext()->Insert(PROP_CHAR_FONT_FAMILY,
+                                                     uno::Any(pFontEntry->m_nFontFamily));
+                }
             }
             break;
         case NS_ooxml::LN_CT_Fonts_asciiTheme:

sw/qa/writerfilter/dmapper/FontTable.cxx
@@ -9,6 +9,10 @@
 
 #include <test/unoapi_test.hxx>
 
+#include <com/sun/star/awt/FontFamily.hpp>
+#include <com/sun/star/beans/XPropertySet.hpp>
+#include <com/sun/star/text/XTextDocument.hpp>
+
 #include <vcl/embeddedfontshelper.hxx>
 
 using namespace com::sun::star;
@@ -56,6 +60,38 @@ CPPUNIT_TEST_FIXTURE(Test, testSubsettedFullEmbeddedFont)
     // glyphs:
     CPPUNIT_ASSERT(!aUrl.isEmpty());
 #endif
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testFontFamily)
+{
+    // Given a document with 2 paragraphs, first is sans, second is serif:
+    // When loading that document:
+    loadFromFile(u"font-family.docx");
+
+    // Then make sure we import <w:family w:val="...">:
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(xTextDocument->getText(),
+                                                                  uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+    // First paragraph: sans.
+    uno::Reference<container::XEnumerationAccess> xPortionEnumAccess(xParaEnum->nextElement(),
+                                                                     uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xPortionEnum = xPortionEnumAccess->createEnumeration();
+    uno::Reference<beans::XPropertySet> xPortion(xPortionEnum->nextElement(), uno::UNO_QUERY);
+    sal_Int16 nFontFamily = awt::FontFamily::DONTKNOW;
+    xPortion->getPropertyValue(u"CharFontFamily"_ustr) >>= nFontFamily;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 5 (SWISS)
+    // - Actual  : 3 (ROMAN)
+    // i.e. the font family was not imported, all font family was roman.
+    CPPUNIT_ASSERT_EQUAL(awt::FontFamily::SWISS, nFontFamily);
+    // Second paragraph: serif.
+    xPortionEnumAccess.set(xParaEnum->nextElement(), uno::UNO_QUERY);
+    xPortionEnum = xPortionEnumAccess->createEnumeration();
+    xPortion.set(xPortionEnum->nextElement(), uno::UNO_QUERY);
+    nFontFamily = awt::FontFamily::DONTKNOW;
+    xPortion->getPropertyValue(u"CharFontFamily"_ustr) >>= nFontFamily;
+    CPPUNIT_ASSERT_EQUAL(awt::FontFamily::ROMAN, nFontFamily);
 }
 }
 

sw/source/writerfilter/dmapper/FontTable.cxx
@@ -136,7 +136,21 @@ void FontTable::lcl_sprm(Sprm& rSprm)
         case NS_ooxml::LN_CT_Font_panose1:
             break;
         case NS_ooxml::LN_CT_Font_family:
+        {
+            Value::Pointer_t pValue = rSprm.getValue();
+            sal_Int32 nIntValue = pValue ? pValue->getInt() : 0;
+            // Map OOXML's ST_FontFamily to UNO's awt::FontFamily.
+            switch (nIntValue)
+            {
+                case NS_ooxml::LN_Value_ST_FontFamily_roman:
+                    m_pImpl->pCurrentEntry->m_nFontFamily = awt::FontFamily::ROMAN;
+                    break;
+                case NS_ooxml::LN_Value_ST_FontFamily_swiss:
+                    m_pImpl->pCurrentEntry->m_nFontFamily = awt::FontFamily::SWISS;
+                    break;
+            }
             break;
+        }
         case NS_ooxml::LN_CT_Font_sig:
             break;
         case NS_ooxml::LN_CT_Font_notTrueType:
@@ -227,6 +241,19 @@ FontEntry::Pointer_t FontTable::getFontEntry(sal_uInt32 nIndex)
 sal_uInt32 FontTable::size()
 {
     return m_pImpl->aFontEntries.size();
+}
+
+FontEntry::Pointer_t FontTable::getFontEntryByName(std::u16string_view rName)
+{
+    for (const auto& pEntry : m_pImpl->aFontEntries)
+    {
+        if (pEntry->sFontName == rName)
+        {
+            return pEntry;
+        }
+    }
+
+    return nullptr;
 }
 
 bool FontTable::IsReadOnly() const

sw/source/writerfilter/dmapper/FontTable.hxx
@@ -23,6 +23,7 @@
 #include <vector>
 #include "LoggedResources.hxx"
 #include <com/sun/star/io/XInputStream.hpp>
+#include <com/sun/star/awt/FontFamily.hpp>
 
 namespace writerfilter::dmapper
 {
@@ -33,6 +34,7 @@ struct FontEntry : public virtual SvRefBase
     typedef tools::SvRef<FontEntry> Pointer_t;
 
     OUString        sFontName;
+    sal_Int16 m_nFontFamily = css::awt::FontFamily::DONTKNOW;
     sal_Int32       nTextEncoding;
     FontEntry() :
         nTextEncoding( RTL_TEXTENCODING_DONTKNOW )
@@ -50,6 +52,7 @@ class FontTable : public LoggedProperties, public LoggedTable
 
     sal_uInt32          size();
     FontEntry::Pointer_t  getFontEntry(sal_uInt32 nIndex);
+    FontEntry::Pointer_t  getFontEntryByName(std::u16string_view rName);
 
     void addEmbeddedFont(const css::uno::Reference<css::io::XInputStream>& stream,
                          const OUString& fontName, std::u16string_view extra,

sw/source/writerfilter/dmapper/PropertyIds.cxx
@@ -395,6 +395,7 @@ OUString getPropertyName( PropertyIds eId )
         { PROP_PARA_CONNECT_BORDERS, u"ParaIsConnectBorder"},
         { PROP_DECORATIVE, u"Decorative"},
         { PROP_PAPER_TRAY, u"PrinterPaperTray"},
+        { PROP_CHAR_FONT_FAMILY, u"CharFontFamily"},
     };
     auto iterator = constPropertyMap.find(eId);
     if (iterator != constPropertyMap.end())

sw/source/writerfilter/dmapper/PropertyIds.hxx
@@ -397,6 +397,7 @@ enum PropertyIds
         ,PROP_RTL_GUTTER
         ,PROP_CURSOR_NOT_IGNORE_TABLES_IN_HF
         ,PROP_PARA_CONNECT_BORDERS
+        ,PROP_CHAR_FONT_FAMILY
     };
 
 //Returns the UNO string equivalent to eId.

commit 7af540ddeb42a3a733f258a77e17c7ebb16d4571

tdf#164904 RTF import: fix unexpected CharFontFamily
Open the RTF in Word, the font fallback is serif, open in Writer, our
fallback is sans.

\froman is not parsed by the RTF import, but the default font family was
ROMAN and that worked in the past.

Fix the problem by only setting the property in case the tokenizer has a
value for it, there is a difference between not setting it vs setting it
to "don't know".

Regression from commit d06de2e049761b7b9e8a95f17557d309812f7acc
(Related: tdf#162072 DOCX import: handle font family for characters,
2024-07-19).

Change-Id: I4253ac7e27fab7d3e8e61c43659589b4fc177d6c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180934
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -430,7 +430,7 @@ void DomainMapper::lcl_attribute(Id nName, const Value & val)
 
                 // Set the matching font family if we have one.
                 FontEntry::Pointer_t pFontEntry = m_pImpl->GetFontTable()->getFontEntryByName(sStringValue);
-                if (pFontEntry)
+                if (pFontEntry && pFontEntry->m_nFontFamily != awt::FontFamily::DONTKNOW)
                 {
                     m_pImpl->GetTopContext()->Insert(PROP_CHAR_FONT_FAMILY,
                                                      uno::Any(pFontEntry->m_nFontFamily));

sw/qa/writerfilter/dmapper/DomainMapper.cxx
@@ -17,6 +17,7 @@
 #include <com/sun/star/style/XStyleFamiliesSupplier.hpp>
 #include <com/sun/star/document/XFilter.hpp>
 #include <com/sun/star/document/XImporter.hpp>
+#include <com/sun/star/awt/FontFamily.hpp>
 
 #include <tools/UnitConversion.hxx>
 #include <unotools/streamwrap.hxx>
@@ -223,6 +224,30 @@ CPPUNIT_TEST_FIXTURE(Test, testRTFStylePaste)
     // Without the accompanying fix in place, this test would have failed, 'Default Drawing Style'
     // was imported, even if no pasted content referenced it.
     CPPUNIT_ASSERT(!xStyleFamily->hasByName(u"Default Drawing Style"_ustr));
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testRTFFontFamily)
+{
+    // Given an RTF file with a 'Times New (W1)' font:
+    loadFromFile(u"font-family.rtf");
+
+    // When checking the font family:
+    uno::Reference<text::XTextDocument> xTextDocument(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XEnumerationAccess> xParaEnumAccess(xTextDocument->getText(),
+                                                                  uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xParaEnum = xParaEnumAccess->createEnumeration();
+    uno::Reference<container::XEnumerationAccess> xPara(xParaEnum->nextElement(), uno::UNO_QUERY);
+    uno::Reference<container::XEnumeration> xPortionEnum = xPara->createEnumeration();
+    uno::Reference<beans::XPropertySet> xPortion(xPortionEnum->nextElement(), uno::UNO_QUERY);
+    sal_Int16 eFamily{};
+    xPortion->getPropertyValue(u"CharFontFamily"_ustr) >>= eFamily;
+
+    // Then make sure it's roman:
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 3 (ROMAN)
+    // - Actual  : 0 (DONTKNOW)
+    // i.e. the final result was a sans fallback instead of a serif fallback.
+    CPPUNIT_ASSERT_EQUAL(awt::FontFamily::ROMAN, eFamily);
 }
 }
 

sw/qa/writerfilter/dmapper/data/font-family.rtf
@@ -1,0 +1,12 @@
+{\rtf1
+{\fonttbl
+{\f42\fbidi \froman\fcharset0\fprq2
+Times New (W1)
+{\*\falt Times New Roman}
+;}
+}
+\paperw11906\paperh16838\margl1440\margr1440\margt1440\margb1440
+\pard\plain
+\f42 Lorem ipsum
+\par
+}