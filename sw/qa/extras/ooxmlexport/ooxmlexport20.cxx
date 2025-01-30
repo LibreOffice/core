LibreOffice/core

commit 56588663a0fddc005c12afaa7d3f8874d036875f

tdf#131728 sw inline heading: fix DOCX paragraph layout interoperability
Fix layout of paragraph – which contains two paragraph styles –
by importing OOXML style separator using a text frame.

Inline headings – where there is no paragraph break after
the heading, i.e. it's followed by the normal paragraph content –
specified by w:specVanish in OOXML, i.e. a special paragraph with
hidden paragraph mark. These headings were loaded as normal, separated
paragraphs, breaking the paragraph layout with their paragraph breaks.
Map inline headings to inline ODF text frames to keep paragraph layout.
The frame contains the original inline paragraph, still keeping
ODF ToC and PDF bookmark support.

Change-Id: I370245218b860c737e45dc15c6f29d79819c9783
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173326
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -2090,6 +2090,10 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
             }
         }
         break;
+    case NS_ooxml::LN_EG_RPrBase_specVanish:
+        if ( nIntValue && !IsStyleSheetImport() )
+            m_pImpl->m_StreamStateStack.top().bIsInlineParagraph = true;
+        break;
     case NS_ooxml::LN_EG_RPrBase_sz:
     case NS_ooxml::LN_EG_RPrBase_szCs:
         {

sw/qa/extras/ooxmlexport/ooxmlexport20.cxx
@@ -281,6 +281,24 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf119800)
                     u"false"_ustr);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf131728)
+{
+    loadAndSave("tdf131728.docx");
+    // Inline paragraphs specified by w:specVanish were loaded as not
+    // inline paragraphs, breaking the paragraph layout. Use ODF text
+    // frame to keep the paragraph layout, where the frame contains the
+    // original inline paragraph, keeping also ODF ToC/PDF bookmark support.
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+
+    // This was 22 (the 5 inline headings were not inline, i.e. normal paragraphs)
+    assertXPath(pXmlDoc, "/w:document/w:body/w:p"_ostr, 17);
+
+    // Still existing headings (duplicated by alternate content)
+    assertXPath(pXmlDoc, "//w:p"_ostr, 27);
+    assertXPath(pXmlDoc, "//w:txbxContent/w:p"_ostr, 10);
+    assertXPath(pXmlDoc, "//w:pStyle[@w:val='Heading2']"_ostr, 10);
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testFdo77129)
 {
     loadAndSave("fdo77129.docx");

sw/source/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -2822,7 +2822,57 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                         uno::Sequence<beans::PropertyValue> aEmptyValues;
                         xTextAppend->appendTextPortion(u"\x200B"_ustr, aEmptyValues);
                     }
-                    xTextRange = xTextAppend->finishParagraph( comphelper::containerToSequence(aProperties) );
+
+                    // tdf#131728 insert the paragraph before the style separator to
+                    // a frame to get inline paragraphs, keeping also the table of content support
+                    // FIXME: overwrite the non-character styles of the paragraph style
+                    // with the settings of the next paragraph style, like MSO does
+                    if ( m_StreamStateStack.top().bIsInlineParagraph && !m_StreamStateStack.top().bIsPreviousInlineParagraph )
+                    {
+                        uno::Reference<text::XParagraphCursor> xParaCursor(
+                            xTextAppend->createTextCursorByRange(xTextAppend->getEnd()), uno::UNO_QUERY_THROW);
+                        //select paragraph
+                        xParaCursor->gotoStartOfParagraph( true );
+
+                        xTextRange = xTextAppend->finishParagraph( comphelper::containerToSequence(aProperties) );
+
+                        uno::Reference< text::XTextRange > xRangeStart, xRangeEnd;
+                        xRangeStart = xParaCursor->getStart();
+                        xRangeEnd = xParaCursor->getEnd();
+
+                        // TODO anchor as character
+                        std::vector<beans::PropertyValue> aFrameProperties
+                        {
+                            comphelper::makePropertyValue(u"TextWrap"_ustr, css::text::WrapTextMode_RIGHT),
+                            comphelper::makePropertyValue(getPropertyName(PROP_POSITION_PROTECTED), true),
+                            comphelper::makePropertyValue(getPropertyName(PROP_SIZE_PROTECTED), true),
+                            comphelper::makePropertyValue(getPropertyName(PROP_HORI_ORIENT), text::HoriOrientation::LEFT),
+                            comphelper::makePropertyValue(getPropertyName(PROP_OPAQUE), false),
+                            comphelper::makePropertyValue(getPropertyName(PROP_WIDTH_TYPE), text::SizeType::MIN),
+                            comphelper::makePropertyValue(getPropertyName(PROP_SIZE_TYPE), text::SizeType::MIN)
+                        };
+
+                        fillEmptyFrameProperties(aFrameProperties, false);
+
+                        uno::Reference<text::XTextAppendAndConvert> xBodyText(xRangeStart->getText(), uno::UNO_QUERY);
+                        uno::Reference<text::XTextAppend> xTextAppend2(xParaCursor->getText(), uno::UNO_QUERY);
+
+                        xBodyText->convertToTextFrame(xRangeStart, xRangeEnd, comphelper::containerToSequence(aFrameProperties));
+
+                        m_StreamStateStack.top().bIsInlineParagraph = false;
+                        m_StreamStateStack.top().bIsPreviousInlineParagraph = true;
+                    }
+                    else
+                    {
+                        xTextRange = xTextAppend->finishParagraph( comphelper::containerToSequence(aProperties) );
+
+                        if ( m_StreamStateStack.top().bIsPreviousInlineParagraph )
+                        {
+                            m_StreamStateStack.top().bIsInlineParagraph = false;
+                            m_StreamStateStack.top().bIsPreviousInlineParagraph = false;
+                        }
+                    }
+
                     m_StreamStateStack.top().xPreviousParagraph.set(xTextRange, uno::UNO_QUERY);
 
                     if (m_StreamStateStack.top().xPreviousParagraph.is() && // null for SvxUnoTextBase

sw/source/writerfilter/dmapper/DomainMapper_Impl.hxx
@@ -273,6 +273,9 @@ struct SubstreamContext
     bool bIsOutsideAParagraph = true;
     bool bDummyParaAddedForTableInSection = false; // tdf#161631
     std::map<sal_Int32, css::uno::Any> deferredCharacterProperties;
+    // inline paragraph: hidden paragraph mark by w:specVanish
+    bool bIsInlineParagraph = false;
+    bool bIsPreviousInlineParagraph = false;
 };
 
 /// Information about a paragraph to be finished after a field end.

commit f1bbce4068828f3d1240afd50b228d820c17e848

tdf#164901 sw inline heading: fix lost floating tables
Style separators inside floating tables resulted losing of
the floating tables. In this case, don't handle style separators,
as before commit 56588663a0fddc005c12afaa7d3f8874d036875f.

Regression since commit 56588663a0fddc005c12afaa7d3f8874d036875f
"tdf#131728 sw inline heading: fix DOCX paragraph layout interoperability".

Change-Id: I30b6683b2312636405960efc65b5af2b6b4dbfa3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180925
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -2139,7 +2139,11 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
         }
         break;
     case NS_ooxml::LN_EG_RPrBase_specVanish:
-        if ( nIntValue && !IsStyleSheetImport() )
+        // tdf#131728 import inline heading
+        if ( nIntValue && !IsStyleSheetImport() &&
+                // tdf#164901 but not in a floating table
+                !(m_pImpl->hasTableManager() &&
+                        m_pImpl->getTableManager().getCurrentTablePosition().getLength() != 0) )
         {
             // put inline heading inside a text frame to get the same layout with ToC/PDF bookmark support
             m_pImpl->m_StreamStateStack.top().bIsInlineParagraph = true;

sw/qa/extras/ooxmlexport/ooxmlexport20.cxx
@@ -277,6 +277,15 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf48459)
     assertXPath(pXmlDoc, "/w:document/w:body/w:p/w:pPr/w:rPr/w:specVanish", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf164901)
+{
+    loadAndSave("tdf164901.docx");
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+
+    // This was 0 (lost floating table because of containing a style separator)
+    assertXPath(pXmlDoc, "/w:document/w:body/w:tbl", 1);
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testFdo77129)
 {
     loadAndSave("fdo77129.docx");