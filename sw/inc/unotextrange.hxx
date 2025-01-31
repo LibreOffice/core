LibreOffice/core

commit baf8d2c1c16cb3bdc4edad2560f95fea807a034f

sw: delete bookmark if paragraph is fully selected
testTdf96479 requires inserting with absorb=true to be treated as
Replace, and there is of course no string in insertTextContent().
testDeleteFlyAtCharAtStart requires setString("") to be treated as
Delete.

Annoyingly this requires API setString() call to be replaced with
internal call, do this for SwXTextRange and SwXTextCursor which are
the 2 classes typically used in practice.

Change-Id: I87caa1aa11abe298cdd3d9a9bbb602e547c7b443
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/137370
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/inc/unotextrange.hxx
@@ -109,7 +109,7 @@ private:
     //TODO: new exception type for protected content
     /// @throws css::uno::RuntimeException
     void    DeleteAndInsert(
-                const OUString& rText, const bool bForceExpandHints);
+                const OUString& rText, ::sw::DeleteAndInsertMode eMode);
     void    Invalidate();
 
     virtual ~SwXTextRange() override;

sw/source/core/unocore/unoobj2.cxx
@@ -800,7 +800,7 @@ static void DeleteTable(SwDoc & rDoc, SwTable& rTable)
 }
 
 void SwXTextRange::DeleteAndInsert(
-        const OUString& rText, const bool bForceExpandHints)
+        const OUString& rText, ::sw::DeleteAndInsertMode const eMode)
 {
     if (RANGE_IS_TABLE == m_pImpl->m_eRangePosition)
     {
@@ -887,13 +887,14 @@ void SwXTextRange::DeleteAndInsert(
 
     if (aCursor.HasMark())
     {
-        m_pImpl->m_rDoc.getIDocumentContentOperations().DeleteAndJoin(aCursor);
+        m_pImpl->m_rDoc.getIDocumentContentOperations().DeleteAndJoin(aCursor,
+            (!rText.isEmpty() || eMode & ::sw::DeleteAndInsertMode::ForceReplace) ? SwDeleteFlags::ArtificialSelection : SwDeleteFlags::Default);
     }
 
     if (!rText.isEmpty())
     {
         SwUnoCursorHelper::DocInsertStringSplitCR(
-                m_pImpl->m_rDoc, aCursor, rText, bForceExpandHints);
+            m_pImpl->m_rDoc, aCursor, rText, bool(eMode & ::sw::DeleteAndInsertMode::ForceExpandHints));
 
         SwUnoCursorHelper::SelectPam(aCursor, true);
         aCursor.Left(rText.getLength());
@@ -1060,7 +1061,7 @@ void SAL_CALL SwXTextRange::setString(const OUString& rString)
 {
     SolarMutexGuard aGuard;
 
-    DeleteAndInsert(rString, false);
+    DeleteAndInsert(rString, ::sw::DeleteAndInsertMode::Default);
 }
 
 bool SwXTextRange::GetPositions(SwPaM& rToFill, ::sw::TextRangeMode const eMode) const

sw/inc/unobaseclass.hxx
@@ -24,6 +24,9 @@
 #include <com/sun/star/container/XEnumeration.hpp>
 
 #include <cppuhelper/implbase.hxx>
+
+#include <o3tl/typed_flags_set.hxx>
+
 #include <vcl/svapp.hxx>
 
 class SfxPoolItem;
@@ -52,6 +55,22 @@ enum class CursorType
     Meta,         // meta/meta-field
     ContentControl,
 };
+
+namespace sw {
+
+enum class DeleteAndInsertMode
+{
+    Default = 0,
+    ForceExpandHints = (1<<0),
+    ForceReplace = (1<<1),
+};
+
+} // namespace sw
+
+namespace o3tl
+{
+    template<> struct typed_flags<::sw::DeleteAndInsertMode> : is_typed_flags<::sw::DeleteAndInsertMode, 0x03> {};
+}
 
 /*
     Start/EndAction or Start/EndAllAction

sw/inc/unotextcursor.hxx
@@ -102,9 +102,7 @@ public:
     bool IsAtEndOfMeta() const;
     bool IsAtEndOfContentControl() const;
 
-    void DeleteAndInsert(OUString const& rText,
-                const bool bForceExpandHints);
-
+    void DeleteAndInsert(OUString const& rText, ::sw::DeleteAndInsertMode eMode);
     // OTextCursorHelper
     virtual const SwPaM*        GetPaM() const override;
     virtual SwPaM*              GetPaM() override;

sw/source/core/undo/undel.cxx
@@ -1306,6 +1306,7 @@ void SwUndoDelete::RedoImpl(::sw::UndoRedoContext & rContext)
         rDoc.getIDocumentContentOperations().DelFullPara( rPam );
     }
     else
+        // FIXME: this ends up calling DeleteBookmarks() on the entire rPam which deletes too many!
         rDoc.getIDocumentContentOperations().DeleteAndJoin(rPam, m_DeleteFlags);
 }
 

sw/source/core/undo/undobj.cxx
@@ -1136,7 +1136,12 @@ void SwUndoSaveContent::DelContentIndex( const SwPosition& rMark,
                         && (   type == IDocumentMarkAccess::MarkType::TEXT_FIELDMARK
                             || type == IDocumentMarkAccess::MarkType::CHECKBOX_FIELDMARK
                             || type == IDocumentMarkAccess::MarkType::DROPDOWN_FIELDMARK
-                            || type == IDocumentMarkAccess::MarkType::DATE_FIELDMARK)))
+                            || type == IDocumentMarkAccess::MarkType::DATE_FIELDMARK))
+                    || (bMaybe
+                        && !(nDelContentType & DelContentType::Replace)
+                        && type == IDocumentMarkAccess::MarkType::BOOKMARK
+                        && pStt->nContent == 0 // entire paragraph deleted?
+                        && pEnd->nContent == pEnd->nNode.GetNode().GetTextNode()->Len()))
                 {
                     if( bMaybe )
                         bSavePos = true;

sw/source/core/unocore/unoobj.cxx
@@ -705,7 +705,7 @@ SwXTextCursor::~SwXTextCursor()
 }
 
 void SwXTextCursor::DeleteAndInsert(const OUString& rText,
-        const bool bForceExpandHints)
+        ::sw::DeleteAndInsertMode const eMode)
 {
     auto pUnoCursor = static_cast<SwCursor*>(m_pUnoCursor.get());
     if (!pUnoCursor)
@@ -721,13 +721,16 @@ void SwXTextCursor::DeleteAndInsert(const OUString& rText,
     {
         if (pCurrent->HasMark())
         {
-            rDoc.getIDocumentContentOperations().DeleteAndJoin(*pCurrent);
+            rDoc.getIDocumentContentOperations().DeleteAndJoin(*pCurrent,
+                // is it "delete" or "replace"?
+                // FIXME still test failure because insertTextContent calls with empty string
+                (nTextLen != 0 || eMode & ::sw::DeleteAndInsertMode::ForceReplace) ? SwDeleteFlags::ArtificialSelection : SwDeleteFlags::Default);
         }
         if(nTextLen)
         {
             const bool bSuccess(
                 SwUnoCursorHelper::DocInsertStringSplitCR(
-                    rDoc, *pCurrent, rText, bForceExpandHints ) );
+                    rDoc, *pCurrent, rText, bool(eMode & ::sw::DeleteAndInsertMode::ForceExpandHints)));
             OSL_ENSURE( bSuccess, "Doc->Insert(Str) failed." );
 
             SwUnoCursorHelper::SelectPam(*pUnoCursor, true);
@@ -1724,7 +1727,7 @@ SwXTextCursor::setString(const OUString& aString)
     const bool bForceExpandHints( (CursorType::Meta == m_eType)
         && dynamic_cast<SwXMeta&>(*m_xParentText)
                 .CheckForOwnMemberMeta(*GetPaM(), true) );
-    DeleteAndInsert(aString, bForceExpandHints);
+    DeleteAndInsert(aString, bForceExpandHints ? ::sw::DeleteAndInsertMode::ForceExpandHints : ::sw::DeleteAndInsertMode::Default);
 }
 
 uno::Any SwUnoCursorHelper::GetPropertyValue(

sw/source/core/unocore/unotext.cxx
@@ -364,7 +364,8 @@ SwXText::insertString(const uno::Reference< text::XTextRange >& xTextRange,
                 dynamic_cast<SwXTextCursor*>(pCursor) );
             if (pTextCursor)
             {
-                pTextCursor->DeleteAndInsert(rString, bForceExpandHints);
+                pTextCursor->DeleteAndInsert(rString, ::sw::DeleteAndInsertMode::ForceReplace
+                    | (bForceExpandHints ? ::sw::DeleteAndInsertMode::ForceExpandHints : ::sw::DeleteAndInsertMode::Default));
             }
             else
             {
@@ -373,7 +374,8 @@ SwXText::insertString(const uno::Reference< text::XTextRange >& xTextRange,
         }
         else
         {
-            pRange->DeleteAndInsert(rString, bForceExpandHints);
+            pRange->DeleteAndInsert(rString, ::sw::DeleteAndInsertMode::ForceReplace
+                | (bForceExpandHints ? ::sw::DeleteAndInsertMode::ForceExpandHints : ::sw::DeleteAndInsertMode::Default));
         }
     }
     else
@@ -606,7 +608,21 @@ SwXText::insertTextContent(
 
     if (bAbsorb && !bAttribute)
     {
-        xRange->setString(OUString());
+        uno::Reference<lang::XUnoTunnel> const xRangeTunnel(xRange, uno::UNO_QUERY);
+        if (SwXTextRange *const pRange = comphelper::getFromUnoTunnel<SwXTextRange>(xRangeTunnel))
+        {
+            pRange->DeleteAndInsert(OUString(), ::sw::DeleteAndInsertMode::ForceReplace
+                | (bForceExpandHints ? ::sw::DeleteAndInsertMode::ForceExpandHints : ::sw::DeleteAndInsertMode::Default));
+        }
+        else if (SwXTextCursor *const pCursor = dynamic_cast<SwXTextCursor*>(comphelper::getFromUnoTunnel<OTextCursorHelper>(xRangeTunnel)))
+        {
+            pCursor->DeleteAndInsert(OUString(), ::sw::DeleteAndInsertMode::ForceReplace
+                | (bForceExpandHints ? ::sw::DeleteAndInsertMode::ForceExpandHints : ::sw::DeleteAndInsertMode::Default));
+        }
+        else
+        {
+            xRange->setString(OUString());
+        }
     }
     uno::Reference< text::XTextRange > xTempRange =
         (bAttribute && bAbsorb) ? xRange : xRange->getStart();

commit b0d4b4664a295631ce4d8dee2ceb5cd94ae12edb

tdf#158198 sw: prevent xBookmark.getAnchor().setString("") from deleting
... the bookmark.

Add a RANGE_IS_BOOKMARK to SwXTextRange and special case setString() so
that it will do a replace, not a delete, to prevent the bookmark from
being deleted.

SwXBookmark::getAnchor() will create such a SwXTextRange.

Bookmarks may be in table cells of course so we need RANGE_IN_CELL to be
a flag now so it can be used with 2 different enum values.

(regression from commit baf8d2c1c16cb3bdc4edad2560f95fea807a034f)

Change-Id: I69169b9a5f6f7525715122b16de009c894246bef
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180985
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/inc/unotextrange.hxx
@@ -114,14 +114,15 @@ public:
     enum RangePosition
     {
         RANGE_IN_TEXT,  // "ordinary" css::text::TextRange
-        RANGE_IN_CELL,  // position created with a cell that has no uno object
         RANGE_IS_TABLE, // anchor of a table
         RANGE_IS_SECTION, // anchor of a section
+        RANGE_IS_BOOKMARK, ///< anchor of a bookmark
     };
 
     SwXTextRange(SwPaM const & rPam,
             const css::uno::Reference< css::text::XText > & xParent,
-            const enum RangePosition eRange = RANGE_IN_TEXT);
+            const enum RangePosition eRange = RANGE_IN_TEXT,
+            bool isInCell = false);
     // only for RANGE_IS_TABLE
     SwXTextRange(SwTableFormat& rTableFormat);
     // only for RANGE_IS_SECTION
@@ -136,7 +137,8 @@ public:
 
     static rtl::Reference< SwXTextRange > CreateXTextRange(
             SwDoc & rDoc,
-            const SwPosition& rPos, const SwPosition *const pMark);
+            const SwPosition& rPos, const SwPosition *const pMark,
+            RangePosition eRange = RANGE_IN_TEXT);
 
     // XServiceInfo
     virtual OUString SAL_CALL getImplementationName() override;
@@ -209,6 +211,7 @@ public:
 private:
     const SfxItemPropertySet& m_rPropSet;
     const enum RangePosition m_eRangePosition;
+    bool const m_isRangeInCell; //< position created with a cell that has no uno object
     SwDoc& m_rDoc;
     css::uno::Reference<css::text::XText> m_xParentText;
     const SwFrameFormat* m_pTableOrSectionFormat;

sw/source/core/unocore/unobkm.cxx
@@ -266,7 +266,8 @@ uno::Reference< text::XTextRange > SAL_CALL SwXBookmark::getAnchor()
             *m_pImpl->m_pDoc,
             m_pImpl->m_pRegisteredBookmark->GetMarkPos(),
             (m_pImpl->m_pRegisteredBookmark->IsExpanded())
-                ? &m_pImpl->m_pRegisteredBookmark->GetOtherMarkPos() : nullptr);
+                ? &m_pImpl->m_pRegisteredBookmark->GetOtherMarkPos() : nullptr,
+            SwXTextRange::RANGE_IS_BOOKMARK);
 }
 
 void SAL_CALL SwXBookmark::dispose()

sw/source/core/unocore/unoobj2.cxx
@@ -702,9 +702,11 @@ void SwXTextRange::MySvtListener::Notify(const SfxHint& rHint)
 
 SwXTextRange::SwXTextRange(SwPaM const & rPam,
         const uno::Reference< text::XText > & xParent,
-        const enum RangePosition eRange)
+        const enum RangePosition eRange,
+        bool const isInCell)
     : m_rPropSet(*aSwMapProvider.GetPropertySet(PROPERTY_MAP_TEXT_CURSOR))
     , m_eRangePosition(eRange)
+    , m_isRangeInCell(isInCell)
     , m_rDoc(rPam.GetDoc())
     , m_xParentText(xParent)
     , m_pTableOrSectionFormat(nullptr)
@@ -718,6 +720,7 @@ SwXTextRange::SwXTextRange(SwPaM const & rPam,
 SwXTextRange::SwXTextRange(SwTableFormat& rTableFormat)
     : m_rPropSet(*aSwMapProvider.GetPropertySet(PROPERTY_MAP_TEXT_CURSOR))
     , m_eRangePosition(RANGE_IS_TABLE)
+    , m_isRangeInCell(false)
     , m_rDoc(*rTableFormat.GetDoc())
     , m_pTableOrSectionFormat(&rTableFormat)
     , m_pMark(nullptr)
@@ -734,6 +737,7 @@ SwXTextRange::SwXTextRange(SwTableFormat& rTableFormat)
 SwXTextRange::SwXTextRange(SwSectionFormat& rSectionFormat)
     : m_rPropSet(*aSwMapProvider.GetPropertySet(PROPERTY_MAP_TEXT_CURSOR))
     , m_eRangePosition(RANGE_IS_SECTION)
+    , m_isRangeInCell(false)
     , m_rDoc(*rSectionFormat.GetDoc())
     , m_pTableOrSectionFormat(&rSectionFormat)
     , m_pMark(nullptr)
@@ -1025,7 +1029,10 @@ void SAL_CALL SwXTextRange::setString(const OUString& rString)
 {
     SolarMutexGuard aGuard;
 
-    DeleteAndInsert(rString, ::sw::DeleteAndInsertMode::Default);
+    // tdf#158198 avoid deleting bookmark via setString on its anchor
+    DeleteAndInsert(rString, RANGE_IS_BOOKMARK == m_eRangePosition
+                                ? ::sw::DeleteAndInsertMode::ForceReplace
+                                : ::sw::DeleteAndInsertMode::Default);
 }
 
 bool SwXTextRange::GetPositions(SwPaM& rToFill, ::sw::TextRangeMode const eMode) const
@@ -1265,7 +1272,8 @@ lcl_IsStartNodeInFormat(const bool bHeader, SwStartNode const *const pSttNode,
 
 rtl::Reference< SwXTextRange >
 SwXTextRange::CreateXTextRange(
-    SwDoc & rDoc, const SwPosition& rPos, const SwPosition *const pMark)
+    SwDoc & rDoc, const SwPosition& rPos, const SwPosition *const pMark,
+    RangePosition const eRange)
 {
     const uno::Reference<text::XText> xParentText(
             ::sw::CreateParentXText(rDoc, rPos));
@@ -1277,7 +1285,7 @@ SwXTextRange::CreateXTextRange(
     }
     const bool isCell( dynamic_cast<SwXCell*>(xParentText.get()) );
     return new SwXTextRange(*pNewCursor, xParentText,
-            isCell ? RANGE_IN_CELL : RANGE_IN_TEXT);
+            eRange, isCell);
 }
 
 namespace sw {
@@ -1432,7 +1440,7 @@ SwXTextRange::createEnumeration()
         getText();
     }
 
-    const CursorType eSetType = (RANGE_IN_CELL == m_eRangePosition)
+    const CursorType eSetType = m_isRangeInCell
             ? CursorType::SelectionInTable : CursorType::Selection;
     return SwXParagraphEnumeration::Create(m_xParentText, pNewCursor, eSetType);
 }