948d9c6480698772dce95e0a23d26594436f8551
Andras Timar <andras.timar@collabora.com>
2025-02-01 12:42:15 +0100

    misc_extensions is now empty and can be removed
    Change-Id: Ibffac03be5f22852e647340ddc4f5211d0159c4f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180997
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- external/Module_external.mk
+++ external/Module_external.mk
@@ -71,7 +71,6 @@ $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,MARIADB_CONNECTOR_C,mariadb-connector-c) \
 	$(call gb_Helper_optional,MDDS,mdds) \
 	$(call gb_Helper_optional,MDNSRESPONDER,mdnsresponder) \
-	$(if $(WITH_EXTRA_EXTENSIONS),misc_extensions) \
 	$(call gb_Helper_optional,MORE_FONTS,more_fonts) \
 	$(call gb_Helper_optional,MSPUB,libmspub) \
 	$(call gb_Helper_optional,MWAW,libmwaw) \
