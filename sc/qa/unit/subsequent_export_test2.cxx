0212a994fc6674ba314aa57ce367fd1109c497c8
Gülşah Köse <gulsah.kose@collabora.com>
2025-02-07 16:05:16 +0300

    Fix wrong detection of sortCondition reference
    Signed-off-by: Gülşah Köse <gulsah.kose@collabora.com>
    Change-Id: I877ede71f9487ba35efd7da6aeeee954560272cc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181248
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181475

--- sc/qa/unit/subsequent_export_test2.cxx
+++ sc/qa/unit/subsequent_export_test2.cxx
@@ -432,6 +432,19 @@ CPPUNIT_TEST_FIXTURE(ScExportTest2, testSortConditionRef)
     assertXPath(pDoc, "//x:worksheet/x:autoFilter/x:sortState/x:sortCondition", "ref", u"B3:B2");
 }
 
+CPPUNIT_TEST_FIXTURE(ScExportTest2, testSortConditionRef2)
+{
+    // Ascending sortCondition reference detected wrong without fix.
+    //- Expected: A11:A300
+    //- Actual  : J11:J300
+    //- In <>, attribute 'ref' of '//x:worksheet/x:autoFilter/x:sortState/x:sortCondition' incorrect value.
+    createScDoc("xlsx/sortconditionref2.xlsx");
+    save(u"Calc Office Open XML"_ustr);
+    xmlDocUniquePtr pDoc = parseExport(u"xl/worksheets/sheet1.xml"_ustr);
+
+    assertXPath(pDoc, "//x:worksheet/x:autoFilter/x:sortState/x:sortCondition", "ref", u"A11:A300");
+}
+
 CPPUNIT_TEST_FIXTURE(ScExportTest2, testDateAutofilterXLSX)
 {
     // XLSX Roundtripping autofilter with date list
