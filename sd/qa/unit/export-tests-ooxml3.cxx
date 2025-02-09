LibreOffice/core

commit 4d153517183193f468dee9148c94fe9d874bacb3

tdf#149697 PPTX export: fix changing place of connection points
Place of the connection point of a polygon changed during
a PPTX round-trip, connecting other vertices of e.g. a square
or a hexagon, as before.

See also commit c3f73f75772d076dfb2ed0538e7d515503edc038
"tdf#149128 PPTX export: fix <stCxn> and <endCxn> connector properties".

Change-Id: I64fc6377417a99d32e84ea71fbed13cf36760118
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/136474
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/export/shapes.cxx
@@ -63,6 +63,8 @@
 #include <com/sun/star/drawing/XDrawPages.hpp>
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
 #include <com/sun/star/presentation/ClickAction.hpp>
+#include <com/sun/star/drawing/XGluePointsSupplier.hpp>
+#include <com/sun/star/container/XIdentifierAccess.hpp>
 #include <tools/globname.hxx>
 #include <comphelper/classids.hxx>
 #include <comphelper/propertysequence.hxx>
@@ -1636,11 +1638,33 @@ static void lcl_GetConnectorAdjustValue(const Reference<XShape>& xShape, tools::
     }
 }
 
+static sal_Int32 lcl_GetGluePointId(const Reference<XShape>& xShape, sal_Int32& nGluePointId)
+{
+    uno::Reference<drawing::XGluePointsSupplier> xSupplier(xShape, uno::UNO_QUERY);
+    uno::Reference<container::XIdentifierAccess> xGluePoints(xSupplier->getGluePoints(),
+                                                             uno::UNO_QUERY);
+    sal_uInt32 nCount = xGluePoints->getIdentifiers().size();
+    if (nCount > 4)
+        nGluePointId -= 4;
+    else
+    {
+        // change id of the bounding box (1 <-> 3)
+        if (nGluePointId == 1)
+            nGluePointId = 3; // Right
+        else if (nGluePointId == 3)
+            nGluePointId = 1; // Left
+    }
+
+    return nGluePointId;
+}
+
 ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape )
 {
     bool bFlipH = false;
     bool bFlipV = false;
     sal_Int32 nAngle = 0;
+    sal_Int32 nStartGlueId = 0;
+    sal_Int32 nEndGlueId = 0;
 
     SAL_INFO("oox.shape", "write connector shape");
 
@@ -1679,6 +1703,13 @@ ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape
     }
     GET( rXShapeA, EdgeStartConnection );
     GET( rXShapeB, EdgeEndConnection );
+
+    GET(nStartGlueId, StartGluePointIndex);
+    if (nStartGlueId != -1)
+        lcl_GetGluePointId(rXShapeA, nStartGlueId);
+    GET(nEndGlueId, EndGluePointIndex);
+    if (nEndGlueId != -1)
+        lcl_GetGluePointId(rXShapeB, nEndGlueId);
 
     // Position is relative to group in Word, but relative to anchor of group in API.
     if (GetDocumentType() == DOCUMENT_DOCX && !mbUserShapes && m_xParent.is())
@@ -1749,7 +1780,7 @@ ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape
             GetNewShapeID(rXShapeA);
         if (GetShapeID(rXShapeB) == -1)
             GetNewShapeID(rXShapeB);
-        WriteConnectorConnections(aConnectorEntry, GetShapeID(rXShapeA), GetShapeID(rXShapeB));
+        WriteConnectorConnections(nStartGlueId, nEndGlueId, GetShapeID(rXShapeA), GetShapeID(rXShapeB));
         pFS->endElementNS(mnXmlNamespace, XML_cNvCxnSpPr);
         if (GetDocumentType() == DOCUMENT_PPTX)
             pFS->singleElementNS(mnXmlNamespace, XML_nvPr);

include/oox/export/drawingml.hxx
@@ -231,7 +231,7 @@ public:
     void WriteColorTransformations( const css::uno::Sequence< css::beans::PropertyValue >& aTransformations, sal_Int32 nAlpha = MAX_PERCENT );
     void WriteGradientStop(sal_uInt16 nStop, ::Color nColor, sal_Int32 nAlpha = MAX_PERCENT);
     void WriteLineArrow( const css::uno::Reference< css::beans::XPropertySet >& rXPropSet, bool bLineStart );
-    void WriteConnectorConnections( EscherConnectorListEntry& rConnectorEntry, sal_Int32 nStartID, sal_Int32 nEndID );
+    void WriteConnectorConnections( sal_uInt32 nStartGlueId, sal_Int32 nEndGlueId, sal_Int32 nStartID, sal_Int32 nEndID );
 
     bool WriteCharColor(const css::uno::Reference<css::beans::XPropertySet>& xPropertySet);
     bool WriteFillColor(const css::uno::Reference<css::beans::XPropertySet>& xPropertySet);

oox/source/export/drawingml.cxx
@@ -4747,19 +4747,19 @@ void DrawingML::WritePolyPolygon(const css::uno::Reference<css::drawing::XShape>
     mpFS->endElementNS(XML_a, XML_custGeom);
 }
 
-void DrawingML::WriteConnectorConnections( EscherConnectorListEntry& rConnectorEntry, sal_Int32 nStartID, sal_Int32 nEndID )
+void DrawingML::WriteConnectorConnections( sal_uInt32 nStartGlueId, sal_Int32 nEndGlueId, sal_Int32 nStartID, sal_Int32 nEndID )
 {
     if( nStartID != -1 )
     {
         mpFS->singleElementNS( XML_a, XML_stCxn,
                                XML_id, OString::number(nStartID),
-                               XML_idx, OString::number(rConnectorEntry.GetConnectorRule(true)) );
+                               XML_idx, OString::number(nStartGlueId) );
     }
     if( nEndID != -1 )
     {
         mpFS->singleElementNS( XML_a, XML_endCxn,
                                XML_id, OString::number(nEndID),
-                               XML_idx, OString::number(rConnectorEntry.GetConnectorRule(false)) );
+                               XML_idx, OString::number(nEndGlueId) );
     }
 }
 

sd/qa/unit/export-tests-ooxml2.cxx
@@ -70,6 +70,7 @@ static void assertMotionPath(std::u16string_view rStr1, std::u16string_view rStr
 class SdOOXMLExportTest2 : public SdModelTestBaseXML
 {
 public:
+    void testTdf149697();
     void testTdf149126();
     void testTdf131905();
     void testTdf93883();
@@ -138,6 +139,7 @@ public:
 
     CPPUNIT_TEST_SUITE(SdOOXMLExportTest2);
 
+    CPPUNIT_TEST(testTdf149697);
     CPPUNIT_TEST(testTdf149126);
     CPPUNIT_TEST(testTdf131905);
     CPPUNIT_TEST(testTdf93883);
@@ -211,6 +213,29 @@ public:
         XmlTestTools::registerOOXMLNamespaces(pXmlXPathCtx);
     }
 };
+
+void SdOOXMLExportTest2::testTdf149697()
+{
+    ::sd::DrawDocShellRef xDocShRef
+        = loadURL(m_directories.getURLFromSrc(u"/sd/qa/unit/data/pptx/tdf149697.pptx"), PPTX);
+    utl::TempFile tempFile;
+    xDocShRef = saveAndReload(xDocShRef.get(), PPTX, &tempFile);
+
+    xmlDocUniquePtr pXmlDocContent = parseExport(tempFile, "ppt/slides/slide1.xml");
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:cxnSp[1]/p:nvCxnSpPr/p:cNvCxnSpPr/a:stCxn",
+                "idx", "5");
+
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:cxnSp[1]/p:nvCxnSpPr/p:cNvCxnSpPr/a:endCxn",
+                "idx", "4");
+
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:cxnSp[2]/p:nvCxnSpPr/p:cNvCxnSpPr/a:stCxn",
+        "idx", "3");
+
+    assertXPath(pXmlDocContent, "/p:sld/p:cSld/p:spTree/p:cxnSp[2]/p:nvCxnSpPr/p:cNvCxnSpPr/a:endCxn",
+        "idx", "1");
+
+    xDocShRef->DoClose();
+}
 
 void SdOOXMLExportTest2::testTdf149126()
 {

commit 589e523dd967e39f4715cfe120e12fba11cf9c90

tdf#164775 PPTX export: fix connector shape regression
A connector shape can be connected to a shape, if the end of connector shape is released inside the target shape, it will automatically attach to the best possible connection point.
It takes into account the shape type, geometric properties, and
user-defined glue points.
In this case, if we export the ODP file to PPTX, we need to use the GetConnectorRule() method to get the correct connection index(idx).

this regression is
caused by commit 4d153517183193f468dee9148c94fe9d874bacb3
(tdf#149697 PPTX export: fix changing place of connection points).

Change-Id: I8eda059e72f595edc62fd5bc7f3e6466988af5bb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181226
Tested-by: Jenkins
Reviewed-by: Nagy Tibor <tibor.nagy.extern@allotropia.de>

include/filter/msfilter/escherex.hxx
@@ -452,7 +452,7 @@ namespace com::sun::star {
     }
 }
 
-struct UNLESS_MERGELIBS(MSFILTER_DLLPUBLIC) EscherConnectorListEntry
+struct MSFILTER_DLLPUBLIC EscherConnectorListEntry
 {
     css::uno::Reference< css::drawing::XShape >   mXConnector;
     css::awt::Point                               maPointA;

oox/source/export/shapes.cxx
@@ -1893,16 +1893,6 @@ ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape
     if (GetProperty(rXPropSet, u"EdgeEndConnection"_ustr))
         mAny >>= rXShapeB;
 
-    if (GetProperty(rXPropSet, u"StartGluePointIndex"_ustr))
-        mAny >>= nStartGlueId;
-    if (nStartGlueId != -1)
-        nStartGlueId = lcl_GetGluePointId(rXShapeA, nStartGlueId);
-
-    if (GetProperty(rXPropSet, u"EndGluePointIndex"_ustr))
-        mAny >>= nEndGlueId;
-    if (nEndGlueId != -1)
-        nEndGlueId = lcl_GetGluePointId(rXShapeB, nEndGlueId);
-
     // Position is relative to group in Word, but relative to anchor of group in API.
     if (GetDocumentType() == DOCUMENT_DOCX && !mbUserShapes && m_xParent.is())
     {
@@ -1913,6 +1903,24 @@ ShapeExport& ShapeExport::WriteConnectorShape( const Reference< XShape >& xShape
         aEndPoint.Y -= aParentPos.Y;
     }
     EscherConnectorListEntry aConnectorEntry( xShape, aStartPoint, rXShapeA, aEndPoint, rXShapeB );
+
+    if (GetProperty(rXPropSet, u"StartGluePointIndex"_ustr))
+    {
+        mAny >>= nStartGlueId;
+        nStartGlueId = (nStartGlueId != -1) ? lcl_GetGluePointId(rXShapeA, nStartGlueId)
+                                            : (aConnectorEntry.mXConnectToA.is()
+                                                   ? aConnectorEntry.GetConnectorRule(true)
+                                                   : -1);
+    }
+
+    if (GetProperty(rXPropSet, u"EndGluePointIndex"_ustr))
+    {
+        mAny >>= nEndGlueId;
+        nEndGlueId = (nEndGlueId != -1) ? lcl_GetGluePointId(rXShapeB, nEndGlueId)
+                                        : (aConnectorEntry.mXConnectToB.is()
+                                               ? aConnectorEntry.GetConnectorRule(false)
+                                               : -1);
+    }
 
     if (eConnectorType != ConnectorType_LINE)
     {

sd/qa/unit/export-tests-ooxml3.cxx
@@ -27,6 +27,18 @@ public:
     int testTdf115005_FallBack_Images(bool bAddReplacementImages);
 };
 
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest3, testTdf164775_ConnectorShape)
+{
+    createSdImpressDoc("odp/tdf164775.odp");
+    save(u"Impress Office Open XML"_ustr);
+
+    xmlDocUniquePtr pXmlDoc1 = parseExport(u"ppt/slides/slide1.xml"_ustr);
+    assertXPath(pXmlDoc1, "/p:sld/p:cSld/p:spTree/p:cxnSp/p:nvCxnSpPr/p:cNvCxnSpPr/a:stCxn", "idx",
+                u"3");
+    assertXPath(pXmlDoc1, "/p:sld/p:cSld/p:spTree/p:cxnSp/p:nvCxnSpPr/p:cNvCxnSpPr/a:endCxn", "idx",
+                u"1");
+}
+
 CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest3, testTdf153105)
 {
     createSdImpressDoc("odp/tdf153105.odp");