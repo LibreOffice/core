LibreOffice/core

commit 2d208c4e42595bb85fd0ef726a89e10dd32b48b1

tdf#161210 speed xls load
instead of applying row styles to all 16384 columns, only apply them to
columns that have data.

That takes the load time for this file from 19s to 1s for me.

Change-Id: Ifec56995e0053938dc5a7ed0d15ae3a173614dd4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/169506
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/filter/excel/xistyle.cxx
@@ -1890,6 +1890,7 @@ XclImpXFRangeBuffer::~XclImpXFRangeBuffer()
 void XclImpXFRangeBuffer::Initialize()
 {
     maColumns.clear();
+    maRows.clear();
     maHyperlinks.clear();
     maMergeList.RemoveAll();
 }
@@ -1942,8 +1943,10 @@ void XclImpXFRangeBuffer::SetBoolXF( const ScAddress& rScPos, sal_uInt16 nXFInde
 
 void XclImpXFRangeBuffer::SetRowDefXF( SCROW nScRow, sal_uInt16 nXFIndex )
 {
-    for( SCCOL nScCol = 0; nScCol <= GetDoc().MaxCol(); ++nScCol )
-        SetXF( ScAddress( nScCol, nScRow, 0 ), nXFIndex, xlXFModeRow );
+    size_t nIndex = static_cast< size_t >( nScRow );
+    if( maRows.size() <= nIndex )
+        maRows.resize( nIndex + 1 );
+    maRows[ nIndex ].emplace(nXFIndex);
 }
 
 void XclImpXFRangeBuffer::SetColumnDefXF( SCCOL nScCol, sal_uInt16 nXFIndex )
@@ -1989,6 +1992,17 @@ void XclImpXFRangeBuffer::Finalize()
     ScDocumentImport& rDocImport = GetDocImport();
     ScDocument& rDoc = rDocImport.getDoc();
     SCTAB nScTab = GetCurrScTab();
+
+    // apply row styles
+    for( SCROW nScRow = 0; nScRow < static_cast<SCROW>(maRows.size()); ++nScRow )
+    {
+        if (!maRows[nScRow])
+            continue;
+        sal_uInt16 nXFIndex = *maRows[nScRow];
+        for( SCCOL nScCol = 0; nScCol < static_cast<SCCOL>(maColumns.size()); ++nScCol )
+            if (maColumns[nScCol])
+                SetXF( ScAddress( nScCol, nScRow, 0 ), nXFIndex, xlXFModeRow );
+    }
 
     // apply patterns
     XclImpXFBuffer& rXFBuffer = GetXFBuffer();

sc/source/filter/inc/xistyle.hxx
@@ -659,6 +659,8 @@ private:
 
     std::vector< std::optional<XclImpXFRangeColumn> >
                         maColumns;        /// Array of column XF index buffers.
+    std::vector< std::optional<sal_uInt16> >
+                        maRows;        /// Array of row XF index.
     std::vector< std::pair< XclRange, OUString > >
                         maHyperlinks;     /// Maps URLs to hyperlink cells.
     ScRangeList         maMergeList;      /// List of merged cell ranges.

commit c5f3e78f34a31077df6f9f8e3e9093454cdca25f

tdf#165080 Background color cells are reset to white
regression from
    commit 2d208c4e42595bb85fd0ef726a89e10dd32b48b1
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Tue Jun 25 09:03:49 2024 +0200
    tdf#161210 speed xls load

Change-Id: Ided4df0ce448cf811e7f43bcf601641e2b7c29c2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181350
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sc/source/filter/excel/xistyle.cxx
@@ -1993,17 +1993,6 @@ void XclImpXFRangeBuffer::Finalize()
     ScDocument& rDoc = rDocImport.getDoc();
     SCTAB nScTab = GetCurrScTab();
 
-    // apply row styles
-    for( SCROW nScRow = 0; nScRow < static_cast<SCROW>(maRows.size()); ++nScRow )
-    {
-        if (!maRows[nScRow])
-            continue;
-        sal_uInt16 nXFIndex = *maRows[nScRow];
-        for( SCCOL nScCol = 0; nScCol < static_cast<SCCOL>(maColumns.size()); ++nScCol )
-            if (maColumns[nScCol])
-                SetXF( ScAddress( nScCol, nScRow, 0 ), nXFIndex, xlXFModeRow );
-    }
-
     // apply patterns
     XclImpXFBuffer& rXFBuffer = GetXFBuffer();
     ScDocumentImport::Attrs aPendingAttrParam;
@@ -2061,6 +2050,16 @@ void XclImpXFRangeBuffer::Finalize()
     }
     if( pendingColStart != -1 )
         rDocImport.setAttrEntries(nScTab, pendingColStart, pendingColEnd, std::move(aPendingAttrParam));
+
+    // apply row styles
+    for( SCROW nScRow = 0; nScRow < static_cast<SCROW>(maRows.size()); ++nScRow )
+    {
+        if (!maRows[nScRow])
+            continue;
+        sal_uInt16 nXFIndex = *maRows[nScRow];
+        for( nScCol = 0; nScCol < static_cast<SCCOL>(maColumns.size()); ++nScCol )
+            SetXF( ScAddress( nScCol, nScRow, 0 ), nXFIndex, xlXFModeRow );
+    }
 
     // insert hyperlink cells
     for( const auto& [rXclRange, rUrl] : maHyperlinks )