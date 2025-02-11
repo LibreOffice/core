449b905f78737337eef8daff03a6342f96df845e
Xisco Fauli <xiscofauli@libreoffice.org>
2025-02-11 14:41:14 +0100

    tdf#165080: sc_subsequent_filters_test2: Add unittest
    Change-Id: I2352f6c40cf18344a5ac97467ddfe074e9bb4016
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181421
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/qa/unit/subsequent_filters_test2.cxx
+++ sc/qa/unit/subsequent_filters_test2.cxx
@@ -862,6 +862,24 @@ CPPUNIT_TEST_FIXTURE(ScFiltersTest2, testTdf130132)
     }
 }
 
+CPPUNIT_TEST_FIXTURE(ScFiltersTest2, testTdf165080)
+{
+    createScDoc("xls/tdf165080.xls");
+
+    ScDocument* pDoc = getScDoc();
+
+    const ScPatternAttr* pAttr = pDoc->GetPattern(0, 0, 0);
+
+    const SfxPoolItem& rItem = pAttr->GetItem(ATTR_BACKGROUND);
+    const SvxBrushItem& rBackground = static_cast<const SvxBrushItem&>(rItem);
+    const Color& rColor = rBackground.GetColor();
+
+    // Without the fix in place, this test would have failed with
+    // - Expected: rgba[c0c0c0ff]
+    // - Actual  : rgba[ffffff00]
+    CPPUNIT_ASSERT_EQUAL(COL_LIGHTGRAY, rColor);
+}
+
 CPPUNIT_TEST_FIXTURE(ScFiltersTest2, testTdf133327)
 {
     createScDoc("ods/tdf133327.ods");
