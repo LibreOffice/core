LibreOffice/core

commit f91a411340ae204ce1e6997f22e0352a4c6a8355

reduce cost of calc column height calculation
avoid calling GetTextWidth unless we really need to

Change-Id: Ibb0f378e632fa3fce68b4819028630759baf4eb5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/167988
Tested-by: Jenkins
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>

sc/source/core/data/column2.cxx
@@ -307,9 +307,10 @@ tools::Long ScColumn::GetNeededSize(
         {
             //  SetFont is moved up
 
-            Size aSize( pDev->GetTextWidth( aValStr ), pDev->GetTextHeight() );
+            Size aSize;
             if ( eOrient != SvxCellOrientation::Standard )
             {
+                aSize = Size( pDev->GetTextWidth( aValStr ), pDev->GetTextHeight() );
                 tools::Long nTemp = aSize.Width();
                 aSize.setWidth( aSize.Height() );
                 aSize.setHeight( nTemp );
@@ -318,6 +319,7 @@ tools::Long ScColumn::GetNeededSize(
             {
                 //TODO: take different X/Y scaling into consideration
 
+                aSize = Size( pDev->GetTextWidth( aValStr ), pDev->GetTextHeight() );
                 double nRealOrient = toRadians(nRotate);
                 double nCosAbs = fabs( cos( nRealOrient ) );
                 double nSinAbs = fabs( sin( nRealOrient ) );
@@ -347,8 +349,13 @@ tools::Long ScColumn::GetNeededSize(
                 }
 
                 aSize = Size( nWidth, nHeight );
+                nValue = bWidth ? aSize.Width() : aSize.Height();
             }
-            nValue = bWidth ? aSize.Width() : aSize.Height();
+            else if (bBreak && !bWidth)
+                aSize = Size( pDev->GetTextWidth( aValStr ), pDev->GetTextHeight() );
+            else
+                // in the common case (height), avoid calling the expensive GetTextWidth
+                nValue = bWidth ? pDev->GetTextWidth( aValStr ) : pDev->GetTextHeight();
 
             if ( bAddMargin )
             {

commit f2639e2a98f114f053a13da669440587d56a0dd8

tdf#165003 Row height wrong at writing direction 90°
regression from
    commit f91a411340ae204ce1e6997f22e0352a4c6a8355
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Thu May 23 15:09:52 2024 +0200
    reduce cost of calc column height calculation

This also fixes another bug in the above commit where the code did not
update nValue for the "else if (bBreak && !bWidth)" case

Change-Id: I367a56c2cb555336e1d510efb0c7acac1447525f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181417
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sc/source/core/data/column2.cxx
@@ -310,7 +310,10 @@ tools::Long ScColumn::GetNeededSize(
             tools::Long nWidth = 0;
             if ( eOrient != SvxCellOrientation::Standard )
             {
-                nWidth = pDev->GetTextHeight();
+                tools::Long nHeight = pDev->GetTextHeight();
+                // swap width and height
+                nValue = bWidth ? nHeight : pDev->GetTextWidth( aValStr );
+                nWidth = nHeight;
             }
             else if ( nRotate )
             {
@@ -363,7 +366,10 @@ tools::Long ScColumn::GetNeededSize(
                 }
             }
             else if (bBreak && !bWidth)
+            {
                 nWidth = pDev->GetTextWidth(aValStr);
+                nValue = pDev->GetTextHeight();
+            }
             else
                 // in the common case (height), avoid calling the expensive GetTextWidth
                 nValue = bWidth ? pDev->GetTextWidth( aValStr ) : pDev->GetTextHeight();

sc/qa/unit/subsequent_filters_test4.cxx
@@ -440,6 +440,29 @@ CPPUNIT_TEST_FIXTURE(ScFiltersTest4, testRowHeightODS)
     CPPUNIT_ASSERT_MESSAGE("Row should have an automatic height.", !bManual);
 }
 
+CPPUNIT_TEST_FIXTURE(ScFiltersTest4, testRowHeightTdf165003)
+{
+    createScDoc("ods/RowHeightTdf165003.ods");
+
+    SCTAB nTab = 0;
+    SCROW nRow = 0;
+    ScDocument* pDoc = getScDoc();
+
+    int nHeight = pDoc->GetRowHeight(nRow, nTab, false);
+    CPPUNIT_ASSERT_EQUAL(256, nHeight);
+    nHeight = pDoc->GetRowHeight(++nRow, nTab, false);
+    CPPUNIT_ASSERT_EQUAL(256, nHeight);
+    nHeight = pDoc->GetRowHeight(++nRow, nTab, false);
+    CPPUNIT_ASSERT_EQUAL(256, nHeight);
+    nHeight = pDoc->GetRowHeight(++nRow, nTab, false);
+    CPPUNIT_ASSERT_EQUAL(256, nHeight);
+    // this row has 90-degree rotated text, and without the fix, would have had zero height.
+    nHeight = pDoc->GetRowHeight(++nRow, nTab, false);
+    CPPUNIT_ASSERT_EQUAL(582, nHeight);
+    nHeight = pDoc->GetRowHeight(++nRow, nTab, false);
+    CPPUNIT_ASSERT_EQUAL(256, nHeight);
+}
+
 CPPUNIT_TEST_FIXTURE(ScFiltersTest4, testRichTextContentODS)
 {
     createScDoc("ods/rich-text-cells.ods");