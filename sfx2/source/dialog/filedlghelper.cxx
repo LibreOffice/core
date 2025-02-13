67cffdf1b2872b4bcac657f7bcf63f19c261a5c6
Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
2025-02-13 09:55:04 +0100

    tdf#165228 Don't reuse previous path in save dialog
    Change-Id: I8abd005eac663a2d7be1952e0e91acd3cc3ae2de
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181547
    Tested-by: Jenkins
    Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@allotropia.de>
    (cherry picked from commit 22c07826d77adf93ada6e17ed6ac531163dd5059)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181871
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sfx2/source/dialog/filedlghelper.cxx
+++ sfx2/source/dialog/filedlghelper.cxx
@@ -1476,6 +1476,7 @@ ErrCode FileDialogHelper_Impl::execute( std::vector<OUString>& rpURLList,
     }
 
     rpURLList.clear();
+    maPath.clear(); // tdf#147202 This should not survive between calls to execute
 
     if ( ! mxFileDlg.is() )
         return ERRCODE_ABORT;
