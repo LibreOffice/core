91ce2860cc21d4a20888310b8b8835391f20fca6
Christopher Sherlock <chris.sherlock79@gmail.com>
2025-02-15 13:51:44 +1100

    vcl: fix modelines in vcl/source/control/fmtfield.cxx
    Change-Id: I547d4c993f1c1ab6d71dde68d82fc159d1254e28
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181688
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/source/control/fmtfield.cxx
+++ vcl/source/control/fmtfield.cxx
@@ -1,4 +1,4 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
 /*
  * This file is part of the LibreOffice project.
  *
@@ -1150,4 +1150,4 @@ void DoubleCurrencyField::UpdateCurrencyFormat()
     static_cast<DoubleCurrencyFormatter*>(m_pFormatter)->GuardSetFormat(sNewFormat.makeStringAndClear(), eLanguage);
 }
 
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
