LibreOffice/core

commit 89d86b64c93f2f47d7cfdefb928812e4de64fe3b

lokit: move re-initialization of wordbook to addconfig option
- previously we used to re-initialize it in lo_init because we wanted
regonize newly added dictionaries but then we again had to re-initailize
the XDictionary if new dicionary are added after ForKit is created and
when new LoKit is initialized
- this patch now does it on "addconfig" setOption which after kit is
initialized

Change-Id: I115e8ef23e0b3184f35169f6421fe461239c856a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178646
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

desktop/source/lib/init.cxx
@@ -5301,6 +5301,17 @@ static void updateConfig(const OUString& rConfigPath)
             auto xUpdate(css::configuration::Update::get(comphelper::getProcessComponentContext()));
             xUpdate->insertModificationXcuFile(aXcuPath, { u"/"_ustr }, {});
         }
+        else if (sFileName == "wordbook")
+        {
+            uno::Reference<css::linguistic2::XSearchableDictionaryList> xDicList
+                = LinguMgr::GetDictionaryList();
+            if (xDicList.is())
+            {
+                uno::Reference<lang::XInitialization> xReInitDictionaryList(xDicList,
+                                                                            uno::UNO_QUERY_THROW);
+                xReInitDictionaryList->initialize({});
+            }
+        }
     }
 }
 
@@ -8355,14 +8366,6 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
             uno::Reference<lang::XInitialization> xReInitSettings(xPathSettings, uno::UNO_QUERY_THROW);
             xReInitSettings->initialize({});
 
-            uno::Reference<css::linguistic2::XSearchableDictionaryList> xDicList
-                = LinguMgr::GetDictionaryList();
-            if (xDicList.is())
-            {
-                uno::Reference<lang::XInitialization> xReInitDictionaryList(xDicList,
-                                                                            uno::UNO_QUERY_THROW);
-                xReInitDictionaryList->initialize({});
-            }
         }
     }
 

commit 190593257ad7636a9f88ff765ab66ff7307b9dca

fix(sw-lokit): correctly load shared wordbooks
- Fixes a regression introduced in commit 89d86b64c93f2f47d7cfdefb928812e4de64fe3b.
- Ensures dictionaries are reinitialized even when only shared wordbooks are uploaded.
- Previously, reinitialization occurred only when user wordbooks were uploaded.

Change-Id: I0f54b6b01fe1aefadc2c1fec2d9ae57db1c5d02a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181787
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

desktop/source/lib/init.cxx
@@ -5061,6 +5061,17 @@ static void lo_sendDialogEvent(LibreOfficeKit* /*pThis*/, unsigned long long int
     lcl_sendDialogEvent(nWindowId, pArguments);
 }
 
+static void reInitDictionaryList()
+{
+    uno::Reference<css::linguistic2::XSearchableDictionaryList> xDicList
+        = LinguMgr::GetDictionaryList();
+    if (xDicList.is())
+    {
+        uno::Reference<lang::XInitialization> xReInitDictionaryList(xDicList, uno::UNO_QUERY_THROW);
+        xReInitDictionaryList->initialize({});
+    }
+};
+
 static void updateConfig(const OUString& rConfigPath)
 {
     osl::Directory aScanRootDir(rConfigPath);
@@ -5125,14 +5136,7 @@ static void updateConfig(const OUString& rConfigPath)
         }
         else if (sFileName == "wordbook")
         {
-            uno::Reference<css::linguistic2::XSearchableDictionaryList> xDicList
-                = LinguMgr::GetDictionaryList();
-            if (xDicList.is())
-            {
-                uno::Reference<lang::XInitialization> xReInitDictionaryList(xDicList,
-                                                                            uno::UNO_QUERY_THROW);
-                xReInitDictionaryList->initialize({});
-            }
+            reInitDictionaryList();
         }
     }
 }
@@ -8210,7 +8214,7 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
             uno::Reference<css::util::XPathSettings> xPathSettings = util::thePathSettings::get(xContext);
             uno::Reference<lang::XInitialization> xReInitSettings(xPathSettings, uno::UNO_QUERY_THROW);
             xReInitSettings->initialize({});
-
+            reInitDictionaryList();
         }
     }
 