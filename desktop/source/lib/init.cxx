72a0c6abc44fb6a6f1d541f43f76480ea2fd3149
Rashesh <rashesh.padia@collabora.com>
2025-02-17 18:54:58 +0530

    fix(sw-lokit): correctly load shared wordbooks
    - Fixes a regression introduced in commit 89d86b64c93f2f47d7cfdefb928812e4de64fe3b.
    - Ensures dictionaries are reinitialized even when only shared wordbooks are uploaded.
    - Previously, reinitialization occurred only when user wordbooks were uploaded.
    
    Change-Id: I0f54b6b01fe1aefadc2c1fec2d9ae57db1c5d02a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181787
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -5031,6 +5031,17 @@ static void lo_sendDialogEvent(LibreOfficeKit* /*pThis*/, unsigned long long int
     lcl_sendDialogEvent(nWindowId, pArguments);
 }
 
+static void reInitDictionaryList()
+{
+    uno::Reference<css::linguistic2::XSearchableDictionaryList> xDicList
+        = LinguMgr::GetDictionaryList();
+    if (xDicList.is())
+    {
+        uno::Reference<lang::XInitialization> xReInitDictionaryList(xDicList, uno::UNO_QUERY_THROW);
+        xReInitDictionaryList->initialize({});
+    }
+};
+
 static void updateConfig(const OUString& rConfigPath)
 {
     osl::Directory aScanRootDir(rConfigPath);
@@ -5095,14 +5106,7 @@ static void updateConfig(const OUString& rConfigPath)
         }
         else if (sFileName == "wordbook")
         {
-            uno::Reference<css::linguistic2::XSearchableDictionaryList> xDicList
-                = LinguMgr::GetDictionaryList();
-            if (xDicList.is())
-            {
-                uno::Reference<lang::XInitialization> xReInitDictionaryList(xDicList,
-                                                                            uno::UNO_QUERY_THROW);
-                xReInitDictionaryList->initialize({});
-            }
+            reInitDictionaryList();
         }
     }
 }
@@ -8177,7 +8181,7 @@ static int lo_initialize(LibreOfficeKit* pThis, const char* pAppPath, const char
             uno::Reference<css::util::XPathSettings> xPathSettings = util::thePathSettings::get(xContext);
             uno::Reference<lang::XInitialization> xReInitSettings(xPathSettings, uno::UNO_QUERY_THROW);
             xReInitSettings->initialize({});
-
+            reInitDictionaryList();
         }
     }
 
