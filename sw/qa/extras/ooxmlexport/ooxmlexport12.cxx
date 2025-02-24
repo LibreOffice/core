ea0e5148d0b6f9365ed74b4b8547e5171d7fad43
Michael Stahl <michael.stahl@allotropia.de>
2025-02-24 16:02:33 +0100

    tdf#165322 sw: GetRedline() binary search doesn't work with overlaps
    Change-Id: I6d7a271eb96f55abdd6d53a153b277083868109b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182117
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
    Tested-by: Jenkins
    (cherry picked from commit c5f7619d6eebe478332ae4dcb27fde85df6278a9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182152
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -1644,7 +1644,7 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf125546)
     loadAndSave("tdf125546.docx");
     xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
     // compress redlines (it was 15)
-    assertXPath(pXmlDoc, "//w:rPrChange"_ostr, 2);
+    assertXPath(pXmlDoc, "//w:rPrChange"_ostr, 3);
 }
 
 CPPUNIT_TEST_FIXTURE(Test, testLabelWidthAndPosition_Left_FirstLineIndent)
