b4880f7f39a4acde5b78af8fd152d8d6bc149b93
Miklos Vajna <vmiklos@collabora.com>
2025-02-24 08:24:01 +0100

    sw doc model xml dump: show the redline manager's flags
    I'm looking at moving that RedlineFlags::On to the view.
    
    Change-Id: I9cd4d41108a19097d2bed0e1660c084ac7fe8c7a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182081
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/source/core/doc/docfmt.cxx
+++ sw/source/core/doc/docfmt.cxx
@@ -2023,8 +2023,7 @@ void SwDoc::dumpAsXml(xmlTextWriterPtr pWriter) const
     mpSectionFormatTable->dumpAsXml(pWriter);
     mpTableFrameFormatTable->dumpAsXml(pWriter, "tableFrameFormatTable");
     mpNumRuleTable->dumpAsXml(pWriter);
-    getIDocumentRedlineAccess().GetRedlineTable().dumpAsXml(pWriter);
-    getIDocumentRedlineAccess().GetExtraRedlineTable().dumpAsXml(pWriter);
+    getIDocumentRedlineAccess().dumpAsXml(pWriter);
     if (const SdrModel* pModel = getIDocumentDrawModelAccess().GetDrawModel())
         pModel->dumpAsXml(pWriter);
 
