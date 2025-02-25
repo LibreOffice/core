LibreOffice/core

commit e8bae67b3dbcc90ace8264b6b1aefaf0ce459aba

tdf#125894: DOCX: import tracked changes in frames
Tracked changes weren't imported in frames, losing also
the actual text content: also tracked deletion reappeared
as normal text here, not only the tracked insertions.

Change-Id: I5fc8135cfb7402ad71c5c24eae606aa7ed707dfc
Reviewed-on: https://gerrit.libreoffice.org/75779
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -251,6 +251,7 @@ DomainMapper_Impl::DomainMapper_Impl(
         m_bCheckFirstFootnoteTab(false),
         m_bIgnoreNextTab(false),
         m_bIsSplitPara(false),
+        m_bIsActualParagraphFramed( false ),
         m_vTextFramesForChaining(),
         m_bParaHadField(false),
         m_bParaAutoBefore(false),
@@ -2085,7 +2086,6 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
             default:
                 throw lang::IllegalArgumentException("illegal redline token type", nullptr, 0);
             }
-            uno::Reference < text::XRedline > xRedline( xRange, uno::UNO_QUERY_THROW );
             beans::PropertyValues aRedlineProperties( 3 );
             beans::PropertyValue * pRedlineProperties = aRedlineProperties.getArray(  );
             pRedlineProperties[0].Name = getPropertyName( PROP_REDLINE_AUTHOR );
@@ -2094,7 +2094,17 @@ void DomainMapper_Impl::CreateRedline(uno::Reference<text::XTextRange> const& xR
             pRedlineProperties[1].Value <<= ConversionHelper::ConvertDateStringToDateTime( pRedline->m_sDate );
             pRedlineProperties[2].Name = getPropertyName( PROP_REDLINE_REVERT_PROPERTIES );
             pRedlineProperties[2].Value <<= pRedline->m_aRevertProperties;
-            xRedline->makeRedline( sType, aRedlineProperties );
+            if (!m_bIsActualParagraphFramed)
+            {
+                uno::Reference < text::XRedline > xRedline( xRange, uno::UNO_QUERY_THROW );
+                xRedline->makeRedline( sType, aRedlineProperties );
+            }
+            else
+            {
+                aFramedRedlines.push_back( uno::makeAny(xRange) );
+                aFramedRedlines.push_back( uno::makeAny(sType) );
+                aFramedRedlines.push_back( uno::makeAny(aRedlineProperties) );
+            }
         }
         catch( const uno::Exception & )
         {
@@ -5709,15 +5719,46 @@ void DomainMapper_Impl::ExecuteFrameConversion()
         try
         {
             uno::Reference< text::XTextAppendAndConvert > xTextAppendAndConvert( GetTopTextAppend(), uno::UNO_QUERY_THROW );
-            xTextAppendAndConvert->convertToTextFrame(
+            // convert redline ranges to cursor movement and character length
+            std::vector<sal_Int32> redPos, redLen;
+            for( size_t i = 0; i < aFramedRedlines.size(); i+=3)
+            {
+                uno::Reference< text::XTextRange > xRange;
+                aFramedRedlines[i] >>= xRange;
+                uno::Reference<text::XTextCursor> xRangeCursor = GetTopTextAppend()->createTextCursorByRange( xRange );
+                sal_Int32 nLen = xRange->getString().getLength();
+                redLen.push_back(nLen);
+                xRangeCursor->gotoRange(m_xFrameStartRange, true);
+                redPos.push_back(xRangeCursor->getString().getLength() - nLen);
+            }
+
+            const uno::Reference< text::XTextContent >& xTextContent = xTextAppendAndConvert->convertToTextFrame(
                 m_xFrameStartRange,
                 m_xFrameEndRange,
                 comphelper::containerToSequence(m_aFrameProperties) );
+
+            // create redlines in the previous frame
+            for( size_t i = 0; i < aFramedRedlines.size(); i+=3)
+            {
+                OUString sType;
+                beans::PropertyValues aRedlineProperties( 3 );
+                aFramedRedlines[i+1] >>= sType;
+                aFramedRedlines[i+2] >>= aRedlineProperties;
+                uno::Reference< text::XTextFrame > xFrame( xTextContent, uno::UNO_QUERY_THROW );
+                uno::Reference< text::XTextCursor > xCrsr = xFrame->getText()->createTextCursor();
+                xCrsr->goRight(redPos[i/3], false);
+                xCrsr->goRight(redLen[i/3], true);
+                uno::Reference < text::XRedline > xRedline( xCrsr, uno::UNO_QUERY_THROW );
+                xRedline->makeRedline( sType, aRedlineProperties );
+            }
         }
         catch( const uno::Exception&)
         {
             DBG_UNHANDLED_EXCEPTION( "writerfilter.dmapper", "Exception caught when converting to frame");
         }
+
+        m_bIsActualParagraphFramed = false;
+        aFramedRedlines.clear();
     }
     m_xFrameStartRange = nullptr;
     m_xFrameEndRange = nullptr;

sw/qa/extras/ooxmlexport/ooxmlexport11.cxx
@@ -883,6 +883,14 @@ DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf126245, "tdf126245.docx")
     assertXPath(pXmlDoc, "/w:document/w:body/w:p[1]/w:pPr/w:pPrChange/w:pPr/w:numPr/w:numId", "val", "1");
 }
 
+DECLARE_OOXMLEXPORT_EXPORTONLY_TEST(testTdf125894, "tdf125894.docx")
+{
+    xmlDocPtr pXmlDoc = parseExport("word/document.xml");
+    // import change tracking in frames
+    assertXPath(pXmlDoc, "//w:del", 2);
+    assertXPath(pXmlDoc, "//w:ins");
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf118691, "tdf118691.docx")
 {
     uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1991,6 +1991,8 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
             ParagraphPropertyMap* pParaContext = dynamic_cast< ParagraphPropertyMap* >( pContext.get() );
             if (pParaContext)
                 pParaContext->SetFrameMode();
+
+            m_pImpl->m_bIsActualParagraphFramed = true;
         }
         else
         {

writerfilter/source/dmapper/DomainMapper_Impl.hxx
@@ -1004,6 +1004,11 @@ public:
     /// start/end node.
     void ClearPreviousParagraph();
 
+    /// Handle redline text portions in frames:
+    /// store their data, and create them after frame creation
+    bool m_bIsActualParagraphFramed;
+    std::vector<css::uno::Any> aFramedRedlines;
+
 private:
     void PushPageHeaderFooter(bool bHeader, SectionPropertyMap::PageType eType);
     std::vector<css::uno::Reference< css::drawing::XShape > > m_vTextFramesForChaining ;

commit d09f3f24c38eda633825f4ac214731db06bc9a9a

tdf#165330 writerfilter: fix import of redline in paragraph after frame
The flag m_bIsActualParagraphFramed is reset only in
ExecuteFrameConversion() which is called at the end of the paragraph
that follows the one that is in the frame, so all redlines in that
paragraph were erroneously moved to the frame; reset it when a paragraph
ends, if the next paragraph is also in the frame it can set the flag
again.

(regression from commit e8bae67b3dbcc90ace8264b6b1aefaf0ce459aba)

Change-Id: I75a18d2e7b600737958e984c135ebf6201e6f979
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182181
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -3105,6 +3105,7 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
     else
         SetIsPreviousParagraphFramed(false);
 
+    m_bIsActualParagraphFramed = false;
     m_StreamStateStack.top().bRemoveThisParagraph = false;
     if( !IsInHeaderFooter() && !IsInShape()
         && (!pParaContext || !pParaContext->props().IsFrameMode()) )
@@ -9713,8 +9714,6 @@ void DomainMapper_Impl::ExecuteFrameConversion()
         {
             DBG_UNHANDLED_EXCEPTION( "writerfilter.dmapper", "Exception caught when converting to frame");
         }
-
-        m_bIsActualParagraphFramed = false;
 
         if (redPos.size() == m_aStoredRedlines[StoredRedlines::FRAME].size())
         {

sw/qa/extras/ooxmlexport/ooxmlexport12.cxx
@@ -1603,6 +1603,16 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf128156)
     assertXPath(pXmlDoc, "//w:ins", 2);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf165330)
+{
+    loadAndSave("CT-with-frame.docx");
+
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+    // check that *both* tracked changes were round tripped
+    assertXPathContent(pXmlDoc, "//w:p[1]/w:ins/w:r/w:t", u"world! ");
+    assertXPathContent(pXmlDoc, "//w:p[3]/w:ins/w:r/w:t", u"hello ");
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testTdf125546)
 {
     loadAndSave("tdf125546.docx");