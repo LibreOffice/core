9e2e98cf8676398542555c42df21458dec0b356a
Thorsten Behrens <thorsten.behrens@allotropia.de>
2025-02-26 06:19:18 +0100

    Add firebird intl DLLs for code signing
    Change-Id: Ic67f51ddc2e8957c4cc228a98a888a4129aac36e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182198
    Tested-by: Jenkins
    Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

--- postprocess/CustomTarget_signing.mk
+++ postprocess/CustomTarget_signing.mk
@@ -55,6 +55,7 @@ ifneq ($(ENABLE_DBGUTIL),TRUE)
 			$(INSTDIR)/$(LIBO_BIN_FOLDER)/python-core-$(PYTHON_VERSION)/lib/*.pyd \
 			$(INSTDIR)/$(LIBO_BIN_FOLDER)/python-core-$(PYTHON_VERSION)/lib/distutils/command/*.exe \
 			$(INSTDIR)/program/shlxthdl/*.dll \
+			$(INSTDIR)/intl/*.dll \
 			$(INSTDIR)/sdk/cli/*.dll \
 			$(INSTDIR)/sdk/bin/*.exe \
 	&& rm $$EXCLUDELIST && touch $@
