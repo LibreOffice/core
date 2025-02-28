6ba6e2203d3ab6b78d9086f5e30937fde6b254e2
Michael Stahl <michael.stahl@allotropia.de>
2025-02-28 19:19:28 +0100

    tdf#165351 sw: fix crash after Undo of inserting table row...
    ... if an existing row of the table has a drawing object anchored to it
    as character, and the table is in a fly frame.
    
    The problem is that the SwTextFrame inside the table cell doesn't have
    the mbInfFly flag set, because SetInfFlags() was called too early, when
    the frame was attached to its SwCellFrame but the SwCellFrame didn't
    have an upper yet:
    
     0  SwFrame::SetInfFlags () at sw/source/core/layout/findfrm.cxx:1600
     1  SwFrame::IsInSct () at sw/source/core/inc/frame.hxx:1009
     2  SwFrame::FindSctFrame () at sw/source/core/inc/frame.hxx:1178
     3  SwFrame::IsHiddenNow () at sw/source/core/layout/trvlfrm.cxx:1734
     4  SwTextFrame::IsHiddenNowImpl () at sw/source/core/text/txtfrm.cxx:1514
     5  SwTextFrame::Prepare (ePrep=PrepareHint::FlyFrameAttributesChanged) at sw/source/core/text/txtfrm.cxx:3031
     6  SwAnchoredDrawObject::InvalidateObjPos () at sw/source/core/layout/anchoreddrawobject.cxx:595
     7  SwFrame::AppendDrawObj () at sw/source/core/layout/fly.cxx:2803
     8  AppendObj () at sw/source/core/layout/frmtool.cxx:1079
     9  AppendObjsOfNode () at sw/source/core/layout/frmtool.cxx:1277
     10 AppendObjs () at sw/source/core/layout/frmtool.cxx:1330
     11 InsertCnt_() at sw/source/core/layout/frmtool.cxx:1666
     12 SwCellFrame::SwCellFrame () at sw/source/core/layout/tabfrm.cxx:5729
     13 SwRowFrame::SwRowFrame () at sw/source/core/layout/tabfrm.cxx:4627
     14 lcl_InsertRow () at sw/source/core/frmedt/tblsel.cxx:2053
     15 FndBox_::MakeFrames () at sw/source/core/frmedt/tblsel.cxx:2402
     16 SaveTable::CreateNew () at sw/source/core/undo/untbl.cxx:1100
     17 SwUndoTableNdsChg::UndoImpl () at sw/source/core/undo/untbl.cxx:1783
    
    Reset flags in InsertCnt_().
    
    (regression from commit 0a3f5169acc6708c352cad0fa07fdb4af8c8b2dd)
    
    Conflicts:
    	sw/qa/extras/uiwriter/uiwriter9.cxx
    
    Change-Id: I048196278b314ea516bd5a4877b3e86f696dfce0
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182367
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

--- sw/qa/extras/uiwriter/uiwriter9.cxx
+++ sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -39,6 +39,7 @@
 #include <fmtfsize.hxx>
 #include <fmtinfmt.hxx>
 #include <rootfrm.hxx>
+#include <svx/svxids.hrc>
 
 namespace
 {
@@ -305,6 +306,23 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf159816)
     // or in BigPtrArray::operator[] (tdf#159816):
     // Assertion failed: idx < m_nSize
     xTransfer->PrivateDrop(*pWrtShell, ptTo, /*bMove=*/true, /*bXSelection=*/true);
+}
+
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf165351)
+{
+    createSwDoc("tdf165351.fodt");
+
+    SwDoc* pDoc = getSwDoc();
+    SwWrtShell* pWrtShell = getSwDocShell()->GetWrtShell();
+
+    // Move the cursor into the fly frame
+    pWrtShell->GotoFly(u"Frame1"_ustr, FLYCNTTYPE_FRM, false);
+    pWrtShell->EndOfSection();
+    pWrtShell->GoNextCell(/*bAppendLine=*/true);
+    pWrtShell->Undo();
+    // getting this item crashed
+    SfxItemSet temp{ pDoc->GetAttrPool(), svl::Items<SID_RULER_LR_MIN_MAX, SID_RULER_LR_MIN_MAX> };
+    pWrtShell->GetView().StateTabWin(temp);
 }
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf160898)
