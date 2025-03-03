LibreOffice/core

commit 857dd6000c877f2c6d8bb73806a8557fa0baea73

tdf#161810 sw smart justify: fix overhanging lines containing tabs
Length of tabulator portions wasn't taken into account
during calculating overhanging lines, resulting missing
space shrinking.

Regression from commit 17eaebee279772b6062ae3448012133897fc71bb
"tdf#119908 sw smart justify: fix justification by shrinking".

Change-Id: I34bb007940b91e861b24413e76c910ee62aed158
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173451
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

sw/source/core/text/itradj.cxx
@@ -58,10 +58,9 @@ void SwTextAdjuster::FormatBlock( )
         const SwLinePortion *pPos = m_pCurr->GetNextPortion();
         while( pPos && bSkip )
         {
-            if( !pPos->InGlueGrp() &&
-                // don't calculate with the terminating space,
+            if( // don't calculate with the terminating space,
                 // otherwise it would result justified line mistakenly
-                ( pPos->GetNextPortion() || !pPos->IsHolePortion() ) )
+                pPos->GetNextPortion() || !pPos->IsHolePortion() )
             {
                 nBreakWidth += pPos->Width();
             }
@@ -343,6 +342,8 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
 
     while( pPos )
     {
+        nBreakWidth += pPos->Width();
+
         if ( ( bDoNotJustifyLinesWithManualBreak || bDoNotJustifyTab ) &&
              pPos->IsBreakPortion() && !IsLastBlock() )
         {
@@ -456,10 +457,6 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
             }
             else
                 ++nGluePortion;
-        }
-        else
-        {
-            nBreakWidth += pPos->Width();
         }
         GetInfo().SetIdx( GetInfo().GetIdx() + pPos->GetLen() );
         if ( pPos == pStopAt )

sw/qa/extras/layout/layout3.cxx
@@ -13,6 +13,7 @@
 #include <com/sun/star/text/WrapTextMode.hpp>
 #include <com/sun/star/text/XTextSectionsSupplier.hpp>
 #include <vcl/event.hxx>
+#include <vcl/metaact.hxx>
 #include <vcl/scheduler.hxx>
 #include <editeng/fontitem.hxx>
 #include <editeng/fhgtitem.hxx>
@@ -395,6 +396,40 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf162725)
     // There was no SwGluePortion, because of missing justification of the last paragraph line,
     // despite it is a full line with shrunk spaces
     assertXPath(pXmlDoc, "/root/page/body/txt[1]/SwParaPortion/SwLineLayout[1]/SwGluePortion"_ostr);
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf161810)
+{
+    createSwDoc("tdf161810.fodt");
+    // Ensure that all text portions are calculated before testing.
+    SwDoc* pDoc = getSwDoc();
+    SwDocShell* pShell = pDoc->GetDocShell();
+
+    // Dump the rendering of the first page as an XML file.
+    std::shared_ptr<GDIMetaFile> xMetaFile = pShell->GetPreviewMetaFile();
+    MetafileXmlDump dumper;
+
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(dumper, *xMetaFile);
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // Find the first text array action
+    for (size_t nAction = 0; nAction < xMetaFile->GetActionSize(); nAction++)
+    {
+        auto pAction = xMetaFile->GetAction(nAction);
+        if (pAction->GetType() == MetaActionType::TEXTARRAY)
+        {
+            auto pTextArrayAction = static_cast<MetaTextArrayAction*>(pAction);
+            auto pDXArray = pTextArrayAction->GetDXArray();
+
+            // There should be 73 chars on the first line
+            CPPUNIT_ASSERT_EQUAL(size_t(73), pDXArray.size());
+
+            // Assert we are using the expected position for the last char
+            // This was 9369, now 9165, according to the fixed space shrinking
+            CPPUNIT_ASSERT_LESS(sal_Int32(9300), pDXArray[72]);
+            break;
+        }
+    }
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf132599_always)

commit 434af9dea64f95e1d5f93a5563106b9124bb17af

tdf#164905 tdf#164499 sw smart justify: fix overlapping text in ToC
Unnecessary calculation with the width of the glue portions resulted
bad ToC, i.e. text lines with tabulators got overlapping text.

Regression from commit 857dd6000c877f2c6d8bb73806a8557fa0baea73
"tdf#161810 sw smart justify: fix overhanging lines containing tabs".

Revert only the calculation, but not the unit test, which is still
correct, according to commit ed7fbfce0b434f7d2a5542483d2838af51958e34
"tdf#164499 sw smart justify: disable in tabulated lines to fix ToC".

Change-Id: I3fe6d0d56ea8593ddee23d3c4063024762b65bab
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182433
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/itradj.cxx
@@ -61,9 +61,10 @@ void SwTextAdjuster::FormatBlock( )
         const SwLinePortion *pPos = m_pCurr->GetNextPortion();
         while( pPos && bSkip )
         {
-            if( // don't calculate with the terminating space,
+            if( !pPos->InGlueGrp() &&
+                // don't calculate with the terminating space,
                 // otherwise it would result justified line mistakenly
-                pPos->GetNextPortion() || !pPos->IsHolePortion() )
+                ( pPos->GetNextPortion() || !pPos->IsHolePortion() ) )
             {
                 nBreakWidth += pPos->Width();
             }
@@ -426,8 +427,6 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
 
     while( pPos )
     {
-        nBreakWidth += pPos->Width();
-
         if ( ( bDoNotJustifyLinesWithManualBreak || bDoNotJustifyTab ) &&
              pPos->IsBreakPortion() && !IsLastBlock() )
         {
@@ -551,6 +550,10 @@ void SwTextAdjuster::CalcNewBlock( SwLineLayout *pCurrent,
             }
             else
                 ++nGluePortion;
+        }
+        else
+        {
+            nBreakWidth += pPos->Width();
         }
         GetInfo().SetIdx( GetInfo().GetIdx() + pPos->GetLen() );
         if ( pPos == pStopAt )

sw/qa/extras/layout/layout3.cxx
@@ -488,6 +488,22 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf161810)
     }
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf164905)
+{
+    createSwDoc("tdf164905.docx");
+    // Ensure that all text portions are calculated before testing.
+    SwViewShell* pViewShell = getSwDoc()->getIDocumentLayoutAccess().GetCurrentViewShell();
+    CPPUNIT_ASSERT(pViewShell);
+    pViewShell->Reformat();
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    // This was 9 (resulting broken ToC layout)
+    assertXPath(pXmlDoc, "//SwGluePortion", 3);
+    // For example, it was an unnecessary glue portion here
+    assertXPath(pXmlDoc,
+                "/root/page/body/section[2]/txt[1]/SwParaPortion/SwLineLayout/SwGluePortion", 0);
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf163149)
 {
     createSwDoc("tdf163149.docx");