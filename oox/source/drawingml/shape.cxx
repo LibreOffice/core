89d9c6b41a084e99ef705bf5af39b621d392c435
Balazs Varga <balazs.varga.extern@allotropia.de>
2025-03-06 13:29:07 +0100

    tdf#165321 - sd ooxml import fix missing shapes or shapes with near
    zero height
    
    Use the calculated shapes size (before we calculate the shape size with
    NbcAdjustTextFrameWidthAndHeight if the autoTextGrowHeight is true) instead of the shape size directly from the xml, which need to be transformed in some cases.
    
    Regression from commit: ebf13e890766f4cadbba81444784cfe3c8e1bbc3
    (tdf#156857: sd ooxml import: fix shape size if spAutofit is set)
    
    Change-Id: Icddaf22d10a89b172a3f1ca3e55150162e51a877
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182581
    Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
    Tested-by: Jenkins
    Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
    (cherry picked from commit 620b89adea77519fd5d057cac7e1acf8a6fb2544)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182638
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- oox/source/drawingml/shape.cxx
+++ oox/source/drawingml/shape.cxx
@@ -1235,6 +1235,7 @@ Reference< XShape > const & Shape::createAndInsert(
         }
     }
     HomogenMatrix3 aMatrix;
+    tools::Rectangle aOrigSize;
     if ( aServiceName == "com.sun.star.drawing.ConnectorShape" )
     {
         ::basegfx::B2DPolygon aPoly;
@@ -2007,6 +2008,10 @@ Reference< XShape > const & Shape::createAndInsert(
         if (mbWps && aServiceName == "com.sun.star.drawing.LineShape" && !pParentGroupShape)
             mxShape->setPosition(maPosition);
 
+        SdrObject* pShape = SdrObject::getSdrObjectFromXShape(mxShape);
+        if (pShape)
+            aOrigSize = pShape->GetLogicRect();
+
         if (bIsConnectorShape)
         {
             msConnectorName = mpCustomShapePropertiesPtr->getShapePresetTypeName();
@@ -2304,16 +2309,14 @@ Reference< XShape > const & Shape::createAndInsert(
             }
 
             SdrObject* pShape = SdrObject::getSdrObjectFromXShape(mxShape);
-            if (pShape && bAutoHeight && bIsCustomShape)
+            if (pShape && bAutoHeight)
             {
-                tools::Rectangle aOrigSize(aShapeRectHmm.X, aShapeRectHmm.Y,
-                    aShapeRectHmm.X + aShapeRectHmm.Width, aShapeRectHmm.Y + aShapeRectHmm.Height);
-                tools::Rectangle aAdaptSize = pShape->GetLogicRect();
-                // a little tolerance same as in \svx\source\svdraw\svdoashp.cxx:AdjustTextFrameWidthAndHeight
-                if (std::abs(aOrigSize.GetHeight() - aAdaptSize.GetHeight()) > 1)
+                tools::Rectangle aAutoSize = pShape->GetLogicRect();
+                // little tolerance same as in \svx\source\svdraw\svdoashp.cxx:AdjustTextFrameWidthAndHeight
+                if (!aOrigSize.IsEmpty() && (std::abs(aOrigSize.GetHeight() - aAutoSize.GetHeight()) > 1 ||
+                    std::abs(aOrigSize.GetWidth() - aAutoSize.GetWidth()) > 1))
                 {
-                    aAdaptSize.setHeight(aOrigSize.GetHeight());
-                    pShape->NbcSetLogicRect(aAdaptSize, false);
+                    pShape->NbcSetLogicRect(aOrigSize, false);
                 }
             }
         }
