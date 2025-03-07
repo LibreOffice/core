02b17fe003c34481ba166b1f9d493662e372682e
Mike Kaganski <mike.kaganski@collabora.com>
2025-03-07 16:55:18 +0500

    ReqIF export: avoid writing border properties
    Change-Id: I05e7ba3fda54300f99fd2121aaa5d167dd27851c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182617
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 75135c6c7d43b9d4f979a026cdd2529209cb9dea)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182634
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/qa/extras/htmlexport/htmlexport2.cxx
+++ sw/qa/extras/htmlexport/htmlexport2.cxx
@@ -1622,6 +1622,20 @@ CPPUNIT_TEST_FIXTURE(SwHtmlDomExportTest, testReqifNoTargetInA)
     assertXPathNoAttribute(pXmlDoc, "//reqif-xhtml:a", "target");
 }
 
+CPPUNIT_TEST_FIXTURE(SwHtmlDomExportTest, testReqifNoObjectBorderExport)
+{
+    // Given a document with an image with black border
+    createSwDoc("img_with_border.fodt");
+
+    // When exporting to XHTML:
+    ExportToReqif();
+
+    // Check that there's no 'font' element that used to be exported, containing the border color:
+    xmlDocUniquePtr pXmlDoc = WrapReqifFromTempFile();
+    // Without the accompanying fix in place, this test would have failed
+    assertXPath(pXmlDoc, "//reqif-xhtml:font", 0);
+}
+
 } // end of anonymous namespace
 CPPUNIT_PLUGIN_IMPLEMENT();
 
