LibreOffice/core

commit ebf13e890766f4cadbba81444784cfe3c8e1bbc3

tdf#156857: sd ooxml import: fix shape size if spAutofit is set
and the size of the shape from ooxml file is different then the
inew calculated by TextAutoGrowHeight.

Furthermore, no need to check and resize the shape size during
the spell checking.

Change-Id: Iffd15e02579942d4f5f20db73f2194f8298c0838
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171621
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

oox/source/drawingml/shape.cxx
@@ -2166,6 +2166,31 @@ Reference< XShape > const & Shape::createAndInsert(
             rFilterBase.getMediaDescriptor()[utl::MediaDescriptor::PROP_DOCUMENTSERVICE] >>= sDocumentService;
             if (sDocumentService != u"com.sun.star.text.TextDocument")
                 mpTextBody->getTextProperties().readjustTextDistances(mxShape);
+
+            // tdf#156857: ooxml files can have shape size with spAutoFit=true and the first priority of
+            // shape size is the fix size even if TextAutoGrowHeight is true.
+            bool bAutoHeight = false;
+            Reference< XPropertySetInfo > xSetInfo(xSet->getPropertySetInfo());
+            const OUString& rPropName = PropertyMap::getPropertyName(PROP_TextAutoGrowHeight);
+            if (xSetInfo.is() && xSetInfo->hasPropertyByName(rPropName))
+            {
+                uno::Any aTextAutoGrowHeight = xSet->getPropertyValue(u"TextAutoGrowHeight"_ustr);
+                aTextAutoGrowHeight >>= bAutoHeight;
+            }
+
+            SdrObject* pShape = SdrObject::getSdrObjectFromXShape(mxShape);
+            if (pShape && bAutoHeight && bIsCustomShape)
+            {
+                tools::Rectangle aOrigSize(aShapeRectHmm.X, aShapeRectHmm.Y,
+                    aShapeRectHmm.X + aShapeRectHmm.Width, aShapeRectHmm.Y + aShapeRectHmm.Height);
+                tools::Rectangle aAdaptSize = pShape->GetLogicRect();
+                // a little tolerance same as in \svx\source\svdraw\svdoashp.cxx:AdjustTextFrameWidthAndHeight
+                if (std::abs(aOrigSize.GetHeight() - aAdaptSize.GetHeight()) > 1)
+                {
+                    aAdaptSize.setHeight(aOrigSize.GetHeight());
+                    pShape->NbcSetLogicRect(aAdaptSize, false);
+                }
+            }
         }
     }
     return mxShape;

sd/qa/unit/data/xml/n593612_0.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <XShapes>
- <XShape positionX="11429" positionY="1324" sizeX="2259" sizeY="15124" type="com.sun.star.drawing.CustomShape" name="Rectangle 52" text="&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;" fontHeight="24.000000" fontColor="ffffffff" textAutoGrowHeight="true" textAutoGrowWidth="false" textContourFrame="false" textFitToSize="NONE" textHorizontalAdjust="CENTER" textVerticalAdjust="TOP" textLeftDistance="254" textRightDistance="254" textUpperDistance="127" textLowerDistance="127" textMaximumFrameHeight="0" textMaximumFrameWidth="0" textMinimumFrameHeight="0" textMinimumFrameWidth="0" textAnimationAmount="0" textAnimationCount="0" textAnimationDelay="0" textAnimationDirection="LEFT" textAnimationKind="NONE" textAnimationStartInside="false" textAnimationStopInside="false" textWritingMode="LR_TB" fillStyle="SOLID" fillColor="3c8c93" fillTransparence="0" fillTransparenceGradientName="">
+ <XShape positionX="11429" positionY="1324" sizeX="2259" sizeY="15971" type="com.sun.star.drawing.CustomShape" name="Rectangle 52" text="&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;" fontHeight="24.000000" fontColor="ffffffff" textAutoGrowHeight="true" textAutoGrowWidth="false" textContourFrame="false" textFitToSize="NONE" textHorizontalAdjust="CENTER" textVerticalAdjust="TOP" textLeftDistance="254" textRightDistance="254" textUpperDistance="127" textLowerDistance="127" textMaximumFrameHeight="0" textMaximumFrameWidth="0" textMinimumFrameHeight="0" textMinimumFrameWidth="0" textAnimationAmount="0" textAnimationCount="0" textAnimationDelay="0" textAnimationDirection="LEFT" textAnimationKind="NONE" textAnimationStartInside="false" textAnimationStopInside="false" textWritingMode="LR_TB" fillStyle="SOLID" fillColor="3c8c93" fillTransparence="0" fillTransparenceGradientName="">
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="3465a4" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
@@ -10,7 +10,7 @@
   <LineEnd/>
   <Transformation>
    <Line1 column1="2260.000000" column2="0.000000" column3="11429.000000"/>
-   <Line2 column1="0.000000" column2="15125.000000" column3="1324.000000"/>
+   <Line2 column1="0.000000" column2="15972.000000" column3="1324.000000"/>
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <CustomShapeGeometry>

sd/qa/unit/import-tests2.cxx
@@ -111,18 +111,18 @@ CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf160490)
     sal_Int32 nHeight1 = xShape1->getSize().Height;
 
     // Without the fix in place, this test would have failed with
-    // Expected: placeholder height: 2627
+    // Expected: placeholder height: 3728
     // Actual  : placeholder height: 3476
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(2627), nHeight1);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3728), nHeight1);
 
     uno::Reference<drawing::XShape> xShape2(getShapeFromPage(0, 1), uno::UNO_QUERY);
     CPPUNIT_ASSERT(xShape2.is());
     sal_Int32 nHeight2 = xShape2->getSize().Height;
 
     // Without the fix in place, this test would have failed with
-    // Expected: placeholder height: 2373
+    // Expected: placeholder height: 3367
     // Actual  : placeholder height: 3116
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(2373), nHeight2);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3367), nHeight2);
 }
 
 CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf157285)

include/svx/svdoashp.hxx
@@ -218,7 +218,7 @@ public:
     virtual basegfx::B2DPolyPolygon TakeXorPoly() const override;
     virtual basegfx::B2DPolyPolygon TakeContour() const override;
 
-    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject) override;
+    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject, bool bAdjustTextFrameWidthAndHeight = true) override;
 
     virtual rtl::Reference<SdrObject> DoConvertToPolyObj(bool bBezier, bool bAddText) const override;
 

include/svx/svdobj.hxx
@@ -633,7 +633,7 @@ public:
     // keep text in outliner's format
     // SetOutlinerParaObject: transfer ownership of *pTextObject!
     void SetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject);
-    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject);
+    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject, bool bAdjustTextFrameWidthAndHeight = true);
     virtual OutlinerParaObject* GetOutlinerParaObject() const;
     virtual void NbcReformatText();
 

include/svx/svdomeas.hxx
@@ -138,7 +138,7 @@ public:
     SAL_DLLPRIVATE virtual void TakeTextAnchorRect(tools::Rectangle& rAnchorRect) const override;
     SAL_DLLPRIVATE virtual void TakeTextEditArea(Size* pPaperMin, Size* pPaperMax, tools::Rectangle* pViewInit, tools::Rectangle* pViewMin) const override;
     SAL_DLLPRIVATE virtual EEAnchorMode GetOutlinerViewAnchorMode() const override;
-    SAL_DLLPRIVATE virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject) override;
+    SAL_DLLPRIVATE virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject, bool bAdjustTextFrameWidthAndHeight = true) override;
     SAL_DLLPRIVATE virtual OutlinerParaObject* GetOutlinerParaObject() const override;
 
     SAL_DLLPRIVATE virtual bool CalcFieldValue(const SvxFieldItem& rField, sal_Int32 nPara, sal_uInt16 nPos,

include/svx/svdotable.hxx
@@ -239,7 +239,7 @@ public:
     void TakeTextEditArea(const sdr::table::CellPos& rPos, Size* pPaperMin, Size* pPaperMax, tools::Rectangle* pViewInit, tools::Rectangle* pViewMin) const;
     virtual EEAnchorMode GetOutlinerViewAnchorMode() const override;
 
-    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject) override;
+    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject, bool bAdjustTextFrameWidthAndHeight = true) override;
 
     virtual OutlinerParaObject* GetOutlinerParaObject() const override;
 

include/svx/svdotext.hxx
@@ -518,7 +518,7 @@ public:
     virtual void EndTextEdit(SdrOutliner& rOutl);
     virtual EEAnchorMode GetOutlinerViewAnchorMode() const;
 
-    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject) override;
+    virtual void NbcSetOutlinerParaObject(std::optional<OutlinerParaObject> pTextObject, bool bAdjustTextFrameWidthAndHeight = true) override;
     // @param bAdjustTextFrameWidthAndHeight pass false if you know it is safe to avoid the cost of doing
     //              text layout right now.
     void NbcSetOutlinerParaObjectForText( std::optional<OutlinerParaObject> pTextObject, SdrText* pText, bool bAdjustTextFrameWidthAndHeight = true );

commit 620b89adea77519fd5d057cac7e1acf8a6fb2544

tdf#165321 - sd ooxml import fix missing shapes or shapes with near
zero height

Use the calculated shapes size (before we calculate the shape size with
NbcAdjustTextFrameWidthAndHeight if the autoTextGrowHeight is true) instead of the shape size directly from the xml, which need to be transformed in some cases.

Regression from commit: ebf13e890766f4cadbba81444784cfe3c8e1bbc3
(tdf#156857: sd ooxml import: fix shape size if spAutofit is set)

Change-Id: Icddaf22d10a89b172a3f1ca3e55150162e51a877
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182581
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Tested-by: Jenkins
Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

oox/source/drawingml/shape.cxx
@@ -1235,6 +1235,7 @@ Reference< XShape > const & Shape::createAndInsert(
         }
     }
     HomogenMatrix3 aMatrix;
+    tools::Rectangle aOrigSize;
     if ( aServiceName == "com.sun.star.drawing.ConnectorShape" )
     {
         ::basegfx::B2DPolygon aPoly;
@@ -2002,6 +2003,10 @@ Reference< XShape > const & Shape::createAndInsert(
         if (mbWps && aServiceName == "com.sun.star.drawing.LineShape" && !pParentGroupShape)
             mxShape->setPosition(maPosition);
 
+        SdrObject* pShape = SdrObject::getSdrObjectFromXShape(mxShape);
+        if (pShape)
+            aOrigSize = pShape->GetLogicRect();
+
         if (bIsConnectorShape)
         {
             msConnectorName = mpCustomShapePropertiesPtr->getShapePresetTypeName();
@@ -2299,16 +2304,14 @@ Reference< XShape > const & Shape::createAndInsert(
             }
 
             SdrObject* pShape = SdrObject::getSdrObjectFromXShape(mxShape);
-            if (pShape && bAutoHeight && bIsCustomShape)
+            if (pShape && bAutoHeight)
             {
-                tools::Rectangle aOrigSize(aShapeRectHmm.X, aShapeRectHmm.Y,
-                    aShapeRectHmm.X + aShapeRectHmm.Width, aShapeRectHmm.Y + aShapeRectHmm.Height);
-                tools::Rectangle aAdaptSize = pShape->GetLogicRect();
-                // a little tolerance same as in \svx\source\svdraw\svdoashp.cxx:AdjustTextFrameWidthAndHeight
-                if (std::abs(aOrigSize.GetHeight() - aAdaptSize.GetHeight()) > 1)
+                tools::Rectangle aAutoSize = pShape->GetLogicRect();
+                // little tolerance same as in \svx\source\svdraw\svdoashp.cxx:AdjustTextFrameWidthAndHeight
+                if (!aOrigSize.IsEmpty() && (std::abs(aOrigSize.GetHeight() - aAutoSize.GetHeight()) > 1 ||
+                    std::abs(aOrigSize.GetWidth() - aAutoSize.GetWidth()) > 1))
                 {
-                    aAdaptSize.setHeight(aOrigSize.GetHeight());
-                    pShape->NbcSetLogicRect(aAdaptSize, false);
+                    pShape->NbcSetLogicRect(aOrigSize, false);
                 }
             }
         }

sd/qa/unit/data/xml/n593612_0.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <XShapes>
- <XShape positionX="11429" positionY="1324" sizeX="2259" sizeY="15971" type="com.sun.star.drawing.CustomShape" name="Rectangle 52" text="&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;" fontHeight="24.000000" fontColor="ffffffff" textAutoGrowHeight="true" textAutoGrowWidth="false" textContourFrame="false" textFitToSize="NONE" textHorizontalAdjust="CENTER" textVerticalAdjust="TOP" textLeftDistance="254" textRightDistance="254" textUpperDistance="127" textLowerDistance="127" textMaximumFrameHeight="0" textMaximumFrameWidth="0" textMinimumFrameHeight="0" textMinimumFrameWidth="0" textAnimationAmount="0" textAnimationCount="0" textAnimationDelay="0" textAnimationDirection="LEFT" textAnimationKind="NONE" textAnimationStartInside="false" textAnimationStopInside="false" textWritingMode="LR_TB" fillStyle="SOLID" fillColor="3c8c93" fillTransparence="0" fillTransparenceGradientName="">
+ <XShape positionX="11429" positionY="1324" sizeX="2259" sizeY="15969" type="com.sun.star.drawing.CustomShape" name="Rectangle 52" text="&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;&#10;" fontHeight="24.000000" fontColor="ffffffff" textAutoGrowHeight="true" textAutoGrowWidth="false" textContourFrame="false" textFitToSize="NONE" textHorizontalAdjust="CENTER" textVerticalAdjust="TOP" textLeftDistance="254" textRightDistance="254" textUpperDistance="127" textLowerDistance="127" textMaximumFrameHeight="0" textMaximumFrameWidth="0" textMinimumFrameHeight="0" textMinimumFrameWidth="0" textAnimationAmount="0" textAnimationCount="0" textAnimationDelay="0" textAnimationDirection="LEFT" textAnimationKind="NONE" textAnimationStartInside="false" textAnimationStopInside="false" textWritingMode="LR_TB" fillStyle="SOLID" fillColor="3c8c93" fillTransparence="0" fillTransparenceGradientName="">
   <FillTransparenceGradient style="LINEAR" startColor="000000" endColor="000000" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillGradient style="LINEAR" startColor="3465a4" endColor="ffffff" angle="0" border="0" xOffset="50" yOffset="50" startIntensity="100" endIntensity="100" stepCount="0"/>
   <FillHatch style="SINGLE" color="3465a4" distance="20" angle="0"/>
@@ -10,7 +10,7 @@
   <LineEnd/>
   <Transformation>
    <Line1 column1="2260.000000" column2="0.000000" column3="11429.000000"/>
-   <Line2 column1="0.000000" column2="15972.000000" column3="1324.000000"/>
+   <Line2 column1="0.000000" column2="15970.000000" column3="1324.000000"/>
    <Line3 column1="0.000000" column2="0.000000" column3="1.000000"/>
   </Transformation>
   <CustomShapeGeometry>

sd/qa/unit/import-tests2.cxx
@@ -111,18 +111,41 @@ CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf160490)
     sal_Int32 nHeight1 = xShape1->getSize().Height;
 
     // Without the fix in place, this test would have failed with
-    // Expected: placeholder height: 3728
+    // Expected: placeholder height: 3726
     // Actual  : placeholder height: 3476
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(3728), nHeight1);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3726), nHeight1);
 
     uno::Reference<drawing::XShape> xShape2(getShapeFromPage(0, 1), uno::UNO_QUERY);
     CPPUNIT_ASSERT(xShape2.is());
     sal_Int32 nHeight2 = xShape2->getSize().Height;
 
     // Without the fix in place, this test would have failed with
-    // Expected: placeholder height: 3367
+    // Expected: placeholder height: 3365
     // Actual  : placeholder height: 3116
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(3367), nHeight2);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3365), nHeight2);
+}
+
+CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf165321)
+{
+    createSdImpressDoc("pptx/tdf165321.pptx");
+
+    uno::Reference<container::XIndexAccess> xGroupShape(getShapeFromPage(0, 0),
+                                                        uno::UNO_QUERY_THROW);
+    uno::Reference<drawing::XShape> xShape(xGroupShape->getByIndex(0), uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT(xShape.is());
+    // Without the fix in place, this test would have failed with
+    // Expected: shape height: 3597
+    // Actual  : shape height: 3
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3597), xShape->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(6592), xShape->getSize().Width);
+
+    xShape.set(xGroupShape->getByIndex(1), uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT(xShape.is());
+    // Without the fix in place, this test would have failed with
+    // Expected: shape height: 3597
+    // Actual  : shape height: 3
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(3597), xShape->getSize().Height);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(6402), xShape->getSize().Width);
 }
 
 CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf157285)
@@ -134,18 +157,18 @@ CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf157285)
     sal_Int32 nHeight1 = xShape1->getSize().Height;
 
     // Without the fix in place, this test would have failed with
-    // Expected: placeholder height: 2565
+    // Expected: placeholder height: 2795
     // Actual  : placeholder height: 3435
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(2565), nHeight1);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2795), nHeight1);
 
     uno::Reference<drawing::XShape> xShape2(getShapeFromPage(1, 0), uno::UNO_QUERY);
     CPPUNIT_ASSERT(xShape2.is());
     sal_Int32 nHeight2 = xShape2->getSize().Height;
 
     // Without the fix in place, this test would have failed with
-    // Expected: placeholder height: 1180
+    // Expected: placeholder height: 1271
     // Actual  : placeholder height: 11303
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(1180), nHeight2);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1271), nHeight2);
 }
 
 CPPUNIT_TEST_FIXTURE(SdImportTest2, testTdf152186)