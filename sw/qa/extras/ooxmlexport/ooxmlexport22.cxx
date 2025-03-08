1a6e586394678e059a5daa8dba8b21f318c83b40
Justin Luth <justin.luth@collabora.com>
2025-03-08 15:30:41 -0500

    tdf#165642 docx export glossary: RT endnotes.xml/footnotes.xml too
    settings.xml might refer to endnotePr
    (in which case there must be an endnotes.xml file)
    and / or footnotePr
    (in which case there must be a footnotes.xml file)
    so round-trip these 2 files also if they exist in the glossary folder.
    
    Otherwise MS Word complains about invalid content.
    
    For this particular unit test it went bad in LO 7.4.4,
    but in other circumstances, it already goes bad in LO 4.3.
    
    make CppunitTest_sw_ooxmlexport22 \
        CPPUNIT_TEST_NAME=testTdf165642_glossaryFootnote
    
    Change-Id: Ib4c2513fa915fc4de4aee056b67a5936be5c52a1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182674
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>

---
+++ sw/qa/extras/ooxmlexport/ooxmlexport22.cxx
@@ -0,0 +1,49 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <com/sun/star/beans/XPropertyState.hpp>
+
+#include <comphelper/configuration.hxx>
+#include <comphelper/sequenceashashmap.hxx>
+#include <officecfg/Office/Common.hxx>
+
+#include <pam.hxx>
+#include <unotxdoc.hxx>
+#include <docsh.hxx>
+
+namespace
+{
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase(u"/sw/qa/extras/ooxmlexport/data/"_ustr, u"Office Open XML Text"_ustr)
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf165642_glossaryFootnote)
+{
+    loadAndSave("tdf165642_glossaryFootnote.docx");
+    // round-trip'ing the settings.xml file as is, it contains footnote/endnote references
+    xmlDocUniquePtr pXmlSettings = parseExport(u"word/glossary/settings.xml"_ustr);
+    assertXPath(pXmlSettings, "//w:endnotePr", 1);
+    assertXPath(pXmlSettings, "//w:footnotePr", 1);
+
+    // thus, the footnote and endnote files must also be round-tripped
+    parseExport(u"word/glossary/endnotes.xml"_ustr);
+    parseExport(u"word/glossary/footnotes.xml"_ustr);
+}
+
+} // end of anonymous namespace
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
