15b07aae78d85aa81611ed7a901cd4666d6cd32e
Justin Luth <justin.luth@collabora.com>
2025-03-08 15:30:41 -0500

    tdf#165642 docx export glossary: RT endnotes.xml/footnotes.xml too
    settings.xml might refer to endnotePr
    (in which case there must be an endnotes.xml file)
    and / or footnotePr
    (in which case there must be a footnotes.xml file)
    so round-trip these 2 files also if they exist in the glossary folder.
    
    Otherwise MS Word complains about invalid content.
    
    For this particular unit test it went bad in LO 7.4.4,
    but in other circumstances, it already goes bad in LO 4.3.
    
    make CppunitTest_sw_ooxmlexport22 \
        CPPUNIT_TEST_NAME=testTdf165642_glossaryFootnote
    
    Change-Id: Ib4c2513fa915fc4de4aee056b67a5936be5c52a1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182674
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>
    (cherry picked from commit 60788682d3d62264e8a5ac42f3469f2d2f55815c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182678
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sw/CppunitTest_sw_ooxmlexport22.mk
@@ -0,0 +1,22 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call sw_ooxmlexport_test,22))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,sw_ooxmlexport22,\
+    officecfg/registry \
+))
+
+ifneq ($(filter MORE_FONTS,$(BUILD_TYPE)),)
+$(eval $(call gb_CppunitTest_set_non_application_font_use,sw_ooxmlexport22,abort))
+endif
+
+# vim: set noet sw=4 ts=4:
--- sw/Module_sw.mk
+++ sw/Module_sw.mk
@@ -99,6 +99,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sw,\
     CppunitTest_sw_ooxmlexport19 \
     CppunitTest_sw_ooxmlexport20 \
     CppunitTest_sw_ooxmlexport21 \
+    CppunitTest_sw_ooxmlexport22 \
     CppunitTest_sw_ooxmlexport_template \
     CppunitTest_sw_ooxmlfieldexport \
     CppunitTest_sw_ooxmllinks \
