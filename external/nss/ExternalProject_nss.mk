cd62850712985512d80b1f04972e7aa9cbaf391c
Xisco Fauli <xiscofauli@libreoffice.org>
2025-03-14 12:28:51 +0100

    nss: remove obsolete target
    it has been removed in recent versions of nss.
    Do it in preparation for the upgrade.
    
    Change-Id: I2dbcdb065c59e3d79235fb8a17dc072f4826319b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/182914
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins
    (cherry picked from commit acf84f9e21999327b831580691430db8c8fe8a71)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193562
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    (cherry picked from commit c7fca2a8248293ef01574c56fa55e9a584ccc7bb)
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/1352
    Tested-by: Szűcs Attila
    Reviewed-by: Szűcs Attila

--- external/nss/ExternalProject_nss.mk
+++ external/nss/ExternalProject_nss.mk
@@ -30,7 +30,6 @@ $(call gb_ExternalProject_get_state_target,nss,build): \
 			MOZ_DEBUG_SYMBOLS=1 \
 			MOZ_DEBUG_FLAGS=" " \
 			OPT_CODE_SIZE=0) \
-		OS_TARGET=WIN95 \
 		USE_SYSTEM_ZLIB=1 \
 		$(if $(filter X86_64,$(CPUNAME)),USE_64=1) \
 		$(if $(filter AARCH64,$(CPUNAME)),USE_64=1 CPU_ARCH=aarch64) \
