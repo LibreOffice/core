92f6ec0835baca3b9f0be785c90bb46aad75bc38
Tor Lillqvist <tml@iki.fi>
2025-03-17 14:15:27 +0200

    Avoid test failures with --disable-pdfimport
    Change-Id: I05b0af5dfff2fa4b12ec1216ca4e9667c0388b72
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183040
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/qa/extras/htmlexport/htmlexport2.cxx
+++ sw/qa/extras/htmlexport/htmlexport2.cxx
@@ -7,6 +7,8 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <config_features.h>
+
 #include "htmlmodeltestbase.hxx"
 #include <config_poppler.h>
 #include <memory>
@@ -1171,6 +1173,7 @@ CPPUNIT_TEST_FIXTURE(SwHtmlDomExportTest, testReqIF_ExportFormulasAsPDF)
     };
     saveWithParams(aStoreProperties);
 
+#if HAVE_FEATURE_PDFIMPORT
     // Make sure that the formula is exported as PDF:
     xmlDocUniquePtr pXmlDoc = WrapReqifFromTempFile();
     assertXPath(pXmlDoc, "/reqif-xhtml:html/reqif-xhtml:div/reqif-xhtml:p[2]/reqif-xhtml:object",
