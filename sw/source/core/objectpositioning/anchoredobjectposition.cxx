69296ae46f00c4066d983fe112a8bf0a8111c93b
Miklos Vajna <vmiklos@collabora.com>
2025-03-17 08:23:35 +0100

    tdf#164902 sw DoNotCaptureDrawObjsOnPage: enable non-textbox wrap=no draw objs
    Open the bugdoc, the poor layout expects Writer to not capture the shape
    at the top of the page, but we captured it.
    
    This went wrong with commit a0b6587c4acb1d74e1b00904147821640c98b323
    (tdf#161199 sw DoNotCaptureDrawObjsOnPage: capture wrap=none draw
    objects, 2024-06-13), which had a draw obj (part of a draw-fly format
    pair, forming a textbox), that was captured in Word, but not in Writer.
    Notice how the old bugdoc was a textbox draw obj, but the new draw obj
    is not a textbox one.
    
    Fix the problem by once again refining the condition when to capture
    objects inside the page frame. When it comes to draw objects, continue
    to capture wrap=none ones, but only in case they are part of a textbox.
    This keeps the wanted capturing for the old case and restores the lack
    of capturing for the new case.
    
    Note that the in-header shape is actually wrap-through in Word, and at
    the moment not in Writer, which would be also nice to fix, but that's
    not done here.
    
    Change-Id: I163ea9366bb34b09b2319c91c4cd53b82c19964a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183020
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sw/source/core/objectpositioning/anchoredobjectposition.cxx
+++ sw/source/core/objectpositioning/anchoredobjectposition.cxx
@@ -122,12 +122,24 @@ void SwAnchoredObjectPosition::GetInfoAboutObj()
     // determine, if anchored object has not to be captured on the page.
     // the following conditions must be hold to *not* capture it:
     // - corresponding document compatibility flag is set
-    // - it's a drawing object or it's a non-textbox fly frame with wrap-though
+    // - shape is considered:
+    //   - draw case: wrap through or is not part of a textbox
+    //   - fly case: wrap through and is not part of a textbox
     // - it doesn't follow the text flow
     {
-        bool bTextBox = SwTextBoxHelper::isTextBox(mpFrameFormat, RES_FLYFRMFMT);
         bool bWrapThrough = mpFrameFormat->GetSurround().GetSurround() == css::text::WrapTextMode_THROUGH;
-        mbDoNotCaptureAnchoredObj = ((!mbIsObjFly || !bTextBox) && bWrapThrough) && !mbFollowTextFlow &&
+        bool bConsidered{};
+        if (mbIsObjFly)
+        {
+            bool bTextBox = SwTextBoxHelper::isTextBox(mpFrameFormat, RES_FLYFRMFMT);
+            bConsidered = bWrapThrough && !bTextBox;
+        }
+        else
+        {
+            bool bTextBox = SwTextBoxHelper::isTextBox(mpFrameFormat, RES_DRAWFRMFMT);
+            bConsidered = bWrapThrough || !bTextBox;
+        }
+        mbDoNotCaptureAnchoredObj = bConsidered && !mbFollowTextFlow &&
                                     mpFrameFormat->getIDocumentSettingAccess().get(DocumentSettingId::DO_NOT_CAPTURE_DRAW_OBJS_ON_PAGE);
     }
 }
