ef9586eb7de51bd683563bd4caa5c376f687ab47
Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
2025-03-20 11:09:41 +0200

    Revert "tdf#164490 Use stronger typed SwClient"
    This reverts commit 13c5a3b07e4dd66a80bd20556dffbd96a910bb74.
    
    No improvement.
    
    Change-Id: Ib2c315b0b464068998b299e4f73fffda2a1a4688
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183153
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Tested-by: Jenkins
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- sw/inc/calbck.hxx
+++ sw/inc/calbck.hxx
@@ -193,7 +193,7 @@ typedef sw::ClientBase<SwModify> SwClient;
 // SwModify
 
 // class has a doubly linked list for dependencies
-class SW_DLLPUBLIC SwModify: public sw::ClientBase<SwModify>
+class SW_DLLPUBLIC SwModify: public SwClient
 {
     friend class sw::ClientIteratorBase;
     friend void sw::ClientNotifyAttrChg(SwModify&, const SwAttrSet&, SwAttrSet&, SwAttrSet&);
@@ -211,7 +211,7 @@ protected:
     void PrepareFormatDeath(const SwFormatChangeHint&);
 public:
     SwModify()
-        : sw::ClientBase<SwModify>(), m_pWriterListeners(nullptr), m_bModifyLocked(false)
+        : SwClient(), m_pWriterListeners(nullptr), m_bModifyLocked(false)
     {}
 
     // broadcasting mechanism
