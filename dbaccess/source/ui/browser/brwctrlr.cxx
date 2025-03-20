LibreOffice/core

commit 09e2627a83d4769983c39fb2cb260e11fce354f3

use more concrete UNO types in some local vars
found by a little plugin I created.

Plugin parked into store/ folder because it needs hand-holding
when run.

Change-Id: I2b4da7378f0becbc5f020ac9e78cd765aa0119b4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181768
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

dbaccess/source/ui/browser/brwctrlr.cxx
@@ -436,8 +436,8 @@ Sequence< Reference< css::awt::XControl > > SAL_CALL SbaXDataBrowserController::
 {
     if (m_pOwner->getBrowserView())
     {
-        Reference< css::awt::XControl >  xGrid = m_pOwner->getBrowserView()->getGridControl();
-        return Sequence< Reference< css::awt::XControl > >(&xGrid, 1);
+        rtl::Reference< SbaXGridControl > xGrid = m_pOwner->getBrowserView()->getGridControl();
+        return { Reference< css::awt::XControl >(xGrid) };
     }
     return Sequence< Reference< css::awt::XControl > >();
 }
@@ -2195,16 +2195,12 @@ bool SbaXDataBrowserController::CommitCurrent()
     if (!getBrowserView())
         return true;
 
-    Reference< css::awt::XControl >  xActiveControl(getBrowserView()->getGridControl());
-    Reference< css::form::XBoundControl >  xLockingTest(xActiveControl, UNO_QUERY);
-    bool bControlIsLocked = xLockingTest.is() && xLockingTest->getLock();
+    rtl::Reference< SbaXGridControl >  xActiveControl(getBrowserView()->getGridControl());
+    Reference< css::form::XBoundControl >  xLockingTest(static_cast<cppu::OWeakObject*>(xActiveControl.get()), UNO_QUERY);
+    bool bControlIsLocked = xActiveControl.is() && xLockingTest->getLock();
     if (xActiveControl.is() && !bControlIsLocked)
     {
-        // At first check Control if it supports the IFace
-        Reference< css::form::XBoundComponent >  xBoundControl(xActiveControl, UNO_QUERY);
-        if (!xBoundControl.is())
-            xBoundControl.set(xActiveControl->getModel(), UNO_QUERY);
-        if (xBoundControl.is() && !xBoundControl->commit())
+        if (!xActiveControl->commit())
             return false;
     }
     return true;

basic/source/uno/namecont.cxx
@@ -2110,9 +2110,8 @@ SfxLibraryContainer::createLibrary_Impl(const OUString& Name, std::unique_lock<s
     checkStorageURL(pNewLib->maUnexpandedStorageURL, pNewLib->maLibInfoFileURL,
                     pNewLib->maStorageURL, pNewLib->maUnexpandedStorageURL);
 
-    Reference< XNameAccess > xNameAccess( pNewLib );
     Any aElement;
-    aElement <<= xNameAccess;
+    aElement <<= Reference< XNameAccess >( pNewLib );
     maNameContainer.insertByName(Name, aElement, guard);
     maModifiable.setModified(true, guard);
     return pNewLib;
@@ -2150,9 +2149,8 @@ SfxLibraryContainer::createLibraryLink_Impl(const OUString& Name, const OUString
     implLoadLibraryIndexFile(pNewLib.get(), aLibDesc, xDummyStor, OUString(), guard);
     implImportLibDescriptor(pNewLib.get(), aLibDesc, guard);
 
-    Reference< XNameAccess > xRet( pNewLib );
     Any aElement;
-    aElement <<= xRet;
+    aElement <<= Reference< XNameAccess >( pNewLib );
     maNameContainer.insertByName(Name, aElement, guard);
     maModifiable.setModified(true, guard);
 
@@ -2167,7 +2165,7 @@ SfxLibraryContainer::createLibraryLink_Impl(const OUString& Name, const OUString
         pNewLib->mbReadOnly = true;
     }
 
-    return xRet;
+    return pNewLib;
 }
 
 void SAL_CALL SfxLibraryContainer::removeLibrary( const OUString& Name )

compilerplugins/clang/store/rtlref.cxx
@@ -1,0 +1,143 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * Based on LLVM/Clang.
+ *
+ * This file is distributed under the University of Illinois Open Source
+ * License. See LICENSE.TXT for details.
+ *
+ */
+#ifndef LO_CLANG_SHARED_PLUGINS
+
+#include "plugin.hxx"
+#include "check.hxx"
+#include "config_clang.h"
+#include <iostream>
+
+/*
+This is a compile-time checker.
+
+Check for cases where we have
+ - two IDL interfaces A and B,
+ - B extends A
+ - we are converting a Reference<B> to a Reference<A> using UNO_QUERY
+
+This makes the code simpler and cheaper, because UNO_QUERY can be surprisingly expensive if used a lot.
+
+*/
+
+namespace
+{
+class RtlRef : public loplugin::FilteringPlugin<RtlRef>
+{
+public:
+    explicit RtlRef(loplugin::InstantiationData const& data)
+        : FilteringPlugin(data)
+    {
+    }
+
+    bool preRun() override
+    {
+        std::string fn(handler.getMainFileName());
+        loplugin::normalizeDotDotInFilePath(fn);
+        if (fn == SRCDIR "/ucb/source/ucp/ext/ucpext_provider.cxx")
+            return false;
+        if (fn == SRCDIR "/ucb/source/ucp/hierarchy/hierarchyprovider.cxx")
+            return false;
+        if (fn == SRCDIR "/ucb/source/ucp/cmis/cmis_provider.cxx")
+            return false;
+        if (fn == SRCDIR "/ucb/source/ucp/webdav-curl/webdavprovider.cxx")
+            return false;
+        if (fn == SRCDIR "/ucb/source/ucp/webdav-curl/webdavresponseparser.cxx")
+            return false;
+        if (fn == SRCDIR "/ucb/source/cacher/dynamicresultsetwrapper.cxx")
+            return false;
+        if (fn == SRCDIR "/sax/source/fastparser/fastparser.cxx")
+            return false;
+        if (fn == SRCDIR "/chart2/source/controller/main/ObjectHierarchy.cxx")
+            return false;
+        if (fn == SRCDIR "/chart2/source/controller/main/ShapeController.cxx")
+            return false;
+        if (fn == SRCDIR "/chart2/source/view/axes/VCartesianAxis.cxx")
+            return false;
+        if (fn == SRCDIR "/chart2/source/controller/chartapiwrapper/ChartDocumentWrapper.cxx")
+            return false;
+        if (fn == SRCDIR "/chart2/source/view/main/DataTableView.cxx")
+            return false;
+        if (fn == SRCDIR "/svx/source/form/datanavi.cxx")
+            return false;
+        if (fn == SRCDIR "/sc/source/ui/Accessibility/AccessibleSpreadsheet.cxx")
+            return false;
+        if (fn == SRCDIR "/sd/source/ui/annotations/annotationwindow.cxx")
+            return false;
+        if (fn == SRCDIR "/sd/source/ui/unoidl/unopage.cxx")
+            return false;
+        if (fn == SRCDIR "/xmlsecurity/source/helper/xsecsign.cxx")
+            return false;
+        if (fn == SRCDIR "/xmlsecurity/source/helper/xsecctl.cxx")
+            return false;
+        if (fn == SRCDIR "/unoxml/source/events/eventdispatcher.cxx")
+            return false;
+        if (fn == SRCDIR "/unoxml/source/dom/element.cxx")
+            return false;
+        return true;
+    }
+
+    void run() override
+    {
+        if (preRun())
+        {
+            TraverseDecl(compiler.getASTContext().getTranslationUnitDecl());
+        }
+    }
+
+    bool VisitVarDecl(const VarDecl*);
+};
+
+bool RtlRef::VisitVarDecl(const VarDecl* varDecl)
+{
+    if (ignoreLocation(varDecl))
+        return true;
+
+    // don't bother processing anything in the Reference.h file. Makes my life easier when debugging this.
+    StringRef aFileName
+        = getFilenameOfLocation(compiler.getSourceManager().getSpellingLoc(varDecl->getBeginLoc()));
+    if (loplugin::isSamePathname(aFileName, SRCDIR "/include/com/sun/star/uno/Reference.h"))
+        return true;
+    if (loplugin::isSamePathname(aFileName, SRCDIR "/include/com/sun/star/uno/Reference.hxx"))
+        return true;
+
+    if (!loplugin::TypeCheck(varDecl->getType()).Class("Reference").Namespace("uno"))
+        return true;
+
+    if (varDecl->isStaticLocal())
+        return true;
+
+    auto init = varDecl->getInit();
+    if (!init)
+        return true;
+    init = init->IgnoreImpCasts();
+
+    auto memberCall = dyn_cast<CXXMemberCallExpr>(init);
+    if (!memberCall)
+        return true;
+    if (memberCall->getNumArgs() > 1)
+        return true;
+
+    auto innerExpr = memberCall->getImplicitObjectArgument()->IgnoreImpCasts();
+    if (!loplugin::TypeCheck(innerExpr->getType()).Class("Reference").Namespace("rtl"))
+        return true;
+
+    report(DiagnosticsEngine::Warning, "cast from %0", varDecl->getBeginLoc())
+        << innerExpr->getType() << varDecl->getSourceRange();
+    return true;
+}
+
+loplugin::Plugin::Registration<RtlRef> rtlref("rtlref");
+
+} // namespace
+
+#endif // LO_CLANG_SHARED_PLUGINS
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

compilerplugins/clang/store/test/rtlref.cxx
@@ -1,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "sal/config.h"
+
+#include "com/sun/star/uno/Sequence.hxx"
+#include "com/sun/star/uno/XInterface.hpp"
+#include "com/sun/star/io/XStreamListener.hpp"
+#include "com/sun/star/io/XInputStream.hpp"
+#include "com/sun/star/lang/XTypeProvider.hpp"
+#include "com/sun/star/lang/XComponent.hpp"
+#include "cppuhelper/implbase.hxx"
+#include "cppuhelper/weak.hxx"
+#include "rtl/ref.hxx"
+
+class FooStream : public css::io::XStreamListener
+{
+    virtual ~FooStream();
+};
+void test(rtl::Reference<FooStream> fooStream)
+{
+    // expected-error@+1 {{cast from rtl::Reference<FooStream> [loplugin:rtlref]}}
+    css::uno::Reference<css::io::XStreamListener> a(fooStream);
+    (void)a;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

dbaccess/source/core/dataaccess/datasource.cxx
@@ -504,7 +504,7 @@ weld::Window* ODatabaseModelImpl::GetFrameWeld()
     if (m_xDialogParent.is())
         return Application::GetFrameWeld(m_xDialogParent);
 
-    Reference<XModel> xModel = getModel_noCreate();
+    rtl::Reference<ODatabaseDocument> xModel = getModel_noCreate();
     if (!xModel.is())
         return nullptr;
     Reference<XController> xController(xModel->getCurrentController());
@@ -526,7 +526,7 @@ Reference< XConnection > ODatabaseSource::buildLowLevelConnection(const OUString
 #if ENABLE_FIREBIRD_SDBC
     bool bIgnoreMigration = false;
     bool bNeedMigration = false;
-    Reference< XModel > xModel = m_pImpl->getModel_noCreate();
+    rtl::Reference< ODatabaseDocument > xModel = m_pImpl->getModel_noCreate();
     if ( xModel)
     {
         //See ODbTypeWizDialogSetup::SaveDatabaseDocument
@@ -1306,11 +1306,11 @@ Reference< XOfficeDatabaseDocument > SAL_CALL ODatabaseSource::getDatabaseDocume
 {
     ModelMethodGuard aGuard( *this );
 
-    Reference< XModel > xModel( m_pImpl->getModel_noCreate() );
+    rtl::Reference< ODatabaseDocument > xModel( m_pImpl->getModel_noCreate() );
     if ( !xModel.is() )
         xModel = m_pImpl->createNewModel_deliverOwnership();
 
-    return Reference< XOfficeDatabaseDocument >( xModel, UNO_QUERY_THROW );
+    return Reference< XOfficeDatabaseDocument >( static_cast<cppu::OWeakObject*>(xModel.get()), UNO_QUERY_THROW );
 }
 
 void SAL_CALL ODatabaseSource::initialize( css::uno::Sequence< css::uno::Any > const & rArguments)

dbaccess/source/core/dataaccess/documentdefinition.cxx
@@ -1408,7 +1408,7 @@ namespace
 {
     Reference< XFrame > lcl_getDatabaseDocumentFrame( ODatabaseModelImpl const & _rImpl )
     {
-        Reference< XModel > xDatabaseDocumentModel( _rImpl.getModel_noCreate() );
+        rtl::Reference< ODatabaseDocument > xDatabaseDocumentModel( _rImpl.getModel_noCreate() );
 
         Reference< XController > xDatabaseDocumentController;
         if ( xDatabaseDocumentModel.is() )
@@ -1469,10 +1469,9 @@ Sequence< PropertyValue > ODocumentDefinition::fillLoadArgs( const Reference< XC
     }
 
     m_pInterceptor = new OInterceptor( this );
-    Reference<XDispatchProviderInterceptor> xInterceptor = m_pInterceptor;
 
     ::comphelper::NamedValueCollection aEmbeddedDescriptor;
-    aEmbeddedDescriptor.put( u"OutplaceDispatchInterceptor"_ustr, xInterceptor );
+    aEmbeddedDescriptor.put( u"OutplaceDispatchInterceptor"_ustr, Reference<XDispatchProviderInterceptor>(m_pInterceptor) );
 
     ::comphelper::NamedValueCollection aMediaDesc;
     separateOpenCommandArguments( i_rOpenCommandArguments, aMediaDesc, aEmbeddedDescriptor );
@@ -1771,7 +1770,7 @@ namespace
     {
         Reference< XDatabaseDocumentUI > xUI;
 
-        Reference< XModel > xModel( _rModelImpl.getModel_noCreate() );
+        rtl::Reference< ODatabaseDocument > xModel( _rModelImpl.getModel_noCreate() );
         if ( xModel.is() )
             xUI.set( xModel->getCurrentController(), UNO_QUERY );
         return xUI;

dbaccess/source/core/dataaccess/myucp_datasupplier.cxx
@@ -103,7 +103,7 @@ DataSupplier::queryContent( std::unique_lock<std::mutex>& rResultSetGuard, sal_u
 
     if ( static_cast<size_t>(_nIndex) < m_aResults.size() )
     {
-        Reference< XContent > xContent = m_aResults[ _nIndex ]->xContent;
+        rtl::Reference< OContentHelper > xContent = m_aResults[ _nIndex ]->xContent;
         if ( xContent.is() )
         {
             // Already cached.

dbaccess/source/ui/browser/brwview.cxx
@@ -43,7 +43,7 @@ namespace
     {
         bool bGrabFocus = false;
         SbaGridControl* pVclControl = _pView->getVclControl();
-        const Reference< css::awt::XControl > xGrid = _pView->getGridControl();
+        const rtl::Reference< SbaXGridControl > xGrid = _pView->getGridControl();
         if (pVclControl && xGrid.is())
         {
             bGrabFocus = true;

commit c8f9ede9ecd14ed2679d51fe4c1cc3ed1496c75e

tdf#165815 Base crashes on saving edited table
thanks to adamseskunas for finding the source file that contained the
bug

regression from
    commit 09e2627a83d4769983c39fb2cb260e11fce354f3
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Mon Feb 17 11:06:55 2025 +0200
    use more concrete UNO types in some local vars

Change-Id: I6884e5b7b20e45e3e7cfd11d5aa20d6e03ae34d9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183151
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

dbaccess/source/ui/browser/brwctrlr.cxx
@@ -2197,7 +2197,7 @@ bool SbaXDataBrowserController::CommitCurrent()
 
     rtl::Reference< SbaXGridControl >  xActiveControl(getBrowserView()->getGridControl());
     Reference< css::form::XBoundControl >  xLockingTest(static_cast<cppu::OWeakObject*>(xActiveControl.get()), UNO_QUERY);
-    bool bControlIsLocked = xActiveControl.is() && xLockingTest->getLock();
+    bool bControlIsLocked = xLockingTest.is() && xLockingTest->getLock();
     if (xActiveControl.is() && !bControlIsLocked)
     {
         if (!xActiveControl->commit())