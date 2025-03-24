dd8f76a4c8aa24afa2b736e51d7cc64d913cf53c
Xisco Fauli <xiscofauli@libreoffice.org>
2025-03-24 13:21:02 +0100

    nss: change target from WIN95 to WINNT
    In preparation for the upgrade to nss 3.104
    
    In nss 3.103 or older, when OS_TARGET is not specified, it defaults to
    WIN95, See comment
    
    -  When building on Windows, OS_TARGET now defaults to WIN95. To use the WINNT build
          configuration, specify OS_TARGET=WINNT.
    
    in doc/rst/legacy/nss_releases/nss_3.15.4_release_notes/index.rst
    However, in nss 3.104 or newer, WIN95 support has been dropped and
    the WINNT has become the default. It needs some tweaks though:
    
    * Add external/nss/nspr-win95-target.patch to keep building
    nspr with WIN95 target, which is still available in nspr.
    Explicitly pass --enable-win32-target=WIN95 to NSPR_CONFIGURE_OPTS,
    otherwise, it would use WINNT, as nss, and it will fail with
    
    make[7]: *** No rule to make target 'libnspr4.lib', needed by 'build'.  Stop.
    make[7]: *** Waiting for unfinished jobs....
    
    * Add external/nss/winnt.patch.0 to remove the 'lib' prefix as WIN95 does,
    otherwise it fails with
    
    LINK : fatal error LNK1181: cannot open input file '..\..\..\..\dist\out\lib\libplc4.lib'
    
    Change-Id: Ifaf19eeb1398389256a5c441a056f542b409622a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183262
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    (cherry picked from commit 81b820a7ea7fded7c30a71a799b9fe1fac1915f9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193485
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- external/nss/UnpackedTarball_nss.mk
+++ external/nss/UnpackedTarball_nss.mk
@@ -19,6 +19,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
     external/nss/nss-win32-make.patch.1 \
     external/nss/ubsan.patch.0 \
     external/nss/clang-cl.patch.0 \
+    external/nss/winnt.patch.0 \
     external/nss/nss.vs2015.patch \
     external/nss/nss.vs2015.pdb.patch \
     external/nss/nss.bzmozilla1238154.patch \
@@ -26,6 +27,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,nss,\
     external/nss/macos-dlopen.patch.0 \
     external/nss/nss-restore-manual-pre-dependencies.patch.1 \
     external/nss/Wincompatible-function-pointer-types.patch.0 \
+    external/nss/nspr-win95-target.patch \
     $(if $(filter LINUX,$(OS)), \
         external/nss/nss.disablefsync.patch \
         external/nss/nss.getrandom.patch) \
