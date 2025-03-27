LibreOffice/core

commit da06166015689eca260c702602bef4cea58afbd3

fix bug #60700 - de-crutify ODF files
Initialization of storage elements in Configuration folder is now
done in read-only mode. Modification does not affect these folders
in case of actual storage within them.
Only tested for example posted on bugzilla page.

Added open mode directly into function call.

Change-Id: Ib2b4ae1f4cab35f2c9cd1fc7081302e1231da7a4
Reviewed-on: https://gerrit.libreoffice.org/3401
Reviewed-by: Chris Sherlock <chris.sherlock79@gmail.com>
Reviewed-by: Michael Meeks <michael.meeks@suse.com>
Tested-by: Michael Meeks <michael.meeks@suse.com>

framework/source/uiconfiguration/uiconfigurationmanager.cxx
@@ -540,8 +540,6 @@ void UIConfigurationManager::impl_Initialize()
     // Initialize the top-level structures with the storage data
     if ( m_xDocConfigStorage.is() )
     {
-        long nModes = m_bReadOnly ? ElementModes::READ : ElementModes::READWRITE;
-
         // Try to access our module sub folder
         for ( sal_Int16 i = 1; i < ::com::sun::star::ui::UIElementType::COUNT;
               i++ )
@@ -549,7 +547,7 @@ void UIConfigurationManager::impl_Initialize()
             Reference< XStorage > xElementTypeStorage;
             try
             {
-                xElementTypeStorage = m_xDocConfigStorage->openStorageElement( OUString::createFromAscii( UIELEMENTTYPENAMES[i] ), nModes );
+                xElementTypeStorage = m_xDocConfigStorage->openStorageElement( OUString::createFromAscii( UIELEMENTTYPENAMES[i] ), ElementModes::READ );
             }
             catch ( const com::sun::star::container::NoSuchElementException& )
             {

commit aaed16eef8b2d32ac02c8cb3a42ca25246cb0d33

tdf#69500 Regression test for saving toolbar to document
The regression was introduced in commit da06166015689eca260c702602bef4cea58afbd3
for tdf#60700 and fixed in commit 42f4261f835ad4ff7d98126cc7fb3be84dc76126

This change adds a regression test to verify that saving a custom toolbar
to a fresh document works. It passes for master and fails with
cherry-pick da06166015689eca260c702602bef4cea58afbd3

Change-Id: I569c1172d3e2360a0364768d0b4fa799da9194d4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183356
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/qa/extras/odfexport/odfexport.cxx
@@ -38,6 +38,8 @@
 #include <com/sun/star/text/XTextDocument.hpp>
 #include <com/sun/star/util/XRefreshable.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/ui/XUIConfigurationManagerSupplier.hpp>
+#include <com/sun/star/ui/XUIConfigurationManager.hpp>
 
 #include <comphelper/storagehelper.hxx>
 #include <comphelper/fileformat.h>
@@ -1379,6 +1381,52 @@ DECLARE_ODFEXPORT_TEST(testTextFrameVertAdjust, "textframe-vertadjust.odt")
     // 3rd frame's context is adjusted to the bottom
     xFrame.set(getTextFrameByName(u"Rectangle 3"_ustr), uno::UNO_QUERY);
     CPPUNIT_ASSERT_EQUAL(drawing::TextVerticalAdjust_BOTTOM, getProperty<drawing::TextVerticalAdjust>(xFrame, u"TextVerticalAdjust"_ustr));
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf69500)
+{
+    createSwDoc();
+
+    static constexpr OUString sToolBarName = u"private:resource/toolbar/custom_toolbar_1"_ustr;
+
+    auto getUIConfigManager = [this]() {
+        css::uno::Reference<css::uno::XComponentContext> xContext
+            = comphelper::getProcessComponentContext();
+        uno::Reference<frame::XModel> xModel(mxComponent, uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xModel.is());
+        uno::Reference<ui::XUIConfigurationManagerSupplier> xConfigSupplier(xModel, uno::UNO_QUERY);
+        CPPUNIT_ASSERT(xConfigSupplier.is());
+        uno::Reference<ui::XUIConfigurationManager> xConfigManager
+            = xConfigSupplier->getUIConfigurationManager();
+        return xConfigManager;
+    };
+
+    // Create and persist a custom toolbar to the document
+    {
+        uno::Reference<ui::XUIConfigurationManager> xConfigManager = getUIConfigManager();
+
+        uno::Reference<container::XIndexContainer> xIndexContainer(xConfigManager->createSettings(),
+                                                                   uno::UNO_SET_THROW);
+        uno::Reference<container::XIndexAccess> xIndexAccess(xIndexContainer, uno::UNO_QUERY_THROW);
+        uno::Reference<beans::XPropertySet> xProps(xIndexContainer, uno::UNO_QUERY_THROW);
+
+        xProps->setPropertyValue(u"UIName"_ustr, uno::Any(u"Custom Toolbar 1"_ustr));
+
+        xConfigManager->insertSettings(sToolBarName, xIndexAccess);
+
+        uno::Reference<ui::XUIConfigurationPersistence> xPersistence(xConfigManager,
+                                                                     uno::UNO_QUERY_THROW);
+        xPersistence->store();
+    }
+
+    saveAndReload(mpFilter);
+
+    // Without the fix, the toolbar will be gone after save-and-reload
+    {
+        uno::Reference<ui::XUIConfigurationManager> xConfigManager = getUIConfigManager();
+
+        CPPUNIT_ASSERT(xConfigManager->hasSettings(sToolBarName));
+    }
 }
 
 } // end of anonymous namespace