LibreOffice/core

commit 7d384fb1c232f7aa720436bc68dc1de334bf7297

tdf#165984 sw cui xmloff: add Paragraph/Column/Page/Spread end zone
Add new hyphenation options to adjust hyphenation, like DTP software do
in accordance with typographic requirements:

– Paragraph/Column/Page/Spread end zone spin boxes to Hyphenate Across
  section of Text Flow pane of paragraph settings;

– loext:hyphenation-zone-always, loext:hyphenation-zone-column,
  loext:hyphenation-zone-page and loext:hyphenation-zone-spread
  ODF extensions;

– widorp.cxx, guess.cxx: handle Column/Page/Spread end zones;

– guess.cxx: handle Paragraph end zone (without portions, yet).

According to their typography, languages with long words need better
adjustment of line break of the last full paragraph/column/page/spread
lines, than simply disabling hyphenation by the recent ODF hyphenation-
keep or its equivalent MS Word OOXML extension. DTP software use
paragraph and column/page/spread end zones to adjust the hyphenation
zones in the last lines, for example, setting Column end zone to the
150% percent of the Hyphenation zone, resulting less hyphenation, also
less short words parts at the end of the columns, pages and spreads,
increasing readability.

Note: the default OOXML hyphenation zone 18 pt (or its MS Word variants,
for example 21.25 pt) disables single letter or narrow 2-letter (li-,
ﬁ-, i.e. fi ligature) hyphenations at 12 pt font height. Setting 27 pt
for Column end zone, all 2-letter hyphenation is disabled in last lines
of columns and pages.

Note: textflow.ui needs multiple adjustment definitions for correct work
of the multiple spin boxes.

Change-Id: I37bbb4da30de0ec75eb8636f70d8e16588c56ed1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183565
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/widorp.cxx
@@ -470,6 +470,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
     // hyphenation-keep: truncate a hyphenated line at the end of
     // the column, page or spread (but not more)
     // hyphenation-keep-line: disable hyphenation in the last line instead of truncating it
+    // hyphenation-zone-always/page/column/spread: modify hyphenation in the last line (end zone)
     int nExtraWidLines = 0;
     if( rLine.GetLineNr() >= m_nWidLines && pMaster->HasPara() )
     {
@@ -480,26 +481,58 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
         bool bKeep = rAttr.IsHyphen() && rAttr.IsKeep() && rAttr.GetKeepType();
         bool bKeepLine = bKeep && rAttr.IsKeepLine();
 
+        // last line of a column inside a page
+        auto pMasterPage = pMaster->FindPageFrame();
+        auto pPage = pFrame->FindPageFrame();
+        // across column, but not page or spread, when both parts are there on the same page
+        bool bAcrossColumnNotPage = pMasterPage == pPage;
+        // across page, but not spread, when the parts are there not on the same page
+        bool bAcrossPageNotSpread = !bAcrossColumnNotPage &&
+                   !pMasterPage->OnRightPage() && pPage->OnRightPage() &&
+                   // linked text frames can be on a different spread, so check
+                   // that the master is there on the previous page
+                   pMasterPage == pPage->GetPrev();
+
+        // calculate end zones, based on their inheritance (0 means inheritance)
+        sal_Int16 nEndZoneParagraph = rAttr.GetTextHyphenZoneAlways() > 0
+                ? rAttr.GetTextHyphenZoneAlways()
+                : rAttr.GetTextHyphenZone();
+        sal_Int16 nEndZoneColumn = rAttr.GetTextHyphenZoneColumn() > 0
+                ? rAttr.GetTextHyphenZoneColumn()
+                : nEndZoneParagraph;
+        sal_Int16 nEndZonePage = rAttr.GetTextHyphenZonePage() > 0
+                ? rAttr.GetTextHyphenZonePage()
+                : nEndZoneColumn;
+        sal_Int16 nEndZoneSpread = rAttr.GetTextHyphenZoneSpread() > 0
+                ? rAttr.GetTextHyphenZoneSpread()
+                : nEndZonePage;
+
+        // set end zone
+        sal_Int16 nNoHyphEndZone = bAcrossColumnNotPage
+            ? nEndZoneColumn
+            : bAcrossPageNotSpread
+                ? nEndZonePage
+                : nEndZoneSpread;
+
         // if PAGE or SPREAD, allow hyphenation in the not last column or in the
         // not last linked frame on the same page
-        if( bKeep && (
+        if( bKeep && bAcrossColumnNotPage && (
                 rAttr.GetKeepType() == css::text::ParagraphHyphenationKeepType::SPREAD ||
-                rAttr.GetKeepType() == css::text::ParagraphHyphenationKeepType::PAGE ) &&
-            pMaster->FindPageFrame() == pFrame->FindPageFrame() )
+                rAttr.GetKeepType() == css::text::ParagraphHyphenationKeepType::PAGE ) )
         {
             bKeep = false;
         }
 
         // if SPREAD, allow hyphenation at bottom of left page on the same spread
         if ( bKeep && rAttr.GetKeepType() == css::text::ParagraphHyphenationKeepType::SPREAD &&
-                   pFrame->FindPageFrame()->OnRightPage() &&
-                   pMaster->FindPageFrame() == pFrame->FindPageFrame()->GetPrev() )
+                   bAcrossPageNotSpread )
         {
             bKeep = false;
         }
 
         // remove remaining NoHyphOffset after enabling Hyphenate Across Spread (!bKeep) or
-        // enabling Move Line (!bKeepLine), and invalidate the master to update the last line
+        // enabling Move Line (!bKeepLine), or setting End Zone to no-limit,
+        // and invalidate the master to update the last line
         if ( (!bKeep || !bKeepLine) &&
                     pMaster->GetNoHyphOffset() != TextFrameIndex(COMPLETE_STRING) )
         {
@@ -508,7 +541,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
             pMaster->InvalidateSize_();
         }
 
-        if ( bKeep && pMasterPara && pMasterPara->GetNext() )
+        if ( ( bKeep || nNoHyphEndZone ) && pMasterPara && pMasterPara->GetNext() )
         {
             // calculate the beginning of last hyphenated line
             TextFrameIndex nIdx(pMasterPara->GetLen());
@@ -526,13 +559,14 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
             nIdx -= pNext->GetLen();
             // hyphenated line, but not the last remaining one
             // in the case of shifting full line (bKeepLine = false)
-            if ( pNext->IsEndHyph() && ( bKeepLine || !pNext->IsLastHyph() ) )
+            if ( pNext->IsEndHyph() && ( bKeepLine || !pNext->IsLastHyph() || nNoHyphEndZone ) )
             {
                 nExtraWidLines = rLine.GetLineNr() - m_nWidLines + 1;
                 // shift only a word: disable hyphenation in the line, if needed
-                if ( bKeepLine && nExtraWidLines )
+                if ( ( bKeepLine || nNoHyphEndZone ) && nExtraWidLines )
                 {
                     pMaster->SetNoHyphOffset(nIdx);
+                    pMaster->SetNoHyphEndZone(bKeep ? -1 : nNoHyphEndZone);
                     // update also columns and frames
                     pMaster->Prepare( PrepareHint::AdjustSizeWithoutFormatting );
                     pMaster->InvalidateSize_();
@@ -542,7 +576,7 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
                 // set remaining line to "last remaining hyphenated line"
                 // to avoid truncating multiple hyphenated lines instead
                 // of a single one
-                else if ( !bKeepLine && pCurr->IsEndHyph() )
+                else if ( bKeep && !bKeepLine && pCurr->IsEndHyph() )
                     pCurr->SetLastHyph( true );
                 // also unset the line before the remaining one
                 // TODO: check also the line after the truncated line?

cui/source/inc/paragrph.hxx
@@ -246,6 +246,14 @@ private:
     std::unique_ptr<weld::SpinButton> m_xMinWordLength;
     std::unique_ptr<weld::Label> m_xHyphenZoneLabel;
     SvxRelativeField m_aHyphenZone;
+    std::unique_ptr<weld::Label> m_xParagraphEndZoneLabel;
+    SvxRelativeField m_aParagraphEndZone;
+    std::unique_ptr<weld::Label> m_xColumnEndZoneLabel;
+    SvxRelativeField m_aColumnEndZone;
+    std::unique_ptr<weld::Label> m_xPageEndZoneLabel;
+    SvxRelativeField m_aPageEndZone;
+    std::unique_ptr<weld::Label> m_xSpreadEndZoneLabel;
+    SvxRelativeField m_aSpreadEndZone;
 
     // pagebreak
     std::unique_ptr<weld::CheckButton> m_xPageBreakBox;

cui/source/tabpages/paragrph.cxx
@@ -1666,6 +1666,10 @@ bool SvxExtParagraphTabPage::FillItemSet( SfxItemSet* rOutSet )
          m_xMaxHyphenEdit->get_value_changed_from_saved() ||
          m_xMinWordLength->get_value_changed_from_saved() ||
          m_aHyphenZone.get_value_changed_from_saved() ||
+         m_aParagraphEndZone.get_value_changed_from_saved() ||
+         m_aColumnEndZone.get_value_changed_from_saved() ||
+         m_aPageEndZone.get_value_changed_from_saved() ||
+         m_aSpreadEndZone.get_value_changed_from_saved() ||
          m_xAcrossParagraphBox->get_state_changed_from_saved() ||
          m_xAcrossColumnBox->get_state_changed_from_saved() ||
          m_xAcrossPageBox->get_state_changed_from_saved() ||
@@ -1691,6 +1695,10 @@ bool SvxExtParagraphTabPage::FillItemSet( SfxItemSet* rOutSet )
         DBG_ASSERT( pPool, "Where is the pool?" );
         MapUnit eUnit = pPool->GetMetric( _nWhich );
         aHyphen.GetTextHyphenZone() = static_cast<sal_uInt16>(m_aHyphenZone.GetCoreValue(eUnit));
+        aHyphen.GetTextHyphenZoneAlways() = static_cast<sal_uInt16>(m_aParagraphEndZone.GetCoreValue(eUnit));
+        aHyphen.GetTextHyphenZoneColumn() = static_cast<sal_uInt16>(m_aColumnEndZone.GetCoreValue(eUnit));
+        aHyphen.GetTextHyphenZonePage() = static_cast<sal_uInt16>(m_aPageEndZone.GetCoreValue(eUnit));
+        aHyphen.GetTextHyphenZoneSpread() = static_cast<sal_uInt16>(m_aSpreadEndZone.GetCoreValue(eUnit));
         aHyphen.SetHyphen( eHyphenState == TRISTATE_TRUE );
         aHyphen.SetNoLastWordHyphenation(m_xHyphenNoLastWordBox->get_state() != TRISTATE_TRUE);
         const TriState eAcrossParagraphState = m_xAcrossParagraphBox->get_state();
@@ -1943,6 +1951,26 @@ void SvxExtParagraphTabPage::Reset( const SfxItemSet* rSet )
         m_xMinWordLength->set_value(rHyphen.GetMinWordLength());
         m_aHyphenZone.SetFieldUnit(eFUnit);
         m_aHyphenZone.SetMetricValue(rHyphen.GetTextHyphenZone(), MapUnit::MapTwip);
+        m_aParagraphEndZone.SetFieldUnit(eFUnit);
+        m_aParagraphEndZone.SetMetricValue(rHyphen.GetTextHyphenZoneAlways(), MapUnit::MapTwip);
+        // 0 means inheritance, mark this with empty box
+        if ( !rHyphen.GetTextHyphenZoneAlways() )
+            m_aParagraphEndZone.set_text(OUString());
+        m_aColumnEndZone.SetFieldUnit(eFUnit);
+        m_aColumnEndZone.SetMetricValue(rHyphen.GetTextHyphenZoneColumn(), MapUnit::MapTwip);
+        // 0 means inheritance, mark this with empty box
+        if ( !rHyphen.GetTextHyphenZoneColumn() )
+            m_aColumnEndZone.set_text(OUString());
+        m_aPageEndZone.SetFieldUnit(eFUnit);
+        m_aPageEndZone.SetMetricValue(rHyphen.GetTextHyphenZonePage(), MapUnit::MapTwip);
+        // 0 means inheritance, mark this with empty box
+        if ( !rHyphen.GetTextHyphenZonePage() )
+            m_aPageEndZone.set_text(OUString());
+        m_aSpreadEndZone.SetFieldUnit(eFUnit);
+        m_aSpreadEndZone.SetMetricValue(rHyphen.GetTextHyphenZoneSpread(), MapUnit::MapTwip);
+        // 0 means inheritance, mark this with empty box
+        if ( !rHyphen.GetTextHyphenZoneSpread() )
+            m_aSpreadEndZone.set_text(OUString());
         m_xAcrossParagraphBox->set_state(!rHyphen.IsKeep() || rHyphen.GetKeepType() < 4 ? TRISTATE_TRUE : TRISTATE_FALSE);
         m_xAcrossColumnBox->set_state(!rHyphen.IsKeep() || rHyphen.GetKeepType() < 3 ? TRISTATE_TRUE : TRISTATE_FALSE);
         m_xAcrossPageBox->set_state(!rHyphen.IsKeep() || rHyphen.GetKeepType() < 2 ? TRISTATE_TRUE : TRISTATE_FALSE);
@@ -1976,11 +2004,32 @@ void SvxExtParagraphTabPage::Reset( const SfxItemSet* rSet )
     m_xMinWordLength->set_sensitive(bEnable);
     m_xHyphenZoneLabel->set_sensitive(bEnable);
     m_aHyphenZone.set_sensitive(bEnable);
+    m_xParagraphEndZoneLabel->set_sensitive(bEnable);
+    m_aParagraphEndZone.set_sensitive(bEnable);
+    m_xColumnEndZoneLabel->set_sensitive(bEnable);
+    m_aColumnEndZone.set_sensitive(bEnable);
+    m_xPageEndZoneLabel->set_sensitive(bEnable);
+    m_aPageEndZone.set_sensitive(bEnable);
+    m_xSpreadEndZoneLabel->set_sensitive(bEnable);
+    m_aSpreadEndZone.set_sensitive(bEnable);
     m_xAcrossText->set_sensitive(bEnable);
     m_xAcrossParagraphBox->set_sensitive(bEnable);
     m_xAcrossColumnBox->set_sensitive(bEnable);
     m_xAcrossPageBox->set_sensitive(bEnable);
     m_xAcrossSpreadBox->set_sensitive(bEnable);
+    // gray out end zones, if hyphenation across is disabled
+    bool bAcross = bEnable && m_xAcrossParagraphBox->get_state() == TRISTATE_TRUE;
+    m_xParagraphEndZoneLabel->set_sensitive(bAcross);
+    m_aParagraphEndZone.set_sensitive(bAcross);
+    bAcross = bEnable && m_xAcrossColumnBox->get_state() == TRISTATE_TRUE;
+    m_xColumnEndZoneLabel->set_sensitive(bAcross);
+    m_aColumnEndZone.set_sensitive(bAcross);
+    bAcross = bEnable && m_xAcrossPageBox->get_state() == TRISTATE_TRUE;
+    m_xPageEndZoneLabel->set_sensitive(bAcross);
+    m_aPageEndZone.set_sensitive(bAcross);
+    bAcross = bEnable && m_xAcrossSpreadBox->get_state() == TRISTATE_TRUE;
+    m_xSpreadEndZoneLabel->set_sensitive(bAcross);
+    m_aSpreadEndZone.set_sensitive(bAcross);
     // always gray out MoveLine, if hyphenation is forbidden across spreads
     bool bAcrossSpread = m_xAcrossSpreadBox->get_state() == TRISTATE_TRUE;
     m_xAcrossMoveLineBox->set_sensitive( bEnable && !bAcrossSpread );
@@ -2247,6 +2296,10 @@ void SvxExtParagraphTabPage::ChangesApplied()
     m_xMaxHyphenEdit->save_value();
     m_xMinWordLength->save_value();
     m_aHyphenZone.save_value();
+    m_aParagraphEndZone.save_value();
+    m_aColumnEndZone.save_value();
+    m_aPageEndZone.save_value();
+    m_aSpreadEndZone.save_value();
     m_xAcrossParagraphBox->save_state();
     m_xAcrossColumnBox->save_state();
     m_xAcrossPageBox->save_state();
@@ -2307,6 +2360,14 @@ SvxExtParagraphTabPage::SvxExtParagraphTabPage(weld::Container* pPage, weld::Dia
     , m_xMinWordLength(m_xBuilder->weld_spin_button(u"spinMinLen"_ustr))
     , m_xHyphenZoneLabel(m_xBuilder->weld_label(u"labelHyphenZone"_ustr))
     , m_aHyphenZone(m_xBuilder->weld_metric_spin_button(u"spinHyphenZone"_ustr, FieldUnit::CM))
+    , m_xParagraphEndZoneLabel(m_xBuilder->weld_label(u"labelParagraphEndZone"_ustr))
+    , m_aParagraphEndZone(m_xBuilder->weld_metric_spin_button(u"spinParagraphEndZone"_ustr, FieldUnit::CM))
+    , m_xColumnEndZoneLabel(m_xBuilder->weld_label(u"labelColumnEndZone"_ustr))
+    , m_aColumnEndZone(m_xBuilder->weld_metric_spin_button(u"spinColumnEndZone"_ustr, FieldUnit::CM))
+    , m_xPageEndZoneLabel(m_xBuilder->weld_label(u"labelPageEndZone"_ustr))
+    , m_aPageEndZone(m_xBuilder->weld_metric_spin_button(u"spinPageEndZone"_ustr, FieldUnit::CM))
+    , m_xSpreadEndZoneLabel(m_xBuilder->weld_label(u"labelSpreadEndZone"_ustr))
+    , m_aSpreadEndZone(m_xBuilder->weld_metric_spin_button(u"spinSpreadEndZone"_ustr, FieldUnit::CM))
     //Page break
     , m_xPageBreakBox(m_xBuilder->weld_check_button(u"checkInsert"_ustr))
     , m_xBreakTypeFT(m_xBuilder->weld_label(u"labelType"_ustr))
@@ -2392,6 +2453,14 @@ SvxExtParagraphTabPage::SvxExtParagraphTabPage(weld::Container* pPage, weld::Dia
     m_xMinWordLength->set_sensitive(false);
     m_xHyphenZoneLabel->set_sensitive(false);
     m_aHyphenZone.set_sensitive(false);
+    m_xParagraphEndZoneLabel->set_sensitive(false);
+    m_aParagraphEndZone.set_sensitive(false);
+    m_xColumnEndZoneLabel->set_sensitive(false);
+    m_aColumnEndZone.set_sensitive(false);
+    m_xPageEndZoneLabel->set_sensitive(false);
+    m_aPageEndZone.set_sensitive(false);
+    m_xSpreadEndZoneLabel->set_sensitive(false);
+    m_aSpreadEndZone.set_sensitive(false);
     m_xPageNumBox->set_sensitive(false);
     m_xPagenumEdit->set_sensitive(false);
     m_xAcrossText->set_sensitive(false);
@@ -2538,9 +2607,21 @@ void SvxExtParagraphTabPage::HyphenClickHdl()
     m_aHyphenZone.set_sensitive(bEnable);
     m_xAcrossText->set_sensitive(bEnable);
     m_xAcrossParagraphBox->set_sensitive(bEnable);
+    bool bAcross = bEnable && m_xAcrossParagraphBox->get_state() == TRISTATE_TRUE;
+    m_xParagraphEndZoneLabel->set_sensitive(bAcross);
+    m_aParagraphEndZone.set_sensitive(bAcross);
     m_xAcrossColumnBox->set_sensitive(bEnable);
+    bAcross = bEnable && m_xAcrossColumnBox->get_state() == TRISTATE_TRUE;
+    m_xColumnEndZoneLabel->set_sensitive(bAcross);
+    m_aColumnEndZone.set_sensitive(bAcross);
     m_xAcrossPageBox->set_sensitive(bEnable);
+    bAcross = bEnable && m_xAcrossPageBox->get_state() == TRISTATE_TRUE;
+    m_xPageEndZoneLabel->set_sensitive(bAcross);
+    m_aPageEndZone.set_sensitive(bAcross);
     m_xAcrossSpreadBox->set_sensitive(bEnable);
+    bAcross = bEnable && m_xAcrossSpreadBox->get_state() == TRISTATE_TRUE;
+    m_xSpreadEndZoneLabel->set_sensitive(bAcross);
+    m_aSpreadEndZone.set_sensitive(bAcross);
     // only sensitive, if the hyphenation is disabled across spreads
     m_xAcrossMoveLineBox->set_sensitive( m_xAcrossSpreadBox->get_state() != TRISTATE_TRUE );
     m_xHyphenBox->set_state(bEnable ? TRISTATE_TRUE : TRISTATE_FALSE);
@@ -2653,6 +2734,19 @@ IMPL_LINK(SvxExtParagraphTabPage, AcrossParagraphHdl_Impl, weld::Toggleable&, rT
         m_xAcrossPageBox->set_state( TRISTATE_FALSE );
         m_xAcrossSpreadBox->set_state( TRISTATE_FALSE );
         m_xAcrossMoveLineBox->set_sensitive( true );
+        m_xParagraphEndZoneLabel->set_sensitive(false);
+        m_aParagraphEndZone.set_sensitive(false);
+        m_xColumnEndZoneLabel->set_sensitive(false);
+        m_aColumnEndZone.set_sensitive(false);
+        m_xPageEndZoneLabel->set_sensitive(false);
+        m_aPageEndZone.set_sensitive(false);
+        m_xSpreadEndZoneLabel->set_sensitive(false);
+        m_aSpreadEndZone.set_sensitive(false);
+    }
+    else
+    {
+        m_xParagraphEndZoneLabel->set_sensitive(true);
+        m_aParagraphEndZone.set_sensitive(true);
     }
 }
 
@@ -2664,9 +2758,21 @@ IMPL_LINK(SvxExtParagraphTabPage, AcrossColumnHdl_Impl, weld::Toggleable&, rTogg
         m_xAcrossPageBox->set_state( TRISTATE_FALSE );
         m_xAcrossSpreadBox->set_state( TRISTATE_FALSE );
         m_xAcrossMoveLineBox->set_sensitive( true );
+        m_xColumnEndZoneLabel->set_sensitive(false);
+        m_aColumnEndZone.set_sensitive(false);
+        m_xPageEndZoneLabel->set_sensitive(false);
+        m_aPageEndZone.set_sensitive(false);
+        m_xSpreadEndZoneLabel->set_sensitive(false);
+        m_aSpreadEndZone.set_sensitive(false);
     }
     else
+    {
         m_xAcrossParagraphBox->set_state( TRISTATE_TRUE );
+        m_xParagraphEndZoneLabel->set_sensitive(true);
+        m_aParagraphEndZone.set_sensitive(true);
+        m_xColumnEndZoneLabel->set_sensitive(true);
+        m_aColumnEndZone.set_sensitive(true);
+    }
 }
 
 IMPL_LINK(SvxExtParagraphTabPage, AcrossPageHdl_Impl, weld::Toggleable&, rToggle, void)
@@ -2676,11 +2782,23 @@ IMPL_LINK(SvxExtParagraphTabPage, AcrossPageHdl_Impl, weld::Toggleable&, rToggle
     {
         m_xAcrossParagraphBox->set_state( TRISTATE_TRUE );
         m_xAcrossColumnBox->set_state( TRISTATE_TRUE );
+        m_xParagraphEndZoneLabel->set_sensitive(true);
+        m_aParagraphEndZone.set_sensitive(true);
+        m_xColumnEndZoneLabel->set_sensitive(true);
+        m_aColumnEndZone.set_sensitive(true);
+        m_xPageEndZoneLabel->set_sensitive(true);
+        m_aPageEndZone.set_sensitive(true);
+        m_xSpreadEndZoneLabel->set_sensitive(false);
+        m_aSpreadEndZone.set_sensitive(false);
     }
     else
     {
         m_xAcrossSpreadBox->set_state( TRISTATE_FALSE );
         m_xAcrossMoveLineBox->set_sensitive( true );
+        m_xPageEndZoneLabel->set_sensitive(false);
+        m_aPageEndZone.set_sensitive(false);
+        m_xSpreadEndZoneLabel->set_sensitive(false);
+        m_aSpreadEndZone.set_sensitive(false);
     }
 }
 
@@ -2692,12 +2810,24 @@ IMPL_LINK(SvxExtParagraphTabPage, AcrossSpreadHdl_Impl, weld::Toggleable&, rTogg
         m_xAcrossParagraphBox->set_state( TRISTATE_TRUE );
         m_xAcrossColumnBox->set_state( TRISTATE_TRUE );
         m_xAcrossPageBox->set_state( TRISTATE_TRUE );
-    }
-    // only sensitive, if the hyphenation is disabled across spreads
-    bool bAcrossSpread = m_xAcrossSpreadBox->get_state() == TRISTATE_TRUE;
-    m_xAcrossMoveLineBox->set_sensitive( !bAcrossSpread );
-    if ( bAcrossSpread )
+        m_xParagraphEndZoneLabel->set_sensitive(true);
+        m_aParagraphEndZone.set_sensitive(true);
+        m_xColumnEndZoneLabel->set_sensitive(true);
+        m_aColumnEndZone.set_sensitive(true);
+        m_xPageEndZoneLabel->set_sensitive(true);
+        m_aPageEndZone.set_sensitive(true);
+        m_xSpreadEndZoneLabel->set_sensitive(true);
+        m_aSpreadEndZone.set_sensitive(true);
+        m_xAcrossMoveLineBox->set_sensitive( false );
         m_xAcrossMoveLineBox->set_state( TRISTATE_FALSE );
+    }
+    else
+    {
+        m_xSpreadEndZoneLabel->set_sensitive(false);
+        m_aSpreadEndZone.set_sensitive(false);
+        // only sensitive, if the hyphenation is disabled across spreads
+        m_xAcrossMoveLineBox->set_sensitive( true );
+    }
 }
 
 SvxAsianTabPage::SvxAsianTabPage(weld::Container* pPage, weld::DialogController* pController, const SfxItemSet& rSet)

commit 5daab76058e38ae16180b1bea239161b7c8be274

tdf#165984 sw: don't apply hyphenation zone at disabled hyphenation
Fix unit tests testTdf132599_column and
testTdf132599_page_in_last_column.

In widorp.cxx, end zone is calculated by the inheritance of
the hyphenation zone, and as a regression, it was applied in
the last line enabling hyphenation disabled by hyphenation-keep
and hyphenation-keep-type.

As an optimization, skip also frame update of the last line, when
end zone is not different from the hyphenation zone, i.e. when
the last line has already had correct line break.

Regression from commit 7d384fb1c232f7aa720436bc68dc1de334bf7297
"tdf#165984 sw cui xmloff: add Paragraph/Column/Page/Spread end zone".

Change-Id: Ia54c9164d5297b5a73649259b4a25a808ece68e3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183673
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/widorp.cxx
@@ -541,7 +541,11 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
             pMaster->InvalidateSize_();
         }
 
-        if ( ( bKeep || nNoHyphEndZone ) && pMasterPara && pMasterPara->GetNext() )
+        // applied end zone, i.e. hyphenation is not disabled completely,
+        // end zone is not zero and different from the hyphenation zone
+        bool bApplyEndZone = !bKeep && nNoHyphEndZone > 0 &&
+                nNoHyphEndZone != rAttr.GetTextHyphenZone();
+        if ( ( bKeep || bApplyEndZone ) && pMasterPara && pMasterPara->GetNext() )
         {
             // calculate the beginning of last hyphenated line
             TextFrameIndex nIdx(pMasterPara->GetLen());
@@ -559,14 +563,14 @@ bool WidowsAndOrphans::FindWidows( SwTextFrame *pFrame, SwTextMargin &rLine )
             nIdx -= pNext->GetLen();
             // hyphenated line, but not the last remaining one
             // in the case of shifting full line (bKeepLine = false)
-            if ( pNext->IsEndHyph() && ( bKeepLine || !pNext->IsLastHyph() || nNoHyphEndZone ) )
+            if ( pNext->IsEndHyph() && ( bKeepLine || !pNext->IsLastHyph() || bApplyEndZone ) )
             {
                 nExtraWidLines = rLine.GetLineNr() - m_nWidLines + 1;
                 // shift only a word: disable hyphenation in the line, if needed
-                if ( ( bKeepLine || nNoHyphEndZone ) && nExtraWidLines )
+                if ( ( bKeepLine || bApplyEndZone ) && nExtraWidLines )
                 {
                     pMaster->SetNoHyphOffset(nIdx);
-                    pMaster->SetNoHyphEndZone(bKeep ? -1 : nNoHyphEndZone);
+                    pMaster->SetNoHyphEndZone(bApplyEndZone ? nNoHyphEndZone : -1);
                     // update also columns and frames
                     pMaster->Prepare( PrepareHint::AdjustSizeWithoutFormatting );
                     pMaster->InvalidateSize_();