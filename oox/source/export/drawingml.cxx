0151a5a2e9114375cd3256e3cca495b2347ebefb
Simon Chenery <simon_chenery@yahoo.com>
2025-04-05 09:28:34 +0200

    Check oox-drawingml-adj-names has delimiter to avoid std::out_of_range exception
    Skip line with warning if no tab delimiter found. This avoids a
    std::string_view.substr(-1) call, that throws an std::out_of_range
    exception.
    
    Change-Id: Ie35ea692ce3c2cbc8c21c01b0c488a6bb5eb5e32
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183741
    Tested-by: Jenkins
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- oox/source/export/drawingml.cxx
+++ oox/source/export/drawingml.cxx
@@ -4504,8 +4504,15 @@ static std::map< OString, std::vector<OString> > lcl_getAdjNames()
         sal_Int32 nIndex = 0;
         // Each line is in a "key\tvalue" format: read the key, the rest is the value.
         OString aKey( o3tl::getToken(aLine, 0, '\t', nIndex) );
-        OString aValue( std::string_view(aLine).substr(nIndex) );
-        aRet[aKey].push_back(aValue);
+        if (nIndex >= 0)
+        {
+            OString aValue( std::string_view(aLine).substr(nIndex) );
+            aRet[aKey].push_back(aValue);
+        }
+        else
+        {
+            SAL_WARN("oox.shape", "skipping invalid line: " << std::string_view(aLine));
+        }
         bNotDone = aStream.ReadLine(aLine);
     }
     return aRet;
