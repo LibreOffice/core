guillaume-carbone/LibreOffice-core

related tdf#162786, tdf#159988: fix arm64 for linux
Change-Id: Ia2670b3827880f4aba642390e68ffcc77dc63740

external/python3/ExternalPackage_python3.mk
@@ -58,7 +58,12 @@ $(eval $(call gb_ExternalPackage_add_file,python3,$(LIBO_BIN_FOLDER)/libpython$(
 # Obviously this list should not contain stuff with external dependencies
 # that may not be available on baseline systems.
 
-python3_EXTENSION_MODULE_SUFFIX=cpython-$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)$(if $(ENABLE_DBGUTIL),d)-x86_64-linux-gnu
+ifeq ($(CPUNAME),ARM)
+SOABI=-aarch64-linux-gnu
+else
+SOABI=-x86_64-linux-gnu
+endif
+python3_EXTENSION_MODULE_SUFFIX=cpython-$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)$(if $(ENABLE_DBGUTIL),d)$(SOABI)
 python3_EXTENSION_MODULES= \
 	LO_lib/array.$(python3_EXTENSION_MODULE_SUFFIX).so \
 	LO_lib/_asyncio.$(python3_EXTENSION_MODULE_SUFFIX).so \

LibreOffice/core

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183371
Reviewed-by: Guillaume Paquereau <guillaume@carbone.io>
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Tested-by: Jenkins
(cherry picked from commit 315febd7ead36519b627bb3d8980b32f393c6062)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183471
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

external/python3/ExternalPackage_python3.mk
@@ -58,7 +58,12 @@ $(eval $(call gb_ExternalPackage_add_file,python3,$(LIBO_BIN_FOLDER)/libpython$(
 # Obviously this list should not contain stuff with external dependencies
 # that may not be available on baseline systems.
 
-python3_EXTENSION_MODULE_SUFFIX=cpython-$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)$(if $(ENABLE_DBGUTIL),d)-x86_64-linux-gnu
+ifeq ($(CPUNAME),AARCH64)
+SOABI=-aarch64-linux-gnu
+else
+SOABI=-x86_64-linux-gnu
+endif
+python3_EXTENSION_MODULE_SUFFIX=cpython-$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)$(if $(ENABLE_DBGUTIL),d)$(SOABI)
 python3_EXTENSION_MODULES= \
 	LO_lib/array.$(python3_EXTENSION_MODULE_SUFFIX).so \
 	LO_lib/_asyncio.$(python3_EXTENSION_MODULE_SUFFIX).so \