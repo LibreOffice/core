8811111e3729c2fb10ece0ce34f911e9454b5485
Tor Lillqvist <tml@collabora.com>
2025-04-08 16:58:59 +0300

    Add --enable-msvc-debug-runtime to use that even without dbgutil
    Adapt libxml2 build accordingly. It's MSVC_USE_DEBUG_RUNTIME we should
    check to see whether the ICU libraries have a 'd' suffix in their
    names, not ENABLE_DBGUTIL.
    
    Change-Id: Ic817e3c15e7164478ae557627047d3793e3a88c7

--- external/libxml2/ExternalProject_libxml2.mk
+++ external/libxml2/ExternalProject_libxml2.mk
@@ -27,7 +27,7 @@ $(call gb_ExternalProject_get_state_target,libxml2,build):
 	$(call gb_ExternalProject_run,build,\
 		cscript /e:javascript configure.js \
 			iconv=no icu=yes sax1=yes $(if $(MSVC_USE_DEBUG_RUNTIME),cruntime=/MDd) \
-			$(if $(filter TRUE,$(ENABLE_DBGUTIL)),debug=yes) \
+			$(if $(filter TRUE,$(MSVC_USE_DEBUG_RUNTIME)),debug=yes) \
 		&& nmake \
 	,win32)
 	$(call gb_Trace_EndRange,libxml2,EXTERNAL)
