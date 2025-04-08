b07230384c51b99f8e98737afef6d0df908c240f
Szymon Kłos <szymon.klos@collabora.com>
2025-04-08 06:42:58 +0000

    Fix potentially uninitialized local pointer variable
    Change-Id: Iaa7619491c1462303f4df82c1cb12f23409c4986
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183833
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sd/source/ui/docshell/docshel3.cxx
+++ sd/source/ui/docshell/docshel3.cxx
@@ -234,11 +234,11 @@ void DrawDocShell::Execute( SfxRequest& rReq )
         case SID_SAVEDOC:
         case SID_SAVEASDOC:
         {
-            const SfxPoolItem* pItem;
+            const SfxPoolItem* pItem = nullptr;
             bool bCommitChanges = false;
             const SfxItemSet* pReqArgs = rReq.GetArgs();
             bool bHasDontTerminateEdit = pReqArgs && pReqArgs->HasItem(FN_PARAM_1, &pItem);
-            if (bHasDontTerminateEdit)
+            if (bHasDontTerminateEdit && pItem)
                 bCommitChanges = !static_cast<const SfxBoolItem*>(pItem)->GetValue();
 
             if (mpViewShell && bCommitChanges)
