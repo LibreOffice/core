LibreOffice/core

commit 898e4ae1364e76af8be22183ac64d73b6a6d8d90

tdf#128794 Chart: Fix OOXML import/export of Radial gradient
Style should be radial at least when the horizontal/vertical
center is not in the corner of a shape. Otherwise import
as a linear gradient, because it is the most similar to the
MSO radial style.

Change-Id: I9bab7b787897bde51a06a950487de9843eb717a3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/93497
Tested-by: Jenkins
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: Tünde Tóth <tundeth@gmail.com>
Reviewed-by: László Németh <nemeth@numbertext.org>

oox/source/drawingml/fillproperties.cxx
@@ -374,23 +374,35 @@ void FillProperties::pushToPropMap( ShapePropertyMap& rPropMap,
 
                 if( maGradientProps.moGradientPath.has() )
                 {
-                    // position of gradient center (limited to [30%;100%], otherwise gradient is too hidden)
                     IntegerRectangle2D aFillToRect = maGradientProps.moFillToRect.get( IntegerRectangle2D( 0, 0, MAX_PERCENT, MAX_PERCENT ) );
                     sal_Int32 nCenterX = (MAX_PERCENT + aFillToRect.X1 - aFillToRect.X2) / 2;
                     aGradient.XOffset = getLimitedValue<sal_Int16, sal_Int32>(
-                        nCenterX / PER_PERCENT, 30, 100);
-
-                    // Style should be radial at least when the horizontal center is at 50%.
-                    awt::GradientStyle eCircle = aGradient.XOffset == 50
-                                                     ? awt::GradientStyle_RADIAL
-                                                     : awt::GradientStyle_ELLIPTICAL;
-                    aGradient.Style = (maGradientProps.moGradientPath.get() == XML_circle)
-                                          ? eCircle
-                                          : awt::GradientStyle_RECT;
-
+                        nCenterX / PER_PERCENT, 0, 100);
                     sal_Int32 nCenterY = (MAX_PERCENT + aFillToRect.Y1 - aFillToRect.Y2) / 2;
                     aGradient.YOffset = getLimitedValue<sal_Int16, sal_Int32>(
-                        nCenterY / PER_PERCENT, 30, 100);
+                        nCenterY / PER_PERCENT, 0, 100);
+
+                    if( maGradientProps.moGradientPath.get() == XML_circle )
+                    {
+                        // Style should be radial at least when the horizontal center is at 50%.
+                        // Otherwise import as a linear gradient, because it is the most similar to the MSO radial style.
+                        aGradient.Style = awt::GradientStyle_LINEAR;
+                        if( aGradient.XOffset == 100 && aGradient.YOffset == 100 )
+                            aGradient.Angle = 450;
+                        else if( aGradient.XOffset == 0 && aGradient.YOffset == 100 )
+                            aGradient.Angle = 3150;
+                        else if( aGradient.XOffset == 100 && aGradient.YOffset == 0 )
+                            aGradient.Angle = 1350;
+                        else if( aGradient.XOffset == 0 && aGradient.YOffset == 0 )
+                            aGradient.Angle = 2250;
+                        else
+                            aGradient.Style = awt::GradientStyle_RADIAL;
+                    }
+                    else
+                    {
+                        aGradient.Style = awt::GradientStyle_RECT;
+                    }
+
                     ::std::swap( aGradient.StartColor, aGradient.EndColor );
                     ::std::swap( nStartTrans, nEndTrans );
 

chart2/qa/extras/chart2export.cxx
@@ -96,6 +96,7 @@ public:
     void testColorGradientWithTransparancyDOCX();
     void testColorGradientWithTransparancyODS();
     void testColorGradientStopXLSX();
+    void testRadialColorGradientDOCX();
     void testBarChartDataPointPropDOCX();
     void testFdo83058dlblPos();
     void testAutoTitleDelXLSX();
@@ -225,6 +226,7 @@ public:
     CPPUNIT_TEST(testColorGradientWithTransparancyDOCX);
     CPPUNIT_TEST(testColorGradientWithTransparancyODS);
     CPPUNIT_TEST(testColorGradientStopXLSX);
+    CPPUNIT_TEST(testRadialColorGradientDOCX);
     CPPUNIT_TEST(testBarChartDataPointPropDOCX);
     CPPUNIT_TEST(testFdo83058dlblPos);
     CPPUNIT_TEST(testAutoTitleDelXLSX);
@@ -1345,6 +1347,17 @@ void Chart2ExportTest::testColorGradientStopXLSX()
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:barChart/c:ser/c:spPr/a:gradFill/a:gsLst/a:gs[1]", "pos", "45000");
     // Test the position of the second color
     assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:barChart/c:ser/c:spPr/a:gradFill/a:gsLst/a:gs[2]", "pos", "100000");
+}
+
+void Chart2ExportTest::testRadialColorGradientDOCX()
+{
+    load("/chart2/qa/extras/data/docx/", "tdf128794.docx");
+    xmlDocPtr pXmlDoc = parseExport("word/charts/chart", "Office Open XML Text");
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Test the gradeint style (if there is no 'a:path' attribute, it is a linear gradient)
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:path", 0);
+    // Test the linear gradeint angle
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:lin", "ang", "13500000");
 }
 
 void Chart2ExportTest::testBarChartDataPointPropDOCX()

commit 65439ab2c4fc87e7cc115710a605e9f35dfb39d8

tdf#154858 - FILEOPEN PPTX: fix Radial gradient in shape imported as
a Linear gradient

XML_circle gradients should be always imported as GradientStyle_RADIAL.

Regression from: 898e4ae1364e76af8be22183ac64d73b6a6d8d90

Change-Id: I8166a2afd3dadce61c742eeb0a55a0c8a8b9ada6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183836
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

oox/source/drawingml/fillproperties.cxx
@@ -525,8 +525,8 @@ void FillProperties::pushToPropMap(ShapePropertyMap& rPropMap, const GraphicHelp
                             aGradient.SetAngle( Degree10(1350) );
                         else if( 0 == aGradient.GetXOffset() && 0 == aGradient.GetYOffset() )
                             aGradient.SetAngle( Degree10(2250) );
-                        else
-                            aGradient.SetGradientStyle(awt::GradientStyle_RADIAL);
+
+                        aGradient.SetGradientStyle(awt::GradientStyle_RADIAL);
                     }
                     else
                     {

chart2/qa/extras/chart2export3.cxx
@@ -141,9 +141,13 @@ CPPUNIT_TEST_FIXTURE(Chart2ExportTest3, testRadialColorGradientDOCX)
     xmlDocUniquePtr pXmlDoc = parseExport(u"word/charts/chart1.xml"_ustr);
     CPPUNIT_ASSERT(pXmlDoc);
     // Test the gradient style (if there is no 'a:path' attribute, it is a linear gradient)
-    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:path", 0);
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:path", "path", u"circle");
     // Test the linear gradient angle
-    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:lin", "ang", u"13500000");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:gsLst/a:gs[1]", "pos", u"0");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:gsLst/a:gs[1]/a:srgbClr", "val", u"ffffff");
+
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:gsLst/a:gs[2]", "pos", u"100000");
+    assertXPath(pXmlDoc, "/c:chartSpace/c:chart/c:plotArea/c:spPr/a:gradFill/a:gsLst/a:gs[2]/a:srgbClr", "val", u"5b9bd5");
 }
 
 CPPUNIT_TEST_FIXTURE(Chart2ExportTest3, testBarChartDataPointPropDOCX)

sd/qa/unit/import-tests.cxx
@@ -35,6 +35,7 @@
 #include <sax/tools/converter.hxx>
 
 #include <com/sun/star/awt/Gradient.hpp>
+#include <com/sun/star/awt/Gradient2.hpp>
 #include <com/sun/star/awt/FontWeight.hpp>
 #include <com/sun/star/document/XDocumentPropertiesSupplier.hpp>
 #include <com/sun/star/document/XEventsSupplier.hpp>
@@ -355,6 +356,15 @@ CPPUNIT_TEST_FIXTURE(SdImportTest, testTdf154363)
         nGlueId = xConnector2->getPropertyValue(u"EndGluePointIndex"_ustr).get<sal_Int32>();
         CPPUNIT_ASSERT_EQUAL(sal_Int32(1), nGlueId);
     }
+}
+
+CPPUNIT_TEST_FIXTURE(SdImportTest, testTdf154858)
+{
+    createSdImpressDoc("pptx/tdf154858.pptx");
+    uno::Reference<beans::XPropertySet> xShapeProp(getShapeFromPage(0, 0), uno::UNO_SET_THROW);
+    awt::Gradient2 aGradient;
+    CPPUNIT_ASSERT(xShapeProp->getPropertyValue(u"FillGradient"_ustr) >>= aGradient);
+    CPPUNIT_ASSERT_EQUAL(awt::GradientStyle_RADIAL, aGradient.Style);
 }
 
 CPPUNIT_TEST_FIXTURE(SdImportTest, testTdf153466)