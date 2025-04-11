137a7fa96819d84557c9f1fb13e95d6a1e23d0c8
Miklos Vajna <vmiklos@collabora.com>
2025-04-11 16:50:43 +0200

    tdf#166125 Revert "tdf#158532 sw: fix toolbar buttons remain disabled ...
    ... after pasting an image"
    
    This reverts commit a586ce188086ff27b08b8a4de4672c07ddf8ed7c (tdf#158532
    sw: fix toolbar buttons remain disabled after pasting an image,
    2024-01-03). The original problem is not reproducible anymore and it
    causes tdf#166125, so best to revert it.
    
    Change-Id: I7bee45da2835106915d4f9f7e6410725d6c854d5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184054
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit b595dff0bf2fcdb63586e36d007408cf23359a65)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184075
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184267

--- sw/qa/extras/ooxmlexport/ooxmlexport22.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport22.cxx
@@ -13,6 +13,7 @@
 
 #include <comphelper/configuration.hxx>
 #include <comphelper/sequenceashashmap.hxx>
+#include <comphelper/propertyvalue.hxx>
 #include <officecfg/Office/Common.hxx>
 
 #include <pam.hxx>
@@ -41,6 +42,30 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf165642_glossaryFootnote)
     // thus, the footnote and endnote files must also be round-tripped
     parseExport(u"word/glossary/endnotes.xml"_ustr);
     parseExport(u"word/glossary/footnotes.xml"_ustr);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testBadFormulaResult)
+{
+    // Given a loaded by not laid out document:
+    uno::Sequence<beans::PropertyValue> aFilterOptions = {
+        comphelper::makePropertyValue("Hidden", true),
+    };
+    mxComponent = loadFromDesktop(m_directories.getURLFromSrc(u"/sw/qa/extras/ooxmlexport/data/")
+                                      + "formula-result.fodt",
+                                  "com.sun.star.text.TextDocument", aFilterOptions);
+
+    // When saving to DOCX:
+    save(mpFilter);
+
+    // Then make sure that the field result in the last row's last run is not empty:
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+    CPPUNIT_ASSERT(pXmlDoc);
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 6
+    // - Actual  : 0
+    // i.e. the SUM() field evaluated to an empty result.
+    assertXPathContent(pXmlDoc, "/w:document/w:body/w:tbl/w:tr[4]/w:tc/w:p/w:r[4]/w:t"_ostr,
+                       u"6"_ustr);
 }
 
 } // end of anonymous namespace
