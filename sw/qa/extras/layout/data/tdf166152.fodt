LibreOffice/core

commit 537645c0834eab2d277113f1e3fcf039c994832d

tdf#66791 sw: Treat weak punctuation as Asian in Asian paragraphs
This change modifies script detection to treat certain weak punctuation
marks, particularly left- and right- quotation marks, as Asian script in
paragraphs containing Chinese and Japanese characters, but no Complex
characters. This change improves our script detection compatibility with
other programs.

As part of this change, duplicated script detection code has been
extracted from Writer and Edit Engine, and consolidated.

Change-Id: Ib2880f2e832aaac4c0093971daa88223c7232d63
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178608
Tested-by: Jenkins
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>

sw/source/core/text/porlay.cxx
@@ -80,6 +80,7 @@
 #include <i18nutil/scripttypedetector.hxx>
 #include <i18nutil/unicode.hxx>
 #include <i18nutil/kashida.hxx>
+#include <i18nutil/scriptchangescanner.hxx>
 #include <unotxdoc.hxx>
 
 using namespace ::com::sun::star;
@@ -1410,40 +1411,17 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
     // remove invalid entries from kashida array
     m_Kashida.erase(m_Kashida.begin() + nCntKash, m_Kashida.end());
 
-    // TAKE CARE OF WEAK CHARACTERS: WE MUST FIND AN APPROPRIATE
-    // SCRIPT FOR WEAK CHARACTERS AT THE BEGINNING OF A PARAGRAPH
-
-    if (WEAK == g_pBreakIt->GetBreakIter()->getScriptType(rText, sal_Int32(nChg)))
+    // Construct the script change scanner and advance it to the change range
+    auto pScriptScanner = i18nutil::MakeScriptChangeScanner(
+        rText, SvtLanguageOptions::GetI18NScriptTypeOfLanguage(GetAppLanguage()));
+    while (!pScriptScanner->AtEnd())
     {
-        // If the beginning of the current group is weak, this means that
-        // all of the characters in this group are weak. We have to assign
-        // the scripts to these characters depending on the fonts which are
-        // set for these characters to display them.
-        TextFrameIndex nEnd(
-            g_pBreakIt->GetBreakIter()->endOfScript(rText, sal_Int32(nChg), WEAK));
-
-        if (nEnd > TextFrameIndex(rText.getLength()) || nEnd < TextFrameIndex(0))
-            nEnd = TextFrameIndex(rText.getLength());
-
-        nScript = SvtLanguageOptions::GetI18NScriptTypeOfLanguage( GetAppLanguage() );
-
-        SAL_WARN_IF( i18n::ScriptType::LATIN != nScript &&
-                i18n::ScriptType::ASIAN != nScript &&
-                i18n::ScriptType::COMPLEX != nScript, "sw.core", "Wrong default language" );
-
-        nChg = nEnd;
-
-        // Get next script type or set to weak in order to exit
-        sal_uInt8 nNextScript = (nEnd < TextFrameIndex(rText.getLength()))
-            ? static_cast<sal_uInt8>(g_pBreakIt->GetBreakIter()->getScriptType(rText, sal_Int32(nEnd)))
-            : sal_uInt8(WEAK);
-
-        if ( nScript != nNextScript )
+        if (pScriptScanner->Peek().m_nStartIndex <= static_cast<sal_Int32>(nChg))
         {
-            m_ScriptChanges.emplace_back(nEnd, nScript);
-            nCnt++;
-            nScript = nNextScript;
+            break;
         }
+
+        pScriptScanner->Advance();
     }
 
     // UPDATE THE SCRIPT INFO ARRAYS:
@@ -1451,38 +1429,12 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
     while (nChg < TextFrameIndex(rText.getLength())
            || (m_ScriptChanges.empty() && rText.isEmpty()))
     {
-        SAL_WARN_IF( i18n::ScriptType::WEAK == nScript,
-                "sw.core", "Inserting WEAK into SwScriptInfo structure" );
+        auto stChange = pScriptScanner->Peek();
+        pScriptScanner->Advance();
 
-        TextFrameIndex nSearchStt = nChg;
-        nChg = TextFrameIndex(g_pBreakIt->GetBreakIter()->endOfScript(
-                    rText, sal_Int32(nSearchStt), nScript));
-
-        if (nChg > TextFrameIndex(rText.getLength()) || nChg < TextFrameIndex(0))
-            nChg = TextFrameIndex(rText.getLength());
-
-        // special case for dotted circle since it can be used with complex
-        // before a mark, so we want it associated with the mark's script
-        // tdf#112594: another special case for NNBSP followed by a Mongolian
-        // character, since NNBSP has special uses in Mongolian (tdf#112594)
-        auto nPos = sal_Int32(nChg);
-        auto nPrevPos = nPos;
-        auto nPrevChar = rText.iterateCodePoints(&nPrevPos, -1);
-        if (nChg < TextFrameIndex(rText.getLength()) && nChg > TextFrameIndex(0) &&
-            i18n::ScriptType::WEAK == g_pBreakIt->GetBreakIter()->getScriptType(rText, nPrevPos))
-        {
-            auto nChar = rText.iterateCodePoints(&nPos, 0);
-            auto nType = unicode::getUnicodeType(nChar);
-            if (nType == css::i18n::UnicodeType::NON_SPACING_MARK ||
-                nType == css::i18n::UnicodeType::ENCLOSING_MARK ||
-                nType == css::i18n::UnicodeType::COMBINING_SPACING_MARK ||
-                (nPrevChar == CHAR_NNBSP &&
-                 u_getIntPropertyValue(nChar, UCHAR_SCRIPT) == USCRIPT_MONGOLIAN))
-            {
-                nPos = nPrevPos;
-            }
-        }
-        m_ScriptChanges.emplace_back(TextFrameIndex(nPos), nScript);
+        nScript = stChange.m_nScriptType;
+        nChg = TextFrameIndex{ stChange.m_nEndIndex };
+        m_ScriptChanges.emplace_back(nChg, nScript);
         ++nCnt;
 
         // if current script is asian, we search for compressible characters

editeng/source/editeng/impedit2.cxx
@@ -63,6 +63,7 @@
 #include <svl/asiancfg.hxx>
 #include <svl/voiditem.hxx>
 #include <i18nutil/unicode.hxx>
+#include <i18nutil/scriptchangescanner.hxx>
 #include <comphelper/diagnose_ex.hxx>
 #include <comphelper/flagguard.hxx>
 #include <comphelper/lok.hxx>
@@ -1715,50 +1716,15 @@ void ImpEditEngine::InitScriptTypes( sal_Int32 nPara )
         pField = pField->GetEnd() ? pNode->GetCharAttribs().FindNextAttrib( EE_FEATURE_FIELD, pField->GetEnd() ) : nullptr;
     }
 
-    sal_Int32 nTextLen = aText.getLength();
-
-    sal_Int32 nPos = 0;
-    short nScriptType = _xBI->getScriptType( aText, nPos );
-    rTypes.emplace_back( nScriptType, nPos, nTextLen );
-    nPos = _xBI->endOfScript( aText, nPos, nScriptType );
-    while ( ( nPos != -1 ) && ( nPos < nTextLen ) )
+    auto pScriptScanner = i18nutil::MakeScriptChangeScanner(
+        aText, SvtLanguageOptions::GetI18NScriptTypeOfLanguage(GetDefaultLanguage()));
+    while (!pScriptScanner->AtEnd() || rTypes.empty())
     {
-        rTypes.back().nEndPos = nPos;
+        auto stChange = pScriptScanner->Peek();
+        rTypes.emplace_back(stChange.m_nScriptType, stChange.m_nStartIndex, stChange.m_nEndIndex);
 
-        nScriptType = _xBI->getScriptType( aText, nPos );
-        tools::Long nEndPos = _xBI->endOfScript( aText, nPos, nScriptType );
-
-        if ( ( nScriptType == i18n::ScriptType::WEAK ) || ( nScriptType == rTypes.back().nScriptType ) )
-        {
-            // Expand last ScriptTypePosInfo, don't create weak or unnecessary portions
-            rTypes.back().nEndPos = nEndPos;
-        }
-        else
-        {
-            auto nPrevPos = nPos;
-            auto nPrevChar = aText.iterateCodePoints(&nPrevPos, -1);
-            if (_xBI->getScriptType(aText, nPrevPos) == i18n::ScriptType::WEAK)
-            {
-                auto nChar = aText.iterateCodePoints(&nPos, 0);
-                auto nType = unicode::getUnicodeType(nChar);
-                if (nType == css::i18n::UnicodeType::NON_SPACING_MARK ||
-                    nType == css::i18n::UnicodeType::ENCLOSING_MARK ||
-                    nType == css::i18n::UnicodeType::COMBINING_SPACING_MARK ||
-                    (nPrevChar == 0x202F /* NNBSP, tdf#112594 */ &&
-                     u_getIntPropertyValue(nChar, UCHAR_SCRIPT) == USCRIPT_MONGOLIAN))
-                {
-                    rTypes.back().nEndPos = nPos = nPrevPos;
-                    break;
-                }
-            }
-            rTypes.emplace_back( nScriptType, nPos, nTextLen );
-        }
-
-        nPos = nEndPos;
+        pScriptScanner->Advance();
     }
-
-    if ( rTypes[0].nScriptType == i18n::ScriptType::WEAK )
-        rTypes[0].nScriptType = ( rTypes.size() > 1 ) ? rTypes[1].nScriptType : SvtLanguageOptions::GetI18NScriptTypeOfLanguage( GetDefaultLanguage() );
 
     // create writing direction information:
     WritingDirectionInfos& rDirInfos = pParaPortion->getWritingDirectionInfos();

i18npool/source/breakiterator/breakiteratorImpl.cxx
@@ -22,6 +22,7 @@
 #include <breakiteratorImpl.hxx>
 #include <cppuhelper/supportsservice.hxx>
 #include <unicode/uchar.h>
+#include <i18nutil/scriptclass.hxx>
 #include <i18nutil/unicode.hxx>
 #include <o3tl/string_view.hxx>
 
@@ -449,94 +450,6 @@ sal_Int16 SAL_CALL BreakIteratorImpl::getWordType( const OUString& /*Text*/,
     return 0;
 }
 
-namespace
-{
-sal_Int16 getScriptClassByUAX24Script(sal_uInt32 currentChar)
-{
-    int32_t script = u_getIntPropertyValue(currentChar, UCHAR_SCRIPT);
-    return unicode::getScriptClassFromUScriptCode(static_cast<UScriptCode>(script));
-}
-
-struct UBlock2Script
-{
-    UBlockCode from;
-    UBlockCode to;
-    sal_Int16 script;
-};
-
-const UBlock2Script scriptList[] =
-{
-    {UBLOCK_NO_BLOCK, UBLOCK_NO_BLOCK, ScriptType::WEAK},
-    {UBLOCK_BASIC_LATIN, UBLOCK_SPACING_MODIFIER_LETTERS, ScriptType::LATIN},
-    {UBLOCK_GREEK, UBLOCK_ARMENIAN, ScriptType::LATIN},
-    {UBLOCK_HEBREW, UBLOCK_MYANMAR, ScriptType::COMPLEX},
-    {UBLOCK_GEORGIAN, UBLOCK_GEORGIAN, ScriptType::LATIN},
-    {UBLOCK_HANGUL_JAMO, UBLOCK_HANGUL_JAMO, ScriptType::ASIAN},
-    {UBLOCK_ETHIOPIC, UBLOCK_ETHIOPIC, ScriptType::COMPLEX},
-    {UBLOCK_CHEROKEE, UBLOCK_RUNIC, ScriptType::LATIN},
-    {UBLOCK_KHMER, UBLOCK_MONGOLIAN, ScriptType::COMPLEX},
-    {UBLOCK_LATIN_EXTENDED_ADDITIONAL, UBLOCK_GREEK_EXTENDED, ScriptType::LATIN},
-    {UBLOCK_NUMBER_FORMS, UBLOCK_NUMBER_FORMS, ScriptType::WEAK},
-    {UBLOCK_CJK_RADICALS_SUPPLEMENT, UBLOCK_HANGUL_SYLLABLES, ScriptType::ASIAN},
-    {UBLOCK_CJK_COMPATIBILITY_IDEOGRAPHS, UBLOCK_CJK_COMPATIBILITY_IDEOGRAPHS, ScriptType::ASIAN},
-    {UBLOCK_ARABIC_PRESENTATION_FORMS_A, UBLOCK_ARABIC_PRESENTATION_FORMS_A, ScriptType::COMPLEX},
-    {UBLOCK_CJK_COMPATIBILITY_FORMS, UBLOCK_CJK_COMPATIBILITY_FORMS, ScriptType::ASIAN},
-    {UBLOCK_ARABIC_PRESENTATION_FORMS_B, UBLOCK_ARABIC_PRESENTATION_FORMS_B, ScriptType::COMPLEX},
-    {UBLOCK_HALFWIDTH_AND_FULLWIDTH_FORMS, UBLOCK_HALFWIDTH_AND_FULLWIDTH_FORMS, ScriptType::ASIAN},
-    {UBLOCK_CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B, UBLOCK_CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT, ScriptType::ASIAN},
-    {UBLOCK_CJK_STROKES, UBLOCK_CJK_STROKES, ScriptType::ASIAN},
-    {UBLOCK_LATIN_EXTENDED_C, UBLOCK_LATIN_EXTENDED_D, ScriptType::LATIN}
-};
-
-#define scriptListCount SAL_N_ELEMENTS(scriptList)
-
-//always sets rScriptType
-
-//returns true for characters historically explicitly assigned to
-//latin/weak/asian
-
-//returns false for characters that historically implicitly assigned to
-//weak as unknown
-bool getCompatibilityScriptClassByBlock(sal_uInt32 currentChar, sal_Int16 &rScriptType)
-{
-    bool bKnown = true;
-    //handle specific characters always as weak:
-    //  0x01 - this breaks a word
-    //  0x02 - this can be inside a word
-    //  0x20 & 0xA0 - Bug 102975, declare western space and non-break space as WEAK char.
-    if( 0x01 == currentChar || 0x02 == currentChar || 0x20 == currentChar || 0xA0 == currentChar)
-        rScriptType = ScriptType::WEAK;
-    // Few Spacing Modifier Letters that can be Bopomofo tonal marks.
-    else if ( 0x2CA == currentChar || 0x2CB == currentChar || 0x2C7 == currentChar || 0x2D9 == currentChar )
-        rScriptType = ScriptType::WEAK;
-    // tdf#52577 superscript numbers should be we weak.
-    else if ( 0xB2 == currentChar || 0xB3 == currentChar || 0xB9 == currentChar )
-        rScriptType = ScriptType::WEAK;
-    // workaround for Coptic
-    else if ( 0x2C80 <= currentChar && 0x2CE3 >= currentChar)
-        rScriptType = ScriptType::LATIN;
-    else
-    {
-        UBlockCode block=ublock_getCode(currentChar);
-        size_t i = 0;
-        while (i < scriptListCount)
-        {
-            if (block <= scriptList[i].to)
-                break;
-            ++i;
-        }
-        if (i < scriptListCount && block >= scriptList[i].from)
-            rScriptType = scriptList[i].script;
-        else
-        {
-            rScriptType = ScriptType::WEAK;
-            bKnown = false;
-        }
-    }
-    return bKnown;
-}
-}
-
 sal_Int16  BreakIteratorImpl::getScriptClass(sal_uInt32 currentChar)
 {
     static sal_uInt32 lastChar = 0;
@@ -545,9 +458,7 @@ sal_Int16  BreakIteratorImpl::getScriptClass(sal_uInt32 currentChar)
     if (currentChar != lastChar)
     {
         lastChar = currentChar;
-
-        if (!getCompatibilityScriptClassByBlock(currentChar, nRet))
-            nRet = getScriptClassByUAX24Script(currentChar);
+        nRet = i18nutil::GetScriptClass(currentChar);
     }
 
     return nRet;

i18nutil/CppunitTest_i18nutil.mk
@@ -9,13 +9,16 @@ For makefiles:
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-$(eval $(call gb_CppunitTest_CppunitTest,i18nutil_kashida))
+$(eval $(call gb_CppunitTest_CppunitTest,i18nutil))
 
-$(eval $(call gb_CppunitTest_add_exception_objects,i18nutil_kashida,\
+$(eval $(call gb_CppunitTest_use_sdk_api,i18nutil))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,i18nutil,\
 	i18nutil/qa/cppunit/test_kashida \
+	i18nutil/qa/cppunit/test_scriptchangescanner \
 ))
 
-$(eval $(call gb_CppunitTest_use_libraries,i18nutil_kashida,\
+$(eval $(call gb_CppunitTest_use_libraries,i18nutil,\
 	i18nutil \
 	sal \
 	test \

i18nutil/Library_i18nutil.mk
@@ -47,6 +47,8 @@ $(eval $(call gb_Library_add_exception_objects,i18nutil,\
 	i18nutil/source/utility/kashida \
 	i18nutil/source/utility/oneToOneMapping \
 	i18nutil/source/utility/paper \
+	i18nutil/source/utility/scriptchangescanner \
+	i18nutil/source/utility/scriptclass \
 	i18nutil/source/utility/scripttypedetector \
 	i18nutil/source/utility/unicode \
 	i18nutil/source/utility/widthfolding \

commit 0f097aee61abc5a8244fb51796f0dd2f9bae22c7

tdf#166152 sw: Fix for SwScriptInfo corruption on incremental update
This change fixes a regression in SwScriptInfo::InitScriptInfo, which
was causing some data structures to be populated with duplicated entries
following an incremental update.

Regression from 537645c0834eab2d277113f1e3fcf039c994832d
"tdf#66791 sw: Treat weak punctuation as Asian in Asian paragraphs"

Change-Id: I364effe5579bff35eb7693684e732eafb284d7e0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184151
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>
Tested-by: Jenkins

sw/source/core/text/porlay.cxx
@@ -1401,7 +1401,7 @@ void SwScriptInfo::InitScriptInfo(const SwTextNode& rNode,
         rText, SvtLanguageOptions::GetI18NScriptTypeOfLanguage(GetAppLanguage()), *pDirScanner);
     while (!pScriptScanner->AtEnd())
     {
-        if (pScriptScanner->Peek().m_nStartIndex <= static_cast<sal_Int32>(nChg))
+        if (static_cast<sal_Int32>(nChg) < pScriptScanner->Peek().m_nEndIndex)
         {
             break;
         }

sw/qa/extras/layout/data/tdf166152.fodt
@@ -1,0 +1,118 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<office:document xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" office:version="1.4" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:meta><meta:creation-date>2025-04-14T05:42:38.031483228</meta:creation-date><dc:date>2025-04-14T05:50:21.171365480</dc:date><meta:editing-duration>PT3M8S</meta:editing-duration><meta:editing-cycles>2</meta:editing-cycles><meta:generator>LibreOfficeDev/25.8.0.0.alpha0$Linux_X86_64 LibreOffice_project/ab8a00db55f1f2f24f11560c64020b7b01137fbd</meta:generator><meta:document-statistic meta:table-count="0" meta:image-count="0" meta:object-count="0" meta:page-count="1" meta:paragraph-count="1" meta:word-count="8" meta:character-count="40" meta:non-whitespace-character-count="33"/></office:meta>
+ <office:font-face-decls>
+  <style:font-face style:name="Liberation Serif" svg:font-family="'Liberation Serif'" style:font-family-generic="roman" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Sans1" svg:font-family="'Noto Sans'" style:font-family-generic="system" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Sans2" svg:font-family="'Noto Sans'" style:font-adornments="Regular" style:font-family-generic="swiss" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Serif CJK SC" svg:font-family="'Noto Serif CJK SC'" style:font-family-generic="system" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="graphic">
+   <style:graphic-properties svg:stroke-color="#3465a4" draw:fill-color="#729fcf" fo:wrap-option="no-wrap" draw:shadow-offset-x="0.1181in" draw:shadow-offset-y="0.1181in" draw:start-line-spacing-horizontal="0.1114in" draw:start-line-spacing-vertical="0.1114in" draw:end-line-spacing-horizontal="0.1114in" draw:end-line-spacing-vertical="0.1114in" style:writing-mode="lr-tb" style:flow-with-text="false"/>
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:line-break="strict" loext:tab-stop-distance="0in" style:font-independent-line-spacing="false">
+    <style:tab-stops/>
+   </style:paragraph-properties>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="Noto Serif CJK SC" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Noto Sans1" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN"/>
+  </style:default-style>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties fo:orphans="2" fo:widows="2" fo:hyphenation-ladder-count="no-limit" fo:hyphenation-keep="auto" loext:hyphenation-keep-type="column" loext:hyphenation-keep-line="false" style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:tab-stop-distance="0.4925in" style:writing-mode="page"/>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="Noto Serif CJK SC" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Noto Sans1" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN" fo:hyphenate="false" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false" loext:hyphenation-no-last-word="false" loext:hyphenation-word-char-count="5" loext:hyphenation-zone="no-limit"/>
+  </style:default-style>
+  <style:default-style style:family="table">
+   <style:table-properties table:border-model="collapsing"/>
+  </style:default-style>
+  <style:default-style style:family="table-row">
+   <style:table-row-properties fo:keep-together="auto"/>
+  </style:default-style>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+  <text:outline-style style:name="Outline">
+   <text:outline-level-style text:level="1" loext:num-list-format="%1%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="2" loext:num-list-format="%2%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="3" loext:num-list-format="%3%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="4" loext:num-list-format="%4%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="5" loext:num-list-format="%5%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="6" loext:num-list-format="%6%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="7" loext:num-list-format="%7%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="8" loext:num-list-format="%8%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="9" loext:num-list-format="%9%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="10" loext:num-list-format="%10%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+  </text:outline-style>
+  <text:notes-configuration text:note-class="footnote" style:num-format="1" text:start-value="0" text:footnotes-position="page" text:start-numbering-at="document"/>
+  <text:notes-configuration text:note-class="endnote" style:num-format="i" text:start-value="0"/>
+  <text:linenumbering-configuration text:number-lines="false" text:offset="0.1965in" style:num-format="1" text:number-position="left" text:increment="5"/>
+  </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:parent-style-name="Standard">
+   <style:text-properties style:font-name="Noto Sans2" fo:font-style="normal" fo:font-weight="normal" style:font-name-complex="Noto Sans2" style:font-style-complex="normal" style:font-weight-complex="normal"/>
+  </style:style>
+  <style:style style:name="T1" style:family="text">
+   <style:text-properties/>
+  </style:style>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="8.2681in" fo:page-height="11.6929in" style:num-format="1" style:print-orientation="portrait" fo:margin-top="0.7874in" fo:margin-bottom="0.7874in" fo:margin-left="0.7874in" fo:margin-right="0.7874in" style:writing-mode="lr-tb" style:layout-grid-color="#c0c0c0" style:layout-grid-lines="20" style:layout-grid-base-height="0.278in" style:layout-grid-ruby-height="0.139in" style:layout-grid-mode="none" style:layout-grid-ruby-below="false" style:layout-grid-print="false" style:layout-grid-display="false" style:footnote-max-height="0in" loext:margin-gutter="0in">
+    <style:footnote-sep style:width="0.0071in" style:distance-before-sep="0.0398in" style:distance-after-sep="0.0398in" style:line-style="solid" style:adjustment="left" style:rel-width="25%" style:color="#000000"/>
+   </style:page-layout-properties>
+   <style:header-style/>
+   <style:footer-style/>
+  </style:page-layout>
+  <style:style style:name="dp1" style:family="drawing-page">
+   <style:drawing-page-properties draw:background-size="full"/>
+  </style:style>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1" draw:style-name="dp1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <text:sequence-decls>
+    <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Figure"/>
+   </text:sequence-decls>
+   <text:p text:style-name="P1">कककक <text:span text:style-name="T1">aaaa </text:span>कककक <text:span text:style-name="T1">aaaa </text:span>कककक <text:span text:style-name="T1">aaaa </text:span>कककक <text:span text:style-name="T1">aaaa.</text:span></text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout5.cxx
@@ -19,6 +19,7 @@
 #include <unotools/syslocaleoptions.hxx>
 #include <editeng/unolingu.hxx>
 
+#include <scriptinfo.hxx>
 #include <rootfrm.hxx>
 #include <wrtsh.hxx>
 #include <txtfrm.hxx>
@@ -1577,6 +1578,46 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf88908)
                 u"                                  Without  the  fix,  ");
     assertXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[2]", "portion",
                 u"this  will  use  one  line");
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf166152)
+{
+    createSwDoc("tdf166152.fodt");
+
+    auto* pWrtShell = getSwDocShell()->GetWrtShell();
+    auto& rTextFrame
+        = dynamic_cast<SwTextFrame&>(*pWrtShell->GetLayout()->GetLower()->GetLower()->GetLower());
+    const SwScriptInfo* pSI = rTextFrame.GetScriptInfo();
+
+    // Sanity check script changes before the edit
+    CPPUNIT_ASSERT_EQUAL(size_t(8), pSI->CountScriptChg());
+
+    std::vector<sal_Int32> aExpectedChgBefore{ 5, 10, 15, 20, 25, 30, 35, 40 };
+    std::vector<sal_uInt8> aExpectedTypeBefore{ 3, 1, 3, 1, 3, 1, 3, 1 };
+    for (size_t i = 0; i < 8; ++i)
+    {
+        CPPUNIT_ASSERT_EQUAL(aExpectedChgBefore.at(i), pSI->GetScriptChg(i).get());
+        CPPUNIT_ASSERT_EQUAL(aExpectedTypeBefore.at(i), pSI->GetScriptType(i));
+    }
+
+    // Insert text somewhere in the middle of the document.
+    // This causes an incremental update of the script change vector.
+    pWrtShell->Right(SwCursorSkipMode::Chars, /*bSelect*/ false, /*nCount*/ 17,
+                     /*bBasicCall*/ false);
+    pWrtShell->Insert(u"A"_ustr);
+
+    // Check script changes after edit
+    //
+    // Without the fix, the count of script changes will be 11
+    CPPUNIT_ASSERT_EQUAL(size_t(8), pSI->CountScriptChg());
+
+    std::vector<sal_Int32> aExpectedChgAfter{ 5, 10, 15, 21, 26, 31, 36, 41 };
+    std::vector<sal_uInt8> aExpectedTypeAfter{ 3, 1, 3, 1, 3, 1, 3, 1 };
+    for (size_t i = 0; i < 8; ++i)
+    {
+        CPPUNIT_ASSERT_EQUAL(aExpectedChgAfter.at(i), pSI->GetScriptChg(i).get());
+        CPPUNIT_ASSERT_EQUAL(aExpectedTypeAfter.at(i), pSI->GetScriptType(i));
+    }
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();