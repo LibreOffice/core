0764e84ca5158d87c3e0fc1301715481f28269ca
Noel Grandin <noel.grandin@collabora.co.uk>
2025-04-16 17:12:14 +0200

    rearrange SvxCharacterMap class definition
    Clean it up a little and make most of it private, doesn't need to be public
    
    Change-Id: Ifc5d14faadcfedb4443a60694296b6b4a63c828d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184289
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Jenkins

--- include/svx/cuicharmap.hxx
+++ include/svx/cuicharmap.hxx
@@ -64,67 +64,6 @@ public:
     that are not easily accessible from the keyboard. */
 class SVX_DLLPUBLIC SvxCharacterMap final : public SfxDialogController
 {
-private:
-    void init();
-
-    ScopedVclPtr<VirtualDevice> m_xVirDev;
-    vcl::Font aFont;
-    std::unique_ptr<const SubsetMap> pSubsetMap;
-    bool isSearchMode;
-    css::uno::Reference<css::frame::XFrame> m_xFrame;
-
-    SfxCharmapContainer m_aCharmapContents;
-
-    SvxShowText m_aShowChar;
-
-    std::unique_ptr<weld::Button> m_xOKBtn;
-    std::unique_ptr<weld::Label> m_xFontText;
-    std::unique_ptr<weld::ComboBox> m_xFontLB;
-    std::unique_ptr<weld::Label> m_xSubsetText;
-    std::unique_ptr<weld::ComboBox> m_xSubsetLB;
-    std::unique_ptr<weld::Entry> m_xSearchText;
-    std::unique_ptr<weld::Entry> m_xHexCodeText;
-    std::unique_ptr<weld::Entry> m_xDecimalCodeText;
-    std::unique_ptr<weld::Button> m_xFavouritesBtn;
-    std::unique_ptr<weld::Label> m_xCharName;
-    std::unique_ptr<weld::CustomWeld> m_xShowChar;
-    std::unique_ptr<SvxShowCharSet> m_xShowSet;
-    std::unique_ptr<weld::CustomWeld> m_xShowSetArea;
-    std::unique_ptr<SvxSearchCharSet> m_xSearchSet;
-    std::unique_ptr<weld::CustomWeld> m_xSearchSetArea;
-
-    std::unique_ptr<SfxAllItemSet> m_xOutputSet;
-
-    enum class Radix : sal_Int16
-    {
-        decimal = 10,
-        hexadecimal = 16
-    };
-
-    // inserts the character that is currently selected in the given SvxShowCharSet
-    void insertSelectedCharacter(const SvxShowCharSet* pCharSet);
-
-    DECL_DLLPRIVATE_LINK(FontSelectHdl, weld::ComboBox&, void);
-    DECL_DLLPRIVATE_LINK(SubsetSelectHdl, weld::ComboBox&, void);
-    DECL_DLLPRIVATE_LINK(CharDoubleClickHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(CharSelectHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(CharHighlightHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(CharPreSelectHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(ReturnKeypressOnCharHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(FavClickHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(SearchCharHighlightHdl, SvxShowCharSet*, void);
-    DECL_DLLPRIVATE_LINK(DecimalCodeChangeHdl, weld::Entry&, void);
-    DECL_DLLPRIVATE_LINK(HexCodeChangeHdl, weld::Entry&, void);
-    DECL_DLLPRIVATE_LINK(CharClickHdl, SvxCharView*, void);
-    DECL_DLLPRIVATE_LINK(InsertClickHdl, weld::Button&, void);
-    DECL_DLLPRIVATE_LINK(FavSelectHdl, weld::Button&, void);
-    DECL_DLLPRIVATE_LINK(SearchUpdateHdl, weld::Entry&, void);
-    DECL_DLLPRIVATE_LINK(SearchFieldGetFocusHdl, weld::Widget&, void);
-    DECL_DLLPRIVATE_LINK(UpdateFavHdl, void*, void);
-
-    static void fillAllSubsets(weld::ComboBox& rListBox);
-    void selectCharByCode(Radix radix);
-
 public:
     SvxCharacterMap(weld::Widget* pParent, const SfxItemSet* pSet,
                     css::uno::Reference<css::frame::XFrame> xFrame);
@@ -146,6 +85,22 @@ public:
     void SetChar(sal_UCS4);
     sal_UCS4 GetChar() const;
 
+private:
+    void init();
+
+    enum class Radix : sal_Int16
+    {
+        decimal = 10,
+        hexadecimal = 16
+    };
+
+    // inserts the character that is currently selected in the given SvxShowCharSet
+    void insertSelectedCharacter(const SvxShowCharSet* pCharSet);
+
+    static void fillAllSubsets(weld::ComboBox& rListBox);
+
+    void selectCharByCode(Radix radix);
+
     void insertCharToDoc(const OUString& sChar);
 
     void setFavButtonState(std::u16string_view sTitle, std::u16string_view rFont);
@@ -153,6 +108,49 @@ public:
     void setCharName(sal_UCS4 nDecimalValue);
 
     void toggleSearchView(bool state);
+
+    DECL_DLLPRIVATE_LINK(FontSelectHdl, weld::ComboBox&, void);
+    DECL_DLLPRIVATE_LINK(SubsetSelectHdl, weld::ComboBox&, void);
+    DECL_DLLPRIVATE_LINK(CharDoubleClickHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(CharSelectHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(CharHighlightHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(CharPreSelectHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(ReturnKeypressOnCharHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(FavClickHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(SearchCharHighlightHdl, SvxShowCharSet*, void);
+    DECL_DLLPRIVATE_LINK(DecimalCodeChangeHdl, weld::Entry&, void);
+    DECL_DLLPRIVATE_LINK(HexCodeChangeHdl, weld::Entry&, void);
+    DECL_DLLPRIVATE_LINK(CharClickHdl, SvxCharView*, void);
+    DECL_DLLPRIVATE_LINK(InsertClickHdl, weld::Button&, void);
+    DECL_DLLPRIVATE_LINK(FavSelectHdl, weld::Button&, void);
+    DECL_DLLPRIVATE_LINK(SearchUpdateHdl, weld::Entry&, void);
+    DECL_DLLPRIVATE_LINK(SearchFieldGetFocusHdl, weld::Widget&, void);
+    DECL_DLLPRIVATE_LINK(UpdateFavHdl, void*, void);
+
+    ScopedVclPtr<VirtualDevice> m_xVirDev;
+    vcl::Font aFont;
+    std::unique_ptr<const SubsetMap> pSubsetMap;
+    bool isSearchMode;
+    css::uno::Reference<css::frame::XFrame> m_xFrame;
+    SfxCharmapContainer m_aCharmapContents;
+    SvxShowText m_aShowChar;
+    std::unique_ptr<SfxAllItemSet> m_xOutputSet;
+
+    std::unique_ptr<weld::Button> m_xOKBtn;
+    std::unique_ptr<weld::Label> m_xFontText;
+    std::unique_ptr<weld::ComboBox> m_xFontLB;
+    std::unique_ptr<weld::Label> m_xSubsetText;
+    std::unique_ptr<weld::ComboBox> m_xSubsetLB;
+    std::unique_ptr<weld::Entry> m_xSearchText;
+    std::unique_ptr<weld::Entry> m_xHexCodeText;
+    std::unique_ptr<weld::Entry> m_xDecimalCodeText;
+    std::unique_ptr<weld::Button> m_xFavouritesBtn;
+    std::unique_ptr<weld::Label> m_xCharName;
+    std::unique_ptr<weld::CustomWeld> m_xShowChar;
+    std::unique_ptr<SvxShowCharSet> m_xShowSet;
+    std::unique_ptr<weld::CustomWeld> m_xShowSetArea;
+    std::unique_ptr<SvxSearchCharSet> m_xSearchSet;
+    std::unique_ptr<weld::CustomWeld> m_xSearchSetArea;
 };
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
