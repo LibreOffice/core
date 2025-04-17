LibreOffice/core

commit f439448d21ef2feb03a9920d95d30f23795db6ac

tdf#165322 sw: prefer insert/delete redline in mouse-over tooltip
Unfortunately SwCursorShell::GetContentAtPos() can only report 1 redline
for a given position; in case there is a format and a insert/delete
redline at the same position, the latter is more important.

This is then used in SwEditWin::RequestHelp() to display a tooltip if
the user moves the mouse cursor over a redline.

Change-Id: I32af7a154873e3fa5775a711980bb3dab3e307ad
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181960
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/crsr/crstrvl.cxx
@@ -1912,10 +1912,40 @@ bool SwCursorShell::GetContentAtPos( const Point& rPt,
 
             if( !bRet && IsAttrAtPos::Redline & rContentAtPos.eContentAtPos )
             {
-                const SwRangeRedline* pRedl = GetDoc()->getIDocumentRedlineAccess().GetRedline(aPos, nullptr);
+                SwRedlineTable::size_type index;
+                IDocumentRedlineAccess const& rIDRA{GetDoc()->getIDocumentRedlineAccess()};
+                const SwRangeRedline* pRedl{rIDRA.GetRedline(aPos, &index)};
 
                 if( pRedl )
                 {
+                    // treat insert/delete as more important than formatting
+                    for (; index < rIDRA.GetRedlineTable().size(); ++index)
+                    {
+                        SwRangeRedline const*const pTmp{rIDRA.GetRedlineTable()[index]};
+                        if (aPos < *pTmp->Start())
+                        {
+                            break;
+                        }
+                        switch (pRedl->GetType())
+                        {
+                            case RedlineType::Format:
+                            case RedlineType::FmtColl:
+                            case RedlineType::ParagraphFormat:
+                                switch (pTmp->GetType())
+                                {
+                                    case RedlineType::Insert:
+                                    case RedlineType::Delete:
+                                        pRedl = pTmp;
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            break;
+                            default:
+                                break;
+                        }
+                    };
+
                     rContentAtPos.aFnd.pRedl = pRedl;
                     rContentAtPos.eContentAtPos = IsAttrAtPos::Redline;
                     rContentAtPos.pFndTextAttr = nullptr;

commit 7af97df1612e2f8debbce3b771a8f01a8325f7f2

Related: tdf#165322 sw: add tooltip testcase for format+delete redline combo
Fails with commit f439448d21ef2feb03a9920d95d30f23795db6ac (tdf#165322
sw: prefer insert/delete redline in mouse-over tooltip, 2025-02-25)
reverted.

Change-Id: I4be681d5920c93d85e4c295133ceaa608bb8bb6b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184304
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

sw/qa/extras/layout/layout2.cxx
@@ -29,6 +29,7 @@
 #include <IDocumentLayoutAccess.hxx>
 #include <IDocumentRedlineAccess.hxx>
 #include <fmtanchr.hxx>
+#include <redline.hxx>
 
 /// Test to assert layout / rendering result of Writer.
 class SwLayoutWriter2 : public SwModelTestBase
@@ -540,6 +541,26 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testTdf165322)
                 "//text[text() = 'Nunc viverra imperdiet enim. Fusce est. Vivamus a "
                 "tellus.']/parent::textarray/preceding-sibling::font[1]",
                 "strikeout", u"1");
+
+    // Also check if the tooltip shows the delete, not the format
+    // Given a paragraph with both delete and format:
+    SwWrtShell* pWrtShell = getSwDocShell()->GetWrtShell();
+    SwRootFrame* pLayout = pWrtShell->GetLayout();
+    SwFrame* pPage = pLayout->GetLower();
+    SwFrame* pBody = pPage->GetLower();
+    SwFrame* pPara = pBody->GetLower()->GetNext();
+    // When getting the content at a position for tooltip purposes:
+    Point aPoint = pPara->getFrameArea().Center();
+    SwContentAtPos aContentAtPos(IsAttrAtPos::Redline);
+    CPPUNIT_ASSERT(pWrtShell->GetContentAtPos(aPoint, aContentAtPos));
+    // Then make sure we find a delete redline:
+    CPPUNIT_ASSERT_EQUAL(IsAttrAtPos::Redline, aContentAtPos.eContentAtPos);
+    const SwRangeRedline* pRedline = aContentAtPos.aFnd.pRedl;
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 1 (RedlineType::Delete)
+    // - Actual  : 5 (RedlineType::ParagraphFormat)
+    // i.e. the delete didn't have priority over para format, leading to a misleading tooltip.
+    CPPUNIT_ASSERT_EQUAL(RedlineType::Delete, pRedline->GetType());
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testRedlineNumberInNumbering)