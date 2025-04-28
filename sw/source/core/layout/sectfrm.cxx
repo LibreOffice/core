6db63bb533a5c7436793eab86e83f92322b37044
Mike Kaganski <mike.kaganski@collabora.com>
2025-04-28 16:15:04 +0200

    tdf#166210: invalidate parent's size when hiding section
    Regression from commit 0c96119895b347f8eb5bb89f393351bd3c02b9f1
    
    Change-Id: I4469749c9ff3d9fae43f00e01276c241e7c49f61
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184720
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins

--- sw/source/core/layout/sectfrm.cxx
+++ sw/source/core/layout/sectfrm.cxx
@@ -2794,6 +2794,7 @@ void SwSectionFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rHint)
             SwFrameAreaDefinition::FrameAreaWriteAccess area(*this);
             SwRectFnSet(this).SetHeight(area, HUGE_POSITIVE);
         }
+        GetUpper()->InvalidateSize();
 
         InvalidateFramesInSection(Lower());
         if (Lower())
