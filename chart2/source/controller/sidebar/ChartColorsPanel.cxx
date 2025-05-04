LibreOffice/core

commit fb0cd05702852d29ee50d98d305043ac1993885b

chart color palette: ui panel is wrong in the lok case
1) Current color palette is not visible in the sidebar panel.
2) Setting up a custom color for a data series, the current color
palette icon is not set to the no color palette image.

Change-Id: I6fdcf84878c9e26a6a4ad3e2ba8628ee0dd28924
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184438
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

vcl/inc/jsdialog/jsdialogbuilder.hxx
@@ -649,6 +649,8 @@ public:
     virtual void set_menu_item_active(const OUString& rIdent, bool bActive) override;
     virtual void set_item_sensitive(const OUString& rIdent, bool bSensitive) override;
     virtual void set_item_icon_name(const OUString& rIdent, const OUString& rIconName) override;
+    virtual void set_item_image(const OUString& rIdent,
+                                const css::uno::Reference<css::graphic::XGraphic>& rImage) override;
 };
 
 class JSTextView final : public JSWidget<SalInstanceTextView, ::VclMultiLineEdit>

chart2/source/controller/main/ChartController_Tools.cxx
@@ -946,6 +946,12 @@ void ChartController::executeDispatch_FillColor(sal_uInt32 nColor)
                 ObjectIdentifier::getObjectPropertySet( aCID, xChartModel ) );
             if( xPointProperties.is() )
                 xPointProperties->setPropertyValue( u"FillColor"_ustr, uno::Any( nColor ) );
+
+            ObjectType eType = ObjectIdentifier::getObjectType(aCID);
+            if (eType == OBJECTTYPE_DATA_SERIES || eType == OBJECTTYPE_DATA_POINT)
+            {
+                xChartModel->clearColorPalette();
+            }
         }
     }
     catch( const uno::Exception& )

vcl/jsdialog/jsdialogbuilder.cxx
@@ -1643,6 +1643,16 @@ void JSToolbar::set_item_icon_name(const OUString& rIdent, const OUString& rIcon
     sendUpdate();
 }
 
+void JSToolbar::set_item_image(const OUString& rIdent,
+                               const css::uno::Reference<css::graphic::XGraphic>& rImage)
+{
+    SalInstanceToolbar::set_item_image(rIdent, rImage);
+    if (rIdent == u".uno:ChartColorPalette"_ustr)
+    {
+        sendUpdate();
+    }
+}
+
 JSTextView::JSTextView(JSDialogSender* pSender, ::VclMultiLineEdit* pTextView,
                        SalInstanceBuilder* pBuilder, bool bTakeOwnership)
     : JSWidget<SalInstanceTextView, ::VclMultiLineEdit>(pSender, pTextView, pBuilder,

vcl/source/window/toolbox2.cxx
@@ -1762,16 +1762,22 @@ void ToolBox::DumpAsPropertyTree(tools::JsonWriter& rJsonWriter)
                     rJsonWriter.put("description", sAccDesc);
             }
 
-            Image aImage = GetItemImage(nId);
-            if (!sCommand.startsWith(".uno:") && !!aImage)
+            if (!sCommand.startsWith(".uno:") || sCommand == u".uno:ChartColorPalette"_ustr)
             {
-                SvMemoryStream aOStm(6535, 6535);
-                if(GraphicConverter::Export(aOStm, aImage.GetBitmapEx(), ConvertDataFormat::PNG) == ERRCODE_NONE)
+                Image aImage = GetItemImage(nId);
+                if (!!aImage)
                 {
-                    css::uno::Sequence<sal_Int8> aSeq( static_cast<sal_Int8 const *>(aOStm.GetData()), aOStm.Tell());
-                    OStringBuffer aBuffer("data:image/png;base64,");
-                    ::comphelper::Base64::encode(aBuffer, aSeq);
-                    rJsonWriter.put("image", aBuffer);
+                    SvMemoryStream aOStm(6535, 6535);
+                    if (GraphicConverter::Export(aOStm, aImage.GetBitmapEx(),
+                                                 ConvertDataFormat::PNG)
+                        == ERRCODE_NONE)
+                    {
+                        css::uno::Sequence<sal_Int8> aSeq(
+                            static_cast<sal_Int8 const*>(aOStm.GetData()), aOStm.Tell());
+                        OStringBuffer aBuffer("data:image/png;base64,");
+                        ::comphelper::Base64::encode(aBuffer, aSeq);
+                        rJsonWriter.put("image", aBuffer);
+                    }
                 }
             }
         }

commit 895e05b307ba3af8c7c99974bd1ce12bf534fec3

fixed werrors
caused by fb0cd05702852d29ee50d98d305043ac1993885b

Change-Id: If1e58d2591669b7ec477509cd6e54bb11bfe86cc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184934
Reviewed-by: Marco Cecchetti <marco.cecchetti@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

chart2/source/controller/sidebar/ChartColorsPanel.cxx
@@ -84,9 +84,9 @@ private:
     ChartColorPaletteControl* mpControl;
 };
 
-ColorPaletteWrapper::ColorPaletteWrapper(rtl::Reference<ChartModel> mxModel,
+ColorPaletteWrapper::ColorPaletteWrapper(rtl::Reference<ChartModel> xModel,
                                          ChartColorPaletteControl* pControl)
-    : mxModel(std::move(mxModel))
+    : mxModel(std::move(xModel))
     , mpControl(pControl)
 {
 }

chart2/source/model/main/ChartModel.cxx
@@ -146,6 +146,7 @@ ChartModel::ChartModel(uno::Reference<uno::XComponentContext > xContext)
 
 ChartModel::ChartModel( const ChartModel & rOther )
     : impl::ChartModel_Base(rOther)
+    , SfxListener(rOther)
     , m_aLifeTimeManager( this, this )
     , m_bReadOnly( rOther.m_bReadOnly )
     , m_bModified( rOther.m_bModified )

vcl/inc/jsdialog/jsdialogbuilder.hxx
@@ -649,6 +649,7 @@ public:
     virtual void set_menu_item_active(const OUString& rIdent, bool bActive) override;
     virtual void set_item_sensitive(const OUString& rIdent, bool bSensitive) override;
     virtual void set_item_icon_name(const OUString& rIdent, const OUString& rIconName) override;
+    using JSWidget<SalInstanceToolbar, ::ToolBox>::set_item_image;
     virtual void set_item_image(const OUString& rIdent,
                                 const css::uno::Reference<css::graphic::XGraphic>& rImage) override;
 };