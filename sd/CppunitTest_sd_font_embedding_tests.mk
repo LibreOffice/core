dcd74c1eabfddb4dbc139d853e178c27fe78881f
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2025-05-06 14:25:27 +0900

    sd: Test OOXML embedded fonts - import, export and round-trip
    Import and round-trip tests don't run when libeot is not available.
    Export test doesn't depend on libeot, so it can run.
    
    Change-Id: I71d290b3f93a0ccdb9f8a5bdb9c933464c7059ff
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/184974
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ sd/CppunitTest_sd_font_embedding_tests.mk
@@ -0,0 +1,75 @@
+# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
+#*************************************************************************
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#*************************************************************************
+
+$(eval $(call gb_CppunitTest_CppunitTest,sd_font_embedding_tests))
+
+$(eval $(call gb_CppunitTest_use_externals,sd_font_embedding_tests,\
+    boost_headers \
+    libxml2 \
+))
+
+$(eval $(call gb_CppunitTest_use_common_precompiled_header,sd_font_embedding_tests))
+
+$(eval $(call gb_CppunitTest_add_exception_objects,sd_font_embedding_tests, \
+    sd/qa/unit/FontEmbeddingTest \
+))
+
+$(eval $(call gb_CppunitTest_use_libraries,sd_font_embedding_tests, \
+    basegfx \
+    comphelper \
+    cppu \
+    cppuhelper \
+    drawinglayer \
+    docmodel \
+    editeng \
+    for \
+    forui \
+    i18nlangtag \
+    sal \
+    salhelper \
+    sax \
+    sd \
+    sfx \
+    sot \
+    subsequenttest \
+    svl \
+    svt \
+    svx \
+    svxcore \
+    test \
+    tl \
+    tk \
+    ucbhelper \
+    unotest \
+    utl \
+    vcl \
+    xo \
+))
+
+$(eval $(call gb_CppunitTest_set_include,sd_font_embedding_tests,\
+    -I$(SRCDIR)/sd/source/ui/inc \
+    -I$(SRCDIR)/sd/inc \
+    $$(INCLUDE) \
+))
+
+$(eval $(call gb_CppunitTest_use_custom_headers,sd_font_embedding_tests,\
+    officecfg/registry \
+))
+
+$(eval $(call gb_CppunitTest_use_sdk_api,sd_font_embedding_tests))
+$(eval $(call gb_CppunitTest_use_ure,sd_font_embedding_tests))
+$(eval $(call gb_CppunitTest_use_vcl,sd_font_embedding_tests))
+$(eval $(call gb_CppunitTest_use_rdb,sd_font_embedding_tests,services))
+$(eval $(call gb_CppunitTest_use_configuration,sd_font_embedding_tests))
+
+
+
+# vim: set noet sw=4 ts=4:
--- sd/Module_sd.mk
+++ sd/Module_sd.mk
@@ -41,6 +41,7 @@ $(eval $(call gb_Module_add_slowcheck_targets,sd,\
     CppunitTest_sd_export_tests-ooxml4 \
     CppunitTest_sd_export_tests \
     CppunitTest_sd_filters_test \
+    CppunitTest_sd_font_embedding_tests \
     CppunitTest_sd_layout_tests \
     CppunitTest_sd_misc_tests \
     CppunitTest_sd_uiimpress \
