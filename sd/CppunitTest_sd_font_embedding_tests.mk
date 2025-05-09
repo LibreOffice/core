df2423ac932a16021c7bc4e1a2e32b9caa0579b4
Stephan Bergmann <stephan.bergmann@allotropia.de>
2025-05-09 10:01:45 +0200

    CppunitTest_sd_font_embedding_tests also needs the ASan hack
    ...from e854abe076155fc085b56549ced50b3ee9a095d2 "Avoid external processes
    picking up instdir/program/libnspr4.so", to avoid
    
    > Test name: testExportEmbeddedFontsPPTX::TestBody
    > equality assertion failed
    > - Expected: 0
    > - Actual  : 32512
    > - failed to execute: sh .../bin/officeotron.sh .../tmp/libtest_sd_font_embedding_tests.so1z20bm.tmp > .../tmp/libtest_sd_font_embedding_tests.so1z20bo.tmp 2>&1
    > java: symbol lookup error: .../instdir/program/libnspr4.so: undefined symbol: __asan_init
    
    Change-Id: I13732bc79b36621413ca6cea8ae37419795160a4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185087
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@allotropia.de>

--- sd/CppunitTest_sd_font_embedding_tests.mk
+++ sd/CppunitTest_sd_font_embedding_tests.mk
@@ -70,6 +70,8 @@ $(eval $(call gb_CppunitTest_use_vcl,sd_font_embedding_tests))
 $(eval $(call gb_CppunitTest_use_rdb,sd_font_embedding_tests,services))
 $(eval $(call gb_CppunitTest_use_configuration,sd_font_embedding_tests))
 
-
+$(eval $(call gb_CppunitTest_add_arguments,sd_font_embedding_tests, \
+    -env:arg-env=$(gb_Helper_LIBRARY_PATH_VAR)"$$$${$(gb_Helper_LIBRARY_PATH_VAR)+=$$$$$(gb_Helper_LIBRARY_PATH_VAR)}" \
+))
 
 # vim: set noet sw=4 ts=4:
