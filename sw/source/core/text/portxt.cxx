LibreOffice/core

commit 01d1037262070ae4ab37868bb47f6ac258433618

tdf#126154 tdf#149420 tdf#165984 sw: fix hyphenation zones with portions
When the text line contains text portions, fix disabled hyphenation at
minimum word spacing (implemented by hyphenation zones), paragraph end
zone and hyphenation zone, by calculating with the start of the line
instead of the start of the text portion.

Hyphenation wasn't disabled at minimum word spacing, paragraph end
zone and hyphenation zone, despite the available space, if the text line
contained text portions, e.g. an emphasized word:

tdf#126154: maximum word spacing: if the hyphenated word has
got own text portion, maximum word spacing was not applied.

tdf#165984 paragraph end zone: hyphenation was not always disabled
in the last but one paragraph line (i.e. in the last full paragraph
line), despite the available space in the last paragraph line, if the
line contains a text portion. (Paragraph end zone is applied only
if the text content of the planned last paragraph line is shorter,
than the actual content of the (hyphenated) last full paragraph line.)

tdf#149420: hyphenation zone was not calculated, if the hyphenated word
has own text portion, similar to the paragraph end zone.

Fix also regression of the preference of maximum and minimum
word spacing by checking possible space shrinking in the case, when the
maximum word spacing was also able to disable hyphenation (the unit test
verifies the fix of this regression, too).

Follow-up to commit 3dd2d7f449f13bec0f0a5c6e0c33a143409c3723
"tdf#126154 sw word spacing: fix greedy space shrinking",
commit 5a48070f5904c51dc9e7bbad4213d802fd4bc89b
"tdf#126154 sw offapi xmloff cui: add min/max word spacing",
commit 7d6696757dcdfa3cee481ac7795a91b2b47da363
"tdf#159923 sw cui offapi xmloff: add custom word spacing",
commit 7a1d4b7d1db93ca1f541856a8d00d621d50e7bd6
"tdf#149420 sw offapi xmloff: add hyphenation zone",
and commit 7d384fb1c232f7aa720436bc68dc1de334bf7297
"tdf#165984 sw cui xmloff: add Paragraph/Column/Page/Spread end zone"

sw/source/core/text/portxt.cxx
@@ -421,7 +421,10 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                     pGuess.emplace();
                     bFull = !pGuess->Guess( *this, rInf, Height(), nSpacesInLine, aAdjustItem.GetPropWordSpacing() );
                 }
-                if ( bWordSpacingMinimum && pGuess->HyphWord().is() && pGuess->BreakPos() > rInf.GetLineStart() )
+                // if both maximum word spacing or minimum word spacing can disable
+                // hyphenation, prefer the last one
+                if ( bWordSpacingMinimum && ( bWordSpacingMaximum ||
+                        ( pGuess->HyphWord().is() && pGuess->BreakPos() > rInf.GetLineStart() ) ) )
                 {
                     std::optional<SwTextGuess> pGuess2(std::in_place);
                     SwTwips nOldExtraSpace = rInf.GetExtraSpace();

sw/qa/extras/layout/layout3.cxx
@@ -1214,6 +1214,46 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf126154_minimum_shrinking)
         u",,,,,,,, , , , , , , , Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vesti ");
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf126154_portion)
+{
+    // text portions with word spacing, paragraph end zone and hyphenation zone
+    uno::Reference<linguistic2::XHyphenator> xHyphenator = LinguMgr::GetHyphenator();
+    if (!xHyphenator->hasLocale(lang::Locale(u"en"_ustr, u"US"_ustr, OUString())))
+        return;
+
+    createSwDoc("tdf126154_portion.fodt");
+    // Ensure that all text portions are calculated before testing.
+    SwViewShell* pViewShell = getSwDoc()->getIDocumentLayoutAccess().GetCurrentViewShell();
+    CPPUNIT_ASSERT(pViewShell);
+    pViewShell->Reformat();
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+
+    // only maximum word spacing: 133%
+    // This was "... Vesti bu" (not disabled hyphenation because of text portion)
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[2]/SwParaPortion/SwLineLayout[1]", "portion",
+                u",,,,,,,,,,,,,,,,,,,,, , , , , , , , , , , , , , , , Lorem ipsum dolor sit amet, "
+                u"consectetur adipiscing elit. Vesti ");
+
+    // prefer minimum word spacing, not maximum word spacing to disable hyphenation, if it's possible
+    // between the minimum and maximum values. (minumum: 80%, desired: 100%, maximum word spacing: 133%)
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[4]/SwParaPortion/SwLineLayout[1]", "portion",
+                u",,,,,,,,,,,,,,,,,,,,, , , , , , , , , , , , , , , , Lorem ipsum dolor sit amet, "
+                u"consectetur adipiscing elit. Vesti bulum ");
+
+    // paragraph end zone
+    // This was "... other celes" (not disabled hyphenation because of text portion)
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[6]/SwParaPortion/SwLineLayout[8]", "portion",
+                u"cally atmospherically atmospherically atmospherically. The Earth is no different "
+                u"to any other ");
+
+    // hyphenation
+    // This was "... other celes" (not disabled hyphenation because of text portion)
+    assertXPath(pXmlDoc, "/root/page[1]/body/txt[8]/SwParaPortion/SwLineLayout[8]", "portion",
+                u"cally atmospherically atmospherically atmospherically. The Earth is no different "
+                u"to any other ");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, testTdf155324)
 {
     createSwDoc("tox-update-wrong-pages.odt");

sw/source/core/text/guess.cxx
@@ -354,9 +354,8 @@ bool SwTextGuess::Guess( const SwTextPortion& rPor, SwTextFormatInfo &rInf,
                // if paragraph end zone is not different from the hyphenation zone, skip its handling
                nTextHyphenZoneAlways != nTextHyphenZone &&
                // end of the paragraph
-               // FIXME: handle text portions
                rInf.GetText().getLength() - sal_Int32(nHyphZone) <
-               sal_Int32(m_nCutPos) - sal_Int32(rInf.GetIdx() ) )
+               sal_Int32(m_nCutPos) - sal_Int32(rInf.GetLineStart() ) )
         {
             nParaZone = nTextHyphenZoneAlways >= nLineWidth
                 ? 0
@@ -378,7 +377,7 @@ bool SwTextGuess::Guess( const SwTextPortion& rPor, SwTextFormatInfo &rInf,
             // rInf.GetIdx()[nHyphZone] always disables the hyphenation, don't need to calculate
             // with its fitting part. Moreover, do not check double or more spaces there, they
             // are accepted outside of the hyphenation zone, too.
-            for (; sal_Int32(rInf.GetIdx()) <= nZonePos && nMaxZone <= nZonePos; --nZonePos )
+            for (; sal_Int32(rInf.GetLineStart()) <= nZonePos && nMaxZone <= nZonePos; --nZonePos )
             {
                 sal_Unicode cChar = rInf.GetText()[nZonePos];
                 if ( cChar == CH_BLANK || cChar == CH_FULL_BLANK || cChar == CH_SIX_PER_EM )
@@ -389,9 +388,8 @@ bool SwTextGuess::Guess( const SwTextPortion& rPor, SwTextFormatInfo &rInf,
                         // still end of the paragraph, i.e. still more characters in the original
                         // last full line, then in the planned last paragraph line
                         // FIXME: guarantee, that last not full line won't become a full line
-                        // FIXME: handle text portions
                         rInf.GetText().getLength() - sal_Int32(nZonePos) <
-                        sal_Int32(m_nCutPos) - sal_Int32(rInf.GetIdx() ) )
+                            sal_Int32(m_nCutPos) - sal_Int32(rInf.GetLineStart() ) )
                     {
                         bHyph = false;
                     }
@@ -483,7 +481,7 @@ bool SwTextGuess::Guess( const SwTextPortion& rPor, SwTextFormatInfo &rInf,
                             // apply this only if the space before the word is there
                             // in the actual line, i.e. start the long word in a new
                             // line, but still allows to break its last parts
-                            sal_Int32(rInf.GetIdx()) < nLastWord &&
+                            sal_Int32(rInf.GetLineStart()) < nLastWord &&
                             // if the case of bDoNotHyphenateLastLine == true, skip hyphenation
                             // only if the character length of the very last line of the paragraph
                             // would be still less, than the length of the recent last but one line
@@ -493,9 +491,8 @@ bool SwTextGuess::Guess( const SwTextPortion& rPor, SwTextFormatInfo &rInf,
                                   // FIXME: character count is not fail-safe: remaining characters
                                   // can exceed the line, resulting two last full paragraph lines
                                   // with disabled hyphenation.
-                                  // FIXME: handle text portions
                                   rInf.GetText().getLength() - sal_Int32(nLastWord) <
-                                  sal_Int32(m_nCutPos) - sal_Int32(rInf.GetIdx() ) ) )
+                                      sal_Int32(m_nCutPos) - sal_Int32(rInf.GetLineStart() ) ) )
             {
                 m_nCutPos = TextFrameIndex(nLastWord);
             }

commit 41d3574263d90f1872ff0bad74618761e84d0f61

tdf#126154 sw word spacing: fix greedy shrinking regression
Fix failing unit test file:///home/sberg/lo/core//sw/qa/extras/layout/data//tdf126154_minimum_shrinking.fodt:
/home/sberg/lo/core/test/source/xmltesttools.cxx:169:(anonymous namespace)::testTdf126154_minimum_shrinking::TestBody
equality assertion failed
- Expected: ,, , , , , , , , Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vesti bulum
- Actual  : ,, , , , , , , , Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vesti bulum c
- In <>, attribute 'portion' of '/root/page[1]/body/txt[18]/SwParaPortion/SwLineLayout[1]' incorrect value.

Regression since commit 01d1037262070ae4ab37868bb47f6ac258433618
"tdf#126154 tdf#149420 tdf#165984 sw: fix hyphenation
zones with portions".

Notes: Instead of the desired word spacing, an extra shrinking
was applied, when minimum and maximum word spacing is specified,
despite that there was no hyphenation with the desired
word spacing.

The first Guess() call is for the original line breaking
with 100% word spacing (specified in the font, no shrinking
or expanding yet), detecting also the possible space count
in the line. The second Guess() call is for both the custom
desired and maximum word spacing. The third Guess() call is
for the minimum word spacing. No need for the third call, if

1) The desired word spacing is 100% (as in the first Guess() call, and

2) There was no hyphenation in the first Guess() call (minimum
word spacing is used only for avoid hyphenation, yet).

Change-Id: Ia2e689800f4bdf94512e8b47bc42d6e77c3723ec
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185299
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/portxt.cxx
@@ -420,6 +420,8 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
             }
             else
             {
+                bool bOrigHyphenated = pGuess->HyphWord().is() &&
+                        pGuess->BreakPos() > rInf.GetLineStart();
                 if ( bWordSpacing || bWordSpacingMaximum )
                 {
                     pGuess.emplace();
@@ -428,7 +430,13 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                 // if both maximum word spacing or minimum word spacing can disable
                 // hyphenation, prefer the last one
                 if ( bWordSpacingMinimum && ( bWordSpacingMaximum ||
-                        ( pGuess->HyphWord().is() && pGuess->BreakPos() > rInf.GetLineStart() ) ) )
+                        ( pGuess->HyphWord().is() && pGuess->BreakPos() > rInf.GetLineStart() ) ) &&
+                        // if the desired word spacing is 100% (!bWordSpacing), and it was possible
+                        // to break the line without hyphenation in the first run (where maximum
+                        // word spacing was not used), no need to check minimum word spacing
+                        // FIXME: avoid too much shrinking, if desired word spacing is not 100%
+                        ( bWordSpacing || bOrigHyphenated )
+                   )
                 {
                     std::optional<SwTextGuess> pGuess2(std::in_place);
                     SwTwips nOldExtraSpace = rInf.GetExtraSpace();