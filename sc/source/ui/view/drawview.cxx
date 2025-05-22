LibreOffice/core

commit 7e15c704cedf1ca77b3ebe8b3b60f6a1ad57deb0

tdf#166481 sc shapes edit view: fix wrong draw view shell
was created when an object was in rotation drag mode and we
enter into the shape to text edit mode.
We only need to switch to draw shell if we have not already
created a text shell for text edit mode.

TODO: unit/UI tests would be useful for the original enhancement.
(c628a26cd3f763b39a2e2441fc766dabf04ed492)

Follow-up commit of
c628a26cd3f763b39a2e2441fc766dabf04ed492
677e40c8b83b06ded8e06b6151f71686baf5f6b5

Regression from commit: c628a26cd3f763b39a2e2441fc766dabf04ed492
(tdf#90293 switch draw selection to rotate mode)

Change-Id: Ib857673b1f5469d6502736ecad84fe046f8c967e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185031
Tested-by: Jenkins
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>

sc/source/ui/view/drawview.cxx
@@ -401,8 +401,9 @@ void ScDrawView::MarkListHasChanged()
             bSubShellSet = true;
         }
         else if (pObj->GetObjIdentifier() != SdrObjKind::Text   // prevent switching to the drawing shell
-                    || !pViewSh->IsDrawTextShell())     // when creating a text object @#70206#
+                    && !pViewSh->IsDrawTextShell())     // when creating a text object @#70206#
         {
+            // tdf#166481: we only need to switch to draw shell if we have not already created a text shell for text edit mode
             pViewSh->SetDrawShell(true);
         }
     }

commit 29edbf28fc3135ccc6d84f9484df939c5f891746

tdf#166633 - sc fix wrong context menu for text boxes
Handle separately text SdrObjs from not text SdrObjs
because when we create a new text object we also set
in edit mode automatically.

regression from commit: 7e15c704cedf1ca77b3ebe8b3b60f6a1ad57deb0
(tdf#166481 sc shapes edit view: fix wrong draw view shell)

Change-Id: I4887c5f24a277fbbf01b4449148f3934b043220b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185618
Reviewed-by: Balazs Varga <balazs.varga.extern@allotropia.de>
Tested-by: Jenkins

sc/source/ui/view/drawview.cxx
@@ -380,7 +380,8 @@ void ScDrawView::MarkListHasChanged()
     if (nMarkCount == 1)
     {
         SdrObject* pObj = rMarkList.GetMark(0)->GetMarkedSdrObj();
-        if (pObj->GetObjIdentifier() == SdrObjKind::OLE2)
+        SdrObjKind nSdrObjKind = pObj->GetObjIdentifier();
+        if (nSdrObjKind == SdrObjKind::OLE2)
         {
             pOle2Obj = static_cast<SdrOle2Obj*>(pObj);
             if (!ScDocument::IsChart(pObj) )
@@ -389,21 +390,27 @@ void ScDrawView::MarkListHasChanged()
                 pViewSh->SetChartShell(true);
             bSubShellSet = true;
         }
-        else if (pObj->GetObjIdentifier() == SdrObjKind::Graphic)
+        else if (nSdrObjKind == SdrObjKind::Graphic)
         {
             pGrafObj = static_cast<SdrGrafObj*>(pObj);
             pViewSh->SetGraphicShell(true);
             bSubShellSet = true;
         }
-        else if (pObj->GetObjIdentifier() == SdrObjKind::Media)
+        else if (nSdrObjKind == SdrObjKind::Media)
         {
             pViewSh->SetMediaShell(true);
             bSubShellSet = true;
         }
-        else if (pObj->GetObjIdentifier() != SdrObjKind::Text   // prevent switching to the drawing shell
-                    && !pViewSh->IsDrawTextShell())     // when creating a text object @#70206#
+        else if (nSdrObjKind == SdrObjKind::Text)
         {
-            // tdf#166481: we only need to switch to draw shell if we have not already created a text shell for text edit mode
+            // prevent switching to the drawing shell
+            if (!pViewSh->IsDrawTextShell()) // when creating a text object @#70206#
+                pViewSh->SetDrawShell(true);
+        }
+        else if (nSdrObjKind != SdrObjKind::Text && !pViewSh->IsDrawTextShell())
+        {
+            // tdf#166481: we only need to switch to draw shell if we have not
+            // already created a text shell for text edit mode
             pViewSh->SetDrawShell(true);
         }
     }