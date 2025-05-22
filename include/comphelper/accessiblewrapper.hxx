ef500b65297704ddf50e56eee7cf402053dd0e3c
Michael Weghorn <m.weghorn@posteo.de>
2025-05-22 06:35:35 +0200

    comphelper a11y: Use #pragma once
    Change-Id: I65beee09218e36657d59ed8483e9b90b21b950c6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185638
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- include/comphelper/accessiblewrapper.hxx
+++ include/comphelper/accessiblewrapper.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_COMPHELPER_ACCESSIBLEWRAPPER_HXX
-#define INCLUDED_COMPHELPER_ACCESSIBLEWRAPPER_HXX
+#pragma once
 
 #include <config_options.h>
 #include <sal/config.h>
@@ -373,8 +372,5 @@ private:
 
 
 }   // namespace accessibility
-
-
-#endif // INCLUDED_COMPHELPER_ACCESSIBLEWRAPPER_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
--- include/comphelper/proxyaggregation.hxx
+++ include/comphelper/proxyaggregation.hxx
@@ -17,8 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-#ifndef INCLUDED_COMPHELPER_PROXYAGGREGATION_HXX
-#define INCLUDED_COMPHELPER_PROXYAGGREGATION_HXX
+#pragma once
 
 #include <config_options.h>
 #include <cppuhelper/implbase1.hxx>
@@ -212,8 +211,5 @@ namespace comphelper
 
 
 }   // namespace comphelper
-
-
-#endif // INCLUDED_COMPHELPER_PROXYAGGREGATION_HXX
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
