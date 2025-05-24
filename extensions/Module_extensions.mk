LibreOffice/core

commit ec0a49ecc7ea8449d90c1e69857d62728af19829

Windows 7-8.1 support was removed, prevent install on these old systems
There is no simple "Windows is 10 or newer" check in MSI, with the
Microsoft policy of "you don't need to know real Windows version".
See also https://developercommunity.visualstudio.com/t/detect-a-windows-version-from-msi/412198

The values that MSI sees (e.g. on my Win11) are:

Property(C): VersionNT = 603
Property(C): VersionNT64 = 603
Property(C): WindowsBuild = 9600

So those can't be used for the check. I use the WINMAJORVER that is
read from registry (Software\Microsoft\Windows NT\CurrentVersion).

There is no more need to check and install UCRT; it was dropped.

Change-Id: Ic02d6aeb965e9684bafd4357f969980f77000e73
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/178049
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

config_host.mk.in
@@ -744,7 +744,6 @@ export TMPDIR=@TEMP_DIRECTORY@
 export TOUCH=@TOUCH@
 export UCRTSDKDIR=@UCRTSDKDIR@
 export UCRTVERSION=@UCRTVERSION@
-export UCRT_REDISTDIR=@UCRT_REDISTDIR@
 export UNIQ=@UNIQ@
 export UNIX_DLAPI_LIBS=@UNIX_DLAPI_LIBS@
 export USE_HEADLESS_CODE=@USE_HEADLESS_CODE@

configure.ac
@@ -4158,7 +4158,6 @@ cygwin*|wsl*)
         SCPDEFS="$SCPDEFS -DWINDOWS_AARCH64"
         WIN_HOST_ARCH="arm64"
         WIN_HOST_BITS=64
-        with_ucrt_dir=no
         ;;
     *)
         AC_MSG_ERROR([Unsupported host_cpu $host_cpu for host_os $host_os])
@@ -8945,23 +8944,6 @@ if test "$_os" = "Linux" -a "$host_cpu" = "powerpc"; then
 fi
 AC_SUBST([JITC_PROCESSOR_TYPE])
 
-# Misc Windows Stuff
-AC_ARG_WITH(ucrt-dir,
-    AS_HELP_STRING([--with-ucrt-dir],
-        [path to the directory with the arch-specific MSU packages of the Windows Universal CRT redistributables
-        (MS KB 2999226) for packaging into the installsets (without those the target system needs to install
-        the UCRT or Visual C++ Runtimes manually). The directory must contain the following 6 files:
-            Windows6.1-KB2999226-x64.msu
-            Windows6.1-KB2999226-x86.msu
-            Windows8.1-KB2999226-x64.msu
-            Windows8.1-KB2999226-x86.msu
-            Windows8-RT-KB2999226-x64.msu
-            Windows8-RT-KB2999226-x86.msu
-        A zip archive including those files is available from Microsoft site:
-        https://www.microsoft.com/en-us/download/details.aspx?id=48234]),
-,)
-
-UCRT_REDISTDIR="$with_ucrt_dir"
 if test $_os = "WINNT"; then
     find_msvc_x64_dlls
     MSVC_DLL_PATH=`win_short_path_for_make "$msvcdllpath"`
@@ -8983,36 +8965,8 @@ if test $_os = "WINNT"; then
             break
         fi
     done
-
-    if test "$UCRT_REDISTDIR" = "no"; then
-        dnl explicitly disabled
-        UCRT_REDISTDIR=""
-    else
-        PathFormat "$UCRT_REDISTDIR"
-        UCRT_REDISTDIR="$formatted_path"
-        UCRT_REDISTDIR_unix="$formatted_path_unix"
-        if ! test -f "$UCRT_REDISTDIR_unix/Windows6.1-KB2999226-x64.msu" -a \
-                  -f "$UCRT_REDISTDIR_unix/Windows6.1-KB2999226-x86.msu" -a \
-                  -f "$UCRT_REDISTDIR_unix/Windows8.1-KB2999226-x64.msu" -a \
-                  -f "$UCRT_REDISTDIR_unix/Windows8.1-KB2999226-x86.msu" -a \
-                  -f "$UCRT_REDISTDIR_unix/Windows8-RT-KB2999226-x64.msu" -a \
-                  -f "$UCRT_REDISTDIR_unix/Windows8-RT-KB2999226-x86.msu"; then
-            UCRT_REDISTDIR=""
-            if test -n "$PKGFORMAT"; then
-               for i in $PKGFORMAT; do
-                   case "$i" in
-                   msi)
-                       AC_MSG_WARN([--without-ucrt-dir not specified or MSUs not found - installer will have runtime dependency])
-                       add_warning "--without-ucrt-dir not specified or MSUs not found - installer will have runtime dependency"
-                       ;;
-                   esac
-               done
-            fi
-        fi
-    fi
 fi
 
-AC_SUBST(UCRT_REDISTDIR)
 AC_SUBST(MSVC_DLL_PATH)
 AC_SUBST(MSVC_DLLS)
 AC_SUBST(MSM_PATH)

Repository.mk
@@ -745,7 +745,6 @@ $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,spsuppfiles
 $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,ooobinarytable, \
 	$(if $(WINDOWS_SDK_HOME),\
 		instooofiltmsi \
-		inst_msu_msi \
 		qslnkmsi \
 		reg_dlls \
 		reg4allmsdoc \

RepositoryExternal.mk
@@ -4310,10 +4310,6 @@ $(call gb_ExternalProject_use_static_libraries,$(1),dtoa)
 
 endef
 
-$(eval $(call gb_Helper_register_packages_for_install,ucrt_binarytable,\
-	$(if $(UCRT_REDISTDIR),ucrt) \
-))
-
 ifneq ($(SYSTEM_BOX2D),)
 
 define gb_LinkTarget__use_box2d

external/Module_external.mk
@@ -9,10 +9,6 @@
 
 $(eval $(call gb_Module_Module,external))
 
-ifeq ($(COM),MSC)
-$(eval $(call gb_Module_add_moduledir,external,msc-externals))
-endif
-
 $(eval $(call gb_Module_add_moduledirs,external,\
 	$(call gb_Helper_optional,XMLSEC,xmlsec) \
 	$(call gb_Helper_optional,ABW,libabw) \

external/msc-externals/Makefile
@@ -1,7 +1,0 @@
-# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
-
-module_directory:=$(dir $(realpath $(firstword $(MAKEFILE_LIST))))
-
-include $(module_directory)/../../solenv/gbuild/partial_build.mk
-
-# vim: set noet sw=4 ts=4:

external/msc-externals/Module_msc-externals.mk
@@ -1,29 +1,0 @@
-# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
-#
-# This file is part of the LibreOffice project.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-
-$(eval $(call gb_Module_Module,msc-externals))
-
-ifneq ($(BUILD_X64),)
-
-$(eval $(call gb_Module_add_targets,msc-externals,\
-	Package_msvc_dlls \
-))
-
-endif
-
-# Install the universal crts (tdf#108580)
-ifneq ($(UCRT_REDISTDIR),)
-
-$(eval $(call gb_Module_add_targets,msc-externals,\
-	Package_ucrt \
-))
-
-endif
-
-# vim: set noet sw=4 ts=4:

external/msc-externals/Package_msvc_dlls.mk
@@ -1,16 +1,0 @@
-# -*- Mode: makefile-gmake; tab-width: 4; indent-tabs-mode: t -*-
-#
-# This file is part of the LibreOffice project.
-#
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
-
-$(eval $(call gb_Package_Package,msvc_dlls,$(MSVC_DLL_PATH)))
-
-$(eval $(call gb_Package_add_files,msvc_dlls,program/shlxthdl,\
-	$(MSVC_DLLS) \
-))
-
-# vim:set shiftwidth=4 tabstop=4 noexpandtab:

commit d39842d01b63143a4832be0e01a8b3c216afa9e7

cleanup msvcdll stuff
was needed for the 64bit shell extensions in a 32bit build of
LibreOffice, but that was already broken by
ec0a49ecc7ea8449d90c1e69857d62728af19829 since that not only removed the
ucrt stuff, but also external/msc-externals/Package_msvc_dlls.mk that
would have registered the files to be packaged into the installation
set. So remove that bit for good and do some related cleanup.

Change-Id: I0b963dcf48eb66cb37d3a6bf017c2ee9aa3fbdf8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185566
Tested-by: Jenkins
Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

config_host.mk.in
@@ -57,8 +57,6 @@ export BSH_JAR=@BSH_JAR@
 export BUILD_PLATFORM=@build@
 export BUILD_POSTGRESQL_SDBC=@BUILD_POSTGRESQL_SDBC@
 export BUILD_TYPE=@BUILD_TYPE@
-export BUILD_X64=@BUILD_X64@
-export BUILD_X86=@BUILD_X86@
 export BUILDING_PCH_WITH_OBJ=@BUILDING_PCH_WITH_OBJ@
 export BZIP2_CFLAGS=$(gb_SPACE)@BZIP2_CFLAGS@
 export BZIP2_LIBS=$(gb_SPACE)@BZIP2_LIBS@
@@ -109,8 +107,8 @@ export CUSTOM_BRAND_DIR=@CUSTOM_BRAND_DIR@
 export CUSTOM_BRAND_IMAGES=@CUSTOM_BRAND_IMAGES@
 export CXX=@CXX@
 export CXX_FOR_BUILD=@CXX_FOR_BUILD@
-export CXX_X64_BINARY=@CXX_X64_BINARY@
-export CXX_X86_BINARY=@CXX_X86_BINARY@
+CXX_X64_BINARY=@CXX_X64_BINARY@
+CXX_X86_BINARY=@CXX_X86_BINARY@
 @x_CXXFLAGS@ export CXXFLAGS=@CXXFLAGS@
 export CXXFLAGS_CXX11=@CXXFLAGS_CXX11@
 export CXXFLAGS_INTRINSICS_SSE2=@CXXFLAGS_INTRINSICS_SSE2@
@@ -515,8 +513,6 @@ export MSM_PATH=@MSM_PATH@
 export MSPUB_CFLAGS=$(gb_SPACE)@MSPUB_CFLAGS@
 export MSPUB_LIBS=$(gb_SPACE)@MSPUB_LIBS@
 export MSVC_CXX=@MSVC_CXX@
-export MSVC_DLLS=@MSVC_DLLS@
-export MSVC_DLL_PATH=@MSVC_DLL_PATH@
 export MSVC_USE_DEBUG_RUNTIME=@MSVC_USE_DEBUG_RUNTIME@
 export MWAW_CFLAGS=$(gb_SPACE)@MWAW_CFLAGS@
 export MWAW_LIBS=$(gb_SPACE)@MWAW_LIBS@

configure.ac
@@ -4684,47 +4684,21 @@ if test "$_os" = "WINNT"; then
         add_warning "You should have at least Visual Studio 2019 version 16.10 to avoid build problems. Otherwise, you may face problems with the build of some modules including dragonbox."
     fi
 
-    # Check for 64-bit (cross-)compiler to use to build the 64-bit
-    # version of the Explorer extension (and maybe other small
-    # bits, too) needed when installing a 32-bit LibreOffice on a
-    # 64-bit OS. The 64-bit Explorer extension is a feature that
-    # has been present since long in OOo. Don't confuse it with
-    # building LibreOffice itself as 64-bit code.
-
-    BUILD_X64=
-    CXX_X64_BINARY=
-
-    if test "$WIN_HOST_ARCH" = "x86" -a -n "$WIN_OTHER_ARCH"; then
-        AC_MSG_CHECKING([for the libraries to build the 64-bit Explorer extensions])
-        if test -f "$COMPATH/atlmfc/lib/x64/atls.lib" -o \
-             -f "$COMPATH/atlmfc/lib/spectre/x64/atls.lib"
-        then
-            BUILD_X64=TRUE
-            CXX_X64_BINARY=`win_short_path_for_make "$MSVC_MULTI_PATH/cl.exe"`
-            AC_MSG_RESULT([found])
-        else
-            AC_MSG_RESULT([not found])
-            AC_MSG_WARN([Installation set will not contain 64-bit Explorer extensions])
-        fi
-    elif test "$WIN_HOST_ARCH" = "x64"; then
-        CXX_X64_BINARY=$CXX
-    fi
-    AC_SUBST(BUILD_X64)
-
-    # These are passed to the environment and then used in gbuild/platform/com_MSC_class.mk
-    AC_SUBST(CXX_X64_BINARY)
+    # we used to build 64bit bits for 32bit windows (explorer shell extension, activex, similar)
+    # dating back when there was no 64bit version of LibreOffice yet (pre 5.0)
+    # now (since 25.8) we only care about the inverse, building the 32bit twain and activex spsupp
+    # lib when targeting/building the 64bit version for Windows
 
     # Check for 32-bit compiler to use to build the 32-bit TWAIN shim
     # needed to support TWAIN scan on both 32- and 64-bit systems
 
     case "$WIN_HOST_ARCH" in
     x64)
+        CXX_X64_BINARY=$CXX
         AC_MSG_CHECKING([for a x86 compiler and libraries for 32-bit binaries required for TWAIN support])
         if test -n "$CXX_X86_BINARY"; then
-            BUILD_X86=TRUE
-            AC_MSG_RESULT([preset])
+            AC_MSG_RESULT([provided via environment variable])
         elif test -n "$WIN_MULTI_ARCH"; then
-            BUILD_X86=TRUE
             CXX_X86_BINARY=`win_short_path_for_make "$MSVC_MULTI_PATH/cl.exe"`
             AC_MSG_RESULT([found])
         else
@@ -4733,11 +4707,11 @@ if test "$_os" = "WINNT"; then
         fi
         ;;
     x86)
-        BUILD_X86=TRUE
         CXX_X86_BINARY=$MSVC_CXX
         ;;
     esac
-    AC_SUBST(BUILD_X86)
+    # These are passed to the environment and then used in gbuild/platform/com_MSC_class.mk
+    AC_SUBST(CXX_X64_BINARY)
     AC_SUBST(CXX_X86_BINARY)
 fi
 AC_SUBST(VCVER)
@@ -7173,26 +7147,11 @@ find_msms()
     local version="$1"
 
     AC_MSG_CHECKING([for MSVC $version merge modules directory])
-    local my_msm_file="Microsoft_VC${version}_CRT_x86.msm"
-    local my_msm_dir
-
-    echo "$as_me:$LINENO: searching for $my_msm_file" >&5
-
-    msmdir=
-    case "$VCVER" in
-    16.0 | 17.0 | 17.14)
-        for l in `ls -1 $VC_PRODUCT_DIR/redist/MSVC/`; do
-            my_msm_dir="$VC_PRODUCT_DIR/redist/MSVC/$l/MergeModules/"
-            echo "$as_me:$LINENO: looking for $my_msm_dir${my_msm_file}])" >&5
-            if test -e "$my_msm_dir${my_msm_file}"; then
-                msmdir=$my_msm_dir
-            fi
-        done
-        ;;
-    esac
-
-    if test -n "$msmdir"; then
-        PathFormat "$msmdir"
+    local my_msm_dir="$VC_PRODUCT_DIR/redist/MSVC/${version}/MergeModules"
+    local my_msm_file="$my_msm_dir/Microsoft_VC${version}_CRT_x86.msm"
+    echo "$as_me:$LINENO: looking for ${my_msm_file}" >&5
+    if test -e "${my_msm_file}"; then
+        PathFormat "$my_msm_dir/"
         msmdir="$formatted_path"
         AC_MSG_RESULT([$msmdir])
     else
@@ -7203,40 +7162,6 @@ find_msms()
             add_warning "MSM ${my_msm_file} not found"
         fi
     fi
-}
-
-find_msvc_x64_dlls()
-{
-    # Return value: $msvcdllpath, $msvcdlls
-
-    AC_MSG_CHECKING([for MSVC x64 DLL path])
-
-    dnl Order crtver in increasing order. Then check the directories returned by
-    dnl ls in an inner loop; assuming they are also ordered in increasing order,
-    dnl the result will be the highest CRT version found in the highest directory.
-
-    msvcdllpath="$VC_PRODUCT_DIR/redist/x64/Microsoft.VC${VCVER}.CRT"
-    case "$VCVER" in
-    16.0 | 17.0 | 17.14)
-        for crtver in 141 142 143; do
-            for l in `ls -1 $VC_PRODUCT_DIR/redist/MSVC/`; do
-                echo "$as_me:$LINENO: testing $VC_PRODUCT_DIR/redist/MSVC/$l/x64/Microsoft.VC$crtver.CRT" >&5
-                if test -d "$VC_PRODUCT_DIR/redist/MSVC/$l/x64/Microsoft.VC$crtver.CRT"; then
-                    msvcdllpath="$VC_PRODUCT_DIR/redist/MSVC/$l/x64/Microsoft.VC$crtver.CRT"
-                fi
-            done
-        done
-        ;;
-    esac
-    AC_MSG_RESULT([$msvcdllpath])
-    AC_MSG_CHECKING([for MSVC x64 DLLs])
-    msvcdlls="msvcp140.dll vcruntime140.dll"
-    for dll in $msvcdlls; do
-        if ! test -f "$msvcdllpath/$dll"; then
-            AC_MSG_FAILURE([missing $dll])
-        fi
-    done
-    AC_MSG_RESULT([found all ($msvcdlls)])
 }
 
 dnl =========================================
@@ -9046,18 +8971,10 @@ fi
 AC_SUBST([JITC_PROCESSOR_TYPE])
 
 if test $_os = "WINNT"; then
-    find_msvc_x64_dlls
-    MSVC_DLL_PATH=`win_short_path_for_make "$msvcdllpath"`
-    MSVC_DLLS="$msvcdlls"
-    if echo "$msvcdllpath" | grep -q "VC143.CRT$"; then
-        with_redist=143
-    elif echo "$msvcdllpath" | grep -q "VC142.CRT$"; then
-        with_redist=142
-    elif echo "$msvcdllpath" | grep -q "VC141.CRT$"; then
-        with_redist=141
-    fi
     for i in $PKGFORMAT; do
         if test "$i" = msi; then
+            dnl strip initial v from the version label found by find_msvc
+            with_vcredis=${VCTOOLSET:1}
             find_msms "$with_redist"
             if test -n "$msmdir"; then
                 MSM_PATH=`win_short_path_for_make "$msmdir"`/
@@ -9068,8 +8985,6 @@ if test $_os = "WINNT"; then
     done
 fi
 
-AC_SUBST(MSVC_DLL_PATH)
-AC_SUBST(MSVC_DLLS)
 AC_SUBST(MSM_PATH)
 
 

extensions/Module_extensions.mk
@@ -19,7 +19,7 @@ $(eval $(call gb_Module_add_targets,extensions,\
 	Library_scn \
 	$(if $(filter WNT,$(OS)), \
 		Library_WinUserInfoBe \
-		$(if $(filter TRUE,$(BUILD_X86)),Executable_twain32shim) \
+		$(if $(CXX_X86_BINARY),Executable_twain32shim) \
 	) \
 	UIConfig_sabpilot \
 	UIConfig_scanner \

external/twain_dsm/Module_twain_dsm.mk
@@ -10,7 +10,7 @@
 $(eval $(call gb_Module_Module,twain_dsm))
 
 ifeq ($(OS),WNT)
-ifeq ($(BUILD_X86),TRUE)
+ifneq ($(CXX_X86_BINARY),)
 $(eval $(call gb_Module_add_targets,twain_dsm,\
     UnpackedTarball_twain_dsm \
 ))

postprocess/CustomTarget_signing.mk
@@ -31,7 +31,6 @@ $(gb_CustomTarget_workdir)/postprocess/signing/signing.done:
 ifeq ($(COM),MSC)
 	EXCLUDELIST=$(shell $(gb_MKTEMP)) && \
 	echo "$(foreach lib,$(gb_MERGEDLIBS),$(call gb_Library_get_filename,$(lib)))" | tr ' ' '\n' > $$EXCLUDELIST && \
-	$(if $(BUILD_X64),chmod u+w $(foreach lib,$(MSVC_DLLS),$(INSTDIR)/program/shlxthdl/$(lib)) &&) \
 	$(PERL) $(SRCDIR)/postprocess/signing/signing.pl \
 			-e $$EXCLUDELIST \
 			-l $(subst .done,_log.txt,$@) \

solenv/gbuild/StaticLibrary.mk
@@ -76,9 +76,7 @@ gb_StaticLibrary_add_cxxclrobjects = $(call gb_StaticLibrary__forward_to_Linktar
 gb_StaticLibrary_add_asmobject = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_asmobjects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_exception_objects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
-gb_StaticLibrary_add_x64_generated_exception_objects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_generated_cobjects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
-gb_StaticLibrary_add_x64_generated_cobjects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_generated_exception_objects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_generated_objcobjects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_generated_objcxxobjects = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
@@ -93,7 +91,6 @@ gb_StaticLibrary_add_defs = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),
 gb_StaticLibrary_set_include = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_ldflags = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_set_ldflags = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
-gb_StaticLibrary_set_x64 = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_add_libs = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_set_library_path_flags = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))
 gb_StaticLibrary_use_api = $(call gb_StaticLibrary__forward_to_Linktarget,$(0),$(1),$(2),$(3))