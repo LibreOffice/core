LibreOffice/core

commit ec7ba61a6164c805f5a71b077715b7e1521a2d62

simplify SfxPoolItemArray_Impl (tdf#81765 related)
Since we want to look up items by pointer, just store them in a
std::unordered_set, which allows fast find().
This dramatically simplifies most operations on this data structure.

Fix a dodgy sd test that was relying on items with the same whichid
being in the pool being in a certain order.

Change-Id: I4d79fc718f95e3083a20788be1050fbe9fca7263
Reviewed-on: https://gerrit.libreoffice.org/70881
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/xoutdev/xattr.cxx
@@ -128,17 +128,15 @@ OUString NameOrIndex::CheckNamedItem( const NameOrIndex* pCheckItem, const sal_u
 
     if (!aUniqueName.isEmpty() && pPool1)
     {
-        const sal_uInt32 nCount = pPool1->GetItemCount2( nWhich );
-
-        for( sal_uInt32 nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+        for (const SfxPoolItem* pItem : pPool1->GetItemSurrogates(nWhich))
         {
-            const NameOrIndex *pItem = static_cast<const NameOrIndex*>(pPool1->GetItem2( nWhich, nSurrogate ));
+            const NameOrIndex *pNameOrIndex = static_cast<const NameOrIndex*>(pItem);
 
-            if( pItem && ( pItem->GetName() == pCheckItem->GetName() ) )
+            if( pNameOrIndex->GetName() == pCheckItem->GetName() )
             {
                 // if there is already an item with the same name and the same
                 // value it's ok to set it
-                if( !pCompareValueFunc( pItem, pCheckItem ) )
+                if( !pCompareValueFunc( pNameOrIndex, pCheckItem ) )
                 {
                     // same name but different value, we need a new name for this item
                     aUniqueName.clear();
@@ -215,20 +213,18 @@ OUString NameOrIndex::CheckNamedItem( const NameOrIndex* pCheckItem, const sal_u
 
         if (aUniqueName.isEmpty() && pPool1)
         {
-            const sal_uInt32 nCount = pPool1->GetItemCount2( nWhich );
-            const NameOrIndex *pItem;
-            for( sal_uInt32 nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+            for (const SfxPoolItem* pItem : pPool1->GetItemSurrogates(nWhich))
             {
-                pItem = static_cast<const NameOrIndex*>(pPool1->GetItem2( nWhich, nSurrogate ));
+                const NameOrIndex *pNameOrIndex = static_cast<const NameOrIndex*>(pItem);
 
-                if( pItem && !pItem->GetName().isEmpty() )
+                if( !pNameOrIndex->GetName().isEmpty() )
                 {
-                    if( !bForceNew && pCompareValueFunc( pItem, pCheckItem ) )
-                        return pItem->GetName();
+                    if( !bForceNew && pCompareValueFunc( pNameOrIndex, pCheckItem ) )
+                        return pNameOrIndex->GetName();
 
-                    if( pItem->GetName().startsWith( aUser ) )
+                    if( pNameOrIndex->GetName().startsWith( aUser ) )
                     {
-                        sal_Int32 nThisIndex = pItem->GetName().copy( aUser.getLength() ).toInt32();
+                        sal_Int32 nThisIndex = pNameOrIndex->GetName().copy( aUser.getLength() ).toInt32();
                         if( nThisIndex >= nUserIndex )
                             nUserIndex = nThisIndex + 1;
                     }
@@ -1098,16 +1094,12 @@ std::unique_ptr<XLineStartItem> XLineStartItem::checkForUniqueItem( SdrModel* pM
         // 2. if we have a name check if there is already an item with the
         // same name in the documents pool with a different line end or start
 
-        sal_uInt32 nCount, nSurrogate;
-
         const SfxItemPool& rPool1 = pModel->GetItemPool();
         if (!aUniqueName.isEmpty())
         {
-            nCount = rPool1.GetItemCount2(XATTR_LINESTART);
-
-            for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+            for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINESTART))
             {
-                const XLineStartItem* pItem = rPool1.GetItem2(XATTR_LINESTART, nSurrogate);
+                auto pItem = dynamic_cast<const XLineStartItem*>(p);
 
                 if( pItem && ( pItem->GetName() == pLineStartItem->GetName() ) )
                 {
@@ -1125,11 +1117,9 @@ std::unique_ptr<XLineStartItem> XLineStartItem::checkForUniqueItem( SdrModel* pM
 
             if( !bForceNew )
             {
-                nCount = rPool1.GetItemCount2(XATTR_LINEEND);
-
-                for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+                for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINEEND))
                 {
-                    const XLineEndItem* pItem = rPool1.GetItem2(XATTR_LINEEND, nSurrogate);
+                    auto pItem = dynamic_cast<const XLineEndItem*>(p);
 
                     if( pItem && ( pItem->GetName() == pLineStartItem->GetName() ) )
                     {
@@ -1150,10 +1140,9 @@ std::unique_ptr<XLineStartItem> XLineStartItem::checkForUniqueItem( SdrModel* pM
         const SfxItemPool* pPool2 = pModel->GetStyleSheetPool() ? &pModel->GetStyleSheetPool()->GetPool() : nullptr;
         if( !aUniqueName.isEmpty() && pPool2)
         {
-            nCount = pPool2->GetItemCount2( XATTR_LINESTART );
-            for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+            for (const SfxPoolItem* p : pPool2->GetItemSurrogates(XATTR_LINESTART))
             {
-                const XLineStartItem* pItem = pPool2->GetItem2( XATTR_LINESTART, nSurrogate );
+                auto pItem = dynamic_cast<const XLineStartItem*>(p);
 
                 if( pItem && ( pItem->GetName() == pLineStartItem->GetName() ) )
                 {
@@ -1171,10 +1160,9 @@ std::unique_ptr<XLineStartItem> XLineStartItem::checkForUniqueItem( SdrModel* pM
 
             if( !bForceNew )
             {
-                nCount = pPool2->GetItemCount2( XATTR_LINEEND );
-                for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+                for (const SfxPoolItem* p : pPool2->GetItemSurrogates(XATTR_LINEEND))
                 {
-                    const XLineEndItem* pItem = pPool2->GetItem2( XATTR_LINEEND, nSurrogate );
+                    auto pItem = dynamic_cast<const XLineEndItem*>(p);
 
                     if( pItem && ( pItem->GetName() == pLineStartItem->GetName() ) )
                     {
@@ -1201,12 +1189,9 @@ std::unique_ptr<XLineStartItem> XLineStartItem::checkForUniqueItem( SdrModel* pM
             sal_Int32 nUserIndex = 1;
             const OUString aUser(SvxResId(RID_SVXSTR_LINEEND));
 
-            nCount = rPool1.GetItemCount2(XATTR_LINESTART);
-            sal_uInt32 nSurrogate2;
-
-            for (nSurrogate2 = 0; nSurrogate2 < nCount; nSurrogate2++)
+            for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINESTART))
             {
-                const XLineStartItem* pItem = rPool1.GetItem2(XATTR_LINESTART, nSurrogate2);
+                auto pItem = dynamic_cast<const XLineStartItem*>(p);
 
                 if (pItem && !pItem->GetName().isEmpty())
                 {
@@ -1226,10 +1211,9 @@ std::unique_ptr<XLineStartItem> XLineStartItem::checkForUniqueItem( SdrModel* pM
                 }
             }
 
-            nCount = rPool1.GetItemCount2(XATTR_LINEEND);
-            for (nSurrogate2 = 0; nSurrogate2 < nCount; nSurrogate2++)
+            for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINEEND))
             {
-                const XLineEndItem* pItem = rPool1.GetItem2(XATTR_LINEEND, nSurrogate2);
+                auto pItem = dynamic_cast<const XLineEndItem*>(p);
 
                 if (pItem && !pItem->GetName().isEmpty())
                 {
@@ -1349,16 +1333,12 @@ std::unique_ptr<XLineEndItem> XLineEndItem::checkForUniqueItem( SdrModel* pModel
         // 2. if we have a name check if there is already an item with the
         // same name in the documents pool with a different line end or start
 
-        sal_uInt16 nCount, nSurrogate;
-
         const SfxItemPool& rPool1 = pModel->GetItemPool();
         if (!aUniqueName.isEmpty())
         {
-            nCount = rPool1.GetItemCount2(XATTR_LINESTART);
-
-            for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+            for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINESTART))
             {
-                const XLineStartItem* pItem = rPool1.GetItem2(XATTR_LINESTART, nSurrogate);
+                auto pItem = dynamic_cast<const XLineStartItem*>(p);
 
                 if( pItem && ( pItem->GetName() == pLineEndItem->GetName() ) )
                 {
@@ -1376,11 +1356,9 @@ std::unique_ptr<XLineEndItem> XLineEndItem::checkForUniqueItem( SdrModel* pModel
 
             if( !bForceNew )
             {
-                nCount = rPool1.GetItemCount2(XATTR_LINEEND);
-
-                for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+                for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINEEND))
                 {
-                    const XLineEndItem* pItem = rPool1.GetItem2(XATTR_LINEEND, nSurrogate);
+                    auto pItem = dynamic_cast<const XLineEndItem*>(p);
 
                     if( pItem && ( pItem->GetName() == pLineEndItem->GetName() ) )
                     {
@@ -1401,10 +1379,9 @@ std::unique_ptr<XLineEndItem> XLineEndItem::checkForUniqueItem( SdrModel* pModel
         const SfxItemPool* pPool2 = pModel->GetStyleSheetPool() ? &pModel->GetStyleSheetPool()->GetPool() : nullptr;
         if( !aUniqueName.isEmpty() && pPool2)
         {
-            nCount = pPool2->GetItemCount2( XATTR_LINESTART );
-            for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+            for (const SfxPoolItem* p : pPool2->GetItemSurrogates(XATTR_LINESTART))
             {
-                const XLineStartItem* pItem = pPool2->GetItem2( XATTR_LINESTART, nSurrogate );
+                auto pItem = dynamic_cast<const XLineStartItem*>(p);
 
                 if( pItem && ( pItem->GetName() == pLineEndItem->GetName() ) )
                 {
@@ -1422,10 +1399,9 @@ std::unique_ptr<XLineEndItem> XLineEndItem::checkForUniqueItem( SdrModel* pModel
 
             if( !bForceNew )
             {
-                nCount = pPool2->GetItemCount2( XATTR_LINEEND );
-                for( nSurrogate = 0; nSurrogate < nCount; nSurrogate++ )
+                for (const SfxPoolItem* p : pPool2->GetItemSurrogates(XATTR_LINEEND))
                 {
-                    const XLineEndItem* pItem = pPool2->GetItem2( XATTR_LINEEND, nSurrogate );
+                    auto pItem = dynamic_cast<const XLineEndItem*>(p);
 
                     if( pItem && ( pItem->GetName() == pLineEndItem->GetName() ) )
                     {
@@ -1452,12 +1428,9 @@ std::unique_ptr<XLineEndItem> XLineEndItem::checkForUniqueItem( SdrModel* pModel
             sal_Int32 nUserIndex = 1;
             const OUString aUser(SvxResId(RID_SVXSTR_LINEEND));
 
-            nCount = rPool1.GetItemCount2(XATTR_LINESTART);
-            sal_uInt32 nSurrogate2;
-
-            for (nSurrogate2 = 0; nSurrogate2 < nCount; nSurrogate2++)
+            for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINESTART))
             {
-                const XLineStartItem* pItem = rPool1.GetItem2(XATTR_LINESTART, nSurrogate2);
+                auto pItem = dynamic_cast<const XLineStartItem*>(p);
 
                 if (pItem && !pItem->GetName().isEmpty())
                 {
@@ -1477,10 +1450,9 @@ std::unique_ptr<XLineEndItem> XLineEndItem::checkForUniqueItem( SdrModel* pModel
                 }
             }
 
-            nCount = rPool1.GetItemCount2(XATTR_LINEEND);
-            for (nSurrogate2 = 0; nSurrogate2 < nCount; nSurrogate2++)
+            for (const SfxPoolItem* p : rPool1.GetItemSurrogates(XATTR_LINEEND))
             {
-                const XLineEndItem* pItem = rPool1.GetItem2(XATTR_LINEEND, nSurrogate2);
+                auto pItem = dynamic_cast<const XLineEndItem*>(p);
 
                 if (pItem && !pItem->GetName().isEmpty())
                 {

editeng/source/editeng/impedit4.cxx
@@ -310,15 +310,9 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel )
         else if ( nScriptType == 2 )
             nWhich = EE_CHAR_FONTINFO_CTL;
 
-        auto const nFonts(aEditDoc.GetItemPool().GetItemCount2(nWhich));
-        for (sal_uInt32 i = 0; i < nFonts; ++i)
+        for (const SfxPoolItem* pItem : aEditDoc.GetItemPool().GetItemSurrogates(nWhich))
         {
-            SvxFontItem const*const pFontItem = static_cast<const SvxFontItem*>(
-                    aEditDoc.GetItemPool().GetItem2(nWhich, i));
-            if (!pFontItem)
-            {
-                continue;
-            }
+            SvxFontItem const*const pFontItem = static_cast<const SvxFontItem*>(pItem);
             bool bAlreadyExist = false;
             sal_uLong nTestMax = nScriptType ? aFontTable.size() : 1;
             for ( sal_uLong nTest = 0; !bAlreadyExist && ( nTest < nTestMax ); nTest++ )
@@ -390,10 +384,9 @@ ErrCode ImpEditEngine::WriteRTF( SvStream& rOutput, EditSelection aSel )
     {
         aColorList.push_back(rDefault.GetValue());
     }
-    auto const nColors(aEditDoc.GetItemPool().GetItemCount2(EE_CHAR_COLOR));
-    for (sal_uInt32 i = 0; i < nColors; ++i)
+    for (const SfxPoolItem* pItem : aEditDoc.GetItemPool().GetItemSurrogates(EE_CHAR_COLOR))
     {
-        SvxColorItem const*const pColorItem(aEditDoc.GetItemPool().GetItem2(EE_CHAR_COLOR, i));
+        auto pColorItem(dynamic_cast<SvxColorItem const*>(pItem));
         if (pColorItem && pColorItem->GetValue() != COL_AUTO) // may be null!
         {
             aColorList.push_back(pColorItem->GetValue());

include/svl/itempool.hxx
@@ -25,6 +25,7 @@
 #include <svl/typedwhich.hxx>
 #include <memory>
 #include <vector>
+#include <o3tl/sorted_vector.hxx>
 
 class SfxBroadcaster;
 struct SfxItemPool_Impl;
@@ -154,15 +155,19 @@ public:
 
     bool                            CheckItemInPool(const SfxPoolItem *) const;
 
-    const SfxPoolItem *             GetItem2(sal_uInt16 nWhich, sal_uInt32 nSurrogate) const;
-    template<class T> const T*      GetItem2( TypedWhichId<T> nWhich, sal_uInt32 nSurrogate ) const
-    { return dynamic_cast<const T*>(GetItem2(sal_uInt16(nWhich), nSurrogate)); }
-
+    struct Item2Range
+    {
+        o3tl::sorted_vector<SfxPoolItem*>::const_iterator m_begin;
+        o3tl::sorted_vector<SfxPoolItem*>::const_iterator m_end;
+        o3tl::sorted_vector<SfxPoolItem*>::const_iterator begin() { return m_begin; }
+        o3tl::sorted_vector<SfxPoolItem*>::const_iterator end() { return m_end; }
+    };
     const SfxPoolItem *             GetItem2Default(sal_uInt16 nWhich) const;
     template<class T> const T*      GetItem2Default( TypedWhichId<T> nWhich ) const
     { return static_cast<const T*>(GetItem2Default(sal_uInt16(nWhich))); }
 
     sal_uInt32                      GetItemCount2(sal_uInt16 nWhich) const;
+    Item2Range                      GetItemSurrogates(sal_uInt16 nWhich) const;
 
     sal_uInt16                      GetFirstWhich() const;
     sal_uInt16                      GetLastWhich() const;

sc/source/core/data/docpool.cxx
@@ -349,10 +349,9 @@ const SfxPoolItem& ScDocumentPool::Put( const SfxPoolItem& rItem, sal_uInt16 nWh
 
 void ScDocumentPool::StyleDeleted( const ScStyleSheet* pStyle )
 {
-    sal_uInt32 nCount = GetItemCount2(ATTR_PATTERN);
-    for (sal_uInt32 i=0; i<nCount; i++)
+    for (const SfxPoolItem* pItem : GetItemSurrogates( ATTR_PATTERN ))
     {
-        ScPatternAttr* pPattern = const_cast<ScPatternAttr*>(GetItem2(ATTR_PATTERN, i));
+        ScPatternAttr* pPattern = const_cast<ScPatternAttr*>(dynamic_cast<const ScPatternAttr*>(pItem));
         if ( pPattern && pPattern->GetStyleSheet() == pStyle )
             pPattern->StyleToName();
     }
@@ -365,11 +364,9 @@ void ScDocumentPool::CellStyleCreated( const OUString& rName, const ScDocument* 
     // Calling StyleSheetChanged isn't enough because the pool may still contain items
     // for undo or clipboard content.
 
-    sal_uInt32 nCount = GetItemCount2(ATTR_PATTERN);
-    for (sal_uInt32 i=0; i<nCount; i++)
+    for (const SfxPoolItem* pItem : GetItemSurrogates( ATTR_PATTERN ))
     {
-        ScPatternAttr *const pPattern =
-            const_cast<ScPatternAttr*>(GetItem2(ATTR_PATTERN, i));
+        auto pPattern = const_cast<ScPatternAttr*>(dynamic_cast<const ScPatternAttr*>(pItem));
         if ( pPattern && pPattern->GetStyleSheet() == nullptr )
         {
             const OUString* pStyleName = pPattern->GetStyleName();

sc/source/core/data/documen9.cxx
@@ -553,25 +553,23 @@ void ScDocument::UpdateFontCharSet()
     if ( eSrcSet != eSysSet || bUpdateOld )
     {
         ScDocumentPool* pPool = mxPoolHelper->GetDocPool();
-        sal_uInt32 nCount = pPool->GetItemCount2(ATTR_FONT);
-        for (sal_uInt32 i=0; i<nCount; i++)
+        for (const SfxPoolItem* pItem : pPool->GetItemSurrogates(ATTR_FONT))
         {
-            SvxFontItem* pItem = const_cast<SvxFontItem*>(pPool->GetItem2(ATTR_FONT, i));
-            if ( pItem && ( pItem->GetCharSet() == eSrcSet ||
-                            ( bUpdateOld && pItem->GetCharSet() != RTL_TEXTENCODING_SYMBOL ) ) )
-                pItem->SetCharSet(eSysSet);
+            auto pFontItem = const_cast<SvxFontItem*>(dynamic_cast<const SvxFontItem*>(pItem));
+            if ( pFontItem && ( pFontItem->GetCharSet() == eSrcSet ||
+                               ( bUpdateOld && pFontItem->GetCharSet() != RTL_TEXTENCODING_SYMBOL ) ) )
+                pFontItem->SetCharSet(eSysSet);
         }
 
         if ( mpDrawLayer )
         {
             SfxItemPool& rDrawPool = mpDrawLayer->GetItemPool();
-            nCount = rDrawPool.GetItemCount2(EE_CHAR_FONTINFO);
-            for (sal_uInt32 i=0; i<nCount; i++)
+            for (const SfxPoolItem* pItem : rDrawPool.GetItemSurrogates(EE_CHAR_FONTINFO))
             {
-                SvxFontItem* pItem = const_cast<SvxFontItem*>(rDrawPool.GetItem2(EE_CHAR_FONTINFO, i));
-                if ( pItem && ( pItem->GetCharSet() == eSrcSet ||
-                                ( bUpdateOld && pItem->GetCharSet() != RTL_TEXTENCODING_SYMBOL ) ) )
-                    pItem->SetCharSet( eSysSet );
+                SvxFontItem* pFontItem = const_cast<SvxFontItem*>(dynamic_cast<const SvxFontItem*>(pItem));
+                if ( pFontItem && ( pFontItem->GetCharSet() == eSrcSet ||
+                                   ( bUpdateOld && pFontItem->GetCharSet() != RTL_TEXTENCODING_SYMBOL ) ) )
+                    pFontItem->SetCharSet( eSysSet );
             }
         }
     }

commit cb5a4314c3561179e556107b81830a9949b20517

tdf#149207 fix image not getting a unique name
Using "background" as the name, CheckNamedItem doesn't correctly
determine that another XFillBitmapItem exists with an same
"background" name already, which is then a problem when exporting
the document and from all "background" images only one will be
exported.

regression from
  commit ec7ba61a6164c805f5a71b077715b7e1521a2d62
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Wed Apr 17 15:19:25 2019 +0200
  simplify SfxPoolItemArray_Impl (tdf#81765 related)

I am not sure why the above commit caused this problem.

Right now, the reason CheckNamedItem doesn't work is because
with other subsequent changes to the pool infrastructure, the
new PoolItem is already in the surrogates/NameOrIndex array
and so it finds itself when scanning the array.

But back then, the new PoolItem should not have been inserted
yet.

Any way, fix it by making CheckNamedItem exclude itself when
scanning the surrogates data.

Added a test for uniqueness of names.

Change-Id: I3c04065a4875a21649ad8141243cc39a8d1a5b3c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185719
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

sd/inc/sdpage.hxx
@@ -343,7 +343,7 @@ public:
 
     /** @return the presentation style with the given helpid from this masterpage or this
         slides masterpage */
-    SdStyleSheet* getPresentationStyle( sal_uInt32 nHelpId ) const;
+    SD_DLLPUBLIC SdStyleSheet* getPresentationStyle( sal_uInt32 nHelpId ) const;
 
     /** removes all empty presentation objects from this slide */
     void RemoveEmptyPresentationObjects();

sd/source/ui/inc/DrawViewShell.hxx
@@ -513,8 +513,8 @@ private:
     css::uno::Reference<css::presentation::XSlideShow> mxSlideShow;
 };
 
-    /// Merge the background properties together and deposit the result in rMergeAttr
-    void MergePageBackgroundFilling(SdPage *pPage, SdStyleSheet *pStyleSheet, bool bMasterPage, SfxItemSet& rMergedAttr);
+/// Merge the background properties together and deposit the result in rMergeAttr
+SD_DLLPUBLIC void MergePageBackgroundFilling(SdPage *pPage, SdStyleSheet *pStyleSheet, bool bMasterPage, SfxItemSet& rMergedAttr);
 
 } // end of namespace sd
 

svx/source/xoutdev/xattr.cxx
@@ -153,7 +153,7 @@ OUString NameOrIndex::CheckNamedItem(const sal_uInt16 nWhich, const SfxItemPool*
             const NameOrIndex *pNameOrIndex = static_cast<const NameOrIndex*>(pItem);
 
             // need to check for WhichID, GetItemSurrogatesForItem does buffer on type only
-            if( pNameOrIndex->Which() == nWhich && pNameOrIndex->GetName() == GetName() )
+            if( pNameOrIndex != this && pNameOrIndex->Which() == nWhich && pNameOrIndex->GetName() == GetName() )
             {
                 // if there is already an item with the same name and the same
                 // value it's ok to set it
@@ -242,7 +242,7 @@ OUString NameOrIndex::CheckNamedItem(const sal_uInt16 nWhich, const SfxItemPool*
                 const NameOrIndex *pNameOrIndex = static_cast<const NameOrIndex*>(pItem);
 
                 // need to check for WhichID, GetItemSurrogatesForItem does buffer on type only
-                if( pNameOrIndex->Which() == nWhich && !pNameOrIndex->GetName().isEmpty() )
+                if( pNameOrIndex != this && pNameOrIndex->Which() == nWhich && !pNameOrIndex->GetName().isEmpty() )
                 {
                     if( !bForceNew && pCompareValueFunc( pNameOrIndex, this ) )
                         return pNameOrIndex->GetName();

sd/qa/unit/misc-tests.cxx
@@ -38,10 +38,13 @@
 #include <editeng/outlobj.hxx>
 #include <editeng/editobj.hxx>
 #include <comphelper/base64.hxx>
+#include <comphelper/propertysequence.hxx>
 #include <docmodel/uno/UnoGradientTools.hxx>
 #include <undo/undomanager.hxx>
 #include <GraphicViewShell.hxx>
 #include <sdpage.hxx>
+#include <app.hrc>
+#include <DrawViewShell.hxx>
 #include <LayerTabBar.hxx>
 #include <vcl/event.hxx>
 #include <vcl/keycodes.hxx>
@@ -50,6 +53,8 @@
 #include <svx/view3d.hxx>
 #include <svx/scene3d.hxx>
 #include <svx/sdmetitm.hxx>
+#include <svx/xfillit0.hxx>
+#include <svx/xbtmpit.hxx>
 #include <unomodel.hxx>
 
 using namespace ::com::sun::star;
@@ -86,6 +91,7 @@ public:
     void testTdf164284();
     void testEncodedTableStyles();
     void testTdf157117();
+    void testPageBackgroundImages();
 
     CPPUNIT_TEST_SUITE(SdMiscTest);
     CPPUNIT_TEST(testTdf99396);
@@ -111,6 +117,7 @@ public:
     CPPUNIT_TEST(testTdf164284);
     CPPUNIT_TEST(testEncodedTableStyles);
     CPPUNIT_TEST(testTdf157117);
+    CPPUNIT_TEST(testPageBackgroundImages);
     CPPUNIT_TEST_SUITE_END();
 };
 
@@ -1023,6 +1030,146 @@ void SdMiscTest::testTdf157117()
     // Check that the new last page is moved to. Before, the first page was always moved to when
     // the last page was deleted.
     CPPUNIT_ASSERT_EQUAL(1, (nPageNum - 1) / 2);
+}
+
+void SdMiscTest::testPageBackgroundImages()
+{
+    // Create empty document
+    createSdDrawDoc();
+
+    auto pXImpressDocument = dynamic_cast<SdXImpressDocument*>(mxComponent.get());
+    sd::ViewShell* pViewShell = pXImpressDocument->GetDocShell()->GetViewShell();
+    CPPUNIT_ASSERT(pViewShell);
+
+    auto* pDrawViewShell = dynamic_cast<sd::DrawViewShell*>(pViewShell);
+    CPPUNIT_ASSERT(pDrawViewShell);
+
+    SdDrawDocument* pDocument = pXImpressDocument->GetDocShell()->GetDoc();
+    CPPUNIT_ASSERT(pDocument);
+
+    // Check we have 1 Page
+    CPPUNIT_ASSERT_EQUAL(sal_uInt16(1), pDocument->GetSdPageCount(PageKind::Standard));
+
+    // Add 3 pages
+    dispatchCommand(mxComponent, u".uno:InsertPage"_ustr, {});
+    dispatchCommand(mxComponent, u".uno:InsertPage"_ustr, {});
+    dispatchCommand(mxComponent, u".uno:InsertPage"_ustr, {});
+
+    // Check we have 4 Pages now
+    CPPUNIT_ASSERT_EQUAL(sal_uInt16(4), pDocument->GetSdPageCount(PageKind::Standard));
+
+    // Add a background graphic to page 1
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(0));
+        uno::Sequence<beans::PropertyValue> aArgs(comphelper::InitPropertySequence({
+            { "FileName", uno::Any(createFileURL(u"TestImage1.png")) },
+        }));
+
+        dispatchCommand(mxComponent, u".uno:SelectBackground"_ustr, aArgs);
+    }
+
+    // Add a background graphic to page 2
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(1));
+        uno::Sequence<beans::PropertyValue> aArgs(comphelper::InitPropertySequence({
+            { "FileName", uno::Any(createFileURL(u"TestImage2.png")) },
+        }));
+
+        dispatchCommand(mxComponent, u".uno:SelectBackground"_ustr, aArgs);
+    }
+
+    // Add a background graphic to page 3
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(2));
+        uno::Sequence<beans::PropertyValue> aArgs(comphelper::InitPropertySequence({
+            { "FileName", uno::Any(createFileURL(u"TestImage3.png")) },
+        }));
+
+        dispatchCommand(mxComponent, u".uno:SelectBackground"_ustr, aArgs);
+    }
+
+    // Add a background graphic to page 4
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(3));
+        uno::Sequence<beans::PropertyValue> aArgs(comphelper::InitPropertySequence({
+            { "FileName", uno::Any(createFileURL(u"TestImage4.png")) },
+        }));
+
+        dispatchCommand(mxComponent, u".uno:SelectBackground"_ustr, aArgs);
+    }
+
+    // Store graphic names
+    std::unordered_set<OUString> aGraphicNames;
+
+    // Check page 1
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(0));
+        SdPage* pPage = pViewShell->GetActualPage();
+
+        SfxItemSetFixed<XATTR_FILL_FIRST, XATTR_FILL_LAST> aMergedAttr(pDocument->GetPool());
+        SdStyleSheet* pStyleSheet = pPage->getPresentationStyle(HID_PSEUDOSHEET_BACKGROUND);
+        sd::MergePageBackgroundFilling(pPage, pStyleSheet, false, aMergedAttr);
+
+        // Style should be "BITMAP"
+        CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_BITMAP,
+                             aMergedAttr.Get(XATTR_FILLSTYLE).GetValue());
+        auto aItem = aMergedAttr.Get<XFillBitmapItem>(XATTR_FILLBITMAP);
+        aGraphicNames.insert(aItem.GetName());
+    }
+
+    // Check page 2
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(1));
+        SdPage* pPage = pViewShell->GetActualPage();
+
+        // Style should be "BITMAP"
+        SfxItemSetFixed<XATTR_FILL_FIRST, XATTR_FILL_LAST> aMergedAttr(pDocument->GetPool());
+        SdStyleSheet* pStyleSheet = pPage->getPresentationStyle(HID_PSEUDOSHEET_BACKGROUND);
+        sd::MergePageBackgroundFilling(pPage, pStyleSheet, false, aMergedAttr);
+
+        CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_BITMAP,
+                             aMergedAttr.Get(XATTR_FILLSTYLE).GetValue());
+        auto aItem = aMergedAttr.Get<XFillBitmapItem>(XATTR_FILLBITMAP);
+        aGraphicNames.insert(aItem.GetName());
+    }
+
+    // Check page 3
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(2));
+        SdPage* pPage = pViewShell->GetActualPage();
+
+        // Style should be "BITMAP"
+        SfxItemSetFixed<XATTR_FILL_FIRST, XATTR_FILL_LAST> aMergedAttr(pDocument->GetPool());
+        SdStyleSheet* pStyleSheet = pPage->getPresentationStyle(HID_PSEUDOSHEET_BACKGROUND);
+        sd::MergePageBackgroundFilling(pPage, pStyleSheet, false, aMergedAttr);
+
+        CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_BITMAP,
+                             aMergedAttr.Get(XATTR_FILLSTYLE).GetValue());
+        auto aItem = aMergedAttr.Get<XFillBitmapItem>(XATTR_FILLBITMAP);
+        aGraphicNames.insert(aItem.GetName());
+    }
+
+    // Check page 4
+    {
+        CPPUNIT_ASSERT_EQUAL(true, pDrawViewShell->SwitchPage(3));
+        SdPage* pPage = pViewShell->GetActualPage();
+
+        SfxItemSetFixed<XATTR_FILL_FIRST, XATTR_FILL_LAST> aMergedAttr(pDocument->GetPool());
+        SdStyleSheet* pStyleSheet = pPage->getPresentationStyle(HID_PSEUDOSHEET_BACKGROUND);
+        sd::MergePageBackgroundFilling(pPage, pStyleSheet, false, aMergedAttr);
+
+        // Style should be "BITMAP"
+        CPPUNIT_ASSERT_EQUAL(drawing::FillStyle_BITMAP,
+                             aMergedAttr.Get(XATTR_FILLSTYLE).GetValue());
+        auto aItem = aMergedAttr.Get<XFillBitmapItem>(XATTR_FILLBITMAP);
+        aGraphicNames.insert(aItem.GetName());
+    }
+
+    // Size of graphic names should be 4 - this means each page has a unique name
+    CPPUNIT_ASSERT_EQUAL(size_t(4), aGraphicNames.size());
+    // Check none of the graphic names is empty
+    for (OUString const& rName : aGraphicNames)
+        CPPUNIT_ASSERT(!rName.isEmpty());
 }
 
 CPPUNIT_TEST_SUITE_REGISTRATION(SdMiscTest);