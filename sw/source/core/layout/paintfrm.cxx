LibreOffice/core

commit 08007b5f0ab0538bb907b99770938bb9f10dba20

sw: do not paint hidden frames' borders
SwFrame::PaintSwFrameShadowAndBorder() is called by upper's
SwFrame::PaintSwFrameBackground() so it needs another check.

(regression from commit ff7f1b59e22092d8548459e75fe912db852f056f)

Change-Id: Ic7ac28e4af8e0abf4970d87edb40a79ad4d25156
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177032
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
(cherry picked from commit ea1d0a62d9b9790c235044a754604166ec5df6d4)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177041
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>
(cherry picked from commit dd8eeb63368a657b54e645e3549bafc5a112ef07)

sw/source/core/layout/paintfrm.cxx
@@ -5546,6 +5546,11 @@ void SwFrame::PaintSwFrameShadowAndBorder(
             return;
     }
 
+    if (!getFramePrintArea().HasArea())
+    { // hidden text frame, may be called by upper from PaintSwFrameBackground()
+        return;
+    }
+
     const bool bLine = rAttrs.IsLine();
     const bool bShadow = rAttrs.GetShadow().GetLocation() != SvxShadowLocation::NONE;
 

commit e887210914951b7b3f72dac5609fb12e4c3319f5

tdf#166517 paint borders of frame with zero print area
allow painting borders when text frame in the cell is hidden, but
the cell is not hidden

(regression from commit 08007b5f0ab0538bb907b99770938bb9f10dba20)

Change-Id: Idc1810d95d6d32a3a9b09486a1581614b5546434
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185728
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>

sw/source/core/layout/paintfrm.cxx
@@ -5529,7 +5529,7 @@ void SwFrame::PaintSwFrameShadowAndBorder(
             return;
     }
 
-    if (!getFramePrintArea().HasArea())
+    if (!getFrameArea().HasArea())
     { // hidden text frame, may be called by upper from PaintSwFrameBackground()
         return;
     }