558bd068e204c08010433f98d400a5103a053d83
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-05-26 10:31:35 +0100

    drop unused vcl::TranslateChar
    and its converter cache
    
    Change-Id: Ib8ca08b7b8693cd92a576786e327ff7e63486920
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185790
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- vcl/Library_vcl.mk
+++ vcl/Library_vcl.mk
@@ -508,7 +508,6 @@ $(eval $(call gb_Library_add_exception_objects,vcl,\
     vcl/source/fontsubset/fontsubset \
     vcl/source/fontsubset/sft \
     vcl/source/fontsubset/ttcr \
-    vcl/source/fontsubset/xlat \
     vcl/source/pdf/COSWriter \
     vcl/source/pdf/EncryptionHashTransporter \
     vcl/source/pdf/ExternalPDFStreams \
