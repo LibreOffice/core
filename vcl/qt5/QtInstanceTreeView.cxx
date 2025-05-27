8190aefd1ff80fb8679febdadca784f551bf6f08
Michael Weghorn <m.weghorn@posteo.de>
2025-05-27 09:09:49 +0200

    tdf#130857 qt weld: Implement QtInstanceTreeView::get_iter_index_in_parent
    This is used in the "View" -> "User Interface"
    dialog's new "Toolbars" tab page introduced in
    
        commit 4ab26f4ea0c627844531144a6a903c357a604a5e
        Date:   Mon May 26 12:03:40 2025 +0200
    
            Resolves tdf#158880 - Let users select multiple toolbars at once
    
    , so support it in preparation of declaring support
    for that tab page in an upcoming commit.
    
    Change-Id: If31f1b920d106432760d5b521f2dde02306c79f1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185883
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceTreeView.cxx
+++ vcl/qt5/QtInstanceTreeView.cxx
@@ -438,10 +438,17 @@ int QtInstanceTreeView::get_iter_depth(const weld::TreeIter& rIter) const
     return nDepth;
 }
 
-int QtInstanceTreeView::get_iter_index_in_parent(const weld::TreeIter&) const
+int QtInstanceTreeView::get_iter_index_in_parent(const weld::TreeIter& rIter) const
 {
-    assert(false && "Not implemented yet");
-    return -1;
+    SolarMutexGuard g;
+
+    int nIndex;
+    GetQtInstance().RunInMainThread([&] {
+        const QModelIndex aIndex = modelIndex(rIter);
+        nIndex = aIndex.row();
+    });
+
+    return nIndex;
 }
 
 int QtInstanceTreeView::iter_compare(const weld::TreeIter&, const weld::TreeIter&) const
