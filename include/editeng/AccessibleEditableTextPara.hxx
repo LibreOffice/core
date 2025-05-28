0465942c3350d0e6a7eae5efbf727d53aa5560d3
Noel Grandin <noel.grandin@collabora.co.uk>
2025-05-28 09:28:17 +0200

    unused constant
    Change-Id: I6bd021857656b912e7c6f5687d331c67b35ace50
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185948
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- include/editeng/AccessibleEditableTextPara.hxx
+++ include/editeng/AccessibleEditableTextPara.hxx
@@ -87,8 +87,6 @@ public:
     virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleParent() override;
     virtual sal_Int64 SAL_CALL getAccessibleIndexInParent() override;
     virtual sal_Int16 SAL_CALL getAccessibleRole() override;
-    /// Maximal length of text returned by getAccessibleDescription()
-    enum { MaxDescriptionLen = 40 };
     virtual OUString SAL_CALL getAccessibleDescription() override;
     virtual OUString SAL_CALL getAccessibleName() override;
     virtual css::uno::Reference< css::accessibility::XAccessibleRelationSet > SAL_CALL getAccessibleRelationSet() override;
