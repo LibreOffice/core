905342d5100f801f95a6d560197d6d1c32452c81
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-05-30 20:06:17 +0100

    store default TableAutoFormatTable in SwModule
    throw this away if there is a TableAutoFormatTable save so the
    next use is updated to the current saved table.
    
    Change-Id: I647dc73495f5865f5c1b151f02f9913c23ccefc5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186054
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sw/inc/swmodule.hxx
+++ sw/inc/swmodule.hxx
@@ -71,6 +71,7 @@ namespace com::sun::star::linguistic2 { class XLanguageGuessing; }
 namespace com::sun::star::linguistic2 { class XLinguServiceEventListener; }
 namespace ooo::vba { class XSinkCaller; }
 class SwLinguServiceEventListener;
+class SwTableAutoFormatTable;
 
 class SAL_DLLPUBLIC_RTTI SwModule final : public SfxModule, public SfxListener, public utl::ConfigurationListener
 {
@@ -93,6 +94,8 @@ class SAL_DLLPUBLIC_RTTI SwModule final : public SfxModule, public SfxListener, 
     std::unique_ptr<SvtUserOptions>      m_pUserOptions;
 
     std::unique_ptr<SfxErrorHandler> m_pErrorHandler;
+
+    std::unique_ptr<SwTableAutoFormatTable> m_xTableAutoFormatTable;
 
     rtl::Reference<SwAttrPool> m_pAttrPool;
 
@@ -251,6 +254,9 @@ public:
 
     SW_DLLPUBLIC void RegisterAutomationApplicationEventsCaller(css::uno::Reference< ooo::vba::XSinkCaller > const& xCaller);
     void CallAutomationApplicationEventSinks(const OUString& Method, css::uno::Sequence< css::uno::Any >& Arguments);
+
+    const SwTableAutoFormatTable& GetAutoFormatTable();
+    void InvalidateAutoFormatTable();;
 
 //    Access to SwModule
     static auto get() { return static_cast<SwModule*>(SfxApplication::GetModule(SfxToolsModule::Writer)); }
