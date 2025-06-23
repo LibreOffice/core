LibreOffice/core

commit 81994cb2b8b32453a92bcb011830fcb884f22ff3

Convert internal vcl bitmap formats transparency->alpha (II)
(Second attempt at landing this)

Image formats and graphics APIs use alpha, not transparency,
so change our internal formats and data structures to work directly
with alpha, so we don't need to modify data before we push it to
graphics APIs.

Add a couple of new Color constants to make the intention
of the vcl code clearer.

Notes
(*) On macOS, tweaking the logic in CreateWithSalBitmapAndMask
to more accurately reflect the requirements of the
CGImageCreateWithMask function seems to fix some
tests.

(*) The vcl code does not properly support gradients
with transparency. So the previous code was wrong, and this
change is going to result in slightly different wrongness.

Change-Id: I9e21c2e98d88ecfdc5f75db13bd1ffff7c38db98
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114168
Tested-by: Jenkins
Reviewed-by: Patrick Luby <plubius@neooffice.org>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/ipsd/ipsd.cxx
@@ -727,7 +727,7 @@ bool PSDReader::ImplReadBody()
                     m_rPSD.ReadUChar( nDummy );
                 for ( sal_uInt16 i = 0; i < ( -nRunCount + 1 ); i++ )
                 {
-                    mpBitmap->SetAlpha(nY, nX, nDat ? 0 : 255);
+                    mpBitmap->SetAlpha(nY, nX, nDat ? 255 : 0);
                     if ( ++nX == mpFileHeader->nColumns )
                     {
                         nX = 0;
@@ -748,7 +748,7 @@ bool PSDReader::ImplReadBody()
                         nDat = 1;
                     if ( mpFileHeader->nDepth == 16 )   // 16 bit depth is to be skipped
                         m_rPSD.ReadUChar( nDummy );
-                    mpBitmap->SetAlpha(nY, nX, nDat ? 0 : 255);
+                    mpBitmap->SetAlpha(nY, nX, nDat ? 255 : 0);
                     if ( ++nX == mpFileHeader->nColumns )
                     {
                         nX = 0;

commit 33560caa34a6532fb27641c19d7cc3bb4dc33057

tdf#167152 PSD-Filews created by Photoshop will not be visible
regression from
    commit 81994cb2b8b32453a92bcb011830fcb884f22ff3
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Fri Apr 16 20:33:10 2021 +0200
    Convert internal vcl bitmap formats transparency->alpha (II)

Change-Id: I995e5edef0e16a75b3ea702259753a480d825133
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186854
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/ipsd/ipsd.cxx
@@ -727,7 +727,7 @@ bool PSDReader::ImplReadBody()
                     m_rPSD.ReadUChar( nDummy );
                 for ( sal_uInt16 i = 0; i < ( -nRunCount + 1 ); i++ )
                 {
-                    mpBitmap->SetAlpha(nY, nX, nDat ? 255 : 0);
+                    mpBitmap->SetAlpha(nY, nX, nDat ? 0 : 255);
                     if ( ++nX == mpFileHeader->nColumns )
                     {
                         nX = 0;
@@ -748,7 +748,7 @@ bool PSDReader::ImplReadBody()
                         nDat = 1;
                     if ( mpFileHeader->nDepth == 16 )   // 16 bit depth is to be skipped
                         m_rPSD.ReadUChar( nDummy );
-                    mpBitmap->SetAlpha(nY, nX, nDat ? 255 : 0);
+                    mpBitmap->SetAlpha(nY, nX, nDat ? 0 : 255);
                     if ( ++nX == mpFileHeader->nColumns )
                     {
                         nX = 0;