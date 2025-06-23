LibreOffice/core

commit 25f007984f91393dbc34adbd2bf59d8b4bae490a

tdf#166932 use BitmapEx in preview style cache instead of VirtualDevice
which reduces the number of open GDI handles we need

Change-Id: I9620d120b9ec88d972908eb713560e414384dec7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186333
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
(cherry picked from commit 8899ae01feb9b9088372d76a6d0562397174c89c)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186352
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

starmath/source/ElementsDockingWindow.cxx
@@ -594,7 +594,8 @@ void SmElementsControl::addElement(const OUString& aElementVisual, const OUStrin
 
     maItemDatas.push_back(std::make_unique<ElementData>(aElementSource, aHelpText, maItemDatas.size()));
     const OUString aId(weld::toId(maItemDatas.back().get()));
-    mpIconView->insert(-1, nullptr, &aId, pDevice, nullptr);
+    BitmapEx aBitmap = pDevice->GetBitmapEx(Point(0,0), pDevice->GetOutputSizePixel());
+    mpIconView->insert(-1, nullptr, &aId, &aBitmap, nullptr);
     mpIconView->set_item_accessible_name(mpIconView->n_children() - 1, GetElementHelpText(aId));
     if (mpIconView->get_item_width() < aSize.Width())
         mpIconView->set_item_width(aSize.Width());

include/svx/fontworkgallery.hxx
@@ -53,7 +53,7 @@ class SAL_WARN_UNUSED SVXCORE_DLLPUBLIC FontWorkGalleryDialog final : public wel
     rtl::Reference<SdrObject> mxSdrObject;
     SdrModel*           mpDestModel;
 
-    std::vector<VclPtr< VirtualDevice >> maFavoritesHorizontal;
+    std::vector<BitmapEx> maFavoritesHorizontal;
     // mapping between item ID and item title
     std::map<OUString, OUString> maIdToTitleMap;
 

include/vcl/weld.hxx
@@ -60,6 +60,7 @@ class TransferDataContainer;
 class OutputDevice;
 class VirtualDevice;
 struct SystemEnvData;
+class BitmapEx;
 
 namespace vcl
 {
@@ -1461,8 +1462,8 @@ public:
                         const OUString* pIconName, TreeIter* pRet)
         = 0;
 
-    virtual void insert(int pos, const OUString* pStr, const OUString* pId,
-                        const VirtualDevice* pIcon, TreeIter* pRet)
+    virtual void insert(int pos, const OUString* pStr, const OUString* pId, const BitmapEx* pIcon,
+                        TreeIter* pRet)
         = 0;
 
     virtual void insert_separator(int pos, const OUString* pId) = 0;
@@ -1472,7 +1473,7 @@ public:
         insert(-1, &rStr, &rId, &rImage, nullptr);
     }
 
-    void append(const OUString& rId, const OUString& rStr, const VirtualDevice* pImage)
+    void append(const OUString& rId, const OUString& rStr, const BitmapEx* pImage)
     {
         insert(-1, &rStr, &rId, pImage, nullptr);
     }

sd/source/ui/sidebar/MasterPagesSelector.cxx
@@ -391,16 +391,18 @@ void MasterPagesSelector::SetItem (
 
         if (aPreview.GetSizePixel().Width() > 0)
         {
-            VclPtr<VirtualDevice> pVDev = GetVirtualDevice(aPreview);
             if (!mxPreviewIconView->get_id(nIndex).isEmpty())
             {
+                VclPtr<VirtualDevice> pVDev = GetVirtualDevice(aPreview);
                 mxPreviewIconView->set_image(nIndex, *pVDev);
                 mxPreviewIconView->set_id(nIndex, OUString::number(aToken));
+                pVDev.disposeAndClear();
             }
             else
             {
+                BitmapEx aPreviewBitmap = GetPreviewAsBitmap(aPreview);
                 OUString sId = OUString::number(aToken);
-                mxPreviewIconView->insert(nIndex, nullptr, &sId, pVDev, nullptr);
+                mxPreviewIconView->insert(nIndex, nullptr, &sId, &aPreviewBitmap, nullptr);
                 mxPreviewIconView->set_item_accessible_name(
                     nIndex, mpContainer->GetPageNameForToken(aToken));
             }
@@ -479,6 +481,16 @@ VclPtr<VirtualDevice> MasterPagesSelector::GetVirtualDevice(const Image& rImage)
     pVDev->DrawBitmapEx(aNull, aPreviewBitmap);
 
     return pVDev;
+}
+
+BitmapEx MasterPagesSelector::GetPreviewAsBitmap(const Image& rImage)
+{
+    BitmapEx aPreviewBitmap = rImage.GetBitmapEx();
+    ScopedVclPtr<VirtualDevice> pVDev = VclPtr<VirtualDevice>::Create();
+    if (pVDev->GetDPIScaleFactor() > 1)
+        aPreviewBitmap.Scale(pVDev->GetDPIScaleFactor(), pVDev->GetDPIScaleFactor());
+
+    return aPreviewBitmap;
 }
 
 void MasterPagesSelector::UpdateAllPreviews()

sd/source/ui/sidebar/MasterPagesSelector.hxx
@@ -171,6 +171,7 @@ private:
         MasterPageContainer::Token aToken);
 
     static VclPtr<VirtualDevice> GetVirtualDevice(const Image& rPreview);
+    static BitmapEx GetPreviewAsBitmap(const Image& rPreview);
 };
 
 } // end of namespace sd::sidebar

svx/source/inc/StylesPreviewWindow.hxx
@@ -123,7 +123,7 @@ public:
 
     void Select(const OUString& rStyleName);
     void RequestStylesListUpdate();
-    static VclPtr<VirtualDevice> GetCachedPreview(const std::pair<OUString, OUString>& rStyle);
+    static BitmapEx GetCachedPreview(const std::pair<OUString, OUString>& rStyle);
     static OString GetCachedPreviewJson(const std::pair<OUString, OUString>& rStyle);
 
 private:

svx/source/tbxctrls/StylesPreviewWindow.cxx
@@ -80,20 +80,17 @@ private:
         virtual void Invoke() override { StylePreviewCache::gJsonStylePreviewCache.clear(); }
     };
 
-    static std::map<OUString, VclPtr<VirtualDevice>> gStylePreviewCache;
+    static std::map<OUString, BitmapEx> gStylePreviewCache;
     static std::map<OUString, OString> gJsonStylePreviewCache;
     static int gStylePreviewCacheClients;
     static JsonStylePreviewCacheClear gJsonIdleClear;
 
 public:
-    static std::map<OUString, VclPtr<VirtualDevice>>& Get() { return gStylePreviewCache; }
+    static std::map<OUString, BitmapEx>& Get() { return gStylePreviewCache; }
     static std::map<OUString, OString>& GetJson() { return gJsonStylePreviewCache; }
 
     static void ClearCache(bool bHard)
     {
-        for (auto& aPreview : gStylePreviewCache)
-            aPreview.second.disposeAndClear();
-
         gStylePreviewCache.clear();
         if (bHard)
         {
@@ -122,7 +119,7 @@ public:
     }
 };
 
-std::map<OUString, VclPtr<VirtualDevice>> StylePreviewCache::gStylePreviewCache;
+std::map<OUString, BitmapEx> StylePreviewCache::gStylePreviewCache;
 std::map<OUString, OString> StylePreviewCache::gJsonStylePreviewCache;
 int StylePreviewCache::gStylePreviewCacheClients;
 StylePreviewCache::JsonStylePreviewCacheClear StylePreviewCache::gJsonIdleClear;
@@ -580,23 +577,23 @@ IMPL_LINK(StylesPreviewWindow_Base, GetPreviewImage, const weld::encoded_image_q
     return true;
 }
 
-VclPtr<VirtualDevice>
-StylesPreviewWindow_Base::GetCachedPreview(const std::pair<OUString, OUString>& rStyle)
+BitmapEx StylesPreviewWindow_Base::GetCachedPreview(const std::pair<OUString, OUString>& rStyle)
 {
     auto aFound = StylePreviewCache::Get().find(rStyle.second);
     if (aFound != StylePreviewCache::Get().end())
         return StylePreviewCache::Get()[rStyle.second];
     else
     {
-        VclPtr<VirtualDevice> pImg = VclPtr<VirtualDevice>::Create();
+        ScopedVclPtrInstance<VirtualDevice> pImg;
         const Size aSize(100, 30);
         pImg->SetOutputSizePixel(aSize);
 
         StyleItemController aStyleController(rStyle);
         aStyleController.Paint(*pImg);
-        StylePreviewCache::Get()[rStyle.second] = pImg;
+        BitmapEx aBitmap = pImg->GetBitmapEx(Point(0, 0), aSize);
+        StylePreviewCache::Get()[rStyle.second] = aBitmap;
 
-        return pImg;
+        return aBitmap;
     }
 }
 
@@ -606,8 +603,7 @@ OString StylesPreviewWindow_Base::GetCachedPreviewJson(const std::pair<OUString,
     if (aJsonFound != StylePreviewCache::GetJson().end())
         return StylePreviewCache::GetJson()[rStyle.second];
 
-    VclPtr<VirtualDevice> xDev = GetCachedPreview(rStyle);
-    BitmapEx aBitmap(xDev->GetBitmapEx(Point(0, 0), xDev->GetOutputSize()));
+    BitmapEx aBitmap = GetCachedPreview(rStyle);
     OString sResult = extractPngString(aBitmap);
     StylePreviewCache::GetJson()[rStyle.second] = sResult;
     return sResult;
@@ -645,8 +641,13 @@ void StylesPreviewWindow_Base::UpdateStylesList()
     const bool bNeedInsertPreview = !comphelper::LibreOfficeKit::isActive();
     for (const auto& rStyle : m_aAllStyles)
     {
-        VclPtr<VirtualDevice> pImg = bNeedInsertPreview ? GetCachedPreview(rStyle) : nullptr;
-        m_xStylesView->append(rStyle.first, rStyle.second, pImg);
+        if (bNeedInsertPreview)
+        {
+            BitmapEx aPreview = GetCachedPreview(rStyle);
+            m_xStylesView->append(rStyle.first, rStyle.second, &aPreview);
+        }
+        else
+            m_xStylesView->append(rStyle.first, rStyle.second, nullptr);
     }
     m_xStylesView->thaw();
 }

svx/source/tbxctrls/fontworkgallery.cxx
@@ -100,7 +100,7 @@ void FontWorkGalleryDialog::initFavorites(sal_uInt16 nThemeId)
 
         if (GalleryExplorer::GetSdrObj(nThemeId, nModelPos, pModel, &aThumb) && !aThumb.IsEmpty())
         {
-            VclPtr< VirtualDevice > pVDev = VclPtr<VirtualDevice>::Create();
+            ScopedVclPtrInstance< VirtualDevice > pVDev;
             const Point aNull(0, 0);
 
             if (pVDev->GetDPIScaleFactor() > 1)
@@ -117,7 +117,7 @@ void FontWorkGalleryDialog::initFavorites(sal_uInt16 nThemeId)
             pVDev->DrawCheckered(aNull, aSize, nLen, aW, aG);
 
             pVDev->DrawBitmapEx(aNull, aThumb);
-            maFavoritesHorizontal.emplace_back(pVDev);
+            maFavoritesHorizontal.emplace_back(pVDev->GetBitmapEx(Point(0,0), aSize));
         }
     }
 
@@ -142,7 +142,7 @@ void FontWorkGalleryDialog::fillFavorites(sal_uInt16 nThemeId)
     {
         OUString sId = OUString::number(static_cast<sal_uInt16>(nFavorite));
         maIdToTitleMap.emplace(sId, aTitles.at(nFavorite - 1));
-        maCtlFavorites->insert(-1, nullptr, &sId, maFavoritesHorizontal[nFavorite - 1], nullptr);
+        maCtlFavorites->insert(-1, nullptr, &sId, &maFavoritesHorizontal[nFavorite - 1], nullptr);
         maCtlFavorites->set_item_accessible_name(maCtlFavorites->n_children() - 1, aTitles.at(nFavorite -1));
     }
 

commit 2a6f4a2f4f5b1b61fb859b33fd889ad55fe4eee8

tdf#167112 MATH: Elements sidebar is empty
regression from
  commit 25f007984f91393dbc34adbd2bf59d8b4bae490a
  Author: Noel Grandin <noelgrandin@gmail.com>
  Date:   Tue Jun 10 18:20:18 2025 +0200
  tdf#166932 use BitmapEx in preview style cache instead of VirtualDevice

Change-Id: I385c3428e97b68e3ce7429ffaf87ddfc81a547b0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186853
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

starmath/source/ElementsDockingWindow.cxx
@@ -594,7 +594,7 @@ void SmElementsControl::addElement(const OUString& aElementVisual, const OUStrin
 
     maItemDatas.push_back(std::make_unique<ElementData>(aElementSource, aHelpText, maItemDatas.size()));
     const OUString aId(weld::toId(maItemDatas.back().get()));
-    BitmapEx aBitmap = pDevice->GetBitmapEx(Point(0,0), pDevice->GetOutputSizePixel());
+    BitmapEx aBitmap = pDevice->GetBitmapEx(Point(0,0), pDevice->GetOutputSize());
     mpIconView->insert(-1, nullptr, &aId, &aBitmap, nullptr);
     mpIconView->set_item_accessible_name(mpIconView->n_children() - 1, GetElementHelpText(aId));
     if (mpIconView->get_item_width() < aSize.Width())