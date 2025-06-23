e1c3bbf462c08cc12ee9fc5d01fe184491f1669c
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-06-23 10:47:52 +0100

    hold SolarMutex when calling dropCaches
    libreoffice/instdir/program/libvcllo.so(+0x18726de) [0x7ec11a6726de]
    libreoffice/instdir/program/libtllo.so(_Z17DbgTestSolarMutexv+0x110) [0x7ec12172b5d0]
    libreoffice/instdir/program/libsvllo.so(_ZN14SfxBroadcaster14RemoveListenerER11SfxListener+0x39) [0x7ec11eb51139]
    libreoffice/instdir/program/libsvllo.so(_ZN11SfxListener12EndListeningER14SfxBroadcasterb+0x153) [0x7ec11eb75803]
    libreoffice/instdir/program/../program/libchartcorelo.so(_ZN5chart10ChartModelD2Ev+0x58) [0x7ec0f0181498]
    libreoffice/instdir/program/../program/libchartcorelo.so(_ZN5chart10ChartModelD0Ev+0x19) [0x7ec0f01817f9]
    libreoffice/instdir/program/libuno_cppuhelpergcc3.so.3(_ZN4cppu11OWeakObject7releaseEv+0x45) [0x7ec1216301b5]
    libreoffice/instdir/program/../program/libchartcorelo.so(+0x344da5) [0x7ec0eff44da5]
    libreoffice/instdir/program/../program/libswlo.so(+0x1bce0fa) [0x7ec0fe7ce0fa]
    libreoffice/instdir/program/../program/libswlo.so(_ZN8SwOLEObj12UnloadObjectERKN3com3sun4star3uno9ReferenceINS2_5embed15XEmbeddedObjectEEEPK5SwDocl+0x33d) [0x7ec0fe7cabdd]
    libreoffice/instdir/program/../program/libswlo.so(_ZN8SwOLEObj12UnloadObjectEv+0x6b) [0x7ec0fe7ca88b]
    libreoffice/instdir/program/../program/libswlo.so(+0x1bccffe) [0x7ec0fe7ccffe]
    libreoffice/instdir/program/../program/libswlo.so(+0x1bcceb7) [0x7ec0fe7cceb7]
    libreoffice/instdir/program/libvcllo.so(+0x195a089) [0x7ec11a75a089]
    libreoffice/instdir/program/libvcllo.so(_ZN3vcl3lok10trimMemoryEi+0x2f) [0x7ec11a74e10f]
    libreoffice/instdir/program/libsofficeapp.so(+0x2048c7) [0x7ec1230048c7]
    
    Change-Id: I674d047f815f80af2d8f11acf0181d6c62bfcf72
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186819
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3359,12 +3359,12 @@ static int joinThreads(JoinThreads eCategory);
 
 static void lo_trimMemory(LibreOfficeKit* /* pThis */, int nTarget)
 {
+    SolarMutexGuard aGuard;
+
     vcl::lok::trimMemory(nTarget);
 
     if (nTarget > 2000)
     {
-        SolarMutexGuard aGuard;
-
         // Flush all buffered VOC primitives from the pages.
         SfxViewShell* pViewShell = SfxViewShell::Current();
         if (pViewShell)
