LibreOffice/core

commit 1f61a2ec4e252d2eeccabd65a10650c8ee231a1b

tdf#166715 speed up transparent fill operation
Transparent bitmapex drawing works fine these days, so we dont
need this fallback path anymore.

Dramatically speeds up rendering on most backends (the cairo drawinglayer renderer already has an optimised path here).

Change-Id: Ic66a4f2d76a6815d1cb3a439e416f1235be7a544
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185795
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -553,13 +553,6 @@ void VclProcessor2D::RenderFillGraphicPrimitive2D(
         return;
     }
 
-    if (rFillBitmapCandidate.hasTransparency())
-    {
-        // cannot handle yet, use decomposition
-        process(rFillBitmapCandidate);
-        return;
-    }
-
     bool bPrimitiveAccepted = RenderFillGraphicPrimitive2DImpl(rFillBitmapCandidate);
 
     if (!bPrimitiveAccepted)

commit 9c8368955b8f512d75268e7ed89e692fa9d1c930

tdf#167135 Transparency setting page style area for image background
regression from
    commit 1f61a2ec4e252d2eeccabd65a10650c8ee231a1b
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Mon May 26 13:26:19 2025 +0200
    tdf#166715 speed up transparent fill operation

where I mistook "hasTransparency" to mean "does the bitmap have an alpha layer", instead of "there is an __additional__ alpha value applied to this bitmap"

Change-Id: If89d95103fc13e7ce78e7056d0a53aa40d48cde2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187034
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -553,6 +553,13 @@ void VclProcessor2D::RenderFillGraphicPrimitive2D(
         return;
     }
 
+    if (rFillBitmapCandidate.hasTransparency())
+    {
+        // cannot handle yet, use decomposition
+        process(rFillBitmapCandidate);
+        return;
+    }
+
     bool bPrimitiveAccepted = RenderFillGraphicPrimitive2DImpl(rFillBitmapCandidate);
 
     if (!bPrimitiveAccepted)