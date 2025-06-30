LibreOffice/core

commit 529755f0919217a84a12daad0fddfddd1124f0e9

tdf#166113 sw smart justify: adjust algorithm for interoperability
Skip space shrinking, if space expansion is closer (with a
weight) to the desired (100%) word space.

Import DOCX documents with 75% minimum word spacing,
100% desired word spacing and 133% maximum word spacing.

These changes cover MSO smart justify algorithm. Hyphenation and
text portions may need future adjustments.

Modify unit tests according to the changes:

– testTdf126154, testTdf126154_minimum_shrinking,
  testTdf126154_portion: choose maximum word spacing, i.e.
  expansion instead of shrinking, according to the weighted
  distance from the desired word space;

– testTdf163720: set desired word spacing to the minimum
  word spacing in the test document to get back the original
  regression test.

Note: the previous algorithm for disabled hyphenation within
the minimum and maximum word spacing range is replaced
by optimization for the desired word spacing (temporarily).
Interoperability analysis and custom more/less hyphenation
can change this.

Change-Id: Iec0582a497945743f97686743af5ff6609866abe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/186138
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/portxt.cxx
@@ -367,19 +367,19 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
     const SvxAdjust aAdjust = aAdjustItem.GetAdjust();
     bool bFullJustified = bFull && aAdjust == SvxAdjust::Block &&
          pGuess->BreakPos() != TextFrameIndex(COMPLETE_STRING);
-    bool bInteropSmartJustify = bFullJustified &&
+    bool bInteropSmartJustify =
             rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
-                    DocumentSettingId::JUSTIFY_LINES_WITH_SHRINKING) &&
-                    // support different Kashida etc. values
-                    aAdjustItem.GetPropWordSpacing() == 100 &&
+                    DocumentSettingId::JUSTIFY_LINES_WITH_SHRINKING);
+    bool bNoWordSpacing = aAdjustItem.GetPropWordSpacing() == 100 &&
                     aAdjustItem.GetPropWordSpacingMinimum() == 100 &&
                     aAdjustItem.GetPropWordSpacingMaximum() == 100;
-    bool bWordSpacing = bFullJustified && !bInteropSmartJustify &&
-           aAdjustItem.GetPropWordSpacing() != 100;
-    bool bWordSpacingMaximum = bFullJustified && !bInteropSmartJustify &&
+    // support old ODT documents, where only JustifyLinesWithShrinking was set
+    bool bOldInterop = bInteropSmartJustify && bNoWordSpacing;
+    bool bWordSpacing = bFullJustified && (!bNoWordSpacing || bOldInterop);
+    bool bWordSpacingMaximum = bWordSpacing && !bOldInterop &&
            aAdjustItem.GetPropWordSpacingMaximum() > aAdjustItem.GetPropWordSpacing();
-    bool bWordSpacingMinimum = bFullJustified && !bInteropSmartJustify &&
-           aAdjustItem.GetPropWordSpacingMinimum() < aAdjustItem.GetPropWordSpacing();
+    bool bWordSpacingMinimum = bWordSpacing && ( bOldInterop ||
+           aAdjustItem.GetPropWordSpacingMinimum() < aAdjustItem.GetPropWordSpacing() );
 
     if ( ( bInteropSmartJustify || bWordSpacing || bWordSpacingMaximum || bWordSpacingMinimum ) &&
          // tdf#164499 no shrinking in tabulated line
@@ -388,13 +388,9 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
          // very short word resulted endless loop
          !rInf.IsUnderflow() )
     {
-        sal_Int32 nSpacesInLine(0);
-        for (sal_Int32 i = sal_Int32(rInf.GetLineStart()); i < sal_Int32(pGuess->BreakPos()); ++i)
-        {
-            sal_Unicode cChar = rInf.GetText()[i];
-            if ( cChar == CH_BLANK )
-                ++nSpacesInLine;
-        }
+        sal_Int32 nSpacesInLine = rInf.GetLineSpaceCount( pGuess->BreakPos() );
+        sal_Int32 nSpacesInLineOrig = nSpacesInLine;
+        SwTextSizeInfo aOrigInf( rInf );
 
         // call with an extra space: shrinking can result a new word in the line
         // and a new space before that, which is also a shrank space
@@ -410,70 +406,111 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
             ++nSpacesInLine;
         }
 
-        if ( nSpacesInLine > 0 )
+        // there are spaces in the line, so it's possible to shrink them
+        if ( nSpacesInLineOrig > 0 )
         {
             SwTwips nOldWidth = pGuess->BreakWidth();
-            if ( bInteropSmartJustify )
+            bool bIsPortion = rInf.GetLineWidth() < rInf.GetBreakWidth();
+
+            // measure ten spaces for higher precision
+            static constexpr OUStringLiteral STR_BLANK = u"          ";
+            sal_Int16 nSpaceWidth = rInf.GetTextSize(STR_BLANK).Width();
+            sal_Int32 nRealSpaces = rInf.GetLineSpaceCount( pGuess->BreakPos() );
+            float fSpaceNormal = (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nRealSpaces * nSpaceWidth/10.0))/nRealSpaces;
+
+            bool bOrigHyphenated = pGuess->HyphWord().is() &&
+                        pGuess->BreakPos() > rInf.GetLineStart();
+            // calculate line breaking with desired word spacing, also
+            // if the desired word spacing is 100%, but there is a greater
+            // maximum word spacing, and the word is hyphenated at the desired
+            // word spacing: to skip hyphenation, if the maximum word spacing allows it
+            if ( bWordSpacing || ( bWordSpacingMaximum && bOrigHyphenated ) )
             {
                 pGuess.emplace();
-                bFull = !pGuess->Guess( *this, rInf, Height(), nSpacesInLine, SAL_MAX_UINT16 );
+                bFull = !pGuess->Guess( *this, rInf, Height(), nSpacesInLine, aAdjustItem.GetPropWordSpacing(), nSpaceWidth );
+                sal_Int32 nSpacesInLine2 = rInf.GetLineSpaceCount( pGuess->BreakPos() );
+
+                if ( rInf.GetBreakWidth() <= rInf.GetLineWidth() )
+                    fSpaceNormal = (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nSpacesInLine2 * nSpaceWidth/10.0))/nSpacesInLine2;
             }
-            else
+
+            sal_Int32 nSpacesInLineShrink = 0;
+            // TODO if both maximum word spacing or minimum word spacing can disable hyphenation, prefer the last one
+            if ( bWordSpacingMinimum )
             {
-                bool bOrigHyphenated = pGuess->HyphWord().is() &&
-                        pGuess->BreakPos() > rInf.GetLineStart();
-                if ( bWordSpacing || bWordSpacingMaximum )
+                std::optional<SwTextGuess> pGuess2(std::in_place);
+                SwTwips nOldExtraSpace = rInf.GetExtraSpace();
+                // break the line after the hyphenated word, if it's possible
+                // (hyphenation is disabled in Guess(), when called with GetPropWordSpacingMinimum())
+                sal_uInt16 nMinimum = bOldInterop ? 75 : aAdjustItem.GetPropWordSpacingMinimum();
+                bool bFull2 = !pGuess2->Guess( *this, rInf, Height(), nSpacesInLine, nMinimum, nSpaceWidth );
+                nSpacesInLineShrink = rInf.GetLineSpaceCount( pGuess2->BreakPos() );
+                if ( pGuess2->BreakWidth() > nOldWidth )
                 {
-                    pGuess.emplace();
-                    bFull = !pGuess->Guess( *this, rInf, Height(), nSpacesInLine, aAdjustItem.GetPropWordSpacing() );
-                }
-                // if both maximum word spacing or minimum word spacing can disable
-                // hyphenation, prefer the last one
-                if ( bWordSpacingMinimum && ( bWordSpacingMaximum ||
-                        ( pGuess->HyphWord().is() && pGuess->BreakPos() > rInf.GetLineStart() ) ) &&
-                        // if the desired word spacing is 100% (!bWordSpacing), and it was possible
-                        // to break the line without hyphenation in the first run (where maximum
-                        // word spacing was not used), no need to check minimum word spacing
-                        // FIXME: avoid too much shrinking, if desired word spacing is not 100%
-                        ( bWordSpacing || bOrigHyphenated )
-                   )
-                {
-                    std::optional<SwTextGuess> pGuess2(std::in_place);
-                    SwTwips nOldExtraSpace = rInf.GetExtraSpace();
-                    // break the line after the hyphenated word, if it's possible
-                    // (hyphenation is disabled in Guess(), when called with GetPropWordSpacingMinimum())
-                    bool bFull2 = !pGuess2->Guess( *this, rInf, Height(), nSpacesInLine, aAdjustItem.GetPropWordSpacingMinimum() );
-                    if ( pGuess2->BreakWidth() > nOldWidth )
+                    // instead of the maximum shrinking, break after the word which was hyphenated before
+                    sal_Int32 i = sal_Int32(pGuess->BreakPos());
+                    sal_Int32 j = sal_Int32(pGuess2->BreakPos());
+                    // skip terminal spaces
+                    for (; i < j && rInf.GetText()[i] == CH_BLANK; ++i);
+                    for (; j > i && rInf.GetText()[i] == CH_BLANK; --j);
+                    sal_Int32 nOldBreakTrim = i;
+                    sal_Int32 nOldBreak = j - i;
+                    for (; i < j; ++i)
                     {
-                        // instead of the maximum shrinking, break after the word which was hyphenated before
-                        for (sal_Int32 i = sal_Int32(pGuess->BreakPos()); i < sal_Int32(pGuess2->BreakPos()); ++i)
+                        sal_Unicode cChar = rInf.GetText()[i];
+                        // first space after the hyphenated word, and it's not the chosen one
+                        if ( cChar == CH_BLANK )
                         {
-                            sal_Unicode cChar = rInf.GetText()[i];
-                            // first space after the hyphenated word, and it's not the chosen one
-                            if ( cChar == CH_BLANK )
+                            // using a weighted word spacing, try to break the line after the hyphenated word
+                            sal_Int32 nNewBreak = i - nOldBreakTrim;
+                            SwTwips nWeightedSpacing = nMinimum * (1.0 * nNewBreak/nOldBreak) +
+                                   aAdjustItem.GetPropWordSpacing() * (1.0 * (nOldBreak - nNewBreak)/nOldBreak);
+                            std::optional<SwTextGuess> pGuess3(std::in_place);
+                            pGuess3->Guess( *this, rInf, Height(), nSpacesInLineShrink-1, nWeightedSpacing, nSpaceWidth );
+
+                            sal_Int32 nSpacesInLineShrink2 = rInf.GetLineSpaceCount( pGuess3->BreakPos() );
+                            if ( nSpacesInLineShrink2 == nSpacesInLineShrink )
                             {
-                                // using a weighted word spacing, try to break the line after the hyphenated word
-                                sal_Int32 nOldBreak = sal_Int32(pGuess2->BreakPos()) - sal_Int32(pGuess->BreakPos());
-                                sal_Int32 nNewBreak = i - sal_Int32(pGuess->BreakPos());
-                                SwTwips nWeightedSpacing = aAdjustItem.GetPropWordSpacingMinimum() * (1.0 * nNewBreak/nOldBreak) +
-                                        aAdjustItem.GetPropWordSpacing() * (1.0 * (nOldBreak - nNewBreak)/nOldBreak);
-                                std::optional<SwTextGuess> pGuess3(std::in_place);
-                                pGuess3->Guess( *this, rInf, Height(), nSpacesInLine, nWeightedSpacing );
-                                if ( pGuess3->BreakWidth() > nOldWidth )
-                                {
-                                    pGuess2.emplace();
-                                    pGuess2 = std::move(pGuess3);
-                                }
-                                break;
+                                nNewBreak = i - nOldBreakTrim - 1;
+                                nWeightedSpacing = nMinimum * (1.0 * nNewBreak/nOldBreak) +
+                                    aAdjustItem.GetPropWordSpacing() * (1.0 * (nOldBreak - nNewBreak)/nOldBreak);
+                                pGuess3->Guess( *this, rInf, Height(), nSpacesInLineShrink-1, nWeightedSpacing, nSpaceWidth );
                             }
+
+                            if ( pGuess3->BreakWidth() > nOldWidth )
+                            {
+                                pGuess2.emplace();
+                                pGuess2 = std::move(pGuess3);
+                            }
+                            break;
                         }
+                    }
+
+                    nSpacesInLineShrink = rInf.GetLineSpaceCount( pGuess2->BreakPos() );
+                    if ( rInf.GetBreakWidth() > rInf.GetLineWidth() || bIsPortion )
+                    {
+                        float fExpansionWeight = static_cast<float>(1/1.7);
+                        float fSpaceShrunk = nSpacesInLineShrink > 0
+                                ? (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nSpacesInLineShrink * nSpaceWidth/10.0))/nSpacesInLineShrink
+                                : 0;
+                        float z0 = (nSpaceWidth/10.0)/fSpaceShrunk;
+                        float z1 = (nSpaceWidth/10.0+((fSpaceNormal-nSpaceWidth/10.0)*fExpansionWeight))/(nSpaceWidth/10.0);
+                        // TODO shrink line portions only if needed
+                        if ( z1 >= z0 || bIsPortion )
+                        {
+                            pGuess = std::move(pGuess2);
+                            bFull = bFull2;
+                        }
+                    }
+                    else if ( bOldInterop )
+                    {
                         pGuess = std::move(pGuess2);
                         bFull = bFull2;
                     }
-                    else
-                        // minimum word spacing is not applicable
-                        rInf.SetExtraSpace(nOldExtraSpace);
                 }
+                else
+                    // minimum word spacing is not applicable
+                    rInf.SetExtraSpace(nOldExtraSpace);
             }
 
             if ( pGuess->BreakWidth() != nOldWidth )

sw/qa/extras/layout/data/tdf163720.fodt
@@ -178,7 +178,7 @@
    <style:paragraph-properties fo:text-align="start" style:justify-single-word="false" text:number-lines="false" text:line-number="0" style:writing-mode="lr-tb"/>
   </style:style>
   <style:style style:name="P2" style:family="paragraph" style:parent-style-name="Standard">
-   <style:paragraph-properties fo:text-align="justify" style:justify-single-word="false" fo:hyphenation-ladder-count="no-limit" fo:hyphenation-keep="auto" loext:hyphenation-keep-type="column" style:writing-mode="lr-tb"/>
+   <style:paragraph-properties fo:text-align="justify" style:justify-single-word="false" fo:hyphenation-ladder-count="no-limit" fo:hyphenation-keep="auto" loext:hyphenation-keep-type="column" style:writing-mode="lr-tb" loext:word-spacing-minimum="75%" loext:word-spacing="75%"/>
    <style:text-properties fo:hyphenate="true" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false" loext:hyphenation-no-last-word="false" loext:hyphenation-word-char-count="5" loext:hyphenation-zone="567" loext:hyphenation-compound-remain-char-count="2"/>
   </style:style>
   <style:page-layout style:name="pm1">

commit c05bc38067b2ab61ae919fb1fef1381343d03844

tdf#167298 tdf#166113 sw word spacing: add word spacing indicator
tdf#167298: As an advanced feature for typography and related
development – including bug fixing –, add visualization for
justified text lines with too big word spacing.

tdf#166113: fix bInteropSmartJustify by adding bFullJustified
to avoid unnecessary extra calculation for the last paragraph line.
(Regression since commit 529755f0919217a84a12daad0fddfddd1124f0e9
"tdf#166113 sw smart justify: adjust algorithm for interoperability",
detected by the word spacing indicator).

The red word spacing indicator line is positioned before
the justified text line, and its width is the extra space
of a space character over the maximum word spacing.

For example, if the font-defined space width is 3 pt,
the maximum word spacing is 150%, spaces expanded to 6 pt in
the justified line results a 6 pt - 3 pt * 1.5 = 1.5 pt width
word spacing indicator.

To enable the word spacing indicator in a justified text,

1) Open "Tools" -> Options" -> "Advanced" -> "Open Expert Configuration";

2) search for "ShowWordSpacingIndicator";

3) double-click on the config entry (or press Enter) to set it to True;

4) Enable Formatting Marks (Control-F10).

Follow-up to commit 5a48070f5904c51dc9e7bbad4213d802fd4bc89b
"tdf#126154 sw offapi xmloff cui: add min/max word spacing"
and commit 7d6696757dcdfa3cee481ac7795a91b2b47da363
"tdf#159923 sw cui offapi xmloff: add custom word spacing".

Change-Id: I315b5e6b8239f4fdc8e69eb45fc292b8d5891138
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187199
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

officecfg/registry/schema/org/openoffice/Office/Writer.xcs
@@ -867,6 +867,13 @@
           </info>
           <value>false</value>
         </prop>
+        <prop oor:name="ShowWordSpacingIndicator" oor:type="xs:boolean" oor:nillable="false">
+          <info>
+            <desc>Enables the word spacing indicator, which shows before each justified text line how much the expanded space in the current line exceeds the maximum word spacing set for the paragraph.</desc>
+            <label>Show word spacing indicator at justified lines.</label>
+          </info>
+          <value>false</value>
+        </prop>
         <prop oor:name="ShowWarningHiddenSection" oor:type="xs:boolean" oor:nillable="false">
           <!-- UIHints: Not accessible via user interface -->
           <info>

sw/source/core/layout/frmtool.cxx
@@ -80,6 +80,7 @@
 #include <frameformats.hxx>
 #include <boost/circular_buffer.hpp>
 #include <svx/sdr/attribute/sdrallfillattributeshelper.hxx>
+#include <officecfg/Office/Writer.hxx>
 
 using namespace ::com::sun::star;
 
@@ -3984,6 +3985,9 @@ bool IsExtraData( const SwDoc& rDoc )
     {
         return true;
     }
+
+    if ( officecfg::Office::Writer::Content::Display::ShowWordSpacingIndicator::get() )
+        return true;
 
     const SwEditShell* pSh = rDoc.GetEditShell();
     const SwViewOption* pViewOptions = pSh ? pSh->GetViewOptions() : nullptr;

sw/source/core/text/frmpaint.cxx
@@ -43,6 +43,8 @@
 #include <tabfrm.hxx>
 #include <numrule.hxx>
 #include <wrong.hxx>
+#include <vcl/lineinfo.hxx>
+#include <officecfg/Office/Writer.hxx>
 
 #include <EnhancedPDFExportHelper.hxx>
 
@@ -96,7 +98,7 @@ public:
     }
 
     void PaintExtra( SwTwips nY, tools::Long nAsc, tools::Long nMax, bool bRed, const OUString* pRedlineText = nullptr );
-    void PaintRedline( SwTwips nY, tools::Long nMax );
+    void PaintRedline( SwTwips nY, tools::Long nMax, sal_Int16 nWordSpacing = 0 );
 };
 
 }
@@ -279,7 +281,8 @@ void SwExtraPainter::PaintExtra( SwTwips nY, tools::Long nAsc, tools::Long nMax,
     }
 }
 
-void SwExtraPainter::PaintRedline( SwTwips nY, tools::Long nMax )
+// paint redline or word spacing indicator
+void SwExtraPainter::PaintRedline( SwTwips nY, tools::Long nMax, sal_Int16 nWordSpacing )
 {
     Point aStart( m_nRedX, nY );
     Point aEnd( m_nRedX, nY + nMax );
@@ -295,15 +298,18 @@ void SwExtraPainter::PaintRedline( SwTwips nY, tools::Long nMax )
         }
     }
     const Color aOldCol( m_pSh->GetOut()->GetLineColor() );
-    m_pSh->GetOut()->SetLineColor(SwModule::get()->GetRedlineMarkColor());
+    m_pSh->GetOut()->SetLineColor(nWordSpacing ? COL_LIGHTRED : SwModule::get()->GetRedlineMarkColor());
 
-    if ( m_pTextFrame->IsVertical() )
+    if ( nWordSpacing )
     {
-        m_pTextFrame->SwitchHorizontalToVertical( aStart );
-        m_pTextFrame->SwitchHorizontalToVertical( aEnd );
-    }
+        LineInfo aLineInfo;
+        aLineInfo.SetStyle(LineStyle::Solid);
+        aLineInfo.SetWidth( nWordSpacing * 2540/1440 );
 
-    m_pSh->GetOut()->DrawLine( aStart, aEnd );
+        m_pSh->GetOut()->DrawLine( aStart, aEnd, aLineInfo );
+    }
+    else
+        m_pSh->GetOut()->DrawLine( aStart, aEnd );
     m_pSh->GetOut()->SetLineColor( aOldCol );
 }
 
@@ -321,19 +327,22 @@ void SwTextFrame::PaintExtraData( const SwRect &rRect ) const
     bool bLineNum = !IsInTab() && rLineInf.IsPaintLineNumbers() &&
                ( !IsInFly() || rLineInf.IsCountInFlys() ) && rLineNum.IsCount();
     sal_Int16 eHor = static_cast<sal_Int16>(SwModule::get()->GetRedlineMarkPos());
+    SwViewShell *pSh = getRootFrame()->GetCurrShell();
+    bool bWordSpacingIndicator = officecfg::Office::Writer::Content::Display::ShowWordSpacingIndicator::get()
+        && pSh->GetViewOptions()->IsViewMetaChars();
     if (eHor != text::HoriOrientation::NONE
+        && !bWordSpacingIndicator
         && (!IDocumentRedlineAccess::IsShowChanges(rIDRA.GetRedlineFlags())
             || getRootFrame()->IsHideRedlines()))
     {
         eHor = text::HoriOrientation::NONE;
     }
     bool bRedLine = eHor != text::HoriOrientation::NONE;
-    if ( !bLineNum && !bRedLine )
+    if ( !bLineNum && !bRedLine && !bWordSpacingIndicator )
         return;
 
     if( IsLocked() || IsHiddenNow() || !getFramePrintArea().Height() )
         return;
-    SwViewShell *pSh = getRootFrame()->GetCurrShell();
 
     SwSwapIfNotSwapped swap(const_cast<SwTextFrame *>(this));
     SwRect rOldRect( rRect );
@@ -383,6 +392,10 @@ void SwTextFrame::PaintExtraData( const SwRect &rRect ) const
         {
             if( bNoDummy || !aLine.GetCurr()->IsDummy() )
             {
+                SwTwips nExtraSpaceSize = aLine.GetCurr()->GetFirstPortion()->ExtraSpaceSize();
+                if ( nExtraSpaceSize && bWordSpacingIndicator )
+                    aExtra.PaintRedline( aLine.Y(), aLine.GetLineHeight(), nExtraSpaceSize );
+
                 bool bRed = bRedLine && aLine.GetCurr()->HasRedline();
                 if( rLineInf.IsCountBlankLines() || aLine.GetCurr()->HasContent() )
                 {

sw/source/core/text/porlin.hxx
@@ -51,6 +51,7 @@ private:
     bool m_bIsFieldmarkText;
     SwTwips m_nExtraBlankWidth = 0;    // width of spaces after the break
     SwTwips m_nExtraShrunkWidth = 0;   // width of not shrunk line
+    SwTwips m_nExtraSpaceSize = 0;     // extra space over normal space width
 
     std::optional<SwLinePortionLayoutContext> m_nLayoutContext;
 
@@ -77,6 +78,8 @@ public:
     void ExtraBlankWidth(const SwTwips nNew) { m_nExtraBlankWidth = nNew; }
     SwTwips ExtraShrunkWidth() const { return m_nExtraShrunkWidth; }
     void ExtraShrunkWidth(const SwTwips nNew) { m_nExtraShrunkWidth = nNew; }
+    SwTwips ExtraSpaceSize() const { return m_nExtraSpaceSize; }
+    void ExtraSpaceSize(const SwTwips nNew) { m_nExtraSpaceSize = nNew; }
     SwTwips GetHangingBaseline() const { return mnHangingBaseline; }
     void SetHangingBaseline( const SwTwips nNewBaseline ) { mnHangingBaseline = nNewBaseline; }
     const std::optional<SwLinePortionLayoutContext> & GetLayoutContext() const { return m_nLayoutContext; }
@@ -193,6 +196,7 @@ inline SwLinePortion &SwLinePortion::operator=(const SwLinePortion &rPortion)
     m_bJoinBorderWithNext = rPortion.m_bJoinBorderWithNext;
     m_nExtraBlankWidth = rPortion.m_nExtraBlankWidth;
     m_nExtraShrunkWidth = rPortion.m_nExtraShrunkWidth;
+    m_nExtraSpaceSize = rPortion.m_nExtraSpaceSize;
     m_nLayoutContext = rPortion.m_nLayoutContext;
     return *this;
 }
@@ -209,6 +213,7 @@ inline SwLinePortion::SwLinePortion(const SwLinePortion &rPortion) :
     m_bIsFieldmarkText( rPortion.m_bIsFieldmarkText ),
     m_nExtraBlankWidth(rPortion.m_nExtraBlankWidth),
     m_nExtraShrunkWidth(rPortion.m_nExtraShrunkWidth),
+    m_nExtraSpaceSize(rPortion.m_nExtraSpaceSize),
     m_nLayoutContext(rPortion.m_nLayoutContext)
 {
 }

sw/source/core/text/portxt.cxx
@@ -304,6 +304,7 @@ void SwTextPortion::BreakCut( SwTextFormatInfo &rInf, const SwTextGuess &rGuess 
         SetLen( TextFrameIndex(0) );
         Width( 0 );
         ExtraShrunkWidth( 0 );
+        ExtraSpaceSize( 0 );
     }
 }
 
@@ -313,6 +314,7 @@ void SwTextPortion::BreakUnderflow( SwTextFormatInfo &rInf )
     Height( 0 );
     Width( 0 );
     ExtraShrunkWidth( 0 );
+    ExtraSpaceSize( 0 );
     SetLen( TextFrameIndex(0) );
     SetAscent( 0 );
     rInf.SetUnderflow( this );
@@ -358,6 +360,7 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
     }
 
     ExtraShrunkWidth( 0 );
+    ExtraSpaceSize( 0 );
     std::optional<SwTextGuess> pGuess(std::in_place);
     bool bFull = !pGuess->Guess( *this, rInf, Height() );
 
@@ -367,7 +370,7 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
     const SvxAdjust aAdjust = aAdjustItem.GetAdjust();
     bool bFullJustified = bFull && aAdjust == SvxAdjust::Block &&
          pGuess->BreakPos() != TextFrameIndex(COMPLETE_STRING);
-    bool bInteropSmartJustify =
+    bool bInteropSmartJustify = bFullJustified &&
             rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
                     DocumentSettingId::JUSTIFY_LINES_WITH_SHRINKING);
     bool bNoWordSpacing = aAdjustItem.GetPropWordSpacing() == 100 &&
@@ -418,6 +421,9 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
             sal_Int32 nRealSpaces = rInf.GetLineSpaceCount( pGuess->BreakPos() );
             float fSpaceNormal = (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nRealSpaces * nSpaceWidth/10.0))/nRealSpaces;
 
+            float fExpansionOverMax = fSpaceNormal - nSpaceWidth/10.0 * aAdjustItem.GetPropWordSpacingMaximum()/100.0;
+            ExtraSpaceSize( rInf.GetBreakWidth() > rInf.GetLineWidth()/2 && fExpansionOverMax > 0 ? fExpansionOverMax : 0);
+
             bool bOrigHyphenated = pGuess->HyphWord().is() &&
                         pGuess->BreakPos() > rInf.GetLineStart();
             // calculate line breaking with desired word spacing, also
@@ -431,7 +437,11 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                 sal_Int32 nSpacesInLine2 = rInf.GetLineSpaceCount( pGuess->BreakPos() );
 
                 if ( rInf.GetBreakWidth() <= rInf.GetLineWidth() )
+                {
                     fSpaceNormal = (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nSpacesInLine2 * nSpaceWidth/10.0))/nSpacesInLine2;
+                    fExpansionOverMax = fSpaceNormal - nSpaceWidth/10.0 * aAdjustItem.GetPropWordSpacingMaximum()/100.0;
+                    ExtraSpaceSize( rInf.GetBreakWidth() > rInf.GetLineWidth()/2 && fExpansionOverMax > 0 ? fExpansionOverMax : 0);
+                }
             }
 
             sal_Int32 nSpacesInLineShrink = 0;
@@ -499,12 +509,14 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                         if ( z1 >= z0 || bIsPortion )
                         {
                             pGuess = std::move(pGuess2);
+                            ExtraSpaceSize(0);
                             bFull = bFull2;
                         }
                     }
                     else if ( bOldInterop )
                     {
                         pGuess = std::move(pGuess2);
+                        ExtraSpaceSize(0);
                         bFull = bFull2;
                     }
                 }
@@ -514,7 +526,10 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
             }
 
             if ( pGuess->BreakWidth() != nOldWidth )
+            {
                 ExtraShrunkWidth( pGuess->BreakWidth() );
+                ExtraSpaceSize( 0 );
+            }
         }
     }
 
@@ -692,6 +707,7 @@ bool SwTextPortion::Format( SwTextFormatInfo &rInf )
         Height( 0 );
         Width( 0 );
         ExtraShrunkWidth( 0 );
+        ExtraSpaceSize( 0 );
         SetLen( TextFrameIndex(0) );
         SetAscent( 0 );
         SetNextPortion( nullptr );  // ????