c71c712857d14747fc700594d6f6878b7bd6231b
Noel Grandin <noel.grandin@collabora.co.uk>
2025-06-30 08:33:01 +0200

    crashtesting forum-mso-en4-307539.xlsx
    regression from
        commit 79322a7025bb6fd61928204f90230560b09b4ba9
        Author: Noel Grandin <noel.grandin@collabora.co.uk>
        Date:   Wed May 21 15:28:41 2025 +0200
        tdf#166684 strip out some unnecessary UNO work
    
    Change-Id: Ifab9b928f6c4dd660e9bf3812a8f6e4c2cd73b6c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187160
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Jenkins
    (cherry picked from commit 8ebd114b91678d3cc4f1582fbe2ba4f7d479993e)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187163
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/source/filter/oox/worksheethelper.cxx
+++ sc/source/filter/oox/worksheethelper.cxx
@@ -1073,6 +1073,8 @@ void WorksheetGlobals::finalizeValidationRanges() const
     for (auto const& validation : maValidations)
     {
         rtl::Reference<ScCellRangesObj> xRanges = getCellRangeList(validation.maRanges);
+        if (!xRanges)
+            continue;
         rtl::Reference<ScTableValidationObj> xValidation = xRanges->getValidation();
 
         if( xValidation.is() )
