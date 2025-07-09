e2be650d97765945538614463b9269acb164947c
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2025-07-09 10:13:55 +0200

    tdf#167409 set alt. text to the internal hyperlinks in a TOC
    The alt. text that is set is the TOC entry text.
    
    Change-Id: I8a9012cd7a6155f0672e0e8019ec1183d01cdcb6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187556
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Jenkins

--- sw/qa/core/test_ToxLinkProcessor.cxx
+++ sw/qa/core/test_ToxLinkProcessor.cxx
@@ -56,11 +56,11 @@ ToxLinkProcessorTest::NoExceptionIsThrownIfTooManyLinksAreClosed()
 {
     ToxLinkProcessor sut;
     sut.StartNewLink(0, UIName(STYLE_NAME_1));
-    sut.CloseLink(1, URL_1, /*bRelative=*/true);
+    sut.CloseLink(1, URL_1, OUString(), /*bRelative=*/true);
     // fdo#85872 actually it turns out the UI does something like this
     // so an exception must not be thrown!
     // should not succeed either (for backward compatibility)
-    sut.CloseLink(2, URL_1, /*bRelative=*/true);
+    sut.CloseLink(2, URL_1, OUString(), /*bRelative=*/true);
     CPPUNIT_ASSERT_EQUAL(1u, static_cast<unsigned>(sut.m_ClosedLinks.size()));
     CPPUNIT_ASSERT_EQUAL(1u, static_cast<unsigned>(sut.m_ClosedLinks.at(0)->mEndTextPos));
     CPPUNIT_ASSERT_MESSAGE("no links are open", !sut.m_oStartedLink);
@@ -72,10 +72,10 @@ ToxLinkProcessorTest::AddingAndClosingTwoOverlappingLinksResultsInOneClosedLink(
     ToxLinkProcessor sut;
     sut.StartNewLink(0, UIName(STYLE_NAME_1));
     sut.StartNewLink(0, UIName(STYLE_NAME_2));
-    sut.CloseLink(1, URL_1, /*bRelative=*/true);
+    sut.CloseLink(1, URL_1, OUString(), /*bRelative=*/true);
     // this should not cause an error, and should not succeed either
     // (for backward compatibility)
-    sut.CloseLink(1, URL_2, /*bRelative=*/true);
+    sut.CloseLink(1, URL_2, OUString(), /*bRelative=*/true);
     CPPUNIT_ASSERT_EQUAL(1u, static_cast<unsigned>(sut.m_ClosedLinks.size()));
     CPPUNIT_ASSERT_MESSAGE("no links are open", !sut.m_oStartedLink);
     // backward compatibility: the last start is closed by the first end
@@ -108,7 +108,7 @@ ToxLinkProcessorTest::LinkIsCreatedCorrectly()
     ToxLinkProcessorWithOverriddenObtainPoolId sut;
 
     sut.StartNewLink(0, UIName(STYLE_NAME_1));
-    sut.CloseLink(1, URL_1, /*bRelative=*/true);
+    sut.CloseLink(1, URL_1, OUString(), /*bRelative=*/true);
 
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Style is stored correctly in link", STYLE_NAME_1, sut.m_ClosedLinks.at(0)->mINetFormat.GetVisitedFormat().toString());
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Url is stored correctly in link", URL_1, sut.m_ClosedLinks.at(0)->mINetFormat.GetValue());
@@ -122,9 +122,9 @@ ToxLinkProcessorTest::LinkSequenceIsPreserved()
     ToxLinkProcessorWithOverriddenObtainPoolId sut;
 
     sut.StartNewLink(0, UIName(STYLE_NAME_2));
-    sut.CloseLink(1, URL_2, /*bRelative=*/true);
+    sut.CloseLink(1, URL_2, OUString(), /*bRelative=*/true);
     sut.StartNewLink(1, UIName(STYLE_NAME_1));
-    sut.CloseLink(2, URL_1, /*bRelative=*/true);
+    sut.CloseLink(2, URL_1, OUString(), /*bRelative=*/true);
 
     // check first closed element
     CPPUNIT_ASSERT_EQUAL_MESSAGE("Style is stored correctly in link",
