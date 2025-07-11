b9b12234a4c635457ce83a3f8eec90edc7cb2667
Mohit Marathe <mohit.marathe@collabora.com>
2025-07-11 12:46:22 +0530

    sc: Add TogglePrintGrid uno command
    This command toggles the state of ATTR_PAGE_GRID in page style
    which is responsible for setting grid lines for print.
    This allows adding a toolbar button for this functionality instead
    of going through "Format Page" dialog->"Sheet" tab->"Grid"
    
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I69cca87f7bb0fbe10bcc1119bd12eb5e5597725d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187682
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Pranam Lashkari <lpranam@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200308

--- sc/source/ui/view/viewdata.cxx
+++ sc/source/ui/view/viewdata.cxx
@@ -17,6 +17,7 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <stlpool.hxx>
 #include <scitems.hxx>
 #include <editeng/eeitem.hxx>
 #include <framework/windowstatehelper.hxx>
@@ -1124,6 +1125,50 @@ void ScViewData::SetShowGrid( bool bShow )
 {
     CreateSelectedTabData();
     maTabData[GetTabNumber()]->bShowGrid = bShow;
+}
+
+bool ScViewData::GetPrintGrid() const
+{
+    bool bPrintGrid;
+    ScStyleSheetPool* pStylePool = mrDoc.GetStyleSheetPool();
+    SfxStyleSheetBase* pStyleSheet = pStylePool->Find( mrDoc.GetPageStyle( GetTabNumber() ), SfxStyleFamily::Page );
+    if (pStyleSheet)
+    {
+        SfxItemSet& rSet = pStyleSheet->GetItemSet();
+        bPrintGrid  = rSet.Get(ATTR_PAGE_GRID).GetValue();
+    }
+    else
+    {
+        const ScViewOptions& rOpt = mrDoc.GetViewOptions();
+        bPrintGrid  = rOpt.GetOption(sc::ViewOption::GRID);
+    }
+    return bPrintGrid;
+}
+
+void ScViewData::SetPrintGrid( bool bPrintGrid )
+{
+    ScStyleSheetPool* pStylePool = mrDoc.GetStyleSheetPool();
+    SfxStyleSheetBase* pStyleSheet = pStylePool->Find( mrDoc.GetPageStyle( GetTabNumber() ), SfxStyleFamily::Page );
+    if (pStyleSheet)
+    {
+        SfxItemSet& rSet = pStyleSheet->GetItemSet();
+        SfxItemState eState = rSet.GetItemState(ATTR_PAGE_GRID, true);
+        bool bOld = rSet.Get(ATTR_PAGE_GRID).GetValue();
+
+        if (bOld == bPrintGrid && eState == SfxItemState::DEFAULT)
+            rSet.ClearItem(ATTR_PAGE_GRID);
+        else
+            rSet.Put(SfxBoolItem(ATTR_PAGE_GRID, bPrintGrid));
+
+        mrDoc.PageStyleModified(GetTabNumber(), pStyleSheet->GetName());
+    }
+    else
+    {
+        // If no style sheet exists, modify the view options directly
+        ScViewOptions aNewOptions = mrDoc.GetViewOptions();
+        aNewOptions.SetOption(sc::ViewOption::GRID, bPrintGrid);
+        mrDoc.SetViewOptions(aNewOptions);
+    }
 }
 
 void ScViewData::RefreshZoom()
