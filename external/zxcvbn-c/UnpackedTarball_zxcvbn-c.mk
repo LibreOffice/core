8f7055adc0036df47891243d8b8c7548f03e56fd
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-07-12 21:26:59 +0100

    const up zxcvbn-c
    Change-Id: Idbcff3cba17233a9d9746650cdda5129c0f4790b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187788
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 645ede66b260bb5a95e229d3a7f4f24c6b9f18d7)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188073
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- external/zxcvbn-c/UnpackedTarball_zxcvbn-c.mk
+++ external/zxcvbn-c/UnpackedTarball_zxcvbn-c.mk
@@ -13,9 +13,12 @@ $(eval $(call gb_UnpackedTarball_set_tarball,zxcvbn-c,$(ZXCVBN_C_TARBALL)))
 
 # * external/zxcvbn-c/0001-There-is-no-std-basic_string-int.patch.1 sent upstream as
 #   <https://github.com/tsyrogit/zxcvbn-c/pull/31> "There is no std::basic_string<int>":
+# * external/zxcvbn-c/0001-use-const-for-these-arrays.patch.1 sent upstream as
+#   <https://github.com/tsyrogit/zxcvbn-c/pull/37> "use const for these arrays":
 $(eval $(call gb_UnpackedTarball_add_patches,zxcvbn-c,\
 	external/zxcvbn-c/zxcvbn-c-2.5-do-not-use-stdafx.patch \
 	external/zxcvbn-c/0001-There-is-no-std-basic_string-int.patch.1 \
+	external/zxcvbn-c/0001-use-const-for-these-arrays.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
