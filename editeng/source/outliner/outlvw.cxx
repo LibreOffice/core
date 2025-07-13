58360eaf43c4557ac850f7ad90e488b706163b9d
Andrea Gelmini <andrea.gelmini@gelma.net>
2025-07-13 19:30:27 +0200

    Fix typo
    Change-Id: I5f4f2fb1191b70cc7ab72b4808e84ea19c7b6a5c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/187815
    Tested-by: Julien Nabet <serval2412@yahoo.fr>
    Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

--- editeng/source/outliner/outlvw.cxx
+++ editeng/source/outliner/outlvw.cxx
@@ -69,7 +69,7 @@ void OutlinerView::DrawText_ToEditView( const tools::Rectangle& rRect, OutputDev
 
     // use TextHierarchyBreakupOutliner to get all text embedded to the
     // TextHierarchy.*Primitive2D groupings for better processing, plus
-    // the correct paragtaph countings
+    // the correct paragraph countings
     const drawinglayer::geometry::ViewInformation2D aViewInformation2D;
     TextHierarchyBreakupOutliner aHelper(aViewInformation2D, rOwner);
 
