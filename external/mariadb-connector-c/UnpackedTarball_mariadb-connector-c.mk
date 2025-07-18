94d2add57c96ab6f38ef2e53bd14549218cc86fb
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-07-18 22:00:00 +0100

    const up mariadb more
    Change-Id: Idea6798d1df00e7cc1e9fa9a6c520bd1db3ee6c7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188470
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
+++ external/mariadb-connector-c/UnpackedTarball_mariadb-connector-c.mk
@@ -24,13 +24,16 @@ endif
 endif # $(OS),WNT
 
 $(eval $(call gb_UnpackedTarball_set_patchlevel,mariadb-connector-c,1))
-# 0001-const-up-my_uca1400_collation_definitions.patch upstreamed as:
+# 0001-const-up-my_uca1400_collation_definitions.patch and
+# 0001-const-up-mariadb_defaults-and-MADB_OS_CHARSET.patch
+# upstreaming attempt at:
 # https://github.com/mariadb-corporation/mariadb-connector-c/pull/290
 
 $(eval $(call gb_UnpackedTarball_add_patches,mariadb-connector-c,\
     external/mariadb-connector-c/clang-cl.patch.0 \
     external/mariadb-connector-c/c23.patch.0 \
     external/mariadb-connector-c/0001-const-up-my_uca1400_collation_definitions.patch \
+    external/mariadb-connector-c/0001-const-up-mariadb_defaults-and-MADB_OS_CHARSET.patch \
 ))
 
 # TODO are any "plugins" needed?
