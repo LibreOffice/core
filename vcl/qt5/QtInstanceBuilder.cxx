dad98d3959e511c549d9192205c5928358808856
Michael Weghorn <m.weghorn@posteo.de>
2025-07-18 18:20:33 +0200

    tdf#130857 qt weld: Support Calc "Go to Sheet" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    To trigger the dialog:
    
    * start Calc
    * insert a few more sheets
    * "Sheet" -> "Navigate" -> "Go to Sheet"
    
    Change-Id: I8fe3383f5857d25ee08db84be681ecb39dea4d3c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188049
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -106,6 +106,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"filter/ui/xsltfilterdialog.ui"_ustr,
         u"modules/BasicIDE/ui/gotolinedialog.ui"_ustr,
         u"modules/BasicIDE/ui/newlibdialog.ui"_ustr,
+        u"modules/scalc/ui/gotosheetdialog.ui"_ustr,
         u"modules/scalc/ui/groupbydate.ui"_ustr,
         u"modules/scalc/ui/inputstringdialog.ui"_ustr,
         u"modules/scalc/ui/insertcells.ui"_ustr,
