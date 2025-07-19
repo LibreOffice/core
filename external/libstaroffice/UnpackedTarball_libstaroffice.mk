fe0dca387a668c7b1e73bc0c253fc0fcb6f8a051
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-07-19 14:57:56 +0100

    const up libstaroffice
    Change-Id: I04a24f373cb24a2061acdd323b4da279ef7c9c07
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188186
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins

--- external/libstaroffice/UnpackedTarball_libstaroffice.mk
+++ external/libstaroffice/UnpackedTarball_libstaroffice.mk
@@ -15,6 +15,14 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libstaroffice,0))
 
 $(eval $(call gb_UnpackedTarball_update_autoconf_configs,libstaroffice))
 
+# 0001-const-up-these-arrays.patch.1
+# upstream attempt as:
+# https://github.com/fosnola/libstaroffice/pull/12
+
+$(eval $(call gb_UnpackedTarball_add_patches,libstaroffice, \
+    external/libstaroffice/0001-const-up-these-arrays.patch.1 \
+))
+
 ifneq ($(OS),MACOSX)
 ifneq ($(OS),WNT)
 $(eval $(call gb_UnpackedTarball_add_patches,libstaroffice, \
