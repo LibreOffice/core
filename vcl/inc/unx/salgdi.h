LibreOffice/core

commit 088a7c7c451321a800ca8d3523a18b6bb93239b7

remove alpha device from OutputDevice
and render to a combined color+alpha bitmap

The strategy is to remove the mpAlphaVDev from OutputDevice,
and have the OutputDevice create 32-bit SalBitmap's
and then have the various backends correctly render to
those 32-bit bitmaps.

However, whenever we extract a BitmapEx from OutputDevice
(e.g. via VirtualDevice) and hand it to the rest of the
system, we split the color and alpha information.
So the rest of the codebase doesn't have to deal with
this yet.

We have some major problems to overcome here:
(*) Cairo uses 32-bit data, even if we ask for 24-bit color with no alpha.
    This is because the current version of cairo does not support RGB 24-bit data.
(*) Sometimes we want the bitmap to be initialised with an opaque white background,
    sometimes we want it to be transparent.
(*) Sometimes when we extract a bitmap from a device, we want the alpha channel,
    sometimes not.
(*) There are lots of left-over bits of previous attempts of this change lying around,
    e.g. in skia.

Notes
(*) we allow Bitmap (ie. SalBitmap) to hold color+alpha
(*) The BitmapEx(Bitmap) constructor checks if the incoming
Bitmap is color+alpha, and if so, splits into a color bitmap and an alpha bitmap
(*) Because the underlying image data is stored pre-multipled, if
we write a transparent pixel with color, and then read the pixel value back,
we might lose the color information.

Change-Id: I0fc0042d6e5be5edd99b320892fb00a8eb2842fe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173937
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/unx/generic/gdi/salvd.cxx
@@ -36,8 +36,9 @@
 
 std::unique_ptr<SalVirtualDevice> X11SalInstance::CreateX11VirtualDevice(const SalGraphics& rGraphics,
         tools::Long nDX, tools::Long nDY, DeviceFormat eFormat,
-        std::unique_ptr<X11SalGraphics> pNewGraphics)
+        std::unique_ptr<X11SalGraphics> pNewGraphics, bool bAlphaMaskTransparent)
 {
+    assert(!bAlphaMaskTransparent && "TODO"); (void)bAlphaMaskTransparent;
     assert(pNewGraphics);
 #if HAVE_FEATURE_SKIA
     if (SkiaHelper::isVCLSkiaEnabled())
@@ -61,9 +62,9 @@ std::unique_ptr<SalVirtualDevice> X11SalInstance::CreateX11VirtualDevice(const S
 }
 
 std::unique_ptr<SalVirtualDevice> X11SalInstance::CreateVirtualDevice(SalGraphics& rGraphics,
-        tools::Long nDX, tools::Long nDY, DeviceFormat eFormat)
+        tools::Long nDX, tools::Long nDY, DeviceFormat eFormat, bool bAlphaMaskTransparent)
 {
-    return CreateX11VirtualDevice(rGraphics, nDX, nDY, eFormat, std::make_unique<X11SalGraphics>());
+    return CreateX11VirtualDevice(rGraphics, nDX, nDY, eFormat, std::make_unique<X11SalGraphics>(), bAlphaMaskTransparent);
 }
 
 std::unique_ptr<SalVirtualDevice> X11SalInstance::CreateVirtualDevice(SalGraphics& rGraphics,
@@ -229,8 +230,9 @@ SalGraphics* X11SalVirtualDevice::AcquireGraphics()
 void X11SalVirtualDevice::ReleaseGraphics( SalGraphics* )
 { bGraphics_ = false; }
 
-bool X11SalVirtualDevice::SetSize( tools::Long nDX, tools::Long nDY )
+bool X11SalVirtualDevice::SetSize( tools::Long nDX, tools::Long nDY, bool bAlphaMaskTransparent )
 {
+    assert(!bAlphaMaskTransparent && "TODO"); (void)bAlphaMaskTransparent;
     if( bExternPixmap_ )
         return false;
 

vcl/inc/quartz/salgdi.h
@@ -146,13 +146,13 @@ struct AquaSharedAttributes
     bool checkContext();
     void setState();
 
-    bool isPenVisible() const
+    bool isPenActive() const
     {
-        return maLineColor.IsVisible();
+        return maLineColor.IsActive();
     }
-    bool isBrushVisible() const
+    bool isBrushActive() const
     {
-        return maFillColor.IsVisible();
+        return maFillColor.IsActive();
     }
 
     void refreshRect(float lX, float lY, float lWidth, float lHeight)
@@ -331,7 +331,7 @@ public:
                   Color nMaskColor) override;
 
     std::shared_ptr<SalBitmap> getBitmap(tools::Long nX, tools::Long nY, tools::Long nWidth,
-                                         tools::Long nHeight) override;
+                                         tools::Long nHeight, bool bWithoutAlpha) override;
 
     Color getPixel(tools::Long nX, tools::Long nY) override;
 

vcl/inc/win/salgdi.h
@@ -262,7 +262,7 @@ protected:
                                   const SalBitmap& rSalBitmap,
                                   Color nMaskColor ) override;
 
-    virtual std::shared_ptr<SalBitmap> getBitmap( tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight ) override;
+    virtual std::shared_ptr<SalBitmap> getBitmap( tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight, bool bWithoutAlpha ) override;
     virtual Color       getPixel( tools::Long nX, tools::Long nY ) override;
 
     // invert --> ClipRegion (only Windows or VirDevs)

vcl/quartz/salvd.cxx
@@ -39,19 +39,20 @@
 
 std::unique_ptr<SalVirtualDevice> AquaSalInstance::CreateVirtualDevice( SalGraphics& rGraphics,
                                                         tools::Long nDX, tools::Long nDY,
-                                                        DeviceFormat eFormat )
+                                                        DeviceFormat eFormat,
+                                                        bool bAlphaMaskTransparent)
 {
     // #i92075# can be called first in a thread
     SalData::ensureThreadAutoreleasePool();
 
 #ifdef IOS
     (void)rGraphics;
-    std::unique_ptr<SalVirtualDevice> pNew(new AquaSalVirtualDevice( nullptr, nDX, nDY, eFormat ));
+    std::unique_ptr<SalVirtualDevice> pNew(new AquaSalVirtualDevice( nullptr, nDX, nDY, eFormat, bAlphaMaskTransparent ));
     pNew->SetSize( nDX, nDY );
     return pNew;
 #else
     return std::unique_ptr<SalVirtualDevice>(new AquaSalVirtualDevice( static_cast< AquaSalGraphics* >(&rGraphics),
-                                     nDX, nDY, eFormat ));
+                                     nDX, nDY, eFormat, bAlphaMaskTransparent ));
 #endif
 }
 
@@ -69,7 +70,7 @@ std::unique_ptr<SalVirtualDevice> AquaSalInstance::CreateVirtualDevice( SalGraph
 
 AquaSalVirtualDevice::AquaSalVirtualDevice(
     AquaSalGraphics* pGraphic, tools::Long nDX, tools::Long nDY,
-    DeviceFormat eFormat )
+    DeviceFormat eFormat, bool bAlphaMaskTransparent )
   : mbGraphicsUsed( false )
   , mnBitmapDepth( 0 )
   , mnWidth(0)
@@ -109,7 +110,7 @@ AquaSalVirtualDevice::AquaSalVirtualDevice(
 #endif
     if( nDX && nDY )
     {
-        SetSize( nDX, nDY );
+        SetSize( nDX, nDY, bAlphaMaskTransparent );
     }
     // NOTE: if SetSize does not succeed, we just ignore the nDX and nDY
 }

vcl/skia/x11/salvd.cxx
@@ -78,8 +78,10 @@ SalGraphics* X11SkiaSalVirtualDevice::AcquireGraphics()
 
 void X11SkiaSalVirtualDevice::ReleaseGraphics(SalGraphics*) { mbGraphicsAcquired = false; }
 
-bool X11SkiaSalVirtualDevice::SetSize(tools::Long nDX, tools::Long nDY)
+bool X11SkiaSalVirtualDevice::SetSize(tools::Long nDX, tools::Long nDY, bool bAlphaMaskTransparent)
 {
+    assert(!bAlphaMaskTransparent && "TODO");
+    (void)bAlphaMaskTransparent;
     if (!nDX)
         nDX = 1;
     if (!nDY)

vcl/win/gdi/salvd.cxx
@@ -31,12 +31,13 @@
 #include <sal/log.hxx>
 #include <o3tl/temporary.hxx>
 
-HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, tools::Long nDY, sal_uInt16 nBitCount, void **ppData)
+HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, tools::Long nDY, sal_uInt16 nBitCount, void **ppData, bool bAlphaMaskTransparent)
 {
     HBITMAP hBitmap;
 
     if ( nBitCount == 1 )
     {
+        assert(!bAlphaMaskTransparent && "does not make sense here");
         hBitmap = CreateBitmap( static_cast<int>(nDX), static_cast<int>(nDY), 1, 1, nullptr );
         SAL_WARN_IF( !hBitmap, "vcl", "CreateBitmap failed: " << comphelper::WindowsErrorString( GetLastError() ) );
         ppData = nullptr;
@@ -52,7 +53,7 @@ HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, to
         BITMAPINFO aBitmapInfo;
         aBitmapInfo.bmiHeader.biSize = sizeof( BITMAPINFOHEADER );
         aBitmapInfo.bmiHeader.biWidth = nDX;
-        aBitmapInfo.bmiHeader.biHeight = nDY;
+        aBitmapInfo.bmiHeader.biHeight = -nDY; // negative for top-down bitmap
         aBitmapInfo.bmiHeader.biPlanes = 1;
         aBitmapInfo.bmiHeader.biBitCount = nBitCount;
         aBitmapInfo.bmiHeader.biCompression = BI_RGB;
@@ -62,10 +63,23 @@ HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, to
         aBitmapInfo.bmiHeader.biClrUsed = 0;
         aBitmapInfo.bmiHeader.biClrImportant = 0;
 
+        void* pData = nullptr;
         hBitmap = CreateDIBSection( hDC, &aBitmapInfo,
-                                    DIB_RGB_COLORS, ppData, nullptr,
+                                    DIB_RGB_COLORS, &pData, nullptr,
                                     0 );
         SAL_WARN_IF( !hBitmap, "vcl", "CreateDIBSection failed: " << comphelper::WindowsErrorString( GetLastError() ) );
+
+        if (hBitmap)
+        {
+            if (bAlphaMaskTransparent)
+            {
+                assert(nBitCount == 32);
+                memset(pData, nDX * nDY * 4, 0);
+            }
+
+            if (ppData)
+                *ppData = pData;
+        }
     }
 
     return hBitmap;
@@ -73,7 +87,8 @@ HBITMAP WinSalVirtualDevice::ImplCreateVirDevBitmap(HDC hDC, tools::Long nDX, to
 
 std::unique_ptr<SalVirtualDevice> WinSalInstance::CreateVirtualDevice( SalGraphics& rSGraphics,
                                                        tools::Long nDX, tools::Long nDY,
-                                                       DeviceFormat /*eFormat*/ )
+                                                       DeviceFormat eFormat,
+                                                       bool bAlphaMaskTransparent )
 {
     WinSalGraphics& rGraphics = static_cast<WinSalGraphics&>(rSGraphics);
 
@@ -83,13 +98,14 @@ std::unique_ptr<SalVirtualDevice> WinSalInstance::CreateVirtualDevice( SalGraphi
     if (!hDC)
         return nullptr;
 
-    const sal_uInt16 nBitCount = 0;
+    const sal_uInt16 nBitCount = eFormat == DeviceFormat::WITHOUT_ALPHA ? 0 : 32;
     // #124826# continue even if hBmp could not be created
     // if we would return a failure in this case, the process
     // would terminate which is not required
     HBITMAP hBmp = WinSalVirtualDevice::ImplCreateVirDevBitmap(rGraphics.getHDC(),
                                                            nDX, nDY, nBitCount,
-                                                           &o3tl::temporary<void*>(nullptr));
+                                                           &o3tl::temporary<void*>(nullptr),
+                                                           bAlphaMaskTransparent);
 
     auto pVDev = std::make_unique<WinSalVirtualDevice>(hDC, hBmp, nBitCount,
                                                        /*bForeignDC*/false, nDX, nDY, rGraphics.isScreen());
@@ -195,13 +211,13 @@ void WinSalVirtualDevice::ReleaseGraphics( SalGraphics* )
     mbGraphicsAcquired = false;
 }
 
-bool WinSalVirtualDevice::SetSize( tools::Long nDX, tools::Long nDY )
+bool WinSalVirtualDevice::SetSize( tools::Long nDX, tools::Long nDY, bool bAlphaMaskTransparent )
 {
     if( mbForeignDC || !mhBmp )
         return true;    // ???
 
     HBITMAP hNewBmp = ImplCreateVirDevBitmap(getHDC(), nDX, nDY, mnBitCount,
-                                             &o3tl::temporary<void*>(nullptr));
+                                             &o3tl::temporary<void*>(nullptr), bAlphaMaskTransparent);
     if (!hNewBmp)
     {
         mnWidth = 0;

commit 2951e2ac0e80fc982cd1fe52a849f40e44da65dc

tdf#167630 "assert !bAlphaMaskTransparent" with gen VCL plugin
regression from
    commit 088a7c7c451321a800ca8d3523a18b6bb93239b7
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Tue Sep 24 16:18:11 2024 +0200
    remove alpha device from OutputDevice

Change-Id: I2b38b4d714bca66df0c3bd6a40fcbcb1cddb778c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188140
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

vcl/inc/unx/salgdi.h
@@ -70,7 +70,7 @@ public:
 
     void                            Init(X11SalFrame& rFrame, Drawable aDrawable, SalX11Screen nXScreen);
     void                            Init(X11SalVirtualDevice *pVirtualDevice, SalColormap* pColormap = nullptr,
-                                         bool bDeleteColormap = false);
+                                         bool bDeleteColormap = false, bool bAlphaMaskTransparent = false);
     void                            Init( X11SkiaSalVirtualDevice *pVirtualDevice );
 
     virtual SalGraphicsImpl*        GetImpl() const override;

vcl/unx/generic/gdi/salvd.cxx
@@ -73,7 +73,7 @@ std::unique_ptr<SalVirtualDevice> X11SalInstance::CreateVirtualDevice(SalGraphic
     return CreateX11VirtualDevice(rGraphics, nDX, nDY, eFormat, rData, std::make_unique<X11SalGraphics>());
 }
 
-void X11SalGraphics::Init(X11SalVirtualDevice *pDevice, SalColormap* pColormap, bool bDeleteColormap)
+void X11SalGraphics::Init(X11SalVirtualDevice *pDevice, SalColormap* pColormap, bool bDeleteColormap, bool bAlphaMaskTransparent)
 {
     SalDisplay *pDisplay  = pDevice->GetDisplay();
     m_nXScreen = pDevice->GetXScreenNumber();
@@ -100,6 +100,12 @@ void X11SalGraphics::Init(X11SalVirtualDevice *pDevice, SalColormap* pColormap, 
 
     SetDrawable(pDevice->GetDrawable(), pDevice->GetSurface(), m_nXScreen);
     mxImpl->UpdateX11GeometryProvider();
+
+    if (bAlphaMaskTransparent)
+    {
+        mxImpl->SetFillColor(COL_TRANSPARENT);
+        mxImpl->drawRect(0, 0, pDevice->GetWidth(), pDevice->GetHeight());
+    }
 }
 
 X11SalVirtualDevice::X11SalVirtualDevice(const SalGraphics& rGraphics, tools::Long nDX, tools::Long nDY,
@@ -232,7 +238,6 @@ void X11SalVirtualDevice::ReleaseGraphics( SalGraphics* )
 
 bool X11SalVirtualDevice::SetSize( tools::Long nDX, tools::Long nDY, bool bAlphaMaskTransparent )
 {
-    assert(!bAlphaMaskTransparent && "TODO"); (void)bAlphaMaskTransparent;
     if( bExternPixmap_ )
         return false;
 
@@ -282,7 +287,7 @@ bool X11SalVirtualDevice::SetSize( tools::Long nDX, tools::Long nDY, bool bAlpha
     }
 
     if( pGraphics_ )
-        pGraphics_->Init( this );
+        pGraphics_->Init( this, nullptr, false, bAlphaMaskTransparent );
 
     return true;
 }