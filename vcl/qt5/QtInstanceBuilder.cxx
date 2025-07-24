e5339dc5bd4e608e014daa85d4f676d0d4e6e0c9
Michael Weghorn <m.weghorn@posteo.de>
2025-07-24 06:44:50 +0200

    tdf#130857 qt weld: Support sw "Rename AutoText" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    To trigger the dialog in Writer:
    
    * "Tools" -> "AutoText"
    * select an entry in the treeview
    * "AutoText" -> "Rename"
    
    Change-Id: I102d03ab1a2950155d11d5a9331159f6ac6f2ce5
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188254
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -136,6 +136,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"modules/swriter/ui/inserttable.ui"_ustr,
         u"modules/swriter/ui/linenumbering.ui"_ustr,
         u"modules/swriter/ui/printoptionspage.ui"_ustr,
+        u"modules/swriter/ui/renameautotextdialog.ui"_ustr,
         u"modules/swriter/ui/renameobjectdialog.ui"_ustr,
         u"modules/swriter/ui/rowheight.ui"_ustr,
         u"modules/swriter/ui/splittable.ui"_ustr,
