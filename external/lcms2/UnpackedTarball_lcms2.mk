57bd14c2422a88c0ad11b06b981e729238c61f9c
Noel Grandin <noel.grandin@collabora.co.uk>
2025-07-25 11:25:33 +0200

    constify lcms2 some more
    Change-Id: I969bca29072826db5e6fd26b9d5c945d289e89a7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188324
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 7b35ff08abcda30786d6d1bfaa7cb93484f2eed9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188338
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- external/lcms2/UnpackedTarball_lcms2.mk
+++ external/lcms2/UnpackedTarball_lcms2.mk
@@ -26,6 +26,7 @@ endif
 $(eval $(call gb_UnpackedTarball_add_patches,lcms2,\
 	external/lcms2/lcms2-2.4-windows.patch \
 	external/lcms2/0001-const-up-some-static-arrays.patch.1 \
+	external/lcms2/0002-mark-more-const.patch.1 \
 ))
 
 # vim: set noet sw=4 ts=4:
