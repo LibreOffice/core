808734af9de79089e2da4369b8b07a477ff5b78c
Michael Weghorn <m.weghorn@posteo.de>
2025-07-25 22:39:10 +0200

    svx a11y: clang-format charmapacc.hxx
    This in particular gets rid of an extra level of
    indentation.
    
    Change-Id: Ib1f1bbd70d6fa973e99676a231b3a2b82b40176e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188370
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- svx/source/inc/charmapacc.hxx
+++ svx/source/inc/charmapacc.hxx
@@ -32,172 +32,178 @@ class SvxShowCharSet;
 
 namespace svx
 {
-    class SvxShowCharSetAcc;
+class SvxShowCharSetAcc;
 
-    class SvxShowCharSetItemAcc;
+class SvxShowCharSetItemAcc;
 
-    // - SvxShowCharSetItem -
+// - SvxShowCharSetItem -
 
-    /** Simple struct to hold some information about the single items of the table.
-    */
-    struct SvxShowCharSetItem
+/** Simple struct to hold some information about the single items of the table. */
+struct SvxShowCharSetItem
+{
+    SvxShowCharSet& mrParent;
+    sal_uInt16 mnId;
+    OUString maText;
+    tools::Rectangle maRect;
+    rtl::Reference<SvxShowCharSetItemAcc> m_xItem;
+    SvxShowCharSetAcc* m_pParent;
+
+    SvxShowCharSetItem(SvxShowCharSet& rParent, SvxShowCharSetAcc* _pParent, sal_uInt16 _nPos);
+    ~SvxShowCharSetItem();
+
+    const rtl::Reference<SvxShowCharSetItemAcc>& GetAccessible();
+};
+
+// - SvxShowCharSetAcc -
+
+/** The table implementation of the vcl control. */
+
+class SvxShowCharSetAcc final
+    : public cppu::ImplInheritanceHelper<::comphelper::OAccessibleSelectionHelper,
+                                         css::accessibility::XAccessibleTable>
+{
+    ::std::vector<rtl::Reference<SvxShowCharSetItemAcc>> m_aChildren;
+    SvxShowCharSet* m_pParent; // the vcl control
+    virtual void SAL_CALL disposing() override;
+
+public:
+    SvxShowCharSetAcc(SvxShowCharSet* pParent);
+
+    // XAccessibleComponent
+    virtual void SAL_CALL grabFocus() override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleAtPoint(const css::awt::Point& aPoint) override;
+
+    // OAccessible
+    // XAccessibleContext - still waiting to be overwritten
+    virtual sal_Int64 SAL_CALL getAccessibleChildCount() override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleChild(sal_Int64 i) override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleParent() override;
+    virtual sal_Int16 SAL_CALL getAccessibleRole() override;
+    virtual OUString SAL_CALL getAccessibleDescription() override;
+    virtual OUString SAL_CALL getAccessibleName() override;
+    virtual css::uno::Reference<css::accessibility::XAccessibleRelationSet>
+        SAL_CALL getAccessibleRelationSet() override;
+    virtual sal_Int64 SAL_CALL getAccessibleStateSet() override;
+
+    virtual sal_Int32 SAL_CALL getForeground() override;
+    virtual sal_Int32 SAL_CALL getBackground() override;
+
+    // XAccessibleTable
+    virtual sal_Int32 SAL_CALL getAccessibleRowCount() override;
+    virtual sal_Int32 SAL_CALL getAccessibleColumnCount() override;
+    virtual OUString SAL_CALL getAccessibleRowDescription(sal_Int32 nRow) override;
+    virtual OUString SAL_CALL getAccessibleColumnDescription(sal_Int32 nColumn) override;
+    virtual sal_Int32 SAL_CALL getAccessibleRowExtentAt(sal_Int32 nRow, sal_Int32 nColumn) override;
+    virtual sal_Int32 SAL_CALL getAccessibleColumnExtentAt(sal_Int32 nRow,
+                                                           sal_Int32 nColumn) override;
+    virtual css::uno::Reference<css::accessibility::XAccessibleTable>
+        SAL_CALL getAccessibleRowHeaders() override;
+    virtual css::uno::Reference<css::accessibility::XAccessibleTable>
+        SAL_CALL getAccessibleColumnHeaders() override;
+    virtual css::uno::Sequence<sal_Int32> SAL_CALL getSelectedAccessibleRows() override;
+    virtual css::uno::Sequence<sal_Int32> SAL_CALL getSelectedAccessibleColumns() override;
+    virtual sal_Bool SAL_CALL isAccessibleRowSelected(sal_Int32 nRow) override;
+    virtual sal_Bool SAL_CALL isAccessibleColumnSelected(sal_Int32 nColumn) override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleCellAt(sal_Int32 nRow, sal_Int32 nColumn) override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleCaption() override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleSummary() override;
+    virtual sal_Bool SAL_CALL isAccessibleSelected(sal_Int32 nRow, sal_Int32 nColumn) override;
+    virtual sal_Int64 SAL_CALL getAccessibleIndex(sal_Int32 nRow, sal_Int32 nColumn) override;
+    virtual sal_Int32 SAL_CALL getAccessibleRow(sal_Int64 nChildIndex) override;
+    virtual sal_Int32 SAL_CALL getAccessibleColumn(sal_Int64 nChildIndex) override;
+
+    void fireEvent(const sal_Int16 _nEventId, const css::uno::Any& _rOldValue,
+                   const css::uno::Any& _rNewValue)
     {
-        SvxShowCharSet&             mrParent;
-        sal_uInt16                  mnId;
-        OUString                    maText;
-        tools::Rectangle                   maRect;
-        rtl::Reference<SvxShowCharSetItemAcc>  m_xItem;
-        SvxShowCharSetAcc*          m_pParent;
+        NotifyAccessibleEvent(_nEventId, _rOldValue, _rNewValue);
+    }
 
-        SvxShowCharSetItem( SvxShowCharSet& rParent, SvxShowCharSetAcc* _pParent, sal_uInt16 _nPos );
-        ~SvxShowCharSetItem();
+    void clearCharSetControl() { m_pParent = nullptr; }
 
-        const rtl::Reference< SvxShowCharSetItemAcc > & GetAccessible();
-    };
+private:
+    virtual ~SvxShowCharSetAcc() override;
 
+    // OCommonAccessibleSelection
+    // return if the specified child is visible => watch for special ChildIndexes (ACCESSIBLE_SELECTION_CHILD_xxx)
+    virtual bool implIsSelected(sal_Int64 nAccessibleChildIndex) override;
 
-    // - SvxShowCharSetAcc -
+    // select the specified child => watch for special ChildIndexes (ACCESSIBLE_SELECTION_CHILD_xxx)
+    virtual void implSelect(sal_Int64 nAccessibleChildIndex, bool bSelect) override;
 
-    /** The table implementation of the vcl control.
-    */
+    // OAccessible
+    /// implements the calculation of the bounding rectangle - still waiting to be overwritten
+    virtual css::awt::Rectangle implGetBounds() override;
+};
 
-    class SvxShowCharSetAcc final
-        : public cppu::ImplInheritanceHelper<::comphelper::OAccessibleSelectionHelper,
-                                             css::accessibility::XAccessibleTable>
+// - SvxShowCharSetItemAcc -
+
+/** The child implementation of the table. */
+class SvxShowCharSetItemAcc final
+    : public cppu::ImplInheritanceHelper<::comphelper::OAccessible,
+                                         css::accessibility::XAccessibleAction>
+{
+private:
+    SvxShowCharSetItem* mpParent;
+
+    virtual ~SvxShowCharSetItemAcc() override;
+
+    // OAccessible
+    /// implements the calculation of the bounding rectangle - still waiting to be overwritten
+    virtual css::awt::Rectangle implGetBounds() override;
+
+public:
+    SvxShowCharSetItemAcc(SvxShowCharSetItem* pParent);
+
+    void ParentDestroyed();
+
+    // XAccessibleComponent
+    virtual void SAL_CALL grabFocus() override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleAtPoint(const css::awt::Point& aPoint) override;
+
+    // OAccessible
+    // XAccessibleContext - still waiting to be overwritten
+    virtual sal_Int64 SAL_CALL getAccessibleChildCount() override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleChild(sal_Int64 i) override;
+    virtual css::uno::Reference<css::accessibility::XAccessible>
+        SAL_CALL getAccessibleParent() override;
+    virtual sal_Int16 SAL_CALL getAccessibleRole() override;
+    virtual OUString SAL_CALL getAccessibleDescription() override;
+    virtual OUString SAL_CALL getAccessibleName() override;
+    virtual css::uno::Reference<css::accessibility::XAccessibleRelationSet>
+        SAL_CALL getAccessibleRelationSet() override;
+    virtual sal_Int64 SAL_CALL getAccessibleStateSet() override;
+
+    virtual sal_Int32 SAL_CALL getForeground() override
     {
-        ::std::vector< rtl::Reference< SvxShowCharSetItemAcc > > m_aChildren;
-        SvxShowCharSet*             m_pParent; // the vcl control
-        virtual void SAL_CALL disposing() override;
-    public:
-        SvxShowCharSetAcc(SvxShowCharSet* pParent);
-
-        // XAccessibleComponent
-        virtual void SAL_CALL grabFocus(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleAtPoint( const css::awt::Point& aPoint ) override;
-
-        // OAccessible
-        // XAccessibleContext - still waiting to be overwritten
-        virtual sal_Int64 SAL_CALL getAccessibleChildCount() override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleChild( sal_Int64 i ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleParent(  ) override;
-        virtual sal_Int16 SAL_CALL getAccessibleRole(  ) override;
-        virtual OUString SAL_CALL getAccessibleDescription(  ) override;
-        virtual OUString SAL_CALL getAccessibleName(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessibleRelationSet > SAL_CALL getAccessibleRelationSet(  ) override;
-        virtual sal_Int64 SAL_CALL getAccessibleStateSet(  ) override;
-
-        virtual sal_Int32 SAL_CALL getForeground(  ) override;
-        virtual sal_Int32 SAL_CALL getBackground(  ) override;
-
-        // XAccessibleTable
-        virtual sal_Int32 SAL_CALL getAccessibleRowCount(  ) override;
-        virtual sal_Int32 SAL_CALL getAccessibleColumnCount(  ) override;
-        virtual OUString SAL_CALL getAccessibleRowDescription( sal_Int32 nRow ) override;
-        virtual OUString SAL_CALL getAccessibleColumnDescription( sal_Int32 nColumn ) override;
-        virtual sal_Int32 SAL_CALL getAccessibleRowExtentAt( sal_Int32 nRow, sal_Int32 nColumn ) override;
-        virtual sal_Int32 SAL_CALL getAccessibleColumnExtentAt( sal_Int32 nRow, sal_Int32 nColumn ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessibleTable > SAL_CALL getAccessibleRowHeaders(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessibleTable > SAL_CALL getAccessibleColumnHeaders(  ) override;
-        virtual css::uno::Sequence< sal_Int32 > SAL_CALL getSelectedAccessibleRows(  ) override;
-        virtual css::uno::Sequence< sal_Int32 > SAL_CALL getSelectedAccessibleColumns(  ) override;
-        virtual sal_Bool SAL_CALL isAccessibleRowSelected( sal_Int32 nRow ) override;
-        virtual sal_Bool SAL_CALL isAccessibleColumnSelected( sal_Int32 nColumn ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleCellAt( sal_Int32 nRow, sal_Int32 nColumn ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleCaption(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleSummary(  ) override;
-        virtual sal_Bool SAL_CALL isAccessibleSelected( sal_Int32 nRow, sal_Int32 nColumn ) override;
-        virtual sal_Int64 SAL_CALL getAccessibleIndex( sal_Int32 nRow, sal_Int32 nColumn ) override;
-        virtual sal_Int32 SAL_CALL getAccessibleRow( sal_Int64 nChildIndex ) override;
-        virtual sal_Int32 SAL_CALL getAccessibleColumn( sal_Int64 nChildIndex ) override;
-
-
-        void fireEvent(
-                    const sal_Int16 _nEventId,
-                    const css::uno::Any& _rOldValue,
-                    const css::uno::Any& _rNewValue
-                )
-        {
-            NotifyAccessibleEvent(_nEventId,_rOldValue,_rNewValue);
-        }
-
-        void clearCharSetControl() { m_pParent = nullptr; }
-    private:
-
-        virtual ~SvxShowCharSetAcc() override;
-
-        // OCommonAccessibleSelection
-        // return if the specified child is visible => watch for special ChildIndexes (ACCESSIBLE_SELECTION_CHILD_xxx)
-        virtual bool
-            implIsSelected( sal_Int64 nAccessibleChildIndex ) override;
-
-        // select the specified child => watch for special ChildIndexes (ACCESSIBLE_SELECTION_CHILD_xxx)
-        virtual void
-            implSelect( sal_Int64 nAccessibleChildIndex, bool bSelect ) override;
-
-        // OAccessible
-        /// implements the calculation of the bounding rectangle - still waiting to be overwritten
-        virtual css::awt::Rectangle implGetBounds(  ) override;
-    };
-
-
-    // - SvxShowCharSetItemAcc -
-
-    /** The child implementation of the table.
-    */
-    class SvxShowCharSetItemAcc final
-        : public cppu::ImplInheritanceHelper<::comphelper::OAccessible,
-                                             css::accessibility::XAccessibleAction>
+        return mpParent->m_pParent->getForeground();
+    }
+    virtual sal_Int32 SAL_CALL getBackground() override
     {
-    private:
-        SvxShowCharSetItem* mpParent;
+        return mpParent->m_pParent->getBackground();
+    }
 
-        virtual ~SvxShowCharSetItemAcc() override;
+    // XAccessibleAction
+    virtual sal_Int32 SAL_CALL getAccessibleActionCount() override;
+    virtual sal_Bool SAL_CALL doAccessibleAction(sal_Int32 nIndex) override;
+    virtual OUString SAL_CALL getAccessibleActionDescription(sal_Int32 nIndex) override;
+    virtual css::uno::Reference<css::accessibility::XAccessibleKeyBinding>
+        SAL_CALL getAccessibleActionKeyBinding(sal_Int32 nIndex) override;
 
-        // OAccessible
-        /// implements the calculation of the bounding rectangle - still waiting to be overwritten
-        virtual css::awt::Rectangle implGetBounds(  ) override;
-    public:
-
-        SvxShowCharSetItemAcc( SvxShowCharSetItem* pParent );
-
-        void    ParentDestroyed();
-
-        // XAccessibleComponent
-        virtual void SAL_CALL grabFocus(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleAtPoint( const css::awt::Point& aPoint ) override;
-
-        // OAccessible
-        // XAccessibleContext - still waiting to be overwritten
-        virtual sal_Int64 SAL_CALL getAccessibleChildCount(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleChild( sal_Int64 i ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessible > SAL_CALL getAccessibleParent(  ) override;
-        virtual sal_Int16 SAL_CALL getAccessibleRole(  ) override;
-        virtual OUString SAL_CALL getAccessibleDescription(  ) override;
-        virtual OUString SAL_CALL getAccessibleName(  ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessibleRelationSet > SAL_CALL getAccessibleRelationSet(  ) override;
-        virtual sal_Int64 SAL_CALL getAccessibleStateSet(  ) override;
-
-        virtual sal_Int32 SAL_CALL getForeground(  ) override { return mpParent->m_pParent->getForeground(); }
-        virtual sal_Int32 SAL_CALL getBackground(  ) override { return mpParent->m_pParent->getBackground(); }
-
-        // XAccessibleAction
-        virtual sal_Int32 SAL_CALL getAccessibleActionCount( ) override;
-        virtual sal_Bool SAL_CALL doAccessibleAction ( sal_Int32 nIndex ) override;
-        virtual OUString SAL_CALL getAccessibleActionDescription ( sal_Int32 nIndex ) override;
-        virtual css::uno::Reference< css::accessibility::XAccessibleKeyBinding > SAL_CALL getAccessibleActionKeyBinding( sal_Int32 nIndex ) override;
-
-
-        void fireEvent(
-                    const sal_Int16 _nEventId,
-                    const css::uno::Any& _rOldValue,
-                    const css::uno::Any& _rNewValue
-                )
-        {
-            NotifyAccessibleEvent(_nEventId,_rOldValue,_rNewValue);
-        }
-    };
+    void fireEvent(const sal_Int16 _nEventId, const css::uno::Any& _rOldValue,
+                   const css::uno::Any& _rNewValue)
+    {
+        NotifyAccessibleEvent(_nEventId, _rOldValue, _rNewValue);
+    }
+};
 }
-
 
 #endif // INCLUDED_SVX_SOURCE_INC_CHARMAPACC_HXX
 
