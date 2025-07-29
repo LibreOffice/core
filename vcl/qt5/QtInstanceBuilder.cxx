214886351bc4a8f7f20f1fd817847d8ed406e44e
Michael Weghorn <m.weghorn@posteo.de>
2025-07-29 22:08:49 +0200

    tdf#130857 qt weld: Support "Line" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    To trigger the dialog:
    
    * start Draw
    * insert a shape (e.g. a rectangle)
    * right-click on newly inserted shape to open context menu
    * select "Line" context menu entry
    
    Change-Id: I4233ff3fb630a7bcc72cd3e0b6cce73459137839
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188555
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -75,6 +75,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"cui/ui/imageviewer.ui"_ustr,
         u"cui/ui/insertrowcolumn.ui"_ustr,
         u"cui/ui/javastartparametersdialog.ui"_ustr,
+        u"cui/ui/linedialog.ui"_ustr,
         u"cui/ui/namedialog.ui"_ustr,
         u"cui/ui/numberdialog.ui"_ustr,
         u"cui/ui/objecttitledescdialog.ui"_ustr,
@@ -191,11 +192,12 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
     // These UI files are only supported inside native Qt dialogs/widgets
     static std::unordered_set<OUString> aSupportedWithQtParent = {
         u"cui/ui/appearance.ui"_ustr,         u"cui/ui/graphictestentry.ui"_ustr,
-        u"cui/ui/possizetabpage.ui"_ustr,     u"cui/ui/rotationtabpage.ui"_ustr,
-        u"cui/ui/slantcornertabpage.ui"_ustr, u"cui/ui/textattrtabpage.ui"_ustr,
-        u"cui/ui/textanimtabpage.ui"_ustr,    u"cui/ui/textcolumnstabpage.ui"_ustr,
-        u"svt/ui/datewindow.ui"_ustr,         u"svx/ui/colorwindow.ui"_ustr,
-        u"vcl/ui/editmenu.ui"_ustr,
+        u"cui/ui/lineendstabpage.ui"_ustr,    u"cui/ui/linetabpage.ui"_ustr,
+        u"cui/ui/linestyletabpage.ui"_ustr,   u"cui/ui/possizetabpage.ui"_ustr,
+        u"cui/ui/rotationtabpage.ui"_ustr,    u"cui/ui/slantcornertabpage.ui"_ustr,
+        u"cui/ui/textattrtabpage.ui"_ustr,    u"cui/ui/textanimtabpage.ui"_ustr,
+        u"cui/ui/textcolumnstabpage.ui"_ustr, u"svt/ui/datewindow.ui"_ustr,
+        u"svx/ui/colorwindow.ui"_ustr,        u"vcl/ui/editmenu.ui"_ustr,
     };
 
     if (aSupportedUIFiles.contains(rUIFile))
