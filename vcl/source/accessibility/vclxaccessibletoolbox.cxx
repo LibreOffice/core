79fdda5112f8b4931e35f2c8190319a7dd99b1c1
Michael Weghorn <m.weghorn@posteo.de>
2025-07-30 16:35:20 +0200

    vcl a11y: Use OAccessible in VCLXAccessibleToolBox::ProcessWindowEvent
    Drop one unnecessary call to OAccessible::getAccessibleContext,
    as OAccessible implements the XAccessibleContext interface
    itself.
    
    Change-Id: I9456ddb9cc95b88c73681efa21f49ca9d413c3bf
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188629
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/source/accessibility/vclxaccessibletoolbox.cxx
+++ vcl/source/accessibility/vclxaccessibletoolbox.cxx
@@ -449,8 +449,8 @@ void VCLXAccessibleToolBox::ProcessWindowEvent( const VclWindowEvent& rVclWindow
             if ( pWin && pWin->GetParent() &&
                  pWin->GetParent()->GetType() == WindowType::TOOLBOX )
             {
-                auto pParentAccContext = pWin->GetParent()->GetAccessible()->getAccessibleContext();
-                VCLXAccessibleToolBox* pParent = static_cast< VCLXAccessibleToolBox* >( pParentAccContext.get() );
+                rtl::Reference<comphelper::OAccessible> pParentAcc = pWin->GetParent()->GetAccessible();
+                VCLXAccessibleToolBox* pParent = static_cast<VCLXAccessibleToolBox*>(pParentAcc.get());
                 if ( pParent )
                     pParent->ReleaseSubToolBox(static_cast<ToolBox *>(pWin.get()));
             }
