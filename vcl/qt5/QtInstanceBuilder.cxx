358de2c2a91bd33786745a79f2d090a0aeb2cf26
Michael Weghorn <m.weghorn@posteo.de>
2025-08-01 12:29:27 +0200

    tdf#130857 qt weld: Support dlg asking who to install extension for
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    This dialog can be triggered as follows:
    
    * start LO
    * "Tools" -> "Extensions"
    * click "Add" button
    * select the .oxt file of a previously downloaded
      extension, e.g. APSO [1] and confirm
    
    [1] https://extensions.libreoffice.org/en/extensions/show/apso-alternative-script-organizer-for-python
    
    Change-Id: Ieb6cd075666ecc12f45bf485bdd72f89b7a47103
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188764
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -109,6 +109,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"cui/ui/zoomdialog.ui"_ustr,
         u"dbaccess/ui/savedialog.ui"_ustr,
         u"dbaccess/ui/tabledesignsavemodifieddialog.ui"_ustr,
+        u"desktop/ui/installforalldialog.ui"_ustr,
         u"filter/ui/testxmlfilter.ui"_ustr,
         u"filter/ui/xmlfiltertabpagegeneral.ui"_ustr,
         u"filter/ui/xmlfiltertabpagetransformation.ui"_ustr,
