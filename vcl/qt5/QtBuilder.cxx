LibreOffice/core

commit 8e33908b538c7700be6f766c472f069d7f4dd15b

tdf#130857 qt weld: Evaluate GtkCellRenderers to know supported types
So far, The GtkCellRenderer objects in .ui files were ignored
by QtBuilder.
However, for the automatic detection of what is the first column
of a specific type (e.g. the first text column), that information
is relevant, and e.g. the GTK implementation in GtkInstanceTreeView
sets `GtkInstanceTreeView::m_nTextCol` based on that, then uses
it when inserting text via the weld::TreeView::insert method.

Extend the logic in QtBuilder and QtInstanceTreeView to also
evaluate the cell renderers set in the .ui file and store the
item data roles supported for each column based on those renderers.

The Qt data model allows setting any of the roles exactly once for
each item, so there is generally no need to explicitly enable
something. But in order to adhere to the weld::TreeView logic/API,
that kind of information is relevant.

Let QtBuilder store a list of supported roles for each column
(i.e. use a list of lists for all columns) in a property set
for the QTreeView, and then use that property in the
QtInstanceTreeView ctor to set the newly introduced
QtInstanceTreeView::m_pColumnRoles member.

Adjust QtInstanceTreeView::firstTextColumnModelIndex to
make use of those stored roles, to reliably detect what
is the first text column according to the GTK/weld::TreeView
model, without requiring any entries to have been inserted
previously.

This is one step towards addressing the issue mentioned in
previous commit

    Change-Id: Id94e22e879f712bd967431ec0f9dc34e4480e3dd
    Author: Michael Weghorn <m.weghorn@posteo.de>
    Date:   Sun Aug 3 22:15:20 2025 +0200

        tdf#130857 qt weld: Support Java "Class Path" dialog

> Currently, both the icon and the path are displayed
> in the first column of the tree view and the second
> column remains empty. This will be addressed in
> upcoming commits.

Change-Id: Ic8a59b254f3bac14c3b812f00d0cbfa3740432f2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188891

vcl/qt5/QtBuilder.cxx
@@ -14,6 +14,7 @@
 #include <QtInstanceMenu.hxx>
 #include <QtInstanceMessageDialog.hxx>
 #include <QtInstanceNotebook.hxx>
+#include <QtInstanceTreeView.hxx>
 #include <QtHyperlinkLabel.hxx>
 
 #include <vcl/qt/QtUtils.hxx>
@@ -47,7 +48,6 @@
 #include <QtWidgets/QTabWidget>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QToolButton>
-#include <QtWidgets/QTreeView>
 #include <QtWidgets/QWizard>
 
 namespace
@@ -123,9 +123,8 @@ QObject* QtBuilder::makeObject(QObject* pParent, std::u16string_view sName, std:
     if (sName.empty())
         return nullptr;
 
-    // nothing to do for these
-    if (sName == u"GtkCellRendererPixbuf" || sName == u"GtkCellRendererText"
-        || sName == u"GtkCellRendererToggle" || sName == u"GtkTreeSelection")
+    // nothing to do for this one
+    if (sName == u"GtkTreeSelection")
         return nullptr;
 
     QWidget* pParentWidget = qobject_cast<QWidget*>(pParent);
@@ -202,6 +201,18 @@ QObject* QtBuilder::makeObject(QObject* pParent, std::u16string_view sName, std:
     else if (sName == u"GtkCalendar")
     {
         pObject = new QCalendarWidget(pParentWidget);
+    }
+    else if (sName == u"GtkCellRendererPixbuf")
+    {
+        enableTreeViewColumnDataRole(pParentWidget, Qt::DecorationRole);
+    }
+    else if (sName == u"GtkCellRendererText")
+    {
+        enableTreeViewColumnDataRole(pParentWidget, Qt::DisplayRole);
+    }
+    else if (sName == u"GtkCellRendererToggle")
+    {
+        enableTreeViewColumnDataRole(pParentWidget, Qt::CheckStateRole);
     }
     else if (sName == u"GtkCheckButton")
     {
@@ -415,6 +426,12 @@ QObject* QtBuilder::makeObject(QObject* pParent, std::u16string_view sName, std:
         const int nCol = pModel->columnCount();
         pModel->insertColumn(nCol);
         pModel->setHeaderData(nCol, Qt::Horizontal, toQString(extractTitle(rMap)));
+
+        // add initially empty list of supported roles for the new column that will
+        // be extended based on the GtkCellRenderer children of the column
+        QList<QList<Qt::ItemDataRole>> aColumnRoles = QtInstanceTreeView::columnRoles(*pTreeView);
+        aColumnRoles.push_back({});
+        QtInstanceTreeView::setColumnRoles(*pTreeView, aColumnRoles);
 
         // nothing else to do, return tree view parent for the widget
         return pTreeView;
@@ -824,6 +841,24 @@ void QtBuilder::deleteObject(QObject* pObject)
     if (pObject->isWidgetType())
         static_cast<QWidget*>(pObject)->hide();
     pObject->deleteLater();
+}
+
+QTreeView* QtBuilder::enableTreeViewColumnDataRole(QWidget* pParentWidget,
+                                                   Qt::ItemDataRole eDataRole)
+{
+    QTreeView* pTreeView = qobject_cast<QTreeView*>(pParentWidget);
+    assert(pTreeView && "No tree view for cell renderer");
+
+    // Mark support for the new role for the tree view's last inserted column
+    // (the GtkCellRenderer is a child object of the GtkTreeViewColumn)
+    QList<QList<Qt::ItemDataRole>> aColumnRoles = QtInstanceTreeView::columnRoles(*pTreeView);
+    assert(!aColumnRoles.empty() && "Missing list of column data roles");
+    assert(!aColumnRoles.back().contains(eDataRole)
+           && "Using the same role multiple times in one column is not supported");
+    aColumnRoles.back().push_back(eDataRole);
+    QtInstanceTreeView::setColumnRoles(*pTreeView, aColumnRoles);
+
+    return pTreeView;
 }
 
 void QtBuilder::replaceWidget(QWidget* pOldWidget, QWidget* pNewWidget)

vcl/inc/qt5/QtBuilder.hxx
@@ -22,6 +22,7 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSlider>
 #include <QtWidgets/QToolButton>
+#include <QtWidgets/QTreeView>
 
 #include <rtl/ustring.hxx>
 #include <unotools/resmgr.hxx>
@@ -84,6 +85,8 @@ public:
 
 private:
     static void deleteObject(QObject* pObject);
+    static QTreeView* enableTreeViewColumnDataRole(QWidget* pParentWidget,
+                                                   Qt::ItemDataRole eDataRole);
     // remove pOldWidget from the widget hierarchy and set (child widget) pNewWidget in its place
     static void replaceWidget(QWidget* pOldWidget, QWidget* pNewWidget);
     void setButtonProperties(QAbstractButton& rButton, stringmap& rProps, QWidget* pParentWidget);

vcl/inc/qt5/QtInstanceTreeView.hxx
@@ -31,6 +31,9 @@ class QtInstanceTreeView : public QtInstanceWidget, public virtual weld::TreeVie
 
     QItemSelectionModel* m_pSelectionModel;
 
+    /** List containing a list of item data roles supported by each column. */
+    QList<QList<Qt::ItemDataRole>> m_pColumnRoles;
+
     bool m_bExtraToggleButtonsEnabled = false;
 
 public:
@@ -199,6 +202,12 @@ public:
 
     using QtInstanceWidget::set_sensitive;
     using QtInstanceWidget::get_sensitive;
+
+    // methods to get/set which roles are supported by the individual columns
+    // based on the underlying GtkTreeViewColumns and their GtkCellRenderers
+    static QList<QList<Qt::ItemDataRole>> columnRoles(QTreeView& rTreeView);
+    static void setColumnRoles(QTreeView& rTreeView,
+                               const QList<QList<Qt::ItemDataRole>>& rDataRoles);
 
 private:
     QModelIndex modelIndex(int nRow, int nCol = 0,

vcl/qt5/QtInstanceTreeView.cxx
@@ -17,6 +17,9 @@
 // role used for the ID in the QStandardItem
 constexpr int ROLE_ID = Qt::UserRole + 1000;
 
+// Property used to store the supported roles for each of the columns
+const char* const PROPERTY_COLUMN_ROLES = "column-roles";
+
 QtInstanceTreeView::QtInstanceTreeView(QTreeView* pTreeView)
     : QtInstanceWidget(pTreeView)
     , m_pTreeView(pTreeView)
@@ -31,6 +34,9 @@ QtInstanceTreeView::QtInstanceTreeView(QTreeView* pTreeView)
 
     m_pSelectionModel = m_pTreeView->selectionModel();
     assert(m_pSelectionModel);
+
+    m_pColumnRoles = columnRoles(*pTreeView);
+    assert(m_pColumnRoles.size() == m_pModel->columnCount() && "column count doesn't match");
 
     connect(m_pTreeView, &QTreeView::activated, this, &QtInstanceTreeView::handleActivated);
     connect(m_pSelectionModel, &QItemSelectionModel::selectionChanged, this,
@@ -1008,6 +1014,24 @@ QAbstractItemView::SelectionMode QtInstanceTreeView::mapSelectionMode(SelectionM
     }
 }
 
+QList<QList<Qt::ItemDataRole>> QtInstanceTreeView::columnRoles(QTreeView& rTreeView)
+{
+    QVariant aVariant = rTreeView.property(PROPERTY_COLUMN_ROLES);
+    if (aVariant.isValid())
+    {
+        assert(aVariant.canConvert<QList<QList<Qt::ItemDataRole>>>());
+        return aVariant.value<QList<QList<Qt::ItemDataRole>>>();
+    }
+
+    return {};
+}
+
+void QtInstanceTreeView::setColumnRoles(QTreeView& rTreeView,
+                                        const QList<QList<Qt::ItemDataRole>>& rDataRoles)
+{
+    rTreeView.setProperty(PROPERTY_COLUMN_ROLES, QVariant::fromValue(rDataRoles));
+}
+
 QModelIndex QtInstanceTreeView::modelIndex(int nRow, int nCol,
                                            const QModelIndex& rParentIndex) const
 {
@@ -1042,12 +1066,10 @@ QModelIndex QtInstanceTreeView::toggleButtonModelIndex(const weld::TreeIter& rIt
 
 QModelIndex QtInstanceTreeView::firstTextColumnModelIndex(const weld::TreeIter& rIter) const
 {
-    for (int i = 0; i < m_pModel->columnCount(); i++)
+    for (qsizetype i = 0; i < m_pColumnRoles.size(); i++)
     {
-        const QModelIndex aIndex = modelIndex(rIter, i);
-        QVariant data = m_pModel->data(aIndex, Qt::DisplayRole);
-        if (data.canConvert<QString>())
-            return aIndex;
+        if (m_pColumnRoles.at(i).contains(Qt::DisplayRole))
+            return modelIndex(rIter, i);
     }
 
     assert(false && "No text column found");

commit 68b9b16ad28f41f1e5f67abcf4f6a99202ca3b33

tdf#130857 qt weld: Ignore GtkCellRenderer without tree view parent
No more assert that all GtkCellRenderers have
a GtkTreeView ancestor (i.e. are children of a
GtkTreeViewColumn that is a child of a GtkTreeView).

That assert was introduced in

    commit 8e33908b538c7700be6f766c472f069d7f4dd15b
    Author: Michael Weghorn <m.weghorn@posteo.de>
    Date:   Sun Aug 3 22:17:34 2025 +0200

        tdf#130857 qt weld: Evaluate GtkCellRenderers to know supported types

However, GtkCellRenderers can also be used as children
for GtkComboboxText as is the case e.g. in the Spelling
Dialog (UI file: cui/uiconfig/ui/spellingdialog.ui) for
the text language combobox that displays an icon in
addition to the text.

QtInstanceComboBox doesn't need the model/cell renderer
information for that one, so simply return early again
instead of triggering an assert, as used to be the case before
the above-mentioned commit.

Also return from QtBuilder::makeObject after
running QtBuilder::enableTreeViewColumnDataRole
as there is nothing else to do in case of the cell
renderers.

(Assert seen getting triggered in a WIP branch for declaring
support for the spelling dialog.)

Change-Id: Id35a4d1f6c9a9728b547a5c0103502a89e4a8b9d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188911
Tested-by: Jenkins
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

vcl/qt5/QtBuilder.cxx
@@ -204,15 +204,15 @@ QObject* QtBuilder::makeObject(QObject* pParent, std::u16string_view sName, std:
     }
     else if (sName == u"GtkCellRendererPixbuf")
     {
-        enableTreeViewColumnDataRole(pParentWidget, Qt::DecorationRole);
+        return enableTreeViewColumnDataRole(pParentWidget, Qt::DecorationRole);
     }
     else if (sName == u"GtkCellRendererText")
     {
-        enableTreeViewColumnDataRole(pParentWidget, Qt::DisplayRole);
+        return enableTreeViewColumnDataRole(pParentWidget, Qt::DisplayRole);
     }
     else if (sName == u"GtkCellRendererToggle")
     {
-        enableTreeViewColumnDataRole(pParentWidget, Qt::CheckStateRole);
+        return enableTreeViewColumnDataRole(pParentWidget, Qt::CheckStateRole);
     }
     else if (sName == u"GtkCheckButton")
     {
@@ -847,7 +847,8 @@ QTreeView* QtBuilder::enableTreeViewColumnDataRole(QWidget* pParentWidget,
                                                    Qt::ItemDataRole eDataRole)
 {
     QTreeView* pTreeView = qobject_cast<QTreeView*>(pParentWidget);
-    assert(pTreeView && "No tree view for cell renderer");
+    if (!pTreeView)
+        return nullptr;
 
     // Mark support for the new role for the tree view's last inserted column
     // (the GtkCellRenderer is a child object of the GtkTreeViewColumn)