LibreOffice/core

commit 088a7c7c451321a800ca8d3523a18b6bb93239b7

remove alpha device from OutputDevice
and render to a combined color+alpha bitmap

The strategy is to remove the mpAlphaVDev from OutputDevice,
and have the OutputDevice create 32-bit SalBitmap's
and then have the various backends correctly render to
those 32-bit bitmaps.

However, whenever we extract a BitmapEx from OutputDevice
(e.g. via VirtualDevice) and hand it to the rest of the
system, we split the color and alpha information.
So the rest of the codebase doesn't have to deal with
this yet.

We have some major problems to overcome here:
(*) Cairo uses 32-bit data, even if we ask for 24-bit color with no alpha.
    This is because the current version of cairo does not support RGB 24-bit data.
(*) Sometimes we want the bitmap to be initialised with an opaque white background,
    sometimes we want it to be transparent.
(*) Sometimes when we extract a bitmap from a device, we want the alpha channel,
    sometimes not.
(*) There are lots of left-over bits of previous attempts of this change lying around,
    e.g. in skia.

Notes
(*) we allow Bitmap (ie. SalBitmap) to hold color+alpha
(*) The BitmapEx(Bitmap) constructor checks if the incoming
Bitmap is color+alpha, and if so, splits into a color bitmap and an alpha bitmap
(*) Because the underlying image data is stored pre-multipled, if
we write a transparent pixel with color, and then read the pixel value back,
we might lose the color information.

Change-Id: I0fc0042d6e5be5edd99b320892fb00a8eb2842fe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173937
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/skia/gdiimpl.hxx
@@ -139,7 +139,7 @@ public:
                           Color nMaskColor) override;
 
     virtual std::shared_ptr<SalBitmap> getBitmap(tools::Long nX, tools::Long nY, tools::Long nWidth,
-                                                 tools::Long nHeight) override;
+                                                 tools::Long nHeight, bool bWithoutAlpha) override;
 
     virtual Color getPixel(tools::Long nX, tools::Long nY) override;
 
@@ -387,6 +387,8 @@ inline SkPaint SkiaSalGraphicsImpl::makePaintInternal() const
         SkiaHelper::setBlenderInvert(&paint);
     else if (mXorMode == XorMode::Xor)
         SkiaHelper::setBlenderXor(&paint);
+    else
+        paint.setBlendMode(SkBlendMode::kSrc); // set as is, including alpha
     return paint;
 }
 
@@ -415,7 +417,19 @@ inline SkPaint SkiaSalGraphicsImpl::makeFillPaint(double transparency) const
     return paint;
 }
 
-inline SkPaint SkiaSalGraphicsImpl::makeBitmapPaint() const { return makePaintInternal(); }
+inline SkPaint SkiaSalGraphicsImpl::makeBitmapPaint() const
+{
+    SkPaint paint;
+    // Invert could be done using a blend mode like invert() does, but
+    // intentionally use SkBlender to make sure it's not overwritten
+    // by a blend mode set later (which would be probably a mistake),
+    // and so that the drawing color does not actually matter.
+    if (mXorMode == XorMode::Invert)
+        SkiaHelper::setBlenderInvert(&paint);
+    else if (mXorMode == XorMode::Xor)
+        SkiaHelper::setBlenderXor(&paint);
+    return paint;
+}
 
 inline SkPaint SkiaSalGraphicsImpl::makeGradientPaint() const { return makePaintInternal(); }
 

vcl/skia/gdiimpl.cxx
@@ -1452,7 +1452,8 @@ void SkiaSalGraphicsImpl::drawMask(const SalTwoRect& rPosAry, const SalBitmap& r
 }
 
 std::shared_ptr<SalBitmap> SkiaSalGraphicsImpl::getBitmap(tools::Long nX, tools::Long nY,
-                                                          tools::Long nWidth, tools::Long nHeight)
+                                                          tools::Long nWidth, tools::Long nHeight,
+                                                          bool bWithoutAlpha)
 {
     SkiaZone zone;
     checkSurface();
@@ -1464,7 +1465,7 @@ std::shared_ptr<SalBitmap> SkiaSalGraphicsImpl::getBitmap(tools::Long nX, tools:
     // in blendAlphaBitmap(), where we could simply use the proper rect of the image.
     sk_sp<SkImage> image = makeCheckedImageSnapshot(
         mSurface, scaleRect(SkIRect::MakeXYWH(nX, nY, nWidth, nHeight), mScaling));
-    std::shared_ptr<SkiaSalBitmap> bitmap = std::make_shared<SkiaSalBitmap>(image);
+    std::shared_ptr<SkiaSalBitmap> bitmap = std::make_shared<SkiaSalBitmap>(image, bWithoutAlpha);
     // If the surface is scaled for HiDPI, the bitmap needs to be scaled down, otherwise
     // it would have incorrect size from the API point of view. The DirectImage::Yes handling
     // in mergeCacheBitmaps() should access the original unscaled bitmap data to avoid

vcl/win/gdi/gdiimpl.cxx
@@ -34,6 +34,7 @@
 #include <basegfx/polygon/b2dpolygon.hxx>
 #include <basegfx/polygon/b2dpolygontools.hxx>
 #include <basegfx/polygon/b2dpolypolygontools.hxx>
+#include <comphelper/windowserrorstring.hxx>
 #include <win/wincomp.hxx>
 #include <win/saldata.hxx>
 #include <win/salgdi.h>
@@ -54,6 +55,7 @@
 #include <gdiplus.h>
 #include <gdiplusenums.h>
 #include <gdipluscolor.h>
+#include <Gdipluspixelformats.h>
 
 #include <postwin.h>
 
@@ -717,7 +719,7 @@ void WinSalGraphicsImpl::drawMask(const SalTwoRect& rPosAry,
         ImplDrawBitmap( hDC, aPosAry, rSalBitmap, false, 0x00B8074AUL );// raster operation PSDPxax
 }
 
-std::shared_ptr<SalBitmap> WinSalGraphicsImpl::getBitmap( tools::Long nX, tools::Long nY, tools::Long nDX, tools::Long nDY )
+std::shared_ptr<SalBitmap> WinSalGraphicsImpl::getBitmap( tools::Long nX, tools::Long nY, tools::Long nDX, tools::Long nDY, bool bWithoutAlpha )
 {
     SAL_WARN_IF( mrParent.isPrinter(), "vcl", "No ::GetBitmap() from printer possible!" );
 
@@ -727,12 +729,34 @@ std::shared_ptr<SalBitmap> WinSalGraphicsImpl::getBitmap( tools::Long nX, tools:
     nDY = std::abs( nDY );
 
     HDC     hDC = mrParent.getHDC();
-    HBITMAP hBmpBitmap = CreateCompatibleBitmap( hDC, nDX, nDY );
-    bool    bRet;
+    HBITMAP hBmpBitmap;
+    if (bWithoutAlpha && GetBitCount() == 32)
+    {
+        BITMAPINFO aBitmapInfo;
+        aBitmapInfo.bmiHeader.biSize = sizeof( BITMAPINFOHEADER );
+        aBitmapInfo.bmiHeader.biWidth = nDX;
+        aBitmapInfo.bmiHeader.biHeight = -nDY; // negative for top down
+        aBitmapInfo.bmiHeader.biPlanes = 1;
+        aBitmapInfo.bmiHeader.biBitCount = 24;
+        aBitmapInfo.bmiHeader.biCompression = BI_RGB;
+        aBitmapInfo.bmiHeader.biSizeImage = 0;
+        aBitmapInfo.bmiHeader.biXPelsPerMeter = 0;
+        aBitmapInfo.bmiHeader.biYPelsPerMeter = 0;
+        aBitmapInfo.bmiHeader.biClrUsed = 0;
+        aBitmapInfo.bmiHeader.biClrImportant = 0;
 
+        void* pData;
+        hBmpBitmap = CreateDIBSection( hDC, &aBitmapInfo,
+                                    DIB_RGB_COLORS, &pData, nullptr,
+                                    0 );
+        SAL_WARN_IF( !hBmpBitmap, "vcl", "CreateDIBSection failed: " << comphelper::WindowsErrorString( GetLastError() ) );
+    }
+    else
+        hBmpBitmap = CreateCompatibleBitmap( hDC, nDX, nDY );
+
+    bool    bRet;
     {
         ScopedCachedHDC<CACHED_HDC_1> hBmpDC(hBmpBitmap);
-
         bRet = BitBlt(hBmpDC.get(), 0, 0,
                       static_cast<int>(nDX), static_cast<int>(nDY), hDC,
                       static_cast<int>(nX), static_cast<int>(nY), SRCCOPY) ? TRUE : FALSE;
@@ -758,14 +782,38 @@ std::shared_ptr<SalBitmap> WinSalGraphicsImpl::getBitmap( tools::Long nX, tools:
 
 Color WinSalGraphicsImpl::getPixel( tools::Long nX, tools::Long nY )
 {
-    COLORREF aWinCol = ::GetPixel( mrParent.getHDC(), static_cast<int>(nX), static_cast<int>(nY) );
+    // The only way to read a pixel color with alpha is via GDI+
+    // This is all hideously inefficient, but we only really use it for unit tests.
+    Gdiplus::Bitmap screenPixel(1, 1, PixelFormat32bppARGB);
+    Gdiplus::Graphics gdest(&screenPixel);
+    auto hDestDC = gdest.GetHDC();
+    BOOL retval = BitBlt(hDestDC, 0, 0, 1, 1,
+                        mrParent.getHDC(), static_cast<int>(nX), static_cast<int>(nY),
+                        SRCCOPY);
+    gdest.ReleaseHDC(hDestDC);
 
-    if ( CLR_INVALID == aWinCol )
-        return Color( 0, 0, 0 );
+    if (!retval)
+    {
+        SAL_WARN("vcl", "GetPixel failed1");
+        return Color(0, 0, 0);
+    }
+    Gdiplus::Color nGdiColor;
+    if (screenPixel.GetPixel(0, 0, &nGdiColor) != Gdiplus::Ok)
+    {
+        SAL_WARN("vcl", "GetPixel failed2");
+        return Color(0, 0, 0);
+    }
+    // seems to be returning premultiplied color, despite the pixel format that I pass in the constructor
+    if (nGdiColor.GetAlpha() == 0)
+        return Color(ColorAlpha, nGdiColor.GetAlpha(),
+                     nGdiColor.GetRed(),
+                     nGdiColor.GetGreen(),
+                     nGdiColor.GetBlue());
     else
-        return Color( GetRValue( aWinCol ),
-                              GetGValue( aWinCol ),
-                              GetBValue( aWinCol ) );
+        return Color(ColorAlpha, nGdiColor.GetAlpha(),
+                     nGdiColor.GetRed() * 255 / nGdiColor.GetAlpha(),
+                     nGdiColor.GetGreen() * 255 / nGdiColor.GetAlpha(),
+                     nGdiColor.GetBlue() * 255 / nGdiColor.GetAlpha());
 }
 
 namespace
@@ -1379,11 +1427,42 @@ void WinSalGraphicsImpl::drawPixel( tools::Long nX, tools::Long nY )
 
 void WinSalGraphicsImpl::drawPixel( tools::Long nX, tools::Long nY, Color nColor )
 {
-    COLORREF nCol = RGB( nColor.GetRed(),
+    COLORREF nColorRef = RGB( nColor.GetRed(),
                         nColor.GetGreen(),
                         nColor.GetBlue() );
 
-    DrawPixelImpl( nX, nY, nCol );
+    if (nColor.GetAlpha() != 255)
+    {
+        const HDC hDC = mrParent.getHDC();
+
+        if (!mbXORMode)
+        {
+            // the only way to draw a pixel with alpha is via GDI+
+            Gdiplus::Graphics g (hDC);
+            g.SetCompositingMode(Gdiplus::CompositingModeSourceCopy);
+            Gdiplus::SolidBrush brush(Gdiplus::Color(nColor.GetAlpha(), nColor.GetRed(), nColor.GetGreen(), nColor.GetBlue()));
+            g.FillRectangle(&brush, static_cast<int>(nX), static_cast<int>(nY), 1, 1);
+        }
+        else
+        {
+            assert(false && "I am not even sure what it would mean to use alpha in this mode");
+            ScopedSelectedHBRUSH hBrush(hDC, CreateSolidBrush(nColorRef));
+            PatBlt(hDC, static_cast<int>(nX), static_cast<int>(nY), int(1), int(1), PATINVERT);
+        }
+    }
+    else
+    {
+        const HDC hDC = mrParent.getHDC();
+
+        if (!mbXORMode)
+        {
+            SetPixel(hDC, static_cast<int>(nX), static_cast<int>(nY), nColorRef);
+            return;
+        }
+
+        ScopedSelectedHBRUSH hBrush(hDC, CreateSolidBrush(nColorRef));
+        PatBlt(hDC, static_cast<int>(nX), static_cast<int>(nY), int(1), int(1), PATINVERT);
+    }
 }
 
 void WinSalGraphicsImpl::drawLine( tools::Long nX1, tools::Long nY1, tools::Long nX2, tools::Long nY2 )
@@ -1403,8 +1482,24 @@ void WinSalGraphicsImpl::drawRect( tools::Long nX, tools::Long nY, tools::Long n
     {
         if ( !mrParent.isPrinter() )
         {
-            PatBlt( mrParent.getHDC(), static_cast<int>(nX), static_cast<int>(nY), static_cast<int>(nWidth), static_cast<int>(nHeight),
-                    mbXORMode ? PATINVERT : PATCOPY );
+            if (maFillColor.GetAlpha() == 255)
+            {
+                PatBlt( mrParent.getHDC(), static_cast<int>(nX), static_cast<int>(nY), static_cast<int>(nWidth), static_cast<int>(nHeight),
+                        mbXORMode ? PATINVERT : PATCOPY );
+            }
+            else if (mbXORMode)
+                assert(false && "don't even know what this would mean");
+            else
+            {
+                // the only way to draw with alpha is via GDI+
+                Gdiplus::Graphics g(mrParent.getHDC());
+                if (g.SetCompositingMode(Gdiplus::CompositingModeSourceCopy) != Gdiplus::Ok)
+                    SAL_WARN("vcl", "SetCompositingMode failed");
+                Gdiplus::SolidBrush brush(
+                    Gdiplus::Color(maFillColor.GetAlpha(), maFillColor.GetRed(),
+                                   maFillColor.GetGreen(), maFillColor.GetBlue()));
+                g.FillRectangle(&brush, static_cast<int>(nX), static_cast<int>(nY), nWidth, nHeight);
+            }
         }
         else
         {
@@ -1417,7 +1512,25 @@ void WinSalGraphicsImpl::drawRect( tools::Long nX, tools::Long nY, tools::Long n
         }
     }
     else
-        Rectangle( mrParent.getHDC(), static_cast<int>(nX), static_cast<int>(nY), static_cast<int>(nX+nWidth), static_cast<int>(nY+nHeight) );
+    {
+        if (maFillColor.GetAlpha() == 255)
+            Rectangle(mrParent.getHDC(), static_cast<int>(nX), static_cast<int>(nY),
+                        static_cast<int>(nX + nWidth), static_cast<int>(nY + nHeight));
+        else if (mbXORMode)
+            assert(false && "don't even know what this would mean");
+        else
+        {
+            // the only way to draw with alpha is via GDI+
+            Gdiplus::Graphics g(mrParent.getHDC());
+            if (g.SetCompositingMode(Gdiplus::CompositingModeSourceCopy) != Gdiplus::Ok)
+                SAL_WARN("vcl", "SetCompositingMode failed");
+            Gdiplus::SolidBrush brush(
+                Gdiplus::Color(maFillColor.GetAlpha(), maFillColor.GetRed(),
+                                maFillColor.GetGreen(), maFillColor.GetBlue()));
+            g.FillRectangle(&brush, static_cast<int>(nX), static_cast<int>(nY), nWidth,
+                            nHeight);
+        }
+    }
 }
 
 void WinSalGraphicsImpl::drawPolyLine( sal_uInt32 nPoints, const Point* pPtAry )
@@ -2266,30 +2379,15 @@ static void paintToGdiPlus(
     const SalTwoRect& rTR,
     Gdiplus::Bitmap& rBitmap)
 {
-    // only parts of source are used
-    Gdiplus::PointF aDestPoints[3];
-    Gdiplus::ImageAttributes aAttributes;
-
-    // define target region as parallelogram
-    aDestPoints[0].X = Gdiplus::REAL(rTR.mnDestX);
-    aDestPoints[0].Y = Gdiplus::REAL(rTR.mnDestY);
-    aDestPoints[1].X = Gdiplus::REAL(rTR.mnDestX + rTR.mnDestWidth);
-    aDestPoints[1].Y = Gdiplus::REAL(rTR.mnDestY);
-    aDestPoints[2].X = Gdiplus::REAL(rTR.mnDestX);
-    aDestPoints[2].Y = Gdiplus::REAL(rTR.mnDestY + rTR.mnDestHeight);
-
-    aAttributes.SetWrapMode(Gdiplus::WrapModeTileFlipXY);
+    Gdiplus::Rect aDestRect{ INT(rTR.mnDestX), INT(rTR.mnDestY),
+                             INT(rTR.mnDestWidth), INT(rTR.mnDestHeight) };
 
     rGraphics.DrawImage(
         &rBitmap,
-        aDestPoints,
-        3,
-        Gdiplus::REAL(rTR.mnSrcX),
-        Gdiplus::REAL(rTR.mnSrcY),
-        Gdiplus::REAL(rTR.mnSrcWidth),
-        Gdiplus::REAL(rTR.mnSrcHeight),
+        aDestRect,
+        rTR.mnSrcX, rTR.mnSrcY, rTR.mnSrcWidth, rTR.mnSrcHeight,
         Gdiplus::UnitPixel,
-        &aAttributes);
+        nullptr);
 }
 
 static void setInterpolationMode(
@@ -2372,6 +2470,10 @@ bool WinSalGraphicsImpl::drawAlphaBitmap(
     const SalBitmap& rAlphaBmp)
 {
     if(!rTR.mnSrcWidth || !rTR.mnSrcHeight || !rTR.mnDestWidth || !rTR.mnDestHeight)
+        return false;
+
+    // neither GDI nor GDI+ can properly blend to a surface that has alpha
+    if (GetBitCount() == 32)
         return false;
 
     assert(dynamic_cast<const WinSalBitmap*>(&rSrcBitmap));

vcl/win/gdi/gdiimpl.hxx
@@ -170,7 +170,7 @@ public:
                 const SalBitmap& rSalBitmap,
                 Color nMaskColor ) override;
 
-    virtual std::shared_ptr<SalBitmap> getBitmap( tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight ) override;
+    virtual std::shared_ptr<SalBitmap> getBitmap( tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight, bool bWithoutAlpha ) override;
 
     virtual Color getPixel( tools::Long nX, tools::Long nY ) override;
 

commit 9791aaaa31749857e719ab8b6926b180966f959b

tdf#167643 Selection is black
regression from
    commit 088a7c7c451321a800ca8d3523a18b6bb93239b7
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Tue Sep 24 16:18:11 2024 +0200
    remove alpha device from OutputDevice

I am a little confused here. I would have thought kSrcOver would
be the right blend mode, but instead it is kSrcATop
which produces the "right" answer.

Change-Id: If5bfb0ab139cbed5acaba2cc66c5036ca091818c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188957
Reviewed-by: Patrick Luby <guibomacdev@gmail.com>
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/skia/gdiimpl.hxx
@@ -305,11 +305,11 @@ protected:
     // Create SkPaint to use when drawing to the surface. It is not to be used
     // when doing internal drawing such as when merging two bitmaps together.
     // This may apply some default settings to the paint as necessary.
-    SkPaint makePaintInternal() const;
+    SkPaint makePaintInternal(bool bSrcATop = false) const;
     // Create SkPaint set up for drawing lines (using mLineColor etc.).
     SkPaint makeLinePaint(double transparency = 0) const;
     // Create SkPaint set up for filling (using mFillColor etc.).
-    SkPaint makeFillPaint(double transparency = 0) const;
+    SkPaint makeFillPaint(double transparency = 0, bool bSrcATop = false) const;
     // Create SkPaint set up for bitmap drawing.
     SkPaint makeBitmapPaint() const;
     // Create SkPaint set up for gradient drawing.
@@ -370,7 +370,7 @@ protected:
     bool mInWindowBackingPropertiesChanged;
 };
 
-inline SkPaint SkiaSalGraphicsImpl::makePaintInternal() const
+inline SkPaint SkiaSalGraphicsImpl::makePaintInternal(bool bSrcATop) const
 {
     SkPaint paint;
     // Invert could be done using a blend mode like invert() does, but
@@ -381,6 +381,8 @@ inline SkPaint SkiaSalGraphicsImpl::makePaintInternal() const
         SkiaHelper::setBlenderInvert(&paint);
     else if (mXorMode == XorMode::Xor)
         SkiaHelper::setBlenderXor(&paint);
+    else if (bSrcATop)
+        paint.setBlendMode(SkBlendMode::kSrcATop);
     else
         paint.setBlendMode(SkBlendMode::kSrc); // set as is, including alpha
     return paint;
@@ -397,10 +399,10 @@ inline SkPaint SkiaSalGraphicsImpl::makeLinePaint(double transparency) const
     return paint;
 }
 
-inline SkPaint SkiaSalGraphicsImpl::makeFillPaint(double transparency) const
+inline SkPaint SkiaSalGraphicsImpl::makeFillPaint(double transparency, bool bSrcATop) const
 {
     assert(moFillColor.has_value());
-    SkPaint paint = makePaintInternal();
+    SkPaint paint = makePaintInternal(bSrcATop);
     paint.setColor(transparency == 0
                        ? SkiaHelper::toSkColor(*moFillColor)
                        : SkiaHelper::toSkColorWithTransparency(*moFillColor, transparency));

vcl/skia/gdiimpl.cxx
@@ -965,7 +965,7 @@ void SkiaSalGraphicsImpl::performDrawPolyPolygon(const basegfx::B2DPolyPolygon& 
     }
     if (moFillColor)
     {
-        SkPaint aPaint = makeFillPaint(fTransparency);
+        SkPaint aPaint = makeFillPaint(fTransparency, /*bSrcATop*/ true);
         aPaint.setAntiAlias(useAA);
         // HACK: If the polygon is just a line, it still should be drawn. But when filling
         // Skia doesn't draw empty polygons, so in that case ensure the line is drawn.