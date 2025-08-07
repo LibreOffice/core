5b2f54ac15685a964d125e3eb550822d30a27bdc
Michael Stahl <michael.stahl@collabora.com>
2025-08-07 16:28:20 +0200

    libxml2: add upstream patch for CVE-2025-7425
    Change-Id: I84110fc1ed54eac4a0ce4d8b8070a031c761fb39
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189104
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit 88e2be781bc22f8500f57c84cec572ead8af32c4)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189122
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- external/libxml2/UnpackedTarball_libxml2.mk
+++ external/libxml2/UnpackedTarball_libxml2.mk
@@ -25,6 +25,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,libxml2,\
 	$(if $(gb_Module_CURRENTMODULE_SYMBOLS_ENABLED), \
 		external/libxml2/libxml2-icu-sym.patch.0, \
 		external/libxml2/libxml2-icu.patch.0) \
+	external/libxml2/9de92ed78d8495527c5d7a4d0cc76c1f83768195.patch.1 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_file,libxml2,xml2-config.in,external/libxml2/xml2-config.in))
