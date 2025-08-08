e8f269e610c467b8223c82ba271c6ae9ceba4d87
Michael Weghorn <m.weghorn@posteo.de>
2025-08-08 09:00:46 +0200

    tdf#130857 qt weld: Support dialog asking whether to print form letter
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered as follows:
    
        * start Writer
        * "Insert" -> "Field" -> "More Fields"
        * switch to "Database" tab
        * select "Mail merge fields" for the type
        * click "Browse" button next to the "Add database file"
          label and select any database file
        * in the "Database Selection" treeview, select any column
          of the database
        * press the "Insert" button
        * close the "Fields" dialog
        * press Ctrl+P to trigger the print dialog, which
          triggers the dialog asking whether to print
          a form letter
    
    Change-Id: Iab9e5869f7540d5b7bc751156c63d6411defc40f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189150
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -159,6 +159,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"modules/swriter/ui/inserttable.ui"_ustr,
         u"modules/swriter/ui/linenumbering.ui"_ustr,
         u"modules/swriter/ui/numberingnamedialog.ui"_ustr,
+        u"modules/swriter/ui/printmergedialog.ui"_ustr,
         u"modules/swriter/ui/renameautotextdialog.ui"_ustr,
         u"modules/swriter/ui/renameobjectdialog.ui"_ustr,
         u"modules/swriter/ui/rowheight.ui"_ustr,
