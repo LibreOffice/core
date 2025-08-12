LibreOffice/core

commit d8ab8992bd35aedfc1d2b0debebab0d623aa8d09

ScViewData: pass ScDocShell by ref
After this change, testTdf159174 started to fail.
Move the fix introduced in

commit 0b93cc83993b3cd78aa05556342839a21c96dc5a
Author: Mike Kaganski <mike.kaganski@collabora.com>
Date:   Sun Jan 14 12:16:48 2024 +0600

    tdf#159174: there may be no tabs in the clipboard document

to the caller in ScTransferObj::PaintToDev

Change-Id: I770305dd0e1ac31f897ddcf4209f7aba1387f8fb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185786
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Tested-by: Jenkins

sc/source/ui/app/transobj.cxx
@@ -100,6 +100,9 @@ void ScTransferObj::GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab
 void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
                                 const ScRange& rBlock )
 {
+    if (rDoc.GetMaxTableNumber() < 0)
+        return;
+
     tools::Rectangle aBound( Point(), pDev->GetOutputSize() );      //! use size from clip area?
 
     ScViewData aViewData(rDoc);

commit 671564bce4541244ad3d67b5b98899321176231e

tdf#167075: init doc shell before pasting as png/bmp (take 2)
Regression after
commit d8ab8992bd35aedfc1d2b0debebab0d623aa8d09
Author: Xisco Fauli <xiscofauli@libreoffice.org>
Date:   Mon May 26 11:06:06 2025 +0200

    ScViewData: pass ScDocShell by ref

and

commit 52e3b2b040ac54c2a5661e3676b135170f028f50
Author: Xisco Fauli <xiscofauli@libreoffice.org>
Date:   Mon May 26 16:06:12 2025 +0200

    ScViewData: simplify constructors

Change-Id: I6a709ab82f51c34a022c43d35b97f661e5c283e9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189434
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sc/source/ui/app/transobj.cxx
@@ -97,21 +97,17 @@ void ScTransferObj::GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab
     nCol = nMaxCol;
 }
 
-void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
+void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
                                 const ScRange& rBlock )
 {
-    if (!rDoc.GetDocumentShell())
-        return;
-
     tools::Rectangle aBound( Point(), pDev->GetOutputSize() );      //! use size from clip area?
 
-    ScViewData aViewData(*rDoc.GetDocumentShell(), nullptr);
+    ScViewData aViewData(rDocSh, nullptr);
 
-    aViewData.SetTabNo( rBlock.aEnd.Tab() );
     aViewData.SetScreen( rBlock.aStart.Col(), rBlock.aStart.Row(),
                             rBlock.aEnd.Col(), rBlock.aEnd.Row() );
 
-    ScPrintFunc::DrawToDev( rDoc, pDev, nPrintFactor, aBound, aViewData, false/*bMetaFile*/ );
+    ScPrintFunc::DrawToDev( rDocSh.GetDocument(), pDev, nPrintFactor, aBound, aViewData, false/*bMetaFile*/ );
 }
 
 ScTransferObj::ScTransferObj( const std::shared_ptr<ScDocument>& pClipDoc, TransferableObjectDescriptor aDesc ) :
@@ -437,7 +433,8 @@ bool ScTransferObj::GetData( const datatransfer::DataFlavor& rFlavor, const OUSt
 
             pVirtDev->SetOutputSizePixel(aPixelSize);
 
-            PaintToDev( pVirtDev, *m_pDoc, 1.0, aReducedBlock );
+            InitDocShell(true);
+            PaintToDev( pVirtDev, *m_aDocShellRef, 1.0, aReducedBlock );
 
             pVirtDev->SetMapMode( MapMode( MapUnit::MapPixel, Point(), aScale, aScale ) );
             Bitmap aBmp( pVirtDev->GetBitmap( Point(), pVirtDev->GetOutputSize() ) );

sc/source/ui/inc/transobj.hxx
@@ -58,7 +58,7 @@ private:
     static void StripRefs( ScDocument& rDoc, SCCOL nStartX, SCROW nStartY,
                             SCCOL nEndX, SCROW nEndY,
                             ScDocument& rDestDoc );
-    static void PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
+    static void PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
                             const ScRange& rBlock );
     static void GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab2, SCROW& nRow, SCCOL& nCol );
 