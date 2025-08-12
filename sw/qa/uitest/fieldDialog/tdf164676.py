2d9596d9dd0f4b83c0228dbb7bc69e51b7186858
Xisco Fauli <xiscofauli@libreoffice.org>
2025-08-12 14:26:06 +0200

    tdf#164676: sw: Add UItest
    Change-Id: I4b6cc0357ee4b31ff61a0e90ebd2ae59877e973b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189427
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

---
+++ sw/qa/uitest/fieldDialog/tdf164676.py
@@ -0,0 +1,42 @@
+# -*- tab-width: 4; indent-tabs-mode: nil; py-indent-offset: 4 -*-
+#
+# This file is part of the LibreOffice project.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from uitest.framework import UITestCase
+from libreoffice.uno.propertyvalue import mkPropertyValues
+from uitest.uihelper.common import get_state_as_dict
+from uitest.uihelper.common import select_pos
+
+class Tdf164676(UITestCase):
+
+    def test_tdf164676(self):
+
+        with self.ui_test.create_doc_in_start_center("writer") as document:
+
+            with self.ui_test.execute_modeless_dialog_through_command(".uno:InsertField", close_button="cancel") as xDialog:
+                xTab = xDialog.getChild("tabcontrol")
+                select_pos(xTab, "4")
+
+                xType = xDialog.getChild("type-var")
+                xType.getChild('0').executeAction("SELECT", tuple())
+                self.assertEqual("Set variable", get_state_as_dict(xType)['SelectEntryText'])
+
+                xName = xDialog.getChild("name-var")
+                xName.executeAction("TYPE", mkPropertyValues({"TEXT": "AA"}))
+
+                xValue = xDialog.getChild("value-var")
+                xValue.executeAction("TYPE", mkPropertyValues({"TEXT": "1234"}))
+
+                xOkBtn = xDialog.getChild("ok")
+                xOkBtn.executeAction("CLICK", ())
+
+            # Without the fix in place, this test would have failed with
+            # AssertionError: '1234' != ''
+            self.assertEqual("1234", document.Text.createEnumeration().nextElement().String)
+
+# vim: set shiftwidth=4 softtabstop=4 expandtab:
