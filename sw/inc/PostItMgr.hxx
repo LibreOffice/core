2d2166759e9bfd8e942b996ee94dc8babc6ddd48
Jaume Pujantell <jaume.pujantell@collabora.com>
2025-08-13 14:31:31 +0200

    lok: pdf export comments in margin
    Enable print comments in margin for PDF in LOK without tiled comments.
    Right now comments are only printed if they are being shown in the LO
    document which excludes LOK without tiled comments. Also the post-it
    manager and annotation window assume in some places that MapMode is used
    and correctly set, which is not the case in LOK.
    
    Adapted an existing test about printing comments in margin to test also
    for LOK.
    
    Change-Id: I362cb89ade6d4f53832fd9a722196d214360aae6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189511
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit 3a633abe5c6a884d152c73737debe215a980c79d)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192728
    Tested-by: Jenkins
    Reviewed-by: Jaume Pujantell <jaume.pujantell@collabora.com>

--- sw/inc/PostItMgr.hxx
+++ sw/inc/PostItMgr.hxx
@@ -99,6 +99,7 @@ class SAL_DLLPUBLIC_RTTI SwPostItMgr final : public SfxListener,
         FieldShadowState                mShadowState;
         std::optional<OutlinerParaObject> mpAnswer;
         OUString                        maAnswerText;
+        bool mbForceShow = false;
 
         // data structure to collect the <SwAnnotationWin> instances for certain <SwFrame> instances.
         std::unique_ptr<sw::sidebarwindows::SwFrameSidebarWinContainer> mpFrameSidebarWinContainer;
@@ -253,6 +254,8 @@ class SAL_DLLPUBLIC_RTTI SwPostItMgr final : public SfxListener,
         void PaintTile(OutputDevice& rRenderContext);
 
         sw::sidebarwindows::SidebarPosition GetSidebarPos(const Point& rPointLogic);
+
+        void SetForceShow(bool bForce) { mbForceShow = bForce; }
 
         // The commands that directly delete comments (as opposed to deletion of the text that the
         // comments are anchored to) behave differently, depending on the document type, when the
