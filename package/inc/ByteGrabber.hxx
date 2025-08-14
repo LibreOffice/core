LibreOffice/core

commit a6ad198d097fb4a503c8d5831d484ff46721134b

tdf#158556 use more comphelper::ByteReader
which avoids a ton of temporary uno::Sequence being created

Change-Id: I237bb69395f692bb0272ca0daec05b81af828e01
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/171968
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

package/inc/ByteGrabber.hxx
@@ -21,6 +21,8 @@
 
 #include <com/sun/star/uno/Sequence.h>
 #include <com/sun/star/uno/Reference.h>
+#include <comphelper/bytereader.hxx>
+#include <array>
 
 namespace com::sun::star {
     namespace io { class XSeekable; class XInputStream; }
@@ -29,8 +31,8 @@ class ByteGrabber final
 {
     css::uno::Reference < css::io::XInputStream > xStream;
     css::uno::Reference < css::io::XSeekable > xSeek;
-    css::uno::Sequence < sal_Int8 > aSequence;
-    const sal_Int8 *pSequence;
+    comphelper::ByteReader* mpByteReader;
+    std::array<sal_Int8, 8> maBuffer;
 
 public:
     ByteGrabber (css::uno::Reference < css::io::XInputStream > const & xIstream);
@@ -43,6 +45,7 @@ public:
     /// @throws css::io::IOException
     /// @throws css::uno::RuntimeException
     sal_Int32 readBytes( css::uno::Sequence< sal_Int8 >& aData, sal_Int32 nBytesToRead );
+    sal_Int32 readBytes( sal_Int8* aData, sal_Int32 nBytesToRead );
     // XSeekable
     /// @throws css::lang::IllegalArgumentException
     /// @throws css::io::IOException

package/source/zipapi/ByteGrabber.cxx
@@ -39,9 +39,9 @@ using namespace ::com::sun::star;
 ByteGrabber::ByteGrabber(uno::Reference  < io::XInputStream > const & xIstream)
 : xStream(xIstream)
 , xSeek (xIstream, uno::UNO_QUERY )
-, aSequence ( 4 )
 {
-    pSequence = aSequence.getArray();
+    mpByteReader = dynamic_cast<comphelper::ByteReader*>(xStream.get());
+    assert(mpByteReader);
 }
 
 ByteGrabber::~ByteGrabber()
@@ -52,6 +52,8 @@ void ByteGrabber::setInputStream (const uno::Reference < io::XInputStream >& xNe
 {
     xStream = xNewStream;
     xSeek.set(xNewStream, uno::UNO_QUERY);
+    mpByteReader = dynamic_cast<comphelper::ByteReader*>(xStream.get());
+    assert(mpByteReader);
 }
 
 // XInputStream chained
@@ -59,6 +61,12 @@ sal_Int32 ByteGrabber::readBytes( uno::Sequence< sal_Int8 >& aData,
                                         sal_Int32 nBytesToRead )
 {
     return xStream->readBytes(aData, nBytesToRead );
+}
+
+sal_Int32 ByteGrabber::readBytes( sal_Int8* aData,
+                                        sal_Int32 nBytesToRead )
+{
+    return mpByteReader->readSomeBytes(aData, nBytesToRead );
 }
 
 // XSeekable chained...
@@ -88,42 +96,39 @@ sal_Int64 ByteGrabber::getLength(  )
 
 sal_uInt16 ByteGrabber::ReadUInt16()
 {
-    if (xStream->readBytes(aSequence, 2) != 2)
+    if (mpByteReader->readSomeBytes(maBuffer.data(), 2) != 2)
         return 0;
 
-    pSequence = aSequence.getConstArray();
     return static_cast <sal_uInt16>
-           ( (pSequence[0] & 0xFF)
-          | (pSequence[1] & 0xFF) << 8);
+           ( (maBuffer[0] & 0xFF)
+          | (maBuffer[1] & 0xFF) << 8);
 }
 
 sal_uInt32 ByteGrabber::ReadUInt32()
 {
-    if (xStream->readBytes(aSequence, 4) != 4)
+    if (mpByteReader->readSomeBytes(maBuffer.data(), 4) != 4)
         return 0;
 
-    pSequence = aSequence.getConstArray();
     return static_cast < sal_uInt32 >
-            ( (pSequence[0] & 0xFF)
-          | ( pSequence[1] & 0xFF ) << 8
-          | ( pSequence[2] & 0xFF ) << 16
-          | ( pSequence[3] & 0xFF ) << 24 );
+            ( (maBuffer[0] & 0xFF)
+          | ( maBuffer[1] & 0xFF ) << 8
+          | ( maBuffer[2] & 0xFF ) << 16
+          | ( maBuffer[3] & 0xFF ) << 24 );
 }
 
 sal_uInt64 ByteGrabber::ReadUInt64()
 {
-    if (xStream->readBytes(aSequence, 8) != 8)
+    if (mpByteReader->readSomeBytes(maBuffer.data(), 8) != 8)
         return 0;
 
-    pSequence = aSequence.getConstArray();
-    return  static_cast<sal_uInt64>(pSequence[0] & 0xFF)
-          | static_cast<sal_uInt64>(pSequence[1] & 0xFF) << 8
-          | static_cast<sal_uInt64>(pSequence[2] & 0xFF) << 16
-          | static_cast<sal_uInt64>(pSequence[3] & 0xFF) << 24
-          | static_cast<sal_uInt64>(pSequence[4] & 0xFF) << 32
-          | static_cast<sal_uInt64>(pSequence[5] & 0xFF) << 40
-          | static_cast<sal_uInt64>(pSequence[6] & 0xFF) << 48
-          | static_cast<sal_uInt64>(pSequence[7] & 0xFF) << 56;
+    return  static_cast<sal_uInt64>(maBuffer[0] & 0xFF)
+          | static_cast<sal_uInt64>(maBuffer[1] & 0xFF) << 8
+          | static_cast<sal_uInt64>(maBuffer[2] & 0xFF) << 16
+          | static_cast<sal_uInt64>(maBuffer[3] & 0xFF) << 24
+          | static_cast<sal_uInt64>(maBuffer[4] & 0xFF) << 32
+          | static_cast<sal_uInt64>(maBuffer[5] & 0xFF) << 40
+          | static_cast<sal_uInt64>(maBuffer[6] & 0xFF) << 48
+          | static_cast<sal_uInt64>(maBuffer[7] & 0xFF) << 56;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

comphelper/source/streaming/memorystream.cxx
@@ -55,7 +55,7 @@ namespace {
 
 class UNOMemoryStream :
     public WeakImplHelper<XServiceInfo, XStream, XSeekableInputStream, XOutputStream, XTruncate>,
-    public comphelper::ByteWriter
+    public comphelper::ByteWriter, public comphelper::ByteReader
 {
 public:
     UNOMemoryStream();
@@ -91,6 +91,9 @@ public:
 
     // comphelper::ByteWriter
     virtual void writeBytes(const sal_Int8* aData, sal_Int32 nBytesToWrite) override;
+
+    // comphelper::ByteReader
+    virtual sal_Int32 readSomeBytes(sal_Int8* aData, sal_Int32 nBytesToRead) override;
 
 private:
     std::vector< sal_Int8, boost::noinit_adaptor<std::allocator<sal_Int8>> > maData;
@@ -146,6 +149,26 @@ sal_Int32 SAL_CALL UNOMemoryStream::readBytes( Sequence< sal_Int8 >& aData, sal_
         sal_Int8* pData = &(*maData.begin());
         sal_Int8* pCursor = &(pData[mnCursor]);
         memcpy( aData.getArray(), pCursor, nBytesToRead );
+
+        mnCursor += nBytesToRead;
+    }
+
+    return nBytesToRead;
+}
+
+// ByteReader
+sal_Int32 UNOMemoryStream::readSomeBytes( sal_Int8* aData, sal_Int32 nBytesToRead )
+{
+    if( nBytesToRead < 0 )
+        throw IOException(u"nBytesToRead < 0"_ustr);
+
+    nBytesToRead = std::min( nBytesToRead, available() );
+
+    if( nBytesToRead )
+    {
+        sal_Int8* pData = &(*maData.begin());
+        sal_Int8* pCursor = &(pData[mnCursor]);
+        memcpy( aData, pCursor, nBytesToRead );
 
         mnCursor += nBytesToRead;
     }

comphelper/source/streaming/seekableinput.cxx
@@ -107,7 +107,11 @@ void OSeekableInputWrapper::PrepareCopy_Impl()
             xTempSeek->seek( 0 );
             m_xCopyInput.set( xTempOut, uno::UNO_QUERY );
             if ( m_xCopyInput.is() )
+            {
                 m_xCopySeek = xTempSeek;
+                m_pCopyByteReader = dynamic_cast<comphelper::ByteReader*>(xTempOut.get());
+                assert(m_pCopyByteReader);
+            }
         }
     }
 
@@ -140,6 +144,18 @@ sal_Int32 SAL_CALL OSeekableInputWrapper::readSomeBytes( uno::Sequence< sal_Int8
     PrepareCopy_Impl();
 
     return m_xCopyInput->readSomeBytes( aData, nMaxBytesToRead );
+}
+
+sal_Int32 OSeekableInputWrapper::readSomeBytes( sal_Int8* aData, sal_Int32 nMaxBytesToRead )
+{
+    std::scoped_lock aGuard( m_aMutex );
+
+    if ( !m_xOriginalStream.is() )
+        throw io::NotConnectedException();
+
+    PrepareCopy_Impl();
+
+    return m_pCopyByteReader->readSomeBytes( aData, nMaxBytesToRead );
 }
 
 
@@ -186,6 +202,7 @@ void SAL_CALL OSeekableInputWrapper::closeInput()
     }
 
     m_xCopySeek.clear();
+    m_pCopyByteReader = nullptr;
 }
 
 

include/comphelper/seekableinput.hxx
@@ -23,6 +23,7 @@
 #include <com/sun/star/io/XInputStream.hpp>
 #include <com/sun/star/io/XSeekable.hpp>
 #include <cppuhelper/implbase.hxx>
+#include <comphelper/bytereader.hxx>
 #include <comphelper/comphelperdllapi.h>
 #include <mutex>
 
@@ -37,7 +38,7 @@ class SAL_DLLPUBLIC_TEMPLATE OSeekableInputWrapper_BASE
 {};
 
 class COMPHELPER_DLLPUBLIC OSeekableInputWrapper final
-    : public OSeekableInputWrapper_BASE
+    : public OSeekableInputWrapper_BASE, public comphelper::ByteReader
 {
     std::mutex    m_aMutex;
 
@@ -47,6 +48,7 @@ class COMPHELPER_DLLPUBLIC OSeekableInputWrapper final
 
     css::uno::Reference< css::io::XInputStream > m_xCopyInput;
     css::uno::Reference< css::io::XSeekable > m_xCopySeek;
+    comphelper::ByteReader* m_pCopyByteReader { nullptr };
 
 private:
     COMPHELPER_DLLPRIVATE void PrepareCopy_Impl();
@@ -74,6 +76,8 @@ public:
     virtual sal_Int64 SAL_CALL getPosition() override;
     virtual sal_Int64 SAL_CALL getLength() override;
 
+// comphelper::ByteReader
+    virtual sal_Int32 readSomeBytes(sal_Int8* aData, sal_Int32 nBytesToRead) override;
 };
 
 }   // namespace comphelper

include/package/Inflater.hxx
@@ -51,6 +51,28 @@ public:
     sal_Int32 getLastInflateError() const { return nLastInflateError; }
 };
 
+class UNLESS_MERGELIBS(DLLPUBLIC_PACKAGE) InflaterBytes final
+{
+    typedef struct z_stream_s z_stream;
+
+    bool                    bFinished, bNeedDict;
+    sal_Int32               nOffset, nLength, nLastInflateError;
+    std::unique_ptr<z_stream>  pStream;
+    const sal_Int8*  sInBuffer;
+    sal_Int32   doInflateBytes (sal_Int8* pOutBuffer, sal_Int32 nNewOffset, sal_Int32 nNewLength);
+
+public:
+    InflaterBytes(bool bNoWrap);
+    ~InflaterBytes();
+    void setInput( const sal_Int8* pBuffer, sal_Int32 nLen );
+    bool needsDictionary() const { return bNeedDict; }
+    bool finished() const { return bFinished; }
+    sal_Int32 doInflateSegment( sal_Int8* pOutBuffer, sal_Int32 nBufLen, sal_Int32 nNewOffset, sal_Int32 nNewLength );
+    void end(  );
+
+    sal_Int32 getLastInflateError() const { return nLastInflateError; }
+};
+
 }
 
 #endif

package/inc/CRC32.hxx
@@ -35,6 +35,7 @@ public:
     sal_Int64 updateStream (css::uno::Reference < css::io::XInputStream > const & xStream);
     /// @throws css::uno::RuntimeException
     void updateSegment(const css::uno::Sequence< sal_Int8 > &b, sal_Int32 len);
+    void updateSegment(const sal_Int8* b, sal_Int32 len);
     /// @throws css::uno::RuntimeException
     void update(const css::uno::Sequence< sal_Int8 > &b);
     /// @throws css::uno::RuntimeException

package/source/xstor/ocompinstream.cxx
@@ -38,6 +38,7 @@ OInputCompStream::OInputCompStream( OWriteStream_Impl& aImpl,
 : m_pImpl( &aImpl )
 , m_xMutex( m_pImpl->m_xMutex )
 , m_xStream(std::move( xStream ))
+, m_pByteReader( dynamic_cast<comphelper::ByteReader*>(m_xStream.get()) )
 , m_aProperties( aProps )
 , m_bDisposed( false )
 , m_nStorageType( nStorageType )
@@ -47,6 +48,7 @@ OInputCompStream::OInputCompStream( OWriteStream_Impl& aImpl,
         throw uno::RuntimeException(); // just a disaster
 
     assert(m_xStream.is());
+    assert(m_pByteReader);
 }
 
 OInputCompStream::OInputCompStream( uno::Reference < io::XInputStream > xStream,
@@ -55,11 +57,13 @@ OInputCompStream::OInputCompStream( uno::Reference < io::XInputStream > xStream,
 : m_pImpl( nullptr )
 , m_xMutex( new comphelper::RefCountedMutex )
 , m_xStream(std::move( xStream ))
+, m_pByteReader( dynamic_cast<comphelper::ByteReader*>(m_xStream.get()) )
 , m_aProperties( aProps )
 , m_bDisposed( false )
 , m_nStorageType( nStorageType )
 {
     assert(m_xStream.is());
+    assert(m_pByteReader);
 }
 
 OInputCompStream::~OInputCompStream()
@@ -122,6 +126,19 @@ sal_Int32 SAL_CALL OInputCompStream::readSomeBytes( uno::Sequence< sal_Int8 >& a
     }
 
     return m_xStream->readSomeBytes( aData, nMaxBytesToRead );
+
+}
+
+sal_Int32 OInputCompStream::readSomeBytes( sal_Int8* aData, sal_Int32 nMaxBytesToRead )
+{
+    ::osl::MutexGuard aGuard( m_xMutex->GetMutex() );
+    if ( m_bDisposed )
+    {
+        SAL_INFO("package.xstor", "Disposed!");
+        throw lang::DisposedException();
+    }
+
+    return m_pByteReader->readSomeBytes( aData, nMaxBytesToRead );
 
 }
 

commit 7e71af5af43da1f7a7cb0bc4d5319f277a67738a

tdf#167936 crash when opening document from UNO pipe
regression from
    commit a6ad198d097fb4a503c8d5831d484ff46721134b
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Sat Aug 17 13:19:54 2024 +0200
    tdf#158556 use more comphelper::ByteReader

Change-Id: I2cc14d0dbb0214dcf54938796eeeebefbd86bd88
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189577
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

package/inc/ByteGrabber.hxx
@@ -33,6 +33,7 @@ class ByteGrabber final
     css::uno::Reference < css::io::XSeekable > xSeek;
     comphelper::ByteReader* mpByteReader;
     std::array<sal_Int8, 8> maBuffer;
+    css::uno::Sequence < sal_Int8 > aSequence;
 
 public:
     ByteGrabber (css::uno::Reference < css::io::XInputStream > const & xIstream);

package/source/zipapi/ByteGrabber.cxx
@@ -39,9 +39,11 @@ using namespace ::com::sun::star;
 ByteGrabber::ByteGrabber(uno::Reference  < io::XInputStream > const & xIstream)
 : xStream(xIstream)
 , xSeek (xIstream, uno::UNO_QUERY )
+, aSequence ( 4 )
 {
+    // The input stream here could be from a remote UNO connection, in which
+    // case it will not implement comphelper::ByteReader.
     mpByteReader = dynamic_cast<comphelper::ByteReader*>(xStream.get());
-    assert(mpByteReader);
 }
 
 ByteGrabber::~ByteGrabber()
@@ -53,7 +55,6 @@ void ByteGrabber::setInputStream (const uno::Reference < io::XInputStream >& xNe
     xStream = xNewStream;
     xSeek.set(xNewStream, uno::UNO_QUERY);
     mpByteReader = dynamic_cast<comphelper::ByteReader*>(xStream.get());
-    assert(mpByteReader);
 }
 
 sal_Int32 ByteGrabber::readBytes( sal_Int8* aData,
@@ -89,39 +90,85 @@ sal_Int64 ByteGrabber::getLength(  )
 
 sal_uInt16 ByteGrabber::ReadUInt16()
 {
-    if (mpByteReader->readSomeBytes(maBuffer.data(), 2) != 2)
-        return 0;
+    if (mpByteReader)
+    {
+        if (mpByteReader->readSomeBytes(maBuffer.data(), 2) != 2)
+            return 0;
 
-    return static_cast <sal_uInt16>
-           ( (maBuffer[0] & 0xFF)
-          | (maBuffer[1] & 0xFF) << 8);
+        return static_cast <sal_uInt16>
+               ( (maBuffer[0] & 0xFF)
+                 | (maBuffer[1] & 0xFF) << 8);
+    }
+    else
+    {
+        if (xStream->readBytes(aSequence, 2) != 2)
+            return 0;
+
+        const sal_Int8 *pSequence = aSequence.getConstArray();
+        return static_cast <sal_uInt16>
+               ( (pSequence[0] & 0xFF)
+                 | (pSequence[1] & 0xFF) << 8);
+    }
 }
 
 sal_uInt32 ByteGrabber::ReadUInt32()
 {
-    if (mpByteReader->readSomeBytes(maBuffer.data(), 4) != 4)
-        return 0;
+    if (mpByteReader)
+    {
+        if (mpByteReader->readSomeBytes(maBuffer.data(), 4) != 4)
+            return 0;
 
-    return static_cast < sal_uInt32 >
-            ( (maBuffer[0] & 0xFF)
-          | ( maBuffer[1] & 0xFF ) << 8
-          | ( maBuffer[2] & 0xFF ) << 16
-          | ( maBuffer[3] & 0xFF ) << 24 );
+        return static_cast < sal_uInt32 >
+                ( (maBuffer[0] & 0xFF)
+              | ( maBuffer[1] & 0xFF ) << 8
+              | ( maBuffer[2] & 0xFF ) << 16
+              | ( maBuffer[3] & 0xFF ) << 24 );
+    }
+    else
+    {
+        if (xStream->readBytes(aSequence, 4) != 4)
+            return 0;
+
+        const sal_Int8 *pSequence = aSequence.getConstArray();
+        return static_cast < sal_uInt32 >
+                ( (pSequence[0] & 0xFF)
+              | ( pSequence[1] & 0xFF ) << 8
+              | ( pSequence[2] & 0xFF ) << 16
+              | ( pSequence[3] & 0xFF ) << 24 );
+    }
 }
 
 sal_uInt64 ByteGrabber::ReadUInt64()
 {
-    if (mpByteReader->readSomeBytes(maBuffer.data(), 8) != 8)
-        return 0;
+    if (mpByteReader)
+    {
+        if (mpByteReader->readSomeBytes(maBuffer.data(), 8) != 8)
+            return 0;
 
-    return  static_cast<sal_uInt64>(maBuffer[0] & 0xFF)
-          | static_cast<sal_uInt64>(maBuffer[1] & 0xFF) << 8
-          | static_cast<sal_uInt64>(maBuffer[2] & 0xFF) << 16
-          | static_cast<sal_uInt64>(maBuffer[3] & 0xFF) << 24
-          | static_cast<sal_uInt64>(maBuffer[4] & 0xFF) << 32
-          | static_cast<sal_uInt64>(maBuffer[5] & 0xFF) << 40
-          | static_cast<sal_uInt64>(maBuffer[6] & 0xFF) << 48
-          | static_cast<sal_uInt64>(maBuffer[7] & 0xFF) << 56;
+        return  static_cast<sal_uInt64>(maBuffer[0] & 0xFF)
+              | static_cast<sal_uInt64>(maBuffer[1] & 0xFF) << 8
+              | static_cast<sal_uInt64>(maBuffer[2] & 0xFF) << 16
+              | static_cast<sal_uInt64>(maBuffer[3] & 0xFF) << 24
+              | static_cast<sal_uInt64>(maBuffer[4] & 0xFF) << 32
+              | static_cast<sal_uInt64>(maBuffer[5] & 0xFF) << 40
+              | static_cast<sal_uInt64>(maBuffer[6] & 0xFF) << 48
+              | static_cast<sal_uInt64>(maBuffer[7] & 0xFF) << 56;
+    }
+    else
+    {
+        if (xStream->readBytes(aSequence, 8) != 8)
+            return 0;
+
+        const sal_Int8 *pSequence = aSequence.getConstArray();
+        return  static_cast<sal_uInt64>(pSequence[0] & 0xFF)
+              | static_cast<sal_uInt64>(pSequence[1] & 0xFF) << 8
+              | static_cast<sal_uInt64>(pSequence[2] & 0xFF) << 16
+              | static_cast<sal_uInt64>(pSequence[3] & 0xFF) << 24
+              | static_cast<sal_uInt64>(pSequence[4] & 0xFF) << 32
+              | static_cast<sal_uInt64>(pSequence[5] & 0xFF) << 40
+              | static_cast<sal_uInt64>(pSequence[6] & 0xFF) << 48
+              | static_cast<sal_uInt64>(pSequence[7] & 0xFF) << 56;
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */