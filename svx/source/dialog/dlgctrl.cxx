LibreOffice/core

commit 01a31f689fff39dfaed8864b765b89696c361b62

no need to use unique_ptr for Bitmap
it is already a COW type

Change-Id: I779fb5758bcb7fb2ee1c8d303e67a47381fe9035
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189284
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

svx/source/dialog/dlgctrl.cxx
@@ -55,10 +55,10 @@ using namespace ::com::sun::star::accessibility;
 
 Bitmap& SvxRectCtl::GetRectBitmap()
 {
-    if( !pBitmap )
+    if( !maBitmap.IsEmpty() )
         InitRectBitmap();
 
-    return *pBitmap;
+    return maBitmap;
 }
 
 SvxRectCtl::SvxRectCtl(SvxTabPage* pPage)
@@ -89,7 +89,6 @@ void SvxRectCtl::SetControlSettings(RectPoint eRpt, sal_uInt16 nBorder)
 
 SvxRectCtl::~SvxRectCtl()
 {
-    pBitmap.reset();
 #if !ENABLE_WASM_STRIP_ACCESSIBILITY
     pAccContext.clear();
 #endif
@@ -120,12 +119,10 @@ void SvxRectCtl::Resize_Impl(const Size &rSize)
 
 void SvxRectCtl::InitRectBitmap()
 {
-    pBitmap.reset();
-
     const StyleSettings& rStyles = Application::GetSettings().GetStyleSettings();
     svtools::ColorConfig aColorConfig;
 
-    pBitmap.reset(new Bitmap(RID_SVXCTRL_RECTBTNS));
+    maBitmap = Bitmap(RID_SVXCTRL_RECTBTNS);
 
     // set bitmap-colors
     Color aColorAry1[7];
@@ -162,12 +159,12 @@ void SvxRectCtl::InitRectBitmap()
     }
 #endif
 
-    pBitmap->Replace( aColorAry1, aColorAry2, 7, nullptr );
+    maBitmap.Replace( aColorAry1, aColorAry2, 7, nullptr );
 }
 
 void SvxRectCtl::StyleUpdated()
 {
-    pBitmap.reset(); // forces new creating of bitmap
+    maBitmap.SetEmpty(); // forces new creating of bitmap
     CustomWidgetController::StyleUpdated();
 }
 

avmedia/source/gtk/gtkplayer.cxx
@@ -411,12 +411,12 @@ public:
 
         gsk_render_node_unref(node);
 
-        std::unique_ptr<BitmapEx> xBitmap(
+        BitmapEx aBitmap =
             vcl::bitmap::CreateFromCairoSurface(Size(m_aSize.Width, m_aSize.Height), surface));
 
         cairo_surface_destroy(surface);
 
-        return Graphic(*xBitmap).GetXGraphic();
+        return Graphic(aBitmap).GetXGraphic();
     }
 };
 }

avmedia/source/viewer/mediawindow_impl.cxx
@@ -160,8 +160,8 @@ void MediaWindowImpl::dispose()
 
     mpMediaWindow = nullptr;
 
-    mpEmptyBmp.reset();
-    mpAudioBmp.reset();
+    maEmptyBmp.SetEmpty();
+    maAudioBmp.SetEmpty();
     mpMediaWindowControl.disposeAndClear();
     mpChildWindow.disposeAndClear();
 
@@ -589,17 +589,17 @@ void MediaWindowImpl::Paint(vcl::RenderContext& rRenderContext, const tools::Rec
 
     if (!mxPlayer.is())
     {
-        if (!mpEmptyBmp)
-            mpEmptyBmp.reset(new Bitmap(AVMEDIA_BMP_EMPTYLOGO));
+        if (maEmptyBmp.IsEmpty())
+            maEmptyBmp = Bitmap(AVMEDIA_BMP_EMPTYLOGO);
 
-        pLogo = mpEmptyBmp.get();
+        pLogo = &maEmptyBmp;
     }
     else if (!mxPlayerWindow.is())
     {
-        if (!mpAudioBmp)
-            mpAudioBmp.reset(new Bitmap(AVMEDIA_BMP_AUDIOLOGO));
+        if (maAudioBmp.IsEmpty())
+            maAudioBmp = Bitmap(AVMEDIA_BMP_AUDIOLOGO);
 
-        pLogo = mpAudioBmp.get();
+        pLogo = &maAudioBmp;
     }
 
     if (!mpChildWindow)

avmedia/source/viewer/mediawindow_impl.hxx
@@ -21,6 +21,7 @@
 
 #include <vcl/transfer.hxx>
 #include <vcl/syschild.hxx>
+#include <vcl/bitmap.hxx>
 
 #include <mediacontrol.hxx>
 
@@ -32,8 +33,6 @@ namespace com::sun::star::media {
 namespace com::sun::star::uno {
     class XComponentContext;
 }
-
-class BitmapEx;
 
 namespace avmedia
 {
@@ -151,8 +150,8 @@ private:
     rtl::Reference<MediaEventListenersImpl> mxEvents;
     VclPtr<MediaChildWindow> mpChildWindow;
     VclPtr<MediaWindowControl> mpMediaWindowControl;
-    std::unique_ptr<Bitmap> mpEmptyBmp;
-    std::unique_ptr<Bitmap> mpAudioBmp;
+    Bitmap maEmptyBmp;
+    Bitmap maAudioBmp;
     const MediaItem* mpItem = nullptr;
 };
 

canvas/source/cairo/cairo_canvasbitmap.cxx
@@ -118,11 +118,11 @@ namespace cairocanvas
                 if ( !mbHasAlpha )
                     break;
 
-                BitmapEx* pBitmapEx = vcl::bitmap::CreateFromCairoSurface(
+                BitmapEx aBitmapEx = vcl::bitmap::CreateFromCairoSurface(
                                           ::Size( maSize.getWidth(), maSize.getHeight() ),
                                           getSurface()->getCairoSurface().get());
-                if (pBitmapEx)
-                    aRV <<= reinterpret_cast<sal_Int64>( pBitmapEx );
+                if (!aBitmapEx.IsEmpty())
+                    aRV <<= reinterpret_cast<sal_Int64>( new BitmapEx(aBitmapEx) );
 
                 break;
             }

filter/source/svg/svgwriter.cxx
@@ -3002,7 +3002,7 @@ void SVGActionWriter::ImplWriteBmp( const BitmapEx& rBmpEx,
     }
 
     const BitmapEx* pBitmap = &rBmpEx;
-    std::unique_ptr<BitmapEx> pNewBitmap;
+    BitmapEx aNewBitmap;
 
     // for preview we generate downscaled images (1280x720 max)
     if (mbIsPreview)
@@ -3015,9 +3015,9 @@ void SVGActionWriter::ImplWriteBmp( const BitmapEx& rBmpEx,
         {
             aSize.setWidth(aSize.getWidth() / fFactor);
             aSize.setHeight(aSize.getHeight() / fFactor);
-            pNewBitmap = std::make_unique<BitmapEx>(rBmpEx);
-            pNewBitmap->Scale(aSize);
-            pBitmap = pNewBitmap.get();
+            aNewBitmap = rBmpEx;
+            aNewBitmap.Scale(aSize);
+            pBitmap = &aNewBitmap;
         }
     }
 

include/svx/dlgctrl.hxx
@@ -81,7 +81,7 @@ private:
     Point aPtLB, aPtMB, aPtRB;
     Point aPtNew;
     RectPoint eRP, eDefRP;
-    std::unique_ptr<Bitmap> pBitmap;
+    Bitmap maBitmap;
     CTL_STATE m_nState;
     bool mbCompleteDisable : 1;
 

include/vcl/BitmapTools.hxx
@@ -60,7 +60,7 @@ void VCL_DLLPUBLIC fillWithData(sal_uInt8* pData, BitmapEx const& rBitmapEx);
 Bitmap VCL_DLLPUBLIC CreateFromData( RawBitmap && data );
 
 #if ENABLE_CAIRO_CANVAS
-VCL_DLLPUBLIC BitmapEx* CreateFromCairoSurface(Size size, cairo_surface_t* pSurface);
+VCL_DLLPUBLIC BitmapEx CreateFromCairoSurface(Size size, cairo_surface_t* pSurface);
 #endif
 
 VCL_DLLPUBLIC ::Bitmap CanvasTransformBitmap( const ::Bitmap& rBitmap,

commit 72d99207d2e1dece80d632283b9ed95d1203513f

tdf#168014 pivot point and base point control in Position&Size dialog is wrong
regression from
    commit 01a31f689fff39dfaed8864b765b89696c361b62
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Sat Aug 9 20:04:00 2025 +0200
    no need to use unique_ptr for Bitmap

Change-Id: Ie7e0972942d5835f9f5118c88f5dd2a33dfcd6f7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189992
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

svx/source/dialog/dlgctrl.cxx
@@ -55,7 +55,7 @@ using namespace ::com::sun::star::accessibility;
 
 Bitmap& SvxRectCtl::GetRectBitmap()
 {
-    if( !maBitmap.IsEmpty() )
+    if( maBitmap.IsEmpty() )
         InitRectBitmap();
 
     return maBitmap;