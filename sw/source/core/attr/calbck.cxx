LibreOffice/core

commit ca65ed8a37847b334868b8cbf05f1ba77665d5aa

move RES_FMT_CHG to SfxHint
SwUpdateAttr is re-using this constant for a similar purpose,
so rather give that its own constant to play with
RES_UPDATEATTR_FMT_CHG.

Change-Id: I5ffe2a861c44948d8c7dbdd6cf1435c913985c6c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179305
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/181266

sw/source/core/attr/calbck.cxx
@@ -111,9 +111,7 @@ void SwClient::CheckRegistrationFormat(SwFormat& rOld)
     SAL_INFO("sw.core", "reparenting " << typeid(*this).name() << " at " << this << " from " << typeid(rOld).name() << " at " << &rOld << " to "  << typeid(*pNew).name() << " at " << pNew);
     assert(pNew);
     pNew->Add(*this);
-    const SwFormatChg aOldFormat(&rOld);
-    const SwFormatChg aNewFormat(pNew);
-    const sw::LegacyModifyHint aHint(&aOldFormat, &aNewFormat);
+    const SwFormatChangeHint aHint(&rOld, pNew);
     SwClientNotify(rOld, aHint);
 }
 
@@ -291,7 +289,7 @@ sw::ClientIteratorBase* sw::ClientIteratorBase::s_pClientIters = nullptr;
 
 void SwModify::SwClientNotify(const SwModify&, const SfxHint& rHint)
 {
-    if (rHint.GetId() != SfxHintId::SwLegacyModify && rHint.GetId() != SfxHintId::SwRemoveUnoObject)
+    if (rHint.GetId() != SfxHintId::SwLegacyModify && rHint.GetId() != SfxHintId::SwRemoveUnoObject && rHint.GetId() != SfxHintId::SwRemoveUnoObject)
         return;
 
     DBG_TESTSOLARMUTEX();

sw/source/core/layout/flyincnt.cxx
@@ -100,6 +100,13 @@ void SwFlyInContentFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rH
         static_cast<const sw::AutoFormatUsedHint&>(rHint).SetUsed();
         return;
     }
+    if (rHint.GetId() == SfxHintId::SwFormatChange)
+    {
+        SwFlyFrame::SwClientNotify(rMod, rHint);
+        if(GetAnchorFrame())
+            AnchorFrame()->Prepare(PrepareHint::FlyFrameAttributesChanged, GetFormat());
+        return;
+    }
     if (rHint.GetId() != SfxHintId::SwLegacyModify)
         return;
     auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);

sw/source/core/layout/pagechg.cxx
@@ -537,34 +537,42 @@ void SwPageFrame::SwClientNotify(const SwModify& rModify, const SfxHint& rHint)
         static_cast<const sw::AutoFormatUsedHint&>(rHint).SetUsed();
         return;
     }
-    else if (rHint.GetId() == SfxHintId::SwLegacyModify)
+    else if (rHint.GetId() == SfxHintId::SwLegacyModify || rHint.GetId() == SfxHintId::SwFormatChange)
     {
-        auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);
         if(auto pSh = getRootFrame()->GetCurrShell())
             pSh->SetFirstVisPageInvalid();
 
         SwPageFrameInvFlags eInvFlags = SwPageFrameInvFlags::NONE;
-        if(pLegacy->m_pNew && RES_ATTRSET_CHG == pLegacy->m_pNew->Which())
+        if (rHint.GetId() == SfxHintId::SwLegacyModify)
         {
-            auto& rOldSetChg = *static_cast<const SwAttrSetChg*>(pLegacy->m_pOld);
-            auto& rNewSetChg = *static_cast<const SwAttrSetChg*>(pLegacy->m_pNew);
-            SfxItemIter aOIter(*rOldSetChg.GetChgSet());
-            SfxItemIter aNIter(*rNewSetChg.GetChgSet());
-            const SfxPoolItem* pOItem = aOIter.GetCurItem();
-            const SfxPoolItem* pNItem = aNIter.GetCurItem();
-            SwAttrSetChg aOldSet(rOldSetChg);
-            SwAttrSetChg aNewSet(rNewSetChg);
-            do
+            auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);
+            if(pLegacy->m_pNew && RES_ATTRSET_CHG == pLegacy->m_pNew->Which())
             {
-                UpdateAttr_(pOItem, pNItem, eInvFlags, &aOldSet, &aNewSet);
-                pOItem = aOIter.NextItem();
-                pNItem = aNIter.NextItem();
-            } while(pNItem);
-            if(aOldSet.Count() || aNewSet.Count())
-                SwLayoutFrame::SwClientNotify(rModify, sw::LegacyModifyHint(&aOldSet, &aNewSet));
+                auto& rOldSetChg = *static_cast<const SwAttrSetChg*>(pLegacy->m_pOld);
+                auto& rNewSetChg = *static_cast<const SwAttrSetChg*>(pLegacy->m_pNew);
+                SfxItemIter aOIter(*rOldSetChg.GetChgSet());
+                SfxItemIter aNIter(*rNewSetChg.GetChgSet());
+                const SfxPoolItem* pOItem = aOIter.GetCurItem();
+                const SfxPoolItem* pNItem = aNIter.GetCurItem();
+                SwAttrSetChg aOldSet(rOldSetChg);
+                SwAttrSetChg aNewSet(rNewSetChg);
+                do
+                {
+                    UpdateAttr_(pOItem, pNItem, eInvFlags, &aOldSet, &aNewSet);
+                    pOItem = aOIter.NextItem();
+                    pNItem = aNIter.NextItem();
+                } while(pNItem);
+                if(aOldSet.Count() || aNewSet.Count())
+                    SwLayoutFrame::SwClientNotify(rModify, sw::LegacyModifyHint(&aOldSet, &aNewSet));
+            }
+            else
+                UpdateAttr_(pLegacy->m_pOld, pLegacy->m_pNew, eInvFlags);
         }
-        else
-            UpdateAttr_(pLegacy->m_pOld, pLegacy->m_pNew, eInvFlags);
+        else // rHint.GetId() == SfxHintId::SwFormatChange
+        {
+            auto pChangeHint = static_cast<const SwFormatChangeHint*>(&rHint);
+            UpdateAttrForFormatChange(pChangeHint->m_pOldFormat, pChangeHint->m_pNewFormat, eInvFlags);
+        }
 
         if (eInvFlags == SwPageFrameInvFlags::NONE)
             return;
@@ -594,60 +602,6 @@ void SwPageFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
     const sal_uInt16 nWhich = pOld ? pOld->Which() : pNew ? pNew->Which() : 0;
     switch( nWhich )
     {
-        case RES_FMT_CHG:
-        {
-            // state of m_bEmptyPage needs to be determined newly
-            const bool bNewState(GetFormat() == GetFormat()->GetDoc()->GetEmptyPageFormat());
-
-            if(m_bEmptyPage != bNewState)
-            {
-                // copy new state
-                m_bEmptyPage = bNewState;
-
-                if(nullptr == GetLower())
-                {
-                    // if we were an empty page before there is not yet a BodyArea in the
-                    // form of a SwBodyFrame, see constructor
-                    SwViewShell* pSh(getRootFrame()->GetCurrShell());
-                    vcl::RenderContext* pRenderContext(pSh ? pSh->GetOut() : nullptr);
-                    Calc(pRenderContext); // so that the PrtArea is correct
-                    SwBodyFrame* pBodyFrame = new SwBodyFrame(GetFormat(), this);
-                    pBodyFrame->ChgSize(getFramePrintArea().SSize());
-                    pBodyFrame->Paste(this);
-                    pBodyFrame->InvalidatePos();
-                }
-            }
-
-            // If the frame format is changed, several things might also change:
-            // 1. columns:
-            assert(pOld && pNew); //FMT_CHG Missing Format
-            const SwFormat *const pOldFormat = static_cast<const SwFormatChg*>(pOld)->pChangedFormat;
-            const SwFormat *const pNewFormat = static_cast<const SwFormatChg*>(pNew)->pChangedFormat;
-            assert(pOldFormat && pNewFormat); //FMT_CHG Missing Format
-            const SwFormatCol &rOldCol = pOldFormat->GetCol();
-            const SwFormatCol &rNewCol = pNewFormat->GetCol();
-            if( rOldCol != rNewCol )
-            {
-                SwLayoutFrame *pB = FindBodyCont();
-                assert(pB && "Page without Body.");
-                pB->ChgColumns( rOldCol, rNewCol );
-                rInvFlags |= SwPageFrameInvFlags::CheckGrid;
-            }
-
-            // 2. header and footer:
-            const SwFormatHeader &rOldH = pOldFormat->GetHeader();
-            const SwFormatHeader &rNewH = pNewFormat->GetHeader();
-            if( rOldH != rNewH )
-                rInvFlags |= SwPageFrameInvFlags::PrepareHeader;
-
-            const SwFormatFooter &rOldF = pOldFormat->GetFooter();
-            const SwFormatFooter &rNewF = pNewFormat->GetFooter();
-            if( rOldF != rNewF )
-                rInvFlags |= SwPageFrameInvFlags::PrepareFooter;
-            CheckDirChange();
-
-            [[fallthrough]];
-        }
         case RES_FRM_SIZE:
         {
             const SwRect aOldPageFrameRect( getFrameArea() );
@@ -668,16 +622,6 @@ void SwPageFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
             }
             else if (pNew)
             {
-                const SwFormatFrameSize &rSz = nWhich == RES_FMT_CHG ?
-                        static_cast<const SwFormatChg*>(pNew)->pChangedFormat->GetFrameSize() :
-                        static_cast<const SwFormatFrameSize&>(*pNew);
-
-                {
-                    SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*this);
-                    aFrm.Height( std::max( rSz.GetHeight(), tools::Long(MINLAY) ) );
-                    aFrm.Width ( std::max( rSz.GetWidth(),  tools::Long(MINLAY) ) );
-                }
-
                 if ( GetUpper() )
                 {
                     static_cast<SwRootFrame*>(GetUpper())->CheckViewLayout( nullptr, nullptr );
@@ -743,6 +687,106 @@ void SwPageFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
         SwModify aMod;
         SwLayoutFrame::SwClientNotify(aMod, sw::LegacyModifyHint(pOld, pNew));
     }
+}
+
+void SwPageFrame::UpdateAttrForFormatChange( SwFormat* pOldFormat, SwFormat* pNewFormat,
+                             SwPageFrameInvFlags &rInvFlags )
+{
+    // state of m_bEmptyPage needs to be determined newly
+    const bool bNewState(GetFormat() == GetFormat()->GetDoc()->GetEmptyPageFormat());
+
+    if(m_bEmptyPage != bNewState)
+    {
+        // copy new state
+        m_bEmptyPage = bNewState;
+
+        if(nullptr == GetLower())
+        {
+            // if we were an empty page before there is not yet a BodyArea in the
+            // form of a SwBodyFrame, see constructor
+            SwViewShell* pSh(getRootFrame()->GetCurrShell());
+            vcl::RenderContext* pRenderContext(pSh ? pSh->GetOut() : nullptr);
+            Calc(pRenderContext); // so that the PrtArea is correct
+            SwBodyFrame* pBodyFrame = new SwBodyFrame(GetFormat(), this);
+            pBodyFrame->ChgSize(getFramePrintArea().SSize());
+            pBodyFrame->Paste(this);
+            pBodyFrame->InvalidatePos();
+        }
+    }
+
+    // If the frame format is changed, several things might also change:
+    // 1. columns:
+    assert(pOldFormat && pNewFormat); //FMT_CHG Missing Format
+    const SwFormatCol &rOldCol = pOldFormat->GetCol();
+    const SwFormatCol &rNewCol = pNewFormat->GetCol();
+    if( rOldCol != rNewCol )
+    {
+        SwLayoutFrame *pB = FindBodyCont();
+        assert(pB && "Page without Body.");
+        pB->ChgColumns( rOldCol, rNewCol );
+        rInvFlags |= SwPageFrameInvFlags::CheckGrid;
+    }
+
+    // 2. header and footer:
+    const SwFormatHeader &rOldH = pOldFormat->GetHeader();
+    const SwFormatHeader &rNewH = pNewFormat->GetHeader();
+    if( rOldH != rNewH )
+        rInvFlags |= SwPageFrameInvFlags::PrepareHeader;
+
+    const SwFormatFooter &rOldF = pOldFormat->GetFooter();
+    const SwFormatFooter &rNewF = pNewFormat->GetFooter();
+    if( rOldF != rNewF )
+        rInvFlags |= SwPageFrameInvFlags::PrepareFooter;
+    CheckDirChange();
+
+    const SwRect aOldPageFrameRect( getFrameArea() );
+    SwViewShell *pSh = getRootFrame()->GetCurrShell();
+    if( pSh && pSh->GetViewOptions()->getBrowseMode() )
+    {
+        setFrameAreaSizeValid(false);
+        // OD 28.10.2002 #97265# - Don't call <SwPageFrame::MakeAll()>
+        // Calculation of the page is not necessary, because its size is
+        // invalidated here and further invalidation is done in the
+        // calling method <SwPageFrame::Modify(..)> and probably by calling
+        // <SwLayoutFrame::SwClientNotify(..)> at the end.
+        // It can also causes inconsistences, because the lowers are
+        // adjusted, but not calculated, and a <SwPageFrame::MakeAll()> of
+        // a next page is called. This is performed on the switch to the
+        // online layout.
+        //MakeAll();
+    }
+    else if (pNewFormat)
+    {
+        const SwFormatFrameSize &rSz = pNewFormat->GetFrameSize();
+
+        {
+            SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*this);
+            aFrm.Height( std::max( rSz.GetHeight(), tools::Long(MINLAY) ) );
+            aFrm.Width ( std::max( rSz.GetWidth(),  tools::Long(MINLAY) ) );
+        }
+
+        if ( GetUpper() )
+        {
+            static_cast<SwRootFrame*>(GetUpper())->CheckViewLayout( nullptr, nullptr );
+        }
+    }
+    // cleanup Window
+    if( pSh && pSh->GetWin() && aOldPageFrameRect.HasArea() )
+    {
+        // #i9719# - consider border and shadow of
+        // page frame for determine 'old' rectangle - it's used for invalidating.
+        const bool bRightSidebar = (SidebarPosition() == sw::sidebarwindows::SidebarPosition::RIGHT);
+        SwRect aOldRectWithBorderAndShadow;
+        SwPageFrame::GetBorderAndShadowBoundRect( aOldPageFrameRect, pSh, pSh->GetOut(), aOldRectWithBorderAndShadow,
+            IsLeftShadowNeeded(), IsRightShadowNeeded(), bRightSidebar );
+        pSh->InvalidateWindows( aOldRectWithBorderAndShadow );
+    }
+    rInvFlags |= SwPageFrameInvFlags::InvalidatePrt | SwPageFrameInvFlags::SetCompletePaint;
+    if ( aOldPageFrameRect.Height() != getFrameArea().Height() )
+        rInvFlags |= SwPageFrameInvFlags::InvalidateNextPos;
+
+    SwModify aMod;
+    SwLayoutFrame::SwClientNotify(aMod, SwFormatChangeHint(pOldFormat, pNewFormat));
 }
 
 void  SwPageFrame::SetPageDesc( SwPageDesc *pNew, SwFrameFormat *pFormat )

sw/source/core/txtnode/atrfld.cxx
@@ -249,7 +249,7 @@ void SwFormatField::SwClientNotify( const SwModify& rModify, const SfxHint& rHin
         rDoc.getIDocumentContentOperations().DeleteRange( *pPaM );
         rDoc.getIDocumentContentOperations().InsertString( *pPaM, aEntry );
     }
-    else if (rHint.GetId() == SfxHintId::SwLegacyModify)
+    else if (rHint.GetId() == SfxHintId::SwLegacyModify || rHint.GetId() == SfxHintId::SwFormatChange)
     {
         if(!mpTextField)
             return;
@@ -410,6 +410,20 @@ void SwFormatField::UpdateTextNode(const SfxHint& rHint)
         CallSwClientNotify(rHint);
         return;
     }
+    if (rHint.GetId() == SfxHintId::SwFormatChange)
+    {
+        auto pChangeHint = static_cast<const SwFormatChangeHint*>(&rHint);
+        if (!IsFieldInDoc())
+            return;
+
+        SwTextNode* pTextNd = &mpTextField->GetTextNode();
+        OSL_ENSURE(pTextNd, "Where is my Node?");
+
+        bool bTriggerNode = pChangeHint->m_pNewFormat != nullptr;
+        if(bTriggerNode)
+            pTextNd->TriggerNodeUpdate(*pChangeHint);
+        return;
+    }
     if(SfxHintId::SwLegacyModify != rHint.GetId())
         return;
     auto pLegacy = static_cast<const sw::LegacyModifyHint*>(&rHint);
@@ -438,7 +452,6 @@ void SwFormatField::UpdateTextNode(const SfxHint& rHint)
         switch(pNew->Which())
         {
         case RES_ATTRSET_CHG:
-        case RES_FMT_CHG:
             break;
         default:
             {

commit 9ba5977c8f92fb130851b87d405029ec7ea0be36

tdf#167774 Writer shows font of defined style with 10,8 pt instead of 9 pt
regression from
    commit ca65ed8a37847b334868b8cbf05f1ba77665d5aa
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Tue Dec 24 12:28:29 2024 +0200
    move RES_FMT_CHG to SfxHint

The important fix is in SwModify::SwClientNotify.

The other changes are small things I picked up when going over the original patch, which
likely don't make much different, but better safe than sorry.

Change-Id: I65c79b60740dc7f1b711522db8d8682c69d632e1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189998
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sw/source/core/attr/calbck.cxx
@@ -253,6 +253,7 @@ void SwModify::SwClientNotify(const SwModify&, const SfxHint& rHint)
 {
     if (rHint.GetId() != SfxHintId::SwLegacyModify
         && rHint.GetId() != SfxHintId::SwRemoveUnoObject
+        && rHint.GetId() != SfxHintId::SwFormatChange
         && rHint.GetId() != SfxHintId::SwAttrSetChange
         && rHint.GetId() != SfxHintId::SwObjectDying
         && rHint.GetId() != SfxHintId::SwUpdateAttr)

sw/source/core/layout/flyincnt.cxx
@@ -102,9 +102,13 @@ void SwFlyInContentFrame::SwClientNotify(const SwModify& rMod, const SfxHint& rH
     }
     if (rHint.GetId() == SfxHintId::SwFormatChange)
     {
-        SwFlyFrame::SwClientNotify(rMod, rHint);
-        if(GetAnchorFrame())
-            AnchorFrame()->Prepare(PrepareHint::FlyFrameAttributesChanged, GetFormat());
+        auto pFormatChangeHint = static_cast<const SwFormatChangeHint*>(&rHint);
+        if (pFormatChangeHint->m_pNewFormat)
+        {
+            SwFlyFrame::SwClientNotify(rMod, rHint);
+            if(GetAnchorFrame())
+                AnchorFrame()->Prepare(PrepareHint::FlyFrameAttributesChanged, GetFormat());
+        }
         return;
     }
     if (rHint.GetId() == SfxHintId::SwAttrSetChange)

sw/source/core/layout/pagechg.cxx
@@ -630,6 +630,14 @@ void SwPageFrame::UpdateAttr_( const SfxPoolItem *pOld, const SfxPoolItem *pNew,
             }
             else if (pNew)
             {
+                const SwFormatFrameSize &rSz = static_cast<const SwFormatFrameSize&>(*pNew);
+
+                {
+                    SwFrameAreaDefinition::FrameAreaWriteAccess aFrm(*this);
+                    aFrm.Height( std::max( rSz.GetHeight(), tools::Long(MINLAY) ) );
+                    aFrm.Width ( std::max( rSz.GetWidth(),  tools::Long(MINLAY) ) );
+                }
+
                 if ( GetUpper() )
                 {
                     static_cast<SwRootFrame*>(GetUpper())->CheckViewLayout( nullptr, nullptr );

sw/source/core/txtnode/atrfld.cxx
@@ -441,6 +441,11 @@ void SwFormatField::UpdateTextNode(const SfxHint& rHint)
     else if (rHint.GetId() == SfxHintId::SwFormatChange)
     {
         auto pChangeHint = static_cast<const SwFormatChangeHint*>(&rHint);
+        if (pChangeHint->m_pOldFormat == nullptr && pChangeHint->m_pNewFormat == nullptr)
+        {
+            ForceUpdateTextNode();
+            return;
+        }
         if (!IsFieldInDoc())
             return;
 

sw/source/core/txtnode/ndtxt.cxx
@@ -5719,46 +5719,43 @@ void SwTextNode::TriggerNodeUpdate(const sw::AttrSetChangeHint& rHint)
 
 void SwTextNode::TriggerNodeUpdate(const SwFormatChangeHint& rHint)
 {
-    assert(!rHint.m_pOldFormat || dynamic_cast<const SwTextFormatColl*>(rHint.m_pOldFormat));
-    assert(!rHint.m_pNewFormat || dynamic_cast<const SwTextFormatColl*>(rHint.m_pNewFormat));
-    const SwTextFormatColl* pTxtFmtColOld = static_cast<const SwTextFormatColl*>(
-            rHint.m_pOldFormat);
-    const SwTextFormatColl* pTxtFmtColNew = static_cast<const SwTextFormatColl*>(
-            rHint.m_pNewFormat);
+    sw::TextNodeNotificationSuppressor(*this);
+
+    // Override Modify so that deleting styles works properly (outline
+    // numbering!).
+    // Never call ChgTextCollUpdateNum for Nodes in Undo.
+    if( rHint.m_pOldFormat
+            && rHint.m_pNewFormat
+            && GetRegisteredIn() == rHint.m_pNewFormat
+            && GetNodes().IsDocNodes() )
     {
-        sw::TextNodeNotificationSuppressor(*this);
-
-        // Override Modify so that deleting styles works properly (outline
-        // numbering!).
-        // Never call ChgTextCollUpdateNum for Nodes in Undo.
-        if( GetRegisteredIn() == rHint.m_pNewFormat
-                && GetNodes().IsDocNodes() )
+        assert(dynamic_cast<const SwTextFormatColl*>(rHint.m_pNewFormat));
+        if (const SwTextFormatColl* pTxtFmtColOld = dynamic_cast<const SwTextFormatColl*>(rHint.m_pOldFormat))
         {
-            if (pTxtFmtColOld)
-            {
-                ChgTextCollUpdateNum(pTxtFmtColOld, pTxtFmtColNew);
-            }
+            ChgTextCollUpdateNum(
+                pTxtFmtColOld, static_cast<const SwTextFormatColl*>(rHint.m_pNewFormat));
         }
+    }
 
-        // reset fill information
-        if (maFillAttributes)
-        {
-            maFillAttributes.reset();
-        }
+    // reset fill information
+    if (maFillAttributes && rHint.m_pNewFormat)
+    {
+        // ..on format change (e.g. style changed)
+        maFillAttributes.reset();
+    }
 
-        if ( !mbInSetOrResetAttr )
-        {
-            HandleModifyAtTextNodeFormatChange( *this );
-        }
+    if ( !mbInSetOrResetAttr )
+    {
+        HandleModifyAtTextNodeFormatChange( *this );
+    }
 
-        SwContentNode::SwClientNotify(*this, rHint);
+    SwContentNode::SwClientNotify(*this, rHint);
 
-        SwDoc& rDoc = GetDoc();
-        // #125329# - assure that text node is in document nodes array
-        if ( !rDoc.IsInDtor() && &rDoc.GetNodes() == &GetNodes() )
-        {
-            rDoc.GetNodes().UpdateOutlineNode(*this);
-        }
+    SwDoc& rDoc = GetDoc();
+    // #125329# - assure that text node is in document nodes array
+    if ( !rDoc.IsInDtor() && &rDoc.GetNodes() == &GetNodes() )
+    {
+        rDoc.GetNodes().UpdateOutlineNode(*this);
     }
 }
 