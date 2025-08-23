LibreOffice/core

commit 6e16e1812292236ed61b637371f70459cf993b7f

tdf#64991 exit some loops early
Change-Id: Ic3b5a8a2df5a484150edb95fcacf8960b53a6ead
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/183855
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/txtnode/thints.cxx
@@ -663,6 +663,10 @@ void SwpHints::BuildPortions( SwTextNode& rNode, SwTextAttr& rNewHint,
                 continue;
 
             sal_Int32 nOtherStart = pOther->GetStart();
+            if (nThisEnd < nOtherStart)
+                break;
+            if (nThisStart < nOtherStart)
+                continue;
             const sal_Int32 nOtherEnd = *pOther->GetEnd();
 
             // Check if start of new attribute overlaps with pOther:
@@ -734,6 +738,10 @@ void SwpHints::BuildPortions( SwTextNode& rNode, SwTextAttr& rNewHint,
                 continue;
 
             const sal_Int32 nOtherStart = pOther->GetStart();
+            if (nThisEnd < nOtherStart)
+                break;
+            if (nThisStart < nOtherStart)
+                continue;
             const sal_Int32 nOtherEnd = *pOther->End();
 
             if (nThisStart <= nOtherStart && nOtherStart <= nThisEnd)

commit 864bd25ae13c07037a818f51889cd7c578af850f

tdf#168068 Bold/italic not functioning
regression from
    commit 6e16e1812292236ed61b637371f70459cf993b7f
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Mon Apr 7 20:14:46 2025 +0200
    tdf#64991 exit some loops early

Change-Id: I9167708bb8e6d3477b711f0ce773b0f9594e91c4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190102
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/txtnode/thints.cxx
@@ -740,8 +740,6 @@ void SwpHints::BuildPortions( SwTextNode& rNode, SwTextAttr& rNewHint,
             const sal_Int32 nOtherStart = pOther->GetStart();
             if (nThisEnd < nOtherStart)
                 break;
-            if (nThisStart < nOtherStart)
-                continue;
             const sal_Int32 nOtherEnd = *pOther->End();
 
             if (nThisStart <= nOtherStart && nOtherStart <= nThisEnd)