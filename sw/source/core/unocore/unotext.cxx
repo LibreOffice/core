LibreOffice/core

commit 27e9fd67260b8ad321cf0f8722e2db8a08473927

tdf#144208 speedup doc with lots of redline (7)
reduce dynamic_cast cost

Change-Id: I724fb5cfa7961d68c7011255d4ed5c2c89974566
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/168764
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sw/source/core/unocore/unotext.cxx
@@ -970,10 +970,12 @@ SwXText::Impl::ComparePositions(
     {
         throw lang::IllegalArgumentException();
     }
+#ifdef DBG_UTIL
     if (!CheckForOwnMember(aPam1) || !CheckForOwnMember(aPam2))
     {
         throw lang::IllegalArgumentException();
     }
+#endif
 
     sal_Int16 nCompare = 0;
     SwPosition const*const pStart1 = aPam1.Start();

sw/source/core/unocore/unoobj2.cxx
@@ -1122,6 +1122,16 @@ bool XTextRangeToSwPaM( SwUnoInternalPaM & rToFill,
         const uno::Reference<text::XTextRange> & xTextRange,
         ::sw::TextRangeMode const eMode)
 {
+    SwXTextRange* pRange = dynamic_cast<SwXTextRange*>(xTextRange.get());
+    if(pRange && &pRange->GetDoc() == &rToFill.GetDoc())
+    {
+        return pRange->GetPositions(rToFill, eMode);
+    }
+    if (SwXParagraph* pPara = dynamic_cast<SwXParagraph*>(xTextRange.get()))
+    {
+        return pPara->SelectPaM(rToFill);
+    }
+
     bool bRet = false;
 
     SwXHeadFootText* pHeadText
@@ -1153,43 +1163,31 @@ bool XTextRangeToSwPaM( SwUnoInternalPaM & rToFill,
         pCursor = dynamic_cast<OTextCursorHelper*>(xTextRange.get());
     }
 
-    SwXTextRange* pRange = dynamic_cast<SwXTextRange*>(xTextRange.get());
-    if(pRange && &pRange->GetDoc() == &rToFill.GetDoc())
+    SwDoc* pDoc = nullptr;
+    const SwPaM* pUnoCursor = nullptr;
+    if (pCursor)
     {
-        bRet = pRange->GetPositions(rToFill, eMode);
+        pDoc = pCursor->GetDoc();
+        pUnoCursor = pCursor->GetPaM();
     }
-    else if (SwXParagraph* pPara = dynamic_cast<SwXParagraph*>(xTextRange.get()))
+    else if (SwXTextPortion* pPortion = dynamic_cast<SwXTextPortion*>(xTextRange.get()))
     {
-        bRet = pPara->SelectPaM(rToFill);
+        pDoc = &pPortion->GetCursor().GetDoc();
+        pUnoCursor = &pPortion->GetCursor();
     }
-    else
+    if (pUnoCursor && pDoc == &rToFill.GetDoc())
     {
-        SwDoc* pDoc = nullptr;
-        const SwPaM* pUnoCursor = nullptr;
-        if (pCursor)
+        OSL_ENSURE(!pUnoCursor->IsMultiSelection(),
+                "what to do about rings?");
+        bRet = true;
+        *rToFill.GetPoint() = *pUnoCursor->GetPoint();
+        if (pUnoCursor->HasMark())
         {
-            pDoc = pCursor->GetDoc();
-            pUnoCursor = pCursor->GetPaM();
+            rToFill.SetMark();
+            *rToFill.GetMark() = *pUnoCursor->GetMark();
         }
-        else if (SwXTextPortion* pPortion = dynamic_cast<SwXTextPortion*>(xTextRange.get()))
-        {
-            pDoc = &pPortion->GetCursor().GetDoc();
-            pUnoCursor = &pPortion->GetCursor();
-        }
-        if (pUnoCursor && pDoc == &rToFill.GetDoc())
-        {
-            OSL_ENSURE(!pUnoCursor->IsMultiSelection(),
-                    "what to do about rings?");
-            bRet = true;
-            *rToFill.GetPoint() = *pUnoCursor->GetPoint();
-            if (pUnoCursor->HasMark())
-            {
-                rToFill.SetMark();
-                *rToFill.GetMark() = *pUnoCursor->GetMark();
-            }
-            else
-                rToFill.DeleteMark();
-        }
+        else
+            rToFill.DeleteMark();
     }
     return bRet;
 }

commit 14e542d58778963df461965f129286fc75459d3b

tdf#168008 XTextRangeCompare doesn't throw when ranges are in different texts
regression from
    commit 27e9fd67260b8ad321cf0f8722e2db8a08473927
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Jun 13 09:01:50 2024 +0200
    tdf#144208 speedup doc with lots of redline (7)

Change-Id: I2df6a7c345735be84b459bab0bbfd874c991f6f7
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190092
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sw/source/core/unocore/unotext.cxx
@@ -914,12 +914,13 @@ sal_Int16 SwXText::ComparePositions(
     {
         throw lang::IllegalArgumentException();
     }
-#ifdef DBG_UTIL
+    // The UNO API documentation for this method says we have to throw
+    // if either aPam1 or aPam2 is not within this text, and some
+    // extensions rely on that behaviour.
     if (!CheckForOwnMember(aPam1) || !CheckForOwnMember(aPam2))
     {
         throw lang::IllegalArgumentException();
     }
-#endif
 
     sal_Int16 nCompare = 0;
     SwPosition const*const pStart1 = aPam1.Start();