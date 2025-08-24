LibreOffice/core

commit 3846485ba75c5f73fac4ab3e5bd7de582ced673b

BitmapEx->Bitmap in TBCBitMap
now that Bitmap can handle transparency

Change-Id: I4d1c68b1930682fb558dde1f46658c4107f00282
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188615
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/bitmap/dibtools.cxx
@@ -1681,6 +1681,78 @@ bool ReadDIBBitmapEx(
     return bRetval;
 }
 
+bool ReadDIBBitmapEx(
+    Bitmap& rTarget,
+    SvStream& rIStm,
+    bool bFileHeader,
+    bool bMSOFormat)
+{
+    Bitmap aBmp;
+    if (!ImplReadDIB(aBmp, nullptr, rIStm, bFileHeader, bMSOFormat) && !rIStm.GetError())
+        return false;
+
+    // base bitmap was read, set as return value and try to read alpha extra-data
+    const sal_uInt64 nStmPos(rIStm.Tell());
+    sal_uInt32 nMagic1(0);
+    sal_uInt32 nMagic2(0);
+
+    rTarget = aBmp;
+    if (rIStm.remainingSize() >= 4)
+        rIStm.ReadUInt32( nMagic1 ).ReadUInt32( nMagic2 );
+    bool bRetval = (0x25091962 == nMagic1) && (0xACB20201 == nMagic2) && !rIStm.GetError();
+
+    if(bRetval)
+    {
+        sal_uInt8 tmp = 0;
+        rIStm.ReadUChar( tmp );
+        bRetval = !rIStm.GetError();
+
+        if(bRetval)
+        {
+            switch (tmp)
+            {
+            case 2: // TransparentType::Bitmap
+                {
+                    Bitmap aMask;
+
+                    bRetval = ImplReadDIB(aMask, nullptr, rIStm, true);
+
+                    if(bRetval && !aMask.IsEmpty())
+                        rTarget = Bitmap(BitmapEx(aBmp, aMask));
+
+                    break;
+                }
+            case 1: // backwards compat for old option TransparentType::Color
+                {
+                    Color aTransparentColor;
+
+                    tools::GenericTypeSerializer aSerializer(rIStm);
+                    aSerializer.readColor(aTransparentColor);
+
+                    bRetval = rIStm.good();
+
+                    if(bRetval)
+                    {
+                        rTarget = Bitmap(BitmapEx(aBmp, aTransparentColor));
+                    }
+                    break;
+                }
+            default: break;
+            }
+        }
+    }
+
+    if(!bRetval)
+    {
+        // alpha extra data could not be read; reset, but use base bitmap as result
+        rIStm.ResetError();
+        rIStm.Seek(nStmPos);
+        bRetval = true;
+    }
+
+    return bRetval;
+}
+
 bool ReadDIBV5(
     Bitmap& rTarget,
     AlphaMask& rTargetAlpha,

filter/source/msfilter/mstoolbar.cxx
@@ -296,7 +296,7 @@ void TBCData::ImportToolBarControl( CustomToolBarImportHelper& helper, std::vect
                     TBCBitMap* pIconMask = pSpecificInfo->getIconMask();
                     if (pIconMask)
                     {
-                        const Bitmap& rMaskBase(pIconMask->getBitMap().GetBitmap());
+                        const Bitmap& rMaskBase(pIconMask->getBitMap());
                         Size aMaskSize = rMaskBase.GetSizePixel();
                         if (aMaskSize.Width() && aMaskSize.Height())
                         {

include/filter/msfilter/mstoolbar.hxx
@@ -161,7 +161,7 @@ class TBCBitMap final : public TBBase
 {
 friend class TBCBSpecific; // #FIXME hacky access, need to fix
     sal_Int32 cbDIB;
-    BitmapEx mBitMap;
+    Bitmap mBitMap;
 public:
     TBCBitMap();
     virtual ~TBCBitMap() override;
@@ -170,7 +170,7 @@ public:
     virtual void Print( FILE* ) override;
 #endif
    // #FIXME Const-ness
-    BitmapEx& getBitMap() { return mBitMap;}
+    Bitmap& getBitMap() { return mBitMap;}
 };
 
 class MSFILTER_DLLPUBLIC TBCMenuSpecific final : public TBBase

include/vcl/dibtools.hxx
@@ -49,6 +49,12 @@ bool VCL_DLLPUBLIC ReadDIBBitmapEx(
     bool bFileHeader = true,
     bool bMSOFormat = false);
 
+bool VCL_DLLPUBLIC ReadDIBBitmapEx(
+    Bitmap& rTarget,
+    SvStream& rIStm,
+    bool bFileHeader = true,
+    bool bMSOFormat = false);
+
 bool VCL_DLLPUBLIC ReadDIBV5(
     Bitmap& rTarget,
     AlphaMask& rTargetAlpha,

commit a50aad83a6782b129e897c6520c4867fab49b906

BitmapEx->Bitmap in TypeSerializer
which flushes out a bug in the ReadDIBBitmapEx function
I introduced in
    commit 3846485ba75c5f73fac4ab3e5bd7de582ced673b
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Wed Jul 30 15:32:40 2025 +0200
    BitmapEx->Bitmap in TBCBitMap

Change-Id: Id762058a803ab0f3b27a594b8e18de378e6c250c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190141
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/bitmap/dibtools.cxx
@@ -1698,7 +1698,7 @@ bool ReadDIBBitmapEx(
     bool bMSOFormat)
 {
     Bitmap aBmp;
-    if (!ImplReadDIB(aBmp, nullptr, rIStm, bFileHeader, bMSOFormat) && !rIStm.GetError())
+    if (!ImplReadDIB(aBmp, nullptr, rIStm, bFileHeader, bMSOFormat) || rIStm.GetError())
         return false;
 
     // base bitmap was read, set as return value and try to read alpha extra-data

vcl/source/gdi/TypeSerializer.cxx
@@ -208,12 +208,12 @@ void TypeSerializer::readGraphic(Graphic& rGraphic)
     }
     else
     {
-        BitmapEx aBitmapEx;
+        Bitmap aBitmap;
         const SvStreamEndian nOldFormat = mrStream.GetEndian();
 
         mrStream.SeekRel(-4);
         mrStream.SetEndian(SvStreamEndian::LITTLE);
-        ReadDIBBitmapEx(aBitmapEx, mrStream);
+        ReadDIBBitmapEx(aBitmap, mrStream);
 
         if (!mrStream.GetError())
         {
@@ -235,12 +235,12 @@ void TypeSerializer::readGraphic(Graphic& rGraphic)
 
                     // #108077# manually set loaded BmpEx to Animation
                     // (which skips loading its BmpEx if already done)
-                    aAnimation.SetBitmapEx(aBitmapEx);
+                    aAnimation.SetBitmapEx(BitmapEx(aBitmap));
                     rGraphic = Graphic(aAnimation);
                 }
                 else
                 {
-                    rGraphic = Graphic(aBitmapEx);
+                    rGraphic = Graphic(aBitmap);
                 }
             }
             else