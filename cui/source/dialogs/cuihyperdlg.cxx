c71cf60d1cee7f1d37667049084e4f0e1b4d1889
Maya Stephens <maya.stephens@collabora.com>
2025-08-28 11:40:55 +0000

    Remove redundant code when hyperlink dialog is created
    The code for creating the hyperlink dialog end up calling the same functions
    multiple times, more than we need to. Previously the code would:
     - Create an empty SID_HYPERLINK_GETLINK item
     - Create the tabs, resetting the fields on each based on this empty item
     - Swap to the last used tab
     - Switch to the active page, and reset it again
     - Get the data about the object that was clicked
     - Reset all the tabs again from having loaded a new link
     - ... and reset them all
    
    A lot of these calls can be removed. Now, we:
     - Create the tabs, but without calling reset on them
    	(because we don't have the information yet)
     - Get information about the currently selected shape
    	(This automatically calls reset on them, and
    	selects the most appropriate tab to show)
    
    Change-Id: I6a6a6964c16e8b3ba447029b4cfe0f41090ae2a8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190805
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit 7f3ef22976c0c0a793e9c5f6855626b1c57f50dc)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190974
    Tested-by: Jenkins

--- cui/source/dialogs/cuihyperdlg.cxx
+++ cui/source/dialogs/cuihyperdlg.cxx
@@ -129,7 +129,6 @@ SvxHpLinkDlg::SvxHpLinkDlg(SfxBindings* pBindings, SfxChildWindow* pChild, weld:
 
     SetInputSet (mpItemSet.get());
 
-    // insert pages
     AddTabPage(u"internet"_ustr, SvxHyperlinkInternetTp::Create);
     AddTabPage(u"mail"_ustr, SvxHyperlinkMailTp::Create);
     if (!comphelper::LibreOfficeKit::isActive())
@@ -138,21 +137,8 @@ SvxHpLinkDlg::SvxHpLinkDlg(SfxBindings* pBindings, SfxChildWindow* pChild, weld:
         AddTabPage(u"newdocument"_ustr, SvxHyperlinkNewDocTp::Create);
     }
 
-    // tdf#90496 - remember last used view in hyperlink dialog
-    SetCurPageId(msRememberedPageId);
-
-    // Init Dialog
-    Start();
-
     GetBindings().Update(SID_HYPERLINK_GETLINK);
     GetBindings().Update(SID_READONLY_MODE);
-
-    // Now that we have got the link data, we can reset
-    // all the pages with the correct information
-    assert(pSet);
-    for (std::unique_ptr<IconChoicePageData>& page: maPageList) {
-        page->xPage->Reset(*pSet);
-    }
 
     m_xResetBtn->connect_clicked( LINK( this, SvxHpLinkDlg, ResetHdl ) );
     m_xOKBtn->connect_clicked( LINK ( this, SvxHpLinkDlg, ClickOkHdl_Impl ) );
