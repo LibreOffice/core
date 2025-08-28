LibreOffice/core

commit 44e08e5b7fef8d9aa65e81e6a9a1367b177f473c

tdf#130857 qt weld: Use text col in TreeView::insert
Don't unconditionally insert the text into the first
column in QtInstanceTreeView::insert, but into the
first column that supports text according to the
underlying GtkTreeViewColumn/GtkCellRenderers in the .ui
file. (Use QtInstanceTreeView::set_text which implements
the corresponding logic.)

Together with previous commit

    Change-Id: Ic8a59b254f3bac14c3b812f00d0cbfa3740432f2
    Author: Michael Weghorn <m.weghorn@posteo.de>
    Date:   Sun Aug 3 22:17:34 2025 +0200

        tdf#130857 qt weld: Evaluate GtkCellRenderers to know supported types

this fixes the problem mentioned in previous commit

    Change-Id: Id94e22e879f712bd967431ec0f9dc34e4480e3dd
    Author: Michael Weghorn <m.weghorn@posteo.de>
    Date:   Sun Aug 3 22:15:20 2025 +0200

        tdf#130857 qt weld: Support Java "Class Path" dialog

> Currently, both the icon and the path are displayed
> in the first column of the tree view and the second
> column remains empty. This will be addressed in
> upcoming commits.

Change-Id: I16f661fb47fedb889743bb8f91b5447a21f9f88f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188892
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
Tested-by: Jenkins

vcl/qt5/QtInstanceTreeView.cxx
@@ -71,7 +71,7 @@ void QtInstanceTreeView::insert(const weld::TreeIter* pParent, int nPos, const O
         const QModelIndex aIndex = modelIndex(nPos, 0, aParentIndex);
         QStandardItem* pItem = itemFromIndex(aIndex);
         if (pStr)
-            pItem->setText(toQString(*pStr));
+            set_text(treeIter(nPos), *pStr);
         if (pId)
             pItem->setData(toQString(*pId), ROLE_ID);
 

commit f4d87639be7741cd95426bfc216f9ea51255fdca

tdf#130857 qt weld: Pass parent when setting tree entry text on insert
Take the parent into account when creating the TreeIter
passed to QtInstanceTreeView::set_text.

This makes the "Tools" -> "AutoText" show the tree view
entries as expected again in a WIP branch where support for
using native Qt widgets with SAL_VCL_QT_USE_WELDED_WIDGETS=1
is declared.

This broke previously with

    commit 44e08e5b7fef8d9aa65e81e6a9a1367b177f473c
    Author: Michael Weghorn <m.weghorn@posteo.de>
    Date:   Sun Aug 3 22:36:37 2025 +0200

        tdf#130857 qt weld: Use text col in TreeView::insert

which had resulted in top-level entries always getting used
instead of the children of the given parent.

Change-Id: I0b63ccba426e737c6bae968cd9dc8c88d8436e9e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190327
Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
Tested-by: Jenkins

vcl/qt5/QtInstanceTreeView.cxx
@@ -75,7 +75,7 @@ void QtInstanceTreeView::insert(const weld::TreeIter* pParent, int nPos, const O
         const QModelIndex aIndex = modelIndex(nPos, 0, aParentIndex);
         QStandardItem* pItem = itemFromIndex(aIndex);
         if (pStr)
-            set_text(treeIter(nPos), *pStr);
+            set_text(treeIter(nPos, aParentIndex), *pStr);
         if (pId)
             pItem->setData(toQString(*pId), ROLE_ID);
 