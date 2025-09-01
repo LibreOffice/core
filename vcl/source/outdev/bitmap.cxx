LibreOffice/core

commit 47a8ba44e619f0c80c0a1611d56f58df5f6575bd

vcl: rename parameters of lcl_BlendBitmapWithAlpha()
Change-Id: I2daff5bf4ab3c07b8f8b656999ba1afe0f9c6f4e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188798
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/outdev/bitmap.cxx
@@ -450,8 +450,8 @@ BitmapColor lcl_AlphaBlend(int nX, int nY,
 
 Bitmap lcl_BlendBitmapWithAlpha(
             Bitmap&             aBmp,
-            BitmapReadAccess const *   pP,
-            BitmapReadAccess const *   pA,
+            BitmapReadAccess const* pSrcBmp,
+            BitmapReadAccess const* pSrcAlphaBmp,
             const sal_Int32     nDstHeight,
             const sal_Int32     nDstWidth,
             const sal_Int32*    pMapX,
@@ -461,24 +461,25 @@ Bitmap lcl_BlendBitmapWithAlpha(
     Bitmap      res;
 
     {
-        BitmapScopedWriteAccess pB(aBmp);
-        if (pB && pP && pA)
+        BitmapScopedWriteAccess pDstBmp(aBmp);
+        if (pDstBmp && pSrcBmp && pSrcAlphaBmp)
         {
             for( int nY = 0; nY < nDstHeight; nY++ )
             {
                 const tools::Long  nMapY = pMapY[ nY ];
-                Scanline pScanlineB = pB->GetScanline(nY);
+                Scanline pScanlineB = pSrcBmp->GetScanline(nY);
 
                 for( int nX = 0; nX < nDstWidth; nX++ )
                 {
                     const tools::Long nMapX = pMapX[ nX ];
-                    BitmapColor aDstCol = lcl_AlphaBlend(nX, nY, nMapX, nMapY, pP, pA, pB.get());
+                    BitmapColor aDstCol = lcl_AlphaBlend(nX, nY, nMapX, nMapY, pSrcBmp, pSrcAlphaBmp, pDstBmp.get());
 
-                    pB->SetPixelOnData(pScanlineB, nX, aDstCol);
+                    pDstBmp->SetPixelOnData(pScanlineB, nX, aDstCol);
                 }
             }
         }
-        pB.reset();
+
+        pDstBmp.reset();
         res = aBmp;
     }
 

commit b97097dea322754a543251b810f77836ace62d2d

fix lcl_BlendBitmapWithAlpha
broken since
    commit 47a8ba44e619f0c80c0a1611d56f58df5f6575bd
    Author: Christopher Sherlock <chris.sherlock79@gmail.com>
    Date:   Sat Aug 2 15:38:19 2025 +1000
    vcl: rename parameters of lcl_BlendBitmapWithAlpha()

Change-Id: I87d3b2b8da0833120edb128a9d2589f2b5c04ac6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190471
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/outdev/bitmap.cxx
@@ -457,7 +457,7 @@ Bitmap lcl_BlendBitmapWithAlpha(
             for( int nY = 0; nY < nDstHeight; nY++ )
             {
                 const tools::Long  nMapY = pMapY[ nY ];
-                Scanline pScanlineB = pSrcBmp->GetScanline(nY);
+                Scanline pScanlineB = pDstBmp->GetScanline(nY);
 
                 for( int nX = 0; nX < nDstWidth; nX++ )
                 {