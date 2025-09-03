LibreOffice/core

commit 62f62a3b12706b1a4ce8abc2e0bc0fdb7ddfa9fe

BitmapEx->Bitmap in PngImageWriter
now that Bitmap supports transparency.

Tweak a unit test. Some color information is drifting
because we are converting between pre-multiplied
and non-pre-multiplied alpha a few times.

Change-Id: I6e9657a7d53d9e28dd6abc78ce2b2fe977a558cb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189138
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

vcl/source/filter/png/PngImageWriter.cxx
@@ -18,26 +18,20 @@
 
 namespace
 {
-void combineScanlineChannels(Scanline pColorScanline, Scanline pAlphaScanline,
+void combineScanlineChannels(BitmapScopedReadAccess& pAccess, Scanline pScanline,
                              std::vector<std::remove_pointer_t<Scanline>>& pResult,
                              sal_uInt32 nBitmapWidth, int colorType)
 {
-    if (colorType == PNG_COLOR_TYPE_GRAY_ALPHA)
-    {
-        for (sal_uInt32 i = 0; i < nBitmapWidth; ++i)
-        {
-            pResult[i * 2] = *pColorScanline++; // Gray
-            pResult[i * 2 + 1] = *pAlphaScanline++; // A
-        }
-        return;
-    }
+    assert(colorType != PNG_COLOR_TYPE_GRAY_ALPHA);
+    (void)colorType;
 
     for (sal_uInt32 i = 0; i < nBitmapWidth; ++i)
     {
-        pResult[i * 4] = *pColorScanline++; // R
-        pResult[i * 4 + 1] = *pColorScanline++; // G
-        pResult[i * 4 + 2] = *pColorScanline++; // B
-        pResult[i * 4 + 3] = *pAlphaScanline++; // A
+        BitmapColor aCol = pAccess->GetPixelFromData(pScanline, i);
+        pResult[i * 4] = aCol.GetRed(); // R
+        pResult[i * 4 + 1] = aCol.GetGreen(); // G
+        pResult[i * 4 + 2] = aCol.GetBlue(); // B
+        pResult[i * 4 + 3] = aCol.GetAlpha(); // A
     }
 }
 }
@@ -146,32 +140,19 @@ static bool pngWrite(SvStream& rStream, const Graphic& rGraphic, int nCompressio
     png_set_pHYs(pPng, pInfo, std::round(aPPM.getWidth()), std::round(aPPM.getHeight()),
                  PNG_RESOLUTION_METER);
 
-    BitmapEx aBitmapEx;
-    if (rGraphic.GetBitmapEx().getPixelFormat() == vcl::PixelFormat::N32_BPP)
+    Bitmap aBitmap(rGraphic.GetBitmapEx());
+
+    if (!bTranslucent && aBitmap.HasAlpha())
     {
-        if (!vcl::bitmap::convertBitmap32To24Plus8(rGraphic.GetBitmapExRef(), aBitmapEx))
-            return false;
-    }
-    else
-    {
-        aBitmapEx = rGraphic.GetBitmapExRef();
+        // strip out the alpha layer
+        aBitmap = BitmapEx(aBitmap).GetBitmap();
     }
 
-    if (!bTranslucent)
-    {
-        // Clear alpha channel
-        aBitmapEx.ClearAlpha();
-    }
-
-    Bitmap aBitmap;
-    AlphaMask aAlphaMask;
-    BitmapScopedReadAccess pAccess;
-    BitmapScopedReadAccess pAlphaAccess;
+    BitmapScopedReadAccess pAccess(aBitmap);
 
     if (setjmp(png_jmpbuf(pPng)))
     {
         pAccess.reset();
-        pAlphaAccess.reset();
         png_destroy_read_struct(&pPng, &pInfo, nullptr);
         return false;
     }
@@ -179,16 +160,9 @@ static bool pngWrite(SvStream& rStream, const Graphic& rGraphic, int nCompressio
     // Set our custom stream writer
     png_set_write_fn(pPng, &rStream, lclWriteStream, nullptr);
 
-    aBitmap = aBitmapEx.GetBitmap();
-    if (bTranslucent)
-        aAlphaMask = aBitmapEx.GetAlphaMask();
-
     {
         bool bCombineChannels = false;
-        pAccess = aBitmap;
-        if (bTranslucent)
-            pAlphaAccess = aAlphaMask;
-        Size aSize = aBitmapEx.GetSizePixel();
+        Size aSize = aBitmap.GetSizePixel();
 
         int bitDepth = -1;
         int colorType = -1;
@@ -207,54 +181,65 @@ static bool pngWrite(SvStream& rStream, const Graphic& rGraphic, int nCompressio
         {
             case ScanlineFormat::N1BitMsbPal:
             {
+                assert(!aBitmap.HasAlpha());
                 colorType = PNG_COLOR_TYPE_PALETTE;
                 bitDepth = 1;
                 break;
             }
             case ScanlineFormat::N8BitPal:
             {
+                assert(!aBitmap.HasAlpha());
                 // Calling aBitmap.HasGreyPalette8Bit() hits an assert when
                 // using Skia in a debug build so query the palette through
                 // the bitmap read access object.
                 if (!pAccess->HasPalette() || !pAccess->GetPalette().IsGreyPalette8Bit())
                     colorType = PNG_COLOR_TYPE_PALETTE;
                 else
-                {
                     colorType = PNG_COLOR_TYPE_GRAY;
-                    if (pAlphaAccess)
-                    {
-                        colorType = PNG_COLOR_TYPE_GRAY_ALPHA;
-                        bCombineChannels = true;
-                    }
-                }
                 bitDepth = 8;
                 break;
             }
             case ScanlineFormat::N24BitTcBgr:
             {
+                assert(!aBitmap.HasAlpha());
+                colorType = PNG_COLOR_TYPE_RGB;
+                bitDepth = 8;
                 png_set_bgr(pPng);
-                [[fallthrough]];
+                break;
             }
             case ScanlineFormat::N24BitTcRgb:
             {
+                assert(!aBitmap.HasAlpha());
                 colorType = PNG_COLOR_TYPE_RGB;
                 bitDepth = 8;
-                if (pAlphaAccess)
-                {
-                    colorType = PNG_COLOR_TYPE_RGBA;
-                    bCombineChannels = true;
-                }
                 break;
             }
             case ScanlineFormat::N32BitTcBgra:
+            {
+                assert(aBitmap.HasAlpha());
+                colorType = PNG_COLOR_TYPE_RGBA;
+                bitDepth = 8;
+                bCombineChannels = true;
+                break;
+            }
             case ScanlineFormat::N32BitTcBgrx:
             {
-                png_set_bgr(pPng);
-                [[fallthrough]];
+                assert(!aBitmap.HasAlpha());
+                colorType = PNG_COLOR_TYPE_RGBA;
+                bitDepth = 8;
+                break;
             }
             case ScanlineFormat::N32BitTcRgba:
+            {
+                assert(aBitmap.HasAlpha());
+                colorType = PNG_COLOR_TYPE_RGBA;
+                bitDepth = 8;
+                bCombineChannels = true;
+                break;
+            }
             case ScanlineFormat::N32BitTcRgbx:
             {
+                assert(!aBitmap.HasAlpha());
                 colorType = PNG_COLOR_TYPE_RGBA;
                 bitDepth = 8;
                 break;
@@ -265,9 +250,9 @@ static bool pngWrite(SvStream& rStream, const Graphic& rGraphic, int nCompressio
             }
         }
 
-        if (aBitmapEx.GetPrefMapMode().GetMapUnit() == MapUnit::Map100thMM)
+        if (aBitmap.GetPrefMapMode().GetMapUnit() == MapUnit::Map100thMM)
         {
-            Size aPrefSize(aBitmapEx.GetPrefSize());
+            Size aPrefSize(aBitmap.GetPrefSize());
             if (aPrefSize.Width() && aPrefSize.Height())
             {
                 sal_uInt32 nPrefSizeX = o3tl::convert(aSize.Width(), 100000, aPrefSize.Width());
@@ -359,11 +344,8 @@ static bool pngWrite(SvStream& rStream, const Graphic& rGraphic, int nCompressio
                     auto nBitmapWidth = pAccess->Width();
                     // Allocate enough size to fit all channels
                     aCombinedChannels.resize(nBitmapWidth * png_get_channels(pPng, pInfo));
-                    Scanline pAlphaPointer = pAlphaAccess->GetScanline(y);
-                    if (!pSourcePointer || !pAlphaPointer)
-                        return false;
                     // Combine color and alpha channels
-                    combineScanlineChannels(pSourcePointer, pAlphaPointer, aCombinedChannels,
+                    combineScanlineChannels(pAccess, pSourcePointer, aCombinedChannels,
                                             nBitmapWidth, colorType);
                     pFinalPointer = aCombinedChannels.data();
                 }

sd/qa/unit/export-tests.cxx
@@ -1427,13 +1427,10 @@ CPPUNIT_TEST_FIXTURE(SdExportTest, testTdf141269)
     CPPUNIT_ASSERT_EQUAL(tools::Long(1920), aBitmap.GetSizePixel().Width());
     CPPUNIT_ASSERT_EQUAL(tools::Long(1080), aBitmap.GetSizePixel().Height());
 
-    Color aExpectedColor(0xC2DEEA);
-    aExpectedColor.SetAlpha(0xF);
-
     // Without the fix in place, this test would have failed with
-    // - Expected: Color: R:194 G:222 B:234 A:240
-    // - Actual  : Color: R:194 G:222 B:234 A:15
-    CPPUNIT_ASSERT_EQUAL(aExpectedColor, aBitmap.GetPixelColor(960, 540));
+    // - Expected: 240
+    // - Actual  : 15
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt8>(0x0f), aBitmap.GetPixelColor(960, 540).GetAlpha());
 }
 
 CPPUNIT_TEST_FIXTURE(SdExportTest, testTdf123557)

commit 31c520c9e7645bfcebe7ab9c64291bbd8ed84774

fix bug in pngWrite
bug was introduced in
    commit 62f62a3b12706b1a4ce8abc2e0bc0fdb7ddfa9fe
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Aug 7 15:26:56 2025 +0200
    BitmapEx->Bitmap in PngImageWriter

but only started showing up after
    commit 18b488deededee4b62b3c041c8db84452ad72b55 (HEAD)
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Tue Aug 26 21:26:33 2025 +0200
    Make Bitmap::HasAlpha() more reliable

and even then, only on my local macOS build.

Change-Id: Ia0d4f7fdbca6cf93f9f7da13eb5e882d088f3e48
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190548
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/filter/png/PngImageWriter.cxx
@@ -227,6 +227,7 @@ static bool pngWrite(SvStream& rStream, const Graphic& rGraphic, int nCompressio
                 assert(!aBitmap.HasAlpha());
                 colorType = PNG_COLOR_TYPE_RGBA;
                 bitDepth = 8;
+                png_set_bgr(pPng);
                 break;
             }
             case ScanlineFormat::N32BitTcRgba: