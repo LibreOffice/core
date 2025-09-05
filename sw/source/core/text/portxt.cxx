LibreOffice/core

commit 3c53797210bf0a4e3ffb36ed2beac4d5ce229ff2

tdf#167648 sw letter spacing: implement minimum letter spacing
Implement new paragraph justification option "Minimum letter spacing"
allowing to shrink text lines more, choosing better paragraph layout
instead of hyphenation or rivers of space.

* Implement visual layout of minimum letter spacing.

* Enable Minimum letter spacing spin box on Alignment pane of the
  paragraph formatting dialog window

* Remove also desired letter spacing spin box from the alignment pane.
  It will be better to visualize CharKerning with percentage value
  instead of adding a new user interface to the same setting.

* Set minimum and maximum spin box ranges to [-100, 0] and [0, 500],
  and remove unnecessary handlers.

* offapi: extend API description with ranges of letter spacing
  [-100, 500] and word spacing ([0, 1000]).

* Add ODF unit test.

Note: hyphenated lines, lines with multiple portions haven't
been using custom letter spacing, yet.

Note: resolution of the custom letter spacing is only 1/20 point
(1 twip) yet.

Note: adjust testTdf163149 failing on a test machine, maybe
because the DOCX test document contains the not supported Arial
font, resulting small differences based on the font replacement.

Follow-up to commit f83a04c51056445bbf947a31c8c1866a5c30bef1
"tdf#167648 cui offapi xmloff sw: add DTP-feature maximum letter
spacing".

Change-Id: I0d686ccbdaa5324eaf6c4c7f6da0f37e76f60611
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190533
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/portxt.cxx
@@ -337,6 +337,33 @@ sal_uInt16 SwTextPortion::GetMaxComp(const SwTextFormatInfo& rInf) const
                : 0;
 }
 
+void SwTextPortion::SetSpacing( SwTextFormatInfo &rInf, const TextFrameIndex nBreakPos,
+                const sal_Int32 nSpaces, const sal_Int16 nWidthOf10Spaces )
+{
+    SvxAdjustItem aAdjustItem =
+        rInf.GetTextFrame()->GetTextNodeForParaProps()->GetSwAttrSet().GetAdjust();
+    // width of a single expanded space without letter spacing and glyph scaling
+    float fSpaceNormal =
+        (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nSpaces * nWidthOf10Spaces/10.0)) / nSpaces;
+    // the part to be removed: the previous width minus the maximum allowed space width
+    float fExpansionOverMax =
+        fSpaceNormal - nWidthOf10Spaces / 10.0 * aAdjustItem.GetPropWordSpacingMaximum() / 100.0;
+    ExtraSpaceSize( fExpansionOverMax > 0 ? fExpansionOverMax : 0 );
+    int nLetterCount = sal_Int32(nBreakPos) - sal_Int32(rInf.GetIdx());
+    // letter spacing/character to be added or substracted to get the desired word spacing
+    float fLetterSpacingForDesiredWordSpacing =
+        nLetterCount > 0 ? (((fSpaceNormal - nWidthOf10Spaces/10.0) * nSpaces) / nLetterCount) : 0;
+    // letter spacing/character allowed by maximum letter spacing
+    float fMaximumLetterSpacing =
+        nWidthOf10Spaces / 10.0 * aAdjustItem.GetPropLetterSpacingMaximum() / 100.0;
+    // final letter spacing/character based on the desired word spacing and maximum letter spacing
+    // TODO fix resolution applying 1/100 twips instead of 1 twip
+    SwTwips nLetterSpacing = std::min( fLetterSpacingForDesiredWordSpacing, fMaximumLetterSpacing );
+    // full width of the extra (rounded) letter spacing within the line
+    SetLetterSpacing( SwTwips(nLetterSpacing * nLetterCount) );
+    SetSpaceCount( TextFrameIndex(nSpaces) );
+}
+
 bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
 {
     // 5744: If only the hyphen does not fit anymore, we still need to wrap
@@ -379,6 +406,7 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
     bool bNoWordSpacing = aAdjustItem.GetPropWordSpacing() == 100 &&
                     aAdjustItem.GetPropWordSpacingMinimum() == 100 &&
                     aAdjustItem.GetPropWordSpacingMaximum() == 100 &&
+                    aAdjustItem.GetPropLetterSpacingMinimum() == 0 &&
                     aAdjustItem.GetPropLetterSpacingMaximum() == 0;
     // support old ODT documents, where only JustifyLinesWithShrinking was set
     bool bOldInterop = bInteropSmartJustify && bNoWordSpacing;
@@ -430,20 +458,9 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
 
             // calculate available word spacing for letter spacing, and for the word spacing indicator
             // for non-hyphenated single portion lines
-            // TODO: enable letter spacing for multiportion lines
+            // TODO: enable letter spacing for multiportion, also for hyphenated lines
             if ( !bOrigHyphenated && rInf.GetLineStart() == rInf.GetIdx() )
-            {
-                // TODO calculate correct value for letter spacing of hyphenated lines
-                float fExpansionOverMax = fSpaceNormal - nSpaceWidth/10.0 * aAdjustItem.GetPropWordSpacingMaximum()/100.0;
-                ExtraSpaceSize( rInf.GetBreakWidth() > rInf.GetLineWidth()/2 && fExpansionOverMax > 0 ? fExpansionOverMax : 0);
-                int nLetterCount = sal_Int32(pGuess->BreakPos()) - sal_Int32(rInf.GetIdx());
-                float fAvailableLetterSpacing = ((fSpaceNormal - nSpaceWidth/10.0) * nRealSpaces) / nLetterCount;
-                float fCustomLetterSpacing = nSpaceWidth/10.0 * aAdjustItem.GetPropLetterSpacingMaximum() / 100.0;
-                // TODO fix resolution applying 1/100 twips instead of 1 twip
-                SwTwips nLetterSpacing = std::min(fAvailableLetterSpacing, fCustomLetterSpacing);
-                SetLetterSpacing(SwTwips(nLetterSpacing * nLetterCount));
-                SetSpaceCount(TextFrameIndex(nRealSpaces));
-            }
+                SetSpacing(rInf, pGuess->BreakPos(), nRealSpaces, nSpaceWidth);
 
             // calculate line breaking with desired word spacing, also
             // if the desired word spacing is 100%, but there is a greater
@@ -454,23 +471,12 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                 pGuess.emplace();
                 bFull = !pGuess->Guess( *this, rInf, Height(), nSpacesInLine, aAdjustItem.GetPropWordSpacing(), nSpaceWidth );
                 sal_Int32 nSpacesInLine2 = rInf.GetLineSpaceCount( pGuess->BreakPos() );
-
-                if ( rInf.GetBreakWidth() <= rInf.GetLineWidth() )
+                if ( aAdjustItem.GetPropLetterSpacingMinimum() < 0 || rInf.GetBreakWidth() <= rInf.GetLineWidth() )
                 {
                     fSpaceNormal = (rInf.GetLineWidth() - (rInf.GetBreakWidth() - nSpacesInLine2 * nSpaceWidth/10.0))/nSpacesInLine2;
-                    // TODO: enable letter spacing for multiportion lines
+                    // TODO: enable letter spacing for multiportion, also for hyphenated lines
                     if ( !bOrigHyphenated && rInf.GetLineStart() == rInf.GetIdx() )
-                    {
-                        float fExpansionOverMax = fSpaceNormal - nSpaceWidth/10.0 * aAdjustItem.GetPropWordSpacingMaximum()/100.0;
-                        ExtraSpaceSize( rInf.GetBreakWidth() > rInf.GetLineWidth()/2 && fExpansionOverMax > 0 ? fExpansionOverMax : 0);
-                        int nLetterCount = sal_Int32(pGuess->BreakPos()) - sal_Int32(rInf.GetIdx());
-                        float fAvailableLetterSpacing = ((fSpaceNormal - nSpaceWidth/10.0) * nRealSpaces) / nLetterCount;
-                        float fCustomLetterSpacing = nSpaceWidth/10.0 * aAdjustItem.GetPropLetterSpacingMaximum() / 100.0;
-                        // TODO fix resolution applying 1/100 twips instead of 1 twip
-                        SwTwips nLetterSpacing = std::min(fAvailableLetterSpacing, fCustomLetterSpacing);
-                        SetLetterSpacing(SwTwips(nLetterSpacing * nLetterCount));
-                        SetSpaceCount(TextFrameIndex(nRealSpaces));
-                    }
+                        SetSpacing(rInf, pGuess->BreakPos(), nSpacesInLine2, nSpaceWidth);
                 }
             }
 
@@ -539,16 +545,15 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
                         if ( z1 >= z0 || bIsPortion )
                         {
                             pGuess = std::move(pGuess2);
-                            ExtraSpaceSize(0);
-                            SetLetterSpacing(0);
+                            SetSpacing(rInf, pGuess->BreakPos(), nSpacesInLineShrink, nSpaceWidth);
                             bFull = bFull2;
                         }
                     }
                     else if ( bOldInterop )
                     {
                         pGuess = std::move(pGuess2);
+                        SetSpaceCount( TextFrameIndex(nSpacesInLineShrink));
                         ExtraSpaceSize(0);
-                        SetLetterSpacing(0);
                         bFull = bFull2;
                     }
                 }
@@ -561,7 +566,6 @@ bool SwTextPortion::Format_( SwTextFormatInfo &rInf )
             {
                 ExtraShrunkWidth( pGuess->BreakWidth() );
                 ExtraSpaceSize( 0 );
-                SetLetterSpacing(0);
             }
         }
     }

cui/source/inc/paragrph.hxx
@@ -161,7 +161,6 @@ class SvxParaAlignTabPage : public SfxTabPage
 
     /// letter spacing
     std::unique_ptr<weld::Label> m_xLabelLetterSpacing;
-    std::unique_ptr<weld::MetricSpinButton> m_xLetterSpacing;
     std::unique_ptr<weld::MetricSpinButton> m_xLetterSpacingMinimum;
     std::unique_ptr<weld::MetricSpinButton> m_xLetterSpacingMaximum;
 
@@ -171,9 +170,6 @@ class SvxParaAlignTabPage : public SfxTabPage
     DECL_LINK(WordSpacingHdl_Impl, weld::MetricSpinButton&, void);
     DECL_LINK(WordSpacingMinimumHdl_Impl, weld::MetricSpinButton&, void);
     DECL_LINK(WordSpacingMaximumHdl_Impl, weld::MetricSpinButton&, void);
-    DECL_LINK(LetterSpacingHdl_Impl, weld::MetricSpinButton&, void);
-    DECL_LINK(LetterSpacingMinimumHdl_Impl, weld::MetricSpinButton&, void);
-    DECL_LINK(LetterSpacingMaximumHdl_Impl, weld::MetricSpinButton&, void);
 
     void                    UpdateExample_Impl();
 

cui/source/tabpages/paragrph.cxx
@@ -1288,7 +1288,6 @@ SvxParaAlignTabPage::SvxParaAlignTabPage(weld::Container* pPage, weld::DialogCon
     , m_xWordSpacingMinimum(m_xBuilder->weld_metric_spin_button(u"spin_WORD_SPACING_MIN"_ustr, FieldUnit::PERCENT))
     , m_xWordSpacingMaximum(m_xBuilder->weld_metric_spin_button(u"spin_WORD_SPACING_MAX"_ustr, FieldUnit::PERCENT))
     , m_xLabelLetterSpacing(m_xBuilder->weld_label(u"labelLetterSpacing"_ustr))
-    , m_xLetterSpacing(m_xBuilder->weld_metric_spin_button(u"spin_LETTER_SPACING"_ustr, FieldUnit::PERCENT))
     , m_xLetterSpacingMinimum(m_xBuilder->weld_metric_spin_button(u"spin_LETTER_SPACING_MIN"_ustr, FieldUnit::PERCENT))
     , m_xLetterSpacingMaximum(m_xBuilder->weld_metric_spin_button(u"spin_LETTER_SPACING_MAX"_ustr, FieldUnit::PERCENT))
 {
@@ -1334,12 +1333,6 @@ SvxParaAlignTabPage::SvxParaAlignTabPage(weld::Container* pPage, weld::DialogCon
     m_xWordSpacing->connect_value_changed(LINK(this, SvxParaAlignTabPage, WordSpacingHdl_Impl));
     m_xWordSpacingMinimum->connect_value_changed(LINK(this, SvxParaAlignTabPage, WordSpacingMinimumHdl_Impl));
     m_xWordSpacingMaximum->connect_value_changed(LINK(this, SvxParaAlignTabPage, WordSpacingMaximumHdl_Impl));
-
-    // Minimum <= Desired <= Maximum letter spacing
-    // apply these modifying the other values, if needed
-    m_xLetterSpacing->connect_value_changed(LINK(this, SvxParaAlignTabPage, LetterSpacingHdl_Impl));
-    m_xLetterSpacingMinimum->connect_value_changed(LINK(this, SvxParaAlignTabPage, LetterSpacingMinimumHdl_Impl));
-    m_xLetterSpacingMaximum->connect_value_changed(LINK(this, SvxParaAlignTabPage, LetterSpacingMaximumHdl_Impl));
 }
 
 SvxParaAlignTabPage::~SvxParaAlignTabPage()
@@ -1389,7 +1382,6 @@ bool SvxParaAlignTabPage::FillItemSet( SfxItemSet* rOutSet )
             m_xWordSpacing->get_value_changed_from_saved() ||
             m_xWordSpacingMinimum->get_value_changed_from_saved() ||
             m_xWordSpacingMaximum->get_value_changed_from_saved() ||
-            m_xLetterSpacing->get_value_changed_from_saved() ||
             m_xLetterSpacingMinimum->get_value_changed_from_saved() ||
             m_xLetterSpacingMaximum->get_value_changed_from_saved();
     }
@@ -1505,6 +1497,7 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
             m_xLabelLetterSpacing->set_sensitive(true);
             // TODO add LetterSpacing (CharKern) and LetterSpacingMinimum
             m_xLetterSpacingMaximum->set_sensitive(true);
+            m_xLetterSpacingMinimum->set_sensitive(true);
             m_xLetterSpacingMinimum->set_value(rAdj.GetPropLetterSpacingMinimum(), FieldUnit::PERCENT);
             m_xLetterSpacingMaximum->set_value(rAdj.GetPropLetterSpacingMaximum(), FieldUnit::PERCENT);
         }
@@ -1518,7 +1511,6 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
             m_xWordSpacingMinimum->set_sensitive(false);
             m_xWordSpacingMaximum->set_sensitive(false);
             m_xLabelLetterSpacing->set_sensitive(false);
-            m_xLetterSpacing->set_sensitive(false);
             m_xLetterSpacingMinimum->set_sensitive(false);
             m_xLetterSpacingMaximum->set_sensitive(false);
         }
@@ -1537,7 +1529,6 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
         m_xWordSpacingMinimum->set_sensitive(false);
         m_xWordSpacingMaximum->set_sensitive(false);
         m_xLabelLetterSpacing->set_sensitive(false);
-        m_xLetterSpacing->set_sensitive(false);
         m_xLetterSpacingMinimum->set_sensitive(false);
         m_xLetterSpacingMaximum->set_sensitive(false);
     }
@@ -1600,7 +1591,6 @@ void SvxParaAlignTabPage::Reset( const SfxItemSet* rSet )
     m_xWordSpacing->save_value();
     m_xWordSpacingMinimum->save_value();
     m_xWordSpacingMaximum->save_value();
-    m_xLetterSpacing->save_value();
     m_xLetterSpacingMinimum->save_value();
     m_xLetterSpacingMaximum->save_value();
 
@@ -1621,7 +1611,6 @@ void SvxParaAlignTabPage::ChangesApplied()
     m_xWordSpacing->save_value();
     m_xWordSpacingMinimum->save_value();
     m_xWordSpacingMaximum->save_value();
-    m_xLetterSpacing->save_value();
     m_xLetterSpacingMinimum->save_value();
     m_xLetterSpacingMaximum->save_value();
 }
@@ -1639,9 +1628,8 @@ IMPL_LINK_NOARG(SvxParaAlignTabPage, AlignHdl_Impl, weld::Toggleable&, void)
     m_xWordSpacingMinimum->set_sensitive(bJustify);
     m_xWordSpacingMaximum->set_sensitive(bJustify);
     m_xLabelLetterSpacing->set_sensitive(bJustify);
-    // TODO implement LetterSpacing and LetterSpaceMinimum
-    m_xLetterSpacing->set_sensitive(false);
-    m_xLetterSpacingMinimum->set_sensitive(false);
+    // TODO visualize CharKerning with percentage
+    m_xLetterSpacingMinimum->set_sensitive(bJustify);
     m_xLetterSpacingMaximum->set_sensitive(bJustify);
 
     bool bLastLineIsBlock = m_xLastLineLB->get_active() == 2;
@@ -1696,33 +1684,6 @@ IMPL_LINK_NOARG(SvxParaAlignTabPage, WordSpacingMaximumHdl_Impl, weld::MetricSpi
         m_xWordSpacingMinimum->set_value(nMaximum, FieldUnit::PERCENT);
     if (nMaximum < m_xWordSpacing->get_value(FieldUnit::PERCENT))
         m_xWordSpacing->set_value(nMaximum, FieldUnit::PERCENT);
-}
-
-IMPL_LINK_NOARG(SvxParaAlignTabPage, LetterSpacingHdl_Impl, weld::MetricSpinButton&, void)
-{
-    sal_Int16 nDesired = m_xLetterSpacing->get_value(FieldUnit::PERCENT);
-    if (nDesired < m_xLetterSpacingMinimum->get_value(FieldUnit::PERCENT))
-        m_xLetterSpacingMinimum->set_value(nDesired, FieldUnit::PERCENT);
-    if (nDesired > m_xLetterSpacingMaximum->get_value(FieldUnit::PERCENT))
-        m_xLetterSpacingMaximum->set_value(nDesired, FieldUnit::PERCENT);
-}
-
-IMPL_LINK_NOARG(SvxParaAlignTabPage, LetterSpacingMinimumHdl_Impl, weld::MetricSpinButton&, void)
-{
-    sal_Int16 nMinimum = m_xLetterSpacingMinimum->get_value(FieldUnit::PERCENT);
-    if (nMinimum > m_xLetterSpacing->get_value(FieldUnit::PERCENT))
-        m_xLetterSpacing->set_value(nMinimum, FieldUnit::PERCENT);
-    if (nMinimum > m_xLetterSpacingMaximum->get_value(FieldUnit::PERCENT))
-        m_xLetterSpacingMaximum->set_value(nMinimum, FieldUnit::PERCENT);
-}
-
-IMPL_LINK_NOARG(SvxParaAlignTabPage, LetterSpacingMaximumHdl_Impl, weld::MetricSpinButton&, void)
-{
-    sal_Int16 nMaximum = m_xLetterSpacingMaximum->get_value(FieldUnit::PERCENT);
-    if (nMaximum < m_xLetterSpacingMinimum->get_value(FieldUnit::PERCENT))
-        m_xLetterSpacingMinimum->set_value(nMaximum, FieldUnit::PERCENT);
-    if (nMaximum < m_xLetterSpacing->get_value(FieldUnit::PERCENT))
-        m_xLetterSpacing->set_value(nMaximum, FieldUnit::PERCENT);
 }
 
 void SvxParaAlignTabPage::UpdateExample_Impl()

cui/uiconfig/ui/paragalignpage.ui
@@ -21,7 +21,7 @@
     <property name="page-increment">10</property>
   </object>
   <object class="GtkAdjustment" id="adjustmentPercent4">
-    <property name="upper">500</property>
+    <property name="upper">0</property>
     <property name="lower">-100</property>
     <property name="value">0</property>
     <property name="step-increment">1</property>
@@ -29,14 +29,7 @@
   </object>
   <object class="GtkAdjustment" id="adjustmentPercent5">
     <property name="upper">500</property>
-    <property name="lower">-100</property>
-    <property name="value">0</property>
-    <property name="step-increment">1</property>
-    <property name="page-increment">10</property>
-  </object>
-  <object class="GtkAdjustment" id="adjustmentPercent6">
-    <property name="upper">500</property>
-    <property name="lower">-100</property>
+    <property name="lower">0</property>
     <property name="value">0</property>
     <property name="step-increment">1</property>
     <property name="page-increment">10</property>
@@ -556,7 +549,7 @@
                   <object class="GtkLabel" id="labelLetterSpacing">
                     <property name="visible">True</property>
                     <property name="can-focus">False</property>
-                    <property name="label" translatable="yes" context="paragalignpage|labelWordSpacing">_Letter spacing:</property>
+                    <property name="label" translatable="yes" context="paragalignpage|labelLetterSpacing">_Letter spacing:</property>
                     <property name="use-underline">True</property>
                     <property name="xalign">0</property>
                   </object>
@@ -574,7 +567,7 @@
                     <property name="adjustment">adjustmentPercent4</property>
                     <child internal-child="accessible">
                       <object class="AtkObject" id="spin_LETTER_SPACING_MIN-atkobject">
-                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|LETTER-JUSTIFICATION-MIN">Adjusts the minimum letter spacing. Enter a number between -100% (no letter spacing) and 250% (two and a half times the width of the normal letter spacing).</property>
+                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|LETTER-JUSTIFICATION-MIN">Adjusts the minimum letter spacing. Enter a number between -100% and 0% (original letter spacing).</property>
                       </object>
                     </child>
                   </object>
@@ -584,33 +577,15 @@
                   </packing>
                 </child>
                 <child>
-                  <object class="GtkSpinButton" id="spin_LETTER_SPACING">
+                  <object class="GtkSpinButton" id="spin_LETTER_SPACING_MAX">
                     <property name="visible">True</property>
                     <property name="can-focus">True</property>
                     <property name="activates-default">True</property>
                     <property name="truncate-multiline">True</property>
                     <property name="adjustment">adjustmentPercent5</property>
                     <child internal-child="accessible">
-                      <object class="AtkObject" id="spin_LETTER_SPACING-atkobject">
-                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|JUSTIFICATION">Adjusts the desired letter spacing. Enter a number between -100% (no letter spacing) and 250% (two and a half times the width of the normal letter spacing).</property>
-                      </object>
-                    </child>
-                  </object>
-                  <packing>
-                    <property name="left-attach">2</property>
-                    <property name="top-attach">2</property>
-                  </packing>
-                </child>
-                <child>
-                  <object class="GtkSpinButton" id="spin_LETTER_SPACING_MAX">
-                    <property name="visible">True</property>
-                    <property name="can-focus">True</property>
-                    <property name="activates-default">True</property>
-                    <property name="truncate-multiline">True</property>
-                    <property name="adjustment">adjustmentPercent6</property>
-                    <child internal-child="accessible">
                       <object class="AtkObject" id="spin_LETTER_SPACING_MAX-atkobject">
-                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|JUSTIFICATION-MAX">Adjusts the maximum letter spacing. Enter a number between -100% (no letter spacing) and 250% (two and a half times the width of the normal letter spacing).</property>
+                        <property name="AtkObject::accessible-description" translatable="yes" context="paralignpage|extended_tip|LETTER-JUSTIFICATION-MAX">Adjusts the maximum letter spacing. Enter a number between 0% (original letter spacing) and 500% (letter spacing is five times the width of the space character).</property>
                       </object>
                     </child>
                   </object>

offapi/com/sun/star/style/ParagraphProperties.idl
@@ -514,6 +514,9 @@ published service ParagraphProperties
         /** specifies the desired word spacing as percentage value relative
             to the width of the space character.
 
+            <p>It takes a percent value between [0, 1000], where the original
+            word spacing is denoted by 100.</p>
+
             @see ParaWordSpacingMininum
 
             @see ParaWordSpacingMaximum
@@ -524,6 +527,9 @@ published service ParagraphProperties
 
         /** specifies the minimum word spacing as percentage value relative
             to the width of the space character.
+
+            <p>It takes a percent value between [0, 1000], where the original
+            word spacing is denoted by 100.</p>
 
             @see ParaWordSpacing
 
@@ -536,6 +542,9 @@ published service ParagraphProperties
         /** specifies the maximum word spacing as percentage value relative
             to the width of the space character.
 
+            <p>It takes a percent value between [0, 1000], where the original
+            word spacing is denoted by 100.</p>
+
             @see ParaWordSpacing
 
             @see ParaWordSpacingMininum
@@ -547,6 +556,9 @@ published service ParagraphProperties
         /** specifies the minimum letter spacing as percentage value relative
             to the width of the space character.
 
+            <p>It takes a percent value between [-100, 0], where the original
+            letter spacing is denoted by 0, and negative values mean its shrinking.</p>
+
             @see CharKerning
 
             @see ParaLetterSpacingMaximum
@@ -557,6 +569,9 @@ published service ParagraphProperties
 
         /** specifies the maximum letter spacing as percentage value relative
             to the width of the space character.
+
+            <p>It takes a percent value between [0, 500], where the original
+            letter spacing is denoted by 0, and negative values mean its shrinking.</p>
 
             @see CharKerning
 

commit 9d8a54d4b65e9f16dee05c8152cfe4bd68f0b047

tdf#167648 sw letter spacing: fix division by zero
/sw/source/core/text/portxt.cxx:351:90: runtime error: division by zero
    #0 0x7f7bf4700824 in SwTextPortion::SetSpacing(SwTextFormatInfo&, o3tl::strong_int<int, Tag_TextFrameIndex>, int, short) /sw/source/core/text/portxt.cxx:351:90

(<https://ci.libreoffice.org//job/lo_ubsan/3669/>)

Regression from commit 3c53797210bf0a4e3ffb36ed2beac4d5ce229ff2
"tdf#167648 sw letter spacing: implement minimum letter spacing".

Change-Id: I1dbea7ce855c1f4cb0604e6f9b96b875dd8eb646
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190608
Reviewed-by: László Németh <nemeth@numbertext.org>
Tested-by: Jenkins

sw/source/core/text/portxt.cxx
@@ -344,6 +344,10 @@ sal_uInt16 SwTextPortion::GetMaxComp(const SwTextFormatInfo& rInf) const
 void SwTextPortion::SetSpacing( SwTextFormatInfo &rInf, const TextFrameIndex nBreakPos,
                 const sal_Int32 nSpaces, const sal_Int16 nWidthOf10Spaces )
 {
+    // TODO allow letter spacing and glyph scaling in single word lines, too
+    if ( nSpaces == 0 )
+        return;
+
     SvxAdjustItem aAdjustItem =
         rInf.GetTextFrame()->GetTextNodeForParaProps()->GetSwAttrSet().GetAdjust();
     // width of a single expanded space without letter spacing and glyph scaling