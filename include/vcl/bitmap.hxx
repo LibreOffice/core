LibreOffice/core

commit 81260259067e706dec84542efaf7c4fc807b9a36

BitmapEx->Bitmap in various icons
now that Bitmap can handle transparency

Change-Id: I00ea1e086af3266b1ebfd7e668c49a3abd97a131
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189272
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

include/vcl/bitmap.hxx
@@ -111,7 +111,7 @@ class SAL_WARN_UNUSED VCL_DLLPUBLIC Bitmap final
 public:
 
                             Bitmap();
-                            Bitmap( const OUString& rIconName );
+    explicit                Bitmap( const OUString& rIconName );
                             Bitmap( const Bitmap& rBitmap );
     explicit                Bitmap( const BitmapEx& rBitmapEx );
                             Bitmap( const Size& rSizePixel, vcl::PixelFormat ePixelFormat, const BitmapPalette* pPal = nullptr );

avmedia/source/viewer/mediawindow.cxx
@@ -390,16 +390,16 @@ MediaWindow::grabFrame(const uno::Reference<media::XPlayer>& xPlayer,
 
             if( !aPrefSize.Width && !aPrefSize.Height )
             {
-                const BitmapEx aBmpEx(AVMEDIA_BMP_AUDIOLOGO);
-                oGraphic.emplace( aBmpEx );
+                const Bitmap aBmp(AVMEDIA_BMP_AUDIOLOGO);
+                oGraphic.emplace( aBmp );
             }
         }
     }
 
     if (!xRet.is() && !oGraphic)
     {
-        const BitmapEx aBmpEx(AVMEDIA_BMP_EMPTYLOGO);
-        oGraphic.emplace( aBmpEx );
+        const Bitmap aBmp(AVMEDIA_BMP_EMPTYLOGO);
+        oGraphic.emplace( aBmp );
     }
 
     if (oGraphic)

avmedia/source/viewer/mediawindow_impl.cxx
@@ -160,8 +160,8 @@ void MediaWindowImpl::dispose()
 
     mpMediaWindow = nullptr;
 
-    mpEmptyBmpEx.reset();
-    mpAudioBmpEx.reset();
+    mpEmptyBmp.reset();
+    mpAudioBmp.reset();
     mpMediaWindowControl.disposeAndClear();
     mpChildWindow.disposeAndClear();
 
@@ -585,21 +585,21 @@ void MediaWindowImpl::Paint(vcl::RenderContext& rRenderContext, const tools::Rec
     if (mxPlayerWindow.is())
         mxPlayerWindow->update();
 
-    BitmapEx* pLogo = nullptr;
+    Bitmap* pLogo = nullptr;
 
     if (!mxPlayer.is())
     {
-        if (!mpEmptyBmpEx)
-            mpEmptyBmpEx.reset(new BitmapEx(AVMEDIA_BMP_EMPTYLOGO));
+        if (!mpEmptyBmp)
+            mpEmptyBmp.reset(new Bitmap(AVMEDIA_BMP_EMPTYLOGO));
 
-        pLogo = mpEmptyBmpEx.get();
+        pLogo = mpEmptyBmp.get();
     }
     else if (!mxPlayerWindow.is())
     {
-        if (!mpAudioBmpEx)
-            mpAudioBmpEx.reset(new BitmapEx(AVMEDIA_BMP_AUDIOLOGO));
+        if (!mpAudioBmp)
+            mpAudioBmp.reset(new Bitmap(AVMEDIA_BMP_AUDIOLOGO));
 
-        pLogo = mpAudioBmpEx.get();
+        pLogo = mpAudioBmp.get();
     }
 
     if (!mpChildWindow)

avmedia/source/viewer/mediawindow_impl.hxx
@@ -151,8 +151,8 @@ private:
     rtl::Reference<MediaEventListenersImpl> mxEvents;
     VclPtr<MediaChildWindow> mpChildWindow;
     VclPtr<MediaWindowControl> mpMediaWindowControl;
-    std::unique_ptr<BitmapEx> mpEmptyBmpEx;
-    std::unique_ptr<BitmapEx> mpAudioBmpEx;
+    std::unique_ptr<Bitmap> mpEmptyBmp;
+    std::unique_ptr<Bitmap> mpAudioBmp;
     const MediaItem* mpItem = nullptr;
 };
 

dbaccess/source/ui/app/AppIconControl.cxx
@@ -84,7 +84,7 @@ void OApplicationIconControl::Fill()
         // E_TABLE is 0, but 0 means void so use id of enum + 1
         std::unique_ptr<ThumbnailViewItem> xItem(new ThumbnailViewItem(*this, aCategory.eType + 1));
         xItem->mbBorder = false;
-        xItem->maPreview = Bitmap(BitmapEx(aCategory.aImageResId));
+        xItem->maPreview = Bitmap(aCategory.aImageResId);
         const Size aSize = xItem->maPreview.GetSizePixel();
         m_nMaxWidth = std::max(m_nMaxWidth, aSize.Width());
         m_nMaxHeight = std::max(m_nMaxHeight, aSize.Height());

extensions/source/scanner/grid.cxx
@@ -211,7 +211,7 @@ GridDialog::GridDialog(weld::Window* pParent, double* pXValues, double* pYValues
     , m_xGridWindow(new GridWindow)
     , m_xGridWindowWND(new weld::CustomWeld(*m_xBuilder, u"gridwindow"_ustr, *m_xGridWindow))
 {
-    m_xGridWindow->Init(pXValues, pYValues, nValues, true/*bCutValues*/, BitmapEx(RID_SCANNER_HANDLE));
+    m_xGridWindow->Init(pXValues, pYValues, nValues, true/*bCutValues*/, BitmapEx(Bitmap(RID_SCANNER_HANDLE)));
     m_xResetTypeBox->set_active(0);
     m_xResetButton->connect_clicked( LINK( this, GridDialog, ClickButtonHdl ) );
 }

framework/source/uielement/recentfilesmenucontroller.cxx
@@ -281,7 +281,7 @@ void RecentFilesMenuController::fillPopupMenu(std::unique_lock<std::mutex>& /*rG
             if ( bIsIconsAllowed ) {
                 // tdf#146219: don't use SvFileInformationManager::GetImageId,
                 // which needs to access the URL to detect if it's a directory
-                BitmapEx aThumbnail(SvFileInformationManager::GetFileImageId(aURL));
+                Bitmap aThumbnail(SvFileInformationManager::GetFileImageId(aURL));
                 rPopupMenu->setItemImage(sal_uInt16(i + 1), Graphic(aThumbnail).GetXGraphic(), false);
             }
 

include/vcl/bitmapex.hxx
@@ -39,7 +39,6 @@ class SAL_WARN_UNUSED VCL_DLLPUBLIC BitmapEx
 public:
 
                         BitmapEx();
-    explicit            BitmapEx( const OUString& rIconName );
                         BitmapEx( const BitmapEx& rBitmapEx );
                         BitmapEx( const BitmapEx& rBitmapEx, Point aSrc, Size aSize );
                         BitmapEx(Size aSize, vcl::PixelFormat ePixelFormat);

commit a80777836b362c551347cfef3b9cc42b4b1c7388

tdf#168303 Option button has a rectangular border around it
regression from
    commit 81260259067e706dec84542efaf7c4fc807b9a36
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Sat Aug 9 19:29:08 2025 +0200
    BitmapEx->Bitmap in various icons

The old BitmapEx::Replace methods had the odd behaviour that
they left the alpha layer alone, so we need to replicate that
in Bitmap::Replace

Change-Id: Ic304178fb9a45750a887bee2493546bc497eeb06
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190649
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/vcl/bitmap.hxx
@@ -532,7 +532,8 @@ public:
      */
     SAL_DLLPRIVATE bool ReplaceMask( const AlphaMask& rMask, const Color& rReplaceColor );
 
-    /** Replace all pixel having the search color with the specified color
+    /** Replace all pixel having the search color with the specified color.
+        Note that, if present, alpha data is not modified.
 
         @param rSearchColor
         Color specifying which pixel should be replaced
@@ -549,7 +550,8 @@ public:
      */
     bool Replace( const Color& rSearchColor, const Color& rReplaceColor, sal_uInt8 nTol = 0 );
 
-    /** Replace all pixel having one the search colors with the corresponding replace color
+    /** Replace all pixel having one the search colors with the corresponding replace color.
+        Note that, if present, alpha data is not modified.
 
         @param pSearchColors
         Array of colors specifying which pixel should be replaced

vcl/source/bitmap/bitmappaint.cxx
@@ -987,7 +987,9 @@ bool Bitmap::Replace(const Color& rSearchColor, const Color& rReplaceColor, sal_
                     && nMaxG >= aCol.GetGreen() && nMinB <= aCol.GetBlue()
                     && nMaxB >= aCol.GetBlue())
                 {
-                    pAcc->SetPixelOnData(pScanline, nX, aReplace);
+                    BitmapColor aNewCol = aReplace;
+                    aNewCol.SetAlpha(aCol.GetAlpha());
+                    pAcc->SetPixelOnData(pScanline, nX, aNewCol);
                 }
             }
         }
@@ -1081,7 +1083,9 @@ bool Bitmap::Replace(const Color* pSearchColors, const Color* pReplaceColors, si
                         && aMinG[i] <= aCol.GetGreen() && aMaxG[i] >= aCol.GetGreen()
                         && aMinB[i] <= aCol.GetBlue() && aMaxB[i] >= aCol.GetBlue())
                     {
-                        pAcc->SetPixelOnData(pScanline, nX, aReplaces[i]);
+                        BitmapColor aNewCol = aReplaces[i];
+                        aNewCol.SetAlpha(aCol.GetAlpha());
+                        pAcc->SetPixelOnData(pScanline, nX, aNewCol);
                         break;
                     }
                 }