e8dfb72d8c1e2df652997b15326687bf18f6297b
Xisco Fauli <xiscofauli@libreoffice.org>
2025-09-10 12:22:44 +0200

    curl: patch CVE-2025-9086 and CVE-2025-10148
    More info in https://curl.se/docs/CVE-2025-9086.html
    and https://curl.se/docs/CVE-2025-10148.html
    
    Change-Id: Ib4993a00efdabf0baa3fe0e25c66d850f370572a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190741
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190752
    Reviewed-by: Taichi Haradaguchi <20001722@ymail.ne.jp>
    (cherry picked from commit 523930027bb7f3850ae0bb4628af1c5101a14de4)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201450
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- external/curl/UnpackedTarball_curl.mk
+++ external/curl/UnpackedTarball_curl.mk
@@ -25,6 +25,8 @@ $(eval $(call gb_UnpackedTarball_add_patches,curl,\
 	external/curl/curl-msvc-disable-protocols.patch.1 \
 	external/curl/zlib.patch.0 \
 	external/curl/configurable-z-option.patch.0 \
+	external/curl/0001-cookie-don-t-treat-the-leading-slash-as-trailing.patch \
+	external/curl/0001-ws-get-a-new-mask-for-each-new-outgoing-frame.patch \
 ))
 
 ifeq ($(OS)-$(COM_IS_CLANG),WNT-TRUE)
