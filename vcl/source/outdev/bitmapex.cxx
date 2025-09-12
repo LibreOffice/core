LibreOffice/core

commit e435c0749b9a66e6e8c77b969c976ad4030b60e0

drop OutputDevice::GetBitmapEx in favour of returning Bitmap
as part of the project of removing BitmapEx in favour of Bitmap, now that we can store RGBA data in Bitmap.

This mostly just moves the Bitmap->BitmapEx from inside OutputDevice to the various call sites,
from where I can make further improvements.

Change-Id: Id5366c1eb5ebca98fee7c974737552f4a09a5c2f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188341
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/outdev/bitmapex.cxx
@@ -49,6 +49,24 @@ void OutputDevice::DrawBitmapEx( const Point& rDestPt,
                  MetaActionType::BMPEX);
 }
 
+void OutputDevice::DrawBitmapEx( const Point& rDestPt,
+                                 const Bitmap& rBitmap )
+{
+    assert(!is_double_buffered_window());
+
+    if( ImplIsRecordLayout() )
+        return;
+
+    if( !rBitmap.HasAlpha() )
+    {
+        DrawBitmap(rDestPt, rBitmap);
+    }
+
+    const Size aSizePx = rBitmap.GetSizePixel();
+    DrawBitmapEx(rDestPt, PixelToLogic(aSizePx), Point(), aSizePx, rBitmap,
+                 MetaActionType::BMPEX);
+}
+
 void OutputDevice::DrawBitmapEx( const Point& rDestPt, const Size& rDestSize,
                                  const BitmapEx& rBitmapEx )
 {
@@ -65,6 +83,24 @@ void OutputDevice::DrawBitmapEx( const Point& rDestPt, const Size& rDestSize,
 
     DrawBitmapEx(rDestPt, rDestSize, Point(), rBitmapEx.GetSizePixel(),
                  rBitmapEx, MetaActionType::BMPEXSCALE);
+}
+
+void OutputDevice::DrawBitmapEx( const Point& rDestPt, const Size& rDestSize,
+                                 const Bitmap& rBitmap )
+{
+    assert(!is_double_buffered_window());
+
+    if( ImplIsRecordLayout() )
+        return;
+
+    if ( !rBitmap.HasAlpha() )
+    {
+        DrawBitmap(rDestPt, rDestSize, rBitmap);
+        return;
+    }
+
+    DrawBitmapEx(rDestPt, rDestSize, Point(), rBitmap.GetSizePixel(),
+                 rBitmap, MetaActionType::BMPEXSCALE);
 }
 
 void OutputDevice::DrawBitmapEx( const Point& rDestPt, const Size& rDestSize,
@@ -148,9 +184,64 @@ void OutputDevice::DrawBitmapEx( const Point& rDestPt, const Size& rDestSize,
     DrawDeviceBitmapEx(rDestPt, rDestSize, rSrcPtPixel, rSrcSizePixel, aBmpEx);
 }
 
-BitmapEx OutputDevice::GetBitmapEx( const Point& rSrcPt, const Size& rSize ) const
+void OutputDevice::DrawBitmapEx( const Point& rDestPt, const Size& rDestSize,
+                                 const Point& rSrcPtPixel, const Size& rSrcSizePixel,
+                                 const Bitmap& rBitmap, const MetaActionType nAction )
 {
-    return BitmapEx(GetBitmap( rSrcPt, rSize ));
+    assert(!is_double_buffered_window());
+
+    if( ImplIsRecordLayout() )
+        return;
+
+    if( !rBitmap.HasAlpha() )
+    {
+        DrawBitmap(rDestPt, rDestSize, rSrcPtPixel, rSrcSizePixel, rBitmap);
+        return;
+    }
+
+    if (RasterOp::Invert == meRasterOp)
+    {
+        DrawRect(tools::Rectangle(rDestPt, rDestSize));
+        return;
+    }
+
+    BitmapEx aBmpEx(vcl::drawmode::GetBitmapEx(BitmapEx(rBitmap), GetDrawMode()));
+
+    if (mpMetaFile)
+    {
+        switch(nAction)
+        {
+            case MetaActionType::BMPEX:
+                mpMetaFile->AddAction(new MetaBmpExAction(rDestPt, aBmpEx));
+                break;
+
+            case MetaActionType::BMPEXSCALE:
+                mpMetaFile->AddAction(new MetaBmpExScaleAction(rDestPt, rDestSize, aBmpEx));
+                break;
+
+            case MetaActionType::BMPEXSCALEPART:
+                mpMetaFile->AddAction(new MetaBmpExScalePartAction(rDestPt, rDestSize,
+                                                                   rSrcPtPixel, rSrcSizePixel, aBmpEx));
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    if (!IsDeviceOutputNecessary())
+        return;
+
+    if (!mpGraphics && !AcquireGraphics())
+        return;
+
+    if (mbInitClipRegion)
+        InitClipRegion();
+
+    if (mbOutputClipped)
+        return;
+
+    DrawDeviceBitmapEx(rDestPt, rDestSize, rSrcPtPixel, rSrcSizePixel, aBmpEx);
 }
 
 void OutputDevice::DrawDeviceBitmapEx( const Point& rDestPt, const Size& rDestSize,

commit 316c8f0d4c30fcea9352c2118ef319ab1e4d2218

fix missing return statement in OutputDevice::DrawBitmapEx
regression from
  commit e435c0749b9a66e6e8c77b969c976ad4030b60e0
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Fri Jul 25 15:06:07 2025 +0200
  drop OutputDevice::GetBitmapEx in favour of returning Bitmap

Change-Id: Ic2c638cb6a07446cfbe198a49cac742ffad0d4ee
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190732
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/outdev/bitmapex.cxx
@@ -42,6 +42,7 @@ void OutputDevice::DrawBitmapEx( const Point& rDestPt,
     if( !rBitmap.HasAlpha() )
     {
         DrawBitmap(rDestPt, rBitmap);
+        return;
     }
 
     const Size aSizePx = rBitmap.GetSizePixel();