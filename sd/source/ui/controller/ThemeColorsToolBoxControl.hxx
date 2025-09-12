1ffc0d67234306ee1f44684048d3129a8b9967a8
Samuel Mehrbrodt <samuel.mehrbrodt@collabora.com>
2025-09-12 13:10:11 +0300

    Add a Notebookbar widget for theme colors
    Co-authored-by: Banobe Pascal <banobe.pascal@collabora.com>
    Change-Id: I1becca9852745b9972d270e280f386d1803d2731
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190867
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>
    Reviewed-by: Samuel Mehrbrodt <samuel.mehrbrodt@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198151
    Tested-by: Jenkins
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200379

---
+++ sd/source/ui/controller/ThemeColorsToolBoxControl.hxx
@@ -0,0 +1,68 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <svtools/toolboxcontroller.hxx>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <vcl/vclptr.hxx>
+#include <vcl/InterimItemWindow.hxx>
+#include <svx/dialog/ThemeColorsPaneBase.hxx>
+
+namespace sd
+{
+class ViewShellBase;
+}
+
+// Used to put theme colors pane to the notebookbar
+
+class ThemeColorsPaneWrapper final : public InterimItemWindow, public svx::ThemeColorsPaneBase
+{
+private:
+    sd::ViewShellBase& mrViewShellBase;
+
+public:
+    ThemeColorsPaneWrapper(vcl::Window* pParent, sd::ViewShellBase& rBase);
+    virtual ~ThemeColorsPaneWrapper() override;
+    virtual void dispose() override;
+    void SetOptimalSize();
+
+    DECL_LINK(SelectionChangedHdl, weld::IconView&, void);
+
+protected:
+    void onColorSetActivated() override;
+};
+
+class ThemeColorsToolBoxControl final
+    : public cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>
+{
+    VclPtr<ThemeColorsPaneWrapper> m_xVclBox;
+
+public:
+    ThemeColorsToolBoxControl();
+    virtual ~ThemeColorsToolBoxControl() override;
+
+    // XStatusListener
+    virtual void SAL_CALL statusChanged(const css::frame::FeatureStateEvent& rEvent) override;
+
+    // XToolbarController
+    virtual css::uno::Reference<css::awt::XWindow>
+        SAL_CALL createItemWindow(const css::uno::Reference<css::awt::XWindow>& rParent) override;
+
+    // XComponent
+    virtual void SAL_CALL dispose() override;
+
+    // XUpdatable
+    virtual void SAL_CALL update() override;
+
+    // XServiceInfo
+    virtual OUString SAL_CALL getImplementationName() override;
+    virtual sal_Bool SAL_CALL supportsService(const OUString& rServiceName) override;
+    virtual css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
