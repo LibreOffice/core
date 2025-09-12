LibreOffice/core

commit 7d924018f3ea58050081936bde067391714a8bb5

don't use GetItemSurrogates in SwFrame::GetVirtPageNum
So lets implement this just by walking the document node structure.
I have tried to limit how much of the document this method now scans,
to avoid it also turning into an O(n^2) problem, but I might
have missed something, so
(a) the loop might be too conservative, in which case
performance could be better.
(b) the loop might not go far enough, in which case we might see
bugs with virtual page numbers.

This shaves 10% off the rendering time of large complex docx
documents.

Change-Id: I69711c65197caf278bd3ad2931c4817e72b28c5e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170451
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/trvlfrm.cxx
@@ -1868,49 +1868,44 @@ sal_uInt16 SwFrame::GetVirtPageNum() const
         return 0;
 
     sal_uInt16 nPhyPage = pPage->GetPhyPageNum();
-    if ( !static_cast<const SwRootFrame*>(pPage->GetUpper())->IsVirtPageNum() )
+    const SwRootFrame* pRootFrame = static_cast<const SwRootFrame*>(pPage->GetUpper());
+    if ( !pRootFrame->IsVirtPageNum() )
         return nPhyPage;
 
     //Search the nearest section using the virtual page number.
-    //Because searching backwards needs a lot of time we search specific using
-    //the dependencies. From the PageDescs we get the attributes and from the
-    //attributes we get the sections.
-    const SwPageFrame *pVirtPage = nullptr;
-    const SwFrame *pFrame = nullptr;
-    const SfxItemPool &rPool = pPage->GetFormat()->GetDoc()->GetAttrPool();
-    ItemSurrogates aSurrogates;
-    rPool.GetItemSurrogates(aSurrogates, RES_PAGEDESC);
-    for (const SfxPoolItem* pItem : aSurrogates)
+    const SwFrame *pFoundFrame = nullptr;
+    const SwPageFrame* pPageFrameIter = pPage;
+    while (pPageFrameIter)
     {
-        const SwFormatPageDesc *pDesc = dynamic_cast<const SwFormatPageDesc*>(pItem);
-        if ( !pDesc )
-            continue;
-
-        if ( pDesc->GetNumOffset() && pDesc->GetDefinedIn() )
+        const SwContentFrame* pContentFrame = pPageFrameIter->FindFirstBodyContent();
+        if (pContentFrame)
         {
-            auto pMod = pDesc->GetDefinedIn();
-            sw::VirtPageNumHint aHint(pPage);
-            pMod->CallSwClientNotify(aHint);
-            if(aHint.GetPage())
+            const SwFormatPageDesc& rFormatPageDesc = pContentFrame->GetPageDescItem();
+
+            if ( rFormatPageDesc.GetNumOffset() && rFormatPageDesc.GetDefinedIn() )
             {
-                if(!pVirtPage || aHint.GetPage()->GetPhyPageNum() > pVirtPage->GetPhyPageNum())
+                const sw::BroadcastingModify* pMod = rFormatPageDesc.GetDefinedIn();
+                sw::VirtPageNumHint aHint(pPage);
+                pMod->CallSwClientNotify(aHint);
+                if(aHint.GetPage())
                 {
-                    pVirtPage = aHint.GetPage();
-                    pFrame = aHint.GetFrame();
+                    pFoundFrame = aHint.GetFrame();
+                    break;
                 }
             }
         }
+        pPageFrameIter = static_cast<const SwPageFrame*>(pPageFrameIter->GetPrev());
     }
-    if ( pFrame )
+    if ( pFoundFrame )
     {
-        ::std::optional<sal_uInt16> oNumOffset = pFrame->GetPageDescItem().GetNumOffset();
+        ::std::optional<sal_uInt16> oNumOffset = pFoundFrame->GetPageDescItem().GetNumOffset();
         if (oNumOffset)
         {
-            return nPhyPage - pFrame->GetPhyPageNum() + *oNumOffset;
+            return nPhyPage - pFoundFrame->GetPhyPageNum() + *oNumOffset;
         }
         else
         {
-            return nPhyPage - pFrame->GetPhyPageNum();
+            return nPhyPage - pFoundFrame->GetPhyPageNum();
         }
     }
     return nPhyPage;

commit 46720a15474c6c6cfbbb78ff15b446fbd004120c

tdf#168355 Page break with page style with page number in table
regression from
    commit 7d924018f3ea58050081936bde067391714a8bb5
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Fri Jul 19 10:44:10 2024 +0200
    don't use GetItemSurrogates in SwFrame::GetVirtPageNum

Change-Id: Ibdc223842013ad11b56f402571f276de38a01f6a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190814
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/core/layout/trvlfrm.cxx
@@ -1888,11 +1888,35 @@ static const SwFrame* lcl_FindStartOfVirtualPages(const SwPageFrame *pPage)
                     return aHint.GetFrame();
                 }
             }
+            // might have to search tables too, they may set page number in their text flow properties
+            const SwLayoutFrame* pParentFrame = pContentFrame->GetUpper();
+            while (pParentFrame)
+            {
+                if (const SwTabFrame* pTabFrame = pParentFrame->FindTabFrame())
+                    if (const SwTable* pTable = pTabFrame->GetTable())
+                        if (const SwTableFormat* pTableFormat = pTable->GetFrameFormat())
+                        {
+                            const SwFormatPageDesc& rFormatPageDesc2 = pTableFormat->GetPageDesc();
+
+                            if ( rFormatPageDesc2.GetNumOffset() && rFormatPageDesc2.GetDefinedIn() )
+                            {
+                                const sw::BroadcastingModify* pMod = rFormatPageDesc2.GetDefinedIn();
+                                sw::VirtPageNumHint aHint(pPage);
+                                pMod->CallSwClientNotify(aHint);
+                                if(aHint.GetPage())
+                                {
+                                    return aHint.GetFrame();
+                                }
+                            }
+                        }
+                pParentFrame = pParentFrame->GetUpper();
+            }
         }
         pPageFrameIter = static_cast<const SwPageFrame*>(pPageFrameIter->GetPrev());
     }
     return nullptr;
 }
+
 /** @return the virtual page number with the offset. */
 sal_uInt16 SwFrame::GetVirtPageNum() const
 {

sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -62,6 +62,7 @@
 #include <fmtinfmt.hxx>
 #include <rootfrm.hxx>
 #include <svx/svxids.hrc>
+#include <pagefrm.hxx>
 #include <svx/svdview.hxx>
 #include <svx/svdmark.hxx>
 
@@ -1643,6 +1644,23 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf167006)
     selectShape(1);
     // attempt to edit the OLE object. This must not crash.
     getSwDocShell()->GetWrtShell()->LaunchOLEObj();
+}
+
+// tdf#168355 Page break with page style with page number in table properties is wrong
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf168355)
+{
+    createSwDoc("tdf168355.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    SwDoc* pDoc = pTextDoc->GetDocShell()->GetDoc();
+    const SwRootFrame* pLayout = pDoc->getIDocumentLayoutAccess().GetCurrentLayout();
+    const SwPageFrame* pPageFrm = static_cast<const SwPageFrame*>(pLayout->Lower());
+    CPPUNIT_ASSERT_EQUAL(sal_uInt16(1), pPageFrm->GetVirtPageNum());
+    pPageFrm = static_cast<const SwPageFrame*>(pPageFrm->GetNext());
+    CPPUNIT_ASSERT_EQUAL(sal_uInt16(2), pPageFrm->GetVirtPageNum());
+    pPageFrm = static_cast<const SwPageFrame*>(pPageFrm->GetNext());
+    // this used to be page 3
+    CPPUNIT_ASSERT_EQUAL(sal_uInt16(1), pPageFrm->GetVirtPageNum());
 }
 
 } // end of anonymous namespace