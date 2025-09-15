14c0c9afde2e6f54af35ded3cb3ae238971c784d
Michael Weghorn <m.weghorn@posteo.de>
2025-09-15 23:20:14 +0200

    tdf#130857 qt weld: Support "Target in Document" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    Sample steps to trigger the dialog:
    
    * start Writer
    * "Insert" -> "Hyperlink"
    * switch to "Documents" tab
    * press the "Target in Document" button
    
    Change-Id: I6655a9723e215d4a8db88023bac1772bce946521
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191008
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -74,6 +74,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"cui/ui/fontfragment.ui"_ustr,
         u"cui/ui/gallerytitledialog.ui"_ustr,
         u"cui/ui/graphictestdlg.ui"_ustr,
+        u"cui/ui/hyperlinkmarkdialog.ui"_ustr,
         u"cui/ui/imageviewer.ui"_ustr,
         u"cui/ui/insertrowcolumn.ui"_ustr,
         u"cui/ui/javaclasspathdialog.ui"_ustr,
