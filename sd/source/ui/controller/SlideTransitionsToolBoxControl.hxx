dc8fc20059ec3abda91e4025c34856e41cf36edd
Miklos Vajna <vmiklos@collabora.com>
2025-09-16 09:37:15 +0200

    sd, SlideTransitionsToolBoxControl: add missing headers/footers
    Missed in commit bb3316dfadd903afa090107ffbdf8f9ae4d7275b (notebookbar:
    move slide transition pane, 2025-09-15).
    
    Change-Id: Ic8630fcb03226784deda73a9cba94362e5fa5133
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191017
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sd/source/ui/controller/SlideTransitionsToolBoxControl.hxx
+++ sd/source/ui/controller/SlideTransitionsToolBoxControl.hxx
@@ -1,3 +1,12 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 #include <svtools/toolboxcontroller.hxx>
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <vcl/vclptr.hxx>
@@ -49,3 +58,5 @@ public:
     virtual sal_Bool SAL_CALL supportsService(const OUString& rServiceName) override;
     virtual css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
 };
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */
