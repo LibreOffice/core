fde51a00cc17d3bbc279b27ac730b7c9f4f0b920
Miklos Vajna <vmiklos@collabora.com>
2025-09-17 08:53:40 +0200

    sw: document SwAuthorField
    I.e. current user on the UI is an author field, not a doc info field.
    
    Change-Id: I9bb4c7e01697cbb786b9edd9ba59253053e10c2a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191081
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins

--- sw/inc/docufld.hxx
+++ sw/inc/docufld.hxx
@@ -206,6 +206,8 @@ public:
     virtual std::unique_ptr<SwFieldType> Copy() const override;
 };
 
+/// Insert -> Field -> More fields -> Document -> Author -> Name on the UI.  This expands to the
+/// name of the current user, first author would be an SwDocInfoField.
 class SAL_DLLPUBLIC_RTTI SwAuthorField final : public SwField
 {
     OUString m_aContent;
