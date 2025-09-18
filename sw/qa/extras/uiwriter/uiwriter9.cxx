dda92446b245eae9a35e50f86da3909183a54704
Michael Stahl <michael.stahl@collabora.com>
2025-09-18 16:34:45 +0200

    sw: add test for bug fixed in 7a036a64581a96a05e4833611801ac304d75524f
    Change-Id: I8a665383a2e71678e7ec1b267f8775736d09b2f9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191139
    Tested-by: Jenkins
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit c850dd1c3636f1ad8dd5ac3d3b238c9b49acf23d)

--- sw/qa/extras/uiwriter/uiwriter9.cxx
+++ sw/qa/extras/uiwriter/uiwriter9.cxx
@@ -180,6 +180,21 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testSetStringDeletesShape)
     CPPUNIT_ASSERT_EQUAL(size_t(1), pDoc->GetFlyCount(FLYCNTTYPE_FRM));
 }
 
+CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testPDFExportCrash)
+{
+    createSwDoc("section-table-section.fodt");
+
+    uno::Sequence<beans::PropertyValue> aFilterData(
+        comphelper::InitPropertySequence({ { "PDFUACompliance", uno::Any(true) } }));
+    uno::Sequence<beans::PropertyValue> aDescriptor(
+        comphelper::InitPropertySequence({ { "FilterName", uno::Any(u"writer_pdf_Export"_ustr) },
+                                           { "FilterData", uno::Any(aFilterData) },
+                                           { "URL", uno::Any(maTempFile.GetURL()) } }));
+
+    // Without the fix in place, this test would have crashed here
+    dispatchCommand(mxComponent, u".uno:ExportToPDF"_ustr, aDescriptor);
+}
+
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest9, testTdf159049)
 {
     // The document contains a shape which has a text with a line break. When copying the text to
