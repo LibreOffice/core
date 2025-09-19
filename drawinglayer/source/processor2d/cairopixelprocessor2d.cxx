LibreOffice/core

commit cd93f83bbcba0379bf39f3a4e76b955b7fa368b5

tdf#167831 avoid painting bitmap data with zero dimensions
See task, there may be bitmap-filled objects in files that
have a logical size of (0, 0), despite the bitmap having
valid data. That leads to problems *inside* cairo paint,
so I have to test and avoid that.

Change-Id: I18db3b030fb5cdf77caa5afd1bf901095fa1c67b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189135
Tested-by: Jenkins
Reviewed-by: Armin Le Grand <Armin.Le.Grand@me.com>

drawinglayer/source/processor2d/cairopixelprocessor2d.cxx
@@ -1322,6 +1322,17 @@ void CairoPixelProcessor2D::paintBitmapAlpha(const Bitmap& rBitmap,
         return;
     }
 
+    // work with dimensions in discrete target pixels to use evtl. MipMap pre-scale
+    const tools::Long nDestWidth((aLocalTransform * basegfx::B2DVector(1.0, 0.0)).getLength());
+    const tools::Long nDestHeight((aLocalTransform * basegfx::B2DVector(0.0, 1.0)).getLength());
+
+    // tdf#167831 check for output size, may have zero discrete dimension in X and/or Y
+    if (0 == nDestWidth || 0 == nDestHeight)
+    {
+        // it has and is thus invisible
+        return;
+    }
+
     if (maBColorModifierStack.count())
     {
         // need to apply ColorModifier to Bitmap data
@@ -1352,10 +1363,6 @@ void CairoPixelProcessor2D::paintBitmapAlpha(const Bitmap& rBitmap,
         SAL_WARN("drawinglayer", "SDPRCairo: No SurfaceHelper from BitmapEx (!)");
         return;
     }
-
-    // work with dimensions in discrete target pixels to use evtl. MipMap pre-scale
-    const tools::Long nDestWidth((aLocalTransform * basegfx::B2DVector(1.0, 0.0)).getLength());
-    const tools::Long nDestHeight((aLocalTransform * basegfx::B2DVector(0.0, 1.0)).getLength());
 
     cairo_surface_t* pTarget(aCairoSurfaceHelper->getCairoSurface(nDestWidth, nDestHeight));
     if (nullptr == pTarget)
@@ -2517,6 +2524,22 @@ void CairoPixelProcessor2D::processFillGraphicPrimitive2D(
         return;
     }
 
+    // work with dimensions in discrete target pixels to use evtl. MipMap pre-scale
+    const basegfx::B2DHomMatrix aLocalTransform(
+        getViewInformation2D().getObjectToViewTransformation()
+        * rFillGraphicPrimitive2D.getTransformation());
+    const tools::Long nDestWidth(
+        (aLocalTransform * basegfx::B2DVector(aFillUnitRange.getWidth(), 0.0)).getLength());
+    const tools::Long nDestHeight(
+        (aLocalTransform * basegfx::B2DVector(0.0, aFillUnitRange.getHeight())).getLength());
+
+    // tdf#167831 check for output size, may have zero discrete dimension in X and/or Y
+    if (0 == nDestWidth || 0 == nDestHeight)
+    {
+        // it has and is thus invisible
+        return;
+    }
+
     constexpr DrawModeFlags BITMAP(DrawModeFlags::BlackBitmap | DrawModeFlags::WhiteBitmap
                                    | DrawModeFlags::GrayBitmap);
     basegfx::BColor aReplacementColor(0, 0, 0);
@@ -2586,15 +2609,6 @@ void CairoPixelProcessor2D::processFillGraphicPrimitive2D(
         SAL_WARN("drawinglayer", "SDPRCairo: No SurfaceHelper from BitmapEx (!)");
         return;
     }
-
-    // work with dimensions in discrete target pixels to use evtl. MipMap pre-scale
-    const basegfx::B2DHomMatrix aLocalTransform(
-        getViewInformation2D().getObjectToViewTransformation()
-        * rFillGraphicPrimitive2D.getTransformation());
-    const tools::Long nDestWidth(
-        (aLocalTransform * basegfx::B2DVector(aFillUnitRange.getWidth(), 0.0)).getLength());
-    const tools::Long nDestHeight(
-        (aLocalTransform * basegfx::B2DVector(0.0, aFillUnitRange.getHeight())).getLength());
 
     cairo_surface_t* pTarget(aCairoSurfaceHelper->getCairoSurface(nDestWidth, nDestHeight));
     if (nullptr == pTarget)

commit e565a0d634c80a258340f73dc6f4a45cc3e180cc

tdf#168392 Disappearing image fill at lower zoom
regression from
  commit cd93f83bbcba0379bf39f3a4e76b955b7fa368b5
  Author: Armin Le Grand (collabora) <Armin.LeGrand@collabora.com>
  Date:   Thu Aug 7 21:02:41 2025 +0200
  tdf#167831 avoid painting bitmap data with zero dimensions

we cannot skip these fills if the fill bitmap is too small,
we need to fill with something or the resulting picture
is very wrong

Change-Id: I194b59414db8f675df2415c3ee4fb9a0305611b3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190981
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

drawinglayer/source/processor2d/cairopixelprocessor2d.cxx
@@ -2482,7 +2482,43 @@ void CairoPixelProcessor2D::processFillGraphicPrimitive2D(
     // tdf#167831 check for output size, may have zero discrete dimension in X and/or Y
     if (0 == nDestWidth || 0 == nDestHeight)
     {
-        // it has and is thus invisible
+        // In which case, maybe we are zoomed out far enough to make the fill bitmap less than one pixel by one pixel,
+        // and so we need to fill with a color that is an average of the bitmap's color.
+        basegfx::BColor aFillColor = aPreparedBitmap.GetAverageColor().getBColor();
+        bool bTemporaryGrayColorModifier(false);
+        const DrawModeFlags aDrawModeFlags(getViewInformation2D().getDrawModeFlags());
+        if (aDrawModeFlags & DrawModeFlags::GrayBitmap)
+        {
+            bTemporaryGrayColorModifier = true;
+            const basegfx::BColorModifierSharedPtr aBColorModifier(
+                std::make_shared<basegfx::BColorModifier_gray>());
+            maBColorModifierStack.push(aBColorModifier);
+        }
+
+        if (maBColorModifierStack.count())
+        {
+            // apply ColorModifier to Bitmap data
+            aFillColor = maBColorModifierStack.getModifiedColor(aFillColor);
+
+            if (bTemporaryGrayColorModifier)
+                // cleanup temporary BColorModifier
+                maBColorModifierStack.pop();
+        }
+
+        // draw geometry in single color using prepared ReplacementColor
+
+        // use unit geometry as fallback object geometry. Do *not*
+        // transform, the below used method will use the already
+        // correctly initialized local ViewInformation
+        basegfx::B2DPolygon aPolygon(basegfx::utils::createUnitPolygon());
+
+        // what we still need to apply is the object transform from the
+        // local primitive, that is not part of DisplayInfo yet
+        aPolygon.transform(rFillGraphicPrimitive2D.getTransformation());
+
+        // draw directly, done
+        paintPolyPolygonRGBA(basegfx::B2DPolyPolygon(aPolygon), aFillColor,
+                             rFillGraphicPrimitive2D.getTransparency());
         return;
     }
 

include/vcl/bitmap.hxx
@@ -687,6 +687,11 @@ public:
     [[nodiscard]]
     Bitmap            Modify( const basegfx::BColorModifierStack& rBColorModifierStack) const;
 
+    /** Get the average color of the entire image.
+        i.e. like scaling it down to a 1x1 pixel image.
+    */
+    Color             GetAverageColor() const;
+
     [[nodiscard]]
     static Bitmap     AutoScaleBitmap( Bitmap const & rBitmap, const tools::Long aStandardSize );
 

vcl/source/bitmap/bitmap.cxx
@@ -2141,6 +2141,40 @@ Bitmap Bitmap::Modify(const basegfx::BColorModifierStack& rBColorModifierStack) 
     return aChangedBitmap;
 }
 
+/** Get the average color of the entire image.
+    i.e. like scaling it down to a 1x1 pixel image.
+*/
+Color Bitmap::GetAverageColor() const
+{
+    BitmapScopedReadAccess xContent(*this);
+    assert(xContent);
+    if(!xContent)
+        return Color();
+
+    double fRed = 0;
+    double fGreen = 0;
+    double fBlue = 0;
+    double fAlpha = 0;
+    int nCnt = 0;
+    for(tools::Long y(0), nHeight(xContent->Height()); y < nHeight; y++)
+    {
+        Scanline pScanline = xContent->GetScanline( y );
+        for(tools::Long x(0), nWidth(xContent->Width()); x < nWidth; x++)
+        {
+            const BitmapColor aCol(xContent->GetPixelFromData(pScanline, x));
+            fRed += aCol.GetRed();
+            fGreen += aCol.GetGreen();
+            fBlue += aCol.GetBlue();
+            fAlpha += aCol.GetAlpha();
+            nCnt++;
+        }
+    }
+    return Color(ColorAlpha, static_cast<sal_uInt8>(fAlpha / nCnt),
+                   static_cast<sal_uInt8>(fRed / nCnt),
+                   static_cast<sal_uInt8>(fGreen / nCnt),
+                   static_cast<sal_uInt8>(fBlue / nCnt));
+}
+
 void Bitmap::Draw( OutputDevice* pOutDev, const Point& rDestPt ) const
 {
     pOutDev->DrawBitmapEx( rDestPt, *this );