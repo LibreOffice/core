LibreOffice/core

commit 3b89969c60e44110a07b12c92c538b310f17ab1b

sc: make sheet view have different header colors
So we can see when the sheet is using a sheet view. This also
mimics the UI of MSO when a sheet view is used. The color was
chosen arbitrarily. This has no effect in online os it's mostly
for debugging purpuse only.

Change-Id: Idb0a7c499ec4d87ea83ac5998d5ee7e17bd724d9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189915
Tested-by: Jenkins
Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

sc/source/ui/view/hdrcont.cxx
@@ -321,11 +321,13 @@ void ScHeaderControl::Paint( vcl::RenderContext& /*rRenderContext*/, const tools
     if ( nLineEnd * nLayoutSign >= nInitScrPos * nLayoutSign )
     {
         Color aFaceColor(rStyleSettings.GetFaceColor());
+        if (pTabView->GetViewData().GetSheetViewID() >= 0)
+            aFaceColor.Merge(COL_LIGHTBLUE, 220);
         if (bDark)
             aFaceColor.IncreaseLuminance(20);
         else
             aFaceColor.DecreaseLuminance(20);
-        GetOutDev()->SetFillColor( aFaceColor );
+        GetOutDev()->SetFillColor(aFaceColor);
         if ( bVertical )
             aFillRect = tools::Rectangle( 0, nInitScrPos, nBarSize-1, nLineEnd );
         else
@@ -335,7 +337,8 @@ void ScHeaderControl::Paint( vcl::RenderContext& /*rRenderContext*/, const tools
 
     if ( nLineEnd * nLayoutSign < nPEnd * nLayoutSign )
     {
-        GetOutDev()->SetFillColor( mod->GetColorConfig().GetColorValue(svtools::APPBACKGROUND).nColor );
+        Color aAppBackgroundColor = mod->GetColorConfig().GetColorValue(svtools::APPBACKGROUND).nColor;
+        GetOutDev()->SetFillColor(aAppBackgroundColor);
         if ( bVertical )
             aFillRect = tools::Rectangle( 0, nLineEnd+nLayoutSign, nBarSize-1, nPEnd );
         else

commit 67f0f999ad76ca5398e8c678efd01195bb180e65

tdf#168497: fix crash when opening data provider
Regression from 3b89969c60e44110a07b12c92c538b310f17ab1b
sc: make sheet view have different header colors

bt:
ScViewData::GetSheetViewID (this=0x8) at sc/source/ui/inc/viewdata.hxx:420
0x00007f46f3aac5e0 in ScHeaderControl::Paint (this=0x55cbb71c0500, rRect=...)
at /home/julien/lo/libreoffice/sc/source/ui/view/hdrcont.cxx:324

Change-Id: Id6554015cdfe53bbf86da50a206941a793f0457b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191281
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sc/source/ui/view/hdrcont.cxx
@@ -321,7 +321,7 @@ void ScHeaderControl::Paint( vcl::RenderContext& /*rRenderContext*/, const tools
     if ( nLineEnd * nLayoutSign >= nInitScrPos * nLayoutSign )
     {
         Color aFaceColor(rStyleSettings.GetFaceColor());
-        if (pTabView->GetViewData().GetSheetViewID() >= 0)
+        if (pTabView && pTabView->GetViewData().GetSheetViewID() >= 0)
             aFaceColor.Merge(COL_LIGHTBLUE, 220);
         if (bDark)
             aFaceColor.IncreaseLuminance(20);