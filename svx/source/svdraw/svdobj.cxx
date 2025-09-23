11fd1adad3b23b57cfc6ffde88672154b02b15b6
Noel Grandin <noel.grandin@collabora.co.uk>
2025-09-23 14:33:41 +0200

    tdf#154913 empty is a valid value for m_aOutRect
    use a separate flag to indicate whether this cached value has been calculated.
    Otherwise we calculate it every time we need it, which is very expensive.
    This makes this document considerably better to use (not perfect though)
    
    I did not change all the call sites that treat an empty m_bOutRectangleSet as "not set",
    because of the high risk of unintended consequences in this code.
    
    Change-Id: Ia147902269ef3ff02b01a295df28c6661a98aa03
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191400
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Jenkins

--- svx/source/svdraw/svdobj.cxx
+++ svx/source/svdraw/svdobj.cxx
@@ -337,6 +337,7 @@ void impRemoveIncarnatedSdrObjectToSdrModel(SdrObject& rSdrObject, SdrModel& rSd
 SdrObject::SdrObject(SdrModel& rSdrModel)
     : mpFillGeometryDefiningShape(nullptr)
     , mrSdrModelFromSdrObject(rSdrModel)
+    , m_bOutRectangleSet(false)
     , m_pUserCall(nullptr)
     , mpImpl(new Impl)
     , mpParentOfSdrObject(nullptr)
@@ -372,6 +373,7 @@ SdrObject::SdrObject(SdrModel& rSdrModel)
 SdrObject::SdrObject(SdrModel& rSdrModel, SdrObject const & rSource)
     : mpFillGeometryDefiningShape(nullptr)
     , mrSdrModelFromSdrObject(rSdrModel)
+    , m_bOutRectangleSet(false)
     , m_pUserCall(nullptr)
     , mpImpl(new Impl)
     , mpParentOfSdrObject(nullptr)
@@ -952,13 +954,12 @@ void SdrObject::SetNavigationPosition (const sal_uInt32 nNewPosition)
 // GetCurrentBoundRect().
 const tools::Rectangle& SdrObject::GetCurrentBoundRect() const
 {
-    auto const& rRectangle = getOutRectangle();
-    if (rRectangle.IsEmpty())
+    if (!isOutRectangleSet())
     {
         const_cast< SdrObject* >(this)->RecalcBoundRect();
     }
 
-    return rRectangle;
+    return getOutRectangle();
 }
 
 // To have a possibility to get the last calculated BoundRect e.g for producing
@@ -988,22 +989,27 @@ void SdrObject::RecalcBoundRect()
     GetViewContact().getViewIndependentPrimitive2DContainer(xPrimitives);
 
     if (xPrimitives.empty())
+    {
+        setOutRectangle(tools::Rectangle());
         return;
+    }
 
     // use neutral ViewInformation and get the range of the primitives
     const drawinglayer::geometry::ViewInformation2D aViewInformation2D;
     const basegfx::B2DRange aRange(xPrimitives.getB2DRange(aViewInformation2D));
 
-    if (!aRange.isEmpty())
+    if (aRange.isEmpty())
     {
-        tools::Rectangle aNewRectangle(
-            tools::Long(floor(aRange.getMinX())),
-            tools::Long(floor(aRange.getMinY())),
-            tools::Long(ceil(aRange.getMaxX())),
-            tools::Long(ceil(aRange.getMaxY())));
-        setOutRectangle(aNewRectangle);
+        setOutRectangle(tools::Rectangle());
         return;
     }
+
+    tools::Rectangle aNewRectangle(
+        tools::Long(floor(aRange.getMinX())),
+        tools::Long(floor(aRange.getMinY())),
+        tools::Long(ceil(aRange.getMaxX())),
+        tools::Long(ceil(aRange.getMaxY())));
+    setOutRectangle(aNewRectangle);
 }
 
 void SdrObject::BroadcastObjectChange() const
@@ -3216,16 +3222,19 @@ const tools::Rectangle& SdrObject::getOutRectangle() const
 void SdrObject::setOutRectangleConst(tools::Rectangle const& rRectangle) const
 {
     m_aOutRect = rRectangle;
+    m_bOutRectangleSet = true;
 }
 
 void SdrObject::setOutRectangle(tools::Rectangle const& rRectangle)
 {
     m_aOutRect = rRectangle;
+    m_bOutRectangleSet = true;
 }
 
 void SdrObject::resetOutRectangle()
 {
     m_aOutRect = tools::Rectangle();
+    m_bOutRectangleSet = false;
 }
 
 void SdrObject::moveOutRectangle(sal_Int32 nXDelta, sal_Int32 nYDelta)
