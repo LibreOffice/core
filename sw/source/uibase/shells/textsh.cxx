LibreOffice/core

commit b1170251fbca5b6b243902cf10695ab4c4c5642b

Improve case rotation in sentence case
Rotating case (Shift+F3) applies sentence case only
if multiple words are already selected or selects
sentence if SENTENCE_CASE is going to be applied.

Change-Id: I9e8536d7744a344d7ad54150783e91e843e0e81e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/165986
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

sw/source/uibase/shells/textsh.cxx
@@ -878,20 +878,33 @@ void SwTextShell::ExecRotateTransliteration( SfxRequest const & rReq )
 {
     if( rReq.GetSlot() == SID_TRANSLITERATE_ROTATE_CASE )
     {
+        TransliterationFlags transFlags = m_aRotateCase.getNextMode();
+        bool bSentenceCase = TransliterationFlags::SENTENCE_CASE == transFlags;
         SwWrtShell& rSh = GetShell();
         if (rSh.HasSelection())
         {
-            rSh.TransliterateText(m_aRotateCase.getNextMode());
+            if (bSentenceCase)
+            {
+                OUString aSelection = rSh.GetSelText().trim();
+                if (aSelection.getLength() <= 2 || (aSelection.indexOf(' ') < 0 && aSelection.indexOf('\t') < 0))
+                    transFlags = m_aRotateCase.getNextMode();
+            }
+            rSh.TransliterateText(transFlags);
         }
         else
         {
+            if (bSentenceCase)
+            {
+                if (!rSh.IsEndSentence())
+                    rSh.EndSentence(false);
+            }
             if (rSh.IsEndSentence())
             {
                 rSh.BwdSentence(true);
-                rSh.TransliterateText(m_aRotateCase.getNextMode());
+                rSh.TransliterateText(transFlags);
             }
             else if ((rSh.IsEndWrd() || rSh.IsStartWord() || rSh.IsInWord()) && rSh.SelWrd())
-                rSh.TransliterateText(m_aRotateCase.getNextMode());
+                rSh.TransliterateText(transFlags);
         }
     }
 }

sw/qa/extras/uiwriter/uiwriter6.cxx
@@ -742,10 +742,6 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf157988)
 
     dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
 
-    CPPUNIT_ASSERT(getParagraph(1)->getString().startsWith("Integer sodalesSODALES"));
-
-    dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
-
     CPPUNIT_ASSERT(getParagraph(1)->getString().startsWith("Integer sodales tincidunt"));
 
     dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
@@ -761,10 +757,6 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest6, testTdf157988)
     dispatchCommand(mxComponent, ".uno:Escape", {});
 
     dispatchCommand(mxComponent, ".uno:GoRight", {});
-
-    dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
-
-    CPPUNIT_ASSERT(getParagraph(1)->getString().startsWith("Integer sodalesSODALES"));
 
     dispatchCommand(mxComponent, ".uno:ChangeCaseRotateCase", {});
 

sw/qa/extras/uiwriter/uiwriter8.cxx
@@ -856,15 +856,6 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf116315)
         pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, KEY_SHIFT | KEY_F3);
         Scheduler::ProcessEventsToIdle();
 
-        // Sentence Case
-        // Without the fix in place, this test would have failed with
-        // - Expected: This is a Test
-        // - Actual  : This is a TEST
-        CPPUNIT_ASSERT_EQUAL(OUString("This is a Test"), getParagraph(1)->getString());
-
-        pTextDoc->postKeyEvent(LOK_KEYEVENT_KEYINPUT, 0, KEY_SHIFT | KEY_F3);
-        Scheduler::ProcessEventsToIdle();
-
         // Upper Case
         CPPUNIT_ASSERT_EQUAL(OUString("This is a TEST"), getParagraph(1)->getString());
 

sw/source/uibase/inc/wrtsh.hxx
@@ -239,6 +239,8 @@ typedef bool (SwWrtShell::*FNSimpleMove)();
                 { SimpleMove( &SwWrtShell::BwdPara_, false/*bSelect*/ ); }
     void FwdSentence( bool bSelect = false )
                 { SimpleMove( &SwWrtShell::FwdSentence_, bSelect ); }
+    void EndSentence( bool bSelect = false )
+                { SimpleMove( &SwWrtShell::EndSentence_, bSelect ); }
     void BwdSentence( bool bSelect = false )
                 { SimpleMove( &SwWrtShell::BwdSentence_, bSelect ); }
 
@@ -590,6 +592,7 @@ private:
     bool NxtWrdForDelete();
     bool PrvWrdForDelete();
     bool FwdSentence_();
+    bool EndSentence_();
     bool BwdSentence_();
     SW_DLLPUBLIC bool FwdPara_();
     bool BwdPara_();

sw/source/uibase/wrtsh/wrtsh4.cxx
@@ -190,6 +190,23 @@ bool SwWrtShell::FwdSentence_()
     return true;
 }
 
+bool SwWrtShell::EndSentence_()
+{
+    Push();
+    ClearMark();
+    if(!SwCursorShell::Right(1,SwCursorSkipMode::Chars))
+    {
+        Pop(SwCursorShell::PopMode::DeleteCurrent);
+        return false;
+    }
+    if( !GoEndSentence() && !IsEndPara() )
+        SwCursorShell::MovePara(GoCurrPara, fnParaEnd);
+
+    ClearMark();
+    Combine();
+    return true;
+}
+
 bool SwWrtShell::BwdSentence_()
 {
     Push();

commit bb496f5494a23c247c86fdf5ae65054908df44ce

tdf#167973 Fixes case rotation in selection in multiple paragraphs
Broken in commit b1170251fbca5b6b243902cf10695ab4c4c5642b because it checked
the selection in a single paragraph only.

Change-Id: If84aca87c9fee5750751cd91cb08372714f17eba
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191472
Tested-by: Jenkins
Reviewed-by: Oliver Specht <oliver.specht@cib.de>

sw/source/uibase/shells/textsh.cxx
@@ -896,7 +896,9 @@ void SwTextShell::ExecRotateTransliteration( SfxRequest& rReq )
         {
             if (bSentenceCase)
             {
-                OUString aSelection = rSh.GetSelText().trim();
+                OUString aSelection;
+                rSh.GetSelectedText(aSelection);
+                aSelection = aSelection.trim();
                 if (aSelection.getLength() <= 2 || (aSelection.indexOf(' ') < 0 && aSelection.indexOf('\t') < 0))
                     transFlags = m_aRotateCase.getNextMode();
             }