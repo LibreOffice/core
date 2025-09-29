LibreOffice/core

commit 788cc6ff3b186ceb8f265e53b5482f808f6536f4

tdf#148578: Do not apply table shift for RTF
Table shift based on cell lect margin should be applied
for DOCX (earlier compat options) but not actual for RTF.

Maybe earlier RTF version did behave this way, but nowadays
this behavior does not match MS Word 365.

Change-Id: Icdc4fa6298167fe5f263c85164d7c4c4176be25f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/145088
Tested-by: Jenkins
Reviewed-by: Thorsten Behrens <thorsten.behrens@allotropia.de>

writerfilter/source/dmapper/DomainMapperTableHandler.cxx
@@ -613,12 +613,9 @@ TableStyleSheetEntry * DomainMapperTableHandler::endTableGetTableStyle(TableInfo
 
         // tdf#106742: since MS Word 2013 (compatibilityMode >= 15), top-level tables are handled the same as nested tables;
         // the default behavior when DOCX doesn't define "compatibilityMode" option is to add the cell spacing
-
-        // Undefined should not be possible any more for DOCX, but it is for RTF.
-        // In any case, continue to treat undefined as version 12 during import.
         sal_Int32 nMode = m_rDMapper_Impl.GetSettingsTable()->GetWordCompatibilityMode();
 
-        if (((nMode < 0) || (0 < nMode && nMode <= 14)) && rInfo.nNestLevel == 1)
+        if (0 < nMode && nMode <= 14 && rInfo.nNestLevel == 1)
         {
             const sal_Int32 nAdjustedMargin = nLeftMargin - rInfo.nLeftBorderDistance;
             m_aTableProperties->Insert( PROP_LEFT_MARGIN, uno::Any( nAdjustedMargin ) );

sw/qa/extras/rtfexport/data/tdf148578.rtf
@@ -1,0 +1,12 @@
+{\rtf1
+
+\trowd
+\trgaph567
+\cellx1851
+\cellx9206
+\pard\plain\intbl 1000\cell
+\pard\plain\intbl XX_XX_XXX1\cell 
+\row
+
+\pard MMMM
+}

sw/qa/extras/rtfexport/rtfexport3.cxx
@@ -514,6 +514,27 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf127806)
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(635), aSize.Width);
 }
 
+DECLARE_RTFEXPORT_TEST(testTdf148578, "tdf148578.rtf")
+{
+    // \trgaph567 should affect only table cell margings (~1cm),
+    // but do not shift table, since \trleft is not provided
+    uno::Reference<text::XTextTable> xTable(getParagraphOrTable(1), uno::UNO_QUERY);
+
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), getProperty<sal_Int32>(xTable, "LeftMargin"));
+
+    uno::Reference<text::XTextRange> xCell(xTable->getCellByName("A1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1000),
+                         getProperty<sal_Int32>(xCell, "LeftBorderDistance"));
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1000),
+                         getProperty<sal_Int32>(xCell, "RightBorderDistance"));
+
+    xCell.set(xTable->getCellByName("B1"), uno::UNO_QUERY);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1000),
+                         getProperty<sal_Int32>(xCell, "LeftBorderDistance"));
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(1000),
+                         getProperty<sal_Int32>(xCell, "RightBorderDistance"));
+}
+
 DECLARE_RTFEXPORT_TEST(testInvalidParagraphStyle, "invalidParagraphStyle.rtf")
 {
     // Given test has character style #30, but referred as paragraph style #30

commit a80d7ba9c01c8c5c95bf01960d969b82dc7edffc

tdf#168598 Fix for tdf#148578 should only apply to RTF
It also affected DOCX files witch compatibilityMode
not specified.

Regression from 788cc6ff3b186ceb8f265e53b5482f808f6536f4

Change-Id: I1368066800ce4e6f4caa1131b1b6cf3590d8b1c1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191587
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Reviewed-by: Aron Budea <aron.budea@collabora.com>

sw/source/writerfilter/dmapper/DomainMapperTableHandler.cxx
@@ -599,8 +599,9 @@ TableStyleSheetEntry * DomainMapperTableHandler::endTableGetTableStyle(TableInfo
 
         // tdf#106742: since MS Word 2013 (compatibilityMode >= 15), top-level tables are handled the same as nested tables;
         // the default behavior when DOCX doesn't define "compatibilityMode" option is to add the cell spacing
+        // but not for RTF
 
-        if (0 < nMode && nMode <= 14 && rInfo.nNestLevel == 1)
+        if (m_rDMapper_Impl.IsOOXMLImport() && (nMode < 0 || (0 < nMode && nMode <= 14)) && rInfo.nNestLevel == 1)
         {
             const sal_Int32 nAdjustedMargin = nLeftMargin - rInfo.nLeftBorderDistance;
             m_aTableProperties->Insert( PROP_LEFT_MARGIN, uno::Any( nAdjustedMargin ) );

sw/qa/extras/ooxmlexport/ooxmlexport23.cxx
@@ -892,6 +892,15 @@ CPPUNIT_TEST_FIXTURE(Test, testVmlLineShapeRotated)
                 u"36.05pt,300.55pt");
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf168598, "tdf168598.docx")
+{
+    uno::Reference<text::XTextTable> xTable(getParagraphOrTable(2), uno::UNO_QUERY);
+
+    // Without the fix in place this would be -302
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(-501),
+                         getProperty<sal_Int32>(xTable, "LeftMargin"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */