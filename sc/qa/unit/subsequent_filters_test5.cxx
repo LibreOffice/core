LibreOffice/core

commit b89047a0f0100fb30121084cf42815aa792c1f88

tdf#164843 - sc optimize "SubTotal" function's reference ranges
Double references from relative named ranges can point to large
ranges (MAXCOL/MAXROW) and because of that some function evaluation
like SubTotal can be extreamly slow when we call ScTable::CompileHybridFormula
with these large ranges. Since all the SubTotal functions ignore empty cells
its worth to optimize and trim the double references in SubTotal functions.

Change-Id: I9340b520cd3ac333b63b4aec35a3c647f5d9a119
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180773
Tested-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>
Tested-by: Jenkins
Reviewed-by: Gabor Kelemen <gabor.kelemen.extern@allotropia.de>

sc/source/core/tool/compiler.cxx
@@ -6817,6 +6817,35 @@ void ScCompiler::AnnotateTrimOnDoubleRefs()
             --ppTok;
         }
     }
+    else if (eOpCode == ocSubTotal)
+    {
+        // tdf#164843: Double references from relative named ranges can point to large
+        // ranges (MAXCOL/MAXROW) and because of that some function evaluation
+        // like SubTotal can be extreamly slow when we call ScTable::CompileHybridFormula
+        // with these large ranges. Since all the SubTotal functions ignore empty cells
+        // its worth to optimize and trim the double references in SubTotal functions.
+        FormulaToken** ppTok = pCode - 2;
+        while (*ppTok)
+        {
+            FormulaToken* pTok = *ppTok;
+            if (pTok->GetType() == svDoubleRef)
+            {
+                ScComplexRefData* pRefData = pTok->GetDoubleRef();
+                // no need to set pRefData->SetTrimToData(true); because we already trim here
+                ScRange rRange = pRefData->toAbs(rDoc, aPos);
+                SCCOL nTempStartCol = rRange.aStart.Col();
+                SCROW nTempStartRow = rRange.aStart.Row();
+                SCCOL nTempEndCol = rRange.aEnd.Col();
+                SCROW nTempEndRow = rRange.aEnd.Row();
+                rDoc.ShrinkToDataArea(rRange.aStart.Tab(), nTempStartCol, nTempStartRow, nTempEndCol, nTempEndRow);
+                rRange.aStart.Set(nTempStartCol, nTempStartRow, rRange.aStart.Tab());
+                rRange.aEnd.Set(nTempEndCol, nTempEndRow, rRange.aEnd.Tab());
+                rRange.PutInOrder();
+                pRefData->SetRange(rDoc.GetSheetLimits(), rRange, aPos);
+            }
+            --ppTok;
+        }
+    }
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

sc/qa/unit/ucalc_formula.cxx
@@ -4761,6 +4761,78 @@ CPPUNIT_TEST_FIXTURE(TestFormula, testFuncSUMPRODUCT)
     m_pDoc->DeleteTab(0);
 }
 
+CPPUNIT_TEST_FIXTURE(TestFormula, testFuncSUBTOTAL)
+{
+    m_pDoc->InsertTab(0, u"Formula"_ustr);
+
+    sc::AutoCalcSwitch aACSwitch(*m_pDoc, true); // turn auto calc on.
+
+    // Fill C1:C1025 with values and insert formulas in D1:D1025 using the named expression.
+    for (size_t i = 0; i < 1025; i++)
+        m_pDoc->SetValue(ScAddress(2, i, 0), i + 1);
+
+    // Add a named expression for a function.
+    ScRangeName* pGlobalNames = m_pDoc->GetRangeName();
+    CPPUNIT_ASSERT_MESSAGE("Failed to obtain global named expression object.", pGlobalNames);
+    ScRangeData* pName = new ScRangeData(
+        *m_pDoc, u"MyRelative"_ustr, u"$C1:$C$1000"_ustr, ScAddress(2, 999, 0),
+        ScRangeData::Type::Name, formula::FormulaGrammar::GRAM_NATIVE);
+
+    bool bInserted = pGlobalNames->insert(pName);
+    CPPUNIT_ASSERT_MESSAGE("Failed to insert a new name.", bInserted);
+
+    for (size_t i = 0; i < 1025; i++)
+        m_pDoc->SetString(ScAddress(3, i, 0), u"=IF(SUBTOTAL(3;MyRelative)=1;"";SUBTOTAL(3;MyRelative))"_ustr);
+
+    // Make sure the results are correct.
+    for (size_t i = 0; i < 1025; i++)
+    {
+        if (i < 999.0)
+            CPPUNIT_ASSERT_EQUAL(26.0, m_pDoc->GetValue(3, i, 0));
+        else
+            CPPUNIT_ASSERT_EQUAL((1000.0 - (i - 999.0)), m_pDoc->GetValue(3, i, 0));
+    }
+
+    ScRange aTrimedRange(2, 999, 0, 2, 1024, 0);
+    ScRange aValidRange(2, 0, 0, 2, 999, 0);
+    for (size_t i = 0; i < 1025; i++)
+    {
+        ScFormulaCell* pCell = m_pDoc->GetFormulaCell(ScAddress(3, i, 0));
+        ScTokenArray* pCode = pCell->GetCode();
+        sal_uInt16 nLen = pCode->GetCodeLen();
+        FormulaToken** pRPNArray = pCode->GetCode();
+        OUString aCellName = pCell->aPos.GetColRowString();
+
+        for (sal_uInt16 nIdx = 0; nIdx < nLen; ++nIdx)
+        {
+            FormulaToken* pTok = pRPNArray[nIdx];
+            if (pTok && pTok->GetType() == svDoubleRef)
+            {
+                ScRange aRange = pTok->GetDoubleRef()->toAbs(*m_pDoc, ScAddress(3, i, 0));
+                if (i < 999)
+                {
+                    CPPUNIT_ASSERT_EQUAL_MESSAGE(OUString("Double ref is incorrectly trimmed in: " + aCellName).toUtf8().getStr(),
+                        aRange, aTrimedRange);
+                    // Without the trim it would failed with
+                    // assertion failed
+                    // - Expression: aRange == aTrimedRange
+                    // - Double ref is incorrectly trimmed in : D1
+                    // ScRange aTrimmableRange(2, 999, 0, 0, 1048575, 0);
+                }
+                else
+                {
+                    CPPUNIT_ASSERT_EQUAL_MESSAGE(OUString("Double ref is incorrectly trimmed in: " + aCellName).toUtf8().getStr(),
+                        aRange, aValidRange);
+                }
+            }
+        }
+        if (i >= 999)
+            aValidRange.aStart.IncRow();
+    }
+
+    m_pDoc->DeleteTab(0);
+}
+
 CPPUNIT_TEST_FIXTURE(TestFormula, testFuncSUMXMY2)
 {
     m_pDoc->InsertTab(0, u"Test SumXMY2"_ustr);

commit fdf6777e02f4897bdb798457d3e6b8fbd343f8ab

tdf#168589 - sc fix xls import with subtotal function
Check if the trimmed data range of Subtotal double references
is valid otherwise do not trim.

regression after: b89047a0f0100fb30121084cf42815aa792c1f88
(tdf#164843 - sc optimize "SubTotal" function's reference ranges)

Change-Id: I5f1fa4f67a685deb4a391e19e3acd9849b03c1b4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191691
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga@collabora.com>

sc/source/core/tool/compiler.cxx
@@ -6879,17 +6879,21 @@ void ScCompiler::AnnotateTrimOnDoubleRefs()
             if (pTok->GetType() == svDoubleRef)
             {
                 ScComplexRefData* pRefData = pTok->GetDoubleRef();
-                // no need to set pRefData->SetTrimToData(true); because we already trim here
+                // do no set pRefData->SetTrimToData(true); because we need to trim here if possible
                 ScRange rRange = pRefData->toAbs(rDoc, aPos);
                 SCCOL nTempStartCol = rRange.aStart.Col();
                 SCROW nTempStartRow = rRange.aStart.Row();
                 SCCOL nTempEndCol = rRange.aEnd.Col();
                 SCROW nTempEndRow = rRange.aEnd.Row();
                 rDoc.ShrinkToDataArea(rRange.aStart.Tab(), nTempStartCol, nTempStartRow, nTempEndCol, nTempEndRow);
-                rRange.aStart.Set(nTempStartCol, nTempStartRow, rRange.aStart.Tab());
-                rRange.aEnd.Set(nTempEndCol, nTempEndRow, rRange.aEnd.Tab());
-                rRange.PutInOrder();
-                pRefData->SetRange(rDoc.GetSheetLimits(), rRange, aPos);
+                // check if range is still valid
+                if (nTempStartRow <= nTempEndRow && nTempStartCol <= nTempEndCol)
+                {
+                    rRange.aStart.Set(nTempStartCol, nTempStartRow, rRange.aStart.Tab());
+                    rRange.aEnd.Set(nTempEndCol, nTempEndRow, rRange.aEnd.Tab());
+                    if (rRange.IsValid())
+                        pRefData->SetRange(rDoc.GetSheetLimits(), rRange, aPos);
+                }
             }
             --ppTok;
         }

sc/qa/unit/subsequent_filters_test5.cxx
@@ -276,6 +276,15 @@ CPPUNIT_TEST_FIXTURE(ScFiltersTest5, testTdf161948_NaturalSortSaveLoad)
     CPPUNIT_ASSERT(!aSortParam.bNaturalSort);
 }
 
+CPPUNIT_TEST_FIXTURE(ScFiltersTest5, testTdf168589)
+{
+    // Open xls document with Subtotal function
+    createScDoc("xls/tdf168589.xls");
+    ScDocument* pDoc = getScDoc();
+    CPPUNIT_ASSERT(pDoc);
+    ASSERT_DOUBLES_EQUAL(6.0, pDoc->GetValue(4, 5, 0));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */