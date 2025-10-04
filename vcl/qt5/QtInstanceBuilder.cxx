f463a48a9bc525d4ecfc34799a2b33c1cb1a89a0
Michael Weghorn <m.weghorn@posteo.de>
2025-10-05 01:27:17 +0200

    tdf#130857 qt weld: Support mail merge test account settings dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered as follows:
    
    * "Tools" -> "Options" -> "LibreOfficeDev Writer"
      -> "Mail Merge Email"
    * click the "Test Settings" button
    
    Change-Id: I47886d4954f59ae3d38c93b70272e9260a93bbba
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191870
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -172,6 +172,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"modules/swriter/ui/renameobjectdialog.ui"_ustr,
         u"modules/swriter/ui/rowheight.ui"_ustr,
         u"modules/swriter/ui/splittable.ui"_ustr,
+        u"modules/swriter/ui/testmailsettings.ui"_ustr,
         u"modules/swriter/ui/wordcount.ui"_ustr,
         u"sfx/ui/documentpropertiesdialog.ui"_ustr,
         u"sfx/ui/editdurationdialog.ui"_ustr,
