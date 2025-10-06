LibreOffice/core

commit 170a1dfe9b01ffea49328a3abbaeb70c725f4c90

simplify use of EnhancedCustomShapeEngine
this is an internal UNO API, and there is only one implementation,
so make that obvious in the code.

That required moving some classes from the svx library to the
svxcore library

Change-Id: Ic5c19327df271920dc1077185ff692abbf5fb091
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191494
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sd/source/ui/dlg/sdtreelb.cxx
@@ -910,17 +910,17 @@ OUString SdPageObjsTLV::GetObjectName(
         OUString sObjName;
         if (pObject->GetObjIdentifier() == SdrObjKind::CustomShape)
         {
-            // taken from SdrObjCustomShape::GetCustomShapeName
+#ifndef NDEBUG
             OUString aEngine(pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_ENGINE).GetValue());
-            if (aEngine.isEmpty() || aEngine == "com.sun.star.drawing.EnhancedCustomShapeEngine")
-            {
-                OUString sShapeType;
-                const SdrCustomShapeGeometryItem& rGeometryItem
-                    = pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_GEOMETRY);
-                const uno::Any* pAny = rGeometryItem.GetPropertyValueByName(u"Type"_ustr);
-                if (pAny && (*pAny >>= sShapeType))
-                    sObjName = SdResId(STR_NAVIGATOR_CUSTOMSHAPE) + u": " + sShapeType;
-            }
+            assert(aEngine.isEmpty() || aEngine == "com.sun.star.drawing.EnhancedCustomShapeEngine");
+#endif
+            // taken from SdrObjCustomShape::GetCustomShapeName
+            OUString sShapeType;
+            const SdrCustomShapeGeometryItem& rGeometryItem
+                = pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_GEOMETRY);
+            const uno::Any* pAny = rGeometryItem.GetPropertyValueByName(u"Type"_ustr);
+            if (pAny && (*pAny >>= sShapeType))
+                sObjName = SdResId(STR_NAVIGATOR_CUSTOMSHAPE) + u": " + sShapeType;
         }
         else
             sObjName = pObject->TakeObjNameSingul();

include/svx/svdoashp.hxx
@@ -24,7 +24,7 @@
 #include <svx/svdhdl.hxx>
 #include <vector>
 #include <com/sun/star/uno/Reference.h>
-#include <com/sun/star/drawing/XCustomShapeEngine.hpp>
+#include <com/sun/star/drawing/XCustomShapeHandle.hpp>
 #include <com/sun/star/drawing/EnhancedCustomShapeAdjustmentValue.hpp>
 #include <svx/svxdllapi.h>
 #include <o3tl/typed_flags_set.hxx>
@@ -36,6 +36,7 @@ namespace sdr
         class CustomShapeProperties;
     }
 }
+class EnhancedCustomShapeEngine;
 
 class SdrAShapeObjGeoData final : public SdrTextObjGeoData
 {
@@ -90,12 +91,12 @@ public:
 
     css::uno::Reference< css::drawing::XShape > mXRenderedCustomShape;
 
-    mutable css::uno::Reference< css::drawing::XCustomShapeEngine > mxCustomShapeEngine;
+    mutable rtl::Reference< EnhancedCustomShapeEngine  > mxCustomShapeEngine;
 
     // #i37011# render geometry shadow
     rtl::Reference<SdrObject> mpLastShadowGeometry;
 
-    css::uno::Reference< css::drawing::XCustomShapeEngine > const & GetCustomShapeEngine() const;
+    rtl::Reference< EnhancedCustomShapeEngine > const & GetCustomShapeEngine() const;
 
     std::vector< SdrCustomShapeInteraction > GetInteractionHandles() const; // needed in unit test
     SVX_DLLPRIVATE void DragCreateObject( SdrDragStat& rDrag );

offapi/com/sun/star/drawing/CustomShapeEngine.idl
@@ -25,11 +25,6 @@ module com {  module sun {  module star {  module drawing {
  */
 service CustomShapeEngine
 {
-    /** initializing the CustomShapeEngine
-
-     */
-    interface ::com::sun::star::lang::XInitialization;
-
     interface com::sun::star::drawing::XCustomShapeEngine;
 };
 

oox/qa/unit/shape.cxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/drawing/TextHorizontalAdjust.hpp>
 #include <com/sun/star/drawing/TextVerticalAdjust.hpp>
 #include <com/sun/star/drawing/XDrawPagesSupplier.hpp>
+#include <com/sun/star/drawing/PolyPolygonBezierCoords.hpp>
 #include <com/sun/star/graphic/XGraphic.hpp>
 #include <com/sun/star/lang/Locale.hpp>
 #include <com/sun/star/text/XTextFrame.hpp>

solenv/bin/native-code.py
@@ -381,7 +381,6 @@ core_constructor_list = [
     "com_sun_star_comp_svx_NumberingToolBoxControl_get_implementation",
     "com_sun_star_comp_svx_SmartTagMenuController_get_implementation",
     "com_sun_star_comp_svx_UnderlineToolBoxControl_get_implementation",
-    "com_sun_star_drawing_EnhancedCustomShapeEngine_get_implementation",
     "com_sun_star_drawing_SvxShapeCollection_get_implementation",
     "com_sun_star_svx_FontHeightToolBoxController_get_implementation",
     "org_apache_openoffice_comp_svx_sidebar_PanelFactory_get_implementation",
@@ -397,6 +396,7 @@ core_constructor_list = [
     "com_sun_star_comp_svx_LineStyleToolBoxControl_get_implementation",
     "com_sun_star_comp_svx_StyleToolBoxControl_get_implementation",
     "com_sun_star_comp_svx_StylesPreviewToolBoxControl_get_implementation",
+    "com_sun_star_drawing_EnhancedCustomShapeEngine_get_implementation",
 # toolkit/util/tk.component
     "com_sun_star_comp_embed_HatchWindowFactory_get_implementation",
     "stardiv_Toolkit_StdTabController_get_implementation",

svx/Library_svx.mk
@@ -121,10 +121,6 @@ $(eval $(call gb_Library_add_exception_objects,svx,\
     svx/source/accessibility/charmapacc \
     svx/source/accessibility/svxpixelctlaccessiblecontext \
     svx/source/accessibility/svxrectctaccessiblecontext \
-    svx/source/customshapes/EnhancedCustomShape3d \
-    svx/source/customshapes/EnhancedCustomShapeEngine \
-    svx/source/customshapes/EnhancedCustomShapeFontWork \
-    svx/source/customshapes/EnhancedCustomShapeHandle \
     svx/source/dialog/GenericCheckDialog \
     svx/source/dialog/GenericDropDownFieldDialog \
     svx/source/dialog/_bmpmask \

svx/Library_svxcore.mk
@@ -485,6 +485,10 @@ $(eval $(call gb_Library_add_exception_objects,svxcore,\
     svx/source/xoutdev/xtabptrn \
     svx/source/xoutdev/XPropertyEntry \
     svx/source/xoutdev/xtablend \
+    svx/source/customshapes/EnhancedCustomShape3d \
+    svx/source/customshapes/EnhancedCustomShapeEngine \
+    svx/source/customshapes/EnhancedCustomShapeFontWork \
+    svx/source/customshapes/EnhancedCustomShapeHandle \
 ))
 
 $(eval $(call gb_SdiTarget_SdiTarget,svx/sdi/svxslots,svx/sdi/svx))

svx/inc/EnhancedCustomShapeEngine.hxx
@@ -1,0 +1,56 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This file incorporates work covered by the following license notice:
+ *
+ *   Licensed to the Apache Software Foundation (ASF) under one or more
+ *   contributor license agreements. See the NOTICE file distributed
+ *   with this work for additional information regarding copyright
+ *   ownership. The ASF licenses this file to you under the Apache
+ *   License, Version 2.0 (the "License"); you may not use this file
+ *   except in compliance with the License. You may obtain a copy of
+ *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
+ */
+#pragma once
+
+#include <cppuhelper/implbase.hxx>
+#include <com/sun/star/drawing/XCustomShapeEngine.hpp>
+#include <com/sun/star/lang/XServiceInfo.hpp>
+#include <svx/svxdllapi.h>
+
+class SVXCORE_DLLPUBLIC EnhancedCustomShapeEngine
+    : public cppu::WeakImplHelper<css::lang::XServiceInfo, css::drawing::XCustomShapeEngine>
+{
+    css::uno::Reference<css::drawing::XShape> mxShape;
+    bool mbForceGroupWithText;
+
+    rtl::Reference<SdrObject> ImplForceGroupWithText(const SdrObjCustomShape& rSdrObjCustomShape,
+                                                     SdrObject* pRenderedShape);
+
+public:
+    EnhancedCustomShapeEngine(const css::uno::Sequence<css::uno::Any>& aArguments);
+    EnhancedCustomShapeEngine(const css::uno::Reference<css::drawing::XShape>& xShape);
+
+    // XInterface
+    virtual void SAL_CALL acquire() noexcept override;
+    virtual void SAL_CALL release() noexcept override;
+
+    // XServiceInfo
+    virtual OUString SAL_CALL getImplementationName() override;
+    virtual sal_Bool SAL_CALL supportsService(const OUString& rServiceName) override;
+    virtual css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
+
+    // XCustomShapeEngine
+    virtual css::uno::Reference<css::drawing::XShape> SAL_CALL render() override;
+    virtual css::awt::Rectangle SAL_CALL getTextBounds() override;
+    virtual css::drawing::PolyPolygonBezierCoords SAL_CALL getLineGeometry() override;
+    virtual css::uno::Sequence<css::uno::Reference<css::drawing::XCustomShapeHandle>>
+        SAL_CALL getInteraction() override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 96834fab67ab4719ab276b6edf67ecc2f2899bee

crashtesting fix
regression from
    commit 170a1dfe9b01ffea49328a3abbaeb70c725f4c90
    Author: Noel Grandin <noel.grandin@collabora.co.uk>
    Date:   Thu Sep 25 10:47:48 2025 +0200
    simplify use of EnhancedCustomShapeEngine

Change-Id: Ic7f17e8bf05bc0462cf28c0420f4e58b6ec1c579
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191957
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sd/source/ui/dlg/sdtreelb.cxx
@@ -910,17 +910,17 @@ OUString SdPageObjsTLV::GetObjectName(
         OUString sObjName;
         if (pObject->GetObjIdentifier() == SdrObjKind::CustomShape)
         {
-#ifndef NDEBUG
-            OUString aEngine(pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_ENGINE).GetValue());
-            assert(aEngine.isEmpty() || aEngine == "com.sun.star.drawing.EnhancedCustomShapeEngine");
-#endif
             // taken from SdrObjCustomShape::GetCustomShapeName
-            OUString sShapeType;
-            const SdrCustomShapeGeometryItem& rGeometryItem
-                = pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_GEOMETRY);
-            const uno::Any* pAny = rGeometryItem.GetPropertyValueByName(u"Type"_ustr);
-            if (pAny && (*pAny >>= sShapeType))
-                sObjName = SdResId(STR_NAVIGATOR_CUSTOMSHAPE) + u": " + sShapeType;
+            OUString aEngine(pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_ENGINE).GetValue());
+            if(aEngine.isEmpty() || aEngine == "com.sun.star.drawing.EnhancedCustomShapeEngine")
+            {
+                OUString sShapeType;
+                const SdrCustomShapeGeometryItem& rGeometryItem
+                    = pObject->GetMergedItem(SDRATTR_CUSTOMSHAPE_GEOMETRY);
+                const uno::Any* pAny = rGeometryItem.GetPropertyValueByName(u"Type"_ustr);
+                if (pAny && (*pAny >>= sShapeType))
+                    sObjName = SdResId(STR_NAVIGATOR_CUSTOMSHAPE) + u": " + sShapeType;
+            }
         }
         else
             sObjName = pObject->TakeObjNameSingul();