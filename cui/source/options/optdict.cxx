LibreOffice/core

commit 00e2762c664614a1b33f54dfc990ba29f0f81a07

Drop uses of css::uno::Sequence::getConstArray in cppuhelper .. cui
where it was obsoleted by commits 2484de6728bd11bb7949003d112f1ece2223c7a1
(Remove non-const Sequence::begin()/end() in internal code, 2021-10-15) and
fb3c04bd1930eedacd406874e1a285d62bbf27d9 (Drop non-const Sequence::operator[]
in internal code 2021-11-05).

Change-Id: Ia2b60af973183bbe79656e67b5e37d7efa39a308
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166817
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

cui/source/options/optdict.cxx
@@ -136,18 +136,6 @@ IMPL_LINK_NOARG(SvxNewDictionaryDialog, OKHdl_Impl, weld::Button&, void)
 
     Reference< XSearchableDictionaryList >  xDicList( LinguMgr::GetDictionaryList() );
 
-    Sequence< Reference< XDictionary >  > aDics;
-    if (xDicList.is())
-        aDics = xDicList->getDictionaries();
-    const Reference< XDictionary >  *pDic = aDics.getConstArray();
-    sal_Int32 nCount = aDics.getLength();
-
-    bool bFound = false;
-    sal_Int32 i;
-    for (i = 0; !bFound && i < nCount; ++i )
-        if ( sDict.equalsIgnoreAsciiCase( pDic[i]->getName()) )
-            bFound = true;
-
     if ( sDict.indexOf("/") != -1 || sDict.indexOf("\\") != -1 )
     {
         // Detected an invalid character.
@@ -159,7 +147,12 @@ IMPL_LINK_NOARG(SvxNewDictionaryDialog, OKHdl_Impl, weld::Button&, void)
         return;
     }
 
-    if ( bFound )
+    Sequence< Reference< XDictionary >  > aDics;
+    if (xDicList.is())
+        aDics = xDicList->getDictionaries();
+
+    if (std::any_of(aDics.begin(), aDics.end(),
+                    [&sDict](auto& d) { return sDict.equalsIgnoreAsciiCase(d->getName()); }))
     {
         // duplicate names?
         std::unique_ptr<weld::MessageDialog> xInfoBox(Application::CreateMessageDialog(m_xDialog.get(),
@@ -276,13 +269,9 @@ SvxEditDictionaryDialog::SvxEditDictionaryDialog(weld::Window* pParent, std::u16
     m_xReplaceED->connect_activate(LINK(this, SvxEditDictionaryDialog, NewDelActionHdl));
 
     // fill listbox with all available WB's
-    const Reference< XDictionary >  *pDic = aDics.getConstArray();
-    sal_Int32 nCount = aDics.getLength();
-
     OUString aLookUpEntry;
-    for ( sal_Int32 i = 0; i < nCount; ++i )
+    for (auto& xDic : aDics)
     {
-        Reference< XDictionary >  xDic = pDic[i];
         if (xDic.is())
         {
             bool bNegative = xDic->getDictionaryType() == DictionaryType_NEGATIVE;
@@ -298,7 +287,7 @@ SvxEditDictionaryDialog::SvxEditDictionaryDialog(weld::Window* pParent, std::u16
 
     m_xLangLB->SetLanguageList( SvxLanguageListFlags::ALL, true, true );
 
-    if ( nCount > 0 )
+    if (aDics.hasElements())
     {
         m_xAllDictsLB->set_active_text(aLookUpEntry);
         int nPos = m_xAllDictsLB->get_active();
@@ -396,7 +385,7 @@ void SvxEditDictionaryDialog::RemoveDictEntry(int nEntry)
     {
         OUString sTmpShort(m_pWordsLB->get_text(nEntry, 0));
 
-        Reference<XDictionary> xDic = aDics.getConstArray()[nLBPos];
+        Reference<XDictionary> xDic = aDics[nLBPos];
         if (xDic->remove(sTmpShort))  // sal_True on success
         {
             m_pWordsLB->remove(nEntry);
@@ -462,7 +451,7 @@ IMPL_LINK_NOARG(SvxEditDictionaryDialog, SelectLangHdl_Impl, weld::ComboBox&, vo
 
 void SvxEditDictionaryDialog::ShowWords_Impl( sal_uInt16 nId )
 {
-    Reference< XDictionary >  xDic = aDics.getConstArray()[ nId ];
+    Reference<XDictionary> xDic = aDics[nId];
 
     weld::WaitObject aWait(m_xDialog.get());
 
@@ -512,16 +501,14 @@ void SvxEditDictionaryDialog::ShowWords_Impl( sal_uInt16 nId )
     m_pWordsLB->clear();
 
     Sequence< Reference< XDictionaryEntry >  > aEntries( xDic->getEntries() );
-    const Reference< XDictionaryEntry >  *pEntry = aEntries.getConstArray();
-    sal_Int32 nCount = aEntries.getLength();
     std::vector<OUString> aSortedDicEntries;
-    aSortedDicEntries.reserve(nCount);
-    for (sal_Int32 i = 0;  i < nCount;  i++)
+    aSortedDicEntries.reserve(aEntries.getLength());
+    for (auto& xDictionaryEntry : aEntries)
     {
-        OUString aStr = pEntry[i]->getDictionaryWord();
-        if(!pEntry[i]->getReplacementText().isEmpty())
+        OUString aStr = xDictionaryEntry->getDictionaryWord();
+        if (!xDictionaryEntry->getReplacementText().isEmpty())
         {
-            aStr += "\t" + pEntry[i]->getReplacementText();
+            aStr += "\t" + xDictionaryEntry->getReplacementText();
         }
         aSortedDicEntries.push_back(aStr);
     }
@@ -540,10 +527,11 @@ void SvxEditDictionaryDialog::ShowWords_Impl( sal_uInt16 nId )
     int nRow = 0;
     for (OUString const & rStr : aSortedDicEntries)
     {
-        m_pWordsLB->append_text(rStr.getToken(0, '\t'));
-        if (m_pWordsLB == m_xDoubleColumnLB.get())
+        sal_Int32 index = 0;
+        m_pWordsLB->append_text(rStr.getToken(0, '\t', index));
+        if (index != -1 && m_pWordsLB == m_xDoubleColumnLB.get())
         {
-            OUString sReplace = rStr.getToken(1, '\t');
+            OUString sReplace = rStr.getToken(0, '\t', index);
             m_pWordsLB->set_text(nRow, sReplace, 1);
             ++nRow;
         }

cppuhelper/source/implbase_ex.cxx
@@ -255,16 +255,11 @@ Sequence< Type >  SAL_CALL ImplInhHelper_getTypes(
     class_data * cd, Sequence< Type > const & rAddTypes )
 {
     sal_Int32 nImplTypes = cd->m_nTypes;
-    sal_Int32 nAddTypes = rAddTypes.getLength();
-    Sequence< Type > types( nImplTypes + nAddTypes );
+    Sequence<Type> types(nImplTypes + rAddTypes.getLength());
     Type * pTypes = types.getArray();
     fillTypes( pTypes, cd );
     // append base types
-    Type const * pAddTypes = rAddTypes.getConstArray();
-    while (nAddTypes--)
-    {
-        pTypes[ nImplTypes + nAddTypes ] = pAddTypes[ nAddTypes ];
-    }
+    std::copy(rAddTypes.begin(), rAddTypes.end(), pTypes + nImplTypes);
     return types;
 }
 

cppuhelper/source/implementationentry.cxx
@@ -44,10 +44,8 @@ sal_Bool component_writeInfoHelper(
                 Reference< XRegistryKey > xNewKey(
                     static_cast< XRegistryKey * >( pRegistryKey )->createKey( sKey ) );
 
-                Sequence< OUString > seq = entries[i].getSupportedServiceNames();
-                const OUString *pArray = seq.getConstArray();
-                for ( sal_Int32 nPos = 0 ; nPos < seq.getLength(); nPos ++ )
-                    xNewKey->createKey( pArray[nPos] );
+                for (auto& serviceName : entries[i].getSupportedServiceNames())
+                    xNewKey->createKey(serviceName);
             }
             bRet = true;
         }

cppuhelper/source/propertysetmixin.cxx
@@ -149,9 +149,6 @@ void Data::initProperties(
         css::uno::Reference<
             css::reflection::XInterfaceMemberTypeDescription > > members(
         ifc->getMembers());
-    OUString const * absentBegin = absentOptional.getConstArray();
-    OUString const * absentEnd =
-        absentBegin + absentOptional.getLength();
     for (const auto & m : members) {
         if (m->getTypeClass()
             == css::uno::TypeClass_INTERFACE_ATTRIBUTE)
@@ -250,8 +247,8 @@ void Data::initProperties(
                                 css::uno::Type(
                                     t->getTypeClass(), t->getName()),
                                 attrAttribs),
-                            (std::find(absentBegin, absentEnd, name)
-                             == absentEnd))).
+                            (std::find(absentOptional.begin(), absentOptional.end(), name)
+                             == absentOptional.end()))).
                 second)
             {
                 throw css::uno::RuntimeException(

cppuhelper/source/propshlp.cxx
@@ -926,7 +926,6 @@ void OPropertySetHelper::firePropertiesChangeEvent(
     std::unique_ptr<sal_Int32[]> pHandles(new sal_Int32[nLen]);
     IPropertyArrayHelper & rPH = getInfoHelper();
     rPH.fillHandles( pHandles.get(), rPropertyNames );
-    const OUString* pNames = rPropertyNames.getConstArray();
 
     // get the count of matching properties
     sal_Int32 nFireLen = 0;
@@ -948,7 +947,7 @@ void OPropertySetHelper::firePropertiesChangeEvent(
         if( pHandles[i] != -1 )
         {
             pChanges[nFirePos].Source = xSource;
-            pChanges[nFirePos].PropertyName = pNames[i];
+            pChanges[nFirePos].PropertyName = rPropertyNames[i];
             pChanges[nFirePos].PropertyHandle = pHandles[i];
             getFastPropertyValue( pChanges[nFirePos].OldValue, pHandles[i] );
             pChanges[nFirePos].NewValue = pChanges[nFirePos].OldValue;
@@ -979,11 +978,10 @@ static int compare_Property_Impl( const void *arg1, const void *arg2 )
 void OPropertyArrayHelper::init( sal_Bool bSorted )
 {
     sal_Int32 i, nElements = aInfos.getLength();
-    const Property* pProperties = aInfos.getConstArray();
 
     for( i = 1; i < nElements; i++ )
     {
-        if(  pProperties[i-1].Name > pProperties[i].Name )
+        if (aInfos[i - 1].Name > aInfos[i].Name)
         {
             if (bSorted) {
                 OSL_FAIL( "Property array is not sorted" );
@@ -991,12 +989,11 @@ void OPropertyArrayHelper::init( sal_Bool bSorted )
             // not sorted
             qsort( aInfos.getArray(), nElements, sizeof( Property ),
                     compare_Property_Impl );
-            pProperties = aInfos.getConstArray();
             break;
         }
     }
     for( i = 0; i < nElements; i++ )
-        if( pProperties[i].Handle != i )
+        if (aInfos[i].Handle != i)
             return;
     // The handle is the index
     bRightOrdered = true;
@@ -1037,7 +1034,6 @@ sal_Bool OPropertyArrayHelper::fillPropertyMembersByHandle
     sal_Int32 nHandle
 )
 {
-    const Property* pProperties = aInfos.getConstArray();
     sal_Int32 nElements = aInfos.getLength();
 
     if( bRightOrdered )
@@ -1045,20 +1041,20 @@ sal_Bool OPropertyArrayHelper::fillPropertyMembersByHandle
         if( nHandle < 0 || nHandle >= nElements )
             return false;
         if( pPropName )
-            *pPropName = pProperties[ nHandle ].Name;
+            *pPropName = aInfos[nHandle].Name;
         if( pAttributes )
-            *pAttributes = pProperties[ nHandle ].Attributes;
+            *pAttributes = aInfos[nHandle].Attributes;
         return true;
     }
     // normally the array is sorted
     for( sal_Int32 i = 0; i < nElements; i++ )
     {
-        if( pProperties[i].Handle == nHandle )
+        if (aInfos[i].Handle == nHandle)
         {
             if( pPropName )
-                *pPropName = pProperties[ i ].Name;
+                *pPropName = aInfos[i].Name;
             if( pAttributes )
-                *pAttributes = pProperties[ i ].Attributes;
+                *pAttributes = aInfos[i].Attributes;
             return true;
         }
     }
@@ -1108,10 +1104,9 @@ sal_Int32 OPropertyArrayHelper::getHandleByName( const OUString & rPropName )
 sal_Int32 OPropertyArrayHelper::fillHandles( sal_Int32 * pHandles, const Sequence< OUString > & rPropNames )
 {
     sal_Int32 nHitCount = 0;
-    const OUString * pReqProps = rPropNames.getConstArray();
     sal_Int32 nReqLen = rPropNames.getLength();
-    const Property * pCur = aInfos.getConstArray();
-    const Property * pEnd = pCur + aInfos.getLength();
+    const Property * pCur = aInfos.begin();
+    const Property * pEnd = aInfos.end();
 
     for( sal_Int32 i = 0; i < nReqLen; i++ )
     {
@@ -1129,11 +1124,11 @@ sal_Int32 OPropertyArrayHelper::fillHandles( sal_Int32 * pHandles, const Sequenc
         if( (nReqLen - i) * nLog >= pEnd - pCur )
         {
             // linear search is better
-            while( pCur < pEnd && pReqProps[i] > pCur->Name )
+            while (pCur < pEnd && rPropNames[i] > pCur->Name)
             {
                 pCur++;
             }
-            if( pCur < pEnd && pReqProps[i] == pCur->Name )
+            if (pCur < pEnd && rPropNames[i] == pCur->Name)
             {
                 pHandles[i] = pCur->Handle;
                 nHitCount++;
@@ -1152,7 +1147,7 @@ sal_Int32 OPropertyArrayHelper::fillHandles( sal_Int32 * pHandles, const Sequenc
             {
                 pMid = (pEnd - pCur) / 2 + pCur;
 
-                nCompVal = pReqProps[i].compareTo( pMid->Name );
+                nCompVal = rPropNames[i].compareTo(pMid->Name);
 
                 if( nCompVal > 0 )
                     pCur = pMid + 1;

cppuhelper/source/tdmgr.cxx
@@ -75,9 +75,6 @@ static typelib_TypeDescription * createCTD(
         const Sequence<Reference< XTypeDescription > > & rMemberTypes = xType->getMemberTypes();
         const Sequence< OUString > & rMemberNames                     = xType->getMemberNames();
 
-        const Reference< XTypeDescription > * pMemberTypes = rMemberTypes.getConstArray();
-        const OUString * pMemberNames                      = rMemberNames.getConstArray();
-
         sal_Int32 nMembers = rMemberTypes.getLength();
         OSL_ENSURE( nMembers == rMemberNames.getLength(), "### lens differ!" );
 
@@ -90,14 +87,14 @@ static typelib_TypeDescription * createCTD(
         for ( nPos = nMembers; nPos--; )
         {
             typelib_CompoundMember_Init & rInit = pMemberInits[nPos];
-            rInit.eTypeClass = static_cast<typelib_TypeClass>(pMemberTypes[nPos]->getTypeClass());
+            rInit.eTypeClass = static_cast<typelib_TypeClass>(rMemberTypes[nPos]->getTypeClass());
 
-            OUString aMemberTypeName( pMemberTypes[nPos]->getName() );
+            OUString aMemberTypeName(rMemberTypes[nPos]->getName());
             rInit.pTypeName = aMemberTypeName.pData;
             rtl_uString_acquire( rInit.pTypeName );
 
             // string is held by rMemberNames
-            rInit.pMemberName = pMemberNames[nPos].pData;
+            rInit.pMemberName = rMemberNames[nPos].pData;
         }
 
         typelib_typedescription_new(
@@ -134,9 +131,6 @@ static typelib_TypeDescription * createCTD(
         const Sequence<Reference< XTypeDescription > > & rMemberTypes = xType->getMemberTypes();
         const Sequence< OUString > & rMemberNames                     = xType->getMemberNames();
 
-        const Reference< XTypeDescription > * pMemberTypes = rMemberTypes.getConstArray();
-        const OUString * pMemberNames                      = rMemberNames.getConstArray();
-
         sal_Int32 nMembers = rMemberTypes.getLength();
         OSL_ENSURE( nMembers == rMemberNames.getLength(), "### lens differ!" );
 
@@ -163,14 +157,14 @@ static typelib_TypeDescription * createCTD(
         {
             typelib_StructMember_Init & rInit = pMemberInits[nPos];
             rInit.aBase.eTypeClass
-                = static_cast<typelib_TypeClass>(pMemberTypes[nPos]->getTypeClass());
+                = static_cast<typelib_TypeClass>(rMemberTypes[nPos]->getTypeClass());
 
-            OUString aMemberTypeName( pMemberTypes[nPos]->getName() );
+            OUString aMemberTypeName(rMemberTypes[nPos]->getName());
             rInit.aBase.pTypeName = aMemberTypeName.pData;
             rtl_uString_acquire( rInit.aBase.pTypeName );
 
             // string is held by rMemberNames
-            rInit.aBase.pMemberName = pMemberNames[nPos].pData;
+            rInit.aBase.pMemberName = rMemberNames[nPos].pData;
 
             rInit.bParameterizedType = templateMemberTypes.hasElements()
                 && (templateMemberTypes[nPos]->getTypeClass()
@@ -242,7 +236,6 @@ static typelib_TypeDescription * createCTD(
 
         // init all params
         const Sequence<Reference< XMethodParameter > > & rParams = xMethod->getParameters();
-        const Reference< XMethodParameter > * pParams            = rParams.getConstArray();
         sal_Int32 nParams = rParams.getLength();
 
         typelib_Parameter_Init * pParamInit = static_cast<typelib_Parameter_Init *>(alloca(
@@ -251,7 +244,7 @@ static typelib_TypeDescription * createCTD(
         sal_Int32 nPos;
         for ( nPos = nParams; nPos--; )
         {
-            const Reference< XMethodParameter > & xParam = pParams[nPos];
+            const Reference<XMethodParameter>& xParam = rParams[nPos];
             const Reference< XTypeDescription > & xType  = xParam->getType();
             typelib_Parameter_Init & rInit = pParamInit[xParam->getPosition()];
 
@@ -268,14 +261,13 @@ static typelib_TypeDescription * createCTD(
 
         // init all exception strings
         const Sequence<Reference< XTypeDescription > > & rExceptions = xMethod->getExceptions();
-        const Reference< XTypeDescription > * pExceptions = rExceptions.getConstArray();
         sal_Int32 nExceptions = rExceptions.getLength();
         rtl_uString ** ppExceptionNames = static_cast<rtl_uString **>(alloca(
             sizeof(rtl_uString *) * nExceptions ));
 
         for ( nPos = nExceptions; nPos--; )
         {
-            OUString aExceptionTypeName( pExceptions[nPos]->getName() );
+            OUString aExceptionTypeName(rExceptions[nPos]->getName());
             ppExceptionNames[nPos] = aExceptionTypeName.pData;
             rtl_uString_acquire( ppExceptionNames[nPos] );
         }
@@ -337,18 +329,16 @@ static typelib_TypeDescription * createCTD(
         typelib_TypeDescriptionReference ** ppMemberRefs = static_cast<typelib_TypeDescriptionReference **>(alloca(
             sizeof(typelib_TypeDescriptionReference *) * nMembers ));
 
-        const Reference< XInterfaceMemberTypeDescription > * pMembers = rMembers.getConstArray();
-
         OUString aTypeName( xType->getName() );
 
         sal_Int32 nPos;
         for ( nPos = nMembers; nPos--; )
         {
-            OUString aMemberTypeName( pMembers[nPos]->getName() );
+            OUString aMemberTypeName(rMembers[nPos]->getName());
             ppMemberRefs[nPos] = nullptr;
             typelib_typedescriptionreference_new(
                 ppMemberRefs + nPos,
-                static_cast<typelib_TypeClass>(pMembers[nPos]->getTypeClass()),
+                static_cast<typelib_TypeClass>(rMembers[nPos]->getTypeClass()),
                 aMemberTypeName.pData );
         }
 

cpputools/source/unoexe/unoexe.cxx
@@ -458,11 +458,10 @@ SAL_IMPLEMENT_MAIN()
 
             // init params
             Sequence< Any > aInitParams( aParams.getLength() );
-            const OUString * p = aParams.getConstArray();
             Any * pInitParams = aInitParams.getArray();
             for ( sal_Int32 i = aParams.getLength(); i--; )
             {
-                pInitParams[i] <<= p[i];
+                pInitParams[i] <<= aParams[i];
             }
 
             // instance provider

cui/source/customize/cfgutil.cxx
@@ -742,13 +742,11 @@ OUString CuiConfigGroupListBox::GetImage(
                 {
                     throw RuntimeException("SFTreeListBox::Init: failed to get PropertyValue");
                 }
-                beans::PropertyValue const * pmoduleDescr =
-                    moduleDescr.getConstArray();
                 for ( sal_Int32 pos = moduleDescr.getLength(); pos--; )
                 {
-                    if ( pmoduleDescr[ pos ].Name == "ooSetupFactoryEmptyDocumentURL" )
+                    if (moduleDescr[pos].Name == "ooSetupFactoryEmptyDocumentURL")
                     {
-                        pmoduleDescr[ pos ].Value >>= factoryURL;
+                        moduleDescr[pos].Value >>= factoryURL;
                         SAL_INFO("cui.customize", "factory url for doc images is " << factoryURL);
                         break;
                     }

commit f5cd16a252a73a3e594fc19eba365c52ec987fbb

tdf#167143: set text of the correct row
Regression after commit 00e2762c664614a1b33f54dfc990ba29f0f81a07
(Drop uses of css::uno::Sequence::getConstArray in cppuhelper ..
cui, 2024-04-29).

Change-Id: I9544160ee3857c3ef8322c85529f5ff6a4ce0e2c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192055
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

cui/source/options/optdict.cxx
@@ -523,7 +523,6 @@ void SvxEditDictionaryDialog::ShowWords_Impl( sal_uInt16 nId )
         });
 
     m_pWordsLB->freeze(); // speed up insert
-    int nRow = 0;
     for (OUString const & rStr : aSortedDicEntries)
     {
         sal_Int32 index = 0;
@@ -531,8 +530,7 @@ void SvxEditDictionaryDialog::ShowWords_Impl( sal_uInt16 nId )
         if (index != -1 && m_pWordsLB == m_xDoubleColumnLB.get())
         {
             OUString sReplace = rStr.getToken(0, '\t', index);
-            m_pWordsLB->set_text(nRow, sReplace, 1);
-            ++nRow;
+            m_pWordsLB->set_text(m_pWordsLB->n_children() - 1, sReplace, 1);
         }
     }
     m_pWordsLB->thaw();