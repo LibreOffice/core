LibreOffice/core

commit 0849ddd0b1b3c384c5f3de8fe0bbb9df558fa786

sw: text formatting: implement per-line paragraph properties like Word
This doesn't make a whole lot of sense.

Add compatibility setting "HiddenParagraphMarkPerLineProperties" for RTF
and DOCX compatibilityMode < 15.

Apparently what Word's "Compatibility Mode" is doing in case a paragraph
mark has hidden formatting is that it merges the last line of the first
paragraph and the first line of the second paragraph together, and
applies the first paragraph's properties to the line (and the preceding
lines); but for the second line of the second paragraph, it applies the
second paragraph's properties.

This is now implemented here; firstly, by adding a flag to the
MergedPara's Extents so that the situation can be distinguished (if the
paragraphs are joined by a delete redline, Word does something different
of course).  Because it's possible that the hidden paragraph break is on
a paragraph that doesn't have any extents, but a preceding paragraph has
extents that are affected, this sometimes requires 0-length dummy
extents.

FindParaPropsNodeIgnoreHidden() sets the last paragraph as
pParaPropsNode, which is used for all non-per-line properties.

Note that it's somewhat likely that the various Update functions in
txtfrm.cxx don't maintain the isHiddenParaMerge flag on extents
correctly, so it may look different than Word when editing.

Currently it affects these properties:

* line spacing

* tab stops

These are now set per line by SwTextIter::Next() calling
SwAttrIter::GetTextNodeForLinePropsWordCompat() and a factored out
SwLineInfo::InitLineInfo().

Evidently Word also does adjustment this way, but it's not implemented
here.

Change-Id: I216d9e2afdac9ab6f97c0ea822d4d501689df7a6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192077
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

sw/source/core/text/redlnitr.cxx
@@ -36,6 +36,7 @@
 #include <IDocumentRedlineAccess.hxx>
 #include <IDocumentLayoutAccess.hxx>
 #include <IDocumentMarkAccess.hxx>
+#include <IDocumentSettingAccess.hxx>
 #include <IMark.hxx>
 #include <bookmark.hxx>
 #include <rootfrm.hxx>
@@ -83,6 +84,10 @@ private:
 public:
     SwPosition const* GetStartPos() const { return m_pStartPos; }
     SwPosition const* GetEndPos() const { return m_pEndPos; }
+    bool IsHiddenParagraphBreak() const
+    {   // only if it is merged *by* hidden break (it could be deleted at the same time)
+        return m_oParagraphBreak.has_value();
+    }
 
     HideIterator(const SwTextNode & rTextNode,
             bool const isHideRedlines, sw::FieldmarkMode const eMode,
@@ -106,6 +111,7 @@ public:
     // Note: caller is responsible for checking for immediately adjacent hides
     bool Next()
     {
+        m_oParagraphBreak.reset();
         SwPosition const* pNextRedlineHide(nullptr);
         assert(m_pEndPos);
         if (m_isHideRedlines)
@@ -293,23 +299,36 @@ void FindParaPropsNodeIgnoreHidden(sw::MergedPara & rMerged,
         pScriptInfo->GetBoundsOfHiddenRange(TextFrameIndex{0}, nHiddenStart, nHiddenEnd);
         if (TextFrameIndex{0} == nHiddenStart)
         {
-            if (nHiddenEnd == TextFrameIndex{rMerged.mergedText.getLength()})
+            // Word compatibilityMode < 15 changes properties per line, so just set the last node here
+            if (rMerged.pLastNode->getIDocumentSettingAccess()->get(
+                    DocumentSettingId::HIDDEN_PARAGRAPH_MARK_PER_LINE_PROPERTIES))
             {
                 rMerged.pParaPropsNode = const_cast<SwTextNode*>(rMerged.pLastNode);
             }
-            else
-            {   // this requires MapViewToModel to never return a position at
-                // the end of a node (when all its text is hidden)
-                rMerged.pParaPropsNode = sw::MapViewToModel(rMerged, nHiddenEnd).first;
+            else // Word compatibilityMode 15 works differently!
+            {    // (and this is just an approximation of what it does)
+                if (nHiddenEnd == TextFrameIndex{rMerged.mergedText.getLength()})
+                {
+                    rMerged.pParaPropsNode = const_cast<SwTextNode*>(rMerged.pLastNode);
+                }
+                else
+                {   // this requires MapViewToModel to never return a position at
+                    // the end of a node (when all its text is hidden)
+                    rMerged.pParaPropsNode = sw::MapViewToModel(rMerged, nHiddenEnd).first;
+                }
             }
             return;
         }
     }
-    if (!rMerged.extents.empty())
+    for (auto const& it : rMerged.extents)
     {   // para props from first node that isn't empty (OOo/LO compat)
-        rMerged.pParaPropsNode = rMerged.extents.begin()->pNode;
+        if (it.nStart != it.nEnd) // filter isHiddenParaMerge dummy extents
+        {
+            rMerged.pParaPropsNode = it.pNode;
+            return;
+        }
+        else assert(it.isHiddenParaMerge);
     }
-    else
     {   // if every node is empty, the last one wins (Word compat)
         // (OOo/LO historically used first one)
         rMerged.pParaPropsNode = const_cast<SwTextNode*>(rMerged.pLastNode);
@@ -344,11 +363,22 @@ CheckParaRedlineMerge(SwTextFrame & rFrame, SwTextNode & rTextNode,
         assert(pNode != &rTextNode || &pStart->GetNode() == &rTextNode); // detect calls with wrong start node
         if (pStart->GetContentIndex() != nLastEnd) // not 0 so we eliminate adjacent deletes
         {
-            extents.emplace_back(pNode, nLastEnd, pStart->GetContentIndex());
+            extents.emplace_back(pNode, nLastEnd, pStart->GetContentIndex(), false);
             mergedText.append(pNode->GetText().subView(nLastEnd, pStart->GetContentIndex() - nLastEnd));
         }
         if (&pEnd->GetNode() != pNode)
         {
+            if (iter.IsHiddenParagraphBreak())
+            {
+                if (!extents.empty() && extents.back().pNode == pNode)
+                {
+                    extents.back().isHiddenParaMerge = true;
+                }
+                else
+                {   // dummy extent - must have "true" on one that has pNode!
+                    extents.emplace_back(pNode, pNode->Len(), pNode->Len(), true);
+                }
+            }
             if (pNode == &rTextNode)
             {
                 pNode->SetRedlineMergeFlag(SwNode::Merge::First);
@@ -465,7 +495,7 @@ CheckParaRedlineMerge(SwTextFrame & rFrame, SwTextNode & rTextNode,
     }
     if (nLastEnd != pNode->Len())
     {
-        extents.emplace_back(pNode, nLastEnd, pNode->Len());
+        extents.emplace_back(pNode, nLastEnd, pNode->Len(), false);
         mergedText.append(pNode->GetText().subView(nLastEnd, pNode->Len() - nLastEnd));
     }
     if (extents.empty()) // there was no text anywhere
@@ -474,7 +504,9 @@ CheckParaRedlineMerge(SwTextFrame & rFrame, SwTextNode & rTextNode,
     }
     else
     {
-        assert(!mergedText.isEmpty());
+        assert(!mergedText.isEmpty()
+            || ::std::all_of(extents.begin(), extents.end(),
+                    [](auto const& it){ return it.isHiddenParaMerge; }));
     }
     auto pRet{std::make_unique<sw::MergedPara>(rFrame, std::move(extents),
                 mergedText.makeStringAndClear(), &rTextNode, nodes.back())};

sw/inc/IDocumentSettingAccess.hxx
@@ -101,6 +101,7 @@ enum class DocumentSettingId
     JUSTIFY_LINES_WITH_SHRINKING,
     APPLY_TEXT_ATTR_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH,
     APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH,
+    HIDDEN_PARAGRAPH_MARK_PER_LINE_PROPERTIES,
     DO_NOT_MIRROR_RTL_DRAW_OBJS,
     // COMPATIBILITY FLAGS END
     BROWSE_MODE,

sw/source/core/doc/DocumentSettingManager.cxx
@@ -269,6 +269,8 @@ bool sw::DocumentSettingManager::get(/*[in]*/ DocumentSettingId id) const
             return mbApplyTextAttrToEmptyLineAtEndOfParagraph;
         case DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH:
             return mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph;
+        case DocumentSettingId::HIDDEN_PARAGRAPH_MARK_PER_LINE_PROPERTIES:
+            return mbHiddenParagraphMarkPerLineProperties;
         case DocumentSettingId::DO_NOT_BREAK_WRAPPED_TABLES:
             return mbDoNotBreakWrappedTables;
         case DocumentSettingId::ALLOW_TEXT_AFTER_FLOATING_TABLE_BREAK:
@@ -488,6 +490,9 @@ void sw::DocumentSettingManager::set(/*[in]*/ DocumentSettingId id, /*[in]*/ boo
             mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph = value;
             break;
 
+        case DocumentSettingId::HIDDEN_PARAGRAPH_MARK_PER_LINE_PROPERTIES:
+            mbHiddenParagraphMarkPerLineProperties = value;
+            break;
 
         case DocumentSettingId::DO_NOT_MIRROR_RTL_DRAW_OBJS:
             mbDoNotMirrorRtlDrawObjs = value;
@@ -1198,6 +1203,11 @@ void sw::DocumentSettingManager::dumpAsXml(xmlTextWriterPtr pWriter) const
     (void)xmlTextWriterStartElement(pWriter, BAD_CAST("mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph"));
     (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("value"),
                                 BAD_CAST(OString::boolean(mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph).getStr()));
+    (void)xmlTextWriterEndElement(pWriter);
+
+    (void)xmlTextWriterStartElement(pWriter, BAD_CAST("mbHiddenParagraphMarkPerLineProperties"));
+    (void)xmlTextWriterWriteAttribute(pWriter, BAD_CAST("value"),
+                                BAD_CAST(OString::boolean(mbHiddenParagraphMarkPerLineProperties).getStr()));
     (void)xmlTextWriterEndElement(pWriter);
 
     (void)xmlTextWriterStartElement(pWriter, BAD_CAST("mbDoNotMirrorRtlDrawObjs"));

sw/source/core/inc/DocumentSettingManager.hxx
@@ -180,6 +180,7 @@ class DocumentSettingManager final :
     bool mbJustifyLinesWithShrinking = false;
     bool mbApplyTextAttrToEmptyLineAtEndOfParagraph = false; // this was a mistake
     bool mbApplyParagraphMarkFormatToEmptyLineAtEndOfParagraph = false;
+    bool mbHiddenParagraphMarkPerLineProperties = false;
     bool mbIgnoreHiddenCharsForLineCalculation = true;
     bool mbDoNotMirrorRtlDrawObjs = false;
     // If this is on as_char flys wrapping will be handled the same like in Word

sw/source/core/inc/txtfrm.hxx
@@ -92,11 +92,12 @@ struct Extent
     SwTextNode * /*const logically, but need assignment for std::vector*/ pNode;
     sal_Int32 nStart;
     sal_Int32 nEnd;
-    Extent(SwTextNode *const p, sal_Int32 const s, sal_Int32 const e)
-        : pNode(p), nStart(s), nEnd(e)
+    bool isHiddenParaMerge; //< for Word Compatibility Mode
+    Extent(SwTextNode *const p, sal_Int32 const s, sal_Int32 const e, bool const b)
+        : pNode(p), nStart(s), nEnd(e), isHiddenParaMerge(b)
     {
         assert(pNode);
-        assert(nStart != nEnd);
+        assert(nStart != nEnd || isHiddenParaMerge);
     }
 };
 

sw/source/core/layout/frmtool.cxx
@@ -1142,7 +1142,11 @@ static bool IsShown(SwNodeOffset const nIndex,
         }
         for (auto iter = *pIter; iter != *pEnd; ++iter)
         {
-            assert(iter->nStart != iter->nEnd); // TODO possible?
+            if (iter->nStart == iter->nEnd)
+            {
+                assert(iter->isHiddenParaMerge);
+                continue;
+            }
             assert(iter->pNode->GetIndex() == nIndex);
             if (rAnch.GetAnchorContentOffset() < iter->nStart)
             {

sw/source/core/text/inftxt.cxx
@@ -114,11 +114,11 @@ SwLineInfo::~SwLineInfo()
 {
 }
 
-void SwLineInfo::CtorInitLineInfo( const SwAttrSet& rAttrSet,
-                                   const SwTextNode& rTextNode )
+void SwLineInfo::InitLineInfo(SwTextNode const& rTextNodeForLineProps)
 {
-    m_oRuler.emplace( rAttrSet.GetTabStops() );
-    if ( rTextNode.GetListTabStopPosition( m_nListTabStopPosition ) )
+    SwAttrSet const& rAttrSetForLineProps{rTextNodeForLineProps.GetSwAttrSet()};
+    m_oRuler.emplace(rAttrSetForLineProps.GetTabStops());
+    if (rTextNodeForLineProps.GetListTabStopPosition(m_nListTabStopPosition))
     {
         m_bListTabStopIncluded = true;
 
@@ -139,7 +139,7 @@ void SwLineInfo::CtorInitLineInfo( const SwAttrSet& rAttrSet,
         }
     }
 
-    if ( !rTextNode.getIDocumentSettingAccess()->get(DocumentSettingId::TABS_RELATIVE_TO_INDENT) )
+    if (!rTextNodeForLineProps.getIDocumentSettingAccess()->get(DocumentSettingId::TABS_RELATIVE_TO_INDENT))
     {
         // remove default tab stop at position 0
         for ( sal_uInt16 i = 0; i < m_oRuler->Count(); i++ )
@@ -153,7 +153,13 @@ void SwLineInfo::CtorInitLineInfo( const SwAttrSet& rAttrSet,
         }
     }
 
-    m_pSpace = &rAttrSet.GetLineSpacing();
+    m_pSpace = &rAttrSetForLineProps.GetLineSpacing();
+}
+
+void SwLineInfo::CtorInitLineInfo(const SwAttrSet& rAttrSet,
+                                  const SwTextNode& rTextNodeForLineProps)
+{
+    InitLineInfo(rTextNodeForLineProps);
     m_nVertAlign = rAttrSet.GetParaVertAlign().GetValue();
     m_nDefTabStop = std::numeric_limits<SwTwips>::max();
 }
@@ -530,6 +536,7 @@ SwTextPaintInfo::SwTextPaintInfo( const SwTextPaintInfo &rInf, const OUString* p
       m_aPos( rInf.GetPos() ),
       m_aPaintRect( rInf.GetPaintRect() ),
       m_nSpaceIdx( rInf.GetSpaceIdx() )
+    , m_pLineInfo(rInf.m_pLineInfo)
 { }
 
 SwTextPaintInfo::SwTextPaintInfo( const SwTextPaintInfo &rInf )
@@ -543,6 +550,7 @@ SwTextPaintInfo::SwTextPaintInfo( const SwTextPaintInfo &rInf )
       m_aPos( rInf.GetPos() ),
       m_aPaintRect( rInf.GetPaintRect() ),
       m_nSpaceIdx( rInf.GetSpaceIdx() )
+    , m_pLineInfo(rInf.m_pLineInfo)
 { }
 
 SwTextPaintInfo::SwTextPaintInfo( SwTextFrame *pFrame, const SwRect &rPaint )
@@ -796,8 +804,8 @@ void SwTextPaintInfo::CalcRect( const SwLinePortion& rPor,
                                SwRect* pRect, SwRect* pIntersect,
                                const bool bInsideBox ) const
 {
-    const SwAttrSet& rAttrSet = GetTextFrame()->GetTextNodeForParaProps()->GetSwAttrSet();
-    const SvxLineSpacingItem& rSpace = rAttrSet.GetLineSpacing();
+    assert(m_pLineInfo);
+    SvxLineSpacingItem const& rSpace{*m_pLineInfo->GetLineSpacing()};
     tools::Long nPropLineSpace = rSpace.GetPropLineSpace();
 
     SwTwips nHeight = rPor.Height();

sw/source/core/text/inftxt.hxx
@@ -66,8 +66,9 @@ class SwLineInfo
     bool m_bListTabStopIncluded;
     tools::Long m_nListTabStopPosition;
 
+    void InitLineInfo(SwTextNode const& rTextNodeForLineProps);
     void CtorInitLineInfo( const SwAttrSet& rAttrSet,
-                           const SwTextNode& rTextNode );
+                           const SwTextNode& rTextNodeForLineProps);
 
     SW_DLLPUBLIC SwLineInfo();
     SW_DLLPUBLIC ~SwLineInfo();
@@ -365,6 +366,8 @@ class SwTextPaintInfo : public SwTextSizeInfo
     SwRect      m_aPaintRect; // Original paint rect (from Layout paint)
 
     sal_uInt16 m_nSpaceIdx;
+    SwLineInfo const* m_pLineInfo{nullptr}; // hack: need this to get line props
+
     void DrawText_(const OUString &rText, const SwLinePortion &rPor,
                    const TextFrameIndex nIdx, const TextFrameIndex nLen,
                    const bool bKern, const bool bWrong = false,
@@ -481,6 +484,7 @@ public:
 
     void SetSmartTags(sw::WrongListIterator *const pNew) { m_pSmartTags = pNew; }
     sw::WrongListIterator* GetSmartTags() const { return m_pSmartTags; }
+    void SetLineInfo(SwLineInfo const*const pLineInfo) { m_pLineInfo = pLineInfo; }
 };
 
 class SwTextFormatInfo : public SwTextPaintInfo

commit 2ae71afbef8e767a313584949268b62e5e695279

sw: fix too early reset of m_oParagraphBreak
Thanks to Mike Kaganski for finding this

(regression from commit 0849ddd0b1b3c384c5f3de8fe0bbb9df558fa786)

Change-Id: I1dd68f2b45f346f4dc0ac5242900d67970998dfa
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192105
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Tested-by: Jenkins

sw/source/core/text/redlnitr.cxx
@@ -111,7 +111,6 @@ public:
     // Note: caller is responsible for checking for immediately adjacent hides
     bool Next()
     {
-        m_oParagraphBreak.reset();
         SwPosition const* pNextRedlineHide(nullptr);
         assert(m_pEndPos);
         if (m_isHideRedlines)
@@ -204,6 +203,7 @@ public:
             m_pStartPos = pRed->Start();
             m_pEndPos = pRed->End();
             ++m_RedlineIndex;
+            m_oParagraphBreak.reset();
             return true;
         }
         else if (m_oNextFieldmarkHide)
@@ -211,6 +211,7 @@ public:
             assert(!pNextRedlineHide || *m_oNextFieldmarkHide <= *pNextRedlineHide);
             m_pStartPos = &*m_oNextFieldmarkHide;
             m_pEndPos = &*m_Fieldmark.second;
+            m_oParagraphBreak.reset();
             return true;
         }
         else
@@ -271,6 +272,7 @@ public:
             {
                 m_pStartPos = nullptr;
                 m_pEndPos = nullptr;
+                m_oParagraphBreak.reset();
                 return false;
             }
         }