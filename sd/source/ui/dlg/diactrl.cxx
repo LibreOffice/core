LibreOffice/core

commit cfff893b9c82843a90aac4ecdb3a3936721b74a0

Move unit conversion code to o3tl, and unify on that in more places
This also allows to easily add more units, both of length and for other
unit categories.

The conversion for "Line" unit (312 twip) is questionable. Corresponding
entries in aImplFactor in vcl/source/control/field.cxx were inconsistent
(45/11 in; 10/13 pc; 156/10 pt). They were added without explanation in
commit c85db626029fd8a5e0dfcb312937279df32339a0. I haven't found a spec
of the unit (https://en.wikipedia.org/wiki/Line_(unit) is not specific).
I used the definition based on "by pt", "by mm/100", "by char" (they all
were consistent); "by pc" seems inverted; "by twip" was half as much.
This accepted conversion makes unit test for tdf#79236 pass.

Change-Id: Iae5a21d915fa8e934a1f47f8ba9f6df03b79a9fd
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/110839
Tested-by: Mike Kaganski <mike.kaganski@collabora.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

vcl/source/control/field.cxx
@@ -19,12 +19,14 @@
 
 #include <sal/config.h>
 
+#include <cmath>
 #include <string_view>
 
 #include <sal/log.hxx>
 #include <osl/diagnose.h>
 
 #include <comphelper/string.hxx>
+#include <tools/UnitConversion.hxx>
 
 #include <vcl/builder.hxx>
 #include <vcl/fieldvalues.hxx>
@@ -972,35 +974,6 @@ static FieldUnit ImplMetricGetUnit(const OUString& rStr)
     return vcl::StringToMetric(aStr);
 }
 
-#define K *1000L
-#define M *1000000LL
-#define X *5280L
-
-// twip in km = 254 / 14 400 000 000
-// expressions too big for default size 32 bit need LL to avoid overflow
-
-const sal_Int64 aImplFactor[sal_uInt16(FieldUnit::LINE) + 1]
-                                  [sal_uInt16(FieldUnit::LINE) + 1] =
-{ /*
-mm/100    mm    cm       m     km  twip point  pica  inch    foot       mile     char     line  */
-{    1,  100,  1 K,  100 K, 100 M, 2540, 2540, 2540, 2540,2540*12,2540*12 X ,   53340, 396240},
-{    1,    1,   10,    1 K,   1 M, 2540, 2540, 2540, 2540,2540*12,2540*12 X ,    5334, 396240},
-{    1,    1,    1,    100, 100 K,  254,  254,  254,  254, 254*12, 254*12 X ,    5334,  39624},
-{    1,    1,    1,      1,   1 K,  254,  254,  254,  254, 254*12, 254*12 X ,  533400,  39624},
-{    1,    1,    1,      1,     1,  254,  254,  254,  254, 254*12, 254*12 X ,533400 K,  39624},
-{ 1440,144 K,144 K,14400 K,14400LL M, 1,   20,  240, 1440,1440*12,1440*12 X ,     210,   3120},
-{   72, 7200, 7200,  720 K, 720 M,    1,    1,   12,   72,  72*12,  72*12 X ,     210,    156},
-{    6,  600,  600,   60 K,  60 M,    1,    1,    1,    6,   6*12,   6*12 X ,     210,     10},
-{    1,  100,  100,   10 K,  10 M,    1,    1,    1,    1,     12,     12 X ,     210,     45},
-{    1,  100,  100,   10 K,  10 M,    1,    1,    1,    1,      1,      1 X ,     210,     45},
-{    1,  100,  100,   10 K,  10 M,    1,    1,    1,    1,      1,        1 ,     210,     45},
-{  144, 1440,14400,  14400, 14400,    1,   20,  240, 1440,1440*12, 1440*12 X,       1,   156 },
-{  720,72000,72000, 7200 K,7200LL M, 20,   10,   13,   11,  11*12,   11*12 X,     105,     1 }
-};
-#undef X
-#undef M
-#undef K
-
 static FieldUnit ImplMap2FieldUnit( MapUnit meUnit, tools::Long& nDecDigits )
 {
     switch( meUnit )
@@ -1057,7 +1030,7 @@ namespace vcl
         else if ( nDouble >= double(SAL_MAX_INT64) )
             nLong = SAL_MAX_INT64;
         else
-            nLong = static_cast<sal_Int64>( nDouble );
+            nLong = static_cast<sal_Int64>( std::round(nDouble) );
 
         return nLong;
     }
@@ -1094,20 +1067,11 @@ double convertValue( double nValue, tools::Long nDigits, FieldUnit eInUnit, Fiel
 
     if ( eInUnit != eOutUnit )
     {
-        sal_Int64 nDiv  = aImplFactor[sal_uInt16(eInUnit)][sal_uInt16(eOutUnit)];
-        sal_Int64 nMult = aImplFactor[sal_uInt16(eOutUnit)][sal_uInt16(eInUnit)];
-
-        SAL_WARN_IF( nMult <= 0, "vcl", "illegal *" );
-        SAL_WARN_IF( nDiv  <= 0, "vcl", "illegal /" );
-
-        if ( nMult != 1 && nMult > 0)
-            nValue *= nMult;
-        if ( nDiv != 1 && nDiv > 0 )
-        {
-            nValue += (nValue < 0) ? (-nDiv/2) : (nDiv/2);
-            nValue /= nDiv;
-        }
+        const o3tl::Length eFrom = FieldToO3tlLength(eInUnit), eTo = FieldToO3tlLength(eOutUnit);
+        if (eFrom != o3tl::Length::invalid && eTo != o3tl::Length::invalid)
+            nValue = o3tl::convert(nValue, eFrom, eTo);
     }
+
     return nValue;
 }
 
@@ -1143,49 +1107,22 @@ namespace vcl
     {
         if ( eInUnit != eOutUnit )
         {
-            sal_Int64 nMult = 1, nDiv = 1;
-
-            if (eInUnit == FieldUnit::PERCENT)
+            if (eInUnit == FieldUnit::PERCENT && mnBaseValue > 0 && nValue > 0)
             {
-                if ( (mnBaseValue <= 0) || (nValue <= 0) )
-                    return nValue;
-                nDiv = 100 * ImplPower10(nDecDigits);
+                sal_Int64 nDiv = 100 * ImplPower10(nDecDigits);
 
-                nMult = mnBaseValue;
+                if (mnBaseValue != 1)
+                    nValue *= mnBaseValue;
+
+                nValue += nDiv / 2;
+                nValue /= nDiv;
             }
-            else if ( eOutUnit == FieldUnit::PERCENT ||
-                      eOutUnit == FieldUnit::CUSTOM ||
-                      eOutUnit == FieldUnit::NONE ||
-                      eOutUnit == FieldUnit::DEGREE ||
-                      eOutUnit == FieldUnit::SECOND ||
-                      eOutUnit == FieldUnit::MILLISECOND ||
-                      eOutUnit == FieldUnit::PIXEL ||
-                      eInUnit  == FieldUnit::CUSTOM ||
-                      eInUnit  == FieldUnit::NONE ||
-                      eInUnit  == FieldUnit::DEGREE ||
-                      eInUnit  == FieldUnit::MILLISECOND ||
-                      eInUnit  == FieldUnit::PIXEL )
-                 return nValue;
             else
             {
-                if (eOutUnit == FieldUnit::MM_100TH)
-                    eOutUnit = FieldUnit::NONE;
-                if (eInUnit == FieldUnit::MM_100TH)
-                    eInUnit = FieldUnit::NONE;
-
-                nDiv  = aImplFactor[sal_uInt16(eInUnit)][sal_uInt16(eOutUnit)];
-                nMult = aImplFactor[sal_uInt16(eOutUnit)][sal_uInt16(eInUnit)];
-
-                SAL_WARN_IF( nMult <= 0, "vcl", "illegal *" );
-                SAL_WARN_IF( nDiv  <= 0, "vcl", "illegal /" );
-            }
-
-            if ( nMult != 1 && nMult > 0 )
-                nValue *= nMult;
-            if ( nDiv != 1 && nDiv > 0 )
-            {
-                nValue += ( nValue < 0 ) ? (-nDiv/2) : (nDiv/2);
-                nValue /= nDiv;
+                const o3tl::Length eFrom = FieldToO3tlLength(eInUnit, o3tl::Length::invalid);
+                const o3tl::Length eTo = FieldToO3tlLength(eOutUnit, o3tl::Length::invalid);
+                if (eFrom != o3tl::Length::invalid && eTo != o3tl::Length::invalid)
+                    nValue = o3tl::convert(nValue, eFrom, eTo);
             }
         }
 
@@ -1240,19 +1177,10 @@ namespace vcl
 
         if ( eFieldUnit != eInUnit )
         {
-            sal_Int64 nDiv  = aImplFactor[sal_uInt16(eInUnit)][sal_uInt16(eFieldUnit)];
-            sal_Int64 nMult = aImplFactor[sal_uInt16(eFieldUnit)][sal_uInt16(eInUnit)];
-
-            SAL_WARN_IF( nMult <= 0, "vcl", "illegal *" );
-            SAL_WARN_IF( nDiv  <= 0, "vcl", "illegal /" );
-
-            if( nMult != 1 && nMult > 0 )
-                nValue *= nMult;
-            if( nDiv != 1 && nDiv > 0 )
-            {
-                nValue += (nValue < 0) ? (-nDiv/2) : (nDiv/2);
-                nValue /= nDiv;
-            }
+            const o3tl::Length eFrom = FieldToO3tlLength(eInUnit, o3tl::Length::invalid);
+            const o3tl::Length eTo = FieldToO3tlLength(eFieldUnit, o3tl::Length::invalid);
+            if (eFrom != o3tl::Length::invalid && eTo != o3tl::Length::invalid)
+                nValue = o3tl::convert(nValue, eFrom, eTo);
         }
         return nValue;
     }

cui/source/tabpages/chardlg.cxx
@@ -59,6 +59,7 @@
 #include <FontFeaturesDialog.hxx>
 #include <sal/log.hxx>
 #include <osl/diagnose.h>
+#include <o3tl/unit_conversion.hxx>
 
 using namespace ::com::sun::star;
 
@@ -441,7 +442,9 @@ namespace
             // old value, scaled
             tools::Long nHeight;
             if ( _pFontSizeLB->IsPtRelative() )
-                nHeight = rOldItem.GetHeight() + PointToTwips( static_cast<tools::Long>(_pFontSizeLB->get_value() / 10) );
+                nHeight = rOldItem.GetHeight()
+                          + o3tl::convert(_pFontSizeLB->get_value(), o3tl::Length::pt,
+                                          o3tl::Length::twip) / 10;
             else
                 nHeight = static_cast<tools::Long>(rOldItem.GetHeight() * _pFontSizeLB->get_value() / 100);
 
@@ -450,7 +453,8 @@ namespace
                 ItemToControl( nHeight, _pPage->GetItemSet().GetPool()->GetMetric( _nFontHeightWhich ), FieldUnit::TWIP ) );
         }
         else if ( !_pFontSizeLB->get_active_text().isEmpty() )
-            aSize.setHeight( PointToTwips( static_cast<tools::Long>(_pFontSizeLB->get_value() / 10) ) );
+            aSize.setHeight(o3tl::convert(_pFontSizeLB->get_value(), o3tl::Length::pt,
+                                          o3tl::Length::twip) / 10);
         else
             aSize.setHeight( 200 );   // default 10pt
         aFontMetrics.SetFontSize( aSize );

cui/source/tabpages/grfpage.cxx
@@ -38,9 +38,9 @@
 #include <svtools/optionsdrawinglayer.hxx>
 #include <basegfx/matrix/b2dhommatrix.hxx>
 #include <basegfx/polygon/b2dpolygontools.hxx>
+#include <o3tl/unit_conversion.hxx>
 
-#define CM_1_TO_TWIP        567
-#define TWIP_TO_INCH        1440
+constexpr auto CM_1_TO_TWIP = o3tl::convert(1, o3tl::Length::cm, o3tl::Length::twip); // 567
 
 
 static int lcl_GetValue(const weld::MetricSpinButton& rMetric, FieldUnit eUnit)
@@ -646,16 +646,17 @@ void SvxGrfCropPage::GraphicHasChanged( bool bFound )
         }
 
         if ( aOrigPixelSize.Width() && aOrigPixelSize.Height() ) {
-             sal_Int32 ax = sal_Int32(floor(static_cast<float>(aOrigPixelSize.Width()) /
-                        (static_cast<float>(aOrigSize.Width())/TWIP_TO_INCH)+0.5));
-             sal_Int32 ay = sal_Int32(floor(static_cast<float>(aOrigPixelSize.Height()) /
-                        (static_cast<float>(aOrigSize.Height())/TWIP_TO_INCH)+0.5));
-             sTemp += " " + CuiResId( RID_SVXSTR_PPI );
+             sal_Int32 ax = 0.5 + aOrigPixelSize.Width() /
+                 o3tl::convert<double>(aOrigSize.Width(), o3tl::Length::twip,
+                                                          o3tl::Length::in);
+             sal_Int32 ay = 0.5 + aOrigPixelSize.Height() /
+                 o3tl::convert<double>(aOrigSize.Height(), o3tl::Length::twip,
+                                                           o3tl::Length::in);
              OUString sPPI = OUString::number(ax);
              if (abs(ax - ay) > 1) {
                 sPPI += u"\u00D7" + OUString::number(ay);
              }
-             sTemp = sTemp.replaceAll("%1", sPPI);
+             sTemp += " " + CuiResId(RID_SVXSTR_PPI).replaceAll("%1", sPPI);
         }
         m_xOrigSizeFT->set_label(sTemp);
     }

emfio/source/reader/wmfreader.cxx
@@ -24,6 +24,7 @@
 #include <memory>
 #include <optional>
 #include <o3tl/safeint.hxx>
+#include <o3tl/unit_conversion.hxx>
 #include <rtl/crc.h>
 #include <rtl/tencinfo.h>
 #include <sal/log.hxx>
@@ -1291,8 +1292,8 @@ namespace emfio
             {
                 // #n417818#: If we have an external header then overwrite the bounds!
                 tools::Rectangle aExtRect(0, 0,
-                    static_cast<double>(mpExternalHeader->xExt) * 567 * mnUnitsPerInch / 1440000,
-                    static_cast<double>(mpExternalHeader->yExt) * 567 * mnUnitsPerInch / 1440000);
+                    o3tl::convert(mpExternalHeader->xExt, o3tl::Length::mm100, o3tl::Length::px),
+                    o3tl::convert(mpExternalHeader->yExt, o3tl::Length::mm100, o3tl::Length::px));
                 aPlaceableBound = aExtRect;
 
                 SAL_INFO("vcl.wmf", "External header size "

include/o3tl/unit_conversion.hxx
@@ -1,0 +1,231 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <o3tl/safeint.hxx>
+#include <sal/macros.h>
+#include <sal/types.h>
+
+#include <array>
+#include <cassert>
+#include <utility>
+#include <type_traits>
+
+namespace o3tl
+{
+// Length units
+enum class Length
+{
+    mm100 = 0, // 1/100th mm
+    mm10, // 1/10 mm, corresponds to MapUnit::Map10thMM
+    mm, // millimeter
+    cm, // centimeter
+    m, // meter
+    km, // kilometer
+    emu, // English Metric Unit: 1/360000 cm, 1/914400 in
+    twip, // "Twentieth of a point" aka "dxa": 1/20 pt
+    pt, // Point: 1/72 in
+    pc, // Pica: 1/6 in, corresponds to FieldUnit::PICA and MeasureUnit::PICA
+    in1000, // 1/1000 in, corresponds to MapUnit::Map1000thInch
+    in100, // 1/100 in, corresponds to MapUnit::Map100thInch
+    in10, // 1/10 in, corresponds to MapUnit::Map10thInch
+    in, // inch
+    ft, // foot
+    mi, // mile
+    master, // PPT Master Unit: 1/576 in
+    px, // "pixel" unit: 15 twip (96 ppi), corresponds to MeasureUnit::PIXEL
+    ch, // "char" unit: 210 twip (14 px), corresponds to FieldUnit::CHAR
+    line, // "line" unit: 312 twip, corresponds to FieldUnit::LINE
+    count, // <== add new units above this last entry
+    invalid = -1
+};
+
+// If other categories of units would be needed (like time), a separate scoped enum
+// should be created, respective conversion array prepared in detail namespace, and
+// respective md(NewUnit, NewUnit) overload introduced, which would allow using
+// o3tl::convert() and o3tl::convertSanitize() with the new category in a type-safe
+// way, without mixing unrelated units.
+
+namespace detail
+{
+// Common utilities
+
+// A special function to avoid compiler warning comparing signed and unsigned values
+template <typename I> constexpr bool isBetween(I n, sal_Int64 min, sal_Int64 max)
+{
+    assert(max > 0 && min < 0);
+    if constexpr (std::is_signed_v<I>)
+        return n >= min && n <= max;
+    else
+        return n <= sal_uInt64(max);
+}
+
+// Ensure correct rounding for both positive and negative integers
+template <typename I, std::enable_if_t<std::is_integral_v<I>, int> = 0>
+constexpr sal_Int64 MulDiv(I n, sal_Int64 m, sal_Int64 d)
+{
+    assert(m > 0 && d > 0);
+    assert(isBetween(n, (SAL_MIN_INT64 + d / 2) / m, (SAL_MAX_INT64 - d / 2) / m));
+    return (n >= 0 ? (n * m + d / 2) : (n * m - d / 2)) / d;
+}
+template <typename F, std::enable_if_t<std::is_floating_point_v<F>, int> = 0>
+constexpr double MulDiv(F f, sal_Int64 m, sal_Int64 d)
+{
+    assert(m > 0 && d > 0);
+    return f * (double(m) / d);
+}
+
+template <typename I, std::enable_if_t<std::is_integral_v<I>, int> = 0>
+constexpr sal_Int64 MulDiv(I n, sal_Int64 m, sal_Int64 d, bool& bOverflow, sal_Int64 nDefault)
+{
+    if (!isBetween(n, (SAL_MIN_INT64 + d / 2) / m, (SAL_MAX_INT64 - d / 2) / m))
+    {
+        bOverflow = true;
+        return nDefault;
+    }
+    bOverflow = false;
+    return MulDiv(n, m, d);
+}
+
+template <typename I, std::enable_if_t<std::is_integral_v<I>, int> = 0>
+constexpr sal_Int64 MulDivSaturate(I n, sal_Int64 m, sal_Int64 d)
+{
+    if (!isBetween(n, (SAL_MIN_INT64 + d / 2) / m, (SAL_MAX_INT64 - d / 2) / m))
+    {
+        if (m > d && !isBetween(n, SAL_MIN_INT64 / m * d + d / 2, SAL_MAX_INT64 / m * d - d / 2))
+            return n > 0 ? SAL_MAX_INT64 : SAL_MIN_INT64; // saturate
+        return (n >= 0 ? n + d / 2 : n - d / 2) / d * m; // divide before multiplication
+    }
+    return MulDiv(n, m, d);
+}
+
+// Greatest common divisor
+constexpr int gcd(sal_Int64 a, sal_Int64 b) { return b == 0 ? a : gcd(b, a % b); }
+
+// Packs integral multiplier and divisor for conversion from one unit to another
+struct m_and_d
+{
+    sal_Int64 m; // multiplier
+    sal_Int64 d; // divisor
+    constexpr m_and_d(sal_Int64 _m, sal_Int64 _d)
+        : m(_m / gcd(_m, _d)) // make sure to use smallest quotients here because
+        , d(_d / gcd(_m, _d)) // they will be multiplied when building final table
+    {
+        assert(_m > 0 && _d > 0);
+    }
+};
+
+// Resulting static array N x N of all quotients to convert between all units. The
+// quotients are minimal to allow largest range of converted numbers without overflow.
+// Maybe o3tl::enumarray could be used here, but it's not constexpr yet.
+template <int N> constexpr auto prepareMDArray(const m_and_d (&mdBase)[N])
+{
+    std::array<std::array<sal_Int64, N>, N> a{};
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            if (i == j)
+                a[i][j] = 1;
+            else
+            {
+                assert(mdBase[i].m < SAL_MAX_INT64 / mdBase[j].d);
+                assert(mdBase[i].d < SAL_MAX_INT64 / mdBase[j].m);
+                const sal_Int64 m = mdBase[i].m * mdBase[j].d, d = mdBase[i].d * mdBase[j].m;
+                const sal_Int64 g = gcd(m, d);
+                a[i][j] = m / g;
+                a[j][i] = d / g;
+            }
+        }
+    }
+    return a;
+}
+
+// Length units implementation
+
+// Array of conversion quotients for mm, used to build final conversion table. Entries
+// are { multiplier, divider } to convert respective unit *to* mm. Order of elements
+// corresponds to order in o3tl::Length enum (Length::count and Length::invalid omitted).
+constexpr m_and_d mdBaseLen[] = {
+    { 1, 100 }, // mm100 => mm
+    { 1, 10 }, // mm10 => mm
+    { 1, 1 }, // mm => mm
+    { 10, 1 }, // cm => mm
+    { 1000, 1 }, // m => mm
+    { 1000000, 1 }, // km => mm
+    { 1, 36000 }, // emu => mm
+    { 254, 10 * 1440 }, // twip => mm
+    { 254, 10 * 72 }, // pt => mm
+    { 254, 10 * 6 }, // pc => mm
+    { 254, 10000 }, // in1000 => mm
+    { 254, 1000 }, // in100 => mm
+    { 254, 100 }, // in10 => mm
+    { 254, 10 }, // in => mm
+    { 254 * 12, 10 }, // ft => mm
+    { 254 * 12 * 5280, 10 }, // mi => mm
+    { 254, 10 * 576 }, // master => mm
+    { 254 * 15, 10 * 1440 }, // px => mm
+    { 254 * 210, 10 * 1440 }, // ch => mm
+    { 254 * 312, 10 * 1440 }, // line => mm
+};
+static_assert(SAL_N_ELEMENTS(mdBaseLen) == static_cast<int>(Length::count),
+              "mdBaseL must have an entry for each unit in o3tl::Length");
+
+// an overload taking Length
+constexpr sal_Int64 md(Length i, Length j)
+{
+    // The resulting multipliers and divisors array
+    constexpr auto aMDArray = prepareMDArray(mdBaseLen);
+
+    const int nI = static_cast<int>(i), nJ = static_cast<int>(j);
+    assert(nI >= 0 && o3tl::make_unsigned(nI) < aMDArray.size());
+    assert(nJ >= 0 && o3tl::make_unsigned(nJ) < aMDArray.size());
+    return aMDArray[nI][nJ];
+}
+
+// here might go overloads of md() taking other units ...
+}
+
+// Unchecked conversion. Takes a number value, multiplier and divisor
+template <typename N> constexpr auto convert(N n, sal_Int64 mul, sal_Int64 div)
+{
+    return detail::MulDiv(n, mul, div);
+}
+
+// Unchecked conversion. Takes a number value and units defined in this header
+template <typename N, typename U> constexpr auto convert(N n, U from, U to)
+{
+    return convert(n, detail::md(from, to), detail::md(to, from));
+}
+
+// Returns nDefault if intermediate multiplication overflows sal_Int64 (only for integral types).
+// On return, bOverflow indicates if overflow happened.
+template <typename N, typename U>
+constexpr auto convert(N n, U from, U to, bool& bOverflow, sal_Int64 nDefault = 0)
+{
+    return detail::MulDiv(n, detail::md(from, to), detail::md(to, from), bOverflow, nDefault);
+}
+
+// Conversion with saturation (only for integral types). For too large input returns SAL_MAX_INT64.
+// When intermediate multiplication would overflow, but otherwise result in in sal_Int64 range, the
+// precision is decreased because of inversion of multiplication and division.
+template <typename N, typename U> constexpr auto convertSaturate(N n, U from, U to)
+{
+    return detail::MulDivSaturate(n, detail::md(from, to), detail::md(to, from));
+}
+
+// Return a pair { multiplier, divisor } for a given conversion
+template <typename U> constexpr std::pair<sal_Int64, sal_Int64> getConversionMulDiv(U from, U to)
+{
+    return { detail::md(from, to), detail::md(to, from) };
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab cinoptions=b1,g0,N-s cinkeys+=0=break: */

include/oox/drawingml/drawingmltypes.hxx
@@ -31,6 +31,7 @@
 #include <com/sun/star/style/TabAlign.hpp>
 #include <com/sun/star/uno/Reference.hxx>
 #include <o3tl/safeint.hxx>
+#include <o3tl/unit_conversion.hxx>
 #include <oox/dllapi.h>
 #include <oox/helper/helper.hxx>
 #include <rtl/ustring.hxx>
@@ -171,33 +172,30 @@ inline OString calcRotationValue(sal_Int32 nRotation)
     return OString::number(nRotation);
 }
 
-const sal_Int32 EMU_PER_HMM = 360;      /// 360 EMUs per 1/100 mm.
-const sal_Int32 EMU_PER_PT = 12700;
-
 /** Converts the passed 32-bit integer value from 1/100 mm to EMUs. */
 inline sal_Int64 convertHmmToEmu( sal_Int32 nValue )
 {
-    return static_cast< sal_Int64 >( nValue ) * EMU_PER_HMM;
+    return o3tl::convert(nValue, o3tl::Length::mm100, o3tl::Length::emu);
 }
 
 /** Converts the passed 64-bit integer value from EMUs to 1/100 mm. */
 inline sal_Int32 convertEmuToHmm( sal_Int64 nValue )
 {
-    sal_Int32 nCorrection = (nValue > 0 ? 1 : -1) * EMU_PER_HMM / 2; // So that the implicit floor will round.
-    return getLimitedValue<sal_Int32, sal_Int64>(o3tl::saturating_add<sal_Int64>(nValue, nCorrection) / EMU_PER_HMM, SAL_MIN_INT32, SAL_MAX_INT32);
+    return getLimitedValue<sal_Int32, sal_Int64>(
+        o3tl::convert(nValue, o3tl::Length::emu, o3tl::Length::mm100), SAL_MIN_INT32,
+        SAL_MAX_INT32);
 }
 
 /** Converts the passed 64-bit integer value from EMUs to Points. */
 inline float convertEmuToPoints( sal_Int64 nValue )
 {
-    return static_cast<float>(nValue) / EMU_PER_PT;
+    return o3tl::convert<double>(nValue, o3tl::Length::emu, o3tl::Length::pt);
 }
 
 /** Converts the passed double value from points to mm. */
 inline double convertPointToMms(double fValue)
 {
-    constexpr double fFactor = static_cast<double>(EMU_PER_PT) / (EMU_PER_HMM * 100);
-    return fValue * fFactor;
+    return o3tl::convert(fValue, o3tl::Length::pt, o3tl::Length::mm);
 }
 
 /** A structure for a point with 64-bit integer components. */

include/svtools/unitconv.hxx
@@ -46,8 +46,6 @@ SVT_DLLPUBLIC FieldUnit MapToFieldUnit( const MapUnit eUnit );
 SVT_DLLPUBLIC void      SetMetricValue(weld::MetricSpinButton& rField, int lCoreValue, MapUnit eUnit);
 SVT_DLLPUBLIC int       GetCoreValue(const weld::MetricSpinButton& rField, MapUnit eUnit);
 
-SVT_DLLPUBLIC tools::Long  PointToTwips( tools::Long nIn );
-
 SVT_DLLPUBLIC tools::Long  TransformMetric( tools::Long nVal, FieldUnit aOld, FieldUnit aNew );
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit dd544837226d9af2a777c145f91972fd0b755587

tdf#168709: round double value instead of truncation
Regression from commit cfff893b9c82843a90aac4ecdb3a3936721b74a0
(Move unit conversion code to o3tl, and unify on that in more places,
2021-02-14).

Change-Id: If18e8537cd03d3fd516bd164c6a816f1bba38f7f
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192235
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

sd/source/ui/dlg/diactrl.cxx
@@ -127,7 +127,7 @@ IMPL_LINK(SdPagesField, spin_button_input, const OUString&, rText, std::optional
     else if (fResult < SAL_MIN_INT32)
         fResult = SAL_MIN_INT32;
 
-    return std::optional<int>(fResult);
+    return std::optional<int>(std::round(fResult));
 }
 
 IMPL_LINK_NOARG(SdPagesField, ModifyHdl, weld::SpinButton&, void)

svtools/source/control/ctrlbox.cxx
@@ -1409,7 +1409,7 @@ int FontSizeBox::get_value() const
         else if (fResult > nMax)
             fResult = nMax;
     }
-    return fResult;
+    return std::round(fResult);
 }
 
 SvxBorderLineStyle SvtLineListBox::GetSelectEntryStyle() const

vcl/source/control/field.cxx
@@ -1222,7 +1222,7 @@ void MetricFormatter::ImplMetricReformat( const OUString& rStr, double& rValue, 
         nTempVal = static_cast<double>(GetMax());
     else if ( nTempVal < GetMin())
         nTempVal = static_cast<double>(GetMin());
-    rOutStr = CreateFieldText( static_cast<sal_Int64>(nTempVal) );
+    rOutStr = CreateFieldText( static_cast<sal_Int64>(std::round(nTempVal)) );
 }
 
 MetricFormatter::MetricFormatter(Edit* pEdit)
@@ -1322,7 +1322,7 @@ sal_Int64 MetricFormatter::GetValueFromStringUnit(const OUString& rStr, FieldUni
         nTempValue = static_cast<double>(mnMin);
 
     // convert to requested units
-    return vcl::ConvertValue(static_cast<sal_Int64>(nTempValue), 0, GetDecimalDigits(), meUnit, eOutUnit);
+    return vcl::ConvertValue(static_cast<sal_Int64>(std::round(nTempValue)), 0, GetDecimalDigits(), meUnit, eOutUnit);
 }
 
 sal_Int64 MetricFormatter::GetValueFromString(const OUString& rStr) const

vcl/source/window/builder.cxx
@@ -393,7 +393,7 @@ namespace weld
         else if (fResult < SAL_MIN_INT32)
             fResult = SAL_MIN_INT32;
 
-        return std::optional<int>(fResult);
+        return std::optional<int>(std::round(fResult));
     }
 
     EntryTreeView::EntryTreeView(std::unique_ptr<Entry> xEntry, std::unique_ptr<TreeView> xTreeView)