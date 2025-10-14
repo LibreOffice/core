LibreOffice/core

commit 6ee33d0772f991f04e97b5b61d7b11810a1b8ac0

tdf#104079 RTF import: fix handling fields inside TOC fields
The marker trick is not needed for these, but the paragraph margins are
lost when using it, so avoid it.

Change-Id: I3fc9644cb85138b5473cb1478196ae8538041fb1
Reviewed-on: https://gerrit.libreoffice.org/43446
Reviewed-by: Miklos Vajna <vmiklos@collabora.co.uk>
Tested-by: Jenkins <ci@libreoffice.org>

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -1182,10 +1182,11 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap )
                 else
                 {
                     uno::Reference<text::XTextCursor> xCursor;
-                    if (m_bParaHadField && !m_bIsInComments)
+                    if (m_bParaHadField && !m_bIsInComments && !xTOCMarkerCursor.is())
                     {
                         // Workaround to make sure char props of the field are not lost.
                         // Not relevant for editeng-based comments.
+                        // Nor revelent for fields inside a TOC field.
                         OUString const sMarker("X");
                         xCursor = xTextAppend->getText()->createTextCursor();
                         if (xCursor.is())

sw/qa/extras/rtfexport/data/tdf104079.rtf
@@ -1,0 +1,48 @@
+{\rtf1\mac\ansicpg10000\uc1 \deff0\deflang1033\deflangfe1033
+{\colortbl;\red0\green0\blue0;\red0\green0\blue255;\red0\green255\blue255;\red0\green255\blue0;\red255\green0\blue255;\red255\green0\blue0;\red255\green255\blue0;\red255\green255\blue255;\red0\green0\blue128;\red0\green128\blue128;\red0\green128\blue0;
+\red128\green0\blue128;\red128\green0\blue0;\red128\green128\blue0;\red128\green128\blue128;\red192\green192\blue192;}
+{\stylesheet
+{\sa120\nowidctlpar\adjustright \cgrid \snext0 Normal;}
+{\s24\sa120\nowidctlpar\tqr\tx9360\adjustright \b\caps\cgrid \sbasedon0 \snext0 \sautoupd toc 1;}
+}
+\margl1440\margr1440\margt1296\margb1296 \widowctrl\ftnbj\aenddoc\hyphcaps0\viewkind1\viewscale100 \fet0\sectd \linex0\sectdefaultcl
+\pard\plain \s24\sa120\nowidctlpar\tqr\tx9360\adjustright \b\caps\cgrid
+{\field\fldedit
+{\*\fldinst TOC \\o "1-3" }
+{\fldrslt
+{\lang1024 toc1\tab }
+{\field\flddirty
+{\*\fldinst GOTOBUTTON _Toc434317063
+{\field
+{\*\fldinst PAGEREF _Toc434317063 }
+{\fldrslt 3 }
+}
+}
+}
+{\lang1024
+\par }
+\pard\plain \s24\sa120\nowidctlpar\tqr\tx9360\adjustright \b\caps\cgrid
+{\lang1024 toc2\tab }
+{\field\flddirty
+{\*\fldinst GOTOBUTTON _Toc434317068
+{\field
+{\*\fldinst PAGEREF _Toc434317068 }
+{\fldrslt 8 }
+}
+}
+}
+{\lang1024
+\par toc3\tab }
+{\field\flddirty
+{\*\fldinst GOTOBUTTON _Toc434317069
+{\field
+{\*\fldinst PAGEREF _Toc434317069 }
+{\fldrslt 8 }
+}
+}
+}
+\par
+\pard\plain \s24\sa120\nowidctlpar\tqr\tx9360\adjustright \b\caps\cgrid }
+}
+\prad\par
+}

sw/qa/extras/rtfexport/rtfexport.cxx
@@ -1001,6 +1001,22 @@ DECLARE_RTFEXPORT_TEST(testTdf94377, "tdf94377.rtf")
     CPPUNIT_ASSERT_EQUAL(12.f, getProperty<float>(getRun(getParagraphOfText(2, xText, "asdf12"), 1), "CharHeight"));
 }
 
+DECLARE_RTFEXPORT_TEST(testTdf104079, "tdf104079.rtf")
+{
+    bool bFound = false;
+    int nIndex = 0;
+    while (!bFound)
+    {
+        uno::Reference<text::XTextRange> xParagraph = getParagraph(++nIndex);
+        if (!xParagraph->getString().startsWith("toc3"))
+            continue;
+
+        bFound = true;
+        // This was 0, 3rd paragraph of ToC lost its bottom paragraph margin.
+        CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(212), getProperty<sal_Int32>(xParagraph, "ParaBottomMargin"));
+    }
+}
+
 DECLARE_RTFEXPORT_TEST(testPageBackground, "page-background.rtf")
 {
     // The problem was that \background was ignored.

commit 52b423993a5d34927c11674f8bb6e2c9a6ad927f

tdf#168688 DOCX import: fix missing char props in field in header in ToC
Regression from commit 6ee33d0772f991f04e97b5b61d7b11810a1b8ac0
(tdf#104079 RTF import: fix handling fields inside TOC fields,
2017-10-17), open the bugdoc, check the font size of "TEST" in the
header, it should be 24pt, but it's 12pt.

What happens is that the document ends with the table of contents DOCX
field, so the section properties at the end of the document (including a
reference to a header) get parsed while the ToC field is still open, so
we think that m_xTOCMarkerCursor is in use, but it's for the body text
and we're in the header, so it's not relevant.

Fix the problem by moving m_xTOCMarkerCursor to m_StreamStateStack,
where all state that should be stashed away & restored later belongs.
This way the old tdf#104079 use-case keeps working and the new use-case
gets fixed.

Interestingly this xTOCMarkerCursor is not actually used as a cursor
anymore and it just indicates if we're inside a ToC or not, but leave
that unchanged in this commit.

Change-Id: I1d1c9f55413d94dcde9e7c715bc9f9863f354dcb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192367
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sw/source/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -2670,7 +2670,7 @@ void DomainMapper_Impl::finishParagraph( const PropertyMapPtr& pPropertyMap, con
                 {
                     uno::Reference<text::XTextCursor> xCursor;
                     if (m_StreamStateStack.top().bParaHadField
-                        && !IsInComments() && !m_xTOCMarkerCursor.is())
+                        && !IsInComments() && !m_StreamStateStack.top().xTOCMarkerCursor.is())
                     {
                         // Workaround to make sure char props of the field are not lost.
                         // Not relevant for editeng-based comments.
@@ -7372,7 +7372,7 @@ void DomainMapper_Impl::handleToc
             const auto xTextCursor = xTextAppend->getText()->createTextCursor();
             if (xTextCursor)
                 xTextCursor->gotoEnd(false);
-            m_xTOCMarkerCursor = xTextCursor;
+            m_StreamStateStack.top().xTOCMarkerCursor = xTextCursor;
         }
         else
         {
@@ -7382,7 +7382,7 @@ void DomainMapper_Impl::handleToc
 
             // init [xTOCMarkerCursor]
             uno::Reference< text::XText > xText = xTextAppend->getText();
-            m_xTOCMarkerCursor = xText->createTextCursor();
+            m_StreamStateStack.top().xTOCMarkerCursor = xText->createTextCursor();
 
             // create header of the TOC with the TOC title inside
             createSectionForRange(m_StreamStateStack.top().xSdtEntryStart, xTextRangeEndOfTocHeader, u"com.sun.star.text.IndexHeaderSection", true);

sw/source/writerfilter/dmapper/DomainMapper_Impl.hxx
@@ -274,6 +274,8 @@ struct SubstreamContext
     // inline paragraph: hidden paragraph mark by w:specVanish
     bool bIsInlineParagraph = false;
     bool bIsPreviousInlineParagraph = false;
+
+    css::uno::Reference< css::text::XTextCursor > xTOCMarkerCursor;
 };
 
 /// Information about a paragraph to be finished after a field end.
@@ -674,8 +676,6 @@ private:
 
     bool                            m_bIsLastSectionGroup;
     bool                            m_bUsingEnhancedFields;
-
-    css::uno::Reference< css::text::XTextCursor > m_xTOCMarkerCursor;
 
     ::std::set<::std::pair<PagePartType, PageType>> m_HeaderFooterSeen;
 

sw/qa/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -7,7 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <test/unoapi_test.hxx>
+#include <swmodeltestbase.hxx>
 
 #include <com/sun/star/text/XTextDocument.hpp>
 #include <com/sun/star/beans/XPropertySet.hpp>
@@ -23,17 +23,23 @@
 #include <com/sun/star/text/XPageCursor.hpp>
 
 #include <vcl/scheduler.hxx>
+#include <editeng/fhgtitem.hxx>
+
+#include <docsh.hxx>
+#include <ndtxt.hxx>
+#include <swtable.hxx>
+#include <txatbase.hxx>
 
 using namespace ::com::sun::star;
 
 namespace
 {
 /// Tests for sw/source/writerfilter/dmapper/DomainMapper_Impl.cxx.
-class Test : public UnoApiTest
+class Test : public SwModelTestBase
 {
 public:
     Test()
-        : UnoApiTest(u"/sw/qa/writerfilter/dmapper/data/"_ustr)
+        : SwModelTestBase(u"/sw/qa/writerfilter/dmapper/data/"_ustr)
     {
     }
 };
@@ -477,6 +483,38 @@ CPPUNIT_TEST_FIXTURE(Test, testIfField)
     // Without the accompanying fix in place, this test would have failed, the document failed to
     // load.
     loadFromFile(u"if-field.docx");
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testFieldCharHeightHeaderToC)
+{
+    // Given a document with a header that has a field with a custom font size, while the document
+    // ends with a table of contents:
+    // When importing that document:
+    createSwDoc("field-char-height-header-toc.docx");
+
+    // Then make sure the custom font size is not lost:
+    SwDocShell* pDocShell = getSwDocShell();
+    // Navigate to the start of the only table's B1 cell.
+    SwDoc* pDoc = pDocShell->GetDoc();
+    sw::TableFrameFormats& rTableFormats = *pDoc->GetTableFrameFormats();
+    CPPUNIT_ASSERT_EQUAL(static_cast<size_t>(1), rTableFormats.size());
+    SwTableFormat* pTableFormat = rTableFormats[0];
+    SwTable* pTable = SwTable::FindTable(pTableFormat);
+    const SwTableBox* pCell = pTable->GetTableBox(u"B1"_ustr);
+    // Get the font size there.
+    const SwStartNode* pStartNode = pCell->GetSttNd();
+    SwNodeIndex aNodeIndex(*pStartNode);
+    ++aNodeIndex;
+    const SwTextNode* pTextNode = aNodeIndex.GetNode().GetTextNode();
+    SwTextAttr* pAttr = pTextNode->GetTextAttrAt(0, RES_TXTATR_AUTOFMT);
+    // Without the accompanying fix in place, this test would have failed, there was no direct
+    // formatting, so we got 12pt from style instead of the wanted 24pt.
+    CPPUNIT_ASSERT(pAttr);
+    const SwFormatAutoFormat& rAutoFormat
+        = static_txtattr_cast<SwTextAttrEnd*>(pAttr)->GetAutoFormat();
+    const SvxFontHeightItem& rFontHeightItem
+        = rAutoFormat.GetStyleHandle()->Get(RES_CHRATR_FONTSIZE);
+    CPPUNIT_ASSERT_EQUAL(static_cast<sal_uInt32>(480), rFontHeightItem.GetHeight());
 }
 }
 