a13ac49f87244fbc57ef43833bad4c247ef6ceac
Michael Weghorn <m.weghorn@posteo.de>
2025-10-16 23:47:26 +0200

    tdf#130857 qt weld: Support dialog to show extension license
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    To trigger the dialog:
    
    * install an extension, e.g. "Tango color palette" [1],
      restart LO
    * "Tools" -> "Extensions"
    * right-click on the extension to open the context menu
    * select "Show License" context menu entry
    
    [1] https://extensions.libreoffice.org/en/extensions/show/tango-color-variations
    
    Change-Id: I7e499c6547385518226ed8f1689f8290b5b85571
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192558
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -119,6 +119,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"dbaccess/ui/savedialog.ui"_ustr,
         u"dbaccess/ui/tabledesignsavemodifieddialog.ui"_ustr,
         u"desktop/ui/installforalldialog.ui"_ustr,
+        u"desktop/ui/showlicensedialog.ui"_ustr,
         u"desktop/ui/licensedialog.ui"_ustr,
         u"filter/ui/pdfoptionsdialog.ui"_ustr,
         u"filter/ui/testxmlfilter.ui"_ustr,
