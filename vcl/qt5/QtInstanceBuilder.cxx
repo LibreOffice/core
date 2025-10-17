d24e74b16b728ebca060bd5349cba945a55a018b
Michael Weghorn <m.weghorn@posteo.de>
2025-10-17 17:57:17 +0200

    tdf#130857 qt weld: Support Calc "F-Test" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered in Calc using "Data" -> "Statistics"
    -> "F-Test".
    
    Change-Id: I3b9d11a0b55792272e3dd630b3d8aa3180aec1c8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192588
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -145,6 +145,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"modules/scalc/ui/selectsource.ui"_ustr,
         u"modules/scalc/ui/showsheetdialog.ui"_ustr,
         u"modules/scalc/ui/sortdialog.ui"_ustr,
+        u"modules/scalc/ui/ttestdialog.ui"_ustr,
         u"modules/schart/ui/insertaxisdlg.ui"_ustr,
         u"modules/sdraw/ui/dlgsnap.ui"_ustr,
         u"modules/sdraw/ui/insertlayer.ui"_ustr,
