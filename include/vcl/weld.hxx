e9396f82412863e69ac9852ec1f48bf83787f10b
Michael Weghorn <m.weghorn@posteo.de>
2025-10-18 02:50:43 +0200

    weld: Use #pragma once
    Change-Id: I158791c0b4a8396feb08a8b46dddbeefe39e73d4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192596
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- include/vcl/weld.hxx
+++ include/vcl/weld.hxx
@@ -7,8 +7,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#ifndef INCLUDED_VCL_WELD_HXX
-#define INCLUDED_VCL_WELD_HXX
+#pragma once
 
 #include <basegfx/range/b2irange.hxx>
 #include <rtl/ustring.hxx>
@@ -2947,6 +2946,5 @@ void Dialog::set_default_response(int nResponse)
     change_default_button(nullptr, pButton.get());
 }
 }
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
