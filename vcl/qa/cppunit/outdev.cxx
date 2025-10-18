LibreOffice/core

commit 088a7c7c451321a800ca8d3523a18b6bb93239b7

remove alpha device from OutputDevice
and render to a combined color+alpha bitmap

The strategy is to remove the mpAlphaVDev from OutputDevice,
and have the OutputDevice create 32-bit SalBitmap's
and then have the various backends correctly render to
those 32-bit bitmaps.

However, whenever we extract a BitmapEx from OutputDevice
(e.g. via VirtualDevice) and hand it to the rest of the
system, we split the color and alpha information.
So the rest of the codebase doesn't have to deal with
this yet.

We have some major problems to overcome here:
(*) Cairo uses 32-bit data, even if we ask for 24-bit color with no alpha.
    This is because the current version of cairo does not support RGB 24-bit data.
(*) Sometimes we want the bitmap to be initialised with an opaque white background,
    sometimes we want it to be transparent.
(*) Sometimes when we extract a bitmap from a device, we want the alpha channel,
    sometimes not.
(*) There are lots of left-over bits of previous attempts of this change lying around,
    e.g. in skia.

Notes
(*) we allow Bitmap (ie. SalBitmap) to hold color+alpha
(*) The BitmapEx(Bitmap) constructor checks if the incoming
Bitmap is color+alpha, and if so, splits into a color bitmap and an alpha bitmap
(*) Because the underlying image data is stored pre-multipled, if
we write a transparent pixel with color, and then read the pixel value back,
we might lose the color information.

Change-Id: I0fc0042d6e5be5edd99b320892fb00a8eb2842fe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173937
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/headless/CairoCommon.cxx
@@ -642,6 +642,27 @@ void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
     }
 }
 
+void CairoCommon::applyColor2(cairo_t* cr, Color aColor)
+{
+    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+    cairo_content_t eContentType = cairo_surface_get_content(cairo_get_target(cr));
+    if (eContentType == CAIRO_CONTENT_COLOR_ALPHA)
+    {
+        cairo_set_source_rgba(cr, aColor.GetRed() / 255.0, aColor.GetGreen() / 255.0,
+                              aColor.GetBlue() / 255.0, aColor.GetAlpha() / 255.0);
+    }
+    else if (eContentType == CAIRO_CONTENT_COLOR)
+    {
+        cairo_set_source_rgba(cr, aColor.GetRed() / 255.0, aColor.GetGreen() / 255.0,
+                              aColor.GetBlue() / 255.0, 1.0);
+    }
+    else // CAIRO_CONTENT_ALPHA
+    {
+        double fSet = aColor == COL_BLACK ? 1.0 : 0.0;
+        cairo_set_source_rgba(cr, 1, 1, 1, fSet);
+    }
+}
+
 void CairoCommon::clipRegion(cairo_t* cr, const vcl::Region& rClipRegion)
 {
     RectangleVector aRectangles;
@@ -713,7 +734,25 @@ void CairoCommon::drawPixel(const std::optional<Color>& rLineColor, tools::Long 
     clipRegion(cr);
 
     cairo_rectangle(cr, nX, nY, 1, 1);
-    CairoCommon::applyColor(cr, *rLineColor, 0.0);
+
+    cairo_content_t eContentType = cairo_surface_get_content(cairo_get_target(cr));
+    if (eContentType == CAIRO_CONTENT_COLOR_ALPHA)
+    {
+        cairo_set_source_rgba(cr, rLineColor->GetRed() / 255.0, rLineColor->GetGreen() / 255.0,
+                              rLineColor->GetBlue() / 255.0, rLineColor->GetAlpha() / 255.0);
+    }
+    else if (eContentType == CAIRO_CONTENT_COLOR)
+    {
+        cairo_set_source_rgba(cr, rLineColor->GetRed() / 255.0, rLineColor->GetGreen() / 255.0,
+                              rLineColor->GetBlue() / 255.0, 1.0);
+    }
+    else // eContentType == CAIRO_CONTENT_ALPHA
+    {
+        double fSet = *rLineColor == COL_BLACK ? 1.0 : 0.0;
+        cairo_set_source_rgba(cr, 1, 1, 1, fSet);
+        cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
+    }
+
     cairo_fill(cr);
 
     basegfx::B2DRange extents = getClippedFillDamage(cr);
@@ -801,7 +840,6 @@ void CairoCommon::drawRect(double nX, double nY, double nWidth, double nHeight, 
     // fast path for the common case of simply creating a solid block of color
     if (onlyFillRect(m_oFillColor, m_oLineColor))
     {
-        double fTransparency = 0;
         // don't bother trying to draw stuff which is effectively invisible
         if (nWidth < 0.1 || nHeight < 0.1)
             return;
@@ -820,7 +858,7 @@ void CairoCommon::drawRect(double nX, double nY, double nWidth, double nHeight, 
         }
         cairo_rectangle(cr, nX, nY, nWidth, nHeight);
 
-        CairoCommon::applyColor(cr, *m_oFillColor, fTransparency);
+        CairoCommon::applyColor2(cr, *m_oFillColor);
         // Get FillDamage
         basegfx::B2DRange extents = getClippedFillDamage(cr);
 
@@ -1876,14 +1914,15 @@ void CairoCommon::drawMask(const SalTwoRect& rTR, const SalBitmap& rSalBitmap, C
 }
 
 std::shared_ptr<SalBitmap> CairoCommon::getBitmap(tools::Long nX, tools::Long nY,
-                                                  tools::Long nWidth, tools::Long nHeight)
+                                                  tools::Long nWidth, tools::Long nHeight,
+                                                  bool bWithoutAlpha)
 {
     std::shared_ptr<SvpSalBitmap> pBitmap = std::make_shared<SvpSalBitmap>();
     BitmapPalette aPal;
     assert(GetBitCount() != 1 && "not supported anymore");
     vcl::PixelFormat ePixelFormat = vcl::PixelFormat::N32_BPP;
 
-    if (!pBitmap->ImplCreate(Size(nWidth, nHeight), ePixelFormat, aPal, false))
+    if (!pBitmap->ImplCreate(Size(nWidth, nHeight), ePixelFormat, aPal, false, bWithoutAlpha))
     {
         SAL_WARN("vcl.gdi", "SvpSalGraphics::getBitmap, cannot create bitmap");
         return nullptr;
@@ -2023,8 +2062,8 @@ std::optional<BitmapBuffer> FastConvert24BitRgbTo32BitCairo(const BitmapBuffer* 
         for (tools::Long x = 0; x < nWidth; ++x)
         {
 #if ENABLE_CAIRO_RGBA
-            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcRgbx,
-                          "Expected SVP_CAIRO_FORMAT set to N32BitTcRgbx");
+            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcRgba,
+                          "Expected SVP_CAIRO_FORMAT set to N32BitTcRgba");
             static_assert(SVP_24BIT_FORMAT == ScanlineFormat::N24BitTcRgb,
                           "Expected SVP_24BIT_FORMAT set to N24BitTcRgb");
             pD[0] = pS[0];
@@ -2032,8 +2071,8 @@ std::optional<BitmapBuffer> FastConvert24BitRgbTo32BitCairo(const BitmapBuffer* 
             pD[2] = pS[2];
             pD[3] = 0xff; // Alpha
 #elif defined OSL_BIGENDIAN
-            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcXrgb,
-                          "Expected SVP_CAIRO_FORMAT set to N32BitTcXrgb");
+            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcArgb,
+                          "Expected SVP_CAIRO_FORMAT set to N32BitTcArgb");
             static_assert(SVP_24BIT_FORMAT == ScanlineFormat::N24BitTcRgb,
                           "Expected SVP_24BIT_FORMAT set to N24BitTcRgb");
             pD[0] = 0xff; // Alpha
@@ -2041,8 +2080,8 @@ std::optional<BitmapBuffer> FastConvert24BitRgbTo32BitCairo(const BitmapBuffer* 
             pD[2] = pS[1];
             pD[3] = pS[2];
 #else
-            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcBgrx,
-                          "Expected SVP_CAIRO_FORMAT set to N32BitTcBgrx");
+            static_assert(SVP_CAIRO_FORMAT == ScanlineFormat::N32BitTcBgra,
+                          "Expected SVP_CAIRO_FORMAT set to N32BitTcAgrx");
             static_assert(SVP_24BIT_FORMAT == ScanlineFormat::N24BitTcBgr,
                           "Expected SVP_24BIT_FORMAT set to N24BitTcBgr");
             pD[0] = pS[0];

vcl/qa/cppunit/outdev.cxx
@@ -890,6 +890,7 @@ public:
     void ReleaseGraphics(bool) override {}
     bool UsePolyPolygonForComplexGradient() override { return false; }
     bool CanAnimate() const override { return false; }
+    virtual bool HasAlpha() const override { return false; }
 
     bool testShouldDrawWavePixelAsRect(tools::Long nLineWidth)
     {

vcl/source/outdev/outdev.cxx
@@ -70,7 +70,6 @@ OutputDevice::OutputDevice(OutDevType eOutDevType) :
     mpFontInstance                  = nullptr;
     mpForcedFallbackInstance        = nullptr;
     mpFontFaceCollection            = nullptr;
-    mpAlphaVDev                     = nullptr;
     mpExtOutDevData                 = nullptr;
     mnOutOffX                       = 0;
     mnOutOffY                       = 0;
@@ -178,7 +177,6 @@ void OutputDevice::dispose()
     // release ImplFontList specific to this OutputDevice
     mxFontCollection.reset();
 
-    mpAlphaVDev.disposeAndClear();
     mpPrevGraphics.reset();
     mpNextGraphics.reset();
     VclReferenceBase::dispose();
@@ -217,9 +215,6 @@ void OutputDevice::SetConnectMetaFile( GDIMetaFile* pMtf )
 void OutputDevice::SetSettings( const AllSettings& rSettings )
 {
     *moSettings = rSettings;
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->SetSettings( rSettings );
 }
 
 SystemGraphicsData OutputDevice::GetSystemGfxData() const
@@ -307,9 +302,6 @@ void OutputDevice::SetRefPoint()
     mbRefPoint = false;
     maRefPoint.setX(0);
     maRefPoint.setY(0);
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->SetRefPoint();
 }
 
 void OutputDevice::SetRefPoint( const Point& rRefPoint )
@@ -319,9 +311,6 @@ void OutputDevice::SetRefPoint( const Point& rRefPoint )
 
     mbRefPoint = true;
     maRefPoint = rRefPoint;
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->SetRefPoint( rRefPoint );
 }
 
 void OutputDevice::SetRasterOp( RasterOp eRasterOp )
@@ -340,17 +329,11 @@ void OutputDevice::SetRasterOp( RasterOp eRasterOp )
             mpGraphics->SetXORMode( (RasterOp::Invert == meRasterOp) || (RasterOp::Xor == meRasterOp), RasterOp::Invert == meRasterOp );
         }
     }
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->SetRasterOp( eRasterOp );
 }
 
 void OutputDevice::EnableOutput( bool bEnable )
 {
     mbOutput = bEnable;
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->EnableOutput( bEnable );
 }
 
 void OutputDevice::SetAntialiasing( AntialiasingFlags nMode )
@@ -363,17 +346,11 @@ void OutputDevice::SetAntialiasing( AntialiasingFlags nMode )
         if (mpGraphics)
             mpGraphics->setAntiAlias(bool(mnAntialiasing & AntialiasingFlags::Enable));
     }
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->SetAntialiasing( nMode );
 }
 
 void OutputDevice::SetDrawMode(DrawModeFlags nDrawMode)
 {
     mnDrawMode = nDrawMode;
-
-    if (mpAlphaVDev)
-        mpAlphaVDev->SetDrawMode(nDrawMode);
 }
 
 sal_uInt16 OutputDevice::GetBitCount() const
@@ -469,9 +446,6 @@ void OutputDevice::DrawOutDev( const Point& rDestPt, const Size& rDestSize,
         if ( aPosAry.mnSrcWidth && aPosAry.mnSrcHeight && aPosAry.mnDestWidth && aPosAry.mnDestHeight )
             mpGraphics->CopyBits(aPosAry, *this);
     }
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->DrawOutDev( rDestPt, rDestSize, rSrcPt, rSrcSize );
 }
 
 void OutputDevice::DrawOutDev( const Point& rDestPt, const Size& rDestSize,
@@ -489,16 +463,8 @@ void OutputDevice::DrawOutDev( const Point& rDestPt, const Size& rDestSize,
 
     if ( mpMetaFile )
     {
-        if (rOutDev.mpAlphaVDev)
-        {
-            const BitmapEx aBmpEx(rOutDev.GetBitmapEx(rSrcPt, rSrcSize));
-            mpMetaFile->AddAction(new MetaBmpExScaleAction(rDestPt, rDestSize, aBmpEx));
-        }
-        else
-        {
-            const Bitmap aBmp(rOutDev.GetBitmap(rSrcPt, rSrcSize));
-            mpMetaFile->AddAction(new MetaBmpScaleAction(rDestPt, rDestSize, aBmp));
-        }
+        const BitmapEx aBmpEx(rOutDev.GetBitmapEx(rSrcPt, rSrcSize));
+        mpMetaFile->AddAction(new MetaBmpExScaleAction(rDestPt, rDestSize, aBmpEx));
     }
 
     if ( !IsDeviceOutputNecessary() )
@@ -514,28 +480,17 @@ void OutputDevice::DrawOutDev( const Point& rDestPt, const Size& rDestSize,
     if ( mbOutputClipped )
         return;
 
-    if (rOutDev.mpAlphaVDev)
-    {
-        // alpha-blend source over destination
-        DrawBitmapEx(rDestPt, rDestSize, rOutDev.GetBitmapEx(rSrcPt, rSrcSize));
-    }
-    else
-    {
-        SalTwoRect aPosAry(rOutDev.ImplLogicXToDevicePixel(rSrcPt.X()),
-                           rOutDev.ImplLogicYToDevicePixel(rSrcPt.Y()),
-                           rOutDev.ImplLogicWidthToDevicePixel(rSrcSize.Width()),
-                           rOutDev.ImplLogicHeightToDevicePixel(rSrcSize.Height()),
-                           ImplLogicXToDevicePixel(rDestPt.X()),
-                           ImplLogicYToDevicePixel(rDestPt.Y()),
-                           ImplLogicWidthToDevicePixel(rDestSize.Width()),
-                           ImplLogicHeightToDevicePixel(rDestSize.Height()));
+    SalTwoRect aPosAry(rOutDev.ImplLogicXToDevicePixel(rSrcPt.X()),
+                             rOutDev.ImplLogicYToDevicePixel(rSrcPt.Y()),
+                             rOutDev.ImplLogicWidthToDevicePixel(rSrcSize.Width()),
+                             rOutDev.ImplLogicHeightToDevicePixel(rSrcSize.Height()),
+                             ImplLogicXToDevicePixel(rDestPt.X()),
+                             ImplLogicYToDevicePixel(rDestPt.Y()),
+                             ImplLogicWidthToDevicePixel(rDestSize.Width()),
+                             ImplLogicHeightToDevicePixel(rDestSize.Height()));
 
-        drawOutDevDirect(rOutDev, aPosAry);
-
-        // #i32109#: make destination rectangle opaque - source has no alpha
-        if (mpAlphaVDev)
-            mpAlphaVDev->ImplFillOpaqueRectangle(tools::Rectangle(rDestPt, rDestSize));
-    }
+    // if we have alpha, this will blend source over destination
+    drawOutDevDirect(rOutDev, aPosAry);
 }
 
 void OutputDevice::CopyArea( const Point& rDestPt,
@@ -576,9 +531,6 @@ void OutputDevice::CopyArea( const Point& rDestPt,
     }
 
     SetRasterOp( eOldRop );
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->CopyArea( rDestPt, rSrcPt, rSrcSize, bWindowInvalidate );
 }
 
 // Direct OutputDevice drawing protected function
@@ -654,9 +606,6 @@ tools::Rectangle OutputDevice::GetBackgroundComponentBounds() const
 void OutputDevice::EnableRTL( bool bEnable )
 {
     mbEnableRTL = bEnable;
-
-    if( mpAlphaVDev )
-        mpAlphaVDev->EnableRTL( bEnable );
 }
 
 bool OutputDevice::ImplIsAntiparallel() const

commit 99d7bbf367eeb85055853d0d03ec46b2d6bbebe3

tdf#168730 fix DrawRect with alpha
Where we were merging the fill color with the background
instead of overwriting it.

regression from
  commit 088a7c7c451321a800ca8d3523a18b6bb93239b7
  Author: Noel Grandin <noelgrandin@gmail.com>
  Date:   Tue Sep 24 16:18:11 2024 +0200
  remove alpha device from OutputDevice

And add some unit tests for this and for DrawPolyPolygon.
Verified both the new unit tests on a build from
before the regression commit.

Change-Id: Ia27e075d8be71fd35a2d6ef6b08e8f42b36a7776
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192482
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/headless/CairoCommon.cxx
@@ -629,6 +629,7 @@ cairo_t* CairoCommon::createTmpCompatibleCairoContext() const
 
 void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
 {
+    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     if (cairo_surface_get_content(cairo_get_target(cr)) != CAIRO_CONTENT_ALPHA)
     {
         cairo_set_source_rgba(cr, aColor.GetRed() / 255.0, aColor.GetGreen() / 255.0,
@@ -638,7 +639,6 @@ void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
     {
         double fSet = aColor == COL_BLACK ? 1.0 : 0.0;
         cairo_set_source_rgba(cr, 1, 1, 1, fSet);
-        cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     }
 }
 
@@ -880,7 +880,8 @@ void CairoCommon::drawRect(double nX, double nY, double nWidth, double nHeight, 
             basegfx::B2DRectangle(nX, nY, nX + nWidth, nY + nHeight));
 
         m_oFillColor = aOrigFillColor;
-        drawPolyPolygon(basegfx::B2DHomMatrix(), basegfx::B2DPolyPolygon(aRect), 0.0, bAntiAlias);
+        drawPolyPolygon(basegfx::B2DHomMatrix(), basegfx::B2DPolyPolygon(aRect),
+                        (255 - aOrigFillColor->GetAlpha()) / 255.0, bAntiAlias);
         m_oFillColor = std::nullopt;
     }
 

vcl/qa/cppunit/outdev.cxx
@@ -1847,6 +1847,59 @@ CPPUNIT_TEST_FIXTURE(VclOutdevTest, testDrawPolyPolygon)
     }
 }
 
+// Test and document existing behaviour
+CPPUNIT_TEST_FIXTURE(VclOutdevTest, testDrawPolyPolygonAlpha)
+{
+    Size aSize(100, 100);
+    ScopedVclPtrInstance<VirtualDevice> pVDev(DeviceFormat::WITH_ALPHA);
+    pVDev->SetOutputSizePixel(aSize);
+
+    // create a square polypolygon
+    tools::Polygon aPolygon(4);
+    aPolygon.SetPoint(Point(0, 0), 0);
+    aPolygon.SetPoint(Point(0, 100), 1);
+    aPolygon.SetPoint(Point(100, 100), 2);
+    aPolygon.SetPoint(Point(0, 0), 3);
+    tools::PolyPolygon aPolyPolygon(aPolygon);
+    aPolyPolygon.Optimize(PolyOptimizeFlags::CLOSE);
+    basegfx::B2DPolyPolygon aB2DPolyPolygon(aPolyPolygon.getB2DPolyPolygon());
+
+    Color aLineColor(ColorAlpha, 255, 255, 0, 0); // opaque red
+    Color aFillColor(ColorAlpha, 127, 255, 0, 0); // 50% transparent red
+    pVDev->SetLineColor(aLineColor);
+    pVDev->SetFillColor(aFillColor);
+    pVDev->DrawPolyPolygon(aB2DPolyPolygon);
+
+    CPPUNIT_ASSERT_EQUAL(aLineColor, pVDev->GetPixel(Point(0, 0)));
+    // existing behaviour - we drop the alpha when we fill a polypolygon
+    CPPUNIT_ASSERT_EQUAL(Color(ColorAlpha, 255, 255, 0, 0), pVDev->GetPixel(Point(1, 1)));
+}
+
+// Test and document existing behaviour
+CPPUNIT_TEST_FIXTURE(VclOutdevTest, testDrawRectAlpha)
+{
+    Size aSize(100, 100);
+    ScopedVclPtrInstance<VirtualDevice> pVDev(DeviceFormat::WITH_ALPHA);
+    pVDev->SetOutputSizePixel(aSize);
+
+    const Color RED_OPAQUE(ColorAlpha, 255, 255, 0, 0); // opaque red
+    const Color RED_TRANSPARENT(ColorAlpha, 127, 255, 0, 0); // 50% transparent red
+    pVDev->SetLineColor(RED_OPAQUE);
+    pVDev->SetFillColor(RED_TRANSPARENT);
+    pVDev->DrawRect(tools::Rectangle(0, 0, 100, 100));
+
+    CPPUNIT_ASSERT_EQUAL(RED_OPAQUE, pVDev->GetPixel(Point(0, 0)));
+    CPPUNIT_ASSERT_EQUAL(RED_TRANSPARENT, pVDev->GetPixel(Point(1, 1)));
+
+    pVDev->SetLineColor(RED_TRANSPARENT);
+    pVDev->SetFillColor(RED_OPAQUE);
+    pVDev->DrawRect(tools::Rectangle(0, 0, 100, 100));
+
+    // existing behaviour - we drop the alpha when we draw the line for a rect
+    CPPUNIT_ASSERT_EQUAL(RED_OPAQUE, pVDev->GetPixel(Point(0, 0)));
+    CPPUNIT_ASSERT_EQUAL(RED_OPAQUE, pVDev->GetPixel(Point(1, 1)));
+}
+
 static size_t ClipGradientTest(GDIMetaFile& rMtf, size_t nIndex)
 {
     MetaAction* pAction = rMtf.GetAction(nIndex);