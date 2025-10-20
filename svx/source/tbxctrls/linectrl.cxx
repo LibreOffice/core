25e08e4ea0df956f46bd7ef81493fcf5a5c1879c
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-10-20 21:04:09 +0100

    Resolves: tdf#164920 add scrollbar width to desired size if enabled
    Change-Id: Ib6e354d5a4a9076b81e6a26fe78bdd4994024ec1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192755
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- svx/source/tbxctrls/linectrl.cxx
+++ svx/source/tbxctrls/linectrl.cxx
@@ -432,6 +432,8 @@ void SvxLineEndWindow::SetSize()
     aSize.AdjustWidth(6 );
     aSize.AdjustHeight(6 );
     aSize = mxLineEndSet->CalcWindowSizePixel( aSize );
+    if (nBits | WB_VSCROLL)
+        aSize.AdjustWidth(mxLineEndSet->GetScrollWidth());
     mxLineEndSet->GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     mxLineEndSet->SetOutputSizePixel(aSize);
 }
@@ -571,6 +573,8 @@ void SvxLineBox::Fill( const XDashListRef &pList )
     aSize.AdjustWidth(6);
     aSize.AdjustHeight(6);
     aSize = mxLineStyleSet->CalcWindowSizePixel(aSize);
+    if (nBits | WB_VSCROLL)
+        aSize.AdjustWidth(mxLineStyleSet->GetScrollWidth());
     mxLineStyleSet->GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     mxLineStyleSet->SetOutputSizePixel(aSize);
 }
