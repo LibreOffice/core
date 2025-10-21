0347c512676fed5b1e644826364b8ead13f72953
Michael Weghorn <m.weghorn@posteo.de>
2025-10-21 22:20:25 +0200

    tdf#130857 qt weld: Support Calc "Fourier Analysis" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    This dialog can be triggered in Calc using
    "Data" -> "Statistics" -> "Fourier Analysis...".
    
    Change-Id: I9da0a58e12d14832efd0efe3c5cd03e61b8279cd
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192814
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -133,6 +133,7 @@ bool QtInstanceBuilder::IsUIFileSupported(const OUString& rUIFile, const weld::W
         u"modules/scalc/ui/deletecells.ui"_ustr,
         u"modules/scalc/ui/deletecontents.ui"_ustr,
         u"modules/scalc/ui/duplicaterecordsdlg.ui"_ustr,
+        u"modules/scalc/ui/fourieranalysisdialog.ui"_ustr,
         u"modules/scalc/ui/gotosheetdialog.ui"_ustr,
         u"modules/scalc/ui/groupbydate.ui"_ustr,
         u"modules/scalc/ui/groupdialog.ui"_ustr,
