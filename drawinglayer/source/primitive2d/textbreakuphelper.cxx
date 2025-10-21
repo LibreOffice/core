3e162e8ec1530966e6ba5cbe17a49a92a11430e1
Khaled Hosny <khaled@aliftype.com>
2025-10-21 13:11:52 +0300

    tdf#168884: Restore missing transparency in EMF files
    Revert unrelated changes from:
    
    commit 1bdf1e2d52a7fae3e1a58ddf15a7657df3d47004
    Author: Khaled Hosny <khaled@aliftype.com>
    Date:   Thu Sep 25 16:33:09 2025 +0300
    
        tdf#168371: Disable ligatures in Impress/Draw with character spacing
    
    I added a new parameter to TextSimplePortionPrimitive2D constructor, and
    rTextFillColor that previously was using the default value had now to be
    explicitly set, but I should have used COL_TRANSPARENT as that is the
    default.
    
    Change-Id: Ie51e8f3f4c68a9539eb3f6cc77fb1e6cf44f673f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192779
    Reviewed-by: Khaled Hosny <khaled@libreoffice.org>
    Tested-by: Jenkins
    (cherry picked from commit 1dce54d8fb070d3d4134317612c145a923497bb4)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192790
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- drawinglayer/source/primitive2d/textbreakuphelper.cxx
+++ drawinglayer/source/primitive2d/textbreakuphelper.cxx
@@ -182,7 +182,7 @@ namespace drawinglayer::primitive2d
                         mrSource.getFontAttribute(),
                         mrSource.getLocale(),
                         mrSource.getFontColor(),
-                        mrSource.getTextFillColor(),
+                        COL_TRANSPARENT,
                         mrSource.getLetterSpacing()));
             }
         }
