76d85a1364ce333d62be7e16665e2cf9f70ec024
Stephan Bergmann <stephan.bergmann@collabora.com>
2025-10-22 20:43:13 +0200

    Parametrize assemble-flatpak over the flatpak ID
    (solenv/bin/assemble-flatpak-desktop.sh in one place uses that parameterized
    value in a sed s command replacement text, which could potentially cause syntax
    errors.  But since flatpak IDs shall be formed from a restricted alphabet, that
    should not cause problems in practice.  Similarly, various places across the
    solenv/bin/assemble-flatpak*.sh scripts use that perameterized value in forming
    pathnames, and assume that it does not contain any slahses.)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192883
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Sarper Akdemir <sarper.akdemir@allotropia.de>
    (cherry picked from commit 15007243c11363c636d74c240946237da5a866b8)
    Conflicts:
    	solenv/bin/assemble-flatpak-appdata.sh
    	solenv/bin/assemble-flatpak-desktop.sh
    	solenv/bin/assemble-flatpak.sh
    	solenv/flatpak-manifest.in
    
    Change-Id: I5b4c031acf19b04ac8b501d9d8557924747fb38b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194174
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>

--- solenv/bin/assemble-flatpak-desktop.sh
+++ solenv/bin/assemble-flatpak-desktop.sh
@@ -14,29 +14,30 @@
 # $1  pathname, ending in a slash, of the directory containing the source libreoffice-*.desktop
 #     files
 # $2  pathname, ending in a slash, of the directory into which to put the target
-#     org.libreoffice.LibreOffice.*.desktop files
+#     $3.*.desktop files
+# $3  the flatpak ID
 
 set -e
 
-## libreoffice-*.desktop -> org.libreoffice.LibreOffice.*.desktop:
+## libreoffice-*.desktop -> $3.*.desktop:
 for i in "${1?}"libreoffice-*.desktop
 do
- sed -e 's/^Icon=libreoffice-/Icon=org.libreoffice.LibreOffice./' "$i" \
-  >"${2?}"org.libreoffice.LibreOffice."${i#"${1?}"libreoffice-}"
+ sed -e "s/^Icon=libreoffice-/Icon=${3?}./" "$i" \
+  >"${2?}${3?}"."${i#"${1?}"libreoffice-}"
 done
-mv "${2?}"org.libreoffice.LibreOffice.startcenter.desktop "${2?}"org.libreoffice.LibreOffice.desktop
+mv "${2?}${3?}".startcenter.desktop "${2?}${3?}".desktop
 
 # Flatpak .desktop exports take precedence over system ones due to
 # the order of XDG_DATA_DIRS - re-associating text/plain seems a bit much
-sed -i "s/text\/plain;//" "${2?}"org.libreoffice.LibreOffice.writer.desktop
+sed -i "s/text\/plain;//" "${2?}${3?}".writer.desktop
 
 desktop-file-edit --set-key=X-Endless-Alias --set-value=libreoffice-startcenter \
  --set-key=X-Flatpak-RenamedFrom --set-value='libreoffice-startcenter.desktop;' \
- "${2?}"org.libreoffice.LibreOffice.desktop
+ "${2?}${3?}".desktop
 for i in base calc draw impress math writer xsltfilter
 do
  desktop-file-edit --set-key=X-Endless-Alias --set-value=libreoffice-"$i" \
   --set-key=X-Flatpak-RenamedFrom \
-  --set-value="libreoffice-$i.desktop;org.libreoffice.LibreOffice-$i.desktop;" \
-  "${2?}"org.libreoffice.LibreOffice."$i".desktop
+  --set-value="libreoffice-$i.desktop;${3?}-$i.desktop;" \
+  "${2?}${3?}"."$i".desktop
 done
