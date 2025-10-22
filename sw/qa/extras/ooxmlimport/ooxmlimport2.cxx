LibreOffice/core

commit 7c4dba1deffd81f647a4a3be7a79f68f3bf9f1ba

fix a regression crash from commit 5082d50
The commit generated crashes in the crashtest documents
7711 bugtrackers/docx/fdo78333-1.docx and
695 forums/docx/forum-mso-en-4096.docx.
This was due to "m_xFieldStartRange", the start of the text
portion of the block SDT, being recorded too early. This led to
including characters that shouldn't be there in the generated
content control.
This patch moves the calls to setFieldStartRange to just before
the first appendTextPortion call.

Change-Id: I7230346fee9a37ebac70beb9bcafd9d7b612eb00
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/159816
Tested-by: Jenkins
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

writerfilter/qa/cppunittests/dmapper/DomainMapper.cxx
@@ -149,6 +149,12 @@ CPPUNIT_TEST_FIXTURE(Test, testSdtBlockText)
     xContentControlProps->getPropertyValue("Alias") >>= aAlias;
     CPPUNIT_ASSERT_EQUAL(OUString("myalias"), aAlias);
 }
+
+CPPUNIT_TEST_FIXTURE(Test, testFdo78333)
+{
+    // just care that it doesn't crash/assert
+    loadFromURL(u"fdo78333-1-minimized.docx");
+}
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1192,9 +1192,6 @@ void DomainMapper::lcl_attribute(Id nName, Value & val)
                 m_pImpl->PushSdt();
                 break;
             }
-            if (m_pImpl->m_pSdtHelper->getControlType() == SdtControlType::plainText
-                && GetCurrentTextRange().is())
-                m_pImpl->m_pSdtHelper->setFieldStartRange(GetCurrentTextRange()->getEnd());
             m_pImpl->SetSdt(true);
         }
         break;
@@ -4229,7 +4226,6 @@ void DomainMapper::lcl_utext(const sal_Unicode *const data_, size_t len)
         {
             m_pImpl->m_pSdtHelper->createPlainTextControl();
             finishParagraph();
-            m_pImpl->m_pSdtHelper->setFieldStartRange(GetCurrentTextRange()->getEnd());
             return;
         }
     }
@@ -4487,10 +4483,9 @@ void DomainMapper::lcl_utext(const sal_Unicode *const data_, size_t len)
                 m_pImpl->clearDeferredBreaks();
             }
 
-            bool bSdtBlockUnusedText
-                = m_pImpl->m_pSdtHelper->GetSdtType() != NS_ooxml::LN_CT_SdtRun_sdtContent
-                  && m_pImpl->m_pSdtHelper->getControlType() == SdtControlType::plainText
-                  && m_pImpl->m_pSdtHelper->hasUnusedText();
+            bool bInSdtBlockText
+                = m_pImpl->m_pSdtHelper->GetSdtType() == NS_ooxml::LN_CT_SdtBlock_sdtContent
+                  && m_pImpl->m_pSdtHelper->getControlType() == SdtControlType::plainText;
             if (pContext && pContext->GetFootnote().is())
             {
                 pContext->GetFootnote()->setLabel( sText );
@@ -4500,32 +4495,29 @@ void DomainMapper::lcl_utext(const sal_Unicode *const data_, size_t len)
             }
             else if (m_pImpl->IsOpenFieldCommand() && !m_pImpl->IsForceGenericFields())
             {
-                if (bSdtBlockUnusedText)
+                if (bInSdtBlockText && m_pImpl->m_pSdtHelper->hasUnusedText())
                     m_pImpl->m_pSdtHelper->createPlainTextControl();
                 m_pImpl->AppendFieldCommand(sText);
-                if (bSdtBlockUnusedText)
-                    m_pImpl->m_pSdtHelper->setFieldStartRange(GetCurrentTextRange()->getEnd());
             }
             else if( m_pImpl->IsOpenField() && m_pImpl->IsFieldResultAsString())
             {
-                if (bSdtBlockUnusedText)
+                if (bInSdtBlockText && m_pImpl->m_pSdtHelper->hasUnusedText())
                     m_pImpl->m_pSdtHelper->createPlainTextControl();
                 /*depending on the success of the field insert operation this result will be
                   set at the field or directly inserted into the text*/
                 m_pImpl->AppendFieldResult(sText);
-                if (bSdtBlockUnusedText)
-                    m_pImpl->m_pSdtHelper->setFieldStartRange(GetCurrentTextRange()->getEnd());
             }
             else
             {
                 if (pContext == nullptr)
                     pContext = new PropertyMap();
 
+                if (bInSdtBlockText && !m_pImpl->m_pSdtHelper->hasUnusedText())
+                    m_pImpl->m_pSdtHelper->setFieldStartRange(GetCurrentTextRange()->getEnd());
+
                 m_pImpl->appendTextPortion( sText, pContext );
 
-                if (m_pImpl->m_pSdtHelper->GetSdtType() == NS_ooxml::LN_CT_SdtBlock_sdtContent
-                    && m_pImpl->m_pSdtHelper->getControlType() == SdtControlType::plainText
-                    && !sText.isEmpty())
+                if (bInSdtBlockText && !sText.isEmpty())
                     m_pImpl->m_pSdtHelper->setHasUnusedText(true);
             }
 

writerfilter/source/dmapper/SdtHelper.cxx
@@ -604,6 +604,7 @@ void SdtHelper::clear()
     m_nId = 0;
     m_nTabIndex = 0;
     m_aLock.clear();
+    m_xFieldStartRange.clear();
 }
 
 void SdtHelper::SetPlaceholderDocPart(const OUString& rPlaceholderDocPart)

commit 72a326b2899115860a87cfa55b82f745a76128da

tdf#168567: Fix import of SDTs with empty content
regression from commit 7c4dba1deffd81f647a4a3be7a79f68f3bf9f1

SDTs(content controls) with empty paragraph content are not properly
imported.

Change-Id: I340a060f03b419b3352fad2a8fb5ac3af043be54
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192466
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -4445,6 +4445,9 @@ void DomainMapper::lcl_utext(const sal_Unicode *const data_, size_t len)
     {
         if (bNewLine)
         {
+            if (!m_pImpl->m_pSdtHelper->isFieldStartRangeSet())
+                m_pImpl->m_pSdtHelper->setFieldStartRange(GetCurrentTextRange()->getEnd());
+
             m_pImpl->m_pSdtHelper->createPlainTextControl();
             finishParagraph();
             return;

sw/qa/extras/ooxmlimport/ooxmlimport2.cxx
@@ -22,6 +22,7 @@
 #include <com/sun/star/style/BreakType.hpp>
 #include <com/sun/star/text/XTextDocument.hpp>
 #include <com/sun/star/text/XTextTable.hpp>
+#include <com/sun/star/table/XCellRange.hpp>
 
 #include <comphelper/propertysequence.hxx>
 #include <vcl/BitmapReadAccess.hxx>
@@ -1351,6 +1352,43 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf153196)
     // - Expected: 0
     // - Actual  : 4265
     CPPUNIT_ASSERT_EQUAL(static_cast<sal_Int32>(0), nRightMargin);
+}
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf168567)
+{
+    createSwDoc("tdf168567.docx");
+
+    // Access the first table in the doc
+    uno::Reference<text::XTextTablesSupplier> xTablesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<container::XIndexAccess> xTables(xTablesSupplier->getTextTables(),
+                                                    uno::UNO_QUERY);
+    uno::Reference<text::XTextTable> xTable(xTables->getByIndex(0), uno::UNO_QUERY);
+    uno::Reference<table::XTableRows> xRows = xTable->getRows();
+    uno::Reference<table::XCellRange> xCellRange(xTable, uno::UNO_QUERY);
+
+    sal_Int32 nLastRow = xRows->getCount() - 1;
+
+    // Check if all cells in the last row contain `Content Controls`(SDTs)
+    for (sal_Int32 col = 0; col < xTable->getColumns()->getCount(); col++)
+    {
+        uno::Reference<text::XText> xCell(xCellRange->getCellByPosition(col, nLastRow),
+                                          uno::UNO_QUERY);
+
+        uno::Reference<container::XEnumerationAccess> xParaAccess(xCell, uno::UNO_QUERY);
+        uno::Reference<container::XEnumeration> xParagraphs = xParaAccess->createEnumeration();
+
+        uno::Reference<container::XEnumerationAccess> xParagraph(xParagraphs->nextElement(),
+                                                                 uno::UNO_QUERY);
+        uno::Reference<container::XEnumeration> xPortions = xParagraph->createEnumeration();
+
+        uno::Reference<beans::XPropertySet> xTextPortion(xPortions->nextElement(), uno::UNO_QUERY);
+
+        // Check if it's a content control
+        // Without the fix the last cell will result false
+        OUString aPortionType;
+        xTextPortion->getPropertyValue(u"TextPortionType"_ustr) >>= aPortionType;
+        CPPUNIT_ASSERT_EQUAL(u"ContentControl"_ustr, aPortionType);
+    }
 }
 
 // tests should only be added to ooxmlIMPORT *if* they fail round-tripping in ooxmlEXPORT