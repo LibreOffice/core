LibreOffice/core

commit 25e08e4ea0df956f46bd7ef81493fcf5a5c1879c

Resolves: tdf#164920 add scrollbar width to desired size if enabled
Change-Id: Ib6e354d5a4a9076b81e6a26fe78bdd4994024ec1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192755
Tested-by: Jenkins
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

svx/source/tbxctrls/linectrl.cxx
@@ -432,6 +432,8 @@ void SvxLineEndWindow::SetSize()
     aSize.AdjustWidth(6 );
     aSize.AdjustHeight(6 );
     aSize = mxLineEndSet->CalcWindowSizePixel( aSize );
+    if (nBits | WB_VSCROLL)
+        aSize.AdjustWidth(mxLineEndSet->GetScrollWidth());
     mxLineEndSet->GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     mxLineEndSet->SetOutputSizePixel(aSize);
 }
@@ -571,6 +573,8 @@ void SvxLineBox::Fill( const XDashListRef &pList )
     aSize.AdjustWidth(6);
     aSize.AdjustHeight(6);
     aSize = mxLineStyleSet->CalcWindowSizePixel(aSize);
+    if (nBits | WB_VSCROLL)
+        aSize.AdjustWidth(mxLineStyleSet->GetScrollWidth());
     mxLineStyleSet->GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     mxLineStyleSet->SetOutputSizePixel(aSize);
 }

commit ad6e817e38545903f5fbbe11a2dcb033e5affead

Fix presumed | vs. & typos
...introduced in 25e08e4ea0df956f46bd7ef81493fcf5a5c1879c "Resolves: tdf#164920
add scrollbar width to desired size if enabled" and causing

> svx/source/tbxctrls/linectrl.cxx:435:15: error: bitwise or with non-zero value always evaluates to true [-Werror,-Wtautological-bitwise-compare]
>   435 |     if (nBits | WB_VSCROLL)
>       |         ~~~~~~^~~~~~~~~~~~
> svx/source/tbxctrls/linectrl.cxx:576:15: error: bitwise or with non-zero value always evaluates to true [-Werror,-Wtautological-bitwise-compare]
>   576 |     if (nBits | WB_VSCROLL)
>       |         ~~~~~~^~~~~~~~~~~~

Change-Id: I6f77cd30f3b98abb15afb665b336fba13d46ee01
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192963
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>

svx/source/tbxctrls/linectrl.cxx
@@ -432,7 +432,7 @@ void SvxLineEndWindow::SetSize()
     aSize.AdjustWidth(6 );
     aSize.AdjustHeight(6 );
     aSize = mxLineEndSet->CalcWindowSizePixel( aSize );
-    if (nBits | WB_VSCROLL)
+    if (nBits & WB_VSCROLL)
         aSize.AdjustWidth(mxLineEndSet->GetScrollWidth());
     mxLineEndSet->GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     mxLineEndSet->SetOutputSizePixel(aSize);
@@ -573,7 +573,7 @@ void SvxLineBox::Fill( const XDashListRef &pList )
     aSize.AdjustWidth(6);
     aSize.AdjustHeight(6);
     aSize = mxLineStyleSet->CalcWindowSizePixel(aSize);
-    if (nBits | WB_VSCROLL)
+    if (nBits & WB_VSCROLL)
         aSize.AdjustWidth(mxLineStyleSet->GetScrollWidth());
     mxLineStyleSet->GetDrawingArea()->set_size_request(aSize.Width(), aSize.Height());
     mxLineStyleSet->SetOutputSizePixel(aSize);