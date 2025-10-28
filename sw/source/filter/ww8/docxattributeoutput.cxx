43e293e744bfe153d74e3f20cd4235636f51ef36
Noel Grandin <noel.grandin@collabora.co.uk>
2025-10-28 16:07:09 +0200

    mso-test: emit w:sz in correct order in document.xml
    When loading and then saving the document from forum-mso-en4-364124.docx,
    we end up with incorrect ordering of w:sz under w:rPr in document.xml
    
    Re-use our existing ordering logic to get the right order here.
    
    Change-Id: Ide57bd44b59033cd8cd6e885e8b9578a87314856
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193097
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -3176,6 +3176,8 @@ void DocxAttributeOutput::DoWriteFieldRunProperties( const SwTextNode * pNode, s
 
     {
         m_pSerializer->startElementNS(XML_w, XML_rPr);
+        // mark() before child elements.
+        InitCollectedRunProperties();
 
         // 1. output webHidden flag
         if(GetExport().m_bHideTabLeaderAndPageNumbers && m_pHyperlinkAttrList.is() )
@@ -3189,6 +3191,9 @@ void DocxAttributeOutput::DoWriteFieldRunProperties( const SwTextNode * pNode, s
 
         // 3. write the character properties
         WriteCollectedRunProperties();
+
+        // Merge the marks for the ordered elements
+        m_pSerializer->mergeTopMarks(Tag_InitCollectedRunProperties);
 
         m_pSerializer->endElementNS( XML_w, XML_rPr );
     }
