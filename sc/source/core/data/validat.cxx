LibreOffice/core

commit 72bbb0dd4a368dab2b1bd99917cd07d23a92a3f0

sc: make data validy error dialog async (invalid value error)
Change-Id: Icd077776b26e21b226b4dee5beb1a5ff6dcf301a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/179509
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
(cherry picked from commit 495be2dcb34d22af59a2028d3a686a0d48774166)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/180286
Tested-by: Jenkins
Reviewed-by: Pranam Lashkari <lpranam@collabora.com>

sc/source/core/data/validat.cxx
@@ -245,10 +245,10 @@ bool ScValidationData::DoScript( const ScAddress& rPos, const OUString& rInput,
     // Macro not found (only with input)
     {
         //TODO: different error message, if found, but not bAllowed ??
-        std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(pParent,
+        std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(pParent,
                                                   VclMessageType::Warning, VclButtonsType::Ok,
                                                   ScResId(STR_VALID_MACRONOTFOUND)));
-        xBox->run();
+        xBox->runAsync(xBox, [] (sal_uInt32){ });
     }
 
     return bScriptReturnedFalse;
@@ -351,10 +351,10 @@ bool ScValidationData::DoMacro( const ScAddress& rPos, const OUString& rInput,
     if ( !bDone && !pCell )         // Macro not found (only with input)
     {
         //TODO: different error message, if found, but not bAllowed ??
-        std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(pParent,
+        std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(pParent,
                                                   VclMessageType::Warning, VclButtonsType::Ok,
                                                   ScResId(STR_VALID_MACRONOTFOUND)));
-        xBox->run();
+        xBox->runAsync(xBox, [](sal_uInt32) {});
     }
 
     return bRet;
@@ -373,14 +373,16 @@ IMPL_STATIC_LINK_NOARG(ScValidationData, InstallLOKNotifierHdl, void*, vcl::ILib
 
     // true -> abort
 
-bool ScValidationData::DoError(weld::Window* pParent, const OUString& rInput,
-                               const ScAddress& rPos) const
+void ScValidationData::DoError(weld::Window* pParent, const OUString& rInput, const ScAddress& rPos,
+                               std::function<void(bool forget)> callback) const
 {
-    if ( eErrorStyle == SC_VALERR_MACRO )
-        return DoMacro(rPos, rInput, nullptr, pParent);
+    if ( eErrorStyle == SC_VALERR_MACRO ) {
+        DoMacro(rPos, rInput, nullptr, pParent);
+        return;
+    }
 
     if (!bShowError)
-        return true;
+        return;
 
     //  Output error message
 
@@ -407,7 +409,7 @@ bool ScValidationData::DoError(weld::Window* pParent, const OUString& rInput,
             break;
     }
 
-    std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(pParent, eType,
+    std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(pParent, eType,
                                               eStyle, aMessage, SfxViewShell::Current()));
     xBox->set_title(aTitle);
     xBox->SetInstallLOKNotifierHdl(LINK(nullptr, ScValidationData, InstallLOKNotifierHdl));
@@ -424,9 +426,8 @@ bool ScValidationData::DoError(weld::Window* pParent, const OUString& rInput,
             break;
     }
 
-    short nRet = xBox->run();
-
-    return ( eErrorStyle == SC_VALERR_STOP || nRet == RET_CANCEL );
+    xBox->runAsync(xBox, [&, callback](sal_uInt32 result)
+                   { callback(eErrorStyle == SC_VALERR_STOP || result == RET_CANCEL); });
 }
 
 bool ScValidationData::IsDataValidCustom(

sc/inc/validat.hxx
@@ -159,7 +159,8 @@ public:
         OUString& rStrResult, double& nVal, sal_uInt32& nFormat, bool& bIsVal) const;
 
                     // TRUE -> break
-    bool DoError(weld::Window* pParent, const OUString& rInput, const ScAddress& rPos) const;
+    void DoError(weld::Window* pParent, const OUString& rInput, const ScAddress& rPos,
+                 std::function<void(bool forget)> callback) const;
     void DoCalcError( ScFormulaCell* pCell ) const;
 
     bool IsEmpty() const;

sc/source/ui/app/inputhdl.cxx
@@ -3118,13 +3118,6 @@ void ScInputHandler::EnterHandler( ScEnterMode nBlockMode, bool bBeforeSavingInL
 
     ImplCreateEditEngine();
 
-    bool bMatrix = ( nBlockMode == ScEnterMode::MATRIX );
-
-    SfxApplication* pSfxApp     = SfxGetpApp();
-    std::unique_ptr<EditTextObject> pObject;
-    std::unique_ptr<ScPatternAttr> pCellAttrs;
-    bool            bForget     = false; // Remove due to validity?
-
     OUString aString = GetEditText(mpEditEngine.get());
     OUString aPreAutoCorrectString(aString);
     EditView* pActiveView = pTopView ? pTopView : pTableView;
@@ -3193,12 +3186,24 @@ void ScInputHandler::EnterHandler( ScEnterMode nBlockMode, bool bBeforeSavingInL
                     return;
                 }
 
-                if (pData->DoError(pActiveViewSh->GetFrameWeld(), aString, aCursorPos))
-                    bForget = true;                 // Do not take over input
-
+                pData->DoError(
+                    pActiveViewSh->GetFrameWeld(), aString, aCursorPos,
+                    [this, nBlockMode, &aString, &aPreAutoCorrectString](bool bForget)
+                    { EnterHandler2(nBlockMode, bForget, aString, aPreAutoCorrectString); });
+                return;
             }
         }
     }
+    EnterHandler2(nBlockMode, false, aString, aPreAutoCorrectString);
+}
+
+void ScInputHandler::EnterHandler2(ScEnterMode nBlockMode, bool bForget, OUString& aString,
+                                   OUString& aPreAutoCorrectString)
+{
+    std::unique_ptr<EditTextObject> pObject;
+    std::unique_ptr<ScPatternAttr> pCellAttrs;
+    bool bMatrix = (nBlockMode == ScEnterMode::MATRIX);
+    SfxApplication* pSfxApp = SfxGetpApp();
 
     // Check for input into DataPilot table
     if ( bModified && !bForget )

sc/source/ui/inc/inputhdl.hxx
@@ -198,6 +198,8 @@ public:
 
     bool            KeyInput( const KeyEvent& rKEvt, bool bStartEdit );
     void            EnterHandler( ScEnterMode nBlockMode = ScEnterMode::NORMAL, bool bBeforeSavingInLOK = false );
+    void EnterHandler2(ScEnterMode nBlockMode, bool bForget, OUString& aString,
+                       OUString& aPreAutoCorrectString);
     void            CancelHandler();
     void            SetReference( const ScRange& rRef, const ScDocument& rDoc );
     void            AddRefEntry();

commit 7be1b7be9c90399068cd2feaad8f057009c089b5

tdf#168261: resume ScInputHandler even if error not displayed
problem:
regression from 72bbb0dd4a368dab2b1bd99917cd07d23a92a3f0
when data validity dialog was not displayed, error function returned early
this caused issue with cell invalid data cleaning and it was not possible
to enter new data in any new cell

to reproduce:
select data validity as list and also disable error message showing
enter the invalid data in the validity cell
subsequently its not possible to type in any cells

Change-Id: Ia2157ae57ca90dd9365daae7881640a00c3f1216
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193132
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

sc/source/core/data/validat.cxx
@@ -381,8 +381,10 @@ void ScValidationData::DoError(weld::Window* pParent, const OUString& rInput, co
         return;
     }
 
-    if (!bShowError)
+    if (!bShowError) {
+        callback(true);
         return;
+    }
 
     //  Output error message
 