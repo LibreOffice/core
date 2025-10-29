LibreOffice/core

commit 9d6e1594a298c9d41f3dc64ad80edf4b9868c2e8

Deduplicate and unify using vcl::GetBitmap
Change-Id: Ie940990b604cf23c961aa5b6fad124f1ebaf338c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/176186
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

framework/source/fwe/helper/actiontriggerhelper.cxx
@@ -30,6 +30,7 @@
 #include <tools/stream.hxx>
 #include <vcl/dibtools.hxx>
 #include <vcl/graph.hxx>
+#include <vcl/graphic/BitmapHelper.hxx>
 #include <vcl/svapp.hxx>
 #include <o3tl/string_view.hxx>
 
@@ -64,7 +65,7 @@ static void GetMenuItemAttributes( const Reference< XPropertySet >& xActionTrigg
                             OUString& aMenuLabel,
                             OUString& aCommandURL,
                             OUString& aHelpURL,
-                            Reference< XBitmap >& xBitmap,
+                            Any& rImage,
                             Reference< XIndexContainer >& xSubContainer )
 {
     try
@@ -72,7 +73,7 @@ static void GetMenuItemAttributes( const Reference< XPropertySet >& xActionTrigg
         // mandatory properties
         xActionTriggerPropertySet->getPropertyValue(u"Text"_ustr) >>= aMenuLabel;
         xActionTriggerPropertySet->getPropertyValue(u"CommandURL"_ustr) >>= aCommandURL;
-        xActionTriggerPropertySet->getPropertyValue(u"Image"_ustr) >>= xBitmap;
+        rImage = xActionTriggerPropertySet->getPropertyValue(u"Image"_ustr);
         xActionTriggerPropertySet->getPropertyValue(u"SubContainer"_ustr) >>= xSubContainer;
     }
     catch (const Exception&)
@@ -116,11 +117,11 @@ static void InsertSubMenuItems(const Reference<XPopupMenu>& rSubMenu, sal_uInt16
                     OUString aLabel;
                     OUString aCommandURL;
                     OUString aHelpURL;
-                    Reference< XBitmap > xBitmap;
+                    Any aImage;
                     Reference< XIndexContainer > xSubContainer;
 
                     sal_uInt16 nNewItemId = nItemId++;
-                    GetMenuItemAttributes( xPropSet, aLabel, aCommandURL, aHelpURL, xBitmap, xSubContainer );
+                    GetMenuItemAttributes( xPropSet, aLabel, aCommandURL, aHelpURL, aImage, xSubContainer );
 
                     {
                         // insert new menu item
@@ -141,43 +142,10 @@ static void InsertSubMenuItems(const Reference<XPopupMenu>& rSubMenu, sal_uInt16
                         }
 
                         // handle bitmap
-                        if ( xBitmap.is() )
+                        if (aImage.hasValue())
                         {
-                            bool bImageSet = false;
-
-                            Reference<css::graphic::XGraphic> xGraphic(xBitmap, UNO_QUERY);
-                            if (xGraphic.is())
-                            {
-                                // we can take the optimized route if XGraphic is supported
+                            if (auto xGraphic = vcl::GetGraphic(aImage))
                                 rSubMenu->setItemImage(nNewItemId, xGraphic, false);
-                                bImageSet = true;
-                            }
-
-                            if ( !bImageSet )
-                            {
-                                // This is an unknown implementation of a XBitmap interface. We have to
-                                // use a more time consuming way to build an Image!
-                                BitmapEx aBitmap;
-
-                                Sequence< sal_Int8 > aDIBSeq;
-                                {
-                                    aDIBSeq = xBitmap->getDIB();
-                                    SvMemoryStream aMem( const_cast<sal_Int8 *>(aDIBSeq.getConstArray()), aDIBSeq.getLength(), StreamMode::READ );
-                                    ReadDIBBitmapEx(aBitmap, aMem);
-                                }
-
-                                aDIBSeq = xBitmap->getMaskDIB();
-                                if ( aDIBSeq.hasElements() )
-                                {
-                                    Bitmap aMaskBitmap;
-                                    SvMemoryStream aMem( const_cast<sal_Int8 *>(aDIBSeq.getConstArray()), aDIBSeq.getLength(), StreamMode::READ );
-                                    ReadDIB(aMaskBitmap, aMem, true);
-                                    aBitmap = BitmapEx(aBitmap.GetBitmap(), aMaskBitmap);
-                                }
-
-                                if (!aBitmap.IsEmpty())
-                                    rSubMenu->setItemImage(nNewItemId, Graphic(aBitmap).GetXGraphic(), false);
-                            }
                         }
                         else
                         {

svx/source/unodraw/unoshap2.cxx
@@ -36,6 +36,7 @@
 #include <vcl/svapp.hxx>
 #include <vcl/wmf.hxx>
 #include <vcl/cvtgrf.hxx>
+#include <vcl/graphic/BitmapHelper.hxx>
 #include <vcl/GraphicLoader.hxx>
 
 #include <svx/svdpool.hxx>
@@ -1196,23 +1197,11 @@ bool SvxGraphicObject::setPropertyValueImpl( const OUString& rName, const SfxIte
                 bOk = true;
             }
         }
-        else if (uno::Reference<graphic::XGraphic> xGraphic; rValue >>= xGraphic)
+        else if (uno::Reference<graphic::XGraphic> xGraphic = vcl::GetGraphic(rValue))
         {
-            if (xGraphic.is())
-            {
-                static_cast<SdrGrafObj*>(GetSdrObject())->SetGraphic(Graphic(xGraphic));
-                bOk = true;
-            }
-        }
-        else if (uno::Reference<awt::XBitmap> xBitmap; rValue >>= xBitmap)
-        {
-            if (xBitmap.is())
-            {
-                xGraphic.set(xBitmap, uno::UNO_QUERY);
-                Graphic aGraphic(xGraphic);
-                static_cast<SdrGrafObj*>(GetSdrObject())->SetGraphic(aGraphic);
-                bOk = true;
-            }
+            // Handles both graphic::XGraphic and awt::XBitmap
+            static_cast<SdrGrafObj*>(GetSdrObject())->SetGraphic(Graphic(xGraphic));
+            bOk = true;
         }
         if (bOk)
             GetSdrObject()->SetEmptyPresObj(false);

toolkit/source/helper/vclunohelper.cxx
@@ -21,6 +21,7 @@
 #include <vcl/dibtools.hxx>
 #include <vcl/event.hxx>
 #include <vcl/graph.hxx>
+#include <vcl/graphic/BitmapHelper.hxx>
 #include <vcl/metric.hxx>
 #include <vcl/ptrstyle.hxx>
 #include <vcl/unohelp.hxx>
@@ -60,37 +61,10 @@ uno::Reference< css::awt::XToolkit> VCLUnoHelper::CreateToolkit()
 
 BitmapEx VCLUnoHelper::GetBitmap( const css::uno::Reference< css::awt::XBitmap>& rxBitmap )
 {
-    BitmapEx aBmp;
+    if (VCLXBitmap* pVCLBitmap = dynamic_cast<VCLXBitmap*>(rxBitmap.get()))
+        return pVCLBitmap->GetBitmap();
 
-    css::uno::Reference< css::graphic::XGraphic > xGraphic( rxBitmap, css::uno::UNO_QUERY );
-    if( xGraphic.is() )
-    {
-        Graphic aGraphic( xGraphic );
-        aBmp = aGraphic.GetBitmapEx();
-    }
-    else if ( rxBitmap.is() )
-    {
-        VCLXBitmap* pVCLBitmap = dynamic_cast<VCLXBitmap*>( rxBitmap.get() );
-        if ( pVCLBitmap )
-            aBmp = pVCLBitmap->GetBitmap();
-        else
-        {
-            Bitmap aDIB, aMask;
-            {
-                css::uno::Sequence<sal_Int8> aBytes = rxBitmap->getDIB();
-                SvMemoryStream aMem( aBytes.getArray(), aBytes.getLength(), StreamMode::READ );
-                ReadDIB(aDIB, aMem, true);
-            }
-            {
-                css::uno::Sequence<sal_Int8> aBytes = rxBitmap->getMaskDIB();
-                SvMemoryStream aMem( aBytes.getArray(), aBytes.getLength(), StreamMode::READ );
-                ReadDIB(aMask, aMem, true);
-            }
-            aMask.Invert(); // Convert from transparency to alpha
-            aBmp = BitmapEx( aDIB, aMask );
-        }
-    }
-    return aBmp;
+    return vcl::GetBitmap(rxBitmap);
 }
 
 css::uno::Reference< css::awt::XBitmap> VCLUnoHelper::CreateBitmap( const BitmapEx& rBitmap )

commit a8216f500c223797b13976678924b76c5effec65

tdf#168314: check if xGraphic is empty, not Any
Regression after commit 9d6e1594a298c9d41f3dc64ad80edf4b9868c2e8
(Deduplicate and unify using vcl::GetBitmap, 2024-11-08).

An Any with an empty graphic is not itself empty; such an any caused
the outer check to succeed, and therefore its 'else' branch never
executed, when it ought to.

Change-Id: I97115c7f7dbab4ec54b5e999e4b47ecdc8874e47
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193161
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

framework/source/fwe/helper/actiontriggerhelper.cxx
@@ -65,7 +65,7 @@ static void GetMenuItemAttributes( const Reference< XPropertySet >& xActionTrigg
                             OUString& aMenuLabel,
                             OUString& aCommandURL,
                             OUString& aHelpURL,
-                            Any& rImage,
+                            Reference<css::graphic::XGraphic>& xGraphic,
                             Reference< XIndexContainer >& xSubContainer )
 {
     try
@@ -73,7 +73,7 @@ static void GetMenuItemAttributes( const Reference< XPropertySet >& xActionTrigg
         // mandatory properties
         xActionTriggerPropertySet->getPropertyValue(u"Text"_ustr) >>= aMenuLabel;
         xActionTriggerPropertySet->getPropertyValue(u"CommandURL"_ustr) >>= aCommandURL;
-        rImage = xActionTriggerPropertySet->getPropertyValue(u"Image"_ustr);
+        xGraphic = vcl::GetGraphic(xActionTriggerPropertySet->getPropertyValue(u"Image"_ustr));
         xActionTriggerPropertySet->getPropertyValue(u"SubContainer"_ustr) >>= xSubContainer;
     }
     catch (const Exception&)
@@ -117,11 +117,11 @@ static void InsertSubMenuItems(const Reference<XPopupMenu>& rSubMenu, sal_uInt16
                     OUString aLabel;
                     OUString aCommandURL;
                     OUString aHelpURL;
-                    Any aImage;
+                    Reference<css::graphic::XGraphic> xGraphic;
                     Reference< XIndexContainer > xSubContainer;
 
                     sal_uInt16 nNewItemId = nItemId++;
-                    GetMenuItemAttributes( xPropSet, aLabel, aCommandURL, aHelpURL, aImage, xSubContainer );
+                    GetMenuItemAttributes( xPropSet, aLabel, aCommandURL, aHelpURL, xGraphic, xSubContainer );
 
                     {
                         // insert new menu item
@@ -142,10 +142,9 @@ static void InsertSubMenuItems(const Reference<XPopupMenu>& rSubMenu, sal_uInt16
                         }
 
                         // handle bitmap
-                        if (aImage.hasValue())
+                        if (xGraphic)
                         {
-                            if (auto xGraphic = vcl::GetGraphic(aImage))
-                                rSubMenu->setItemImage(nNewItemId, xGraphic, false);
+                            rSubMenu->setItemImage(nNewItemId, xGraphic, false);
                         }
                         else
                         {