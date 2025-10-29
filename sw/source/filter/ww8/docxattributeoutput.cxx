ada2f4f515745bc5bdc8cbc1191da990627cab3e
Noel Grandin <noel.grandin@collabora.co.uk>
2025-10-29 12:23:39 +0200

    mso-test: fix order of w:pStyle and w:rPr elements
    When loading and then saving the document from forum-mso-en-10184.docx,
    the ordering of elements under w:pPr was wrong in two different ways.
    
    Change-Id: Ia99b759a871d5d324ec4c6689de3eb5dcf9c4f9b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193142
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit a43482e27644e45bd13d6eed3f550e3843cc1e58)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193251
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -1447,14 +1447,14 @@ void DocxAttributeOutput::StartParagraphProperties()
     m_pSerializer->startElementNS(XML_w, XML_pPr);
     m_bOpenedParaPr = true;
 
+    InitCollectedParagraphProperties();
+
     // and output the section break now (if it appeared)
     if (m_pSectionInfo && m_rExport.m_nTextTyp == TXT_MAINTEXT)
     {
         m_rExport.SectionProperties( *m_pSectionInfo );
         m_pSectionInfo.reset();
     }
-
-    InitCollectedParagraphProperties();
 }
 
 void DocxAttributeOutput::InitCollectedParagraphProperties()
@@ -1619,9 +1619,6 @@ void DocxAttributeOutput::EndParagraphProperties(const SfxItemSet& rParagraphMar
         PopulateFrameProperties(&rFrameFormat, aSize);
     }
 
-    // Merge the marks for the ordered elements
-    m_pSerializer->mergeTopMarks(Tag_InitCollectedParagraphProperties);
-
     // Write 'Paragraph Mark' properties
     m_pSerializer->startElementNS(XML_w, XML_rPr);
     // mark() before paragraph mark properties child elements.
@@ -1707,6 +1704,9 @@ void DocxAttributeOutput::EndParagraphProperties(const SfxItemSet& rParagraphMar
         m_aFramePr.SetUseFrameBackground(true);
         m_aFramePr.SetUseFrameTextDirection(true);
     }
+
+    // Merge the marks for the ordered elements
+    m_pSerializer->mergeTopMarks(Tag_InitCollectedParagraphProperties);
 
     m_pSerializer->endElementNS( XML_w, XML_pPr );
 
