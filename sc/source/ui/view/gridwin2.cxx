b65ae3578d876ff31563ade394209d2e76bfb0a2
Dennis Francis <dennis.francis@collabora.com>
2025-11-03 22:59:17 +0530

    cool#9176: do not incorrectly set bIsMultiField
    pNotifier should not be used to set this field which will be non-null in
    lok mode. In this particular branch of code bIsMultiField should be
    false which is the default in ScCheckListMenuControl ctor.
    
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Change-Id: I482495fd50f48fc7830eda2361503a60bb713f03
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193392
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Jenkins

--- sc/source/ui/view/gridwin2.cxx
+++ sc/source/ui/view/gridwin2.cxx
@@ -730,13 +730,9 @@ void ScGridWindow::DPLaunchFieldPopupMenu(const Point& rScrPos, const Size& rScr
 
     mpDPFieldPopup.reset();
 
-    vcl::ILibreOfficeKitNotifier* pNotifier = nullptr;
-    if (comphelper::LibreOfficeKit::isActive())
-        pNotifier = SfxViewShell::Current();
-
     weld::Window* pPopupParent = GetFrameWeld();
     mpDPFieldPopup.reset(new ScCheckListMenuControl(pPopupParent, mrViewData,
-                                                    false, -1, pNotifier));
+                                                    false, -1));
 
     DPSetupFieldPopup(std::move(pDPData), bDimOrientNotPage, pDPObj);
 
