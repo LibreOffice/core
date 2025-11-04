348161ac8c7eee7b78cf618f271b9c32b9a7ce65
Andras Timar <andras.timar@collabora.com>
2025-11-04 16:02:58 +0100

    if LOK_ALLOWLIST_LANGUAGES is not defined, allow all languages
    Change-Id: I7cab7b87dea84449ceae65896841909362390235
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193425
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/195660
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- comphelper/source/misc/lok.cxx
+++ comphelper/source/misc/lok.cxx
@@ -285,6 +285,10 @@ bool isAllowlistedLanguage(const OUString& lang)
             }
             std::cerr << std::endl;
         }
+        else
+        {
+            aList.emplace_back("*"); // LOK_ALLOWLIST_LANGUAGES not defined, allow all
+        }
 
         if (aList.empty())
             std::cerr << "No language allowlisted, turning off the language support." << std::endl;
@@ -294,6 +298,9 @@ bool isAllowlistedLanguage(const OUString& lang)
 
     if (aAllowlist.empty())
         return false;
+
+    if (aAllowlist.size() == 1 && aAllowlist[0] == "*")
+        return true;
 
     for (const auto& entry : aAllowlist)
     {
