a95c6bc086b84d069aa9c219330b16cab333b0c0
Mike Kaganski <mike.kaganski@collabora.com>
2025-11-07 12:39:10 +0200

    Drop an obsolete comment
    Initially, commit 95450c1b73e0c183b458101c4416f6cf2a30f0fe (INTEGRATION:
    CWS hedaburemove01 (1.1.2); FILE ADDED, 2007-04-11) used "_MyEDITDATA_HXX"
    as include guard; and the comment explained the choice of the name.
    
    Now the include guard is different.
    
    Change-Id: Ia2f904512276cb2a7a90163657bac0617efb8381
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193577
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- include/editeng/editdata.hxx
+++ include/editeng/editdata.hxx
@@ -17,7 +17,6 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
-// MyEDITDATA, wegen exportiertem EditData
 #ifndef INCLUDED_EDITENG_EDITDATA_HXX
 #define INCLUDED_EDITENG_EDITDATA_HXX
 
