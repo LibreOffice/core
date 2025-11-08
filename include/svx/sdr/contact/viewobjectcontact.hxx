LibreOffice/core

commit e6a8c40e5051363d5825bd09131b86cf8a2b659b

tdf#116975 Laggy behavior when clicking between table cells in impress
Only compute the object range when we actually need it.
Takes the hit test processing completely off the performance profile,
reducing CPU usage by 50%

Change-Id: I3bc7efa187897274d6ef86e7b259b0dba40dc398
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191667
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/svx/sdr/contact/viewobjectcontact.hxx
@@ -97,6 +97,7 @@ public:
 
     // get the object size range
     const basegfx::B2DRange& getObjectRange() const;
+    bool hasCachedObjectRange() const { return !maObjectRange.isEmpty(); }
 
     // React on changes of the object of this ViewContact
     virtual void ActionChanged();

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -517,9 +517,7 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
         // check for animated stuff
         const_cast< ViewObjectContact* >(this)->checkForPrimitive2DAnimations();
 
-        // always update object range when PrimitiveSequence changes
-        const drawinglayer::geometry::ViewInformation2D& rViewInformation2D(GetObjectContact().getViewInformation2D());
-        const_cast< ViewObjectContact* >(this)->maObjectRange = mxPrimitive2DSequence.getB2DRange(rViewInformation2D);
+        const_cast< ViewObjectContact* >(this)->maObjectRange.reset();
     }
 
     // return current Primitive2DContainer

svx/source/svdraw/sdrhittesthelper.cxx
@@ -120,20 +120,25 @@ bool ViewObjectContactPrimitiveHit(
     bool bTextOnly,
     drawinglayer::primitive2d::Primitive2DContainer* pHitContainer)
 {
-    basegfx::B2DRange aObjectRange(rVOC.getObjectRange());
-
-    if(aObjectRange.isEmpty())
-        return false;
-
-    // first do a rough B2DRange based HitTest; do not forget to
-    // include the HitTolerance if given
-    if(rLogicHitTolerance.getX() > 0 || rLogicHitTolerance.getY() > 0)
+    // Only use the range if we already have it, computing the range can be more expensive
+    // than running the HitTestProcessor.
+    if (rVOC.hasCachedObjectRange())
     {
-        aObjectRange.grow(rLogicHitTolerance);
-    }
+        basegfx::B2DRange aObjectRange(rVOC.getObjectRange());
 
-    if(!aObjectRange.isInside(rHitPosition))
-        return false;
+        if(aObjectRange.isEmpty())
+            return false;
+
+        // first do a rough B2DRange based HitTest; do not forget to
+        // include the HitTolerance if given
+        if(rLogicHitTolerance.getX() > 0 || rLogicHitTolerance.getY() > 0)
+        {
+            aObjectRange.grow(rLogicHitTolerance);
+        }
+
+        if(!aObjectRange.isInside(rHitPosition))
+            return false;
+    }
 
     // get primitive sequence
     sdr::contact::DisplayInfo aDisplayInfo;

commit 1c8b8d1fb80e6445aaeca2f2df6816d223277dbc

tdf#168928 Draw object not removed from the view after delete
regression from
  commit e6a8c40e5051363d5825bd09131b86cf8a2b659b
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Tue Sep 30 14:54:37 2025 +0200
  tdf#116975 Laggy behavior when clicking between table cells in impress

Change-Id: I9f430aece90badae7e0911ef82fffa84fb707755
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193582
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

include/svx/sdr/contact/viewobjectcontact.hxx
@@ -65,6 +65,7 @@ private:
     // This bool gets set when the object gets invalidated by ActionChanged() and
     // can be used from the OC to late-invalidates
     bool                                            mbLazyInvalidate : 1;
+    mutable bool                                    mbInvalidateViewOnDestruct : 1;
 
 protected:
     // make redirector a protected friend, it needs to call createPrimitives as default action

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -155,7 +155,8 @@ ViewObjectContact::ViewObjectContact(ObjectContact& rObjectContact, ViewContact&
 :   mrObjectContact(rObjectContact),
     mrViewContact(rViewContact),
     mnActionChangedCount(0),
-    mbLazyInvalidate(false)
+    mbLazyInvalidate(false),
+    mbInvalidateViewOnDestruct(false)
 {
     // make the ViewContact remember me
     mrViewContact.AddViewObjectContact(*this);
@@ -166,11 +167,21 @@ ViewObjectContact::ViewObjectContact(ObjectContact& rObjectContact, ViewContact&
 
 ViewObjectContact::~ViewObjectContact()
 {
-    // if the object range is empty, then we have never had the primitive range change, so nothing to invalidate
-    if (!maObjectRange.isEmpty())
+    if (mbInvalidateViewOnDestruct)
     {
-        // invalidate in view
-        GetObjectContact().InvalidatePartOfView(maObjectRange);
+        // used cached object range to limit invalidation
+        if (!maObjectRange.isEmpty())
+        {
+            // invalidate in view
+            GetObjectContact().InvalidatePartOfView(maObjectRange);
+        }
+        else
+        {
+            // we do not currently have cached range information, so just invalidate the whole viewport
+            const drawinglayer::geometry::ViewInformation2D& rViewInfo2D = GetObjectContact().getViewInformation2D();
+            if (!rViewInfo2D.getViewport().isEmpty())
+                GetObjectContact().InvalidatePartOfView(rViewInfo2D.getViewport());
+        }
     }
 
     // delete PrimitiveAnimation
@@ -518,6 +529,7 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
         const_cast< ViewObjectContact* >(this)->checkForPrimitive2DAnimations();
 
         const_cast< ViewObjectContact* >(this)->maObjectRange.reset();
+        mbInvalidateViewOnDestruct = true;
     }
 
     // return current Primitive2DContainer