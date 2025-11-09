29ec160a9dd8b3f45c387c1ad10f5ebf20c96bf6
Balazs Varga <balazs.varga@collabora.com>
2025-11-09 21:01:02 +0100

    Table style: fix wrong sidebar context after selecting
    table style area. After we selected a draw object and then
    we clicked inside a table style area the context was not updated
    and only the cell context UI elements appeared.
    Also need force the update in case of we set the cursor position
    from one table cell area to another one, otherwise the sidebar
    will not update the different Table area properties on the panel.
    
    Change-Id: I70a25e30071779ad4053b967a3fb15ec0328d6cb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193690
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- sc/source/ui/drawfunc/drawsh2.cxx
+++ sc/source/ui/drawfunc/drawsh2.cxx
@@ -45,6 +45,7 @@
 #include <drwlayer.hxx>
 #include <drtxtob.hxx>
 #include <gridwin.hxx>
+#include <dbdata.hxx>
 #include <svx/svdoole2.hxx>
 #include <svx/xflgrit.hxx>
 #include <comphelper/lok.hxx>
@@ -567,9 +568,28 @@ void ScDrawShell::Activate (const bool)
 
 const OUString & ScDrawShell::GetSidebarContextName()
 {
-    return vcl::EnumContext::GetContextName(
-        svx::sidebar::SelectionAnalyzer::GetContextForSelection_SC(
-            GetDrawView()->GetMarkedObjectList()));
+    vcl::EnumContext::Context eContext = svx::sidebar::SelectionAnalyzer::GetContextForSelection_SC(
+            GetDrawView()->GetMarkedObjectList());
+
+    // If no special context detected, check for table context
+    if (eContext == vcl::EnumContext::Context::Unknown)
+    {
+        ScDocument& rDocument = rViewData.GetDocument();
+        if (!rDocument.HasDataPilotAtPosition(rViewData.GetCurPos()))
+        {
+            const ScAddress aAddr = rViewData.GetCurPos();
+            if (!rDocument.HasSparkline(aAddr))
+            {
+                const ScDBData* pDbData = rDocument.GetDBAtCursor(
+                    aAddr.Col(), aAddr.Row(), aAddr.Tab(), ScDBDataPortion::AREA);
+                if (pDbData && pDbData->GetTableStyleInfo())
+                {
+                    eContext = vcl::EnumContext::Context::Table;
+                }
+            }
+        }
+    }
+    return vcl::EnumContext::GetContextName(eContext);
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
