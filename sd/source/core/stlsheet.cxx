754470f30f0cb8f7cd2bdda0156d9f94901c6187
Noel Grandin <noelgrandin@gmail.com>
2025-11-11 18:27:58 +0200

    improve SAL_WARN message
    Change-Id: I582606c9c90b512a9227ef14f0430481503c61af
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193816
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sd/source/core/stlsheet.cxx
+++ sd/source/core/stlsheet.cxx
@@ -1182,7 +1182,8 @@ css::uno::Any SdStyleSheet::getPropertyValue_Impl(const OUString& PropertyName)
         }
         else
         {
-            SAL_WARN_IF(aAny.hasValue(), "sd", "SvxShape::GetAnyForItem() Returnvalue has wrong Type!");
+            SAL_WARN_IF(aAny.hasValue(), "sd", "SvxShape::GetAnyForItem() Return value has wrong type, expected "
+                << pEntry->aType << " but got " << aAny.getValueType());
         }
     }
 
