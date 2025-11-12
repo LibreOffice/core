a91e834e3c598393acee6a89e685229280196d59
Michael Weghorn <m.weghorn@posteo.de>
2025-11-12 15:42:48 +0100

    android: Drop dead SubdocumentScrollHelper logic
    Both, SubdocumentsScrollHelper.mOverridePanning
    and SubdocumentsScrollHelper.mScrollSucceeded
    don't get any value explicitly assigned in the
    ctor, i.e. they'r initialized to the default
    boolean value of false. Both never get
    assigned a different value, i.e. they're
    always false.
    
    Drop all logic that would only be run
    if they were true, and drop now unused
    class members.
    
    This demonstrates the whole class is
    eventually pointless, and prepares
    for its removal in an upcoming
    commit.
    
    Change-Id: I01794dedc7b5d3582d67eef6b9f8c36cce4e686f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193892
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- android/source/src/java/org/mozilla/gecko/gfx/SubdocumentScrollHelper.java
+++ android/source/src/java/org/mozilla/gecko/gfx/SubdocumentScrollHelper.java
@@ -8,56 +8,21 @@ package org.mozilla.gecko.gfx;
 import android.graphics.PointF;
 
 class SubdocumentScrollHelper {
-    private static final String LOGTAG = "GeckoSubdocumentScrollHelper";
-
-    /* This is the amount of displacement we have accepted but not yet sent to JS. */
-    private final PointF mPendingDisplacement;
-
-    /* When this is true, we're sending scroll events to JS to scroll the active subdocument. */
-    private boolean mOverridePanning;
-
-    /* When this is true, we have received an ack for the last scroll event we sent to JS, and
-     * are ready to send the next scroll event. Note we only ever have one scroll event inflight
-     * at a time. */
-    private boolean mOverrideScrollAck;
-
-    /* When this is true, the last scroll event we sent actually did some amount of scrolling on
-     * the subdocument; we use this to decide when we have reached the end of the subdocument. */
-    private boolean mScrollSucceeded;
-
-    SubdocumentScrollHelper() {
-        mPendingDisplacement = new PointF();
+     SubdocumentScrollHelper() {
     }
 
     boolean scrollBy(PointF displacement) {
-        if (! mOverridePanning) {
-            return false;
-        }
-
-        if (! mOverrideScrollAck) {
-            mPendingDisplacement.x += displacement.x;
-            mPendingDisplacement.y += displacement.y;
-            return true;
-        }
-
-        mOverrideScrollAck = false;
-        // clear the |mPendingDisplacement| after serializing |displacement| to
-        // JSON because they might be the same object
-        mPendingDisplacement.x = 0;
-        mPendingDisplacement.y = 0;
-
-        return true;
+        return false;
     }
 
     void cancel() {
-        mOverridePanning = false;
     }
 
     boolean scrolling() {
-        return mOverridePanning;
+        return false;
     }
 
     boolean lastScrollSucceeded() {
-        return mScrollSucceeded;
+        return false;
     }
 }
