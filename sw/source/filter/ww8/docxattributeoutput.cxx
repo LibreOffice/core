LibreOffice/core

commit 05f3ecbf1e1dac7f24b1dee80dd7054bf1557afe

mso-test: fix duplicate w:rFonts elements under w:rPr
When loading and then saving the document from forum-mso-de-56075.docx,
we ended up with a duplicate rFonts element.

The xml looked something like:

<w:numbering ...
  <w:lvl w:ilvl="0">
      ...
      <w:rPr>
          <w:rFonts w:ascii="Calibri" w:hAnsi="Calibri" w:cs="Calibri" w:hint="default"/>
          <w:rFonts w:eastAsiaTheme="minorEastAsia"/>
      </w:rPr>
  </w:lvl>

Change-Id: I958023fb0c9e40af5aec31b07628bb72b25e730e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193146
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
(cherry picked from commit f6140a360dced4fc45816bd0605089110dd6835c)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193252
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -7937,7 +7937,7 @@ void DocxAttributeOutput::NumberingLevel( sal_uInt8 nLevel,
         {
             GetExport().GetId( *pFont ); // ensure font info is written to fontTable.xml
             OString aFamilyName( OUStringToOString( pFont->GetFamilyName(), RTL_TEXTENCODING_UTF8 ) );
-            m_pSerializer->singleElementNS( XML_w, XML_rFonts,
+            AddToAttrList( m_pFontsAttrList,
                     FSNS( XML_w, XML_ascii ), aFamilyName,
                     FSNS( XML_w, XML_hAnsi ), aFamilyName,
                     FSNS( XML_w, XML_cs ), aFamilyName,

commit 77906e1eb01e8fbbe62704bf272021cc0d545b16

crashtesting --convert-to docx tdf134458-1.doc
regression from
  commit 05f3ecbf1e1dac7f24b1dee80dd7054bf1557afe
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Wed Oct 29 13:40:30 2025 +0200
  mso-test: fix duplicate w:rFonts elements under w:rPr

Change-Id: Idda6b635fc8d034b01f21c9673ee8a8298d0a12c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193958
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/ww8/docxattributeoutput.cxx
@@ -7956,6 +7956,7 @@ void DocxAttributeOutput::NumberingLevel( sal_uInt8 nLevel,
                     FSNS( XML_w, XML_hint ), "default" );
             aTempSet.ClearItem(RES_CHRATR_FONT);
             aTempSet.ClearItem(RES_CHRATR_CTL_FONT);
+            aTempSet.ClearItem(RES_CHRATR_SCRIPT_HINT);
         }
         m_rExport.OutputItemSet(aTempSet, false, true, i18n::ScriptType::LATIN, m_rExport.m_bExportModeRTF);
 