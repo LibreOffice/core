LibreOffice/core

commit 0c29ba0c4690e9f135390f2cef5da02842987329

officeotron: config elements out of schema order
triggered in test
    testToolsLineNumbering
with a custom build that turns on officeotron validation.

Change-Id: I527febfc66732a0d34b0e196883faebbe3289dfb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194042
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

sw/source/filter/ww8/docxexport.cxx
@@ -1069,9 +1069,10 @@ static auto
 WriteCompat(SwDoc const& rDoc, ::sax_fastparser::FSHelperPtr const& rpFS) -> void
 {
     const IDocumentSettingAccess& rIDSA = rDoc.getIDocumentSettingAccess();
-    if (!rIDSA.get(DocumentSettingId::ADD_EXT_LEADING))
+    // tdf#88908 adjust CJK-context normal spaces to half of an ideographic space
+    if (rIDSA.get(DocumentSettingId::BALANCE_SPACES_AND_IDEOGRAPHIC_SPACES))
     {
-        rpFS->singleElementNS(XML_w, XML_noLeading);
+        rpFS->singleElementNS(XML_w, XML_balanceSingleByteDoubleByteWidth);
     }
     if (rIDSA.get(DocumentSettingId::MS_WORD_UL_TRAIL_SPACE))
         rpFS->singleElementNS(XML_w, XML_ulTrailSpace);
@@ -1080,25 +1081,23 @@ WriteCompat(SwDoc const& rDoc, ::sax_fastparser::FSHelperPtr const& rpFS) -> voi
     {
         rpFS->singleElementNS(XML_w, XML_doNotExpandShiftReturn);
     }
-    // tdf#88908 adjust CJK-context normal spaces to half of an ideographic space
-    if (rIDSA.get(DocumentSettingId::BALANCE_SPACES_AND_IDEOGRAPHIC_SPACES))
+    if (!rIDSA.get(DocumentSettingId::ADD_EXT_LEADING))
     {
-        rpFS->singleElementNS(XML_w, XML_balanceSingleByteDoubleByteWidth);
+        rpFS->singleElementNS(XML_w, XML_noLeading);
     }
     // tdf#146515 export "Use printer metrics for document formatting"
     if (!rIDSA.get(DocumentSettingId::USE_VIRTUAL_DEVICE))
         rpFS->singleElementNS(XML_w, XML_usePrinterMetrics);
-
+    if (rIDSA.get(DocumentSettingId::ADJUST_TABLE_LINE_HEIGHTS_TO_GRID_HEIGHT))
+    {
+        rpFS->singleElementNS(XML_w, XML_adjustLineHeightInTable);
+    }
     if (rIDSA.get(DocumentSettingId::DO_NOT_BREAK_WRAPPED_TABLES))
     {
         // Map the DoNotBreakWrappedTables compat flag to <w:doNotBreakWrappedTables>.
         rpFS->singleElementNS(XML_w, XML_doNotBreakWrappedTables);
     }
 
-    if (rIDSA.get(DocumentSettingId::ADJUST_TABLE_LINE_HEIGHTS_TO_GRID_HEIGHT))
-    {
-        rpFS->singleElementNS(XML_w, XML_adjustLineHeightInTable);
-    }
 }
 
 void DocxExport::WriteSettings()

commit f2e49634cdb1826ae55d5fc3479f1c20cb2dbb8e

improve element ordering in writeCompat
regression from
    commit 0c29ba0c4690e9f135390f2cef5da02842987329
    Author: Noel Grandin <noelgrandin@gmail.com>
    Date:   Fri Nov 14 17:47:34 2025 +0200
    officeotron: config elements out of schema order

Change-Id: Ic5f0524a379aa5e7961af3e6019219fffef69b30
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194145
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

sw/source/filter/ww8/docxexport.cxx
@@ -1069,6 +1069,10 @@ static auto
 WriteCompat(SwDoc const& rDoc, ::sax_fastparser::FSHelperPtr const& rpFS) -> void
 {
     const IDocumentSettingAccess& rIDSA = rDoc.getIDocumentSettingAccess();
+    if (!rIDSA.get(DocumentSettingId::ADD_EXT_LEADING))
+    {
+        rpFS->singleElementNS(XML_w, XML_noLeading);
+    }
     // tdf#88908 adjust CJK-context normal spaces to half of an ideographic space
     if (rIDSA.get(DocumentSettingId::BALANCE_SPACES_AND_IDEOGRAPHIC_SPACES))
     {
@@ -1080,10 +1084,6 @@ WriteCompat(SwDoc const& rDoc, ::sax_fastparser::FSHelperPtr const& rpFS) -> voi
     if (rIDSA.get(DocumentSettingId::DO_NOT_JUSTIFY_LINES_WITH_MANUAL_BREAK))
     {
         rpFS->singleElementNS(XML_w, XML_doNotExpandShiftReturn);
-    }
-    if (!rIDSA.get(DocumentSettingId::ADD_EXT_LEADING))
-    {
-        rpFS->singleElementNS(XML_w, XML_noLeading);
     }
     // tdf#146515 export "Use printer metrics for document formatting"
     if (!rIDSA.get(DocumentSettingId::USE_VIRTUAL_DEVICE))