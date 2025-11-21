LibreOffice/core

commit 088a7c7c451321a800ca8d3523a18b6bb93239b7

remove alpha device from OutputDevice
and render to a combined color+alpha bitmap

The strategy is to remove the mpAlphaVDev from OutputDevice,
and have the OutputDevice create 32-bit SalBitmap's
and then have the various backends correctly render to
those 32-bit bitmaps.

However, whenever we extract a BitmapEx from OutputDevice
(e.g. via VirtualDevice) and hand it to the rest of the
system, we split the color and alpha information.
So the rest of the codebase doesn't have to deal with
this yet.

We have some major problems to overcome here:
(*) Cairo uses 32-bit data, even if we ask for 24-bit color with no alpha.
    This is because the current version of cairo does not support RGB 24-bit data.
(*) Sometimes we want the bitmap to be initialised with an opaque white background,
    sometimes we want it to be transparent.
(*) Sometimes when we extract a bitmap from a device, we want the alpha channel,
    sometimes not.
(*) There are lots of left-over bits of previous attempts of this change lying around,
    e.g. in skia.

Notes
(*) we allow Bitmap (ie. SalBitmap) to hold color+alpha
(*) The BitmapEx(Bitmap) constructor checks if the incoming
Bitmap is color+alpha, and if so, splits into a color bitmap and an alpha bitmap
(*) Because the underlying image data is stored pre-multipled, if
we write a transparent pixel with color, and then read the pixel value back,
we might lose the color information.

Change-Id: I0fc0042d6e5be5edd99b320892fb00a8eb2842fe
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/173937
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/skia/gdiimpl.hxx
@@ -139,7 +139,7 @@ public:
                           Color nMaskColor) override;
 
     virtual std::shared_ptr<SalBitmap> getBitmap(tools::Long nX, tools::Long nY, tools::Long nWidth,
-                                                 tools::Long nHeight) override;
+                                                 tools::Long nHeight, bool bWithoutAlpha) override;
 
     virtual Color getPixel(tools::Long nX, tools::Long nY) override;
 
@@ -387,6 +387,8 @@ inline SkPaint SkiaSalGraphicsImpl::makePaintInternal() const
         SkiaHelper::setBlenderInvert(&paint);
     else if (mXorMode == XorMode::Xor)
         SkiaHelper::setBlenderXor(&paint);
+    else
+        paint.setBlendMode(SkBlendMode::kSrc); // set as is, including alpha
     return paint;
 }
 
@@ -415,7 +417,19 @@ inline SkPaint SkiaSalGraphicsImpl::makeFillPaint(double transparency) const
     return paint;
 }
 
-inline SkPaint SkiaSalGraphicsImpl::makeBitmapPaint() const { return makePaintInternal(); }
+inline SkPaint SkiaSalGraphicsImpl::makeBitmapPaint() const
+{
+    SkPaint paint;
+    // Invert could be done using a blend mode like invert() does, but
+    // intentionally use SkBlender to make sure it's not overwritten
+    // by a blend mode set later (which would be probably a mistake),
+    // and so that the drawing color does not actually matter.
+    if (mXorMode == XorMode::Invert)
+        SkiaHelper::setBlenderInvert(&paint);
+    else if (mXorMode == XorMode::Xor)
+        SkiaHelper::setBlenderXor(&paint);
+    return paint;
+}
 
 inline SkPaint SkiaSalGraphicsImpl::makeGradientPaint() const { return makePaintInternal(); }
 

vcl/win/gdi/gdiimpl.hxx
@@ -170,7 +170,7 @@ public:
                 const SalBitmap& rSalBitmap,
                 Color nMaskColor ) override;
 
-    virtual std::shared_ptr<SalBitmap> getBitmap( tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight ) override;
+    virtual std::shared_ptr<SalBitmap> getBitmap( tools::Long nX, tools::Long nY, tools::Long nWidth, tools::Long nHeight, bool bWithoutAlpha ) override;
 
     virtual Color getPixel( tools::Long nX, tools::Long nY ) override;
 

commit 1e1c9a6da141e37e862cdb8bacfb685f4da40251

tdf#169225 Color emoji background appears black with certain fonts
regression from
  commit 088a7c7c451321a800ca8d3523a18b6bb93239b7
  Author: Noel Grandin <noelgrandin@gmail.com>
  Date:   Tue Sep 24 16:18:11 2024 +0200
  remove alpha device from OutputDevice

Change-Id: I4ae192136479bcb1ef8e6195bc5b1115d7a0425b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194312
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/inc/skia/gdiimpl.hxx
@@ -420,7 +420,7 @@ inline SkPaint SkiaSalGraphicsImpl::makeGradientPaint() const { return makePaint
 inline SkPaint SkiaSalGraphicsImpl::makeTextPaint(std::optional<Color> color) const
 {
     assert(color.has_value());
-    SkPaint paint = makePaintInternal();
+    SkPaint paint = makePaintInternal(/*bSrcATop*/ true);
     paint.setColor(SkiaHelper::toSkColor(*color));
     return paint;
 }