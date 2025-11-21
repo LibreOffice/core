LibreOffice/core

commit 4bc08277e8fcfc37105f1427ca2f0d596d39eccd

fix DrawPolyPolygon alpha behaviour
I dont think this ever worked.

I checked before
  commit 088a7c7c451321a800ca8d3523a18b6bb93239b7
  Author: Noel Grandin <noelgrandin@gmail.com>
  Date:   Tue Sep 24 16:18:11 2024 +0200
  remove alpha device from OutputDevice
and back then, it would simply write opaque white pixels
if you set the line or fill colors to anything transparent.

Change-Id: I4bf4173a45cf98c705ebfa5917d7cdab887fac64
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192577
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/headless/CairoCommon.cxx
@@ -629,6 +629,7 @@ cairo_t* CairoCommon::createTmpCompatibleCairoContext() const
 
 void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
 {
+    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     if (cairo_surface_get_content(cairo_get_target(cr)) != CAIRO_CONTENT_ALPHA)
     {
         cairo_set_source_rgba(cr, aColor.GetRed() / 255.0, aColor.GetGreen() / 255.0,
@@ -638,7 +639,6 @@ void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
     {
         double fSet = aColor == COL_BLACK ? 1.0 : 0.0;
         cairo_set_source_rgba(cr, 1, 1, 1, fSet);
-        cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     }
 }
 
@@ -880,7 +880,8 @@ void CairoCommon::drawRect(double nX, double nY, double nWidth, double nHeight, 
             basegfx::B2DRectangle(nX, nY, nX + nWidth, nY + nHeight));
 
         m_oFillColor = aOrigFillColor;
-        drawPolyPolygon(basegfx::B2DHomMatrix(), basegfx::B2DPolyPolygon(aRect), 0.0, bAntiAlias);
+        drawPolyPolygon(basegfx::B2DHomMatrix(), basegfx::B2DPolyPolygon(aRect),
+                        (255 - aOrigFillColor->GetAlpha()) / 255.0, bAntiAlias);
         m_oFillColor = std::nullopt;
     }
 
@@ -892,7 +893,8 @@ void CairoCommon::drawRect(double nX, double nY, double nWidth, double nHeight, 
             basegfx::B2DRectangle(nX, nY, nX + nWidth - 1, nY + nHeight - 1));
 
         m_oLineColor = aOrigLineColor;
-        drawPolyPolygon(basegfx::B2DHomMatrix(), basegfx::B2DPolyPolygon(aRect), 0.0, bAntiAlias);
+        drawPolyPolygon(basegfx::B2DHomMatrix(), basegfx::B2DPolyPolygon(aRect),
+                        (255 - aOrigLineColor->GetAlpha()) / 255.0, bAntiAlias);
         m_oLineColor = std::nullopt;
     }
 
@@ -1137,6 +1139,7 @@ bool CairoCommon::drawPolyLine(const basegfx::B2DHomMatrix& rObjectToDevice,
         }
     }
 
+    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     cairo_set_source_rgba(cr, m_oLineColor->GetRed() / 255.0, m_oLineColor->GetGreen() / 255.0,
                           m_oLineColor->GetBlue() / 255.0, 1.0 - fTransparency);
 

vcl/qa/cppunit/outdev.cxx
@@ -1856,6 +1856,58 @@ CPPUNIT_TEST_FIXTURE(VclOutdevTest, testDrawRectAlpha2)
     CPPUNIT_ASSERT_EQUAL(RED_TRANSPARENT, pVDev->GetPixel(Point(1, 1)));
 }
 
+CPPUNIT_TEST_FIXTURE(VclOutdevTest, testDrawPolyPolygonAlpha1)
+{
+    Size aSize(100, 100);
+    ScopedVclPtrInstance<VirtualDevice> pVDev(DeviceFormat::WITH_ALPHA);
+    pVDev->SetOutputSizePixel(aSize, /*bErase*/ true, /*bAlphaMaskTransparent*/ true);
+
+    // create a square polypolygon
+    tools::Polygon aPolygon(4);
+    aPolygon.SetPoint(Point(0, 0), 0);
+    aPolygon.SetPoint(Point(0, 100), 1);
+    aPolygon.SetPoint(Point(100, 100), 2);
+    aPolygon.SetPoint(Point(0, 0), 3);
+    tools::PolyPolygon aPolyPolygon(aPolygon);
+    aPolyPolygon.Optimize(PolyOptimizeFlags::CLOSE);
+    basegfx::B2DPolyPolygon aB2DPolyPolygon(aPolyPolygon.getB2DPolyPolygon());
+
+    const Color RED_OPAQUE(ColorAlpha, 255, 255, 0, 0); // opaque red
+
+    pVDev->SetLineColor(RED_OPAQUE);
+    pVDev->SetFillColor(RED_OPAQUE);
+    pVDev->DrawPolyPolygon(aB2DPolyPolygon);
+
+    CPPUNIT_ASSERT_EQUAL(RED_OPAQUE, pVDev->GetPixel(Point(0, 0)));
+    CPPUNIT_ASSERT_EQUAL(RED_OPAQUE, pVDev->GetPixel(Point(1, 1)));
+}
+
+CPPUNIT_TEST_FIXTURE(VclOutdevTest, testDrawPolyPolygonAlpha2)
+{
+    Size aSize(100, 100);
+    ScopedVclPtrInstance<VirtualDevice> pVDev(DeviceFormat::WITH_ALPHA);
+    pVDev->SetOutputSizePixel(aSize, /*bErase*/ true, /*bAlphaMaskTransparent*/ true);
+
+    // create a square polypolygon
+    tools::Polygon aPolygon(4);
+    aPolygon.SetPoint(Point(0, 0), 0);
+    aPolygon.SetPoint(Point(0, 100), 1);
+    aPolygon.SetPoint(Point(100, 100), 2);
+    aPolygon.SetPoint(Point(0, 0), 3);
+    tools::PolyPolygon aPolyPolygon(aPolygon);
+    aPolyPolygon.Optimize(PolyOptimizeFlags::CLOSE);
+    basegfx::B2DPolyPolygon aB2DPolyPolygon(aPolyPolygon.getB2DPolyPolygon());
+
+    const Color RED_TRANSPARENT(ColorAlpha, 127, 255, 0, 0); // 50% transparent red
+
+    pVDev->SetLineColor(RED_TRANSPARENT);
+    pVDev->SetFillColor(RED_TRANSPARENT);
+    pVDev->DrawPolyPolygon(aB2DPolyPolygon);
+
+    CPPUNIT_ASSERT_EQUAL(RED_TRANSPARENT, pVDev->GetPixel(Point(0, 0)));
+    CPPUNIT_ASSERT_EQUAL(RED_TRANSPARENT, pVDev->GetPixel(Point(1, 1)));
+}
+
 static size_t ClipGradientTest(GDIMetaFile& rMtf, size_t nIndex)
 {
     MetaAction* pAction = rMtf.GetAction(nIndex);

vcl/source/outdev/polygon.cxx
@@ -305,7 +305,7 @@ void OutputDevice::ImplDrawPolyPolygonWithB2DPolyPolygon(const basegfx::B2DPolyP
                 bSuccess = mpGraphics->DrawPolyLine(
                     aTransform,
                     rPolygon,
-                    0.0,
+                    (255 - GetLineColor().GetAlpha()) / 255.0,
                     0.0, // tdf#124848 hairline
                     nullptr, // MM01
                     basegfx::B2DLineJoin::NONE,

commit e895d29150cce2900d62d670dad8ee33452c4a01

tdf#169533 slide background images not shown in Presenter console
regression from
  commit 4bc08277e8fcfc37105f1427ca2f0d596d39eccd
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Fri Oct 17 15:11:59 2025 +0200
  fix DrawPolyPolygon alpha behaviour

Change-Id: Icf2f1789e6f5ef5d67668b7b71f23948018a2dff
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194299
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/headless/CairoCommon.cxx
@@ -629,7 +629,6 @@ cairo_t* CairoCommon::createTmpCompatibleCairoContext() const
 
 void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
 {
-    cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     if (cairo_surface_get_content(cairo_get_target(cr)) != CAIRO_CONTENT_ALPHA)
     {
         cairo_set_source_rgba(cr, aColor.GetRed() / 255.0, aColor.GetGreen() / 255.0,
@@ -639,6 +638,7 @@ void CairoCommon::applyColor(cairo_t* cr, Color aColor, double fTransparency)
     {
         double fSet = aColor == COL_BLACK ? 1.0 : 0.0;
         cairo_set_source_rgba(cr, 1, 1, 1, fSet);
+        cairo_set_operator(cr, CAIRO_OPERATOR_SOURCE);
     }
 }
 