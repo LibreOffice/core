c2620241d9654da6194e3e0e648bfcbe6826590d
Tor Lillqvist <tml@collabora.com>
2025-11-22 09:31:33 +0100

    Don't package conflicting and pointless fontconfig configuration files
    For instance, having both 10-no-antialias.conf and
    10-yes-antialias.conf in the same conf.d folder is silly.
    
    Look at what Ubuntu has in /etc/fonts/conf.d and keep only those. Note
    that ExternalPackage_fontconfig_data is used only for WASM and CODA-W
    anyway, so the "local" and "user" ones are surely irrelevant.
    
    Change-Id: I76219eb38675f2e3018d84ab1fc845b1131cc4f5

--- external/fontconfig/ExternalPackage_fontconfig_data.mk
+++ external/fontconfig/ExternalPackage_fontconfig_data.mk
@@ -24,28 +24,12 @@ $(eval $(call gb_ExternalPackage_add_files,fontconfig_data,$(FONTCONFIG_DATA_FOL
 # The list below is created with
 # find workdir/UnpackedTarball/fontconfig/conf.d/ -name "*.conf" | LC_COLLATE=C sort | sed -e 's/^/    /' -e 's/$/ \\/' -e 's/workdir\/UnpackedTarball\/fontconfig\///'
 $(eval $(call gb_ExternalPackage_add_unpacked_files,fontconfig_data,$(FONTCONFIG_DATA_FOLDER)/conf.d,\
-    conf.d/05-reset-dirs-sample.conf \
-    conf.d/09-autohint-if-no-hinting.conf \
-    conf.d/10-autohint.conf \
-    conf.d/10-hinting-full.conf \
-    conf.d/10-hinting-medium.conf \
-    conf.d/10-hinting-none.conf \
     conf.d/10-hinting-slight.conf \
-    conf.d/10-no-antialias.conf \
     conf.d/10-scale-bitmap-fonts.conf \
-    conf.d/10-sub-pixel-bgr.conf \
-    conf.d/10-sub-pixel-none.conf \
     conf.d/10-sub-pixel-rgb.conf \
-    conf.d/10-sub-pixel-vbgr.conf \
-    conf.d/10-sub-pixel-vrgb.conf \
-    conf.d/10-unhinted.conf \
     conf.d/10-yes-antialias.conf \
     conf.d/11-lcdfilter-default.conf \
-    conf.d/11-lcdfilter-legacy.conf \
-    conf.d/11-lcdfilter-light.conf \
-    conf.d/11-lcdfilter-none.conf \
     conf.d/20-unhint-small-vera.conf \
-    conf.d/25-unhint-nonlatin.conf \
     conf.d/30-metric-aliases.conf \
     conf.d/35-lang-normalize.conf \
     conf.d/40-nonlatin.conf \
@@ -53,18 +37,13 @@ $(eval $(call gb_ExternalPackage_add_unpacked_files,fontconfig_data,$(FONTCONFIG
     conf.d/45-latin.conf \
     conf.d/48-spacing.conf \
     conf.d/49-sansserif.conf \
-    conf.d/50-user.conf \
-    conf.d/51-local.conf \
     conf.d/60-generic.conf \
     conf.d/60-latin.conf \
     conf.d/65-fonts-persian.conf \
     conf.d/65-khmer.conf \
     conf.d/65-nonlatin.conf \
     conf.d/69-unifont.conf \
-    conf.d/70-no-bitmaps-and-emoji.conf \
     conf.d/70-no-bitmaps-except-emoji.conf \
-    conf.d/70-no-bitmaps.conf \
-    conf.d/70-yes-bitmaps.conf \
     conf.d/80-delicious.conf \
     conf.d/90-synthetic.conf \
 ))
