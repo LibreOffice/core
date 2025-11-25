f38f481301d20c51c2661e627ef712cb1f2e258c
Stephan Bergmann <stephan.bergmann@collabora.com>
2025-11-25 16:19:37 +0100

    Host only solenv/flatpak-external-tarballs.json.in here
    ...and include the external-tarballs.json resulting from
    
    > make -s cmd cmd='${SRCDIR}/solenv/bin/generate-flatpak-manifest.sh' > external-tarballs.json
    
    in the org.libreoffice.LibreOffice.json that will then only be hosted at
    <https://github.com/flathub/org.libreoffice.LibreOffice> (to avoid needless
    duplication)
    
    Change-Id: I35c2f7b4613457126202f82f7b63ae4c34f8b1b4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194525
    Code-Style: Stephan Bergmann <stephan.bergmann@collabora.com>
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>

--- solenv/bin/generate-flatpak-manifest.sh
+++ solenv/bin/generate-flatpak-manifest.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
 # usage:
-# make -s cmd cmd='${SRCDIR}/solenv/bin/generate-flatpak-manifest.sh master' > org.libreoffice.LibreOffice.json
+# make -s cmd cmd='${SRCDIR}/solenv/bin/generate-flatpak-manifest.sh' > external-tarballs.json
 
 set -euo pipefail
 
 # convert FOO := BAR$(MICRO) to export FOO=BAR$MICRO
 source <(sed -e's#\([^ ]\{1,\}\) := #export \1=#g' ${SRCDIR}/download.lst | sed -e 's#[)(]##g')
 
-my_gitbranch="${1?}"
-subst="-e s!@BRANCH@!${my_gitbranch?}!"
-
-subst="${subst} $(
-    < ${SRCDIR}/solenv/flatpak-manifest.in \
-    sed ${subst} | \
+subst=$(
+    < ${SRCDIR}/solenv/flatpak-external-tarballs.json.in \
     grep -o '@[A-Z0-9_]*@' | while read var; do
         temp=${var:1:-1}
         echo -n " -e s/${var}/${!temp}/"
     done
-)"
+)
 
-exec sed ${subst} < "${SRCDIR}"/solenv/flatpak-manifest.in
+exec sed ${subst} < "${SRCDIR}"/solenv/flatpak-external-tarballs.json.in
