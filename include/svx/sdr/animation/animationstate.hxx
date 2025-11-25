LibreOffice/core

commit e6a8c40e5051363d5825bd09131b86cf8a2b659b

tdf#116975 Laggy behavior when clicking between table cells in impress
Only compute the object range when we actually need it.
Takes the hit test processing completely off the performance profile,
reducing CPU usage by 50%

Change-Id: I3bc7efa187897274d6ef86e7b259b0dba40dc398
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/191667
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -517,9 +517,7 @@ drawinglayer::primitive2d::Primitive2DContainer const & ViewObjectContact::getPr
         // check for animated stuff
         const_cast< ViewObjectContact* >(this)->checkForPrimitive2DAnimations();
 
-        // always update object range when PrimitiveSequence changes
-        const drawinglayer::geometry::ViewInformation2D& rViewInformation2D(GetObjectContact().getViewInformation2D());
-        const_cast< ViewObjectContact* >(this)->maObjectRange = mxPrimitive2DSequence.getB2DRange(rViewInformation2D);
+        const_cast< ViewObjectContact* >(this)->maObjectRange.reset();
     }
 
     // return current Primitive2DContainer

include/svx/sdr/contact/viewobjectcontact.hxx
@@ -97,6 +97,7 @@ public:
 
     // get the object size range
     const basegfx::B2DRange& getObjectRange() const;
+    bool hasCachedObjectRange() const { return !maObjectRange.isEmpty(); }
 
     // React on changes of the object of this ViewContact
     virtual void ActionChanged();

svx/source/svdraw/sdrhittesthelper.cxx
@@ -120,20 +120,25 @@ bool ViewObjectContactPrimitiveHit(
     bool bTextOnly,
     drawinglayer::primitive2d::Primitive2DContainer* pHitContainer)
 {
-    basegfx::B2DRange aObjectRange(rVOC.getObjectRange());
-
-    if(aObjectRange.isEmpty())
-        return false;
-
-    // first do a rough B2DRange based HitTest; do not forget to
-    // include the HitTolerance if given
-    if(rLogicHitTolerance.getX() > 0 || rLogicHitTolerance.getY() > 0)
+    // Only use the range if we already have it, computing the range can be more expensive
+    // than running the HitTestProcessor.
+    if (rVOC.hasCachedObjectRange())
     {
-        aObjectRange.grow(rLogicHitTolerance);
-    }
+        basegfx::B2DRange aObjectRange(rVOC.getObjectRange());
 
-    if(!aObjectRange.isInside(rHitPosition))
-        return false;
+        if(aObjectRange.isEmpty())
+            return false;
+
+        // first do a rough B2DRange based HitTest; do not forget to
+        // include the HitTolerance if given
+        if(rLogicHitTolerance.getX() > 0 || rLogicHitTolerance.getY() > 0)
+        {
+            aObjectRange.grow(rLogicHitTolerance);
+        }
+
+        if(!aObjectRange.isInside(rHitPosition))
+            return false;
+    }
 
     // get primitive sequence
     sdr::contact::DisplayInfo aDisplayInfo;

commit 12baba92e0a581866c6dedea428e888dd0934d78

tdf#169656 Crash upon starting animated GIF
this is caused by freeing an object that is currently on the callstack.

I dont have a complete fix here, this will fix it 99% of the time.

regression from
  commit e6a8c40e5051363d5825bd09131b86cf8a2b659b
  Author: Noel Grandin <noel.grandin@collabora.co.uk>
  Date:   Tue Sep 30 14:54:37 2025 +0200
  tdf#116975 Laggy behavior when clicking between table cells in impress
but only because it made it more obvious, this has been a lurking problem.

Change-Id: I4f2f56a54a74f2a51e7ca05421d3db82de47f2ca
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194502
Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins
Code-Style: Noel Grandin <noel.grandin@collabora.co.uk>
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

include/svx/sdr/animation/animationstate.hxx
@@ -47,6 +47,8 @@ namespace sdr::animation
             // destructor
             virtual ~PrimitiveAnimation() override;
 
+            void SetPrimitives(drawinglayer::primitive2d::Primitive2DContainer&&);
+
             // execute event, from base class Event
             virtual void Trigger(sal_uInt32 nTime) override;
         };

svx/source/sdr/animation/animationstate.cxx
@@ -122,6 +122,12 @@ namespace sdr::animation
                 prepareNextEvent();
         }
 
+        void PrimitiveAnimation::SetPrimitives(drawinglayer::primitive2d::Primitive2DContainer&& rAnimatedPrimitives)
+        {
+            // called from ViewObjectContact::checkForPrimitive2DAnimations
+            maAnimatedPrimitives = std::move(rAnimatedPrimitives);
+        }
+
 } // end of namespace
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svx/source/sdr/contact/viewobjectcontact.cxx
@@ -287,30 +287,45 @@ void ViewObjectContact::ActionChildInserted(ViewContact& rChild)
     // GetObjectContact().InvalidatePartOfView(rChildVOC.getObjectRange());
 }
 
+// Check for animated primitives.
 void ViewObjectContact::checkForPrimitive2DAnimations()
 {
-    // remove old one
-    mpPrimitiveAnimation.reset();
-
-    // check for animated primitives
-    if(mxPrimitive2DSequence.empty())
-        return;
-
-    const bool bTextAnimationAllowed(GetObjectContact().IsTextAnimationAllowed());
-    const bool bGraphicAnimationAllowed(GetObjectContact().IsGraphicAnimationAllowed());
-
-    if(bTextAnimationAllowed || bGraphicAnimationAllowed)
+    // Somewhat odd structure here, because we are trying to avoid deleting and re-allocating the mpPrimitiveAnimation
+    // object, because we might be inside a call from the Scheduler, in which case we will be deleting an object
+    // on the call stack.
+    // Yes, this does still leave a possible situation where the user could turn animations off while we are
+    // animating and we could crash. I dont have a better solution right now.
+    //
+    drawinglayer::primitive2d::Primitive2DContainer aNewAnimatedPrimitives;
+    if(!mxPrimitive2DSequence.empty())
     {
-        AnimatedExtractingProcessor2D aAnimatedExtractor(GetObjectContact().getViewInformation2D(),
-            bTextAnimationAllowed, bGraphicAnimationAllowed);
-        aAnimatedExtractor.process(mxPrimitive2DSequence);
+        const bool bTextAnimationAllowed(GetObjectContact().IsTextAnimationAllowed());
+        const bool bGraphicAnimationAllowed(GetObjectContact().IsGraphicAnimationAllowed());
 
-        if(!aAnimatedExtractor.getPrimitive2DSequence().empty())
+        if(bTextAnimationAllowed || bGraphicAnimationAllowed)
         {
-            // derived primitiveList is animated, setup new PrimitiveAnimation
-            mpPrimitiveAnimation.reset( new sdr::animation::PrimitiveAnimation(*this, aAnimatedExtractor.extractPrimitive2DSequence()) );
+            AnimatedExtractingProcessor2D aAnimatedExtractor(GetObjectContact().getViewInformation2D(),
+                bTextAnimationAllowed, bGraphicAnimationAllowed);
+            aAnimatedExtractor.process(mxPrimitive2DSequence);
+
+            if(!aAnimatedExtractor.getPrimitive2DSequence().empty())
+            {
+                // derived primitiveList is animated, setup new PrimitiveAnimation
+                aNewAnimatedPrimitives = aAnimatedExtractor.extractPrimitive2DSequence();
+            }
         }
     }
+    if (!aNewAnimatedPrimitives.empty())
+    {
+        // derived primitiveList is animated, setup new PrimitiveAnimation
+        if (mpPrimitiveAnimation)
+            mpPrimitiveAnimation->SetPrimitives(std::move(aNewAnimatedPrimitives));
+        else
+            mpPrimitiveAnimation.reset( new sdr::animation::PrimitiveAnimation(*this, std::move(aNewAnimatedPrimitives)) );
+    }
+    else
+        // remove old one
+        mpPrimitiveAnimation.reset();
 }
 
 void ViewObjectContact::createPrimitive2DSequence(const DisplayInfo& rDisplayInfo, drawinglayer::primitive2d::Primitive2DDecompositionVisitor& rVisitor) const