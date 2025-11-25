7439a873217eb75aa7256b5b4008d8e6324d27f9
Caolán McNamara <caolan.mcnamara@collabora.com>
2025-11-25 20:18:18 +0000

    a null mpDoc was seen here
    under windows
    
    Change-Id: I638a0bd77538db7200bdc2c705f51ec031bcdb46
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194548
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Code-Style: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit e257b5a28896f140c4b47c258491031a0bec06bb)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194678
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins
    Code-Style: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit ef93d3951da8a61543eac82baa99a977c8b70935)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194702
    Code-Style: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sd/source/ui/unoidl/unomodel.cxx
+++ sd/source/ui/unoidl/unomodel.cxx
@@ -3962,7 +3962,7 @@ OString SdXImpressDocument::getViewRenderState(SfxViewShell* pViewShell)
     if (pView)
     {
         const SdViewOptions& pVOpt = pView->GetViewOptions();
-        if (mpDoc->GetOnlineSpell())
+        if (mpDoc && mpDoc->GetOnlineSpell())
             aState.append('S');
         if (!ThemeColors::UseOnlyWhiteDocBackground())
         {
