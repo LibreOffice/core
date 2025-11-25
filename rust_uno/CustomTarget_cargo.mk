6d12a896d604269c9f2c0f9499d32dfe7f4400e8
Stephan Bergmann <stephan.bergmann@collabora.com>
2025-11-25 11:39:25 +0100

    Clean the artifacts that rust_uno/CustomTarget_* put in $(SRCDIR)
    This is a temporary measure.  The right fix will be to not have those
    CustomTargets put anything into $(SRCDIR) in the first place.
    
    Change-Id: Ib211b5bc5e0059c8c8206c044e28443265d69353
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194517
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Code-Style: Stephan Bergmann <stephan.bergmann@collabora.com>

--- rust_uno/CustomTarget_cargo.mk
+++ rust_uno/CustomTarget_cargo.mk
@@ -15,4 +15,13 @@ $(call gb_CustomTarget_get_target,rust_uno/cargo): \
         $(gb_CustomTarget_workdir)/rust_uno/rustmaker/cpp
 	cd $(SRCDIR)/rust_uno && cargo build $(if $(verbose),--verbose,) --release
 
+#TODO: Add to the general pattern rule from solenv/gbuild/CustomTarget.mk, instead of having to
+# repeat its recipe here:
+$(call gb_CustomTarget_get_clean_target,rust_uno/cargo):
+	$(call gb_Output_announce,rust_uno/cargo,$(false),CUS,3)
+	$(call gb_Helper_abbreviate_dirs,\
+		rm -rf $(gb_CustomTarget_workdir)/rust_uno/cargo && \
+		rm -f $(call gb_CustomTarget_get_target,rust_uno/cargo))
+	rm -fr $(SRCDIR)/rust_uno/Cargo.lock $(SRCDIR)/rust_uno/target
+
 # vim: set noet sw=4 ts=4:
