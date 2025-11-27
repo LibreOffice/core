ea940b7985921883f8f75009186cae24846b1bd4
Xisco Fauli <xiscofauli@libreoffice.org>
2025-11-27 13:08:17 +0100

    tdf#169554: Revert "tdf#167075: init doc shell before pasting as png/bmp (take 2)"
    This reverts commit 671564bce4541244ad3d67b5b98899321176231e.
    
    I can't reproduce the issue on Windows nor on Linux
    but two people confirmed that
    671564bce4541244ad3d67b5b98899321176231e introduced
    the regression in the report
    
    Change-Id: Iaf5df6a30bf01db738c3b4eb0f9fee51cf0a957c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194724
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

--- sc/source/ui/inc/transobj.hxx
+++ sc/source/ui/inc/transobj.hxx
@@ -58,7 +58,7 @@ private:
     static void StripRefs( ScDocument& rDoc, SCCOL nStartX, SCROW nStartY,
                             SCCOL nEndX, SCROW nEndY,
                             ScDocument& rDestDoc );
-    static void PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
+    static void PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
                             const ScRange& rBlock );
     static void GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab2, SCROW& nRow, SCCOL& nCol );
 
