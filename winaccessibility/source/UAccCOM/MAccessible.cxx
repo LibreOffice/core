LibreOffice/core

commit 3b86569fcba210eb6570fabef7ff8abf6aff91f0

winaccessibility: replace CoCreateInstance with direct instantiation
This is an alternative (to 732ec36edfd09d2091d70c4d71b5f182fe279c45)
solution to the "CoCreateInstance does not work" problem:
replace all CoCreateInstance calls with equivalent calls to create
the components directly.

Since the only reason why this COM stuff needs to be registered
at all is that AccObject uses CoCreateInstance() to create its
COM objects, another possible solution appears to be to simply link
the libraries and instantiate the COM objects directly, without COM.

The only difference appears to be that CoCreateInstance would
automatically add proxy objects in case the COM objects reside in a
single-threaded appartment; not sure if that is relevant here.

Change-Id: I8ffb8af501f6084f3145fa4d4f53366a070e1691
Reviewed-on: https://gerrit.libreoffice.org/6792
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Tested-by: Michael Meeks <michael.meeks@collabora.com>

winaccessibility/source/UAccCOM/MAccessible.cxx
@@ -23,6 +23,15 @@
 
 #include <algorithm>
 #include "AccAction.h"
+#include "AccRelation.h"
+#include "AccComponent.h"
+#include "AccText.h"
+#include "AccEditableText.h"
+#include "AccImage.h"
+#include "AccTable.h"
+#include "AccValue.h"
+#include "AccHypertext.h"
+#include "AccHyperLink.h"
 
 #include <com/sun/star/accessibility/XAccessibleText.hpp>
 #include <com/sun/star/accessibility/XAccessibleEditableText.hpp>
@@ -1720,11 +1729,8 @@ STDMETHODIMP CMAccessible::get_relation( long relationIndex, IAccessibleRelation
             }
 
             IAccessibleRelation* pRelation = NULL;
-            ActivateActContext();
-            HRESULT hr = CoCreateInstance( CLSID_AccRelation, NULL, CLSCTX_SERVER ,
-                IID_IAccessibleRelation,
-                (void **)&pRelation);
-            DeactivateActContext();
+            HRESULT hr = createInstance<CAccRelation>(IID_IAccessibleRelation,
+                            &pRelation);
             if(SUCCEEDED(hr))
             {
                 IUNOXWrapper* wrapper = NULL;
@@ -1784,11 +1790,8 @@ STDMETHODIMP CMAccessible::get_relations( long, IAccessibleRelation __RPC_FAR *_
         for(int i=0; i<nCount ; i++)
         {
             IAccessibleRelation* pRelation = NULL;
-            ActivateActContext();
-            HRESULT hr = CoCreateInstance( CLSID_AccRelation, NULL, CLSCTX_SERVER ,
-                IID_IAccessibleRelation,
-                (void **)&pRelation);
-            DeactivateActContext();
+            HRESULT hr = createInstance<CAccRelation>(IID_IAccessibleRelation,
+                            &pRelation);
             if(SUCCEEDED(hr))
             {
                 IUNOXWrapper* wrapper = NULL;
@@ -2557,7 +2560,18 @@ BOOL CMAccessible::GetXInterfaceFromXAccessible(XAccessible* pXAcc, XInterface**
     return FALSE;
 }
 
-HRESULT WINAPI CMAccessible::SmartQI(void* pv, REFIID iid, void** ppvObject)
+template<typename T> HRESULT
+createAggInstance(CMAccessible &rOuter, REFIID iid, void ** ppvObject)
+{
+//    return CComCreator< CComAggObject<T> >::CreateInstance(
+//    XXX: do not use CComAggObject - the aggregation is hand-crafted!
+//         the SmartQI method must not call itself recursively -
+//         which it will do if CComAggObject redirects QueryInterface.
+    return CComCreator< CComObject<T> >::CreateInstance(
+            rOuter.GetControllingUnknown(), iid, ppvObject);
+}
+
+HRESULT WINAPI CMAccessible::SmartQI(void* /*pv*/, REFIID iid, void** ppvObject)
 {
     ENTER_PROTECTED_BLOCK
         ISDESTROY()
@@ -2589,9 +2603,40 @@ HRESULT WINAPI CMAccessible::SmartQI(void* pv, REFIID iid, void** ppvObject)
             }
             else
             {
-                ActivateActContext();
-                HRESULT hr = pMap->pfnCreateInstance(pv, iid, ppvObject);
-                DeactivateActContext();
+                HRESULT hr(REGDB_E_CLASSNOTREG);
+                switch (pMap->XIFIndex)
+                {
+                    case XI_COMPONENT:
+                        hr = createAggInstance<CAccComponent>(*this, iid, ppvObject);
+                        break;
+                    case XI_TEXT:
+                        hr = createAggInstance<CAccText>(*this, iid, ppvObject);
+                        break;
+                    case XI_EDITABLETEXT:
+                        hr = createAggInstance<CAccEditableText>(*this, iid, ppvObject);
+                        break;
+                    case XI_IMAGE:
+                        hr = createAggInstance<CAccImage>(*this, iid, ppvObject);
+                        break;
+                    case XI_TABLE:
+                        hr = createAggInstance<CAccTable>(*this, iid, ppvObject);
+                        break;
+                    case XI_ACTION:
+                        hr = createAggInstance<CAccAction>(*this, iid, ppvObject);
+                        break;
+                    case XI_VALUE:
+                        hr = createAggInstance<CAccValue>(*this, iid, ppvObject);
+                        break;
+                    case XI_HYPERTEXT:
+                        hr = createAggInstance<CAccHypertext>(*this, iid, ppvObject);
+                        break;
+                    case XI_HYPERLINK:
+                        hr = createAggInstance<CAccHyperLink>(*this, iid, ppvObject);
+                        break;
+                    default:
+                        assert(false);
+                }
+                assert(hr == S_OK);
                 if(hr == S_OK)
                 {
                     m_containedObjects.insert(XGUIDToComObjHash::value_type(*pMap->piid,(IUnknown*)*ppvObject));

winaccessibility/Library_uacccom.mk
@@ -20,6 +20,7 @@ $(eval $(call gb_Library_set_include,UAccCOM,\
 ))
 
 $(eval $(call gb_Library_add_defs,UAccCOM,\
+	-DUACCCOM_DLLIMPLEMENTATION \
 	-D_UNICODE=1 \
 ))
 

winaccessibility/Library_winaccessibility.mk
@@ -53,6 +53,7 @@ $(eval $(call gb_Library_use_externals,winaccessibility,\
 ))
 
 $(eval $(call gb_Library_use_libraries,winaccessibility,\
+	UAccCOM \
 	cppu \
 	cppuhelper \
 	vcl \

winaccessibility/inc/UAccCOMdllapi.h
@@ -1,0 +1,24 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_UACCCOM_UACCCOMDLLAPI_H
+#define INCLUDED_UACCCOM_UACCCOMDLLAPI_H
+
+#include <sal/types.h>
+
+#if defined(UACCCOM_DLLIMPLEMENTATION)
+#define UACCCOM_DLLPUBLIC SAL_DLLPUBLIC_EXPORT
+#else
+#define UACCCOM_DLLPUBLIC SAL_DLLPUBLIC_IMPORT
+#endif
+#define UACCCOM_DLLPRIVATE SAL_DLLPRIVATE
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

winaccessibility/inc/accHelper.hxx
@@ -20,6 +20,8 @@
 #ifndef __ACCHELPER_HXX
 #define __ACCHELPER_HXX
 
+#include <UAccCOMdllapi.h>
+
 enum DIRECTION
 {
     FIRST_CHILD=0,
@@ -30,6 +32,8 @@ enum DIRECTION
 
 #define UNO_MSAA_UNMAPPING       0x00000000
 
+struct IMAccessible;
+UACCCOM_DLLPUBLIC IMAccessible * UAccCOMCreateInstance();
 
 #endif
 

winaccessibility/source/UAccCOM/AccHypertext.cxx
@@ -20,6 +20,8 @@
 #include "stdafx.h"
 #include "UAccCOM.h"
 #include "AccHypertext.h"
+#include "AccHyperLink.h"
+#include "acccommon.h"
 
 #include "act.hxx"
 
@@ -325,11 +327,7 @@ STDMETHODIMP CAccHypertext::get_hyperlink(long index,IAccessibleHyperlink **hype
     }
 
     IAccessibleHyperlink* plink = NULL;
-    ActivateActContext();
-    HRESULT hr = CoCreateInstance( CLSID_AccHyperLink, NULL, CLSCTX_SERVER ,
-                                   IID_IAccessibleHyperlink,
-                                   (void **)&plink);
-    DeactivateActContext();
+    HRESULT hr = createInstance<CAccHyperLink>(IID_IAccessibleHyperlink, &plink);
     if( SUCCEEDED(hr) )
     {
         IUNOXWrapper* wrapper = NULL;

winaccessibility/source/UAccCOM/AccTable.cxx
@@ -214,13 +214,7 @@ STDMETHODIMP CAccTable::get_columnHeader(IAccessibleTable __RPC_FAR *__RPC_FAR *
     *startingRowIndex = 0 ;
 
     IMAccessible* pIMacc = NULL;
-    ActivateActContext();
-    HRESULT hr = CoCreateInstance( CLSID_MAccessible, NULL, CLSCTX_ALL ,
-                                    IID_IMAccessible,
-                                    (void **)&pIMacc
-                                  );
-    DeactivateActContext();
-
+    HRESULT hr = createInstance<CMAccessible>(IID_IMAccessible, &pIMacc);
     if (!SUCCEEDED(hr))
     {
         return E_FAIL;
@@ -448,13 +442,7 @@ STDMETHODIMP CAccTable::get_rowHeader(IAccessibleTable __RPC_FAR *__RPC_FAR *acc
     *startingColumnIndex = 0 ;
 
     IMAccessible* pIMacc = NULL;
-    ActivateActContext();
-    HRESULT hr = CoCreateInstance( CLSID_MAccessible, NULL, CLSCTX_ALL ,
-                                    IID_IMAccessible,
-                                    (void **)&pIMacc
-                                  );
-    DeactivateActContext();
-
+    HRESULT hr = createInstance<CMAccessible>(IID_IMAccessible, &pIMacc);
     if (!SUCCEEDED(hr))
     {
         return E_FAIL;

winaccessibility/source/UAccCOM/EnumVariant.cxx
@@ -155,10 +155,7 @@ HRESULT STDMETHODCALLTYPE CEnumVariant::Clone(IEnumVARIANT __RPC_FAR *__RPC_FAR 
    */
 HRESULT STDMETHODCALLTYPE CEnumVariant::Create(CEnumVariant __RPC_FAR *__RPC_FAR *ppenum)
 {
-    ActivateActContext();
-    HRESULT hr = CoCreateInstance(CLSID_EnumVariant,NULL,
-                                 CLSCTX_SERVER,IID_IEnumVariant,(void **)ppenum);
-    DeactivateActContext();
+    HRESULT hr = createInstance<CEnumVariant>(IID_IEnumVariant, ppenum);
     if (S_OK != hr)
     {
         return E_FAIL;

commit 9502a9f76731afa0f5105e6d79b069222677f88d

tdf#168897 winaccessibility: fix JAWS crash with CAccTable
stack:

 	UAccCOM.dll!ATL::CComObjectRootBase::OuterAddRef() Line 2631
 	UAccCOM.dll!ATL::CComContainedObject<CAccImage>::AddRef() Line 3719
 	AccEventCache.dll!00007ffd2039be28()
 	AccEventCache.dll!00007ffd20355677()
 	AccEventCache.dll!00007ffd20346d41()
 	AccEventCache.dll!00007ffd20347f9a()
 	AccEventCache.dll!00007ffd20338ba2()
 	AccEventCache.dll!00007ffd20380dd8()
 	AccEventCache.dll!00007ffd2037fd7b()
 	AccEventCache.dll!00007ffd2037f7ee()
 	AccEventCache.dll!00007ffd2037d9e6()
 	AccEventCache.dll!00007ffd2037d07f()
 		^ JAWS
 	GlobalHooksDispatcher.dll!00007ffd214daed4()
 	GlobalHooksDispatcher.dll!00007ffd214d8d84()
 	user32.dll!00007ffd492068ac()
 	ntdll.dll!00007ffd4ae71424()
 	win32u.dll!NtUserNotifyWinEvent()
 	winaccessibility.dll!AccObjectWinManager::NotifyAccEvent(com::sun::star::accessibility::XAccessible * pXAcc, UnoMSAAEvent eEvent) Line 182
        	^ selfAccObj looks very dead here but that is a red herring!
 	winaccessibility.dll!AccContainerEventListener::FireStateFocusedChange(bool enable) Line 351
 	winaccessibility.dll!AccContainerEventListener::SetComponentState(__int64 state, bool enable) Line 178
 	winaccessibility.dll!AccParagraphEventListener::SetComponentState(__int64 state, bool enable) Line 123
 	winaccessibility.dll!AccContainerEventListener::HandleStateChangedEvent(com::sun::star::uno::Any oldValue, com::sun::star::uno::Any newValue) Line 106
 	winaccessibility.dll!AccContainerEventListener::notifyEvent(const com::sun::star::accessibility::AccessibleEventObject & aEvent) Line 72
 	winaccessibility.dll!AccParagraphEventListener::notifyEvent(const com::sun::star::accessibility::AccessibleEventObject & aEvent) Line 77
 	comphelper.dll!comphelper::AccessibleEventNotifier::addEvent(const unsigned long _nClient, const com::sun::star::accessibility::AccessibleEventObject & _rEvent) Line 256
 	swlo.dll!SwAccessibleContext::FireAccessibleEvent(com::sun::star::accessibility::AccessibleEventObject & rEvent) Line 457
 	swlo.dll!SwAccessibleContext::FireStateChangedEvent(__int64 nState, bool bNewState) Line 478
 	swlo.dll!SwAccessibleParagraph::InvalidateFocus_() Line 397
 	swlo.dll!SwAccessibleContext::InvalidateFocus() Line 1314
 	swlo.dll!SwAccessibleMap::InvalidateFocus() Line 2695
 	swlo.dll!SwViewShell::InvalidateAccessibleFocus() Line 2597
 	swlo.dll!SwEditWin::GetFocus() Line 5569
 	vcllo.dll!vcl::Window::CompatGetFocus() Line 3891
 	vcllo.dll!vcl::Window::ImplGrabFocus(GetFocusFlags nFlags) Line 385
 	vcllo.dll!vcl::Window::GrabFocus() Line 2983
 	sfxlo.dll!SfxFrame::GrabFocusOnComponent_Impl() Line 650

The problem is quite simply that CMAccessible::SmartQI() returns a
pointer that is the result of querying the newly created COM aggregation
for IID_IUnknown instead of the requested `iid` interface.

Apparently the wrong result happens to work in many cases, but the
CAccTable actually has 2 interfaces IAccessibleTable and
IAccessibleTable2, and the COM aggravation for IAccessibleTable2 is the
one that crashes; in this case the correct pointer is an offset of 8 to
the start of CAccTable, while in the working cases there is no offset.

So likely the bug was introduced by commits
3b86569fcba210eb6570fabef7ff8abf6aff91f0 and

winaccessibility/source/UAccCOM/MAccessible.cxx
@@ -2416,15 +2416,31 @@ HRESULT WINAPI CMAccessible::SmartQI(void* /*pv*/, REFIID iid, void** ppvObject)
                 assert(hr == S_OK);
                 if(hr == S_OK)
                 {
+                    // does it matter which IID was used to query before it is
+                    // put into the map?  probably not, and currently
+                    // QueryInterface is called after lookup anyway...
                     m_containedObjects.emplace(*rEntry.piid, static_cast<IUnknown*>(*ppvObject));
+                    // very nonobvious: this QI only succeeds on an IID_IUnknown
+                    // pointer because that one points directly to the
+                    // CComAggObject whereas the other-IID ones point into
+                    // CAcc* objects and end up forward back here to SmartQI
+                    // which firstly doesn't know IID_IUNOXWrapper and secondly
+                    // it would be useless to return a new instance for it, it
+                    // *must* be the same CAcc* instance that was just created.
+                    // Also, it's not possible to get the CAccTable out of the
+                    // CComAggObject because it is private.
+                    // However for the aggregated objects this here is also the
+                    // *only* place where this interface is called; every other
+                    // call is on a non-aggregated object - so it's sufficient
+                    // if it works once right after construction.
                     IUNOXWrapper* wrapper = nullptr;
                     static_cast<IUnknown*>(*ppvObject)->QueryInterface(IID_IUNOXWrapper, reinterpret_cast<void**>(&wrapper));
-                    if(wrapper)
-                    {
-                        wrapper->put_XInterface(
-                                reinterpret_cast<hyper>(m_xAccessible.get()));
-                        wrapper->Release();
-                    }
+                    assert(wrapper); // every map entry implements it currently
+                    wrapper->put_XInterface(
+                            reinterpret_cast<hyper>(m_xAccessible.get()));
+                    wrapper->Release();
+                    // ppvObject is IID_IUnknown - Query for requested target!
+                    static_cast<IUnknown*>(*ppvObject)->QueryInterface(iid, ppvObject);
                     return S_OK;
                 }
             }