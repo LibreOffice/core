b67d9767755c7f16fcf7037b062287b15cd752b1
Justin Luth <justin.luth@collabora.com>
2025-12-01 17:35:42 -0500

    tdf#165180 tdf#149756 tdf#152545 pptx: treat all MS PP filters the same
    I can't see any reason why all MS PowerPoint 2010 filters
    would not fall under the same rules
    as the PowerPoint 2007 ones.
    
    The first clause came from
    bnc#657899: Paragraph vertical spacing differs from PowerPoint
    without a unit test.
    
    Unfortunately, there is nothing visually different in the unit test...
    
    make CppunitTest_oox_shape \
        CPPUNIT_TEST_NAME=testTdf165180_standardConnectorsISO29500
    
    Change-Id: Ia65cce2e00a4dca41a3f92266eda7c217b14fc02
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/195019
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/195254

--- oox/qa/unit/shape.cxx
+++ oox/qa/unit/shape.cxx
@@ -206,6 +206,42 @@ CPPUNIT_TEST_FIXTURE(OoxShapeTest, testTdf165180_standardConnectorsECMA)
     }
 }
 
+CPPUNIT_TEST_FIXTURE(OoxShapeTest, testTdf165180_standardConnectorsISO29500)
+{
+    loadFromFile(u"tdf165180_standardConnectorsISO29500.potx");
+
+    uno::Reference<drawing::XDrawPagesSupplier> xDrawPagesSupplier(mxComponent, uno::UNO_QUERY);
+    uno::Reference<drawing::XDrawPage> xDrawPage(xDrawPagesSupplier->getDrawPages()->getByIndex(0),
+                                                 uno::UNO_QUERY);
+
+    sal_Int32 aEdgeValue[] = { -1352, -2457, 2402, // bentConnector5
+                               3977,  0,     0, // bentConnector3
+                               -2899, 0,     0, // bentConnector3
+                               -1260, 4612,  0, // bentConnector4
+                               -1431, -2642, 0, // bentConnector4
+                               3831,  3438,  -1578 }; // bentConnector5
+    sal_Int32 nCount = 0;
+    sal_Int32 nEdgeLineDelta;
+    for (size_t i = 0; i < 10; i++)
+    {
+        uno::Reference<drawing::XShape> xShape(xDrawPage->getByIndex(i), uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> xShapeProps(xShape, uno::UNO_QUERY);
+        bool bConnector = xShapeProps->getPropertySetInfo()->hasPropertyByName(u"EdgeKind"_ustr);
+        if (bConnector)
+        {
+            xShapeProps->getPropertyValue(UNO_NAME_EDGELINE1DELTA) >>= nEdgeLineDelta;
+            CPPUNIT_ASSERT_EQUAL(aEdgeValue[nCount], nEdgeLineDelta);
+            nCount++;
+            xShapeProps->getPropertyValue(UNO_NAME_EDGELINE2DELTA) >>= nEdgeLineDelta;
+            CPPUNIT_ASSERT_EQUAL(aEdgeValue[nCount], nEdgeLineDelta);
+            nCount++;
+            xShapeProps->getPropertyValue(UNO_NAME_EDGELINE3DELTA) >>= nEdgeLineDelta;
+            CPPUNIT_ASSERT_EQUAL(aEdgeValue[nCount], nEdgeLineDelta);
+            nCount++;
+        }
+    }
+}
+
 CPPUNIT_TEST_FIXTURE(OoxShapeTest, testTdf164623)
 {
     loadFromFile(u"tdf164623.pptx");
