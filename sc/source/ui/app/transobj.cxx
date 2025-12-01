LibreOffice/core

commit 671564bce4541244ad3d67b5b98899321176231e

tdf#167075: init doc shell before pasting as png/bmp (take 2)
Regression after
commit d8ab8992bd35aedfc1d2b0debebab0d623aa8d09
Author: Xisco Fauli <xiscofauli@libreoffice.org>
Date:   Mon May 26 11:06:06 2025 +0200

    ScViewData: pass ScDocShell by ref

and

commit 52e3b2b040ac54c2a5661e3676b135170f028f50
Author: Xisco Fauli <xiscofauli@libreoffice.org>
Date:   Mon May 26 16:06:12 2025 +0200

    ScViewData: simplify constructors

Change-Id: I6a709ab82f51c34a022c43d35b97f661e5c283e9
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/189434
Tested-by: Jenkins
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

sc/source/ui/app/transobj.cxx
@@ -97,21 +97,17 @@ void ScTransferObj::GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab
     nCol = nMaxCol;
 }
 
-void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
+void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
                                 const ScRange& rBlock )
 {
-    if (!rDoc.GetDocumentShell())
-        return;
-
     tools::Rectangle aBound( Point(), pDev->GetOutputSize() );      //! use size from clip area?
 
-    ScViewData aViewData(*rDoc.GetDocumentShell(), nullptr);
+    ScViewData aViewData(rDocSh, nullptr);
 
-    aViewData.SetTabNo( rBlock.aEnd.Tab() );
     aViewData.SetScreen( rBlock.aStart.Col(), rBlock.aStart.Row(),
                             rBlock.aEnd.Col(), rBlock.aEnd.Row() );
 
-    ScPrintFunc::DrawToDev( rDoc, pDev, nPrintFactor, aBound, aViewData, false/*bMetaFile*/ );
+    ScPrintFunc::DrawToDev( rDocSh.GetDocument(), pDev, nPrintFactor, aBound, aViewData, false/*bMetaFile*/ );
 }
 
 ScTransferObj::ScTransferObj( const std::shared_ptr<ScDocument>& pClipDoc, TransferableObjectDescriptor aDesc ) :
@@ -437,7 +433,8 @@ bool ScTransferObj::GetData( const datatransfer::DataFlavor& rFlavor, const OUSt
 
             pVirtDev->SetOutputSizePixel(aPixelSize);
 
-            PaintToDev( pVirtDev, *m_pDoc, 1.0, aReducedBlock );
+            InitDocShell(true);
+            PaintToDev( pVirtDev, *m_aDocShellRef, 1.0, aReducedBlock );
 
             pVirtDev->SetMapMode( MapMode( MapUnit::MapPixel, Point(), aScale, aScale ) );
             Bitmap aBmp( pVirtDev->GetBitmap( Point(), pVirtDev->GetOutputSize() ) );

sc/source/ui/inc/transobj.hxx
@@ -58,7 +58,7 @@ private:
     static void StripRefs( ScDocument& rDoc, SCCOL nStartX, SCROW nStartY,
                             SCCOL nEndX, SCROW nEndY,
                             ScDocument& rDestDoc );
-    static void PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
+    static void PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
                             const ScRange& rBlock );
     static void GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab2, SCROW& nRow, SCCOL& nCol );
 

commit 894dbe5208bb40b7be11f1880f42c6efaf5a76c4

tdf#169554: Revert "tdf#167075: init doc shell before pasting as png/bmp
(take 2)"

This reverts commit 671564bce4541244ad3d67b5b98899321176231e.

I can't reproduce the issue on Windows nor on Linux
but two people confirmed that
671564bce4541244ad3d67b5b98899321176231e introduced
the regression in the report

Change-Id: Iead1e460b8280883128b84c1be67209bbb3c6f4c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194767
Tested-by: Jenkins
Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

sc/source/ui/app/transobj.cxx
@@ -97,17 +97,21 @@ void ScTransferObj::GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab
     nCol = nMaxCol;
 }
 
-void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
+void ScTransferObj::PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
                                 const ScRange& rBlock )
 {
+    if (!rDoc.GetDocumentShell())
+        return;
+
     tools::Rectangle aBound( Point(), pDev->GetOutputSize() );      //! use size from clip area?
 
-    ScViewData aViewData(rDocSh, nullptr);
+    ScViewData aViewData(*rDoc.GetDocumentShell(), nullptr);
 
+    aViewData.SetTabNo( rBlock.aEnd.Tab() );
     aViewData.SetScreen( rBlock.aStart.Col(), rBlock.aStart.Row(),
                             rBlock.aEnd.Col(), rBlock.aEnd.Row() );
 
-    ScPrintFunc::DrawToDev( rDocSh.GetDocument(), pDev, nPrintFactor, aBound, aViewData, false/*bMetaFile*/ );
+    ScPrintFunc::DrawToDev( rDoc, pDev, nPrintFactor, aBound, aViewData, false/*bMetaFile*/ );
 }
 
 ScTransferObj::ScTransferObj( const std::shared_ptr<ScDocument>& pClipDoc, TransferableObjectDescriptor aDesc ) :
@@ -433,8 +437,7 @@ bool ScTransferObj::GetData( const datatransfer::DataFlavor& rFlavor, const OUSt
 
             pVirtDev->SetOutputSizePixel(aPixelSize);
 
-            InitDocShell(true);
-            PaintToDev( pVirtDev, *m_aDocShellRef, 1.0, aReducedBlock );
+            PaintToDev( pVirtDev, *m_pDoc, 1.0, aReducedBlock );
 
             pVirtDev->SetMapMode( MapMode( MapUnit::MapPixel, Point(), aScale, aScale ) );
             BitmapEx aBmp = pVirtDev->GetBitmapEx( Point(), pVirtDev->GetOutputSize() );

sc/source/ui/inc/transobj.hxx
@@ -58,7 +58,7 @@ private:
     static void StripRefs( ScDocument& rDoc, SCCOL nStartX, SCROW nStartY,
                             SCCOL nEndX, SCROW nEndY,
                             ScDocument& rDestDoc );
-    static void PaintToDev( OutputDevice* pDev, ScDocShell& rDocSh, double nPrintFactor,
+    static void PaintToDev( OutputDevice* pDev, ScDocument& rDoc, double nPrintFactor,
                             const ScRange& rBlock );
     static void GetAreaSize( const ScDocument& rDoc, SCTAB nTab1, SCTAB nTab2, SCROW& nRow, SCCOL& nCol );
 