LibreOffice/core

commit 70d8195a2b72440fe929e4e3761d1f1d5ace0096

LOK: show warning/error dialogs
show error dialog when invalid images are inserted
show error dialog when sheet rename was invalid

Change-Id: I9bf6bb25ac247e966da36bffcda3284d5d82ae42
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194778
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

sc/source/core/data/document.cxx
@@ -23,6 +23,7 @@
 #include <editeng/editobj.hxx>
 #include <svx/svditer.hxx>
 #include <sfx2/docfile.hxx>
+#include <sfx2/viewfrm.hxx>
 #include <svl/numformat.hxx>
 #include <poolcach.hxx>
 #include <svl/zforlist.hxx>
@@ -914,6 +915,14 @@ bool ScDocument::RenameTab( SCTAB nTab, const OUString& rName, bool bExternalDoc
                 ScModelObj* pModel = GetDocumentShell()->GetModel();
                 SfxLokHelper::notifyDocumentSizeChangedAllViews(pModel);
             }
+        }
+        else
+        {
+            OUString aErrMsg(ScResId(STR_INVALIDTABNAME));
+            std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(
+                GetDocumentShell()->GetFrame()->GetFrameWeld(), VclMessageType::Warning,
+                VclButtonsType::Ok, aErrMsg));
+            xBox->runAsync(xBox, [](sal_uInt32) {});
         }
     }
 

sw/source/uibase/uiview/view2.cxx
@@ -392,7 +392,7 @@ bool SwView::InsertGraphicDlg( SfxRequest& rReq )
     OUString sGraphicFormat = SwResId(STR_POOLFRM_GRAPHIC);
 
     const SfxStringItem* pName = rReq.GetArg<SfxStringItem>(SID_INSERT_GRAPHIC);
-    bool bShowError = !pName;
+    bool bShowError = !pName || comphelper::LibreOfficeKit::isActive();
 
     // No file pickers in a non-desktop (mobile app) build.
 

commit 707d23e29ba130a98357c46d5b266a720d52263b

fix multiple sheet rename error dialogs appearing
regression from 70d8195a2b72440fe929e4e3761d1f1d5ace0096

Change-Id: I285a87f6a315034527749662eb96321afe081994
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194802
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

sc/source/core/data/document.cxx
@@ -916,14 +916,6 @@ bool ScDocument::RenameTab( SCTAB nTab, const OUString& rName, bool bExternalDoc
                 SfxLokHelper::notifyDocumentSizeChangedAllViews(pModel);
             }
         }
-        else
-        {
-            OUString aErrMsg(ScResId(STR_INVALIDTABNAME));
-            std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(
-                GetDocumentShell()->GetFrame()->GetFrameWeld(), VclMessageType::Warning,
-                VclButtonsType::Ok, aErrMsg));
-            xBox->runAsync(xBox, [](sal_uInt32) {});
-        }
     }
 
     collectUIInformation({{"NewName", rName}}, u"Rename_Sheet"_ustr);

sc/source/ui/view/tabvwshf.cxx
@@ -1101,6 +1101,14 @@ void ScTabViewShell::ExecuteAppendOrRenameTable(SfxRequest& rReq)
         {
             rReq.Done( *pReqArgs );
         }
+        else
+        {
+            OUString aErrMsg(ScResId(STR_INVALIDTABNAME));
+            std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(
+                GetFrameWeld(), VclMessageType::Warning,
+                VclButtonsType::Ok, aErrMsg));
+            xBox->runAsync(xBox, [](sal_uInt32) {});
+        }
     }
     else
     {
@@ -1192,9 +1200,9 @@ bool ScTabViewShell::DoAppendOrRenameTableDialog(sal_Int32 nResult, const VclPtr
         else
         {
             OUString aErrMsg ( ScResId( STR_INVALIDTABNAME ) );
-            std::shared_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(
+            std::unique_ptr<weld::MessageDialog> xBox(Application::CreateMessageDialog(
                 GetFrameWeld(), VclMessageType::Warning, VclButtonsType::Ok, aErrMsg));
-            xBox->runAsync(xBox, [](sal_uInt32) {});
+            xBox->run();
         }
     }
 