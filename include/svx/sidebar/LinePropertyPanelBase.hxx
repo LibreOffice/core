7390eeb0b023991c7ad64d1cc31aaff2fcd1621d
Simon Chenery <simon_chenery@yahoo.com>
2025-12-03 20:51:48 +0100

    tdf#143148 Replace include guards with pragma once
    Used pragma once in include/svx/sidebar
    
    Change-Id: Iaafd0cd7d6818c3da6b805adcc454bed1645c10f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194966
    Reviewed-by: Hossein <hossein@libreoffice.org>
    Tested-by: Jenkins

--- include/svx/sidebar/LinePropertyPanelBase.hxx
+++ include/svx/sidebar/LinePropertyPanelBase.hxx
@@ -16,8 +16,7 @@
  *   except in compliance with the License. You may obtain a copy of
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
-#ifndef INCLUDED_SVX_SOURCE_SIDEBAR_LINE_LINEPROPERTYPANELBASE_HXX
-#define INCLUDED_SVX_SOURCE_SIDEBAR_LINE_LINEPROPERTYPANELBASE_HXX
+#pragma once
 
 #include <vcl/weld/MetricSpinButton.hxx>
 #include <vcl/weld/weld.hxx>
@@ -119,7 +118,5 @@ private:
 };
 
 } // end of namespace svx::sidebar
-
-#endif
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
