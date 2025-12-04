LibreOffice/core

commit b058526f16724d72f87810c438ac0cccb2ca16ff

sd: make sure that canvas page remains as the last page
This commit restricts the following operations:
- duplicating canvas page
- moving canvas page up
- inserting a page after the canvas page
- including canvas page in the slideshow

Also, prevent deleting the only non-canvas page.

Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
Change-Id: I8050dfc9836719f345e0f5349f25b62cd164344d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192769
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

sd/source/core/drawdoc2.cxx
@@ -486,11 +486,16 @@ void SdDrawDocument::DeletePage(sal_uInt16 nPgNum)
 // Remove page
 rtl::Reference<SdrPage> SdDrawDocument::RemovePage(sal_uInt16 nPgNum)
 {
+    // Do not remove the only non-canvas page
+    if (HasCanvasPage() && GetSdPageCount(PageKind::Standard) == 2)
+        return nullptr;
     rtl::Reference<SdrPage> pPage = FmFormModel::RemovePage(nPgNum);
 
     bool bLast = ((nPgNum+1)/2 == (GetPageCount()+1)/2);
 
     auto pSdPage = static_cast<SdPage*>(pPage.get());
+    if (pSdPage->IsCanvasPage())
+        mpCanvasPage = nullptr;
     pSdPage->DisconnectLink();
     ReplacePageInCustomShows( pSdPage, nullptr );
     UpdatePageObjectsInNotes(nPgNum);
@@ -1221,6 +1226,8 @@ sal_uInt16 SdDrawDocument::CreatePage (
     bool bIsPageObj,
     const sal_Int32 nInsertPosition)
 {
+    if (pActualPage->IsCanvasPage())
+        return 0xffff;
     SdPage* pPreviousStandardPage;
     SdPage* pPreviousNotesPage;
     rtl::Reference<SdPage> pStandardPage;
@@ -1304,6 +1311,9 @@ sal_uInt16 SdDrawDocument::DuplicatePage (sal_uInt16 nPageNum)
     // Get current page
     SdPage* pActualPage = GetSdPage(nPageNum, ePageKind);
 
+    if (pActualPage->IsCanvasPage())
+        return 0xffff;
+
     // Get background flags
     SdrLayerAdmin& rLayerAdmin = GetLayerAdmin();
     SdrLayerID aBckgrnd = rLayerAdmin.GetLayerID(sUNO_LayerName_background);
@@ -1327,6 +1337,8 @@ sal_uInt16 SdDrawDocument::DuplicatePage (
     bool bIsPageObj,
     const sal_Int32 nInsertPosition)
 {
+    if (pActualPage->IsCanvasPage())
+        return 0xffff;
     SdPage* pPreviousStandardPage;
     SdPage* pPreviousNotesPage;
     rtl::Reference<SdPage> pStandardPage;
@@ -1470,12 +1482,17 @@ sal_uInt16 SdDrawDocument::GetOrInsertCanvasPage()
     sal_uInt16 nCanvasPageNum = CreatePage(pLastStandardPage, PageKind::Standard, u"Canvas Page"_ustr, u"Canvas notes page"_ustr, AutoLayout::AUTOLAYOUT_NONE, AutoLayout::AUTOLAYOUT_NONE, false, true, pLastStandardPage->GetPageNum() + 2);
 
     SdPage* pCanvasPage = GetSdPage(nCanvasPageNum, PageKind::Standard);
+    if (!pCanvasPage)
+        return 0xffff;
 
     const Size aCanvasSize(500000, 500000);
 
     ResizeCurrentPage(pCanvasPage, aCanvasSize, PageKind::Standard);
     pCanvasPage->SetCanvasPage();
     mpCanvasPage = pCanvasPage;
+
+    SdPage* pMasterCanvas = static_cast<SdPage*>(&pCanvasPage->TRG_GetMasterPage());
+    pMasterCanvas->SetCanvasMasterPage();
 
     populatePagePreviewsGrid();
 

officecfg/registry/data/org/openoffice/Office/UI/ImpressWindowState.xcu
@@ -125,6 +125,11 @@
           <value xml:lang="en-US">Slide Sorter/Pane</value>
         </prop>
       </node>
+      <node oor:name="private:resource/popupmenu/pagepanecanvas" oor:op="replace">
+        <prop oor:name="UIName" oor:type="xs:string">
+          <value xml:lang="en-US">Slide Sorter/Pane (Canvas)</value>
+        </prop>
+      </node>
       <node oor:name="private:resource/popupmenu/pagepanenosel" oor:op="replace">
         <prop oor:name="UIName" oor:type="xs:string">
           <value xml:lang="en-US">Slide Sorter/Pane (no selection)</value>
@@ -133,6 +138,11 @@
       <node oor:name="private:resource/popupmenu/pagepanemaster" oor:op="replace">
         <prop oor:name="UIName" oor:type="xs:string">
           <value xml:lang="en-US">Master Slide Sorter/Pane</value>
+        </prop>
+      </node>
+      <node oor:name="private:resource/popupmenu/pagepanecanvasmaster" oor:op="replace">
+        <prop oor:name="UIName" oor:type="xs:string">
+          <value xml:lang="en-US">Master Slide Sorter/Pane (Canvas)</value>
         </prop>
       </node>
       <node oor:name="private:resource/popupmenu/pagepanenoselmaster" oor:op="replace">

sd/UIConfig_simpress.mk
@@ -37,9 +37,11 @@ $(eval $(call gb_UIConfig_add_popupmenufiles,modules/simpress,\
 	sd/uiconfig/simpress/popupmenu/oleobject \
 	sd/uiconfig/simpress/popupmenu/outline \
 	sd/uiconfig/simpress/popupmenu/pagepanemaster \
+	sd/uiconfig/simpress/popupmenu/pagepanecanvasmaster \
 	sd/uiconfig/simpress/popupmenu/pagepanenoselmaster \
 	sd/uiconfig/simpress/popupmenu/pagepanenosel \
 	sd/uiconfig/simpress/popupmenu/pagepane \
+	sd/uiconfig/simpress/popupmenu/pagepanecanvas \
 	sd/uiconfig/simpress/popupmenu/pagetab \
 	sd/uiconfig/simpress/popupmenu/page \
 	sd/uiconfig/simpress/popupmenu/table \

sd/inc/drawdoc.hxx
@@ -1032,7 +1032,7 @@ public:
         bool bIsPageObj,
         const sal_Int32 nInsertPosition);
 
-    SAL_DLLPRIVATE bool HasCanvasPage() const { return mpCanvasPage != nullptr; }
+    SAL_DLLPRIVATE bool HasCanvasPage() const { return mpCanvasPage.is(); }
 
     SAL_DLLPRIVATE sal_uInt16 GetOrInsertCanvasPage ();
 

sd/inc/sdpage.hxx
@@ -123,6 +123,7 @@ friend class sd::UndoAttrObject;
     sal_uInt16  mnPaperBin;               ///< PaperBin
     SdPageLink* mpPageLink;               ///< Page link (at left sides only)
     bool    mbIsCanvasPage;               ///< whether the page is a canvas page
+    bool    mbIsCanvasMasterPage;         ///< whether it is the master page of canvas page
 
     // PDF link annotations for read-only pdfium
     std::vector<std::pair<basegfx::B2DRectangle, OUString>> maLinkAnnotations;
@@ -404,6 +405,9 @@ public:
 
     bool IsCanvasPage() const { return mbIsCanvasPage; }
     void SetCanvasPage() { mbIsCanvasPage = true; }
+
+    bool IsCanvasMasterPage() const { return mbIsCanvasMasterPage; }
+    void SetCanvasMasterPage() { mbIsCanvasMasterPage = true; }
 
 private:
     bool mbIsPrecious;

sd/source/core/sdpage.cxx
@@ -121,6 +121,8 @@ SdPage::SdPage(SdDrawDocument& rNewDoc, bool bMasterPage)
 ,   meCharSet(osl_getThreadTextEncoding())
 ,   mnPaperBin(PAPERBIN_PRINTER_SETTINGS)
 ,   mpPageLink(nullptr)
+,   mbIsCanvasPage(false)
+,   mbIsCanvasMasterPage(false)
 ,   mnTransitionType(0)
 ,   mnTransitionSubtype(0)
 ,   mbTransitionDirection(true)

sd/source/ui/slidesorter/controller/SlideSorterController.cxx
@@ -302,12 +302,22 @@ bool SlideSorterController::Command (
             if (mrModel.GetEditMode() == EditMode::Page)
             {
                 if (pPage != nullptr)
-                    aPopupId = "pagepane";
+                {
+                    if (pPage->IsCanvasPage())
+                        aPopupId = "pagepanecanvas";
+                    else
+                        aPopupId = "pagepane";
+                }
                 else
                     aPopupId = "pagepanenosel";
             }
             else if (pPage != nullptr)
-                aPopupId = "pagepanemaster";
+            {
+                if (pPage->IsCanvasMasterPage())
+                    aPopupId = "pagepanecanvasmaster";
+                else
+                    aPopupId = "pagepanemaster";
+            }
             else
                 aPopupId = "pagepanenoselmaster";
 

sd/source/ui/slidesorter/shell/SlideSorterViewShell.cxx
@@ -797,6 +797,13 @@ void SlideSorterViewShell::GetStateMovePageFirst (SfxItemSet& rSet)
         rSet.DisableItem( SID_MOVE_PAGE_FIRST );
         rSet.DisableItem( SID_MOVE_PAGE_UP );
     }
+
+    if (GetDoc()->HasCanvasPage())
+        if (firstSelectedPageNo == GetDoc()->GetSdPageCount(PageKind::Standard) - 1)
+        {
+            rSet.DisableItem( SID_MOVE_PAGE_FIRST );
+            rSet.DisableItem( SID_MOVE_PAGE_UP );
+        }
 }
 
 void SlideSorterViewShell::ExecMovePageUp (SfxRequest& /*rReq*/)
@@ -818,6 +825,10 @@ void SlideSorterViewShell::ExecMovePageUp (SfxRequest& /*rReq*/)
 
     if (firstSelectedPageNo == 0)
         return;
+
+    if (GetDoc()->HasCanvasPage())
+        if (firstSelectedPageNo == GetDoc()->GetSdPageCount(PageKind::Standard) - 1)
+            return;
 
     // Move pages before firstSelectedPageNo - 1 (so after firstSelectedPageNo - 2),
     // remembering that -1 means at first, which is good.
@@ -848,6 +859,9 @@ void SlideSorterViewShell::ExecMovePageDown (SfxRequest& /*rReq*/)
     lastSelectedPageNo = (lastSelectedPageNo - 1) / 2;
     if (lastSelectedPageNo == nNoOfPages - 1)
         return;
+    if (GetDoc()->HasCanvasPage())
+        if (lastSelectedPageNo == nNoOfPages - 2)
+            return;
 
     // Move to position after lastSelectedPageNo
     GetDoc()->MoveSelectedPages( lastSelectedPageNo + 1 );
@@ -874,7 +888,10 @@ void SlideSorterViewShell::ExecMovePageLast (SfxRequest& /*rReq*/)
     sal_uInt16 nNoOfPages = GetDoc()->GetSdPageCount(PageKind::Standard);
 
     // Move to position after last page No (=Number of pages - 1)
-    GetDoc()->MoveSelectedPages( nNoOfPages - 1 );
+    if (!GetDoc()->HasCanvasPage())
+        GetDoc()->MoveSelectedPages( nNoOfPages - 1 );
+    else
+        GetDoc()->MoveSelectedPages( nNoOfPages - 2 );
 
     PostMoveSlidesActions(xSelection);
 }
@@ -905,6 +922,14 @@ void SlideSorterViewShell::GetStateMovePageLast (SfxItemSet& rSet)
     {
         rSet.DisableItem( SID_MOVE_PAGE_LAST );
         rSet.DisableItem( SID_MOVE_PAGE_DOWN );
+    }
+    if (GetDoc()->HasCanvasPage())
+    {
+        if (lastSelectedPageNo == nNoOfPages - 2)
+        {
+            rSet.DisableItem( SID_MOVE_PAGE_LAST );
+            rSet.DisableItem( SID_MOVE_PAGE_DOWN );
+        }
     }
 }
 

commit 2d31b3a094133009abbbd52257f258c448a35b5f

sd: fix remove page logic for non-canvas page
regression from b058526f16724d72f87810c438ac0cccb2ca16ff

Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
Change-Id: I9dc939239ec9683680f5541ded77958d46a486b8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/194996
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sd/source/core/drawdoc2.cxx
@@ -505,7 +505,8 @@ void SdDrawDocument::DeletePage(sal_uInt16 nPgNum)
 rtl::Reference<SdrPage> SdDrawDocument::RemovePage(sal_uInt16 nPgNum)
 {
     // Do not remove the only non-canvas page
-    if (HasCanvasPage() && GetSdPageCount(PageKind::Standard) == 2)
+    if (HasCanvasPage() && GetSdPageCount(PageKind::Standard) == 2
+        && nPgNum == 3)
         return nullptr;
     rtl::Reference<SdrPage> pPage = FmFormModel::RemovePage(nPgNum);
 