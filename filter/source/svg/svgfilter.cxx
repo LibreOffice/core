LibreOffice/core

commit 58ba249df589b0c9f91667f2938bfa18d2a1ce61

tdf#166779: use model first to obtain selection
The current frame can be Basic, so only use it as a fallback to obtain selection.
Also, failure to get the selection is not fatal here.

The throwing behavior was since commit aa1927dc257b52edf96de220cc3797e02c83a0ae
(Make svg export use slidesorter selection in most cases., 2012-10-10).

Change-Id: I06f80151b4184f353d1d5387c11837f1e883a068
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185983
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

filter/source/svg/svgfilter.cxx
@@ -149,15 +149,28 @@ sal_Bool SAL_CALL SVGFilter::filter( const Sequence< PropertyValue >& rDescripto
     return filterImpressOrDraw(rDescriptor);
 }
 
+css::uno::Reference<css::frame::XController> SVGFilter::getSourceController() const
+{
+    uno::Reference<frame::XController> xController;
+    // Current frame may be e.g. Basic. Try to get a controller from the source model first.
+    if (auto xModel = mxSrcDoc.query<frame::XModel>())
+        xController = xModel->getCurrentController();
+    // Try current frame as a fallback.
+    if (!xController)
+    {
+        uno::Reference<frame::XDesktop2> xDesktop(frame::Desktop::create(mxContext));
+        if (auto xFrame = xDesktop->getCurrentFrame()) // Manage headless case
+            xController = xFrame->getController();
+    }
+    return xController;
+}
+
 css::uno::Reference<css::frame::XController> SVGFilter::fillDrawImpressSelectedPages()
 {
-    uno::Reference<frame::XDesktop2> xDesktop(frame::Desktop::create(mxContext));
-    uno::Reference<frame::XFrame> xFrame = xDesktop->getCurrentFrame(); // Manage headless case
-    if (!xFrame)
+    uno::Reference<frame::XController> xController = getSourceController();
+    uno::Reference<drawing::framework::XControllerManager> xManager(xController, uno::UNO_QUERY);
+    if (!xManager)
         return {};
-    uno::Reference<frame::XController> xController(xFrame->getController(), uno::UNO_SET_THROW);
-    uno::Reference<drawing::framework::XControllerManager> xManager(xController,
-                                                                    uno::UNO_QUERY_THROW);
     uno::Reference<drawing::framework::XConfigurationController> xConfigController(
         xManager->getConfigurationController());
 
@@ -210,9 +223,9 @@ css::uno::Reference<css::frame::XController> SVGFilter::fillDrawImpressSelectedP
 
     if (mSelectedPages.empty())
     {
-        // apparently failed to clean selection - fallback to current page
-        uno::Reference<drawing::XDrawView> xDrawView(xController, uno::UNO_QUERY_THROW);
-        mSelectedPages.push_back(xDrawView->getCurrentPage());
+        // apparently failed to get a selection - fallback to current page
+        if (auto xDrawView = xController.query<drawing::XDrawView>())
+            mSelectedPages.push_back(xDrawView->getCurrentPage());
     }
     return xController;
 }
@@ -580,12 +593,7 @@ bool SVGFilter::filterWriterOrCalc( const Sequence< PropertyValue >& rDescriptor
     if(!bSelectionOnly) // For Writer only the selection-only mode is supported
         return false;
 
-    uno::Reference<frame::XDesktop2> xDesktop(frame::Desktop::create(mxContext));
-    uno::Reference<frame::XController > xController;
-    if (uno::Reference<frame::XFrame> xFrame = xDesktop->getCurrentFrame())
-        xController.set(xFrame->getController(), uno::UNO_SET_THROW);
-
-    Reference< view::XSelectionSupplier > xSelection (xController, UNO_QUERY);
+    Reference<view::XSelectionSupplier> xSelection(getSourceController(), UNO_QUERY);
     if (!xSelection.is())
         return false;
 

filter/source/svg/svgfilter.hxx
@@ -284,6 +284,7 @@ private:
     bool filterImpressOrDraw( const Sequence< PropertyValue >& rDescriptor );
     bool filterWriterOrCalc( const Sequence< PropertyValue >& rDescriptor );
 
+    css::uno::Reference<css::frame::XController> getSourceController() const;
     css::uno::Reference<css::frame::XController> fillDrawImpressSelectedPages();
 
 protected:

commit b152ca62b9fdb4a667440ae20e96612dcc12191e

tdf#168054: Export all ODG pages to SVG from command line by default
Regression from commit 58ba249df589b0c9f91667f2938bfa18d2a1ce61, which
fixed obtaining controller, including command line scenario. Before it,
no controller was found, and mSelectedPages was empty after a call to
fillDrawImpressSelectedPages. Now the document's controller is found,
and that puts the first page as the selection into mSelectedPages.

This change uses ConversionRequestOrigin property, added to medium in
DispatchWatcher::executeDispatchRequests, to detect that the export
is initiated from command line. In that case, it is assumed that all
pages should be exported.

I don't really know why the behavior restored in this change is wanted.
The result is all pages' objects overlapped. Even though SVG markup
has the pages in separate 'g' elements, it doesn't look reasonable to
have this result by default. Yet, as it was a pre-existing behavior,
let's keep it this way for now.

Change-Id: I6d6923e627df4282c61342210ee7d762ea08e2b5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/195003
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

filter/source/svg/svgfilter.cxx
@@ -423,19 +423,26 @@ bool SVGFilter::filterImpressOrDraw( const Sequence< PropertyValue >& rDescripto
         bool bPageProvided = comphelper::LibreOfficeKit::isActive();
         sal_Int32 nPageToExport = -1;
 
-        for( const PropertyValue& rProp : rDescriptor )
+        comphelper::SequenceAsHashMap args(rDescriptor);
+        if (args.contains(u"ConversionRequestOrigin"_ustr))
         {
-            if (rProp.Name == "SelectionOnly")
-            {
-                // #i124608# extract single selection wanted from dialog return values
-                rProp.Value >>= bSelectionOnly;
-                bPageProvided = false;
-            }
-            else if (rProp.Name == "PagePos")
-            {
-                rProp.Value >>= nPageToExport;
+            // when invoked from command line, default to exporting everything (-1)
+            if (args[u"ConversionRequestOrigin"_ustr] == u"CommandLine"_ustr)
                 bPageProvided = true;
-            }
+        }
+
+        if (args.contains(u"PagePos"_ustr))
+        {
+            args[u"PagePos"_ustr] >>= nPageToExport;
+            bPageProvided = true;
+        }
+
+        if (args.contains(u"SelectionOnly"_ustr))
+        {
+            // #i124608# extract single selection wanted from dialog return values
+            args[u"SelectionOnly"_ustr] >>= bSelectionOnly;
+            if (bSelectionOnly)
+                bPageProvided = false;
         }
 
         uno::Reference<frame::XController > xController;
@@ -461,12 +468,12 @@ bool SVGFilter::filterImpressOrDraw( const Sequence< PropertyValue >& rDescripto
 
                     for (sal_Int32 i = 0; i < nDPCount; ++i)
                     {
-                        if( nPageToExport != -1 && nPageToExport == i )
+                        if (nPageToExport == i)
                         {
                             uno::Reference< drawing::XDrawPage > xDrawPage( xDrawPages->getByIndex( i ), uno::UNO_QUERY );
                             mSelectedPages.push_back(xDrawPage);
                         }
-                        else
+                        else if (nPageToExport == -1)
                         {
                             uno::Reference< drawing::XDrawPage > xDrawPage( xDrawPages->getByIndex( i ), uno::UNO_QUERY );
                             Reference< XPropertySet > xPropSet( xDrawPage, UNO_QUERY );

sfx2/source/doc/objstor.cxx
@@ -2837,6 +2837,7 @@ bool SfxObjectShell::ExportTo( SfxMedium& rMedium )
         bool bHasFilterName = false;
         bool bIsRedactMode = false;
         bool bIsPreview = false;
+        std::optional<OUString> oConversionRequestOrigin;
         sal_Int32 nEnd = aOldArgs.getLength();
 
         for ( sal_Int32 i = 0; i < nEnd; i++ )
@@ -2859,6 +2860,11 @@ bool SfxObjectShell::ExportTo( SfxMedium& rMedium )
         {
             if( rMediumArgs[i].Name == "IsPreview" )
                 rMediumArgs[i].Value >>= bIsPreview;
+            else if (rMediumArgs[i].Name == "ConversionRequestOrigin")
+            {
+                if (OUString s; rMediumArgs[i].Value >>= s)
+                    oConversionRequestOrigin = s;
+            }
         }
 
         // FIXME: Handle this inside TransformItems()
@@ -2912,6 +2918,13 @@ bool SfxObjectShell::ExportTo( SfxMedium& rMedium )
             auto pArgs = aArgs.getArray();
             pArgs[nEnd-1].Name = "IsPreview";
             pArgs[nEnd-1].Value <<= bIsPreview;
+        }
+        if (oConversionRequestOrigin)
+        {
+            aArgs.realloc( ++nEnd );
+            auto pArgs = aArgs.getArray();
+            pArgs[nEnd-1].Name = u"ConversionRequestOrigin"_ustr;
+            pArgs[nEnd-1].Value <<= *oConversionRequestOrigin;
         }
 
         return xFilter->filter( aArgs );