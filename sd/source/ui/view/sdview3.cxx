863334b176cb4e9b2aefa7f0569bc50d390a9937
Balazs Varga <balazs.varga@collabora.com>
2025-12-22 14:59:46 +0100

    tdf#170027 - sd fix crash when undoing shape drag with Alt pressed
    Do not overwrite original shape during drag&drop with Alt,
    when we try to drag&drop the shape on the same shape,
    but create a new shape with new name, same as if we would drag&drop
    the object in general.
    
    Change-Id: I08b2197b3b9755455a165a566eb3452846c0f78e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196097
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Balazs Varga <balazs.varga@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit 9f980e021ff1a7613003e5c93ec49a37c5ca3420)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196177
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sd/source/ui/view/sdview3.cxx
+++ sd/source/ui/view/sdview3.cxx
@@ -764,7 +764,7 @@ bool View::InsertData( const TransferableDataHelper& rDataHelper,
                         SdrPageView*    pPV = nullptr;
                         SdrObject* pPickObj2 = PickObj(rPos, getHitTolLog(), pPV);
 
-                        if( ( mnAction & DND_ACTION_MOVE ) && pPickObj2 && pObj )
+                        if( ( mnAction & DND_ACTION_MOVE ) && pPickObj2 && pObj && pPickObj2->GetName() != pObj->GetName() )
                         {
                             // replace object
                             SdrPage* pWorkPage = GetSdrPageView()->GetPage();
@@ -804,6 +804,7 @@ bool View::InsertData( const TransferableDataHelper& rDataHelper,
                             mnAction = DND_ACTION_COPY;
                         }
                         else if( ( mnAction & DND_ACTION_LINK ) && pPickObj2 && pObj &&
+                            pPickObj2->GetName() != pObj->GetName() &&
                             dynamic_cast< const SdrGrafObj *>( pPickObj2 ) ==  nullptr &&
                                 dynamic_cast< const SdrOle2Obj *>( pPickObj2 ) ==  nullptr )
                         {
