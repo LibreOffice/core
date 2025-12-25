LibreOffice/core

commit 74c29345a7c179b048c157582a1145e381616e5c

tdf#167657 writerfilter: only move sectPr bottomMargin after pageBreak
This fixes a 24.2.4 regression from
my commit 5ff1fd13f1879105ecc0541c55f30cea32ae54f4.

The problem was that I treated all sectPr's as page breaks,
and that meant that the below spacing of the last paragraph
before a continuous section break was being unintentionally changed.

A continuous section break's above and below spacing
should have no direct effect on layout, but it does affect
how much the next paragraph's top spacing collapses.

This is a bit tricky because at the time of the section break
we have no idea WHY the new break started.
Only MUCH LATER at the end of the section can we discover
whether it was a page break instead of a continuous (or column) break.
Thankfully someone has already calculated and stored the
"last paragraph before this section started".

TODO: somehow the below spacing from a continuous section break
needs to reduce the top spacing from the following paragraph.
At this point I can't imagine how this could reliably happen.

make CppunitTest_sw_ooxmlexport25 \
    CPPUNIT_TEST_NAME=testTdf167657_sectPr_bottomSpacing

Change-Id: Ifd908e2104f7e1efb86947dd88e7ea6874346d58
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188298
Reviewed-by: Justin Luth <jluth@mail.com>
Tested-by: Jenkins

sw/source/writerfilter/dmapper/PropertyMap.cxx
@@ -1941,6 +1941,26 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
         ApplyBorderToPageStyles( rDM_Impl, m_eBorderApply, m_eBorderOffsetFrom );
         ApplyPaperSource(rDM_Impl);
 
+        // Emulation: now apply the previous section's unused belowSpacing
+        // to the last paragraph before the section page break
+        // so that the consolidation/collapse of this section's 1st paragraph's aboveSpacing
+        // works correctly (since below spacing before a page break otherwise has no relevance).
+        if (pPrevSection && pPrevSection->GetBelowSpacing().has_value() && m_xPreStartingRange.is())
+        {
+            try
+            {
+                const uno::Any aBelowSpacingOfPrevSection(*pPrevSection->GetBelowSpacing());
+                const OUString sProp(getPropertyName(PROP_PARA_BOTTOM_MARGIN));
+                uno::Reference<beans::XPropertySet> xLastParaInPrevSection(m_xPreStartingRange,
+                                                                           uno::UNO_QUERY_THROW);
+                xLastParaInPrevSection->setPropertyValue(sProp, aBelowSpacingOfPrevSection);
+            }
+            catch (uno::Exception&)
+            {
+                TOOLS_WARN_EXCEPTION("writerfilter", "Transfer below spacing to last para.");
+            }
+        }
+
         try
         {
             //now apply this break at the first paragraph of this section

sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
@@ -81,6 +81,20 @@ DECLARE_OOXMLEXPORT_TEST(testTdf166510_sectPr_bottomSpacing, "tdf166510_sectPr_b
     CPPUNIT_ASSERT_EQUAL(sal_Int32(4253), nHeight);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf167657_sectPr_bottomSpacing, "tdf167657_sectPr_bottomSpacing.docx")
+{
+    // given with a continuous break sectPr with no belowSpacing
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+
+    auto pXmlDoc = parseLayoutDump();
+
+    // Since there is no page break, the prior paragraph's belowSpacing should not be zero'd out.
+    // NOTE: apparently layout assigns the belowSpacing to the following section, not body/txt[1],
+    sal_Int32 nHeight = getXPath(pXmlDoc, "//section/infos/bounds", "height").toInt32();
+    // Without the fix, the section's height (showing no bottom margin for the 1st para) was 309
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1409), nHeight);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf165478_bottomAligned, "tdf165478_bottomAligned.docx")
 {
     // given a layoutInCell, wrap-through image, paragraph-anchored to a bottom-aligned cell

sw/source/writerfilter/dmapper/DomainMapper_Impl.cxx
@@ -10294,21 +10294,15 @@ void DomainMapper_Impl::handleSectPrBeforeRemoval()
     if (!pSectPrContext)
         return;
 
-    // transfer below-spacing to previous paragraph - needed to layout above-spacing after pageBreak
-    if (!m_StreamStateStack.top().xPreviousParagraph.is())
+    SectionPropertyMap* pSectionContext = GetSectionContext();
+    if (!pSectionContext)
         return;
 
-    uno::Any aBelowSpacing = GetAnyProperty(PROP_PARA_BOTTOM_MARGIN, pSectPrContext);
-    if (!aBelowSpacing.hasValue())
-        aBelowSpacing <<= sal_Int32(0);
+    sal_Int32 nBelowSpacing = 0;
+    GetAnyProperty(PROP_PARA_BOTTOM_MARGIN, pSectPrContext) >>= nBelowSpacing;
 
-    std::optional<uno::Any> oPrevBelowSpacing;
-    OUString sProp(getPropertyName(PROP_PARA_BOTTOM_MARGIN));
-    if (m_StreamStateStack.top().xPreviousParagraph->getPropertySetInfo()->hasPropertyByName(sProp))
-        oPrevBelowSpacing = m_StreamStateStack.top().xPreviousParagraph->getPropertyValue(sProp);
-
-    if (!oPrevBelowSpacing.has_value() || aBelowSpacing != *oPrevBelowSpacing)
-        m_StreamStateStack.top().xPreviousParagraph->setPropertyValue(sProp, aBelowSpacing);
+    // store this section's unused belowSpacing so that emulation can use it later
+    pSectionContext->SetBelowSpacing(nBelowSpacing);
 }
 
 OUString DomainMapper_Impl::getFontNameForTheme(const Id id)

sw/source/writerfilter/dmapper/PropertyMap.hxx
@@ -304,6 +304,10 @@ private:
     sal_Int32                                       m_nPaperSourceFirst;
     sal_Int32                                       m_nPaperSourceOther;
 
+    // Although an empty sectPr's spacing doesn't directly affect layout,
+    // below spacing is needed to consolidate/collapse the top spacing of the following paragraph.
+    std::optional<sal_Int32> m_oBelowSpacing;
+
     bool                                            m_bDynamicHeightTop;
     bool                                            m_bDynamicHeightBottom;
 
@@ -414,6 +418,9 @@ public:
     void SetLnnMin( sal_Int32 nValue ) { m_nLnnMin = nValue; }
 
     void SetPaperSource(sal_Int32 first, sal_Int32 other) { m_nPaperSourceFirst = first; m_nPaperSourceOther = other;}
+
+    const std::optional<sal_Int32>& GetBelowSpacing() const { return m_oBelowSpacing; }
+    void SetBelowSpacing(sal_Int32 nSet) { m_oBelowSpacing = nSet; }
 
     void addRelativeWidthShape( const css::uno::Reference<css::drawing::XShape>& xShape ) { m_xRelativeWidthShapes.push_back( xShape ); }
 

commit 582c032aaa4524abf5b5a630400cf7ecdc2acf8a

tdf#170119 writerfilter: cont-sectPr w/ pgbrk == emulate bottomSpacing
Technically this is a 25.2.6 regression from my
commit 74c29345a7c179b048c157582a1145e381616e5c
tdf#167657 writerfilter: only move sectPr bottomMargin after pageBreak
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/188298

It was just lucky before that it worked for that particular instance,
and it only worked since 25.2.4...

make CppunitTest_sw_ooxmlexport25 \
    CPPUNIT_TEST_NAME=testTdf170119_bottomSpacing

Change-Id: I9d0ae90087d79887489357eaa00eeabe53bcb58c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196205
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/writerfilter/dmapper/PropertyMap.cxx
@@ -1560,6 +1560,22 @@ void SectionPropertyMap::EmulateSectPrBelowSpacing(DomainMapper_Impl& rDM_Impl)
     // to the last paragraph in that section.
     // This emulation works because below spacing before a page break normally has no relevance.
 
+    if (m_nBreakType == NS_ooxml::LN_Value_ST_SectionMark_continuous)
+    {
+        // The complication with continuous breaks is that the below spacing of a sectPr
+        // does NOT directly affect the layout.
+        // [So (except before a page break) it MUST NOT be applied to the previous para.]
+        // However, it IS (indirectly) used to reduce the top margin of the following paragraph.
+        uno::Reference<beans::XPropertySet> const xPSet(m_xStartingRange, uno::UNO_QUERY);
+        if (!xPSet)
+            return; // TODO tdf#170119: emulation not possible without a page break
+
+        style::BreakType eBreakType(style::BreakType_NONE);
+        xPSet->getPropertyValue(u"BreakType"_ustr) >>= eBreakType;
+        if (eBreakType != style::BreakType_PAGE_BEFORE)
+            return; // emulation not possible without a page break.
+    }
+
     // m_xPreStartingRange may have skipped over a table as the last thing before the break!
     // If so, then the below spacing can be ignored since tables don't have below spacing.
     // Also, if m_xStartingRange starts with a table (which also doesn't have above spacing)
@@ -1690,6 +1706,9 @@ void SectionPropertyMap::CloseSectionGroup( DomainMapper_Impl& rDM_Impl )
             setHeaderFooterProperties(rDM_Impl);
             InheritOrFinalizePageStyles( rDM_Impl );
             ApplySectionProperties( xSection, rDM_Impl );  //depends on InheritOrFinalizePageStyles
+
+            EmulateSectPrBelowSpacing(rDM_Impl);
+
             uno::Reference< beans::XPropertySet > xRangeProperties( lcl_GetRangeProperties( m_bIsFirstSection, rDM_Impl, m_xStartingRange ) );
             if ( m_bIsFirstSection && !m_sPageStyleName.isEmpty() && xRangeProperties.is() )
             {

sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
@@ -107,6 +107,14 @@ DECLARE_OOXMLEXPORT_TEST(testTdf170003_bottomSpacing, "tdf170003_bottomSpacing.d
                          getProperty<sal_Int32>(getParagraph(1), u"ParaBottomMargin"_ustr));
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf170119_bottomSpacing, "tdf170119_bottomSpacing.docx")
+{
+    // Given a document with a page break and a sectPr with a huge bottom spacing
+
+    // Without the fix, page 2 started with a 150pt gap, pushing content to the third page.
+    CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf167657_sectPr_bottomSpacing, "tdf167657_sectPr_bottomSpacing.docx")
 {
     // given with a continuous break sectPr with no belowSpacing