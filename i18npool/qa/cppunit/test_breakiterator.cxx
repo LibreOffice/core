LibreOffice/core

commit 44699b3de37f07090ac6fee1cd97aa76036e9700

tdf#49885 BreakIterator rule upgrades
This change re-bases the BreakIterator rule customizations on top of a
clean copy of the ICU 74.2 rules.

Change-Id: Iadcf16cab138cc6c869fac61ad64e996e65b5ae4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/166273
Tested-by: Jenkins
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

i18npool/CustomTarget_breakiterator.mk
@@ -16,16 +16,12 @@ $(call gb_CustomTarget_get_target,i18npool/breakiterator) : \
 
 i18npool_BRKTXTS := \
     count_word.brk \
-    $(call gb_Helper_optional_locale,he,dict_word_he.brk) \
     $(call gb_Helper_optional_locale,hu,dict_word_hu.brk) \
-    dict_word_nodash.brk \
     dict_word_prepostdash.brk \
     dict_word.brk \
-    $(call gb_Helper_optional_locale,he,edit_word_he.brk) \
     $(call gb_Helper_optional_locale,hu,edit_word_hu.brk) \
     edit_word.brk \
-    line.brk \
-    sent.brk
+    line.brk
 
 # 'gencmn', 'genbrk' and 'genccode' are tools generated and delivered by icu project to process icu breakiterator rules.
 # The output of gencmn generates warnings under Windows. We want to minimize the patches to external tools,

commit 167bbe31c0620d6ca1c4640a81f2e759f8f65e6a

tdf#162514 i18npool: apply fix for Hungarian abbreviations, too
Restores Hungarian abbreviation handling to spell checking by
applying the fix for dict_word_hu.txt.

Regression from commit 44699b3de37f07090ac6fee1cd97aa76036e9700
"tdf#49885 BreakIterator rule upgrades".

Follow-up to commit f4fe6df6aa92573368c3fa0edb9fd03e64d9d059
"tdf#162514 i18npool: Handle abbreviations in dictionary breakiterator".

Change-Id: I83e30c831759ae896f1db2da697287b8c4dcd26b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196224
Tested-by: Jenkins
Reviewed-by: László Németh <nemeth@numbertext.org>

i18npool/qa/cppunit/test_breakiterator.cxx
@@ -46,6 +46,7 @@ public:
     void testKorean();
 
     void testDictWordAbbreviation();
+    void testDictWordAbbreviationHU();
     void testDictWordPrepostDash();
     void testHebrewGereshGershaim();
     void testLegacySurrogatePairs();
@@ -69,6 +70,7 @@ public:
     CPPUNIT_TEST(testChinese);
     CPPUNIT_TEST(testKorean);
     CPPUNIT_TEST(testDictWordAbbreviation);
+    CPPUNIT_TEST(testDictWordAbbreviationHU);
     CPPUNIT_TEST(testDictWordPrepostDash);
     CPPUNIT_TEST(testHebrewGereshGershaim);
     CPPUNIT_TEST(testLegacySurrogatePairs);
@@ -1831,6 +1833,38 @@ void TestBreakIterator::testDictWordAbbreviation()
             = m_xBreak->getWordBoundary(aTest, 30, rLocale, i18n::WordType::DICTIONARY_WORD, false);
         CPPUNIT_ASSERT_EQUAL(sal_Int32(29), aBounds.startPos);
         CPPUNIT_ASSERT_EQUAL(sal_Int32(33), aBounds.endPos);
+    }
+}
+
+void TestBreakIterator::testDictWordAbbreviationHU()
+{
+    std::vector<lang::Locale> aLocale{
+        { "hu", "HU", "" } // dict_word_hu locale
+    };
+
+    for (const auto& rLocale : aLocale)
+    {
+        auto aTest = u"Pl. stb. dr.-ral Mo.-gal"_ustr;
+
+        i18n::Boundary aBounds
+            = m_xBreak->getWordBoundary(aTest, 1, rLocale, i18n::WordType::DICTIONARY_WORD, false);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), aBounds.startPos);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(3), aBounds.endPos);
+
+        aBounds
+            = m_xBreak->getWordBoundary(aTest, 4, rLocale, i18n::WordType::DICTIONARY_WORD, false);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(4), aBounds.startPos);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(8), aBounds.endPos);
+
+        aBounds
+            = m_xBreak->getWordBoundary(aTest, 9, rLocale, i18n::WordType::DICTIONARY_WORD, false);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(9), aBounds.startPos);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(16), aBounds.endPos);
+
+        aBounds
+            = m_xBreak->getWordBoundary(aTest, 17, rLocale, i18n::WordType::DICTIONARY_WORD, false);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(17), aBounds.startPos);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(24), aBounds.endPos);
     }
 }
 