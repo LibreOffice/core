808b88b88b6e7fcddf100e473416e33d4145cfcb
Karthik Godha <karthik.godha@collabora.com>
2025-12-28 20:50:43 +0530

    tdf#170153: PPTX->PPTX handle ".bin" media file
    Media files are exported based on file extensions. ".bin" file extension
    is not handled.
    
    Change-Id: Ib7af829e6a5bd042c59edcef9d09b6c5d4fb80a3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196267
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit b3e0324dcc240a08ba75ce7916e5e1b5dee82295)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198173
    Tested-by: Jenkins

--- oox/source/drawingml/graphicshapecontext.cxx
+++ oox/source/drawingml/graphicshapecontext.cxx
@@ -87,6 +87,7 @@ ContextHandlerRef GraphicShapeContext::onCreateContext( sal_Int32 aElementToken,
             {
                 mpShapePtr->getGraphicProperties().m_xMediaStream = std::move(xMediaStream);
                 mpShapePtr->getGraphicProperties().m_sMediaPackageURL = lcl_GetMediaReference(path);
+                mpShapePtr->getGraphicProperties().m_sMediaMimeType = "audio/unknown";
             }
         }
         break;
@@ -113,6 +114,11 @@ ContextHandlerRef GraphicShapeContext::onCreateContext( sal_Int32 aElementToken,
                     mpShapePtr->getGraphicProperties().m_sMediaPackageURL
                         = getFilter().getAbsoluteUrl(rPath);
             }
+
+            if (getBaseToken(aElementToken) == XML_audioFile)
+                mpShapePtr->getGraphicProperties().m_sMediaMimeType = "audio/unknown";
+            else
+                mpShapePtr->getGraphicProperties().m_sMediaMimeType = "video/unknown";
         }
         break;
     }
