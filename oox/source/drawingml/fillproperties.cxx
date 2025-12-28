b3e0324dcc240a08ba75ce7916e5e1b5dee82295
Karthik Godha <karthik.godha@collabora.com>
2025-12-28 20:50:43 +0530

    tdf#170153: PPTX->PPTX handle ".bin" media file
    Media files are exported based on file extensions. ".bin" file extension
    is not handled.
    
    Change-Id: Ib7af829e6a5bd042c59edcef9d09b6c5d4fb80a3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196267
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- oox/source/drawingml/fillproperties.cxx
+++ oox/source/drawingml/fillproperties.cxx
@@ -918,6 +918,11 @@ void GraphicProperties::pushToPropMap( PropertyMap& rPropMap, const GraphicHelpe
         if (m_xMediaStream.is())
             rPropMap.setProperty(PROP_PrivateStream, m_xMediaStream);
     }
+    // Media Type
+    if (!m_sMediaMimeType.isEmpty())
+    {
+        rPropMap.setProperty(PROP_MediaMimeType, m_sMediaMimeType);
+    }
 }
 
 bool ArtisticEffectProperties::isEmpty() const
