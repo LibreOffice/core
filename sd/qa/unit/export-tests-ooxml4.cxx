LibreOffice/core

commit ef32ba978d7be844440ee0d9c78ad37128b35512

Related: tdf#111927 - PPTX: fix placeholder title text becomes small
after clicking in&out

Follow-up after a7a02a742237d739e7b6b25fcfb9dbe2d4675c28

Wrong  styles were applied to the different Outline (1, 2, ...) levels
during ooxml import.

Should also the Title, Subtitle, Outliner placeholders properties saved
in LstStyles as OOXML standard requires.

Even if we have no LstStyles in the xml, still need to apply
the char/para properties to the placeholder styles.

Change-Id: I5086f706fd8dc2704a439a2e410d74db2656d09b
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190360
Tested-by: Jenkins
Reviewed-by: Balazs Varga <balazs.varga@collabora.com>
(cherry picked from commit 629105991ee77335f856d746ef44408414cc150a)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190495
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -2482,7 +2482,9 @@ ShapeExport& PowerPointShapeExport::WritePlaceholderShape(const Reference< XShap
     }
     mpFS->endElementNS(XML_p, XML_spPr);
 
-    WriteTextBox(xShape, XML_p, /*bWritePropertiesAsLstStyles=*/bUsePlaceholderIndex);
+    bool bWritePropertiesAsLstStyles = bUsePlaceholderIndex || ePlaceholder == Title
+                                       || ePlaceholder == Subtitle || ePlaceholder == Outliner;
+    WriteTextBox(xShape, XML_p, bWritePropertiesAsLstStyles);
 
     mpFS->endElementNS(XML_p, XML_sp);
 

sd/qa/unit/export-tests-ooxml4.cxx
@@ -27,6 +27,7 @@
 #include <com/sun/star/drawing/TextHorizontalAdjust.hpp>
 #include <com/sun/star/lang/IndexOutOfBoundsException.hpp>
 #include <com/sun/star/lang/Locale.hpp>
+#include <com/sun/star/style/ParagraphAdjust.hpp>
 #include <com/sun/star/text/GraphicCrop.hpp>
 #include <com/sun/star/text/WritingMode2.hpp>
 
@@ -1436,6 +1437,85 @@ CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testCustomPromptTexts)
         CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong placeholder text was set!", aText,
                                      u"Text placeholder"_ustr);
     }
+}
+
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testPresObjTextStyles)
+{
+    createSdImpressDoc("pptx/tdf163239_v2.pptx");
+    saveAndReload(u"Impress Office Open XML"_ustr);
+
+    uno::Reference<style::XStyleFamiliesSupplier> xStyleFamiliesSupplier(mxComponent,
+                                                                         uno::UNO_QUERY);
+    uno::Reference<container::XNameAccess> xStyleFamilies
+        = xStyleFamiliesSupplier->getStyleFamilies();
+    // 1st slide
+    uno::Reference<container::XNameAccess> xStyleFamily(
+        xStyleFamilies->getByName(u"Title Slide"_ustr), uno::UNO_QUERY);
+    {
+        // Title style
+        uno::Reference<style::XStyle> xStyle(xStyleFamily->getByName(u"title"_ustr),
+                                             uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> xPropSet(xStyle, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(60.0f, xPropSet->getPropertyValue(u"CharHeight"_ustr).get<float>());
+        CPPUNIT_ASSERT_EQUAL(sal_Int16(style::ParagraphAdjust_CENTER),
+                             xPropSet->getPropertyValue(u"ParaAdjust"_ustr).get<sal_Int16>());
+        CPPUNIT_ASSERT_EQUAL(u"Marianne"_ustr,
+                             xPropSet->getPropertyValue(u"CharFontName"_ustr).get<OUString>());
+    }
+    // 2nd slide
+    xStyleFamily.set(xStyleFamilies->getByName(u"Title and Content"_ustr), uno::UNO_QUERY);
+    {
+        // Title style
+        uno::Reference<style::XStyle> xStyle(xStyleFamily->getByName(u"title"_ustr),
+                                             uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> xPropSet(xStyle, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(44.0f, xPropSet->getPropertyValue(u"CharHeight"_ustr).get<float>());
+        CPPUNIT_ASSERT_EQUAL(sal_Int16(style::ParagraphAdjust_LEFT),
+                             xPropSet->getPropertyValue(u"ParaAdjust"_ustr).get<sal_Int16>());
+        CPPUNIT_ASSERT_EQUAL(u"Marianne"_ustr,
+                             xPropSet->getPropertyValue(u"CharFontName"_ustr).get<OUString>());
+
+        // outline1 style
+        xStyle.set(xStyleFamily->getByName(u"outline1"_ustr), uno::UNO_QUERY);
+        xPropSet.set(xStyle, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(28.0f, xPropSet->getPropertyValue(u"CharHeight"_ustr).get<float>());
+        CPPUNIT_ASSERT_EQUAL(sal_Int16(style::ParagraphAdjust_LEFT),
+                             xPropSet->getPropertyValue(u"ParaAdjust"_ustr).get<sal_Int16>());
+        CPPUNIT_ASSERT_EQUAL(u"Marianne"_ustr,
+                             xPropSet->getPropertyValue(u"CharFontName"_ustr).get<OUString>());
+
+        // outline2 style
+        xStyle.set(xStyleFamily->getByName(u"outline2"_ustr), uno::UNO_QUERY);
+        xPropSet.set(xStyle, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(24.0f, xPropSet->getPropertyValue(u"CharHeight"_ustr).get<float>());
+        CPPUNIT_ASSERT_EQUAL(sal_Int16(style::ParagraphAdjust_LEFT),
+                             xPropSet->getPropertyValue(u"ParaAdjust"_ustr).get<sal_Int16>());
+        CPPUNIT_ASSERT_EQUAL(u"Marianne"_ustr,
+                             xPropSet->getPropertyValue(u"CharFontName"_ustr).get<OUString>());
+
+        // outline5 style
+        xStyle.set(xStyleFamily->getByName(u"outline5"_ustr), uno::UNO_QUERY);
+        xPropSet.set(xStyle, uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(18.0f, xPropSet->getPropertyValue(u"CharHeight"_ustr).get<float>());
+        CPPUNIT_ASSERT_EQUAL(sal_Int16(style::ParagraphAdjust_LEFT),
+                             xPropSet->getPropertyValue(u"ParaAdjust"_ustr).get<sal_Int16>());
+        CPPUNIT_ASSERT_EQUAL(u"Marianne"_ustr,
+                             xPropSet->getPropertyValue(u"CharFontName"_ustr).get<OUString>());
+    }
+}
+
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testTextStylesXML)
+{
+    createSdImpressDoc("pptx/tdf163239_v2.pptx");
+    save(u"Impress Office Open XML"_ustr);
+
+    xmlDocUniquePtr pXmlDocRels = parseExport(u"ppt/slideLayouts/slideLayout1.xml"_ustr);
+
+    assertXPath(pXmlDocRels, "/p:sldLayout/p:cSld/p:spTree/p:sp[1]/p:txBody/a:lstStyle/a:lvl1pPr",
+                "algn", u"ctr");
+    assertXPath(pXmlDocRels,
+                "/p:sldLayout/p:cSld/p:spTree/p:sp[1]/p:txBody/a:lstStyle/a:lvl1pPr/a:defRPr", "sz",
+                u"6000");
 }
 
 CPPUNIT_PLUGIN_IMPLEMENT();

oox/inc/drawingml/textbody.hxx
@@ -101,7 +101,7 @@ public:
                             const ::oox::core::XmlFilterBase& rFilterBase,
                             const css::uno::Reference< css::style::XStyle > & aXStyle,
                             const TextCharacterProperties& rTextStyleProperties,
-                            const TextListStylePtr& pMasterTextListStylePtr) const;
+                            const TextListStylePtr& pMasterTextListStylePtr, size_t nLevel) const;
 private:
     TextParagraphVector maParagraphs;
     TextBodyProperties  maTextProperties;

oox/source/drawingml/textbody.cxx
@@ -171,16 +171,16 @@ void TextBody::ApplyMasterTextStyle(
     const ::oox::core::XmlFilterBase& rFilterBase,
     const css::uno::Reference< css::style::XStyle >& aXStyle,
     const TextCharacterProperties& rTextStyleProperties,
-    const TextListStylePtr& pMasterTextListStylePtr) const
+    const TextListStylePtr& pMasterTextListStylePtr, size_t nLevel) const
 {
     assert(!isEmpty());
 
-    if (maParagraphs.empty())
+    if (maParagraphs.empty() || maParagraphs.size() <= nLevel)
         return;
 
     // Apply character properties
     PropertySet aPropSet(aXStyle);
-    TextCharacterProperties aTextCharacterProps(maParagraphs[0]->getCharacterStyle(
+    TextCharacterProperties aTextCharacterProps(maParagraphs[nLevel]->getCharacterStyle(
         rTextStyleProperties, *pMasterTextListStylePtr, maTextListStyle));
     aTextCharacterProps.pushToPropSet(aPropSet, rFilterBase);
 
@@ -189,7 +189,7 @@ void TextBody::ApplyMasterTextStyle(
     aCombinedTextStyle.apply(*pMasterTextListStylePtr);
     aCombinedTextStyle.apply(maTextListStyle);
 
-    TextParagraphProperties* pTextParagraphStyle = maParagraphs[0]->getParagraphStyle(aCombinedTextStyle);
+    TextParagraphProperties* pTextParagraphStyle = maParagraphs[nLevel]->getParagraphStyle(aCombinedTextStyle);
     if (pTextParagraphStyle)
     {
         Reference< XPropertySet > xProps(aXStyle, UNO_QUERY_THROW);

oox/source/drawingml/textparagraph.cxx
@@ -63,6 +63,7 @@ TextCharacterProperties TextParagraph::getCharacterStyle (
     if (pTextParagraphStyle)
         aTextCharacterStyle.assignUsed(pTextParagraphStyle->getTextCharacterProperties());
     aTextCharacterStyle.assignUsed(maProperties.getTextCharacterProperties());
+    aTextCharacterStyle.assignUsed(maEndProperties);
     return aTextCharacterStyle;
 }
 

oox/source/ppt/pptshape.cxx
@@ -198,7 +198,29 @@ void PPTShape::setTextMasterStyles( const SlidePersist& rSlidePersist, const oox
             if (xFamilies->getByName(aStyle) >>= aXStyle)
             {
                 TextCharacterProperties aCharStyleProperties;
-                getTextBody()->ApplyMasterTextStyle(rFilterBase, aXStyle, aCharStyleProperties, mpMasterTextListStyle);
+                getTextBody()->ApplyMasterTextStyle(rFilterBase, aXStyle, aCharStyleProperties,
+                                                    mpMasterTextListStyle, size_t(0));
+                if (aStyle.equals(u"outline1"_ustr)/* BodyStyle */)
+                {
+                    for (size_t nLevel = 2; nLevel < 10; nLevel++) // outline2 ... outline9
+                    {
+                        char pOutline[9] = "outline1";
+                        pOutline[7] = static_cast<char>('0' + nLevel);
+                        OUString sOutlineStyle(OUString::createFromAscii(pOutline));
+                        if (xFamilies->hasByName(sOutlineStyle))
+                        {
+                            if (aXStyle.is())
+                                aXStyle.clear();
+
+                            xFamilies->getByName(sOutlineStyle) >>= aXStyle;
+                            if (aXStyle.is())
+                            {
+                                TextCharacterProperties aCharStyleLvlProps;
+                                getTextBody()->ApplyMasterTextStyle(rFilterBase, aXStyle, aCharStyleLvlProps, mpMasterTextListStyle, nLevel - 1);
+                            }
+                        }
+                    }
+                }
             }
         }
     }

commit 415c7e04dc6a4a1638a63bf7195c8319b1cb8f85

tdf#170166: sd ooxml export: fix bullets in subtitle presobj
Properly export of Subtitles as presObj was missed with
this commit: 238cfa9efa21f08514703ea6cc181ce02d8feb12
Such as missing '<a:buNone/>' attributes in slides/slide1.xml

But still keep not to export subtitle presboj into masterslide.xml
which still would cause corrupt xml when opening in mso.

regression after: ef32ba978d7be844440ee0d9c78ad37128b35512

Change-Id: I4f3e069ad1ade5bbcfa50f9096539c882c66e0b6
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196308
Tested-by: Jenkins
Reviewed-by: Aron Budea <aron.budea@collabora.com>

oox/source/export/shapes.cxx
@@ -2189,7 +2189,7 @@ constexpr auto constMap = frozen::make_unordered_map<std::u16string_view, ShapeC
     { u"com.sun.star.presentation.OutlinerShape", &ShapeExport::WriteTextShape },
     { u"com.sun.star.presentation.SlideNumberShape", &ShapeExport::WriteTextShape },
     { u"com.sun.star.presentation.TitleTextShape", &ShapeExport::WriteTextShape },
-    //{ u"com.sun.star.presentation.SubtitleShape", &ShapeExport::WriteTextShape }, TODO: handle subtitle shape: see tdf#112557 workaround
+    { u"com.sun.star.presentation.SubtitleShape", &ShapeExport::WriteTextShape }
 });
 
 } // end anonymous namespace

sd/source/filter/eppt/pptx-epptooxml.cxx
@@ -355,12 +355,16 @@ ShapeExport& PowerPointShapeExport::WriteTextShape(const Reference< XShape >& xS
         if (!WritePlaceholder(xShape, Title, mbMaster))
             ShapeExport::WriteTextShape(xShape);
     }
-    /*else if (sShapeType == "com.sun.star.presentation.SubtitleShape")
+    else if (sShapeType == "com.sun.star.presentation.SubtitleShape")
     {
-        TODO: handle subtitle shape: see tdf#112557 workaround
-        if (!WritePlaceholder(xShape, Subtitle, mbMaster))
-            ShapeExport::WriteTextShape(xShape);
-    }*/
+        // TODO: handle subtitle shape: see tdf#112557 workaround
+        // MSO does not like subtitles on master slides
+        if (mePageType != MASTER)
+        {
+            if (!WritePlaceholder(xShape, Subtitle, mbMaster))
+                ShapeExport::WriteTextShape(xShape);
+        }
+    }
     else
         SAL_WARN("sd.eppt", "PowerPointShapeExport::WriteTextShape: shape of type '" << sShapeType << "' is ignored");
 
@@ -376,14 +380,6 @@ ShapeExport& PowerPointShapeExport::WriteUnknownShape(const Reference< XShape >&
     if (sShapeType == "com.sun.star.presentation.PageShape")
     {
         WritePageShape(xShape, mePageType, mrExport.GetPresObj());
-    }
-    else if (sShapeType == "com.sun.star.presentation.SubtitleShape")
-    {
-        if(mePageType != MASTER)
-        {
-            if (!WritePlaceholder(xShape, Subtitle, mbMaster))
-                ShapeExport::WriteTextShape(xShape);
-        }
     }
     else
         SAL_WARN("sd.eppt", "unknown shape not handled: " << sShapeType.toUtf8());

sd/qa/unit/export-tests-ooxml4.cxx
@@ -1645,6 +1645,27 @@ CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testTextAlignLeft)
     assertXPath(pXmlDocRels, "/p:sld/p:cSld/p:spTree/p:sp[2]/p:txBody/a:p/a:pPr", "algn", u"l");
 }
 
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testSubtitleNoBullets)
+{
+    createSdImpressDoc("odp/tdf170166.odp");
+    saveAndReload(TestFilter::PPTX);
+
+    const SdrPage* pPage1 = GetPage(1);
+    {
+        // subtitle placeholder object
+        SdrTextObj* pTxtObj = DynCastSdrTextObj(pPage1->GetObj(0));
+        CPPUNIT_ASSERT_MESSAGE("no text object", pTxtObj != nullptr);
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Wrong object type!", SdrObjKind::Text,
+                                     pTxtObj->GetObjIdentifier());
+        const EditTextObject& aEdit = pTxtObj->GetOutlinerParaObject()->GetTextObject();
+        const SvxNumBulletItem* pNumFmt = aEdit.GetParaAttribs(0).GetItem(EE_PARA_NUMBULLET);
+        // Without a fix, it will fail with numbering type: SVX_NUM_CHAR_SPECIAL
+        CPPUNIT_ASSERT(pNumFmt);
+        CPPUNIT_ASSERT_EQUAL_MESSAGE("Bullet's numbering type is wrong!", SVX_NUM_NUMBER_NONE,
+                                     pNumFmt->GetNumRule().GetLevel(0).GetNumberingType());
+    }
+}
+
 CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testOLEObjectAnimationTarget)
 {
     createSdImpressDoc("pptx/tdf169088.pptx");