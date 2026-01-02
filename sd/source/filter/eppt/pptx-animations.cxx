f3943837d75695bc351a55340addd362de33097b
Aron Budea <aron.budea@collabora.com>
2026-01-02 21:50:24 +1100

    tdf#170202 avoid exporting ANIMATIONPHYSICS node types
    Implement cbf102d4c1ba73519b49df0d43a87ebdf9267538 differently.
    
    After previous commit, tdf136301-1.odp still contained
    animation elements in slide1.xml when converted to PPTX, and
    couldn't be opened in PP.
    
    Set such nodes invalid instead to avoid processing them at all.
    
    Change-Id: I799026c389681b89792bb9765b8f81562743deb4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196424
    Reviewed-by: Aron Budea <aron.budea@collabora.com>
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sd/source/filter/eppt/pptx-animations.cxx
+++ sd/source/filter/eppt/pptx-animations.cxx
@@ -951,28 +951,18 @@ void PPTXAnimationExport::WriteAnimationNodeCommonPropsStart()
     if (!aChildNodes.empty())
     {
         bool bSubTnLst = false;
-        bool bWroteChildTnList = false;
+        mpFS->startElementNS(XML_p, XML_childTnLst);
         for (const NodeContextPtr& pChildContext : aChildNodes)
         {
             if (pChildContext->isValid())
             {
                 if (pChildContext->isOnSubTnLst())
                     bSubTnLst = true;
-                else if (pChildContext->getNode()->getType() == AnimationNodeType::ANIMATEPHYSICS)
-                    ; // ignore we don't support exporting this node type
                 else
-                {
-                    if (!bWroteChildTnList)
-                    {
-                        bWroteChildTnList = true;
-                        mpFS->startElementNS(XML_p, XML_childTnLst);
-                    }
                     WriteAnimationNode(pChildContext);
-                }
             }
         }
-        if (bWroteChildTnList)
-            mpFS->endElementNS(XML_p, XML_childTnLst);
+        mpFS->endElementNS(XML_p, XML_childTnLst);
 
         if (bSubTnLst)
         {
