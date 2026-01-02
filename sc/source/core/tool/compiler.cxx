9eed2e4f491cb4f5432157eec77e6f5b525acb8f
Karthik Godha <karthik.godha@collabora.com>
2026-01-02 17:49:44 +0530

    tdf#170201: XLSX - empty values in array formulas
    LO supports empty values in array formulas but Excel doesn't. Export
    '0' when an array formula contains an empty value.
    
    Change-Id: I5f289c0bf3f98cb75dffa925245afab67641eac2
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196421
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -5436,7 +5436,7 @@ void ScCompiler::CreateStringFromMatrix( OUStringBuffer& rBuffer, const FormulaT
                 }
             }
             else if( pMatrix->IsEmpty( nC, nR ) )
-                ;
+                AppendDouble(rBuffer, 0);
             else if( pMatrix->IsStringOrEmpty( nC, nR ) )
                 AppendString( rBuffer, pMatrix->GetString(nC, nR).getString() );
         }
