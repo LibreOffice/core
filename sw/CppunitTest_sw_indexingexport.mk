2db6dcb790c52a8d9c18e394399c7c5de8ea9120
Noel Grandin <noel.grandin@collabora.co.uk>
2026-01-06 12:29:49 +0200

    CppunitTest_sw_indexingexport: gb_CppunitTest_set_non_application_font_use
    Change-Id: I3948d343c63771109bfc926d964126e3cc630f23
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196613
    Tested-by: Jenkins
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    (cherry picked from commit 2e85a9cdb1ffc08685140d5075497560ccd582b3)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/203103
    Tested-by: Noel Grandin <noel.grandin@collabora.co.uk>

--- sw/CppunitTest_sw_indexingexport.mk
+++ sw/CppunitTest_sw_indexingexport.mk
@@ -64,4 +64,8 @@ $(eval $(call gb_CppunitTest_use_vcl,sw_indexingexport))
 $(eval $(call gb_CppunitTest_use_rdb,sw_indexingexport,services))
 $(eval $(call gb_CppunitTest_use_configuration,sw_indexingexport))
 
+ifneq ($(filter MORE_FONTS,$(BUILD_TYPE)),)
+$(eval $(call gb_CppunitTest_set_non_application_font_use,sw_indexingexport,abort))
+endif
+
 # vim: set noet sw=4 ts=4:
