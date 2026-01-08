LibreOffice/core

commit e725c5b9327424fb927ed251d54b6913ae9d8568

Table Style improvements: ooxml export of Total Row
attributes. XML_totalsRowLabel, XML_totalsRowFunction,
XML_totalsRowFormula and XML_totalsRowShown.

cherry-pick from: dbe65f04327924e0d3c7bcefab5bbfba07687033

Change-Id: I2b7c356cf71abe940ddbe6672c8fdb52d2513f79
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193206
Reviewed-by: Balazs Varga <balazs.varga@collabora.com>
Tested-by: Balazs Varga <balazs.varga@collabora.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193682
Tested-by: Andras Timar <andras.timar@collabora.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>

sc/source/filter/excel/xedbdata.cxx
@@ -189,6 +189,18 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
     if (hasTableTypeAttr)
         tableType = rData.GetTableType();
 
+    const std::vector<TableColumnAttributes> aTotalValues
+        = rData.GetTotalRowAttributes(formula::FormulaGrammar::GRAM_OOXML);
+
+    // if the Total row have ever been showed it will be true
+    bool hasAnySetValue = std::any_of(aTotalValues.begin(), aTotalValues.end(),
+                                      [](const TableColumnAttributes& attr)
+                                      {
+                                          return attr.maTotalsRowLabel.has_value()
+                                                 || attr.maTotalsFunction.has_value()
+                                                 || attr.maCustomFunction.has_value();
+                                      });
+
     pTableStrm->startElement( XML_table,
         XML_xmlns, rStrm.getNamespaceURL(OOX_NS(xls)).toUtf8(),
         XML_id, OString::number( rEntry.mnTableId),
@@ -198,7 +210,7 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
         XML_tableType, tableType,
         XML_headerRowCount, ToPsz10(rData.HasHeader()),
         XML_totalsRowCount, ToPsz10(rData.HasTotals()),
-        XML_totalsRowShown, ToPsz10(rData.HasTotals())  // we don't support that but if there are totals they are shown
+        XML_totalsRowShown, ToPsz10(hasAnySetValue)
         // OOXTODO: XML_comment, ...,
         // OOXTODO: XML_connectionId, ...,
         // OOXTODO: XML_dataCellStyle, ...,
@@ -233,6 +245,10 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
     const std::vector< TableColumnModel >& rTableColumnModel = rData.GetTableColumnModel();
     if (!rColNames.empty())
     {
+        // rColNames and aTotalValues size should always be equal
+        assert((rColNames.size() == aTotalValues.size()) &&
+               "XclExpTables::SaveTableXml - mismatch between column names and total values");
+
         pTableStrm->startElement(XML_tableColumns,
                 XML_count, OString::number(aRange.aEnd.Col() - aRange.aStart.Col() + 1));
 
@@ -241,8 +257,6 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
             // OOXTODO: write <calculatedColumnFormula> once we support it, in
             // which case we'd need start/endElement XML_tableColumn for such
             // column.
-
-            // OOXTODO: write <totalsRowFormula> once we support it.
 
             std::optional<OUString> uniqueName = std::nullopt;
 
@@ -259,9 +273,9 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
             pTableStrm->startElement( XML_tableColumn,
                     XML_id, OString::number(i+1),
                     XML_uniqueName, uniqueName,
-                    XML_name, rColNames[i].toUtf8()
-                    // XML_totalsRowLabel, (i < rColAttributes.size() ? rColAttributes[i].maTotalsRowLabel : std::nullopt),
-                    // XML_totalsRowFunction, (i < rColAttributes.size() ? rColAttributes[i].maTotalsFunction : std::nullopt)
+                    XML_name, rColNames[i].toUtf8(),
+                    XML_totalsRowLabel, (i < aTotalValues.size() ? aTotalValues[i].maTotalsRowLabel : std::nullopt),
+                    XML_totalsRowFunction, (i < aTotalValues.size() ? aTotalValues[i].maTotalsFunction : std::nullopt)
                     // OOXTODO: XML_dataCellStyle, ...,
                     // OOXTODO: XML_dataDxfId, ...,
                     // OOXTODO: XML_headerRowCellStyle, ...,
@@ -286,6 +300,15 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
                 pXmlColumnPrAttrList->add(XML_denormalized, ToPsz10(rXmlColRef->mbDenormalized));
 
                 pTableStrm->singleElement(XML_xmlColumnPr, pXmlColumnPrAttrList);
+            }
+
+            if (i < aTotalValues.size() && aTotalValues[i].maTotalsFunction.has_value()
+                && aTotalValues[i].maTotalsFunction.value() == u"custom")
+            {
+                // write custom functions
+                pTableStrm->startElement(XML_totalsRowFormula);
+                pTableStrm->writeEscaped(aTotalValues[i].maCustomFunction.value());
+                pTableStrm->endElement(XML_totalsRowFormula);
             }
 
             // put </tableColumn>

sc/inc/dbdata.hxx
@@ -36,6 +36,7 @@ class ScDocument;
 struct ScSortParam;
 struct ScQueryParam;
 struct ScSubTotalParam;
+class ScTokenArray;
 
 class SC_DLLPUBLIC ScDatabaseSettingItem final : public SfxPoolItem
 {
@@ -276,10 +277,17 @@ public:
 
     SC_DLLPUBLIC void       GetSubTotalParam(ScSubTotalParam& rSubTotalParam) const;
     SC_DLLPUBLIC void       SetSubTotalParam(const ScSubTotalParam& rSubTotalParam);
-    SC_DLLPUBLIC void       ImportSubTotalParam(ScSubTotalParam& rSubTotalParam,
+
+    // Total row param handling for Table Styles
+    SC_DLLPUBLIC void       ImportTotalRowParam(ScSubTotalParam& rSubTotalParam,
                                                 const std::vector<TableColumnAttributes>& rAttributesVector,
                                                 formula::FormulaGrammar::Grammar eGrammar) const;
-    SC_DLLPUBLIC void       CreateSubTotalParam(ScSubTotalParam& rSubTotalParam) const;
+    SC_DLLPUBLIC void       CreateTotalRowParam(ScSubTotalParam& rSubTotalParam) const;
+
+    SC_DLLPUBLIC std::vector<TableColumnAttributes>
+                            GetTotalRowAttributes(formula::FormulaGrammar::Grammar eGrammar) const;
+
+    OUString    GetSimpleSubTotalFunction(const ScTokenArray* pTokens, SCCOL nCol, SCROW nHeaderRow) const;
 
     void        GetImportParam(ScImportParam& rImportParam) const;
     void        SetImportParam(const ScImportParam& rImportParam);
@@ -315,6 +323,7 @@ public:
     SC_DLLPUBLIC const ScTableStyleParam* GetTableStyleInfo() const;
 
     static ScSubTotalFunc GetSubTotalFuncFromString(std::u16string_view sFunction);
+    static OUString GetStringFromSubTotalFunc(ScSubTotalFunc eFunc);
 
 private:
 

sc/source/core/data/table3.cxx
@@ -2305,9 +2305,9 @@ bool ScTable::DoSubTotals( ScSubTotalParam& rParam )
 bool ScTable::DoSimpleSubTotals( ScSubTotalParam& rParam )
 {
     RowEntry aRowEntry;
-    aRowEntry.nGroupNo = 0;
-    aRowEntry.nSubStartRow = rParam.nRow1 + static_cast<SCROW>(rParam.bHasHeader); // Header
-    aRowEntry.nFuncStart = rParam.nRow1 + static_cast<SCROW>(rParam.bHasHeader); // Header
+    aRowEntry.nGroupNo = 0; // only one group can have
+    //aRowEntry.nSubStartRow = rParam.nRow1 + static_cast<SCROW>(rParam.bHasHeader); // Header
+    //aRowEntry.nFuncStart = rParam.nRow1 + static_cast<SCROW>(rParam.bHasHeader); // Header
     aRowEntry.nDestRow = rParam.nRow2 + 1;
     aRowEntry.nFuncEnd = rParam.nRow2;
 

sc/source/core/tool/dbdata.cxx
@@ -26,6 +26,7 @@
 
 #include <dbdata.hxx>
 #include <compiler.hxx>
+#include <tokenarray.hxx>
 #include <globalnames.hxx>
 #include <refupdat.hxx>
 #include <document.hxx>
@@ -695,7 +696,7 @@ void ScDBData::SetSubTotalParam(const ScSubTotalParam& rSubTotalParam)
     mpSubTotal.reset(new ScSubTotalParam(rSubTotalParam));
 }
 
-void ScDBData::ImportSubTotalParam(ScSubTotalParam& rSubTotalParam,
+void ScDBData::ImportTotalRowParam(ScSubTotalParam& rSubTotalParam,
                                    const std::vector<TableColumnAttributes>& rAttributesVector,
                                    formula::FormulaGrammar::Grammar eGrammar) const
 {
@@ -772,7 +773,7 @@ void ScDBData::ImportSubTotalParam(ScSubTotalParam& rSubTotalParam,
     }
 }
 
-void ScDBData::CreateSubTotalParam(ScSubTotalParam& rSubTotalParam) const
+void ScDBData::CreateTotalRowParam(ScSubTotalParam& rSubTotalParam) const
 {
     rSubTotalParam.bDoSort = false;
     rSubTotalParam.bGroupedBy = false;
@@ -813,6 +814,209 @@ void ScDBData::CreateSubTotalParam(ScSubTotalParam& rSubTotalParam) const
         rSubTotalParam.SetSubLabels(static_cast<sal_uInt16>(0), vColLabels, vColLabels.size());
         rSubTotalParam.SetCustFuncs(static_cast<sal_uInt16>(0), vColFuncs, vColFuncs.size());
     }
+}
+
+std::vector<TableColumnAttributes> ScDBData::GetTotalRowAttributes(formula::FormulaGrammar::Grammar eGrammar) const
+{
+    ScSubTotalParam rParam;
+    GetSubTotalParam(rParam);
+
+    const SCCOL nEntryCount = rParam.nCol2 - rParam.nCol1 + 1; // col count
+    std::vector<TableColumnAttributes> aAttributesVector(nEntryCount);
+    if (nEntryCount > 0)
+    {
+        if (HasTotals())
+        {
+            if (!mpContainer)
+                assert(!"ScDBData::GetTotalRowAttributes - how did we end up here without container?");
+            else
+            {
+                ScDocument& rDoc = mpContainer->GetDocument();
+                ScHorizontalCellIterator aIter(rDoc, nTable, rParam.nCol1, rParam.nRow2,
+                                               rParam.nCol2,
+                                               rParam.nRow2); // Total row only
+                ScRefCellValue* pCell;
+                SCCOL nCol = rParam.nCol1 - 1;
+                SCROW nRow;
+                while ((pCell = aIter.GetNext(nCol, nRow)) != nullptr)
+                {
+                    TableColumnAttributes aNameAttr;
+                    if (pCell->getType() != CELLTYPE_FORMULA)
+                    {
+                        OUString aStr = pCell->getString(&rDoc);
+                        if (!aStr.isEmpty())
+                            aNameAttr.maTotalsRowLabel = aStr;
+                    }
+                    else
+                    {
+                        if (ScFormulaCell* pFC = pCell->getFormula())
+                        {
+                            bool bSubTotal = pFC->IsSubTotal();
+                            ScTokenArray* pTokens = pFC->GetCode();
+                            if (bSubTotal && pTokens)
+                            {
+                                OUString aFunctype = GetSimpleSubTotalFunction(pTokens, nCol, rParam.nRow1);
+                                if (aFunctype != u"custom")
+                                    aNameAttr.maTotalsFunction = aFunctype;
+                                else
+                                    bSubTotal = false; // fallback to custom
+                            }
+
+                            if (!bSubTotal && pTokens)
+                            {
+                                ScAddress aPos(nCol, rParam.nRow2, nTable);
+                                ScCompiler aComp(rDoc, aPos, *pTokens, eGrammar);
+                                OUStringBuffer aBuf;
+                                aComp.CreateStringFromTokenArray(aBuf);
+                                OUString aFormula = aBuf.makeStringAndClear();
+                                aNameAttr.maTotalsFunction = "custom";
+                                aNameAttr.maCustomFunction = aFormula;
+                            }
+                        }
+                    }
+                    SCCOL nPos = nCol - rParam.nCol1;
+                    if (nPos < nEntryCount)
+                        aAttributesVector[nPos] = std::move(aNameAttr);
+                    else
+                        SAL_WARN("sc.core", "ScDBData::GetTotalRowAttributes - invalid attributes/columns");
+                }
+            }
+        }
+        else
+        {
+            const auto& group = rParam.aGroups[0];
+            if (group.nSubLabels > 0)
+            {
+                for (SCCOL nResult = 0; nResult < group.nSubLabels; ++nResult)
+                {
+                    SCCOL nPos = group.collabels(nResult) - rParam.nCol1;
+                    if (nPos < nEntryCount)
+                        aAttributesVector[nPos].maTotalsRowLabel = group.label(nResult);
+                    else
+                        SAL_WARN("sc.core", "ScDBData::GetTotalRowAttributes - invalid attributes/columns");
+                }
+            }
+
+            // insert the formulas
+            if (group.nCustFuncs > 0)
+            {
+                for (SCCOL nResult = 0; nResult < group.nCustFuncs; ++nResult)
+                {
+                    if (ScTokenArray* pTokens = group.custToken(nResult))
+                    {
+                        SCCOL nCol = group.colcust(nResult);
+                        bool bSubTotal = pTokens->HasOpCode(ocSubTotal);
+                        if (bSubTotal)
+                        {
+                            OUString aFunctype = GetSimpleSubTotalFunction(pTokens, nCol, rParam.nRow1);
+                            if (aFunctype != u"custom")
+                            {
+                                SCCOL nPos = nCol - rParam.nCol1;
+                                if (nPos < nEntryCount)
+                                    aAttributesVector[nPos].maTotalsFunction = aFunctype;
+                                else
+                                    SAL_WARN("sc.core", "ScDBData::GetTotalRowAttributes - invalid "
+                                                        "attributes/columns");
+                            }
+                            else
+                                bSubTotal = false; // fallback to custom
+                        }
+
+                        if (!bSubTotal)
+                        {
+                            if (!mpContainer)
+                                assert(!"ScDBData::GetTotalRowAttributes - how did we end up here without container?");
+                            else
+                            {
+                                ScDocument& rDoc = mpContainer->GetDocument();
+                                ScAddress aPos(nCol, rParam.nRow2 + 1, nTable);
+                                ScCompiler aComp(rDoc, aPos, *pTokens, eGrammar);
+                                OUStringBuffer aBuf;
+                                aComp.CreateStringFromTokenArray(aBuf);
+                                OUString aFormula = aBuf.makeStringAndClear();
+
+                                SCCOL nPos = nCol - rParam.nCol1;
+                                if (nPos < nEntryCount)
+                                {
+                                    aAttributesVector[nPos].maTotalsFunction = "custom";
+                                    aAttributesVector[nPos].maCustomFunction = aFormula;
+                                }
+                                else
+                                    SAL_WARN("sc.core", "ScDBData::GetTotalRowAttributes - invalid "
+                                                        "attributes/columns");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return aAttributesVector;
+}
+
+OUString ScDBData::GetSimpleSubTotalFunction(const ScTokenArray* pTokens, SCCOL nCol, SCROW nHeaderRow) const
+{
+    std::vector<std::pair<OpCode, formula::StackVar>> expected
+        = { { ocSubTotal, formula::svByte },  { ocOpen, formula::svSep },
+            { ocPush, formula::svDouble },    { ocSep, formula::svSep },
+            { ocTableRef, formula::svIndex }, { ocTableRefOpen, formula::svSep },
+            { ocPush, formula::svSingleRef }, { ocTableRefClose, formula::svSep },
+            { ocClose, formula::svSep } };
+
+    size_t nIdx = 0;
+    ScSubTotalFunc eSubType = SUBTOTAL_FUNC_NONE;
+    formula::FormulaTokenArrayPlainIterator aIterResult(*pTokens);
+    for (formula::FormulaToken* t = aIterResult.NextNoSpaces(); t; t = aIterResult.NextNoSpaces())
+    {
+        // check for subtotal opcode
+        OpCode eOpCode = t->GetOpCode();
+        formula::StackVar eType = t->GetType();
+
+        if (nIdx < expected.size() && eOpCode == expected[nIdx].first
+            && eType == expected[nIdx].second)
+        {
+            if (nIdx == 2) // { ocPush, formula::svDouble }
+            {
+                sal_Int32 nFunc = static_cast<sal_Int32>(t->GetDouble());
+                if (nFunc > 100.)
+                    nFunc -= 100;
+
+                if (nFunc < 1 || nFunc > 11)
+                {
+                    return u"custom"_ustr;
+                }
+                else
+                    eSubType = static_cast<ScSubTotalFunc>(nFunc);
+            }
+            else if (nIdx == 4) // { ocTableRef, formula::svIndex }
+            {
+                sal_uInt16 nDbIndex = t->GetIndex();
+                if (GetIndex() != nDbIndex)
+                {
+                    return u"custom"_ustr;
+                }
+            }
+            else if (nIdx == 6) // { ocPush, formula::svSingleRef }
+            {
+                const ScSingleRefData* pRef = t->GetSingleRef();
+                if (!(pRef && pRef->Col() == nCol && pRef->Row() == nHeaderRow))
+                {
+                    return u"custom"_ustr;
+                }
+            }
+            else
+            { /*Nothing to do*/
+            }
+
+            ++nIdx;
+        }
+        else
+        {
+            return u"custom"_ustr;
+        }
+    }
+
+    return GetStringFromSubTotalFunc(eSubType);
 }
 
 void ScDBData::GetImportParam(ScImportParam& rImportParam) const
@@ -1347,29 +1551,58 @@ ScSubTotalFunc ScDBData::GetSubTotalFuncFromString(std::u16string_view sFunction
 {
     if (sFunction == u"sum")
         return SUBTOTAL_FUNC_SUM;
-    if (sFunction == u"countNums")
+    else if (sFunction == u"countNums")
         return SUBTOTAL_FUNC_CNT;
-    if (sFunction == u"count")
+    else if (sFunction == u"count")
         return SUBTOTAL_FUNC_CNT2;
-    /*if (sFunction)
+    /*else if (sFunction)
         return SUBTOTAL_FUNC_PROD;*/
-    if (sFunction == u"average")
+    else if (sFunction == u"average")
         return SUBTOTAL_FUNC_AVE;
-    /*if (sFunction)
+    /*else if (sFunction)
         return SUBTOTAL_FUNC_MED;*/
-    if (sFunction == u"max")
+    else if (sFunction == u"max")
         return SUBTOTAL_FUNC_MAX;
-    if (sFunction == u"min")
+    else if (sFunction == u"min")
         return SUBTOTAL_FUNC_MIN;
-    if (sFunction == u"stdDev")
+    else if (sFunction == u"stdDev")
         return SUBTOTAL_FUNC_STD;
-    /*if (sFunction)
+    /*else if (sFunction)
         return SUBTOTAL_FUNC_STDP;*/
-    if (sFunction == u"var")
+    else if (sFunction == u"var")
         return SUBTOTAL_FUNC_VAR;
-    /*if (sFunction)
+    /*else if (sFunction)
         return SUBTOTAL_FUNC_VARP;*/
-    return SUBTOTAL_FUNC_NONE;
+    else
+        return SUBTOTAL_FUNC_NONE;
+}
+
+OUString ScDBData::GetStringFromSubTotalFunc(ScSubTotalFunc eFunc)
+{
+    if (eFunc == SUBTOTAL_FUNC_SUM)
+        return u"sum"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_CNT)
+        return u"countNums"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_CNT2)
+        return u"count"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_PROD)
+        return u"custom"_ustr; // ooxml not support in Total row
+    else if (eFunc == SUBTOTAL_FUNC_AVE)
+        return u"average"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_MED)
+        return u"custom"_ustr; // ooxml not support in Total row
+    else if (eFunc == SUBTOTAL_FUNC_MAX)
+        return u"max"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_MIN)
+        return u"min"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_STD)
+        return u"stdDev"_ustr;
+    else if (eFunc == SUBTOTAL_FUNC_STDP)
+        return u"custom"_ustr; // ooxml not support in Total row
+    else if (eFunc == SUBTOTAL_FUNC_VAR)
+        return u"var"_ustr;
+    else
+        return u"none"_ustr;
 }
 
 namespace {

sc/source/filter/oox/tablecolumnsbuffer.cxx
@@ -161,12 +161,12 @@ bool TableColumns::finalizeImport( ScDBData* pDBData )
         pDBData->SetTableColumnNames( std::move(aNames) );
 
         // Import subtotal parameters for columns
-        if (hasAnySetValue && !pDBData->HasTotals())
+        if (hasAnySetValue)
         {
             ScSubTotalParam aSubTotalParam;
             pDBData->GetSubTotalParam(aSubTotalParam);
             aSubTotalParam.bHasHeader = pDBData->HasHeader();
-            pDBData->ImportSubTotalParam(aSubTotalParam, aAttributes,
+            pDBData->ImportTotalRowParam(aSubTotalParam, aAttributes,
                                          formula::FormulaGrammar::GRAM_OOXML);
             pDBData->SetSubTotalParam(aSubTotalParam);
         }

sc/source/ui/view/gridwin.cxx
@@ -2502,7 +2502,7 @@ void ScGridWindow::MouseButtonUp( const MouseEvent& rMEvt )
                 pDBData->GetSubTotalParam(aSubTotalParam);
                 aSubTotalParam.bHasHeader = aNewDBData.HasHeader();
                 // store current subtotal settings
-                pDBData->CreateSubTotalParam(aSubTotalParam);
+                pDBData->CreateTotalRowParam(aSubTotalParam);
                 aNewDBData.SetSubTotalParam(aSubTotalParam);
                 // add/replace total row
                 aSubTotalParam.bRemoveOnly = false;

sc/source/ui/view/tableshell.cxx
@@ -102,10 +102,9 @@ void ScTableShell::ExecuteDatabaseSettings(SfxRequest& rReq)
 
                         if (!aNewDBData.HasTotals())
                         {
-                            // store current subtotal settings
-                            pDBData->CreateSubTotalParam(aSubTotalParam);
+                            // store current subtotal settings before removing total row
+                            pDBData->CreateTotalRowParam(aSubTotalParam);
                             aNewDBData.SetSubTotalParam(aSubTotalParam);
-                            // remove total row
                             aSubTotalParam.bRemoveOnly = true;
                             aSubTotalParam.bReplace = true;
                             aFunc.DoTableSubTotals(aNewDBData.GetTab(), aNewDBData, aSubTotalParam,

commit dc4a0d64b2d0baaea0299406dd9cc2cc7bcd1ec8

Table Style: ooxml export: fix corrupt ooxml files because
of XML_totalsRowShown with value "0". We don't support totalsRowShown
in core as it should be according to ooxml standard. totalsRowShown is
false until we didn't set the Total row for the first time, after that
it is always true.
If there are totals or have any total row attribute is set, Total Row was shown.

TODO: handle XML_totalsRowShown in core.

regression after: e725c5b9327424fb927ed251d54b6913ae9d8568

Change-Id: Id3233afa760c198401d5a9f4a9637e907dd5cd63
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196532
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Balazs Varga <balazs.varga@collabora.com>

sc/source/filter/excel/xedbdata.cxx
@@ -193,7 +193,7 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
         = rData.GetTotalRowAttributes(formula::FormulaGrammar::GRAM_OOXML);
 
     // if the Total row have ever been showed it will be true
-    bool hasAnySetValue = std::any_of(aTotalValues.begin(), aTotalValues.end(),
+    bool hasAnySetValue = rData.HasTotals() || std::any_of(aTotalValues.begin(), aTotalValues.end(),
                                       [](const TableColumnAttributes& attr)
                                       {
                                           return attr.maTotalsRowLabel.has_value()
@@ -210,7 +210,8 @@ void XclExpTables::SaveTableXml( XclExpXmlStream& rStrm, const Entry& rEntry )
         XML_tableType, tableType,
         XML_headerRowCount, ToPsz10(rData.HasHeader()),
         XML_totalsRowCount, ToPsz10(rData.HasTotals()),
-        XML_totalsRowShown, ToPsz10(hasAnySetValue)
+        XML_totalsRowShown, (hasAnySetValue ? nullptr : ToPsz10(false)) // we don't support that but
+        // if there are totals or any total row attribute is set they are shown
         // OOXTODO: XML_comment, ...,
         // OOXTODO: XML_connectionId, ...,
         // OOXTODO: XML_dataCellStyle, ...,

sc/qa/unit/subsequent_export_test4.cxx
@@ -1793,6 +1793,17 @@ CPPUNIT_TEST_FIXTURE(ScExportTest4, testTotalsRowFunction)
     }
 }
 
+CPPUNIT_TEST_FIXTURE(ScExportTest4, testTotalsRowShown)
+{
+    createScDoc("xlsx/totalsRowShown.xlsx");
+    saveAndReload(u"Calc Office Open XML"_ustr);
+    {
+        xmlDocUniquePtr pDocXml = parseExport(u"xl/tables/table1.xml"_ustr);
+        CPPUNIT_ASSERT(pDocXml);
+        assertXPathNoAttribute(pDocXml, "/x:table", "totalsRowShown");
+    }
+}
+
 CPPUNIT_TEST_FIXTURE(ScExportTest4, testTdf167689_xmlMaps_and_xmlColumnPr)
 {
     createScDoc("xlsx/tdf167689_xmlMaps_and_xmlColumnPr.xlsx");