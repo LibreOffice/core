eba9018701e980610266b4fc8b6aab96c47eeb48
Pher217 <61641590+Pher217@users.noreply.github.com>
2026-01-11 02:30:31 +0100

    Fix harfbuzz PKG_CONFIG_PATH - use semicolon separator for Windows pkgconf
    Co-Authored-By: Pher

--- external/harfbuzz/ExternalProject_harfbuzz.mk
+++ external/harfbuzz/ExternalProject_harfbuzz.mk
@@ -56,7 +56,7 @@ $(call gb_ExternalProject_get_state_target,harfbuzz,build) : | $(call gb_Externa
 	$(file >$(gb_UnpackedTarball_workdir)/harfbuzz/cross-file.txt,$(gb_harfbuzz_cross_compile))
 	cp -f $(gb_UnpackedTarball_workdir)/graphite/graphite2-uninstalled.pc $(gb_UnpackedTarball_workdir)/graphite/graphite2.pc 2>/dev/null || true
 	$(call gb_ExternalProject_run,build,\
-		PKG_CONFIG_PATH="${PKG_CONFIG_PATH}$(LIBO_PATH_SEPARATOR)$(gb_UnpackedTarball_workdir)/graphite$(if $(SYSTEM_ICU),,$(LIBO_PATH_SEPARATOR)$(gb_UnpackedTarball_workdir)/icu)" \
+		PKG_CONFIG_PATH="$(gb_UnpackedTarball_workdir)/graphite$(if $(SYSTEM_ICU),,;$(gb_UnpackedTarball_workdir)/icu)" \
 		PYTHONWARNINGS= \
 		$(MESON) setup --wrap-mode nofallback builddir \
 			-Ddefault_library=static -Dbuildtype=$(if $(ENABLE_DBGUTIL),debug,$(if $(ENABLE_DEBUG),debugoptimized,release \
