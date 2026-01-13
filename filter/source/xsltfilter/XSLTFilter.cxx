LibreOffice/core

commit d22f3b8c787b93c38925adf92af542362dbda3e6

Remove css.xml.xslt.XSLT2Transformer not implemented by LO
(see f4238ed900cdfa65d447fbef81b2e8ae95095a4e "Revert 'XSLT2Transformer::create
should always succeed'": "LO does not ship this service, it's only provided by
Saxon extension")

Change-Id: I8ff8c9e4bd0eae7055dcb4fc83a04ea41310b9a8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/147038
Tested-by: Jenkins
Reviewed-by: Stephan Bergmann <sbergman@redhat.com>

filter/source/xsltfilter/XSLTFilter.cxx
@@ -65,7 +65,6 @@
 #include <com/sun/star/beans/NamedValue.hpp>
 #include <com/sun/star/task/XInteractionHandler.hpp>
 #include <com/sun/star/ucb/InteractiveAugmentedIOException.hpp>
-#include <com/sun/star/xml/xslt/XSLT2Transformer.hpp>
 #include <com/sun/star/xml/xslt/XSLTTransformer.hpp>
 #include <utility>
 
@@ -234,7 +233,10 @@ namespace XSLT
         {
             try
             {
-                xTransformer = xslt::XSLT2Transformer::create(m_xContext, rArgs);
+                xTransformer.set(
+                    m_xContext->getServiceManager()->createInstanceWithArgumentsAndContext(
+                        "com.sun.star.xml.xslt.XSLT2Transformer", rArgs, m_xContext),
+                    css::uno::UNO_QUERY_THROW);
             }
             catch (const Exception&)
             {

offapi/UnoApi_offapi.mk
@@ -484,7 +484,6 @@ $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,com/sun/star/xml/xpath,\
 ))
 $(eval $(call gb_UnoApi_add_idlfiles_nohdl,offapi,com/sun/star/xml/xslt,\
 	XSLTTransformer \
-	XSLT2Transformer \
 ))
 
 

offapi/com/sun/star/xml/xslt/XSLT2Transformer.idl
@@ -1,23 +1,0 @@
-/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
-/*
- * This file is part of the LibreOffice project.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
-module com { module sun { module star { module xml { module xslt {
-
-/** Get XSLT filter transformer supporting XSLT 2.0.
-
-    @since LibreOffice 4.0
- */
-service XSLT2Transformer : XXSLTTransformer
-{
-    create([in] sequence<any> args);
-};
-
-}; }; }; }; };
-
-/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit d92905f099992c48fc7ec0a6d0be5675d123b833

filter: XSLTFilter: unbreak XSLT2Transformer
Creating the service fails:

  java.lang.ClassCastException: class com.sun.star.beans.NamedValue cannot be cast to class [Ljava.lang.Object; (com.sun.star.beans.NamedValue is in unnamed module of loader com.sun.star.lib.unoloader.UnoClassLoader @6ff3c5b5; [Ljava.lang.Object; is in module java.base of loader 'bootstrap')
  at com.sun.star.comp.xsltfilter.XSLTransformer.initialize(XSLTransformer.java:143)
  at com.sun.star.comp.loader.FactoryHelper$Factory.createInstanceWithArgumentsAndContext(FactoryHelper.java:271)

because initialize gets a sequence<any> where the anys are NamedValues,
but it expects a sequence<any> where the any is sequence<any> where the
anys are NamedValues.

(regression from commit d22f3b8c787b93c38925adf92af542362dbda3e6)

Change-Id: I4fd13f3e04d79f7646e6bb31c8f0d35efd42ae69
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197132
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

filter/source/xsltfilter/XSLTFilter.cxx
@@ -234,7 +234,8 @@ namespace XSLT
             {
                 xTransformer.set(
                     m_xContext->getServiceManager()->createInstanceWithArgumentsAndContext(
-                        u"com.sun.star.xml.xslt.XSLT2Transformer"_ustr, rArgs, m_xContext),
+                        u"com.sun.star.xml.xslt.XSLT2Transformer"_ustr,
+                        {Any{rArgs}}, m_xContext),
                     css::uno::UNO_QUERY_THROW);
             }
             catch (const Exception&)