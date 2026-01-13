ce568d357ff132fb35f3fec07f432976b0002844
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-01-13 21:15:14 +0000

    Don't null-deref if pView is not present
    And default to COL_WHITE as view background if not present.
    
    Related: tdf#168710
    Change-Id: I184fb018d16ac83381b689093e751167bad2c2fb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197222
    Tested-by: Jenkins
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit 1772c60c7a8b31edd1baebf60b0b589dc6d865df)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197254
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- svx/source/svdraw/svdpage.cxx
+++ svx/source/svdraw/svdpage.cxx
@@ -1800,7 +1800,7 @@ SfxStyleSheet* SdrPage::GetTextStyleSheetForObject( SdrObject* pObj ) const
 // #i75566# GetBackgroundColor -> GetPageBackgroundColor and bScreenDisplay hint value
 Color SdrPage::GetPageBackgroundColor( SdrPageView const * pView, bool bScreenDisplay ) const
 {
-    Color aColor;
+    Color aColor(COL_WHITE);
 
     if(bScreenDisplay && (!pView || pView->GetApplicationDocumentColor() == COL_AUTO))
     {
@@ -1812,7 +1812,7 @@ Color SdrPage::GetPageBackgroundColor( SdrPageView const * pView, bool bScreenDi
             aColor = aColorConfig.GetColorValue( svtools::DOCCOLOR ).nColor;
         }
     }
-    else
+    else if (pView)
     {
         aColor = pView->GetApplicationDocumentColor();
     }
