LibreOffice/core

commit df4873973ed6951ab63c59129226d18055f771fc

simplify Graphic::applyDuotone
make the filter code preserve the alpha layer instead of
copying it around separately.

Change-Id: I2f9c2717ee4daa1f9f3f9673227ae5829c1364e0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190303
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/bitmap/BitmapDuoToneFilter.cxx
@@ -27,7 +27,8 @@ Bitmap BitmapDuoToneFilter::execute(Bitmap const& rBitmap) const
     const sal_Int32 nWidth = aBitmap.GetSizePixel().Width();
     const sal_Int32 nHeight = aBitmap.GetSizePixel().Height();
 
-    Bitmap aResultBitmap(aBitmap.GetSizePixel(), vcl::PixelFormat::N24_BPP);
+    Bitmap aResultBitmap(aBitmap.GetSizePixel(), rBitmap.HasAlpha() ? vcl::PixelFormat::N32_BPP
+                                                                    : vcl::PixelFormat::N24_BPP);
     BitmapScopedReadAccess pReadAcc(aBitmap);
     BitmapScopedWriteAccess pWriteAcc(aResultBitmap);
     const BitmapColor aColorOne(mnColorOne);

vcl/source/graphic/UnoGraphic.cxx
@@ -220,30 +220,14 @@ uno::Reference< graphic::XGraphic > SAL_CALL Graphic::applyDuotone(
     const uno::Reference< graphic::XGraphic >& rxGraphic, sal_Int32 nColorOne, sal_Int32 nColorTwo )
 {
     ::Graphic aGraphic(rxGraphic);
-    ::Graphic aReturnGraphic;
-
     Bitmap    aBitmap( aGraphic.GetBitmap() );
-    if (aBitmap.HasAlpha())
-    {
-        const AlphaMask   aMask( aBitmap.CreateAlphaMask() );
 
-        Bitmap    aTmpBmp(aBitmap.CreateColorBitmap());
-        BitmapFilter::Filter(aTmpBmp,
-                        BitmapDuoToneFilter(
-                            Color(ColorTransparency, nColorOne),
-                            Color(ColorTransparency, nColorTwo)));
+    BitmapFilter::Filter(aBitmap,
+                    BitmapDuoToneFilter(
+                        Color(ColorTransparency, nColorOne),
+                        Color(ColorTransparency, nColorTwo)));
 
-        aReturnGraphic = ::Graphic( BitmapEx( aTmpBmp, aMask ) );
-    }
-    else
-    {
-        BitmapFilter::Filter(aBitmap,
-                        BitmapDuoToneFilter(
-                            Color(ColorTransparency, nColorOne),
-                            Color(ColorTransparency, nColorTwo)));
-
-        aReturnGraphic = ::Graphic( aBitmap );
-    }
+    ::Graphic aReturnGraphic(aBitmap);
     aReturnGraphic.setOriginURL(aGraphic.getOriginURL());
     return aReturnGraphic.GetXGraphic();
 }

commit 0c1df806d965609b46a0c7cec08752c0234b2538

tdf#169934 Transparency is lost when applying a duotone filter
regression from
  commit df4873973ed6951ab63c59129226d18055f771fc
  Author: Noel Grandin <noelgrandin@gmail.com>
  Date:   Wed Aug 27 20:25:54 2025 +0200
  simplify Graphic::applyDuotone

Change-Id: I69a3da35b53355488c2d6a1f1288048d4096bc6c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197298
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

vcl/source/bitmap/BitmapDuoToneFilter.cxx
@@ -41,10 +41,12 @@ Bitmap BitmapDuoToneFilter::execute(Bitmap const& rBitmap) const
             BitmapColor aColor = pReadAcc->GetColor(y, x);
             sal_uInt8 nLuminance = aColor.GetLuminance();
             BitmapColor aResultColor(
+                ColorAlpha,
                 lcl_getDuotoneColorComponent(nLuminance, aColorOne.GetRed(), aColorTwo.GetRed()),
                 lcl_getDuotoneColorComponent(nLuminance, aColorOne.GetGreen(),
                                              aColorTwo.GetGreen()),
-                lcl_getDuotoneColorComponent(nLuminance, aColorOne.GetBlue(), aColorTwo.GetBlue()));
+                lcl_getDuotoneColorComponent(nLuminance, aColorOne.GetBlue(), aColorTwo.GetBlue()),
+                aColor.GetAlpha());
             pWriteAcc->SetPixel(y, x, aResultColor);
         }
     }