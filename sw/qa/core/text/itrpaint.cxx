21217325e9ecfe70b8b39215b44923286c1c1c8a
Miklos Vajna <vmiklos@collabora.com>
2026-01-15 09:27:37 +0100

    cool#13574 sw redline render mode: avoid coloring, set lightness
    User A managed to format a piece of text as red, formatted with
    underline and then user B is now confused why rejecting this tracked
    change doesn't work.
    
    This is working as intended for the normal redline render mode, but we
    can try something different for the 'omit insert/delete' redline render
    mode: when showing the old version, we can render deletes unchanged and
    when showing the new version, we can render inserts unchanged. The rest
    of the redlines can be semi-hidden.
    
    That semi-hidden is a bit tricky to provide. Now that we don't add
    layout-level colors in SwModule::GetInsertAuthorAttr(), we go with
    automatic color, which gets resolved quite late. So first figure out if
    we need to "omit" (semi-hide) the current redline portion in
    SwTextPainter::DrawTextLine(), and then pass around a flag, so that once
    SwFntObj::DrawText() is past ApplyAutoColor(), we can set lightness to a
    medium value.
    
    This is needed, because a typical auto color resolves to either white or
    black, and changing saturation has no effect for those colors. And this
    way we still get readable text in both light and dark mode.
    
    Change-Id: I0ff57cd996fda80fadc315ad0c6c85e5af1ff3e7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197350
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins
    (cherry picked from commit a24dd7b1d742ccd59768db8b6b6a522588952108)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197390
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- sw/qa/core/text/itrpaint.cxx
+++ sw/qa/core/text/itrpaint.cxx
@@ -12,10 +12,13 @@
 #include <memory>
 
 #include <o3tl/string_view.hxx>
+#include <svtools/colorcfg.hxx>
 
 #include <docsh.hxx>
 #include <wrtsh.hxx>
 #include <ndtxt.hxx>
+#include <swmodule.hxx>
+#include <swdll.hxx>
 
 namespace
 {
@@ -29,8 +32,8 @@ public:
     }
 };
 
-/// #RRGGBB -> HSL saturation.
-sal_Int16 GetColorSaturation(std::u16string_view rRGB)
+/// #RRGGBB -> HSL lightness.
+sal_Int16 GetColorLightness(std::u16string_view rRGB)
 {
     Color aColor(o3tl::toInt32(rRGB.substr(1, 2), 16), o3tl::toInt32(rRGB.substr(3, 2), 16),
                  o3tl::toInt32(rRGB.substr(5, 2), 16));
@@ -38,12 +41,16 @@ sal_Int16 GetColorSaturation(std::u16string_view rRGB)
     sal_uInt16 nSaturation;
     sal_uInt16 nBrightness;
     aColor.RGBtoHSB(nHue, nSaturation, nBrightness);
-    return nSaturation;
+    return nBrightness;
 }
 
 CPPUNIT_TEST_FIXTURE(Test, testRedlineRenderModeOmitInsertDelete)
 {
-    // Default rendering: default, normal saturation, normal saturation.
+    // Reset redline author IDs to a predictable default.
+    SwGlobals::ensure();
+    SwModule::get()->ClearRedlineAuthors();
+
+    // Default rendering: default, normal lightness, normal lightness.
     createSwDoc("redline.docx");
 
     SwDocShell* pDocShell = getSwDocShell();
@@ -64,15 +71,17 @@ CPPUNIT_TEST_FIXTURE(Test, testRedlineRenderModeOmitInsertDelete)
     CPPUNIT_ASSERT_EQUAL(u"oldcontent"_ustr, aContent.copy(nIndex2, nLength2));
     OUString aColor2
         = getXPath(pXmlDoc, "(//textarray)[2]/preceding-sibling::textcolor[1]", "color");
-    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int16>(50), GetColorSaturation(aColor2));
+    Color aRedlineColor = SwModule::get()->GetColorConfig().GetColorValue(svtools::AUTHOR1).nColor;
+    OUString aRedlineColorString = u"#"_ustr + aRedlineColor.AsRGBHexString();
+    CPPUNIT_ASSERT_EQUAL(aRedlineColorString, aColor2);
     sal_Int32 nIndex3 = getXPath(pXmlDoc, "(//textarray)[3]", "index").toInt32();
     sal_Int32 nLength3 = getXPath(pXmlDoc, "(//textarray)[3]", "length").toInt32();
     CPPUNIT_ASSERT_EQUAL(u"newcontent"_ustr, aContent.copy(nIndex3, nLength3));
     OUString aColor3
         = getXPath(pXmlDoc, "(//textarray)[3]/preceding-sibling::textcolor[1]", "color");
-    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int16>(50), GetColorSaturation(aColor3));
+    CPPUNIT_ASSERT_EQUAL(aRedlineColorString, aColor3);
 
-    // Omit inserts: default, normal saturation, de-saturated.
+    // Omit inserts: default, normal lightness, increased lightness.
     SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
     SwViewOption aOpt(*pWrtShell->GetViewOptions());
     aOpt.SetRedlineRenderMode(SwRedlineRenderMode::OmitInserts);
@@ -91,16 +100,16 @@ CPPUNIT_TEST_FIXTURE(Test, testRedlineRenderModeOmitInsertDelete)
     nLength2 = getXPath(pXmlDoc, "(//textarray)[2]", "length").toInt32();
     CPPUNIT_ASSERT_EQUAL(u"oldcontent"_ustr, aContent.copy(nIndex2, nLength2));
     aColor2 = getXPath(pXmlDoc, "(//textarray)[2]/preceding-sibling::textcolor[1]", "color");
-    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int16>(50), GetColorSaturation(aColor2));
+    CPPUNIT_ASSERT_EQUAL(u"#000000"_ustr, aColor2);
     nIndex3 = getXPath(pXmlDoc, "(//textarray)[3]", "index").toInt32();
     nLength3 = getXPath(pXmlDoc, "(//textarray)[3]", "length").toInt32();
     CPPUNIT_ASSERT_EQUAL(u"newcontent"_ustr, aContent.copy(nIndex3, nLength3));
     aColor3 = getXPath(pXmlDoc, "(//textarray)[3]/preceding-sibling::textcolor[1]", "color");
     // Without the accompanying fix in place, this test would have failed with:
-    // - Expected less or equal than: 50
-    // - Actual  : 100
-    // i.e. the 3rd text portion was not de-saturated.
-    CPPUNIT_ASSERT_LESSEQUAL(static_cast<sal_Int16>(50), GetColorSaturation(aColor3));
+    // - Expected greater or equal than: 49
+    // - Actual  : 0
+    // i.e. the 3rd text portion had no increased lightness from black.
+    CPPUNIT_ASSERT_GREATEREQUAL(static_cast<sal_Int16>(49), GetColorLightness(aColor3));
 
     // Omit deletes: default, de-saturated, normal saturation.
     aOpt.SetRedlineRenderMode(SwRedlineRenderMode::OmitDeletes);
@@ -119,12 +128,12 @@ CPPUNIT_TEST_FIXTURE(Test, testRedlineRenderModeOmitInsertDelete)
     nLength2 = getXPath(pXmlDoc, "(//textarray)[2]", "length").toInt32();
     CPPUNIT_ASSERT_EQUAL(u"oldcontent"_ustr, aContent.copy(nIndex2, nLength2));
     aColor2 = getXPath(pXmlDoc, "(//textarray)[2]/preceding-sibling::textcolor[1]", "color");
-    CPPUNIT_ASSERT_LESSEQUAL(static_cast<sal_Int16>(50), GetColorSaturation(aColor2));
+    CPPUNIT_ASSERT_GREATEREQUAL(static_cast<sal_Int16>(49), GetColorLightness(aColor2));
     nIndex3 = getXPath(pXmlDoc, "(//textarray)[3]", "index").toInt32();
     nLength3 = getXPath(pXmlDoc, "(//textarray)[3]", "length").toInt32();
     CPPUNIT_ASSERT_EQUAL(u"newcontent"_ustr, aContent.copy(nIndex3, nLength3));
     aColor3 = getXPath(pXmlDoc, "(//textarray)[3]/preceding-sibling::textcolor[1]", "color");
-    CPPUNIT_ASSERT_GREATER(static_cast<sal_Int16>(50), GetColorSaturation(aColor3));
+    CPPUNIT_ASSERT_EQUAL(u"#000000"_ustr, aColor3);
 }
 }
 
