0fcf8f75bb9a979197be7daa1430a288a85edbfe
Karthik Godha <karthik.godha@collabora.com>
2026-01-18 14:23:03 +0530

    sc: XLSX - Skip writing empty scenarios
    In XLSX export, scenarios must contain at least one valid scenario
    element.
    
    bug-document: forum-fr-42975.xls
    
    Change-Id: I543a5637971d71de1d9d55f6e81a23d946495b6e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197534
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit 9fd010d8423f01c0a33b40746be6189e4d5034d9)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199419
    Reviewed-by: Karthik Godha <karthik.godha@collabora.com>
    Tested-by: Jenkins
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199656

--- sc/source/filter/xcl97/xcl97rec.cxx
+++ sc/source/filter/xcl97/xcl97rec.cxx
@@ -1656,7 +1656,17 @@ void ExcEScenarioManager::Save( XclExpStream& rStrm )
 
 void ExcEScenarioManager::SaveXml( XclExpXmlStream& rStrm )
 {
-    if( aScenes.empty() )
+    bool bValidScenarios = false;
+    for (ExcEScenario& rScenario : aScenes)
+    {
+        if (rScenario.GetCells().size())
+        {
+            bValidScenarios = true;
+            break;
+        }
+    }
+
+    if (!bValidScenarios)
         return;
 
     sax_fastparser::FSHelperPtr& rWorkbook = rStrm.GetCurrentStream();
@@ -1666,8 +1676,11 @@ void ExcEScenarioManager::SaveXml( XclExpXmlStream& rStrm )
             // OOXTODO: XML_sqref
     );
 
-    for( ExcEScenario& rScenario : aScenes )
-        rScenario.SaveXml( rStrm );
+    for (ExcEScenario& rScenario : aScenes)
+    {
+        if (rScenario.GetCells().size())
+            rScenario.SaveXml(rStrm);
+    }
 
     rWorkbook->endElement( XML_scenarios );
 }
