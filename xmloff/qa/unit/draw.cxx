de070c8e5932f1bf338f6eb9a2d9b43582fb9d5a
Michael Stahl <michael.stahl@collabora.com>
2026-01-19 16:16:15 +0100

    sw: fix setting mbIsTextBox on shapes' text boxes
    SetTextBoxes(true) is never called because it checks the shape's
    SdrObject, while previously this code was in
    SwFrameFormat::SetOtherTextBoxFormat() where it would be called a second
    time for the text box SwFrameFormat which has the SwFlyDrawObj.
    
    SwTextBoxHelper::getCount() can't ignore the text boxes if the flag is
    never set; this ends up confusing
    ShapeGroupContext::popGroupAndPostProcess() (and causes tests like
    testTdf84695 to fail with the fixes in the next commit).
    
    Because shapes with text boxes are sorted again with this, testTdf146272
    and testTextBoxLoss need adapting.
    
    (regression from commit 504d78acb866495fd954fcd6db22ea68f174a5ab)
    
    Change-Id: I80c375d900a10f1956cd112d60c3f25332c485ac
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197594
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit 327e92c78603e011e7ddf8b57749dfd9e690addf)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197729
    Tested-by: Jenkins
    (cherry picked from commit 9e0d4c693ce3ac496372906da2c452c82c3d5d3d)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197772
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- xmloff/qa/unit/draw.cxx
+++ xmloff/qa/unit/draw.cxx
@@ -98,7 +98,7 @@ CPPUNIT_TEST_FIXTURE(XmloffDrawTest, testTextBoxLoss)
     // Make sure that the shape is still a textbox.
     uno::Reference<drawing::XDrawPageSupplier> xDrawPageSupplier(mxComponent, uno::UNO_QUERY);
     uno::Reference<drawing::XDrawPage> xDrawPage = xDrawPageSupplier->getDrawPage();
-    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(1), uno::UNO_QUERY);
+    uno::Reference<beans::XPropertySet> xShape(xDrawPage->getByIndex(0), uno::UNO_QUERY);
     bool bTextBox = false;
     xShape->getPropertyValue(u"TextBox"_ustr) >>= bTextBox;
 
