f8b72d104ad32171ab637fc83c6f942a930b69e1
justin.luth@collabora.com <jluth@mail.com>
2026-01-19 11:34:38 -0500

    tdf#170389 docx export: limit w:tabs to 64 entries
    Files with more than 64 tabstops are reported as corrupt
    by MS Word.
    
    Exposed by 6.2 commit 2bc84658cce1df5050fe788dd0c8a0906a1ca2c3
    Author: Justin Luth on Wed Jul 18 07:37:41 2018 +0200
        related tdf#63561 docx: styles inherit tabstops too
        Reviewed-on: https://gerrit.libreoffice.org/57278
    
    Probably dedup needs to happen here,
    since AFAICS we accumulate the inherited tabs
    in each successive inherted style/paragraph.
    
    make CppunitTest_sw_ooxmlexport25 \
        CPPUNIT_TEST_NAME=testTdf170389_manyTabstops
    
    Change-Id: I2fa356184e5322f9483678c9a50ae6ce96599920
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197606
    Tested-by: Jenkins
    Reviewed-by: Justin Luth <jluth@mail.com>

--- sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
@@ -150,6 +150,18 @@ DECLARE_OOXMLEXPORT_TEST(testTdf165478_bottomAligned, "tdf165478_bottomAligned.d
     CPPUNIT_ASSERT_EQUAL(sal_Int32(1887), nFlyTop);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf170389_manyTabstops)
+{
+    createSwDoc("tdf170389_manyTabstops.odt");
+
+    save(TestFilter::DOCX);
+
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+    // MS Word reports document as corrupt if it has more than 64 tabstops defined
+    // The paragraph itself defines 40, and inherits 40. Without the fix, this was 80
+    assertXPath(pXmlDoc, "//w:tabs/w:tab", 64);
+}
+
 CPPUNIT_TEST_FIXTURE(Test, testInvalidDatetimeInProps)
 {
     createSwDoc("invalidDatetimeInProps.fodt");
