58da9d878db744f871072ef85f643a39e3c71a1f
Simon Chenery <simon_chenery@yahoo.com>
2026-01-20 22:33:03 +0100

    tdf#154285 Check too many arguments to BASIC INT function
    Change-Id: Ia0d71efc326fcfad13bc8edc9d15d5ef491db265
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197702
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins

--- basic/source/runtime/methods.cxx
+++ basic/source/runtime/methods.cxx
@@ -939,7 +939,7 @@ void SbRtl_InStrRev(StarBASIC *, SbxArray & rPar, bool)
 
 void SbRtl_Int(StarBASIC *, SbxArray & rPar, bool)
 {
-    if (rPar.Count() < 2)
+    if (rPar.Count() != 2)
         return StarBASIC::Error( ERRCODE_BASIC_BAD_ARGUMENT );
 
     SbxVariableRef pArg = rPar.Get(1);
