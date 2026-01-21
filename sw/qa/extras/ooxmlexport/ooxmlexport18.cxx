LibreOffice/core

commit a8b1699ca9c7e8c43eff79467451fd1fcb4fde9b

speed-up shape import if shapes need z-order rearranging
setting z-order individually on each shape and broadcasting is
O(n^2) (remaining shapes also need reordering) and this is bad
bad bad for performance

Change-Id: Ic9c9137a097f6ff524192693910f221885f77cc4
Reviewed-on: https://gerrit.libreoffice.org/75055
Tested-by: Jenkins
Reviewed-by: Michael Stahl <michael.stahl@cib.de>

xmloff/source/draw/shapeimport.cxx
@@ -23,6 +23,7 @@
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/text/PositionLayoutDir.hpp>
+#include <com/sun/star/drawing/XShapes3.hpp>
 
 #include <utility>
 #include <xmloff/unointerfacetouniqueidentifiermapper.hxx>
@@ -795,8 +796,44 @@ void ShapeSortContext::popGroupAndSort()
         while( nCount );
     }
 
+    bool bSorted = std::is_sorted(maZOrderList.begin(), maZOrderList.end(),
+                       [&](const ZOrderHint& rLeft, const ZOrderHint& rRight)
+                       { return rLeft.nShould < rRight.nShould; } );
+
+    if (bSorted)
+        return; // nothin' to do
+
     // sort z-ordered shapes by nShould field
     std::sort(maZOrderList.begin(), maZOrderList.end());
+
+    uno::Reference<drawing::XShapes3> xShapes3(mxShapes, uno::UNO_QUERY);
+    if( xShapes3.is())
+    {
+        uno::Sequence<sal_Int32> aNewOrder(maZOrderList.size() + maUnsortedList.size());
+        sal_Int32 nIndex = 0;
+
+        for (ZOrderHint& rHint : maZOrderList)
+        {
+            // fill in the gaps from unordered list
+            for (vector<ZOrderHint>::iterator aIt = maUnsortedList.begin(); aIt != maUnsortedList.end() && nIndex < rHint.nShould; )
+            {
+                aNewOrder[nIndex++] = (*aIt).nIs;
+                aIt = maUnsortedList.erase(aIt);
+            }
+
+            aNewOrder[nIndex] = rHint.nIs;
+            nIndex++;
+        }
+
+        try
+        {
+            xShapes3->sort(aNewOrder);
+            maZOrderList.clear();
+            return;
+        }
+        catch (const css::lang::IllegalArgumentException& /*e*/)
+        {}
+    }
 
     // this is the current index, all shapes before that
     // index are finished

include/svx/svdpage.hxx
@@ -108,6 +108,7 @@ public:
     bool           IsObjOrdNumsDirty() const        { return mbObjOrdNumsDirty; }
     virtual void   NbcInsertObject(SdrObject* pObj, size_t nPos=SAL_MAX_SIZE);
     virtual void   InsertObject(SdrObject* pObj, size_t nPos=SAL_MAX_SIZE);
+    virtual void sort( std::vector<sal_Int32>& sortOrder );
 
     /// remove from list without delete
     virtual SdrObject* NbcRemoveObject(size_t nObjNum);

include/svx/unopage.hxx
@@ -24,12 +24,13 @@
 #include <com/sun/star/lang/XServiceInfo.hpp>
 #include <com/sun/star/drawing/XDrawPage.hpp>
 #include <com/sun/star/drawing/XShapes2.hpp>
+#include <com/sun/star/drawing/XShapes3.hpp>
 #include <com/sun/star/drawing/XShapeGrouper.hpp>
 #include <com/sun/star/lang/XUnoTunnel.hpp>
 #include <editeng/mutxhelp.hxx>
 #include <svx/svxdllapi.h>
 
-#include <cppuhelper/implbase6.hxx>
+#include <cppuhelper/implbase7.hxx>
 #include <comphelper/servicehelper.hxx>
 
 #include <memory>
@@ -50,9 +51,10 @@ enum class SdrInventor : sal_uInt32;
 #define TWIPS_TO_MM(val) ((val * 127 + 36) / 72)
 #define MM_TO_TWIPS(val) ((val * 72 + 63) / 127)
 
-class SVX_DLLPUBLIC SvxDrawPage : public ::cppu::WeakAggImplHelper6< css::drawing::XDrawPage,
+class SVX_DLLPUBLIC SvxDrawPage : public ::cppu::WeakAggImplHelper7< css::drawing::XDrawPage,
                                                css::drawing::XShapeGrouper,
                                                css::drawing::XShapes2,
+                                               css::drawing::XShapes3,
                                                css::lang::XServiceInfo,
                                                css::lang::XUnoTunnel,
                                                css::lang::XComponent>,
@@ -108,6 +110,9 @@ class SVX_DLLPUBLIC SvxDrawPage : public ::cppu::WeakAggImplHelper6< css::drawin
     // XShapes2
     virtual void SAL_CALL addTop( const css::uno::Reference< css::drawing::XShape >& xShape ) override;
     virtual void SAL_CALL addBottom( const css::uno::Reference< css::drawing::XShape >& xShape ) override;
+
+    // XShapes3
+    virtual void SAL_CALL sort( const css::uno::Sequence< sal_Int32 >& sortOrder ) override;
 
     // XElementAccess
     virtual css::uno::Type SAL_CALL getElementType() override;

offapi/UnoApi_offapi.mk
@@ -2376,6 +2376,7 @@ $(eval $(call gb_UnoApi_add_idlfiles,offapi,com/sun/star/drawing,\
 	XShapeMirror \
 	XShapes \
 	XShapes2 \
+	XShapes3 \
 	XSlidePreviewCache \
 	XSlidePreviewCacheListener \
 	XSlideRenderer \

offapi/com/sun/star/drawing/XShapes3.idl
@@ -1,0 +1,43 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef __com_sun_star_drawing_XShapes3_idl__
+#define __com_sun_star_drawing_XShapes3_idl__
+
+#include <com/sun/star/drawing/XShapes.idl>
+
+module com {  module sun {  module star { module drawing {
+
+/**
+ * Yet another XShapes interface, enables sorting shapes with
+ * some extra attention paid to shapes with textboxes and overall
+ * performance
+ *
+ * @since LibreOffice 6.4
+ */
+interface XShapes3
+{
+    /**
+     * Sort shapes according to given sort order, for perf reason
+     * just rearrange and don't broadcast
+     *
+     * @param desired order of the shapes
+     *
+     * @since LibreOffice 6.4
+     */
+
+    void sort( [in] sequence< long> sortOrder )
+        raises( com::sun::star::lang::IllegalArgumentException );
+};
+
+}; }; }; };
+
+#endif
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

svx/source/svdraw/svdpage.cxx
@@ -57,6 +57,8 @@
 #include <rtl/strbuf.hxx>
 #include <libxml/xmlwriter.h>
 
+#include <com/sun/star/lang/IllegalArgumentException.hpp>
+
 using namespace ::com::sun::star;
 
 class SdrObjList::WeakSdrObjectContainerType
@@ -554,6 +556,100 @@ SdrObject* SdrObjList::SetObjectOrdNum(size_t nOldObjNum, size_t nNewObjNum)
         pObj->getSdrModelFromSdrObject().SetChanged();
     }
     return pObj;
+}
+
+void SdrObjList::sort( std::vector<sal_Int32>& sortOrder)
+{
+    // no negative indexes and indexes larger than maList size are allowed
+    auto it = std::find_if( sortOrder.begin(), sortOrder.end(), [this](const sal_Int32& rIt)
+         { return ( rIt < 0 || static_cast<size_t>(rIt) >= maList.size() ); } );
+    if ( it != sortOrder.end())
+        throw css::lang::IllegalArgumentException("negative index of shape", nullptr, 1);
+
+    // no duplicates
+    std::vector<bool> aNoDuplicates(sortOrder.size(), false);
+    for (size_t i = 0; i < sortOrder.size(); ++i )
+    {
+        size_t idx =  static_cast<size_t>( sortOrder[i] );
+
+        if ( aNoDuplicates[idx] )
+            throw css::lang::IllegalArgumentException("duplicate index of shape", nullptr, 2);
+
+        aNoDuplicates[idx] = true;
+    }
+
+    // example sortOrder [2 0 1]
+    // example maList [T T S T T] ( T T = shape with textbox, S = just a shape )
+    // (shapes at positions 0 and 2 have a textbox)
+
+    std::vector<SdrObject*> aNewList(maList.size());
+    std::set<sal_Int32> aShapesWithTextbox;
+    std::vector<sal_Int32> aIncrements;
+    std::vector<sal_Int32> aDuplicates;
+
+    if ( maList.size() > 1)
+    {
+        for (size_t i = 1; i< maList.size(); ++i)
+        {
+            // if this shape is a textbox, then look at its left neighbour
+            // (shape this textbox is in)
+            // and insert the number of textboxes to the left of it
+            if (maList[i]->IsTextBox())
+              aShapesWithTextbox.insert( i - 1 - aShapesWithTextbox.size() );
+        }
+        // example aShapesWithTextbox [0 2]
+    }
+
+    aIncrements.push_back(0);
+    aDuplicates.push_back(sortOrder[0]);
+
+    // corner case: 1st shape is a textbox, add it twice
+    // otherwise sortOrder loop below will skip it
+    if (aShapesWithTextbox.count(sortOrder[0]) > 0)
+        aDuplicates.push_back(sortOrder[0]);
+
+    for (size_t i = 1; i< sortOrder.size(); ++i)
+    {
+         aDuplicates.push_back(sortOrder[i]);
+
+         if (aShapesWithTextbox.count(sortOrder[i]) > 0)
+         {
+             aIncrements.push_back(aIncrements[i-1] + 1 );
+             aDuplicates.push_back(sortOrder[i]);
+         }
+         else
+         {
+             aIncrements.push_back(aIncrements[i-1]);
+         }
+
+         // example aDuplicates [2 2 0 0 1]
+         // example aIncrements [0 1 1]
+    }
+
+    std::vector<sal_Int32> aNewSortOrder(maList.size());
+    sal_Int32 nPrev = -1;
+    for (size_t i = 0; i< aDuplicates.size(); ++i)
+    {
+        if (nPrev != aDuplicates[i])
+            aNewSortOrder[i] = aDuplicates[i] + aIncrements[aDuplicates[i]];
+        else
+            aNewSortOrder[i] = aNewSortOrder[i-1] + 1;
+
+        nPrev = aDuplicates[i];
+
+        // example aNewSortOrder [3 4 0 1 2]
+    }
+
+    if ( aNewSortOrder.size() !=  maList.size())
+        throw css::lang::IllegalArgumentException("mismatch of no. of shapes", nullptr, 0);
+
+    for (size_t i = 0; i < aNewSortOrder.size(); ++i)
+    {
+        aNewList[i] = maList[ sortOrder[i] ];
+        aNewList[i]->SetOrdNum(i);
+    }
+
+    std::swap(aNewList, maList);
 }
 
 const tools::Rectangle& SdrObjList::GetAllObjSnapRect() const

svx/source/unodraw/unopage.cxx
@@ -27,6 +27,7 @@
 #include <osl/mutex.hxx>
 #include <sfx2/dispatch.hxx>
 #include <comphelper/classids.hxx>
+#include <comphelper/sequence.hxx>
 #include <cppuhelper/supportsservice.hxx>
 
 #include <sfx2/objsh.hxx>
@@ -328,6 +329,12 @@ void SAL_CALL SvxDrawPage::remove( const Reference< drawing::XShape >& xShape )
     }
 
     mpModel->SetChanged();
+}
+
+void SvxDrawPage::sort( const css::uno::Sequence< sal_Int32 >& sortOrder )
+{
+    auto newOrder = comphelper::sequenceToContainer<std::vector<sal_Int32>>(sortOrder);
+    mpPage->sort(newOrder);
 }
 
 // css::container::XIndexAccess

commit 66a1ad2163d5799a79b23554b22e08a07f693225

xmloff: ODF import: fix z-index rotation in header/footer
ShapeGroupContext::popGroupAndPostProcess() is called 2 times when
importing an ODF package file, once at the end of styles.xml, then at
the end of content.xml; in the 2nd case, all shapes of styles.xml end up
in maUnsortedList.

There are 2 problems with it:

1. in the XShapes3 branch, once maZOrderList is empty, any remaining
   elements in maUnsortedList are ignored and thus the aNewOrder
   contains duplicate 0s at the end which will cause sort() to throw

2. the XShapes3 branch removes elements from maUnsortedList, hence when
   the exception is caught and the fallback path runs, it only moves
   the elements that happened to remain in maUnsortedList

   Effectively, what happens is that the z-order of everything in
   headers/footers is rotated so that the last (topmost) N elements go
   to the start (background).

3. In case the objects in content.xml happen to be sorted correctly
   wrt each other, but there was an object with higher z-index in
   styles.xml, sorting is skipped.
   Turns out that testTdf159158_zOrder_headerBehind already tests this.

(regression from commit a8b1699ca9c7e8c43eff79467451fd1fcb4fde9b)

Change-Id: Ifdde6fc43af4c655b430d723b875dfd46e8533f2
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197475
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>

xmloff/source/draw/shapeimport.cxx
@@ -621,8 +621,12 @@ void ShapeGroupContext::popGroupAndPostProcess()
                        [](const ZOrderHint& rLeft, const ZOrderHint& rRight)
                        { return rLeft.nShould < rRight.nShould; } );
 
-    if (bSorted)
+    if (maZOrderList.empty() || (bSorted
+        // check that no content.xml shape goes before last styles.xml shape
+            && maUnsortedList.size() <= o3tl::make_unsigned(maZOrderList.front().nShould)))
+    {
         return; // nothin' to do
+    }
 
     // sort z-ordered shapes by nShould field
     std::sort(maZOrderList.begin(), maZOrderList.end());
@@ -634,27 +638,37 @@ void ShapeGroupContext::popGroupAndPostProcess()
         auto pNewOrder = aNewOrder.getArray();
         sal_Int32 nIndex = 0;
 
+        auto itU{maUnsortedList.begin()};
         for (const ZOrderHint& rHint : maZOrderList)
         {
             // fill in the gaps from unordered list
-            for (std::vector<ZOrderHint>::iterator aIt = maUnsortedList.begin(); aIt != maUnsortedList.end() && nIndex < rHint.nShould; )
+            for (; itU != maUnsortedList.end() && nIndex < rHint.nShould; ++itU)
             {
-                pNewOrder[nIndex++] = (*aIt).nIs;
-                aIt = maUnsortedList.erase(aIt);
+                pNewOrder[nIndex] = (*itU).nIs;
+                ++nIndex;
             }
 
             pNewOrder[nIndex] = rHint.nIs;
             nIndex++;
         }
+        for (; itU != maUnsortedList.end(); ++itU)
+        {
+            pNewOrder[nIndex] = (*itU).nIs;
+            ++nIndex;
+        }
+        assert(nIndex == aNewOrder.getLength());
 
         try
         {
             xShapes3->sort(aNewOrder);
+            maUnsortedList.clear();
             maZOrderList.clear();
             return;
         }
-        catch (const css::lang::IllegalArgumentException& /*e*/)
-        {}
+        catch (const css::lang::IllegalArgumentException&)
+        {
+            TOOLS_WARN_EXCEPTION("xmloff.draw", "XShapes3::sort() throws: ");
+        }
     }
 
     // this is the current index, all shapes before that
@@ -674,6 +688,7 @@ void ShapeGroupContext::popGroupAndPostProcess()
 
         nIndex++;
     }
+    maUnsortedList.clear(); // it's not necessary to move remaining elements
     maZOrderList.clear();
 }
 
@@ -724,7 +739,7 @@ void XMLShapeImportHelper::shapeWithZIndexAdded( css::uno::Reference< css::drawi
     aNewHint.nShould = nZIndex;
     aNewHint.pShape = xShape.get();
 
-    if( nZIndex == -1 )
+    if (nZIndex < 0) // not set or invalid value
     {
         // don't care, so add to unsorted list
         mpImpl->mpGroupContext->maUnsortedList.push_back(aNewHint);

sw/qa/extras/odfexport/odfexport2.cxx
@@ -608,6 +608,19 @@ DECLARE_ODFEXPORT_TEST(tdf149420, "tdf149420.odt")
     CPPUNIT_ASSERT_EQUAL(sal_uInt16(567), getProperty<sal_uInt16>(getParagraph(4), u"ParaHyphenationZone"_ustr));
 }
 
+DECLARE_ODFEXPORT_TEST(zIndexHeader, "z-index-header.odt")
+{
+    uno::Reference<beans::XPropertySet> xOrder0(getShape(1), uno::UNO_QUERY_THROW);
+    uno::Reference<beans::XPropertySet> xOrder1(getShape(2), uno::UNO_QUERY_THROW);
+    uno::Reference<beans::XPropertySet> xOrder2(getShape(3), uno::UNO_QUERY_THROW);
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(xOrder0, u"ZOrder"_ustr));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getProperty<sal_Int32>(xOrder1, u"ZOrder"_ustr));
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(2), getProperty<sal_Int32>(xOrder2, u"ZOrder"_ustr));
+    CPPUNIT_ASSERT_EQUAL(u"HeaderFrameIndex0"_ustr, uno::Reference<container::XNamed>(xOrder0, uno::UNO_QUERY_THROW)->getName());
+    CPPUNIT_ASSERT_EQUAL(u"BodyFrameIndex1"_ustr, uno::Reference<container::XNamed>(xOrder1, uno::UNO_QUERY_THROW)->getName());
+    CPPUNIT_ASSERT_EQUAL(u"HeaderFrameIndex2"_ustr, uno::Reference<container::XNamed>(xOrder2, uno::UNO_QUERY_THROW)->getName());
+}
+
 DECLARE_ODFEXPORT_TEST(testArabicZeroNumbering, "arabic-zero-numbering.odt")
 {
     CPPUNIT_ASSERT_EQUAL(1, getPages());

sw/qa/extras/ooxmlexport/ooxmlexport18.cxx
@@ -1076,19 +1076,38 @@ CPPUNIT_TEST_FIXTURE(Test, testTdf159158_zOrder_behindDocB)
     verify(/*bIsExport*/ true);
 }
 
-DECLARE_OOXMLEXPORT_TEST(testTdf159158_zOrder_headerBehind, "tdf159158_zOrder_headerBehind.odt")
+CPPUNIT_TEST_FIXTURE(Test, testTdf159158_zOrder_headerBehind)
 {
-    // given a blue star (not marked as behind text) anchored in the header
-    // and an overlapping yellow rectangle anchored in the body text.
-    // (note that in ODT format the star is on top, but for DOCX format it must be behind (hidden)
-    uno::Reference<beans::XPropertySet> zOrder0(getShape(1), uno::UNO_QUERY);
-    uno::Reference<beans::XPropertySet> zOrder1(getShape(2), uno::UNO_QUERY);
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(zOrder0, u"ZOrder"_ustr)); // lower
-    CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getProperty<sal_Int32>(zOrder1, u"ZOrder"_ustr)); // higher
-    // I don't know why the star is the lowest order in ODT import (maybe header weirdness),
-    // but it certainly needs to be the lowest on docx round-trip (also for header weirdness)
-    CPPUNIT_ASSERT_EQUAL(u"StarInHeader"_ustr, getProperty<OUString>(zOrder0, u"Name"_ustr));
-    CPPUNIT_ASSERT_EQUAL(u"RectangleInBody"_ustr, getProperty<OUString>(zOrder1,u"Name"_ustr));
+    createSwDoc("tdf159158_zOrder_headerBehind.odt");
+
+    {
+        // given a blue star (not marked as behind text) anchored in the header
+        // and an overlapping yellow rectangle anchored in the body text.
+        // (note that in ODT format the star is on top, but for DOCX format it must be behind (hidden)
+        uno::Reference<beans::XPropertySet> zOrder0(getShape(1), uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> zOrder1(getShape(2), uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(zOrder0, u"ZOrder"_ustr)); // lower
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getProperty<sal_Int32>(zOrder1, u"ZOrder"_ustr)); // higher
+        CPPUNIT_ASSERT_EQUAL(u"RectangleInBody"_ustr, getProperty<OUString>(zOrder0, u"Name"_ustr));
+        CPPUNIT_ASSERT_EQUAL(u"StarInHeader"_ustr, getProperty<OUString>(zOrder1, u"Name"_ustr));
+    }
+
+    maTempFile.EnableKillingFile(false);
+    saveAndReload(mpFilter);
+
+    {
+        // given a blue star (not marked as behind text) anchored in the header
+        // and an overlapping yellow rectangle anchored in the body text.
+        // (note that in ODT format the star is on top, but for DOCX format it must be behind (hidden)
+        uno::Reference<beans::XPropertySet> zOrder0(getShape(1), uno::UNO_QUERY);
+        uno::Reference<beans::XPropertySet> zOrder1(getShape(2), uno::UNO_QUERY);
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(0), getProperty<sal_Int32>(zOrder0, u"ZOrder"_ustr)); // lower
+        CPPUNIT_ASSERT_EQUAL(sal_Int32(1), getProperty<sal_Int32>(zOrder1, u"ZOrder"_ustr)); // higher
+        CPPUNIT_ASSERT_EQUAL(u"StarInHeader"_ustr, getProperty<OUString>(zOrder0, u"Name"_ustr));
+        CPPUNIT_ASSERT_EQUAL(u"RectangleInBody"_ustr, getProperty<OUString>(zOrder1, u"Name"_ustr));
+    }
+
+    maTempFile.EnableKillingFile();
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf159158_zOrder_headerBehind2, "tdf159158_zOrder_headerBehind2.docx")