143a490293be92914ceda95d9a9e9b2672ad6f62
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-01-21 11:22:17 +0000

    background color should start initialized to COL_AUTO
    Change-Id: I09ad8f22f7ea6236d4a0b2a842571c32c2d0684b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197733
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins

--- editeng/source/editeng/impedit3.cxx
+++ editeng/source/editeng/impedit3.cxx
@@ -4756,7 +4756,7 @@ Color ImpEditEngine::GetAutoColor(const SvxFont* pFont) const
 {
     Color aColor;
 
-    Color aBackgroundColor;
+    Color aBackgroundColor(COL_AUTO);
     if (pFont) //check for char background color
         aBackgroundColor = pFont->GetFillColor();
     if (aBackgroundColor == COL_AUTO) // check for aother background (i.e: cell color)
