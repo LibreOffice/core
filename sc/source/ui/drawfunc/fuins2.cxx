3537eeaf33bf69e8cf52e0c2ada414c0fddddc77
Xisco Fauli <xiscofauli@libreoffice.org>
2026-01-23 12:13:12 +0100

    crashreporting: check xObj
    like it's done a few lines above
    
    Seen in https://crashreport.libreoffice.org/stats/crash_details/978290ff-67d0-4ad4-afdc-b1e79a58b7c2
    
    Change-Id: Ic890513ba6f87b4172966ea004496cbaa755b2a7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197941
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit 0dde33310b5c53b42e56850422a36273e7d5f7e2)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198013
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- sc/source/ui/drawfunc/fuins2.cxx
+++ sc/source/ui/drawfunc/fuins2.cxx
@@ -503,24 +503,28 @@ FuInsertChart::FuInsertChart(ScTabViewShell& rViewSh, vcl::Window* pWin, ScDrawV
         xChartModel->lockControllers();
 
     // object size
-    awt::Size aSz = xObj->getVisualAreaSize( nAspect );
-    Size aSize( aSz.Width, aSz.Height );
-
-    MapUnit aMapUnit = VCLUnoHelper::UnoEmbed2VCLMapUnit( xObj->getMapUnit( nAspect ) );
-
-    bool bSizeCh = false;
-    if (aSize.IsEmpty())
+    Size aSize;
+    if (xObj.is())
     {
-        aSize.setWidth( 5000 );
-        aSize.setHeight( 5000 );
-        bSizeCh = true;
-    }
-    if (bSizeCh)
-    {
-        aSize = OutputDevice::LogicToLogic( aSize, MapMode( MapUnit::Map100thMM ), MapMode( aMapUnit ) );
-        aSz.Width = aSize.Width();
-        aSz.Height = aSize.Height();
-        xObj->setVisualAreaSize( nAspect, aSz );
+        awt::Size aSz = xObj->getVisualAreaSize( nAspect );
+        aSize = Size( aSz.Width, aSz.Height );
+
+        MapUnit aMapUnit = VCLUnoHelper::UnoEmbed2VCLMapUnit( xObj->getMapUnit( nAspect ) );
+
+        bool bSizeCh = false;
+        if (aSize.IsEmpty())
+        {
+            aSize.setWidth( 5000 );
+            aSize.setHeight( 5000 );
+            bSizeCh = true;
+        }
+        if (bSizeCh)
+        {
+            aSize = OutputDevice::LogicToLogic( aSize, MapMode( MapUnit::Map100thMM ), MapMode( aMapUnit ) );
+            aSz.Width = aSize.Width();
+            aSz.Height = aSize.Height();
+            xObj->setVisualAreaSize( nAspect, aSz );
+        }
     }
 
     ScViewData& rData = rViewSh.GetViewData();
