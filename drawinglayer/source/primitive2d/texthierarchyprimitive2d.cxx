fc11defb7efe1ab6be8d9a8005f0678b6df6305b
Mohit Marathe <mohit.marathe@collabora.com>
2026-01-23 11:13:25 +0530

    drawinglayer: suppress text for all views if text edit is active in any view
    This fixes the duplicated text issue for a user who sees text from both
    drawinglayer and EditEngine overlay.
    
    Using `isTiledPainting()` in place of `isActive()` does not interfere
    with SVG export of text (and bullet) content so the Cypress test passes.
    
    See 316c33628e67b41c69796c1af69b21f8d6c205c4 which mentions the failed
    cypress test in online.git
    
    Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
    Change-Id: I73ae74795b34729d570ad7a7be830085787ca5c3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197909
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/901
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- drawinglayer/source/primitive2d/texthierarchyprimitive2d.cxx
+++ drawinglayer/source/primitive2d/texthierarchyprimitive2d.cxx
@@ -147,8 +147,15 @@ namespace drawinglayer::primitive2d
             const geometry::ViewInformation2D& rViewInformation) const
         {
             // check if TextEdit is active. If not, process. If yes, suppress the content
+            // COKit case: if TextEdit is active in view, suppress text in all views.
+            // use `isTiledPainting()` instead of a more generic `isActive()` so that it
+            // does not suppress text for SVG exports
+            bool bSuppress = rViewInformation.getTextEditActive()
+                        || (comphelper::COKit::isTiledPainting());
             // COKit case: always decompose it when we're rendering a slide show
-            if (!rViewInformation.getTextEditActive() || comphelper::COKit::isSlideshowRendering())
+            if (comphelper::COKit::isSlideshowRendering())
+                bSuppress = false;
+            if (!bSuppress)
                 GroupPrimitive2D::get2DDecomposition(rVisitor, rViewInformation);
         }
 
