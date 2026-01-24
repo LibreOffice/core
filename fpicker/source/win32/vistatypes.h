ed98a5519ea19d9ef427a7529ab94345b4bf5cc7
Mike Kaganski <mike.kaganski@collabora.com>
2026-01-24 17:25:55 +0100

    Drop some more typedefs
    Makes the code more clear.
    
    Change-Id: I5336503ab929b7d24701b831e3ad687fd84583bc
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198047
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

--- fpicker/source/win32/vistatypes.h
+++ fpicker/source/win32/vistatypes.h
@@ -31,8 +31,6 @@ namespace vista{
 
 
 typedef sal::systools::COMReference<IFileDialog> TFileDialog;
-typedef sal::systools::COMReference<IFileOpenDialog> TFileOpenDialog;
-typedef sal::systools::COMReference<IFileSaveDialog> TFileSaveDialog;
 typedef sal::systools::COMReference<IFileDialogCustomize> TFileDialogCustomize;
 
 } // namespace vista
