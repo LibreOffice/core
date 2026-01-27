LibreOffice/core

commit bd047bbb9e43e0a4432e8e84408c2117a7ffe7a3

SmartArt: Preparations for roundtrip
The DiagramModel uses 4 mandatory and three optional XMLDoms
for it's data. These get imported, but to get to a roundtrip
we will need to be able to create & save these again.

Last changes prepared this by deleting XMLDoms which were no
longer valid dependent of changes done to that DiagramModel.
When exporting the availability of these gets checked and if
all are there, it gets exported as Diagram (in MSO format,
not yet in ODF). If nothing was changed the original Doms
get exported - untouched.

That test will now try - if e.g. the most important XMLDom
OOXData is missing - to re-create this in that action. The 1st
one which I now implemented that is the OOXData which homes
the Point/Connections/BGFill data (and some more).

Due to this needing testing I for now encapsulated this
to a EnvVar called ACTIVATE_RECREATE_DIAGRAM_DATADOMS, so
this will for now only happen when this is set. Anyways all
that stuff is still behind the 'experimental features' wall.
This is and stays experimental for now.

These are already deep changes which need to get tested and
get to master, opening the path to experiment with it and
refine it. Due to the EnvVar gate no real changes should
happen in master, but due to the changes around it this
will need to be checked this way.

Change-Id: Iabba69b19387e45f1028843a25f3bae1fc4f8fe8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197676
Tested-by: Jenkins
Reviewed-by: Armin Le Grand <Armin.Le.Grand@me.com>

svx/source/diagram/IDiagramHelper.cxx
@@ -410,7 +410,7 @@ IDiagramHelper::IDiagramHelper(bool bSelfCreated)
 , mbUseDiagramModelData(true)
 , mbForceThemePtrRecreation(false)
 , mbSelfCreated(bSelfCreated)
-, mpAssociatedSdrObjGroup(nullptr)
+, mxGroupShape()
 {
 }
 
@@ -418,26 +418,35 @@ IDiagramHelper::~IDiagramHelper() {}
 
 void IDiagramHelper::disconnectFromSdrObjGroup()
 {
-    if (nullptr != mpAssociatedSdrObjGroup)
+    SdrObjGroup* pGroupObject(dynamic_cast<SdrObjGroup*>(SdrObject::getSdrObjectFromXShape(mxGroupShape)));
+    if (nullptr != pGroupObject)
     {
-        auto const p = mpAssociatedSdrObjGroup;
-        mpAssociatedSdrObjGroup = nullptr;
-        p->mp_DiagramHelper.reset();
+        mxGroupShape.clear();
+        pGroupObject->mp_DiagramHelper.reset();
     }
 }
 
-void IDiagramHelper::connectToSdrObjGroup(SdrObjGroup& rTarget)
+void IDiagramHelper::connectToSdrObjGroup(com::sun::star::uno::Reference< com::sun::star::drawing::XShape >& rTarget)
 {
-    if (mpAssociatedSdrObjGroup == &rTarget && rTarget.mp_DiagramHelper.get() == this)
-        // connection already established
-        return;
+    SdrObjGroup* pGroupObject(nullptr);
+    if (pGroupObject && mxGroupShape && rTarget == mxGroupShape)
+    {
+        pGroupObject = dynamic_cast<SdrObjGroup*>(SdrObject::getSdrObjectFromXShape(mxGroupShape));
+        if (pGroupObject != nullptr &&  pGroupObject->mp_DiagramHelper.get() == this)
+        {
+            // connection already established
+            return;
+        }
+    }
 
     // ensure unconnect if already connected
     disconnectFromSdrObjGroup();
 
     // connect to target
-    mpAssociatedSdrObjGroup = &rTarget;
-    rTarget.mp_DiagramHelper.reset(this);
+    mxGroupShape = rTarget;
+    pGroupObject = dynamic_cast<SdrObjGroup*>(SdrObject::getSdrObjectFromXShape(mxGroupShape));
+    if (nullptr != pGroupObject)
+        pGroupObject->mp_DiagramHelper.reset(this);;
 }
 
 void IDiagramHelper::AddAdditionalVisualization(const SdrObjGroup& rTarget, SdrHdlList& rHdlList)

bin/find-can-be-private-symbols.functions.results
@@ -15372,8 +15372,8 @@ svx::ThemeDialog::initColorSets()
 svx::ThemeDialog::runThemeColorEditDialog()
 svx::classification::containsProperty(com::sun::star::uno::Sequence<com::sun::star::beans::Property> const&, std::basic_string_view<char16_t, std::char_traits<char16_t> >)
 svx::classification::convertClassificationResultToString(std::__debug::vector<svx::ClassificationResult, std::allocator<svx::ClassificationResult> > const&)
-svx::diagram::DiagramData::addConnection(svx::diagram::TypeConstant, rtl::OUString const&, rtl::OUString const&)
-svx::diagram::DiagramData::getChildrenString(rtl::OUStringBuffer&, svx::diagram::Point const*, int) const
+svx::diagram::DiagramData_svx::addConnection(svx::diagram::TypeConstant, rtl::OUString const&, rtl::OUString const&)
+svx::diagram::DiagramData_svx::getChildrenString(rtl::OUStringBuffer&, svx::diagram::Point const*, int) const
 svx::diagram::DiagramDataState::DiagramDataState(std::__debug::vector<svx::diagram::Connection, std::allocator<svx::diagram::Connection> >, std::__debug::vector<svx::diagram::Point, std::allocator<svx::diagram::Point> >)
 svx::diagram::DiagramFrameHdl::CreateB2dIAObject()
 svx::diagram::DiagramFrameHdl::DiagramFrameHdl(basegfx::B2DHomMatrix const&)

bin/find-mergedlib-can-be-private-symbols.functions.results
@@ -15746,8 +15746,8 @@ svx::administrateDatabaseRegistration(weld::Window*)
 svx::checkForFontWork(SdrObject const*)
 svx::classification::containsProperty(com::sun::star::uno::Sequence<com::sun::star::beans::Property> const&, std::basic_string_view<char16_t, std::char_traits<char16_t> >)
 svx::classification::convertClassificationResultToString(std::vector<svx::ClassificationResult, std::allocator<svx::ClassificationResult> > const&)
-svx::diagram::DiagramData::addConnection(svx::diagram::TypeConstant, rtl::OUString const&, rtl::OUString const&)
-svx::diagram::DiagramData::getChildrenString(rtl::OUStringBuffer&, svx::diagram::Point const*, int) const
+svx::diagram::DiagramData_svx::addConnection(svx::diagram::TypeConstant, rtl::OUString const&, rtl::OUString const&)
+svx::diagram::DiagramData_svx::getChildrenString(rtl::OUStringBuffer&, svx::diagram::Point const*, int) const
 svx::diagram::DiagramFrameHdl::CreateB2dIAObject()
 svx::diagram::DiagramFrameHdl::DiagramFrameHdl(basegfx::B2DHomMatrix const&)
 svx::diagram::IDiagramHelper::AddAdditionalVisualization(SdrObjGroup const&, SdrHdlList&)

compilerplugins/clang/mergeclasses.results
@@ -684,7 +684,7 @@ merge svx::IControllerFeatureInvalidation with FmXFormShell
 merge svx::IFocusObserver with svx::FmTextControlShell
 maybe merge svx::ODataAccessObjectTransferable with dbaui::ODataClipboard, in modules svx and dbaccess
 merge svx::RegistrationItemSetHolder with svx::DatabaseRegistrationDialog
-maybe merge svx::diagram::DiagramData with oox::drawingml::DiagramData, in modules svx and oox
+maybe merge svx::diagram::DiagramData_svx with oox::drawingml::DiagramData, in modules svx and oox
 maybe merge svx::diagram::IDiagramHelper with oox::drawingml::AdvancedDiagramHelper, in modules svx and oox
 maybe merge svx::sidebar::InspectorTextPanel with sw::sidebar::WriterInspectorTextPanel, in modules svx and sw
 merge svx::sidebar::SvxShapeCommandsMap with svx::sidebar::DefaultShapesPanel

compilerplugins/clang/unusedfields.writeonly.results
@@ -731,7 +731,7 @@ include/svx/diagram/datamodel.hxx:153
 include/svx/diagram/datamodel.hxx:155
     svx::diagram::Point mbIsPlaceholder _Bool
 include/svx/diagram/datamodel.hxx:266
-    svx::diagram::DiagramData maConnectionNameMap ConnectionNameMap
+    svx::diagram::DiagramData_svx maConnectionNameMap ConnectionNameMap
 include/svx/fillctrl.hxx:83
     FillControl mxColorDispatch std::unique_ptr<ToolbarUnoDispatcher>
 include/svx/float3d.hxx:202

cui/source/dialogs/DiagramDialog.cxx
@@ -14,7 +14,7 @@
 #include <svx/svdmodel.hxx>
 #include <svx/svdundo.hxx>
 #include <com/sun/star/beans/PropertyValue.hpp>
-#include <svx/diagram/datamodel.hxx>
+#include <svx/diagram/datamodel_svx.hxx>
 #include <svx/diagram/IDiagramHelper.hxx>
 
 DiagramDialog::DiagramDialog(weld::Window* pWindow, SdrObjGroup& rDiagram)

include/oox/drawingml/diagram/diagramhelper.hxx
@@ -80,7 +80,7 @@ public:
     virtual std::shared_ptr< svx::diagram::DiagramDataState > extractDiagramDataState() const override;
     virtual void applyDiagramDataState(const std::shared_ptr< svx::diagram::DiagramDataState >& rState) override;
 
-    void doAnchor(SdrObjGroup& rTarget, ::oox::drawingml::Shape& rRootShape);
+    void doAnchor(com::sun::star::uno::Reference<com::sun::star::drawing::XShape>& rTarget, ::oox::drawingml::Shape& rRootShape);
     const std::shared_ptr< ::oox::drawingml::Theme >& getOrCreateThemePtr(
         const rtl::Reference< oox::shape::ShapeFilterBase>& rxFilter ) const;
 
@@ -89,7 +89,8 @@ public:
     virtual com::sun::star::uno::Any getOOXDomValue(svx::diagram::DomMapFlag aDomMapFlag) const override;
 
     // check if mandatory DiagramDomS exist (or can be created)
-    virtual bool checkOrCreateMinimalDataDoms() override;
+    bool checkMinimalDataDoms() const;
+    void tryToCreateMissingDataDoms(oox::core::XmlFilterBase& rFB);
 };
 
 }

include/oox/export/drawingml.hxx
@@ -518,7 +518,7 @@ public:
     OString WriteWdpPicture( const OUString& rFileId, const css::uno::Sequence< sal_Int8 >& rPictureData );
 
     // Diagram helpers
-    OOX_DLLPUBLIC static bool PrepareToWriteAsDiagram(const css::uno::Reference<css::drawing::XShape>& rXShape);
+    OOX_DLLPUBLIC bool PrepareToWriteAsDiagram(const css::uno::Reference<css::drawing::XShape>& rXRootShape);
     OOX_DLLPUBLIC void WriteDiagram(const css::uno::Reference<css::drawing::XShape>& rXShape,
                                     sal_Int32 nDiagramId, sal_Int32 nShapeId = -1);
     void writeDiagramRels(const css::uno::Sequence<css::uno::Sequence<css::uno::Any>>& xRelSeq,

commit 39f331e50bc1349071348dca944c14398d2c7aa9

cid#1680643 Logically dead code
regression from
  commit bd047bbb9e43e0a4432e8e84408c2117a7ffe7a3
  Author: Armin Le Grand (collabora) <Armin.LeGrand@collabora.com>
  Date:   Tue Jan 20 16:18:20 2026 +0100
  SmartArt: Preparations for roundtrip

Change-Id: I65a5bec1c448f10644e4435b13241eaccc58ce22
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198120
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>

svx/source/diagram/IDiagramHelper.cxx
@@ -429,7 +429,7 @@ void IDiagramHelper::disconnectFromSdrObjGroup()
 void IDiagramHelper::connectToSdrObjGroup(com::sun::star::uno::Reference< com::sun::star::drawing::XShape >& rTarget)
 {
     SdrObjGroup* pGroupObject(nullptr);
-    if (pGroupObject && mxGroupShape && rTarget == mxGroupShape)
+    if (mxGroupShape && rTarget == mxGroupShape)
     {
         pGroupObject = dynamic_cast<SdrObjGroup*>(SdrObject::getSdrObjectFromXShape(mxGroupShape));
         if (pGroupObject != nullptr &&  pGroupObject->mp_DiagramHelper.get() == this)