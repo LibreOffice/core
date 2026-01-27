45bf72312bd5a902fdd1e138941d1160e6b1babb
Mike Kaganski <mike.kaganski@collabora.com>
2026-01-27 13:07:22 +0500

    tdf#170386: Delay broadcasting object changes, not drop it
    Creating a SvxShapeText (and inserting it into the model) will call
    SetEditSource with a newly created SvxTextEditSource; and that one
    will register its impl as a listener to underlying SdrObject.
    
    Saving the document, the SvxTextEditSource will be queried for the
    properties of the text, when generating autostyles. Therefore, it
    is important that the properties known to SvxTextEditSource match
    the properties applied to the SdrObject.
    
    The problem was, that the notifications to the listeners were not
    sent, when the model was locked; and when it was unlocked, there
    was no mechanism to re-send notifications at this time. This meant
    that SvxTextEditSource was unaware of e.g. weight changes applied
    to SdrObject inside lockControllers ... unlockControllers pair.
    
    This change introduces a list where objects can register for a
    postponed change notification.
    
    The unit test is implemented as UITest; trying to do that in a
    CppunitTest failed (the test succeeded even without the fix, maybe
    because the CppunitTest has no UI, and so the mechanics of using
    SvxTextEditSource differ).
    
    There already was a case that relied on dropping the notifications,
    specifically ScDrawLayer::SetPageSize, which can move / resize
    objects as a result of underlying page size change (like hiding
    columns), but doesn't want the objects to update their anchoring
    information. That was addressed by adding a flag to ignore adding
    deferred notifications.
    
    Change-Id: I3aac61bf91a2461b024b26ad7e7c199cc7def37b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198187
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Jenkins
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 3f87466d6db4ba694a8ba9f74dcac1b142a15f30)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198361
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

--- chart2/source/view/main/ChartView.cxx
+++ chart2/source/view/main/ChartView.cxx
@@ -1472,6 +1472,11 @@ void ChartView::impl_updateView( bool bCheckLockedCtrler )
         return;
 
     m_bInViewUpdate = true;
+
+    // Rendering the chart must not set its (or its parent) modified status.
+    // Note that unlockControllers() may send notifications that set modified state.
+    ChartModelDisableSetModified dontSetModified(mrChartModel);
+
     //bool bOldRefreshAddIn = m_bRefreshAddIn;
     //m_bRefreshAddIn = false;
     try
@@ -1483,9 +1488,6 @@ void ChartView::impl_updateView( bool bCheckLockedCtrler )
             SolarMutexGuard aSolarGuard;
             m_pDrawModelWrapper->lockControllers();
         }
-
-        // Rendering the chart must not set its (or its parent) modified status
-        ChartModelDisableSetModified dontSetModified(mrChartModel);
 
         //create chart view
         {
