572a9463230d145436a870c4cbbb6e5d4d391a4e
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2026-01-28 10:57:15 +0900

    sc: Introduce Operation abstract class and DeleteContentOperation
    An Operation abstract class is an interface for writing operations
    whcih are an atomic coarse change to the document model. The
    Operation is needed so we can make it easier to handle and extend
    various operations we have, and to separate various task that are
    typically run in an operation. This should also reduce duplication
    of variosu tasks. It also makes dealing with operations easier as
    each operation is now confined in it's own class / file.
    
    Currently the Operation doesn't include a lot yet, but this will
    be added on demand when more and more operations are converted
    to use Oepration derived classes.
    
    This change also includes the first DeleteContentOperation which
    uses the code from ScDocFunc::DeleteContents.
    
    Change-Id: I023b4544041ba4b19fdba00d5c8b3559375653a6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198730
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201952
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>

---
+++ sc/source/ui/operation/Operation.cxx
@@ -0,0 +1,23 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <operation/Operation.hxx>
+#include <SheetViewOperationsTester.hxx>
+#include <docsh.hxx>
+
+namespace sc
+{
+bool Operation::checkSheetViewProtection()
+{
+    sc::SheetViewOperationsTester aSheetViewTester(ScDocShell::GetViewData());
+    return aSheetViewTester.check(meType);
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
