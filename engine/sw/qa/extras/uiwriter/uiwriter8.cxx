f1dd77b14d398eb3629bb93572a65f4318437c19
Mike Kaganski <mike.kaganski@collabora.com>
2026-02-02 10:21:44 +0100

    This test is now expected to work independent of DPI
    After commit fb52efb4aecaf5034c2ef84c185e3d7a6ca8c416 (tdf#163194: Make
    width of comments in margin fixed on print / PDF export, 2026-01-31),
    which made explicit changes to make width of comments in margins stable
    regardless of DPI, this test should produce consistent results. At least
    that is the hope :-)
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198519
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
    Tested-by: Jenkins
    Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
    Change-Id: If1ccc8440797aa511f6dfbf87d15b884652586e1
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/1528
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- engine/sw/qa/extras/uiwriter/uiwriter8.cxx
+++ engine/sw/qa/extras/uiwriter/uiwriter8.cxx
@@ -806,10 +806,6 @@ CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf95239)
 
 CPPUNIT_TEST_FIXTURE(SwUiWriterTest8, testTdf152575)
 {
-    // FIXME: the DPI check should be removed when either (1) the test is fixed to work with
-    // non-default DPI; or (2) unit tests on Windows are made to use svp VCL plugin.
-    if (!IsDefaultDPI())
-        return;
     std::shared_ptr<vcl::pdf::PDFium> pPDFium = vcl::pdf::PDFiumLibrary::get();
     if (!pPDFium)
         return;
