87b19251da0e07a24e35cf2a82957abe9285cf2b
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-02-03 13:36:31 +0000

    add calc ui coverage details
    Change-Id: I23fec7d82b49805ca87fd0efe0b18cba07f9fb1f
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198616
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200110
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- vcl/inc/jsdialog/enabled.hxx
+++ vcl/inc/jsdialog/enabled.hxx
@@ -30,6 +30,10 @@ bool isInterimBuilderEnabledForNotebookbar(std::u16string_view rUIFile);
 std::vector<OUString> completeWriterDialogList(const o3tl::sorted_vector<OUString>& entries);
 // returns vector of writer sidebar .ui files not seen that should be seen to have complete coverage
 std::vector<OUString> completeWriterSidebarList(const o3tl::sorted_vector<OUString>& entries);
+// returns vector of calc dialog .ui files not seen that should be seen to have complete coverage
+std::vector<OUString> completeCalcDialogList(const o3tl::sorted_vector<OUString>& entries);
+// returns vector of calc sidebar .ui files not seen that should be seen to have complete coverage
+std::vector<OUString> completeCalcSidebarList(const o3tl::sorted_vector<OUString>& entries);
 // returns vector of common/shared dialog .ui files not seen that should be seen to have complete coverage
 std::vector<OUString> completeCommonDialogList(const o3tl::sorted_vector<OUString>& entries,
                                                bool linguisticDataAvailable);
