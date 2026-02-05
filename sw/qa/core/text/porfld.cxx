901fb74eea08ca68cb3bc8dd54ead7954979fd2d
Miklos Vajna <vmiklos@collabora.com>
2026-02-05 15:13:12 +0100

    cool#13988 sw redline render mode: handle number portions
    Open the bugdoc, switch to non-standard redline render mode (e.g.
    dispatch .uno:RedlineRenderMode), the "2." number portion is still
    underlined, while this mode doesn't underline insertions otherwise.
    
    The reason this happens is because number portions don't decide redline
    coloring/markup at render time, like SwTextPainter::DrawTextLine()
    does, instead the font to be used is decided earlier at layout time in
    SwTextFormatter::NewNumberPortion().
    
    Fix the problem by creating two fonts in
    SwTextFormatter::NewNumberPortion(), one without the redline markup and
    one with it. And then later in SwNumberPortion::Paint() use the font
    that matches the current rendline render mode. This works, because
    changing the redline render mode means SwNumberPortion::Paint() will be
    invoked, but the layout's portions won't be re-created for performance
    reasons.
    
    Note that similar to table redlines, this just disables the unwanted
    standard redline render decoration. That is probably fine, since the
    surrounding "real" text is still colored gray/red/green. But we possibly
    this can be improved in the future to apply similar coloring even for
    the number portion.
    
    Change-Id: I16b4123d20bf0c14c76bb23e574eb21a719d15df
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198758
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

---
+++ sw/qa/core/text/porfld.cxx
@@ -0,0 +1,71 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <memory>
+
+#include <vcl/gdimtf.hxx>
+
+#include <docsh.hxx>
+#include <wrtsh.hxx>
+
+namespace
+{
+/// Covers sw/source/core/text/porfld.cxx fixes.
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase(u"/sw/qa/core/text/data/"_ustr)
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testNumberPortionRedlineRenderMode)
+{
+    // Given a document with redlines, the "2." number portion is inserted:
+    createSwDoc("redline-number-portion.docx");
+
+    // When redline render mode is standard:
+    SwDocShell* pDocShell = getSwDocShell();
+    std::shared_ptr<GDIMetaFile> xMetaFile = pDocShell->GetPreviewMetaFile();
+
+    // Then make sure we paint an underline:
+    MetafileXmlDump aDumper;
+    xmlDocUniquePtr pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
+    OUString aContent = getXPathContent(pXmlDoc, "(//textarray)[3]/text");
+    CPPUNIT_ASSERT_EQUAL(u"2."_ustr, aContent);
+    OUString aUnderline
+        = getXPath(pXmlDoc, "(//textarray)[3]/preceding-sibling::font[1]", "underline");
+    CPPUNIT_ASSERT_EQUAL(u"1"_ustr, aUnderline);
+
+    // And given "omit inserts" redline render mode:
+    SwWrtShell* pWrtShell = pDocShell->GetWrtShell();
+    SwViewOption aOpt(*pWrtShell->GetViewOptions());
+    aOpt.SetRedlineRenderMode(SwRedlineRenderMode::OmitInserts);
+    pWrtShell->ApplyViewOptions(aOpt);
+
+    // When rendering:
+    xMetaFile = pDocShell->GetPreviewMetaFile();
+
+    // Then make sure we don't paint an underline:
+    pXmlDoc = dumpAndParse(aDumper, *xMetaFile);
+    aContent = getXPathContent(pXmlDoc, "(//textarray)[3]/text");
+    CPPUNIT_ASSERT_EQUAL(u"2."_ustr, aContent);
+    aUnderline = getXPath(pXmlDoc, "(//textarray)[3]/preceding-sibling::font[1]", "underline");
+    // Without the accompanying fix in place, this test would have failed with:
+    // - Expected: 0
+    // - Actual  : 1
+    // i.e. there was an unexpected underline.
+    CPPUNIT_ASSERT_EQUAL(u"0"_ustr, aUnderline);
+}
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
