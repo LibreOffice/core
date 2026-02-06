c8dd921334864b3dc61230fb66d1ad1021edadcf
Justin Luth <justin.luth@collabora.com>
2026-02-06 10:00:42 -0500

    tdf#149240 docx import: don't grabbag ShowingPlcHdr for runSDTs
    This improves my first SDT patch in LO 7.5
    
    This section is tricky to code-read.
    The 'break' is necessary to stop doing 'more stuff'.
    All of the other run properties are 'done'
    when they set the value,
    and only continue to grabbag them
    if they are in blockSDTs.
    
    Without this patch,
    runSDTs were getting a SdtPr grabbag collection
    with only this one ShowingPlcHdr property.
    
    Since the runSdt already knows the value
    it doesn't need to be grabbagged,
    and should 'break' just like all the others.
    
    Change-Id: I5777eea80289b012bb8464e7f45b1c6432f4e5a8
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198847
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Jenkins
    (cherry picked from commit 6068b676b6887bc14901f0da56048f71ce0a4a25)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198956

--- sw/source/writerfilter/dmapper/DomainMapper.cxx
+++ sw/source/writerfilter/dmapper/DomainMapper.cxx
@@ -3433,6 +3433,7 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
             {
                 if (nIntValue)
                     m_pImpl->m_pSdtHelper->SetShowingPlcHdr();
+                break;
             }
 
             if (nSprmId == NS_ooxml::LN_CT_SdtPr_color)
