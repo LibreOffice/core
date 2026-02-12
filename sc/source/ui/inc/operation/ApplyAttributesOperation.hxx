efd7c6d635b6087f8e7aafad6693f6c6e7440f58
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2026-02-12 18:51:02 +0900

    sc: Create operations to apply attributes that are in ScViewFunc
    When changing the attributes for a cell or cell range, there is
    no direct operation in ScDocFunc or similar, but the change is
    performed in ScViewFunc. This refactores the calles and creates
    operations that don't depend much on the view (can't really
    completely get rid of it yet), so we can use those to implement
    sync in a follow-up commit.
    
    Change-Id: I02d5370201f441a09560e49999bd265cfa184d30
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199288
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201965
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>

--- sc/source/ui/inc/operation/ApplyAttributesOperation.hxx
+++ sc/source/ui/inc/operation/ApplyAttributesOperation.hxx
@@ -37,6 +37,44 @@ public:
                              const ScPatternAttr& rPattern, bool bApi);
 };
 
+/** Operation which applies input attributes to marked cells or range and remembers changed ranges. */
+class ApplyAttributesWithChangedRangeOperation : public Operation
+{
+private:
+    ScDocShell& mrDocShell;
+    ScMarkData const& mrMark;
+    ScPatternAttr const& mrPattern;
+    bool mbMultiMarked = false;
+    sal_uInt16 mnExtFlags;
+    ScRangeList maChangeRanges;
+
+    bool runImplementation() override;
+
+public:
+    ApplyAttributesWithChangedRangeOperation(ScDocShell& rDocShell, const ScMarkData& rMark,
+                                             bool bMultiMarked, const ScPatternAttr& rPattern,
+                                             sal_uInt16 nExtFlags, bool bApi);
+    ScRangeList const& getChangedRangeList() { return maChangeRanges; }
+};
+
+/** Operation which applies input attributes to content to a cell. */
+class ApplyAttributesToCellOperation : public Operation
+{
+private:
+    ScDocShell& mrDocShell;
+    ScAddress const& mrPosition;
+    ScPatternAttr const& mrPattern;
+    sal_uInt16 mnExtFlags;
+    ScRangeList maChangeRanges;
+
+    bool runImplementation() override;
+
+public:
+    ApplyAttributesToCellOperation(ScDocShell& rDocShell, ScAddress const& rPosition,
+                                   const ScPatternAttr& rPattern, sal_uInt16 nExtFlags, bool bApi);
+    ScRangeList const& getChangedRangeList() { return maChangeRanges; }
+};
+
 } // end sc namespace
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
