ea86fa03fd459cde3645936fe93c9b4e21e92bfb
codewithvk <vivek.javiya@collabora.com>
2026-02-17 15:34:18 +0530

    a11y: add grouping roles to sidebar paragraph panel toolbars
    Signed-off-by: codewithvk <vivek.javiya@collabora.com>
    Change-Id: I39357a4b755a31a7c1a293ff9a74d423701b16ff
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199534
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-by: Parth Raiyani <parth.raiyani@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200534
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- vcl/source/window/window.cxx
+++ vcl/source/window/window.cxx
@@ -3414,6 +3414,8 @@ void Window::DumpAsPropertyTree(tools::JsonWriter& rJsonWriter)
     sal_uInt16 nAccessibleRole = GetAccessibleRole();
     if (nAccessibleRole == css::accessibility::AccessibleRole::PAGE_TAB_LIST)
         sAccRole = "tablist";
+    else if (nAccessibleRole == css::accessibility::AccessibleRole::GROUP_BOX)
+        sAccRole = "group";
 
     vcl::Window* pAccLabelFor = getAccessibleRelationLabelFor();
     if (pAccLabelFor)
