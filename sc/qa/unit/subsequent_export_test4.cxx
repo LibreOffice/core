LibreOffice/core

commit 108f4aa94b98ca6deffdc169068c175605d5ba67

XLSX: Completely discard formulas containing macros
Change-Id: I8f2b034d83c14cf62f74ee3be1ed23663d5dfdad
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199153
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

sc/source/filter/excel/xetable.cxx
@@ -1016,7 +1016,8 @@ void XclExpFormulaCell::SaveXml( XclExpXmlStream& rStrm )
     if (bWriteFormula)
     {
         ScTokenArray aTokenArray(*mrScFmlaCell.GetCode());
-        // If XLSX export then remove macro tokens from the array
+        bool bValid = true;
+        // Discard formulas containing macros in XLSX export.
         if (!rStrm.IsExportVBA())
         {
             formula::FormulaTokenArrayPlainIterator aIter(aTokenArray);
@@ -1025,38 +1026,25 @@ void XclExpFormulaCell::SaveXml( XclExpXmlStream& rStrm )
             {
                 if (t->GetOpCode() == ocMacro)
                 {
-                    sal_uInt16 nStart = aIter.GetIndex() - 1;
-                    formula::FormulaToken* pNext = aIter.PeekNext();
-                    if (pNext && pNext->GetOpCode() == ocOpen)
-                    {
-                        sal_uInt16 nParenthesis = 0;
-                        do
-                        {
-                            if (pNext->GetOpCode() == ocOpen)
-                                nParenthesis++;
-                            else if (pNext->GetOpCode() == ocClose)
-                                nParenthesis--;
-
-                            aIter.Next();
-                            pNext = aIter.PeekNext();
-                        } while (nParenthesis > 0 && pNext);
-                    }
-                    aTokenArray.RemoveToken(nStart, aIter.GetIndex() - nStart);
-                    aIter.AfterRemoveToken(nStart, aIter.GetIndex() - nStart);
+                    bValid = false;
+                    break;
                 }
                 t = aIter.Next();
             }
         }
-        if (!bTagStarted)
+        if (bValid)
         {
-            rWorksheet->startElement( XML_f,
-                    XML_aca, ToPsz( (mxTokArr && mxTokArr->IsVolatile()) ||
-                        (mxAddRec && mxAddRec->IsVolatile()) ) );
+            if (!bTagStarted)
+            {
+                rWorksheet->startElement(XML_f, XML_aca,
+                                         ToPsz((mxTokArr && mxTokArr->IsVolatile())
+                                               || (mxAddRec && mxAddRec->IsVolatile())));
+            }
+            rWorksheet->writeEscaped(XclXmlUtils::ToOUString(
+                rStrm.GetRoot().GetCompileFormulaContext(), mrScFmlaCell.aPos, &aTokenArray,
+                mrScFmlaCell.GetErrCode()));
+            rWorksheet->endElement(XML_f);
         }
-        rWorksheet->writeEscaped(XclXmlUtils::ToOUString(rStrm.GetRoot().GetCompileFormulaContext(),
-                                                         mrScFmlaCell.aPos, &aTokenArray,
-                                                         mrScFmlaCell.GetErrCode()));
-        rWorksheet->endElement( XML_f );
     }
 
     if( strcmp( sType, "inlineStr" ) == 0 )

commit f20b05d40f628b00f2990d3dd895f3bb23d0ae17

XLSX: Properly handle macros in array formulas
This is a regression from 108f4aa94b98ca6deffdc169068c175605d5ba67,
XML_f element of array formulas containing macros was not closed in
XLSX export.

Change-Id: Iffd482ec23797bc64647a07e991220f373986d95
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199539
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

sc/source/filter/excel/xetable.cxx
@@ -1039,13 +1039,16 @@ void XclExpFormulaCell::SaveXml( XclExpXmlStream& rStrm )
                 rWorksheet->startElement(XML_f, XML_aca,
                                          ToPsz((mxTokArr && mxTokArr->IsVolatile())
                                                || (mxAddRec && mxAddRec->IsVolatile())));
+                bTagStarted = true;
             }
             rWorksheet->writeEscaped(XclXmlUtils::ToOUString(
                 rStrm.GetRoot().GetCompileFormulaContext(), mrScFmlaCell.aPos, &aTokenArray,
                 mrScFmlaCell.GetErrCode()));
-            rWorksheet->endElement(XML_f);
         }
     }
+
+    if (bTagStarted)
+        rWorksheet->endElement(XML_f);
 
     if( strcmp( sType, "inlineStr" ) == 0 )
     {

sc/qa/unit/subsequent_export_test4.cxx
@@ -2470,6 +2470,23 @@ CPPUNIT_TEST_FIXTURE(ScExportTest4, testTdf170565_empty_functions)
     assertXPathContent(pSheet, "/x:worksheet/x:sheetData/x:row[6]/x:c[2]/x:f", u"PRODUCT(,)");
 }
 
+CPPUNIT_TEST_FIXTURE(ScExportTest4, testMacrosInXLSX)
+{
+    // Array formula
+    createScDoc("xls/formula_with_macros.xls");
+    save(u"Calc Office Open XML"_ustr);
+    xmlDocUniquePtr pSheet = parseExport(u"xl/worksheets/sheet1.xml"_ustr);
+    CPPUNIT_ASSERT(pSheet);
+    assertXPath(pSheet, "/x:worksheet/x:sheetData/x:row[2]/x:c[1]/x:f", 0);
+
+    // Regular formula
+    createScDoc("xls/array_formula_with_macros.xls");
+    save(u"Calc Office Open XML"_ustr);
+    pSheet = parseExport(u"xl/worksheets/sheet1.xml"_ustr);
+    CPPUNIT_ASSERT(pSheet);
+    assertXPathContent(pSheet, "/x:worksheet/x:sheetData/x:row[2]/x:c[2]/x:f", u"");
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */