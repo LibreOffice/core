7df9b969a0371d8db7ab57ff419a460786203f38
Xisco Fauli <xiscofauli@libreoffice.org>
2026-02-20 15:55:42 +0100

    tdf#167290: export CreationDate with DateTime format
    Otherwise, the validator fails with
    "The value of CreationDate entry from the document
    Info dictionary and its matching XMP property "xmp:CreateDate"
    are not equivalent
    (Info /CreationDate = D:20260220154049+01'00', XMP xmp:CreateDate = 2025-12-31T11:13:40.000+01)
    
    The call to osl_getDateTimeFromTimeValue was removed in
    commit 07ac61f2c67020ad2941462ea7081f06495bcb92
    Author: Sarper Akdemir <sarper.akdemir.extern@allotropia.de>
    Date:   Mon Aug 28 11:16:34 2023 +0300
    
        tdf#138792: PDF export: fix date of xmp:CreateDate
    
    for no apparent reason
    
    In libreoffice-26-2 and libreoffice-25-8 the test doesn't
    work because the verapdf validator is not implemented
    
    Change-Id: I4c6948769797a4e446239316de2857851b31565e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199886
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199907
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- vcl/source/pdf/pdfwriterimpl_utils.cxx
+++ vcl/source/pdf/pdfwriterimpl_utils.cxx
@@ -237,6 +237,7 @@ void computeDocumentIdentifier(std::vector<sal_uInt8>& o_rIdentifier,
 
     osl_getSystemTime(&aGMT);
     osl_getLocalTimeFromSystemTime(&aGMT, &aTVal);
+    osl_getDateTimeFromTimeValue( &aTVal, &aDT );
     OStringBuffer aCreationMetaDateString(64);
 
     // i59651: we fill the Metadata date string as well, if PDF/A is requested
