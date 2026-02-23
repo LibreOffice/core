6dfc36015891967973411f6be7ffc562caaf86ef
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-02-23 10:40:41 +0100

    Missing include (USHRT_MAX)
    Change-Id: I088c23f4011919ef3a572872622230c3dc2f870a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200037
    Tested-by: Jenkins
    Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>

--- framework/source/fwe/classes/addonmenu.cxx
+++ framework/source/fwe/classes/addonmenu.cxx
@@ -17,6 +17,10 @@
  *   the License at http://www.apache.org/licenses/LICENSE-2.0 .
  */
 
+#include <sal/config.h>
+
+#include <climits>
+
 #include <addonmenu.hxx>
 #include <framework/addonsoptions.hxx>
 #include <menuconfiguration.hxx>
