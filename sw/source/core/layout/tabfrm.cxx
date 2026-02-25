d61b1173bcc7bcd6ab6465431a58d38cbadf1c1c
Michael Stahl <michael.stahl@collabora.com>
2026-02-25 09:33:48 +0100

    sw: layout: invalidate first row when table upper margin changes
    The problem is that when the user inserts a paragraph break in the
    yellow section on the first page, the table below this section
    disappears.
    
    Previously, one line of text of the table was on the first page, but it
    must move to the next page now, and then what happens is that inside
    SwTabFrame::Format() the CalcFlyOffsets() finds an overlap with the text
    frame that was already on the 2nd page; at this time the table print
    area height is 9711, and this causes the table frame to have the Y
    position of the print area set to 10383.
    
    Now the first row is positioned based on that, but then another
    iteration of SwTabFrame::Format() sees its height as 7672 and thus the
    table now fits before the text frame, the table frame upper margin is
    reset to 0.
    
    But the problem is, the first row's position is not invalidated, so all
    the table content remains positioned outside the table frame.
    
    Reportedly this started to happen since commit
    8e3afdb5989d571410350f1d43fcf26492a4eaff and apparently the problem
    doesn't happen when there isn't a fly anchored in the footer but there's
    no obvious relation to the missing table invalidation.
    
    Change-Id: I6552ef3340b47abcfa5eb3a70aa1dc27733da253
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200294
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    (cherry picked from commit 66723c13181e0e56a6a9e55862ece318c9d45f23)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200339

--- sw/source/core/layout/tabfrm.cxx
+++ sw/source/core/layout/tabfrm.cxx
@@ -3929,6 +3929,10 @@ void SwTabFrame::Format( vcl::RenderContext* /*pRenderContext*/, const SwBorderA
         if ( nOldHeight != aRectFnSet.GetHeight(getFramePrintArea()) )
         {
             setFrameAreaSizeValid(false);
+            if (GetLower())
+            {   // ensure first row is repositioned
+                GetLower()->InvalidatePos_();
+            }
         }
     }
 
