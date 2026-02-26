cf27fa6d7879fd199972f036d9b6985f0682aad6
Michael Weghorn <m.weghorn@posteo.de>
2026-02-26 01:08:36 +0100

    tdf#130857 qt weld: Support "Properties: Formatted Field" dlg
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * "Form" -> "Formatted Field"
    * click and drag mouse in the document to insert the
      form control
    * double-click on the inserted form control to open the
      properties dialog
    
    Previously, the top-level dialog was declared as supported
    but these UI files used in the dialog were not, resulting in an
    assert getting triggered when trying to start the dialog:
    
        soffice.bin: /home/michi/development/git/libreoffice/vcl/qt6/../qt5/QtInstanceContainer.cxx:27: auto QtInstanceContainer::move(weld::Widget *, weld::Container *)::(anonymous class)::operator()() const: Assertion `pQtInstanceWidget' failed.
    
    Change-Id: I315f0b91d88ba829347584a70f886f8ea95a6c1c
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200370
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -357,6 +357,8 @@ constexpr auto SUPPORTED_WITH_QT_PARENT = frozen::make_unordered_set<std::u16str
     u"modules/spropctrlr/ui/colorlistbox.ui",
     u"modules/spropctrlr/ui/combobox.ui",
     u"modules/spropctrlr/ui/datefield.ui",
+    u"modules/spropctrlr/ui/formattedcontrol.ui",
+    u"modules/spropctrlr/ui/formattedsample.ui",
     u"modules/spropctrlr/ui/formproperties.ui",
     u"modules/spropctrlr/ui/listbox.ui",
     u"modules/spropctrlr/ui/multiline.ui",
