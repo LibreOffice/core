04067529d04db1148f37350a45e5657cc2330324
Michael Weghorn <m.weghorn@posteo.de>
2026-02-26 17:54:15 +0100

    tdf#130857 qt weld: Support "Content Control List Item Properties" dlg
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * "Form" -> "Content Controls" -> "Drop-Down List"
    * right-click on the newly inserted control and
      activate the "Content Control Properties..."
      menu entry
    * select the "Choose an item" entry in the table
    * press the "Modify" button
    
    Change-Id: I9006182310ac244bf9241d55a55925aa7656a75b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200477
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -211,6 +211,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/swriter/ui/ccdialog.ui",
     u"modules/swriter/ui/columndialog.ui",
     u"modules/swriter/ui/columnwidth.ui",
+    u"modules/swriter/ui/contentcontrollistitemdlg.ui",
     u"modules/swriter/ui/createaddresslist.ui",
     u"modules/swriter/ui/createauthorentry.ui",
     u"modules/swriter/ui/customizeaddrlistdialog.ui",
