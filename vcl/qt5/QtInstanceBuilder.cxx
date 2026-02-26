1948a1d1dca1a1f512012144b71fad2f08950931
Michael Weghorn <m.weghorn@posteo.de>
2026-02-27 00:21:27 +0100

    tdf#130857 qt weld: Support sc "Protect Sheet" dialog
    This means that native Qt widgets are used for that dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog using the UI file can be triggered like this:
    
    * start Calc
    * right-click on the sheet name ("Sheet 1") at the
      bottom tab bar to open the context menu
    * activate the "Protect Sheet..." menu entry
    
    Change-Id: Icdcf09a9428f880c6ecf2a09b70003c7e7d67dae
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200550
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -167,6 +167,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/scalc/ui/optimalcolwidthdialog.ui",
     u"modules/scalc/ui/optimalrowheightdialog.ui",
     u"modules/scalc/ui/pastespecial.ui",
+    u"modules/scalc/ui/protectsheetdlg.ui",
     u"modules/scalc/ui/rowheightdialog.ui",
     u"modules/scalc/ui/scenariodialog.ui",
     u"modules/scalc/ui/selectrange.ui",
