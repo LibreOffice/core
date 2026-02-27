LibreOffice/core

commit be36f4c40bb7b0c1b8ceb974c932a687f5c488be

tdf#169088 Fix PPTX export of OLE animations
Presentations with OLE animations when saved as PPTX can't be opened
in PowerPoint.

When exporting OLE animations, the target of the animation is set to
the `pic` inside the OLE instead of OLE.

Change-Id: Ic3e870fff2e896404960f475c67f44df0aef1860
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193110
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

oox/source/export/shapes.cxx
@@ -3021,7 +3021,9 @@ sal_Int32 ShapeExport::GetNewShapeID( const Reference< XShape >& rXShape, XmlFil
 
     sal_Int32 nID = pFB->GetUniqueId();
 
-    (*mpShapeMap)[ rXShape ] = nID;
+    auto it = mpShapeMap->find(rXShape);
+    if (it == mpShapeMap->end())
+        (*mpShapeMap)[rXShape] = nID;
 
     return nID;
 }

sd/qa/unit/export-tests-ooxml4.cxx
@@ -1520,6 +1520,30 @@ CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testTextStylesXML)
                 u"6000");
 }
 
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testOLEObjectAnimationTarget)
+{
+    createSdImpressDoc("pptx/tdf169088.pptx");
+    save(u"Impress Office Open XML"_ustr);
+
+    xmlDocUniquePtr pXmlDoc = parseExport(u"ppt/slides/slide1.xml"_ustr);
+
+    const OUString sOleId = getXPath(
+        pXmlDoc, "/p:sld/p:cSld/p:spTree/p:graphicFrame/p:nvGraphicFramePr/p:cNvPr", "id");
+
+    // Check animation target spid matches OLE object id
+    assertXPath(pXmlDoc,
+                "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst/p:seq/p:cTn/p:childTnLst/p:par/"
+                "p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:set/"
+                "p:cBhvr/p:tgtEl/p:spTgt",
+                "spid", sOleId);
+
+    assertXPath(pXmlDoc,
+                "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst/p:seq/p:cTn/p:childTnLst/p:par/"
+                "p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:animEffect/"
+                "p:cBhvr/p:tgtEl/p:spTgt",
+                "spid", sOleId);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 0d1046718152ab4aef2c1188702514c1238d57b5

PPTX: Use correct shape-id for connector shapes
This is a regression from be36f4c40bb7b0c1b8ceb974c932a687f5c488be

bug-document: forum-mso-de-136509.pptx
Change-Id: If1f4280459245c1cb30cbe5559d22d22d91c558c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200003
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

include/oox/export/shapes.hxx
@@ -200,6 +200,7 @@ public:
 
     sal_Int32 GetNewShapeID( const css::uno::Reference< css::drawing::XShape >& rShape );
     sal_Int32 GetNewShapeID( const css::uno::Reference< css::drawing::XShape >& rShape, ::oox::core::XmlFilterBase* pFB );
+    sal_Int32 GetTmpShapeID();
     sal_Int32 GetShapeID( const css::uno::Reference< css::drawing::XShape >& rShape );
     static sal_Int32 GetShapeID( const css::uno::Reference< css::drawing::XShape >& rShape, ShapeHashMap* pShapeMap );
 };

oox/source/export/shapes.cxx
@@ -1456,11 +1456,12 @@ void ShapeExport::WriteGraphicObjectShapePart( const Reference< XShape >& xShape
     if (GetProperty(xShapeProps, u"OnClick"_ustr))
         mAny >>= eClickAction;
 
-    pFS->startElementNS( mnXmlNamespace, XML_cNvPr,
-                         XML_id,     OString::number(GetNewShapeID(xShape)),
-                         XML_name,   GetShapeName(xShape),
-                         XML_descr,  sax_fastparser::UseIf(sDescr, !sDescr.isEmpty()),
-                         XML_hidden, sax_fastparser::UseIf("1", !bVisible));
+    sal_Int32 nShapeId = GetShapeID(xShape) == -1 ? GetNewShapeID(xShape) : GetTmpShapeID();
+
+    pFS->startElementNS(mnXmlNamespace, XML_cNvPr, XML_id, OString::number(nShapeId), XML_name,
+                        GetShapeName(xShape), XML_descr,
+                        sax_fastparser::UseIf(sDescr, !sDescr.isEmpty()), XML_hidden,
+                        sax_fastparser::UseIf("1", !bVisible));
 
     if (eClickAction != presentation::ClickAction_NONE)
     {
@@ -3072,9 +3073,13 @@ sal_Int32 ShapeExport::GetNewShapeID( const Reference< XShape >& rXShape, XmlFil
     auto it = mpShapeMap->find(rXShape);
     if (it == mpShapeMap->end())
         (*mpShapeMap)[rXShape] = nID;
+    else
+        nID = it->second;
 
     return nID;
 }
+
+sal_Int32 ShapeExport::GetTmpShapeID() { return GetFB()->GetUniqueId(); }
 
 sal_Int32 ShapeExport::GetShapeID( const Reference< XShape >& rXShape )
 {

sd/qa/unit/export-tests-ooxml4.cxx
@@ -1886,6 +1886,31 @@ CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testTdf114443_PPSX)
                 u"application/vnd.openxmlformats-officedocument.presentationml.slideshow.main+xml");
 }
 
+CPPUNIT_TEST_FIXTURE(SdOOXMLExportTest4, testConnectorShapeAnimationTarget)
+{
+    createSdImpressDoc("pptx/connector-shape-animations.pptx");
+    save(u"Impress Office Open XML"_ustr);
+
+    xmlDocUniquePtr pXmlDoc = parseExport(u"ppt/slides/slide1.xml"_ustr);
+
+    const OUString sConnectorId = getXPath(
+        pXmlDoc, "/p:sld/p:cSld/p:spTree/p:cxnSp/p:nvCxnSpPr/p:cNvCxnSpPr/a:stCxn", "id");
+
+    const OUString sShapeId = getXPath(
+        pXmlDoc, "/p:sld/p:cSld/p:spTree/p:graphicFrame/p:nvGraphicFramePr/p:cNvPr", "id");
+
+    // Check connector and shape both have the same ID
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Connector and the shape doesn't have the same ID", sConnectorId,
+                                 sShapeId);
+
+    // Check animation spid matches ShapeId
+    assertXPath(pXmlDoc,
+                "/p:sld/p:timing/p:tnLst/p:par/p:cTn/p:childTnLst/p:seq/p:cTn/p:childTnLst/p:par/"
+                "p:cTn/p:childTnLst/p:par/p:cTn/p:childTnLst/p:par[2]/p:cTn/p:childTnLst/p:set/"
+                "p:cBhvr/p:tgtEl/p:spTgt",
+                "spid", sShapeId);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */