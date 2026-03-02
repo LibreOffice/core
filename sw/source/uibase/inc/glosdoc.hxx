7a2efe02293382374cd80f9d23215740e731c755
Michael Stahl <michael.stahl@collabora.com>
2026-03-02 19:47:00 +0100

    sw: improve error reporting of networked AutoText
    Every error about an AutoText path is reported as "Inadmissible path",
    which might imply that the path doesn't exist.
    
    In the situation where the AutoText is on a WebDAV share that is mounted
    by Windows Explorer, the path will be a file:// URL, and it is possible
    that the user needs to authenticate before being able to access the
    files, but it's not known how an authentication dialog could be shown in
    LO (the Windows File Picker dialog is able to do this btw), so detect
    this situation and show a more specific error message. (As far as
    possible; we cannot tell if the problem is that the user is not
    authenticated or if it is that the user is not authorised to access).
    
    Use `PathIsNetworkPathW` to check if it is a mounted
    network location.
    
    In case the network location is not reachable, the error code will be
    different and the generic error dialog will be shown.
    
    Change-Id: I633ea331f957edb4a943426464524a5decf0fe15
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200856
    Tested-by: allotropia jenkins <jenkins@allotropia.de>
    Reviewed-by: Thorsten Behrens <thorsten.behrens@collabora.com>

--- sw/source/uibase/inc/glosdoc.hxx
+++ sw/source/uibase/inc/glosdoc.hxx
@@ -32,6 +32,9 @@ typedef tools::SvRef<SwDocShell> SwDocShellRef;
 #endif
 #include <cppuhelper/weakref.hxx>
 
+#include <com/sun/star/ucb/IOErrorCode.hpp>
+
+#include <optional>
 #include <vector>
 #include <swdllapi.h>
 
@@ -46,7 +49,7 @@ class SW_DLLPUBLIC SwGlossaries
     UnoAutoTextEntries      m_aGlossaryEntries;
 
     OUString                m_aPath;
-    std::vector<OUString>   m_aInvalidPaths;
+    std::vector<std::pair<OUString, std::optional<css::ucb::IOErrorCode>>> m_aInvalidPaths;
     std::vector<OUString>   m_PathArr;
     std::vector<OUString>   m_GlosArr;
     bool                m_bError;
