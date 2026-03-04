022c4ad8a99fba43e17d6cba4b860c1dc6811cae
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2026-03-05 00:09:56 +0900

    sc: Create Sheet views in named sheet view OOXML Import
    Previously we only read the structure of NamedSheetView fragments
    from OOXML document to a temporary structure, but did nothing with
    the imported data yet. This change creates new sheet view and
    applies filters and sorting to the autofilters according to the data
    in the imported structures. The bulk of the work for this is done
    in the newly introduced NamedSheetViewImporter class.
    
    Change-Id: I4524ef797ef945969b9e5db73c43c69debe89c3e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200946
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202029
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit a07d6f7787685b1579be897638357e56e85f32e0)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205469
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sc/Library_scfilt.mk
+++ sc/Library_scfilt.mk
@@ -175,6 +175,7 @@ $(eval $(call gb_Library_add_exception_objects,scfilt,\
 	sc/source/filter/oox/personsfragment \
 	sc/source/filter/oox/threadedcommentsfragment \
 	sc/source/filter/oox/NamedSheetViewFragment \
+	sc/source/filter/oox/NamedSheetViewImporter \
 	sc/source/filter/oox/condformatbuffer \
 	sc/source/filter/oox/condformatcontext \
 	sc/source/filter/oox/connectionsbuffer \
