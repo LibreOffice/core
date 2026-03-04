LibreOffice/core

commit f04247351a22d77f4f219342fdc425a832e711df

uitest: use different names for header and footer elements
Change-Id: Ie0aced391136a1417b50f05e4b609ce682ae7afc
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/190893
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Tested-by: Jenkins

svx/source/dialog/hdft.cxx
@@ -167,6 +167,10 @@ SvxHFPage::SvxHFPage(weld::Container* pPage, weld::DialogController* pController
         m_xHeightEdit->set_help_id( u"SVX_HID_FOOTER_SPINHEIGHT"_ustr );
         m_xHeightDynBtn->set_help_id( u"SVX_HID_FOOTER_CHECKAUTOFIT"_ustr );
         m_xBackgroundBtn->set_help_id( u"SVX_HID_FOOTER_BUTTONMORE"_ustr );
+
+        // uitest
+        m_xCntSharedBox->set_buildable_name(m_xCntSharedBox->get_buildable_name() + "-footer");
+        m_xCntSharedFirstBox->set_buildable_name(m_xCntSharedFirstBox->get_buildable_name() + "-footer");
     }
     else //Header
     {
@@ -174,6 +178,10 @@ SvxHFPage::SvxHFPage(weld::Container* pPage, weld::DialogController* pController
         m_xFrame->set_label(SvxResId(RID_SVXSTR_HEADER));
         m_xFrame->set_help_id(u"svx/ui/headfootformatpage/HFormatPage"_ustr);
         m_xTurnOnBox = m_xBuilder->weld_check_button(u"checkHeaderOn"_ustr);
+
+        // uitest
+        m_xCntSharedBox->set_buildable_name(m_xCntSharedBox->get_buildable_name() + "-header");
+        m_xCntSharedFirstBox->set_buildable_name(m_xCntSharedFirstBox->get_buildable_name() + "-header");
     }
     m_xTurnOnBox->show();
 

sc/qa/uitest/calc_tests8/tdf144940.py
@@ -29,8 +29,8 @@ class tdf144940(UITestCase):
                     select_pos(xTabs, "4")
 
                     xCheckHeaderOn = xDialog.getChild("checkHeaderOn")
-                    xCheckSameFP = xDialog.getChild("checkSameFP")
-                    xCheckSameLR = xDialog.getChild("checkSameLR")
+                    xCheckSameFP = xDialog.getChild("checkSameFP-header")
+                    xCheckSameLR = xDialog.getChild("checkSameLR-header")
 
                     self.assertEqual("true", get_state_as_dict(xCheckHeaderOn)["Selected"])
                     self.assertEqual("true", get_state_as_dict(xCheckSameLR)["Selected"])

sc/qa/uitest/calc_tests9/tdf142763.py
@@ -26,7 +26,7 @@ class Tdf142763(UITestCase):
 
                 xHeaderOnBtn = xPageFormatDialog.getChild("checkHeaderOn")
                 xHeaderOnBtn.executeAction("CLICK", tuple())
-                xFirstHeaderSameBtn = xPageFormatDialog.getChild("checkSameFP")
+                xFirstHeaderSameBtn = xPageFormatDialog.getChild("checkSameFP-header")
                 xFirstHeaderSameBtn.executeAction("CLICK", tuple())
 
                 with self.ui_test.execute_dialog_through_command(".uno:EditHeaderAndFooter") as xHeaderDialog:
@@ -53,7 +53,7 @@ class Tdf142763(UITestCase):
 
                 xFooterOnBtn = xPageFormatDialog.getChild("checkFooterOn")
                 xFooterOnBtn.executeAction("CLICK", tuple())
-                xFirstFooterSameBtn = xPageFormatDialog.getChild("checkSameFP")
+                xFirstFooterSameBtn = xPageFormatDialog.getChild("checkSameFP-header")
                 xFirstFooterSameBtn.executeAction("CLICK", tuple())
 
                 with self.ui_test.execute_dialog_through_command(".uno:EditHeaderAndFooter") as xFooterDialog:

sw/qa/uitest/writer_tests7/tdf46561.py
@@ -44,7 +44,7 @@ class tdf46561(UITestCase):
                     xTabs = PageDialog.getChild("tabcontrol")
                     select_pos(xTabs, "4")
 
-                    Button = xTabs.getChild('checkSameLR')
+                    Button = xTabs.getChild('checkSameLR-header')
                     Button.executeAction("CLICK",tuple())
 
             # We should be back to the starting state after 2*k on/off changes

sw/qa/uitest/writer_tests8/tdf140117.py
@@ -52,7 +52,7 @@ class tdf140117(UITestCase):
                         select_pos(xTabs, "4")
 
                         # Change option "same content on left and right pages" for the next iteration
-                        Button = xTabs.getChild('checkSameLR')
+                        Button = xTabs.getChild('checkSameLR-header')
                         Button.executeAction("CLICK", tuple())
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

sw/qa/uitest/writer_tests8/tdf141158.py
@@ -23,7 +23,7 @@ class TestTdf141158(UITestCase):
                 xTabs = xDialog.getChild("tabcontrol")
                 select_pos(xTabs, "4")
                 # make the same left right page header state to off  and apply the setting
-                checkSameLR = xDialog.getChild("checkSameLR")
+                checkSameLR = xDialog.getChild("checkSameLR-header")
                 checkSameLR.executeAction("CLICK",tuple())
 
             # go to the left header

commit fd9887f802b9baefcb3fb2a4408b52909c6e3327

jsdialog: Don't do anything in JSWidget::set_buildable_name
As

    commit ef1acd9cba6521ae00f71f7bb8336f9e20d72796
    Author: Sahil Gautam <sahil.gautam@collabora.com>
    Date:   Fri Feb 27 17:21:56 2026 +0530

        fix: don't use set_buildable_name with LOKit as it crashes

        JSWidget::set_buildable_name isn't handled on the online side, it has
        an assert(false) which is reached because of changes introduced in
        commit f04247351a22d77f4f219342fdc425a832e711df

mentions, calling weld::Widget::set_buildable_name so far
triggered an assert. That commit also introduced a workaround
in SvxHFPage to avoid calling the method when LibreOfficeKit
is active.

Instead of spreading workarounds across the code base to
avoid calling the method altogether, rather just let the
method do nothing for the jsdialog case.

This also reverts the workaround from the above commit as it
should be obsolete now.

Change-Id: I81bf87267b43d29276fae7894e4af7e3ea2e6780
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/200889
Tested-by: Jenkins
Reviewed-by: Sahil Gautam <sahil.gautam@collabora.com>

svx/source/dialog/hdft.cxx
@@ -19,7 +19,6 @@
 
 #include <sal/config.h>
 
-#include <comphelper/lok.hxx>
 #include <o3tl/unit_conversion.hxx>
 #include <svl/itemiter.hxx>
 #include <sfx2/objsh.hxx>
@@ -173,12 +172,8 @@ SvxHFPage::SvxHFPage(weld::Container* pPage, weld::DialogController* pController
         m_xBackgroundBtn->set_help_id( u"SVX_HID_FOOTER_BUTTONMORE"_ustr );
 
         // uitest
-        if (!comphelper::LibreOfficeKit::isActive())
-        {
-            m_xCntSharedBox->set_buildable_name(m_xCntSharedBox->get_buildable_name() + "-footer");
-            m_xCntSharedFirstBox->set_buildable_name(m_xCntSharedFirstBox->get_buildable_name()
-                                                     + "-footer");
-        }
+        m_xCntSharedBox->set_buildable_name(m_xCntSharedBox->get_buildable_name() + "-footer");
+        m_xCntSharedFirstBox->set_buildable_name(m_xCntSharedFirstBox->get_buildable_name() + "-footer");
     }
     else //Header
     {
@@ -188,12 +183,8 @@ SvxHFPage::SvxHFPage(weld::Container* pPage, weld::DialogController* pController
         m_xTurnOnBox = m_xBuilder->weld_check_button(u"checkHeaderOn"_ustr);
 
         // uitest
-        if (!comphelper::LibreOfficeKit::isActive())
-        {
-            m_xCntSharedBox->set_buildable_name(m_xCntSharedBox->get_buildable_name() + "-header");
-            m_xCntSharedFirstBox->set_buildable_name(m_xCntSharedFirstBox->get_buildable_name()
-                                                     + "-header");
-        }
+        m_xCntSharedBox->set_buildable_name(m_xCntSharedBox->get_buildable_name() + "-header");
+        m_xCntSharedFirstBox->set_buildable_name(m_xCntSharedFirstBox->get_buildable_name() + "-header");
     }
     m_xTurnOnBox->show();
 

vcl/inc/jsdialog/jsdialogbuilder.hxx
@@ -427,11 +427,10 @@ public:
             m_pSender->sendClosePopup(nWindowId);
     }
 
-    virtual void set_buildable_name(const OUString& rName) override
+    virtual void set_buildable_name(const OUString&) override
     {
-        SalInstanceWidget::set_buildable_name(rName);
-        assert(false); // we remember old name in GetLOKWeldWidgetsMap()
-        // TODO: implement renaming or avoid it for LOK
+        // we remember old name in GetLOKWeldWidgetsMap()
+        // Therefore, avoid renaming for LOK
     }
 };
 