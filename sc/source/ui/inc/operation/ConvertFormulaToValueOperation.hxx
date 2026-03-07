d05a7ae4d323e3ac69d6bd8888100c487ef70e5d
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2026-03-07 02:09:51 +0000

    sc: Introduce ConvertFormulaToValueOperation, moving code from ScDocFunc
    Pure refactoring only.
    
    Converts the formula to computed value (i.e. =A1+B1 converted to 3).
    
    Change-Id: Ia6a0cad4afafac709a2f99c09a7ee3024d0c7195
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201331
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202086
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 65fce95267c98d355741fc073790f5d0cf92d98c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205434
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sc/source/ui/inc/operation/ConvertFormulaToValueOperation.hxx
@@ -0,0 +1,33 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "Operation.hxx"
+#include <address.hxx>
+
+class ScDocShell;
+
+namespace sc
+{
+/** Operation which converts formula cells to their computed values. */
+class ConvertFormulaToValueOperation : public Operation
+{
+private:
+    ScDocShell& mrDocShell;
+    ScRange maRange;
+
+    bool runImplementation() override;
+
+public:
+    ConvertFormulaToValueOperation(ScDocShell& rDocShell, const ScRange& rRange, bool bInteraction);
+};
+} // end sc namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
