f65888c1b4adb6864d435badd626968ee3810208
Mike Kaganski <mike.kaganski@collabora.com>
2026-03-08 17:50:01 +0500

    tdf#171174: also correct target index for Move/Copy Sheet dialog
    See also: I13504468119706597ca25c14309e6548ea4ca588.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201210
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins
    Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
    Change-Id: Ifcc2afac4e8382794328e487da50fb54a5af76cd
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/1565
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>

--- engine/sc/source/ui/view/tabvwshf.cxx
+++ engine/sc/source/ui/view/tabvwshf.cxx
@@ -216,7 +216,6 @@ void ScTabViewShell::ExecuteTable( SfxRequest& rReq )
                 SCTAB nTab = rViewData.GetTabNumber();
                 OUString aDocName = GetViewData().GetDocShell()->GetTitle(SFX_TITLE_FULLNAME);
                 sal_uInt16 nDoc = 0;
-                bool bCpy = true;
 
                 SfxObjectShell* pSh = SfxObjectShell::GetFirst();
                 ScDocShell* pScSh = nullptr;
@@ -240,7 +239,7 @@ void ScTabViewShell::ExecuteTable( SfxRequest& rReq )
                     pSh = SfxObjectShell::GetNext( *pSh );
                 }
 
-                MoveTable( nDoc, nTab + 1, bCpy );
+                MoveTable(nDoc, nTab + 1, true);
             }
             break;
 
@@ -1010,6 +1009,15 @@ void ScTabViewShell::DoMoveTableFromDialog( SfxRequest& rReq, const VclPtr<Abstr
     sal_uInt16 nDoc = pDlg->GetSelectedDocument();
     SCTAB nTab = pDlg->GetSelectedTable();
     bool bCpy = pDlg->GetCopyTable();
+    if (!bCpy)
+    {
+        SCTAB nOldIndex = GetViewData().GetMarkData().GetFirstSelected();
+        if (nTab > nOldIndex)
+        {
+            // move right: target index will be one less after source tab is removed
+            --nTab;
+        }
+    }
     bool bRna = pDlg->GetRenameTable();
     OUString aTabName;
     // Leave aTabName string empty, when Rename is FALSE.
