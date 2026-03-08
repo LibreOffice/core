4edcfd4edfeb0b400e9c04546e6e160088f4b85d
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2026-03-08 04:30:44 +0000

    sc: Move ScDocFunc::MoveBlock to MoveBlockOperation
    Moves a block of cells from source range to destination position.
    
    Pure refactoring, no behavior change.
    
    Change-Id: Ib238c40a13fb5006d2c5827685a6b6bd86c21926
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201474
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202098
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit afe8e54bc60224bfce0bafa718650e92159455c0)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205534
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sc/source/ui/inc/operation/MoveBlockOperation.hxx
@@ -0,0 +1,37 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "Operation.hxx"
+#include <address.hxx>
+
+class ScDocShell;
+
+namespace sc
+{
+/** Moves or copies a block of cells from a source range to a destination position. */
+class MoveBlockOperation : public Operation
+{
+private:
+    ScDocShell& mrDocShell;
+    ScRange maSource;
+    ScAddress maDestPos;
+    bool mbCut;
+    bool mbPaint;
+
+    bool runImplementation() override;
+
+public:
+    MoveBlockOperation(ScDocShell& rDocShell, const ScRange& rSource, const ScAddress& rDestPos,
+                       bool bCut, bool bRecord, bool bPaint, bool bApi);
+};
+} // end sc namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
