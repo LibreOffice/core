996b912b10cadcddb52f7cbe124f05edeee9fbab
Skyler Grey <skyler.grey@collabora.com>
2026-03-10 09:08:02 +0000

    fix: gate new clipboard link behavior to LOK
    In Id7d85333f76f06d83a4966deb551f3846a6a6964 I forced link pasting
    when a link was detected. Unfortunately this works poorly in classic
    where pasting a file creates a link and should instead open the "insert
    section" dialog. It's better to restrict this fix only to LOK to avoid
    breaking this other case
    
    Change-Id: I807509541419b7465ca66d85e9c497266a6a6964
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201357
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- sw/source/uibase/dochdl/swdtflvr.cxx
+++ sw/source/uibase/dochdl/swdtflvr.cxx
@@ -3384,15 +3384,17 @@ bool SwTransferable::PasteFileList( const TransferableDataHelper& rData,
                                     SwWrtShell& rSh, bool bLink,
                                     const Point* pPt, bool bMsg )
 {
-    for( sal_uInt32 i = 0, nFormatCount = rData.GetFormatCount(); ( i < nFormatCount ) && !bLink; ++i )
-    {
-        if( SotClipboardFormatId::FILE_LIST == rData.GetFormat( i ) )
+    if ( comphelper::LibreOfficeKit::isActive() ) {
+        for( sal_uInt32 i = 0, nFormatCount = rData.GetFormatCount(); ( i < nFormatCount ) && !bLink; ++i )
         {
-            const DataFlavor aFlavor( rData.GetFormatDataFlavor( i ) );
-
-            if( aFlavor.MimeType.indexOf( "text/uri-list" ) > -1 )
+            if( SotClipboardFormatId::FILE_LIST == rData.GetFormat( i ) )
             {
-                bLink = true;
+                const DataFlavor aFlavor( rData.GetFormatDataFlavor( i ) );
+
+                if( aFlavor.MimeType.indexOf( "text/uri-list" ) > -1 )
+                {
+                    bLink = true;
+                }
             }
         }
     }
