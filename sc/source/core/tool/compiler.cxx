6bb1277a6e46268b113111a380e159cf8cf7b605
Ujjawal Kumar <ujjawal.kumarchouhan@collabora.com>
2026-03-14 01:24:45 +0530

    xlsx: sanitize defined names containing true or false as name
    Change-Id: I31c643a93520b77a814ca6f9509b003103c78dde
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201674
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -5470,6 +5470,9 @@ OUString ScCompiler::SanitizeDefinedName(const OUString& rStr, const ScDocument&
             != ScRefFlags::ZERO))
         bValidName = false;
 
+    if (sName.equalsIgnoreAsciiCase("true") || sName.equalsIgnoreAsciiCase("false"))
+        bValidName = false;
+
     if (!bValidName || sName != rStr)
     {
         sName = bValidName ? sName : "_" + sName;
