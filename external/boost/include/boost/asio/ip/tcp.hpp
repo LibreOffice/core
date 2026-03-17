4bbeb86e97899f618a990f54dd891b1ad4b2e057
Miklos Vajna <vmiklos@collabora.com>
2026-03-17 08:36:29 +0100

    boost: fix -Werror=overloaded-virtual=
    Seen with GCC-15. See
    <https://gerrit.libreoffice.org/c/core/+/201714/11#message-b41756267426a3a0d9373f6378e56c77d4beb8fa>.
    
    Change-Id: Idcfc454a0a396a32e3d03793241aa74fbfb389a1
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201888
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208735
    Tested-by: Jenkins

--- external/boost/include/boost/asio/ip/tcp.hpp
+++ external/boost/include/boost/asio/ip/tcp.hpp
@@ -28,5 +28,6 @@
 #pragma GCC diagnostic ignored "-Wunused-macros"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
 #include_next <boost/asio/ip/tcp.hpp>
 #pragma GCC diagnostic pop
