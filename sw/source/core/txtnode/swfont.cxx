7db762d33a1e7619b472a1521a66adc2811fe5f1
László Németh <nemeth@numbertext.org>
2026-03-18 00:03:30 +0100

    tdf#169491 tdf#168251 sw glyph scaling: fix all scale width regressions
    Do not disable scale width character setting, if it is applied in a text
    span (except when the paragraph uses automatic glyph scaling, yet).
    
    Regression since commit 45ec7bd76196dcc60b4c2db2f6f00623ecbaf5a4
    "tdf#168251 cui offapi xmloff sw glyph scaling: extend UNO/UX/ODF".
    
    Follow-up to commit 7ed8a8ee39951f74e364091fafff20269359ba15
    "tdf#171161 tdf#168251 sw glyph scaling: fix scale width regression".
    
    Change-Id: Ic42e58d1b96d9697eb0ddd0d7b999171fc0f39eb
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201946
    Tested-by: László Németh <nemeth@numbertext.org>
    Reviewed-by: László Németh <nemeth@numbertext.org>
    (cherry picked from commit f25371fa95b2c46ff4a73ef6995c341c157e62d4)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202089
    Tested-by: Jenkins
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- sw/source/core/txtnode/swfont.cxx
+++ sw/source/core/txtnode/swfont.cxx
@@ -1146,7 +1146,7 @@ void SwSubFont::DrawText_( SwDrawTextInfo &rInf, const bool bGrey )
         ChgFnt( rInf.GetShell(), rInf.GetOut() );
 
     // change glyph scaling, if needed
-    if ( GetPropWidth() != rInf.GetScaleWidth() )
+    if ( rInf.GetScaleWidth() > 0 && GetPropWidth() != rInf.GetScaleWidth() )
     {
         SetPropWidth( rInf.GetScaleWidth() );
         ChgFnt( rInf.GetShell(), rInf.GetOut() );
