3b1edc4b5957e1c291a7311e3d681c3402607250
Karthik Godha <karthik.godha@collabora.com>
2026-03-18 18:13:17 +0530

    tdf#168964: OLEs inside a frame can't have absolute positioning
    When exporting to DOCX, OLEs inside a frame are using
    "position:absolute" in the style attribute. When a frame is exported to
    DOCX, it becomes a text-box. Child objects of a text box can't have
    absolute positioning, they can only be positioned relative to the
    text-box.
    
    bug-document: fdo31183-1.odt
    Change-Id: Ifddf80dbd2402b6182b27467239bb6a2bf4dd71b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201999
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
    (cherry picked from commit a8aef3915f7479f9b6b478971e7f1f7bdf756ab7)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202813

--- sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
+++ sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
@@ -703,6 +703,21 @@ CPPUNIT_TEST_FIXTURE(Test, testQFormatPreservation)
     assertXPath(pXmlStyles, "//w:style[@w:styleId='No spacing']/w:qFormat", 0);
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf168964)
+{
+    createSwDoc("tdf168964.odt");
+    save(TestFilter::DOCX);
+
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+
+    OUString sStyle
+        = getXPath(pXmlDoc, "//wps:txbx/w:txbxContent/w:p[2]/w:r/w:object/v:shape", "style");
+
+    // Test that OLEs inside a textbox are anchored as-char
+    // Before the fix the 'style' attribute contained "position:absolute", making it to-char
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(-1), sStyle.indexOf("position:absolute"));
+}
+
 } // end of anonymous namespace
 CPPUNIT_PLUGIN_IMPLEMENT();
 
