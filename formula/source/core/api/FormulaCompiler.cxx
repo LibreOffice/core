LibreOffice/core

commit bce84f279755171addf29b61f4e8b09e89026c44

sc: Add new ocUDExternal token for user-defined functions
Excel uses "_xludf." prefix for formulas containing unkown external
functions. A new token ocUDExternal is created to handle unknown
external functions

bug-document: forum-mso-en4-352508.xls
Change-Id: If08469e496ff76bb8b912211bd82c67363711d74
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/199631
Reviewed-by: Balazs Varga <balazs.varga@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

formula/source/core/api/FormulaCompiler.cxx
@@ -1735,6 +1735,7 @@ void FormulaCompiler::Factor()
                     // functions may have to be recalculated or not, we don't
                     // know, classify as ONLOAD_LENIENT.
                 case ocExternal:
+                case ocUDExternal:
                     if (mpToken->GetExternal() == "com.sun.star.sheet.addin.Analysis.getRandbetween")
                         pArr->SetExclusiveRecalcModeAlways();
                     else
@@ -1885,6 +1886,7 @@ void FormulaCompiler::Factor()
         }
         else if ((SC_OPCODE_START_2_PAR <= eOp && eOp < SC_OPCODE_STOP_2_PAR)
                 || eOp == ocExternal
+                || eOp == ocUDExternal
                 || eOp == ocMacro
                 || eOp == ocAnd
                 || eOp == ocOr
@@ -2653,7 +2655,10 @@ const FormulaToken* FormulaCompiler::CreateStringFromToken( OUStringBuffer& rBuf
         // Don't export "#name!" in OOXML
     }
     else if( static_cast<sal_uInt16>(eOp) < mxSymbols->getSymbolCount())        // Keyword:
-        rBuffer.append( mxSymbols->getSymbol( eOp));
+    {
+        if (eOp != ocUDExternal || maArrIterator.PeekNext()->GetOpCode() == ocOpen)
+            rBuffer.append(mxSymbols->getSymbol(eOp));
+    }
     else
     {
         SAL_WARN( "formula.core","unknown OpCode");

formula/inc/core_resource.hrc
@@ -964,6 +964,7 @@ const std::pair<const char *, int> RID_STRLIST_FUNCTION_NAMES_ENGLISH_OOXML[] =
     { "_xlfn.ORG.LIBREOFFICE.RANDBETWEEN.NV" , SC_OPCODE_RANDBETWEEN_NV },
     { "_xlfn.RANDARRAY" , SC_OPCODE_RANDARRAY },
     { "#REF!", SC_OPCODE_STOP },
+    { "_xludf.", SC_OPCODE_UD_EXTERNAL },
     { nullptr,  -1 }
 };
 

formula/source/core/api/token.cxx
@@ -79,7 +79,7 @@ bool FormulaToken::IsFunction() const
         || (SC_OPCODE_START_1_PAR <= eOp && eOp < SC_OPCODE_STOP_1_PAR)     // one parameter
         || (SC_OPCODE_START_2_PAR <= eOp && eOp < SC_OPCODE_STOP_2_PAR)     // x parameters (cByte==0 in
                                                                             // FuncAutoPilot)
-        || eOp == ocMacro || eOp == ocExternal                  // macros, AddIns
+        || eOp == ocMacro || eOp == ocExternal || eOp == ocUDExternal       // macros, AddIns
         || eOp == ocAnd || eOp == ocOr                          // former binary, now x parameters
         || (eOp >= ocInternalBegin && eOp <= ocInternalEnd)     // internal
         ));
@@ -88,9 +88,8 @@ bool FormulaToken::IsFunction() const
 
 sal_uInt8 FormulaToken::GetParamCount() const
 {
-    if ( eOp < SC_OPCODE_STOP_DIV && eOp != ocExternal && eOp != ocMacro &&
-         !FormulaCompiler::IsOpCodeJumpCommand( eOp ) &&
-         eOp != ocPercentSign )
+    if (eOp < SC_OPCODE_STOP_DIV && eOp != ocExternal && eOp != ocMacro && eOp != ocUDExternal
+        && !FormulaCompiler::IsOpCodeJumpCommand(eOp) && eOp != ocPercentSign)
         return 0;       // parameters and specials
                         // ocIf... jump commands not for FAP, have cByte then
 //2do: bool parameter whether FAP or not?
@@ -435,7 +434,7 @@ bool FormulaTokenArray::AddFormulaToken(
                     AddStringXML( aStrVal );
                 else if ( eOpCode == ocStringName )
                     AddStringName( aStrVal );
-                else if ( eOpCode == ocExternal || eOpCode == ocMacro )
+                else if ( eOpCode == ocExternal || eOpCode == ocMacro || eOpCode == ocUDExternal)
                     Add( new formula::FormulaExternalToken( eOpCode, aStrVal ) );
                 else if ( eOpCode == ocWhitespace )
                 {

include/formula/compiler.hxx
@@ -532,7 +532,8 @@
 #define SC_OPCODE_UNIQUE            517
 #define SC_OPCODE_WRAPCOLS          518
 #define SC_OPCODE_WRAPROWS          519
-#define SC_OPCODE_STOP_2_PAR        520     /* last function with two or more parameters' OpCode + 1 */
+#define SC_OPCODE_UD_EXTERNAL       520     /* User-defined external function */
+#define SC_OPCODE_STOP_2_PAR        521     /* last function with two or more parameters' OpCode + 1 */
 
 #define SC_OPCODE_STOP_FUNCTION     SC_OPCODE_STOP_2_PAR            /* last function's OpCode + 1 */
 #define SC_OPCODE_LAST_OPCODE_ID    (SC_OPCODE_STOP_FUNCTION - 1)   /* last OpCode */

include/formula/opcode.hxx
@@ -32,6 +32,7 @@ enum OpCode : sal_uInt16
         ocCall              = SC_OPCODE_CALL,
         ocStop              = SC_OPCODE_STOP,
         ocExternal          = SC_OPCODE_EXTERNAL,
+        ocUDExternal        = SC_OPCODE_UD_EXTERNAL,
         ocName              = SC_OPCODE_NAME,
     // Jump commands
         ocIf                = SC_OPCODE_IF,
@@ -552,6 +553,7 @@ inline std::string OpCodeEnumToString(OpCode eCode)
     case ocCall: return "Call";
     case ocStop: return "Stop";
     case ocExternal: return "External";
+    case ocUDExternal: return "UDExternal";
     case ocName: return "Name";
     case ocIf: return "If";
     case ocIfError: return "IfError";

sc/inc/compiler.hxx
@@ -166,7 +166,7 @@ public:
     void SetExternalSingleRef( sal_uInt16 nFileId, const OUString& rTabName, const ScSingleRefData& rRef );
     void SetExternalDoubleRef( sal_uInt16 nFileId, const OUString& rTabName, const ScComplexRefData& rRef );
     void SetExternalName( sal_uInt16 nFileId, const OUString& rName );
-    void SetExternal(const OUString& rStr);
+    void SetExternal(const OUString& rStr, OpCode eCode = ocExternal);
 
     /** If the token is a non-external reference, determine if the reference is
         valid. If the token is an external reference, return true. Else return

sc/inc/global.hxx
@@ -578,6 +578,7 @@ public:
     static void                 ClearAutoFormat(); //BugId 54209
     static LegacyFuncCollection*      GetLegacyFuncCollection();
     SC_DLLPUBLIC static ScUnoAddInCollection* GetAddInCollection();
+    SC_DLLPUBLIC static bool IsValidExternal(const OUString& rAddIn);
     SC_DLLPUBLIC static ScUserList&         GetUserList();
     static void                 SetUserList( const ScUserList* pNewList );
     /**

commit 66e7fb01de87ad8cb15fe56539549eca158e7ab6

XLSX: Use "#REF!" for invalid user-defined functions
This is a regression from bce84f279755171addf29b61f4e8b09e89026c44,
for empty user-defined functions use "#REF!"

bug-document: forum-mso-de-49320.xls
Change-Id: I67d43b8adaf53c1d54aaf91db58dfff3b005310d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201983
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

formula/source/core/api/FormulaCompiler.cxx
@@ -2656,7 +2656,9 @@ const FormulaToken* FormulaCompiler::CreateStringFromToken( OUStringBuffer& rBuf
     }
     else if (eOp == ocUDExternal)
     {
-        if (maArrIterator.PeekNext() && maArrIterator.PeekNext()->GetOpCode() == ocOpen)
+        if (t->GetType() == svByte)
+            rBuffer.append(mxSymbols->getSymbol(ocErrRef));
+        else if (maArrIterator.PeekNext() && maArrIterator.PeekNext()->GetOpCode() == ocOpen)
             rBuffer.append(mxSymbols->getSymbol(eOp));
     }
     else if( static_cast<sal_uInt16>(eOp) < mxSymbols->getSymbolCount())        // Keyword: