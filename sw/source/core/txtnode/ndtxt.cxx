LibreOffice/core

commit 93fe03c05987434df95d4dea2dcf45fd76020f63

LOCRDT sw: fix multiple view shell problems
CppunitTest_desktop_lib testCommentsCallbacksWriter uses 2 view shells
to insert comments.

The first problem is that SwTextNode::Update() avoids moving the wrong
(i.e. current shell's) cursor, because it uses SwDocShell::m_pWrtShell
to check if it's the current shell, but only
DocumentLayoutManager::mpCurrentView has the correct value.

The other problem is that SwViewShell::GetPostItMgr() also uses
SwDocShell::m_pWrtShell and so doesn't return its own m_rView's one,
hence a loop over all shells always uses the same SwPostItMgr.

Change-Id: I043602f88f2f458532224554e5c13de7d37081c0
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185400
Reviewed-by: Michael Stahl <michael.stahl@allotropia.de>
Tested-by: Jenkins

sw/source/core/txtnode/ndtxt.cxx
@@ -1582,7 +1582,7 @@ void SwTextNode::Update(
                 for (SwViewShell& rShell : pDocShell->GetWrtShell()->GetRingContainer())
                 {
                     auto pWrtShell = dynamic_cast<SwWrtShell*>(&rShell);
-                    if (!pWrtShell || pWrtShell == pDocShell->GetWrtShell())
+                    if (!pWrtShell || pWrtShell == dynamic_cast<SwWrtShell*>(GetDoc().getIDocumentLayoutAccess().GetCurrentViewShell()))
                         continue;
 
                     SwShellCursor* pCursor = pWrtShell->GetCursor_();

sw/inc/viewsh.hxx
@@ -611,7 +611,7 @@ public:
     void DeleteReplacementBitmaps();
 
     const SwPostItMgr* GetPostItMgr() const { return const_cast<SwViewShell*>(this)->GetPostItMgr(); }
-    SW_DLLPUBLIC SwPostItMgr* GetPostItMgr();
+    SW_DLLPUBLIC virtual SwPostItMgr* GetPostItMgr();
 
     /// Acts both for headers / footers, depending on the bShow(Header|Footer)Separator flags
     void ToggleHeaderFooterEdit();

sw/source/uibase/inc/wrtsh.hxx
@@ -491,6 +491,8 @@ typedef bool (SwWrtShell::*FNSimpleMove)();
                 const SwViewOption *pViewOpt);
     virtual ~SwWrtShell() override;
 
+    SW_DLLPUBLIC virtual SwPostItMgr* GetPostItMgr() override;
+
     bool TryRemoveIndent(); // #i23725#
 
     OUString GetSelDescr() const;

sw/source/uibase/wrtsh/wrtsh3.cxx
@@ -389,4 +389,9 @@ bool SwWrtShell::GetURLFromButton( OUString& rURL, OUString& rDescr ) const
     return bRet;
 }
 
+SwPostItMgr* SwWrtShell::GetPostItMgr()
+{
+    return m_rView.GetPostItMgr();
+}
+
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 0194ef39a5f07d6152117994704d1d4f82072165

sw: fix cursor update problem
(regression from commit 93fe03c05987434df95d4dea2dcf45fd76020f63)

SwTextNode::Update() was changed to check the
getIDocumentLayoutAccess().GetCurrentViewShell() instead of
pDocShell->GetWrtShell() but apparently that was based partially on
incorrect assumptions.

getIDocumentLayoutAccess().GetCurrentViewShell() is updated on
SwView::GotFocus(), but that happens only in Classic.

SwDocShell::SetView() actually updates the other one and is actually
called from ChildSession before any sort of editing operation.

Unfortunately i don't remember what exactly failed in
CppunitTest_desktop_lib and the change was with experimental yrs feature
enabled so just revert this for now.

Change-Id: I6fececd1f85bd8ba347865a198845250a9946e2e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202057
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

sw/source/core/txtnode/ndtxt.cxx
@@ -1587,7 +1587,7 @@ void SwTextNode::Update(
                 for (SwViewShell& rShell : pDocShell->GetWrtShell()->GetRingContainer())
                 {
                     auto pWrtShell = dynamic_cast<SwWrtShell*>(&rShell);
-                    if (!pWrtShell || pWrtShell == dynamic_cast<SwWrtShell*>(GetDoc().getIDocumentLayoutAccess().GetCurrentViewShell()))
+                    if (!pWrtShell || pWrtShell == pDocShell->GetWrtShell())
                         continue;
 
                     SwShellCursor* pCursor = pWrtShell->GetCursor_();