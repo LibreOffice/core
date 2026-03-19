8e65861090b845f7708197f20befe5d056f5d98e
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2026-03-19 01:24:02 +0000

    sc: Introduce MultipleOpsOperation, moving the code from ScDocFunc
    Pure refactoring only, but renamed from TabOperation to MultipleOps,
    so it reflects what is the name on the UI.
    
    Used when running "Data -> Multiple Operations..." dialog.
    
    Change-Id: Iae5a554a22e0215d33b726f8dbdd33339870c0e3
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201087
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit e2ff3ad98226)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202068
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    Tested-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 5790217e15f01390a9fcae1b73e7c422df9c6b6c)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205420
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>

---
+++ sc/source/ui/inc/operation/MultipleOpsOperation.hxx
@@ -0,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include "Operation.hxx"
+#include <address.hxx>
+#include <paramisc.hxx>
+
+class ScDocShell;
+class ScMarkData;
+
+namespace sc
+{
+/** Operation which fills a range with formulas substituting varying input values (Multiple Operations). */
+class MultipleOpsOperation : public Operation
+{
+private:
+    ScDocShell& mrDocShell;
+    ScRange maRange;
+    ScMarkData const* mpTabMark;
+    ScTabOpParam maParam;
+
+    bool runImplementation() override;
+
+public:
+    MultipleOpsOperation(ScDocShell& rDocShell, const ScRange& rRange, const ScMarkData* pTabMark,
+                         const ScTabOpParam& rParam, bool bRecord, bool bApi);
+};
+} // end sc namespace
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
