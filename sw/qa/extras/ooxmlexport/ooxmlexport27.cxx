64ddc6a33da13051fcd635b384e7812bd856bd02
Justin Luth <justin.luth@collabora.com>
2026-03-21 13:38:25 -0400

    tdf#171433 starmath ooxmlimport: handleSetString must encloseOrEscape
    This fixes a 25.8.5 regression from
        tdf#169843 math: OOXML set letter not interpreted correctly
    
    make CppunitTest_sw_ooxmlexport27 \
        CPPUNIT_TEST_NAME=testTdf171433_equation
    
    Change-Id: I5f2757e1b9023f226f6c8dc14376e170f41f4c3d
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202321
    Reviewed-by: Justin Luth <jluth@mail.com>
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>

---
+++ sw/qa/extras/ooxmlexport/ooxmlexport27.cxx
@@ -0,0 +1,46 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <swmodeltestbase.hxx>
+
+#include <com/sun/star/beans/XPropertyState.hpp>
+
+#include <pam.hxx>
+#include <unotxdoc.hxx>
+#include <docsh.hxx>
+#include <IDocumentSettingAccess.hxx>
+#include <wrtsh.hxx>
+
+namespace
+{
+class Test : public SwModelTestBase
+{
+public:
+    Test()
+        : SwModelTestBase(u"/sw/qa/extras/ooxmlexport/data/"_ustr)
+    {
+    }
+};
+
+CPPUNIT_TEST_FIXTURE(Test, testTdf171433_equation)
+{
+    // given a document with formula
+    createSwDoc("tdf171433_equation.docx");
+
+    save(TestFilter::DOCX);
+
+    xmlDocUniquePtr pXmlDoc = parseExport(u"word/document.xml"_ustr);
+    // Without the fix, the formula was completely broken - missing SubSup entirely
+    assertXPath(pXmlDoc, "//m:sSubSup/m:sup", 1);
+}
+
+} // end of anonymous namespace
+CPPUNIT_PLUGIN_IMPLEMENT();
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
