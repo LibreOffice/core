LibreOffice/core

commit e3d17e1abca66cb01f7420fb8003eefd573b00e9

XLSX: Import external references with "#REF!"
Valid external references with "#REF!" are imported as `ocBad`

Change-Id: Iaf1d70a0a3e30d45ea8ce9be8e263d9fe4953bab
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/201533
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

sc/source/core/tool/compiler.cxx
@@ -990,12 +990,17 @@ struct ConventionOOO_A1 : public Convention_A1
             rBuffer.append('.');
         }
 
-        if (!rRef.IsColRel())
-            rBuffer.append('$');
-        MakeColStr( rLimits, rBuffer, aAbsRef.Col());
-        if (!rRef.IsRowRel())
-            rBuffer.append('$');
-        MakeRowStr( rLimits, rBuffer, aAbsRef.Row());
+        if (!rRef.IsColRel() && !rRef.IsRowRel() && rRef.IsColDeleted() && rRef.IsRowDeleted())
+            rBuffer.append("#REF!");
+        else
+        {
+            if (!rRef.IsColRel())
+                rBuffer.append('$');
+            MakeColStr(rLimits, rBuffer, aAbsRef.Col());
+            if (!rRef.IsRowRel())
+                rBuffer.append('$');
+            MakeRowStr(rLimits, rBuffer, aAbsRef.Row());
+        }
 
         return true;
     }
@@ -1319,12 +1324,17 @@ struct ConventionXL_A1 : public Convention_A1, public ConventionXL
 
     static void makeSingleCellStr( const ScSheetLimits& rLimits, OUStringBuffer& rBuf, const ScSingleRefData& rRef, const ScAddress& rAbs )
     {
-        if (!rRef.IsColRel())
-            rBuf.append('$');
-        MakeColStr(rLimits, rBuf, rAbs.Col());
-        if (!rRef.IsRowRel())
-            rBuf.append('$');
-        MakeRowStr(rLimits, rBuf, rAbs.Row());
+        if (!rRef.IsColRel() && !rRef.IsRowRel() && rRef.IsColDeleted() && rRef.IsRowDeleted())
+            rBuf.append("#REF!");
+        else
+        {
+            if (!rRef.IsColRel())
+                rBuf.append('$');
+            MakeColStr(rLimits, rBuf, rAbs.Col());
+            if (!rRef.IsRowRel())
+                rBuf.append('$');
+            MakeRowStr(rLimits, rBuf, rAbs.Row());
+        }
     }
 
     virtual void makeRefStr(
@@ -3456,8 +3466,10 @@ bool ScCompiler::ParseSingleReference( const OUString& rName, const OUString* pE
     {
         // Valid given tab and invalid col or row may indicate a sheet-local
         // named expression, bail out early and don't create a reference token.
-        if (!(nFlags & ScRefFlags::VALID) && mnCurrentSheetEndPos > 0 &&
-                (nFlags & ScRefFlags::TAB_VALID) && (nFlags & ScRefFlags::TAB_3D))
+        bool bErrRef = (rName.subView(mnCurrentSheetEndPos) == mxSymbols->getSymbol(ocErrRef));
+
+        if (!(nFlags & ScRefFlags::VALID) && mnCurrentSheetEndPos > 0
+            && (nFlags & ScRefFlags::TAB_VALID) && (nFlags & ScRefFlags::TAB_3D) && !bErrRef)
         {
             if (aExtInfo.mbExternal)
             {
@@ -3486,8 +3498,8 @@ bool ScCompiler::ParseSingleReference( const OUString& rName, const OUString* pE
 
         ScSingleRefData aRef;
         aRef.InitAddress( aAddr );
-        aRef.SetColRel( (nFlags & ScRefFlags::COL_ABS) == ScRefFlags::ZERO );
-        aRef.SetRowRel( (nFlags & ScRefFlags::ROW_ABS) == ScRefFlags::ZERO );
+        aRef.SetColRel(((nFlags & ScRefFlags::COL_ABS) == ScRefFlags::ZERO) && !bErrRef);
+        aRef.SetRowRel(((nFlags & ScRefFlags::ROW_ABS) == ScRefFlags::ZERO) && !bErrRef);
         aRef.SetTabRel( (nFlags & ScRefFlags::TAB_ABS) == ScRefFlags::ZERO );
         aRef.SetFlag3D( ( nFlags & ScRefFlags::TAB_3D ) != ScRefFlags::ZERO );
         // the reference is really invalid

commit 0518e9457eae3563836299a76a973deca7f73136

XLSX: Handle "#REF!" in external double references
This is a regression from e3d17e1abca66cb01f7420fb8003eefd573b00e9,
Use "#REF! if any one of the reference in a double reference is invalid.

ex: "Data!A1:#REF!" should be exported as "Data!#REF!"

bug-document: forum-mso-de-19736.xls
Change-Id: I9f17c3a42844a5fcd00a7d94eef8e9a7bafc6bd5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202657
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

sc/source/core/tool/compiler.cxx
@@ -1395,14 +1395,22 @@ struct ConventionXL_A1 : public Convention_A1, public ConventionXL
             }
         }
 
-        makeSingleCellStr(rLimits, rBuf, aRef.Ref1, aAbs1);
+        OUStringBuffer aBuf;
+
+        makeSingleCellStr(rLimits, aBuf, aRef.Ref1, aAbs1);
         if (!bSingleRef &&
                 (aAbs1.Row() != aAbs2.Row() || aRef.Ref1.IsRowRel() != aRef.Ref2.IsRowRel() ||
                  aAbs1.Col() != aAbs2.Col() || aRef.Ref1.IsColRel() != aRef.Ref2.IsColRel()))
         {
-            rBuf.append( ':' );
-            makeSingleCellStr(rLimits, rBuf, aRef.Ref2, aAbs2);
+            aBuf.append( ':' );
+            makeSingleCellStr(rLimits, aBuf, aRef.Ref2, aAbs2);
         }
+
+        OUString sErrRef = "#REF!";
+        if (aBuf.indexOf(sErrRef) == -1)
+            rBuf.append(aBuf);
+        else
+            rBuf.append(sErrRef);
     }
 
     virtual ParseResult parseAnyToken( const OUString& rFormula,
@@ -1475,12 +1483,20 @@ struct ConventionXL_A1 : public Convention_A1, public ConventionXL
         ConventionXL::makeExternalTabNameRange(rBuffer, rTabName, rTabNames, aAbsRef);
         rBuffer.append('!');
 
-        makeSingleCellStr(rLimits, rBuffer, rRef.Ref1, aAbsRef.aStart);
+        OUStringBuffer aBuf;
+
+        makeSingleCellStr(rLimits, aBuf, rRef.Ref1, aAbsRef.aStart);
         if (aAbsRef.aStart != aAbsRef.aEnd)
         {
-            rBuffer.append(':');
-            makeSingleCellStr(rLimits, rBuffer, rRef.Ref2, aAbsRef.aEnd);
+            aBuf.append(':');
+            makeSingleCellStr(rLimits, aBuf, rRef.Ref2, aAbsRef.aEnd);
         }
+
+        OUString sErrRef = "#REF!";
+        if (aBuf.indexOf(sErrRef) == -1)
+            rBuffer.append(aBuf);
+        else
+            rBuffer.append(sErrRef);
     }
 };
 
@@ -1644,13 +1660,20 @@ struct ConventionXL_OOX : public ConventionXL_A1
             rBuffer.append( aBuf);
         }
         rBuffer.append('!');
+        aBuf.setLength(0);
 
-        makeSingleCellStr(rLimits, rBuffer, rRef.Ref1, aAbsRef.aStart);
+        makeSingleCellStr(rLimits, aBuf, rRef.Ref1, aAbsRef.aStart);
         if (aAbsRef.aStart != aAbsRef.aEnd)
         {
-            rBuffer.append(':');
-            makeSingleCellStr(rLimits, rBuffer, rRef.Ref2, aAbsRef.aEnd);
+            aBuf.append(':');
+            makeSingleCellStr(rLimits, aBuf, rRef.Ref2, aAbsRef.aEnd);
         }
+
+        OUString sErrRef = "#REF!";
+        if (aBuf.indexOf(sErrRef) == -1)
+            rBuffer.append(aBuf);
+        else
+            rBuffer.append(sErrRef);
     }
 
     static void makeExternalDocStr( OUStringBuffer& rBuffer, sal_uInt16 nFileId )