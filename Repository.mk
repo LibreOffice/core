78bc29a0ebcf3ccce9fdd4e3da1da7c580ed50fa
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-03-29 16:06:03 +0100

    fix --disable-database-connectivity with --enable-mergelibs=more
    Change-Id: Ie078ee84d20cd5e83d269ba1cfab6fdf2179038e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202936
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202992
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- Repository.mk
+++ Repository.mk
@@ -271,8 +271,9 @@ $(eval $(call gb_Helper_register_executables_for_install,UREBIN,ure,\
 
 $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,base, \
 	abp \
-	dbp \
-	dbu \
+	$(call gb_Helper_optional,DBCONNECTIVITY, \
+		dbp \
+		dbu) \
 ))
 
 $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,calc, \
@@ -406,9 +407,9 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	$(if $(filter $(OS),WNT),,cmdmail) \
 	configmgr \
 	ctl \
-	dba \
-	dbahsql \
 	$(call gb_Helper_optional,DBCONNECTIVITY, \
+		dba \
+		dbahsql \
 		dbase \
 		dbaxml) \
 	dbtools \
@@ -429,7 +430,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	fps_office \
 	for \
 	forui \
-	frm \
+	$(call gb_Helper_optional,DBCONNECTIVITY,frm) \
 	fsstorage \
 	fwk \
     $(call gb_Helper_optionals_or,HELPTOOLS XMLHELP,helplinker) \
@@ -446,14 +447,14 @@ $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,ooo, \
 	$(if $(MERGELIBS),merged) \
 	migrationoo2 \
 	migrationoo3 \
-	mozbootstrap \
+	$(call gb_Helper_optional,DBCONNECTIVITY,mozbootstrap) \
 	msfilter \
 	$(call gb_Helper_optional,SCRIPTING,msforms) \
 	mtfrenderer \
 	$(call gb_Helper_optional,DBCONNECTIVITY,mysql_jdbc) \
 	$(call gb_Helper_optional,MARIADBC,$(call gb_Helper_optional,DBCONNECTIVITY,mysqlc)) \
 	numbertext \
-	odbc \
+	$(call gb_Helper_optional,DBCONNECTIVITY,odbc) \
 	odfflatxml \
 	offacc \
 	oox \
@@ -560,8 +561,9 @@ $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,python, \
 ))
 
 $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,reportbuilder, \
-	rpt \
-	rptui \
+	$(call gb_Helper_optional,DBCONNECTIVITY, \
+		rpt \
+		rptui ) \
 ))
 
 $(eval $(call gb_Helper_register_libraries_for_install,OOOLIBS,writer, \
@@ -691,7 +693,7 @@ $(eval $(call gb_Helper_register_libraries_for_install,PLAINLIBS_OOO,ooo, \
 	emboleobj \
 	package2 \
 	$(call gb_Helper_optional,SCRIPTING,scriptframe) \
-	sdbc2 \
+	$(call gb_Helper_optional,DBCONNECTIVITY,sdbc2) \
 	sofficeapp \
 	srtrs1 \
 	ucb1 \
@@ -1211,17 +1213,17 @@ $(eval $(call gb_Helper_register_mos,\
 	chart \
 	cnr \
 	cui \
-	dba \
+	$(call gb_Helper_optional,DBCONNECTIVITY,dba) \
 	dkt \
 	editeng \
 	flt \
 	for \
 	$(call gb_Helper_optional,DESKTOP,fps) \
-	frm \
+	$(call gb_Helper_optional,DBCONNECTIVITY,frm) \
 	fwk \
 	oox \
 	pcr \
-	rpt \
+	$(call gb_Helper_optional,DBCONNECTIVITY,rpt) \
 	$(call gb_Helper_optional,SCRIPTING,sb) \
 	sc \
 	sca \
