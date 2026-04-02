LibreOffice/core

commit 711f6aff699a46d661bc1285be712e158d05b7c2

DOCX: Skip exporting duplicate OLEs
OLEs inside a frame are exported using `mc:AlternateContent`. The same
OLE is exported twice, once in DML and once in VML.

Change-Id: I3cdda222e8d320e0e1599e94dc00f831a3a4fafb
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/202133
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

sw/source/filter/ww8/docxexport.cxx
@@ -485,6 +485,12 @@ OString DocxExport::OutputChart( uno::Reference< frame::XModel > const & xModel,
 
 OString DocxExport::WriteOLEObject(SwOLEObj& rObject, OUString & io_rProgID)
 {
+    // Check if the OLE is already exported
+    const OUString& sPersistName = rObject.GetCurrentPersistName();
+    auto it = m_aOLECache.find(sPersistName);
+    if (it != m_aOLECache.end())
+        return OUStringToOString(it->second, RTL_TEXTENCODING_UTF8);
+
     uno::Reference <embed::XEmbeddedObject> xObj( rObject.GetOleRef() );
     uno::Reference<uno::XComponentContext> const xContext(
         GetFilter().getComponentContext());
@@ -528,6 +534,7 @@ OString DocxExport::WriteOLEObject(SwOLEObj& rObject, OUString & io_rProgID)
         io_rProgID = OUString::createFromAscii(pProgID);
     }
 
+    m_aOLECache[sPersistName] = sId;
     return OUStringToOString( sId, RTL_TEXTENCODING_UTF8 );
 }
 

sw/source/filter/ww8/docxexport.hxx
@@ -26,6 +26,7 @@
 #include <sax/fshelper.hxx>
 #include <rtl/ustring.hxx>
 
+#include <map>
 #include <memory>
 #include <ndole.hxx>
 #include <unotools/securityoptions.hxx>
@@ -99,6 +100,9 @@ class DocxExport : public MSWordExportBase
 
     /// OLE objects counter.
     sal_Int32 m_nOLEObjects;
+
+    /// Cache of already exported OLEs
+    std::map<OUString, OUString> m_aOLECache;
 
     /// ActiveX controls counter
     sal_Int32 m_nActiveXControls;

commit 163927cb5a745b35597d01372ffd8a9d879425b4

DOCX: Create new rel IDs for duplicate OLEs
This is a regression from 711f6aff699a46d661bc1285be712e158d05b7c2,
a single OLE can be used in multiple header/footer files, in those cases
we were using same rel ID across all files for the same OLE, we need
to create a new rel ID for every new file.

bug-doc: fdo44257-1.docx
Change-Id: I03c675802951f3c057d05733776197e516b4a07f
Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/57
Reviewed-by: Michael Stahl <michael.stahl@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

sw/source/filter/ww8/docxexport.cxx
@@ -492,11 +492,23 @@ OString DocxExport::OutputChart( uno::Reference< frame::XModel > const & xModel,
 
 OString DocxExport::WriteOLEObject(SwOLEObj& rObject, OUString & io_rProgID)
 {
-    // Check if the OLE is already exported
+    // If the OLE was already exported, reuse the relationship ID for the same file,
+    // or create a new relationship ID pointing to the existing OLE for new files.
     const OUString& sPersistName = rObject.GetCurrentPersistName();
     auto it = m_aOLECache.find(sPersistName);
     if (it != m_aOLECache.end())
-        return OUStringToOString(it->second, RTL_TEXTENCODING_UTF8);
+    {
+        OLECacheEntry& rEntry = it->second;
+        if (rEntry.xOutputStream == GetFS()->getOutputStream())
+            return OUStringToOString(rEntry.sRelId, RTL_TEXTENCODING_UTF8);
+
+        // Create new relID and update the cache
+        OUString const sId = m_rFilter.addRelation(GetFS()->getOutputStream(), rEntry.sRelationType,
+                                                   rEntry.sFileName);
+        rEntry.sRelId = sId;
+        rEntry.xOutputStream = GetFS()->getOutputStream();
+        return OUStringToOString(sId, RTL_TEXTENCODING_UTF8);
+    }
 
     uno::Reference <embed::XEmbeddedObject> xObj( rObject.GetOleRef() );
     uno::Reference<uno::XComponentContext> const xContext(
@@ -541,7 +553,9 @@ OString DocxExport::WriteOLEObject(SwOLEObj& rObject, OUString & io_rProgID)
         io_rProgID = OUString::createFromAscii(pProgID);
     }
 
-    m_aOLECache[sPersistName] = sId;
+    m_aOLECache[sPersistName]
+        = OLECacheEntry{ sFileName, sRelationType, sId, GetFS()->getOutputStream() };
+
     return OUStringToOString( sId, RTL_TEXTENCODING_UTF8 );
 }
 

sw/source/filter/ww8/docxexport.hxx
@@ -96,7 +96,14 @@ class DocxExport : public MSWordExportBase
     sal_Int32 m_nOLEObjects;
 
     /// Cache of already exported OLEs
-    std::map<OUString, OUString> m_aOLECache;
+    struct OLECacheEntry
+    {
+        OUString sFileName;
+        OUString sRelationType;
+        OUString sRelId;
+        css::uno::Reference<css::io::XOutputStream> xOutputStream;
+    };
+    std::map<OUString, OLECacheEntry> m_aOLECache;
 
     /// ActiveX controls counter
     sal_Int32 m_nActiveXControls;