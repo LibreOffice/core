0246cee300a73e71056dec7de4716a4266d8606e
Noel Grandin <noel.grandin@collabora.co.uk>
2026-04-08 13:27:44 +0200

    fix O(n^2) loop in SvxStyleToolBoxControl::FillStyleBox (again)
    which particularly shows up when opening and browsing test document
       forum-mso-en4-130383.xlsx
    in the desktop application.
    
    This effectively reapplies
    
      commit 1d9cc5964c7a2fd4dd9326c2c0bfa6ee48d67006
      Author: Noel Grandin <noel.grandin@collabora.co.uk>
      Date:   Mon Jan 5 10:29:08 2026 +0200
      tdf#166684 fix O(n^2) loop in SvxStyleToolBoxControl::FillStyleBox
    
    which seems to have been accidentally dumped in
    
      commit 524f68ed5c2fd6795c192f130fcf05b32a678247
      Author: Szymon Kłos <szymon.klos@collabora.com>
      Date:   Fri Dec 26 08:31:12 2025 +0000
      sidebar: unify style list with notebookbar
    
    Change-Id: I882994ef98abbc498dd6c6c1a238b95757a21bb7
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/606
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>

--- svx/source/tbxctrls/tbcontrl.cxx
+++ svx/source/tbxctrls/tbcontrl.cxx
@@ -155,11 +155,6 @@ public:
     OUString get_text(int nIndex) const { return m_xWidget->get_text(nIndex); }
     OUString get_active_text() const { return m_xWidget->get_active_text(); }
 
-    int find_text(const OUString& rStr)
-    {
-        return m_xWidget->find_text(rStr);
-    }
-
     void append_text(const OUString& rStr)
     {
         OUString sId(OUString::number(m_xWidget->get_count()));
@@ -3436,6 +3431,8 @@ void SvxStyleToolBoxControl::FillStyleBox()
     // Add used, favourite and user defined
 
     std::vector<OUString> aStyles;
+    // use a set to avoid O(n^2) performance problem in insert loop
+    std::unordered_set<OUString> aStylesSet;
 
     AppendStyles(aStyles, eFamily, SfxStyleSearchBits::Favourite);
     AppendStyles(aStyles, eFamily, SfxStyleSearchBits::UserDefined);
@@ -3445,14 +3442,17 @@ void SvxStyleToolBoxControl::FillStyleBox()
     if (m_pImpl->bSpecModeWriter || m_pImpl->bSpecModeCalc)
     {
         for( const auto &rStyle : m_pImpl->aDefaultStyles )
+        {
             pBox->append_text(rStyle.second);
+            aStylesSet.insert(rStyle.second);
+        }
     }
 
     // Insert styles
     for (const auto& rStyle : aStyles)
     {
         // do not duplicate default styles
-        if (pBox->find_text(rStyle) == -1)
+        if (aStylesSet.insert(rStyle).second)
             pBox->append_text(rStyle);
     }
 
