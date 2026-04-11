LibreOffice/core

commit 8ed30e4a243a3efdfe380852d20910bc4a02e181

Move ToolboxController to use OPropertyContainerImplHelper
Quite a large inheritance chain, which all needed replace dispose
with disposing, and make sure the locking is correct.

Introduce comphelper::unique_unlock, RAII class for scoped release
of std::unique_lock, to help ensure the invariant established in
commit 4fc1ab0ca8d9bb0f0b8344d0537882aab1d6d2c0 (add assert to keep
WeakComponentImplHelperBase::disposing callees honest, 2026-04-04).

Change-Id: I72c98c25c01b8042428ef2376c6aba3c6b15ec87
Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/248
Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

dbaccess/source/ui/querydesign/limitboxcontroller.hxx
@@ -35,8 +35,8 @@ class LimitBoxController: public LimitBoxController_Base
         /// XServiceInfo
         DECLARE_SERVICE_INFO();
 
-        /// XComponent
-        virtual void SAL_CALL dispose() override;
+        /// WeakComponentImplHelperBase
+        virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         /// XStatusListener
         virtual void SAL_CALL statusChanged( const css::frame::FeatureStateEvent& Event ) override;

framework/inc/uielement/FixedImageToolbarController.hxx
@@ -37,8 +37,8 @@ public:
                                 const css::uno::Reference<css::frame::XFrame>& rFrame,
                                 ToolBox* pToolBar, ToolBoxItemId nID, const OUString& aCommand);
 
-    // XComponent
-    virtual void SAL_CALL dispose() override;
+    // WeakComponentImplHelperBase
+    void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     void CheckAndUpdateImages();
 

framework/inc/uielement/FixedTextToolbarController.hxx
@@ -37,8 +37,8 @@ public:
                                const css::uno::Reference<css::frame::XFrame>& rFrame,
                                ToolBox* pToolBar, ToolBoxItemId nID, const OUString& aCommand);
 
-    // XComponent
-    virtual void SAL_CALL dispose() override;
+    // WeakComponentImplHelperBase
+    void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
 private:
     virtual void executeControlCommand(const css::frame::ControlCommand& rControlCommand) override;

framework/inc/uielement/comboboxtoolbarcontroller.hxx
@@ -40,8 +40,8 @@ class ComboboxToolbarController final : public ComplexToolbarController
                                    const OUString& aCommand );
         virtual ~ComboboxToolbarController() override;
 
-        // XComponent
-        virtual void SAL_CALL dispose() override;
+        // WeakComponentImplHelperBase
+        virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from ComboBoxControl
         void Select();

framework/inc/uielement/complextoolbarcontroller.hxx
@@ -45,8 +45,8 @@ class ComplexToolbarController : public svt::ToolboxController
                                   const OUString& aCommand );
         virtual ~ComplexToolbarController() override;
 
-        // XComponent
-        virtual void SAL_CALL dispose() override;
+        // WeakComponentImplHelperBase
+        virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // XToolbarController
         virtual void SAL_CALL execute( sal_Int16 KeyModifier ) override;

framework/inc/uielement/dropdownboxtoolbarcontroller.hxx
@@ -43,8 +43,8 @@ class DropdownToolbarController final : public ComplexToolbarController
                                    const OUString& aCommand );
         virtual ~DropdownToolbarController() override;
 
-        // XComponent
-        virtual void SAL_CALL dispose() override;
+        // WeakComponentImplHelperBase
+        virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from ListBoxControl
         void Select();

framework/inc/uielement/edittoolbarcontroller.hxx
@@ -42,8 +42,8 @@ class EditToolbarController final : public ComplexToolbarController
                                const OUString& aCommand );
         virtual ~EditToolbarController() override;
 
-        // XComponent
-        virtual void SAL_CALL dispose() override;
+        // WeakComponentImplHelperBase
+        virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from EditControl
         void Modify();

framework/inc/uielement/spinfieldtoolbarcontroller.hxx
@@ -43,8 +43,8 @@ class SpinfieldToolbarController final : public ComplexToolbarController
                                     const OUString& aCommand );
         virtual ~SpinfieldToolbarController() override;
 
-        // XComponent
-        virtual void SAL_CALL dispose() override;
+        // WeakComponentImplHelperBase
+        virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from SpinfieldControl
         void Modify();

commit e95308f8dc2e5c1088686eaa1552126eea9745d0

Fix --enable-werror gcc build on Linux
Commit 8ed30e4a243a3efdfe380852d20910bc4a02e181 (Move ToolboxController
to use OPropertyContainerImplHelper, 2026-04-10) introduced cases similar
to the problem handled in commit 3917b082b9f4530d305cd947ab47658f6883c4f1
(Fix --enable-werror gcc build on Linux, 2026-04-09).

In file included from /home/mk/core/framework/inc/uielement/complextoolbarcontroller.hxx:28,
                 from /home/mk/core/framework/inc/uielement/comboboxtoolbarcontroller.hxx:24,
                 from /home/mk/core/framework/source/uielement/comboboxtoolbarcontroller.cxx:20:
/home/mk/core/include/svtools/toolboxcontroller.hxx:86:31: error: ‘virtual void svt::ToolboxController::disposing(const com::sun::star::lang::EventObject&)’ was hidden [-Werror=overloaded-virtual=]
   86 |         virtual void SAL_CALL disposing( const css::lang::EventObject& Source ) override;
      |                               ^~~~~~~~~
/home/mk/core/framework/inc/uielement/comboboxtoolbarcontroller.hxx:44:22: note:   by ‘virtual void framework::ComboboxToolbarController::disposing(std::unique_lock<std::mutex>&)’
   44 |         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
      |                      ^~~~~~~~~

Change-Id: Ic41be4d39b032864c3427d17a85502e36f31dce9
Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/757
Reviewed-by: Justin Luth <justin.luth@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

dbaccess/source/ui/querydesign/limitboxcontroller.hxx
@@ -36,6 +36,7 @@ class LimitBoxController: public LimitBoxController_Base
         DECLARE_SERVICE_INFO();
 
         /// WeakComponentImplHelperBase
+        using LimitBoxController_Base::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         /// XStatusListener

framework/inc/uielement/FixedImageToolbarController.hxx
@@ -38,6 +38,7 @@ public:
                                 ToolBox* pToolBar, ToolBoxItemId nID, const OUString& aCommand);
 
     // WeakComponentImplHelperBase
+    using ComplexToolbarController::disposing;
     void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     void CheckAndUpdateImages();

framework/inc/uielement/FixedTextToolbarController.hxx
@@ -38,6 +38,7 @@ public:
                                ToolBox* pToolBar, ToolBoxItemId nID, const OUString& aCommand);
 
     // WeakComponentImplHelperBase
+    using ComplexToolbarController::disposing;
     void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
 private:

framework/inc/uielement/comboboxtoolbarcontroller.hxx
@@ -41,6 +41,7 @@ class ComboboxToolbarController final : public ComplexToolbarController
         virtual ~ComboboxToolbarController() override;
 
         // WeakComponentImplHelperBase
+        using ComplexToolbarController::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from ComboBoxControl

framework/inc/uielement/complextoolbarcontroller.hxx
@@ -46,6 +46,7 @@ class ComplexToolbarController : public svt::ToolboxController
         virtual ~ComplexToolbarController() override;
 
         // WeakComponentImplHelperBase
+        using svt::ToolboxController::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // XToolbarController

framework/inc/uielement/dropdownboxtoolbarcontroller.hxx
@@ -44,6 +44,7 @@ class DropdownToolbarController final : public ComplexToolbarController
         virtual ~DropdownToolbarController() override;
 
         // WeakComponentImplHelperBase
+        using ComplexToolbarController::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from ListBoxControl

framework/inc/uielement/edittoolbarcontroller.hxx
@@ -43,6 +43,7 @@ class EditToolbarController final : public ComplexToolbarController
         virtual ~EditToolbarController() override;
 
         // WeakComponentImplHelperBase
+        using ComplexToolbarController::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from EditControl

framework/inc/uielement/spinfieldtoolbarcontroller.hxx
@@ -44,6 +44,7 @@ class SpinfieldToolbarController final : public ComplexToolbarController
         virtual ~SpinfieldToolbarController() override;
 
         // WeakComponentImplHelperBase
+        using ComplexToolbarController::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // called from SpinfieldControl

framework/inc/uielement/styletoolbarcontroller.hxx
@@ -72,6 +72,7 @@ public:
     void SAL_CALL statusChanged( const css::frame::FeatureStateEvent& rEvent ) override;
 
     // WeakComponentImplHelperBase
+    using svt::ToolboxController::disposing;
     void disposing(std::unique_lock<std::mutex>& rGuard) override;
 };
 

include/framework/generictoolbarcontroller.hxx
@@ -41,6 +41,7 @@ class UNLESS_MERGELIBS(FWK_DLLPUBLIC) GenericToolbarController final : public sv
         virtual ~GenericToolbarController() override;
 
         // WeakComponentImplHelperBase
+        using svt::ToolboxController::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // XToolbarController
@@ -76,6 +77,7 @@ public:
                                const OUString& rModuleName);
 
     // WeakComponentImplHelperBase
+    using svt::ToolboxController::disposing;
     void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XStatusListener

include/sfx2/tbxctrl.hxx
@@ -87,6 +87,7 @@ protected:
 
 public:
     // WeakComponentImplHelperBase
+    using svt::ToolboxController::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // new controller API

include/svtools/popupwindowcontroller.hxx
@@ -69,6 +69,7 @@ public:
     virtual css::uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override = 0;
 
     // WeakComponentImplHelperBase
+    using PopupWindowController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XStatusListener

include/svx/ThemeColorsToolBoxControl.hxx
@@ -39,8 +39,10 @@ protected:
     void onColorSetActivated() override;
 };
 
-class SVX_DLLPUBLIC ThemeColorsToolBoxControl final
-    : public cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>
+using ThemeColorsToolBoxControl_Base
+    = cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>;
+
+class SVX_DLLPUBLIC ThemeColorsToolBoxControl final : public ThemeColorsToolBoxControl_Base
 {
     VclPtr<ThemeColorsPaneWrapper> m_xVclBox;
 
@@ -57,6 +59,7 @@ public:
         SAL_CALL createItemWindow(const css::uno::Reference<css::awt::XWindow>& rParent) override;
 
     // WeakComponentImplHelperBase
+    using ThemeColorsToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XUpdatable

include/svx/tbcontrl.hxx
@@ -146,8 +146,10 @@ class PaletteManager;
 namespace svx { class ToolboxButtonColorUpdaterBase; }
 namespace sfx { struct CurrencyID; }
 
-class SvxStyleToolBoxControl final : public cppu::ImplInheritanceHelper<svt::ToolboxController,
-                                                                                          css::lang::XServiceInfo>
+using SvxStyleToolBoxControl_Base = cppu::ImplInheritanceHelper<svt::ToolboxController,
+                                                                css::lang::XServiceInfo>;
+
+class SvxStyleToolBoxControl final : public SvxStyleToolBoxControl_Base
 {
     struct Impl;
     std::unique_ptr<Impl> m_pImpl;
@@ -169,6 +171,7 @@ public:
     virtual void SAL_CALL update() override;
 
     // WeakComponentImplHelperBase
+    using SvxStyleToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XServiceInfo

sd/source/ui/controller/SlideTransitionsToolBoxControl.hxx
@@ -27,8 +27,10 @@ public:
     void SetOptimalSize(); // Add this method
 };
 
-class SlideTransitionsToolBoxControl final
-    : public cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>
+using SlideTransitionsToolBoxControl_Base
+    = cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>;
+
+class SlideTransitionsToolBoxControl final : public SlideTransitionsToolBoxControl_Base
 {
     VclPtr<SlideTransitionsPaneWrapper> m_xVclBox;
 
@@ -47,6 +49,7 @@ public:
     virtual void SAL_CALL initialize(const css::uno::Sequence<css::uno::Any>& rArguments) override;
 
     // WeakComponentImplHelperBase
+    using SlideTransitionsToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XUpdatable

sd/source/ui/sidebar/SlideMasterPagesToolBoxControl.hxx
@@ -20,8 +20,9 @@ public:
     void SetOptimalSize();
 };
 
-class SlideMasterPagesToolBoxControl final
-    : public cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>
+using SlideMasterPagesToolBoxControl_Base
+    = cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>;
+class SlideMasterPagesToolBoxControl final : public SlideMasterPagesToolBoxControl_Base
 {
     VclPtr<SlideMasterPagesWrapper> m_xVclBox;
 
@@ -40,6 +41,7 @@ public:
     virtual void SAL_CALL initialize(const css::uno::Sequence<css::uno::Any>& rArguments) override;
 
     // WeakComponentImplHelperBase
+    using SlideMasterPagesToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XServiceInfo

sfx2/source/view/classificationcontroller.cxx
@@ -84,6 +84,7 @@ public:
     uno::Sequence<OUString> SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using ClassificationCategoriesControllerBase::disposing;
     void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XToolbarController

svx/source/inc/StylesPreviewToolBoxControl.hxx
@@ -25,8 +25,10 @@
 #include "StylesPreviewWindow.hxx"
 #include <com/sun/star/frame/XModel.hpp>
 
-class StylesPreviewToolBoxControl final
-    : public cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>
+using StylesPreviewToolBoxControl_Base
+    = cppu::ImplInheritanceHelper<svt::ToolboxController, css::lang::XServiceInfo>;
+
+class StylesPreviewToolBoxControl final : public StylesPreviewToolBoxControl_Base
 {
     VclPtr<StylesPreviewWindow_Impl> m_xVclBox;
     std::unique_ptr<StylesPreviewWindow_Base> m_xWeldBox;
@@ -48,6 +50,7 @@ public:
     virtual void SAL_CALL initialize(const css::uno::Sequence<css::uno::Any>& rArguments) override;
 
     // WeakComponentImplHelperBase
+    using StylesPreviewToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XUpdatable

svx/source/tbxctrls/tbcontrl.cxx
@@ -281,8 +281,11 @@ private:
 class SvxFontNameBox_Impl;
 class SvxFontNameBox_Base;
 
-class SvxFontNameToolBoxControl final : public cppu::ImplInheritanceHelper<svt::ToolboxController,
-                                                                           css::lang::XServiceInfo>
+using SvxFontNameToolBoxControl_Base = cppu::ImplInheritanceHelper<svt::ToolboxController,
+                                                                   css::lang::XServiceInfo>;
+
+
+class SvxFontNameToolBoxControl final : public SvxFontNameToolBoxControl_Base
 {
 public:
     SvxFontNameToolBoxControl();
@@ -294,6 +297,7 @@ public:
     virtual css::uno::Reference<css::awt::XWindow> SAL_CALL createItemWindow(const css::uno::Reference<css::awt::XWindow>& rParent) override;
 
     // WeakComponentImplHelperBase
+    using SvxFontNameToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XServiceInfo

svx/source/tbxctrls/tbunocontroller.cxx
@@ -66,6 +66,7 @@ class FontHeightToolBoxControl : public FontHeightToolBoxControl_Base
         virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
         // WeakComponentImplHelperBase
+        using FontHeightToolBoxControl_Base::disposing;
         virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
         // XStatusListener

svx/source/tbxctrls/tbunosearchcontrollers.cxx
@@ -648,6 +648,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using FindTextToolbarController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -802,6 +803,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using UpDownSearchToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -896,6 +898,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using MatchCaseToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -999,6 +1002,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using MatchDiacriticsToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -1102,6 +1106,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using SearchFormattedToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -1194,6 +1199,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using FindAllToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -1277,6 +1283,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using ExitSearchToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization
@@ -1371,6 +1378,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using SearchLabelToolboxController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XInitialization

sw/source/uibase/dbui/mailmergetoolbarcontrols.cxx
@@ -129,6 +129,7 @@ public:
     }
 
     // WeakComponentImplHelperBase
+    using MMCurrentEntryController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XToolbarController
@@ -221,6 +222,7 @@ public:
     }
 
     // WeakComponentImplHelperBase
+    using MMExcludeEntryController_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XToolbarController

sw/source/uibase/inc/workctrl.hxx
@@ -163,6 +163,7 @@ public:
     virtual css::uno::Sequence< OUString > SAL_CALL getSupportedServiceNames() override;
 
     // WeakComponentImplHelperBase
+    using NavElementToolBoxControl_Base::disposing;
     virtual void disposing(std::unique_lock<std::mutex>& rGuard) override;
 
     // XStatusListener