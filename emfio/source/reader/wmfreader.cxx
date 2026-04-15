a9c32edacd9d65f81f41e2036fceb2d3584eeecb
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-04-15 21:00:10 +0100

    ofz#496349582 Integer-overflow
    Change-Id: I537f96838f402f20b537440a5cf94967814a64ce
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/1052
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
    (cherry picked from commit 4c4f052a5f3700c332ede70bc71dda3529266ba5)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/204398
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins
    (cherry picked from commit cc32ab7342efa3043eb373cd9e33331978ca3f2f)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/204459

--- emfio/source/reader/wmfreader.cxx
+++ emfio/source/reader/wmfreader.cxx
@@ -1475,6 +1475,8 @@ namespace emfio
             // inch
             mpInputStream->ReadUInt16( mnUnitsPerInch );
 
+            SAL_WARN_IF(!mnUnitsPerInch, "emfio", "Invalid WMF: mnUnitsPerInch is 0");
+
             // reserved
             mpInputStream->SeekRel( 4 );
 
@@ -1541,7 +1543,7 @@ namespace emfio
         SAL_INFO("emfio", "WMF size  w: " << aWMFSize.Width()    << " h: " << aWMFSize.Height());
 
         Size aDevExt( 10000, 10000 );
-        if( ( std::abs( aWMFSize.Width() ) > 1 ) && ( std::abs( aWMFSize.Height() ) > 1 ) )
+        if( ( std::abs( aWMFSize.Width() ) > 1 ) && ( std::abs( aWMFSize.Height() ) > 1 ) && mnUnitsPerInch )
         {
             const Fraction  aFrac( 1, mnUnitsPerInch);
             MapMode         aWMFMap( MapUnit::MapInch, Point(), aFrac, aFrac );
