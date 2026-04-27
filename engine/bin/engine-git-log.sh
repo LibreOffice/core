e3892fc67200d9cffaa4bfa148045c53bf09a2ca
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-04-27 13:21:48 +0200

    Extend engine/bin/engine-git-log.sh to support optional git-log arguments
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: I98f63f55a33e2ae5eb23866a42879b4efb51d768
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/1725
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Tested-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- engine/bin/engine-git-log.sh
+++ engine/bin/engine-git-log.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 # Show the full log for a file across the engine/ subtree merge.
-# Usage: engine-git-log.sh <path>
+# Usage: engine-git-log.sh [git log args...] <path>
 # Path may be given relative to the current directory or from the
-# repo root, with or without the engine/ prefix.
+# repo root, with or without the engine/ prefix. Any preceding
+# arguments are forwarded to git log (e.g. -Sfoo).
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [git log args...] <path>" >&2
+    exit 1
+fi
 prefix=$(git rev-parse --show-prefix) || exit 1
 cd "$(git rev-parse --show-toplevel)" || exit 1
-new=${prefix}$1
+n=$#
+i=0
+for arg do
+    i=$((i+1))
+    if [ $i -eq $n ]; then
+        path=$arg
+    else
+        set -- "$@" "$arg"
+    fi
+    shift
+done
+new=${prefix}$path
 old=${new#engine/}
 if [ -t 1 ]; then pager=$(git var GIT_PAGER); else pager=cat; fi
 {
-    git -P log --follow merge-engine..HEAD -- "$new"
-    git -P log --follow merge-engine^2     -- "$old"
+    git -P log "$@" --follow merge-engine..HEAD -- "$new"
+    git -P log "$@" --follow merge-engine^2     -- "$old"
 } | $pager
