e103440e9be6eb34f889de02bcb8552836533b0c
Gabor Kelemen <gabor.kelemen@collabora.com>
2026-05-03 01:46:14 +0200

    Speed up finding unused defines
    Previously on my fast PC this took 4 hours to run.
    
    But, if we assume that defines in a certain module won't be used
    in another module, then we can speed up the 'git grep' operations.
    
    Now it runs just over an hour. Also gives a bit more hits due to
    finding less false positives.
    
    Change-Id: I373ee12a70cbe89f4ab3fda059db4b465e9871a7
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/1977
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>

--- engine/bin/find-unused-defines.py
+++ engine/bin/find-unused-defines.py
@@ -11,6 +11,7 @@ import subprocess
 import sys
 import re
 import codecs
+import os
 
 exclusionSet = set([
     # List of RID constants where we compute a value using a base before calling one of the RESSTR methods
@@ -106,104 +107,167 @@ def strict_handler(exception):
     return u"", exception.end
 codecs.register_error("strict", strict_handler)
 
-# find defines, excluding the externals folder
-a = subprocess.Popen("git grep -hP '^#define\\s+\\w\\w\\w\\w+\\s*' -- \"[!e][!x][!t]*\" | sort -u", stdout=subprocess.PIPE, shell=True, encoding='UTF-8')
+# don't search in directories with non C++ source files
+excludeDirs = {
+    "android",
+    "animations",
+    "bean",
+    "bin",
+    "compilerplugins",
+    "cpputools",
+    "distro-configs",
+    "docmodel",
+    "eventattacher",
+    "extensions", # TODO add an exclusion for PROPERTY_ID_ stuff in formmetadata.hxx
+    "external",
+    "extras",
+    "i18nlangtag",
+    "icon-themes",
+    "idlc",
+    "instsetoo_native",
+    "jurt",
+    "jvmaccess",
+    "librelogo",
+    "m4",
+    "msicreator",
+    "nlpsolver",
+    "offapi",
+    "officecfg",
+    "oovbaapi",
+    "osx",
+    "pch",
+    "postprocess",
+    "qadevOOo",
+    "readlicense_oo",
+    "remotebridges",
+    "reportbuilder",
+    "ridljar",
+    "ridljar",
+    "scp2",
+    "smoketest",
+    "swext",
+    "sysui",
+    "udkapi",
+    "uitest",
+    "unoil",
+    "unotest",
+    "ure",
+    "xmlreader",
+    "dictionaries"
+    }
 
-name_re = re.compile(r"#define\s+(\w+)")
-with a.stdout as txt:
-    for line in txt:
-        idName = name_re.match(str(line)).group(1)
-        if idName.startswith("INCLUDED_"):
-            continue
-        # the various _START and _END constants are normally unused outside of the .hrc and .src files, and that's fine
-        if idName.endswith("_START"):
-            continue
-        if idName.endswith("_BEGIN"):
-            continue
-        if idName.endswith("_END"):
-            continue
-        if idName == "RID_SVX_FIRSTFREE":
-            continue
-        if idName == "":
-            continue
-        if idName.startswith("__com"):
-            continue # these are the include/header macros for the UNO stuff
-        if in_exclusion_set(idName):
-            continue
-        # search for the constant
-        b = subprocess.Popen(["git", "grep", "-w", idName], stdout=subprocess.PIPE, encoding='UTF-8')
-        found_reason_to_exclude = False
-        with b.stdout as txt2:
-            cnt = 0
-            for line2 in txt2:
-                line2 = line2.strip() # otherwise the comparisons below will not work
-                # ignore if/undef magic, does not indicate an actual use (most of the time)
-                if "ifdef" in line2:
-                    continue
-                if "undef" in line2:
-                    continue
-                # ignore commented out code
-                if line2.startswith("//"):
-                    continue
-                if line2.startswith("/*"):
-                    continue
-                # check if we found one in actual code
-                if idName.startswith("SID_"):
-                    if ".hrc:" not in line2 and ".src:" not in line2 and ".sdi:" not in line2:
+for subdir in sorted(os.listdir(".")):
+    findGlobally = False
+    if not os.path.isdir(subdir):
+        continue
+    if subdir in excludeDirs:
+        continue
+    if subdir == "include":
+        findGlobally=True
+
+    # find defines in the subdirectory, e.g. in include/, sw/, sc/ etc.
+    a = subprocess.Popen(f"git grep -hP '^#define\\s+\\w\\w\\w\\w+\\s*' -- {subdir} | sort -u", stdout=subprocess.PIPE, shell=True, encoding='UTF-8')
+
+    name_re = re.compile(r"#define\s+(\w+)")
+    with a.stdout as txt:
+        for line in txt:
+            idName = name_re.match(str(line)).group(1)
+            if idName.startswith("INCLUDED_"):
+                continue
+            # the various _START and _END constants are normally unused outside of the .hrc and .src files, and that's fine
+            if idName.endswith("_START"):
+                continue
+            if idName.endswith("_BEGIN"):
+                continue
+            if idName.endswith("_END"):
+                continue
+            if idName == "RID_SVX_FIRSTFREE":
+                continue
+            if idName == "":
+                continue
+            if idName.startswith("__com"):
+                continue # these are the include/header macros for the UNO stuff
+            if in_exclusion_set(idName):
+                continue
+            # search for the constant
+            if findGlobally:
+                b = subprocess.Popen(["git", "grep", "-w", idName], stdout=subprocess.PIPE, encoding='UTF-8')
+            else:
+                b = subprocess.Popen(["git", "grep", "-w", idName, subdir ], stdout=subprocess.PIPE, encoding='UTF-8')
+            found_reason_to_exclude = False
+            with b.stdout as txt2:
+                cnt = 0
+                for line2 in txt2:
+                    line2 = line2.strip() # otherwise the comparisons below will not work
+                    # ignore if/undef magic, does not indicate an actual use (most of the time)
+                    if "ifdef" in line2:
+                        continue
+                    if "undef" in line2:
+                        continue
+                    # ignore commented out code
+                    if line2.startswith("//"):
+                        continue
+                    if line2.startswith("/*"):
+                        continue
+                    # check if we found one in actual code
+                    if idName.startswith("SID_"):
+                        if ".hrc:" not in line2 and ".src:" not in line2 and ".sdi:" not in line2:
+                            found_reason_to_exclude = True
+                    else:
+                        if ".hrc:" not in line2 and ".src:" not in line2:
+                            found_reason_to_exclude = True
+                    if idName.startswith("RID_"):
+                            # is the constant being used as an identifier by entries in .src files?
+                            if ".src:" in line2 and "Identifier = " in line2:
+                                found_reason_to_exclude = True
+                            # is the constant being used by the property controller extension or reportdesigner inspection,
+                            # which use macros to declare constants, hiding them from a search
+                            if "extensions/source/propctrlr" in line2:
+                                found_reason_to_exclude = True
+                            if "reportdesign/source/ui/inspection/inspection.src" in line2:
+                                found_reason_to_exclude = True
+                    if idName.startswith("HID_"):
+                            # is the constant being used as an identifier by entries in .src files
+                            if ".src:" in line2 and "HelpId = " in line2:
+                                found_reason_to_exclude = True
+                    # is it being used as a constant in an ItemList  in .src files?
+                    if ".src:" in line2 and (";> ;" in line2 or "; >;" in line2):
                         found_reason_to_exclude = True
+                    # these are used in calculations in other .hrc files
+                    if "sw/inc/rcid.hrc:" in line2:
+                        found_reason_to_exclude = True
+                    # calculations
+                    if "sw/source/uibase/inc/ribbar.hrc:" in line2 and "ST_" in idName:
+                        found_reason_to_exclude = True
+                    if "sw/source/uibase/inc/ribbar.hrc:" in line2 and "STR_IMGBTN_" in idName:
+                        found_reason_to_exclude = True
+                    if "sw/source/core/undo/undo.hrc:" in line2:
+                        found_reason_to_exclude = True
+                    if "sw/inc/poolfmt.hrc:" in line2:
+                        found_reason_to_exclude = True
+                    # used via a macro that hides them from search
+                    if "dbaccess/" in line2 and idName.startswith("PROPERTY_ID_"):
+                        found_reason_to_exclude = True
+                    if "reportdesign/" in line2 and idName.startswith("HID_RPT_PROP_"):
+                        found_reason_to_exclude = True
+                    if "reportdesign/" in line2 and idName.startswith("RID_STR_"):
+                        found_reason_to_exclude = True
+                    if "forms/" in line2 and idName.startswith("PROPERTY_"):
+                        found_reason_to_exclude = True
+                    if "svx/source/tbxctrls/extrusioncontrols.hrc:" in line2 and idName.startswith("DIRECTION_"):
+                        found_reason_to_exclude = True
+                    if "svx/source/tbxctrls/extrusioncontrols.hrc:" in line2 and idName.startswith("FROM_"):
+                        found_reason_to_exclude = True
+                    # if we see more than a few lines then it's probably one of the BASE/START/BEGIN things
+                    cnt = cnt + 1
+                    if cnt > 2:
+                        found_reason_to_exclude = True
+            if not found_reason_to_exclude:
+                print(idName)
+                # otherwise the previous line of output will be incorrectly mixed into the below git output, because of buffering
+                sys.stdout.flush()
+                # search again, so we log the location and filename of stuff we want to remove
+                if findGlobally:
+                    subprocess.call(["git", "grep", "-wn", idName])
                 else:
-                    if ".hrc:" not in line2 and ".src:" not in line2:
-                        found_reason_to_exclude = True
-                if idName.startswith("RID_"):
-                        # is the constant being used as an identifier by entries in .src files?
-                        if ".src:" in line2 and "Identifier = " in line2:
-                            found_reason_to_exclude = True
-                        # is the constant being used by the property controller extension or reportdesigner inspection,
-                        # which use macros to declare constants, hiding them from a search
-                        if "extensions/source/propctrlr" in line2:
-                            found_reason_to_exclude = True
-                        if "reportdesign/source/ui/inspection/inspection.src" in line2:
-                            found_reason_to_exclude = True
-                if idName.startswith("HID_"):
-                        # is the constant being used as an identifier by entries in .src files
-                        if ".src:" in line2 and "HelpId = " in line2:
-                            found_reason_to_exclude = True
-                # is it being used as a constant in an ItemList  in .src files?
-                if ".src:" in line2 and (";> ;" in line2 or "; >;" in line2):
-                    found_reason_to_exclude = True
-                # these are used in calculations in other .hrc files
-                if "sw/inc/rcid.hrc:" in line2:
-                    found_reason_to_exclude = True
-                # calculations
-                if "sw/source/uibase/inc/ribbar.hrc:" in line2 and "ST_" in idName:
-                    found_reason_to_exclude = True
-                if "sw/source/uibase/inc/ribbar.hrc:" in line2 and "STR_IMGBTN_" in idName:
-                    found_reason_to_exclude = True
-                if "sw/source/core/undo/undo.hrc:" in line2:
-                    found_reason_to_exclude = True
-                if "sw/inc/poolfmt.hrc:" in line2:
-                    found_reason_to_exclude = True
-                # used via a macro that hides them from search
-                if "dbaccess/" in line2 and idName.startswith("PROPERTY_ID_"):
-                    found_reason_to_exclude = True
-                if "reportdesign/" in line2 and idName.startswith("HID_RPT_PROP_"):
-                    found_reason_to_exclude = True
-                if "reportdesign/" in line2 and idName.startswith("RID_STR_"):
-                    found_reason_to_exclude = True
-                if "forms/" in line2 and idName.startswith("PROPERTY_"):
-                    found_reason_to_exclude = True
-                if "svx/source/tbxctrls/extrusioncontrols.hrc:" in line2 and idName.startswith("DIRECTION_"):
-                    found_reason_to_exclude = True
-                if "svx/source/tbxctrls/extrusioncontrols.hrc:" in line2 and idName.startswith("FROM_"):
-                    found_reason_to_exclude = True
-                # if we see more than a few lines then it's probably one of the BASE/START/BEGIN things
-                cnt = cnt + 1
-                if cnt > 2:
-                    found_reason_to_exclude = True
-        if not found_reason_to_exclude:
-            print(idName)
-            # otherwise the previous line of output will be incorrectly mixed into the below git output, because of buffering
-            sys.stdout.flush()
-            # search again, so we log the location and filename of stuff we want to remove
-            subprocess.call(["git", "grep", "-wn", idName])
-
+                    subprocess.call(["git", "grep", "-wn", idName, subdir])
