a3d3ae5e931a04ff5718382ccffe9f16de336a42
Armin Le Grand (collabora) <Armin.LeGrand@collabora.com>
2026-05-05 11:46:12 +0200

    CairoSDPR: Improve buffering of huge BitmapData
    We already did some improvements, but I wanted to clean
    this up somewhat. Reason for the needed corrections was
    in principle the sqrt()-based general calculation of
    the HoldTime in the default implementation of
    calculateCombinedHoldCyclesInSeconds. The original idea
    was to hold huge data shorter due to memory constraints.
    To solve that we added an adapted version to
    CairoPixelProcessor2D.
    
    With lessons learned I decided to remove that and
    provide a revised general implementation based on linear
    scaling of the HoldTime. Based on assuming the seen
    huge Bitmap and estimating 20min for it, just create a
    factor and multiply with SizeInBytes.
    
    I checked that and it works as intended. It will spare
    us from this problem re-occurring with future SDPRs.
    
    NOTE: This still keeps the CairoSDPR-specific avoiding
    of buffering of small Bitmaps when these are available
    from Bitmap directly. This stays useful and is
    Cairo-specific.
    
    Signed-off-by: Armin Le Grand (collabora) <Armin.LeGrand@collabora.com>
    Change-Id: I30c7361c6fbfaf0be90be353091cbaf831db5cd2
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/2005
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Tomaž Vajngerl <tomaz.vajngerl@collabora.com>

--- engine/include/basegfx/utils/systemdependentdata.hxx
+++ engine/include/basegfx/utils/systemdependentdata.hxx
@@ -72,7 +72,7 @@ namespace basegfx
         // getHoldCyclesInSeconds and estimateUsageInBytes, executed
         // using getHoldCyclesInSeconds. StartValue is 0 to detect
         // not-yet-calculated state
-        sal_uInt32                      mnCalculatedCycles;
+        mutable sal_uInt32              mnCalculatedCycles;
 
     public:
         SystemDependentData(
@@ -91,7 +91,7 @@ namespace basegfx
         // getHoldCyclesInSeconds and estimateUsageInBytes, the
         // result is created once on-demand and buffered in
         // mnCalculatedCycles
-        virtual sal_uInt32 calculateCombinedHoldCyclesInSeconds() const;
+        sal_uInt32 calculateCombinedHoldCyclesInSeconds() const;
 
         // Allow read access to the calculated cycles in seconds, this
         // can be e.g. used to determine if this instance got added
