849b9a45c7d3483fb7754364288522790176a31d
Andreas Kainz <andreas_k@abwesend.de>
2026-05-11 10:40:18 +0200

    Translated using Weblate (German)
    Currently translated at 97.2% (36 of 37 strings)
    
    Co-authored-by: Andreas Kainz <andreas_k@abwesend.de>
    Co-authored-by: Andreas_K <andreas_k@abwesend.de>
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/de/
    Translation: Collabora Online/Collabora Office Qt app
    Signed-off-by: Andreas Kainz <andreas_k@abwesend.de>
    Change-Id: If87b4101d96b4ef114a38dd82e4386421ecf35aa

--- qt/translations/coda_de.ts
+++ qt/translations/coda_de.ts
@@ -6,42 +6,42 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="49"/>
         <source>ODF text document (.odt)</source>
-        <translation>ODF-Textdokument (.odt)</translation>
+        <translation>ODF Dokument (.odt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="50"/>
         <source>Rich Text (.rtf)</source>
-        <translation type="unfinished"></translation>
+        <translation>Rich Text (.rtf)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="51"/>
         <source>Word Document (.docx)</source>
-        <translation>Word-Dokument (.docx)</translation>
+        <translation>Word Dokument (.docx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="52"/>
         <source>Word 2003 Document (.doc)</source>
-        <translation type="unfinished"></translation>
+        <translation>Word 2003 Document (.doc)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="58"/>
         <source>ODF spreadsheet (.ods)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF Arbeitsmappe (.ods)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="59"/>
         <source>Excel Spreadsheet (.xlsx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Excel Arbeitsmappe (.xlsx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="60"/>
         <source>Excel 2003 Spreadsheet (.xls)</source>
-        <translation type="unfinished"></translation>
+        <translation>Excel 2003 Arbeitsmappe (.xls)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="66"/>
         <source>ODF presentation (.odp)</source>
-        <translation>ODF-Präsentation (.odp)</translation>
+        <translation>ODF Präsentation (.odp)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="67"/>
@@ -51,12 +51,12 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="68"/>
         <source>PowerPoint 2003 Presentation (.ppt)</source>
-        <translation type="unfinished"></translation>
+        <translation>PowerPoint 2003 Präsentation (.ppt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="74"/>
         <source>ODF drawing (.odg)</source>
-        <translation>ODF-Zeichnung (.odg)</translation>
+        <translation>ODF Zeichnung (.odg)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="106"/>
@@ -175,7 +175,7 @@
     <message>
         <location filename="../WebView.cpp" line="166"/>
         <source>Spreadsheet</source>
-        <translation type="unfinished"></translation>
+        <translation>Arbeitsmappe</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="171"/>
