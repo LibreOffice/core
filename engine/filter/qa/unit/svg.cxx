LibreOffice/core

commit 58ba249df589b0c9f91667f2938bfa18d2a1ce61

tdf#166779: use model first to obtain selection
The current frame can be Basic, so only use it as a fallback to obtain selection.
Also, failure to get the selection is not fatal here.

The throwing behavior was since commit aa1927dc257b52edf96de220cc3797e02c83a0ae
(Make svg export use slidesorter selection in most cases., 2012-10-10).

Change-Id: I06f80151b4184f353d1d5387c11837f1e883a068
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185983
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins

filter/source/svg/svgfilter.cxx
@@ -149,15 +149,28 @@ sal_Bool SAL_CALL SVGFilter::filter( const Sequence< PropertyValue >& rDescripto
     return filterImpressOrDraw(rDescriptor);
 }
 
+css::uno::Reference<css::frame::XController> SVGFilter::getSourceController() const
+{
+    uno::Reference<frame::XController> xController;
+    // Current frame may be e.g. Basic. Try to get a controller from the source model first.
+    if (auto xModel = mxSrcDoc.query<frame::XModel>())
+        xController = xModel->getCurrentController();
+    // Try current frame as a fallback.
+    if (!xController)
+    {
+        uno::Reference<frame::XDesktop2> xDesktop(frame::Desktop::create(mxContext));
+        if (auto xFrame = xDesktop->getCurrentFrame()) // Manage headless case
+            xController = xFrame->getController();
+    }
+    return xController;
+}
+
 css::uno::Reference<css::frame::XController> SVGFilter::fillDrawImpressSelectedPages()
 {
-    uno::Reference<frame::XDesktop2> xDesktop(frame::Desktop::create(mxContext));
-    uno::Reference<frame::XFrame> xFrame = xDesktop->getCurrentFrame(); // Manage headless case
-    if (!xFrame)
+    uno::Reference<frame::XController> xController = getSourceController();
+    uno::Reference<drawing::framework::XControllerManager> xManager(xController, uno::UNO_QUERY);
+    if (!xManager)
         return {};
-    uno::Reference<frame::XController> xController(xFrame->getController(), uno::UNO_SET_THROW);
-    uno::Reference<drawing::framework::XControllerManager> xManager(xController,
-                                                                    uno::UNO_QUERY_THROW);
     uno::Reference<drawing::framework::XConfigurationController> xConfigController(
         xManager->getConfigurationController());
 
@@ -210,9 +223,9 @@ css::uno::Reference<css::frame::XController> SVGFilter::fillDrawImpressSelectedP
 
     if (mSelectedPages.empty())
     {
-        // apparently failed to clean selection - fallback to current page
-        uno::Reference<drawing::XDrawView> xDrawView(xController, uno::UNO_QUERY_THROW);
-        mSelectedPages.push_back(xDrawView->getCurrentPage());
+        // apparently failed to get a selection - fallback to current page
+        if (auto xDrawView = xController.query<drawing::XDrawView>())
+            mSelectedPages.push_back(xDrawView->getCurrentPage());
     }
     return xController;
 }
@@ -580,12 +593,7 @@ bool SVGFilter::filterWriterOrCalc( const Sequence< PropertyValue >& rDescriptor
     if(!bSelectionOnly) // For Writer only the selection-only mode is supported
         return false;
 
-    uno::Reference<frame::XDesktop2> xDesktop(frame::Desktop::create(mxContext));
-    uno::Reference<frame::XController > xController;
-    if (uno::Reference<frame::XFrame> xFrame = xDesktop->getCurrentFrame())
-        xController.set(xFrame->getController(), uno::UNO_SET_THROW);
-
-    Reference< view::XSelectionSupplier > xSelection (xController, UNO_QUERY);
+    Reference<view::XSelectionSupplier> xSelection(getSourceController(), UNO_QUERY);
     if (!xSelection.is())
         return false;
 

filter/source/svg/svgfilter.hxx
@@ -284,6 +284,7 @@ private:
     bool filterImpressOrDraw( const Sequence< PropertyValue >& rDescriptor );
     bool filterWriterOrCalc( const Sequence< PropertyValue >& rDescriptor );
 
+    css::uno::Reference<css::frame::XController> getSourceController() const;
     css::uno::Reference<css::frame::XController> fillDrawImpressSelectedPages();
 
 protected:

commit dee5e7fe025a8019b70da4866e0554cf20ccf1ff

engine filter: test "use model first to obtain selection"
Fails with commit 58ba249df589b0c9f91667f2938bfa18d2a1ce61 (tdf#166779:
use model first to obtain selection, 2025-05-29) reverted.

Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
Change-Id: I4534e44219c7a1507c4909e5aa5dc1afb7e21617
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/2196
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

engine/filter/qa/unit/svg.cxx
@@ -422,6 +422,34 @@ CPPUNIT_TEST_FIXTURE(SvgFilterTest, testTdf168135)
                 "height", u"776");
 }
 
+CPPUNIT_TEST_FIXTURE(SvgFilterTest, testExportControllerFromModel)
+{
+    // Given a Writer, then a Draw document loaded, the Writer document is the current one:
+    uno::Sequence<beans::PropertyValue> aEmptyProps;
+    mxComponent2 = mxDesktop->loadComponentFromURL(u"private:factory/swriter"_ustr, u"_blank"_ustr,
+                                                   0, aEmptyProps);
+    CPPUNIT_ASSERT(mxComponent2);
+    uno::Sequence<beans::PropertyValue> aHiddenProps{ comphelper::makePropertyValue(u"Hidden"_ustr,
+                                                                                    true) };
+    mxComponent = mxDesktop->loadComponentFromURL(u"private:factory/sdraw"_ustr, u"_default"_ustr,
+                                                  0, aHiddenProps);
+    CPPUNIT_ASSERT(mxComponent);
+    uno::Reference<frame::XModel> xWriterModel(mxComponent2, uno::UNO_QUERY_THROW);
+    uno::Reference<frame::XFrame> xWriterFrame(xWriterModel->getCurrentController()->getFrame(),
+                                               uno::UNO_SET_THROW);
+    mxDesktop->setActiveFrame(xWriterFrame);
+    CPPUNIT_ASSERT_EQUAL(xWriterFrame, mxDesktop->getCurrentFrame());
+
+    // When exporting the Draw document to SVG:
+    // Then make sure storeToURL() doesn't fail:
+    uno::Reference<frame::XStorable> xStorable(mxComponent, uno::UNO_QUERY_THROW);
+    comphelper::SequenceAsHashMap aMediaDescriptor;
+    aMediaDescriptor[u"FilterName"_ustr] <<= u"draw_svg_Export"_ustr;
+    // Without the accompanying fix in place, this test would have failed with an IOException, as it
+    // tried to export the current Writer document, not the Draw one.
+    xStorable->storeToURL(maTempFile.GetURL(), aMediaDescriptor.getAsConstPropertyValueList());
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */