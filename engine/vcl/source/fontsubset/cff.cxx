51fa2756479e99d1fc8d804d50bfd969d582ebdd
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-05-15 14:47:43 +0100

    ofz: check for failure here too
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ib48de81390607918e58d200a65b869b18c58e8b1
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/3162
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- engine/vcl/source/fontsubset/cff.cxx
+++ engine/vcl/source/fontsubset/cff.cxx
@@ -1734,6 +1734,11 @@ RealType CffContext::readRealVal()
     // nNumber * 10 + 9 must fit in S64; anything beyond is a malformed
     constexpr S64 nDigitCap = (SAL_MAX_INT64 - 9) / 10;
     for(;;){
+        if (mpReadPtr >= mpReadEnd) {
+            SAL_WARN("vcl.fonts.cff", "truncated CFF dict real number");
+            abandonDictParse();
+            return 0.0;
+        }
         const U8 c = *(mpReadPtr++); // read nibbles
         // parse high nibble
         const U8 nH = c >> 4U;
