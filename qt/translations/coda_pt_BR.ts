a8948285e511ce7dcf60aa84ab040c4668b0e9f4
Gabriel <bloxgabriel18@gmail.com>
2026-05-17 12:12:44 +0200

    Translated using Weblate (Portuguese (Brazil))
    Currently translated at 100.0% (37 of 37 strings)
    
    Co-authored-by: Gabriel <bloxgabriel18@gmail.com>
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/pt_BR/
    Translation: Collabora Online/Collabora Office Qt app
    Signed-off-by: Gabriel <bloxgabriel18@gmail.com>
    Change-Id: I38c50c772910ce46a42ebf3af450eb00adfc996f

--- qt/translations/coda_pt_BR.ts
+++ qt/translations/coda_pt_BR.ts
@@ -6,174 +6,174 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="49"/>
         <source>ODF text document (.odt)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documento de texto ODF (.odt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="50"/>
         <source>Rich Text (.rtf)</source>
-        <translation type="unfinished"></translation>
+        <translation>Texto Rico (.rtf)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="51"/>
         <source>Word Document (.docx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documento do Word (.docx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="52"/>
         <source>Word 2003 Document (.doc)</source>
-        <translation type="unfinished"></translation>
+        <translation>Documento do Word 2003 (.doc)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="58"/>
         <source>ODF spreadsheet (.ods)</source>
-        <translation type="unfinished"></translation>
+        <translation>Planilha ODF (.ods)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="59"/>
         <source>Excel Spreadsheet (.xlsx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Planilha do Excel (.xlsx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="60"/>
         <source>Excel 2003 Spreadsheet (.xls)</source>
-        <translation type="unfinished"></translation>
+        <translation>Planilha do Excel 2003 (.xls)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="66"/>
         <source>ODF presentation (.odp)</source>
-        <translation type="unfinished"></translation>
+        <translation>Apresentação ODF (.odp)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="67"/>
         <source>PowerPoint Presentation (.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Apresentação em PowerPoint (.pptx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="68"/>
         <source>PowerPoint 2003 Presentation (.ppt)</source>
-        <translation type="unfinished"></translation>
+        <translation>Apresentação do PowerPoint 2003 (.ppt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="74"/>
         <source>ODF drawing (.odg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Desenho ODF (.odg)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="106"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimir documento</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="114"/>
         <source>Select Printer:</source>
-        <translation type="unfinished"></translation>
+        <translation>Selecione Impressora:</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="123"/>
         <location filename="../DocumentOperations.cpp" line="196"/>
         <source>Default Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>Impressora padrão</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="128"/>
         <source>Print to File</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimir em arquivo</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="132"/>
         <source>Enter file path...</source>
-        <translation type="unfinished"></translation>
+        <translation>Insira o caminho do arquivo...</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="144"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>Salvar saída de impressão como</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="146"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Arquivos PDF (*.pdf);;Todos os arquivos (*)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="162"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>Imprimir</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="163"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancelar</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="187"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro ao imprimir em arquivo</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="188"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>Não foi possível salvar o documento no arquivo. Verifique o caminho do arquivo e as permissões.</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="232"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Erro de impressão</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="233"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Falha ao imprimir o documento. Verifique as configurações da sua impressora.</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="739"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Todos os arquivos (*)</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="249"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Salvar documento</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="625"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Abrir arquivo</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="626"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Todos os arquivos (*);;Documentos de texto (*.odt *.ott *.doc *.docx *.rtf *.txt);;Planilhas (*.ods *.ots *.xls *.xlsx *.csv);;Apresentações (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="737"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>Exportar como</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="156"/>
         <source>Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Apresentação</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="161"/>
         <location filename="../WebView.cpp" line="177"/>
         <source>Text Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Documento de texto</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="166"/>
         <source>Spreadsheet</source>
-        <translation type="unfinished"></translation>
+        <translation>Planilha</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="171"/>
         <source>Drawing</source>
-        <translation type="unfinished"></translation>
+        <translation>Desenho</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="317"/>
         <source>Presenting</source>
-        <translation type="unfinished"></translation>
+        <translation>Apresentando</translation>
     </message>
 </context>
 <context>
@@ -181,17 +181,17 @@
     <message>
         <location filename="../WebView.cpp" line="233"/>
         <source>Unsaved Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>Alterações não salvas</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="234"/>
         <source>The document has unsaved changes. Do you want to close anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>O documento contém alterações não salvas. Deseja fechá-lo mesmo assim?</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="609"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Começar</translation>
     </message>
 </context>
 </TS>
