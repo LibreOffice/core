Mu-L/LibreOffice-core

commit c0b0a18f5c82ff0ea6ceff1c2e3c1fcab9496681

Related tdf#140226: use StaticWhichCast in ww8graf+drwtxtex (sw)
Change-Id: I869f707b40f57c91994dc68545a8154ea8c70e10
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/204472
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins

include/editeng/editids.hrc
@@ -164,7 +164,7 @@ class SvxOpticalSizingItem;
 #define SID_ATTR_PARA_SNAPTOGRID                        TypedWhichId<SvxParaGridItem>( SID_SVX_START + 945 )
 #define SID_ATTR_PARA_AUTOFRAMEDIRECTION                TypedWhichId<SvxAutoFrameDirectionItem>( SID_SVX_START + 946 )
 #define SID_ATTR_PARA_LRSPACE_VERTICAL                  ( SID_SVX_START + 947 )
-#define SID_ATTR_PARA_LEFT_TO_RIGHT                     ( SID_SVX_START + 950 )
+#define SID_ATTR_PARA_LEFT_TO_RIGHT                     TypedWhichId<SfxBoolItem>( SID_SVX_START + 950 )
 #define SID_ATTR_PARA_RIGHT_TO_LEFT                     ( SID_SVX_START + 951 )
 #define SID_ATTR_CHAR_HIDDEN                            TypedWhichId<SvxCharHiddenItem>( SID_SVX_START + 989 )
 #define SID_ATTR_CHAR_LATIN_FONT                        ( SID_SVX_START + 994 )

sw/source/uibase/shells/annotsh.cxx
@@ -601,7 +601,7 @@ void SwAnnotationShell::Exec( SfxRequest &rReq )
             const SfxPoolItem* pPoolItem;
             if( pNewAttrs && SfxItemState::SET == pNewAttrs->GetItemState( nSlot, true, &pPoolItem ) )
             {
-                if( !static_cast<const SfxBoolItem*>(pPoolItem)->GetValue() )
+                if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_LEFT_TO_RIGHT).GetValue() )
                     bLeftToRight = !bLeftToRight;
             }
             SfxItemSet aAttr(SfxItemSet::makeFixedSfxItemSet<

sw/source/uibase/shells/drwtxtex.cxx
@@ -617,7 +617,7 @@ void SwDrawTextShell::Execute( SfxRequest &rReq )
             const SfxPoolItem* pPoolItem;
             if( pNewAttrs && SfxItemState::SET == pNewAttrs->GetItemState( nSlot, true, &pPoolItem ) )
             {
-                if( !static_cast<const SfxBoolItem*>(pPoolItem)->GetValue() )
+                if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_LEFT_TO_RIGHT).GetValue() )
                     bLeftToRight = !bLeftToRight;
             }
             SfxItemSet aAttr(SfxItemSet::makeFixedSfxItemSet<

sw/source/filter/ww8/ww8graf.cxx
@@ -1774,7 +1774,7 @@ void SwWW8ImplReader::MatchSdrItemsIntoFlySet( SdrObject const * pSdrObj,
                 rFlySet.Put( *pPoolItem );
                 if ( i == XATTR_FILLSTYLE )
                 {
-                    const drawing::FillStyle eFill = static_cast<const XFillStyleItem*>(pPoolItem)->GetValue();
+                    const drawing::FillStyle eFill = pPoolItem->StaticWhichCast(XATTR_FILLSTYLE).GetValue();
                     // Transparency forced in certain situations when fillstyle is none - use old logic for that case still
                     // which is especially needed for export purposes (tdf112618).
                     if ( eFill != drawing::FillStyle_NONE )

LibreOffice/core

commit 100fcb9ca39e6de4cb9b2e86c2deac5069440cd0

tdf#172188: Crash when switching para direction in editeng
Test bLeftToRight to know if we must use StaticWhichCast with SID_ATTR_PARA_LEFT_TO_RIGHT
or SID_ATTR_PARA_RIGHT_TO_LEFT

It requires to add "TypedWhichId<SfxBoolItem>" for SID_ATTR_PARA_RIGHT_TO_LEFT

Regression from c0b0a18f5c82ff0ea6ceff1c2e3c1fcab9496681:
Related tdf#140226: use StaticWhichCast in ww8graf+drwtxtex (sw)

Change-Id: I59f98d7b3285c12154ad69e9e2727d66ea1dfd3e
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205476
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>
Tested-by: Jenkins

include/editeng/editids.hrc
@@ -165,7 +165,7 @@ class SvxOpticalSizingItem;
 #define SID_ATTR_PARA_AUTOFRAMEDIRECTION                TypedWhichId<SvxAutoFrameDirectionItem>( SID_SVX_START + 946 )
 #define SID_ATTR_PARA_LRSPACE_VERTICAL                  ( SID_SVX_START + 947 )
 #define SID_ATTR_PARA_LEFT_TO_RIGHT                     TypedWhichId<SfxBoolItem>( SID_SVX_START + 950 )
-#define SID_ATTR_PARA_RIGHT_TO_LEFT                     ( SID_SVX_START + 951 )
+#define SID_ATTR_PARA_RIGHT_TO_LEFT                     TypedWhichId<SfxBoolItem>( SID_SVX_START + 951 )
 #define SID_ATTR_CHAR_HIDDEN                            TypedWhichId<SvxCharHiddenItem>( SID_SVX_START + 989 )
 #define SID_ATTR_CHAR_LATIN_FONT                        ( SID_SVX_START + 994 )
 #define SID_ATTR_CHAR_LATIN_FONTHEIGHT                  TypedWhichId<SvxFontHeightItem>( SID_SVX_START + 995 )

sw/source/uibase/shells/annotsh.cxx
@@ -602,8 +602,16 @@ void SwAnnotationShell::Exec( SfxRequest &rReq )
             const SfxPoolItem* pPoolItem;
             if( pNewAttrs && SfxItemState::SET == pNewAttrs->GetItemState( nSlot, true, &pPoolItem ) )
             {
-                if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_LEFT_TO_RIGHT).GetValue() )
-                    bLeftToRight = !bLeftToRight;
+                if (bLeftToRight)
+                {
+                    if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_LEFT_TO_RIGHT).GetValue() )
+                        bLeftToRight = !bLeftToRight;
+                }
+                else
+                {
+                    if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_RIGHT_TO_LEFT).GetValue() )
+                        bLeftToRight = !bLeftToRight;
+                }
             }
             SfxItemSet aAttr(SfxItemSet::makeFixedSfxItemSet<
                 EE_PARA_WRITINGDIR, EE_PARA_WRITINGDIR,

sw/source/uibase/shells/drwtxtex.cxx
@@ -617,8 +617,16 @@ void SwDrawTextShell::Execute( SfxRequest &rReq )
             const SfxPoolItem* pPoolItem;
             if( pNewAttrs && SfxItemState::SET == pNewAttrs->GetItemState( nSlot, true, &pPoolItem ) )
             {
-                if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_LEFT_TO_RIGHT).GetValue() )
-                    bLeftToRight = !bLeftToRight;
+                if (bLeftToRight)
+                {
+                    if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_LEFT_TO_RIGHT).GetValue() )
+                        bLeftToRight = !bLeftToRight;
+                }
+                else
+                {
+                    if( !pPoolItem->StaticWhichCast(SID_ATTR_PARA_RIGHT_TO_LEFT).GetValue() )
+                        bLeftToRight = !bLeftToRight;
+                }
             }
             SfxItemSet aAttr(SfxItemSet::makeFixedSfxItemSet<
                     EE_PARA_WRITINGDIR, EE_PARA_WRITINGDIR,