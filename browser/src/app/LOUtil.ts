CollaboraOnline/online

commit 221ee06f481cfaf67a3bcf3d4d5814196c46b2c7

tidy shapehandle svg data
Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
Change-Id: Id51bc156481c714f3b7d29822f3d74d3c612f6a3

browser/src/app/LOUtil.ts
@@ -783,9 +783,49 @@ class LOUtil {
 	public static Rectangle = cool.Rectangle;
 	public static createRectangle = cool.createRectangle;
 
-	public static sanitize(html: string): string {
+	public static sanitize(
+		html: string,
+		profile: 'html' | 'svg' = 'html',
+	): string {
 		if (DOMPurify.isSupported) {
-			return DOMPurify.sanitize(html, { USE_PROFILES: { html: true } });
+			if (profile === 'svg') {
+				return DOMPurify.sanitize(html, {
+					// Enable both SVG and HTML profiles to support HTML content inside foreignObject
+					USE_PROFILES: { svg: true, svgFilters: true, html: true },
+					ADD_TAGS: ['foreignObject'],
+					// Allow ODF namespaced attributes used by LibreOffice SVG export.
+					// See: core/filter/source/svg/svgexport.cxx for the full list.
+					ADD_ATTR: [
+						'ooo:background-visibility',
+						'ooo:date-time-field',
+						'ooo:date-time-format',
+						'ooo:date-time-visibility',
+						'ooo:display-name',
+						'ooo:footer-field',
+						'ooo:footer-visibility',
+						'ooo:has-custom-background',
+						'ooo:has-transition',
+						'ooo:header-field',
+						'ooo:id-list',
+						'ooo:master',
+						'ooo:master-objects-visibility',
+						'ooo:name',
+						'ooo:number-of-slides',
+						'ooo:numbering-type',
+						'ooo:page-number-visibility',
+						'ooo:page-numbering-type',
+						'ooo:slide',
+						'ooo:slide-duration',
+						'ooo:start-slide-number',
+						'ooo:text-adjust',
+						'ooo:use-positioned-chars',
+					],
+					// Allow HTML content inside foreignObject (for embedded video)
+					// See: https://github.com/cure53/DOMPurify/issues/1002
+					HTML_INTEGRATION_POINTS: { foreignobject: true },
+				});
+			}
+			return DOMPurify.sanitize(html, { USE_PROFILES: { [profile]: true } });
 		}
 		return '';
 	}

browser/src/app/GraphicSelectionMiddleware.ts
@@ -65,7 +65,8 @@ class GraphicSelection {
 
 		var videoToInsert =
 			'<?xml version="1.0" encoding="UTF-8"?>\
-		<foreignObject xmlns="http://www.w3.org/2000/svg" overflow="visible" width="' +
+		<svg xmlns="http://www.w3.org/2000/svg">\
+		<foreignObject overflow="visible" width="' +
 			videoDesc.width +
 			'" height="' +
 			videoDesc.height +
@@ -83,7 +84,8 @@ class GraphicSelection {
 			'"/>\
 				</video>\
 			</body>\
-		</foreignObject>';
+		</foreignObject>\
+		</svg>';
 
 		this.handlesSection.addEmbeddedVideo(videoToInsert);
 	}

browser/src/canvas/sections/ShapeHandlesSection.ts
@@ -485,7 +485,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 		document.getElementById('canvas-container').appendChild(this.sectionProperties.svg);
 		this.sectionProperties.svg.style.zIndex = 11; // Update z-index or video buttons are unreachable.
 
-		if (!this.sectionProperties.svg.innerHTML.includes('foreignobject')) {
+		if (!this.sectionProperties.svg.innerHTML.includes('foreignObject')) {
 			console.error('Failed to parse svg for embedded video');
 			return;
 		}
@@ -540,7 +540,7 @@ class ShapeHandlesSection extends CanvasSectionObject {
 		this.sectionProperties.svg.style.pointerEvents = 'none';
 		document.getElementById('canvas-container').appendChild(this.sectionProperties.svg);
 
-		this.sectionProperties.svg.innerHTML = data;
+		this.sectionProperties.svg.innerHTML = app.LOUtil.sanitize(data, 'svg');
 		this.sectionProperties.svg.style.position = 'absolute';
 		this.sectionProperties.svg.children[0].style.width = this.sectionProperties.svg.children[0].style.height = 'auto';
 		this.sectionProperties.svg.children[0].style.transformOrigin = 'center';

cypress_test/integration_tests/mobile/writer/shape_properties_spec.js
@@ -34,8 +34,9 @@ describe(['tagmobile', 'tagnextcloud', 'tagproxy'], 'Change shape properties via
 		 */
 		match(pathCommandStr) {
 			// M 1953,10839 L 7945,4847 1953,4847 1953,10839 1953,10839 Z
-			const pathCmdSplit = pathCommandStr.split(' ');
-			expect(pathCmdSplit).to.have.length(9);
+			// Filter empty strings to handle SVG path data with or without trailing whitespace
+			const pathCmdSplit = pathCommandStr.split(' ').filter(s => s.length > 0);
+			expect(pathCmdSplit).to.have.length(8);
 			TriangleCoordinatesMatcher.pointMatch(pathCmdSplit[1], this.xStart, this.yStart, this.delta, 'top of hypotenuse');
 			TriangleCoordinatesMatcher.pointMatch(pathCmdSplit[3], this.xEnd, this.yEnd, this.delta, 'bottom of hypotenuse');
 			TriangleCoordinatesMatcher.pointMatch(pathCmdSplit[4], this.xStart, this.yEnd, this.delta, 'left end of base');

LibreOffice/core

commit 39d3a6c9d38e0cdc9703fa7a60e556f226fda59d

feat(app:linux): play embedded media via cool: URL scheme handler
Without a cool: URL scheme handler in coda-qt, Chromium has no way to
fetch embedded media URLs (cool:/cool/media?Tag=...) emitted by wsd
under MOBILEAPP, so videos in slideshows never load.

Adopted from the iOS handler at ios/Mobile/CoolURLSchemeHandler.mm:
parse WOPISrc and Tag from the URL, look up the DocumentBroker, and
reply with the file at getEmbeddedMediaPath.

In LOUtil.ts, add a DOMPurify hook gated on window.ThisIsAMobileApp
that whitelists cool: URLs via forceKeepAttr; otherwise the default
allow-list strips <source src="cool:..."> on the editor's graphic-
selection SVG path (server-hosted browser unaffected). This likely
also restores embedded-video playback on iOS in the editor's
graphic-selection path, which would have regressed since commit
221ee06f481c / Change-Id Id51bc156481c714f3b7d29822f3d74d3c612f6a3
("tidy shapehandle svg data") routed that SVG through DOMPurify.

Signed-off-by: Sarper Akdemir <sarper.akdemir@collabora.com>
Change-Id: I09ced98df42790e93256384dccdb010c26fcdce9
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/2658
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>

browser/src/app/LOUtil.ts
@@ -29,6 +29,16 @@ interface IconNameMap {
 
 declare var DOMPurify: any;
 
+// cool: URLs are used by mobile/derivative apps (iOS, Android, CODA-W,
+// CODA-Q) for embedded media; DOMPurify's default allow-list rejects them.
+if (window.ThisIsAMobileApp && DOMPurify.isSupported) {
+	DOMPurify.addHook('uponSanitizeAttribute', (_node: Node, data: any) => {
+		if (data.attrValue.startsWith('cool:')) {
+			data.forceKeepAttr = true;
+		}
+	});
+}
+
 // LOUtil contains various LO related utility functions used
 // throughout the code.
 

qt/Application.cpp
@@ -12,6 +12,7 @@
 #include <config.h>
 
 #include <common/MobileApp.hpp>
+#include <qt/CoolUrlSchemeHandler.hpp>
 #include <qt/qt.hpp>
 
 #include <common/Log.hpp>
@@ -40,6 +41,9 @@ void Application::initialize()
         globalProfile->setPersistentStoragePath(appData);
         globalProfile->setCachePath(cacheData);
         globalProfile->setHttpCacheType(QWebEngineProfile::DiskHttpCache);
+
+        globalProfile->installUrlSchemeHandler(
+            "cool", new CoolUrlSchemeHandler(globalProfile));
     }
 
     // Initialize recent files

qt/CoolUrlSchemeHandler.cpp
@@ -1,0 +1,87 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <config.h>
+
+#include "CoolUrlSchemeHandler.hpp"
+
+#include <common/Log.hpp>
+#include <wsd/DocumentBroker.hpp>
+#include <wsd/RequestDetails.hpp>
+
+#include <QByteArray>
+#include <QFile>
+#include <QMultiMap>
+#include <QUrl>
+#include <QUrlQuery>
+#include <QWebEngineUrlRequestJob>
+
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+
+extern std::map<std::string, std::shared_ptr<DocumentBroker>> DocBrokers;
+extern std::mutex DocBrokersMutex;
+
+void CoolUrlSchemeHandler::requestStarted(QWebEngineUrlRequestJob* job)
+{
+    const QUrl url = job->requestUrl();
+    const bool isVtt = (url.path() == QStringLiteral("/cool/mediavtt"));
+
+    // The server percent-encodes the '&' query separators (see
+    // ClientSession::createPublicURI), so decode once before parsing.
+    QUrlQuery query;
+    query.setQuery(QUrl::fromPercentEncoding(url.query(QUrl::FullyEncoded).toUtf8()));
+    const std::string wopiSrc = query.queryItemValue("WOPISrc").toStdString();
+    const std::string tag = query.queryItemValue("Tag").toStdString();
+
+    std::shared_ptr<DocumentBroker> docBroker;
+    {
+        std::lock_guard<std::mutex> lock(DocBrokersMutex);
+        const auto it = DocBrokers.find(RequestDetails::getDocKey(wopiSrc));
+        if (it != DocBrokers.end())
+            docBroker = it->second;
+    }
+
+    if (!docBroker)
+    {
+        LOG_ERR_S("No DocBroker for WOPISrc [" << wopiSrc << "] tag [" << tag << ']');
+        job->fail(QWebEngineUrlRequestJob::UrlNotFound);
+        return;
+    }
+
+    const std::string mediaPath = docBroker->getEmbeddedMediaPath(tag);
+    if (mediaPath.empty())
+    {
+        LOG_ERR_S("No media path for tag [" << tag << "] WOPISrc [" << wopiSrc << ']');
+        job->fail(QWebEngineUrlRequestJob::UrlNotFound);
+        return;
+    }
+
+    auto* file = new QFile(QString::fromStdString(mediaPath), job);
+    if (!file->open(QIODevice::ReadOnly))
+    {
+        LOG_ERR_S("Failed to open media file [" << mediaPath << "] for tag [" << tag
+                  << "]: " << file->errorString().toStdString());
+        job->fail(QWebEngineUrlRequestJob::UrlNotFound);
+        return;
+    }
+
+    // file:// pages have origin "null"; required for <video crossOrigin="anonymous">.
+    QMultiMap<QByteArray, QByteArray> headers;
+    headers.insert("Access-Control-Allow-Origin", "null");
+    job->setAdditionalResponseHeaders(headers);
+
+    job->reply(isVtt ? "text/vtt" : "application/octet-stream", file);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

qt/CoolUrlSchemeHandler.hpp
@@ -1,0 +1,25 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#pragma once
+
+#include <QWebEngineUrlSchemeHandler>
+
+/// Analogous to ios/Mobile/CoolURLSchemeHandler.mm and
+/// android/.../COWebViewClient.java.
+class CoolUrlSchemeHandler : public QWebEngineUrlSchemeHandler
+{
+public:
+    using QWebEngineUrlSchemeHandler::QWebEngineUrlSchemeHandler;
+    void requestStarted(QWebEngineUrlRequestJob* job) override;
+};
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */

qt/Makefile.am
@@ -87,7 +87,7 @@ coda_qt_CXXFLAGS += -mno-direct-extern-access
 endif
 endif
 
-coda_qt_SOURCES = coda-qt.cpp Application.cpp Bridge.cpp QtClipboard.cpp DocumentOperations.cpp WebView.cpp DBusService.cpp Document.cpp $(cmake_list)
+coda_qt_SOURCES = coda-qt.cpp Application.cpp Bridge.cpp CoolUrlSchemeHandler.cpp QtClipboard.cpp DocumentOperations.cpp WebView.cpp DBusService.cpp Document.cpp $(cmake_list)
 nodist_coda_qt_SOURCES = $(BUILT_SOURCES)
 
 metainfodir = $(datadir)/metainfo

qt/coda-qt.cpp
@@ -33,6 +33,7 @@
 #include <QString>
 #include <QTranslator>
 #include <QWebEngineProfile>
+#include <QWebEngineUrlScheme>
 
 #include <pwd.h>
 
@@ -129,6 +130,18 @@ namespace
 
 int main(int argc, char** argv)
 {
+    // Must run before QApplication.
+    // LocalScheme + LocalAccessAllowed lets the file:// page reach cool:.
+    {
+        QWebEngineUrlScheme scheme("cool");
+        scheme.setSyntax(QWebEngineUrlScheme::Syntax::Path);
+        scheme.setFlags(QWebEngineUrlScheme::SecureScheme
+                      | QWebEngineUrlScheme::LocalScheme
+                      | QWebEngineUrlScheme::LocalAccessAllowed
+                      | QWebEngineUrlScheme::CorsEnabled);
+        QWebEngineUrlScheme::registerScheme(scheme);
+    }
+
     QApplication app(argc, argv);
 
     user_name = getUserName();