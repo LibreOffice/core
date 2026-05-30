980348daa68fd0d37e0b590e7ae5eda2c85b8c90
Sebastiaan Veld <notify@virtfields.nl>
2026-05-30 15:58:50 +0200

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (36 of 36 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/nl/
    Signed-off-by: Sebastiaan Veld <notify@virtfields.nl>
    Change-Id: I8c171eee9182dc28d33f916e5074dc9544616921

--- qt/translations/coda_nl.ts
+++ qt/translations/coda_nl.ts
@@ -165,7 +165,7 @@
     <message>
         <location filename="../Bridge.cpp" line="803"/>
         <source>Save File</source>
-        <translation type="unfinished"></translation>
+        <translation>Bestand opslaan</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="158"/>
