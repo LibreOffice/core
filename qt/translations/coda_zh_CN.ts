8793f72f18c707e5eed1e4f9bc448d50c9185817
Poesty Li <poesty7450@gmail.com>
2026-05-30 09:47:34 +0200

    Translated using Weblate (Chinese (Simplified Han script))
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/zh_Hans/
    Signed-off-by: Poesty Li <poesty7450@gmail.com>
    Change-Id: Icd5d111c5b549b7f9d05c9d6169aee308a963441

--- qt/translations/coda_zh_CN.ts
+++ qt/translations/coda_zh_CN.ts
@@ -6,174 +6,174 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="49"/>
         <source>ODF text document (.odt)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF 文本文档 (.odt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="50"/>
         <source>Rich Text (.rtf)</source>
-        <translation type="unfinished"></translation>
+        <translation>富文本格式 (.rtf)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="51"/>
         <source>Word Document (.docx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Word 文档 (.docx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="52"/>
         <source>Word 2003 Document (.doc)</source>
-        <translation type="unfinished"></translation>
+        <translation>Word 2003 文档 (.doc)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="58"/>
         <source>ODF spreadsheet (.ods)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF 电子表格 (.ods)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="59"/>
         <source>Excel Spreadsheet (.xlsx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Excel 电子表格 (.xlsx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="60"/>
         <source>Excel 2003 Spreadsheet (.xls)</source>
-        <translation type="unfinished"></translation>
+        <translation>Excel 2003 电子表格 (.xls)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="66"/>
         <source>ODF presentation (.odp)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF 演示文稿 (.odp)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="67"/>
         <source>PowerPoint Presentation (.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>PowerPoint 演示文稿 (.pptx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="68"/>
         <source>PowerPoint 2003 Presentation (.ppt)</source>
-        <translation type="unfinished"></translation>
+        <translation>PowerPoint 2003 演示文稿 (.ppt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="74"/>
         <source>ODF drawing (.odg)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF 绘图 (.odg)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="106"/>
         <source>Print Document</source>
-        <translation type="unfinished"></translation>
+        <translation>打印文档</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="114"/>
         <source>Select Printer:</source>
-        <translation type="unfinished"></translation>
+        <translation>选择打印机：</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="123"/>
         <location filename="../DocumentOperations.cpp" line="196"/>
         <source>Default Printer</source>
-        <translation type="unfinished"></translation>
+        <translation>默认打印机</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="128"/>
         <source>Print to File</source>
-        <translation type="unfinished"></translation>
+        <translation>打印到文件</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="132"/>
         <source>Enter file path...</source>
-        <translation type="unfinished"></translation>
+        <translation>输入文件路径...</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="144"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>将打印输出另存为</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="146"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF 文件 (*.pdf);;所有文件 (*)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="162"/>
         <source>Print</source>
-        <translation type="unfinished"></translation>
+        <translation>打印</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="163"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="187"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>打印到文件错误</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="188"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法将文档保存到文件。请检查文件路径和权限。</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="232"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>打印错误</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="233"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>无法打印文档。请检查打印机设置。</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="739"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>所有文件 (*)</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="249"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>保存文档</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="625"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>打开文件</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="626"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>所有文件 (*);;文本文档 (*.odt *.ott *.doc *.docx *.rtf *.txt);;电子表格 (*.ods *.ots *.xls *.xlsx *.csv);;演示文稿 (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="737"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>导出为</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="156"/>
         <source>Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>演示文稿</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="161"/>
         <location filename="../WebView.cpp" line="177"/>
         <source>Text Document</source>
-        <translation type="unfinished"></translation>
+        <translation>文本文档</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="166"/>
         <source>Spreadsheet</source>
-        <translation type="unfinished"></translation>
+        <translation>电子表格</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="171"/>
         <source>Drawing</source>
-        <translation type="unfinished"></translation>
+        <translation>绘图</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="317"/>
         <source>Presenting</source>
-        <translation type="unfinished"></translation>
+        <translation>演示中</translation>
     </message>
 </context>
 <context>
@@ -181,17 +181,17 @@
     <message>
         <location filename="../WebView.cpp" line="233"/>
         <source>Unsaved Changes</source>
-        <translation type="unfinished"></translation>
+        <translation>未保存的更改</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="234"/>
         <source>The document has unsaved changes. Do you want to close anyway?</source>
-        <translation type="unfinished"></translation>
+        <translation>文档有未保存的更改。是否仍要关闭？</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="609"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>开始</translation>
     </message>
 </context>
 </TS>
