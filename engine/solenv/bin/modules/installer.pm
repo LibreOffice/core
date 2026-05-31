90e5882d64781426bbe04c797c9cd9d0b3186a6e
Andras Timar <andras.timar@collabora.com>
2026-05-31 23:28:25 +0200

    installer: fall back to PATH md5sum when $MD5SUM isn't exported
    Both call sites interpolated $ENV{'MD5SUM'} directly into backtick
    shell pipelines for hashing a long language list into a short id.
    In a build environment that doesn't export MD5SUM (e.g. a clean
    container without distro packaging glue), the pipe collapsed to
    `echo … |  | sed …` — a dash syntax error and a Perl uninitialized-
    value warning — and the language-pack subdir lost its 8-char id
    suffix, breaking uniqueness across long-list installs.
    
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: I8a63474d9a9f8cf9ceee834297f8f18eeedaa248

--- engine/solenv/bin/modules/installer.pm
+++ engine/solenv/bin/modules/installer.pm
@@ -390,7 +390,8 @@ sub run {
         if (length($loglanguagestring) > $installer::globals::max_lang_length)
         {
             my $number_of_languages = installer::systemactions::get_number_of_langs($loglanguagestring);
-            chomp(my $shorter = `echo $loglanguagestring | $ENV{'MD5SUM'} | sed -e "s/ .*//g"`);
+            my $md5sum = $ENV{'MD5SUM'} || 'md5sum';
+            chomp(my $shorter = `echo $loglanguagestring | $md5sum | sed -e "s/ .*//g"`);
             my $id = substr($shorter, 0, 8); # taking only the first 8 digits
             $loglanguagestring = "lang_" . $number_of_languages . "_id_" . $id;
         }
--- engine/solenv/bin/modules/installer/systemactions.pm
+++ engine/solenv/bin/modules/installer/systemactions.pm
@@ -255,7 +255,8 @@ sub create_directories
                 if (length($languagestring) > $installer::globals::max_lang_length )
                 {
                     my $number_of_languages = get_number_of_langs($languagestring);
-                    chomp(my $shorter = `echo $languagestring | $ENV{'MD5SUM'} | sed -e "s/ .*//g"`);
+                    my $md5sum = $ENV{'MD5SUM'} || 'md5sum';
+                    chomp(my $shorter = `echo $languagestring | $md5sum | sed -e "s/ .*//g"`);
                     my $id = substr($shorter, 0, 8); # taking only the first 8 digits
                     $languagestring = "lang_" . $number_of_languages . "_id_" . $id;
                 }
