467b3ff0e39044519386a101808bd211e3a0c9d5
Xisco Fauli <xiscofauli@libreoffice.org>
2026-06-01 11:23:23 +0200

    libcmis: upgrade to 0.6.3
    Change-Id: I835c68a209386d051d059b5b7d3a23ad8b2356f9
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205865
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins
    (cherry picked from commit 7281f570cc67d16dec79923cfdde280ae0d8ef09)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205872
    Tested-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>
    Reviewed-by: Christian Lohmaier <lohmaier+LibreOffice@googlemail.com>

--- external/libcmis/UnpackedTarball_libcmis.mk
+++ external/libcmis/UnpackedTarball_libcmis.mk
@@ -11,19 +11,4 @@ $(eval $(call gb_UnpackedTarball_UnpackedTarball,libcmis))
 
 $(eval $(call gb_UnpackedTarball_set_tarball,libcmis,$(LIBCMIS_TARBALL)))
 
-# 0001-cid-1545775-COPY_INSTEAD_OF_MOVE.patch is upstreamed
-
-$(eval $(call gb_UnpackedTarball_add_patches,libcmis,\
-	external/libcmis/0001-cid-1545775-COPY_INSTEAD_OF_MOVE.patch \
-	external/libcmis/0001-Fix-warning-C4589-when-building-with-MSVC.patch \
-	external/libcmis/http-session-cleanup.patch.1 \
-	external/libcmis/factory-no-retry-ssl.patch.1 \
-	external/libcmis/sharepoint-auth.patch.1 \
-	external/libcmis/exceptions.patch.1 \
-	external/libcmis/0001-cppcheck-operatorEqVarError-in-src-libcmis-http-sess.patch \
-	external/libcmis/0001-Take-into-account-m_CurlInitProtocolsFunction-in-cop.patch \
-	external/libcmis/initprotocols.patch.1 \
-	external/libcmis/0001-Fix-boost-1.86-breakage.patch \
-))
-
 # vim: set noet sw=4 ts=4:
