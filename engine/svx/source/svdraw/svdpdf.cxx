61d89c1c37610aa3116429b13bd8d8ba3ddb4774
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-06-07 16:56:41 +0100

    ofz#518578175 Null-dereference READ
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I3d959f515fe6af816bb5c309495093dfd66a5416
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/4004
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- engine/svx/source/svdraw/svdpdf.cxx
+++ engine/svx/source/svdraw/svdpdf.cxx
@@ -1734,8 +1734,9 @@ Color ImpSdrPdfImport::getStrokeColor(
         = pPageObject->getRenderedStrokePattern(*mpPdfDocument, *pPage))
     {
         Bitmap aBitmap(bitmap->createBitmapFromBuffer());
-        return aBitmap.GetPixelColor(aBitmap.GetSizePixel().Width() / 2,
-                                     aBitmap.GetSizePixel().Height() / 2);
+        if (!aBitmap.IsEmpty())
+            return aBitmap.GetPixelColor(aBitmap.GetSizePixel().Width() / 2,
+                                         aBitmap.GetSizePixel().Height() / 2);
     }
     return pPageObject->getStrokeColor();
 }
@@ -1749,8 +1750,9 @@ Color ImpSdrPdfImport::getFillColor(std::unique_ptr<vcl::pdf::PDFiumPageObject> 
         = pPageObject->getRenderedFillPattern(*mpPdfDocument, *pPage))
     {
         Bitmap aBitmap(bitmap->createBitmapFromBuffer());
-        return aBitmap.GetPixelColor(aBitmap.GetSizePixel().Width() / 2,
-                                     aBitmap.GetSizePixel().Height() / 2);
+        if (!aBitmap.IsEmpty())
+            return aBitmap.GetPixelColor(aBitmap.GetSizePixel().Width() / 2,
+                                         aBitmap.GetSizePixel().Height() / 2);
     }
     return pPageObject->getFillColor();
 }
