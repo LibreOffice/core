b73b04826f461189af4c8b2e17fd807654f1258d
Michael Weghorn <m.weghorn@posteo.de>
2026-06-11 11:43:32 +0200

    tdf#130857 qt weld: Support "Accessibility" options tab page
    Declare support for this options tab page conditional
    on the parent being a native widget.
    
    This means that native Qt widgets will be used once support
    for the "Tools" -> "Options" dialog is declared when
    using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    While more work will be needed for that dialog, this
    page newly declared as supported works fine in a quick
    test with the dialog's UI file ("cui/ui/optionsdialog.ui")
    also being declared as supported locally.
    
    Before that dialog can be enabled "for real", remaining issues
    will have to be addressed. But then, all already supported
    tab pages will "just work", without having to be added then,
    so this is one step towards future support for the dialog.
    
    The tab page can be reached like this:
    
    * start Writer
    * "Tools" -> "Options"
    * "LibreOfficeDev" -> "Accessibility"
    
    Change-Id: I28eb811d8ababe13ef86f12f540ae104ca225e54
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/206382
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -386,6 +386,7 @@ constexpr auto SUPPORTED_WITH_QT_PARENT = frozen::make_unordered_set<std::u16str
     u"cui/ui/numberingformatpage.ui",
     u"cui/ui/numberingoptionspage.ui",
     u"cui/ui/numberingpositionpage.ui",
+    u"cui/ui/optaccessibilitypage.ui",
     u"cui/ui/optgeneralpage.ui",
     u"cui/ui/optlingupage.ui",
     u"cui/ui/optpathspage.ui",
