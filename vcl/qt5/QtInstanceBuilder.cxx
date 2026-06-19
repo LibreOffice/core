3caf680ec875c471c015306b02f1d31b524f5073
Michael Weghorn <m.weghorn@posteo.de>
2026-06-19 15:27:20 +0200

    tdf#130857 qt weld: Support Hangul/Hanja "New Dictionary" dlg
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * paste text "한글맞춤법" into the document
    * select the text and set the language to "Korean (RoK")
    * "Tools" -> "Language" -> "Hangul/Hanja Conversion..."
    * in the "Hangul/Hanja Conversion" dialog, press the "Options..."
      button
    * in the "Hangul/Hanja Options" dialog, press the "New..." button
    
    Change-Id: I7c503f17df5effa53e893a2b5d855a6a10744624
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207045
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -79,6 +79,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"cui/ui/gallerytitledialog.ui",
     u"cui/ui/galleryupdateprogress.ui",
     u"cui/ui/graphictestdlg.ui",
+    u"cui/ui/hangulhanjaadddialog.ui",
     u"cui/ui/hangulhanjaoptdialog.ui",
     u"cui/ui/hyperlinkdlg.ui",
     u"cui/ui/hyperlinkmarkdialog.ui",
