89fdef7abfa3a09ad583a2574cbc4d716542d933
Michael Weghorn <m.weghorn@posteo.de>
2026-06-19 12:13:17 +0200

    tdf#130857 qt weld: Support "Import Libraries" macro dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * "Tools" -> "Macros" -> "Organize Macros" -> "Basic..."
    * select the current document in the "Macros From" section
    * press "New" and confirm name using the "OK" button
    * close Basic IDE and save the document
    * create a new document
    * "Tools" -> "Macros" -> "Organize Macros" -> "Basic..."
    * press the "Organizer..." button
    * switch to the "Libraries" tab
    * press the "Import..." button
    * select the previously saved file in the file dialog
    
    Change-Id: Ib187931a0ca656e877a89a445a6a7f5db89bd060
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207033
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -153,6 +153,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/BasicIDE/ui/deletelangdialog.ui",
     u"modules/BasicIDE/ui/exportdialog.ui",
     u"modules/BasicIDE/ui/gotolinedialog.ui",
+    u"modules/BasicIDE/ui/importlibdialog.ui",
     u"modules/BasicIDE/ui/managelanguages.ui",
     u"modules/BasicIDE/ui/newlibdialog.ui",
     u"modules/sbibliography/ui/choosedatasourcedialog.ui",
