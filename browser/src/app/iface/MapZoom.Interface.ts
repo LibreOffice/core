733f173c4f808b3f588468642e2d1f33c6cbf4cf
Dennis Francis <dennis.francis@collabora.com>
2026-06-20 09:22:19 +0530

    browser: add missing headers
    Change-Id: I50d94e58a00a49244740e208fe701f494f114b36
    Signed-off-by: Dennis Francis <dennis.francis@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/4876
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Andras Timar <andras.timar@collabora.com>

--- browser/src/app/iface/MapZoom.Interface.ts
+++ browser/src/app/iface/MapZoom.Interface.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 // Subset of MapInterface containing zoom options.
 // Needed in ServerCommand and its unit tests.
 interface MapZoomInterface {
