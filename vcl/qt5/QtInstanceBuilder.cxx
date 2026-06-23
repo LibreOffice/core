3a00f16997e047a1de03f62b4c607d9654763947
Michael Weghorn <m.weghorn@posteo.de>
2026-06-23 08:08:28 +0200

    tdf#130857 qt weld: Support Writer "Frame" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Writer
    * insert an image using "Insert" -> "Image..."
    * with the inserted image still selected, right-click it
      to open context menu
    * select the "Properties..." context menu entry
    
    Change-Id: If2384522c452dfe30702862cd981f39a0b318d4a
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207175
    Reviewed-by: Dan Williams <dcbw@libreoffice.org>
    Tested-by: Jenkins

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -266,6 +266,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/swriter/ui/findentrydialog.ui",
     u"modules/swriter/ui/footendnotedialog.ui",
     u"modules/swriter/ui/footnotepage.ui",
+    u"modules/swriter/ui/framedialog.ui",
     u"modules/swriter/ui/indexentry.ui",
     u"modules/swriter/ui/inforeadonlydialog.ui",
     u"modules/swriter/ui/insertbookmark.ui",
