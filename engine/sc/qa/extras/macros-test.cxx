LibreOffice/core

commit ea2a97438643a440e192dbe30d03988da65c6b18

Simplify NameContainer
Having a map, two vectors, and a counter for what is a job for a single
map is a bit too much.

Change-Id: I50d29f03a2463e6f63ed8e645c36b4c77aa1413a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/177604
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

basic/source/inc/namecont.hxx
@@ -66,12 +66,7 @@ typedef ::comphelper::WeakImplHelper<
 
 class NameContainer final : public NameContainer_BASE
 {
-    typedef std::unordered_map < OUString, sal_Int32 > NameContainerNameMap;
-
-    NameContainerNameMap mHashMap;
-    std::vector< OUString > mNames;
-    std::vector< css::uno::Any > mValues;
-    sal_Int32 mnElementCount;
+    std::unordered_map<OUString, css::uno::Any> maMap;
 
     css::uno::Type mType;
     css::uno::XInterface* mpxEventSource;
@@ -81,19 +76,12 @@ class NameContainer final : public NameContainer_BASE
 
 public:
     NameContainer( const css::uno::Type& rType )
-        : mnElementCount( 0 )
-        , mType( rType )
+        : mType( rType )
         , mpxEventSource( nullptr )
     {}
 
     void setEventSource( css::uno::XInterface* pxEventSource )
         { mpxEventSource = pxEventSource; }
-
-    /// @throws css::lang::IllegalArgumentException
-    /// @throws css::container::ElementExistException
-    /// @throws css::lang::WrappedTargetException
-    /// @throws css::uno::RuntimeException
-    void insertCheck(const OUString& aName, const css::uno::Any& aElement);
 
     /// @throws css::lang::IllegalArgumentException
     /// @throws css::lang::WrappedTargetException

basic/source/uno/namecont.cxx
@@ -107,33 +107,28 @@ Type NameContainer::getElementType()
 
 sal_Bool NameContainer::hasElements()
 {
-    bool bRet = (mnElementCount > 0);
-    return bRet;
+    return !maMap.empty();
 }
 
 // Methods XNameAccess
 Any NameContainer::getByName( const OUString& aName )
 {
-    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
-    if( aIt == mHashMap.end() )
+    auto aIt = maMap.find(aName);
+    if (aIt == maMap.end())
     {
-        throw NoSuchElementException();
+        throw NoSuchElementException(aName);
     }
-    sal_Int32 iHashResult = (*aIt).second;
-    Any aRetAny = mValues[ iHashResult ];
-    return aRetAny;
+    return aIt->second;
 }
 
 Sequence< OUString > NameContainer::getElementNames()
 {
-    return comphelper::containerToSequence(mNames);
+    return comphelper::mapKeysToSequence(maMap);
 }
 
 sal_Bool NameContainer::hasByName( const OUString& aName )
 {
-    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
-    bool bRet = ( aIt != mHashMap.end() );
-    return bRet;
+    return maMap.contains(aName);
 }
 
 
@@ -145,15 +140,13 @@ void NameContainer::replaceByName( const OUString& aName, const Any& aElement )
     {
         throw IllegalArgumentException(u"types do not match"_ustr, getXWeak(), 2);
     }
-    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
-    if( aIt == mHashMap.end() )
+    auto aIt = maMap.find(aName);
+    if (aIt == maMap.end())
     {
-        throw NoSuchElementException();
+        throw NoSuchElementException(aName);
     }
-    sal_Int32 iHashResult = (*aIt).second;
-    Any aOldElement = mValues[ iHashResult ];
-    mValues[ iHashResult ] = aElement;
-
+    Any aOldElement = aIt->second;
+    aIt->second = aElement;
 
     std::unique_lock aGuard(m_aMutex);
 
@@ -181,16 +174,6 @@ void NameContainer::replaceByName( const OUString& aName, const Any& aElement )
     }
 }
 
-void NameContainer::insertCheck(const OUString& aName, const Any& aElement)
-{
-    NameContainerNameMap::iterator aIt = mHashMap.find(aName);
-    if( aIt != mHashMap.end() )
-    {
-        throw ElementExistException();
-    }
-    insertNoCheck(aName, aElement);
-}
-
 void NameContainer::insertNoCheck(const OUString& aName, const Any& aElement)
 {
     const Type& aAnyType = aElement.getValueType();
@@ -199,12 +182,7 @@ void NameContainer::insertNoCheck(const OUString& aName, const Any& aElement)
         throw IllegalArgumentException(u"types do not match"_ustr, getXWeak(), 2);
     }
 
-    sal_Int32 nCount = mNames.size();
-    mNames.push_back( aName );
-    mValues.push_back( aElement );
-
-    mHashMap[ aName ] = nCount;
-    mnElementCount++;
+    maMap[aName] = aElement;
 
     std::unique_lock aGuard(m_aMutex);
 
@@ -234,31 +212,21 @@ void NameContainer::insertNoCheck(const OUString& aName, const Any& aElement)
 // Methods XNameContainer
 void NameContainer::insertByName( const OUString& aName, const Any& aElement )
 {
-    insertCheck(aName, aElement);
+    if (hasByName(aName))
+        throw ElementExistException(aName);
+    insertNoCheck(aName, aElement);
 }
 
 void NameContainer::removeByName( const OUString& aName )
 {
-    NameContainerNameMap::iterator aIt = mHashMap.find( aName );
-    if( aIt == mHashMap.end() )
+    auto aIt = maMap.find(aName);
+    if (aIt == maMap.end())
     {
-        OUString sMessage = "\"" + aName + "\" not found";
-        throw NoSuchElementException(sMessage);
+        throw NoSuchElementException("\"" + aName + "\" not found");
     }
 
-    sal_Int32 iHashResult = (*aIt).second;
-    Any aOldElement = mValues[ iHashResult ];
-    mHashMap.erase( aIt );
-    sal_Int32 iLast = mNames.size() - 1;
-    if( iLast != iHashResult )
-    {
-        mNames[ iHashResult ] = mNames[ iLast ];
-        mValues[ iHashResult ] = mValues[ iLast ];
-        mHashMap[ mNames[ iHashResult ] ] = iHashResult;
-    }
-    mNames.resize( iLast );
-    mValues.resize( iLast );
-    mnElementCount--;
+    Any aOldElement = aIt->second;
+    maMap.erase(aIt);
 
     std::unique_lock aGuard(m_aMutex);
 

commit bfe67bc782cf89e944324042c8e53c42f4b6421b

cool#15956: keep insertion order in NameContainer's getElementNames
This is required to keep content of Basic/script-lc.xml and each
Basic/<lib>/script-lb.xml byte-identical on round-trip, which is
necessary for signatures to stay valid; this broke after commit
ea2a97438643a440e192dbe30d03988da65c6b18 (Simplify NameContainer,
2024-12-01).

Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
Change-Id: Ib30b701dce8bbcb919c88f5b66f5134150ded575
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/5025
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

engine/basic/source/inc/namecont.hxx
@@ -20,6 +20,7 @@
 #pragma once
 
 #include <unordered_map>
+#include <vector>
 #include <com/sun/star/uno/XComponentContext.hpp>
 #include <com/sun/star/lang/XInitialization.hpp>
 #include <com/sun/star/script/XStorageBasedLibraryContainer.hpp>
@@ -58,6 +59,9 @@ class NameContainer final
     cppu::OWeakObject& rOwner;
 
     std::unordered_map<OUString, css::uno::Any> maMap;
+    // Enumeration must keep insertion order: it drives the on-disk order of the
+    // library index file and module streams, which macro signatures sign over.
+    std::vector<OUString> maNamesOrder;
 
     css::uno::Type mType;
     css::uno::XInterface* mpxEventSource;

engine/basic/source/uno/namecont.cxx
@@ -65,6 +65,8 @@
 #include <comphelper/storagehelper.hxx>
 #include <cppuhelper/exc_hlp.hxx>
 #include <cppuhelper/supportsservice.hxx>
+#include <algorithm>
+#include <cassert>
 #include <memory>
 #include <string_view>
 
@@ -93,6 +95,21 @@ using com::sun::star::uno::Reference;
 // #i34411: Flag for error handling during migration
 static bool GbMigrationSuppressErrors = false;
 
+#ifndef NDEBUG
+namespace
+{
+// The name->value map and the insertion-order vector are redundant views of the
+// same set of names; verify they list exactly the same names, once each.
+bool mapAndOrderConsistent(const std::unordered_map<OUString, Any>& rMap,
+                           const std::vector<OUString>& rOrder)
+{
+    if (rMap.size() != rOrder.size())
+        return false;
+    return std::all_of(rMap.begin(), rMap.end(), [&rOrder](const auto& rEntry)
+        { return std::find(rOrder.begin(), rOrder.end(), rEntry.first) != rOrder.end(); });
+}
+}
+#endif
 
 // Implementation class NameContainer
 
@@ -120,7 +137,8 @@ Any NameContainer::getByName( const OUString& aName )
 
 Sequence< OUString > NameContainer::getElementNames()
 {
-    return comphelper::mapKeysToSequence(maMap);
+    // Return the names in insertion order.
+    return comphelper::containerToSequence(maNamesOrder);
 }
 
 bool NameContainer::hasByName( const OUString& aName )
@@ -179,7 +197,10 @@ void NameContainer::insertNoCheck(const OUString& aName, const Any& aElement,
         throw IllegalArgumentException(u"types do not match"_ustr, rOwner, 2);
     }
 
+    assert(!maMap.contains(aName) && "insertNoCheck: name already present");
     maMap[aName] = aElement;
+    maNamesOrder.push_back(aName);
+    assert(mapAndOrderConsistent(maMap, maNamesOrder));
 
     // Fire event
     if (maContainerListeners.getLength(guard) > 0)
@@ -223,6 +244,8 @@ void NameContainer::removeByName(const OUString& aName, std::unique_lock<std::mu
 
     Any aOldElement = aIt->second;
     maMap.erase(aIt);
+    std::erase(maNamesOrder, aName);
+    assert(mapAndOrderConsistent(maMap, maNamesOrder));
 
     // Fire event
     if (maContainerListeners.getLength(guard) > 0)

engine/sc/qa/extras/macros-test.cxx
@@ -20,7 +20,9 @@
 #include <document.hxx>
 #include <scitems.hxx>
 
+#include <algorithm>
 #include <chrono>
+#include <vector>
 
 #include <com/sun/star/sheet/XCellRangeAddressable.hpp>
 #include <com/sun/star/sheet/XCellRangeMovement.hpp>
@@ -28,8 +30,11 @@
 #include <com/sun/star/sheet/XSpreadsheet.hpp>
 #include <com/sun/star/table/XColumnRowRange.hpp>
 
-#include <com/sun/star/script/XLibraryContainerPassword.hpp>
+#include <com/sun/star/container/XNameContainer.hpp>
+#include <com/sun/star/document/XEmbeddedScripts.hpp>
 #include <com/sun/star/drawing/XDrawPageSupplier.hpp>
+#include <com/sun/star/script/XLibraryContainer.hpp>
+#include <com/sun/star/script/XLibraryContainerPassword.hpp>
 #include <editeng/brushitem.hxx>
 
 using namespace ::com::sun::star;
@@ -751,6 +756,40 @@ CPPUNIT_TEST_FIXTURE(ScMacrosTest, testTdf107572)
 
         CPPUNIT_ASSERT_EQUAL(COL_GRAY3, rColor2);
     }
+}
+
+CPPUNIT_TEST_FIXTURE(ScMacrosTest, testCool15956)
+{
+    // A Basic library enumerates its modules in insertion order, and that order must survive a
+    // save/reload unchanged: it determines the on-disk byte layout of the library index and module
+    // streams, which macro signatures sign over (cool#15956).
+    createScDoc();
+
+    // Deliberately non-sorted insertion order.
+    const OUString order[]{ u"Gamma"_ustr, u"Alpha"_ustr, u"Delta"_ustr, u"Beta"_ustr };
+
+    auto checkOrder = [&order](const uno::Sequence<OUString>& names) {
+        CPPUNIT_ASSERT(std::equal(names.begin(), names.end(), std::begin(order), std::end(order)));
+    };
+
+    auto xDocScr = mxComponent.queryThrow<document::XEmbeddedScripts>();
+    auto xLibs = xDocScr->getBasicLibraries().queryThrow<script::XLibraryContainer>();
+    auto xLibrary = xLibs->createLibrary(u"TestLibrary"_ustr);
+    for (const OUString& rName : order)
+        xLibrary->insertByName(rName, uno::Any(u"Sub "_ustr + rName + u"\nEnd Sub\n"_ustr));
+
+    // In memory, getElementNames() must reflect insertion order.
+    checkOrder(xLibrary->getElementNames());
+
+    saveAndReload(TestFilter::ODS);
+
+    // After the store/reload, the reloaded library keeps the same module order;
+    // without insertion-order preservation it came back permuted.
+    xDocScr = mxComponent.queryThrow<document::XEmbeddedScripts>();
+    xLibs = xDocScr->getBasicLibraries().queryThrow<script::XLibraryContainer>();
+    xLibs->loadLibrary(u"TestLibrary"_ustr);
+    auto xReloaded = xLibs->getByName(u"TestLibrary"_ustr).queryThrow<container::XNameAccess>();
+    checkOrder(xReloaded->getElementNames());
 }
 
 CPPUNIT_TEST_FIXTURE(ScMacrosTest, testShapeLayerId)

engine/xmlsecurity/qa/unit/signing/signing.cxx
@@ -22,8 +22,10 @@
 
 #include <com/sun/star/beans/XPropertySet.hpp>
 #include <com/sun/star/document/BrokenPackageRequest.hpp>
+#include <com/sun/star/document/XEmbeddedScripts.hpp>
 #include <com/sun/star/embed/XStorage.hpp>
 #include <com/sun/star/embed/XTransactedObject.hpp>
+#include <com/sun/star/script/XLibraryContainer.hpp>
 #include <com/sun/star/security/CertificateValidity.hpp>
 #include <com/sun/star/security/DocumentDigitalSignatures.hpp>
 #include <com/sun/star/security/XDocumentDigitalSignatures.hpp>
@@ -1246,6 +1248,37 @@ CPPUNIT_TEST_FIXTURE(SigningTest, testImplicitScriptSign)
                 "/odfds:document-signatures/dsig:Signature[1]/dsig:SignedInfo/"
                 "dsig:Reference[@URI='META-INF/macrosignatures.xml']",
                 1);
+}
+
+CPPUNIT_TEST_FIXTURE(SigningTest, testCool15956)
+{
+    // The Basic library and module order is part of the signed macro streams (Basic/script-lc.xml
+    // and each Basic/<lib>/script-lb.xml). Re-saving a document must keep the insertion order to
+    // keep macro signatures valid.
+    loadFromFile(u"Cool15956.ods");
+    SfxBaseModel* pBaseModel = dynamic_cast<SfxBaseModel*>(mxComponent.get());
+    CPPUNIT_ASSERT(pBaseModel);
+    SfxObjectShell* pObjectShell = pBaseModel->GetObjectShell();
+    CPPUNIT_ASSERT(pObjectShell);
+    SignatureState eBefore = pObjectShell->GetScriptingSignatureState();
+    CPPUNIT_ASSERT(eBefore == SignatureState::OK || eBefore == SignatureState::NOTVALIDATED);
+
+    // Load the libraries so that storing regenerates their index and module streams.
+    auto xScripts = mxComponent.queryThrow<document::XEmbeddedScripts>();
+    auto xLibs = xScripts->getBasicLibraries().queryThrow<script::XLibraryContainer>();
+    for (const OUString& rLib : xLibs->getElementNames())
+        xLibs->loadLibrary(rLib);
+
+    saveAndReload(TestFilter::ODS);
+
+    pBaseModel = dynamic_cast<SfxBaseModel*>(mxComponent.get());
+    CPPUNIT_ASSERT(pBaseModel);
+    pObjectShell = pBaseModel->GetObjectShell();
+    CPPUNIT_ASSERT(pObjectShell);
+    SignatureState eAfter = pObjectShell->GetScriptingSignatureState();
+    // Without the NameContainer insertion-order fix the resave reordered the signed
+    // Basic streams and the macro signature was dropped (SignatureState::NOSIGNATURES).
+    CPPUNIT_ASSERT(eAfter == SignatureState::OK || eAfter == SignatureState::NOTVALIDATED);
 }
 
 #if HAVE_FEATURE_GPGVERIFY