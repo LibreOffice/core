f8d329eb43e4a4c07c11a5ed4d7ee430a9ddd5ec
Michael Weghorn <m.weghorn@posteo.de>
2026-06-25 12:36:48 +0200

    tdf#130857 qt weld: Support "Microsoft Office" Load/Save options page
    Declare support for this options tab page conditional
    on the parent being a native widget.
    
    This means that native Qt widgets will be used once support
    for the "Tools" -> "Options" dialog is declared when
    using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    While more work will be needed for that dialog, this
    page newly declared as supported works fine in a quick
    test with the dialog's UI file ("cui/ui/optionsdialog.ui")
    also being declared as supported locally.
    
    Before that dialog can be enabled "for real", remaining issues
    will have to be addressed. But then, all already supported
    tab pages will "just work", without having to be added then,
    so this is one step towards future support for the dialog.
    
    The tab page can be triggered like this:
    
    * start Writer
    * "Tools" -> "Options"
    * "Load/Save" -> "Microsoft Office"
    
    This depends on previous commit
    
        Change-Id: I4d7ce5b0472e3a22c441b49dcd54ba2b068702e4
        Author: Michael Weghorn <m.weghorn@posteo.de>
        Date:   Thu Jun 25 12:27:58 2026 +0200
    
            tdf#130857 Drop extra toggle renderer in MS Office Load/Save options
    
    that addresses an issue otherwise encountered when
    using the qt weld implementation.
    
    Change-Id: Ia139b98a587fe1855906b44d41931bfc7af2a6e7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/207310
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -418,6 +418,7 @@ constexpr auto SUPPORTED_WITH_QT_PARENT = frozen::make_unordered_set<std::u16str
     u"cui/ui/optasianpage.ui",
     u"cui/ui/optbasicidepage.ui",
     u"cui/ui/optctlpage.ui",
+    u"cui/ui/optfltrembedpage.ui",
     u"cui/ui/optfltrpage.ui",
     u"cui/ui/optgeneralpage.ui",
     u"cui/ui/opthtmlpage.ui",
