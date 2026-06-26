LibreOffice/core

commit d336e6c26012255015d3fc0caf8e7fafe14bd8f2

tdf#69647 sw layout: fix line spacing with inline pictures
Line height is now based on (the biggest) font size in the
line rather than on the size of the tallest object there,
according to ODF and like MSO does.

Note: handling of first paragraph line is still different.

Co-authored-by: Attila Bánhegyi (NISZ)

Change-Id: I6cebea7dbdedd21ea173a0a867434525e7b86337
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/101272
Tested-by: László Németh <nemeth@numbertext.org>
Reviewed-by: László Németh <nemeth@numbertext.org>

sw/source/core/text/itrform2.cxx
@@ -324,7 +324,7 @@ void SwTextFormatter::InsertPortion( SwTextFormatInfo &rInf,
 
         // Adjust maxima
         if( m_pCurr->Height() < pPor->Height() )
-            m_pCurr->Height( pPor->Height() );
+            m_pCurr->Height( pPor->Height(), pPor->IsTextPortion() );
         if( m_pCurr->GetAscent() < pPor->GetAscent() )
             m_pCurr->SetAscent( pPor->GetAscent() );
 
@@ -776,7 +776,7 @@ void SwTextFormatter::CalcAscent( SwTextFormatInfo &rInf, SwLinePortion *pPor )
         // height (example: n758883.docx)
         SwLinePortion const*const pLast = rInf.GetLast();
         assert(pLast);
-        pPor->Height( pLast->Height() );
+        pPor->Height( pLast->Height(), false );
         pPor->SetAscent( pLast->GetAscent() );
     }
     else
@@ -1523,7 +1523,7 @@ SwLinePortion *SwTextFormatter::NewPortion( SwTextFormatInfo &rInf )
     if( !m_pCurr->Height() )
     {
         OSL_ENSURE( m_pCurr->Height(), "SwTextFormatter::NewPortion: limbo dance" );
-        m_pCurr->Height( pPor->Height() );
+        m_pCurr->Height( pPor->Height(), false );
         m_pCurr->SetAscent( pPor->GetAscent() );
     }
 
@@ -1613,7 +1613,7 @@ TextFrameIndex SwTextFormatter::FormatLine(TextFrameIndex const nStartPos)
         if( GetInfo().IsStop() )
         {
             m_pCurr->SetLen(TextFrameIndex(0));
-            m_pCurr->Height( GetFrameRstHeight() + 1 );
+            m_pCurr->Height( GetFrameRstHeight() + 1, false );
             m_pCurr->SetRealHeight( GetFrameRstHeight() + 1 );
             m_pCurr->Width(0);
             m_pCurr->Truncate();
@@ -1770,7 +1770,7 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                        ( nLineHeight - m_pCurr->Height() + nRubyHeight ) / 2 :
                        ( nLineHeight - m_pCurr->Height() - nRubyHeight ) / 2 );
 
-        m_pCurr->Height( nLineHeight );
+        m_pCurr->Height( nLineHeight, false );
         m_pCurr->SetAscent( nAsc );
         m_pInf->GetParaPortion()->SetFixLineHeight();
 
@@ -1831,7 +1831,7 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                                 pCurr->SetClipping( true );
 #endif
                             m_pCurr->SetAscent( nAsc );
-                            m_pCurr->Height( nLineHeight );
+                            m_pCurr->Height( nLineHeight, false );
                             m_pInf->GetParaPortion()->SetFixLineHeight();
                         }
                     }
@@ -1849,7 +1849,7 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                     if( nAsc < m_pCurr->GetAscent() ||
                         nLineHeight - nAsc < m_pCurr->Height() - m_pCurr->GetAscent() )
                         m_pCurr->SetClipping( true );
-                    m_pCurr->Height( nLineHeight );
+                    m_pCurr->Height( nLineHeight, false );
                     m_pCurr->SetAscent( nAsc );
                     m_pInf->GetParaPortion()->SetFixLineHeight();
                 }
@@ -1871,10 +1871,13 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                         if( nTmp < 50 )
                             nTmp = nTmp ? 50 : 100;
 
-                        nTmp *= nLineHeight;
+                        // extend line height by (nPropLineSpace - 100) percent of the font height
+                        nTmp -= 100;
+                        nTmp *= m_pCurr->GetTextHeight();
                         nTmp /= 100;
-                        if( !nTmp )
-                            ++nTmp;
+                        nTmp += nLineHeight;
+                        if (nTmp < 1)
+                            nTmp = 1;
                         nLineHeight = static_cast<sal_uInt16>(nTmp);
                         break;
                     }

sw/source/core/text/porlay.cxx
@@ -193,6 +193,13 @@ void SwLineLayout::DeleteNext()
         delete a;
 }
 
+void SwLineLayout::Height(const sal_uInt16 nNew, const bool bText)
+{
+    SwPosSize::Height(nNew);
+    if (bText)
+        m_nTextHeight = nNew;
+}
+
 // class SwLineLayout: This is the layout of a single line, which is made
 // up of its dimension, the character count and the word spacing in the line.
 // Line objects are managed in an own pool, in order to store them continuously
@@ -382,7 +389,7 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
                     if( !pPos->GetNextPortion() )
                     {
                         if( !Height() )
-                            Height( pPos->Height() );
+                            Height( pPos->Height(), false );
                         if( !GetAscent() )
                             SetAscent( pPos->GetAscent() );
                     }
@@ -469,7 +476,7 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
                                 if( nTmp > nPosHeight )
                                     nPosHeight = nTmp;
                             }
-                            Height( nPosHeight );
+                            Height( nPosHeight, false );
                             nAscent = nPosAscent;
                             nMaxDescent = nPosHeight - nPosAscent;
                         }
@@ -479,12 +486,12 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
                         if( Height() < nPosHeight )
                         {
                             // Height is set to 0 when Init() is called.
-                            if (bIgnoreBlanksAndTabsForLineHeightCalculation && pPos->GetWhichPor() == PortionType::FlyCnt)
+                            if (bIgnoreBlanksAndTabsForLineHeightCalculation && pPos->IsFlyCntPortion())
                                 // Compat flag set: take the line height, if it's larger.
-                                Height(std::max(nPosHeight, nLineHeight));
+                                Height(std::max(nPosHeight, nLineHeight), false);
                             else
                                 // Just care about the portion height.
-                                Height(nPosHeight);
+                                Height(nPosHeight, pPos->IsTextPortion());
                         }
                         SwFlyCntPortion* pAsFly(nullptr);
                         if(pPos->IsFlyCntPortion())
@@ -549,12 +556,12 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
             {
                 nAscent = nFlyAscent;
                 if( nFlyDescent > nFlyHeight - nFlyAscent )
-                    Height( nFlyHeight + nFlyDescent );
+                    Height( nFlyHeight + nFlyDescent, false );
                 else
-                    Height( nFlyHeight );
+                    Height( nFlyHeight, false );
             }
             else if( nMaxDescent > Height() - nAscent )
-                Height( nMaxDescent + nAscent );
+                Height( nMaxDescent + nAscent, false );
 
             if( bOnlyPostIts && !( bHasBlankPortion && bHasOnlyBlankPortions ) )
             {
@@ -582,7 +589,7 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
         sal_uInt16 nTmpHeight = Height();
         rLine.GetAttrHandler().GetDefaultAscentAndHeight( rInf.GetVsh(), *rInf.GetOut(), nTmpAscent, nTmpHeight );
         SetAscent( nTmpAscent );
-        Height( nTmpHeight );
+        Height( nTmpHeight, false );
     }
 
     // Robust:
@@ -660,7 +667,9 @@ void SwLineLayout::ResetFlags()
 }
 
 SwLineLayout::SwLineLayout()
-    : m_pNext( nullptr ), m_nRealHeight( 0 ),
+    : m_pNext( nullptr ),
+      m_nRealHeight( 0 ),
+      m_nTextHeight( 0 ),
       m_bUnderscore( false )
 {
     ResetFlags();
@@ -2408,7 +2417,7 @@ const SwDropPortion *SwParaPortion::FindDropPortion() const
 
 void SwLineLayout::Init( SwLinePortion* pNextPortion )
 {
-    Height( 0 );
+    Height( 0, false );
     Width( 0 );
     SetLen(TextFrameIndex(0));
     SetAscent( 0 );

sw/qa/extras/layout/layout.cxx
@@ -2076,6 +2076,22 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testRedlineSections)
     CheckRedlineSectionsHidden();
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TDF69647_images)
+{
+    createDoc("tdf69647_images.odt");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Number of pages does not match!", 2, getPages());
+}
+
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter, TDF69647_text)
+{
+    createDoc("tdf69647_text.docx");
+    SwXTextDocument* pTextDoc = dynamic_cast<SwXTextDocument*>(mxComponent.get());
+    CPPUNIT_ASSERT(pTextDoc);
+    CPPUNIT_ASSERT_EQUAL_MESSAGE("Number of pages does not match!", 2, getPages());
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testRedlineTables)
 {
     createDoc("redline_table.fodt");
@@ -3756,7 +3772,8 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf123651)
     xmlDocUniquePtr pXmlDoc = parseLayoutDump();
     // Without the accompanying fix in place, this test would have failed with 'Expected: 7639;
     // Actual: 12926'. The shape was below the second "Lorem ipsum" text, not above it.
-    assertXPath(pXmlDoc, "//SwAnchoredDrawObject/bounds", "top", "7639");
+    const sal_Int32 nTopValue = getXPath(pXmlDoc, "//SwAnchoredDrawObject/bounds", "top").toInt32();
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(7639, nTopValue, 10);
 }
 
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter, testTdf116501)

sw/source/core/text/porlay.hxx
@@ -82,6 +82,7 @@ private:
     std::unique_ptr<std::vector<long>> m_pLLSpaceAdd;     // Used for justified alignment
     std::unique_ptr<std::deque<sal_uInt16>> m_pKanaComp;  // Used for Kana compression
     sal_uInt16 m_nRealHeight;             // The height resulting from line spacing and register
+    sal_uInt16 m_nTextHeight;             // The max height of all non-FlyCnt portions in this Line
     bool m_bFormatAdj : 1;
     bool m_bDummy     : 1;
     bool m_bEndHyph   : 1;
@@ -100,6 +101,10 @@ private:
 
     void DeleteNext();
 public:
+    // From SwPosSize
+    using SwPosSize::Height;
+    virtual void Height(const sal_uInt16 nNew, const bool bText = true) override;
+
     // From SwLinePortion
     virtual SwLinePortion *Insert( SwLinePortion *pPortion ) override;
     virtual SwLinePortion *Append( SwLinePortion *pPortion ) override;
@@ -151,6 +156,8 @@ public:
 
     void SetRealHeight( sal_uInt16 nNew ) { m_nRealHeight = nNew; }
     sal_uInt16 GetRealHeight() const { return m_nRealHeight; }
+
+    sal_uInt16 GetTextHeight() const { return m_nTextHeight; }
 
     // Creates the glue chain for short lines
     SwMarginPortion *CalcLeftMargin();

sw/source/core/text/possiz.hxx
@@ -39,10 +39,12 @@ public:
     {
     }
 #if defined(__COVERITY__)
-    ~SwPosSize() COVERITY_NOEXCEPT_FALSE {}
+    virtual ~SwPosSize() COVERITY_NOEXCEPT_FALSE {}
+#else
+    virtual ~SwPosSize() {}
 #endif
     sal_uInt16 Height() const { return nHeight; }
-    void Height( const sal_uInt16 nNew ) { nHeight = nNew; }
+    virtual void Height(const sal_uInt16 nNew, const bool = true) { nHeight = nNew; }
     sal_uInt16 Width() const { return nWidth; }
     void Width( const sal_uInt16 nNew ) { nWidth = nNew; }
     Size SvLSize() const { return Size( nWidth, nHeight ); }

commit 83303053d7c296db96e747fb053e76bc2a8ccf7d

tdf#172169 sw ms import: line portion IsUsedToCalcLineSpacingHeight
Most of this patch is just No-Functional-Change cleanup/renaming.
The only change is the new function IsUsedToCalcLineSpacingHeight.

This fixes (only for non-ODF formats and new documents)
a LO 7.1 regression from
commit d336e6c26012255015d3fc0caf8e7fafe14bd8f2
which caused intra-paragraph line-spacing calculations
to ignore the size of most non-plain-text content.

Specifically broken for both ODT and Microsoft formats
were things like fields, content controls, and FieldMarkCheckboxes.

But that 'regression' also had some 'good' results.
Things that it accidentally fixed (in terms of Microsoft interop)
were Tabstops, Hidden text, and Footnote/Endnote markers.

P.S. No attempt was made to 'fix' regressed ODT files.

SAFETY FACTOR: Very safe
It fixes MS formats - and doesn't affect previous ODT files.

make CppunitTest_sw_ooxmlexport25 \
    CPPUNIT_TEST_NAME=testTdf172169_intraLinespacing

The VCL test is hit because the .fodt has no config:config-item,
so it picks up the new default.
This is a reference field with 1.15 line spacing and 96pt font.
make CppunitTest_vcl_pdfexport CPPUNIT_TEST_NAME=testTdf142806

Change-Id: I24cb1af505550b47fae8e7b61b3345f0346da52d
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205456
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/core/text/itrform2.cxx
@@ -331,6 +331,7 @@ void SwTextFormatter::InsertPortion( SwTextFormatInfo &rInf,
     }
     else
     {
+        const IDocumentSettingAccess& rIDSA = GetTextFrame()->GetDoc().getIDocumentSettingAccess();
         pLast = rInf.GetLast();
         if( pLast->GetNextPortion() )
         {
@@ -342,15 +343,14 @@ void SwTextFormatter::InsertPortion( SwTextFormatInfo &rInf,
 
         rInf.SetOtherThanFootnoteInside( rInf.IsOtherThanFootnoteInside() || !pPor->IsFootnotePortion() );
 
-        // Adjust maxima
         if( m_pCurr->Height() < pPor->Height() )
-            m_pCurr->Height( pPor->Height(), pPor->IsTextPortion() );
+            m_pCurr->Height(pPor->Height(), pPor->IsUsedToCalcLineSpacingHeight(rIDSA));
         if( m_pCurr->GetAscent() < pPor->GetAscent() )
             m_pCurr->SetAscent( pPor->GetAscent() );
         if( m_pCurr->GetHangingBaseline() < pPor->GetHangingBaseline() )
             m_pCurr->SetHangingBaseline( pPor->GetHangingBaseline() );
 
-        if (GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(DocumentSettingId::MS_WORD_COMP_MIN_LINE_HEIGHT_BY_FLY))
+        if (rIDSA.get(DocumentSettingId::MS_WORD_COMP_MIN_LINE_HEIGHT_BY_FLY))
         {
             // For DOCX with compat=14 the only shape in line defines height of the line in spite of used font
             if (pLast->IsFlyCntPortion() && pPor->IsTextPortion() && pPor->GetLen() == TextFrameIndex(0))

sw/source/core/text/porlay.cxx
@@ -255,6 +255,8 @@ static bool lcl_HasOnlyBlanks(std::u16string_view rText, TextFrameIndex nStt, Te
 // Swapped out from FormatLine()
 void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
 {
+    const IDocumentSettingAccess& rIDSA = rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess();
+
     const SwTwips nLineWidth = rInf.RealWidth();
 
     sal_uInt16 nFlyAscent = 0;
@@ -271,9 +273,8 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
     SwFlyCntPortion* pFlyCnt = nullptr;
 
     // #i3952#
-    const bool bIgnoreBlanksAndTabsForLineHeightCalculation =
-        rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
-            DocumentSettingId::IGNORE_TABS_AND_BLANKS_FOR_LINE_CALCULATION);
+    const bool bIgnoreBlanksAndTabsForLineHeightCalculation
+        = rIDSA.get(DocumentSettingId::IGNORE_TABS_AND_BLANKS_FOR_LINE_CALCULATION);
 
     bool bHasBlankPortion = false;
     bool bHasOnlyBlankPortions = true; // paragraph line contains only tabstops and spaces
@@ -378,8 +379,7 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
                 if ((pPos->IsDropPortion() && static_cast<SwDropPortion*>(pPos)->GetLines() > 1)
                     || pPos->GetWhichPor() == PortionType::Bookmark
                     || (pPos->GetWhichPor() == PortionType::HiddenText
-                        && rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
-                            DocumentSettingId::IGNORE_HIDDEN_CHARS_FOR_LINE_CALCULATION)))
+                        && rIDSA.get(DocumentSettingId::IGNORE_HIDDEN_CHARS_FOR_LINE_CALCULATION)))
                 {
                     pLast = pPos;
                     pPos = pPos->GetNextPortion();
@@ -463,7 +463,7 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
                                 Height(std::max(nPosHeight, nLineHeight), false);
                             else
                                 // Just care about the portion height.
-                                Height(nPosHeight, pPos->IsTextPortion());
+                                Height(nPosHeight, pPos->IsUsedToCalcLineSpacingHeight(rIDSA));
                         }
                         SwFlyCntPortion* pAsFly(nullptr);
                         if(pPos->IsFlyCntPortion())
@@ -575,7 +575,7 @@ void SwLineLayout::CalcLine( SwTextFormatter &rLine, SwTextFormatInfo &rInf )
     if (!rInf.IsNewLine()
         && TextFrameIndex(rInf.GetText().getLength()) <= rInf.GetIdx()
         && bHasOnlyBlankPortions
-        && rInf.GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
+        && rIDSA.get(
             DocumentSettingId::APPLY_PARAGRAPH_MARK_FORMAT_TO_EMPTY_LINE_AT_END_OF_PARAGRAPH))
     {
         // Word: for empty last line, line height is based on paragraph marker

sw/source/core/text/porlin.cxx
@@ -321,6 +321,39 @@ void SwLinePortion::HandlePortion( SwPortionHandler& rPH ) const
     rPH.Special( GetLen(), OUString(), GetWhichPor() );
 }
 
+bool SwLinePortion::IsUsedToCalcLineSpacingHeight(const IDocumentSettingAccess& rIDSA) const
+{
+    // For inside-the-paragraph line-spacing, LO 7.1 accidentally changed the implementation
+    // from 'everything informs line-spacing' to 'usually only plain text informs line-spacing'.
+    // That was 5 years ago, so at this point legacy LO has no 'correct' implementation.
+
+    if (IsTextPortion())
+    {
+        // NOTE: MSO exception for HasOnlyBlankPortions is handled later in SwLineLayout::CalcLine
+        return true;
+    }
+
+    // Use new (2027) implementation to identify an interoperable set of line-space-defining content
+    if (!rIDSA.get(DocumentSettingId::LINE_SPACING_AS_GAP_BELOW))
+        return false; // LO legacy format
+
+    // exclude anything else that isn't considered to be text
+    if (!InTextGrp()) // excludes flys, tab-stops, bookmarks, Hole, etc
+        return false;
+
+    // exclude the following text-like portions
+    if (InNumberGrp() // bullets and numbering, FootnoteNum, GrfNum
+        || IsFootnotePortion() // numbering of the actual footnote
+        || IsPostItsPortion() // comment marker
+        || GetWhichPor() == PortionType::Hidden // hidden field
+        || GetWhichPor() == PortionType::FieldMark) // the fieldmark commands
+    {
+        return false;
+    }
+
+    return true;
+}
+
 void SwLinePortion::dumpAsXml(xmlTextWriterPtr pWriter, const OUString& rText, TextFrameIndex& nOffset) const
 {
     (void)xmlTextWriterStartElement(pWriter, BAD_CAST("SwLinePortion"));

sw/source/core/text/porlin.hxx
@@ -20,6 +20,7 @@
 
 #include <libxml/xmlwriter.h>
 
+#include <IDocumentSettingAccess.hxx>
 #include "possiz.hxx"
 #include <txttypes.hxx>
 #include <TextFrameIndex.hxx>
@@ -183,6 +184,8 @@ public:
     void SetJoinBorderWithPrev( const bool bJoinPrev ) { m_bJoinBorderWithPrev = bJoinPrev; }
     void SetJoinBorderWithNext( const bool bJoinNext ) { m_bJoinBorderWithNext = bJoinNext; }
     void SetFieldmarkText(bool bSet) { m_bIsFieldmarkText = bSet; }
+
+    bool IsUsedToCalcLineSpacingHeight(const IDocumentSettingAccess& rIDSA) const;
 
     virtual void dumpAsXml(xmlTextWriterPtr pWriter, const OUString& rText,
                            TextFrameIndex& rOffset) const;

sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
@@ -79,6 +79,90 @@ DECLARE_OOXMLEXPORT_TEST(testTdf172169_linespacingFootnote, "tdf172169_linespaci
     CPPUNIT_ASSERT_EQUAL(1, getPages());
 }
 
+CPPUNIT_TEST_FIXTURE(Test, testTdf172169_intraLinespacing)
+{
+    // given a six page, double-spaced document
+    // with various different 60pt SwLinePortions affecting intra-paragraph line spacing
+    createSwDoc("tdf172169_intraLinespacing.odt");
+
+    // NOTE that before LO 7.1 this was 8 pages, but it changed without a compatibility flag...
+    CPPUNIT_ASSERT_EQUAL(6, getPages());
+
+    // Test that the 'broken' layout remains broken in older ODT files
+    auto pXmlDoc = parseLayoutDump();
+
+    OUString sText = getXPathContent(pXmlDoc, "//page[2]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("The change is not limited to footnotes.")); // testing field
+    SwTwips nTextHeight = getXPath(pXmlDoc, "//page[2]/body/txt/infos/bounds", "height").toInt32();
+    // The text only fills half the page.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2484, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[3]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("How about hidden stuff."));
+    nTextHeight = getXPath(pXmlDoc, "//page[3]/body/txt/infos/bounds", "height").toInt32();
+    // The text only fills about a third of the page
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(1380, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[4]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("How about FieldMark stuff."));
+    nTextHeight = getXPath(pXmlDoc, "//page[4]/body/txt/infos/bounds", "height").toInt32();
+    // The text only fills half the page.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2484, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[5]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("Also explicitly test for content controls,"));
+    nTextHeight = getXPath(pXmlDoc, "//page[5]/body/txt/infos/bounds", "height").toInt32();
+    // The text only fills half the page.
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2484, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[6]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("Let us also explicitly test for tabstops."));
+    nTextHeight = getXPath(pXmlDoc, "//page[6]/body/txt/infos/bounds", "height").toInt32();
+    // The text basically fills the entire page
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(3588, nTextHeight, 100);
+
+    saveAndReload(TestFilter::DOCX);
+
+    CPPUNIT_ASSERT_EQUAL(6, getPages());
+
+    saveAndReload(TestFilter::ODT);
+
+    CPPUNIT_ASSERT_EQUAL(6, getPages());
+
+    // This formatting should be the same for DOCX and ODT - only testing ODT
+    pXmlDoc = parseLayoutDump();
+
+    sText = getXPathContent(pXmlDoc, "//page[2]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("The change is not limited to footnotes."));
+    nTextHeight = getXPath(pXmlDoc, "//page[2]/body/txt/infos/bounds", "height").toInt32();
+    // The text basically fills the entire page. (was 2484)
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(3588, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[3]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("How about hidden stuff."));
+    nTextHeight = getXPath(pXmlDoc, "//page[3]/body/txt/infos/bounds", "height").toInt32();
+    // The text only fills about a third of the page
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(1380, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[4]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("How about FieldMark stuff."));
+    nTextHeight = getXPath(pXmlDoc, "//page[4]/body/txt/infos/bounds", "height").toInt32();
+    // The text basically fills the entire page. (was 2484)
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(3588, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[5]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("Also explicitly test for content controls,"));
+    nTextHeight = getXPath(pXmlDoc, "//page[5]/body/txt/infos/bounds", "height").toInt32();
+    // The text basically fills the entire page. (was 2484)
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(3588, nTextHeight, 100);
+
+    sText = getXPathContent(pXmlDoc, "//page[6]/body/txt");
+    CPPUNIT_ASSERT(sText.startsWith("Let us also explicitly test for tabstops."));
+    nTextHeight = getXPath(pXmlDoc, "//page[6]/body/txt/infos/bounds", "height").toInt32();
+    // The text only fills about two thirds of the page
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(2484, nTextHeight, 100);
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf148057_columnBreak, "tdf148057_columnBreak.docx")
 {
     // given a document with a linefeed immediately following a column break (in non-column section)

vcl/qa/cppunit/pdfexport/pdfexport.cxx
@@ -3740,7 +3740,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf142806)
                 CPPUNIT_ASSERT_DOUBLES_EQUAL(56.643, pNumL->GetValue(), 1e-3);
                 const auto* pNumT = dynamic_cast<vcl::filter::PDFNumberElement*>(rElements[1]);
                 CPPUNIT_ASSERT(pNumT);
-                CPPUNIT_ASSERT_DOUBLES_EQUAL(140.005, pNumT->GetValue(), 1e-3);
+                CPPUNIT_ASSERT_DOUBLES_EQUAL(125.505, pNumT->GetValue(), 1e-3);
                 const auto* pNumR = dynamic_cast<vcl::filter::PDFNumberElement*>(rElements[2]);
                 CPPUNIT_ASSERT(pNumR);
                 CPPUNIT_ASSERT_DOUBLES_EQUAL(241.007, pNumR->GetValue(), 1e-3);
@@ -3872,7 +3872,7 @@ CPPUNIT_TEST_FIXTURE(PdfExportTest, testTdf142806)
                 CPPUNIT_ASSERT_DOUBLES_EQUAL(56.643, pNumL->GetValue(), 1e-3);
                 const auto* pNumT = dynamic_cast<vcl::filter::PDFNumberElement*>(rElements[1]);
                 CPPUNIT_ASSERT(pNumT);
-                CPPUNIT_ASSERT_DOUBLES_EQUAL(140.005, pNumT->GetValue(), 1e-3);
+                CPPUNIT_ASSERT_DOUBLES_EQUAL(125.505, pNumT->GetValue(), 1e-3);
                 const auto* pNumR = dynamic_cast<vcl::filter::PDFNumberElement*>(rElements[2]);
                 CPPUNIT_ASSERT(pNumR);
                 CPPUNIT_ASSERT_DOUBLES_EQUAL(184.707, pNumR->GetValue(), 1e-3);