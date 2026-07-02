305c67b4a3842a127c875fc02fee766ed69e8814
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-07-02 10:18:39 +0200

    Fix NSS build on iOS Intel
    In `external/nss/ExternalProject_nss.mk`, include `iOS` in the OS filter
    that passes `USE_64=1 CPU_ARCH=x86_64` to NSS when `CPUNAME=X86_64`.
    Without `USE_64`, NSS picks the 32-bit code path in `lib/freebl/drbg.c`
    and `PR_STATIC_ASSERT(sizeof(size_t) <= 4)` fails the compile on a
    64-bit iOS simulator host. The `AARCH64` branch on the next line is
    already OS-agnostic so iOS arm64 isn't affected.
    
    Change-Id: Ifeb782e9c164b9171c101dc3a92024a7507181c9
    Signed-off-by: Kasper Isager Dalsgarð <kasperisager@hey.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/5978
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6162

--- engine/external/nss/ExternalProject_nss.mk
+++ engine/external/nss/ExternalProject_nss.mk
@@ -55,7 +55,7 @@ $(call gb_ExternalProject_get_state_target,nss,build): \
 		$(SRCDIR)/external/nss/nsinstall.py
 	$(call gb_Trace_StartRange,nss,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
-		$(if $(filter ANDROID FREEBSD LINUX MACOSX,$(OS)),$(if $(filter X86_64,$(CPUNAME)),USE_64=1 CPU_ARCH=x86_64)) \
+		$(if $(filter ANDROID FREEBSD LINUX MACOSX iOS,$(OS)),$(if $(filter X86_64,$(CPUNAME)),USE_64=1 CPU_ARCH=x86_64)) \
 		$(if $(filter AARCH64,$(CPUNAME)),USE_64=1 CPU_ARCH=aarch64) \
 		$(if $(filter POWERPC64,$(CPUNAME)),USE_64=1 CPU_ARCH=ppc64le) \
 		$(if $(filter MACOSX,$(OS)),\
