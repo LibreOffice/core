LibreOffice/core

commit a974dc48d6eb7236bb1b4ec2d168984e544b8a0b

cool#7739 Fix validation error dialog width
Cap the messagebox dialog width at 380 pixels so for short validation
messages the dialog is not unnecessary wide.

Also disable the response button after the first click to prevent a double-send
during the network round-trip to the kit.

Signed-off-by: Samuel Mehrbrodt <samuel.mehrbrodt@collabora.com>
Change-Id: I1d140ee62cde7dae4b175d5ac1c8cfece3e070be
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/5825
Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

browser/src/control/Control.JSDialog.js
@@ -421,6 +421,9 @@ window.L.Control.JSDialog = window.L.Control.extend({
 		if (instance.isModalPopUp || instance.isDocumentAreaPopup || instance.isSnackbar)
 			window.L.DomUtil.addClass(instance.container, 'modalpopup');
 
+		if (instance.isMessageBox)
+			window.L.DomUtil.addClass(instance.container, 'messagebox');
+
 		if (instance.isAutofilter && !this.isChildAutoFilter(instance))
 			window.L.DomUtil.addClass(instance.container, 'autofilter-popup');
 
@@ -1027,6 +1030,7 @@ window.L.Control.JSDialog = window.L.Control.extend({
 		instance.isSnackbar = e.data.type === 'snackbar';
 		instance.isDropdown = e.data.type === 'dropdown';
 		instance.isModalPopUp = e.data.type === 'modalpopup' || instance.isDropdown;
+		instance.isMessageBox = e.data.type === 'messagebox';
 		instance.snackbarTimeout = e.data.timeout;
 		instance.isOnlyChild = false;
 		instance.that = this;

browser/src/control/jsdialog/Widget.PushButton.ts
@@ -119,7 +119,7 @@ JSDialog.pushButton = function (
 	if (customCallback) {
 		pushbutton.onclick = customCallback;
 	} else if (builder._responses[data.id] !== undefined) {
-		pushbutton.onclick = builder.callback.bind(
+		const responseCallback = builder.callback.bind(
 			builder,
 			'responsebutton',
 			'click',
@@ -127,6 +127,11 @@ JSDialog.pushButton = function (
 			builder._responses[data.id],
 			builder,
 		);
+		pushbutton.onclick = function (ev: MouseEvent) {
+			pushbutton.setAttribute('disabled', 'true');
+			pushbutton.setAttribute('aria-disabled', 'true');
+			responseCallback(ev);
+		};
 	} else {
 		pushbutton.onclick = builder.callback.bind(
 			builder,

browser/css/jsdialogs.css
@@ -41,6 +41,11 @@
 	min-width: 400px;
 }
 
+.jsdialog-window.messagebox:not(.modalpopup) .jsdialog-container:not(.snackbar) {
+	min-width: 300px;
+	max-width: 380px;
+}
+
 @media (max-width: 480px) {
 
 	.jsdialog-window:not(.modalpopup) .jsdialog-container:not(.snackbar) {

commit 2a61b803ab3559411e8e4909e9f15e1e9623cacb

jsdialog: only self-disable response buttons in message boxes
The guard that disables a response button on click, so a slow round-trip
to the kit cannot be sent twice, was applied to every response button
instead of only the message box buttons it was written for. An in-dialog
action button that keeps its own dialog open, such as Add on the area
fill tabs, gets no follow-up render to clear that disabled state, so it
stays stuck disabled after the first click. That left the hatch Add step
in calc1/a11y_dialog_spec flaky and stopped a user from adding a second
entry.

Restrict the self-disable to message boxes, which close as soon as a
response button is pressed.

Regression from commit a974dc48d6eb ("cool#7739 Fix validation error
dialog width").

Signed-off-by: Mohit Marathe <mohit.marathe@collabora.com>
Change-Id: Ib08f48cb4d83c18203b3a6bbcea08a6bd36e9e00
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6329
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

browser/src/control/Control.JSDialog.js
@@ -449,6 +449,7 @@ window.L.Control.JSDialog = window.L.Control.extend({
 				cssClass: 'jsdialog' + (instance.isAutoPopup ? ' autofilter' : '') + (instance.isOnlyChild ? ' one-child-popup' : ''),
 				callback: instance.callback,
 				suffix: 'dialog',
+				isMessageBox: instance.isMessageBox,
 			});
 
 		instance.builder.build(instance.content, [instance]);
@@ -1198,6 +1199,7 @@ window.L.Control.JSDialog = window.L.Control.extend({
 			cssClass: 'jsdialog',
 			callback: e.callback,
 			suffix: 'dialog',
+			isMessageBox: this.dialogs[data.id].builder && this.dialogs[data.id].builder.options.isMessageBox,
 		});
 
 		builder.updateWidget(dialog, data.control);

browser/src/control/jsdialog/Definitions.Types.ts
@@ -46,6 +46,7 @@ interface JSBuilderOptions {
 	mobileWizard: JSDialogComponent; // reference to the parent component FIXME: rename
 	useSetTabs?: boolean; // custom tabs placement handled by the parent container
 	useScrollAnimation?: boolean; // do we use animation for scrollIntoView
+	isMessageBox?: boolean; // when the dialog is a message box
 
 	// modifiers
 	noLabelsForUnoButtons?: boolean; // create only icon without label

browser/src/control/jsdialog/Widget.PushButton.ts
@@ -127,11 +127,15 @@ JSDialog.pushButton = function (
 			builder._responses[data.id],
 			builder,
 		);
-		pushbutton.onclick = function (ev: MouseEvent) {
-			pushbutton.setAttribute('disabled', 'true');
-			pushbutton.setAttribute('aria-disabled', 'true');
-			responseCallback(ev);
-		};
+		if (builder.options.isMessageBox) {
+			pushbutton.onclick = function (ev: MouseEvent) {
+				pushbutton.setAttribute('disabled', 'true');
+				pushbutton.setAttribute('aria-disabled', 'true');
+				responseCallback(ev);
+			};
+		} else {
+			pushbutton.onclick = responseCallback;
+		}
 	} else {
 		pushbutton.onclick = builder.callback.bind(
 			builder,