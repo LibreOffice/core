de87637a86d4d97f4346ce125cf766343c6da152
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-07-08 21:39:11 +0100

    upgrade to libstaroffice-0.0.8.tar.xz
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: If15e225516d3435bb3274e1772345759a1d2430d
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6691
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- engine/external/libstaroffice/UnpackedTarball_libstaroffice.mk
+++ engine/external/libstaroffice/UnpackedTarball_libstaroffice.mk
@@ -15,14 +15,6 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,libstaroffice,0))
 
 $(eval $(call gb_UnpackedTarball_update_autoconf_configs,libstaroffice))
 
-# 0001-const-up-these-arrays.patch.1
-# upstream attempt as:
-# https://github.com/fosnola/libstaroffice/pull/12
-
-$(eval $(call gb_UnpackedTarball_add_patches,libstaroffice, \
-    external/libstaroffice/0001-const-up-these-arrays.patch.1 \
-))
-
 ifneq ($(OS),MACOSX)
 ifneq ($(OS),WNT)
 $(eval $(call gb_UnpackedTarball_add_patches,libstaroffice, \
