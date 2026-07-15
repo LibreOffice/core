9de2a4cc1787417a0924b6c069ed33f6bc7625a0
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-07-15 12:36:20 +0100

    bgsave: do not fail on navigator close or popup dismiss
    The two cases seen recently in testing:
    
    WRN  Disabled background save Unexpected jsdialog message: client-915 jsdialog: { "id": 0, "jsontype": "navigator", "action": "close"}|kit/Kit.cpp:2842
    WRN  Disabled background save Unexpected jsdialog message: client-675 jsdialog: { "jsontype": "popup", "action": "action", "id": 134, "data": { "control_id": "menu", "position": "-1", "action_type": "select"}}|kit/Kit.cpp:2842
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I598b47a12eabfc5ce8a76f8b917ade0774292139
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6933
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- kit/KitWebSocket.cpp
+++ kit/KitWebSocket.cpp
@@ -304,13 +304,26 @@ bool BgSaveParentWebSocketHandler::isBenignBgSaveJSDialog(const std::string& jsd
     const std::string jsontype = object->get("jsontype").toString();
 
     std::string action;
-    const bool bClosingDialog =
-        jsontype == "dialog" &&
+    const bool bClosing =
+        (jsontype == "dialog" || jsontype == "navigator") &&
         JsonUtil::findJSONValue(object, "action", action) && action == "close";
+
+    bool bDismissedMenu = false;
+    if (jsontype == "popup")
+    {
+        Poco::JSON::Object::Ptr dataObject = object->getObject("data");
+        std::string actionType, position;
+        bDismissedMenu = dataObject &&
+            JsonUtil::findJSONValue(dataObject, "action_type", actionType) &&
+            actionType == "select" &&
+            JsonUtil::findJSONValue(dataObject, "position", position) &&
+            position == "-1";
+    }
 
     // allow-list of jsdialog messages in bgsave
     return jsontype == "notebookbar" || jsontype == "sidebar" ||
-           jsontype == "formulabar" || jsontype == "quickfind" || bClosingDialog;
+           jsontype == "formulabar" || jsontype == "quickfind" ||
+           bClosing || bDismissedMenu;
 }
 
 void BgSaveParentWebSocketHandler::handleMessage(const std::vector<char>& data)
