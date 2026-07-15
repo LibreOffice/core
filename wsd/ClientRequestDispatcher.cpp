4fce6b56f157219cca8872cc63e807a8788db4d4
Andras Timar <andras.timar@collabora.com>
2026-07-15 12:56:23 +0200

    wsd: send Cache-Control: no-cache for dynamic HTTP responses
    The /cool/getMetrics, /hosting/capabilities and /hosting/discovery
    endpoints return content that is dynamic (live metrics) or derived from
    the request host and configuration. Without a Cache-Control header a
    caching proxy in front of coolwsd may cache these responses and serve
    stale or host-mismatched results. Set Cache-Control: no-cache on all
    three.
    
    Signed-off-by: Andras Timar <andras.timar@collabora.com>
    Change-Id: Ia65de2c1daf6193eaf07761f06dc6ba3c45213e8
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/6931
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- wsd/ClientRequestDispatcher.cpp
+++ wsd/ClientRequestDispatcher.cpp
@@ -1331,6 +1331,8 @@ ClientRequestDispatcher::MessageResult ClientRequestDispatcher::handleMessage(Po
 
             FileServerRequestHandler::hstsHeaders(*response);
             response->add("Last-Modified", Util::getHttpTimeNow());
+            // Prevent caching proxies from serving stale metrics
+            response->add("Cache-Control", "no-cache");
             // Ask UAs to block if they detect any XSS attempt
             response->add("X-XSS-Protection", "1; mode=block");
             // No referrer-policy
@@ -1805,6 +1807,8 @@ bool ClientRequestDispatcher::handleWopiDiscoveryRequest(
     httpResponse.setBody(xml, "text/xml");
     httpResponse.set("Last-Modified", Util::getHttpTimeNow());
     httpResponse.set("X-Content-Type-Options", "nosniff");
+    // Discovery XML depends on the request host and config; prevent caching proxies from serving it to other hosts
+    httpResponse.set("Cache-Control", "no-cache");
     if( requestDetails.closeConnection() )
         httpResponse.setConnectionToken(http::Header::ConnectionToken::Close);
     LOG_TRC("Sending back discovery.xml: " << xml);
@@ -3120,6 +3124,8 @@ void sendCapabilities(bool convertToAvailable, bool closeConnection,
     httpResponse.set("Last-Modified", Util::getHttpTimeNow());
     httpResponse.setBody(getCapabilitiesJson(convertToAvailable), "application/json");
     httpResponse.set("X-Content-Type-Options", "nosniff");
+    // Prevent caching proxies from serving stale capabilities
+    httpResponse.set("Cache-Control", "no-cache");
     if( closeConnection )
         socket->sendAndShutdown(httpResponse);
     else
