9f72a904e41682044f7edfafe56689c37ca24af7
Justin Luth <jluth@mail.com>
2026-07-16 20:28:14 -0400

    tdf#160781 sc SearchAndReplace: also loop by Column backwards
    If you search by Row or Column forwards,
    you will loop back to the beginning.
    If you search by Row backwards,
    you will loop back to the end.
    
    This patch adds looping when searching by Column backwards,
    and it also works when 'All Sheets' is checked.
    
    Change-Id: I23252671b47fe05953cfd386a46b1c065d512127
    Signed-off-by: Miklos Vajna <vmiklos@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/13015
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- engine/sc/source/core/data/table6.cxx
+++ engine/sc/source/core/data/table6.cxx
@@ -628,7 +628,7 @@ bool ScTable::SearchStyle(const SvxSearchItem& rSearchItem, SCCOL& rCol, SCROW& 
                                         rDocument.GetStyleSheetPool()->Find(
                                         rSearchItem.GetSearchString(), SfxStyleFamily::Para ));
 
-    SCCOL nCol = rCol;
+    SCCOL nCol = ClampToAllocatedColumns(rCol);
     SCROW nRow = rRow;
     bool bFound = false;
 
