77cb99399b54664b6524a3461af2233c7fd40cbb
Michael Stahl <michael.stahl@collabora.com>
2026-07-17 16:34:17 +0200

    libxml2: add patch for CVE-2026-6732
    (cherry picked from commit d62fb31da52d9e22452e8151dce2681e1b64a06b)
    changed:
        external/libxml2/0001-parser-Pass-userData-to-SAX-text-callbacks-in-xmlPar.patch.1
    
    Change-Id: I17750740edd5edbbe1f98ba63e44cecafd67fbb2
    Reviewed-on: https://gerrit.collaboraoffice.com/c/core/+/7197
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- external/libxml2/UnpackedTarball_libxml2.mk
+++ external/libxml2/UnpackedTarball_libxml2.mk
@@ -24,6 +24,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,libxml2,\
 		external/libxml2/libxml2-icu-sym.patch.0, \
 		external/libxml2/libxml2-icu.patch.0) \
 	external/libxml2/9de92ed78d8495527c5d7a4d0cc76c1f83768195.patch.1 \
+	external/libxml2/0001-parser-Pass-userData-to-SAX-text-callbacks-in-xmlPar.patch.1 \
 ))
 
 $(eval $(call gb_UnpackedTarball_add_file,libxml2,xml2-config.in,external/libxml2/xml2-config.in))
