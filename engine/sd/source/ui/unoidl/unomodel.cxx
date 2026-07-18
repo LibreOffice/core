7b41c8b3f607ff8e6bd3a0d6ec23ff481921b9aa
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-07-18 14:06:51 +0100

    crashtesting: ClickAction_VANISH missing from translations
    seen on converting ooo103896-3.odp to pdf
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I5fb08b84fd0b84f704421e0af9a47a8ef34af957
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7232
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>

--- engine/sd/source/ui/unoidl/unomodel.cxx
+++ engine/sd/source/ui/unoidl/unomodel.cxx
@@ -202,6 +202,8 @@ using namespace ::com::sun::star;
 using namespace ::sd;
 using namespace ::cpo::uno;
 
+namespace
+{
 const TranslateId aTypeResIds[SdLinkTargetType::Count] =
 {
     STR_SD_PAGE,            // SdLinkTargetType::Page
@@ -210,25 +212,34 @@ const TranslateId aTypeResIds[SdLinkTargetType::Count] =
     STR_MASTERPAGE_NAME,    // SdLinkTargetType::MasterPage
 };
 
+const TranslateId aClickActionResIds[] =
+{
+    STR_CLICK_ACTION_NONE,             // ClickAction_NONE
+    STR_CLICK_ACTION_PREVPAGE,         // ClickAction_PREVPAGE
+    STR_CLICK_ACTION_NEXTPAGE,         // ClickAction_NEXTPAGE
+    STR_CLICK_ACTION_FIRSTPAGE,        // ClickAction_FIRSTPAGE
+    STR_CLICK_ACTION_LASTPAGE,         // ClickAction_LASTPAGE
+    STR_CLICK_ACTION_BOOKMARK,         // ClickAction_BOOKMARK
+    STR_CLICK_ACTION_DOCUMENT,         // ClickAction_DOCUMENT
+    STR_CLICK_ACTION_INVISIBLE,        // ClickAction_INVISIBLE
+    STR_CLICK_ACTION_SOUND,            // ClickAction_SOUND
+    STR_CLICK_ACTION_VERB,             // ClickAction_VERB
+    STR_CLICK_ACTION_VANISH,           // ClickAction_VANISH
+    STR_CLICK_ACTION_PROGRAM,          // ClickAction_PROGRAM
+    STR_CLICK_ACTION_MACRO,            // ClickAction_MACRO
+    STR_CLICK_ACTION_STOPPRESENTATION, // ClickAction_STOPPRESENTATION
+};
+
+static_assert(SAL_N_ELEMENTS(aClickActionResIds)
+              == static_cast<size_t>(presentation::ClickAction_STOPPRESENTATION) + 1,
+              "aClickActionResIds is missing an entry for a presentation::ClickAction value");
+}
+
 TranslateId SdTPAction::GetClickActionSdResId( presentation::ClickAction eCA )
 {
-    switch( eCA )
-    {
-        case presentation::ClickAction_NONE:             return STR_CLICK_ACTION_NONE;
-        case presentation::ClickAction_PREVPAGE:         return STR_CLICK_ACTION_PREVPAGE;
-        case presentation::ClickAction_NEXTPAGE:         return STR_CLICK_ACTION_NEXTPAGE;
-        case presentation::ClickAction_FIRSTPAGE:        return STR_CLICK_ACTION_FIRSTPAGE;
-        case presentation::ClickAction_LASTPAGE:         return STR_CLICK_ACTION_LASTPAGE;
-        case presentation::ClickAction_BOOKMARK:         return STR_CLICK_ACTION_BOOKMARK;
-        case presentation::ClickAction_DOCUMENT:         return STR_CLICK_ACTION_DOCUMENT;
-        case presentation::ClickAction_PROGRAM:          return STR_CLICK_ACTION_PROGRAM;
-        case presentation::ClickAction_MACRO:            return STR_CLICK_ACTION_MACRO;
-        case presentation::ClickAction_SOUND:            return STR_CLICK_ACTION_SOUND;
-        case presentation::ClickAction_VERB:             return STR_CLICK_ACTION_VERB;
-        case presentation::ClickAction_STOPPRESENTATION: return STR_CLICK_ACTION_STOPPRESENTATION;
-        default: OSL_FAIL( "No StringResource for ClickAction available!" );
-    }
-    return {};
+    auto nIndex = static_cast<size_t>(eCA);
+    assert(nIndex < SAL_N_ELEMENTS(aClickActionResIds) && "ClickAction out of range");
+    return aClickActionResIds[nIndex];
 }
 
 class SdUnoForbiddenCharsTable : public SvxUnoForbiddenCharsTable,
