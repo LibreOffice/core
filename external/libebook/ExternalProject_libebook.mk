5fb7836704a4e7dc86a41057dc51105643722c74
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-07-19 20:32:42 +0000

    upgrade to libe-book-0.1.4.tar.xz
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I3d79eae84d6ba44e4378a511b8c0cc28ff0ae116
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208289
    Reviewed-by: Hossein <hossein@libreoffice.org>
    Tested-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Ilmari Lauhakangas <ilmari.lauhakangas@libreoffice.org>

--- external/libebook/ExternalProject_libebook.mk
+++ external/libebook/ExternalProject_libebook.mk
@@ -27,7 +27,7 @@ $(eval $(call gb_ExternalProject_use_externals,libebook,\
 $(call gb_ExternalProject_get_state_target,libebook,build) :
 	$(call gb_Trace_StartRange,libebook,EXTERNAL)
 	$(call gb_ExternalProject_run,build,\
-		export PKG_CONFIG="" \
+		export PKG_CONFIG=true \
 		&& unset MSYS_NO_PATHCONV && MAKE=$(MAKE) $(gb_RUN_CONFIGURE) ./configure \
 			--with-pic \
 			--enable-static \
