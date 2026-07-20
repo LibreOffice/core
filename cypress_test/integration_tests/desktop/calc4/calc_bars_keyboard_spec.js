dcf176539aaa971c3ea505925ad6ef594e523db2
Henry Castro <henry.castro@collabora.com>
2026-07-20 17:23:31 +0000

    cypress: test Calc formula-bar row keyboard access (F6 + Tab)
    Add a desktop spec that F6-focuses the formula bar row (#formulabar-row)
    and Tabs through every visible focusable, asserting focus lands on each in
    turn (the edit field redirects DOM focus to a hidden input and marks its
    wrapper 'focused').
    
    Change-Id: Id4c4d86f170a12a3d5ad458cb543229c4c26add7
    Signed-off-by: Henry Castro <henry.castro@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7392
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

---
+++ cypress_test/integration_tests/desktop/calc4/calc_bars_keyboard_spec.js
@@ -0,0 +1,95 @@
+/* global describe it cy beforeEach require Cypress expect */
+
+var helper = require('../../common/helper');
+var calcHelper = require('../../common/calc_helper');
+
+describe(['tagdesktop'], 'Calc keyboard access for the bars', function () {
+	var FOCUSABLE = 'button:not([disabled]):not([tabindex="-1"]), ' +
+		'input:not([disabled]):not([tabindex="-1"]), [tabindex="0"]';
+
+	beforeEach(function () {
+		helper.setupAndLoadDocument('calc/focus.ods');
+	});
+
+	// Wait one Chrome repaint cycle (two requestAnimationFrame) so a focus
+	// change is applied before we sample it - no server round-trip.
+	function waitAFrame() {
+		cy.getFrameWindow().then(function (win) {
+			return new Cypress.Promise(function (resolve) {
+				win.requestAnimationFrame(function () {
+					win.requestAnimationFrame(resolve);
+				});
+			});
+		});
+	}
+
+	// Press F6 until keyboard focus lands somewhere inside the container.
+	function focusRegionWithF6(containerSelector, remainingTries) {
+		cy.realPress('F6');
+		waitAFrame();
+		cy.cGet(containerSelector).then(function (found) {
+			var container = found[0];
+			if (container.contains(container.ownerDocument.activeElement))
+				return;
+			if (remainingTries <= 1)
+				throw new Error(containerSelector + ' did not get focus after cycling F6');
+			focusRegionWithF6(containerSelector, remainingTries - 1);
+		});
+	}
+
+	// Tab onto each focusable element in turn, asserting the focus landed on
+	// it, until focus leaves the container (the input field redirects DOM
+	// focus to a hidden input and marks its wrapper 'focused').
+	function tabThroughElements(containerSelector, elements, i) {
+		if (i >= elements.length)
+			return;
+
+		// F6 already focused the first element; Tab to reach the following ones
+		if (i > 0) {
+			cy.realPress('Tab');
+			waitAFrame();
+		}
+
+		var element = elements[i];
+		var name = element.id || element.getAttribute('aria-label');
+		cy.cGet(containerSelector).should(function (found) {
+			var active = found[0].ownerDocument.activeElement;
+			expect(element.contains(active) || element.closest('.focused') !== null,
+				name).to.equal(true);
+		});
+
+		cy.cGet(containerSelector).then(function (found) {
+			if (found[0].contains(found[0].ownerDocument.activeElement))
+				tabThroughElements(containerSelector, elements, i + 1);
+		});
+	}
+
+	// Shift+Tab back until the given element has keyboard focus, in case F6
+	// did not land on the first focusable of the container.
+	function focusFirstElement(containerSelector, first, remainingTries) {
+		cy.cGet(containerSelector).then(function (found) {
+			if (found[0].ownerDocument.activeElement === first)
+				return;
+			if (remainingTries <= 1)
+				throw new Error('could not reach the first focusable with Shift+Tab');
+			cy.realPress(['Shift', 'Tab']);
+			waitAFrame();
+			focusFirstElement(containerSelector, first, remainingTries - 1);
+		});
+	}
+
+	function assertTabVisitsFocusables(containerSelector) {
+		cy.cGet(containerSelector).find(FOCUSABLE).filter(':visible').then(function (found) {
+			var elements = found.toArray();
+			expect(elements.length, containerSelector + ' focusable elements').to.be.greaterThan(1);
+			focusFirstElement(containerSelector, elements[0], elements.length);
+			tabThroughElements(containerSelector, elements, 0);
+		});
+	}
+
+	it('F6 then Tab visits every focusable item of the formula bar row', function () {
+		calcHelper.clickOnFirstCell();
+		focusRegionWithF6('#formulabar-row', 10);
+		assertTabVisitsFocusables('#formulabar-row');
+	});
+});
