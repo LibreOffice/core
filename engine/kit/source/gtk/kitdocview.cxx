19e754cae9931950e9f9442768fcecb44a0da0d8
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-07-21 11:22:54 +0200

    -Werror=switch
    ...after f62513f6ae991297971a590ce8b3d6a4c99da677 "vector: Add a transport frame
    for pushed vector primitive deltas" introduced a new
    KIT_CALLBACK_VECTOR_PRIMITIVES_DELTA
    
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: I1dc1bf362e0dc847c1877f22a08fefaf11e6b6b1
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7480
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- engine/kit/source/gtk/kitdocview.cxx
+++ engine/kit/source/gtk/kitdocview.cxx
@@ -1501,6 +1501,7 @@ callback (gpointer pData)
     case KIT_CALLBACK_SHAPE_INNER_TEXT:
     case KIT_CALLBACK_CLIPBOARD_MIMETYPES:
     case KIT_CALLBACK_SHAPE_DRAG_PREVIEW:
+    case KIT_CALLBACK_VECTOR_PRIMITIVES_DELTA:
     {
         // TODO: Implement me
         break;
