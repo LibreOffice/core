522da67c33424c2932208e26be1517bb9bb7b51c
Michael Weghorn <m.weghorn@posteo.de>
2026-07-23 18:41:19 +0200

    tdf#130857 qt weld: Support Calc "Define Label Range" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * "Sheet" -> "Named Ranges and Expressions" -> "Labels..."
    
    Change-Id: I87f009618d8c3514eff6b64d7c67fbc0b9a033d7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208559
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -210,6 +210,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/scalc/ui/managenamesdialog.ui",
     u"modules/scalc/ui/movecopysheet.ui",
     u"modules/scalc/ui/movingaveragedialog.ui",
+    u"modules/scalc/ui/namerangesdialog.ui",
     u"modules/scalc/ui/optimalcolwidthdialog.ui",
     u"modules/scalc/ui/optimalrowheightdialog.ui",
     u"modules/scalc/ui/pagetemplatedialog.ui",
