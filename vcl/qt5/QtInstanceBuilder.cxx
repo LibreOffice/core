207e01e51f0aa178d705efba514a0beb529cac1e
Michael Weghorn <m.weghorn@posteo.de>
2026-07-23 18:29:34 +0200

    tdf#130857 qt weld: Support Calc "Random Number Generator" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * select cells A1:A10
    * "Sheet" -> "Fill Cells" -> "Fill Random Number..."
    
    Change-Id: Ic56ae0ff509c364d454fae57c7cf83b02982d595
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208555
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -217,6 +217,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/scalc/ui/pivotfielddialog.ui",
     u"modules/scalc/ui/printareasdialog.ui",
     u"modules/scalc/ui/protectsheetdlg.ui",
+    u"modules/scalc/ui/randomnumbergenerator.ui",
     u"modules/scalc/ui/regressiondialog.ui",
     u"modules/scalc/ui/rowheightdialog.ui",
     u"modules/scalc/ui/samplingdialog.ui",
