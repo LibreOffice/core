60960cbc06c3ce33d0416114e5c3fb28740e0e8e
Michael Weghorn <m.weghorn@posteo.de>
2026-07-24 16:46:47 +0200

    tdf#130857 qt weld: Support Calc "Solver" dialog
    This means that native Qt widgets are used for this dialog
    now when using the qt5 or qt6 VCL plugin and starting LO with
    environment variable SAL_VCL_QT_USE_WELDED_WIDGETS=1 set.
    
    The dialog can be triggered like this:
    
    * start Calc
    * "Tools" -> "Solver..."
    
    Change-Id: I72462350b92d9802a4c0f487c81c6b0e69bff105
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208592
    Tested-by: Jenkins
    Reviewed-by: Michael Weghorn <m.weghorn@posteo.de>

--- vcl/qt5/QtInstanceBuilder.cxx
+++ vcl/qt5/QtInstanceBuilder.cxx
@@ -232,6 +232,7 @@ constexpr auto SUPPORTED_UI_FILES = frozen::make_unordered_set<std::u16string_vi
     u"modules/scalc/ui/sharedocumentdlg.ui",
     u"modules/scalc/ui/showsheetdialog.ui",
     u"modules/scalc/ui/simplerefdialog.ui",
+    u"modules/scalc/ui/solverdlg.ui",
     u"modules/scalc/ui/solveroptionsdialog.ui",
     u"modules/scalc/ui/sortdialog.ui",
     u"modules/scalc/ui/sparklinedialog.ui",
