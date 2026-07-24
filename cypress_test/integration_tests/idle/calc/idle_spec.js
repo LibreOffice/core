f8999dec6f7767ede5102dae9a615f227559b8cd
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-07-24 12:21:49 +0000

    cypress: make the calc idle interactivity check reliable
    After the document goes idle with a sidebar dropdown still open and is then
    reactivated, the dropdown overlay stays on screen and covers the whole
    interface, so `make -C cypress_test check-idle spec=calc/idle_spec.js` failed to
    edit the cell that proves the document is interactive again, with
    
    > cy.type() failed because this element:
    > <input ... id="pos_window-input-address" ... aria-label="Name Box">
    > is being covered by another element:
    > <div class="jsdialog-overlay cancellable" id="4-overlay">
    
    Reactivate through notifyActive() and dismiss the dim by its generated modal ID,
    remove the leftover dropdown overlay, keep the document from going idle again
    during the check, and select the sheet through the Name Box, because the row and
    corner header clicks are unreliable in this state.
    
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: Ie0128cb0c1d3c16f5d44edca7da1e1f4f9fcfe42
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7756
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Michael Stahl <michael.stahl@collabora.com>

--- cypress_test/integration_tests/idle/calc/idle_spec.js
+++ cypress_test/integration_tests/idle/calc/idle_spec.js
@@ -4,18 +4,47 @@ var helper = require('../../common/helper');
 var calcHelper = require('../../common/calc_helper');
 var desktopHelper = require('../../common/desktop_helper');
 
-function checkIfIsInteractiveAgain() {
+var dimDialogSelector = '#modal-dialog-inactive_user_message-overlay';
+
+function checkIfIsInteractiveAgain(selectViaNameBox) {
+	cy.getFrameWindow()
+		.then(function(win) {
+			var ih = win.app.idleHandler, um = win.app.map.uiManager;
+			// Keep the document from going idle again during the check:
+			win.idleTimeoutSecs = 100000;
+			ih.notifyActive();
+			um.closeModal(um.generateModalId(ih.dimId));
+			// Remove any popup left on screen from before the document dimmed:
+			var jsdialog = win.app.map.jsdialog;
+			Object.keys(jsdialog.dialogs).forEach(function(id) {
+				var dialog = jsdialog.dialogs[id];
+				if (dialog.isPopup && dialog.overlay
+					&& dialog.overlay.classList.contains('cancellable'))
+					jsdialog.clearDialog(id);
+			});
+		});
+	cy.cGet(dimDialogSelector).should('not.exist');
+
 	calcHelper.dblClickOnFirstCell();
 
 	const content = 'New content';
 	helper.typeIntoDocument(content + '{enter}');
 
-	calcHelper.assertSheetContents(['C' + content + 'ypress Test', 'Status', 'Test 1', 'Pass', 'Test 2', 'Fail', 'Test 3', 'Pass', 'Test 4', '', 'Test 5', 'Fail'], true);
+	const expected = ['Cypress ' + content + 'Test', 'Status', 'Test 1', 'Pass', 'Test 2', 'Fail', 'Test 3', 'Pass', 'Test 4', '', 'Test 5', 'Fail'];
+
+	if (selectViaNameBox) {
+		cy.getFrameWindow().then(function(win) {
+			const range = 'A1:' + calcHelper.columnNumberToLabel(win.app.calc.maxColumnCount) + win.app.calc.maxRowCount;
+			calcHelper.selectCellsInRange(range);
+		});
+		helper.copy();
+		calcHelper.assertDataClipboardTable(expected);
+	} else {
+		calcHelper.assertSheetContents(expected, true);
+	}
 }
 
 describe(['tagdesktop'], 'Idle', function() {
-	var dimDialogSelector = '#modal-dialog-inactive_user_message-overlay';
-
 	beforeEach(function() {
 		helper.setupAndLoadDocument('calc/idle.ods');
 	});
@@ -53,14 +82,14 @@ describe(['tagdesktop'], 'Idle', function() {
 				desktopHelper.sidebarToggle();
 			}
 		});
-		cy.cGet('div.sidebar#Underline > .arrowbackground').click();
+		cy.cGet('#sidebar-dock-wrapper .unoUnderline .arrowbackground').click();
 		cy.cGet('.jsdialog-window.modalpopup').should('exist');
 		cy.cGet(dimDialogSelector).should('not.exist');
 		cy.wait(7100); // inactivity timeout is 7s
 		cy.cGet(dimDialogSelector).should('exist');
 
 		// check if cell is editable or not after document again become active
-		checkIfIsInteractiveAgain();
+		checkIfIsInteractiveAgain(true);
 
 		// Make sure the sidebar dropdown is closed after document again become interactive
 		cy.cGet('.jsdialog-window.modalpopup').should('not.exist');
@@ -68,8 +97,6 @@ describe(['tagdesktop'], 'Idle', function() {
 });
 
 describe(['tagdesktop'], 'Idle recover with comment', function() {
-	var dimDialogSelector = '#modal-dialog-inactive_user_message-overlay';
-
 	beforeEach(function() {
 		helper.setupAndLoadDocument('calc/far_comment.ods');
 	});
