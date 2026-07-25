f6f60490ffdaab5ea67cf5f6edde375ab8b6e0ed
Oğuz Ersen <oguz@ersen.moe>
2026-07-25 19:48:20 +0200

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (36 of 36 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/tr/
    Signed-off-by: Oğuz Ersen <oguz@ersen.moe>
    Change-Id: Ia85f750ae019f3d20b26e646ad7be9be5f5950bb

--- qt/translations/coda_tr.ts
+++ qt/translations/coda_tr.ts
@@ -6,57 +6,57 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="49"/>
         <source>ODF text document (.odt)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF metin belgesi (.odt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="50"/>
         <source>Rich Text (.rtf)</source>
-        <translation type="unfinished"></translation>
+        <translation>Zengin Metin (.rtf)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="51"/>
         <source>Word Document (.docx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Word Belgesi (.docx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="52"/>
         <source>Word 2003 Document (.doc)</source>
-        <translation type="unfinished"></translation>
+        <translation>Word 2003 Belgesi (.doc)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="58"/>
         <source>ODF spreadsheet (.ods)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF çalışma sayfası (.ods)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="59"/>
         <source>Excel Spreadsheet (.xlsx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Excel Çalışma Sayfası (.xlsx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="60"/>
         <source>Excel 2003 Spreadsheet (.xls)</source>
-        <translation type="unfinished"></translation>
+        <translation>Excel 2003 Çalışma Sayfası (.xls)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="66"/>
         <source>ODF presentation (.odp)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF sunum (.odp)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="67"/>
         <source>PowerPoint Presentation (.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>PowerPoint Sunum (.pptx)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="68"/>
         <source>PowerPoint 2003 Presentation (.ppt)</source>
-        <translation type="unfinished"></translation>
+        <translation>PowerPoint 2003 Sunum (.ppt)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="74"/>
         <source>ODF drawing (.odg)</source>
-        <translation type="unfinished"></translation>
+        <translation>ODF Çizim (.odg)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="106"/>
@@ -87,12 +87,12 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="144"/>
         <source>Save Print Output As</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazdırma Çıktısını Farklı Kaydet</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="146"/>
         <source>PDF Files (*.pdf);;All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF Dosyaları (*.pdf);;Tüm Dosyalar (*)</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="162"/>
@@ -102,84 +102,84 @@
     <message>
         <location filename="../DocumentOperations.cpp" line="163"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="187"/>
         <source>Print to File Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosyaya Yazdırma Hatası</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="188"/>
         <source>Failed to save document to file. Please check the file path and permissions.</source>
-        <translation type="unfinished"></translation>
+        <translation>Belge dosyaya kaydedilemedi. Lütfen dosya yolunu ve izinlerini gözden geçirin.</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="232"/>
         <source>Print Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazdırma Hatası</translation>
     </message>
     <message>
         <location filename="../DocumentOperations.cpp" line="233"/>
         <source>Failed to print document. Please check your printer settings.</source>
-        <translation type="unfinished"></translation>
+        <translation>Belge yazdırılamadı. Lütfen yazıcı ayarlarınızı gözden geçirin.</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="737"/>
         <location filename="../Bridge.cpp" line="805"/>
         <source>All Files (*)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tüm Dosyalar (*)</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="228"/>
         <source>Save Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Belgeyi Kaydet</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="637"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosya Aç</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="638"/>
         <source>All Files (*);;Text Documents (*.odt *.ott *.doc *.docx *.rtf *.txt);;Spreadsheets (*.ods *.ots *.xls *.xlsx *.csv);;Presentations (*.odp *.otp *.ppt *.pptx)</source>
-        <translation type="unfinished"></translation>
+        <translation>Tüm Dosyalar (*);;Metin Belgeleri (*.odt *.ott *.doc *.docx *.rtf *.txt);;Çalışma Sayfaları (*.ods *.ots *.xls *.xlsx *.csv);;Sunumlar (*.odp *.otp *.ppt *.pptx)</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="735"/>
         <source>Export As</source>
-        <translation type="unfinished"></translation>
+        <translation>Dışarıya farklı aktar</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="803"/>
         <source>Save File</source>
-        <translation type="unfinished"></translation>
+        <translation>Dosyayı Kaydet</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="158"/>
         <source>Presentation</source>
-        <translation type="unfinished"></translation>
+        <translation>Sunum</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="163"/>
         <location filename="../WebView.cpp" line="179"/>
         <source>Text Document</source>
-        <translation type="unfinished"></translation>
+        <translation>Metin Belgesi</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="168"/>
         <source>Spreadsheet</source>
-        <translation type="unfinished"></translation>
+        <translation>Çalışma Sayfası</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="173"/>
         <source>Drawing</source>
-        <translation type="unfinished"></translation>
+        <translation>Çizim</translation>
     </message>
     <message>
         <location filename="../WebView.cpp" line="304"/>
         <source>Presenting</source>
-        <translation type="unfinished"></translation>
+        <translation>Sunum</translation>
     </message>
 </context>
 <context>
@@ -187,7 +187,7 @@
     <message>
         <location filename="../WebView.cpp" line="599"/>
         <source>Start</source>
-        <translation type="unfinished"></translation>
+        <translation>Başlat</translation>
     </message>
 </context>
 </TS>
