d86ee6d21122881ba1db67d3edd1a29b3b89b31f
Xisco Fauli <xiscofauli@libreoffice.org>
2026-07-25 12:27:31 +0200

    sw: fix warning C6011: Dereferencing NULL pointer 'pDelNd'.
    Change-Id: I35d40bfc058a6a1b8d843a0fec15c2a040e2c992
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208608
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- sw/source/filter/basflt/shellio.cxx
+++ sw/source/filter/basflt/shellio.cxx
@@ -710,19 +710,23 @@ void Reader::EndPaste(SwPasteInfo& rPasteInfo)
                 {
                     aPam.GetPoint()->Assign(*pTextNode, pTextNode->GetText().getLength());
                 }
-                // If the first new node isn't empty, convert  the node's text
-                // attributes into hints. Otherwise, set the new node's
-                // paragraph style at the previous (empty) node.
-                if (pTextNode->GetText().getLength())
-                    pDelNd->FormatToTextAttr(pTextNode);
-                else
+
+                if (pDelNd)
                 {
-                    pTextNode->ChgFormatColl(pDelNd->GetTextColl());
-                    if (!pDelNd->GetNoCondAttr(RES_PARATR_LIST_ID, /*bInParents=*/false))
+                    // If the first new node isn't empty, convert  the node's text
+                    // attributes into hints. Otherwise, set the new node's
+                    // paragraph style at the previous (empty) node.
+                    if (pTextNode->GetText().getLength())
+                        pDelNd->FormatToTextAttr(pTextNode);
+                    else
                     {
-                        // Lists would need manual merging, but copy paragraph direct formatting
-                        // otherwise.
-                        pDelNd->CopyCollFormat(*pTextNode);
+                        pTextNode->ChgFormatColl(pDelNd->GetTextColl());
+                        if (!pDelNd->GetNoCondAttr(RES_PARATR_LIST_ID, /*bInParents=*/false))
+                        {
+                            // Lists would need manual merging, but copy paragraph direct formatting
+                            // otherwise.
+                            pDelNd->CopyCollFormat(*pTextNode);
+                        }
                     }
                 }
                 pTextNode->JoinNext();
