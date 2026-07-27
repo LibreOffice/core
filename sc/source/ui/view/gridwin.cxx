LibreOffice/core

commit 0bdf0288d8c05b33b8d095ebd4c54d72df567c73

show indication of selected database range in Calc
This will show a rectangle around the database range covering the
selected cell. The fill handle allows to change the size of the
DB range.

cherry-pick from: d90807926d56dae13dfcacee251d04bda31df548

Change-Id: I9b1bdcebfb957ffe064000f05a379e2fd35e095c
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/193664
Tested-by: Andras Timar <andras.timar@collabora.com>
Reviewed-by: Andras Timar <andras.timar@collabora.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/196732
Reviewed-by: Balazs Varga <balazs.varga@collabora.com>
Tested-by: Balazs Varga <balazs.varga@collabora.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/197960

sc/source/ui/view/gridwin.cxx
@@ -1761,10 +1761,10 @@ bool ScGridWindow::TestMouse( const MouseEvent& rMEvt, bool bAction )
         ScRange aMarkRange;
         if (mrViewData.GetSimpleArea( aMarkRange ) == SC_MARK_SIMPLE)
         {
-            if (aMarkRange.aStart.Tab() == mrViewData.CurrentTabForData() && mpAutoFillRect)
+            if (aMarkRange.aStart.Tab() == mrViewData.CurrentTabForData())
             {
                 Point aMousePos = rMEvt.GetPosPixel();
-                if (mpAutoFillRect->Contains(aMousePos))
+                if (mpAutoFillRect && mpAutoFillRect->Contains(aMousePos))
                 {
                     SetPointer( PointerStyle::Cross );     //! bold cross ?
                     if (bAction)
@@ -1782,6 +1782,19 @@ bool ScGridWindow::TestMouse( const MouseEvent& rMEvt, bool bAction )
                         //  The simple selection must also be recognized when dragging,
                         //  where the Marking flag is set and MarkToSimple won't work anymore.
                         mrViewData.GetMarkData().MarkToSimple();
+                    }
+                    bNewPointer = true;
+                }
+                else if (mpDBExpandRect && mpDBExpandRect->Contains(aMousePos))
+                {
+                    SetPointer( PointerStyle::Cross );     //! bold cross ?
+                    if (bAction)
+                    {
+                        SCCOL nX = maDBRange.aEnd.Col();
+                        SCROW nY = maDBRange.aEnd.Row();
+
+                        mrViewData.SetDragMode(
+                            maDBRange.aStart.Col(), maDBRange.aStart.Row(), nX, nY, ScFillMode::DBEXPAND );
                     }
                     bNewPointer = true;
                 }
@@ -2030,7 +2043,6 @@ void ScGridWindow::HandleMouseButtonDown( const MouseEvent& rMEvt, MouseEventSta
 
     RfCorner rCorner = NONE;
     bool bFound = HitRangeFinder(rMEvt.GetPosPixel(), rCorner, &nRFIndex, &nRFAddX, &nRFAddY);
-    bRFSize = (rCorner != NONE);
     aRFSelectedCorned = rCorner;
 
     if (bFound)
@@ -2472,6 +2484,32 @@ void ScGridWindow::MouseButtonUp( const MouseEvent& rMEvt )
         {
             mrViewData.GetDocShell()->GetDocFunc().ResizeMatrix( aBlockRange, aEndPos );
             mrViewData.GetView()->MarkRange( ScRange( aBlockRange.aStart, aEndPos ) );
+        }
+    }
+    else if (mrViewData.IsDBExpandMode())
+    {
+        SCCOL nStartCol;
+        SCROW nStartRow;
+        SCCOL nEndCol;
+        SCROW nEndRow;
+        mrViewData.GetFillData( nStartCol, nStartRow, nEndCol, nEndRow );
+        SCCOL nFillCol = mrViewData.GetRefEndX();
+        SCROW nFillRow = mrViewData.GetRefEndY();
+        ScViewFunc* pView = mrViewData.GetView();
+        pView->StopRefMode();
+        mrViewData.ResetFillMode();
+        pView->GetFunctionSet().SetAnchorFlag( false );    // #i5819# don't use AutoFill anchor flag for selection
+        ScAddress aCurrentAddress = mrViewData.GetCurPos();
+        ScDocument& rDocument = mrViewData.GetDocument();
+        if (ScDBData* pDBData = rDocument.GetDBAtCursor(aCurrentAddress.Col(), aCurrentAddress.Row(), aCurrentAddress.Tab(), ScDBDataPortion::AREA))
+        {
+            SCTAB nTab = mrViewData.GetTabNumber();
+            ScDBDocFunc aFunc( *mrViewData.GetDocShell() );
+            ScDBData aNewDBData(*pDBData);
+            ScRange aNewDBRange(nStartCol, nStartRow, nTab, nFillCol, nFillRow, nTab);
+            aNewDBRange.PutInOrder();
+            aNewDBData.SetArea(nTab, aNewDBRange.aStart.Col(), aNewDBRange.aStart.Row(), aNewDBRange.aEnd.Col(), aNewDBRange.aEnd.Row());
+            aFunc.ModifyDBData(aNewDBData);
         }
     }
     else if (mrViewData.IsAnyFillMode())
@@ -3202,7 +3240,9 @@ void ScGridWindow::StartDrag( sal_Int8 /* nAction */, const Point& rPosPixel )
     }
     else
         if ( !DrawCommand(aDragEvent) )
+        {
             mrViewData.GetView()->GetSelEngine()->Command( aDragEvent );
+        }
 }
 
 static void lcl_SetTextCursorPos( ScViewData& rViewData, ScSplitPos eWhich, vcl::Window* pWin )
@@ -6305,6 +6345,7 @@ void ScGridWindow::CursorChanged()
     UpdateCursorOverlay();
     UpdateAutoFillOverlay();
     UpdateSparklineGroupOverlay();
+    UpdateDatabaseOverlay();
 }
 
 void ScGridWindow::ImpCreateOverlayObjects()
@@ -6318,6 +6359,7 @@ void ScGridWindow::ImpCreateOverlayObjects()
     UpdateHeaderOverlay();
     UpdateShrinkOverlay();
     UpdateSparklineGroupOverlay();
+    UpdateDatabaseOverlay();
 }
 
 void ScGridWindow::ImpDestroyOverlayObjects()
@@ -6331,6 +6373,7 @@ void ScGridWindow::ImpDestroyOverlayObjects()
     DeleteHeaderOverlay();
     DeleteShrinkOverlay();
     DeleteSparklineGroupOverlay();
+    DeleteDatabaseOverlay();
 }
 
 void ScGridWindow::UpdateAllOverlays()
@@ -6932,6 +6975,66 @@ void ScGridWindow::UpdateSelectionOverlay()
     }
 }
 
+void ScGridWindow::DeleteDatabaseOverlay()
+{
+    mpOODatabase.reset();
+}
+
+void ScGridWindow::UpdateDatabaseOverlay()
+{
+    const MapMode aDrawMode = GetDrawMapMode();
+    const MapMode aOldMode = GetMapMode();
+    comphelper::ScopeGuard aMapModeGuard(
+        [&aOldMode, &aDrawMode, this] {
+            if (aOldMode != aDrawMode)
+                SetMapMode(aOldMode);
+        }
+    );
+    if (aOldMode != aDrawMode)
+        SetMapMode(aDrawMode);
+
+    DeleteDatabaseOverlay();
+
+    ScDocument& rDocument = mrViewData.GetDocument();
+    ScAddress aCurrentAddress = mrViewData.GetCurPos();
+    std::vector<basegfx::B2DRange> aRanges;
+    ScRange aCurrRange;
+    if (ScDBData* pDBData = rDocument.GetDBAtCursor(aCurrentAddress.Col(), aCurrentAddress.Row(), aCurrentAddress.Tab(), ScDBDataPortion::AREA))
+    {
+        pDBData->GetArea(aCurrRange);
+        Point aStart = mrViewData.GetScrPos(aCurrRange.aStart.Col(),
+                                            aCurrRange.aStart.Row(), eWhich);
+        Point aEnd = mrViewData.GetScrPos(aCurrRange.aEnd.Col() + 1,
+                                          aCurrRange.aEnd.Row() + 1, eWhich);
+
+        basegfx::B2DRange aRange(aStart.X(), aStart.Y(), aEnd.X(), aEnd.Y());
+        const basegfx::B2DHomMatrix aTransform(GetOutDev()->GetInverseViewTransformation());
+        aRange.transform(aTransform);
+        aRanges.push_back(aRange);
+
+        maDBRange = aCurrRange;
+    }
+
+    if (!aRanges.empty())
+    {
+        ScModule* mod = ScModule::get();
+        Color aDBColor = mod->GetColorConfig().GetColorValue(svtools::CALCDBFOCUS).nColor;
+
+        // #i70788# get the OverlayManager safely
+        rtl::Reference<sdr::overlay::OverlayManager> xOverlayManager = getOverlayManager();
+        std::unique_ptr<sdr::overlay::OverlayObject> pOverlay(new sdr::overlay::OverlaySelection(
+            sdr::overlay::OverlayType::NoFill,
+            aDBColor,
+            std::move(aRanges),
+            true, true));
+
+        xOverlayManager->add(*pOverlay);
+        std::unique_ptr<sdr::overlay::OverlayObjectList> pOverlayList = DrawFillMarker(aCurrRange.aEnd.Col(), aCurrRange.aEnd.Row(), mpDBExpandRect);
+        mpOODatabase.swap(pOverlayList);
+        mpOODatabase->append(std::move(pOverlay));
+    }
+}
+
 void ScGridWindow::UpdateHighlightOverlay()
 {
     mpOOHighlight.reset();          // DeleteHighlightOverlay
@@ -6966,36 +7069,8 @@ void ScGridWindow::DeleteAutoFillOverlay()
     mpAutoFillRect.reset();
 }
 
-void ScGridWindow::UpdateAutoFillOverlay()
+std::unique_ptr<sdr::overlay::OverlayObjectList> ScGridWindow::DrawFillMarker(SCCOL nX, SCROW nY, std::optional<tools::Rectangle>& rRect)
 {
-    const MapMode aDrawMode = GetDrawMapMode();
-    const MapMode aOldMode = GetMapMode();
-    comphelper::ScopeGuard aMapModeGuard(
-        [&aOldMode, &aDrawMode, this] {
-            if (aOldMode != aDrawMode)
-                SetMapMode(aOldMode);
-        }
-    );
-    if (aOldMode != aDrawMode)
-        SetMapMode(aDrawMode);
-
-    DeleteAutoFillOverlay();
-
-    //  get the AutoFill handle rectangle in pixels
-
-    if ( !(bAutoMarkVisible && aAutoMarkPos.Tab() == mrViewData.CurrentTabForData() &&
-         !mrViewData.HasEditView(eWhich) && mrViewData.IsActive()) )
-        return;
-
-    SCCOL nX = aAutoMarkPos.Col();
-    SCROW nY = aAutoMarkPos.Row();
-
-    if (!maVisibleRange.isInside(nX, nY) && !comphelper::LibreOfficeKit::isActive())
-    {
-        // Autofill mark is not visible.  Bail out.
-        return;
-    }
-
     SCTAB nTab = mrViewData.CurrentTabForData();
     ScDocument& rDoc = mrViewData.GetDocument();
     bool bLayoutRTL = rDoc.IsLayoutRTL( nTab );
@@ -7032,8 +7107,8 @@ void ScGridWindow::UpdateAutoFillOverlay()
     tools::Rectangle aFillRect(aFillPos, aFillHandleSize);
 
     // expand rect to increase hit area
-    mpAutoFillRect = aFillRect;
-    mpAutoFillRect->expand(fScaleFactor);
+    rRect = aFillRect;
+    rRect->expand(fScaleFactor);
 
     // #i70788# get the OverlayManager safely
     rtl::Reference<sdr::overlay::OverlayManager> xOverlayManager = getOverlayManager();
@@ -7082,10 +7157,46 @@ void ScGridWindow::UpdateAutoFillOverlay()
 
         xOverlayManager->add(*pOverlayOuter);
         xOverlayManager->add(*pOverlayInner);
-        mpOOAutoFill.reset(new sdr::overlay::OverlayObjectList);
-        mpOOAutoFill->append(std::move(pOverlayOuter));
-        mpOOAutoFill->append(std::move(pOverlayInner));
+        std::unique_ptr<sdr::overlay::OverlayObjectList> pOverlayList(new sdr::overlay::OverlayObjectList);
+        pOverlayList->append(std::move(pOverlayOuter));
+        pOverlayList->append(std::move(pOverlayInner));
+
+        return pOverlayList;
     }
+    return std::unique_ptr<sdr::overlay::OverlayObjectList>();
+}
+
+void ScGridWindow::UpdateAutoFillOverlay()
+{
+    const MapMode aDrawMode = GetDrawMapMode();
+    const MapMode aOldMode = GetMapMode();
+    comphelper::ScopeGuard aMapModeGuard(
+        [&aOldMode, &aDrawMode, this] {
+            if (aOldMode != aDrawMode)
+                SetMapMode(aOldMode);
+        }
+    );
+    if (aOldMode != aDrawMode)
+        SetMapMode(aDrawMode);
+
+    DeleteAutoFillOverlay();
+
+    //  get the AutoFill handle rectangle in pixels
+
+    if ( !(bAutoMarkVisible && aAutoMarkPos.Tab() == mrViewData.GetTabNumber() &&
+         !mrViewData.HasEditView(eWhich) && mrViewData.IsActive()) )
+        return;
+
+    SCCOL nX = aAutoMarkPos.Col();
+    SCROW nY = aAutoMarkPos.Row();
+
+    if (!maVisibleRange.isInside(nX, nY) && !comphelper::LibreOfficeKit::isActive())
+    {
+        // Autofill mark is not visible.  Bail out.
+        return;
+    }
+
+    mpOOAutoFill = DrawFillMarker(nX, nY, mpAutoFillRect);
 }
 
 void ScGridWindow::DeleteDragRectOverlay()

cui/inc/strings.hrc
@@ -486,6 +486,7 @@
 #define REG_HTMLUNKNOWN                     NC_("REG_HTMLUNKNOWN", "Text Highlighting")
 #define REG_CALCGRID                        NC_("REG_CALCGRID", "Calc Grid")
 #define REG_CALCCELLFOCUS                   NC_("REG_CALCCELLFOCUS", "Cell focus")
+#define REG_CALCDBFOCUS                     NC_("REG_CALCDBFOCUS", "Table Style focus")
 #define REG_CALCPAGEBREAK                   NC_("REG_CALCPAGEBREAK", "Page break")
 #define REG_CALCPAGEBREAKMANUAL             NC_("REG_CALCPAGEBREAKMANUAL", "Manual page breaks")
 #define REG_CALCPAGEBREAKAUTOMATIC          NC_("REG_CALCPAGEBREAKAUTOMATIC", "Automatic page breaks")

cui/source/options/appearance.cxx
@@ -753,6 +753,7 @@ void SvxAppearanceTabPage::FillItemsList()
             { HTMLUNKNOWN, CuiResId(REG_HTMLUNKNOWN) },
             { CALCGRID, CuiResId(REG_CALCGRID) },
             { CALCCELLFOCUS, CuiResId(REG_CALCCELLFOCUS) },
+            { CALCDBFOCUS, CuiResId(REG_CALCDBFOCUS) },
             { CALCPAGEBREAK, CuiResId(REG_CALCPAGEBREAK) },
             { CALCPAGEBREAKMANUAL, CuiResId(REG_CALCPAGEBREAKMANUAL) },
             { CALCPAGEBREAKAUTOMATIC, CuiResId(REG_CALCPAGEBREAKAUTOMATIC) },

include/svtools/colorcfg.hxx
@@ -54,6 +54,7 @@ enum ColorConfigEntry : int
     HTMLUNKNOWN         ,
     CALCGRID            ,
     CALCCELLFOCUS       ,
+    CALCDBFOCUS         ,
     CALCPAGEBREAK       ,
     CALCPAGEBREAKMANUAL,
     CALCPAGEBREAKAUTOMATIC,
@@ -165,6 +166,7 @@ namespace {
         { std::u16string_view(u"HTMLUnknown"), false },
         { std::u16string_view(u"CalcGrid"), false },
         { std::u16string_view(u"CalcCellFocus"), false },
+        { std::u16string_view(u"CalcDBFocus"), false },
         { std::u16string_view(u"CalcPageBreak"), false },
         { std::u16string_view(u"CalcPageBreakManual"), false },
         { std::u16string_view(u"CalcPageBreakAutomatic"), false },

officecfg/registry/data/org/openoffice/Office/UI.xcu
@@ -287,6 +287,11 @@
             <value xsi:nil="true"/>
           </prop>
         </node>
+        <node oor:name="CalcDBFocus">
+          <prop oor:name="Color">
+            <value xsi:nil="true"/>
+          </prop>
+        </node>
         <node oor:name="CalcPageBreak">
           <prop oor:name="Color">
             <value xsi:nil="true"/>

officecfg/registry/schema/org/openoffice/Office/UI.xcs
@@ -342,6 +342,16 @@
           </info>
         </prop>
       </group>
+      <group oor:name="CalcDBFocus">
+        <info>
+        <desc>Specifies the setting to change the database range highlight in Calc</desc>
+        </info>
+        <prop oor:name="Color" oor:type="xs:int">
+          <info>
+            <desc>Specifies the color for the database range highlight in Calc.</desc>
+          </info>
+        </prop>
+      </group>
       <group oor:name="CalcPageBreak">
         <info>
           <desc>Specifies the settings used for page breaks in the page break preview in Calc.</desc>

sc/source/ui/inc/gridwin.hxx
@@ -118,8 +118,11 @@ class SAL_DLLPUBLIC_RTTI ScGridWindow : public vcl::DocWindow, public DropTarget
     std::unique_ptr<sdr::overlay::OverlayObjectList> mpOOHeader;
     std::unique_ptr<sdr::overlay::OverlayObjectList> mpOOShrink;
     std::unique_ptr<sdr::overlay::OverlayObjectList> mpOOSparklineGroup;
+    std::unique_ptr<sdr::overlay::OverlayObjectList> mpOODatabase;
 
     std::optional<tools::Rectangle> mpAutoFillRect;
+    std::optional<tools::Rectangle> mpDBExpandRect;
+    ScRange maDBRange;
 
     /// LibreOfficeKit needs a persistent FmFormView for tiled rendering,
     /// otherwise the invalidations from drawinglayer do not work.
@@ -333,6 +336,7 @@ class SAL_DLLPUBLIC_RTTI ScGridWindow : public vcl::DocWindow, public DropTarget
     void            UpdateFormulaRange(SCCOL nX1, SCROW nY1, SCCOL nX2, SCROW nY2);
 
     std::unique_ptr<sdr::overlay::OverlayObject> DrawOverlay(const std::vector<tools::Rectangle>& rRects, const Color& rColor, bool bBorder, bool bContrastOutline, sdr::overlay::OverlayType eOverlayType);
+    std::unique_ptr<sdr::overlay::OverlayObjectList> DrawFillMarker(SCCOL nCol, SCROW nRow, std::optional<tools::Rectangle>& rRect);
 
 protected:
     virtual void    PrePaint(vcl::RenderContext& rRenderContext) override;
@@ -495,6 +499,8 @@ public:
     void            UpdateCursorOverlay();
     void            DeleteSelectionOverlay();
     void            UpdateSelectionOverlay();
+    void UpdateDatabaseOverlay();
+    void DeleteDatabaseOverlay();
     void            UpdateHighlightOverlay();
     void            DeleteAutoFillOverlay();
     void            UpdateAutoFillOverlay();

sc/source/ui/inc/viewdata.hxx
@@ -39,6 +39,7 @@ enum class ScFillMode
     EMBED_LT    = 2,
     EMBED_RB    = 3,
     MATRIX      = 4,
+    DBEXPAND    = 5, // using a fill mode is a bit of a hack but the DB expansion behaves like a auto fill
 };
 
 enum ScSplitMode { SC_SPLIT_NONE = 0, SC_SPLIT_NORMAL, SC_SPLIT_FIX, SC_SPLIT_MODE_MAX_ENUM = SC_SPLIT_FIX };
@@ -549,6 +550,7 @@ public:
     void            ResetFillMode();
     bool            IsAnyFillMode() const       { return nFillMode != ScFillMode::NONE; }
     bool            IsFillMode() const          { return nFillMode == ScFillMode::FILL; }
+    bool            IsDBExpandMode() const      { return nFillMode == ScFillMode::DBEXPAND; }
     ScFillMode      GetFillMode() const         { return nFillMode; }
 
     SvxAdjust       GetEditAdjust() const {return eEditAdjust; }

commit 0598c5748e63f1f11b13d23b1bf47b499153e6ed

tdf#172895: fix formula range selection
Regression from commit 0bdf0288d8c05b33b8d095ebd4c54d72df567c73
Author: Markus Mohrhard <markus.mohrhard@googlemail.com>
Date:   Mon Jul 21 08:26:26 2025 +0800

    show indication of selected database range in Calc

where that line was removed for no apparent reason

Change-Id: Ied403883a5e1e58bf229fb803b1c49863b38a483
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208664
Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
Tested-by: Jenkins

sc/source/ui/view/gridwin.cxx
@@ -2027,6 +2027,7 @@ void ScGridWindow::HandleMouseButtonDown( const MouseEvent& rMEvt, MouseEventSta
 
     RfCorner rCorner = NONE;
     bool bFound = HitRangeFinder(rMEvt.GetPosPixel(), rCorner, &nRFIndex, &nRFAddX, &nRFAddY);
+    bRFSize = (rCorner != NONE);
     aRFSelectedCorned = rCorner;
 
     if (bFound)