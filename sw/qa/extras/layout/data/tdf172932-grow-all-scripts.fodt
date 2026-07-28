LibreOffice/core

commit 7cbd793cfcd5513c8e5d85686fa0e29c021bd6f2

tdf#72341 sw: Make inc/dec font size affect all scripts
This change updates the FN_GROW_FONT_SIZE and FN_SHRINK_FONT_SIZE
handlers to scale fonts for all script types, even if the
currently-selected text does not contain a particular type.

This fixes the reported issue that bullets in RTL context are not
resized if a selected list item does not contain CTL characters. This
also fixes the broader issue of inserted text not reflecting earlier
size changes, simply because the script type for that inserted text
wasn't present at the time.

Change-Id: I881f0b90b5c3aecbbf2e40ee2ad6758de9c5bad3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192459
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>
Tested-by: Jenkins

sw/source/uibase/shells/txtattr.cxx
@@ -261,7 +261,8 @@ void SwTextShell::ExecCharAttrArgs(SfxRequest &rReq)
             rWrtSh.GetCurAttr( aSetItem.GetItemSet() );
             SfxItemSet aAttrSet( rPool, aSetItem.GetItemSet().GetRanges() );
 
-            SvtScriptType nScriptTypes = rWrtSh.GetScriptType();
+            SvtScriptType nScriptTypes
+                = SvtScriptType::LATIN | SvtScriptType::ASIAN | SvtScriptType::COMPLEX;
             const SvxFontHeightItem* pSize( static_cast<const SvxFontHeightItem*>(
                                         aSetItem.GetItemOfScript( nScriptTypes ) ) );
             std::vector<std::pair< const SfxPoolItem*, std::unique_ptr<SwPaM> >> vItems;

sw/qa/extras/layout/layout5.cxx
@@ -2087,6 +2087,34 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf168116)
     assertXPath(pXmlDoc, "//body/txt[1]/SwParaPortion/SwLineLayout", 1);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf72341GrowAllScripts)
+{
+    createSwDoc();
+
+    auto* pWrtShell = getSwDocShell()->GetWrtShell();
+
+    pWrtShell->Insert(u"AAAAאאאא"_ustr);
+    pWrtShell->Left(SwCursorSkipMode::Chars, /*bSelect*/ false, 7, /*bBasicCall*/ false);
+    pWrtShell->Right(SwCursorSkipMode::Chars, /*bSelect*/ true, 2, /*bBasicCall*/ false);
+
+    for (size_t i = 0; i < 4; ++i)
+    {
+        dispatchCommand(mxComponent, u".uno:Grow"_ustr, {});
+    }
+
+    pWrtShell->Left(SwCursorSkipMode::Chars, /*bSelect*/ false, 1, /*bBasicCall*/ false);
+    pWrtShell->Insert(u"אאאא"_ustr);
+
+    auto pXmlDoc = parseLayoutDump();
+
+    auto nWidthAlephResized
+        = getXPath(pXmlDoc, "//SwLineLayout/SwMultiPortion[1]", "width").toInt32();
+    auto nWidthAlephInitial
+        = getXPath(pXmlDoc, "//SwLineLayout/SwMultiPortion[2]", "width").toInt32();
+
+    CPPUNIT_ASSERT_GREATER(nWidthAlephInitial, nWidthAlephResized);
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */

commit 232d10d8f653a179050963a38a96b0740a56a509

tdf#172932 sw: Restore inc/dec font size special case
This change narrowly restores a code path that was previously taken in
FN_GROW_FONT_SIZE/FN_SHRINK_FONT_SIZE when a selection inherits its font
height entirely from the style hierarchy and does not contain other font
height formatting. This fixes a regression that made these commands
appear to do nothing in certain cases.

These commands are very buggy and require further attention, but due to
limited automated testing the risk of further regression is high.

Regression from commit 7cbd793cfcd5513c8e5d85686fa0e29c021bd6f2
Author: Jonathan Clark <jonathan@libreoffice.org>
Date:   Wed Oct 15 10:28:23 2025 -0600

    tdf#72341 sw: Make inc/dec font size affect all scripts

Change-Id: I6e8e61112b77b0c8bb738f3f5de370c167f77eb4
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/208747
Tested-by: Jenkins
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>

sw/source/uibase/shells/txtattr.cxx
@@ -262,8 +262,7 @@ void SwTextShell::ExecCharAttrArgs(SfxRequest &rReq)
             rWrtSh.GetCurAttr( aSetItem.GetItemSet() );
             SfxItemSet aAttrSet( rPool, aSetItem.GetItemSet().GetRanges() );
 
-            SvtScriptType nScriptTypes
-                = SvtScriptType::LATIN | SvtScriptType::ASIAN | SvtScriptType::COMPLEX;
+            SvtScriptType nScriptTypes = rWrtSh.GetScriptType();
             const SvxFontHeightItem* pSize( static_cast<const SvxFontHeightItem*>(
                                         aSetItem.GetItemOfScript( nScriptTypes ) ) );
             std::vector<std::pair< const SfxPoolItem*, std::unique_ptr<SwPaM> >> vItems;
@@ -300,7 +299,9 @@ void SwTextShell::ExecCharAttrArgs(SfxRequest &rReq)
                         nSize = nFontInc;
 
                     aSize.SetHeight( nSize );
-                    aSetItem.PutItemForScriptType( nScriptTypes, aSize );
+                    aSetItem.PutItemForScriptType(SvtScriptType::LATIN | SvtScriptType::ASIAN
+                                                      | SvtScriptType::COMPLEX,
+                                                  aSize);
                     aAttrSet.Put( aSetItem.GetItemSet() );
                     if( pColl )
                         pColl->SetFormatAttr( aAttrSet );

sw/qa/extras/layout/data/tdf172932-grow-all-scripts.fodt
@@ -1,0 +1,123 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<office:document xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" office:version="1.4" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:meta><meta:generator>LibreOffice/26.2.4.2$Linux_X86_64 LibreOffice_project/620$Build-2</meta:generator><dc:date>2026-07-27T14:32:24.458936507</dc:date><meta:editing-duration>PT10M37S</meta:editing-duration><meta:editing-cycles>5</meta:editing-cycles><meta:document-statistic meta:table-count="0" meta:image-count="0" meta:object-count="0" meta:page-count="1" meta:paragraph-count="3" meta:word-count="75" meta:character-count="508" meta:non-whitespace-character-count="428"/></office:meta>
+ <office:font-face-decls>
+  <style:font-face style:name="Noto Serif" svg:font-family="Noto Serif" style:font-family-generic="roman" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Serif1" svg:font-family="Noto Serif" style:font-family-generic="swiss" style:font-pitch="variable"/>
+  <style:font-face style:name="Liberation Serif" svg:font-family="Liberation Serif" style:font-family-generic="swiss" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Sans" svg:font-family="Noto Sans" style:font-family-generic="system" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="graphic">
+   <style:graphic-properties svg:stroke-color="#3465a4" draw:fill-color="#729fcf" fo:wrap-option="no-wrap" draw:shadow-offset-x="0.3cm" draw:shadow-offset-y="0.3cm" draw:start-line-spacing-horizontal="0.283cm" draw:start-line-spacing-vertical="0.283cm" draw:end-line-spacing-horizontal="0.283cm" draw:end-line-spacing-vertical="0.283cm" style:writing-mode="lr-tb" style:flow-with-text="false"/>
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:line-break="strict" loext:tab-stop-distance="0cm" style:font-independent-line-spacing="false">
+    <style:tab-stops/>
+   </style:paragraph-properties>
+   <style:text-properties fo:color="#000000" loext:opacity="100%" style:font-name="Noto Serif" fo:font-size="12pt" fo:language="en" fo:country="PH" style:letter-kerning="true" style:font-name-asian="Noto Sans" style:font-size-asian="12pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Noto Sans" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN"/>
+  </style:default-style>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties fo:hyphenation-ladder-count="no-limit" fo:hyphenation-keep="auto" loext:hyphenation-keep-type="column" loext:hyphenation-keep-line="false" style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:tab-stop-distance="1.251cm" style:writing-mode="page"/>
+   <style:text-properties fo:color="#000000" loext:opacity="100%" style:font-name="Noto Serif" fo:font-size="12pt" fo:language="en" fo:country="PH" style:letter-kerning="true" style:font-name-asian="Noto Sans" style:font-size-asian="12pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Noto Sans" style:font-size-complex="12pt" style:language-complex="hi" style:country-complex="IN" fo:hyphenate="false" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false" loext:hyphenation-no-last-word="false" loext:hyphenation-word-char-count="5" loext:hyphenation-zone="no-limit"/>
+  </style:default-style>
+  <style:default-style style:family="table">
+   <style:table-properties table:border-model="separating"/>
+  </style:default-style>
+  <style:default-style style:family="table-row">
+   <style:table-row-properties fo:keep-together="auto"/>
+  </style:default-style>
+  <style:style style:name="Body_20_Text" style:display-name="Body Text" style:family="paragraph">
+   <style:paragraph-properties fo:margin-top="0.15cm" fo:margin-bottom="0.15cm" style:contextual-spacing="false" style:line-spacing="0.499cm" fo:text-align="justify" style:justify-single-word="false" fo:orphans="2" fo:widows="2"/>
+   <style:text-properties fo:color="#1a1a2e" loext:opacity="100%" style:font-name="Noto Serif1" fo:font-family="Noto Serif" style:font-family-generic="swiss" style:font-pitch="variable" fo:font-size="11pt"/>
+  </style:style>
+  <style:style style:name="Drop_20_Cap_20_Body" style:display-name="Drop Cap Body" style:family="paragraph" style:parent-style-name="Body_20_Text">
+   <style:paragraph-properties fo:margin-top="0.199cm" fo:margin-bottom="0.15cm" style:contextual-spacing="false" fo:text-align="justify" style:justify-single-word="false">
+    <style:drop-cap style:lines="3" style:distance="0.15cm"/>
+   </style:paragraph-properties>
+  </style:style>
+  <style:style style:name="Drop_20_Cap_20_Letter" style:display-name="Drop Cap Letter" style:family="text">
+   <style:text-properties fo:color="#10b981" loext:opacity="100%" style:font-name="Liberation Serif" fo:font-family="Liberation Serif" style:font-family-generic="swiss" style:font-pitch="variable" fo:font-size="300%" fo:font-weight="bold"/>
+  </style:style>
+  <text:outline-style style:name="Outline">
+   <text:outline-level-style text:level="1" loext:num-list-format="%1%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="2" loext:num-list-format="%2%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="3" loext:num-list-format="%3%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="4" loext:num-list-format="%4%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="5" loext:num-list-format="%5%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="6" loext:num-list-format="%6%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="7" loext:num-list-format="%7%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="8" loext:num-list-format="%8%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="9" loext:num-list-format="%9%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="10" loext:num-list-format="%10%" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="nothing"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+  </text:outline-style>
+  <text:notes-configuration text:note-class="footnote" style:num-format="1" text:start-value="0" text:footnotes-position="page" text:start-numbering-at="document"/>
+  <text:notes-configuration text:note-class="endnote" style:num-format="i" text:start-value="0"/>
+  <text:linenumbering-configuration text:number-lines="false" text:offset="0.499cm" style:num-format="1" text:number-position="left" text:increment="5"/>
+  </office:styles>
+ <office:automatic-styles>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="21.001cm" fo:page-height="29.7cm" style:num-format="1" style:print-orientation="portrait" fo:margin-top="2cm" fo:margin-bottom="2cm" fo:margin-left="2.499cm" fo:margin-right="2.499cm" style:writing-mode="lr-tb" style:layout-grid-color="#c0c0c0" style:layout-grid-lines="20" style:layout-grid-base-height="0.706cm" style:layout-grid-ruby-height="0.353cm" style:layout-grid-mode="none" style:layout-grid-ruby-below="false" style:layout-grid-print="false" style:layout-grid-display="false" style:footnote-max-height="0cm" loext:margin-gutter="0cm">
+    <style:footnote-sep style:width="0.018cm" style:distance-before-sep="0.101cm" style:distance-after-sep="0.101cm" style:line-style="solid" style:adjustment="left" style:rel-width="25%" style:color="#000000"/>
+   </style:page-layout-properties>
+   <style:header-style/>
+   <style:footer-style/>
+  </style:page-layout>
+  <style:style style:name="dp1" style:family="drawing-page">
+   <style:drawing-page-properties draw:background-size="full"/>
+  </style:style>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1" draw:style-name="dp1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <text:sequence-decls>
+    <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Figure"/>
+   </text:sequence-decls>
+   <text:p text:style-name="Drop_20_Cap_20_Body"><text:span text:style-name="Drop_20_Cap_20_Letter">S</text:span>ince the first pair of Eurasian beavers (Castor fiber) was released into the Riverwood catchment in autumn 2024, the programme has expanded from a single enclosed trial into a thriving network of free-living family groups across four connected sub-catchments. This report summarises progress over the reporting period, drawing on field surveys, remote camera data, and volunteer records.</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout5.cxx
@@ -2137,6 +2137,27 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf72341GrowAllScripts)
     CPPUNIT_ASSERT_GREATER(nWidthAlephInitial, nWidthAlephResized);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf172932GrowAllScripts)
+{
+    createSwDoc("tdf172932-grow-all-scripts.fodt");
+
+    auto* pWrtShell = getSwDocShell()->GetWrtShell();
+
+    pWrtShell->Right(SwCursorSkipMode::Chars, /*bSelect*/ false, 10, /*bBasicCall*/ false);
+    pWrtShell->Right(SwCursorSkipMode::Chars, /*bSelect*/ true, 10, /*bBasicCall*/ false);
+
+    for (size_t i = 0; i < 20; ++i)
+    {
+        dispatchCommand(mxComponent, u".uno:Grow"_ustr, {});
+    }
+
+    // Without the fix, the first line contents will be:
+    // "Since the first pair of Eurasian beavers (Castor fiber) was released into the "
+    auto pXmlDoc = parseLayoutDump();
+    assertXPath(pXmlDoc, "//body/txt/SwParaPortion/SwLineLayout[1]", "portion",
+                u"Since the first pair of Eurasian ");
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter5, testTdf168858_singleHiddenSection)
 {
     // Both test documents have no content outside of hidden sections.