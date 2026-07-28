b23529f4c9ecacda2a2f5e063e530207924be469
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-07-28 13:59:28 +0100

    a11y: add aria labels for current/next previews in presenter console
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Ie045cefab3838aac0f3511616905038ece381173
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/7998
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- browser/src/slideshow/PresenterConsole.js
+++ browser/src/slideshow/PresenterConsole.js
@@ -42,6 +42,8 @@ class PresenterConsole {
 			goBack: _('Go Back'),
 			zoomIn: _('Zoom In'),
 			zoomOut: _('Zoom Out'),
+			currentSlidePreview: _('Preview of the current slide'),
+			nextSlidePreview: _('Preview of the next slide'),
 		};
 		let sanitizer = document.createElement('div');
 		sanitizer.innerText = title;
@@ -86,7 +88,7 @@ class PresenterConsole {
 											</button>
 										</div>
                                         <div id='current-slide-container'>
-                                            <canvas id="current-presentation"></canvas>
+                                            <canvas id="current-presentation" role="img" aria-label="${this.labels.currentSlidePreview}"></canvas>
 											<div id="slideshow-control-container">
 											<div id="navigation-container">
 												<button type="button" id="prev" data-cooltip="${this.labels.previous}" aria-label="${this.labels.previous}">
@@ -115,7 +117,7 @@ class PresenterConsole {
                                      <div id="second-presentation">
                                          <div id="title-next">${this.labels.nextSlide}</div>
                                          <div id='next-slide-container'>
-                                            <img id="next-presentation"></img>
+                                            <img id="next-presentation" alt="${this.labels.nextSlidePreview}"></img>
                                          </div>
                                     </div>
                                   </div>
