0e1f3611bf6dd68ec3904d6866676ab82dbaa1a2
Stephan Bergmann <stephan.bergmann@collabora.com>
2026-07-30 14:44:22 +0000

    Automake->gbuild prequel: loplugin:pointerbool
    Signed-off-by: Stephan Bergmann <stephan.bergmann@collabora.com>
    Change-Id: Ia6433f207d4145f2fc91573905dbf59aa10e9c60
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/8304
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- wsd/ClientSession.cpp
+++ wsd/ClientSession.cpp
@@ -1052,7 +1052,7 @@ bool ClientSession::_handleInput(const char *buffer, int length)
         // The savetostorage command is really only used to resolve save conflicts
         // and it seems to always have force=1. However, we should still honor the
         // contract and do as told, not as we expect the API to be used. Use force if provided.
-        docBroker->uploadToStorage(client_from_this(), force);
+        docBroker->uploadToStorage(client_from_this(), force != 0);
     }
     else if (tokens.equals(0, "clientvisiblearea"))
     {
