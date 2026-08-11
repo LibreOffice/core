LibreOffice/core

commit 4031fa9b9aea40ca36b7fd100950f42d5f23f762

tdf#172113 sw ms import: consistent linespacing gap height
I guess they probably used to be consistent before LO 7.1.
After 7.1, only the last line still had a gap
simply determined by the largest portion in the line.
The other lines were determined primarily
by the largest TextPortion in the line.

That is inconsistent and rather confusing.

The other lines get their gap height
from GetLineSpacingBaseHeight.

So change mnHeightOfLastLine
to also use 7.1's GetLineSpacingBaseHeight
which now results in high interoperability with MS formats.

SAFETY FACTOR: very safe
It fixes MS formats and only affects new documents.

make CppunitTest_sw_ooxmlexport25 \
    CPPUNIT_TEST_NAME=testTdf164835_nonDummyLineHeight

make CppunitTest_sw_ooxmlexport25 \
    CPPUNIT_TEST_NAME=testTdf137335_whitespaceLineHeight2

make CppunitTest_sw_layoutwriter2 \
    CPPUNIT_TEST_NAME=testHiddenParaLineHeight

make CppunitTest_sw_ooxmlexport25 \
    CPPUNIT_TEST_NAME=testTdf172113_linespacingNumbering

Change-Id: I55669b1c6018c0f8a030088622d796edd7c781c8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/205316
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/core/text/txtfrm.cxx
@@ -3942,6 +3942,13 @@ void SwTextFrame::CalcHeightOfLastLine( const bool _bUseFont )
                 }
                 if ( pLineLayout )
                 {
+                    if (pIDSA->get(DocumentSettingId::LINE_SPACING_AS_GAP_BELOW))
+                    {
+                        // last line's line-spacing gap calculated the same as all other lines...
+                        mnHeightOfLastLine = pLineLayout->GetLineSpacingBaseHeight();
+                        return;
+                    }
+
                     SwTwips nAscent, nDescent, nDummy1, nDummy2;
                     // i#47162 - suppress consideration of
                     // fly content portions and the line portion.
@@ -4001,8 +4008,6 @@ tools::Long SwTextFrame::GetLineSpace( const bool _bNoPropLineSpace ) const
                 break;
             }
 
-            // i#11860 - adjust spacing implementation for object positioning
-            // - compatibility to MS Word
             nRet = GetHeightOfLastLine();
 
             tools::Long nTmp = nRet;

sw/qa/extras/layout/layout2.cxx
@@ -934,14 +934,14 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter2, testHiddenParaLineHeight)
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[4]/infos/bounds", "height", u"767");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[5]/infos/bounds", "height", u"475");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[6]/infos/bounds", "height", u"450");
-        assertXPath(pXmlDoc, "/root/page[1]/body/txt[7]/infos/bounds", "height", u"475");
+        assertXPath(pXmlDoc, "/root/page[1]/body/txt[7]/infos/bounds", "height", u"450");
         // 4: this was using wrong node's character properties (height 767)
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[8]/infos/bounds", "height", u"450");
-        assertXPath(pXmlDoc, "/root/page[1]/body/txt[9]/infos/bounds", "height", u"475");
+        assertXPath(pXmlDoc, "/root/page[1]/body/txt[9]/infos/bounds", "height", u"450");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[10]/infos/bounds", "height", u"767");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[11]/infos/bounds", "height", u"475");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[12]/infos/bounds", "height", u"450");
-        assertXPath(pXmlDoc, "/root/page[1]/body/txt[13]/infos/bounds", "height", u"475");
+        assertXPath(pXmlDoc, "/root/page[1]/body/txt[13]/infos/bounds", "height", u"450");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[14]/infos/bounds", "height", u"450");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[15]/infos/bounds", "height", u"450");
         assertXPath(pXmlDoc, "/root/page[1]/body/txt[16]/infos/bounds", "height", u"767");

sw/qa/extras/ooxmlexport/ooxmlexport25.cxx
@@ -52,15 +52,23 @@ DECLARE_OOXMLEXPORT_TEST(testTdf137335_whitespaceLineHeight, "tdf137335_whitespa
     CPPUNIT_ASSERT_EQUAL(SwTwips(276), Height);
 }
 
+DECLARE_OOXMLEXPORT_TEST(testTdf137335_whitespaceLineHeight2,
+                         "tdf137335_whitespaceLineHeight2.docx")
+{
+    // given a double-spaced document where the last line contains some very large spaces,
+    // the calculated linespacing between paragraphs should also not be based on the space's height.
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
+}
+
 DECLARE_OOXMLEXPORT_TEST(testTdf164835_nonDummyLineHeight, "tdf164835_nonDummyLineHeight.docx")
 {
     // given a document where whitespace-only lines wrap beside an image
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
     auto pXmlDoc = parseLayoutDump();
 
     SwTwips nImageBottom = getXPath(pXmlDoc, "//fly/infos/bounds", "bottom").toInt32();
-    // CPPUNIT_ASSERT_EQUAL(1, getPages()); // should all be on a single page
-    assertXPathContent(pXmlDoc, "//page[2]/body/txt", u"Text below the picture");
-    SwTwips nTextTop = getXPath(pXmlDoc, "//page[2]/body/txt/infos/bounds", "top").toInt32();
+    assertXPathContent(pXmlDoc, "//page/body/txt[5]", u"Text below the picture");
+    SwTwips nTextTop = getXPath(pXmlDoc, "//page/body/txt[5]/infos/bounds", "top").toInt32();
     // Without the fix, the text in paragraph 5 was beside the image.
     CPPUNIT_ASSERT_GREATER(nImageBottom, nTextTop); // text is below the image
 }
@@ -69,6 +77,13 @@ DECLARE_OOXMLEXPORT_TEST(testTdf172113_linespacingComment, "tdf172113_linespacin
 {
     // given a two page document - triple spaced with a comment in an otherwise empty paragraph
     CPPUNIT_ASSERT_EQUAL(2, getPages());
+}
+
+DECLARE_OOXMLEXPORT_TEST(testTdf172113_linespacingNumbering, "tdf172113_linespacingNumbering.odt")
+{
+    // given a 1 page, double spaced ODT created with the new LINE_SPACING_AS_GAP_BELOW
+    // containing 4 enlarged-numbering lines.
+    CPPUNIT_ASSERT_EQUAL(1, getPages());
 }
 
 DECLARE_OOXMLEXPORT_TEST(testTdf172169_linespacingFootnote, "tdf172169_linespacingFootnote.odt")

commit 8a448e6243d57cf1f9cd34a6725c398cd7aadfcf

tdf#172113 sw ms import: remove early return
This fixes my 27.2 regression from
commit 4031fa9b9aea40ca36b7fd100950f42d5f23f762
    tdf#172113 sw ms import: consistent linespacing gap height

It was OK in my exploratory patch 7, but when I moved it
to the final location, I failed to account for the invalidation.

Thanks to vmiklos for the code review spotting this.

Change-Id: I6af96e02b805f2b0f7836bca9d92f93413757eb5
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/209078
Tested-by: Jenkins
Reviewed-by: Justin Luth <jluth@mail.com>

sw/source/core/text/txtfrm.cxx
@@ -3940,15 +3940,16 @@ void SwTextFrame::CalcHeightOfLastLine( const bool _bUseFont )
                     // iteration to last line
                     pLineLayout = pLineLayout->GetNext();
                 }
-                if ( pLineLayout )
-                {
-                    if (pIDSA->get(DocumentSettingId::LINE_SPACING_AS_GAP_BELOW))
-                    {
-                        // last line's line-spacing gap calculated the same as all other lines...
-                        mnHeightOfLastLine = pLineLayout->GetLineSpacingBaseHeight();
-                        return;
-                    }
+                if (!pLineLayout)
+                    return; // mnHeightOfLastLine is unchanged
 
+                if (pIDSA->get(DocumentSettingId::LINE_SPACING_AS_GAP_BELOW))
+                {
+                    // last line's line-spacing gap calculated the same as all other lines...
+                    mnHeightOfLastLine = pLineLayout->GetLineSpacingBaseHeight();
+                }
+                else
+                {
                     SwTwips nAscent, nDescent, nDummy1, nDummy2;
                     // i#47162 - suppress consideration of
                     // fly content portions and the line portion.