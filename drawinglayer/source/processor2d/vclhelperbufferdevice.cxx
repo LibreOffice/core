LibreOffice/core

commit f5b8ec6b3fbc9a48bded4e621cc1bf3a8093f824

tdf#168730 impBufferDevice and caller should have the same mask rect
VclPixelProcessor2D::processPatternFillPrimitive2D and
impBufferDevice were independently calculating the mask rect in
different ways. So move the calculation to the callers.

This only fixed a tiny part of the problems in this bugreport,
it just helps with off-by-one at zoom levels.

Change-Id: I933f5271e31ba79794951a6b1f469fc28094d0a8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/192572
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
Tested-by: Jenkins

drawinglayer/source/processor2d/vclhelperbufferdevice.cxx
@@ -389,15 +389,12 @@ VDevBuffer& getVDevBuffer()
     return *aVDevBuffer.get();
 }
 
-impBufferDevice::impBufferDevice(OutputDevice& rOutDev, const basegfx::B2DRange& rRange)
+impBufferDevice::impBufferDevice(OutputDevice& rOutDev, const tools::Rectangle& rDestPixel)
     : mrOutDev(rOutDev)
     , mpContent(nullptr)
     , mpAlpha(nullptr)
+    , maDestPixel(rDestPixel)
 {
-    basegfx::B2DRange aRangePixel(rRange);
-    aRangePixel.transform(mrOutDev.GetViewTransformation());
-    maDestPixel = tools::Rectangle(floor(aRangePixel.getMinX()), floor(aRangePixel.getMinY()),
-                                   ceil(aRangePixel.getMaxX()), ceil(aRangePixel.getMaxY()));
     maDestPixel.Intersection(tools::Rectangle{ Point{}, mrOutDev.GetOutputSizePixel() });
 
     if (!isVisible())

drawinglayer/source/processor2d/cairopixelprocessor2d.cxx
@@ -3490,7 +3490,7 @@ void CairoPixelProcessor2D::processPatternFillPrimitive2D(
         return;
     }
 
-    impBufferDevice aBufferDevice(*mpTargetOutputDevice, aMaskRange);
+    impBufferDevice aBufferDevice(*mpTargetOutputDevice, aMaskRect);
     if (!aBufferDevice.isVisible())
         return;
     // remember last OutDev and set to content

drawinglayer/source/processor2d/vclhelperbufferdevice.hxx
@@ -99,7 +99,7 @@ class impBufferDevice
     tools::Rectangle maDestPixel;
 
 public:
-    impBufferDevice(OutputDevice& rOutDev, const basegfx::B2DRange& rRange);
+    impBufferDevice(OutputDevice& rOutDev, const tools::Rectangle& rDestPixel);
     ~impBufferDevice();
 
     void paint(double fTrans = 0.0);

drawinglayer/source/processor2d/vclpixelprocessor2d.cxx
@@ -1095,7 +1095,7 @@ void VclPixelProcessor2D::processPatternFillPrimitive2D(
         return;
     }
 
-    impBufferDevice aBufferDevice(*mpOutputDevice, aMaskRange);
+    impBufferDevice aBufferDevice(*mpOutputDevice, aMaskRect);
 
     if (!aBufferDevice.isVisible())
         return;

drawinglayer/source/processor2d/vclprocessor2d.cxx
@@ -26,6 +26,7 @@
 #include <tools/debug.hxx>
 #include <tools/fract.hxx>
 #include <utility>
+#include <vcl/canvastools.hxx>
 #include <vcl/glyphitemcache.hxx>
 #include <vcl/graph.hxx>
 #include <vcl/kernarray.hxx>
@@ -923,7 +924,8 @@ void VclProcessor2D::RenderMaskPrimitive2DPixel(const primitive2d::MaskPrimitive
     }
 
     const basegfx::B2DRange aRange(basegfx::utils::getRange(aMask));
-    impBufferDevice aBufferDevice(*mpOutputDevice, aRange);
+    tools::Rectangle aMaskRect = vcl::unotools::rectangleFromB2DRectangle(aRange);
+    impBufferDevice aBufferDevice(*mpOutputDevice, aMaskRect);
 
     if (!aBufferDevice.isVisible())
         return;
@@ -977,7 +979,8 @@ void VclProcessor2D::RenderUnifiedTransparencePrimitive2D(
         // transparence is in visible range
         basegfx::B2DRange aRange(rTransCandidate.getChildren().getB2DRange(getViewInformation2D()));
         aRange.transform(maCurrentTransformation);
-        impBufferDevice aBufferDevice(*mpOutputDevice, aRange);
+        tools::Rectangle aRangeRect = vcl::unotools::rectangleFromB2DRectangle(aRange);
+        impBufferDevice aBufferDevice(*mpOutputDevice, aRangeRect);
 
         if (aBufferDevice.isVisible())
         {
@@ -1006,7 +1009,8 @@ void VclProcessor2D::RenderTransparencePrimitive2D(
 
     basegfx::B2DRange aRange(rTransCandidate.getChildren().getB2DRange(getViewInformation2D()));
     aRange.transform(maCurrentTransformation);
-    impBufferDevice aBufferDevice(*mpOutputDevice, aRange);
+    tools::Rectangle aRangeRect = vcl::unotools::rectangleFromB2DRectangle(aRange);
+    impBufferDevice aBufferDevice(*mpOutputDevice, aRangeRect);
 
     if (!aBufferDevice.isVisible())
         return;

commit d2951c2b4588451f7c781dd00f18dc16a7feeb1f

tdf#170126 drawinglayer: Restore deleted transformation from refactor
Partially reverts a commit that was causing incorrect SVG rendering,
depending on VCL backend. This commit deleted code that translated
clipping rects from view space into device space. It seems like this
change was made intentionally, but it doesn't work correctly, and
reverting it doesn't seem to affect the behavior of the linked bug.

Regression from commit f5b8ec6b3fbc9a48bded4e621cc1bf3a8093f824
Author: Noel Grandin <noelgrandin@gmail.com>
Date:   Fri Oct 17 13:25:32 2025 +0200

    tdf#168730 impBufferDevice and caller should have the same mask rect

Change-Id: I7c83a5c372557f658df9ece65cbc6c142a4983ec
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/209070
Tested-by: Jenkins
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>

drawinglayer/source/processor2d/vclhelperbufferdevice.cxx
@@ -28,6 +28,7 @@
 #include <basegfx/range/b2drange.hxx>
 #include <vcl/alpha.hxx>
 #include <vcl/bitmap.hxx>
+#include <vcl/canvastools.hxx>
 #include <basegfx/matrix/b2dhommatrix.hxx>
 #include <basegfx/matrix/b2dhommatrixtools.hxx>
 #include <vcl/timer.hxx>
@@ -395,6 +396,9 @@ impBufferDevice::impBufferDevice(OutputDevice& rOutDev, const tools::Rectangle& 
     , mpAlpha(nullptr)
     , maDestPixel(rDestPixel)
 {
+    auto aRangePixel = vcl::unotools::b2DRectangleFromRectangle(maDestPixel);
+    aRangePixel.transform(mrOutDev.GetViewTransformation());
+    maDestPixel = vcl::unotools::rectangleFromB2DRectangle(aRangePixel);
     maDestPixel.Intersection(tools::Rectangle{ Point{}, mrOutDev.GetOutputSizePixel() });
 
     if (!isVisible())