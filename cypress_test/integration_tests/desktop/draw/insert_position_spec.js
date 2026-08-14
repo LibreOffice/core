2965baa9fa1fc4ecf66862ff31bfbdd9ae783ce8
Henry Castro <henry.castro@collabora.com>
2026-08-14 14:06:41 +0000

    cypress: test an inserted image lands in the visible area
    Insert an image in Draw with the page taller than the viewport and check
    the object's centre falls inside the client visible area. Without the fix
    the centre landed at y=12539 twips with a visible area of 0..9690.
    
    Change-Id: Id2dd00d4b71c0aab514ed6e68356b760f83ba1a6
    Signed-off-by: Henry Castro <henry.castro@collabora.com>
    
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/9229
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

---
+++ cypress_test/integration_tests/desktop/draw/insert_position_spec.js
@@ -0,0 +1,41 @@
+/* global describe it cy beforeEach require expect */
+
+const helper = require('../../common/helper');
+const desktopHelper = require('../../common/desktop_helper');
+
+describe(['tagdesktop'], 'Insert position', function() {
+
+	beforeEach(function() {
+		helper.setupAndLoadDocument('draw/insert_position.fodg');
+		desktopHelper.switchUIToNotebookbar();
+		// A viewport shorter than the A4 page, so the page does not fit and an
+		// object placed by the page geometry alone lands outside the view.
+		cy.viewport(1920, 800);
+	});
+
+	function assertSelectionIsInView() {
+		cy.getFrameWindow().its('app.definitions.graphicSelection.rectangle')
+			.should('not.be.null');
+
+		cy.getFrameWindow().then(function(win) {
+			helper.processToIdle(win);
+
+			const object = win.app.definitions.graphicSelection.rectangle;
+			const visible = win.app.activeDocument.activeLayout.viewedRectangle;
+
+			expect(object.x1 + object.width / 2,
+				'inserted object centre x').to.be.within(visible.x1, visible.x2);
+			expect(object.y1 + object.height / 2,
+				'inserted object centre y').to.be.within(visible.y1, visible.y2);
+		});
+	}
+
+	it('An inserted image lands where the user is looking', function() {
+		cy.cGet('#Insert-tab-label').click();
+		cy.cGet('#Insert-container .unoInsertGraphic').filter(':visible').click();
+		cy.cGet('#insertgraphic[type=file]')
+			.attachFile('/desktop/writer/image_to_insert.png');
+
+		assertSelectionIsInView();
+	});
+});
