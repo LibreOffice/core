LibreOffice/core

commit 622d1c6d03107871b21f305af482c72fa20b480f

python3: upgrade to 3.14.5rc1
* `ubsan.patch.0`: `Parser/tokenizer.c` code can't be found

* `python-3.14-msvc-zlibng.patch.1`: trick the MSVC project files to
  accept CO's bundled zlib, with minimal patch and additional args
  in ExternalProject

* There is a problem with the `decimal` module: there are in fact 2
  implementations: `_pydecimal` and `_decimal`, where the latter is both
  much faster and (allegedly) less precise, using a C library
  `mpdecimal`.

  https://discuss.python.org/t/should-we-keep-now-pydecimal-py/101829

  This has been bundled for a long time, but now they decided to
  unbundle it: https://github.com/python/cpython/issues/115119 - with
  the first step being to adapt the MSVC project file to no longer build
  the bundled sources, while other platforms still build it if no
  `mpdecimal` is detected on the system.

  Let's try to just not build `_decimal` on Windows, perhaps our users
  don't need the performance boost of `mpdecimal`, if they do we'll get
  a bug report.

Signed-off-by: Michael Stahl <michael.stahl@collabora.com>
Change-Id: Ia262c244aa5f70cd4fb0e0017aa130e5b763b073
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/2194
Reviewed-by: Stephan Bergmann <stephan.bergmann@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

engine/external/python3/ExternalProject_python3.mk
@@ -42,7 +42,9 @@ $(call gb_ExternalProject_get_state_target,python3,build) :
 			/p:bz2Dir=$(gb_UnpackedTarball_workdir)/bzip2 \
 			/p:opensslIncludeDir=$(gb_UnpackedTarball_workdir)/openssl/include \
 			/p:opensslOutDir=$(gb_UnpackedTarball_workdir)/openssl \
-			/p:zlibDir=$(gb_UnpackedTarball_workdir)/zlib \
+			/p:IncludeExternals=true \
+			/p:zlibLib=$(gb_StaticLibrary_WORKDIR)/zlib.lib \
+			/p:zlibNgDir=$(gb_UnpackedTarball_workdir)/zlib \
 			/p:sqlite3Dir=$(gb_UnpackedTarball_workdir)/sqlite3 \
 			/p:libffiOutDir=$(gb_UnpackedTarball_workdir)/libffi/$(HOST_PLATFORM)/.libs \
 			/p:libffiIncludeDir=$(gb_UnpackedTarball_workdir)/libffi/$(HOST_PLATFORM)/include \

engine/configure.ac
@@ -10685,8 +10685,8 @@ fi
 if test \( "$cross_compiling" = yes -a -z "$PYTHON_FOR_BUILD" \) -o "$enable_python" = internal; then
     SYSTEM_PYTHON=
     PYTHON_VERSION_MAJOR=3
-    PYTHON_VERSION_MINOR=12
-    PYTHON_VERSION=${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}.13
+    PYTHON_VERSION_MINOR=14
+    PYTHON_VERSION=${PYTHON_VERSION_MAJOR}.${PYTHON_VERSION_MINOR}.5
     if ! grep -q -i python.*${PYTHON_VERSION} ${SRC_ROOT}/download.lst; then
         AC_MSG_ERROR([PYTHON_VERSION ${PYTHON_VERSION} but no matching file in download.lst])
     fi

engine/download.lst
@@ -683,8 +683,8 @@ POSTGRESQL_TARBALL := postgresql-15.17.tar.bz2
 # three static lines
 # so that git cherry-pick
 # will not run into conflicts
-PYTHON_SHA256SUM := c08bc65a81971c1dd5783182826503369466c7e67374d1646519adf05207b684
-PYTHON_TARBALL := Python-3.12.13.tar.xz
+PYTHON_SHA256SUM := 67ee56f36fc22e5ada84d452430362e71081804c4f85c33dc5bf4206c27f973c
+PYTHON_TARBALL := Python-3.14.5rc1.tar.xz
 # three static lines
 # so that git cherry-pick
 # will not run into conflicts

engine/external/python3/UnpackedTarball_python3.mk
@@ -20,7 +20,7 @@ $(eval $(call gb_UnpackedTarball_set_pre_action,python3,\
 		chmod +x externals/pythonx86/tools/* && \
 	) \
 	unzip -q -d Lib/ -o Lib/test/wheeldata/setuptools-79.0.1-py3-none-any.whl && \
-	unzip -q -d Lib/ -o Lib/ensurepip/_bundled/pip-25.0.1-py3-none-any.whl && \
+	unzip -q -d Lib/ -o Lib/ensurepip/_bundled/pip-26.1-py3-none-any.whl && \
 	mv Lib/setuptools/_vendor/jaraco/text/Lorem\ ipsum.txt Lib/setuptools/_vendor/jaraco/text/Lorem_ipsum.txt \
 ))
 
@@ -39,6 +39,7 @@ $(eval $(call gb_UnpackedTarball_add_patches,python3,\
 	external/python3/python-3.3.0-darwin.patch.1 \
 	external/python3/python-3.7.6-msvc-ssl.patch.1 \
 	external/python3/python-3.5.4-msvc-disable.patch.1 \
+	external/python3/python-3.14-msvc-zlibng.patch.1 \
 	external/python3/ubsan.patch.0 \
 	external/python3/init-sys-streams-cant-initialize-stdin.patch.0 \
 	external/python3/setuptools.patch.1 \

engine/external/python3/generateExternalPackage.py
@@ -6,14 +6,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from os import path, walk
+from os import path, walk, environ
 
 if __name__ == '__main__':
 
     ignoredPackages = ["curses", "test", "tkinter", "turtledemo", "idlelib"]
 
-    coreDir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
-    pythonDir = path.join(coreDir, "workdir/UnpackedTarball/python3")
+    buildDir = environ["BUILDDIR"]
+    pythonDir = path.join(buildDir, "workdir/UnpackedTarball/python3")
     libDir = path.join(pythonDir, "Lib")
     subDirDict = {}
     for subdir, dirs, files in walk(libDir):
@@ -46,13 +46,9 @@ if __name__ == '__main__':
 
     for k,v in sorted(subDirDict.items()):
         print()
-        if k == "Lib/msilib":
-            print("ifeq (WNT,$(OS))")
         print("$(eval $(call gb_ExternalPackage_add_unpacked_files,python3,$(LIBO_BIN_FOLDER)/python-core-$(PYTHON_VERSION)/{},\\".format(k.replace("Lib", "lib", 3)))
         for fileName in sorted(v):
             print("\t{} \\".format(fileName))
         print("))")
-        if k == "Lib/msilib":
-            print("endif")
 
 # vim: set shiftwidth=4 softtabstop=4 expandtab:

engine/external/python3/init-sys-streams-cant-initialize-stdin.patch.0
@@ -1,8 +1,8 @@
 --- Python/pylifecycle.c	2024-03-27 14:34:43.905367358 +0000
 +++ Python/pylifecycle.c	2024-03-27 14:40:16.339134322 +0000
 @@ -1789,6 +1789,20 @@
-     if (!is_valid_fd(fd))
          Py_RETURN_NONE;
+     }
  
 +    /* Check that stdin, etc is not a directory
 +       Using shell redirection, you can redirect stdin to a directory,
@@ -38,6 +38,6 @@
 -    }
 -#endif
 -
-     if (!(iomod = PyImport_ImportModule("io"))) {
+     if (!(iomod = PyImport_ImportModule("_io"))) {
          goto error;
      }

engine/external/python3/internal-zlib.patch.0
@@ -11,8 +11,8 @@
  if test ${ac_cv_lib_z_inflateCopy+y}
  then :
    printf %s "(cached) " >&6
- else $as_nop
-   ac_check_lib_save_LIBS=$LIBS
+ else case e in #(
+   e) ac_check_lib_save_LIBS=$LIBS
 -LIBS="-lz  $LIBS"
 +LIBS="-lzlib  $LIBS"
  cat confdefs.h - <<_ACEOF >conftest.$ac_ext

commit 5cfe063691919a8241345ded4af6f5f20c0b7a57

external/python3: Fix macOS install name rewrite
...which was presumably broken by 622d1c6d03107871b21f305af482c72fa20b480f
"python3: upgrade to 3.14.5rc1", where CPython's macOS framework build
apparently no longer embeds the install name as
$(DESTDIR)$(PYTHONFRAMEWORKINSTALLNAMEPREFIX)/Python, but dropped the $(DESTDIR)
prefix.

This caused e.g. `make -C engine PythonTest_pyuno_pytests_testimports` to fail
for me with

> dyld[57441]: Library not loaded: /@__________________________________________________OOO/LibreOfficePython.framework/Versions/3.14/LibreOfficePython
>   Referenced from: <828F0F51-2E9A-3908-9352-E2F5EAF06139> /Users/sberg/co/online/engine/instdir/CollaboraOffice.app/Contents/Frameworks/LibreOfficePython.framework/Versions/3.14/Resources/Python.app/Contents/MacOS/LibreOfficePython
>   Reason: tried: '/@__________________________________________________OOO/LibreOfficePython.framework/Versions/3.14/LibreOfficePython' (no such file), '/System/Volumes/Preboot/Cryptexes/OS/@__________________________________________________OOO/LibreOfficePython.framework/Versions/3.14/LibreOfficePython' (no such file), '/@__________________________________________________OOO/LibreOfficePython.framework/Versions/3.14/LibreOfficePython' (no such file)

engine/external/python3/ExternalProject_python3.mk
@@ -150,7 +150,8 @@ endif
 
 ifeq ($(OS),MACOSX)
 
-python3_fw_prefix:=$(gb_UnpackedTarball_workdir)/python3/python-inst/@__________________________________________________OOO/LibreOfficePython.framework/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
+python3_fw_installname:=/@__________________________________________________OOO/LibreOfficePython.framework/Versions/$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
+python3_fw_prefix:=$(gb_UnpackedTarball_workdir)/python3/python-inst/$(python3_fw_installname)
 python3_EXTENSION_MODULE_SUFFIX:=cpython-$(PYTHON_VERSION_MAJOR)$(PYTHON_VERSION_MINOR)$(if $(ENABLE_DBGUTIL),d)-darwin
 
 # Since python 3.12 setuptools and pip are not available by default
@@ -188,19 +189,19 @@ cd \"$$origpath\"\n\
 $(call gb_ExternalProject_get_state_target,python3,fixinstallnames) : $(call gb_ExternalProject_get_state_target,python3,build) \
         | $(call gb_ExternalProject_get_state_target,python3,removeunnecessarystuff)
 	$(INSTALL_NAME_TOOL) -change \
-		$(python3_fw_prefix)/LibreOfficePython \
+		$(python3_fw_installname)/LibreOfficePython \
 		@executable_path/../../../../LibreOfficePython \
 		$(python3_fw_prefix)/Resources/Python.app/Contents/MacOS/LibreOfficePython
 	for file in $(shell $(FIND) $(python3_fw_prefix)/lib/python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)/lib-dynload -name "*.so") ; do \
 	$(INSTALL_NAME_TOOL) -change \
-		$(python3_fw_prefix)/LibreOfficePython \
+		$(python3_fw_installname)/LibreOfficePython \
 		@loader_path/../../../LibreOfficePython $$file ; done
 	touch $@
 
 $(call gb_ExternalProject_get_state_target,python3,executables) : $(call gb_ExternalProject_get_state_target,python3,build)
 	cd $(python3_fw_prefix)/bin ; \
 	$(INSTALL_NAME_TOOL) -change \
-		$(python3_fw_prefix)/LibreOfficePython \
+		$(python3_fw_installname)/LibreOfficePython \
 		@executable_path/../LibreOfficePython python$(PYTHON_VERSION_MAJOR).$(PYTHON_VERSION_MINOR)
 	touch $@
 