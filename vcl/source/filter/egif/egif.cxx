df813b5afce1475b983e710b48d840e80d89a02e
Mike Kaganski <mike.kaganski@collabora.com>
2026-08-19 10:18:52 +0000

    CreateAlphaMask + CreateColorBitmap -> SplitIntoColorAndAlpha
    See commit f7b67e6c8c5981790fb9ac162dcc68e470b53112 (pdfexport: reduce
    the number of times we split images, 2026-08-19) for rationale.
    
    Change-Id: I4fd3eedda81455f082cdbcd07c323e8c4aab77ff
    Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/9522
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    (cherry picked from commit 90fca744e7cd75173ecd694bb326b7706bce9a6b)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/209523
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- vcl/source/filter/egif/egif.cxx
+++ vcl/source/filter/egif/egif.cxx
@@ -245,12 +245,12 @@ bool GIFWriter::CreateAccess( const Bitmap& rBmp )
 {
     if( bStatus )
     {
-        aAccBmp = rBmp.CreateColorBitmap();
         bTransparent = false;
 
         if( rBmp.HasAlpha() )
         {
-            AlphaMask aMask( rBmp.CreateAlphaMask() );
+            AlphaMask aMask;
+            std::tie(aAccBmp, aMask) = rBmp.SplitIntoColorAndAlpha();
 
             if( aAccBmp.Convert( BmpConversion::N8BitTrans ) )
             {
@@ -263,7 +263,10 @@ bool GIFWriter::CreateAccess( const Bitmap& rBmp )
                 aAccBmp.Convert( BmpConversion::N8BitColors );
         }
         else
+        {
+            aAccBmp = rBmp;
             aAccBmp.Convert( BmpConversion::N8BitColors );
+        }
 
         m_pAcc = aAccBmp;
 
