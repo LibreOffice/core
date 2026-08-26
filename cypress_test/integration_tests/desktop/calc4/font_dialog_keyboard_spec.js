d444659ecac9054b3c0714ea0c1740e98bbed7ac
Henry Castro <henry.castro@collabora.com>
2026-08-26 16:29:31 +0000

    cypress: test the focus cycle wraps on the dialog's own focusables
    MakeFocusCycle puts a marker at each end of the container and sends the
    focus back to the first or the last focusable, and nothing covered that.
    Read both ends from the dialog rather than naming widgets.
    
    Change-Id: I8d8e52a405304e151f29472ef963a9a8ffebb5fb
    Signed-off-by: Henry Castro <henry.castro@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/10119
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>

--- cypress_test/integration_tests/desktop/calc4/font_dialog_keyboard_spec.js
+++ cypress_test/integration_tests/desktop/calc4/font_dialog_keyboard_spec.js
@@ -1,4 +1,4 @@
-/* global describe it cy beforeEach require */
+/* global describe expect it cy beforeEach require */
 
 var helper = require('../../common/helper');
 var calcHelper = require('../../common/calc_helper');
@@ -9,6 +9,44 @@ describe(['tagdesktop', 'tagnextcloud', 'tagproxy'], 'Font dialog keyboard navig
 		cy.getFrameWindow().then((win) => {
 			this.win = win;
 		});
+	});
+
+	it('Tab past the last widget wraps around to the first', function() {
+		calcHelper.dblClickOnFirstCell();
+		cy.then(() => {
+			this.win.app.map.sendUnoCommand('.uno:FontDialog');
+		});
+
+		cy.cGet('.ui-dialog[role="dialog"]').should('have.length', 1);
+		helper.processToIdle(this.win);
+
+		// The cycle wraps on the first and the last of the dialog's focusables,
+		// so read them from the dialog rather than naming widgets.
+		const describeElement = (element) =>
+			element ? element.tagName + '#' + (element.id || '<no id>') : 'nothing';
+
+		cy.cGet('.ui-dialog[role="dialog"]').then(($dialog) => {
+			const order = this.win.JSDialog.GetFocusableElements($dialog[0]);
+			expect(order.length, 'focusable widgets of the dialog').to.be.greaterThan(1);
+			order[order.length - 1].focus();
+		});
+
+		cy.realPress('Tab');
+		cy.cGet('.ui-dialog[role="dialog"]').should(($dialog) => {
+			const order = this.win.JSDialog.GetFocusableElements($dialog[0]);
+			expect(describeElement(this.win.document.activeElement),
+				'Tab from the last widget').to.equal(describeElement(order[0]));
+		});
+
+		cy.realPress(['Shift', 'Tab']);
+		cy.cGet('.ui-dialog[role="dialog"]').should(($dialog) => {
+			const order = this.win.JSDialog.GetFocusableElements($dialog[0]);
+			expect(describeElement(this.win.document.activeElement),
+				'Shift+Tab from the first widget').to.equal(
+				describeElement(order[order.length - 1]));
+		});
+
+		helper.typeIntoDocument('{esc}');
 	});
 
 	it('Up Arrow from the first font entry focuses the search field', function() {
