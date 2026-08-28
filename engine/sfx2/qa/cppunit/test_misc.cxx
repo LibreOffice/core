LibreOffice/core

commit e8d73d53073f1d953c3498bf0ee2bfef996eb4b0

Let TransformItems return comphelper::SequenceAsHashMap
Simplifies its usage in all places where the result needs to be
modified.

Change-Id: I37f8d99827e0f1cb2b6e4f4f7735e26fb87f6703
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/195162
Tested-by: Jenkins
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

forms/source/richtext/parametrizedattributedispatcher.cxx
@@ -75,13 +75,13 @@ namespace frm
             return;
 
         SfxItemSet aEmptySet(getEditView()->GetEmptyItemSet());
-        Sequence< PropertyValue > aUnoStateDescription;
+        comphelper::SequenceAsHashMap aUnoStateDescription;
         if ( _rState.getItem() )
         {
             aEmptySet.Put( *_rState.getItem() );
             SfxSlotId nSlotId = aEmptySet.GetPool()->GetSlotId( _rState.getItem()->Which() );
-            TransformItems( nSlotId, aEmptySet, aUnoStateDescription );
-            _rEvent.State <<= aUnoStateDescription;
+            aUnoStateDescription = TransformItems(nSlotId, aEmptySet);
+            _rEvent.State <<= aUnoStateDescription.getAsConstPropertyValueList();
         }
         else
             OAttributeDispatcher::fillFeatureEventFromAttributeState( _rEvent, _rState );

include/sfx2/sfxuno.hxx
@@ -22,6 +22,8 @@
 #include <sal/config.h>
 #include <sfx2/dllapi.h>
 
+#include <comphelper/sequenceashashmap.hxx>
+
 namespace com::sun::star::beans { struct NamedValue; }
 namespace com::sun::star::beans { struct PropertyValue; }
 namespace com::sun::star::uno { template <class E> class Sequence; }
@@ -35,10 +37,8 @@ SFX2_DLLPUBLIC void TransformParameters(            sal_uInt16                  
                                     SfxAllItemSet&                                      aSet        ,
                             const   SfxSlot*                                            pSlot = nullptr   );
 
-SFX2_DLLPUBLIC void TransformItems(         sal_uInt16                                  nSlotId     ,
-                            const   SfxItemSet&                                         aSet        ,
-                                    css::uno::Sequence< css::beans::PropertyValue >&    seqArgs     ,
-                            const   SfxSlot*                            pSlot = nullptr   );
+SFX2_DLLPUBLIC comphelper::SequenceAsHashMap
+TransformItems(sal_uInt16 nSlotId, const SfxItemSet& aSet, const SfxSlot* pSlot = nullptr);
 
 bool GetEncryptionData_Impl( const SfxItemSet* pSet, css::uno::Sequence< css::beans::NamedValue >& aEncryptionData );
 

sd/source/filter/grf/sdgrffilter.cxx
@@ -260,38 +260,16 @@ bool SdGRFFilter::Export()
                 {
                     rtl::Reference< SdGRFFilter_ImplInteractionHdl > xInteractionHandler;
 
-                    beans::PropertyValues aArgs;
-                    TransformItems( SID_SAVEASDOC, rSet, aArgs );
+                    comphelper::SequenceAsHashMap aArgs = TransformItems(SID_SAVEASDOC, rSet);
 
                     static constexpr OUString sFilterName( u"FilterName"_ustr );
                     OUString sShortName( rGraphicFilter.GetExportFormatShortName( nFilter ) );
 
-                    bool    bFilterNameFound = false;
-                    for ( auto& rArg : asNonConstRange(aArgs) )
+                    aArgs[sFilterName] <<= sShortName;
+                    if (auto xHdl = aArgs.getValue(u"InteractionHandler"_ustr).query<task::XInteractionHandler>())
                     {
-                        OUString& rStr = rArg.Name;
-                        if ( rStr == sFilterName )
-                        {
-                            bFilterNameFound = true;
-                            rArg.Value <<= sShortName;
-                        }
-                        else if ( rStr == "InteractionHandler" )
-                        {
-                            uno::Reference< task::XInteractionHandler > xHdl;
-                            if ( rArg.Value >>= xHdl )
-                            {
-                                xInteractionHandler = new SdGRFFilter_ImplInteractionHdl( xHdl );
-                                rArg.Value <<= uno::Reference< task::XInteractionHandler >(xInteractionHandler);
-                            }
-                        }
-                    }
-                    if ( !bFilterNameFound )
-                    {
-                        sal_Int32 nCount = aArgs.getLength();
-                        aArgs.realloc( nCount + 1 );
-                        auto pArgs = aArgs.getArray();
-                        pArgs[ nCount ].Name = sFilterName;
-                        pArgs[ nCount ].Value <<= sShortName;
+                        xInteractionHandler = new SdGRFFilter_ImplInteractionHdl(xHdl);
+                        aArgs[u"InteractionHandler"_ustr] <<= uno::Reference<task::XInteractionHandler>(xInteractionHandler);
                     }
 
                     // take selection if needed
@@ -310,7 +288,7 @@ bool SdGRFFilter::Export()
                         }
                     }
                     xExporter->setSourceDocument( xSource );
-                    bRet = xExporter->filter( aArgs );
+                    bRet = xExporter->filter(aArgs.getAsConstPropertyValueList());
                     if ( !bRet && xInteractionHandler.is() )
                         SdGRFFilter::HandleGraphicFilterError(
                             xInteractionHandler->GetErrorCode(),

commit 9e7598f4824badecbec5855f0998a11ca45cbefe

tdf#173160: keep the order of a macro's arguments
A macro reached through a hyperlink tells its arguments apart by their
position, so collecting the media descriptor in a hash map lost what it
needs; broken by e8d73d53073f1d953c3498bf0ee2bfef996eb4b0 (Let
TransformItems return comphelper::SequenceAsHashMap, 2025-12-03).

This replaces the type of TransformItems' return from SequenceAsHashMap
to SfxUnoArguments, a new class with similar interface, but keeping the
order of the elements in a vector.

Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
Change-Id: Ie6a737c7b085dddc3ffe2c2620bfb5fc6b8c663b
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/10057
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

engine/forms/source/richtext/parametrizedattributedispatcher.cxx
@@ -76,7 +76,7 @@ namespace frm
             return;
 
         SfxItemSet aEmptySet(getEditView()->GetEmptyItemSet());
-        comphelper::SequenceAsHashMap aUnoStateDescription;
+        SfxUnoArguments aUnoStateDescription;
         if ( _rState.getItem() )
         {
             aEmptySet.Put( *_rState.getItem() );

engine/include/sfx2/sfxuno.hxx
@@ -22,7 +22,11 @@
 #include <sal/config.h>
 #include <sfx2/dllapi.h>
 
-#include <comphelper/sequenceashashmap.hxx>
+#include <cpo/uno/Any.hxx>
+
+#include <string_view>
+#include <utility>
+#include <vector>
 
 namespace com::sun::star::beans { struct NamedValue; }
 namespace com::sun::star::beans { struct PropertyValue; }
@@ -32,12 +36,43 @@ class SfxAllItemSet ;
 class SfxItemSet    ;
 class SfxSlot       ;
 
+/// The UNO arguments that an SfxItemSet was transformed into
+/// This is similar to SequenceAsHashMap, but maintains the order of arguments
+class SFX2_DLLPUBLIC SfxUnoArguments
+{
+public:
+    /// Returns the named argument, appending it to the end when it is not there yet. Note that
+    /// the reference is invalidated on following insertions / deletions
+    cpo::uno::Any& operator[](const OUString& rName);
+
+    /// Returns the named argument, or an empty Any when there is none
+    cpo::uno::Any getValue(std::u16string_view aName) const;
+
+    template <class T>
+    T getUnpackedValueOrDefault(std::u16string_view aName, const T& rDefault) const
+    {
+        if (T aValue = T(); getValue(aName) >>= aValue)
+            return aValue;
+        return rDefault;
+    }
+
+    bool contains(std::u16string_view aName) const;
+
+    /// Drops the named argument; the remaining ones keep their relative order
+    void erase(std::u16string_view aName);
+
+    cpo::uno::Sequence<css::beans::PropertyValue> getAsConstPropertyValueList() const;
+
+private:
+    std::vector<std::pair<OUString, cpo::uno::Any>> m_aArgs;
+};
+
 SFX2_DLLPUBLIC void TransformParameters(            sal_uInt16                          nSlotId     ,
                             const   cpo::uno::Sequence< css::beans::PropertyValue >&    seqArgs     ,
                                     SfxAllItemSet&                                      aSet        ,
                             const   SfxSlot*                                            pSlot = nullptr   );
 
-SFX2_DLLPUBLIC comphelper::SequenceAsHashMap
+SFX2_DLLPUBLIC SfxUnoArguments
 TransformItems(sal_uInt16 nSlotId, const SfxItemSet& aSet, const SfxSlot* pSlot = nullptr);
 
 bool GetEncryptionData_Impl( const SfxItemSet* pSet, cpo::uno::Sequence< css::beans::NamedValue >& aEncryptionData );

engine/sd/source/filter/grf/sdgrffilter.cxx
@@ -264,7 +264,7 @@ bool SdGRFFilter::Export()
                 {
                     rtl::Reference< SdGRFFilter_ImplInteractionHdl > xInteractionHandler;
 
-                    comphelper::SequenceAsHashMap aArgs = TransformItems(SID_SAVEASDOC, rSet);
+                    SfxUnoArguments aArgs = TransformItems(SID_SAVEASDOC, rSet);
 
                     static constexpr OUString sFilterName( u"FilterName"_ustr );
                     OUString sShortName( rGraphicFilter.GetExportFormatShortName( nFilter ) );

engine/sfx2/CppunitTest_sfx2_misc.mk
@@ -25,6 +25,7 @@ $(eval $(call gb_CppunitTest_use_libraries,sfx2_misc, \
 	sal \
     subsequenttest \
 	sfx \
+	svl \
 	utl \
 	tl \
 ))

engine/sfx2/source/appl/appopen.cxx
@@ -394,7 +394,7 @@ ErrCodeMsg SfxApplication::LoadTemplate( SfxObjectShellLock& xDoc, const OUStrin
         std::unique_ptr<SfxItemSet> pNew = xDoc->GetMedium()->GetItemSet().Clone();
         pNew->ClearItem( SID_PROGRESS_STATUSBAR_CONTROL );
         pNew->ClearItem( SID_FILTER_NAME );
-        comphelper::SequenceAsHashMap aArgs = TransformItems(SID_OPENDOC, *pNew);
+        SfxUnoArguments aArgs = TransformItems(SID_OPENDOC, *pNew);
         aArgs[u"Title"_ustr] <<= xDoc->GetTitle(SFX_TITLE_DETECT);
         xModel->attachResource(OUString(), aArgs.getAsConstPropertyValueList());
     }
@@ -1066,7 +1066,7 @@ void SfxApplication::OpenDocExec_Impl( SfxRequest& rReq )
     }
 
     // convert items to properties for framework API calls
-    comphelper::SequenceAsHashMap aArgs = TransformItems(SID_OPENDOC, *rReq.GetArgs());
+    SfxUnoArguments aArgs = TransformItems(SID_OPENDOC, *rReq.GetArgs());
     // Any Referer (that was relevant in the above call to
     // SvtSecurityOptions::isSecureMacroUri) is no longer relevant, assuming
     // this "open" request is initiated directly by the user:

engine/sfx2/source/appl/appuno.cxx
@@ -30,6 +30,7 @@
 #include <sal/config.h>
 #include <sal/log.hxx>
 #include <comphelper/interaction.hxx>
+#include <comphelper/propertyvalue.hxx>
 #include <osl/diagnose.h>
 #include <svl/eitem.hxx>
 #include <svl/intitem.hxx>
@@ -49,6 +50,7 @@
 #include <com/sun/star/task/XStatusIndicator.hpp>
 #include <com/sun/star/ucb/XContent.hpp>
 
+#include <algorithm>
 #include <memory>
 
 using namespace ::com::sun::star;
@@ -910,8 +912,50 @@ void TransformParameters( sal_uInt16 nSlotId, const cpo::uno::Sequence<beans::Pr
 #endif
 }
 
-comphelper::SequenceAsHashMap TransformItems(sal_uInt16 nSlotId, const SfxItemSet& rSet,
-                                             const SfxSlot* pSlot)
+namespace
+{
+// deduces the const and non-const vector, so that operator[] gets a mutable iterator
+template <class TArgs> auto findArg(TArgs& rArgs, std::u16string_view aName)
+{
+    return std::ranges::find_if(rArgs, [aName](const auto& rArg) { return rArg.first == aName; });
+}
+}
+
+Any& SfxUnoArguments::operator[](const OUString& rName)
+{
+    if (auto it = findArg(m_aArgs, rName); it != m_aArgs.end())
+        return it->second;
+    return m_aArgs.emplace_back(rName, Any()).second;
+}
+
+Any SfxUnoArguments::getValue(std::u16string_view aName) const
+{
+    if (auto it = findArg(m_aArgs, aName); it != m_aArgs.end())
+        return it->second;
+    return {};
+}
+
+bool SfxUnoArguments::contains(std::u16string_view aName) const
+{
+    return findArg(m_aArgs, aName) != m_aArgs.end();
+}
+
+void SfxUnoArguments::erase(std::u16string_view aName)
+{
+    if (auto it = findArg(m_aArgs, aName); it != m_aArgs.end())
+        m_aArgs.erase(it);
+}
+
+Sequence<PropertyValue> SfxUnoArguments::getAsConstPropertyValueList() const
+{
+    Sequence<PropertyValue> aRet(m_aArgs.size());
+    std::ranges::transform(m_aArgs, aRet.getArray(), [](const auto& rArg) {
+        return comphelper::makePropertyValue(rArg.first, rArg.second);
+    });
+    return aRet;
+}
+
+SfxUnoArguments TransformItems(sal_uInt16 nSlotId, const SfxItemSet& rSet, const SfxSlot* pSlot)
 {
     if ( !pSlot )
         pSlot = SFX_SLOTPOOL().GetSlot( nSlotId );
@@ -1290,7 +1334,7 @@ comphelper::SequenceAsHashMap TransformItems(sal_uInt16 nSlotId, const SfxItemSe
         return {};
 
     // convert every item into a property
-    comphelper::SequenceAsHashMap aSequ;
+    SfxUnoArguments aSequ;
 
     if ( !pSlot->IsMode(SfxSlotMode::METHOD) )
     {

engine/sfx2/source/doc/docfile.cxx
@@ -85,6 +85,7 @@
 #include <comphelper/propertyvalue.hxx>
 #include <comphelper/interaction.hxx>
 #include <comphelper/sequence.hxx>
+#include <comphelper/sequenceashashmap.hxx>
 #include <comphelper/simplefileaccessinteraction.hxx>
 #include <comphelper/string.hxx>
 #include <framework/interaction.hxx>
@@ -2975,7 +2976,8 @@ void SfxMedium::GetLockingStream_Impl()
         return;
 
     // open the original document
-    comphelper::SequenceAsHashMap aMedium(TransformItems(SID_OPENDOC, GetItemSet()));
+    comphelper::SequenceAsHashMap aMedium(
+        TransformItems(SID_OPENDOC, GetItemSet()).getAsConstPropertyValueList());
 
     utl::MediaDescriptor::addInputStreamOwnLock(aMedium);
 
@@ -3059,7 +3061,8 @@ void SfxMedium::GetMedium_Impl()
         }
         else
         {
-            comphelper::SequenceAsHashMap aMedium(TransformItems(SID_OPENDOC, GetItemSet()));
+            comphelper::SequenceAsHashMap aMedium(
+                TransformItems(SID_OPENDOC, GetItemSet()).getAsConstPropertyValueList());
 
             if ( pImpl->m_xLockingStream.is() && !bFromTempFile )
             {

engine/sfx2/source/doc/guisaveas.cxx
@@ -1116,7 +1116,7 @@ bool ModelData_Impl::OutputFileDialog( sal_Int16 nStoreMode,
     m_bRecommendReadOnly = ( pRecommendReadOnly && pRecommendReadOnly->GetValue() );
     pDialogParams->ClearItem( SID_RECOMMENDREADONLY );
 
-    GetMediaDescr() = TransformItems(nSlotID, *pDialogParams);
+    GetMediaDescr() << TransformItems(nSlotID, *pDialogParams).getAsConstPropertyValueList();
 
     // get the path from the dialog
     INetURLObject aURL( pFileDlg->GetPath() );

engine/sfx2/source/doc/objmisc.cxx
@@ -1138,7 +1138,7 @@ void SfxObjectShell::InitOwnModel_Impl()
         SfxItemSet& rSet = GetMedium()->GetItemSet();
         if ( !GetMedium()->IsReadOnly() )
             rSet.ClearItem( SID_INPUTSTREAM );
-        comphelper::SequenceAsHashMap aArgs = TransformItems(SID_OPENDOC, rSet);
+        SfxUnoArguments aArgs = TransformItems(SID_OPENDOC, rSet);
         xModel->attachResource(GetMedium()->GetOrigURL(), aArgs.getAsConstPropertyValueList());
         impl_addToModelCollection(xModel);
     }

engine/sfx2/source/doc/objstor.cxx
@@ -509,7 +509,7 @@ bool SfxObjectShell::DoInitNew()
         if ( xModel.is() )
         {
             SfxItemSet &rSet = GetMedium()->GetItemSet();
-            comphelper::SequenceAsHashMap aArgs = TransformItems(SID_OPENDOC, rSet);
+            SfxUnoArguments aArgs = TransformItems(SID_OPENDOC, rSet);
             aArgs[u"Title"_ustr] <<= GetTitle(SFX_TITLE_DETECT);
             xModel->attachResource(OUString(), aArgs.getAsConstPropertyValueList());
             if (!comphelper::IsFuzzing())
@@ -2395,7 +2395,7 @@ bool SfxObjectShell::DoSaveCompleted( SfxMedium* pNewMed, bool bRegisterRecent )
             if ( xModel.is() )
             {
                 const OUString& aURL {pNewMed->GetOrigURL()};
-                comphelper::SequenceAsHashMap aMediaDescr = TransformItems(SID_OPENDOC, pNewMed->GetItemSet());
+                SfxUnoArguments aMediaDescr = TransformItems(SID_OPENDOC, pNewMed->GetItemSet());
                 try
                 {
                     xModel->attachResource(aURL, aMediaDescr.getAsConstPropertyValueList());
@@ -2554,7 +2554,7 @@ bool SfxObjectShell::ImportFrom(SfxMedium& rMedium,
             xImporter->setTargetDocument( xComp );
 
             rMedium.GetItemSet().Put( SfxStringItem( SID_FILE_NAME, rMedium.GetName() ) );
-            comphelper::SequenceAsHashMap aArgs = TransformItems(SID_OPENDOC, rMedium.GetItemSet());
+            SfxUnoArguments aArgs = TransformItems(SID_OPENDOC, rMedium.GetItemSet());
 
             static constexpr OUString sInputStream ( u"InputStream"_ustr  );
 
@@ -2711,7 +2711,7 @@ bool SfxObjectShell::ExportTo( SfxMedium& rMedium )
         SfxItemSet& rItems = rMedium.GetItemSet();
 
         // put in the REAL file name, and copy all PropertyValues
-        comphelper::SequenceAsHashMap aNewArgs = TransformItems(SID_SAVEASDOC, rItems);
+        SfxUnoArguments aNewArgs = TransformItems(SID_SAVEASDOC, rItems);
         const comphelper::SequenceAsHashMap& aMediumArgs(rMedium.GetArgs());
 
         if (aNewArgs.contains(u"FileName"_ustr))

engine/sfx2/source/doc/objxtor.cxx
@@ -1049,7 +1049,7 @@ SfxObjectShell* SfxObjectShell::CreateObject( const OUString& rServiceName, SfxO
 
 Reference<lang::XComponent> SfxObjectShell::CreateAndLoadComponent( const SfxItemSet& rSet )
 {
-    comphelper::SequenceAsHashMap aProps = TransformItems(SID_OPENDOC, rSet);
+    SfxUnoArguments aProps = TransformItems(SID_OPENDOC, rSet);
     const SfxStringItem* pFileNameItem = rSet.GetItem<SfxStringItem>(SID_FILE_NAME, false);
     const SfxStringItem* pTargetItem = rSet.GetItem<SfxStringItem>(SID_TARGETNAME, false);
     OUString aURL;

engine/sfx2/source/doc/sfxbasemodel.cxx
@@ -1025,9 +1025,9 @@ Sequence< beans::PropertyValue > SAL_CALL SfxBaseModel::getArgs2(const Sequence<
         // hopefully it is a temporary solution, I guess nonconvertable properties
         // should not be supported so then there will be only ItemSet from medium
 
-        comphelper::SequenceAsHashMap seqArgsNew = TransformItems(SID_OPENDOC, m_pData->m_pObjectShell->GetMedium()->GetItemSet());
+        SfxUnoArguments seqArgsNew = TransformItems(SID_OPENDOC, m_pData->m_pObjectShell->GetMedium()->GetItemSet());
         TransformParameters( SID_OPENDOC, m_pData->m_seqArguments, aSet );
-        comphelper::SequenceAsHashMap seqArgsOld = TransformItems(SID_OPENDOC, aSet);
+        SfxUnoArguments seqArgsOld = TransformItems(SID_OPENDOC, aSet);
 
         if (requestedArgs.empty() || requestedArgs.count(u"WinExtent"))
         {
@@ -1785,7 +1785,7 @@ void SAL_CALL SfxBaseModel::storeAsURL( const   OUString&                   rURL
         impl_store(rURL, rArgs, false);
     }
 
-    comphelper::SequenceAsHashMap aSequence = TransformItems(SID_OPENDOC, m_pData->m_pObjectShell->GetMedium()->GetItemSet());
+    SfxUnoArguments aSequence = TransformItems(SID_OPENDOC, m_pData->m_pObjectShell->GetMedium()->GetItemSet());
     attachResource(rURL, aSequence.getAsConstPropertyValueList());
 
     loadCmisProperties( );
@@ -2668,7 +2668,7 @@ void SAL_CALL SfxBaseModel::checkOut(  )
         m_pData->m_pObjectShell->GetMedium( )->SetName( sURL );
         m_pData->m_pObjectShell->GetMedium( )->GetMedium_Impl( );
         m_pData->m_xDocumentProperties->setTitle( getTitle( ) );
-        comphelper::SequenceAsHashMap aSequence = TransformItems(SID_OPENDOC, pMedium->GetItemSet());
+        SfxUnoArguments aSequence = TransformItems(SID_OPENDOC, pMedium->GetItemSet());
         attachResource(sURL, aSequence.getAsConstPropertyValueList());
 
         // Reload the CMIS properties
@@ -2733,7 +2733,7 @@ void SAL_CALL SfxBaseModel::checkIn( bool bIsMajor, const OUString& rMessage )
         if ( sName != sNewName )
         {
             m_pData->m_xDocumentProperties->setTitle( getTitle( ) );
-            comphelper::SequenceAsHashMap aSequence = TransformItems(SID_OPENDOC, pMedium->GetItemSet());
+            SfxUnoArguments aSequence = TransformItems(SID_OPENDOC, pMedium->GetItemSet());
             attachResource(sNewName, aSequence.getAsConstPropertyValueList());
 
             // Reload the CMIS properties

engine/sfx2/source/view/viewfrm.cxx
@@ -815,7 +815,7 @@ void SfxViewFrame::ExecReload_Impl( SfxRequest& rReq )
                 if ( xOldObj->IsModifyPasswordEntered() )
                     xNewObj->SetModifyPasswordEntered();
 
-                comphelper::SequenceAsHashMap aLoadArgs = TransformItems(SID_OPENDOC, *pNewSet);
+                SfxUnoArguments aLoadArgs = TransformItems(SID_OPENDOC, *pNewSet);
                 try
                 {
                     uno::Reference < frame::XLoadable > xLoad( xNewObj->GetModel(), uno::UNO_QUERY );

engine/sfx2/source/view/viewprn.cxx
@@ -697,7 +697,7 @@ void SfxViewShell::ExecPrint_Impl( SfxRequest &rReq )
             if ( pSelectItem && rReq.GetArgs()->Count() == 1 )
                 bIsAPI = false;
 
-            comphelper::SequenceAsHashMap aProps;
+            SfxUnoArguments aProps;
             if ( bIsAPI )
             {
                 // supported properties:
@@ -716,12 +716,12 @@ void SfxViewShell::ExecPrint_Impl( SfxRequest &rReq )
 
                 if (aProps.contains(u"Copies"_ustr))
                 {
-                    aProps[u"CopyCount"_ustr] = aProps[u"Copies"_ustr];
+                    aProps[u"CopyCount"_ustr] = aProps.getValue(u"Copies"_ustr);
                 }
 
                 if (aProps.contains(u"RangeText"_ustr))
                 {
-                    aProps[u"Pages"_ustr] = aProps[u"RangeText"_ustr];
+                    aProps[u"Pages"_ustr] = aProps.getValue(u"RangeText"_ustr);
                 }
 
                 if (aProps.contains(u"Asynchron"_ustr))

engine/svx/source/form/fmtextcontrolshell.cxx
@@ -850,7 +850,7 @@ using namespace cpo::uno;
                     }
                 }
 
-                comphelper::SequenceAsHashMap aArguments = TransformItems(nSlot, aToggled);
+                SfxUnoArguments aArguments = TransformItems(nSlot, aToggled);
                 aFeaturePos->second->dispatch(aArguments.getAsConstPropertyValueList());
             }
             break;

engine/sc/qa/extras/macros-test.cxx
@@ -13,6 +13,12 @@
 
 #include <helper/qahelper.hxx>
 #include <sal/log.hxx>
+#include <sfx2/dispatch.hxx>
+#include <sfx2/frame.hxx>
+#include <sfx2/sfxsids.hrc>
+#include <sfx2/viewfrm.hxx>
+#include <svl/eitem.hxx>
+#include <svl/stritem.hxx>
 #include <svx/svdpage.hxx>
 #include <comphelper/processfactory.hxx>
 #include <comphelper/propertyvalue.hxx>
@@ -21,6 +27,7 @@
 
 #include <conditio.hxx>
 #include <document.hxx>
+#include <tabvwsh.hxx>
 #include <scitems.hxx>
 
 #include <algorithm>
@@ -34,6 +41,7 @@
 #include <com/sun/star/table/XColumnRowRange.hpp>
 
 #include <com/sun/star/container/XNameContainer.hpp>
+#include <com/sun/star/document/MacroExecMode.hpp>
 #include <com/sun/star/document/XEmbeddedScripts.hpp>
 #include <com/sun/star/drawing/XDrawPageSupplier.hpp>
 #include <com/sun/star/script/XLibraryContainer.hpp>
@@ -1177,6 +1185,41 @@ CPPUNIT_TEST_FIXTURE(ScMacrosTest, testVbaOnTime)
 
     // A1 has the expected number now:
     CPPUNIT_ASSERT_EQUAL(42.0, pDoc->GetValue(0, 0, 0));
+}
+
+CPPUNIT_TEST_FIXTURE(ScMacrosTest, testTdf173160)
+{
+    // A macro reached through a hyperlink is handed the media descriptor as positional arguments,
+    // with the URL first; the dispatch drops the argument names, so the order is all it has
+    loadWithParams(
+        createFileURL(u"tdf173160.ods"),
+        { comphelper::makePropertyValue(u"MacroExecutionMode"_ustr,
+                                        document::MacroExecMode::ALWAYS_EXECUTE_NO_WARN) });
+
+    static constexpr OUString aScriptUrl
+        = u"vnd.sun.star.script:Standard.Module1.Main1?language=Basic&location=document"_ustr;
+
+    // the items ScGlobal::OpenURL builds for a Ctrl+click on a cell hyperlink
+    SfxViewFrame& rViewFrame = getViewShell()->GetViewFrame();
+    SfxStringItem aUrl(SID_FILE_NAME, aScriptUrl);
+    SfxStringItem aTarget(SID_TARGETNAME, u""_ustr);
+    SfxFrameItem aFrame(SID_DOCFRAME, &rViewFrame);
+    SfxStringItem aReferer(SID_REFERER, u"private:user"_ustr);
+    SfxBoolItem aNewView(SID_OPEN_NEW_VIEW, false);
+    SfxBoolItem aBrowsing(SID_BROWSE, true);
+    // asynchronous like the hyperlink itself: a script URL can only be dispatched that way,
+    // because ScriptProtocolHandler has no XSynchronousDispatch
+    rViewFrame.GetDispatcher()->ExecuteList(
+        SID_OPENDOC, SfxCallMode::ASYNCHRON,
+        { &aUrl, &aTarget, &aFrame, &aReferer, &aNewView, &aBrowsing });
+    Scheduler::ProcessEventsToIdle();
+
+    // the macro wrote down what it got: A1 the first argument, B1 how many there were
+    ScDocument* pDoc = getScDoc();
+    // Without the accompanying fix in place, this test would have failed: the arguments reached
+    // the macro in the order their names happened to hash, so the first one was not the URL
+    CPPUNIT_ASSERT_EQUAL(aScriptUrl, pDoc->GetString(0, 0, 0));
+    CPPUNIT_ASSERT_EQUAL(6.0, pDoc->GetValue(1, 0, 0));
 }
 
 ScMacrosTest::ScMacrosTest()

engine/sfx2/qa/cppunit/test_misc.cxx
@@ -42,6 +42,12 @@
 #include <comphelper/propertyvalue.hxx>
 #include <comphelper/processfactory.hxx>
 #include <sfx2/app.hxx>
+#include <sfx2/sfxsids.hrc>
+#include <sfx2/sfxuno.hxx>
+#include <svl/eitem.hxx>
+#include <svl/intitem.hxx>
+#include <svl/itemset.hxx>
+#include <svl/stritem.hxx>
 #include <osl/file.hxx>
 
 #include <libxml/xpathInternals.h>
@@ -628,6 +634,44 @@ CPPUNIT_TEST_FIXTURE(MiscTest, testOverwrite)
     catch (const cpo::uno::Exception&)
     {
     }
+}
+
+CPPUNIT_TEST_FIXTURE(MiscTest, testTransformItemsOrder)
+{
+    // tdf#173160: a macro invoked through a hyperlink receives the media descriptor entries as
+    // positional arguments, so the order they are filled in must be preserved
+    // First, create a document to bring in SfxApplication:
+    loadFromURL(u"private:factory/swriter"_ustr);
+
+    SfxAllItemSet aSet(SfxGetpApp()->GetPool());
+    aSet.Put(SfxStringItem(SID_FILE_NAME, u"foo:bar"_ustr));
+    aSet.Put(SfxStringItem(SID_REFERER, u"private:user"_ustr));
+    aSet.Put(SfxStringItem(SID_TARGETNAME, u"_self"_ustr));
+    aSet.Put(SfxBoolItem(SID_OPEN_NEW_VIEW, false));
+    aSet.Put(SfxUInt16Item(SID_MACROEXECMODE, 3));
+    aSet.Put(SfxUInt16Item(SID_UPDATEDOCMODE, 2));
+
+    SfxUnoArguments aArgs = TransformItems(SID_OPENDOC, aSet);
+    auto assertArgumentNames = [&aArgs](std::initializer_list<OUString> aExpected)
+    {
+        CPPUNIT_ASSERT_MESSAGE("Lengths not equal",
+                               std::ranges::equal(aExpected, aArgs.getAsConstPropertyValueList(),
+                                                  [](auto& rExpected, auto& rActual)
+                                                  {
+                                                      CPPUNIT_ASSERT_EQUAL(rExpected, rActual.Name);
+                                                      return true;
+                                                  }));
+    };
+
+    // Without the accompanying fix in place, this test would have failed: the formal arguments no
+    // longer came first, so a hyperlink macro got something else than the URL as its first argument
+    assertArgumentNames({ u"URL"_ustr, u"Referer"_ustr, u"OpenNewView"_ustr, u"FrameName"_ustr,
+                          u"MacroExecutionMode"_ustr, u"UpdateDocMode"_ustr });
+
+    // this is what SfxApplication::OpenDocExec_Impl hands to the dispatch
+    aArgs.erase(u"Referer"_ustr);
+    assertArgumentNames({ u"URL"_ustr, u"OpenNewView"_ustr, u"FrameName"_ustr,
+                          u"MacroExecutionMode"_ustr, u"UpdateDocMode"_ustr });
 }
 }
 