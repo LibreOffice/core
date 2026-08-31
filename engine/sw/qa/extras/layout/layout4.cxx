LibreOffice/core

commit c012c74e69360dd78890c1d935b802c0baf79da2

tdf#146081 sw: Fix inconsistent prop line spacing
Previously, when using proportional line spacing <100%, Writer applied
inconsistent height and ascent adjustments to the first and non-first
lines of paragraphs. This caused Writer to render text at inconsistent
vertical offsets.

Height and ascent adjustments are now consistent.

Change-Id: I303d6aa379007f3fa3e8c1ebb981ef205864708a
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/170307
Reviewed-by: Jonathan Clark <jonathan@libreoffice.org>
Tested-by: Jenkins

sw/source/core/text/itrform2.cxx
@@ -2333,6 +2333,8 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                         if( nTmp < 50 )
                             nTmp = nTmp ? 50 : 100;
 
+                        bool bPropLineShrinks = (nTmp < 100);
+
                         // extend line height by (nPropLineSpace - 100) percent of the font height
                         nTmp -= 100;
                         nTmp *= m_pCurr->GetTextHeight();
@@ -2341,6 +2343,19 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                         if (nTmp < 1)
                             nTmp = 1;
                         nLineHeight = nTmp;
+
+                        // tdf#146081: The height and ascent of the first line may have been
+                        // adjusted above. In order to have consistent line spacing when rendering,
+                        // the same adjustments must be made to the following lines.
+                        if (bPropLineShrinks
+                            && GetTextFrame()->GetDoc().getIDocumentSettingAccess().get(
+                                DocumentSettingId::PROP_LINE_SPACING_SHRINKS_FIRST_LINE))
+                        {
+                            SwTwips nAsc = (4 * nLineHeight) / 5; // 80%
+                            m_pCurr->SetAscent(nAsc);
+                            m_pCurr->Height(nLineHeight, false);
+                            m_pInf->GetParaPortion()->SetFixLineHeight();
+                        }
                         break;
                     }
                     case SvxInterLineSpaceRule::Fix:

sw/qa/extras/layout/data/tdf146081-prop-spacing.fodt
@@ -1,0 +1,112 @@
+<?xml version='1.0' encoding='UTF-8'?>
+<office:document xmlns:css3t="http://www.w3.org/TR/css3-text/" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:config="urn:oasis:names:tc:opendocument:xmlns:config:1.0" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:drawooo="http://openoffice.org/2010/draw" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:calcext="urn:org:documentfoundation:names:experimental:calc:xmlns:calcext:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:rpt="http://openoffice.org/2005/report" xmlns:formx="urn:openoffice:names:experimental:ooxml-odf-interop:xmlns:form:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:officeooo="http://openoffice.org/2009/office" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:loext="urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0" office:version="1.3" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:meta><meta:creation-date>2024-07-10T03:37:36.946853251</meta:creation-date><dc:date>2024-07-10T03:41:48.895381288</dc:date><meta:editing-duration>PT4M13S</meta:editing-duration><meta:editing-cycles>1</meta:editing-cycles><meta:document-statistic meta:table-count="0" meta:image-count="0" meta:object-count="0" meta:page-count="1" meta:paragraph-count="1" meta:word-count="1" meta:character-count="39" meta:non-whitespace-character-count="39"/><meta:generator>LibreOfficeDev/25.2.0.0.alpha0$Linux_X86_64 LibreOffice_project/c11c5f9b87dfb7a7620341c2bffbd73b4d8eca58</meta:generator></office:meta>
+ <office:font-face-decls>
+  <style:font-face style:name="Liberation Serif" svg:font-family="'Liberation Serif'" style:font-family-generic="roman" style:font-pitch="variable"/>
+  <style:font-face style:name="Noto Serif CJK SC" svg:font-family="'Noto Serif CJK SC'" style:font-family-generic="system" style:font-pitch="variable"/>
+  <style:font-face style:name="Tahoma1" svg:font-family="Tahoma" style:font-family-generic="system" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="graphic">
+   <style:graphic-properties svg:stroke-color="#3465a4" draw:fill-color="#729fcf" fo:wrap-option="no-wrap" draw:shadow-offset-x="8.5pt" draw:shadow-offset-y="8.5pt" draw:start-line-spacing-horizontal="8.02pt" draw:start-line-spacing-vertical="8.02pt" draw:end-line-spacing-horizontal="8.02pt" draw:end-line-spacing-vertical="8.02pt" style:flow-with-text="false"/>
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:line-break="strict" loext:tab-stop-distance="0pt" style:writing-mode="lr-tb" style:font-independent-line-spacing="false">
+    <style:tab-stops/>
+   </style:paragraph-properties>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="Noto Serif CJK SC" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Tahoma1" style:font-size-complex="12pt" style:language-complex="ar" style:country-complex="SA"/>
+  </style:default-style>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties fo:orphans="2" fo:widows="2" fo:hyphenation-ladder-count="no-limit" fo:hyphenation-keep="auto" loext:hyphenation-keep-type="column" style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:tab-stop-distance="35.46pt" style:writing-mode="page"/>
+   <style:text-properties style:use-window-font-color="true" loext:opacity="0%" style:font-name="Liberation Serif" fo:font-size="12pt" fo:language="en" fo:country="US" style:letter-kerning="true" style:font-name-asian="Noto Serif CJK SC" style:font-size-asian="10.5pt" style:language-asian="zh" style:country-asian="CN" style:font-name-complex="Tahoma1" style:font-size-complex="12pt" style:language-complex="ar" style:country-complex="SA" fo:hyphenate="false" fo:hyphenation-remain-char-count="2" fo:hyphenation-push-char-count="2" loext:hyphenation-no-caps="false" loext:hyphenation-no-last-word="false" loext:hyphenation-word-char-count="5" loext:hyphenation-zone="no-limit"/>
+  </style:default-style>
+  <style:default-style style:family="table">
+   <style:table-properties table:border-model="collapsing"/>
+  </style:default-style>
+  <style:default-style style:family="table-row">
+   <style:table-row-properties fo:keep-together="auto"/>
+  </style:default-style>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+  <text:outline-style style:name="Outline">
+   <text:outline-level-style text:level="1" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="2" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="3" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="4" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="5" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="6" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="7" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="8" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="9" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+   <text:outline-level-style text:level="10" style:num-format="">
+    <style:list-level-properties text:list-level-position-and-space-mode="label-alignment">
+     <style:list-level-label-alignment text:label-followed-by="listtab"/>
+    </style:list-level-properties>
+   </text:outline-level-style>
+  </text:outline-style>
+  <text:notes-configuration text:note-class="footnote" style:num-format="1" text:start-value="0" text:footnotes-position="page" text:start-numbering-at="document"/>
+  <text:notes-configuration text:note-class="endnote" style:num-format="i" text:start-value="0"/>
+  <text:linenumbering-configuration text:number-lines="false" text:offset="14.14pt" style:num-format="1" text:number-position="left" text:increment="5"/>
+  </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="P1" style:family="paragraph" style:parent-style-name="Standard">
+   <style:paragraph-properties fo:line-height="60%"/>
+   <style:text-properties fo:font-size="60pt" style:font-size-asian="60pt" style:font-size-complex="60pt"/>
+  </style:style>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="595.3pt" fo:page-height="841.89pt" style:num-format="1" style:print-orientation="portrait" fo:margin-top="56.69pt" fo:margin-bottom="56.69pt" fo:margin-left="56.69pt" fo:margin-right="56.69pt" style:writing-mode="lr-tb" style:footnote-max-height="0pt" loext:margin-gutter="0pt">
+    <style:footnote-sep style:width="0.51pt" style:distance-before-sep="2.86pt" style:distance-after-sep="2.86pt" style:line-style="solid" style:adjustment="left" style:rel-width="25%" style:color="#000000"/>
+   </style:page-layout-properties>
+   <style:header-style/>
+   <style:footer-style/>
+  </style:page-layout>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <text:sequence-decls>
+    <text:sequence-decl text:display-outline-level="0" text:name="Illustration"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Table"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Text"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Drawing"/>
+    <text:sequence-decl text:display-outline-level="0" text:name="Figure"/>
+   </text:sequence-decls>
+   <text:p text:style-name="P1">AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout3.cxx
@@ -3081,6 +3081,38 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf56408NoUnderflow)
                 u"עברית English"_ustr);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter3, TestTdf146081)
+{
+    // Verifies that proportional line spacing is consistent with the
+    // PropLineSpacingShrinksFirstLine compatibility flag set
+    createSwDoc("tdf146081-prop-spacing.fodt");
+    auto pXmlDoc = parseLayoutDump();
+
+    SwTwips nTotalHeight
+        = getXPath(pXmlDoc, "/root/page/body/txt/infos/bounds"_ostr, "height"_ostr).toInt32();
+
+    SwTwips nHeight1
+        = getXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[1]"_ostr, "height"_ostr)
+              .toInt32();
+    SwTwips nHeight2
+        = getXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[2]"_ostr, "height"_ostr)
+              .toInt32();
+    SwTwips nHeight3
+        = getXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[3]"_ostr, "height"_ostr)
+              .toInt32();
+    SwTwips nHeight4
+        = getXPath(pXmlDoc, "/root/page/body/txt/SwParaPortion/SwLineLayout[4]"_ostr, "height"_ostr)
+              .toInt32();
+
+    // All of the lines must have the same height
+    CPPUNIT_ASSERT_EQUAL(nHeight1, nHeight2);
+    CPPUNIT_ASSERT_EQUAL(nHeight1, nHeight3);
+    CPPUNIT_ASSERT_EQUAL(nHeight1, nHeight4);
+
+    // The total height of the paragraph must be equal to the sum of all lines
+    CPPUNIT_ASSERT_EQUAL(nTotalHeight, nHeight1 * 4);
+}
+
 } // end of anonymous namespace
 
 CPPUNIT_PLUGIN_IMPLEMENT();

commit 3c8abe03fb343ae612b7196f115ecb1acebb6ba2

tdf#173259 sw: shrink a line by its text height, off the top
Proportional spacing below 100% cut the whole line height and forced the
ascent to 80%, so a tall as-character object was scaled down and lifted
out of its line. The cut now comes off the text height, like Word.

Regression from commit c012c74e69360dd78890c1d935b802c0baf79da2
(tdf#146081 sw: Fix inconsistent prop line spacing, 2024-07-10) on a
paragraph's following lines; its first line was already wrong.

Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
Change-Id: I3c89530be1f23ed11a936806a724ba5178a83e95
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/10287
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

engine/sw/source/core/text/itrform2.cxx
@@ -19,6 +19,7 @@
 
 #include <hintids.hxx>
 
+#include <algorithm>
 #include <memory>
 #include <com/sun/star/i18n/ScriptType.hpp>
 #include <editeng/lspcitem.hxx>
@@ -90,6 +91,18 @@ static void ClearFly( SwTextFormatInfo &rInf )
 {
     delete rInf.GetFly();
     rInf.SetFly(nullptr);
+}
+
+/// Is an as-character portion taller than the text and at least as tall as the line's ascent?
+static bool IsFlyCntOverText(const SwLineLayout& rLine, SwTwips nTextHeight)
+{
+    SwTwips nFlyCntHeight = 0;
+    for (const SwLinePortion* pPor = rLine.GetNextPortion(); pPor; pPor = pPor->GetNextPortion())
+    {
+        if (pPor->IsFlyCntPortion() && nFlyCntHeight < pPor->Height())
+            nFlyCntHeight = pPor->Height();
+    }
+    return nFlyCntHeight > nTextHeight && nFlyCntHeight >= rLine.GetAscent();
 }
 
 void SwTextFormatter::CtorInitTextFormatter( SwTextFrame *pNewFrame, SwTextFormatInfo *pNewInf )
@@ -2353,6 +2366,12 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
         const SvxLineSpacingItem *pSpace = m_aLineInf.GetLineSpacing();
         if( pSpace )
         {
+            // An as-character object does not take part in the line spacing calculation, so
+            // proportional spacing below 100% cuts into the text height, off the top of the line.
+            const SwTwips nSpacingBaseHeight = m_pCurr->GetLineSpacingBaseHeight()
+                                                   ? m_pCurr->GetLineSpacingBaseHeight()
+                                                   : m_pCurr->Height();
+
             switch( pSpace->GetLineSpaceRule() )
             {
                 case SvxLineSpaceRule::Auto:
@@ -2366,12 +2385,27 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                         if( nTmp < 50 )
                             nTmp = nTmp ? 50 : 100;
                         if (nTmp<100) { // code adapted from fixed line height
-                            nTmp *= nLineHeight;
-                            nTmp /= 100;
-                            if( !nTmp )
-                                ++nTmp;
-                            nLineHeight = nTmp;
-                            SwTwips nAsc = (4 * nLineHeight) / 5; // 80%
+                            SwTwips nAsc;
+                            if (IsFlyCntOverText(*m_pCurr, nSpacingBaseHeight))
+                            {
+                                SwTwips nShrunk = (nTmp * nSpacingBaseHeight) / 100;
+                                if (!nShrunk)
+                                    ++nShrunk;
+                                // the cut cannot exceed the ascent
+                                const SwTwips nCut
+                                    = std::min(nSpacingBaseHeight - nShrunk, m_pCurr->GetAscent());
+                                nLineHeight = std::max(nLineHeight - nCut, SwTwips(1));
+                                nAsc = m_pCurr->GetAscent() - nCut;
+                            }
+                            else
+                            {
+                                nTmp *= nLineHeight;
+                                nTmp /= 100;
+                                if (!nTmp)
+                                    ++nTmp;
+                                nLineHeight = nTmp;
+                                nAsc = (4 * nLineHeight) / 5; // 80%
+                            }
 #if 0
                             // could do clipping here (like Word does)
                             // but at 0.5 its unreadable either way...
@@ -2454,6 +2488,12 @@ void SwTextFormatter::CalcRealHeight( bool bNewLine )
                                 DocumentSettingId::PROP_LINE_SPACING_SHRINKS_FIRST_LINE))
                         {
                             SwTwips nAsc = (4 * nLineHeight) / 5; // 80%
+                            if (IsFlyCntOverText(*m_pCurr, nSpacingBaseHeight))
+                            {
+                                const SwTwips nCut
+                                    = std::max(m_pCurr->Height() - nLineHeight, SwTwips(0));
+                                nAsc = std::max(m_pCurr->GetAscent() - nCut, SwTwips(0));
+                            }
                             m_pCurr->SetAscent(nAsc);
                             m_pCurr->Height(nLineHeight, false);
                             m_pInf->GetParaPortion()->SetFixLineHeight();

engine/sw/qa/extras/layout/layout4.cxx
@@ -1728,6 +1728,40 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter4, TestTdf146081)
     CPPUNIT_ASSERT_EQUAL(nTotalHeight, nHeight1 * 4);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter4, TestTdf173259)
+{
+    // Three paragraphs with 50% proportional line spacing, each holding an as-character picture
+    // 3766twip tall: the first fills the text width and so lands on a second line, the second is
+    // alone on its line, the third is narrow enough to share the line with the text before it.
+    createSwDoc("tdf173259-prop-spacing-image.docx");
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+
+    // The cut comes off the text height and off the top, so the picture keeps its lower edge.
+    auto sSecond = "/root/page/body/txt[2]"_ostr;
+    assertXPath(pXmlDoc, sSecond + "/SwParaPortion/SwLineLayout[2]", "height", u"3628");
+    OUString aParaBottom = getXPath(pXmlDoc, sSecond + "/infos/bounds", "bottom");
+    OUString aPictureBottom = getXPath(pXmlDoc, sSecond + "/anchored/fly/infos/bounds", "bottom");
+    // without the fix their gap was 726twip
+    CPPUNIT_ASSERT_EQUAL(aParaBottom, aPictureBottom);
+
+    auto sThird = "/root/page/body/txt[3]"_ostr;
+    // without the fix the line was 1883, half the height of the picture
+    assertXPath(pXmlDoc, sThird + "/SwParaPortion/SwLineLayout[1]", "height", u"3628");
+    aParaBottom = getXPath(pXmlDoc, sThird + "/infos/bounds", "bottom");
+    aPictureBottom = getXPath(pXmlDoc, sThird + "/anchored/fly/infos/bounds", "bottom");
+    // without the fix their gap was 377twip
+    CPPUNIT_ASSERT_EQUAL(aParaBottom, aPictureBottom);
+
+    // Sharing the line with text leaves the text descent below the picture
+    auto sFourth = "/root/page/body/txt[4]"_ostr;
+    // without the fix the line was 1909
+    assertXPath(pXmlDoc, sFourth + "/SwParaPortion/SwLineLayout[1]", "height", u"3680");
+    aParaBottom = getXPath(pXmlDoc, sFourth + "/infos/bounds", "bottom");
+    aPictureBottom = getXPath(pXmlDoc, sFourth + "/anchored/fly/infos/bounds", "bottom");
+    // without the fix this gap was 382twip
+    CPPUNIT_ASSERT_EQUAL(sal_Int32(52), aParaBottom.toInt32() - aPictureBottom.toInt32());
+}
+
 CPPUNIT_TEST_FIXTURE(SwLayoutWriter4, TestTdf155229RowAtLeast)
 {
     createSwDoc("tdf155229_row_height_at_least.docx");