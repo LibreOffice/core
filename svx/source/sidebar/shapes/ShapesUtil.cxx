d5aa9ad2d0508eddd1582b7107f10c27f7b83209
Henry Castro <henry.castro@collabora.com>
2026-09-07 17:44:38 +0000

    engine: svx: name the sinusoid by the command that exists
    The Shapes deck asked its basic shapes gallery for
    .uno:SymbolShapes.sinusoid, and no such command is defined: the one the
    registry carries is .uno:BasicShapes.sinusoid, labelled "Sinusoid". So that
    entry had no label, no tooltip and therefore no accessible name at all - a
    shape a screen reader could reach and not announce.
    
    The sidebar validator reported it eleven times, once per ancestor as it
    recursed, which is what made a single nameless shape read like a broken panel
    and take the sweep down with it. It now says the sidebar passed all checks.
    
    Change-Id: I4cef17cd0d69ea291e4ddefe85339cadb6b245bd
    Signed-off-by: Henry Castro <henry.castro@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/11770
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    (cherry picked from commit f01368af956ee7f556c4a14e9b711322a9a2a5e1)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210952
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

--- svx/source/sidebar/shapes/ShapesUtil.cxx
+++ svx/source/sidebar/shapes/ShapesUtil.cxx
@@ -88,7 +88,7 @@ SvxShapeCommandsMap::SvxShapeCommandsMap()
         {21, ".uno:BasicShapes.paper"},
         {22, ".uno:BasicShapes.frame"},
         {23, ".uno:BasicShapes.ring"},
-        {24, ".uno:SymbolShapes.sinusoid"}
+        {24, ".uno:BasicShapes.sinusoid"}
     };
 
     mpSymbolShapes = decltype(mpSymbolShapes){
