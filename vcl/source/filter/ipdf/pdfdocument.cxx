7558a892f696fa46a1bc776f7bfab690411d49e8
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-09-09 07:52:10 +0000

    pdf import: skip only one end of line after the stream keyword
    every following newline was skipped, so stream data starting with
    0x0a or 0x0d lost a byte and an encrypted stream decrypted to the
    wrong bytes
    
    CppunitTest_vcl_pdf_encryption failed at random in CI with:
    
     PDFEncryptionTest.cxx:362:Assertion
     Test name: (anonymous namespace)::testEmbeddedDocumentOfEncryptedHybridFile::TestBody
     equality assertion failed
     - Expected: 0
     - Actual  : -38
    
     Failures !!!
     Run: 16   Failure total: 1   Failures: 1   Errors: 0
    
    since:
    
    commit b6f98b71155d3c7af70bfc623cfaa7da0fbb905f
    Date:   Wed Oct 26 17:55:02 2016 +0200
    
        xmlsecurity PDF verify: support SHA-256
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I72e4e72506942ed0151e5d96a1abc94cce1a059f
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/11825
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    (cherry picked from commit b42ffcd8af9427a0ecdfe917d897db98d4f00b6f)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210954
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Tested-by: Jenkins

--- vcl/source/filter/ipdf/pdfdocument.cxx
+++ vcl/source/filter/ipdf/pdfdocument.cxx
@@ -1258,7 +1258,7 @@ bool PDFDocument::Tokenize(SvStream& rStream, TokenizeMode eMode,
                             return false;
                         }
 
-                        PDFDocument::SkipLineBreaks(rStream);
+                        PDFDocument::SkipEndOfLine(rStream);
                         auto pStreamElement = new PDFStreamElement(nLength);
                         if (pObject)
                             pObject->SetStream(pStreamElement);
@@ -1913,22 +1913,26 @@ void PDFDocument::SkipWhitespace(SvStream& rStream)
     }
 }
 
-void PDFDocument::SkipLineBreaks(SvStream& rStream)
+void PDFDocument::SkipEndOfLine(SvStream& rStream)
 {
     char ch = 0;
 
-    while (true)
+    rStream.ReadChar(ch);
+    if (rStream.eof())
+        return;
+
+    if (ch == '\r')
     {
         rStream.ReadChar(ch);
         if (rStream.eof())
-            break;
-
-        if (ch != '\n' && ch != '\r')
-        {
-            rStream.SeekRel(-1);
             return;
-        }
+        if (ch != '\n')
+            rStream.SeekRel(-1);
+        return;
     }
+
+    if (ch != '\n')
+        rStream.SeekRel(-1);
 }
 
 size_t PDFDocument::GetObjectOffset(size_t nIndex) const
