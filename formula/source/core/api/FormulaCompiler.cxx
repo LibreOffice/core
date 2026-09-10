16314d50bc8605fdd889a98b3a6ed494a1753aef
Aron Budea <aron.budea@collabora.com>
2026-09-11 00:57:27 +0930

    sc: Use more is(Unary|Binary)OperatorOpCode()
    Change-Id: I32805328640297cd14efe798f5f8a6cfa712090b
    Signed-off-by: Aron Budea <aron.budea@collabora.com>
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/12173
    Reviewed-by: Noel Grandin <noel.grandin@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    (cherry picked from commit 342fbc6b53ede419b24f69e4b4e16f980b2edb27)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210926
    Tested-by: Jenkins
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>

--- formula/source/core/api/FormulaCompiler.cxx
+++ formula/source/core/api/FormulaCompiler.cxx
@@ -65,7 +65,7 @@ class FormulaCompilerRecursionGuard
 // comes before it, so a binary operator may follow it.
 bool isOperatorExpectingOperand(OpCode eOp)
 {
-    return ocStartBinaryOperators <= eOp && eOp < ocStopUnaryOperators
+    return (isBinaryOperatorOpCode(eOp) || isUnaryOperatorOpCode(eOp))
            && eOp != ocSpill;
 }
 
@@ -2589,8 +2589,7 @@ void FormulaCompiler::UnaryLine()
 {
     if( mpToken->GetOpCode() == ocAdd )
         GetToken();
-    else if (ocStartUnaryOperators <= mpToken->GetOpCode() &&
-            mpToken->GetOpCode() < ocStopUnaryOperators)
+    else if (isUnaryOperatorOpCode(mpToken->GetOpCode()))
     {
         FormulaTokenRef p = mpToken;
         NextToken();
@@ -3370,8 +3369,7 @@ OpCode FormulaCompiler::NextToken()
     {
         // Before an operator there must not be another operator, with the
         // exception of AND and OR.
-        if ( eOp != ocAnd && eOp != ocOr &&
-                (ocStartBinaryOperators <= eOp && eOp < ocStopBinaryOperators )
+        if ( eOp != ocAnd && eOp != ocOr && isBinaryOperatorOpCode(eOp)
                 && (meLastOp == ocOpen || meLastOp == ocSep ||
                     isOperatorExpectingOperand(meLastOp)))
         {
@@ -3629,7 +3627,7 @@ void FormulaCompiler::ForceArrayOperator( FormulaTokenRef const & rCurr )
             // below.
             rCurr->SetInForceArray( ParamClass::ForceArray);
         }
-        else if (mnPC >= 2 && ocStartBinaryOperators <= eOp && eOp < ocStopBinaryOperators)
+        else if (mnPC >= 2 && isBinaryOperatorOpCode(eOp))
         {
             // Binary operators are not functions followed by arguments
             // and need some peeking into RPN to inspect their operands.
@@ -3656,7 +3654,7 @@ void FormulaCompiler::ForceArrayOperator( FormulaTokenRef const & rCurr )
                 rCurr->SetInForceArray( eArrayReturn);
             }
         }
-        else if (mnPC >= 1 && ocStartUnaryOperators <= eOp && eOp < ocStopUnaryOperators)
+        else if (mnPC >= 1 && isUnaryOperatorOpCode(eOp))
         {
             // Similar for unary operators.
             if (mpCode[-1]->GetInForceArray() != ParamClass::Unknown || IsMatrixFunction(mpCode[-1]->GetOpCode()))
--- sc/source/core/tool/compiler.cxx
+++ sc/source/core/tool/compiler.cxx
@@ -3267,11 +3267,9 @@ bool ScCompiler::ParseOpCode( const OUString& rName, bool bInArray )
     OpCode eOp = maRawToken.GetOpCode();
     if (eOp == ocSub || eOp == ocNegSub)
     {
-        bool bShouldBeNegSub =
-            (meLastOp == ocOpen || meLastOp == ocSep || meLastOp == ocNegSub ||
-             (ocStartBinaryOperators <= meLastOp && meLastOp < ocStopBinaryOperators) ||
-             meLastOp == ocArrayOpen ||
-             meLastOp == ocArrayColSep || meLastOp == ocArrayRowSep);
+        bool bShouldBeNegSub = (meLastOp == ocOpen || meLastOp == ocSep || meLastOp == ocNegSub
+                                || isBinaryOperatorOpCode(meLastOp) || meLastOp == ocArrayOpen
+                                || meLastOp == ocArrayColSep || meLastOp == ocArrayRowSep);
         if (bShouldBeNegSub && eOp == ocSub)
             maRawToken.NewOpCode( ocNegSub );
             //TODO: if ocNegSub had ForceArray we'd have to set it here
@@ -6181,8 +6179,7 @@ bool ScCompiler::GetRefColRowNames(const FormulaToken* pToken, ScComplexRefData&
             if (eOp1 != ocColRowName && eOp1 != ocIntersect && eOp2 != ocColRowName
                 && eOp2 != ocIntersect)
             {
-                if ((ocStartBinaryOperators <= eOp1 && eOp1 < ocStopBinaryOperators)
-                    || (ocStartBinaryOperators <= eOp2 && eOp2 < ocStopBinaryOperators))
+                if (isBinaryOperatorOpCode(eOp1) || isBinaryOperatorOpCode(eOp2))
                     bSingle = true;
             }
             if (bSingle)
@@ -6959,9 +6956,8 @@ bool ScCompiler::HandleIIOpCodeInternal(FormulaToken* token, FormulaToken*** ppp
         mPendingImplicitIntersectionOptimizations.emplace_back( pppToken[0], token );
         return true;
     }
-    else if ((nOpCode >= ocStartBinaryOperators && nOpCode < ocStopBinaryOperators
-                && nOpCode != ocAnd && nOpCode != ocOr)
-              || nOpCode == ocRound || nOpCode == ocRoundUp || nOpCode == ocRoundDown)
+    else if ((isBinaryOperatorOpCode(nOpCode) && nOpCode != ocAnd && nOpCode != ocOr)
+             || nOpCode == ocRound || nOpCode == ocRoundUp || nOpCode == ocRoundDown)
     {
         if (nNumParams != 2)
             return false;
@@ -6978,8 +6974,7 @@ bool ScCompiler::HandleIIOpCodeInternal(FormulaToken* token, FormulaToken*** ppp
             mPendingImplicitIntersectionOptimizations.emplace_back( pppToken[1], token );
         return true;
     }
-    else if ((nOpCode >= ocStartUnaryOperators && nOpCode < ocStopUnaryOperators)
-              || nOpCode == ocPercentSign)
+    else if (isUnaryOperatorOpCode(nOpCode) || nOpCode == ocPercentSign)
     {
         if (nNumParams != 1)
             return false;
