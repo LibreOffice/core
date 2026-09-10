fcbd7b9901d85d153fa89a7233cfaedb0ef3493e
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-09-10 11:00:51 +0000

    docx export: write each bound value only into its own customXml item
    every custom xml item was given the values of every data-bound control,
    so a document with two data stores came back with both controls showing
    the same value, and testTdf147724 failed in ci with:
    
     xmltesttools.cxx:214:Assertion
     Test name: testTdf147724::Load_Verify_Reload_Verify
     equality assertion failed
     - Expected: Placeholder -> *ABC*
     - Actual  : Placeholder -> *HERUNTERLADEN*
     - In <>, XPath contents of child does not match
    
    the second control's data binding names no store, so the import picks a
    store by std::unordered_map iteration order. That order differs from one
    builder to the next, and the test accepts either value for that control,
    so only some machines saw the failure.
    
    since:
    
    commit b5c616d10bff3213840d4893d13b4493de71fa56
    Date:   Wed Nov 24 14:50:12 2021 +0300
    
        tdf#104823: support for sdt plain text fields
    
    so keep the storeItemID of the data binding along with the value and
    only update the item whose ds:itemID matches it
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: I794ebb7c647ecddcd2cee3e90c25145ba4c32b97
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/12083
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    (cherry picked from commit b522379deae4c9df243d0493a3d242e80822c171)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210923
    Tested-by: Jenkins
    Tested-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210942
    Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210962

--- sw/source/filter/ww8/docxattributeoutput.cxx
+++ sw/source/filter/ww8/docxattributeoutput.cxx
@@ -2640,7 +2640,7 @@ void DocxAttributeOutput::WriteSdtPlainText(const OUString & sValue, const uno::
         }
 
         // Store databindings data for later writing to corresponding XMLs
-        OUString sPrefixMapping, sXpath;
+        OUString sPrefixMapping, sXpath, sStoreItemID;
         for (const auto& rProp : aGrabBagSdt)
         {
             if (rProp.Name == "ooxml:CT_SdtPr_dataBinding")
@@ -2653,6 +2653,8 @@ void DocxAttributeOutput::WriteSdtPlainText(const OUString & sValue, const uno::
                         sPrefixMapping = rDBProp.Value.get<OUString>();
                     else if (rDBProp.Name == "ooxml:CT_DataBinding_xpath")
                         sXpath = rDBProp.Value.get<OUString>();
+                    else if (rDBProp.Name == "ooxml:CT_DataBinding_storeItemID")
+                        sStoreItemID = rDBProp.Value.get<OUString>();
                 }
             }
         }
@@ -2660,7 +2662,7 @@ void DocxAttributeOutput::WriteSdtPlainText(const OUString & sValue, const uno::
         if (sXpath.getLength())
         {
             // Given xpath is sufficient
-            m_rExport.AddSdtData(sPrefixMapping, sXpath, sValue);
+            m_rExport.AddSdtData(sPrefixMapping, sXpath, sStoreItemID, sValue);
         }
     }
 
@@ -2880,7 +2882,8 @@ void DocxAttributeOutput::WriteContentControlStart()
                 0
             };
             aSnippet = comphelper::string::removeAny(aSnippet, aForbidden);
-            m_rExport.AddSdtData(rPrefixMapping, rXpath, aSnippet);
+            m_rExport.AddSdtData(rPrefixMapping, rXpath,
+                                 m_pContentControl->GetDataBindingStoreItemID(), aSnippet);
         }
     }
 
