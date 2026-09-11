e24992e235fc98d6d758a290e685a8f1ac5bbfe4
bittin1ddc447d824349b2 <bittin@reimu.nl>
2026-09-11 12:50:59 +0200

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/sv/
    Signed-off-by: bittin1ddc447d824349b2 <bittin@reimu.nl>
    Change-Id: I562d7d03e7f978fc90bde131866cca2d66fb93c8

--- qt/translations/coda_sv.ts
+++ qt/translations/coda_sv.ts
@@ -152,22 +152,22 @@
     <message>
         <location filename="../Bridge.cpp" line="353"/>
         <source>Export Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Exportfel</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="354"/>
         <source>Failed to export the document.</source>
-        <translation type="unfinished"></translation>
+        <translation>Misslyckades att exportera dokumentet.</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="939"/>
         <source>Cannot open remote document</source>
-        <translation type="unfinished"></translation>
+        <translation>Kan inte öppna fjärrdokument</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="942"/>
         <source>the Collabora Online server</source>
-        <translation type="unfinished"></translation>
+        <translation>Collabora Online-servern</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="1129"/>
@@ -199,12 +199,12 @@
     <message>
         <location filename="../DBusService.cpp" line="86"/>
         <source>Template Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Mall fel</translation>
     </message>
     <message>
         <location filename="../DBusService.cpp" line="87"/>
         <source>Could not create a new document from this template.</source>
-        <translation type="unfinished"></translation>
+        <translation>Kunde inte skapa ett nytt dokument utifrån den här mallen.</translation>
     </message>
 </context>
 <context>
