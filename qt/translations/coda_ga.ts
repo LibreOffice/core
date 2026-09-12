71af2d5051cf953350a298a26414d1ec98d75b02
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2026-09-12 13:11:24 +0200

    Translated using Weblate (Irish)
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/ga/
    Signed-off-by: Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
    Change-Id: I129da1cd9a5fe78333e1448f850d0ce6810d79ea

--- qt/translations/coda_ga.ts
+++ qt/translations/coda_ga.ts
@@ -152,22 +152,22 @@
     <message>
         <location filename="../Bridge.cpp" line="353"/>
         <source>Export Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Earráid Easpórtála</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="354"/>
         <source>Failed to export the document.</source>
-        <translation type="unfinished"></translation>
+        <translation>Theip ar onnmhairiú na ndoiciméad.</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="939"/>
         <source>Cannot open remote document</source>
-        <translation type="unfinished"></translation>
+        <translation>Ní féidir an doiciméad cianda a oscailt</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="942"/>
         <source>the Collabora Online server</source>
-        <translation type="unfinished"></translation>
+        <translation>freastalaí Collabora Online</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="1129"/>
@@ -199,12 +199,12 @@
     <message>
         <location filename="../DBusService.cpp" line="86"/>
         <source>Template Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Earráid Teimpléid</translation>
     </message>
     <message>
         <location filename="../DBusService.cpp" line="87"/>
         <source>Could not create a new document from this template.</source>
-        <translation type="unfinished"></translation>
+        <translation>Níorbh fhéidir doiciméad nua a chruthú ón teimpléad seo.</translation>
     </message>
 </context>
 <context>
