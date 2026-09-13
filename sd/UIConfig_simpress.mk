c8ad160b73dafb5ef33fc6da9709542f3a1aaae5
Heiko Tietze <heiko.tietze@documentfoundation.org>
2026-09-13 13:53:17 +0200

    Show the section context menu
    Change-Id: I0e0fb6271f2a0c060b3eed9df42a744b7c7e50c4
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210685
    Tested-by: Jenkins
    Reviewed-by: Heiko Tietze <heiko.tietze@documentfoundation.org>

--- sd/UIConfig_simpress.mk
+++ sd/UIConfig_simpress.mk
@@ -40,6 +40,7 @@ $(eval $(call gb_UIConfig_add_popupmenufiles,modules/simpress,\
 	sd/uiconfig/simpress/popupmenu/pagepanecanvasmaster \
 	sd/uiconfig/simpress/popupmenu/pagepanenoselmaster \
 	sd/uiconfig/simpress/popupmenu/pagepanenosel \
+	sd/uiconfig/simpress/popupmenu/sectionheader \
 	sd/uiconfig/simpress/popupmenu/pagepane \
 	sd/uiconfig/simpress/popupmenu/pagepanecanvas \
 	sd/uiconfig/simpress/popupmenu/pagetab \
