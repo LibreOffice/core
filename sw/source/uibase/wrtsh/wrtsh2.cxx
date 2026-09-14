6a83b1538cd1a83862972ad64d14bf27b494d087
DrRobotto <andreas.heinisch@yahoo.de>
2026-09-14 14:19:38 +0200

    tdf#138347 - Do not open a new window on internal services
    As a reference see the same treatment for internal service links in
    ScGlobal::OpenUrl.
    
    Change-Id: I8d18b4bcc0f100548af8c10a3ffb3c1ae7f958da
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/210949
    Reviewed-by: Andreas Heinisch <andreas.heinisch@yahoo.de>
    Tested-by: Jenkins

--- sw/source/uibase/wrtsh/wrtsh2.cxx
+++ sw/source/uibase/wrtsh/wrtsh2.cxx
@@ -638,7 +638,13 @@ static bool LoadURL(const SfxViewShell& rView, const OUString& rURL, LoadUrlFlag
     //#39076# Silent can be removed accordingly to SFX.
     SfxBoolItem aBrowse( SID_BROWSE, true );
 
-    if ((nFilter & LoadUrlFlags::NewView) && !comphelper::LibreOfficeKit::isActive())
+    // tdf#138347 - do not open a new window on internal services
+    const bool bDispatchOnly
+        = rURL.startsWithIgnoreAsciiCase("vnd.sun.star.script:")
+          || rURL.startsWithIgnoreAsciiCase("macro:") || rURL.startsWithIgnoreAsciiCase("slot:")
+          || rURL.startsWithIgnoreAsciiCase("service:") || rURL.startsWithIgnoreAsciiCase(".uno:");
+    if ((nFilter & LoadUrlFlags::NewView) && !comphelper::LibreOfficeKit::isActive()
+        && !bDispatchOnly)
         aTargetFrameName.SetValue( u"_blank"_ustr );
 
     const SfxPoolItem* aArr[] = {
