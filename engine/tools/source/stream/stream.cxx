bbb65e562218787fcc6527aa1e3683017f962eca
Caolán McNamara <caolan.mcnamara@collabora.com>
2026-09-15 22:30:29 +0000

    ReadLine and ReadUniStringLine: stop at the limit in the loop
    the two have drifted apart, so check the limit as each character is
    read instead of correcting the count afterwards
    
    Signed-off-by: Caolán McNamara <caolan.mcnamara@collabora.com>
    Change-Id: Idad62990843d58dfa13a69667295285ba4c7227f
    Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/12715
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
    Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

--- engine/tools/source/stream/stream.cxx
+++ engine/tools/source/stream/stream.cxx
@@ -434,6 +434,11 @@ bool SvStream::ReadLine( OStringBuffer& aBuf, sal_Int32 nMaxBytesToRead )
         sal_uInt16 j, n;
         for( j = n = 0; j < nLen ; ++j )
         {
+            if (nTotalLen + j >= o3tl::make_unsigned(nMaxBytesToRead))
+            {
+                bEnd = true;
+                break;
+            }
             c = buf[j];
             if ( c == '\n' || c == '\r' )
             {
@@ -445,12 +450,6 @@ bool SvStream::ReadLine( OStringBuffer& aBuf, sal_Int32 nMaxBytesToRead )
             ++n;
         }
         nTotalLen += j;
-        if (nTotalLen > o3tl::make_unsigned(nMaxBytesToRead))
-        {
-            n -= nTotalLen - nMaxBytesToRead;
-            nTotalLen = nMaxBytesToRead;
-            bEnd = true;
-        }
         if ( n )
             aBuf.append(buf, n);
     }
@@ -510,6 +509,11 @@ bool SvStream::ReadUniStringLine( OUString& rStr, sal_Int32 nMaxCodepointsToRead
         sal_uInt16 j, n;
         for( j = n = 0; j < nLen ; ++j )
         {
+            if (nTotalLen + j >= o3tl::make_unsigned(nMaxCodepointsToRead))
+            {
+                bEnd = true;
+                break;
+            }
             if (m_isSwap)
                 SwapNumber( buf[n] );
             c = buf[j];
@@ -530,12 +534,6 @@ bool SvStream::ReadUniStringLine( OUString& rStr, sal_Int32 nMaxCodepointsToRead
             }
         }
         nTotalLen += j;
-        if (nTotalLen > o3tl::make_unsigned(nMaxCodepointsToRead))
-        {
-            n -= nTotalLen - nMaxCodepointsToRead;
-            nTotalLen = nMaxCodepointsToRead;
-            bEnd = true;
-        }
         if ( n )
             aBuf.append( buf, n );
     }
