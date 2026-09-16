e063763be4543e0b5bc2e17ae63afed3d6d908bf
Ihor Hordiichuk <igor_ck@outlook.com>
2026-09-16 23:50:39 +0200

    Translated using Weblate (Ukrainian)
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/uk/
    Signed-off-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Change-Id: Ic0888d554949147b463881c680cbf301800b2142

--- qt/translations/coda_uk.ts
+++ qt/translations/coda_uk.ts
@@ -152,22 +152,22 @@
     <message>
         <location filename="../Bridge.cpp" line="353"/>
         <source>Export Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Помилка експорту</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="354"/>
         <source>Failed to export the document.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося експортувати документ.</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="939"/>
         <source>Cannot open remote document</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося відкрити віддалений документ</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="942"/>
         <source>the Collabora Online server</source>
-        <translation type="unfinished"></translation>
+        <translation>Сервер Collabora Online</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="1129"/>
@@ -199,12 +199,12 @@
     <message>
         <location filename="../DBusService.cpp" line="86"/>
         <source>Template Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Помилка шаблону</translation>
     </message>
     <message>
         <location filename="../DBusService.cpp" line="87"/>
         <source>Could not create a new document from this template.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося створити новий документ з цього шаблону.</translation>
     </message>
 </context>
 <context>
