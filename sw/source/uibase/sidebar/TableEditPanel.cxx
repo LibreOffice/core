LibreOffice/core

commit 2568245da54325fc21beff7f3ca05799980e89e4

lok: use locale units in dialogs and table sidebar
this patch updates fix for the dialog which were not fixed by e2708fecb34fb4084f8db5f2c5ffb4d8923002ca
part 3

Change-Id: Icae1254a6a6825ee7b4c78a28c9b8d328b7de4a3
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185694
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Caolán McNamara <caolan.mcnamara@collabora.com>
(cherry picked from commit 1cdff82bfcbf9192ad6e477763e49d7fe3b7de3a)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/185799
Tested-by: Jenkins

sw/source/uibase/sidebar/TableEditPanel.cxx
@@ -188,7 +188,7 @@ void TableEditPanel::InitRowHeightToolitem()
     Link<weld::MetricSpinButton&, void> aLink = LINK(this, TableEditPanel, RowHeightMofiyHdl);
     m_aRowHeightEdit.connect_value_changed(aLink);
 
-    FieldUnit eFieldUnit = SwModule::get()->GetUsrPref(false)->GetMetric();
+    FieldUnit eFieldUnit = SwModule::get()->GetFieldUnit();
     m_aRowHeightEdit.SetFieldUnit(eFieldUnit);
 
     m_aRowHeightEdit.set_min(MINLAY, FieldUnit::TWIP);
@@ -202,7 +202,7 @@ void TableEditPanel::InitColumnWidthToolitem()
     Link<weld::MetricSpinButton&, void> aLink = LINK(this, TableEditPanel, ColumnWidthMofiyHdl);
     m_aColumnWidthEdit.connect_value_changed(aLink);
 
-    FieldUnit eFieldUnit = SwModule::get()->GetUsrPref(false)->GetMetric();
+    FieldUnit eFieldUnit = SwModule::get()->GetFieldUnit();
     m_aColumnWidthEdit.SetFieldUnit(eFieldUnit);
 
     m_aColumnWidthEdit.set_min(MINLAY, FieldUnit::TWIP);
@@ -229,7 +229,7 @@ void TableEditPanel::InitAlignmentControls()
     m_xAlignment->append(OUString::number(text::HoriOrientation::NONE),
                          SwResId(STR_TABLE_PANEL_ALIGN_MANUAL));
 
-    FieldUnit eFieldUnit = SwModule::get()->GetUsrPref(false)->GetMetric();
+    FieldUnit eFieldUnit = SwModule::get()->GetFieldUnit();
     m_aLeftSpacingEdit.SetFieldUnit(eFieldUnit);
     m_aRightSpacingEdit.SetFieldUnit(eFieldUnit);
 }

commit e954521c03640102c0aeee0b52db02c6c2b4b1f3

tdf#173541: Units are expressed in different units in Navigator in Properties
Regression from 2568245da54325fc21beff7f3ca05799980e89e4
lok: use locale units in dialogs and table sidebar

this patch updates fix for the dialog which were not fixed by e2708fecb34fb4084f8db5f2c5ffb4d8923002ca
part 3

Change-Id: I649a4a89980534a73920e2a8df53e6f47cf8d228
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/211235
Tested-by: Jenkins
Reviewed-by: Julien Nabet <serval2412@yahoo.fr>

sw/source/uibase/sidebar/TableEditPanel.cxx
@@ -18,6 +18,7 @@
 #include <sfx2/dispatch.hxx>
 #include <svx/dlgctrl.hxx>
 #include <swmodule.hxx>
+#include <uitool.hxx>
 #include <usrpref.hxx>
 #include <comphelper/lok.hxx>
 #include <vcl/weld/Builder.hxx>
@@ -189,7 +190,7 @@ void TableEditPanel::InitRowHeightToolitem()
     Link<weld::MetricSpinButton&, void> aLink = LINK(this, TableEditPanel, RowHeightMofiyHdl);
     m_aRowHeightEdit.connect_value_changed(aLink);
 
-    FieldUnit eFieldUnit = SwModule::get()->GetFieldUnit();
+    FieldUnit eFieldUnit = ::GetDfltMetric(false);
     m_aRowHeightEdit.SetFieldUnit(eFieldUnit);
 
     m_aRowHeightEdit.set_min(MINLAY, FieldUnit::TWIP);
@@ -203,7 +204,7 @@ void TableEditPanel::InitColumnWidthToolitem()
     Link<weld::MetricSpinButton&, void> aLink = LINK(this, TableEditPanel, ColumnWidthMofiyHdl);
     m_aColumnWidthEdit.connect_value_changed(aLink);
 
-    FieldUnit eFieldUnit = SwModule::get()->GetFieldUnit();
+    FieldUnit eFieldUnit = ::GetDfltMetric(false);
     m_aColumnWidthEdit.SetFieldUnit(eFieldUnit);
 
     m_aColumnWidthEdit.set_min(MINLAY, FieldUnit::TWIP);
@@ -230,7 +231,7 @@ void TableEditPanel::InitAlignmentControls()
     m_xAlignment->append(OUString::number(text::HoriOrientation::NONE),
                          SwResId(STR_TABLE_PANEL_ALIGN_MANUAL));
 
-    FieldUnit eFieldUnit = SwModule::get()->GetFieldUnit();
+    FieldUnit eFieldUnit = ::GetDfltMetric(false);
     m_aLeftSpacingEdit.SetFieldUnit(eFieldUnit);
     m_aRightSpacingEdit.SetFieldUnit(eFieldUnit);
 }