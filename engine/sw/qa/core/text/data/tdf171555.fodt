LibreOffice/core

commit 367ef7c1dca300b1966093de5bbfca8ea97d403a

tdf#167946: reimplement the fix for tdf#120677
Commit 4bb28ad217ea9d6511b6921dcd3d28328edcb4d6 (tdf#120677: restore
treatment of blanks in SwTextGuess::Guess, 2018-11-12) made NBSPs
behave like ordinary spaces for line breaking purposes. But that is
wrong; NBSPs are explicitly defined in UAX #14 to behave differently.

It turns out, that the problem in tdf#120677 was caused by specifics
of handling of SwBlankPortion, which was defined as descendant of
SwExpandPortion (since initial import). SwExpandPortion::Format uses
special logic to change the passed SwTextFormatInfo using SwTextSlot,
to allow formatting text that is different from what was initially
passed to SwTextFormatInfo. This logic isn't needed for blanks, and
happens to confuse the normal line-breaking algorithm to return zero
for blank portions with NBSP. SwExpandPortion::Paint method, also
previously used by SwBlankPortion, doesn't seem to include any logic
specifically useful for blanks as well, and respective methods of
SwTextPortion can be used instead.

This change reverts the functional change of the previous fix for
tdf#120677 (the unit test is kept), and makes SwBlankPortion inherit
from SwTextPortion, which fixes both tdf#120677 and tdf#167946.

Change-Id: I9109069093a7f58a997a93723f81137d1a6bd7f8
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198882
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>
Tested-by: Jenkins
Signed-off-by: Xisco Fauli <xiscofauli@libreoffice.org>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/198954

sw/source/core/text/porexp.cxx
@@ -195,7 +195,7 @@ void SwBlankPortion::FormatEOL( SwTextFormatInfo &rInf )
  */
 bool SwBlankPortion::Format( SwTextFormatInfo &rInf )
 {
-    const bool bFull = rInf.IsUnderflow() || SwExpandPortion::Format( rInf );
+    const bool bFull = rInf.IsUnderflow() || SwTextPortion::Format(rInf);
     if( bFull && MayUnderflow( rInf, rInf.GetIdx(), rInf.IsUnderflow() ) )
     {
         Truncate();
@@ -211,7 +211,7 @@ void SwBlankPortion::Paint( const SwTextPaintInfo &rInf ) const
     // Draw field shade (can be disabled individually)
     if (!m_bMulti) // No gray background for multiportion brackets
         rInf.DrawViewOpt(*this, PortionType::Blank);
-    SwExpandPortion::Paint(rInf);
+    SwTextPortion::Paint(rInf);
 
     if (rInf.GetOpt().IsViewMetaChars() && rInf.GetOpt().IsHardBlank())
     {

sw/source/core/text/porexp.hxx
@@ -39,7 +39,7 @@ public:
 };
 
 /// Non-breaking space or non-breaking hyphen.
-class SwBlankPortion : public SwExpandPortion
+class SwBlankPortion final : public SwTextPortion
 {
     sal_Unicode m_cChar;
     bool m_bMulti;        // For multiportion brackets

sw/qa/extras/layout/data/nbsp-moved-to-new-line.fodt
@@ -1,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" office:version="1.4" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:font-face-decls>
+  <style:font-face style:name="Liberation Serif" svg:font-family="&apos;Liberation Serif&apos;" style:font-family-generic="roman" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="paragraph">
+   <style:paragraph-properties style:text-autospace="ideograph-alpha" style:punctuation-wrap="hanging" style:line-break="strict" style:writing-mode="page"/>
+   <style:text-properties style:font-name="Liberation Serif" fo:font-size="12pt" fo:hyphenate="false"/>
+  </style:default-style>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+ </office:styles>
+ <office:automatic-styles>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="210mm" fo:page-height="297mm" style:print-orientation="portrait" fo:margin-top="20mm" fo:margin-bottom="20mm" fo:margin-left="20mm" fo:margin-right="20mm" style:writing-mode="lr-tb"/>
+  </style:page-layout>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <!-- The first space after "elit." is a normal space; all the following up to comma are NBSPs -->
+   <text:p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.                                                                                 ,</text:p>
+  </office:text>
+ </office:body>
+</office:document>

sw/qa/extras/layout/layout6.cxx
@@ -2144,6 +2144,45 @@ CPPUNIT_TEST_FIXTURE(SwLayoutWriter6, testTdf170630)
     assertXPath(pXmlDoc, "//page[1]/body/txt[3]/anchored/fly", 2);
 }
 
+CPPUNIT_TEST_FIXTURE(SwLayoutWriter6, testTdf167946)
+{
+    // Given a document with a sequence of non-breaking spaces after a text and a space, which
+    // sequence doesn't fit the rest of the line. Before the fix, only the comma was moved to the
+    // second line, and all NBSPs were elided as a hole portion at the end of the first line:
+    createSwDoc("nbsp-moved-to-new-line.fodt");
+
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    CPPUNIT_ASSERT(pXmlDoc);
+
+    // One page:
+    assertXPath(pXmlDoc, "//page", 1);
+
+    // One paragraph:
+    assertXPath(pXmlDoc, "//txt", 1);
+
+    // Two lines in the paragraph:
+    assertXPath(pXmlDoc, "//txt/SwParaPortion/SwLineLayout", 2);
+    assertXPath(pXmlDoc, "//SwLineLayout", 2);
+
+    // First line consists of a text portion with, and a hole portion for the following space:
+    assertXPath(pXmlDoc, "//SwLineLayout[1]/child::*[1]", "type", u"PortionType::Text");
+    assertXPath(pXmlDoc, "//SwLineLayout[1]/child::*[1]", "portion",
+                u"Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+    assertXPath(pXmlDoc, "//SwLineLayout[1]/child::*[2]", "type", u"PortionType::Hole");
+    assertXPath(pXmlDoc, "//SwLineLayout[1]/child::*[2]", "portion", u" ");
+
+    // Second line has blank portions for the leading NBSPs, followed by a text portion with comma:
+    assertXPathChildren(pXmlDoc, "//SwLineLayout[2]", 81);
+    for (int i = 1; i <= 80; ++i)
+    {
+        OString aXPath = "//SwLineLayout[2]/child::*[" + OString::number(i) + "]";
+        assertXPath(pXmlDoc, aXPath, "type", u"PortionType::Blank");
+        assertXPath(pXmlDoc, aXPath, "portion", u"\xA0");
+    }
+    assertXPath(pXmlDoc, "//SwLineLayout[2]/child::*[81]", "type", u"PortionType::Text");
+    assertXPath(pXmlDoc, "//SwLineLayout[2]/child::*[81]", "portion", u",");
+}
+
 } // end of anonymous namespace
 
 CPPUNIT_PLUGIN_IMPLEMENT();

sw/source/core/text/guess.cxx
@@ -43,7 +43,8 @@ using namespace ::com::sun::star::linguistic2;
 
 namespace{
 
-bool IsBlank(sal_Unicode ch) { return ch == CH_BLANK || ch == CH_FULL_BLANK || ch == CH_NB_SPACE || ch == CH_SIX_PER_EM; }
+// UAX #14: spaces from SP and BA classes (elided in the end of a line)
+bool IsBlank(sal_Unicode ch) { return ch == CH_BLANK || ch == CH_FULL_BLANK || ch == CH_SIX_PER_EM; }
 
 // Used when spaces should not be counted in layout
 // Returns adjusted cut position

commit e9ce5e4424deb3a28db630fdb540ccfac1f4c0a2

tdf#171555 sw: draw a non-breaking hyphen as a plain hyphen
Regression after commit 367ef7c1dca300b1966093de5bbfca8ea97d403a
(tdf#167946: reimplement the fix for tdf#120677, 2026-02-11). A
non-breaking hyphen was shown as an empty box on macOS, and on Windows
in a font other than the rest of the text. Inside a field it wasn't
painted at all, and the brackets around double-lined text came out as
the text they enclose.

SwTextFormatter::NewPortion builds the portion for U+2011 with a plain
'-'. The SwTextSlot in SwExpandPortion carried that character to the
font; dropping the base class sent U+2011 instead. Carlito, Caladea,
Arial and Courier New have no glyph for it, nor for the U+2010
HarfBuzz falls back to.

So declare the substitution in SwBlankPortion. A blank stands in for
one character; it doesn't expand to computed text. Format, measure and
paint all use that character where it differs from the model one; a
no-break space is shown as itself, so tdf#120677 and tdf#167946 stay
fixed. SwTextPortion::Paint splits in two for it, since the
decorations read the index that the swap resets, and a blank with no
length still has a width to paint.

Signed-off-by: Mike Kaganski <mike.kaganski@collabora.com>
Change-Id: I93af27f8647e7e04014734af7100372fa66018cb
Reviewed-on: https://gerrit.collaboraoffice.com/c/online/+/13203
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins CPCI <releng@collaboraoffice.com>

engine/sw/source/core/text/porexp.cxx
@@ -204,7 +204,16 @@ bool SwBlankPortion::Format( SwTextFormatInfo &rInf )
         oTextSlot.emplace(&rInf, this, true, false);
     }
 
-    const bool bFull = rInf.IsUnderflow() || SwTextPortion::Format(rInf);
+    bool bFull = rInf.IsUnderflow();
+    if (!bFull)
+    {
+        // format the character that will be painted
+        std::optional<SwTextSlot> oShownChar;
+        if (!oTextSlot && ShowsOtherChar(rInf))
+            oShownChar.emplace(&rInf, this, true, false);
+        bFull = SwTextPortion::Format(rInf);
+    }
+
     if( bFull && MayUnderflow( rInf, rInf.GetIdx(), rInf.IsUnderflow() ) )
     {
         Truncate();
@@ -215,12 +224,38 @@ bool SwBlankPortion::Format( SwTextFormatInfo &rInf )
     return bFull;
 }
 
+bool SwBlankPortion::ShowsOtherChar(const SwTextSizeInfo& rInf) const
+{
+    // a bracket of SwDoubleLinePortion and a field's hook char stand for no model character
+    return m_bMulti || !GetLen() || m_cChar != rInf.GetChar(rInf.GetIdx());
+}
+
+SwPositiveSize SwBlankPortion::GetTextSize(const SwTextSizeInfo& rInf) const
+{
+    std::optional<SwTextSlot> oShownChar;
+    if (ShowsOtherChar(rInf))
+        oShownChar.emplace(&rInf, this, true, false);
+    return SwTextPortion::GetTextSize(rInf);
+}
+
 void SwBlankPortion::Paint( const SwTextPaintInfo &rInf ) const
 {
     // Draw field shade (can be disabled individually)
     if (!m_bMulti) // No gray background for multiportion brackets
         rInf.DrawViewOpt(*this, PortionType::Blank);
-    SwTextPortion::Paint(rInf);
+
+    // a field's hook char has no length, but a width and a character to draw
+    if (GetLen() || Width())
+    {
+        PaintDecorations(rInf);
+
+        std::optional<SwTextSlot> oShownChar;
+        if (ShowsOtherChar(rInf))
+            oShownChar.emplace(&rInf, this, true, true);
+
+        // the slot remaps the other two lists, but not the wrong one
+        PaintText(rInf, !oShownChar && rInf.GetpWrongList() != nullptr);
+    }
 
     if (rInf.GetOpt().IsViewMetaChars() && rInf.GetOpt().IsHardBlank())
     {

engine/sw/source/core/text/porexp.hxx
@@ -43,6 +43,10 @@ class SwBlankPortion final : public SwTextPortion
 {
     sal_Unicode m_cChar;
     bool m_bMulti;        // For multiportion brackets
+
+    /// Whether the shown character differs from the model one: a hyphen shows a plain '-'
+    bool ShowsOtherChar(const SwTextSizeInfo& rInf) const;
+
 public:
     SwBlankPortion( sal_Unicode cCh, bool bMult = false )
         : m_cChar( cCh ), m_bMulti( bMult )
@@ -52,6 +56,7 @@ public:
     virtual bool GetExpText( const SwTextSizeInfo &rInf, OUString &rText ) const override;
     virtual void FormatEOL( SwTextFormatInfo &rInf ) override;
     virtual bool Format( SwTextFormatInfo &rInf ) override;
+    virtual SwPositiveSize GetTextSize(const SwTextSizeInfo& rInf) const override;
     virtual void Paint( const SwTextPaintInfo &rInf ) const override;
     static sal_uInt16 MayUnderflow(const SwTextFormatInfo &rInf, TextFrameIndex nIdx,
         bool bUnderflow );

engine/sw/source/core/text/portxt.cxx
@@ -1094,31 +1094,35 @@ SwPositiveSize SwTextPortion::GetTextSize( const SwTextSizeInfo &rInf ) const
     return aSize;
 }
 
+void SwTextPortion::PaintDecorations(const SwTextPaintInfo& rInf) const
+{
+    rInf.DrawBackBrush(*this);
+    rInf.DrawBorder(*this);
+
+    rInf.DrawCSDFHighlighting(*this);
+
+    // do we have to repaint a post it portion?
+    if (rInf.OnWin() && mpNextPortion && !mpNextPortion->Width())
+        mpNextPortion->PrePaint(rInf, this);
+}
+
+void SwTextPortion::PaintText(const SwTextPaintInfo& rInf, const bool bWrong) const
+{
+    const bool bGrammarCheck = rInf.GetGrammarCheckList() != nullptr;
+    const bool bSmartTags = rInf.GetSmartTags() != nullptr;
+
+    if (bWrong || bSmartTags || bGrammarCheck)
+        rInf.DrawMarkedText(*this, rInf.GetLen(), bWrong, bSmartTags, bGrammarCheck);
+    else
+        rInf.DrawText(*this, rInf.GetLen());
+}
+
 void SwTextPortion::Paint( const SwTextPaintInfo &rInf ) const
 {
     if( GetLen() )
     {
-        rInf.DrawBackBrush( *this );
-        rInf.DrawBorder( *this );
-
-        rInf.DrawCSDFHighlighting(*this);
-
-        // do we have to repaint a post it portion?
-        if( rInf.OnWin() && mpNextPortion && !mpNextPortion->Width() )
-            mpNextPortion->PrePaint( rInf, this );
-
-        auto const* pWrongList = rInf.GetpWrongList();
-        auto const* pGrammarCheckList = rInf.GetGrammarCheckList();
-        auto const* pSmarttags = rInf.GetSmartTags();
-
-        const bool bWrong = nullptr != pWrongList;
-        const bool bGrammarCheck = nullptr != pGrammarCheckList;
-        const bool bSmartTags = nullptr != pSmarttags;
-
-        if ( bWrong || bSmartTags || bGrammarCheck )
-            rInf.DrawMarkedText( *this, rInf.GetLen(), bWrong, bSmartTags, bGrammarCheck );
-        else
-            rInf.DrawText( *this, rInf.GetLen() );
+        PaintDecorations(rInf);
+        PaintText(rInf, rInf.GetpWrongList() != nullptr);
     }
 }
 

engine/sw/source/core/text/portxt.hxx
@@ -32,6 +32,12 @@ class SwTextPortion : public SwLinePortion
             const sal_Int32 nSpaces, const sal_Int16 nWidthOf10Spaces );
     bool Format_( SwTextFormatInfo &rInf );
 
+protected:
+    /// Paints what surrounds the text; reads the index, so never under a SwTextSlot
+    void PaintDecorations(const SwTextPaintInfo& rInf) const;
+    /// Paints the text; pass false under a SwTextSlot, which does not remap the wrong list
+    void PaintText(const SwTextPaintInfo& rInf, bool bWrong) const;
+
 public:
     SwTextPortion(){ SetWhichPor( PortionType::Text ); }
     static SwTextPortion * CopyLinePortion(const SwLinePortion &rPortion);

engine/sw/qa/core/text/data/tdf171555.fodt
@@ -1,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" office:version="1.4" office:mimetype="application/vnd.oasis.opendocument.text">
+ <office:meta><meta:user-defined meta:name="NBH">a&#x2011;b</meta:user-defined></office:meta>
+ <office:font-face-decls>
+  <style:font-face style:name="Carlito" svg:font-family="&apos;Carlito&apos;" style:font-family-generic="swiss" style:font-pitch="variable"/>
+ </office:font-face-decls>
+ <office:styles>
+  <style:default-style style:family="paragraph">
+   <style:text-properties style:font-name="Carlito" fo:font-size="24pt" fo:language="en" fo:country="US"/>
+  </style:default-style>
+  <style:style style:name="Standard" style:family="paragraph" style:class="text"/>
+ </office:styles>
+ <office:automatic-styles>
+  <style:style style:name="TwoLines" style:family="text">
+   <style:text-properties style:text-combine="lines" style:text-combine-start-char="(" style:text-combine-end-char=")"/>
+  </style:style>
+  <style:style style:name="Narrow" style:family="paragraph" style:parent-style-name="Standard">
+   <style:paragraph-properties fo:margin-right="16.3cm"/>
+  </style:style>
+  <style:page-layout style:name="pm1">
+   <style:page-layout-properties fo:page-width="210mm" fo:page-height="297mm" fo:margin-top="2cm" fo:margin-bottom="2cm" fo:margin-left="2cm" fo:margin-right="2cm" style:writing-mode="lr-tb"/>
+  </style:page-layout>
+ </office:automatic-styles>
+ <office:master-styles>
+  <style:master-page style:name="Standard" style:page-layout-name="pm1"/>
+ </office:master-styles>
+ <office:body>
+  <office:text>
+   <text:p text:style-name="Standard">&#x2011;</text:p>
+   <text:p text:style-name="Standard">-</text:p>
+   <text:p text:style-name="Standard"><text:user-defined text:name="NBH"/></text:p>
+   <text:p text:style-name="Narrow">mm&#x2011;nn</text:p>
+   <text:p text:style-name="Standard">a&#xA0;b</text:p>
+   <text:p text:style-name="Standard">X<text:span text:style-name="TwoLines">(1)</text:span>Y</text:p>
+  </office:text>
+ </office:body>
+</office:document>

engine/sw/qa/core/text/text.cxx
@@ -1858,6 +1858,41 @@ CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf136663_Korean_No_Extra_Spacing)
                 "//body/txt[4]/SwParaPortion/SwLineLayout/child::*[@type='PortionType::Kern']", 0);
 }
 
+CPPUNIT_TEST_FIXTURE(SwCoreTextTest, testTdf171555NonBreakingHyphen)
+{
+    // Given paragraphs holding a non-breaking hyphen, on its own and inside a field, in a font
+    // that has no glyph for U+2011:
+    createSwDoc("tdf171555.fodt");
+
+    // When painting them:
+    std::shared_ptr<GDIMetaFile> xMetaFile = getSwDocShell()->GetPreviewMetaFile();
+    CPPUNIT_ASSERT(xMetaFile);
+
+    OUStringBuffer aPainted;
+    for (size_t nAction = 0; nAction < xMetaFile->GetActionSize(); ++nAction)
+    {
+        MetaAction* pAction = xMetaFile->GetAction(nAction);
+        if (pAction->GetType() != MetaActionType::TEXTARRAY)
+            continue;
+
+        auto pTextArray = static_cast<MetaTextArrayAction*>(pAction);
+        aPainted.append(
+            pTextArray->GetText().subView(pTextArray->GetIndex(), pTextArray->GetLen()));
+    }
+
+    // Then the plain hyphen the portion stands for reaches the font, not U+2011 itself, which
+    // Carlito has no glyph for; inside the field the hyphen was not painted at all. A no-break
+    // space stands for itself and still reaches the font as U+00A0. Joining the runs keeps this
+    // independent of where the portions and the narrow column's lines split, while still
+    // catching a character painted twice:
+    CPPUNIT_ASSERT_EQUAL(u"--a-bmm-nna\u00A0bX((1))Y"_ustr, aPainted.makeStringAndClear());
+
+    // ... and the hyphen is as wide as the one the second paragraph spells out directly
+    xmlDocUniquePtr pXmlDoc = parseLayoutDump();
+    CPPUNIT_ASSERT_EQUAL(getXPath(pXmlDoc, "//txt[2]/SwParaPortion/SwLineLayout", "width"),
+                         getXPath(pXmlDoc, "//txt[1]/SwParaPortion/SwLineLayout", "width"));
+}
+
 CPPUNIT_PLUGIN_IMPLEMENT();
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */