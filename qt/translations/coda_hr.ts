c48fcef4d533ecd8e3722f637aee3b3b789500c6
Milo Ivir <mail@milotype.de>
2026-09-22 13:33:05 +0200

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (41 of 41 strings)
    
    Translation: Collabora Online/Collabora Office Qt app
    Translate-URL: https://hosted.weblate.org/projects/collabora-online/collabora-office-qt-app/hr/
    Signed-off-by: Milo Ivir <mail@milotype.de>
    Change-Id: Ic98ae446afe3ee527b5ab8f4d3fa3886a0eb5751

--- qt/translations/coda_hr.ts
+++ qt/translations/coda_hr.ts
@@ -152,22 +152,22 @@
     <message>
         <location filename="../Bridge.cpp" line="353"/>
         <source>Export Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Greška u izvozu</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="354"/>
         <source>Failed to export the document.</source>
-        <translation type="unfinished"></translation>
+        <translation>Izvoz dokumenta nije uspio.</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="939"/>
         <source>Cannot open remote document</source>
-        <translation type="unfinished"></translation>
+        <translation>Nije moguće otvoriti udaljeni dokument</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="942"/>
         <source>the Collabora Online server</source>
-        <translation type="unfinished"></translation>
+        <translation>Collabora Online server</translation>
     </message>
     <message>
         <location filename="../Bridge.cpp" line="1129"/>
@@ -199,12 +199,12 @@
     <message>
         <location filename="../DBusService.cpp" line="86"/>
         <source>Template Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Greška u predlošku</translation>
     </message>
     <message>
         <location filename="../DBusService.cpp" line="87"/>
         <source>Could not create a new document from this template.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nije moguće stvoriti novi dokument iz ovog predloška.</translation>
     </message>
 </context>
 <context>
